45be63b1e53844fb2c7d98d95ab44d907840b9f3
Sara OC <saramoo@hotmail.com>
2015-03-20 14:23:12 +0000

    add feature scenario for taking off and landing

--- spec/feature/grand_finale_spec.rb
+++ spec/feature/grand_finale_spec.rb
@@ -1,3 +1,4 @@
+require 'airport'
 require 'capybara/rspec'
 
 ## Note these are just some guidelines!
@@ -8,9 +9,12 @@ require 'capybara/rspec'
 # Check when all the planes have landed that they have status "landed"
 # Once all planes are in the air again, check that they have status "flying!"
 
-feature 'Grand Finale' do
-
-  xscenario 'all planes can land and all planes can take off'
+feature 'for our airport to be safe' do
+  scenario 'all planes can land and all planes can take off' do
+    airport = Airport.new
+    plane = airport.clear_for_landing
+    expect(plane).not_to be_landed
+  end
   xscenario 'a plane cannot land when an airport is full'
   xscenario 'planes cannot land when the weather is stormy'
 
