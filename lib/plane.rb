3c025f99b3e752db02cbf99ee2d0421e0f45ae7e
Tomiblanchard <tomiblanchard@gmail.com>
2015-03-20 18:03:47 +0000

    define fly and land functions

--- lib/plane.rb
+++ lib/plane.rb
@@ -1,2 +1,16 @@
 class Plane
+  attr_reader :flying
+  alias_method :flying?, :flying
+
+  def initialize
+    @flying = false
+  end
+
+  def fly
+    @flying = true
+  end
+
+  def land
+    @flying = false
+  end
 end
