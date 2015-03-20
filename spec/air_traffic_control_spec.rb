c7c84761a601513d2acdd67198a710e766e72774
Richard Lynch <rjlynch86@googlemail.com>
2015-03-20 17:38:32 +0000

    adds test for prescence of take_off method

--- spec/air_traffic_control_spec.rb
+++ spec/air_traffic_control_spec.rb
@@ -7,7 +7,9 @@ require 'air_traffic_control'
 # should become "flying"
 # airtraffic control can put a plane in an airport
 describe AirTrafficControl do
+  it { is_expected.to respond_to :take_off }
   # it knows about planes in the air
-  it 'should be able to take off a plane' do
-  end
+  # it 'should be able to take off a plane' do
+  #  plane_ground = double('Plane', flying: false)
+  # end
 end
