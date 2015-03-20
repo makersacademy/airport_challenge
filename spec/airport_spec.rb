3c4cc68e1bb11c68fc73e4b61ef66953f775531e
Ed O'Brien <edmund.obrien.2010@live.rhul.ac.uk>
2015-03-20 18:53:34 +0000

    EVERYTHING WORKS

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -15,14 +15,16 @@ describe Airport do
   context 'taking off and landing' do
     context 'In Good Weather' do
     it 'a plane can land' do
+      plane = double :plane, has_landed: true
       allow(subject).to receive(:weather).and_return("Sunny")
-      expect(subject.land :plane).to eq [:plane]
+      expect(subject.land plane).to eq [plane]
     end
 
     it 'a plane can take off' do
+      plane = double :plane, has_landed: true, taken_off: false
       allow(subject).to receive(:weather).and_return("Sunny")
-      subject.land :plane
-      subject.take_off :plane
+      subject.land plane
+      subject.take_off plane
       expect(subject.plane_check).to eq []
     end
   end
@@ -30,9 +32,10 @@ describe Airport do
   context 'traffic control'
 
     it 'a plane cannot land if the airport is full' do
+      plane = double :plane, has_landed: true
       allow(subject).to receive(:weather).and_return("Sunny")
-      10.times { subject.land :plane }
-      expect { subject.land :plane }.to raise_error 'Airport is Full'
+      10.times { subject.land plane }
+      expect { subject.land plane }.to raise_error 'Airport is Full'
     end
 
     # Include a weather condition.
@@ -46,10 +49,11 @@ describe Airport do
 
   context 'weather conditions'
       it 'a plane cannot take off when there is a storm brewing' do
+        plane = double :plane, has_landed: true
         allow(subject).to receive(:weather).and_return("Sunny")
-        subject.land :plane
+        subject.land plane
         allow(subject).to receive(:weather).and_return("Stormy")
-        expect { subject.take_off :plane }.to raise_error 'Storm\'s a Brewin'
+        expect { subject.take_off plane }.to raise_error 'Storm\'s a Brewin'
       end
 
       it 'a plane cannot land in the middle of a storm' do
