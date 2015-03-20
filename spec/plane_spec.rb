28f436f2a042d904aaecb2a81baedf47806832d4
Richard Lynch <rjlynch86@googlemail.com>
2015-03-20 16:45:25 +0000

    adds a test to check for presence of take_off method

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -17,7 +17,7 @@ describe Plane do
   # 'has a flying status when created' do
   # which would be an attr_reader :status
   it { is_expected.to respond_to :flying }
-  # I'll start with new planes flying into our 'airspce'
+  # as planes aren't created in the airport they must start out flying
   it 'should be in the air when created' do
     # subject makes a new instance of plane
     expect(subject.flying).to be true
@@ -26,7 +26,20 @@ describe Plane do
   xit 'has a flying status when in the air'
 
   xit 'can take off'
+  # to take off it needs to not be flying
+  # it should have a take off method
+  it { is_expected.to respond_to :take_off }
+  # which can't be set if already airborne
+  # set the plane to not be flying
+  # subject.flying = false
+  # it needs to be launched from an airport
+
+  # it needs an airport to land at
   # check airport status
+  # so first put it in the airport
+  # then try and have it take off so it is in the air
+  # make a dummy airport to hold the plane
+  # the plane should know it has taken off
 
   xit 'changes its status to flying after taking off'
 
