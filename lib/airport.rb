b29ec49f469e33302aa81a09d617433aefd0b2d0
armi1189 <armi1189@gmail.com>
2015-03-21 14:59:54 +0000

    Airport can park planes

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,9 +1,14 @@
 require_relative 'control_tower'
 
 class Airport
-  attr_reader :control_tower
+  attr_reader :control_tower, :planes
 
   def initialize
     @control_tower = ControlTower.new
+    @planes = []
+  end
+
+  def park plane
+    planes << plane
   end
 end
