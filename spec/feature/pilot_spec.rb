c9e72aad2ce6bc246d2f6bf5bdf620bcb24d8d07
Mark Pokorny <iridium193@gmail.com>
2015-03-21 16:17:03 +0000

    Updated wording in tests.

--- spec/feature/pilot_spec.rb
+++ spec/feature/pilot_spec.rb
@@ -6,7 +6,7 @@ require 'plane'
 # I would like to land my plane at the appropriate airport
 
 feature 'plane should land at airport' do
-  scenario 'plane should land at airport' do
+  scenario 'plane should land at airport when told' do
     expect(Plane.new.land).to be
   end
 end
@@ -16,7 +16,7 @@ end
 # I would like to be able to take off from the appropriate airport
 
 feature 'plane should take-off from airport' do
-  scenario 'plane should take-off from airport' do
+  scenario 'plane should take-off from airport when told' do
     expect(Plane.new.take_off).to be
   end
 end
