b4b77bd3c4a3e0db7e27ff0c54ffa0e9c3557c8a
Stacca <caronstace@gmail.com>
2015-03-21 14:28:46 +0000

    update feature spec

--- spec/feature/end_feature_spec.rb
+++ spec/feature/end_feature_spec.rb
@@ -1,9 +1,13 @@
 require 'plane'
 require 'capybara/rspec'
 
-feature 'planes can land' do
+feature 'plane location' do
   scenario 'plane has landed in airport' do
     plane = Plane.new
     expect(plane.landed?).to eq true
+  end
+  scenario 'plane has taken off' do
+    plane = Plane.new
+    expect(plane.taken_off).to eq false
   end
 end
