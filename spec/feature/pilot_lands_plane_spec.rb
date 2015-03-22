20d494859d629d7822721fbad2e69235fe493db8
Jade King <dev@jadeking.co.uk>
2015-03-22 05:17:42 +0000

    add test pilot takes off

--- spec/feature/pilot_lands_plane_spec.rb
+++ spec/feature/pilot_lands_plane_spec.rb
@@ -3,9 +3,15 @@ feature 'Pilot accesses airport' do
   scenario 'Pilot lands at airport' do
     plane = Plane.new
     airport = Airport.new
-    # response = airport.request_landing(plane)
     plane = airport.land(plane)
-
-    expect(airport.planes).to be [plane]
+    expect(airport.planes).to be plane
   end
+  scenario 'Pilot takes off from airport' do
+    plane = Plane.new
+    airport = Airport.new
+    airport.land(plane)
+    plane = airport.take_off(plane)
+    expect(plane).to be plane
+  end
+
 end
