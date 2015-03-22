e971d30f18ce818f9adc4f59f04c9db3ebf6b923
James Miller <james.fl.miller@live.co.uk>
2015-03-22 18:50:57 +0000

    Challenge files updated

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,8 +1,5 @@
 require 'airport'
 
-## Note these are just some guidelines!
-## Feel free to write more tests!!
-
 # A plane currently in the airport can be requested to take off.
 #
 # No more planes can be added to the airport, if it's full.
@@ -11,18 +8,48 @@ require 'airport'
 #
 # If the airport is full then no planes can land
 
+AIRPORT_CAPACITY = 60
+
 describe Airport do
+
+  let(:airport) { Airport.new }
+  let(:plane) { double :plane, status: 'flying' }
+  let(:landed_plane) { double :plane, status: 'landed' }
+
+  before(:example) { allow(airport).to receive(:stormy?).and_return false }
 
   context 'taking off and landing' do
 
-    xit 'a plane can land'
+    it 'a plane can land' do
+      expect(plane).to receive(:land)
+      airport.land_plane plane
+    end
 
-    xit 'a plane can take off'
+    it 'a plane can take off' do
+      expect(landed_plane).to receive(:take_off)
+      airport.launch_plane landed_plane
+    end
   end
 
   context 'traffic control' do
 
-    xit 'a plane cannot land if the airport is full'
+    before(:example) { allow(plane).to receive :land }
+    before(:example) { allow(landed_plane).to receive :take_off }
+
+    it 'a plane cannot land if the airport is full' do
+      AIRPORT_CAPACITY.times { airport.land_plane plane }
+      expect { airport.land_plane plane }.to raise_error 'Airport full'
+    end
+
+    it 'knows how many landed planes it currently has' do
+      20.times { airport.land_plane plane }
+      expect(airport.plane_count).to eq 20
+    end
+
+    it 'knows if it is full' do
+      AIRPORT_CAPACITY.times { airport.land_plane plane }
+      expect(airport).to be_full
+    end
 
     # Include a weather condition.
     # The weather must be random and only have two states "sunny" or "stormy".
@@ -34,9 +61,19 @@ describe Airport do
     # the plane can not land, and must not be in the airport
 
     context 'weather conditions' do
-      xit 'a plane cannot take off when there is a storm brewing'
 
-      xit 'a plane cannot land in the middle of a storm'
+      before(:example) { allow(airport).to receive(:stormy?).and_return true }
+
+      it 'a plane cannot take off when there is a storm brewing' do
+        expect_err = 'Plane cannot take off in storm' # RuboCop insisted
+        expect { airport.launch_plane landed_plane }. to raise_error expect_err
+      end
+
+      it 'a plane cannot land in the middle of a storm' do
+        expect_err = 'Plane cannot land in storm' # RuboCop insisted (too long)
+        expect { airport.land_plane plane }.to raise_error expect_err
+      end
+
     end
   end
 end
