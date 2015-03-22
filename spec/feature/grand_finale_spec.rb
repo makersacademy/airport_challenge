17588148a4ae8ca8386d5d88711a729fcc411b94
Richard Lynch <rjlynch86@googlemail.com>
2015-03-22 10:10:48 +0000

    passes feature test for landing at a full airport

--- spec/feature/grand_finale_spec.rb
+++ spec/feature/grand_finale_spec.rb
@@ -18,14 +18,33 @@ end
 feature 'As a pilot' do
   plane = Plane.new
   airport = Airport.new
-  weather = Weather.new
-  airport.weather weather
   scenario 'I would like to land my plane at the appropriate airport' do
+    weather = double('Weather', weather: 'sunny')
+    airport.weather weather
     airport.land_plane plane
     expect(plane.flying).to be false
   end
   scenario 'I would like to take off from appropriate airport' do
+    weather = double('Weather', weather: 'sunny')
+    airport.weather weather
     airport.takeoff_plane
     expect(plane.flying).to be true
+  end
+end
+
+feature 'As an airtraffic controller' do
+  airport = Airport.new
+  scenario 'I want to prevent planes landing if airport is full' do
+    weather = double('Weather', weather: 'sunny')
+    airport.weather weather
+    planes = []
+    20.times do
+      plane = Plane.new
+      planes << plane
+      p planes
+    end
+    planes.each { |plane| airport.land_plane plane }
+    plane = Plane.new
+    expect { airport.land_plane plane }.to raise_error 'cannot land'
   end
 end
