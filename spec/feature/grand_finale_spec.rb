10bb4969331b11e9e0e47be831a5e01afb30d481
Sammy Abukmeil <sammyabukmeil@gmail.com>
2015-03-22 11:40:17 +0000

    add feature tests

--- spec/feature/grand_finale_spec.rb
+++ spec/feature/grand_finale_spec.rb
@@ -1,4 +1,5 @@
 require 'capybara/rspec'
+require 'plane'
 
 ## Note these are just some guidelines!
 ## Feel free to write more tests!!
@@ -8,8 +9,13 @@ require 'capybara/rspec'
 # Check when all the planes have landed that they have status "landed"
 # Once all planes are in the air again, check that they have status "flying!"
 
-feature 'Grand Finale' do
-
-  xscenario 'all planes can land and all planes can take off'
-
+feature 'plane status' do
+  scenario 'plane has landed' do
+    plane = Plane.new
+    expect(plane.landed?).to eq true
+  end
+  scenario 'plane has taken off' do
+    plane = Plane.new
+    expect(plane.taken_off).to eq false
+  end
 end
