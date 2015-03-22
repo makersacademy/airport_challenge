1b58ffe433b35d3a7e4d584395d8cad4f0b681b5
Joseph Knowles <joejknowles@gmail.com>
2015-03-22 21:04:23 +0000

    Respaced methods in land and take off specs

--- spec/feature/take_off_plane_spec.rb
+++ spec/feature/take_off_plane_spec.rb
@@ -3,6 +3,7 @@ require 'capybara/rspec'
 feature 'when a plane takes off' do
   let(:airport) { Airport.new }
   let(:plane) { Plane.new }
+
   scenario 'it changes its status to flying at take off' do
     allow(airport).to receive(:bad_weather?).and_return(false)
     airport.land plane
