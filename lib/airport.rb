638e084c1cb66302962af8144cf2215ec23ef374
M1lena <stoevami@gmail.com>
2015-03-23 01:45:21 +0000

    weekend challenge - changes

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,2 +1,34 @@
 class Airport
+
+  DEFAULT_CAPACITY = 100
+
+  attr_reader :planes, :weather
+
+  def initialize(options = {})
+    @capacity = options.fetch(:capacity, DEFAULT_CAPACITY)
+    @planes = []
+    @weather = rand(5)
+  end
+
+  def capacity
+    @capacity
+  end
+
+  def ok_landing(plane)
+    raise "Airport is full" if full?
+    @planes << plane #unless (@weather < 2 == true)
+  end
+
+  def has_planes?
+    !@planes.empty?
+  end
+
+  def full?
+    @planes.count == capacity
+  end
+
+  def ok_takeoff plane
+    @planes.delete plane #unless (@weather < 2 == true)
+  end
+
 end
--- lib/plane.rb
+++ lib/plane.rb
@@ -1,2 +1,50 @@
 class Plane
+
+  def initialize
+    in_air!
+  end
+
+  def flying?
+    @flying
+  end
+
+  def landed!
+    @flying = false
+  end
+
+  def in_air!
+    @flying = true
+  end
+
 end
+
+
+  # def take_off?
+  #   true
+  # end
+
+  # def landing?
+  #   @landing
+  # end
+
+
+  # def on_ground #the plane is not flying
+  #   @on_ground
+  # end
+
+  # def flight_status
+  #   if in_flight?
+  #      flying
+  #   elsif plane.take_off?
+  #      taking_off
+  #   elsif plane.landing?
+  #      landing
+  #   elsif plane.on_ground?
+  #      on_ground
+  #   end
+  #  end
+
+  #def delay_landing
+  #end
+  # not sure if delayed landing should be initiated by plane or by airport
+  # even though this will be affected by weather
