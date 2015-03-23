13506e764ab2f7e787ea5517fdce740fcaafb1c6
Stacca <caronstace@gmail.com>
2015-03-23 22:05:51 +0000

    adding missed plan spec

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -3,4 +3,24 @@ require 'airport'
 
 describe Plane do
   let(:plane) { Plane.new }
+
+  it 'has flying status when created' do
+    expect(plane).to be_flying
+  end
+
+  it 'has a flying status when flying' do
+    expect(plane.status).to eq :flying
+  end
+
+  it 'can land' do
+    plane.land
+    plane.take_off
+    expect(plane.status).to eq :landed
+  end
+
+  it 'can take off' do
+    plane.land
+    plane.take_off
+    expect(plane.status).to eq :flying
+  end
 end
