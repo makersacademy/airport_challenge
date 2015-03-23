9ba98065ee65fac6210b285d44da7000091d6ea5
Nadav Rosenberggi <nadav.rosenberg@gmail.com>
2015-03-23 00:54:01 +0000

    adds all files and a new README file

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,2 +1,45 @@
+require_relative 'plane'
+
 class Airport
+  def initialize
+    @planes = []
+    @capacity = 20
+    @weather = %w(sunny stormy).sample
+  end
+
+  attr_reader :planes
+
+  attr_reader :capacity
+
+  attr_reader :weather
+
+  attr_writer :weather
+
+  def land plane
+    fail 'Bad Weather' if bad_weather?
+    fail 'Airport Full' if full?
+    planes << plane
+    plane.landed
+  end
+
+  def take_off
+    fail 'Bad Weather' if bad_weather?
+    fail 'Airport Empty' if empty?
+    planes.last.take_off
+    planes.pop
+  end
+
+  private
+
+  def full?
+    planes.length >= capacity
+  end
+
+  def empty?
+    planes.empty?
+  end
+
+  def bad_weather?
+    weather == 'stormy'
+  end
 end
--- lib/plane.rb
+++ lib/plane.rb
@@ -1,2 +1,15 @@
 class Plane
+  def initialize
+    @status = 'flying'
+  end
+
+  attr_reader :status
+
+  def landed
+    @status = 'landed'
+  end
+
+  def take_off
+    @status = 'flying'
+  end
 end
