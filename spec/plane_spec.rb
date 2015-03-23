662216731ffcc35b3c8ebd24fc37fa27bd9b3446
Kanishk Walia <kanishk.walia@gmail.com>
2015-03-23 00:46:00 +0000

    Added plane_spec.rb

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -1,25 +1,24 @@
 require 'plane'
 
-## Note these are just some guidelines!
-## Feel free to write more tests!!
-
-# When we create a new plane, it should have a "flying" status,
-# thus planes can not be created in the airport.
-#
-# When we land a plane at the airport, the plane in question should
-# have its status changed to "landed"
-#
-# When the plane takes of from the airport, the plane's status
-# should become "flying"
-
 describe Plane do
+	let(:plane) {Plane.new}
 
-  xit 'has a flying status when created'
+	it "can have a flying status when created" do
+		expect(plane).to be_flying
+	end
 
-  xit 'has a flying status when in the air'
+	it "can have a flying status when in the air" do
+		plane.takeoff!
+		expect(plane).to be_flying
+	end
 
-  xit 'can take off'
+	it "can takeoff" do
+		plane.takeoff!
+		expect(plane).to be_flying
+	end
 
-  xit 'changes its status to flying after taking off'
-
+	it "can land" do
+		plane.land!
+		expect(plane).not_to be_flying  
+	end
 end
