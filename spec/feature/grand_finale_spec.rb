c99c51975eee2dc119a360285d17bec3ba751f20
Tomiblanchard <tomiblanchard@gmail.com>
2015-03-23 02:23:35 +0000

    deleted comments

--- spec/feature/grand_finale_spec.rb
+++ spec/feature/grand_finale_spec.rb
@@ -1,13 +1,5 @@
 require 'capybara/rspec'
 
-## Note these are just some guidelines!
-## Feel free to write more tests!!
-
-# Given 6 planes, each plane must land.
-# Be careful of the weather, it could be stormy!
-# Check when all the planes have landed that they have status "landed"
-# Once all planes are in the air again, check that they have status "flying!"
-
 feature 'Grand Finale' do
   let(:airport) { Airport.new }
   let(:plane) { Plane.new }
@@ -17,7 +9,7 @@ feature 'Grand Finale' do
     airport = Airport.new
     all_planes = Array.new(6, Plane.new)
     allow(airport).to receive(:weather){:'!sunny'}
-    
+
     for plane in @all_plane do airport.landing_order(plane)
     for plane in @all_plane do expect(plane.flying).to eq false
     for plane in @all_plane do airport.takeoff_order(plane)
