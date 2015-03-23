1fe9ff3e4dca03ea468c65191e522701af1a489a
maxlweaver <maxlweaver@gmail.com>
2015-03-23 08:36:30 +0000

    Fixed doubles issue

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -3,14 +3,13 @@ require 'airport'
 describe Airport do
   let(:airport) { Airport.new }
   let(:plane) { double(:plane) }
-  let(:weather?) { double(:weather?,)}
-  # let(:parked_plane) { double(:parked_plane, status: 'land') }
   it 'has a default capacity' do
     expect(airport.capacity).to eq 200
   end
 
   it 'cannot land planes when capacity is reached' do
-    allow(airport).to receive(:weather).and_return 'sunny'
+    allow(airport).to receive(:weather?).and_return 'sunny'
+    allow(plane).to receive(:land)
     allow(plane).to receive(:status)
     200.times { airport.land(plane) }
     expect { airport.land(plane)}.to raise_error
@@ -18,16 +17,16 @@ describe Airport do
   end
 
    it 'cannot land a plane if already landed' do
-    # ßallow(plane).to receive(land)
-    allow(airport).to receive(:weather).and_return 'sunny'
+    allow(plane).to receive(land)
+    # allow(airport).to receive(:weather).and_return 'sunny'
     airport.land(plane)
     expect { airport.land(plane) }.to raise_error
     'You cannot land a plane that\'s already landed'
   end
 
-  # it 'cannot land a plane if the weather is stormy' do
-  #   allow(airport).to receive(:weather).and_return 'stormy'
-  #   expect { airport.land(plane) }.to raise_error
-  #   'Adverse weather. Please stand by, over...'
-  # end
+  it 'cannot land a plane if the weather is stormy' do
+    allow(airport).to receive(:weather?).and_return 'stormy'
+    expect { airport.land(plane) }.to raise_error
+    'Adverse weather. Please stand by, over...'
+  end
 end
