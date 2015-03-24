6aabdefa3cb4403a057d6abecadd6b7dce270d9e
domaro <diasomarov90@gmail.com>
2015-03-24 09:27:36 +0000

    1st draft

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,2 +1,22 @@
+require_relative 'plane'
+
 class Airport
+  attr_reader :planes
+
+  def initialize
+    @capacity = 6
+    @planes = []
+  end
+
+  def land_plane(plane)
+    fail 'No Space' if @planes.count >= @capacity
+    fail 'Bad Weather' if bad_weather?
+    plane.land!
+    @planes << plane
+  end
+
+  def plane_fly(plane)
+    fail 'No Planes' if @planes.empty?
+    plane
+  end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,42 +1,36 @@
 require 'airport'
 
-## Note these are just some guidelines!
-## Feel free to write more tests!!
-
-# A plane currently in the airport can be requested to take off.
-#
-# No more planes can be added to the airport, if it's full.
-# It is up to you how many planes can land in the airport
-# and how that is implemented.
-#
-# If the airport is full then no planes can land
-
 describe Airport do
+  let(:plane) { double :plane }
+  before do
+    allow(plane).to receive(:land!)
+    allow(plane).to receive(:fly!)
+    it { is_expected.to respond_to :land_plane }
+  end
 
-  context 'taking off and landing' do
+  context ' land and fly' do
+    it ' can fly in good weather' do
+      subject.land_plane(plane)
+      expect(subject.plane_fly(plane)).to eq plane
+    end
 
-    xit 'a plane can land'
-
-    xit 'a plane can take off'
+    it ' can land in good weather ' do
+      expect(subject.land_plane(plane)).to eq [plane]
+    end
   end
 
   context 'traffic control' do
+    it 'can return planes' do
+      expect(subject.planes).to eq []
+    end
 
-    xit 'a plane cannot land if the airport is full'
+    it 'plane can not land if the airport is full' do
+      6.times { subject.land_plane(plane) }
+      expect { subject.land_plane(plane) }.to raise_error 'No Space'
+    end
 
-    # Include a weather condition.
-    # The weather must be random and only have two states "sunny" or "stormy".
-    # Try and take off a plane, but if the weather is stormy,
-    # the plane can not take off and must remain in the airport.
-    #
-    # This will require stubbing to stop the random return of the weather.
-    # If the airport has a weather condition of stormy,
-    # the plane can not land, and must not be in the airport
-
-    context 'weather conditions' do
-      xit 'a plane cannot take off when there is a storm brewing'
-
-      xit 'a plane cannot land in the middle of a storm'
+    it 'plane can not go if there is no plane' do
+      expect { subject.plane_fly(plane) }.to raise_error 'No Planes'
     end
   end
 end
