ab016ec59f573d07f468f45c9b0c7ef4b9d4b9eb
Sammy Abukmeil <sammyabukmeil@gmail.com>
2015-04-11 23:56:30 +0100

    airport has a capacity and cannot take in planes once full

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,11 +1,13 @@
 class Airport
-  attr_reader :planes
+  attr_reader :planes, :capacity
 
   def initialize
     @planes = []
+    @capacity = 20
   end
 
   def land plane
+    fail 'Airport is full, cannot land.' if full?
     plane.land!
     planes << plane
   end
@@ -13,6 +15,10 @@ class Airport
   def take_off plane
     plane.take_off
     planes.delete plane
+  end
+
+  def full?
+    planes.count == capacity
   end
 
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,13 +1,17 @@
 require 'airport'
 
 describe Airport do
+  let(:airport) { Airport.new }
+  let(:plane) { double :plane }
 
   context 'taking off and landing' do
-    let(:airport) { Airport.new }
-    let(:plane) { double :plane }
+
+    before do
+      allow(plane).to receive :land!
+      allow(plane).to receive :take_off
+    end
 
     it 'can land a plane' do
-      allow(plane).to receive :land!
       airport.land(plane)
       expect(airport.planes).to eq [plane]
     end
@@ -18,24 +22,29 @@ describe Airport do
     end
 
     it 'can take off a plane' do
-      allow(plane).to receive :land!
       airport.land(plane)
-      allow(plane).to receive :take_off
       airport.take_off(plane)
       expect(airport.planes).to be_empty
     end
 
     it 'changes status of plane to flying when taking off a plane' do
-      allow(plane).to receive :land!
       airport.land plane
-      allow(plane).to receive :take_off
+      expect(plane).to receive :take_off
       airport.take_off plane
     end
   end
 
   context 'traffic control' do
 
-    xit 'a plane cannot land if the airport is full'
+    it 'can have a capacity' do
+      expect(airport.capacity).to eq 20
+    end
+
+    it 'a plane cannot land if the airport is full' do
+      allow(plane).to receive :land!
+      airport.capacity.times { airport.land plane }
+      expect { airport.land(plane) }.to raise_error 'Airport is full, cannot land.'
+    end
 
     context 'weather conditions' do
       xit 'a plane cannot take off when there is a storm brewing'
