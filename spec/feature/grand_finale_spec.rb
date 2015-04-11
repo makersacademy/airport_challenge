f07da6155106ced4bdeab36d7f853ea5975ec823
Sammy Abukmeil <sammyabukmeil@gmail.com>
2015-04-11 22:42:19 +0100

    initial commit

--- spec/feature/grand_finale_spec.rb
+++ spec/feature/grand_finale_spec.rb
@@ -1,13 +1,5 @@
 require 'capybara/rspec'
 
-## Note these are just some guidelines!
-## Feel free to write more tests!!
-
-# Given 6 planes, each plane must land.
-# Be careful of the weather, it could be stormy!
-# Check when all the planes have landed that they have status "landed"
-# Once all planes are in the air again, check that they have status "flying!"
-
 feature 'Grand Finale' do
 
   xscenario 'all planes can land and all planes can take off'
