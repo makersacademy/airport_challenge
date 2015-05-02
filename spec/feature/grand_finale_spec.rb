54b6c0b0dbd975a6bcb2fff3b25f0fed33aba844
Rocco <roccorighi@hotmail.com>
2015-05-02 18:03:28 +0100

    fixes hound error part 2

--- spec/feature/grand_finale_spec.rb
+++ spec/feature/grand_finale_spec.rb
@@ -8,39 +8,39 @@ require 'airport'
 # Once all planes are in the air again, check that they have status "flying!"
 
 feature 'Grand Finale' do
-	
+
 
 
 	feature 'the plane lands at the specified airport' do
 
-	  scenario 'at Luton' do
-			plane = Plane.new
-			luton = Airport.new
-			plane.land luton
-			expect(plane.location).to eq luton
-	  end
+    scenario 'at Luton' do
+  		plane = Plane.new
+  		luton = Airport.new
+  		plane.land luton
+  		expect(plane.location).to eq luton
+    end
 
-	  scenario 'plane has landed' do
-	  	plane = Plane.new
-	  	plane.land Airport.new
-	  	expect(plane).to be_landed
-	  end
-	end
-	feature 'the plane takes off from the specified airport' do 
-		scenario 'at Heathrow' do
-			plane = Plane.new
-			heatrhow = Airport.new
-			plane.take_off heatrhow
-			expect(plane.location).to eq heatrhow
-		end
-	end
+    scenario 'plane has landed' do
+    	plane = Plane.new
+    	plane.land Airport.new
+    	expect(plane).to be_landed
+    end
+  end
+  	feature 'the plane takes off from the specified airport' do
+  		scenario 'at Heathrow' do
+  			plane = Plane.new
+  			heatrhow = Airport.new
+  			plane.take_off heatrhow
+  			expect(plane.location).to eq heatrhow
+  		end
+  	end
 
-	scenario 'plane takes off' do
-		plane = Plane.new
-		plane.take_off Airport.new
-		expect(plane).not_to be_landed
-	end
-end
+  	scenario 'plane takes off' do
+  		plane = Plane.new
+  		plane.take_off Airport.new
+  		expect(plane).not_to be_landed
+  	end
+  end
 
 
 
