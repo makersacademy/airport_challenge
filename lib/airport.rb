36eab6ada3517e43952ab427ae7018a5c4cebbf8
Makers Laptop 66 <makerslaptop66@Makerss-MacBook-Pro-4.local>
2015-05-03 19:22:47 +0100

    code now reflects status of plane

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,11 +1,13 @@
 class Airport
 
+  MAX_CAPACITY = 6
+
   def initialize
     @planes = []
   end
 
   def accept_plane plane
-    fail "Airport is full" if @planes.length == 6
+    fail "Airport is full" if @planes.length == MAX_CAPACITY
     @planes << plane
   end
 
--- lib/plane.rb
+++ lib/plane.rb
@@ -2,12 +2,20 @@ require 'airport.rb'
 
 class Plane
 
+  def initialize
+    @status = "flying"
+  end
+
+  attr_accessor 'status'
+
   def land airport
     @airport = airport
+    @status = "landed"
     @airport
   end
 
   def take_off airport
+    @status = "flying"
     @airport = airport
   end
 
