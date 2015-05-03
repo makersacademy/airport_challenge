4ce46c8d17bd0afcf78ecbc2d2e5b75d0dda23f7
Joanne <joannezanetton@gmail.com>
2015-05-03 15:54:20 +0100

    adding feature 1st attempt

--- spec/feature/grand_finale_spec.rb
+++ spec/feature/grand_finale_spec.rb
@@ -1,15 +1,17 @@
 require 'capybara/rspec'
+require 'plane'
+require 'airport'
 
-## Note these are just some guidelines!
-## Feel free to write more tests!!
+feature 'Pilot can land his plane at airport'do
+  scenario 'airport accepts request to land flying plane and lands plane'do
+    airport = Airport.new
+    plane = airport.land
+    expect(plane).to be_flying
+  end
+end
+
 
 # Given 6 planes, each plane must land.
 # Be careful of the weather, it could be stormy!
 # Check when all the planes have landed that they have status "landed"
 # Once all planes are in the air again, check that they have status "flying!"
-
-feature 'Grand Finale' do
-
-  xscenario 'all planes can land and all planes can take off'
-
-end
