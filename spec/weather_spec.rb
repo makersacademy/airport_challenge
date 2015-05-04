02e108a36351b74a8307671d28729b166c938521
MollieS <mollie.c.stephenson@gmail.com>
2015-05-04 10:19:24 +0100

    updated weather unit tests for extra coverage

--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -1,6 +1,23 @@
+require 'weather'
+
 describe Weather do
-  it 'can be stormy' do
-    allow(subject).to receive(:stormy?) { true }
-    expect(subject).to be_stormy
+  let(:airport) { double :airport }
+
+  context 'weather status' do
+
+    it 'airport responds to stormy' do
+      allow(airport).to receive(:stormy?) { true }
+      expect(airport).to respond_to :stormy?
+    end
+
+    it 'can be stormy' do
+      allow(airport).to receive(:stormy?) { true }
+      expect(airport).to be_stormy
+    end
+
+    it 'can be sunny' do
+      allow(airport).to receive(:sunny?) { true }
+      expect(airport).to be_sunny
+    end
   end
 end
