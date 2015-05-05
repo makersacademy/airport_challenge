81477972187f49ca45429d2188be8624bb01d5e6
Dan Bolger <bowedcymbal@gmail.com>
2015-05-05 01:41:06 +0100

    hound errors begone?

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,16 +1,6 @@
 require 'airport'
 require 'plane'
 
-## Note these are just some guidelines!
-## Feel free to write more tests!!
-
-# A plane currently in the airport can be requested to take off.
-#
-# No more planes can be added to the airport, if it's full.
-# It is up to you how many planes can land in the airport
-# and how that is implemented.
-
-
 describe Airport do
   context 'Permission to land' do
     it { is_expected.to respond_to :allow_landing }
@@ -29,13 +19,13 @@ describe Airport do
   end
 
   # context 'traffic control' do
-  #   xit 'a plane cannot land if the airport is full'
+  #   it 'a plane cannot land if the airport is full'
 
   #   context 'weather conditions' do
-  #     xit 'a plane cannot take off when there is a storm brewing'
+  #     it 'a plane cannot take off when there is a storm brewing'
   #     expect { subject.allow_landing aeroplane }.to raise_error 'It\'s too stormy to fly!'
 
-  #     xit 'a plane cannot land in the middle of a storm'
+  #     it 'a plane cannot land in the middle of a storm'
   #     expect { subject.allow_takeoff aeroplane }.to raise_error 'It\'s too stormy to land!'
   # #   end
   # end
