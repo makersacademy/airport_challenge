e5b6a8414eda1039f366416834da1c4a87acd04d
Gerard Morera <grdmorera@gmail.com>
2015-06-13 19:38:53 +0100

    added specs and lib.. for flight weather air_trafic_controller

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,48 +1,48 @@
-require 'airport'
+# require 'airport'
 
-## Note these are just some guidelines!
-## Feel free to write more tests!!
+# ## Note these are just some guidelines!
+# ## Feel free to write more tests!!
 
-# A plane currently in the airport can be requested to take off.
-#
-# No more planes can be added to the airport, if it's full.
-# It is up to you how many planes can land in the airport
-# and how that is implemented.
-#
-# If the airport is full then no planes can land
+# # A plane currently in the airport can be requested to take off.
+# #
+# # No more planes can be added to the airport, if it's full.
+# # It is up to you how many planes can land in the airport
+# # and how that is implemented.
+# #
+# # If the airport is full then no planes can land
 
-describe Airport do
+# describe Airport do
 
-  describe 'take off' do
-    xit 'instructs a plane to take off'
+#   describe 'take off' do
+#     xit 'instructs a plane to take off'
 
-    xit 'releases a plane'
-  end
+#     xit 'releases a plane'
+#   end
 
-  describe 'landing' do
-    xit 'instructs a plane to land'
+#   describe 'landing' do
+#     xit 'instructs a plane to land'
 
-    xit 'receives a plane'
-  end
+#     xit 'receives a plane'
+#   end
 
-  describe 'traffic control' do
-    context 'when airport is full' do
-      xit 'does not allow a plane to land'
-    end
+#   describe 'traffic control' do
+#     context 'when airport is full' do
+#       xit 'does not allow a plane to land'
+#     end
 
-    # Include a weather condition.
-    # The weather must be random and only have two states "sunny" or "stormy".
-    # Try and take off a plane, but if the weather is stormy,
-    # the plane can not take off and must remain in the airport.
-    #
-    # This will require stubbing to stop the random return of the weather.
-    # If the airport has a weather condition of stormy,
-    # the plane can not land, and must not be in the airport
+#     # Include a weather condition.
+#     # The weather must be random and only have two states "sunny" or "stormy".
+#     # Try and take off a plane, but if the weather is stormy,
+#     # the plane can not take off and must remain in the airport.
+#     #
+#     # This will require stubbing to stop the random return of the weather.
+#     # If the airport has a weather condition of stormy,
+#     # the plane can not land, and must not be in the airport
 
-    context 'when weather conditions are stormy' do
-      xit 'does not allow a plane to take off'
+#     context 'when weather conditions are stormy' do
+#       xit 'does not allow a plane to take off'
 
-      xit 'does not allow a plane to land'
-    end
-  end
-end
+#       xit 'does not allow a plane to land'
+#     end
+#   end
+# end
---
+++ spec/flight.rb
@@ -0,0 +1,5 @@
+require 'flight'
+
+describe Flight do
+
+end
