41cde932606845520ebeaa2acd8f98f41ea8c83d
Nicola Hughes <nicolarhughes@gmail.com>
2015-06-13 19:28:24 +0100

    Ensuring airport can instruct a plane to take off

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,2 +1,7 @@
 class Airport
+
+  def take_off plane
+    plane.take_off
+  end
+
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -14,7 +14,9 @@ require 'airport'
 describe Airport do
 
   describe 'take off' do
-    xit 'instructs a plane to take off'
+    it 'instructs a plane to take off' do
+      expect(subject).to respond_to(:take_off).with(1).argument
+    end
 
     xit 'releases a plane'
   end
