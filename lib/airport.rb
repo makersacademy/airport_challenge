c605dcd26b82a18018ceb5e71a6a6b8f62ea37ef
MassimilianoMura <massimilianomura1983@gmail.com>
2015-06-13 22:56:48 +0100

    made check_the_weather automatic

--- lib/airport.rb
+++ lib/airport.rb
@@ -21,17 +21,20 @@ class Airport
 
 
   def instruct_plane
+    check_the_weather
     return print "\n\n*****   Route assigned, weather is sunny, plane authorized to take off   *****\n\n".upcase if @weather
     return print "\n\n*****   WARNING!! Weather is stormy, plane is not authorized to take off   *****\n\n".upcase if !@weather
   end
 
   def release_plane
+    check_the_weather
     fail "\n\n*****   WARNING!! Weather is stormy, plane is not authorized to take off  *****\n\n".upcase if !@weather
     fail "\n\n*****   Airport is empty   *****\n\n".upcase if empty?
     @planes.pop
   end
 
   def landing plane
+    check_the_weather
     fail "\n\n*****   Airport is full   *****\n\n".upcase if full?
     fail "\n\n*****   WARNING!! Weather is stormy, plane is not authorized to landing  *****\n\n".upcase if !@weather
     planes << plane
