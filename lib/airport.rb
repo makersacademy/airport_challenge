3fdd4d52024915e791330e18f8f2ab9670293fe9
Christian Miller <cwgmiller@gmail.com>
2015-06-14 19:51:14 +0100

    Plane tests passing

--- lib/airport.rb
+++ lib/airport.rb
@@ -3,32 +3,32 @@ require 'plane'
 class Airport
 	DEFAULT_CAPACITY = 20
 
-attr_reader :capacity 
-attr_reader :landed_planes
+	attr_reader :capacity 
+	attr_reader :landed_planes
 
-def initialize 
- @capacity = DEFAULT_CAPACITY
- @landed_planes = []	
-end
+	def initialize 
+	 @capacity = DEFAULT_CAPACITY
+	 @landed_planes = []	
+	end
 
-def land plane
-	fail "Airport is full" if full?
-	landed_planes << plane
-end
+	def land plane
+		fail "Airport is full" if full?
+		landed_planes << plane
+	end
 
-def take_off plane
-	fail 'No planes to take off' if empty?
-	landed_planes.delete plane 
-end
+	def take_off plane
+		fail 'No planes to take off' if empty?
+		landed_planes.delete plane 
+	end
 
-end
+	end
 
-private
+	private
 
-def full?
-	landed_planes.count >= capacity
-end
+	def full?
+		landed_planes.count >= capacity
+	end
 
-def empty?
-	landed_planes == []
+	def empty?
+		landed_planes == []
 end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -22,12 +22,32 @@ describe Plane do
   end
 
 
-  xit 'can land'
+  it 'can land' do 
+  	expect(subject).to respond_to :land
+  end
 
-  xit 'is landed after landing'
+  it 'cannot land if already landed' do 
+  	subject.land 
+  	expect{subject.land}.to raise_error 'Plane has already landed'
+  end
 
-  xit 'can take off'
 
-  xit 'is flying after take off'
+  it 'is landed after landing' do 
+  	subject.land
+  	expect(subject).to be_landed
+  end
+
+  it 'can take off' do 
+  	expect(subject).to respond_to :take_off
+  end
+
+  it 'cannot take off if already flying' do 
+  	expect{subject.take_off}.to raise_error 'Plane cannot take off when flying'
+  end
+
+  it 'is flying after take off' do 
+  	subject.land
+  	expect(subject.take_off).to be_flying
+  end
 
 end
