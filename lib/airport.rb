fca2b08b440601e81fd77c4c8b6b26e2ef669690
MassimilianoMura <massimilianomura1983@gmail.com>
2015-06-14 15:00:50 +0100

    generel updates

--- lib/airport.rb
+++ lib/airport.rb
@@ -3,7 +3,6 @@ require_relative 'plane'
 class Airport
 
   DEFAULT_CAPACITY = 5
-  # DEFAULT_WEATHER = true
 
   attr_accessor :capacity
 
@@ -21,17 +20,16 @@ class Airport
 
 
   def instruct_plane
-    # check_the_weather
-    return print "\n\n*****   Route assigned, plane authorized to take off   *****\n\n".upcase if @weather
-    # return print "\n\n*****   WARNING!! it's stormy, plane is not authorized to take off   *****\n\n".upcase if !@weather
+    return print "\n\n*****   Route assigned   *****\n\n"
   end
 
-  def release_plane
+  def release_plane plane
     check_the_weather
-    fail "\n\n*****   WARNING!! Weather it's stormy, plane is not authorized to take off  *****\n\n".upcase if !@weather
+    fail "\n\n*****   WARNING!! It's stormy, plane is not authorized to take off  *****\n\n".upcase if !@weather
     fail "\n\n*****   Airport is empty   *****\n\n".upcase if empty?
     @planes.pop
-    print "\n\n*****   Plane tooke off   *****\n\n".upcase
+    print self.instruct_plane
+    print "\n\n*****   Plane taken off   *****\n\n".upcase
     plane.flying
 
   end
@@ -39,7 +37,7 @@ class Airport
   def landing plane
     check_the_weather
     fail "\n\n*****   Airport is full   *****\n\n".upcase if full?
-    fail "\n\n*****   WARNING!! Weather it's stormy, plane is not authorized to landing  *****\n\n".upcase if !@weather
+    fail "\n\n*****   WARNING!! It's stormy, plane is not authorized to landing  *****\n\n".upcase if !@weather
     planes << plane
     print "\n\n*****   Plane landed   *****\n\n".upcase
     plane.landed
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -1,18 +1,5 @@
 require 'plane'
 
-## Note these are just some guidelines!
-## Feel free to write more tests!!
-
-# When we create a new plane, it should be "flying",
-# thus planes can not be created in the airport.
-#
-# When we land a plane at the airport, the plane in question should
-# be "landed"
-#
-# When the plane takes of from the airport, it should be "flying" again
-#
-# Think about your implementation - does it allow a plane to be "flying" and landed?
-# Are you testing that?
 
 describe Plane do
 
@@ -48,17 +35,5 @@ describe Plane do
     plane.flying
     expect(@landed==false)
   end
-
-
-
-  # xit 'is flying when created'
-
-  # xit 'can land'
-
-  # xit 'is landed after landing'
-
-  # xit 'can take off'
-
-  # xit 'is flying after take off'
 
 end
