54d6fbb6ab18a01c0f14343c73b51990d7c57752
Makers Laptop 86 <faisal.choura@gmail.com>
2015-06-14 13:29:17 +0100

    added fail conditions in take off and landing to check weather, and new attr_reader for weather

--- lib/airport.rb
+++ lib/airport.rb
@@ -2,28 +2,25 @@ class Airport
 DEFAULT_CAPACITY = 50
 
   attr_accessor :capacity
+  attr_reader :weather
 
   def initialize
     @planes = []
     @capacity = DEFAULT_CAPACITY
+    @weather = [:sunny,:sunny,:sunny,:stormy].sample
   end
 
   def land_plane plane
     fail 'Airport is full' if @planes.count >= capacity
+    fail 'stormy weather cannot land' if weather == :stormy
     plane.land
     @planes << plane
   end
 
   def take_off_plane plane
     fail 'No planes available' if @planes.empty?
-    fail 'Stormy weather cannot take off' if weather == :stormy
+    fail 'stormy weather cannot take off' if weather == :stormy
     plane.take_off
     @planes.delete plane
   end
-
-
-  def weather
-    [:sunny,:sunny,:sunny,:stormy].sample
-  end
-
 end
