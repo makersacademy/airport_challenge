9984082b274390231ab98b4555decb50734730a5
Natalia Antonova <5555482@gmail.com>
2015-06-14 16:57:03 +0100

    Done plane

--- lib/plane.rb
+++ lib/plane.rb
@@ -1,2 +1,28 @@
 class Plane
+	FLYING_STATE='flying'
+	LANDED_STATE='landed'
+
+	attr_reader :state
+
+	def initialize
+		@state = FLYING_STATE
+	end
+
+	def land
+		fail 'Plane is already landed' if landed?
+		@state = LANDED_STATE
+	end
+
+	def take_off
+		fail 'Plane is already flying' if flying?
+		@state = FLYING_STATE
+	end
+
+	def flying?
+		@state == FLYING_STATE
+	end
+
+	def landed?
+		@state == LANDED_STATE
+	end
 end
