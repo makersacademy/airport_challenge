f37797edb76b21b249c860a58a372eaa18aab349
Jonathan Sayer <jon.sayer1@gmail.com>
2015-06-14 18:36:53 +0100

    Update weather_spec.rb

--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -16,18 +16,18 @@ describe Weather do
 	end
 
 	it 'Stormy return false when generator is less than 80' do 
-		 allow(subject).to receive(:generator){50}
+		 allow(subject).to receive(:generator) {50}
 		expect(subject.Stormy?).to eq false
 	end
 
 
 	it'Sunny? must return false when generator is over 80' do 
-		allow(subject).to receive(:generator){90}
+		allow(subject).to receive(:generator) {90}
 		expect(subject.Sunny?).to eq false
 	end
 
 	it 'Stormy? must return true when generator is over 80' do 
-		allow(subject).to receive(:generator){90}
+		allow(subject).to receive(:generator) {90}
 		expect(subject.Stormy?).to eq true
 	end
 
