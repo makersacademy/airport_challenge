1cf565f08a17e6c52d0fda5c8d516bd7b127b1b2
Jack Hall-Tipping <jack.halltipping@gmail.com>
2015-06-14 23:11:33 +0100

    added attr accessor for plane class

--- lib/plane.rb
+++ lib/plane.rb
@@ -1,25 +1,28 @@
 class Plane
 
+  attr_accessor :flying
+  attr_accessor :landed
+
   def initialize
     @flying = true
     @landed = false
   end
 
   def flying?
-    @flying
+    flying
   end
 
   def landed?
-    @landed
+    landed
   end
 
   def landing
-    @flying = false
-    @landed = true
+    self.flying = false
+    self.landed = true
   end
 
   def take_off
-    @flying = true
-    @landed = false
+    self.flying = true
+    self.landed = false
   end
 end
