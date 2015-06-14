d89f8f2472c9ffe85e1242bb215fc3bf9dc186bc
Elin <elinnet@gmail.com>
2015-06-14 22:33:31 +0100

    Add weather_type method and land method to airport.rb

--- lib/airport.rb
+++ lib/airport.rb
@@ -4,8 +4,17 @@ class Airport
 
 def initialize
   @weather = rand(10)
+end
+
+def weather_type
+  @weather <= 8? "sunny" : "stormy"
+end
+
+def land plane
 
 end
+
+
 
 
 
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -3,8 +3,28 @@ require 'airport'
 
 describe Airport do
 
-	it {is_expected.to respond_to :weather}
+    it {is_expected.to respond_to :weather}
 
+  describe 'weather' do
+    it 'sets sunny/stormy weather' do
+      expect(subject).to respond_to :weather_type
+    end
+
+    it 'tests sunny weather' do
+      allow(subject).to receive(:weather){7}
+      expect(subject.weather_type).to eq "sunny"
+    end
+
+  end
+
+  describe 'landing' do
+    it 'responds to land method' do
+      expect(subject).to respond_to :land
+    end
+
+    it{is_expected.to respond_to(:land).with(1).argument}
+    
+  end
 
 end
 
