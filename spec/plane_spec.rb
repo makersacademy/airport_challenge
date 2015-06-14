00454786069ab77004b458e016c5635cd584fd26
Elin <elinnet@gmail.com>
2015-06-15 00:16:31 +0100

    Files to date

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -2,7 +2,18 @@ require 'plane'
 
 
 describe Plane do
+  it 'plane expected to be flying when created' do
+    expect(subject).to respond_to :flying
+  end
 
+  it 'plane approved to can land?' do
+    expect(subject).to respond_to :landing_approved?
+  end
+
+  it 'when landed, plane not flying' do
+    expect(subject).to_not eq :flying
+  end   
+end   
   # xit 'is flying when created'
 
   # xit 'can land'
@@ -13,4 +24,3 @@ describe Plane do
 
   # xit 'is flying after take off'
 
-end
