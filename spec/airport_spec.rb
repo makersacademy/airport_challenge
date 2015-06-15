fd2712a4fb59ee78da202953419acb1182f113c8
Michael Arnott <arnottmj@aol.com>
2015-06-15 21:25:20 +0100

    rspec re-write using doubles

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -9,11 +9,15 @@ describe Airport do
   end
 
   describe 'take off' do
-    let(:plane) {Plane.new}
+    let(:plane) {double :plane}
+    before do
+      allow(subject).to receive(:weather) {'sunny'}
+      allow(plane).to receive(:landing)
+      allow(plane).to receive(:take_off)
+    end
+
     it 'a plane should be able to take off when the weather is sunny' do
-      expect(subject).to receive(:weather) {'sunny'}
       subject.landing(plane)
-      expect(subject).to receive(:weather) {'sunny'}
       subject.take_off plane
       expect(subject).to be_empty
     end
@@ -23,15 +27,16 @@ describe Airport do
     end
 
     it 'a plane is actually released when its sunny' do
-      expect(subject).to receive(:weather) {'sunny'}
       subject.landing(plane)
-      expect(subject).to receive(:weather) {'sunny'}
-      expect(subject.take_off (plane)).to be_an_instance_of(Plane)
+      expect(subject.take_off (plane)).to be plane
     end
   end
 
   describe 'landing' do
-    let(:plane) {Plane.new}
+    let(:plane) {double :plane}
+    before do
+      allow(plane).to receive(:landing)
+    end
 
     it 'a plane is able to land at a new airport if the weather is sunny' do
       expect(subject).to receive(:weather) {'sunny'}
@@ -47,7 +52,12 @@ describe Airport do
   end
 
   describe 'traffic control' do
-    let(:plane) {Plane.new}
+    let(:plane) {double :plane}
+    before do
+      allow(plane).to receive(:landing)
+      allow(plane).to receive(:take_off)
+    end
+
     context 'when airport is full' do
       it 'a plane should not be able to land' do
         expect(subject).to receive(:weather) {'sunny'}
