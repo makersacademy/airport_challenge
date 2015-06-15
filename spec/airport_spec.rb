635a8a77aca44827efd6bc0c7c53730a7189f24b
Christian Miller <cwgmiller@gmail.com>
2015-06-15 12:13:39 +0100

    Possibly final hounding

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -7,15 +7,14 @@ describe Airport do
       expect(subject).to respond_to(:instruct_take_off).with(1).argument
     end
 
-
-    it 'releases a plane when it was the last plane that landed' do 
+    it 'releases a plane when it was the last plane that landed' do
       test_plane = double :plane, landed?: true
       subject.instruct_land test_plane
       subject.instruct_take_off test_plane
       expect(subject.landed_planes).to eq []
     end
 
-    it 'releases specific plane, even if not last to land' do 
+    it 'releases specific plane, even if not last to land' do
       test_plane = double :plane, landed?: true
       filler_plane1 = double :plane, landed?: true
       filler_plane2 = double :plane, landed?: true
@@ -23,10 +22,10 @@ describe Airport do
       subject.instruct_land test_plane
       subject.instruct_land filler_plane2
       subject.instruct_take_off test_plane
-      expect(subject.landed_planes).to eq [filler_plane1,filler_plane2]
+      expect(subject.landed_planes).to eq [filler_plane1, filler_plane2]
     end
 
-    it 'cannot tell a plane to take-off if airport is empty' do 
+    it 'cannot tell a plane to take-off if airport is empty' do
       test_plane = double :plane,landed?: true
       expect{subject.instruct_take_off test_plane}.to raise_error 'No planes to take off'
     end
@@ -34,23 +33,23 @@ describe Airport do
   end
 
   describe 'landing' do
-    it 'instructs a plane to land' do 
+    it 'instructs a plane to land' do
       expect(subject).to respond_to(:instruct_land).with(1).argument
     end
 
-    it 'receives a plane' do 
+    it 'receives a plane' do
       test_plane = double :plane, flying?: true
       subject.instruct_land test_plane
       expect(subject.landed_planes).to eq [test_plane]
     end
   end
 
-  describe 'capacity' do 
+  describe 'capacity' do
     it 'has a (default) maximum capacity' do
       expect(subject.capacity).to eq Airport::DEFAULT_CAPACITY
     end
 
-    it 'airport has no landed planes when created' do 
+    it 'airport has no landed planes when created' do
       expect(subject.landed_planes).to eq []
     end
 
