d241df8e01310383db587251a778a3a26b1cbea0
Christian Miller <cwgmiller@gmail.com>
2015-06-15 12:09:57 +0100

    Hound

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -3,38 +3,38 @@ require 'plane'
 describe Plane do
 
   it 'is flying when created' do
-  	plane = Plane.new
- 		expect(plane).to be_flying
+    plane = Plane.new
+    expect(plane).to be_flying
   end
 
   it 'can land' do
-  	expect(subject).to respond_to :land
+    expect(subject).to respond_to :land
   end
 
   it 'cannot land if already landed' do
-  	subject.land
-  	expect{subject.land}.to raise_error 'Plane has already landed'
+    subject.land
+    expect { subject.land }.to raise_error 'Plane has already landed'
   end
 
   it 'is landed after landing' do
-  	subject.land
-  	expect(subject).to be_landed
+    subject.land
+    expect(subject).to be_landed
   end
 
   it 'can take off' do
-  	expect(subject).to respond_to :take_off
+    expect(subject).to respond_to :take_off
   end
 
   it 'cannot take off if already flying' do
-  	expect{subject.take_off}.to raise_error 'Plane cannot take off when flying'
+    expect { subject.take_off }.to raise_error 'Plane cannot take off when flying'
   end
 
   it 'is flying after take off' do
-  	subject.land
-  	expect(subject.take_off).to be_flying
+    subject.land
+    expect(subject.take_off).to be_flying
   end
 
   it 'cannot be both flying and landed' do
-  	expect(subject.flying?).not_to eq subject.landed?
+    expect(subject.flying?).not_to eq subject.landed?
   end
 end
