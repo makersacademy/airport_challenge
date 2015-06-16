1ffa5e4d3713aceecdeb0ce1297f7010c4b1ebfc
Lucy Oliphant <lucy_oliphant@yahoo.co.uk>
2015-06-16 22:33:19 +0100

    Refactored plane_spec to include context blocks and one-liner sytax where appropriate

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -1,11 +1,7 @@
 require 'plane'
 
-# Think about your implementation - does it allow a plane to be "flying" and landed?
-# Are you testing that? Hmmmm, good question
-
 describe Plane do
 
-  # Great!
   it { is_expected.to respond_to :airborne? }
   it { is_expected.to respond_to :grounded? }
   it { is_expected.to respond_to :land }
@@ -16,33 +12,31 @@ describe Plane do
   end
 
   # possible refactor: group tests related to a particular method together with a context block
-  
+
   describe '#land' do
 
     before do
       subject.land
     end
 
-    it 'is grounded after landing' do
-      expect(subject).to be_grounded
-    end
-
-    it 'is not airborne after landing' do
-      expect(subject).not_to be_airborne
+    context "after landing plane" do
+      it { is_expected.to be_grounded }
+      it { is_expected.not_to be_airborne }
     end
 
   end
 
-  # try a similar refactor with the below
+  describe '#take_off' do
 
-  it 'is airborne after take off' do
-    subject.take_off
-    expect(subject).to be_airborne
-  end
+    before do
+      subject.take_off
+    end
 
-  it 'is not grounded after take off' do
-    subject.take_off
-    expect(subject).not_to be_grounded
+    context "after take off plane" do
+      it { is_expected.to be_airborne }
+      it { is_expected.not_to be_grounded }
+    end
+
   end
 
 end
