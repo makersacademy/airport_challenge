ea760d965e260244cb5aec6f59b5398dbe0f0e8c
Antonio Voce <antonio.v0c3@gmail.com>
2015-07-24 18:53:15 +0100

    plane flies when created, has a status, lands and changes status

--- lib/plane.rb
+++ lib/plane.rb
@@ -1,2 +1,19 @@
 class Plane
+
+  attr_reader :status
+
+  def initialize
+    @status = "flying"
+  end
+
+  def flying?
+    if @status == "flying"
+      true
+    else
+      false
+    end
+  end
+  def land
+    @status = "landed"
+  end
 end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -14,16 +14,38 @@ require 'plane'
 # Think about your implementation - does it allow a plane to be "flying" and landed?
 # Are you testing that?
 
+
+require 'plane'
+
 describe Plane do
 
-  xit 'is flying when created'
+  it {is_expected.to respond_to :flying?}
+  it {is_expected.to respond_to :land}
 
-  xit 'can land'
+  it 'is flying when created' do
+    Plane.new
+    expect(subject).to be_flying
+  end
 
-  xit 'is landed after landing'
-
-  xit 'can take off'
-
-  xit 'is flying after take off'
-
+  it 'can land' do
+    subject.land
+    expect(subject).not_to be_flying
+  end
 end
+
+
+
+
+# describe Plane do
+#
+#   xit 'is flying when created'
+#
+#   xit 'can land'
+#
+#   xit 'is landed after landing'
+#
+#   xit 'can take off'
+#
+#   xit 'is flying after take off'
+#
+# end
