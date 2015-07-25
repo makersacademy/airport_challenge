831f9995f83e2be7ce1172dd88eb1df3cdf3dcd6
Rebecca Appleyard <rklappleyard@gmail.com>
2015-07-25 10:11:16 +0100

    Implementation of capacity - airport can respond to being empty and full

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,22 +1,44 @@
 require_relative 'plane'
-
+DEFAULT_CAPACITY = 100
 class Airport
+
+  attr_reader :planes
+  attr_accessor :capacity, :weather
 
   def initialize
     @planes = []
+    @capacity = DEFAULT_CAPACITY
+    @weather = :sunny
   end
 
   def accept(plane)
+    #weather
+    raise "Airport at capacity!  You can not land here!" if over_capacity?
     plane.land
     planes.push(plane)
+    plane
   end
 
   def allow_take_off
+    weather
+    raise "You can not take off!  The weather is stormy!" if weather == :stormy
+    raise "There are currently no planes ready for take off." if no_planes_available?
     plane = planes.pop
-    #plane.fly
+    plane.fly
+    plane
   end
 
   private
-  attr_reader :planes
 
+  def over_capacity?
+    planes.count >= capacity
+  end
+
+  def no_planes_available?
+    planes.empty?
+  end
+
+  def weather
+    weather = :stormy if Random.rand(100) > 85
+  end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -13,21 +13,33 @@ require 'airport'
 
 describe Airport do
   let(:plane){double(:airplane)}
-
+  # let(:flying_plane){double(:airplane, {:flying? => true})}
 
   describe 'take off' do
+    before(:each) do
+      allow(plane).to receive(:land)
+      subject.accept plane
+      allow(plane).to receive(:fly)
+
+    end
+
     it 'instructs a plane to take off' do
       expect(subject).to respond_to(:allow_take_off)
     end
 
     it 'releases a plane' do
-      wings = instance_double("Plane")
-      allow(wings).to receive(:land)
-      subject.accept wings
-      expect(subject.allow_take_off).to eq(wings)
+      expect(subject.allow_take_off).to eq(plane)
     end
 
-    xit 'after releasing a plane'
+    it 'after releasing a plane' do
+      subject.allow_take_off
+      expect(subject.planes).to be_empty
+    end
+
+    it 'only allows take off if there is a plane available' do
+      subject.allow_take_off
+      expect{subject.allow_take_off}.to raise_error "There are currently no planes ready for take off."
+    end
   end
 
   describe 'landing' do
@@ -36,20 +48,33 @@ describe Airport do
     end
 
     it 'receives a plane' do
-      wings = instance_double("Plane")
-      allow(wings).to receive(:land)
-      expect(subject.accept wings).not_to be_empty
+      allow(plane).to receive(:land)
+      expect(subject.accept plane).to eq(plane)
     end
 
-    xit 'after accepting a plane'
+    it 'after accepting a plane' do
+      allow(plane).to receive(:land)
+      subject.accept plane
+      expect(subject.planes).not_to be_empty
+    end
+  end
+
+  describe 'capacity' do
+    it 'has a default capacity' do
+      expect(subject.capacity).to eq Airport::DEFAULT_CAPACITY
+    end
   end
 
   describe 'traffic control' do
-    # context 'when airport is full' do
-    #   it 'does not allow a plane to land' do
-    #     expect{subject.land :plane}.to raise_error "Airport at capacity!  You can not land here!"
-    #   end
-    #end
+    context 'when airport is full' do
+      before(:each) do
+        allow(plane).to receive(:land)
+      end
+      it 'does not allow a plane to land' do
+        subject.capacity.times{subject.accept plane}
+        expect{subject.accept plane}.to raise_error "Airport at capacity!  You can not land here!"
+      end
+    end
 
     # Include a weather condition.
     # The weather must be random and only have two states "sunny" or "stormy".
@@ -61,7 +86,14 @@ describe Airport do
     # the plane can not land, and must not be in the airport
 
     context 'when weather conditions are stormy' do
-      xit 'does not allow a plane to take off'
+      #let(:stormy_weather){double(:weather, :stormy)}
+      it 'does not allow a plane to take off' do
+        allow(plane).to receive(:land)
+        subject.accept plane
+        allow(plane).to receive(:fly)
+
+        expect{subject.allow_take_off}.to raise_error "You can not take off!  The weather is stormy!"
+      end
 
       xit 'does not allow a plane to land'
     end
