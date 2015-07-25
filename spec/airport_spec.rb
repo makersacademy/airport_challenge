ad4010e22c586f10b3f54997df162990263964ea
Christopher Alcock <christopher.alcock@gmail.com>
2015-07-25 13:07:28 +0100

    tried to address hound issues

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -13,18 +13,19 @@ describe Airport do
     it 'raises an error when you try to launch a plane that isn\'t there' do
       subject.instance_variable_set(:@weather, :stormy)
       subject.instance_variable_set(:@hanger, [:plane1rsr])
-      expect { subject.launch(:plane2) }.to raise_error 'That plane isn\'t here'
+      expect { subject.launch(:plane2) }.to raise_error
+      'That plane isn\'t here'
     end
   end
 
   describe 'launch' do
-  it 'raises a danger error when you try to launch a legit plane in a storm' do
-    subject.instance_variable_set(:@weather, :stormy)
-    subject.instance_variable_set(:@hanger, [:plane])
-    expect { subject.launch(:plane) }.to raise_error 'It\'s too dangerous to launch!'
+    it 'raises error when you try to launch a plane in a storm' do
+      subject.instance_variable_set(:@weather, :stormy)
+      subject.instance_variable_set(:@hanger, [:plane])
+      expect { subject.launch(:plane) }.to raise_error
+      'It\'s too dangerous to launch!'
+    end
   end
-end
-
 
   describe 'land' do
     it 'receives a plane' do
@@ -34,18 +35,20 @@ end
   end
 
   describe 'land' do
-    it 'raises an error when you try to land a plane when there isn\'t room' do
+    it 'raises an error when you land a plane without room' do
       subject.instance_variable_set(:@weather, :sunny)
-      Airport::DEFAULT_CAPACITY.times{subject.land(:plane)}
-      expect { subject.land(:plane) }.to raise_error 'There\'s no room here'
+      Airport::DEFAULT_CAPACITY.times { subject.land(:plane) }
+      expect { subject.land(:plane) }.to raise_error
+      'There\'s no room here'
     end
   end
 
   describe 'land' do
-    it 'raises a specific error when you try to land a plane when there isn\'t room AND it\'s stormy' do
+    it 'raises a space + storm error' do
       subject.instance_variable_set(:@weather, :stormy)
-      Airport::DEFAULT_CAPACITY.times{subject.land(:plane)}
-      expect { subject.land(:plane) }.to raise_error 'There\'s no room here and it\'s too dangerous to land!'
+      Airport::DEFAULT_CAPACITY.times { subject.land(:plane) }
+      expect { subject.land(:plane) }.to raise_error
+      'There\'s no room here and it\'s too dangerous to land!'
     end
   end
 
@@ -56,18 +59,18 @@ end
   end
 
   describe 'space_check' do
-    it 'can return its spare space, which is lowered by a plane landing' do
+    it 'can return its spare space, lowered when plane lands' do
       subject.land(:plane)
-      expect(subject.space_check). to eq (Airport::DEFAULT_CAPACITY-1)
+      expect(subject.space_check). to eq (Airport::DEFAULT_CAPACITY - 1)
     end
   end
 
   describe 'space_check' do
-    it 'can return its spare space, which is raised by a plane launching' do
+    it 'can return its spare space, raised when plane launches' do
       subject.land(:plane1)
       subject.land(:plane2)
       subject.launch(:plane1)
-      expect(subject.space_check). to eq (Airport::DEFAULT_CAPACITY-1)
+      expect(subject.space_check). to eq (Airport::DEFAULT_CAPACITY - 1)
     end
   end
 
