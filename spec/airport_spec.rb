73b7ae2cd4ffd6017b7ab15f771855039939a704
Benjamina Bollag <benjamina.bollag12@imperial.ac.uk>
2015-07-26 23:04:35 +0100

    Weather added

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,11 +1,14 @@
 require 'airport'
 
 describe Airport do
-  let(:landed_plane) {double(:landed_plane,{:flying? => false})}
-  let(:flying_plane) {double(:flying_plane,{:flying? => true})}
-  
+  let(:landed_plane) {double(:landed_plane, :flying? => false)}
+  let(:flying_plane) {double(:flying_plane,:flying? => true)}
+  let(:landed_plane_stormy) {double(:landed_plane_stormy,:flying? => false, :stormy? => true)}
+  let(:flying_plane_stormy) {double(:flying_plane_stormy,:flying? => true, :stormy? => true)}
+
   describe 'take off' do
     it 'instructs a plane to take off' do 
+      allow(subject).to receive(:stormy?).and_return(false)
       expect(landed_plane).to receive(:take_off)
       subject.release landed_plane
     end 
@@ -14,10 +17,15 @@ describe Airport do
       expect(subject).to respond_to(:release).with(1).argument
     end 
 
+    it 'cannot release a plane that is flying' do 
+      allow(subject).to receive(:stormy?).and_return(false)
+      expect{subject.release flying_plane}.to raise_error 'plane is already flying'
+    end 
   end
 
   describe 'landing' do
     it 'instructs a plane to land' do 
+      allow(subject).to receive(:stormy?).and_return(false)
       expect(flying_plane).to receive (:land)
       subject.receive flying_plane
     end 
@@ -25,7 +33,18 @@ describe Airport do
     it 'receives a plane' do 
       expect(subject).to respond_to(:receive).with(1).argument
     end 
+
+    it 'cannot receive a plane that is has landed' do 
+      allow(subject).to receive(:stormy?).and_return(false)
+      expect{subject.receive landed_plane}.to raise_error 'plane has already landed'
+    end 
   end
+  
+  
+  describe 'destination control' do
+    it 'can change name' do 
+    end 
+  end 
 
   describe 'traffic control' do
     context 'when airport is full' do
@@ -38,26 +57,26 @@ describe Airport do
         expect(airport.capacity).to eq 50
       end
 
-      it "test if Airport is full" do
+      it 'test if Airport is full' do
+        allow(subject).to receive(:stormy?).and_return(false)
         subject.capacity.times{subject.receive Plane.new}
         expect{subject.receive Plane.new}.to raise_error 'Airport is full'  
       end
-
     end
 
-    # Include a weather condition.
-    # The weather must be random and only have two states "sunny" or "stormy".
-    # Try and take off a plane, but if the weather is stormy,
-    # the plane can not take off and must remain in the airport.
-    #
-    # This will require stubbing to stop the random return of the weather.
-    # If the airport has a weather condition of stormy,
-    # the plane can not land, and must not be in the airport
-
     context 'when weather conditions are stormy' do
-      xit 'does not allow a plane to take off'
 
-      xit 'does not allow a plane to land'
+      it 'does not allow a plane to take off' do 
+        allow(subject).to receive(:stormy?).and_return(true)
+        plane = landed_plane
+        expect{subject.release plane}.to raise_error 'Cannot take off while weather is stormy'  
+      end 
+
+      it 'does not allow a plane to land' do 
+        allow(subject).to receive(:stormy?).and_return(true)
+        plane = Plane.new
+        expect{subject.receive plane}.to raise_error 'Cannot land while weather is stormy'  
+      end 
     end
   end
 end
