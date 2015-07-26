f3c6880d7580e06cde9f87087155fb48bc0f62d9
Winifred Au <winifredau1990@gmail.com>
2015-07-26 10:23:25 -0400

    adding changes to airport.rb and plane.rb plus made pilot spec and pilot.rb

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,2 +1,31 @@
+require 'plane'
 class Airport
+  DEFAULT_CAPACITY = 20
+  attr_accessor :capacity
+  def initialize
+    @planes = []
+    @capacity = DEFAULT_CAPACITY
+  end
+
+  def release_plane
+    fail 'Inclement weather' if weather.stormy?
+    @planes.delete weather.pop
+  end
+
+  def dock_plane
+    fail 'Airport full' if full?
+    @planes << plane
+  end
+
+  private
+
+  attr_reader :planes
+
+  def full?
+    @planes.count >= capacity
+  end
+
+  def weather
+    @planes.reject {|plane| plane.stormy?}
+  end
 end
--- lib/plane.rb
+++ lib/plane.rb
@@ -1,2 +1,22 @@
+require 'pilot'
+
 class Plane
+
+  def initialize
+    @stormy = false
+    @weather = []
+
+  end
+
+  def landing?
+    if @stormy == true && pilot.arrive? == true
+  end
+
+  def departing?
+    if @stormy == true && pilot.depart? == true
+  end
+
+  def stormy?
+    @stormy
+  end
 end
