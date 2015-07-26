b0e0f62276d92b1ed694dde2956e5898cf415bac
Richard Charman <richardcharman@live.com>
2015-07-26 07:48:31 +0000

    Added random weather element to Airport class. Plane may not land or take off if stormy.

--- lib/airport.rb
+++ lib/airport.rb
@@ -7,18 +7,33 @@ class Airport
   def initialize(capacity= DEFAULT_CAPACITY)
     @planes = []
     @capacity = capacity
+    @weather = "Sunny"
+  end
+  
+  def weather
+    @weather
   end
 
   def take_off plane
     raise "There are no planes at #{self.class.name}" if empty?
+    raise "Weather is stormy. You may not take off from #{self.class.name}" if weather == "Stormy"
     @planes.delete (plane)
     plane.take_off
   end
   
   def land plane
     raise "#{self.class.name} is full" if full?
+    raise "Weather is stormy. You may not land at #{self.class.name}" if weather == "Stormy"
     @planes << plane
     plane.land
+  end
+  
+  def random_weather(skies=rand(7))
+    if skies == 3
+      @weather = 'Stormy'
+    else
+      @weather = 'Sunny'
+    end
   end
   
   private
