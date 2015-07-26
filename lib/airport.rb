7166f87653b9baa6d611fc2d478c32e2ec48eb04
UpstatePedro <piers.m.holden@gmail.com>
2015-07-26 16:03:43 +0100

    Completed airport challenge - no RSpec feature tests

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,2 +1,41 @@
+require_relative 'plane'
+require_relative 'weather_generator'
+
 class Airport
+  include WeatherGenerator
+
+  def initialize
+      @planes = []
+      @capacity=3
+  end
+
+  def clear_to_takeoff plane
+    fail "No such plane in airport" if not @planes.include?(plane)
+    fail "Can't take off in stormy weather" if check_weather=="stormy"
+    plane.take_off
+    @planes.delete(plane)
+  end
+
+  def clear_to_land plane
+    fail "No space at airport" if full?
+    fail "Can't land in stormy weather" if check_weather=="stormy"
+    plane.land
+    @planes << plane
+  end
+
+  def planes
+      @planes
+  end
+
+  def capacity
+    @capacity
+  end
+
+  def full?
+    @planes.length>=@capacity? true : false
+  end
+
+  def check_weather
+    weather_state
+  end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,4 +1,5 @@
 require 'airport'
+require 'support/shared_examples_for_weather_generator'
 
 ## Note these are just some guidelines!
 ## Feel free to write more tests!!
@@ -13,21 +14,53 @@ require 'airport'
 
 describe Airport do
 
-  describe 'take off' do
-    xit 'instructs a plane to take off'
+  describe '-> take off' do
 
-    xit 'releases a plane'
+    it '--> instructs plane to take_off if sunny' do
+      plane = double :plane, land: "landed"
+      allow(subject).to receive( :check_weather ) {"sunny"}
+      subject.clear_to_land plane
+      expect(plane).to receive( :take_off ).and_return "flying"
+      subject.clear_to_takeoff plane
+    end
+
+    it '--> releases a plane' do
+      plane = double :plane, land: "landed", take_off: "flying"
+      allow(subject).to receive( :check_weather ) {"sunny"}
+      subject.clear_to_land plane
+      subject.clear_to_takeoff plane
+      expect( subject.planes ).to be_empty
+    end
+
+    it '--> doesn\'t release a plane if empty' do
+      plane = double :plane
+      expect{ subject.clear_to_takeoff plane }.to raise_error "No such plane in airport"
+    end
   end
 
-  describe 'landing' do
-    xit 'instructs a plane to land'
+  describe '-> landing' do
+    let(:plane) {double :plane, land: "landed"}
+    it '--> instructs a plane to land' do
+      allow(subject).to receive( :check_weather ) {"sunny"}
+      expect(plane).to receive :land
+      subject.clear_to_land plane
+    end
 
-    xit 'receives a plane'
+    it '--> receives a plane' do
+      allow(subject).to receive( :check_weather ) {"sunny"}
+      subject.clear_to_land plane
+      expect( subject.planes ).not_to be_empty
+    end
   end
 
-  describe 'traffic control' do
-    context 'when airport is full' do
-      xit 'does not allow a plane to land'
+  describe '-> traffic control' do
+    context '~ when airport is full' do
+      it '--> does not allow a plane to land' do
+        plane = double :plane, land: "landed"
+        allow(subject).to receive( :check_weather ) {"sunny"}
+        subject.capacity.times { subject.clear_to_land plane }
+        expect { subject.clear_to_land plane }.to raise_error "No space at airport"
+      end
     end
 
     # Include a weather condition.
@@ -37,12 +70,24 @@ describe Airport do
     #
     # This will require stubbing to stop the random return of the weather.
     # If the airport has a weather condition of stormy,
-    # the plane can not land, and must not be in the airport
+    # the plane cannot land, and must not be in the airport
 
-    context 'when weather conditions are stormy' do
-      xit 'does not allow a plane to take off'
+    context '-> when weather conditions are stormy' do
+      it '--> does not allow a plane to take off' do
+        allow(subject).to receive( :check_weather ) {"sunny"}
+        plane = double :plane, land: "landed"
+        subject.clear_to_land plane
+        allow(subject).to receive( :check_weather ) {"stormy"}
+        expect{ subject.clear_to_takeoff plane}.to raise_error "Can't take off in stormy weather"
+      end
 
-      xit 'does not allow a plane to land'
+      it '--> does not allow a plane to land' do
+        plane = double :plane, land: "landed", take_off: "flying"
+        allow(subject).to receive( :check_weather ) {"stormy"}
+        expect{ subject.clear_to_land plane}.to raise_error "Can't land in stormy weather"
+      end
     end
   end
+
+  it_behaves_like WeatherGenerator
 end
