b2c1bca584fd1a1ff359cfaf8a11c61371aafc54
Antonio Voce <antonio.v0c3@gmail.com>
2015-07-26 21:54:52 +0100

    last commit before refactoring, no readme

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,9 +1,4 @@
 
-# No more planes can be added to the airport, if it's full.
-# It is up to you how many planes can land in the airport
-# and how that is implemented.
-#
-# If the airport is full then no planes can land
 require 'airport.rb'
 
 describe Airport do
@@ -30,7 +25,7 @@ describe Airport do
 
      it 'allows a plane to land' do
        allow(subject).to receive(:randomize_weather){"sunny"}
-       expect(plane).to receive(:land) # I had to invert them here, make sure I get why
+       expect(plane).to receive(:land)
        subject.allow_to_land(plane)
      end
      it 'receives a plane' do
@@ -58,15 +53,6 @@ describe Airport do
         end
       end
      end
-
-    # Include a weather condition.
-    # The weather must be random and only have two states "sunny" or "stormy".
-    # Try and take off a plane, but if the weather is stormy,
-    # the plane can not take off and must remain in the airport.
-    #
-    # This will require stubbing to stop the random return of the weather.
-    # If the airport has a weather condition of stormy,
-    # the plane can not land, and must not be in the airport
 
     describe 'weather conditions' do
       context 'when weather conditions are stormy' do
