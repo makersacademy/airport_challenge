0ee13acb329cac40210ac10b6134f683591fd93b
Brandon Graves <bgraves14@gmail.com>
2015-07-26 21:36:14 +0100

    finished the user stories

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -12,24 +12,49 @@ require 'airport'
 # If the airport is full then no planes can land
 
 describe Airport do
+  let(:plane) {double :plane} 
 
   describe 'take off' do
-    xit 'instructs a plane to take off'
+    it 'instructs a plane to take off' do
+      expect(subject).to respond_to :take_off
+    end
 
-    xit 'releases a plane'
-  end
+    it 'releases a plane' do
+
+      subject.land(plane)
+      expect(subject.take_off).to eq plane 
+    end
+
+    end
 
   describe 'landing' do
-    xit 'instructs a plane to land'
+    it 'instructs a plane to land' do
+      expect(subject).to respond_to(:land).with(1).argument
+    end
 
-    xit 'receives a plane'
+    it 'receives a plane' do
+      subject.land(plane)
+      expect(subject).not_to be_empty
+    end
+
+    it 'raises error when full' do
+      subject.capacity.times {subject.land(plane)}
+      expect{subject.land(plane)}.to raise_error "airport full"
+
+
+    end
+
   end
 
   describe 'traffic control' do
     context 'when airport is full' do
       xit 'does not allow a plane to land'
     end
-
+  describe 'initialize stage' do
+    it 'should have a default capacity' do
+      expect(subject.capacity).to eq Airport::DEFAULT_CAPACITY 
+    end
+  end
     # Include a weather condition.
     # The weather must be random and only have two states "sunny" or "stormy".
     # Try and take off a plane, but if the weather is stormy,
@@ -40,9 +65,25 @@ describe Airport do
     # the plane can not land, and must not be in the airport
 
     context 'when weather conditions are stormy' do
-      xit 'does not allow a plane to take off'
+      it 'does not allow a plane to take off' do
+        subject.change_weather(6)
+        expect{subject.take_off}.to raise_error "unable to take-off when stormy"
+      end
+      it 'does not allow a plane to land' do
+        subject.change_weather(6) 
+        expect{subject.land(plane)}.to raise_error "unable to land when stormy"
+      end
 
-      xit 'does not allow a plane to land'
+      it "understands change weather" do
+        expect(subject).to respond_to(:change_weather).with(1).argument
+      end
+
+      it "should be sunny between 0-5" do
+        subject.change_weather(6)
+        sunny = rand(6)
+        expect(subject.change_weather(sunny)).to eq "sunny"
+
+      end
     end
   end
 end
