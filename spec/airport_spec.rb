89657e7dcbd13b830b56a204cb6fc946cb10e38c
Leon Wee <leonwee27@gmail.com>
2015-07-26 13:02:43 +0700

    Updated Spec

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -17,13 +17,10 @@ describe Airport do
   before(:each) { allow(plane).to receive(:fly) }
 
   describe 'take off' do
-    xit 'instructs a plane to take off'
     it 'instructs a plane to take off' do
       expect(subject).to respond_to :take_off
     end
 
-    xit 'releases a plane'
-  end
     it "releases a plane" do
       subject.landing(plane)
       expect(subject.take_off).to eq plane
@@ -32,17 +29,29 @@ describe Airport do
     it "cannot release a plane in empty airport" do
       expect{subject.take_off}.to raise_error "Airport empty"
     end
+  end
 
-    describe 'landing' do
-      xit 'instructs a plane to land'
+  describe "#landing" do
+    it "instructs a plane to land" do
+      expect(subject).to respond_to(:landing).with(1).argument
+    end
+
+    it "ensures that landed plane status is landing" do
+      expect(plane).to receive :land
+      subject.landing(plane)
+    end
+
     it "ensures that taken off plane status is flying" do
       subject.landing(plane)
       expect(plane).to receive :fly
       subject.take_off
     end
 
-    xit 'receives a plane'
-
+    it "receives a plane" do
+      subject.landing(plane)
+      expect(subject).not_to be_empty
+    end
+  end
 
   describe "initialized stage" do
     it "has a default capacity when initialized" do
@@ -50,25 +59,13 @@ describe Airport do
     end
   end
 
-  describe 'traffic control' do
-    context 'when airport is full' do
-      xit 'does not allow a plane to land'
-
-  describe "#landing" do
-    it "instructs a plane to land" do
-      expect(subject).to respond_to(:landing).with(1).argument
+  describe "traffic control" do
+    context "when airport is full" do
+      it "does not allow a plane to land" do
+        subject.capacity.times { subject.landing(plane) }
+        expect{subject.landing(plane)}.to raise_error "Airport is full"
+      end
     end
-
-    it "receives a plane" do
-      subject.landing(plane)
-      expect(subject).not_to be_empty
-    end
-
-    it "ensures that landed plane status is landing" do
-      expect(plane).to receive :land
-      subject.landing(plane)
-    end
-  end
 
     # Include a weather condition.
     # The weather must be random and only have two states "sunny" or "stormy".
@@ -78,17 +75,6 @@ describe Airport do
     # This will require stubbing to stop the random return of the weather.
     # If the airport has a weather condition of stormy,
     # the plane can not land, and must not be in the airport
-
-    context 'when weather conditions are stormy' do
-      xit 'does not allow a plane to take off'
-
-  describe "traffic control" do
-    context "when airport is full" do
-      it "does not allow a plane to land" do
-        subject.capacity.times { subject.landing(plane) }
-        expect{subject.landing(plane)}.to raise_error "Airport is full"
-      end
-    end
 
     context "weather status randomization" do
       it "should be sunny between 0 - 2" do
@@ -124,5 +110,4 @@ describe Airport do
       end
     end
   end
-
 end
