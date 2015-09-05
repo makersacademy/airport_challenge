36c147dedab56ef53ba9cae28d563883ff4e5e6c
chn.challenger <chn.challenger@gmail.com>
2015-09-05 21:21:56 +0100

    Remodelled Plane class and rewrote tests to keep Plane class methods SOLID, passing permissioning to Airport class to handle

--- lib/plane.rb
+++ lib/plane.rb
@@ -10,23 +10,14 @@ class Plane
 		@flying
 	end
 
-	def current_airport
-		@current_airport
-	end
-
 	def land airport
 		fail 'Not an airport' unless airport.is_a?(Airport)
-		fail "Can't land in stormy weather" if airport.is_stormy?
-		fail "Can't land at a full airport" if airport.is_full?
-		@current_airport = airport
 		@flying = false
 	end
 
 	def take_off
 		fail "Can't take off while already flying" if @flying
-		fail "Can't take off in stormy weather" if @current_airport.is_stormy?
 		@flying = true
-		@current_airport = nil
 	end
 
 end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -4,14 +4,11 @@ require 'plane'
 
 describe Plane do
 
-	class Airport;	end  #necessary to test for checking class of airport double
-	#setup airport double for unit tests, 
-	#default arguments allows for successful landing       
-	def airport_double(airport_class=true, storm=false, full=false)
+	class Airport;	end  #needed to test for checking class of airport double
+	#setup airport double for unit tests to keep DRY    
+	def airport_double(airport_class=true)
 		airport = double(:airport_object)
 	  allow(airport).to receive(:is_a?).with(Airport).and_return(airport_class)
-	  allow(airport).to receive(:is_stormy?).and_return(storm)
-	  allow(airport).to receive(:is_full?).and_return(full)
 	  return airport
 	end
 
@@ -19,17 +16,14 @@ describe Plane do
 		it do expect(subject).to respond_to(:flying?).with(0).argument end
 	end
 
-	describe 'method:current_airport' do  
-		it do expect(subject).to respond_to(:current_airport).with(0).argument end
-	end #is it also worth writing a test to test for the return value is the value of 
-	#@current_airport, seems not, and it will require looking at internals - not ideal.
+	# describe 'method:current_airport' do  
+	# 	it do expect(subject).to respond_to(:current_airport).with(0).argument end
+	# end #is it also worth writing a test to test for the return value is the value of 
+	# #@current_airport, seems not, and it will require looking at internals - not ideal.
 
 	describe 'method:new/initialize' do
 	  it 'is flying when created' do 
 	  	expect(subject).to be_flying
-	  end
-	  it 'current airport location is none/nil' do 
-	  	expect(subject.current_airport).to eq(nil)
 	  end
 	end
 
@@ -38,29 +32,13 @@ describe Plane do
 	  	expect(subject).to respond_to(:land).with(1).argument
 	  end
 	  it 'raises error when not given a valid airport object as destination' do
-	  	airport_class, storm, full = false, false, false
-	  	airport = airport_double(airport_class, storm, full)
+	  	airport = airport_double(false)
 			expect{subject.land(airport)}.to raise_error 'Not an airport'
 	  end
-	  it 'raises error when trying to land while weather at the airport is stormy' do
-	  	airport_class, storm, full = true, true, false
-	  	airport = airport_double(airport_class, storm, full)
-	  	expect{subject.land(airport)}.to raise_error "Can't land in stormy weather"
-	  end	
-		it 'raises error when trying to land at a full airport' do
-	  	airport_class, storm, full = true, false, true
-	  	airport = airport_double(airport_class, storm, full)
-	  	expect{subject.land(airport)}.to raise_error "Can't land at a full airport"
-	  end	
 	  it 'is landed/not-flying after landing' do
 	  	airport = airport_double
 	  	subject.land(airport)
 	  	expect(subject).to_not be_flying
-	  end
-	  it 'is landed at the destination airport after landing' do
-	  	airport = airport_double
-	  	subject.land(airport)
-	  	expect(subject.current_airport).to eq(airport)
 	  end
   end
 
@@ -69,24 +47,12 @@ describe Plane do
 	  it 'raises error when trying to take off while in the air' do
 	  	expect{subject.take_off}.to raise_error "Can't take off while already flying"
 	 	end 
-	  it 'raises error when trying to take off while weather at the airport is stormy' do
-	  	airport = airport_double
-	  	subject.land(airport)
-	  	allow(airport).to receive(:is_stormy?).and_return(true)
-	  	expect{subject.take_off}.to raise_error "Can't take off in stormy weather"
-	  end
 	  it 'is flying after take off' do
 	  	airport = airport_double	
 	  	subject.land(airport)
 	  	subject.take_off
 	  	expect(subject).to be_flying
 	  end
-	  it 'has no/nil current airport after take off' do
-	  	airport = airport_double	
-	  	subject.land(airport)
-	  	subject.take_off
-	  	expect(subject.current_airport).to eq(nil)
-		end
 	end
 
 end
