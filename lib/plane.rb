c74ec0adac36e5af0d59aefc55c7cc173055775a
Anindya Bhattacharyya <bat020@gmail.com>
2015-09-05 18:34:44 +0100

    refactored to avoid fiddling with @status directly

--- lib/plane.rb
+++ lib/plane.rb
@@ -1,23 +1,27 @@
 class Plane
 
-def initialize
-  @status = :flying
-end
+  def initialize
+    @status = :flying
+  end
 
-def flying?
-  @status == :flying
-end
+  def flying?
+    status == :flying
+  end
 
-def landed?
-  @status != :flying
-end
+  def landed?
+    status != :flying
+  end
 
-def land_at(airport)
-  @status = airport
-end
+  def land_at(airport)
+    self.status = airport
+  end
 
-def take_off_from(airport)
-  @status = :flying
-end
+  def take_off_from(airport)
+    self.status = :flying
+  end
+
+  private
+
+  attr_accessor :status
 
 end
