024c5806d71b1d1d040c8a4f1977cfa49bb21fd6
Harriet Craven <harrietcraven@Harriets-MacBook-Pro.local>
2015-09-05 21:31:00 +0100

    Planes avoid collisions

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,2 +1,29 @@
+require './lib/plane.rb'
+
 class Airport
+  DEFAULT_CAPACITY = 50
+  attr_reader :planes, :capacity
+
+  def initialize(capacity = DEFAULT_CAPACITY)
+    @planes = []
+    @capacity = capacity
+  end
+
+  def plane_take_off
+    fail 'Airport is empty' if empty?
+    planes.pop
+  end
+
+  def plane_land(plane)
+    fail 'Airport is full' if full?
+    planes << plane
+  end
+
+  def full?
+    planes.count >= capacity
+  end
+
+  def empty?
+    planes.empty?
+  end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,33 +1,44 @@
 require 'airport'
 
-## Note these are just some guidelines!
-## Feel free to write more tests!!
-
-# A plane currently in the airport can be requested to take off.
-#
-# No more planes can be added to the airport, if it's full.
-# It is up to you how many planes can land in the airport
-# and how that is implemented.
-#
-# If the airport is full then no planes can land
-
 describe Airport do
 
   describe 'take off' do
-    xit 'instructs a plane to take off'
+    it 'instructs a plane to take off' do
+      is_expected.to respond_to(:plane_take_off)
+    end
 
-    xit 'releases a plane'
+    it 'releases a plane' do
+      airport = Airport.new
+      plane = Plane.new :destination
+      airport.plane_land(plane)
+      airport.plane_take_off
+      expect(airport.planes.empty?).to eq true
+    end
+    it 'raises an error when no planes at the airport' do
+      expect { subject.plane_take_off}.to raise_error 'Airport is empty'
+    end
   end
 
   describe 'landing' do
-    xit 'instructs a plane to land'
+    it 'instructs a plane to land' do
+      is_expected.to respond_to(:plane_land)
+    end
 
-    xit 'receives a plane'
+    it 'receives a plane' do
+      airport = Airport.new
+      plane = Plane.new :destination
+      airport.plane_land(plane)
+      expect(airport.planes.any?).to eq true
+    end
   end
 
   describe 'traffic control' do
     context 'when airport is full' do
-      xit 'does not allow a plane to land'
+      it 'does not allow a plane to land' do
+        plane = Plane.new :destination
+        subject.capacity.times { subject.plane_land(plane)}
+        expect { subject.plane_land(plane)}.to raise_error 'Airport is full'
+      end
     end
 
     # Include a weather condition.
