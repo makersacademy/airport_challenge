3ae6bc1c94b0936a095f0ef9d29a53015eabe695
chn.challenger <chn.challenger@gmail.com>
2015-09-06 00:38:36 +0100

    major hound cleanup

--- lib/plane.rb
+++ lib/plane.rb
@@ -7,36 +7,7 @@ class Plane
 		@current_airport = nil
 	end
 
-	# def flying?
-	# 	@flying
-	# end
-
-	# def current_airport
-	# 	@current_airport
-	# end
-
 	def land airport
 		fail 'Not an airport' unless airport.is_a?(Airport)
-		# airport.landing_permission
-		# @flying = false
-		# @current_airport = airport
-		# airport.plane_landed(self)
 	end
 end
-
-
-
-
-
-
-
-
-
-
-	# def take_off
-	# 	fail "Can't take off while already flying" if @flying
-	# 	@current_airport.take_off_permission
-	# 	@flying = true
-	# 	@current_airport = nil
-	# 	airport.plane_taken_off(self)
-	# end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -7,10 +7,10 @@ describe Plane do
 	  end
 	end
 	describe 'method:flying?' do
-		it do expect(subject).to respond_to(:flying).with(0).argument end
+		it {expect(subject).to respond_to(:flying).with(0).argument}
 	end
 	describe 'method:current_airport' do
-		it do expect(subject).to respond_to(:current_airport).with(0).argument end
+		it {expect(subject).to respond_to(:current_airport).with(0).argument}
 	end
 	describe 'method:land' do
 		it 'can land at a specified Airport' do
@@ -24,117 +24,3 @@ describe Plane do
 	  end
 	end
 end
-
-
-
-
-	# class Airport;	end  #needed to test for checking class of airport double
-	# #setup airport double for unit tests to keep DRY    
-	# def airport_double(airport_class=true)
-	# 	airport = double(:airport_object)
-	#   allow(airport).to receive(:is_a?).with(Airport).and_return(airport_class)
-	#   return airport
-	# end
-
-
-
- #  describe 'method:land' do
-	
-	#   it 'is landed/not-flying after landing' do
-	#   	airport = airport_double
-	#   	subject.land(airport)
-	#   	expect(subject).to_not be_flying
-	#   end
- #  end
-
- #  describe 'method:take_off' do
-	#   it do expect(subject).to respond_to(:take_off).with(0).argument end
-	#   it 'raises error when trying to take off while in the air' do
-	#   	expect{subject.take_off}.to raise_error "Can't take off while already flying"
-	#  	end 
-	#   it 'is flying after take off' do
-	#   	airport = airport_double	
-	#   	subject.land(airport)
-	#   	subject.take_off
-	#   	expect(subject).to be_flying
-	#   end
-	# end
-
-
-#####################################
-
-
-## Note these are just some guidelines!
-## Feel free to write more tests!!
-
-# When we create a new plane, it should be "flying",
-# thus planes can not be created in the airport.
-#
-# When we land a plane at the airport, the plane in question should
-# be "landed"
-#
-# When the plane takes of from the airport, it should be "flying" again
-#
-# Think about your implementation - does it allow a plane to be "flying"
-# and landed?
-# Are you testing that?
-
-############### OLD WORKING CODE ################
-
-# describe Plane do
-
-#   it 'is flying when created' do 
-#   	expect(subject).to be_flying
-#   end
-
-#   describe 'method:land' do
-# 		it 'can land at a specified Airport' do
-# 	  	expect(subject).to respond_to(:land).with(1).argument########
-# 	  end
-# 	  it 'raises error when not given a valid airport object as destination' do
-# 	  	airport = double(:none_airport_object)
-# 	  	allow(airport).to receive(:is_a?).with(Airport).and_return(false)
-# 	  	expect{subject.land(airport)}.to raise_error 'Not an airport'
-# 	  end
-# 	  it 'raises error when trying to land while weather at the airport is stormy' do
-# 	  	airport = double(:airport_object)
-# 	  	allow(airport).to receive(:is_a?).with(Airport).and_return(true)
-# 	  	allow(airport).to receive(:is_stormy?).and_return(true)
-# 	  	expect{subject.land(airport)}.to raise_error "Can't land in stormy weather"
-# 	  end	
-# 		it 'raises error when trying to land at a full airport' do
-# 	  	airport = double(:airport_object)
-# 	  	allow(airport).to receive(:is_a?).with(Airport).and_return(true)
-# 	  	allow(airport).to receive(:is_stormy?).and_return(false)
-# 	  	allow(airport).to receive(:is_full?).and_return(true)
-# 	  	expect{subject.land(airport)}.to raise_error "Can't land at a full airport"
-# 	  end	
-# 	  it 'is landed after landing' do
-# 	 # 	  	allow(airport).to receive(:is_stormy?).and_return(false)
-# 	  	allow(airport).to receive(:is_full?).and_return(false)
-# 	  	subject.land(airport)
-# 	  	expect(subject).to_not be_flying
-# 	  end
-#   end
-
-
-
-
-
-#   # it 'can take off' do
-#   # 	expect(subject).to respond_to(:take_off)
-#   # end
-
-#   # it 'is flying after take off' do
-#   # 	subject.land
-#   # 	subject.take_off
-#   # 	expect(subject).to be_flying
-#   # end
-
-# end
-
-
-
-
-
-
