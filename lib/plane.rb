8e05645fd7b547fd5f68d34a21ff4b2fef9721eb
Anindya Bhattacharyya <bat020@gmail.com>
2015-09-05 18:07:53 +0100

    basic Plane methods: flying? landed? land_at() take_off_from()

--- lib/plane.rb
+++ lib/plane.rb
@@ -1,2 +1,23 @@
 class Plane
+
+def initialize
+  @status = :flying
+end
+
+def flying?
+  @status == :flying
+end
+
+def landed?
+  @status != :flying
+end
+
+def land_at(airport)
+  @status = airport
+end
+
+def take_off_from(airport)
+  @status = :flying
+end
+
 end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -1,30 +1,52 @@
 require 'plane'
-
-## Note these are just some guidelines!
-## Feel free to write more tests!!
-
-# When we create a new plane, it should be "flying",
-# thus planes can not be created in the airport.
-#
-# When we land a plane at the airport, the plane in question should
-# be "landed"
-#
-# When the plane takes of from the airport, it should be "flying" again
-#
-# Think about your implementation - does it allow a plane to be "flying"
-# and landed?
-# Are you testing that?
+require 'airport'
 
 describe Plane do
 
-  xit 'is flying when created'
+  # When we create a new plane, it should be "flying"
 
-  xit 'can land'
+  it 'is flying when created' do
+    expect(subject).to be_flying
+  end
 
-  xit 'is landed after landing'
+  # As a pilot
+  # So that I can arrive at my specified destination
+  # I would like to land my plane at the appropriate airport
 
-  xit 'can take off'
+  it 'can be landed at an airport' do
+    expect(subject).to respond_to(:land_at).with(1).argument
+  end
 
-  xit 'is flying after take off'
+  # When we land a plane at the airport, the plane in question
+  # should be "landed"
+
+  it 'is landed after landing' do
+    airport = Airport.new
+    subject.land_at(airport)
+    expect(subject).to be_landed
+  end
+
+  # As a pilot
+  # So that I can set off for my specified destination
+  # I would like to be able to take off from the appropriate airport
+
+  it 'can take off from an airport' do
+    expect(subject).to respond_to(:take_off_from).with(1).argument
+  end
+
+  # When the plane takes of from the airport, it should be "flying" again
+
+  it 'is flying after take off' do
+    airport = Airport.new
+    subject.land_at(airport)
+    subject.take_off_from(airport)
+    expect(subject).to be_flying
+  end
 
 end
+
+# Think about your implementation - does it allow a plane to be "flying"
+# and landed? Are you testing that?
+
+# >> I'm not testing that but as it happens the way I've implemented
+# >> Plane.flying? and Plane.landed? makes them mutually exclusive.
