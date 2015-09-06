9e7e85dfe324306773c7d7035b1c011be7108dea
chn.challenger <chn.challenger@gmail.com>
2015-09-06 23:00:57 +0100

    Added plane_landed and full? methods with test suits

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,10 +1,18 @@
 class Airport
 	DEFAULT_CAPACITY = 10
-	
+
 	attr_reader :planes, :capacity
 
 	def initialize
 		@planes = []
 		@capacity = DEFAULT_CAPACITY
+	end
+
+	def plane_landed plane
+		@planes << plane
+	end
+
+	def full?
+		@planes.length >= @capacity ? true : false 
 	end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -13,6 +13,32 @@ describe Airport do
   describe 'method:capacity' do
     it {expect(subject).to respond_to(:capacity).with(0).argument}
   end
+   describe 'method:plane_landed' do
+    it {expect(subject).to respond_to(:plane_landed).with(1).argument}
+    it 'lands plane inside the airport' do
+      plane = double(:plane_object)
+      subject.plane_landed(plane)
+      expect(subject.planes[0]).to eq(plane)
+    end
+  end 
+  describe 'method:full?' do
+    it {expect(subject).to respond_to(:full?).with(0).argument}   
+    it 'returns false when airport is not at full capacity' do
+      plane = double(:plane_object)
+      subject.plane_landed(plane)
+      expect(subject).not_to be_full      
+    end
+    it 'returns true when airport is at full capacity' do
+      (subject.capacity - 1).times do
+        plane = double(:plane_object)
+        subject.plane_landed(plane)
+      end
+      expect(subject).not_to be_full  
+        plane = double(:plane_object)
+        subject.plane_landed(plane)
+      expect(subject).to be_full        
+    end 
+  end
 end
 
 
