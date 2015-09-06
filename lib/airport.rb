1c47f6cb6a959a79712c46bf2e8014dfb6bbf45b
chweeks <chrisharryweeks@gmail.com>
2015-09-06 22:38:38 +0100

    removed plane already in hanger test

--- lib/airport.rb
+++ lib/airport.rb
@@ -16,15 +16,16 @@ class Airport
     weather = "stormy" if rand(21) == 1
     fail 'Cannot take off in stormy weather' if weather == "stormy"
     fail 'That plane is not in hanger' if hanger.include?(plane) == false
-    hanger.delete plane; plane.status = "flying"
+    hanger.delete plane
+    plane.status = "flying"
   end
 
   def instruct_to_land plane
     weather = "stormy" if rand(21) == 1
     fail 'Cannot land in stormy weather' if weather == "stormy"
     fail 'Airport is full' if hanger.size == capacity
-    fail 'Plane already in hanger' if hanger.include?(plane) == true
-    hanger << plane; plane.status = "landed"
+    hanger << plane
+    plane.status = "landed"
   end
 
 end
