6db431641275937dc13664f8b6d5ab9b47dd62c1
Anthony <anthony@Anthonys-MacBook-Pro.local>
2015-09-06 17:33:38 +0100

    Initial commit

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,2 +1,36 @@
 class Airport
+
+attr_accessor :capacity, :aircraft_count
+
+def initialize(limit=3)
+  @capacity = limit
+  @aircraft_count = []
+end
+
+def receive_plane(plane)
+  fail 'Airport at full capacity' if full?
+  fail 'There is a storm!' if weather == "stormy"
+    aircraft_count << plane
+end
+
+def release_plane
+  fail 'No aircraft available' if empty?
+  fail 'There is a storm!' if weather == "stormy"
+  aircraft_count.pop
+end
+
+def weather
+  rand(100) > 50 ? "sunny" : "stormy"
+end
+
+private
+
+def full?
+  aircraft_count.size >= capacity
+end
+
+def empty? 
+  aircraft_count.empty?
+end
+
 end
--- lib/plane.rb
+++ lib/plane.rb
@@ -1,2 +1,25 @@
+require_relative 'airport'
+
 class Plane
+
+  attr_accessor :flying
+
+  def initialize
+    @flying = true
+  end
+
+  def flying?
+    @flying
+  end
+
+  def land
+    @flying = false
+  end
+
+  def landed?
+    true
+  end
+
+
+
 end
