9e860571e54a682b0116c8b4b36cb6c76fd734ee
chweeks <chrisharryweeks@gmail.com>
2015-09-06 21:13:47 +0100

    Added stormy weather errors

--- lib/airport.rb
+++ lib/airport.rb
@@ -4,19 +4,25 @@ class Airport
 
   attr_accessor :capacity
   attr_accessor :hanger
-
+  attr_accessor :weather
+  
   def initialize
     @capacity = DEFAULT_CAPACITY
     @hanger = []
+    @weather = "sunny"
   end
 
   def instruct_to_take_off plane
+    weather = "stormy" if rand(21) == 1
+    fail 'Cannot take off in stormy weather' if weather == "stormy"
     fail 'That plane is not in hanger' if hanger.include?(plane) == false
     hanger.delete plane
     plane.status = "flying"
   end
 
   def instruct_to_land plane
+    weather = "stormy" if rand(21) == 1
+    fail 'Cannot land in stormy weather' if weather == "stormy"
     fail 'Airport is full' if hanger.size == capacity
     fail 'Plane already in hanger' if hanger.include?(plane) == true
     hanger << plane
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,17 +1,6 @@
 require 'airport'
 require 'plane'
 
-## Note these are just some guidelines!
-## Feel free to write more tests!!
-
-# A plane currently in the airport can be requested to take off.
-#
-# No more planes can be added to the airport, if it's full.
-# It is up to you how many planes can land in the airport
-# and how that is implemented.
-#
-# If the airport is full then no planes can land
-
 describe Airport do
 
   it 'Airport has a capacity' do
@@ -43,7 +32,6 @@ describe Airport do
       plane = Plane.new
       expect{subject.instruct_to_take_off(plane)}.to raise_error 'That plane is not in hanger'
     end
-
   end
 
   describe 'landing' do
@@ -73,19 +61,18 @@ describe Airport do
       end
     end
 
-    # Include a weather condition.
-    # The weather must be random and only have two states "sunny" or "stormy".
-    # Try and take off a plane, but if the weather is stormy,
-    # the plane can not take off and must remain in the airport.
-    #
-    # This will require stubbing to stop the random return of the weather.
-    # If the airport has a weather condition of stormy,
-    # the plane can not land, and must not be in the airport
-
     context 'when weather conditions are stormy' do
-      xit 'does not allow a plane to take off'
-
-      xit 'does not allow a plane to land'
+      it 'does not allow a plane to take off' do
+        subject.stub(:rand).and_return( 1 )
+        plane = Plane.new
+        subject.hanger << plane
+        expect{subject.instruct_to_take_off(plane)}.to raise_error 'Cannot take off in stormy weather'
+      end
+      it 'does not allow a plane to land' do
+        subject.stub(:rand).and_return( 1 )
+        plane = Plane.new
+        expect{subject.instruct_to_land(plane)}.to raise_error 'Cannot land in stormy weather'
+      end
     end
   end
 end
