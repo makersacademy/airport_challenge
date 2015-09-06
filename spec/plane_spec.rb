d4984e2895807e16e9d94dc95150dfe27e94f542
Usman Jamil <usman.jamil@live.co.uk>
2015-09-06 14:34:37 +0100

    added let statement to plane_spec

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -16,23 +16,21 @@ require 'plane'
 # Are you testing that?
 
 describe Plane do
-
+  let (:plane) {Plane.new :destination}
   #it 'is flying when created'
   it 'is flying when created' do
-    plane = Plane.new :destination
+    plane
     expect(plane).to be_flying
   end
 
   #it 'can land'
   it 'can land' do
-    plane = Plane.new :destination
     plane.can_land
     expect {plane.can_land}.to raise_error 'Can not land if it is not flying'
   end
 
   #it 'is landed after landing'
   it 'is landed after landing' do
-    plane = Plane.new :destination
   	plane.can_land
     plane.landed?
   	expect(plane).to be_landed
@@ -40,13 +38,11 @@ describe Plane do
 
   #it 'can take off'
   it 'can take off' do
-    plane = Plane.new :destination
     expect {plane.can_take_off}.to raise_error 'Can not take off if it is not landed'
   end
 
   #it 'is flying after take off'
   it 'is flying after take off' do
-    plane = Plane.new :destination
     plane.can_land
     plane.can_take_off
     expect(plane).to be_flying
