e244232ecc9c4a4c799bb8dad1aeaea36e178b85
Harry Venables <venablesh@yahoo.co.uk>
2015-09-06 19:24:21 +0100

    reverted to srand

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,16 +1,5 @@
 require 'airport'
 
-## Note these are just some guidelines!
-## Feel free to write more tests!!
-
-# A plane currently in the airport can be requested to take off.
-#
-# No more planes can be added to the airport, if it's full.
-# It is up to you how many planes can land in the airport
-# and how that is implemented.
-#
-# If the airport is full then no planes can land
-
 describe Airport do
 
   let (:plane) {double('plane', flying?: true, land: false, take_off: true) }
@@ -55,30 +44,17 @@ describe Airport do
       end
     end
 
-    # Include a weather condition.
-    # The weather must be random and only have two states "sunny" or "stormy".
-    # Try and take off a plane, but if the weather is stormy,
-    # the plane can not take off and must remain in the airport.
-    #
-    # This will require stubbing to stop the random return of the weather.
-    # If the airport has a weather condition of stormy,
-    # the plane can not land, and must not be in the airport
-
     context 'when weather conditions are stormy' do
+      before { srand(1) }
 
-      it 'creates stormy conditions' do
-        allow(subject).to receive(:weather_randomiser).and_return(1)
-        expect(subject.stormy?).to eq(true)
-      end
-      it 'checks 2/10 chance of stormy conditions' do
-        allow(subject).to receive(:weather_randomiser).and_return(rand(2))
-        expect(subject.stormy?).to eq(true)
-      end
-
-      # it 'creates good conditions' do
-      #   allow(subject).to receive(:weather_randomiser).and_return(6)
-      #   expect(subject.stormy?).to eq(false)
+      # it 'creates stormy conditions' do
+      #   allow(subject).to receive(:weather_randomiser).and_return(1)
+      #   expect(subject.stormy?).to eq(true)
       # end
+
+      it 'checks 8/10 chance of good conditions' do
+        expect(subject.stormy?).to eq(false)
+      end
 
       it 'does not allow a plane to land' do
         allow(subject).to receive(:stormy?).and_return(:true)
