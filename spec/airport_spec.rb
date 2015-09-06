eb830c8423a6e46e5ee0f06cfd25da872a9e6489
Adrian Booth <Adrian.booth17@gmail.com>
2015-09-06 13:11:51 +0100

    indent changes

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -7,37 +7,37 @@ describe Airport do
 
 	describe "landing" do 
 
-	it { is_expected.to respond_to(:instruct_to_land).with(1).argument}
-	it { is_expected.to respond_to(:stormy?)}
+		it { is_expected.to respond_to(:instruct_to_land).with(1).argument}
+		it { is_expected.to respond_to(:stormy?)}
 
-	it "planes array increase by 1 for each landing" do 
-		airport.instruct_to_land(plane)
-		expect {subject.instruct_to_land(plane)}.to change{subject.plane_count.length}.by(1)
+		it "planes array increase by 1 for each landing" do 
+			airport.instruct_to_land(plane)
+			expect {subject.instruct_to_land(plane)}.to change{subject.plane_count.length}.by(1)
 		end 
 
-	it "Raise error when airport is full" do
-		subject.capacity.times{subject.instruct_to_land Plane.new}
-		expect {subject.instruct_to_land Plane.new}.to raise_error "Plane cannot land. Circle until capacity is released"
+		it "Raise error when airport is full" do
+			subject.capacity.times{subject.instruct_to_land Plane.new}
+			expect {subject.instruct_to_land Plane.new}.to raise_error "Plane cannot land. Circle until capacity is released"
 		end 
 
-	it "Raise error when trying to land same plane twice" do	
-		subject.instruct_to_land(plane)
-		expect {subject.instruct_to_land(plane)}.to raise_error "Plane has already landed"
+		it "Raise error when trying to land same plane twice" do	
+			subject.instruct_to_land(plane)
+			expect {subject.instruct_to_land(plane)}.to raise_error "Plane has already landed"
 		end 
 
-	it "Raise error when trying to land plane in stormy weather" do	
-		allow(subject).to receive(:stormy?).and_return true
-		expect{subject.instruct_to_land(plane)}.to raise_error "Plane cannot land. Wait until wind slows down"
+		it "Raise error when trying to land plane in stormy weather" do	
+			allow(subject).to receive(:stormy?).and_return true
+			expect{subject.instruct_to_land(plane)}.to raise_error "Plane cannot land. Wait until wind slows down"
 		end 
 
  	end 
 
 	describe 'take off' do
 
-	it { is_expected.to respond_to(:instruct_to_take_off).with(1).argument}
- 
- 	it "planes array decrease by 1 for each take-off" do 
-		airport.instruct_to_take_off(plane)
+		it { is_expected.to respond_to(:instruct_to_take_off).with(1).argument}
+	 
+	 	it "planes array decrease by 1 for each take-off" do 
+			airport.instruct_to_take_off(plane)
 		end 
 
 	end 
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -1,29 +1,31 @@
 require 'plane'
 
-describe Plane do
+	describe Plane do
 
 	let(:plane) { Plane.new }
 
-	it { is_expected.to respond_to(:land)}
-	it { is_expected.to respond_to(:take_off)}
-	it { is_expected.to respond_to(:flying?)}
+		it { is_expected.to respond_to(:land)}
+		it { is_expected.to respond_to(:take_off)}
+		it { is_expected.to respond_to(:flying?)}
 
-	it "plane lands at airport and stops flying" do 
-		plane.land
-		expect(plane.flying?).to eql(false)
-    	end
+		it "plane lands at airport and stops flying" do 
+			plane.land
+			expect(plane.flying?).to eql(false)
+	    end
 
-    it "plane takes off from airport and starts flying" do 
-		plane.land
-		plane.take_off
-		expect(plane.flying?).to eql(true)
-    	end
+	    it "plane takes off from airport and starts flying" do 
+			plane.land
+			plane.take_off
+			expect(plane.flying?).to eql(true)
+	    end
+
+	end
 
 	describe '#new' do
 
-	it 'Plane should be flying' do 
-		expect(Plane).to respond_to(:new)
+		it 'Plane should be flying' do 
+			expect(Plane).to respond_to(:new)
 		end 
+
 	end 
 
-end 
