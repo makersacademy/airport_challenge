95516faf9f9bd008507ac391419707b3778918e6
Leonard Person <alexander@Leonards-MacBook-Pro.local>
2015-09-06 17:10:01 +0100

    Planes can land and take off, array and capacity of 20 implemented

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,2 +1,18 @@
+require '~/projects/airport_challenge/lib/plane.rb'
+
 class Airport
+	
+	def initialize
+		@planes = []
+	end
+
+	def take_off
+		fail 'No planes at airport' if @planes.empty?
+		@planes.pop
+	end
+
+	def land plane
+		fail 'Airport is full' if @planes.count >= 20
+		@planes << plane
+	end
 end
