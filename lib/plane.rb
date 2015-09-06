dcf4d15e755490f295b6120fa88031636e0588c6
chweeks <chrisharryweeks@gmail.com>
2015-09-06 13:47:21 +0100

    format changes

--- lib/plane.rb
+++ lib/plane.rb
@@ -2,15 +2,15 @@ class Plane
   attr_accessor :status
 
   def initialize
-    @status = 'flying'
+    @status = "flying"
   end
 
   def land
-    @status = 'landed'
+    @status = "landed"
   end
 
   def take_off
-    @status = 'flying'
+    @status = "flying"
   end
 
 end
