f674c8b2fad7b0c50b20535adc14ac756f300779
chweeks <chrisharryweeks@gmail.com>
2015-09-06 22:32:45 +0100

    refactored to fix hound method.length

--- lib/airport.rb
+++ lib/airport.rb
@@ -5,7 +5,7 @@ class Airport
   attr_accessor :capacity
   attr_accessor :hanger
   attr_accessor :weather
-  
+
   def initialize
     @capacity = DEFAULT_CAPACITY
     @hanger = []
@@ -16,8 +16,7 @@ class Airport
     weather = "stormy" if rand(21) == 1
     fail 'Cannot take off in stormy weather' if weather == "stormy"
     fail 'That plane is not in hanger' if hanger.include?(plane) == false
-    hanger.delete plane
-    plane.status = "flying"
+    hanger.delete plane; plane.status = "flying"
   end
 
   def instruct_to_land plane
@@ -25,8 +24,7 @@ class Airport
     fail 'Cannot land in stormy weather' if weather == "stormy"
     fail 'Airport is full' if hanger.size == capacity
     fail 'Plane already in hanger' if hanger.include?(plane) == true
-    hanger << plane
-    plane.status = "landed"
+    hanger << plane; plane.status = "landed"
   end
 
 end
