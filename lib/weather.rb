59a70130a8c704a33ac115d03e508e55a5571123
Jongmin Kim <kjm141215@gmail.com>
2015-09-06 08:13:06 +0100

    Create initialize, storm_probability, & weather_report method and test in Weather

--- lib/weather.rb
+++ lib/weather.rb
@@ -1,6 +1,15 @@
 class Weather
-  def condition
+  def initialize
+    @weather = [:sunny, :stormy]
+  end
+  def storm_probability
     rand(101)
   end
-
+  def condition
+    return 1 if storm_probability > 90
+    0
+  end
+  def weather_report
+    @weather[condition]
+  end
 end
--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -2,10 +2,27 @@ require 'weather'
 
 describe Weather do
   it { is_expected.to respond_to(:condition) }
-
+  it "generates storm probabilities" do
+    expect(subject.storm_probability).to be_an_instance_of(Fixnum)
+  end
   describe "#condition" do
-    it "creates a weather condition using random number" do
-      expect(subject.condition).to be_an_instance_of(Fixnum)
+    it "converts 91..100 to one" do
+      allow(subject).to receive(:storm_probability) { 91 }
+      expect(subject.condition).to eq(1)
+    end
+    it "converts 0..90 to zero" do
+      allow(subject).to receive(:storm_probability) { 90 }
+      expect(subject.condition).to eq(0)
+    end
+  end
+  describe "#weather_report" do
+    it "reports sunny when storm probabilty is less than 91" do
+      allow(subject).to receive(:storm_probability) { 90 }
+      expect(subject.weather_report).to eq(:sunny)
+    end
+    it "reports stormy when storm probabilty is greater than 90" do
+      allow(subject).to receive(:storm_probability) { 91 }
+      expect(subject.weather_report).to eq(:stormy)
     end
   end
 end
