fecc7b7c3192073f84e4dbd4c0f7244d4728c7dc
Nathaniel Green <nathanielgreen@Nathaniels-MacBook-Pro.local>
2015-09-07 07:12:03 +0100

    plane.rb and airport.rb and resepective specs modified for exisitng tests to pass

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -13,22 +13,53 @@ require 'airport'
 
 describe Airport do
 
-  describe 'take off' do
-    xit 'instructs a plane to take off'
+  before do
+    allow(subject).to receive(:stormy?).and_return(false)
+  end
 
-    xit 'releases a plane'
+ let(:plane){double(:plane, land: nil, take_off: nil)}
+
+  describe 'take off' do
+    it 'instructs a plane to take off' do
+      expect(subject).to respond_to :instruct_to_take_off
+    end
+
+    it 'releases a plane' do
+      subject.instruct_to_land plane
+        num_of_planes = subject.planes.count
+      subject.instruct_to_take_off
+        expect(subject.planes.count).to eq (num_of_planes - 1)
+      end
   end
 
   describe 'landing' do
-    xit 'instructs a plane to land'
+    it 'instructs a plane to land' do
+       expect(subject).to respond_to(:instruct_to_land).with(1).argument
+    end
 
-    xit 'receives a plane'
+    it 'receives a plane' do
+      num_of_planes = subject.planes.count
+      subject.instruct_to_land plane
+      expect(subject.planes.count).to eq (num_of_planes + 1)
+    end
   end
 
   describe 'traffic control' do
     context 'when airport is full' do
-      xit 'does not allow a plane to land'
+      it 'does not allow a plane to land' do
+        subject.capacity.times
+        allow(subject).to receive(:full?).and_return true
+        expect {subject.instruct_to_land plane}.to raise_error 'The airport is currently full'
+      end
     end
+
+    context 'when airport is empty' do
+      it 'does not allow a plane to take off' do
+        allow(subject).to receive(:empty?).and_return true
+        expect {subject.instruct_to_take_off}.to raise_error 'There are no planes at the airport'
+      end
+    end
+
 
     # Include a weather condition.
     # The weather must be random and only have two states "sunny" or "stormy".
@@ -40,9 +71,15 @@ describe Airport do
     # the plane can not land, and must not be in the airport
 
     context 'when weather conditions are stormy' do
-      xit 'does not allow a plane to take off'
-
-      xit 'does not allow a plane to land'
+      it 'does not allow a plane to take off' do
+        subject.instruct_to_land plane
+        allow(subject).to receive(:stormy?).and_return true
+        expect {subject.instruct_to_take_off}.to raise_error 'The weather is too stormy to take off'
+      end
+      it 'does not allow a plane to land' do
+        allow(subject).to receive(:stormy?).and_return true
+        expect {subject.instruct_to_land plane}.to raise_error 'The weather is too stormy to land'
+      end
     end
   end
 end
