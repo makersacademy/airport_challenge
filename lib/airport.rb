0481e63da47b468ad2a2555b01c3d54eeef8d27a
kyle macpherson <kyle1@kyles-MacBook-Pro.local>
2015-09-07 08:56:13 +0100

    refactored airport.rb

--- lib/airport.rb
+++ lib/airport.rb
@@ -4,50 +4,40 @@ class Airport
 
   attr_reader :capacity, :planes, :permission, :stormy
 
-def initialize(capacity=DEFAULT_CAPACITY)
-  @capacity = capacity
-  @planes = []
-end
-
-def stormy
-
-  if rand > 0.75
-    @stormy = true
-  else
-    @stormy = false
+  def initialize(capacity=DEFAULT_CAPACITY)
+    @capacity = capacity
+    @planes = []
   end
 
-end
-
-def permission
-
-  stormy
-
-  if planes.length >= capacity
-    @permission = false
-  elsif stormy == true
-    @permission = false
-  else
-    @permission = true
+  def stormy
+    if rand > 0.75
+      @stormy = true
+    else
+      @stormy = false
+    end
   end
-end
 
-def instruct_to_land plane
+  def permission
+    if planes.length >= capacity
+      @permission = false
+    elsif stormy == true
+      @permission = false
+    else
+      @permission = true
+    end
+  end
 
-  permission
-  fail "Do not have permission to land" if permission == false
-  plane.land
-  @planes << plane
+  def instruct_to_land plane
+    fail "Do not have permission to land" if permission == false
+    plane.land
+    planes << plane
+  end
 
-end
-
-def instruct_to_take_off plane
-
-  fail "Do not have permission to take off" if permission == false
-  fail 'Plane not in airport' unless @planes.include? plane
-  plane.take_off
-  @planes.delete(plane)
-end
-
+  def instruct_to_take_off plane
+    fail "Do not have permission to take off" if permission == false
+    fail 'Plane not in airport' unless @planes.include? plane
+    plane.take_off
+    planes.delete(plane)
+  end
 
 end
