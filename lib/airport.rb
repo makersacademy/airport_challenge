fd832c963db682efecad839878332166a38c777a
chweeks <chrisharryweeks@gmail.com>
2015-09-07 17:48:26 +0100

    refactored for doubles

--- lib/airport.rb
+++ lib/airport.rb
@@ -12,20 +12,32 @@ class Airport
     @weather = "sunny"
   end
 
-  def instruct_to_take_off plane
-    weather = "stormy" if rand(21) == 1
-    fail 'Cannot take off in stormy weather' if weather == "stormy"
-    fail 'That plane is not in hanger' if hanger.include?(plane) == false
-    hanger.delete plane
-    plane.status = "flying"
+  def random_weather
+    weather = 'stormy' if rand(20) == 1
   end
 
-  def instruct_to_land plane
-    weather = "stormy" if rand(21) == 1
+  def landing_checks plane
     fail 'Cannot land in stormy weather' if weather == "stormy"
     fail 'Airport is full' if hanger.size == capacity
     fail 'Plane already in hanger' if hanger.include?(plane) == true
+  end
+
+  def take_off_checks plane
+    fail 'Cannot take off in stormy weather' if weather == "stormy"
+    fail 'That plane is not in hanger' if hanger.include?(plane) == false
+  end
+
+  def instruct_to_take_off plane
+    random_weather
+    take_off_checks plane
+    plane.take_off
+    hanger.delete plane
+  end
+
+  def instruct_to_land plane
+    random_weather
+    landing_checks plane
     hanger << plane
-    plane.status == "landed"
+    plane.land
   end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,82 +1,77 @@
 require 'airport'
-require 'plane'
 
 describe Airport do
 
-  it 'Airport has a capacity' do
-    expect(subject.capacity).to eql Airport::DEFAULT_CAPACITY
-  end
+  let(:plane) { double :plane, land: nil, take_off: nil }
 
-  describe 'take off' do
-    it 'instructs a plane to take off' do
-      is_expected.to respond_to :instruct_to_take_off
+  context 'When weather is sunny' do
+
+    it 'Airport has a capacity' do
+      expect(subject.capacity).to eql Airport::DEFAULT_CAPACITY
     end
 
-    it 'releases a plane' do
-      plane = Plane.new
-      plane.status = "landed"
-      subject.hanger = [plane]
-      subject.instruct_to_take_off plane
-      expect(subject.hanger.include?(plane)).to be false
-    end
-
-    it 'changes status to flying' do
-      plane = Plane.new
-      plane.status = "landed"
-      subject.hanger = [plane]
-      subject.instruct_to_take_off plane
-      expect(plane.status).to eql "flying"
-    end
-
-    it 'can only instruct planes within hanger to take off' do
-      plane = Plane.new
-      expect{subject.instruct_to_take_off(plane)}.to raise_error
-       'That plane is not in hanger'
-    end
-  end
-
-  describe 'landing' do
-    it 'instructs a plane to land' do
-      (is_expected).to respond_to :instruct_to_land
-    end
-
-    it 'receives a landed plane' do
-      plane = Plane.new
-      subject.instruct_to_land plane
-      expect(subject.hanger.include?(plane)).to be true
-    end
-
-    it 'can only land planes not in hanger' do
-      plane = Plane.new
-      subject.hanger << plane
-      expect{subject.instruct_to_land(plane)}.to raise_error
-        'Plane already in hanger'
-    end
-  end
-
-  describe 'traffic control' do
-    context 'when airport is full' do
-      it 'does not allow a plane to land' do
-        plane = Plane.new
-        subject.capacity.times {subject.hanger << :plane}
-        expect{subject.instruct_to_land(plane)}.to raise_error 'Airport is full'
+    describe 'take off' do
+      it 'instructs a plane to take off' do
+        is_expected.to respond_to :instruct_to_take_off
       end
-    end
 
-    context 'when weather conditions are stormy' do
-      it 'does not allow a plane to take off' do
-        subject.stub(:rand).and_return( 1 )
-        plane = Plane.new
-        subject.hanger << plane
+      it 'releases a plane' do
+        subject.instruct_to_land plane
+        subject.instruct_to_take_off plane
+        expect(subject.hanger.include?(plane)).to be false
+      end
+
+      it 'instructs plane to take off' do
+        expect(plane).to receive :take_off
+        subject.instruct_to_land(plane)
+        subject.instruct_to_take_off plane
+      end
+
+      it 'can only instruct planes within hanger to take off' do
         expect{subject.instruct_to_take_off(plane)}.to raise_error
-          'Cannot take off in stormy weather'
+         'That plane is not in hanger'
       end
-      it 'does not allow a plane to land' do
-        subject.stub(:rand).and_return( 1 )
-        plane = Plane.new
+    end
+
+    describe 'landing' do
+      it 'instructs a plane to land' do
+        (is_expected).to respond_to :instruct_to_land
+      end
+
+      it 'receives a landed plane' do
+        subject.instruct_to_land plane
+        expect(subject.hanger.include?(plane)).to be true
+      end
+
+      it 'can only land planes not in hanger' do
+        subject.hanger << plane
         expect{subject.instruct_to_land(plane)}.to raise_error
-         'Cannot land in stormy weather'
+          'Plane already in hanger'
       end
     end
   end
+
+    describe 'traffic control' do
+      context 'when airport is full' do
+        it 'does not allow a plane to land' do
+          subject.capacity.times {subject.hanger << :plane}
+          expect{subject.instruct_to_land(plane)}.to raise_error 'Airport is full'
+        end
+      end
+
+      context 'when weather conditions are stormy' do
+
+        it 'does not allow a plane to take off' do
+          subject.stub(:weather).and_return('stormy')
+          subject.hanger << plane
+          expect{subject.instruct_to_take_off(plane)}.to raise_error
+            'Cannot take off in stormy weather'
+        end
+        it 'does not allow a plane to land' do
+          subject.stub(:weather).and_return('stormy')
+          expect{subject.instruct_to_land(plane)}.to raise_error
+           'Cannot land in stormy weather'
+        end
+      end
+    end
 end
