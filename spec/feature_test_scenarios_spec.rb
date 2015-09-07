722508fe9d7c044b46aa2dbc50ff2876ce379a14
chn.challenger <chn.challenger@gmail.com>
2015-09-07 01:20:27 +0100

    Working on scenarios for feature test

--- spec/feature_test_scenarios_spec.rb
+++ spec/feature_test_scenarios_spec.rb
@@ -4,10 +4,10 @@ require 'capybara/rspec'
 
 feature 'Stormy City Airport Rush Hour' do
 
-	stormy_airport = Airport.new
+	airport = Airport.new
 
 	scenario 'A new Airport object was created' do
-		expect(stormy_airport.class).to eq(Airport) 
+		expect(airport.class).to eq(Airport) 
 	end
 
 	created_planes = []
@@ -23,4 +23,26 @@ feature 'Stormy City Airport Rush Hour' do
 		expect(all_planes).to eq(true)
 	end
 
+	scenario 'All created planes are all flying' do
+		all_flying = true; i = 0
+		created_planes.length.times do
+			all_flying = false unless created_planes[i].flying
+			i += 1		
+		end
+		expect(all_flying).to eq(true)
+	end	
+
+	scenario "Can't land at the airport if weather is stormy" do
+		allow(airport).to receive(:weather).and_return('stormy')
+		expect{created_planes.first.land(airport)}.to raise_error 'Stormy weather'
+	end
+
+	scenario "Can't land at the airport if airport is full" do
+		allow(airport).to receive(:weather).and_return('sunny')
+		airport.capacity.times do
+			created_planes.last.land(airport)
+			created_planes.pop
+		end
+		expect{created_planes.last.land(airport)}.to raise_error 'Airport full'
+	end
 end
