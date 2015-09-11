79c6bd290a83d1df935f8c2ab9562b6037991380
Usman Jamil <usman.jamil@live.co.uk>
2015-09-11 16:45:08 +0100

    Removed all commented code

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,20 +1,9 @@
 require 'airport'
 
-## Note these are just some guidelines!
-## Feel free to write more tests!!
-
-# A plane currently in the airport can be requested to take off.
-#
-# No more planes can be added to the airport, if it's full.
-# It is up to you how many planes can land in the airport
-# and how that is implemented.
-#
-# If the airport is full then no planes can land
-
 describe Airport do
 
   let(:plane){ double :plane }
-  
+
   describe 'take off' do
     it { is_expected.to respond_to :take_off }
   end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -1,20 +1,5 @@
 require 'plane'
 
-## Note these are just some guidelines!
-## Feel free to write more tests!!
-
-# When we create a new plane, it should be "flying",
-# thus planes can not be created in the airport.
-#
-# When we land a plane at the airport, the plane in question should
-# be "landed"
-#
-# When the plane takes of from the airport, it should be "flying" again
-#
-# Think about your implementation - does it allow a plane to be "flying"
-# and landed?
-# Are you testing that?
-
 describe Plane do
   let (:plane) {Plane.new :destination}
   #it 'is flying when created'
