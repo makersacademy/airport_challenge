a2e50e53e79cc740e72fa7dd39640bea22774124
ana <adscarlos@gmail.com>
2015-09-28 16:05:04 +0100

    refactors stubs

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,52 +1,50 @@
 require 'airport'
-require 'plane'
 
 describe Airport do
 
   let(:plane) { double :plane, land!: true, take_off!: false }
-  let(:airport) { Airport.new }
 
   before(:each) do
-    allow(airport).to receive(:stormy?) { false }
+    allow(subject).to receive(:stormy?) { false }
   end
 
   it 'instructs a plane to land' do
-    expect(airport).to respond_to(:land).with(1).argument
+    expect(subject).to respond_to(:land).with(1).argument
   end
 
   it 'receives a plane' do
-    airport.land plane
-    expect(airport.landing_strip.count).to eq(1)
+    subject.land plane
+    expect(subject.landing_strip.count).to eq(1)
   end
 
   it 'instructs a plane to take off' do
-    expect(airport).to respond_to(:take_off).with(1).argument
+    expect(subject).to respond_to(:take_off).with(1).argument
   end
 
   it 'releases a plane' do
-    airport.take_off plane
-    expect(airport.landing_strip.count).to eq(0)
+    subject.take_off plane
+    expect(subject.landing_strip.count).to eq(0)
   end
 
   context 'when airport is full' do
     it 'does not allow a plane to land' do
-      airport.capacity.times { airport.land plane }
-      expect{ airport.land plane }.to raise_error
+      subject.capacity.times { subject.land plane }
+      expect{ subject.land plane }.to raise_error
       "Conditions are not right for landing"
     end
   end
 
   context 'when weather conditions are stormy' do
     it 'does not allow a plane to take off' do
-      airport.land plane
-      allow(airport).to receive(:stormy?) { true }
-      airport.take_off(plane)
-      expect(airport.landing_strip.count).to eq(1)
+      subject.land plane
+      allow(subject).to receive(:stormy?) { true }
+      subject.take_off(plane)
+      expect(subject.landing_strip.count).to eq(1)
     end
 
     it 'does not allow a plane to land' do
-      allow(airport).to receive(:stormy?) { true }
-      expect{ airport.land plane }.to raise_error
+      allow(subject).to receive(:stormy?) { true }
+      expect{ subject.land plane }.to raise_error
       "Conditions are not right for landing"
     end
   end
