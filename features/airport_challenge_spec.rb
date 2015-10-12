60017c2098577e0ed16e9cda3ac63829c6ab36e2
Sam Joseph <tansaku@gmail.com>
2015-10-12 15:43:11 +0100

    makes 2nd feature test pass and starts new for airport full blocking landing

--- features/airport_challenge_spec.rb
+++ features/airport_challenge_spec.rb
@@ -10,4 +10,13 @@ describe 'Airport Challenge' do
     airport = Airport.new
     airport.take_off(plane)
   end
+
+  specify 'a plane cannot be landed if the airport is full' do
+    plane1 = Plane.new
+    plane2 = Plane.new
+    airport = Airport.new
+    airport.land(plane1)
+    message = 'Cannot land since airport is full'
+    expect { airport.land(plane2) }.to raise_error message
+  end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -17,6 +17,9 @@ describe Airport do
   it { is_expected.to respond_to(:land).with(1).argument }
   # end
 
+  it { is_expected.to respond_to(:take_off).with(1).argument }
+
+
   describe 'take off' do
     xit 'instructs a plane to take off'
 
