65de112051dcd2bc40966be135f40c56b0c089fd
Norm Fasey <norm@fasey.net>
2015-10-17 19:14:21 +0100

    Some tidy up

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,6 +1,7 @@
 require 'airport'
 
 describe Airport do
+
   let(:airport_name) { "LHR" }
   subject { Airport.new(airport_name) }
   let(:flying_plane) { double(:plane, :id => 'BA535', :flying => true) }
@@ -11,14 +12,18 @@ describe Airport do
       allow(flying_plane).to receive(:flying=).with(anything).and_return(anything)
       allow(flying_plane).to receive(:location=).with(anything).and_return(anything)
   end
+
   context 'Showing the contents of the Hangar' do
     it { is_expected.to respond_to(:hangar)}
+
     it 'should return an array when asked for the contents of the hangat' do
       expect(subject.hangar).to be_instance_of Array
     end
   end
+
   context 'Airport Capacity' do
     it { is_expected.to respond_to(:capacity) }
+
     it 'Has a default capacity' do
       expect(subject.capacity).to eq Airport::DEFAULT_CAPACITY
     end
@@ -27,6 +32,7 @@ describe Airport do
       expect(airport.capacity).not_to eq Airport::DEFAULT_CAPACITY
     end
   end
+
   context 'An airports name' do
     it { is_expected.to respond_to(:name) }
 
@@ -34,34 +40,42 @@ describe Airport do
       expect(subject.name).to eq airport_name
     end
   end
+
   context 'Weather Conditions' do
     it { is_expected.to respond_to(:weather_conditions) }
+
     it 'Weather conditions are either sunny or stormy' do
       expect(subject.weather_conditions).to satisfy {|value| [:stormy, :sunny].include? value}
     end
   end
+
   context 'Landing Planes' do
     it { is_expected.to respond_to(:land).with(1).argument }
 
     it 'should land a plane if able to do so' do
       expect(subject.land(flying_plane)).to be true
     end
+
     it 'A landed plane should increase the contents of the hanger by 1' do
       number_landed_planes = subject.hangar.count
       subject.land(flying_plane)
       expect(subject.hangar.count).to eq number_landed_planes + 1
     end
+
     it 'A landed plane should now be in the hangar' do
       subject.land(flying_plane)
       expect(subject.hangar).to include(flying_plane)
     end
+
     it 'should not land a plane if already landed at this airport' do
       subject.land(flying_plane)
       expect { subject.land flying_plane }.to raise_error "Plane #{flying_plane.id} Cannot Land. Already Landed!"
     end
+
     it 'should not land a plane if already landed somewhere else' do
       expect { subject.land(grounded_plane) }.to raise_error "Plane #{grounded_plane.id} Cannot Land. Already Landed!"
     end
+
     it 'should not land a plane if the airport is full' do
       subject.capacity.times do |index|
         current_plane = double(:plane, :id => 'BA5' + index.to_s, :flying => true)
@@ -72,11 +86,13 @@ describe Airport do
       expect(subject.hangar.count).to eq subject.capacity
       expect { subject.land(flying_plane) }.to raise_error "Plane #{flying_plane.id} Cannot Land. Airport Full!"
     end
+
     it 'should not land a plane if it is stormy' do
       allow(subject).to receive(:weather_conditions).and_return(:stormy)
       expect { subject.land(flying_plane) }.to raise_error "Plane #{flying_plane.id} Cannot Land. Bad Weather!"
     end
   end
+
   context 'Planes Taking Off' do
     it {is_expected.to respond_to(:take_off).with(1).argument }
 
@@ -86,6 +102,7 @@ describe Airport do
       allow(flying_plane).to receive(:location).and_return(airport_name)
       expect(subject.take_off(flying_plane)).to be true
     end
+
     it 'Once a plane has taken off, it should decrease the number of landed planes by 1' do
       subject.land(flying_plane)
       allow(flying_plane).to receive(:flying).and_return(false)
@@ -94,6 +111,7 @@ describe Airport do
       subject.take_off(flying_plane)
       expect(subject.hangar.count).to eq number_landed_planes - 1
     end
+
     it 'Once a plane has taken off, it should no longer be in the hangar' do
       subject.land(flying_plane)
       allow(flying_plane).to receive(:flying).and_return(false)
@@ -101,6 +119,7 @@ describe Airport do
       subject.take_off(flying_plane)
       expect(subject.hangar).not_to include(flying_plane)
     end
+
     it 'Raises an error when trying to instruct a plane to take off from another airport' do
       subject.land(flying_plane)
       allow(flying_plane).to receive(:flying).and_return(false)
@@ -108,12 +127,15 @@ describe Airport do
       expect { subject.take_off(flying_plane) }.to raise_error "Plane #{flying_plane.id} Cannot Take Off. Wrong Airport #{flying_plane.location}"
 
     end
+
     it 'Raises an error when trying to instruct an unknown plane to take off' do
       expect { subject.take_off(grounded_plane)}.to raise_error "Plane #{grounded_plane.id} Cannot Take Off. Unknown Plane!"
     end
+
     it 'raises an error when trying to instruct an already flying plane to take off' do
       expect { subject.take_off(flying_plane) }.to raise_error "Plane #{flying_plane.id} Cannot Take Off. Already Taken Off!"
     end
+
     it 'Raises an error when trying to instruct a plane that is in the airport to take off in bad weather' do
       allow(subject).to receive(:weather_conditions).and_return(:stormy)
       expect { subject.take_off(grounded_plane) }.to raise_error "Plane #{grounded_plane.id} Cannot Take Off. Bad Weather!"
--- spec/feature_spec.rb
+++ spec/feature_spec.rb
@@ -2,37 +2,45 @@ require 'airport'
 require 'plane'
 
 describe 'Airport Simulator' do
+
   let(:id) { "BA535" }
   let(:capacity) { 5 }
   let(:airport_name) { "LHR" }
+
   before(:each) do
     @plane = Plane.new id
     @airport = Airport.new(airport_name, capacity)
     allow(@airport).to receive(:weather_conditions).and_return(:sunny)
     @plane.flying = true
   end
+
   context 'Airport Capacity' do
     it 'Has the default capacity when no argument is given' do
       airport = Airport.new("NYC")
       expect(airport.capacity).to eq Airport::DEFAULT_CAPACITY
     end
+
     it 'Has a different capacity when a argument is given' do
       airport = Airport.new("NYC", Airport::DEFAULT_CAPACITY+1)
       expect(airport.capacity).not_to eq Airport::DEFAULT_CAPACITY
     end
   end
+
   context 'Landing Planes' do
     it 'lands a plane in sunny weather' do
       expect(@airport.land(@plane)).to be true
     end
+
     it 'A landed plane now has a location set to that airport' do
       @airport.land(@plane)
       expect(@plane.location).to eq airport_name
     end
+
     it 'Raises an error when trying to land a plane in stormy weather' do
       allow(@airport).to receive(:weather_conditions).and_return(:stormy)
       expect { @airport.land(@plane) }.to raise_error "Plane #{@plane.id} Cannot Land. Bad Weather!"
     end
+
     it 'Raises an error when trying to land a plane when airport is full' do
       @airport.capacity.times do |i|
         id = "BA50" + i.to_s
@@ -42,31 +50,38 @@ describe 'Airport Simulator' do
       end
       expect { @airport.land(@plane) }.to raise_error "Plane #{@plane.id} Cannot Land. Airport Full!"
     end
+
     it 'Raises an error when trying to land a plane that is already landed at this airport' do
       @airport.land(@plane)
       expect { @airport.land(@plane) }.to raise_error "Plane #{@plane.id} Cannot Land. Already Landed!"
     end
   end
+
   context 'Planes Taking Off' do
+
     it 'Allows a plane to take off in Sunny Weather' do
       @airport.land(@plane)
       expect(@airport.take_off(@plane)).to be true
     end
+
     it 'A plane that has taken off no longer has a location set to the airport it has left' do
       @airport.land(@plane)
       @airport.take_off(@plane)
       expect(@plane.location).to be nil
     end
+
     it 'can only take off from the airport it has landed at' do
       @airport.land(@plane)
       @plane.location = "NYC"
       expect { @airport.take_off(@plane) }.to raise_error "Plane #{@plane.id} Cannot Take Off. Wrong Airport #{@plane.location}"
     end
+
     it 'Raises an error when trying to instruct a plane to take off in bad weather' do
       @airport.land(@plane)
       allow(@airport).to receive(:weather_conditions).and_return(:stormy)
       expect { @airport.take_off(@plane) }.to raise_error "Plane #{@plane.id} Cannot Take Off. Bad Weather!"
     end
+
     it 'Raises an error when trying to instruct a non existant plane to take off' do
       @plane.flying = false
       expect { @airport.take_off(@plane) }.to raise_error "Plane #{@plane.id} Cannot Take Off. Unknown Plane!"
