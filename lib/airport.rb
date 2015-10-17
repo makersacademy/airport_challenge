41a82894f9b91572bf9f14e272eedff9d8af6932
ALRW <awerner1@googlemail.com>
2015-10-17 15:11:55 +0100

    Updated airport capacity and control measures

--- lib/airport.rb
+++ lib/airport.rb
@@ -2,16 +2,39 @@ require './lib/plane'
 
 class Airport
 
+  DEFAULT_CAPACITY = 1
+
+  attr_reader :planes, :capacity
+
   def initialize
     @planes = []
+    @stormy = false
+    @capacity = DEFAULT_CAPACITY
+  end
+
+  def new_capacity=(capacity)
+    @capacity = capacity
+  end
+
+  def stormy?
+    (rand(50) == 4) ? @stormy = true : @stormy = false
   end
 
   def land(plane)
-    raise "This airport is full" if @planes.length >= 1
-    @planes << plane
+    if stormy?
+      fail "It is stormy"
+    elsif @planes.length >= DEFAULT_CAPACITY
+      fail "This airport is full"
+    else
+      @planes << plane
+    end
   end
 
   def takeoff(plane)
-    @planes.pop
+    if stormy?
+      fail "It is stormy"
+    else
+      @planes.pop
+    end
   end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,21 +2,49 @@ require 'airport'
 
 describe Airport do
   let(:plane) {double(:plane)}
-
+  let(:plane1){double(:plane1)}
+  let(:bad_airport){Airport.new}
   it 'allows planes to land' do
+    allow(subject).to receive(:stormy?) {false}
     expect(subject.land(plane)).to eq([plane])
   end
 
   it 'allows planes to takeoff' do
+    allow(subject).to receive(:stormy?) {false}
     subject.land(plane)
     expect(subject.takeoff(plane)).to eq(plane)
   end
 
   describe '#land' do
-    let(:plane1){double(:plane1)}
     it 'prevents planes landing when airport is full' do
+      allow(subject).to receive(:stormy?) {false}
       subject.land(plane1)
       expect{subject.land(plane)}.to raise_error("This airport is full")
     end
   end
+
+  it "prevents use when stormy" do
+    subject.land(plane1)
+    allow(subject).to receive(:stormy?) {true}
+    expect{subject.land(plane)}.to raise_error("It is stormy")
+    expect{subject.takeoff(plane1)}.to raise_error("It is stormy")
+  end
+
+  describe '#takeoff' do
+    it 'only takeoff present planes' do
+      allow(subject).to receive(:stormy?) {false}
+      subject.land(plane)
+      expect(bad_airport.takeoff(plane)).to be nil
+      expect(subject.planes.last).to eq(subject.takeoff(plane))
+    end
+  end
+
+  it 'has a default capacity' do
+    expect(subject.capacity).to eq Airport::DEFAULT_CAPACITY
+  end
+
+  it 'capacity can be overridden' do
+    expect(subject.new_capacity = 40).to eq(subject.capacity)
+  end
+
 end
