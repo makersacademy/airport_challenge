a68c1721c6b2c668fb2661e55799f5453420d089
Mattia46 <mattia.assogna@gmail.com>
2015-10-17 10:47:04 +0100

    pull request

---
+++ spec/airport_spec.rb
@@ -0,0 +1,14 @@
+require 'Airport'
+
+describe Airport do
+
+
+# subject(:airport){Airport.new}
+#
+# context 'airport' do
+#   it 'Plane can land' do
+#     expect(airport.plane).to be_land
+#   end
+# end
+
+end
