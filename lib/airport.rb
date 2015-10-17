422e2e80c95c0997c8409ad232ad50c66319d967
Mattia46 <mattia.assogna@gmail.com>
2015-10-17 14:10:19 +0100

    challenge 03 airport full

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,3 +1,29 @@
+require_relative 'plane'
+
 class Airport
+
+  attr_reader :plane, :planes, :capacity
+
+  def initialize
+    @planes = []
+    @capacity = 20
+  end
+
+  def land plane
+    fail 'if full' if full?
+    @planes << plane
+    plane
+  end
+
+  def take_off
+    @planes.pop
+  end
+
+  def full?
+      @planes.count >= capacity
+  end
+
+  
+
 
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,14 +1,34 @@
-require 'Airport'
+require 'airport'
 
 describe Airport do
 
 
-# subject(:airport){Airport.new}
-#
-# context 'airport' do
-#   it 'Plane can land' do
-#     expect(airport.plane).to be_land
-#   end
-# end
+#subject(:airport){Airport.new}
 
-end
+  #context 'airport' do
+    it 'Airport to land a plane' do
+      plane = Plane.new
+      expect(subject.land(plane)).to eq plane
+    end
+
+    # it 'plane can not land because the weather is stormy' do
+    #   plane = Plane.new
+    #   subject.land(plane)
+    #   expect{subject.stormy).to raise_error('not possible to land because of the storm')
+    # end
+
+      it 'plane can take off the airport' do
+        plane = Plane.new
+        subject.land(plane)
+        expect(subject.take_off).to eq plane
+      end
+
+
+      it 'not land if full' do
+        subject.capacity.times { subject.land(Plane.new) }
+        expect { subject.land(Plane.new) }.to raise_error
+      end
+
+
+
+    end
