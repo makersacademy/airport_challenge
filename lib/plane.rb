dcc39689408c2d30df8f8e45ce422156f60dc1ea
thisdotrob <thisdotrob@gmail.com>
2015-10-18 21:13:52 +0100

    Add waether behaviour and tests

--- lib/plane.rb
+++ lib/plane.rb
@@ -1,12 +1,16 @@
+require_relative 'weather'
+
 class Plane
 
   def land(airport)
+    raise 'Too stormy.' if Weather.stormy?
     airport.land(self)
     @location = airport
     @flying = false
   end
 
   def take_off
+    raise 'Too stormy.' if Weather.stormy?
     @location.take_off(self)
     @location = nil
     @flying = true
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -2,22 +2,30 @@ require 'plane'
 
 describe Plane do
 
-  let(:airport) { double('airport', land: nil, take_off: nil)}
+  let(:airport) { double('airport', land: nil, take_off: nil) }
 
   it 'lands on command' do
+    allow(Weather).to receive(:stormy?).and_return(false)
     subject.land(airport)
     expect(subject).to_not be_flying
   end
 
   it 'takes-off on command' do
+    allow(Weather).to receive(:stormy?).and_return(false)
     subject.land(airport)
     subject.take_off
     expect(subject).to be_flying
   end
 
   it 'refuses to land if airport full' do
+    allow(Weather).to receive(:stormy?).and_return(false)
     allow(airport).to receive(:land).and_raise('Airport full.')
     expect { subject.land(airport) }.to raise_error 'Airport full.'
+  end
+
+  it 'refuses to land if weather is stormy' do
+    allow(Weather).to receive(:stormy?).and_return(true)
+    expect { subject.land(airport) }.to raise_error 'Too stormy.'
   end
 
 end
