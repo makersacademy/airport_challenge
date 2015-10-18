6c5b19451c2305027b0ba41c80569d6d20a9770d
Yev Dyko <evgeny.dyko@gmail.com>
2015-10-19 02:05:23 +0300

    Added a default capacity of 25 planes

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,8 +1,11 @@
 class Airport
+  def initialize
+    @planes = []
+  end
 
   def land(plane)
-    fail 'Plane can\'t land. Airport is full!' if @plane
-    @plane = plane
+    fail 'Plane can\'t land. Airport is full!' if @planes.length >= 25
+    @planes << plane
   end
 
   def take_off(plane)
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -8,7 +8,7 @@ describe Airport do
     end
 
     it 'raise an error when an airport is full' do
-      subject.land(Plane.new)
+      25.times { subject.land(Plane.new) }
       expect { subject.land(Plane.new) }.to raise_error 'Plane can\'t land. Airport is full!'
     end
   end
