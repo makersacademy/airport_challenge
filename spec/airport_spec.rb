2f8ae2fcfeb87edebdcba688bd71dad6a9eba13e
danwheeler <da-wheeler@hotmail.co.uk>
2015-10-18 15:04:54 +0100

    more rspec tests passed

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -21,27 +21,27 @@ describe Airport do
 
 	context "When landing it" do	
 		it "instructs a plane to land" do
-			allow(weather).to receive(:stormy?).and_return false
+			allow(weather).to receive(:stormy?) {false}
 			allow(plane).to receive(:has_landed)
 			allow(plane).to receive(:is_flying?).and_return true
 			subject.land(plane)
 			expect(subject.send(:planes)).to eq [plane]
 		end
 		it "prevents a plane landing when its not flying" do
-			allow(weather).to receive(:stormy?).and_return false
+			allow(weather).to receive(:stormy?) {false}
 			allow(plane).to receive(:has_landed)
 			allow(plane).to receive(:is_flying?).and_return false
 			expect{subject.land(plane)}.to raise_error "Plane is not flying"
 		end
 		it "prevents a plane landing when the airport is full" do
-			allow(weather).to receive(:stormy?).and_return false
+			allow(weather).to receive(:stormy?) {false}
 			allow(plane).to receive(:has_landed)
 			allow(plane).to receive(:is_flying?).and_return true
 			Airport::DEFAULT_CAPACITY.times {subject.land(plane)}
 			expect{subject.land(plane)}.to raise_error "Airport is full"
 		end
 		it "prevents a plane landing when the weather is stormy" do
-			allow(weather).to receive(:stormy?).and_return true
+			allow(weather).to receive(:stormy?) {true}
 			allow(plane).to receive(:has_landed)
 			allow(plane).to receive(:is_flying?).and_return true
 			expect{subject.land(plane)}.to raise_error "Too stormy to land"
@@ -51,7 +51,7 @@ describe Airport do
 	
 	context "When taking-off it" do
 		it "instructs a plane to take_off only if its in the airport" do
-			allow(weather).to receive(:stormy?).and_return false
+			allow(weather).to receive(:stormy?) {false}
 			allow(plane).to receive(:is_flying?).and_return true
 			allow(plane).to receive(:has_landed)	
 			subject.land(plane)
@@ -61,13 +61,13 @@ describe Airport do
 			expect(subject.send(:planes)).to eq []
 		end
 		it "prevents a plane taking-off when the weather is stormy" do
-			allow(weather).to receive(:stormy?).and_return true
+			allow(weather).to receive(:stormy?) {true}
 			allow(plane).to receive(:is_flying?).and_return false
 			allow(plane).to receive(:has_taken_off)
 			expect(subject.take_off(plane)).to raise_error "Too stormy to take off"
 		end
 		it "prevents a plane taking-off when its flying" do
-			allow(weather).to receive(:stormy?).and_return false
+			allow(weather).to receive(:stormy?) {false}
 			allow(plane).to receive(:is_flying?).and_return true
 			allow(plane).to receive(:has_taken_off)
 			expect{subject.take_off(plane)}.to raise_error "Plane is currently flying" 
--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -1,10 +1,9 @@
 require 'weather'
 
 describe Weather do
-	let(:random_outlook) { double :random_outlook}
-
+	
 	it "It returns whether it is stormy or not" do
-		allow(random_outlook).to receive(:stormy)
+		allow(Weather::OUTLOOKS).to receive(:sample).and_return(:stormy)
 		expect(subject.stormy?).to eq true
 	end
 end
