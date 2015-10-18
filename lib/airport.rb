b009952131527df8ee288c4be5e022e690416e39
mahmudh <mahmudhssn246@gmail.com>
2015-10-18 13:16:28 +0100

    plane cannot land or take off when the weather is stormy

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,20 +1,33 @@
 require_relative 'plane'
 
 class Airport
-  attr_reader :plane
+  attr_reader :plane, :capacity, :stormy
+
+  def initialize(capacity=1)
+    @planes = []
+    @stormy = false
+    @capacity = capacity
+  end
 
   def land_plane plane
-    fail "The airport is full"
-    @stormy == true ? 'Cannot land plane' : @plane = plane
+    fail 'The airport is full' if full?
+    fail 'Is stormy so cannot land' if @stormy
+    @planes << plane
   end
 
   def take_off
+    fail 'Is stormy so cannot take off' if @stormy
     # @plane = @plane - 1
-    @stormy == true ? 'Cannot land plane' : 'You are able to land plane'
+    #@stormy == true ? 'Cannot land plane' : @planes.pop
   end
 
   def stormy
     @stormy = true
+  end
+
+  private
+  def full?
+    @planes.count >= @capacity
   end
 
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,7 +1,7 @@
 require 'airport'
 
 describe Airport do
-  let(:plane) {double :plane}
+  let(:plane) {Plane.new}
 
   it 'instruct a plane to land' do
     expect(subject).to respond_to(:land_plane)
@@ -12,10 +12,17 @@ describe Airport do
   end
 
   it 'raise an error if airport is full' do
-    expect{subject.land_plane(plane)}.to raise_error("The airport is full")
+    subject.capacity.times { subject.land_plane(plane) }
+    expect{subject.land_plane(plane)}.to raise_error('The airport is full')
   end
 
-  it 'cannot land or take off if the weather is stormy' do
-    expect(subject.stormy).to eq true
+  it 'raise an error if weather is stormy when landing' do
+    subject.stormy
+    expect{subject.land_plane(plane)}.to raise_error('Is stormy so cannot land')
+  end
+
+  it 'raise an error if weather is stormy when taking off' do
+    subject.stormy
+    expect{subject.take_off}.to raise_error('Is stormy so cannot take off')
   end
 end
