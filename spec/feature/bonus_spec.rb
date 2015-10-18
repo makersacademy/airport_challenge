80dd96dfdfd49d2753f8dfebf3f6583c7b89bf23
samover <samuel.overloop@gmail.com>
2015-10-18 15:35:15 +0100

    added bonus_feature_spec testing landing and takeoff of 10 planes

--- spec/feature/bonus_spec.rb
+++ spec/feature/bonus_spec.rb
@@ -6,19 +6,33 @@
 # # After all planes have landed, they should all take off again.
 # # All planes have taken off when they have status :flying
 #
-# feature 'mega busy airport' do
-#   scenario 'all planes will land despite averse weather' do
-#     airport = Airport.new
-#     planes =
-#     10.times do
-#       plane = Plane.new
-#       begin
-#         airport.clear_for_landing plane
-#       rescue
-#         airport.clear_for_landing plane
-#       end
-#       expect(plane.status).to eq :landed
-#     end
-#     expect(airport.planes.length).to eq 10
-#   end
-# end
+feature 'mega busy airport' do
+  scenario 'all planes can land and all planes can take off' do
+    airport = Airport.new
+    planes = Array.new(10, Plane.new)
+
+    until planes.sort == airport.planes.sort do
+      planes.each do |plane|
+        begin
+          airport.clear_for_landing plane
+        rescue
+          break
+        end
+      end
+    end
+
+    planes.each { |plane| expect(plane.status).to eq :landed }
+
+    until airport.planes.empty? do
+      planes.each do |plane|
+        begin
+          airport.clear_for_takeoff plane
+        rescue
+          break
+        end
+      end
+    end
+    
+    planes.each { |plane| expect(plane.status).to eq :flying }
+  end
+end
