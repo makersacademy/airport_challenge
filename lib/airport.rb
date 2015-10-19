0e9cdccda72cf49af70c36a9e7158df4948c42d8
Aaron Sweeney <aaron.sweeney@ucdconnect.ie>
2015-10-19 13:04:00 +0100

    Added test for take_off (passed)

--- lib/airport.rb
+++ lib/airport.rb
@@ -3,25 +3,24 @@ class Airport
 
   DEFAULT_CAPACITY = 20
 
-  # this should be refactored out, it's a hangover from a previous version
-  # use 'attr_reader :planes' instead, or better, create a custom method to
+  # use 'attr_reader :planes' instead, or better,
+  # create a custom method to
   # check whether or not a plane has landed
-  attr_reader :plane
+  attr_reader :planes
 
-  def initialize
+  def initialize(capacity = DEFAULT_CAPACITY)
     @planes = []
+    @capacity = capacity
   end
 
+
   def land(plane)
-    # this line is redundant, 'plane' is a local variable of the method already
-    @plane = plane
     fail 'Airport is full' if full?
     @planes << plane
   end
 
-  # this method shouldn't create a new plane out of thin air!
   def take_off(plane)
-    Plane.new
+    @planes.pop
   end
 
 private
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -6,28 +6,27 @@ describe Airport do
 
   it { is_expected.to respond_to(:take_off).with(1).argument}
 
-  # this test only makes sense for the earlier single-plane version
+
+
+describe '#land' do
   it 'can see a landed plane' do
-    # should use a dummy here
-    plane1 = Plane.new
+    plane1 = (double :plane)
     subject.land(plane1)
-    # this should read: expect(subject.planes).to include plane1
-    expect(subject.plane).to eq plane1
+    expect(subject.planes).to include plane1
   end
+end
+
+describe '#take_off' do
+  it 'lets a plane take off' do
+    expect(subject).to respond_to(:take_off)
+  end
+end
 
   it 'should raise an error when airport is full do' do
-    # use 'described_class' instead of 'Airport'
-    # use dummies rather than real planes
-    Airport::DEFAULT_CAPACITY.times { subject.land(Plane.new) }
-    expect {subject.land(Plane.new)}.to raise_error 'Airport is full'
+    described_class::DEFAULT_CAPACITY.times { subject.land(double :plane) }
+    expect {subject.land(double :plane)}.to raise_error 'Airport is full'
   end
 
-  it 'should raise an error for landing if weather is stormy' do
 
-  end
-
-  # it 'should raise an error for taking off if weather is stormy' do
-  #
-  # end
 
 end
