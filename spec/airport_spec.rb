43f61dc8aec818a3397aa24223bf174d57755514
EleniSkouroupathi <EleniSkouroupathi@users.noreply.github.com>
2015-10-19 03:53:52 +0100

    Update airport_spec.rb

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -16,13 +16,13 @@ describe Airport do
     expect(subject.take_off(plane)).to eq plane
   end
 
-  it 'should prevent planes landiing when full' do
-    a = Airport.new(1)
-    allow(a.weather).to receive(:stormy?).and_return(false)
-    pl = Plane.new
-    a.land(pl)
+  it 'should prevent planes from landing when full' do
+    heathrow = Airport.new(1)
+    allow(heathrow.weather).to receive(:stormy?).and_return(false)
+    boeing = Plane.new
+    heathrow.land(boeing)
     message = 'Airport is full'
-    expect{a.land(Plane.new)}.to raise_error(message)
+    expect{heathrow.land(Plane.new)}.to raise_error(message)
   end
 
   it 'should prevent planes from landing when weather is stormy' do
@@ -33,16 +33,16 @@ describe Airport do
 
   it 'should prevent planes from taking off when the weather is stormy' do
     allow(subject.weather).to receive(:stormy?).and_return(false, true)
-    pl = Plane.new
-    subject.land(pl)
+    boeing = Plane.new
+    subject.land(boeing)
     message = 'Weather is stormy'
-    expect{subject.take_off(pl)}.to raise_error(message)
+    expect{subject.take_off(boeing)}.to raise_error(message)
   end
 
   it 'should only allow planes to take off if they are in airport' do
     allow(subject.weather).to receive(:stormy?).and_return(false)
-    pl = Plane.new
-    expect { subject.take_off(pl) }.to raise_error('The plane is not in this airport')
+    boeing = Plane.new
+    expect { subject.take_off(boeing) }.to raise_error('The plane is not in this airport')
   end
 
   it 'should have a default capacity' do
