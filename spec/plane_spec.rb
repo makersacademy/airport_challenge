c4ad6370a279a7721a9bf9774a2181656fec9616
Rebecca Collins <rebeccacollins@Rebeccas-MacBook-Pro-2.local>
2015-10-19 08:27:32 +0100

    refactored again

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -2,37 +2,38 @@ require 'airport'
 
 describe Plane do
   let (:stormy) { double :stormy}
-  it 'when a plane lands it notes the airport' do
-    my_airport = Airport.new
-    my_plane = Plane.new
-    allow(my_airport).to receive(:stormy).and_return(false)
-    my_airport.land(my_plane)
 
-    expect(my_plane.airport_location(my_airport)).to eq my_airport
-  end
+  context 'create airport & plane instance' do
+    before 'create airport & plane instance' do
+      @my_airport = Airport.new
+      @my_plane = Plane.new
+    end
+      it 'when a plane lands it notes the airport' do
 
-  it 'check plane has no airport id when flying' do
-    my_airport = Airport.new
-    my_plane = Plane.new
-    allow(my_airport).to receive(:stormy).and_return(false)
-    my_airport.land(my_plane)
-    my_airport.takeoff(my_plane)
+        allow(@my_airport).to receive(:stormy).and_return(false)
+        @my_airport.land(@my_plane)
 
-    expect(my_plane.flying).to be true
-    expect(my_plane.airport).to be nil
-  end
+        expect(@my_plane.airport_location(@my_airport)).to eq @my_airport
+      end
 
-  it 'check plane that is not flying cannot land and must be in an airport' do
-    my_airport = Airport.new
-    my_plane = Plane.new
-    allow(my_airport).to receive(:stormy).and_return(false)
-    my_airport.land(my_plane)
+      it 'check plane has no airport id when flying' do
+        allow(@my_airport).to receive(:stormy).and_return(false)
+        @my_airport.land(@my_plane)
+        @my_airport.takeoff(@my_plane)
 
-    expect{my_airport.land(my_plane)}.to raise_error 'already in airport'
-  end
+        expect(@my_plane.flying).to be true
+        expect(@my_plane.airport).to be nil
+      end
+
+      it 'check plane that is not flying cannot land and must be in an airport' do
+        allow(@my_airport).to receive(:stormy).and_return(false)
+        @my_airport.land(@my_plane)
+
+        expect{@my_airport.land(@my_plane)}.to raise_error 'already in airport'
+      end
 
 
 
-
+    end
 
 end
