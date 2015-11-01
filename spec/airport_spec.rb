21cf86fc6a1be779cc3867b8bcf468bf9ab678ab
octavianrotari <octavianrotari@gmail.com>
2015-11-01 02:09:52 +0000

    User story 4
    	As an air traffic controller
    	So that I can avoid accidents
    	I want to prevent airplanes landing or taking off when the weather
    	is stormy

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -24,18 +24,27 @@ describe Airport do
         end
       end
 
-    context 'when stormy' do
-      it 'raises an error if asked to land a plane when stormy' do
-        allow(airport).to receive(:stormy?).and_return true
-        expect { airport.land(plane) }.to raise_error 'Cannot land plane: weather is stormy'
+      context 'when stormy' do
+        it 'raises an error if asked to land a plane ' do
+          allow(airport).to receive(:stormy?).and_return true
+          expect { airport.land(plane) }.to raise_error 'Cannot land plane: weather is stormy'
+        end
       end
     end
-  end
 
 
   describe '#take_off' do
-    it 'instruct a plane to take off' do
-      expect(airport).to respond_to(:take_off).with(1).argument
+    context 'when not stormy' do
+      it 'instruct a plane to take off' do
+        expect(airport).to respond_to(:take_off).with(1).argument
+      end
+    end
+
+    context 'when stormy' do
+      it 'raises an error if asked to take off a plane' do
+        allow(airport).to receive(:stormy).and_return true
+        expect { airport.take_off(plane) }.to raise_error 'Cannot take off plane: weather is stormy'
+      end
     end
   end
 end
