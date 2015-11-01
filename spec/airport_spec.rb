18eee844f437735c45d32176eb755347c0a509f3
octavianrotari <octavianrotari@gmail.com>
2015-11-01 00:44:14 +0000

    refactor of previos commit

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,21 +2,28 @@ require 'airport.rb'
 
 describe Airport do
   subject(:airport) { described_class.new(20) }
+  let(:plane) { double :plane }
 
-  it 'instructs a plane to land' do
-    expect(airport).to respond_to(:land).with(1).argument
-  end
+  describe '#land' do
 
-  it 'instruct a plane to take off' do
-    expect(airport).to respond_to(:take_off).with(1).argument
-  end
-
-  it 'does not allow landing when at capacity' do
-    plane = double :plane
-    20.times do
-      airport.land(:plane)
+    it 'instructs a plane to land' do
+      expect(airport).to respond_to(:land).with(1).argument
     end
-    expect { airport.land(:plane) }.to raise_error 'Cannot land plane: airport full'
+
+    context 'when full' do
+      it 'raises an error' do
+        20.times do
+          airport.land(plane)
+        end
+        expect { airport.land(plane) }.to raise_error 'Cannot land plane: airport full'
+      end
+    end
+
   end
 
+  describe '#take_off' do
+    it 'instruct a plane to take off' do
+      expect(airport).to respond_to(:take_off).with(1).argument
+    end
+  end
 end
