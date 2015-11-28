4be9e6ee35f227988440cad39d149575891c71d3
emmabeynon <emma.beynon@gmail.com>
2015-11-28 14:03:30 +0000

    Added @in_airport_status and methods to change it in Plane class. Also created tests for these.

--- lib/Plane.rb
+++ lib/Plane.rb
@@ -1,5 +1,23 @@
 class Plane
+  attr_reader :in_airport_status
+
+  def initialize
+    @in_airport_status = nil
+  end
+
+  def take_off
+    @in_airport_status = false
+  end
+
+  def land
+    @in_airport_status = true
+  end
+
   def landed?
-    true
+    @in_airport_status
+  end
+
+  def in_airport?
+    @in_airport_status
   end
 end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -1,9 +1,40 @@
-require 'Plane'
+require 'plane'
 
 describe Plane do
+  describe '#initialize' do
+    it 'should create an in_airport instance variable' do
+      expect(subject).to respond_to :in_airport_status
+    end
+  end
+
+  describe '#take_off' do
+    it 'should make a plane take off' do
+      subject.take_off
+      expect(subject.in_airport_status). to eq false
+    end
+  end
+
+  describe '#land' do
+    it 'should land a plane' do
+      subject.land
+      expect(subject.in_airport_status). to eq true
+    end
+  end
+
   describe '#landed?' do
     it 'should confirm that a plane has landed' do
+      subject.land
       expect(subject.landed?).to eq true
     end
+  end
+
+  describe '#in_airport?' do
+    it 'should confirm that the plane is no longer in the airport' do
+      subject.take_off
+      expect(subject.in_airport?).to eq false
+    end
+  end
+
+  it 'should not be possible for a plane to be landed and not in the airport' do
   end
 end
