dadff3e8e0737a2eb6518df316c0045d8b03eadc
Thomas Bradley <bradley.tom.r@gmail.com>
2015-11-28 21:31:50 +0000

    Implements land method

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -4,5 +4,10 @@ describe Airport do
 
   describe '#land' do
     it { is_expected.to respond_to(:land).with(1).argument }
+
+    it 'lands a plane at the airport' do
+      plane = double(:plane)
+      expect(subject.land(plane)).to eq [plane]
+    end
   end
 end
