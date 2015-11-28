5e56944f20e84cf77dd91ff9c98c43f39b7c28df
hibreez <4ngus.whitehead@gmail.com>
2015-11-28 17:51:18 +0000

    prevent landing and takeoff when weather stormy

--- lib/airport.rb
+++ lib/airport.rb
@@ -7,18 +7,24 @@ class Airport
 
   def land plane
     fail 'That is not a plane!' if plane.class != Plane
+    fail 'The weather is too stormy!' if stormy?
     planes << plane
     self
   end
 
   def takeoff plane
-    fail 'That plane is not at this airport!' unless contain? plane
+    fail 'Plane not found!' unless contain? plane
+    fail 'The weather is too stormy!' if stormy?
     planes.delete(plane)
     self
   end
 
   def contain? plane
     planes.include? plane
+  end
+
+  def stormy?
+    rand(20) == 0 ? true : false
   end
 
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,38 +1,64 @@
 require 'airport'
 
 describe Airport do 
+
+  storm_seed = (1..100).select{|n| srand(n); rand(20) == 0 }[0]
+  sun_seed = storm_seed - 1
   let(:plane) { double :plane, class: Plane }
 
   describe '#land' do
-
-    it 'can instruct plane to land' do
+   it 'can instruct plane to land' do
+      srand(sun_seed)
       subject.land plane
       expect(subject.planes.include? plane).to eq true
     end
 
+    it 'will not allow landing in stormy weather' do
+      srand(storm_seed)
+      expect{subject.land plane}.to raise_error 'The weather is too stormy!'
+    end
   end
 
-  describe '#takeoff' do
-  
+  describe '#takeoff' do  
     it 'can instruct plane to take off' do
+      srand(sun_seed)
       subject.land plane
+      srand(sun_seed)
       subject.takeoff plane
       expect(subject.planes.include? plane).to eq false
+    end
+
+    it 'will not allow takeoff in stormy weather' do
+      srand(sun_seed)
+      subject.land plane
+      srand(storm_seed)
+      expect{subject.takeoff plane}.to raise_error 'The weather is too stormy!'
     end
 
   end
 
   describe '#contain' do
-
     it 'can confirm contains plane' do
+      srand(sun_seed)
       subject.land plane
       expect(subject.contain? plane).to eq true
     end
 
     it "can confirm does not contain plane" do
       expect(subject.contain? plane).to eq false
+    end  
+  end
+
+  describe '#stormy?' do
+    it 'can check whether weather is stormy' do
+      srand(storm_seed)
+      expect(subject.stormy?).to eq true
     end
-  
+
+    it 'can check whether weather is sunny' do
+      srand(sun_seed)
+      expect(subject.stormy?).to eq false
+    end
   end
 
 end
