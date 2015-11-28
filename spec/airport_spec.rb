10c7eca33f9032a4d6e23da5463523ff6a0d1d16
michaellennox <michaelctlennox@gmail.com>
2015-11-28 21:21:45 +0000

    Passed test to ensure airport responds to #land, wrote failing test to ensure airport responds to planes

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -5,5 +5,10 @@ describe Airport do
   subject(:airport) { described_class.new }
 
   it { is_expected.to respond_to(:land).with(1).argument }
+  it { is_expected.to respond_to(:planes) }
+
+  describe '#land' do
+
+  end
 
 end
