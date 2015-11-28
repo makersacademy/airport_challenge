e051d02a8268a0f794241a2c629cf05af69485a4
emmabeynon <emma.beynon@gmail.com>
2015-11-28 09:27:49 +0000

    Created test to check if landed plane is stored in the airport and method to store plane

--- lib/Airport.rb
+++ lib/Airport.rb
@@ -1,7 +1,12 @@
 class Airport
+  attr_reader :landed_planes
+
+  def initialize
+    @landed_planes = []
+  end
 
   def instruct_to_land(plane)
-    plane
+    landed_planes << plane
   end
 
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -3,8 +3,14 @@ require 'Airport'
 describe Airport do
   it {is_expected.to respond_to :instruct_to_land}
 
-  it 'should return a landed plane' do
+  it 'should land a plane' do
     plane = double(:plane)
-    expect(subject.instruct_to_land(plane)).to eq plane
+    expect(subject.instruct_to_land(plane)).to eq [plane]
+  end
+
+  it 'should check that the plane is in the airport' do
+    plane = double(:plane)
+    subject.instruct_to_land(plane)
+    expect(subject.landed_planes).to eq [plane]
   end
 end
