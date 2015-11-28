208b43ec6cac940df76ea50a06c723ad72d26867
giamir <giamir.buoncristiani@gmail.com>
2015-11-28 12:09:07 +0000

    define and implement land method for Plane

--- lib/plane.rb
+++ lib/plane.rb
@@ -1,2 +1,9 @@
 class Plane
+  attr_reader :flying
+  alias_method :flying?, :flying
+
+  def land
+    @flying = false
+  end
+  
 end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -1,4 +1,13 @@
 require 'plane'
 
 describe Plane do
+  describe '#land' do
+    it { is_expected.to respond_to(:land) }
+    it 'set flying status to false' do
+      subject.land
+      expect(subject.flying?).to eq false
+    end
+  end
+
+
 end
