c6c7d207aa5f32108a919d3a5f89162c63cb5c6a
Chris Wynne <chris.wynndow@gmail.com>
2015-11-28 13:15:42 +0000

    Refactors airport spec file with nested describe blocks

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -4,22 +4,28 @@ describe Airport do
   subject(:airport) {described_class.new}
   let(:plane) {double(:plane)}
 
-  it 'allows the creation of an airport' do
-    array = []
-    array << airport
-    expect(array).to eq [airport]
-  end
-
   it {is_expected.to respond_to(:land).with(1).argument}
   it {is_expected.to respond_to(:planes)}
 
-  it 'allows a plane to land' do
-    expect(airport.land(plane)).to eq plane
+  describe 'creating an airport' do
+    it 'allows the creation of an airport' do
+      array = []
+      array << airport
+      expect(array).to eq [airport]
+    end
   end
 
-  it 'allows confirmation that the plane has landed' do
-    airport.land(plane)
-    expect(airport.planes).to eq plane
+  describe '#land' do
+    it 'allows a plane to land' do
+      expect(airport.land(plane)).to eq plane
+    end
+  end
+
+  describe '#planes' do
+    it 'allows confirmation that the plane has landed' do
+      airport.land(plane)
+      expect(airport.planes).to eq plane
+    end
   end
 
 end
