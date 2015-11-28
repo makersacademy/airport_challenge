f7134fd180a5edcd1be1bf7861eb9b5514e8f784
hibreez <4ngus.whitehead@gmail.com>
2015-11-28 16:31:45 +0000

    test contains returning false

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -4,16 +4,25 @@ describe Airport do
   let(:plane) { double :plane }
 
   describe '#land' do
+
     it 'can land plane' do
       subject.land(plane)
       expect(subject.planes.include? plane).to eq true
     end
+
   end
 
   describe '#contain' do
+
     it 'can confirm contains plane' do
       subject.land(plane)
       expect(subject.contain? plane).to eq true
     end
+
+    it "can confirm doesn't contain plane" do
+      expect(subject.contain? plane).to eq false
+    end
+  
   end
+
 end
--- spec/feature_spec.rb
+++ spec/feature_spec.rb
@@ -4,6 +4,7 @@ require 'airport'
 describe 'Feature: ' do
 
   let(:plane) { Plane.new }
+  let(:empty_airport) { Airport.new }
   let(:airport_with_plane) { Airport.new.land plane }
 
   it 'can land plane at airport' do
@@ -11,9 +12,14 @@ describe 'Feature: ' do
     expect(airport.planes.include? plane).to eq true
   end
 
-  it 'can confirm whether airport contains plane' do
+  it 'can confirm airport contains plane' do
     airport = airport_with_plane
     expect(airport.contain? plane).to eq true
+  end
+
+  it 'can confirm airport doesnt contain plane' do
+    airport = empty_airport
+    expect(airport.contain? plane).to eq false
   end
 
 end
