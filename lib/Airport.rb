567a51cf82e8c049d57680c92358701329dc1bc2
emmabeynon <emma.beynon@gmail.com>
2015-11-28 08:53:41 +0000

    Added a plane argument to instruct_to_land method and created test to return plane

--- lib/Airport.rb
+++ lib/Airport.rb
@@ -1,6 +1,7 @@
 class Airport
 
-  def instruct_to_land
+  def instruct_to_land(plane)
+    plane
   end
 
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,4 +2,9 @@ require 'Airport'
 
 describe Airport do
   it {is_expected.to respond_to :instruct_to_land}
+
+  it 'should return a landed plane' do
+    plane = double(:plane)
+    expect(subject.instruct_to_land(plane)).to eq plane
+  end
 end
