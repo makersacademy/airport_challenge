cb72dc65c334e5cf015d6aa8802c3502aec48f30
Sara Tateno <sara.tateno@gmail.com>
2015-11-28 20:58:45 +0000

    Added test requiring the land method to give a confirmation of the plane

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,6 +1,13 @@
 require 'airport'
 
 describe Airport do
-  it { is_expected.to respond_to(:land).with(1).argument }
+  describe '#land' do
+    it { is_expected.to respond_to(:land).with(1).argument }
+
+    it 'tells the controller that the plane has landed' do
+      plane = double(:plane)
+      expect(subject.land(plane)).to eq plane
+    end
+  end
 
 end
