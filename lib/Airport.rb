7d988b5dab84bcaa1649d4829805a6512172cb86
emmabeynon <emma.beynon@gmail.com>
2015-11-28 14:12:34 +0000

    Created method to test instruct_to_land. Edited instruct_take_off to pass responsibility for changing in_airport_status to Plane class.  Created tests for these.

--- lib/Airport.rb
+++ lib/Airport.rb
@@ -1,3 +1,5 @@
+require_relative 'plane'
+
 class Airport
   attr_reader :landed_planes
 
@@ -6,7 +8,18 @@ class Airport
   end
 
   def instruct_to_land(plane)
+    raise "The plane is already in the airport" if plane.in_airport_status == true
     landed_planes << plane
+    plane.land
   end
 
+  def instruct_take_off
+    raise "There are no planes in the airport" if @landed_planes == []
+    plane = @landed_planes.pop
+    plane.take_off
+  end
+
+  def weather
+    weather_condition = rand(1..5)
+  end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,16 +1,60 @@
-require 'Airport'
+require 'airport'
 
 describe Airport do
-  it {is_expected.to respond_to :instruct_to_land}
+  describe '#instruct_to_land' do
+    it {is_expected.to respond_to :instruct_to_land}
 
-  it 'should land a plane' do
-    plane = double(:plane)
-    expect(subject.instruct_to_land(plane)).to eq [plane]
+    it 'should land a plane' do
+      plane = double(:plane, in_airport_status: false, land: true)
+      subject.instruct_to_land(plane)
+      expect(subject.landed_planes).to eq [plane]
+    end
+
+    it 'should check that the plane is in the airport' do
+      plane = double(:plane, in_airport_status: false, land: true)
+      subject.instruct_to_land(plane)
+      expect(subject.landed_planes).to eq [plane]
+    end
+
+    it "it should change the plane's status to 'in airport'" do
+      plane = double(:plane, in_airport_status: true, land: true)
+      plane.land
+      expect(plane.in_airport_status).to eq true
+    end
+
+    it "it should raise an error if the plane is in the airport" do
+      plane = double(:plane, in_airport_status: true)
+      expect{ subject.instruct_to_land(plane) }.to raise_error "The plane is already in the airport"
+    end
   end
 
-  it 'should check that the plane is in the airport' do
-    plane = double(:plane)
-    subject.instruct_to_land(plane)
-    expect(subject.landed_planes).to eq [plane]
+  describe '#instruct_take_off' do
+    it {is_expected.to respond_to :instruct_take_off}
+
+    it 'should let a plane take off' do
+      plane = double(:plane, in_airport_status: false, take_off: false)
+      subject.landed_planes << plane
+      subject.instruct_take_off
+      expect(subject.landed_planes).not_to eq [plane]
+    end
+
+    it 'should raise an error if no planes are available to take off' do
+      landed_planes = []
+      expect { subject.instruct_take_off }.to raise_error "There are no planes in the airport"
+    end
+
+    it "should change the plane's status to not in airport" do
+      plane = double(:plane, in_airport_status: false)
+      expect(plane.in_airport_status).to eq false
+    end
+  end
+
+  describe '#weather' do
+    it 'should generate a number to indicate weather' do
+      subject.weather
+      test_values = [1,2,3,4,5]
+      Kernel.stub!(:rand).and_return(*test_values)
+  #    Kernel.expects(:rand).with(4).returns(1)
+    end
   end
 end
