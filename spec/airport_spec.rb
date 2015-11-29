f473edbf22c8b0dd2e2ed1763b6af44dfcb8a2b7
michaellennox <michaelctlennox@gmail.com>
2015-11-29 15:17:26 +0000

    Added failing test to check that plane cannot fly while weather stormy

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -4,42 +4,66 @@ describe Airport do
 
   subject(:airport) { described_class.new }
   let(:plane) { double(:plane, land: nil, take_off: nil) }
+  let(:weather) { double(:weather) }
 
   it 'is expected to initialize with @planes set to an empty array' do
     expect(airport.planes).to eq []
   end
 
-  describe '#land' do
+  context 'while weather is fine' do
 
-    it 'is expected to add the plane passed as argument to the @planes array' do
-      airport.land(plane)
-      expect(airport.planes).to include plane
+    before do
+      allow(weather).to receive(:stormy?).and_return(false)
     end
 
-    it 'is expected to instruct the plane to make a landing' do
-      expect(plane).to receive(:land)
-      airport.land(plane)
+    describe '#land' do
+
+      it 'is expected to add the plane to the @planes array' do
+        airport.land(plane)
+        expect(airport.planes).to include plane
+      end
+
+      it 'is expected to instruct the plane to make a landing' do
+        expect(plane).to receive(:land)
+        airport.land(plane)
+      end
+
+    end
+
+    describe '#take_off' do
+
+      it 'is expected to raise an error if plane isn\'t at airport' do
+        not_here = 'Plane is not at this airport'
+        expect { airport.take_off(plane) }.to raise_error not_here
+      end
+
+      it 'is expected to instruct the plane to take off' do
+        airport.land(plane)
+        expect(plane).to receive(:take_off)
+        airport.take_off(plane)
+      end
+
+      it 'is expected to remove the plane from the @planes array' do
+        airport.land(plane)
+        airport.take_off(plane)
+        expect(airport.planes).not_to include plane
+      end
+
     end
 
   end
 
-  describe '#take_off' do
+  context 'while weather is stormy' do
 
-    it 'is expected to raise an error if plane isn\'t at airport' do
-      not_here = 'Plane is not at this airport'
-      expect { airport.take_off(plane) }.to raise_error not_here
+    before do
+      allow(weather).to receive(:stormy?).and_return(false)
     end
 
-    it 'is expected to instruct the plane to take off' do
-      airport.land(plane)
-      expect(plane).to receive(:take_off)
-      airport.take_off(plane)
-    end
-
-    it 'is expected to remove the plane from the @planes array' do
-      airport.land(plane)
-      airport.take_off(plane)
-      expect(airport.planes).not_to include plane
+    describe '#land' do
+      it 'is expected to raise an error due to weather' do
+        stormy_error = 'Weather too stormy to land'
+        expect { airport.land(plane) }.to raise_error stormy_error
+      end
     end
 
   end
