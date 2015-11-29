fcaa65d9844bbfacd8923298dec26adf1de79c91
Jamie Brown <jamie@Jamies-MacBook-Pro.local>
2015-11-29 14:55:00 +0000

    scraped code and start from scratch. Add land and land confirmation

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -1,13 +1,11 @@
 require "plane.rb"
 
 describe Plane do
-
-  it 'can take_of from a an airport' do
-    expect(subject).to respond_to(:take_off).with(1).arguments
-  end
-
-  it 'can confirm it has taken_off?' do
-    subject.take_off(double(:airport))
-    expect(subject.taken_off?).to be(true)
+  describe '#landed' do
+    it 'can confirm that is has landed' do
+      airport = Airport.new
+      airport.land(subject)
+      expect(subject.flying).to eq(false)
+    end
   end
 end
