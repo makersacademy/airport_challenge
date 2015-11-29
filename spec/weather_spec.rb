1488fdc068f84d9d4daa34bff17c912f837bde44
Sara Tateno <sara.tateno@gmail.com>
2015-11-29 20:34:43 +0000

    Corrected random weather tet

--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -3,9 +3,9 @@ require 'weather'
 describe Weather do
   it { is_expected.to respond_to(:change) }
 
-  it 'changes the weather to stormy' do
+  it 'changes the weather to stormy with a random function' do
+    allow(Kernel).to receive(:rand).with(100) {1}
     subject.change
-    allow(Kernel).to receive(:rand).with(100) {5}
     expect(subject.is_stormy).to eq true
   end
 
