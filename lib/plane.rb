71fa2488e017772137db2dc3b48fd786309e08de
hibreez <4ngus.whitehead@gmail.com>
2015-11-29 22:23:44 +0000

    reordered plane methods

--- lib/plane.rb
+++ lib/plane.rb
@@ -1,8 +1,16 @@
 class Plane
-
   def initialize
     @flying = true
     @airport = nil
+  end
+
+  def flying?
+    @flying
+  end
+  
+  def airport
+    fail 'Plane is currently flying!' if flying?
+    @airport
   end
 
   def land! airport
@@ -15,14 +23,5 @@ class Plane
     fail 'Plane already flying!' if flying?
     @airport = nil
     @flying = true
-  end
-
-  def flying?
-    @flying
-  end
-  
-  def airport
-    fail 'Plane is currently flying!' if flying?
-    @airport
   end
 end
