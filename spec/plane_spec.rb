9cc8767db6a281b1a627f3cecc66179957219fc0
Jamie Brown <jamie@Jamies-MacBook-Pro.local>
2015-11-29 12:21:41 +0000

    added land method and landed? confirmation

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -2,12 +2,13 @@ require "plane.rb"
 
 describe Plane do
 
-  describe 'land' do
-
+  describe '#land' do
     it 'can instruct a plan to land' do
       expect(subject).to respond_to(:land).with(1).arguments
     end
+  end
 
+  describe '#landed?' do
     it 'confirms that it is landed' do
       subject.land(double(:airport))
       expect(subject.landed?).to be(true)
