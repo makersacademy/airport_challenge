dbe2c4192c318ae7e9378cd926620c105b3ea33d
michaellennox <michaelctlennox@gmail.com>
2015-11-29 14:40:58 +0000

    Passed previous test, wrote failing test to check Weather initializes as fine

--- lib/weather.rb
+++ lib/weather.rb
@@ -1,3 +1,5 @@
 class Weather
 
+  attr_reader :weather
+  
 end
--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -2,6 +2,11 @@ require 'weather'
 
 describe Weather do
 
+  subject(:weather) { described_class.new }
+
   it { is_expected.to respond_to(:weather) }
-  
+
+  it 'is expected to initialize with weather set to :fine' do
+    expect(weather.weather).to eq(:fine)
+  end
 end
