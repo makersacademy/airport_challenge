34531c27e79b59b83a334be3ac456e07cb6e7e2d
Thomas Bradley <bradley.tom.r@gmail.com>
2015-11-29 23:57:11 +0000

    Added weather class and defined and implemented storm? method.

---
+++ lib/weather.rb
@@ -0,0 +1,5 @@
+class Weather
+  def storm?
+    rand(2) == 0
+  end
+end
---
+++ spec/weather_spec.rb
@@ -0,0 +1,7 @@
+require 'weather'
+
+describe Weather do
+  describe 'storm?' do
+    it { is_expected.to respond_to(:storm?) }
+  end
+end
