b25878588785127a1182b8045b1d7f1d73b50d04
hibreez <4ngus.whitehead@gmail.com>
2015-11-29 14:13:10 +0000

    added feature test

--- spec/feature_spec.rb
+++ spec/feature_spec.rb
@@ -1,8 +1,24 @@
 require 'plane'
 require 'airport'
 
-describe 'Feature: ' do
+describe 'Feature Test' do
   let(:plane) { Plane.new }
-  let(:empty_airport) { Airport.new }
+  let(:airport) { Airport.new weather, 10 }
+  let(:weather) { Weather.new}
+  storm_seed = (1..100).select { |n| srand(n); rand(Weather::DEFAULT) == 0 }[0]
+  sun_seed = storm_seed - 1
+
+  it 'can land and take off multiple planes' do
+    planes = []
+    10.times { planes << Plane.new }
+    planes.each { |plane| srand(sun_seed); airport.land plane }
+    expect(airport.contain? planes[0]).to eq true
+    srand(sun_seed)
+    expect{airport.land plane}.to raise_error 'The airport is full!'
+    srand(storm_seed)
+    expect{airport.land plane}.to raise_error 'It is too stormy!'
+    planes.each { |plane| srand(sun_seed); airport.take_off plane}
+    expect(airport.contain? planes[0]).to eq false
+  end
 
 end
