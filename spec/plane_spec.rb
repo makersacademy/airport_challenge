a8699dc3e5668364706daa49d644ea41e4fb1be7
Jamie Brown <jamie@Jamies-MacBook-Pro.local>
2015-11-29 13:27:03 +0000

    add unit test for plane to take_off and confirm it has taken_off? Also switched around land and landed? to be a Airport method

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -2,16 +2,12 @@ require "plane.rb"
 
 describe Plane do
 
-  describe '#land' do
-    it 'can instruct a plan to land' do
-      expect(subject).to respond_to(:land).with(1).arguments
-    end
+  it 'can take_of from a an airport' do
+    expect(subject).to respond_to(:take_off).with(1).arguments
   end
 
-  describe '#landed?' do
-    it 'confirms that it is landed' do
-      subject.land(double(:airport))
-      expect(subject.landed?).to be(true)
-    end
+  it 'can confirm it has taken_off?' do
+    subject.take_off(double(:airport))
+    expect(subject.taken_off?).to be(true)
   end
 end
