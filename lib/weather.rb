fc1f6b46e4fbd03d9d37694a3e2f824839228746
Alan Gabbianelli <alan.gabbianelli@gmail.com>
2015-11-29 21:49:19 +0000

    weather can be stormy

---
+++ lib/weather.rb
@@ -0,0 +1,7 @@
+class Weather
+
+  def stormy?
+    rand(1..5) == 1
+  end
+
+end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -9,7 +9,7 @@ describe Airport do
     it { is_expected.to respond_to(:land).with(1).argument }
 
     it 'should allow a plane to land' do
-      allow(plane).to receive(:land).and_return(:landed)
+      allow(plane).to receive(:land) {:landed}
       airport.land(plane)
       expect(airport.planes).to include plane
     end
@@ -19,8 +19,8 @@ describe Airport do
     it { is_expected.to respond_to(:take_off).with(1).argument }
 
     it 'should allow a plane to takeoff' do
-      allow(plane).to receive(:land).and_return(:landed)
-      allow(plane).to receive(:take_off).and_return(:taken_off)
+      allow(plane).to receive(:land) {:landed}
+      allow(plane).to receive(:take_off) {:taken_off}
       airport.land(plane)
       airport.take_off(plane)
       expect(airport.planes).not_to include plane
