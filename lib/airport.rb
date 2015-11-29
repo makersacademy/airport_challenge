1ac1d9bd5bc9ca164b9debfb9071ed08beac9769
Adam Lancaster <lancaster28@hotmail.com>
2015-11-29 10:14:46 +0000

    Airport raises an error when at capacity

--- lib/airport.rb
+++ lib/airport.rb
@@ -2,23 +2,31 @@ require_relative 'plane'
 
 class Airport
 
-def initialize
+MAX_CAPACITY = 40
+
+def initialize(capacity=MAX_CAPACITY)
   @planes = []
+  @capacity = capacity
 end
 
 attr_reader :planes
-
-#MAX_CAPACITY = 40
-
+attr_reader :capacity
 
 def land(plane)
+    fail "Airport full! No Planes can land" if full?
     @planes << plane
 end
+
 
 def take_off(plane)
   plane
 end
 
+private
+
+def full?
+  @planes.length >= MAX_CAPACITY
+end
 
 
 
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -8,35 +8,35 @@ let (:plane) {double :plane}
     it { is_expected.to respond_to(:planes)}
 
 
-  describe "#land"
+  describe "#land" do
     it "Lands a Plane" do
-      expect(subject.land(plane)).to eq plane
-    end
-
-    it "Can land more than one plane" do
-      subject.land(plane)
-      subject.land(plane)
-      expect(subject.planes).to eq [planes]
+      expect(subject.land(plane)).to eq [plane]
     end
 
     it "Can tell us which planes have landed" do
       subject.land(plane)
-      expect(subject.planes).to eq [planes]
+      expect(subject.planes).to eq [plane]
     end
 
-  describe "#take off"
+    it "Raises an error when at capacity" do
+        subject.land(plane)
+        expect {Airport::MAX_CAPACITY.times do
+          subject.land(plane) end}.to raise_error "Airport full! No Planes can land"
+    end
+
+
+  describe "#take off" do
     it "Allow a plane to take off" do
     expect(subject.take_off(plane)).to eq plane
+    end
   end
+end
 
-  #  it "has a default capacity of MAX_CAPACITY" do
-  #    expect(subject.capacity).to eq Airport::MAX_CAPACITY
-  #  end
+#  it "has a default capacity of MAX_CAPACITY" do
+#    expect(subject.capacity).to eq Airport::MAX_CAPACITY
+#  end
 
-  #  it "Raises an error when at capacity" do
-#      Airport::MAX_CAPACITY.times { subject.land(plane) }
-#      expect(subject.land(plane)).to raise_error "Airport full! No Planes can land"
-#    end
+
 
 
 end
