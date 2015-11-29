d59b2cdcf5bc9eeb4c706c633926cbdab74c1110
michaellennox <michaelctlennox@gmail.com>
2015-11-29 15:00:09 +0000

    Passed previous test, wrote failing test to ensure stormy? can return try

--- lib/weather.rb
+++ lib/weather.rb
@@ -1,4 +1,6 @@
 class Weather
 
-  
+  def stormy?
+  end
+
 end
--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -4,10 +4,15 @@ describe Weather do
 
   subject(:weather) { described_class.new }
 
-  it { is_expected.to respond_to(:weather) }
+  it { is_expected.to respond_to(:stormy?) }
 
-  it 'is expected to initialize with weather set to :fine' do
-    expect(weather.weather).to eq(:fine)
+  describe '#stormy?' do
+
+    it "is expected to be able to return true" do
+      allow(weather).to receive(:rand).and_return(1)
+      expect(weather.stormy?).to be true
+    end
+
   end
 
 end
