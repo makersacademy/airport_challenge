dec409eb8f8027f4ae777b3a415bfc095053b5ce
emmabeynon <emma.beynon@gmail.com>
2015-11-30 12:20:56 +0000

    Created a subject for Weather.new called weather and replaced all references to subject in the tests with weather

--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -1,15 +1,16 @@
 require 'weather'
 
 describe Weather do
+  subject(:weather) { described_class.new }
   describe '#weather_conditions' do
     it 'should report stormy weather' do
-      allow(subject).to receive(:rand) { 5 }
-      expect(subject.weather_conditions).to eq true
+      allow(weather).to receive(:rand) { 5 }
+      expect(weather.weather_conditions).to eq true
     end
 
     it 'should report fine weather' do
-      allow(subject).to receive(:rand) { 4 }
-      expect(subject.weather_conditions).to eq false
+      allow(weather).to receive(:rand) { 4 }
+      expect(weather.weather_conditions).to eq false
     end
   end
 end
