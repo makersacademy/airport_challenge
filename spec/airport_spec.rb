ce879eaeacb30ca9c895a3bd9e80c7eae501d33d
Tony Young <ty_training@hotmail.co.uk>
2015-11-30 08:30:05 +0000

    added plane doubles and context blocks

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,23 +2,19 @@ require 'airport'
 
 describe Airport do
 
+let(:plane) {double :plane, confirmation: 'landed'}
+
   describe 'landing' do
 
-      it 'allows to land and confirm landing' do
-        plane = Plane.new
+      it 'lands plane and confirms landing' do
+        plane = double(:plane, status: 'landed', confirmation: 'landed')
         weather = double(:weather, condition: false)
         subject.land(plane, weather)
         expect(plane.confirmation).to eq "landed"
       end
 
-      it 'prevents landing when stormy' do
-        plane = Plane.new
-        weather = double(:weather, condition: true)
-        expect {subject.land(plane, weather)}.to raise_error 'cannot land, too stormy'
-      end
-
       it 'prevents landing when airport is full' do
-        plane = Plane.new
+        plane = double(:plane, status: "")
         weather = double(:weather, condition: false)
         20.times {subject.land(plane, weather)}
         expect {subject.land(plane, weather)}.to raise_error 'cannot land plane, airport full'
@@ -27,27 +23,36 @@ describe Airport do
 
   describe 'take_off' do
 
-      it 'allows to take off and confirm taking off' do
-        plane = Plane.new
+      it 'allows and confirms take off' do
+        plane = double(:plane, status: 'flying', confirmation: 'flying')
         weather = double(:weather, condition: false)
         subject.land(plane, weather)
         subject.take_off(plane, weather)
         expect(plane.confirmation).to eq 'flying'
       end
 
-      it 'prevents take off if stormy' do
-        plane = Plane.new
-        weather = double(:weather, condition: false)
-        subject.land(plane, weather)
-        weather = double(:weather, condition: true)
-        expect {subject.take_off(plane, weather)}.to raise_error 'cannot take off, too stormy'
+      context 'when stormy'do
+        it 'prevents landing' do
+          plane = double(:plane, status: "")
+          weather = double(:weather, condition: true)
+          expect {subject.land(plane, weather)}.to raise_error 'cannot land, too stormy'
+        end
+
+        it 'prevents take off' do
+          plane = double(:plane, status: 'landed')
+          weather = double(:weather, condition: false)
+          subject.land(plane, weather)
+          weather = double(:weather, condition: true)
+          expect {subject.take_off(plane, weather)}.to raise_error 'cannot take off, too stormy'
       end
     end
+
+  end
 
     describe 'initialization' do
 
       it 'has a variable capacity' do
-        plane = Plane.new
+        plane = double(:plane, status: 'flying')
         weather = double(:weather, condition: false)
         airport = Airport.new(30)
         30.times {airport.land(plane, weather)}
