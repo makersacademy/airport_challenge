8a8dfd0d1b19a782f3a21f77dbf5ae5d3a2ee968
Adam Lancaster <lancaster28@hotmail.com>
2015-11-30 01:39:40 +0000

    all passing localy

--- lib/airport.rb
+++ lib/airport.rb
@@ -13,26 +13,10 @@ end
 attr_reader :planes
 attr_reader :capacity
 
-def land(plane)
-    fail "Airport full! No Planes can land!" if full?
-    fail "Conditions too stormy. No planes can land." if stormy?
-    fail "That plane is already on the ground!" if present?(plane)
-    plane.landed!
-    planes << plane
-end
-
-def take_off(plane)
-    fail "That plane isn't here!" unless present?(plane)
-    fail "Conditions too stormy. No planes can take off." if stormy?
-    removes_plane_from_airport(plane)
-    makes_flying(plane)
-    plane
-end
 
 def present?(plane)
   planes.include?(plane)
 end
-
 
 private
 
@@ -52,6 +36,24 @@ end
 
 def full?
   planes.length >= MAX_CAPACITY
+end
+
+public
+
+def land(plane)
+    fail "Airport full! No Planes can land!" if full?
+    fail "Conditions too stormy. No planes can land." if stormy?
+    fail "That plane is already on the ground!" if present?(plane)
+    planes << plane
+end
+
+
+def take_off(plane)
+    fail "That plane isn't here!" unless present?(plane)
+    fail "Conditions too stormy. No planes can take off." if stormy?
+    removes_plane_from_airport(plane)
+    makes_flying(plane)
+    plane
 end
 
 end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -12,17 +12,22 @@ describe Plane do
 
   it "Is not flying when it lands" do
     airport = Airport.new
+    allow(airport).to receive(:stormy?).and_return(false)
     airport.land(subject)
     expect(subject.flying?).to eq false
   end
 
   it "Makes flying true when flying." do
-    subject.flying!
+    airport = Airport.new
+    allow(airport).to receive(:stormy?).and_return(false)
+    airport.land(subject)
+    airport.take_off(subject)
     expect(subject.flying?).to eq true
   end
 
   it "Is flying after it takes off" do
     airport = Airport.new
+    allow(airport).to receive(:stormy?).and_return(false)
     airport.land(subject)
     airport.take_off(subject)
     expect(subject.flying?).to eq true
