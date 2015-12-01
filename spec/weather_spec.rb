c39d01e559dfb2ab33dc6a84bebdb5aad3b698ee
vegim qehaja <med.batt.uk@gmail.com>
2015-12-01 13:49:45 +0000

    MAC-AC: weather test changes

--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -6,14 +6,6 @@ describe Weather do
     expect(subject).to be
   end
 
-  xit 'Weather has weather report method' do
-    expect(subject).to respond_to(:weather_report)
-  end
-
-  xit 'weather report method returns string' do
-    expect(subject.weather_report().class).to be(String)
-  end
-
   it 'weather report method can return stormy' do
      allow(subject).to receive(:weather_check).and_return("stormy")
      expect(subject.weather_check).to eq("stormy")
