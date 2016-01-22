c8ecfca6ee34756b04225c27e2e501c24733b03e
Mark Hill <futonfutonda@gmail.com>
2016-01-22 22:34:45 +0000

    edited plane

--- lib/plane.rb
+++ lib/plane.rb
@@ -1,11 +1,20 @@
+require_relative 'weather.rb'
 class Plane
 
   def land(airport)
-    @landed = true
+    if Weather.new.check_weather == "sunny"
+      @landed = true
+    else
+      @landed = false
+    end
   end
 
   def takeoff
-    @landed = false
+    if Weather.new.check_weather == "sunny"
+      @landed = false
+    else
+      @landed = true
+    end
   end
 
   def landed?
