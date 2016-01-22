e7eb93c109a8fdf197c098f4fcd65158988b9d35
Tom Pickard <tcpickard94@gmail.com>
2016-01-22 20:14:11 +0000

    Refactored to use one line double

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -7,8 +7,7 @@ describe Plane do
     it {expect(subject).to respond_to(:land).with(1).argument}
     
     it 'lands at an airport' do
-      airport = double(:airport)
-      allow(airport).to receive(:landed?).and_return('Plane has touched down!!')
+      airport = double(:landed? => 'Plane has touched down!!')
       expect(subject.land(airport)).to eq 'Plane has touched down!!'
     end
   end
