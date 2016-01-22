a0e9dcdaf36c4c3ba12d057bfa9b4ed4478acc7f
Mark Hill <futonfutonda@gmail.com>
2016-01-22 22:21:46 +0000

    completed weather

---
+++ lib/weather.rb
@@ -0,0 +1,9 @@
+class Weather
+  def check_weather
+    if rand(100) < 75
+      "sunny"
+    else
+      "stormy"
+    end
+  end
+end
--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -1,12 +1,15 @@
 require 'weather'
 
 describe Weather do
+
   it 'is mostly sunny'do
     a = []
-    100.times do
-      [] << Weather.new
-      end
-    expect(a.count(sunny)).to be > 50
-    expect(a.count(stormy)).to be > 5
+    100.times{ a << subject.check_weather }
+    expect(a.count("sunny")).to be > 50
+  end
+  it 'is sometimes stormy'do
+    a = []
+    100.times{ a << subject.check_weather }
+    expect(a.count("stormy")).to be > 5
   end
 end
