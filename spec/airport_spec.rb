a59a319699f48d378e5db743aa469f3c21c78f1c
Heather <campbell.hf@gmail.com>
2016-01-23 13:27:20 +0000

    01 user story complete: instruct a plane to land and check that it has landed

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,5 +1,17 @@
-require_relative 'airport'
+require 'airport'
 
-describe Airport do 
+describe Airport do
+
+  describe '#land' do
+
+    it { is_expected.to respond_to(:land).with(1).argument }
+
+    it 'check if plane has landed' do
+      airport = Airport.new
+      plane = Plane.new
+      airport.land(plane)
+      expect(plane.landed?).to eq true
+    end
+  end
 
 end
