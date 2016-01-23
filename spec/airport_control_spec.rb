60554b3fe51a90490212223d80a670a03bd2de96
innlouvate <l.j.franklin85@googlemail.com>
2016-01-23 23:57:21 +0000

    file structure

---
+++ spec/airport_control_spec.rb
@@ -0,0 +1,19 @@
+#instruct a plane to land
+
+
+#confirm plane has landed
+
+
+#prevent landing if stormy
+
+
+#prevent landing if airport is full
+
+
+#instruct plane to take-off
+
+
+#confirm plane has taken off
+
+
+#prevent take-off if stormy
