46540c5ab704eb054e6bad5cf3e3fe9ccbb36d4f
russellvaughan <russellvaughan@gmail.com>
2016-01-23 15:54:39 +0000

    airport lands planes with doubles

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -4,7 +4,7 @@ it { is_expected.to respond_to(:land).with(1).argument }
 
 	it '#land' do
 	airport = described_class.new
-	plane = Plane.new
+	plane = double(:plane)
 	airport.land(plane)
 	expect(airport.planes).to include plane
 	end	
--- spec/feature_spec.rb
+++ spec/feature_spec.rb
@@ -1,8 +1,7 @@
-
 describe 'Landing a plane' do
 	it 'lands a plane' do 
-		airport = Airport.new
 		plane = Plane.new
+		airport = Airport.new
 		airport.land(plane)
 		expect(airport.planes).to include plane
 	end
