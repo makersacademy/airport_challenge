15d85eba82cff6cb90a6ab9f999e171ecc7e9d7e
arnoldmanzano <arnold.manzano@gmail.com>
2016-01-23 15:18:14 +0000

    Added test for Plane #land

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -2,4 +2,12 @@ require 'plane'
 
 describe Plane do
 
+  describe '#land' do
+    it { is_expected.to respond_to(:land) }
+
+    it 'has landed' do
+      subject.land
+      expect(subject.landed).to eq true
+    end
+  end
 end
