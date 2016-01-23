162820f30ff0f682fd747f85519d3ccf3ec49660
Iryna Howarth <irynahowarth@gmail.com>
2016-01-23 19:55:46 +0000

    Fix some tests/refactor

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -3,8 +3,9 @@ require "airport"
 describe Airport do
 
   subject(:airport){ described_class.new }
+  let(:plane){ Plane.new}
 
-  context "status of the airportcle" do
+  context "status of the airport" do
 
     it {is_expected.to respond_to :planes}
 
@@ -14,38 +15,34 @@ describe Airport do
 
   end
 
-  context "instruct plan to #land" do
+  describe "instruct plan to #land" do
 
     it {is_expected.to respond_to :land}
 
     it "- give a confirmation that particular plane landed" do
-      plane = Plane.new
-      expect(subject.land plane).to eq "#{plane} has landed"
+      expect(subject.land(plane)).to eq "#{plane} has landed"
     end
 
     it "- keeps info about landed planes" do
-      plane = Plane.new
-      subject.land plane
+      subject.land(plane)
       expect(subject.planes).to eq [plane]
     end
 
   end
 
-  context "instruct plan to #takeoff" do
+  describe "instruct plan to #takeoff" do
 
     it {is_expected.to respond_to :takeoff}
 
     it "- give a confirmation that particular plane took off" do
-      plane = Plane.new
-      expect(subject.takeoff plane).to eq "#{plane} has taken off"
+      expect(subject.takeoff(plane)).to eq "#{plane} has taken off"
     end
 
     it "- update it's info about plane if it took off" do
-      plane = Plane.new
       plane1 = Plane.new
-      subject.land plane
-      subject.land plane1
-      subject.takeoff plane
+      subject.land(plane)
+      subject.land(plane1)
+      subject.takeoff(plane)
       expect(subject.planes).to eq [plane1]
     end
 
