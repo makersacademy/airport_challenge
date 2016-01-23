b8df13dc70e657a67d42740caa6b6e1281027b4f
Michael Collins <michaelj.collinsuk@gmail.com>
2016-01-23 21:12:36 +0000

    Add landed_planes to airport_spec

--- lib/airport.rb
+++ lib/airport.rb
@@ -6,11 +6,16 @@ class Airport
     @landed_planes = []
   end
 
+  def landed_planes
+    @landed_planes.dup
+  end
+
   def clearance?
     true
   end
 
   def plane_lands(plane)
+    raise "Plane does not have clearance to land" if plane.clearance == false
     @landed_planes << plane
   end
 
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -10,8 +10,7 @@ describe Airport do
   end
   describe '#plane_lands' do
     it 'a plane lands and it is stored at the airport' do
-      # pl = Plane.new
-      # pl.request_clearance(subject)
+      allow(plane).to receive(:clearance).and_return(true)
       landed_planes = [plane]
       expect(subject.plane_lands(plane)).to eq landed_planes
     end
@@ -22,11 +21,13 @@ describe Airport do
     end
   end
 
-
-
-
-
-
+  describe '#landed_planes' do
+    it 'returns an array with a copy of the planes at the airport' do
+      allow(plane).to receive(:clearance).and_return(true)
+      subject.plane_lands(plane)
+      expect(subject.landed_planes).to eq [plane]
+    end
+  end
 end
 
 
