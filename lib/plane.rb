39479abc0599dcba13faf138f8f10379bb1c71c8
Tom Pickard <tcpickard94@gmail.com>
2016-01-23 17:26:34 +0000

    Added extra plane functionality

--- lib/plane.rb
+++ lib/plane.rb
@@ -1,11 +1,26 @@
 require_relative 'airport'
 
 class Plane
+
+  def initialize
+    @flying = false
+    @on_ground = true
+  end
+  
   def land(airport) 
+    fail "Already on the ground" if on_ground
     airport.landed(self)
   end
   
   def take_off(airport)
+    fail "Already in the air" if flying
+    @flying = true
+    @on_ground = false
     airport.leave_gate
   end
+  
+  private
+  
+  attr_reader :flying, :on_ground
+  
 end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -1,33 +1,45 @@
 require 'plane'
 
 describe Plane do
+  let(:leave) {double(leave_gate: 'Plane has taken off!!')}
   it {expect(subject).to be_a Plane}
   
   describe 'Plane should be able to land' do
+    let(:landed) { double(landed: 'Plane has touched down!!') }
+    
     it {expect(subject).to respond_to(:land).with(1).argument}
     
     it 'lands at an airport' do
-      airport = double(landed: 'Plane has touched down!!')
-      expect(subject.land(airport)).to eq 'Plane has touched down!!'
+      subject.take_off(leave)
+      expect(subject.land(landed)).to eq 'Plane has touched down!!'
     end
     
     it "can't land in stormy weather" do
+      subject.take_off(leave)
       stormy_landing = double(landed: "It is too stormy to land now.")
       expect(subject.land(stormy_landing)).to eq 'It is too stormy to land now.'
+    end
+    
+    it 'can only land if in the air' do
+      expect {subject.land(landed)}.to raise_error "Already on the ground"
     end
   end
   
   describe 'can take off from an airport' do
-    let(:take_off) {double(leave_gate: 'Plane has taken off!!')}
     it {expect(subject).to respond_to(:take_off)}
     
     it 'leaves an airport' do
-      expect(subject.take_off(take_off)).to eq 'Plane has taken off!!'
+      expect(subject.take_off(leave)).to eq 'Plane has taken off!!'
     end
     
     it "can't take off in stormy weather" do
       stormy = double(leave_gate: "It is too stormy to take off.")
       expect(subject.take_off(stormy)).to eq "It is too stormy to take off."
+    end
+    
+    it 'can only take off if on the ground' do
+      subject.take_off(leave)
+      expect { subject.take_off(leave) }.to raise_error "Already in the air"
     end
   end
 end
