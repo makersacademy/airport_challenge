5ad7eae0c271d1cf4a873e61c6e58836dd476f16
Rufus Raghunath <rufusraghunath@gmail.com>
2016-01-23 20:18:58 +0000

    further cleanup on unit test, small formatting issue for rufus_README.md fixed

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -3,6 +3,10 @@ require 'airport'
 describe Airport do
   subject(:airport) {described_class.new(50)}
   let(:plane) {double :plane}
+  before do
+    allow(plane).to receive(:land) {subject.land_plane(plane)}
+    allow(plane).to receive(:takeoff) {subject.remove_plane(plane)}
+  end
 
   it{is_expected.to respond_to(:sunny?)}
 
@@ -25,17 +29,13 @@ describe Airport do
 
   context 'it should be able to store and release plane objects' do
 
+    before {plane.land(subject)}
+
     it 'allows Plane objects to land by adding them to @landing_strip' do
-      allow(plane).to receive(:land) {subject.land_plane(plane)}
-      plane.land(subject)
       expect(subject.landing_strip).to eq [plane]
     end
 
     it 'allows planes to takeoff by removing them from @landing_strip' do
-      allow(plane).to receive_messages(
-        :land => subject.land_plane(plane),
-        :takeoff => subject.remove_plane(plane))
-      plane.land(subject)
       plane.takeoff
       expect(subject.landing_strip).to eq []
     end
