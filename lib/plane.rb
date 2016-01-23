7e4e6c8a315e5947331b9f0f2c37a53c70ce510d
Godfrey Got-Wai Chiu <ggwc82@gmail.com>
2016-01-23 19:19:25 +0000

    Implemented takeoff with tests for edge cases - Completed user story 2

--- lib/plane.rb
+++ lib/plane.rb
@@ -1,15 +1,25 @@
 class Plane
   def initialize
     @landed
+    @airport
   end
 
   def land(airport)
-    raise "error: plane already landed" if @landed
+    raise "error: plane already landed" if landed
     @landed = true
+    @airport = airport
+  end
+
+  def takeoff(airport)
+    raise "error: already taken off" unless landed
+    raise "error: cannot take off from a different airport" if airport != @airport
+    @landed = false
     airport
   end
 
+
 attr_reader :landed
-alias_method :landed?, :landed
+attr_reader :airport
+
 
 end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -1,7 +1,8 @@
 require 'plane'
 
 describe Plane do
-  let(:airport) { double :airport}
+  let(:airport) { double :airport }
+  let(:airport2) { double :airport }
 
   describe "#land" do 
     it "responds to land with 1 arg" do 
@@ -18,16 +19,44 @@ describe Plane do
     end
   end
 
-  describe "#landed?" do
+  describe "#landed" do
     it "returns nil as default value" do
-      expect(subject.landed?).to eq nil
+      expect(subject.landed).to eq nil
     end
 
     it "returns true after land and landed? are called" do
       subject.land(airport)
-      expect(subject.landed?).to eq true
+      expect(subject.landed).to eq true
+    end
+
+    it "returns false if successfully taken off" do
+      subject.land(airport)
+      subject.takeoff(airport)
+      expect(subject.landed).to eq false
     end
 
   end
 
+  describe "#takeoff" do
+    it "responds to takeoff with 1 arg" do
+      expect(subject).to respond_to(:takeoff).with(1).arguments
+    end
+
+    it "takes off from the airport that it is landed at" do
+      subject.land(airport)
+      expect(subject.takeoff(airport)).to eq airport
+      end
+
+    it "raises an error if already taken off" do
+      subject.land(airport)
+      subject.takeoff(airport)
+      expect {subject.takeoff(airport)}.to raise_error("error: already taken off")
+    end
+
+    it "raises an error if told to take off from wrong airport" do
+      subject.land(airport)
+      expect {subject.takeoff(airport2)}.to raise_error("error: cannot take off from a different airport")
+
+    end
+  end
 end
