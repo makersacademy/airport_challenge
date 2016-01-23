498954566059f6d0d7853e6a7c9c4d683bdb0088
Rufus Raghunath <rufusraghunath@gmail.com>
2016-01-23 19:56:33 +0000

    Cleaned up unit tests

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -6,23 +6,9 @@ describe Airport do
 
   it{is_expected.to respond_to(:sunny?)}
 
-  context 'it should be able to store and release plane objects' do
+  context 'plane traffic is managed by controlling @landing_strip' do
 
-    it 'has an instance variable @landing_strip' do
-      expect(subject.landing_strip).to eq []
-    end
-
-    it 'allows Plane objects to land by adding them to @landing_strip' do
-      allow(plane).to receive(:land).and_return subject.land_plane(plane)
-      plane.land(subject)
-      expect(subject.landing_strip).to eq [plane]
-    end
-
-    it 'allows planes to takeoff by removing them from @landing_strip' do
-      allow(plane).to receive(:land).and_return subject.land_plane(plane)
-      allow(plane).to receive(:takeoff).and_return subject.remove_plane(plane)
-      plane.land subject
-      plane.takeoff
+    it '@landing_strip is empty be default' do
       expect(subject.landing_strip).to eq []
     end
 
@@ -33,6 +19,25 @@ describe Airport do
     it 'sets @capacity to 100 by default' do
       default_airport = Airport.new
       expect(default_airport.capacity).to eq 100
+    end
+
+  end
+
+  context 'it should be able to store and release plane objects' do
+
+    it 'allows Plane objects to land by adding them to @landing_strip' do
+      allow(plane).to receive(:land) {subject.land_plane(plane)}
+      plane.land(subject)
+      expect(subject.landing_strip).to eq [plane]
+    end
+
+    it 'allows planes to takeoff by removing them from @landing_strip' do
+      allow(plane).to receive_messages(
+        :land => subject.land_plane(plane),
+        :takeoff => subject.remove_plane(plane))
+      plane.land(subject)
+      plane.takeoff
+      expect(subject.landing_strip).to eq []
     end
 
   end
