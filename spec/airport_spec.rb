c3b6b817454931d564234d4515add3d95bed4e90
Rebecca Piper <piper.rebecca93@gmail.com>
2016-01-23 13:37:23 +0000

    Created first feature test and airport_spec.rb file

---
+++ spec/airport_spec.rb
@@ -0,0 +1,3 @@
+describe Airport do
+
+end
---
+++ spec/features/feature_spec.rb
@@ -0,0 +1,19 @@
+#Feature 1: Want to instruct plane to land at airport and confirm that it has landed.
+
+describe "planes can land at airport"
+  it "can land at airport" do
+    airport = Airport.new
+    plane = Plane.new
+    #Land the plane
+    airport.land(plane)
+    #Check that the plane has landed at the airport
+    expect(airport.planes).to eq plane
+  end
+#Check the plane is classed as 'landed'
+  it "can confirm plane is landed" do
+    plane = Plane.new
+    airport = Airport.new
+    airport.land(plane)
+    expect(plane).to be_landed
+  end
+end
