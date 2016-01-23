d3297766ecfdefef987b4b7891dca9b28dcc1ac4
Wim Hekkelman <j.hekkelman4@upcmail.nl>
2016-01-23 18:18:15 +0100

    added 2 attributes to plane class

--- lib/plane.rb
+++ lib/plane.rb
@@ -1,2 +1,8 @@
 class Plane
+  attr_accessor :instruct_to_land
+  attr_accessor :has_landed
+  def initialize
+    @instruct_to_land = false
+    @has_landed = false
+  end
 end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -2,4 +2,18 @@ require 'plane'
 
 describe Plane do
 
+  describe '#instruct_to_land' do
+    it "respond to instruct_to_land" do
+      plane = Plane.new
+      expect(plane).to respond_to :instruct_to_land
+    end
+  end
+
+  describe '#has_landed' do
+    it "respond to has_landed" do
+      plane = Plane.new
+      expect(plane).to respond_to :has_landed
+    end
+  end
+
 end
