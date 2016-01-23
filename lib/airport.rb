099bf8c88d01d5eb8ac44adc4f11ce719d496b08
Iryna Howarth <irynahowarth@gmail.com>
2016-01-23 08:42:22 +0000

    Implement 2st UserStory

--- lib/airport.rb
+++ lib/airport.rb
@@ -2,7 +2,13 @@
 require_relative "plane"
 
 class Airport
+
   def land plane
     "Plane has landed"
   end
+
+  def takeoff plane
+    "Plane has taken off"
+  end
+
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -4,7 +4,7 @@ describe Airport do
 
   subject(:airport){ described_class.new }
 
-  context "accept plan to #land" do
+  context "instruct plan to #land" do
 
     it {is_expected.to respond_to :land}
 
@@ -12,6 +12,15 @@ describe Airport do
       expect(subject.land Plane.new).to eq "Plane has landed"
     end
 
+  end
+
+  context "instruct plan to #takeoff" do
+
+    it {is_expected.to respond_to :takeoff}
+
+    it "give a confirmation when plan took off" do
+      expect(subject.takeoff Plane.new).to eq "Plane has taken off"
+    end
   end
 
 end
