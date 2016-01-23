ee450001ba4c3c182c1040c545d9afb3699039c8
arnoldmanzano <arnold.manzano@gmail.com>
2016-01-23 16:56:14 +0000

    Added tests for Airport #take_off

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,16 +2,28 @@ require 'airport'
 
 describe Airport do
   subject(:airport) { described_class.new }
+  let(:plane) { double(:plane) }
 
   describe '#land' do
-    let(:plane) { double(:plane) }
-
     it { is_expected.to respond_to(:land).with(1).argument }
 
     it 'has the plane that is instructed to land' do
       allow(plane).to receive(:land)
       airport.land(plane)
       expect(airport.planes).to include plane
+    end
+  end
+
+  describe '#take_off' do
+
+    it { is_expected.to respond_to(:take_off).with(1).argument }
+
+    it 'no longer has the plane that is instructed to take off' do
+      allow(plane).to receive(:land)
+      allow(plane).to receive(:take_off)
+      airport.land(plane)
+      airport.take_off(plane)
+      expect(airport.planes).not_to include plane
     end
   end
 end
