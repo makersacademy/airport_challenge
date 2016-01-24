c64e30f337803f96c7c17c8257c121e95d04f1ce
Kait Beaudette <kait@kaitmakesthings.com>
2016-01-24 17:59:13 -0600

    Initial commit

---
+++ spec/features/landing_a_plane_spec.rb
@@ -0,0 +1,10 @@
+require 'plane'
+
+describe "Landing Plane" do
+  it "should land a plane" do
+    airport = Airport.new
+    plane = Plane.new
+    airport.land_plane(plane)
+    airport.planes
+  end
+end
