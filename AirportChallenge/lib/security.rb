458a2434f0f5336bc13008359be410e6271559a2
Scott Gledhill <ScottGledhill@hotmail.co.uk>
2016-01-24 14:47:35 +0000

    blaa

--- AirportChallenge/lib/security.rb
+++ AirportChallenge/lib/security.rb
@@ -3,12 +3,11 @@ class Security
 
   attr_reader :name, :passport
   def initialize(name,passport)
-    @name = name
+    @person = name
     @passport = passport
   end
 
   def securitypassed
-  
   end
 
   def securityfailed
