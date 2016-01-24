b6180e5edd86aac874d76ae1c963aa43a5026f72
arnoldmanzano <arnold.manzano@gmail.com>
2016-01-24 00:21:59 +0000

    Added raise error tests for plane.

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -3,20 +3,28 @@ require 'plane'
 describe Plane do
 
   describe '#land' do
-    it { is_expected.to respond_to(:land) }
-
     it 'has landed' do
       subject.land
       expect(subject.landed).to eq true
     end
+
+    it 'raises error if plane already landed' do
+      subject.land
+      message = 'Cannot land again, plane already landed!'
+      expect { subject.land }.to raise_error(message)
+    end
   end
 
   describe '#take_off' do
-    it { is_expected.to respond_to(:take_off) }
-
     it 'is no longer in land' do
       subject.take_off
-      expect(subject.take_off).to eq false
+      expect(subject.landed).to eq false
+    end
+
+    it 'raises error if plane already took off' do
+      subject.take_off
+      message = 'Cannot take off again, plane already took off!'
+      expect { subject.take_off }.to raise_error(message)
     end
   end
 end
