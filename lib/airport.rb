50320635d29bde27c8db8caf5928f5303f0da813
David Parry <drjparry@gmail.com>
2016-01-24 10:11:58 +0100

    indented properly

--- lib/airport.rb
+++ lib/airport.rb
@@ -3,35 +3,35 @@ require_relative 'weather.rb'
 
 class Airport
 
-attr_reader :planes
-attr_reader :capacity
+  attr_reader :planes
+  attr_reader :capacity
 
 
-DEFAULT_CAPACTITY = 20
+  DEFAULT_CAPACTITY = 20
 
-def initialize(capacity=DEFAULT_CAPACTITY)
+  def initialize(capacity=DEFAULT_CAPACTITY)
 
-  @planes = []
-  @capacity = capacity
-end
+    @planes = []
+    @capacity = capacity
+  end
 
-def land(plane)
-  raise 'plane already in the airport' if planes.include?(plane)
-  raise "weather too stormy to land" if weather == :stormy
-  raise "airport at max capacity" if at_capacity?
-   planes << plane
-end
+  def land(plane)
+    fail 'plane already in the airport' if planes.include?(plane)
+    fail "weather too stormy to land" if weather == :stormy
+    fail "airport at max capacity" if at_capacity?
+     planes << plane
+  end
 
-def take_off(plane)
-  raise "weather is stormy" if weather == :stormy
-  planes.pop
-end
+  def take_off(plane)
+    fail "weather is stormy" if weather == :stormy
+    planes.pop
+  end
 
-private
+  private
 
 
-def at_capacity?
-    planes.count == @capacity
-end
+  def at_capacity?
+      planes.count == @capacity
+  end
 
 end
