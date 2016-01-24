ff15da658f89c8a31ec83113727f5a5653a0a193
Alain Lemaire <jaxdid@gmail.com>
2016-01-25 00:53:40 +0100

    Added private method Airport#weather_update. Initial airport weather now randomly set (80% sunny, 20% stormy)

--- lib/airport.rb
+++ lib/airport.rb
@@ -2,6 +2,12 @@ class Airport
   attr_accessor :weather
 
   def initialize
-    @weather = 'stormy'
+    @weather = weather_update
+  end
+
+  private
+
+  def weather_update
+    rand(5) == 0 ? 'stormy' : 'sunny'
   end
 end
