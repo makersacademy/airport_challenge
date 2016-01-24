2a1029722620ec0f6370ae6bb30d90cae590d200
Michael Collins <michaelj.collinsuk@gmail.com>
2016-01-24 14:11:53 +0000

    Refactored spec tests

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,10 +2,11 @@ require 'airport.rb'
 
 describe Airport do
   let(:plane) { double :plane }
+  subject(:airport) { described_class.new }
 
   describe 'initialization' do
     it 'initializes with a default capacity' do
-      expect(subject.capacity).to eq Airport::DEFAULT_CAPACITY
+      expect(airport.capacity).to eq Airport::DEFAULT_CAPACITY
     end
 
     it 'has a variable capacity' do
@@ -15,61 +16,64 @@ describe Airport do
   end
 
   describe '#clear_to_land' do
-    it 'instructs a plane to land at the airport' do
-      allow(subject).to receive(:stormy?) { false }
-      allow(plane).to receive(:landed?) { false }
-      allow(plane).to receive(:land).and_return(true)
-      expect(subject.clear_to_land(plane)).to include plane
-    end
 
-    it 'landing is prevented when weather is stormy' do
-      allow(subject).to receive(:stormy?) { true }
+    before do
+      allow(airport).to receive(:stormy?) { false }
       allow(plane).to receive(:land)
-      expect{subject.clear_to_land(plane)}.to raise_error 'Stormy weather is preventing landing'
     end
 
-    it 'prevents landing whent he airport is full' do
-      allow(subject).to receive(:full?) { true }
-      expect{subject.clear_to_land(plane)}.to raise_error 'The airport is full'
+    it 'instructs a plane to land at the airport' do
+      allow(plane).to receive(:landed?) { false }
+      expect(airport.clear_to_land(plane)).to include plane
     end
 
     it 'prevents a plane from landing if it is already at the airport' do
-      allow(subject).to receive(:stormy?) { false }
       allow(plane).to receive(:landed?) { true }
-      expect{subject.clear_to_land(plane)}.to raise_error 'The plane has already landed at the airport'
+      expect{airport.clear_to_land(plane)}.to raise_error 'The plane has already landed at the airport'
     end
+
+    it 'landing is prevented when weather is stormy' do
+      allow(airport).to receive(:stormy?) { true }
+      expect{airport.clear_to_land(plane)}.to raise_error 'Stormy weather is preventing landing'
+    end
+
+    it 'prevents landing whent he airport is full' do
+      allow(airport).to receive(:full?) { true }
+      expect{airport.clear_to_land(plane)}.to raise_error 'The airport is full'
+    end
+
   end
 
   describe '#landed_planes' do
     it 'keeps track of planes that have landed at the airport' do
-      allow(subject).to receive(:stormy?) { false }
+      allow(airport).to receive(:stormy?) { false }
       allow(plane).to receive(:landed?) { false }
       allow(plane).to receive(:land)
-      subject.clear_to_land(plane)
-      expect(subject.landed_planes).to include(plane)
+      airport.clear_to_land(plane)
+      expect(airport.landed_planes).to include(plane)
     end
   end
 
   describe '#clear_to_takeoff' do
     it 'instructs a plane to takeoff and removes it from the landed planes array' do
-      allow(subject).to receive(:stormy?) { false }
-      allow(subject).to receive(:empty?) { false }
+      allow(airport).to receive(:stormy?) { false }
+      allow(airport).to receive(:empty?) { false }
       allow(plane).to receive(:land)
       allow(plane).to receive(:landed?) { false }
       allow(plane).to receive(:takeoff)
-      subject.clear_to_land(plane)
-      subject.clear_to_takeoff
-      expect(subject.landed_planes).not_to include plane
+      airport.clear_to_land(plane)
+      airport.clear_to_takeoff
+      expect(airport.landed_planes).not_to include plane
     end
 
     it 'prevents takeoff if weather is stormy' do
-      allow(subject).to receive(:stormy?) { true }
-      allow(subject).to receive(:empty?) { false }
-      expect{subject.clear_to_takeoff}.to raise_error 'Stormy weather is preventing takeoff'
+      allow(airport).to receive(:stormy?) { true }
+      allow(airport).to receive(:empty?) { false }
+      expect{airport.clear_to_takeoff}.to raise_error 'Stormy weather is preventing takeoff'
     end
 
     it 'raises an error if there are no planes to takeoff' do
-      expect{subject.clear_to_takeoff}.to raise_error 'There are no planes at the airport'
+      expect{airport.clear_to_takeoff}.to raise_error 'There are no planes at the airport'
     end
 
   end
@@ -77,80 +81,63 @@ describe Airport do
   describe '#confirm_landed' do
     it 'accepts a plane and confirms if it has landed' do
       allow(plane).to receive(:landed?) {true}
-      expect(subject.confirm_landed(plane)).to eq true
+      expect(airport.confirm_landed(plane)).to eq true
     end
   end
 
   describe '#stormy?' do
     it 'randomly returns true or false if the weather is stormy' do
-      airport = double(:airport, stormy?: true)
+      allow(airport).to receive(:stormy?) { true }
       expect(airport.stormy?).to eq true
     end
   end
 
   describe '#full?' do
     it 'airport has a method called to check if it is full' do
-      expect(subject).to respond_to :full?
+      expect(airport).to respond_to :full?
     end
 
     it 'returns true when the airport has reached its capacity' do
-      allow(subject).to receive(:stormy?) { false }
+      allow(airport).to receive(:stormy?) { false }
       allow(plane).to receive(:landed?) { false }
       allow(plane).to receive(:land)
       described_class::DEFAULT_CAPACITY.times do
-        subject.clear_to_land(plane)
+        airport.clear_to_land(plane)
       end
-      expect(subject.full?).to eq true
+      expect(airport.full?).to eq true
     end
 
     describe '#empty?' do
       it 'returns true of false depening on whether the airport is empty' do
-        expect(subject.empty?).to eq true
+        expect(airport.empty?).to eq true
       end
     end
   end
-
-
-
-
-
-
-
-  # describe '#clearance?' do
-  #   it 'returns clearance as true' do
-  #     expect(subject.clearance?).to eq true
-  #   end
-  # end
-  #
-  # describe '#plane_lands' do
-  #   it 'a plane lands and it is stored at the airport' do
-  #     allow(plane).to receive(:clearance).and_return(true)
-  #     landed_planes = [plane]
-  #     expect(subject.plane_lands(plane)).to eq landed_planes
-  #   end
-  #
-  #   it 'returns an error if a plane does not have clearance to land' do
-  #     allow(plane).to receive(:clearance).and_return(false)
-  #     expect{subject.plane_lands(plane)}.to raise_error "Plane does not have clearance to land"
-  #   end
-  # end
-  #
-  # describe '#landed_planes' do
-  #   it 'returns an array with a copy of the planes at the airport' do
-  #     allow(plane).to receive(:clearance).and_return(true)
-  #     subject.plane_lands(plane)
-  #     expect(subject.landed_planes).to eq [plane]
-  #   end
-  # end
 end
 
-
-# # let(:bike) { double :bike }
-# # # bike = double(:bike, broken?: true)
-#
-# def stormy?
-#   number = rand(0..2)
-#   number == 0 ? true:false
+# describe '#clearance?' do
+#   it 'returns clearance as true' do
+#     expect(subject.clearance?).to eq true
+#   end
 # end
 #
-# p stormy?
+# describe '#plane_lands' do
+#   it 'a plane lands and it is stored at the airport' do
+#     allow(plane).to receive(:clearance).and_return(true)
+#     landed_planes = [plane]
+#     expect(subject.plane_lands(plane)).to eq landed_planes
+#   end
+#
+#   it 'returns an error if a plane does not have clearance to land' do
+#     allow(plane).to receive(:clearance).and_return(false)
+#     expect{subject.plane_lands(plane)}.to raise_error "Plane does not have clearance to land"
+#   end
+# end
+#
+# describe '#landed_planes' do
+#   it 'returns an array with a copy of the planes at the airport' do
+#     allow(plane).to receive(:clearance).and_return(true)
+#     subject.plane_lands(plane)
+#     expect(subject.landed_planes).to eq [plane]
+#   end
+# end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -2,56 +2,57 @@ require 'plane.rb'
 
 describe Plane do
   let(:airport) { double :airport }
+  subject(:plane) { described_class.new}
 
   describe 'initialize' do
     it 'it initializes in the air' do
-      expect(subject.landed?).to eq false
+      expect(plane.landed?).to eq false
     end
   end
   describe 'landed?' do
     it 'returns true is the plane has landed and is on the ground' do
       subject.land
-      expect(subject.landed?).to eq true
+      expect(plane.landed?).to eq true
     end
   end
 
   describe '#land' do
     it 'sets the planes status to landed' do
-      expect(subject.land).to eq true
+      expect(plane.land).to eq true
     end
   end
 
   describe '#takeoff' do
     it 'sets landed to false' do
       subject.takeoff
-      expect(subject.landed?).to eq false
+      expect(plane.landed?).to eq false
     end
   end
-  # describe 'initialize' do
-  #   it 'initializes with airborne false' do
-  #
-  #   end
-  # end
-
-  # describe '#clearance' do
-  #   it 'returns planes clearance' do
-  #     expect(subject.clearance).to eq false
-  #   end
-  # end
-  #
-  # describe '#request_clearance' do
-  #   it 'requests clearance from the airport and sets planes clearance to true' do
-  #     allow(airport).to receive(:clearance?).and_return(true)
-  #     subject.request_clearance(airport)
-  #     expect(subject.clearance).to eq true
-  #   end
-  #
-  #   it 'resets clearance to false if clearance is currently true' do
-  #     allow(airport).to receive(:clearance?).and_return(true)
-  #     subject.request_clearance(airport)
-  #     subject.request_clearance(airport)
-  #     expect(subject.clearance).to eq false
-  #   end
-  # end
-
 end
+
+# describe 'initialize' do
+#   it 'initializes with airborne false' do
+#
+#   end
+# end
+
+# describe '#clearance' do
+#   it 'returns planes clearance' do
+#     expect(subject.clearance).to eq false
+#   end
+# end
+#
+# describe '#request_clearance' do
+#   it 'requests clearance from the airport and sets planes clearance to true' do
+#     allow(airport).to receive(:clearance?).and_return(true)
+#     subject.request_clearance(airport)
+#     expect(subject.clearance).to eq true
+#   end
+#
+#   it 'resets clearance to false if clearance is currently true' do
+#     allow(airport).to receive(:clearance?).and_return(true)
+#     subject.request_clearance(airport)
+#     subject.request_clearance(airport)
+#     expect(subject.clearance).to eq false
+#   end
+# end
