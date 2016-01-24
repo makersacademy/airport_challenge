8eb5aa91ffe0413a088b49deec42115a1cf081a0
Henry Gordon <hwgordon247@gmail.com>
2016-01-24 19:02:22 +0000

    refactor of the airport test spec

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -6,43 +6,47 @@ let(:plane) {double(:plane, :land => false, :take_off => true)}
 let(:weather) {double(:weather)}
 subject(:Airport) {described_class.new weather}
 
-  it 'can request a plane to land and confirm when landed' do
-    allow(weather).to receive(:storm).and_return(false)
-    subject.request_land(plane)
-    expect(subject.planes).to include plane
+  describe 'airport in good weather' do
+    before(:example) do
+      allow(weather).to receive(:storm).and_return(false)
+    end
+
+    it 'can request a plane to land and confirm when landed' do
+      subject.request_land(plane)
+      expect(subject.planes).to include plane
+    end
+
+    it 'can instruct a plane to take off and confirm when left' do
+      subject.request_take_off(plane)
+      expect(subject.planes).not_to include plane
+    end
+
+    it 'can prevent a plane from landing when the airport is full' do
+      Airport::DEFAULT_CAPACITY.times {subject.request_land(plane)}
+      expect {subject.request_land(plane)}.to raise_error("Planes cannot land as the airport is full.")
+    end
+
+    it 'can override the default airport capacity' do
+      airport = Airport.new(weather,30)
+      30.times {airport.request_land(plane)}
+      expect {airport.request_land(plane)}.to raise_error("Planes cannot land as the airport is full.")
+    end
   end
 
-  it 'can instruct a plane to take off and confirm when left' do
-    allow(weather).to receive(:storm).and_return(false)
-    subject.request_take_off(plane)
-    expect(subject.planes).not_to include plane
-  end
+  describe 'airport in a storm' do
+    before(:example) do
+      allow(weather).to receive(:conjure_storm).and_return(true)
+      allow(weather).to receive(:storm).and_return(true)
+    end
+    
+    it 'can prevent a plane take-off during stormy weather' do
+      weather.conjure_storm
+      expect {subject.request_take_off(plane)}.to raise_error("Planes cannot take off in a storm.")
+    end
 
-  it 'can prevent a plane take-off during stormy weather' do
-    allow(weather).to receive(:conjure_storm).and_return(true)
-    allow(weather).to receive(:storm).and_return(true)
-    weather.conjure_storm
-    expect {subject.request_take_off(plane)}.to raise_error("Planes cannot take off in a storm.")
+    it 'can prevent a plane landing during stormy weather' do
+      weather.conjure_storm
+      expect {subject.request_land(plane)}.to raise_error("Planes cannot land in a storm.")
+    end
   end
-
-  it 'can prevent a plane landing during stormy weather' do
-    allow(weather).to receive(:conjure_storm).and_return(true)
-    allow(weather).to receive(:storm).and_return(true)
-    weather.conjure_storm
-    expect {subject.request_land(plane)}.to raise_error("Planes cannot land in a storm.")
-  end
-
-  it 'can prevent a plane from landing when the airport is full' do
-    allow(weather).to receive(:storm).and_return(false)
-    Airport::DEFAULT_CAPACITY.times {subject.request_land(plane)}
-    expect {subject.request_land(plane)}.to raise_error("Planes cannot land as the airport is full.")
-  end
-
-  it 'can override the default airport capacity' do
-    airport = Airport.new(weather,30)
-    allow(weather).to receive(:storm).and_return(false)
-    30.times {airport.request_land(plane)}
-    expect {airport.request_land(plane)}.to raise_error("Planes cannot land as the airport is full.")
-  end
-
 end
