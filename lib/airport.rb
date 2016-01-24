696ad51e9b639862363e9e8cb70f2ad184c4dd19
Marion LV <marion.levisage@gmail.com>
2016-01-24 20:46:34 +0000

    Added test cases for .stormy?

--- lib/airport.rb
+++ lib/airport.rb
@@ -3,8 +3,33 @@ require_relative 'plane'
   class Airport
     attr_reader :planes
 
-      def take_off
-        fail "No plane has taken off" unless @planes
+      def initialize
+        @planes = []
+        @capacity = set_capacity
       end
 
-    end
+      DEFAULT_CAPACITY = 20
+
+      def set_capacity(capacity = DEFAULT_CAPACITY)
+        capacity
+      end
+
+        def land(plane)
+          raise "The airport is at maximum capacity." if full?
+          @planes << plane
+        end
+
+        def take_off
+          raise "There is no plane scheduled to take off" if empty?
+        end
+
+      private
+        def empty?
+          @planes.length == 0
+        end
+
+        def full?
+          @planes.length >= set_capacity
+        end
+
+  end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -5,29 +5,40 @@ describe Airport do
   describe "#landing" do
     it "should land a new plane into the airport" do
       test_plane = Plane.new
-      expect(test_plane).to eq test_plane
+      expect(subject.land(test_plane).last).to eq test_plane
     end
 
-    it "should confirm when a plane has landed" do
+    it "confirms when a plane has landed" do
       test_plane = Plane.new
       expect(test_plane.landed?).to eq true
     end
 
-    it "should not let a plane land if it is stormy" do
+    it "does not let a plane land if it is stormy" do
       test_plane = Plane.new
       expect(test_plane.stormy?).to eq true
+    end
+
+    it "does not let a plane land if over capacity" do
+      test_plane = Plane.new
+       subject.set_capacity.times{subject.land(test_plane)}
+       expect{subject.land(test_plane)}.to raise_error("The airport is at maximum capacity.")
     end
   end
 
   describe "#takeoff" do
-    it "should confirm that a plane has take off from the airport" do
+    it "should confirm that a plane has taken off from the airport" do
       test_plane = Plane.new
       expect(test_plane.taken_off?).to eq true
     end
 
-    it "should not allow planes to take off if there is no plane" do
+    it "should not allow planes to take off if there is no plane scheduled to take off" do
       test_plane = Plane.new
-      expect {subject.take_off}.to raise_error("No plane has taken off")
+      expect {subject.take_off}.to raise_error("There is no plane scheduled to take off")
+    end
+
+    it "does not let a plane take off if it is stormy" do
+      test_plane = Plane.new
+      expect(test_plane.stormy?).to eq true
     end
   end
 
