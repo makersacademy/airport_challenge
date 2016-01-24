cdc276f0868d0ecca2d47896413eeecf9dc16fca
Alain Lemaire <jaxdid@gmail.com>
2016-01-24 23:49:12 +0100

    Unit test: Airport objects should have a starting weather condition

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -4,4 +4,8 @@ describe Airport do
   it 'displays current weather conditions' do
     is_expected.to respond_to(:weather)
   end
+
+  it 'is created with weather conditions' do
+    expect(Airport.new.weather).not_to eq nil
+  end
 end
