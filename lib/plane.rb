20ca0454b4c3b45380e38ef555601f2f5dd99e70
Alain Lemaire <jaxdid@gmail.com>
2016-01-24 23:05:45 +0100

    Unit tests: Plane#land and Plane#take_off must be passed Airport objects as arguments

--- lib/plane.rb
+++ lib/plane.rb
@@ -1,13 +1,23 @@
+require_relative 'airport'
+
 class Plane
   def landed?
     @landed
   end
 
-  def land
-    @landed = true
+  def land(airport)
+    if airport.class != Airport
+      fail 'Must land at a valid airport'
+    else
+      @landed = true
+    end
   end
 
-  def take_off
-    @landed = false
+  def take_off(airport)
+    if airport.class != Airport
+      fail 'Must take off from a valid airport'
+    else
+      @landed = false  
+    end
   end
 end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -1,15 +1,31 @@
 require 'plane'
 
 describe Plane do
+  describe '#land' do
+    it do
+      is_expected.to respond_to(:land).with(1).argument
+    end
+    it 'fails if not passed an Airport object' do
+      expect { subject.land(Plane.new) }.to raise_error 'Must land at a valid airport'
+    end
+  end
+
+  describe '#take_off' do
+    it do
+      is_expected.to respond_to(:take_off).with(1).argument
+    end
+    it 'fails if not passed an Airport object' do
+      expect { subject.take_off(Plane.new) }.to raise_error 'Must take off from a valid airport'
+    end
+  end
+  
   it 'lands at an airport and confirms it has landed' do
-    plane = Plane.new
-    plane.land
-    expect(plane).to be_landed
+    subject.land(Airport.new)
+    expect(subject).to be_landed
   end
 
   it 'takes off from an airport and confirms it is no longer landed' do
-    plane = Plane.new
-    plane.take_off
-    expect(plane).not_to be_landed
+    subject.take_off(Airport.new)
+    expect(subject).not_to be_landed
   end
 end
