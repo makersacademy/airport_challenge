f96cfb89598b415a48ee74d7ef4b8600ba6d3be8
Finble <elainefinn@hotmail.com>
2016-01-24 13:12:31 +0000

    instance of plane responds to land method, in IRB + passes rspec

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -2,8 +2,13 @@ require 'plane'
 
 describe Plane do 
 
+plane = Plane.new
+
 	it 'returns an instance of plane' do
-		plane = Plane.new
-		expect(plane).to be_a(Plane)
+		expect(plane).to be_a(Plane) #it creates new object id in IRB, so creates a new instance
+	end
+
+	it 'expects plane to respond to land method' do
+		expect(plane).to respond_to(:land)  #it returns nil in IRB, so responds to :land
 	end
 end
