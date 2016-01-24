2af7d3f4b07d0605cca6e5bd559672d5fc93d0e1
hedudelgado <hedu.delgado@gmail.com>
2016-01-24 19:06:55 +0000

    land take off done, starting with the storm generator

--- lib/airport.rb
+++ lib/airport.rb
@@ -4,20 +4,39 @@ require_relative 'airplane'
 
 class Airport
 	DEFAULT_CAPACITY = 50
-	attr_reader :capacity
+	attr_reader :capacity, :planes
 
 	def initialize(capacity=DEFAULT_CAPACITY)
 		@capacity = capacity #it will define the capacity of the airport when a different capacity is given
 		@planes = [] #it will store the planes into the airport
 	end
 
-	def land(plane) #it will give permission to land
+	def land(plane) #it will give permission to land 
 		if @planes.size >= capacity 
 			raise 'Airport full, Permission Denied'
 		else
-		@planes << plane
+		@planes << plane #it send the planes into the airport
+		print 'Landing..'
 		end
 	end
+
+	def takeoff(plane)
+		@planes.each do |x|
+			if x == plane 
+				print 'Plane taking off..'
+				@planes.delete(x)
+				break
+			else
+				puts 'The plane selected is not in the airport, it can not take off'
+			end
+		end
+	end
+
+	def stormgenerator
+		x = rand(10)
+		if x > 7 then false else true end
+	end		
+
 
 
 	
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -4,21 +4,79 @@ describe Airport do
 	it {is_expected.to respond_to :land}
 end
 
+describe Airport do
+	it {is_expected.to respond_to :planes}
+end
+
+
+describe Airport do
+	describe '#planes' do 
+	it 'it will be able to read the planes in the airport' do 
+		airport = Airport.new
+		expect(airport.planes).to eq []
+		end
+	end
+end
+
+
+describe Airport do
+    it { is_expected.to respond_to(:land).with(1).argument }
+end
+describe Airport do 
+	describe '#land' do 
+		it 'it allow takes off the plane selected and confirms the plane is not longer there' do 
+			airport = Airport.new
+			plane = double(:plane)	
+			airport.land(plane)
+			airport.takeoff plane
+			expect(airport.planes).to eq []
+		end
+	end
+end
+
 # describe Airport do 
 # 	describe '#land' do 
 # 		it 'give permission to land' do 
 
+describe Airport do
+	describe'takeoff' do 
+	it {is_expected.to respond_to :takeoff}
+	end
+end
 
-# describe Airport do
-#     describe 'initialize' do 
-#         subject { Airport.new } 
-#             let(:plane) { double(:helloo) }
-#             it 'defaults capacity' do 
-#                 described_class::DEFAULT_CAPACITY.times do 
-#                 subject.land(plane)
-#         end
-#     end
+describe Airport do
+    it { is_expected.to respond_to(:takeoff).with(1).argument }
+end
+
+# describe Airport do 
+# 	describe '#takeoff' do 
+# 		it 'it allow takes off the plane only if the plane is actually in the airport' do 
+# 			airport = Airport.new
+# 			plane = double(:plane)	
+# 			airport.land(plane)
+# 			expect(airport.takeoff(plane)).to eq 'Plane taking off..'
+# 		end
+# 	end
 # end
+describe Airport do
+		#it 'it will generate a random number, whe the number is between 7 and 10 i will not let land or take off' do
+		it {is_expected.to respond_to :'stormgenerator'}
+end
+
+
+
+ describe Airport do 
+     describe 'initialize' do 
+         subject { Airport.new } 
+             let(:plane) { double(:helloo) }
+             it 'it will let the user to set the capacity of the airport, or leave the default one' do 
+                 described_class::DEFAULT_CAPACITY.times do 
+                 subject.land(plane)
+             end
+         end
+     end
+end
+
 describe Airport do  
     describe '#land' do 
         it 'raises an error when the airport is full' do
