4f1badefee5bd05d95c22c26dcf1ef13b5aa8b43
barrygrubb <barrygrubb@users.noreply.github.com>
2016-01-24 15:54:25 +0000

    added weather Class and its 'stormy?' method. Added raising of error if attempting to take-off during stormy weather

--- lib/plane.rb
+++ lib/plane.rb
@@ -5,12 +5,13 @@ class Plane
     #airport.landed_planes << self
   end
 
-  def land(airport)
+  def land(airport, weather)
     @landed = true
     add_to_airport_list_of_landed_planes(airport)
   end
 
-  def take_off(airport)
+  def take_off(airport, weather)
+    fail 'The weather is too stormy to take-off' if weather.stormy?
     @landed = false
     remove_from_airport_list_of_landed_planes(airport)
   end
@@ -19,7 +20,7 @@ class Plane
 
   def add_to_airport_list_of_landed_planes(airport)
     airport.landed_planes << self
-end
+  end
 
   def remove_from_airport_list_of_landed_planes(airport)
     airport.landed_planes.delete(self)
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -1,12 +1,18 @@
 require 'plane'
+require 'airport'
 describe Plane do
 
-  it 'Can receive the "land" command with 1 argument.' do
-    expect(subject).to respond_to(:land).with(1).argument
+  it 'Can receive the "land" command with 2 arguments.' do
+    expect(subject).to respond_to(:land).with(2).argument
   end
 
-  it 'Can receive the "take_off" command with 1 argument.' do
-    expect(subject).to respond_to(:take_off).with(1).argument
+  it 'Can receive the "take_off" command with 2 arguments.' do
+    expect(subject).to respond_to(:take_off).with(2).argument
   end
 
+  it 'Will not be allowed to take-off during stormy weather' do
+    weather = double(:Weather, :stormy? => true)
+    a = Airport.new
+    expect{(subject.take_off(a))}.to raise_error{'The weather is too stormy to take-off'}
+  end
 end
