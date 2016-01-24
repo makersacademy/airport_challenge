ddbcc5a59c189cf3276217bbf560884ec0808f2e
Wim Hekkelman <j.hekkelman4@upcmail.nl>
2016-01-24 16:01:07 +0100

    added code for edge conditions

--- lib/plane.rb
+++ lib/plane.rb
@@ -3,13 +3,21 @@ class Plane
 
 
   def to_land_at(airport)
-    1000.times # i am landing}
-    airport.landed_at(self.object_id)
+    if airport.planes.include? self.object_id
+      "The plane can not land on an airport it is already in"
+    else
+      1000.times # i am landing}
+      airport.landed_at(self.object_id)
+    end
   end
 
   def take_off(airport)
-    1000.times  # i am departing
-    airport.departed(self.object_id)
+    if airport.planes.include? self.object_id
+      1000.times  # i am departing
+      airport.departed(self.object_id)
+    else
+      "The plane can not take off when it is not on the airport"
+    end
   end
 
 end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -4,15 +4,26 @@ describe Plane do
 
   let(:plane) { Plane.new }
 
+
   describe '#to_land_at' do
     it "respond to to_land_at with 1 argument" do
       expect(plane).to respond_to(:to_land_at).with(1).argument
+    end
+    it "returns an error when already on the airport" do
+      airport = double { "airport" }
+      expect(airport).to receive(:planes)
+      airport.planes
     end
   end
 
   describe '#take_off' do
     it "respond to take_off" do
       expect(plane).to respond_to(:take_off).with(1).argument
+    end
+    it "returns an eror when not on the airport" do
+      airport = double { "airport" }
+      expect(airport).to receive(:planes)
+      airport.planes
     end
   end
 
