8c3dd4ed7067fd5ff9bc43253ec09d641fb00649
barrygrubb <barrygrubb@users.noreply.github.com>
2016-01-24 12:54:34 +0000

    added the 'confirm_plane_landed' method to the Airport class

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,2 +1,6 @@
 class Airport
+
+  def confirm_plane_landed (plane)
+  end
+
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,4 +2,8 @@ require 'airport'
 
 describe Airport do
 
+  it 'can receive the "confirm_plane_landed" command with 1 argument' do
+    expect(subject).to respond_to(:confirm_plane_landed).with(1).argument
+  end
+
 end
