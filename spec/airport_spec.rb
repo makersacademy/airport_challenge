a1a86e7e90aedee7d5c4f8b895a4ec82435995e0
Mark Hill <futonfutonda@gmail.com>
2016-01-24 14:18:43 +0000

    added new full? test

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,10 +2,15 @@ require 'airport'
 
 describe Airport do
   it { is_expected.to respond_to(:full?)}
+  it { is_expected.to respond_to(:planes)}
+  it { is_expected.to respond_to(:capacity)}
 
   it "reports full when it has reached its capacity"do
     subject.capacity.times{ subject.planes << "plane" }
     expect(subject.full?).to eq true
+  end
+  it "reports not full when it hasn't reached capacity"do
+    expect(subject.full?).to eq false
   end
 
   it 'has a default capacity'do
