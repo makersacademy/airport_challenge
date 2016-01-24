5e7623053b655593d9ce531852eeed2118cdeb0f
valogopedina <valentina.monetta@tiscali.it>
2016-01-24 17:06:14 +0000

    fifth user story

--- lib/airport.rb
+++ lib/airport.rb
@@ -4,11 +4,21 @@ require_relative 'plane'
 class Airport
 
 	def land(plane)
+		@plane = plane
 		"Plane has landed"
 	end
 
 	def take_off
+		@plane = nil
 		@plane
+	end
+
+	def full?
+		if @plane == nil
+			false
+		else
+			true
+		end
 	end
 
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -13,7 +13,24 @@ describe Airport do
 
 	it { is_expected.to respond_to :take_off }
 
-	it 'plane not longer at the airport after take off' do
+	it 'take off when airport is empty' do
+		subject = Airport.new
 		expect(subject.take_off).to eq nil
 	end
+
+	it 'take off after land' do
+		subject = Airport.new
+		subject.land(Plane.new)
+		expect(subject.take_off).to eq nil
+	end
+
+	it { is_expected.to respond_to :full? }
+
+	it 'checks airport is full' do
+		expect(subject.full?).to eq false
+		subject.land Plane.new
+		expect(subject.full?).to eq true
+	end
+
+
 end
