ca04de10f90764967cbf4696e5285f114154f2eb
ViolaCrellin <viola.crellin@gmail.com>
2016-01-24 00:18:57 +0000

    Completed 4th User Story

--- lib/airport.rb
+++ lib/airport.rb
@@ -6,23 +6,30 @@ attr_reader :holding_bay, :weather
 
   def initialize
     @holding_bay = []
-    @weather = weather
   end
 
   def land(plane)
-    plane.change_status
-    @holding_bay << plane
+    if sunny?
+      plane.change_status
+      @holding_bay << plane
+    else
+      "Unsafe to land plane whilst stormy"
+    end
   end
 
   def take_off
-    plane = @holding_bay.pop
-    plane.change_status
-    plane
+    if sunny?
+      plane = @holding_bay.pop
+      plane.change_status
+      plane
+    else
+      "Unsafe to take off plane whilst stormy"
+    end
   end
 
 
-  def weather_generator
-
+  def sunny?
+    rand*100 > 10 ? true : false
   end
 
 #   def setup_holding_bay(capacity)
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -4,6 +4,7 @@ describe Airport do
   subject(:airport) {described_class.new}
   let(:plane) {double :plane}
 
+
   describe "#initialize" do
     context "All airports have holding bay for planes to be stored" do
       it {is_expected.to respond_to(:holding_bay)}
@@ -23,6 +24,7 @@ describe Airport do
 
       before do
         allow(plane).to receive(:change_status)
+        airport.stub(:sunny?).and_return(true)
         subject.land(plane)
       end
 
@@ -42,6 +44,7 @@ describe Airport do
 
         before do
           allow(plane).to receive(:change_status)
+          airport.stub(:sunny?).and_return(true)
           subject.land(plane)
         end
 
@@ -54,9 +57,63 @@ describe Airport do
           expect(airport.holding_bay).to eq []
         end
 
-        it "Changes the plane's status to 'Flying'" do
-          expect(subject.take_off).to eq(plane.change_status)
+        it "Will not release a plane from the holding_bay if it is empty" do
+
         end
+      end
+    end
+
+    context "Behaviour due to bad/not sunny weather" do
+
+      before do
+        allow(plane).to receive(:change_status)
+      end
+
+      describe "#sunny?" do
+
+        it {is_expected.to respond_to(:sunny?)}
+
+        it "Will be Sunny roughly 90% of the time" do
+          100.times do
+            subject.land(plane)
+          end
+          expect(subject.holding_bay.size).to satisfy{|x| x >= 80 }
+        end
+
+        describe "#landing in whilst not sunny" do
+          before do
+            airport.stub(:sunny?).and_return(false)
+          end
+
+          it "Will not allow plane to land if not sunny" do
+            expect(subject.land(plane)).to eq("Unsafe to land plane whilst stormy")
+          end
+
+          it "Will not add another plane to the holding bay if landing whilst not sunny" do
+            before = subject.holding_bay.size
+            subject.land(plane)
+            expect(subject.holding_bay.size).to eq (before)
+          end
+        end
+
+        describe "#taking_off whilst not sunny" do
+          before do
+            airport.land(plane)
+            airport.stub(:sunny?).and_return(false)
+          end
+
+          it "Will not allow plane to take off if not sunny" do
+            expect(subject.take_off).to eq("Unsafe to take off plane whilst stormy")
+          end
+
+          it "Will not take off a plane from the holding bay whilst not sunny" do
+            before = subject.holding_bay.size
+            subject.take_off
+            expect(subject.holding_bay.size).to eq (before)
+          end
+        end
+
+
 
       end
     end
