ed7baf269ba5e9cadae319e16959ddbbab1094ad
Reiss Johnson <reissjohnson@me.com>
2016-01-24 03:04:23 +0000

    planes can now takeoff from a given airport

--- lib/Airport.rb
+++ lib/Airport.rb
@@ -3,15 +3,26 @@ require'./lib/plane.rb'
 class Airport
 
   def initialize
-    @planes = []
+    @planes_on_the_ground = []
+    @planes_in_the_air = []
+
   end
 
   def land plane
-     @planes << plane
+    raise "this plane has already landed" if @planes_on_the_ground.include?(plane)
+     @planes_on_the_ground << plane
+
   end
 
   def planes_landed
-    @planes
+    @planes_on_the_ground
+  end
+
+  def takeoff plane
+    raise "this plane has already taken off" unless @planes_on_the_ground.include?(plane)
+    @planes_in_the_air = @planes_on_the_ground.select {|p| p == plane }
+    @planes_on_the_ground.delete_if {|p| p == plane}
+    @planes_in_the_air
   end
 
 
--- spec/Airport_Spec.rb
+++ spec/Airport_Spec.rb
@@ -4,18 +4,51 @@ describe Airport do
 
 it { is_expected.to respond_to(:land).with(1).argument }
 it { is_expected.to respond_to :planes_landed }
+it { is_expected.to respond_to(:takeoff).with(1).argument}
 
 let(:plane){double :plane}
-
+let(:plane2){double :plane2}
 it 'lands a plane' do
   plane = double(:plane)
   expect(subject.land(plane)).to eq [plane]
 end
+
+it 'cannot land a plane more than once' do
+  plane = double(:plane)
+  subject.land(plane)
+  expect {subject.land(plane)}.to raise_error ("this plane has already landed")
+end
+
 
 it "shows plane has landed" do
   plane = double(:plane)
   subject.land(plane)
   expect(subject.planes_landed).to eq [(plane)]
   end
+
+=begin
+it 'lets a plane takeoff' do
+  plane = double(:plane)
+  subject.land(plane)
+  expect(subject.takeoff(plane)).to eq [plane]
+end
+=end
+
+it 'lets a specified plane takeoff' do
+  plane = double(:plane)
+  plane2 = double(:plane2)
+  subject.land(plane)
+  subject.land(plane2)
+  expect(subject.takeoff(plane)).to eq [plane]
+end
+
+
+it 'makes sure you cannot tell a plane to takeoff more than once' do
+  plane = double(:plane)
+  subject.land(plane)
+  subject.takeoff(plane)
+  expect {subject.takeoff(plane)}.to raise_error ("this plane has already taken off")
+end
+
 
 end
