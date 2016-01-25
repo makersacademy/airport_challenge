9ab568d854ca4daced512a76ac1a82a5195f8e76
russellvaughan <russellvaughan@gmail.com>
2016-01-25 14:31:08 +0000

    tests refactored with described.new in before statement to avoid repetition

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,51 +1,38 @@
 require 'airport'
 require 'plane'
 require 'weather'
+
+
 describe Airport do
-it { is_expected.to respond_to(:land).with(1).argument }
-it { is_expected.to respond_to(:depart).with(1).argument }
-it { is_expected.to respond_to :stormy?}
-it { is_expected.to respond_to :capacity }
 
+subject(:airport) { described_class.new }
+let(:plane) {double(:plane, :flying => true, :land => true)} 
 
-	
+before do
+     allow(airport).to receive(:stormy?).and_return false
+  end	
+
 	it '#land' do
-	airport = described_class.new
-	plane = Plane.new
-	allow(airport).to receive(:stormy?) {false}
 	airport.land(plane)
 	expect(airport.planes).to include plane
 	end	
 
 	it '#depart' do
-	airport = described_class.new
 	plane = Plane.new
-	allow(airport).to receive(:stormy?) {false}
 	airport.land(plane)
 	airport.depart(plane)
 	expect(airport.planes).to_not include plane
 	end	
 
 	it '#depart changes status to flying' do
-	airport = described_class.new
 	plane = Plane.new
-	allow(airport).to receive(:stormy?) {false}	
 	airport.land(plane)
 	airport.depart(plane)
 	expect(plane.flying).to eq true
 	end
 
-
-	it '#stormy?' do 
-	airport = described_class.new
-	allow(airport).to receive(:stormy?) {false}
-	expect(airport.stormy?).to eq false
-	end
-
 	it "error when plans depart and stormy" do
 	plane = Plane.new
-	airport = described_class.new
-	allow(airport).to receive(:stormy?) {false}
 	airport.land(plane)
 	allow(airport).to receive(:stormy?) {true}
 	expect{airport.depart(plane)}.to raise_error("unsafe flying conditions to depart")
@@ -53,21 +40,17 @@ it { is_expected.to respond_to :capacity }
 
 	it "error when planes land stormy" do
 	plane = Plane.new
-	airport = described_class.new
 	allow(airport).to receive(:stormy?) {true}
 	expect{airport.land(plane)}.to raise_error("unsafe flying conditions to land")
 	end
 
 	it 'prevents landing when airport is full' do 
 	plane = Plane.new
-	airport = described_class.new
-	allow(airport).to receive(:stormy?) {false}
 	airport.capacity.times { airport.land(Plane.new) }
 	expect{airport.land(plane)}.to raise_error("airport at capacity")
 	end
 
 	it "#capacity can be overridden" do
-	airport = Airport.new
 	airport.increase_capacity(15)
 	expect(airport.capacity).to eq 15
 	end
@@ -75,16 +58,12 @@ it { is_expected.to respond_to :capacity }
 
 	it "planes only depart airports they are in" do  
 	plane = Plane.new
-	airport = described_class.new
-	allow(airport).to receive(:stormy?) {false}
 	expect{airport.depart(plane)}.to raise_error "can only take off from current airport"
 	end
 
 
 	it "changes a planes status landed" do
 	plane = Plane.new
-	airport = described_class.new
-	allow(airport).to receive(:stormy?) {false}
 	airport.land(plane)
 	allow(plane).to receive(:flying) {true}	
 	expect{airport.depart(plane)}.to raise_error "plane is already flying an cannot take off"
@@ -93,8 +72,6 @@ it { is_expected.to respond_to :capacity }
 
 	it "prevents a plane from landing if already landed" do
 	plane = Plane.new
-	airport = Airport.new
-	allow(airport).to receive(:stormy?) {false}
 	airport.land(plane)
 	expect{airport.land(plane)}.to raise_error "plane is already landed and cannot land"
 	end
