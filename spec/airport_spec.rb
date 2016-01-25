5851a128bda370836730eb901d49f8d8e0c4ce34
Tom Pickard <tcpickard94@gmail.com>
2016-01-25 13:50:35 +0000

    Refactored rspec unit tests to use named subject

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,56 +1,60 @@
 require 'airport'
 
 describe Airport do
+  subject(:airport) {described_class.new}
   let(:plane) { double(:plane) }
   
-  it {expect(subject).to be_a Airport}  
+  it {expect(airport).to be_a Airport}  
       
   describe '#landed' do
+    before(:each) do
+      allow(airport).to receive(:weather)
+    end
     
-    it {expect(subject).to respond_to(:landed).with(1).argument}
+    it {expect(airport).to respond_to(:landed).with(1).argument}
     
     it 'returns string if plane has landed' do
-      allow(subject).to receive(:weather) {:sunny}
-      expect(subject.landed(plane)).to eq 'Plane has touched down!!'
+      allow(airport).to receive(:weather) {:sunny}
+      expect(airport.landed(plane)).to eq 'Plane has touched down!!'
     end
     
     it 'adds a plane to the airport' do
-      allow(subject).to receive(:weather) {:sunny}
-      subject.landed(plane)
-      expect(subject.planes.count).to eq 1
+      allow(airport).to receive(:weather) {:sunny}
+      airport.landed(plane)
+      expect(airport.planes.count).to eq 1
     end
     
     it 'prevents landing if weather is stormy' do
-      allow(subject).to receive(:weather) {:stormy}
-      expect {subject.landed(plane)}.to raise_error "It is too stormy to land."
+      allow(airport).to receive(:weather) {:stormy}
+      expect {airport.landed(plane)}.to raise_error "It is too stormy to land."
     end
   end
   
   describe '#leave_gate' do
-    it {expect(subject).to respond_to(:leave_gate)}
+    it {expect(airport).to respond_to(:leave_gate)}
     
     it 'Removes a plane from the airport' do
-      allow(subject).to receive(:weather) {:sunny}
-      subject.landed(plane)
-      expect(subject.leave_gate).to eq "Plane has taken off!!"
-      expect(subject.planes[0]).to eq nil
+      allow(airport).to receive(:weather) {:sunny}
+      airport.landed(plane)
+      expect(airport.leave_gate).to eq "Plane has taken off!!"
+      expect(airport.planes[0]).to eq nil
     end
   end
   
   describe '#weather' do
     
     it 'prevents take off when stormy' do
-      allow(subject).to receive(:weather) {:stormy}
-      expect{subject.leave_gate}.to raise_error "It is too stormy to take off."
+      allow(airport).to receive(:weather) {:stormy}
+      expect{airport.leave_gate}.to raise_error "It is too stormy to take off."
     end
   end
   
   describe '#capacity' do
     
     it 'raises an error if full' do
-      allow(subject).to receive(:weather) {:sunny}
-      15.times { subject.landed(plane) }
-      expect {subject.landed(plane)}.to raise_error "Airport is full."
+      allow(airport).to receive(:weather) {:sunny}
+      15.times { airport.landed(plane) }
+      expect {airport.landed(plane)}.to raise_error "Airport is full."
     end
     
     it 'can change capacity' do
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -1,45 +1,47 @@
 require 'plane'
 
 describe Plane do
+  subject(:plane) { described_class.new }
   let(:leave) {double(leave_gate: 'Plane has taken off!!')}
-  it {expect(subject).to be_a Plane}
+  
+  it {expect(plane).to be_a Plane}
   
   describe 'Plane should be able to land' do
     let(:landed) { double(landed: 'Plane has touched down!!') }
     
-    it {expect(subject).to respond_to(:land).with(1).argument}
+    it {expect(plane).to respond_to(:land).with(1).argument}
     
     it 'lands at an airport' do
-      subject.take_off(leave)
-      expect(subject.land(landed)).to eq 'Plane has touched down!!'
+      plane.take_off(leave)
+      expect(plane.land(landed)).to eq 'Plane has touched down!!'
     end
     
     it "can't land in stormy weather" do
       stormy_landing = double(landed: "It is too stormy to land now.")
-      subject.take_off(leave)
-      expect(subject.land(stormy_landing)).to eq 'It is too stormy to land now.'
+      plane.take_off(leave)
+      expect(plane.land(stormy_landing)).to eq 'It is too stormy to land now.'
     end
     
     it 'can only land if in the air' do
-      expect {subject.land(landed)}.to raise_error "Already on the ground"
+      expect {plane.land(landed)}.to raise_error "Already on the ground"
     end
   end
   
   describe 'can take off from an airport' do
-    it {expect(subject).to respond_to(:take_off)}
+    it {expect(plane).to respond_to(:take_off)}
     
     it 'leaves an airport' do
-      expect(subject.take_off(leave)).to eq 'Plane has taken off!!'
+      expect(plane.take_off(leave)).to eq 'Plane has taken off!!'
     end
     
     it "can't take off in stormy weather" do
       stormy = double(leave_gate: "It is too stormy to take off.")
-      expect(subject.take_off(stormy)).to eq "It is too stormy to take off."
+      expect(plane.take_off(stormy)).to eq "It is too stormy to take off."
     end
     
     it 'can only take off if on the ground' do
-      subject.take_off(leave)
-      expect { subject.take_off(leave) }.to raise_error "Already in the air"
+      plane.take_off(leave)
+      expect { plane.take_off(leave) }.to raise_error "Already in the air"
     end
   end
 end
