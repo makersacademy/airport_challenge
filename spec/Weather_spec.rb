f6df3144b6a66ca5d3de3549cc2249b708839fd1
rachelsmithcode <rachelcodesmith@outlook.com>
2016-01-25 11:42:07 +0000

    includes weather test though still needs to use kernal instead as currently possible failure

--- spec/Weather_spec.rb
+++ spec/Weather_spec.rb
@@ -3,14 +3,17 @@ require "./lib/Weather.rb"
 describe Weather do
   subject(:weather) { described_class.new }
 
-  # describe "#test weather randomness" do
-  #   it "should be occasionally stormy" do
-  #     array = []
-  #     100.times{array.push(Weather.new.bad?)}
-  #     array.delete(true)
-  #     expect(array.length).to be > 1
-  #     expect(array.length).to be < 100
-  #   end
+  describe "#test weather randomness" do
+     it "should be occasionally stormy" do
+       array = []
+       100.times{array.push(Weather.new.bad?)}
+       array.delete(true)
+       expect(array.length).to be > 1
+       expect(array.length).to be < 100
+     end
 
-  #end
+  end
 end
+
+
+#investigate kernal for computer gen randomness
