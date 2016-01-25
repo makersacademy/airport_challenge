703b9a610cb4a4ab667e825385a6c0b8d1cf7f37
Alain Lemaire <jaxdid@gmail.com>
2016-01-25 01:41:25 +0100

    Plane#land fails when target airport full. Added unit tests and methods for Airport#number_of_planes and Airport#add_plane.

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,10 +1,15 @@
 class Airport
   CAPACITY = 100
 
-  attr_accessor :weather
+  attr_accessor :weather, :number_of_planes
 
   def initialize
     @weather = weather_update
+    @number_of_planes = 0
+  end
+
+  def add_plane
+    @number_of_planes += 1
   end
 
   private
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -5,7 +5,19 @@ describe Airport do
     is_expected.to respond_to(:weather)
   end
 
-  it 'is created with weather conditions' do
+  it 'is created with initial weather conditions' do
     expect(Airport.new.weather).not_to eq nil
+  end
+
+  it 'displays current number of planes' do
+    is_expected.to respond_to(:number_of_planes)
+  end
+
+  it 'is created with an initial number of planes' do
+    expect(Airport.new.number_of_planes).not_to eq nil
+  end
+
+  it 'adds one to number of planes when a plane lands' do
+    expect{subject.add_plane}.to change{subject.number_of_planes}.by(1)
   end
 end
