d167dcdfc10d0804f812f068246e159c3de6bd30
barrygrubb <barrygrubb@users.noreply.github.com>
2016-01-25 07:43:04 +0000

    Refactored so that airports are responsible for producing their own weather system, rather than the planes creating them.

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,6 +1,6 @@
 class Airport
 
-  attr_reader :landed_planes, :capacity
+  attr_reader :landed_planes, :capacity, :weather
   attr_writer :landed_planes
 
   DEFAULT_CAPACITY = 30
@@ -8,6 +8,7 @@ class Airport
   def  initialize(capacity = DEFAULT_CAPACITY)
     @landed_planes = []
     @capacity = capacity
+    @weather = Weather.new
   end
 
   def confirm_plane_landed(plane)
