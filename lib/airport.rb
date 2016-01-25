5bd7c4597e6242a01d7ab1f11d471c3499dcb4da
Alain Lemaire <jaxdid@gmail.com>
2016-01-25 02:50:39 +0100

    Added Airport#capacity attr_accessor and set Airport initial capacity to the default.

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,11 +1,12 @@
 class Airport
   DEFAULT_CAPACITY = 100
 
-  attr_accessor :weather, :number_of_planes
+  attr_accessor :weather, :number_of_planes, :capacity
 
   def initialize
     @weather = weather_update
     @number_of_planes = 0
+    @capacity = DEFAULT_CAPACITY
   end
 
   def add_plane
