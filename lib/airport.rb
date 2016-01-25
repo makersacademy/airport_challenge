00136c96066d38ba006144c24e68ecec431efed8
itsindigo <matthewbridgesbusiness@gmail.com>
2016-01-25 08:11:03 +0000

    Added tests/methods to check airport capacity, and plane landing/take off.

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,12 +1,34 @@
-require './plane.rb'
+require 'plane.rb'
 
-class Airport
+  class Airport
+    attr_reader :planes
 
-  def initialize
-    @in_airport = []
-  end
+      def initialize
+        @planes = []
+        @capacity = set_capacity
+      end
 
-  def land(plane)
-    plane.land_plane
-    @in_airport << plane
-  end
+      DEFAULT_CAPACITY = 10
+
+      def set_capacity(capacity = DEFAULT_CAPACITY)
+        capacity
+      end
+
+        def land(plane)
+          raise "The airport is at maximum capacity." if full?
+          @planes << plane
+        end
+
+        def take_off
+          raise "There is no plane scheduled to take off" if empty?
+        end
+
+      private
+        def empty?
+          @planes.count <= 0
+        end
+
+        def full?
+          @planes.length >= set_capacity
+        end
+end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,1 +1,31 @@
-#require 'airport.rb'
+require 'airport'
+
+describe Airport do
+
+  describe "#landing" do
+    plane = Plane.new
+    it "should land a new plane into the airport" do
+      expect(subject.land(plane).last).to eq plane
+    end
+
+    it "confirms when a plane has landed" do
+      expect(plane.land).to eq false
+    end
+
+    it "does not let a plane land if over capacity" do
+       subject.set_capacity.times{subject.land(plane)}
+       expect{subject.land(plane)}.to raise_error("The airport is at maximum capacity.")
+    end
+  end
+
+  describe "#takeoff" do
+    plane = Plane.new
+    it "should confirm that a plane has taken off from the airport" do
+      expect(plane.take_off).to eq true
+    end
+
+    it "should not allow planes to take off if there is no plane scheduled to take off" do
+      expect {subject.take_off}.to raise_error("There is no plane scheduled to take off")
+    end
+  end
+end
