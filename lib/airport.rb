26f8e4c2755afc18428ed3199ebc996a3c9905d5
BjoernWagner <bjoern.m.wagner@gmail.com>
2016-02-01 00:42:59 +0100

    User Story 3
    As an air traffic controller
    So that I can avoid accidents
    I want to prevent airplanes landing or taking off when the weather is stormy

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,6 +1,13 @@
 class Airport
 
+  def initialize(capacity)
+    @capacity = capacity
+    @planes = []
+  end
+
   def land(plane)
+    raise 'Cannot land plane: airport full' if @planes.length >= @capacity
+    @planes << plane
   end
 
   def take_off(plane)
--- spec/features/user_stories_spec.rb
+++ spec/features/user_stories_spec.rb
@@ -4,7 +4,7 @@ describe 'User Stories' do
   # So planes can land safely at my airport
   # I would like to instruct a plane to land
   it 'so planes land at airports, instruct a plane to land' do
-    airport = Airport.new
+    airport = Airport.new(20)
     plane = Plane.new
     expect { airport.land(plane) }.not_to raise_error
   end
@@ -13,12 +13,22 @@ describe 'User Stories' do
   # So planes can take off safely from my airport
   # I would like to instruct a plane to take off
   it 'so planes can take off at airports, instruct a plane to take off' do
-    airport = Airport.new
+    airport = Airport.new(20)
     plane = Plane.new
     expect { airport.take_off(plane) }.not_to raise_error
   end
 
-
+  # As an air traffic controller
+  # So that I can avoid collisions
+  # I want to prevent airplanes landing when my airport is full
+  it 'does not allow planes to land when airport is full' do
+    airport = Airport.new(20)
+    plane = Plane.new
+    20.times do
+      airport.land(plane)
+    end
+    expect { airport.land(plane) }.to raise_error 'Cannot land plane: airport full'
+  end
 
 
 end
