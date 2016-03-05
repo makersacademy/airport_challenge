d27898025adeb32448383be09495121e5504c562
Caspar Fischer-Zernin <casparfz@gmail.com>
2016-03-05 13:34:24 +0000

    added default capacity and guard condition full

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,22 +1,33 @@
 class Airport
 
   attr_reader :planes
-
-  def initialize
+  DEFAULT_CAPACITY=20
+  
+  def initialize(capacity=DEFAULT_CAPACITY)
   @planes = []
+  @capacity = capacity
   end
 
 
   def arrive(plane, weather)
     raise "there is a storm" if weather.storm? 
+    raise "airport is full" if full?
     @planes << plane
   end
 
-  def depart(plane)
+  def depart(plane, weather)
+    raise "there is a storm" if weather.storm? 
     @planes.delete(plane)
   end
 
   def storm(weather)
     weather.storm?
   end
+
+private
+  
+  def full?
+    planes.length >= @capacity
+  end
+
 end
--- spec/airtport_spec.rb
+++ spec/airtport_spec.rb
@@ -7,6 +7,14 @@ describe Airport do
   
   it{is_expected.to respond_to(:planes)}
 
+  describe 'capacity' do 
+    it 'should have a initial capacity' do
+      allow(weather).to receive(:storm?).and_return(false)
+      described_class::DEFAULT_CAPACITY.times {subject.arrive(plane, weather)}
+      expect{subject.arrive(plane, weather)}.to raise_error "airport is full"
+    end
+  end    
+
   describe 'arrival' do
     it {is_expected.to respond_to(:arrive).with(2).argument}
     
@@ -23,11 +31,18 @@ describe Airport do
   end
 
   describe 'departure' do
-    it {is_expected.to respond_to(:depart).with(1).argument}
+    it {is_expected.to respond_to(:depart).with(2).argument}
   
     it 'should let a plane depart' do
-      subject.depart(plane)
+    allow(weather).to receive(:storm?).and_return(false)
+      subject.depart(plane, weather)
       expect(subject.planes).not_to include(plane)
     end
+
+    it 'when there is a storm planes cant depart' do
+    allow(weather).to receive(:storm?).and_return(true)
+    expect{subject.depart(plane)}.to raise_error
+    end
+
   end
 end
