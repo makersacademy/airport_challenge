21e210937af4cbc01ce10faf8ee917ef2dfa0ba1
Claudia Green <claudia@claudiag.me.uk>
2016-03-05 15:15:57 +0000

    Initial commit

---
+++ lib/feature.rb
@@ -0,0 +1,7 @@
+require_relative 'plane'
+require_relative 'airport'
+
+plane = Plane.new
+airport = Airport.new
+
+plane.instruct_takeoff
---
+++ spec/airport_spec.rb
@@ -0,0 +1,7 @@
+require 'airport'
+
+describe Airport do
+
+  it { is_expect.to respond_to(:instruct_takeoff) }
+
+end
