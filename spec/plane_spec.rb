76fce2336f1e0b74ef455980793a26d6033e789b
olmesm <olmesm@gmail.com>
2016-03-05 23:48:18 +0000

    added land method to plane

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -1,9 +1,13 @@
 require 'plane'
 
 describe Plane do
+  let(:airport) { double(:Airport) }
+
   describe '#land' do
-    
+    it { is_expected.to respond_to(:land).with(1).argument}
   end
+
+  
 
   describe '#take_off' do
 
