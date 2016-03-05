52f37f7c8ee4a37c6614913deb328ceb54e19079
Yasmin Green <yasmingreen@Yasmins-MBP.home>
2016-03-05 21:37:30 +0000

    Updated files to cover for edge cases specified in challenge.

--- lib/airplane.rb
+++ lib/airplane.rb
@@ -13,11 +13,14 @@ attr_reader :landed
 
   def land_plane(airport)
     raise 'Airport is full' if airport.full?
+    raise 'Plane already landed' if self.landed == true
     airport.planes << self
     @landed = true
   end
 
   def take_off(airport)
+    raise 'Plane already taken off' if self.landed == false
+    raise 'Cannot take off because plane not at that airport' if !airport.planes.include? self
     airport.planes.delete(self)
     self.plane_status
   end
--- spec/airplane_spec.rb
+++ spec/airplane_spec.rb
@@ -5,8 +5,11 @@ let(:airport) {double :airport, :planes => [subject], :full? => false}
 let(:airport1) {double :airport, :planes => [plane2], :full? => false}
 let(:airport2) {double :airport, :planes => [], :full? => false}
 let(:airport3) {double :airport, :planes => [], :full? => true}
+let(:airport4) {double :airport, :planes => [], :full? => false}
+let(:airport5) {double :airport, :planes => [plane1, plane2], :full => false}
 let(:plane1) {Airplane.new}
 let(:plane2) {Airplane.new}
+let(:plane3) {Airplane.new}
 
   describe 'plane_status' do
     it 'should be landed when a new plane is initialized' do
@@ -20,6 +23,7 @@ let(:plane2) {Airplane.new}
     end
 
     it 'should push that plane into the airport.planes array' do
+      subject.take_off(airport)
       subject.land_plane(airport2)
       expect(airport2.planes).to eq [subject]
     end
@@ -38,6 +42,11 @@ let(:plane2) {Airplane.new}
       10.times {airport3.planes << Airplane.new}
       expect{ (subject).land_plane(airport3) }.to raise_error 'Airport is full'
     end
+
+    it 'should not land at airport if already landed' do
+      expect{ subject.land_plane(airport4) }.to raise_error 'Plane already landed'
+    end
+
   end
 
   describe 'take_off' do
@@ -46,14 +55,28 @@ let(:plane2) {Airplane.new}
     end
 
     it 'should remove that plane from the airport.planes array' do
-      plane1.land_plane(airport1)
-      plane1.take_off(airport1)
+      subject.take_off(airport)
+      subject.land_plane(airport1)
+      subject.take_off(airport1)
       expect(airport1.planes).to eq [plane2]
     end
 
     it 'should confirm that the plane has taken off with the plane status' do
-      plane2.take_off(airport2)
+      plane2.take_off(airport1)
       expect(plane2.landed).to eq false
+    end
+
+    it 'should not take off from airport if already taken off' do
+      subject.take_off(airport)
+      expect { subject.take_off(airport) }.to raise_error 'Plane already taken off'
+    end
+
+    it 'should not take off from an airport it is not at' do
+      subject.plane_status
+      subject.land_plane(airport4)
+      expect{ subject.take_off(airport5) }.to raise_error 'Cannot take off because plane not at that airport'
+      expect(airport4.planes).to eq [subject]
+      expect(subject.landed).to eq true
     end
   end
 end
