dda24b52875375283ba0b0c135e5cf689d43ae77
Tobenna <tobennandu@gmail.com>
2016-03-06 10:06:06 +0000

    Listened to hound recommendations and refactored

--- bin/plane.rb
+++ bin/plane.rb
@@ -1,13 +1,11 @@
 class Plane
 
+attr_reader :landed
   def initialize
     @landed = false
   end
   def land
     @landed = true
   end
-
-  def landed?
-    @landed
-  end
+  
 end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -4,10 +4,10 @@ describe Plane do
 
   describe '#land' do
     it { is_expected.to respond_to 'land'}
-    it { is_expected.to respond_to 'landed?'}
+    it { is_expected.to respond_to 'landed'}
     it 'Should tell you when a plane has landed' do
       subject.land
-      expect(subject).to be_landed
+      expect(subject.landed).to eq true
     end
   end
 
