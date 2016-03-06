8df15e55bc197a20e2808ac83df8f9617dc03d07
Rhiannon Lolley Neville <rhiannonruth@hotmail.co.uk>
2016-03-06 19:56:21 +0000

    Added extra tests for weather

--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -4,6 +4,17 @@ describe Weather do
   let(:dummy_class) {Class.new {include Weather}}
   subject(:class) {dummy_class.new}
 
-  it{is_expected.to respond_to(:stormy?)}
+  describe 'stormy?' do
 
+  it 'returns true when forecast is stormy' do
+    allow(subject).to receive(:forecast).and_return(:stormy)
+    expect(subject.stormy?).to be(true)
+  end
+
+  it 'returns false when forecast is not stormy' do
+    allow(subject).to receive(:forecast).and_return(:sunny)
+    expect(subject.stormy?).to be(false)
+  end
+
+  end
 end
