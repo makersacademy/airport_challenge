b15249f31471380754d25dc278a68b2586837a4c
Paul Rees <paulalexrees@gmail.com>
2016-03-06 11:06:33 +0000

    changes "raise" to "fail" for errors

--- lib/plane.rb
+++ lib/plane.rb
@@ -7,15 +7,15 @@ class Plane
   end
 
   def land(airport)
-      raise "Cannot land in stormy weather." if storm_check
-      raise "Cannot land if airport is full." if airport.full?
+    fail "Cannot land in stormy weather." if storm_check
+    fail "Cannot land if airport is full." if airport.full?
     airport.receive_plane(self)
     @flying = false
   end
 
   def takeoff(airport)
     #raise "I'm not there!" if !airport.planes.include?(self)
-    raise "Cannot take off in stormy weather." if storm_check
+    fail "Cannot take off in stormy weather." if storm_check
     airport.release_plane(self)
     @flying = true
   end
