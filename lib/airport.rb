a1a4f053fe5c29366df907f195d527d53dd6638a
Jazzy <jasmina.gasper@gmail.com>
2016-03-06 23:35:14 +0000

    airport full?

--- lib/airport.rb
+++ lib/airport.rb
@@ -4,22 +4,30 @@ require_relative 'weather'
 class Airport
 
 attr_reader :plane
-attr_accessor :storage
+attr_accessor :storage, :capacity
 
 def initialize
   @storage = []
+  @capacity = 10
 end
 
-def land(plane)
-  raise "no landing off due to storm" if Weather.new.stormy? == "stormy"
+def land(plane, weather=Weather.new)
+  # raise "no landing due to storm" if weather.stormy? == "stormy"
+  raise "no space to land plane" if full?
   plane.landed?
   @storage << plane
 end
 
-def take_off(plane)
-  raise "no take off due to storm" if Weather.new.stormy? == "stormy"
+def take_off(plane, weather=Weather.new)
+  # raise "no take off due to storm" if weather.stormy? == "stormy"
   plane.taken_off
   @storage.pop
 end
+
+private
+
+def full?
+    storage.size == capacity
+  end
 
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -4,30 +4,36 @@ describe Airport do
 
 it { is_expected.to respond_to(:plane) }
 
-# it 'tells plane to land and puts in storage' do
-#   plane = double(:plane, landed?:false)
-#   subject.land(plane)
-#   expect(subject.storage.size).to eq 1
-# end
-#
-# it 'takes plane out of storage after take off' do
-#   plane = double(:plane, taken_off:true)
-#   weather = double(:weather, stormy:"sunny")
-#   subject.take_off(plane)
-#   expect(subject.storage.size).to eq 0
-# end
-#
+it 'tells plane to land and puts in storage' do
+  plane = double(:plane, landed?:false)
+  subject.land(plane)
+  expect(subject.storage.size).to eq 1
+end
+
+it 'takes plane out of storage after take off' do
+  plane = double(:plane, taken_off:true)
+  weather = double(:weather, stormy?:"sunny")
+  subject.take_off(plane)
+  expect(subject.storage.size).to eq 0
+end
+
 # it 'does not let plane take off if weather is stormy' do
 #   plane = double(:plane, taken_off:true)
-#   weather = double(:weather, stormy:"stormy")
+#   weather = double(:weather, stormy?:"stormy")
 #   expect{ subject.take_off(plane) }.to raise_error "no take off due to storm"
 # end
-#
+
 # it 'does not let plane land if weather is stormy' do
-#   plane = plane = double(:plane, landed?:false,)
-#   weather = double(:weather, stormy:"stormy")
-#   expect{ subject.take_off(plane) }.to raise_error "no landing due to storm"
+#   plane = double(:plane, landed?:false)
+#   weather = double(:weather, stormy?:"stormy")
+#   expect{ subject.land(plane) }.to raise_error "no landing due to storm"
 # end
+
+it 'does not land plane is airport is full' do
+  10.times {subject.land double(:plane, landed?:false)}
+  expect {subject.land double(:plane) }.to raise_error "no space to land plane"
+end
+
 
 
 end
