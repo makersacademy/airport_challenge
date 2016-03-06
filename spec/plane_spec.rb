d682fe29837e175f3ad72a976433c2ef0e6dbc9f
eripheebs <erikapheby@gmail.com>
2016-03-06 20:41:08 +0000

    README file updated

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -2,8 +2,12 @@ require 'plane'
 
 describe Plane do
 
-  it 'expects it to be able to confirm if a plane is landed' do
-    is_expected.to respond_to(:landed?)
+  describe 'landed?'
+
+    it 'expects it to be able to confirm if a plane is landed' do
+      is_expected.to respond_to(:landed?)
+    end
+
   end
 
 end
