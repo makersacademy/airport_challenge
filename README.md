f29615a6f885d386c3e8ce44e1224d1e77790058
Chris Parker <cjp214@gmail.com>
2016-03-06 21:14:12 +0000

    updated README.md and added feature_test.rb

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 I used quite a lot of the learning objectives from the Boris bikes challenge - docking and releasing bikes with a docking station isn't so different from landing and taking off bikes from an airport, after all.
+Implemented user stories 1, 2, 5 and 6 with (I think) general effectiveness - they should perform their functions and deal with edge cases well enough.
+
+I had a lot of issues trying to implement the weather function, especially when trying to change that seemed to alter my initial spec tests that had passed but were now failing. Eventually I reverted to a previous commit that had my spec tests passing but no implementation of the weather.
+
+feature_test.rb contains some feature tests and the errors that they should raise.
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -5,18 +5,21 @@ describe Airport do
 	let(:plane) { double :plane }
 	let(:plane2) { double :plane }
 
+	before(:each) do
+		allow(plane).to receive(:landed)
+		allow(plane).to receive(:taken_off)
+	end
+
 	it 'responds to land method' do
 		expect(subject).to respond_to(:land).with(1).argument
 	end
 
 	it 'allows plane to land' do
-		allow(plane).to receive(:landed)
 		subject.land(plane)
 		expect(subject.planes.last).to eq(plane)
 	end
 
 	it 'changes status of plane to landed when plane has landed' do
-		allow(plane).to receive(:landed)
 		allow(plane).to receive(:has_landed?).and_return(true)
 		subject.land(plane)
 		expect(plane.has_landed?).to eq true
@@ -24,12 +27,10 @@ describe Airport do
 
 	it 'raises an error if airport is full' do
 		airport = Airport.new(0)
-		allow(plane).to receive(:landed)
 		expect { airport.land(plane) }.to raise_error "airport is full"
 	end
 
 	it 'raises an error if a plane which has already landed tries to land' do
-		allow(plane).to receive(:landed)
 		subject.land(plane)
 		expect { subject.land(plane) }.to raise_error "plane has already landed"
 	end
@@ -39,15 +40,11 @@ describe Airport do
 	end
 
 	it 'allows plane to take off' do
-		allow(plane).to receive(:landed)
-		allow(plane).to receive(:taken_off)
 		subject.land(plane)
 		expect(subject.takeoff(plane)).to eq(plane)
 	end
 
 	it 'changes status of plane to taken_off when plane has taken off' do
-		allow(plane).to receive(:landed)
-		allow(plane).to receive(:taken_off)
 		allow(plane).to receive(:has_taken_off?).and_return(true)
 		subject.land(plane)
 		subject.takeoff(plane)
@@ -60,20 +57,11 @@ describe Airport do
 	end
 
 	it 'raises an error if a plane which has already taken off tries to take off' do
-		allow(plane).to receive(:landed)
 		allow(plane2).to receive(:landed)
-		allow(plane).to receive(:taken_off)
 		subject.land(plane)
 		subject.land(plane2)
 		subject.takeoff(plane)
 		expect { subject.takeoff(plane) }.to raise_error "plane has already taken off"
 	end
-
-
-
-
-
-
-
 
 end
