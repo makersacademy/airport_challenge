bfc31d21fb7115b150a4745b41d31aaad30ac710
Shane Oston Stowe <shane@shaneoston.com>
2016-03-06 18:07:20 +0000

    adding edits to address test coverage in weather.rb

--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -4,12 +4,12 @@ describe Weather do
   subject(:weather) { described_class.new }
 
   it 'reports true if inclement' do
-    allow(weather).to receive(:inclement?).and_return(true)
+    allow(weather).to receive(:random_weather).and_return(:inclement)
     expect(weather.inclement?).to eq true
   end
 
   it 'reports false if clear' do
-    allow(weather).to receive(:inclement?).and_return(false)
+    allow(weather).to receive(:random_weather).and_return(:clear)
     expect(weather.inclement?).to eq false
   end
 end
