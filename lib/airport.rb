9648bedeb3e499a8e9afd343b28a4046ba40d37e
NickMountjoy <nick.mountjoy98@gmail.com>
2016-03-06 17:24:33 +0000

    airport full error message

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,7 +1,14 @@
 class Airport
 
-	def land(plane)
+	def initialize(capacity)
+		@capacity = capacity
+		@planes = []
 
+	end
+
+	def land(plane)
+		raise 'Airport full, cannot land plane' if @planes.length >= @capacity
+		@planes << plane
 	end
 
 	def take_off(plane)
--- spec/features/user_stories_spec.rb
+++ spec/features/user_stories_spec.rb
@@ -1,41 +1,51 @@
 describe 'User Stories' do
 
-# As an air traffic controller
+#1. As an air traffic controller
 # So I can get passengers to a destination
 # I want to instruct a plane to land at an airport and confirm that it has landed
 
 it 'planes need to land at airports, instructs planes to land at airport' do
-	airport = Airport.new
+	airport = Airport.new(20)
 	plane = Plane.new
 	airport.land(plane)
   	expect { airport.land(plane)}.not_to raise_error
 end
 
 
-# As an air traffic controller
+#2. As an air traffic controller
 # So I can get passengers on the way to their destination
 # I want to instruct a plane to take off from an airport and confirm that it is no longer in the airport
 
 it 'i want to instruct a plane to take off from an airport' do
-	airport = Airport.new
+	airport = Airport.new(20)
 	plane = Plane.new
 	airport.take_off(plane)
 	expect { airport.take_off(plane).not_to raise_error }
 end
 
-# As an air traffic controller
+#3. As an air traffic controller
+# To ensure safety
+# I want to prevent landing when the airport is full
+
+it 'i want to stop planes landing when the airport is full' do
+	airport = Airport.new(20)
+	plane = Plane.new
+	20.times do 
+		airport.land(plane)
+	end
+	expect { airport.land(plane) }.to raise_error 'Airport full, cannot land plane'
+	end
+end
+
+
+#4.1 As an air traffic controller
 # To ensure safety
 # I want to prevent takeoff when weather is stormy
 
 
-# As an air traffic controller
+#4.2 As an air traffic controller
 # To ensure safety
 # I want to prevent landing when weather is stormy
-
-
-# As an air traffic controller
-# To ensure safety
-# I want to prevent landing when the airport is full
 
 
 
@@ -43,5 +53,3 @@ end
 # So that the software can be used for many different airports
 # I would like a default airport capacity that can be overridden as appropriate
 
-
-end
