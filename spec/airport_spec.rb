fd7d085ef86cbe2b3a8bc4aa461f1794abcee95c
eripheebs <erikapheby@gmail.com>
2016-03-06 19:20:19 +0000

    Made ensuring safety by preventing landing and takeoff when weather is stormy unit tests pass

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,17 +1,19 @@
 require 'airport'
 
 describe Airport do
-  let(:plane) {double :plane, is_landed: true}
+  let(:plane) {double :plane}
 
   describe 'land' do
 
     it 'expects the plane to land into the airport when weather is fine' do
+      allow(plane).to receive(:is_landed).and_return(true)
       allow(subject).to receive(:is_stormy?).and_return(false)
       subject.land(plane)
       expect(subject.list_planes).to include(plane)
     end
 
     it 'expects landing to be prevented when weather is stormy' do
+      allow(plane).to receive(:is_landed).and_return(true)
       allow(subject).to receive(:is_stormy?).and_return(true)
       expect{subject.land(plane)}.to raise_error "It's too stormy to land."
     end
@@ -20,10 +22,14 @@ describe Airport do
 
   describe 'takeoff' do
 
+    before do
+      allow(plane).to receive(:is_landed).and_return(true)
+      subject.land(plane)
+    end
+
     it 'expects the plane to take off from the airport when weather is fine' do
       allow(plane).to receive(:took_off).and_return(true)
       allow(subject).to receive(:is_stormy?).and_return(false)
-      subject.land(plane)
       subject.takeoff(plane)
       expect(subject.list_planes).to eq([])
     end
@@ -31,7 +37,6 @@ describe Airport do
     it 'expects takeoff to be prevented when weather is stormy' do
       allow(plane).to receive(:took_off).and_return(true)
       allow(subject).to receive(:is_stormy?).and_return(true)
-      subject.land(plane)
       expect{subject.takeoff(plane)}.to raise_error "It's too stormy to take-off."
     end
 
