785629b5fd2370e81712ec7937b2fca8d7a053e2
Misa Ogura <misa.ogura01@gmail.com>
2016-03-07 11:24:42 +0000

    Move random weather generator function from initialize to stormy? method

--- lib/weather.rb
+++ lib/weather.rb
@@ -1,10 +1,7 @@
 class Weather
   attr_reader :stormy, :random_number
 
-  # creates a sunny (80%) and stormy (20%) weather
-  def initialize number=Random.rand(1..10)
-    @number = number
-    @number <= 2 ? @stormy=true : @stormy=false
+  def stormy? number=Random.rand(1..10)
+    number <= 2 ? true : false
   end
-
 end
