1efa93b9286559840801f37d23cd40cb58a2d2f9
Junyuan Xue <junyuanxue.is@gmail.com>
2016-03-07 11:50:47 +0000

    added change status methods

--- lib/plane.rb
+++ lib/plane.rb
@@ -11,12 +11,22 @@ class Plane
   def land_at(airport)
     fail "Plane already landed" if flying == false
     @airport = airport
-    @flying = false
+    landed
   end
 
   def depart_from(airport)
     fail "Wrong airport" if airport != @airport
     fail "Plane already flying" if flying
+    fly
+  end
+
+  private
+  
+  def landed
+    @flying = false
+  end
+
+  def fly
     @flying = true
   end
 
