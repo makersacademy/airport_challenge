d94f07a61d65e8d5a588b95bfb7bb9bdf61adef1
Misa Ogura <misa.ogura01@gmail.com>
2016-03-07 12:23:37 +0000

    Add back initialize with random number generation

--- lib/weather.rb
+++ lib/weather.rb
@@ -1,7 +1,12 @@
 class Weather
-  attr_reader :stormy, :random_number
+  attr_reader :random_number
+
+  # creates a sunny (80%) and stormy (20%) weather
+  def initialize number=Random.rand(1..10)
+    @number = number
+  end
 
   def stormy? number=Random.rand(1..10)
-    number <= 2 ? true : false
+    @number <= 2 ? true : false
   end
 end
