198fb8eb4c1a65f8e2ae7fce005ae6af556ab7de
Misa Ogura <misa.ogura01@gmail.com>
2016-03-07 12:33:14 +0000

    Remove ? from method that does not return bools

--- lib/airport.rb
+++ lib/airport.rb
@@ -12,14 +12,14 @@ class Airport
   end
 
   def land aircraft, weather=@weather
-    can_land? aircraft, weather
+    can_land aircraft, weather
     aircraft.change_status
     dock aircraft
     'The aircraft has landed safely to the airport'
   end
 
   def takeoff aircraft, weather=@weather
-    can_takeoff? aircraft, weather
+    can_takeoff aircraft, weather
     aircraft.change_status
     de_dock aircraft
     'The aircraft has successfully taken off from the airport'
@@ -35,13 +35,13 @@ class Airport
 
   private
 
-  def can_land? aircraft, weather
+  def can_land aircraft, weather
     raise 'The aircraft is already on the ground' if aircraft.landed
     raise 'Unable to instruct landing as the airport dock is full' if full
     raise 'Unable to instruct landing due to severe weather' if weather.stormy?
   end
 
-  def can_takeoff? aircraft, weather
+  def can_takeoff aircraft, weather
     raise 'Unable to locate the aircraft' unless @dock.include? aircraft
     raise 'Unable to instruct landing due to severe weather' if weather.stormy?
   end
