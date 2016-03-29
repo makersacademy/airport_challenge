7bdfa3be128e0effe04e7eb3a0f421b8863c9984
Sachin Karia <sachin.karia01@gmail.com>
2016-03-29 18:24:38 +0100

    refactored airport spec

--- jasmine-standalone-2.4.1/spec/AirportSpec.js
+++ jasmine-standalone-2.4.1/spec/AirportSpec.js
@@ -1,22 +1,24 @@
 describe ("airport", function() {
 
   var airport;
+  var plane;
 
   beforeEach(function(){
     airport = new Airport();
+    plane = new Plane();
   });
 
   it("default_capacity defaults to 5", function(){
     expect(airport.DEFAULT_CAPACITY).toEqual(5);
   })
 
-  it("airplanes is an emtpy array", function(){
-    expect(airport.airplanes.length).toEqual(0);
+  it("hanger is an emtpy array", function(){
+    expect(airport.hanger.length).toEqual(0);
   })
 
   it("lands plane and increases docked planes by 1", function(){
-    airport.land(plane)
-    expect(airport.airplanes.length).toEqual(1);
+    airport.land(plane);
+    expect(airport.hanger).toContain(plane);
   })
 
 });
--- jasmine-standalone-2.4.1/src/Airport.js
+++ jasmine-standalone-2.4.1/src/Airport.js
@@ -1,5 +1,13 @@
 function Airport() {
   this.DEFAULT_CAPACITY = 5;
-  this.airplanes = [];
+  this.hanger = [];
 };
+
+Airport.prototype.full = function(){
+  return this.hanger.length >= this.DEFAULT_CAPACITY;
+}
+
+Airport.prototype.land = function(plane){
+    this.hanger.push(plane);
+  }
 
