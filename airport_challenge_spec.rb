f80ff8271b5b30596a08cb0af06497b2c6ccd92a
Michael Harrison <harrim91@hotmail.co.uk>
2016-04-16 13:42:52 +0100

    added feature test for landing planes at airports

--- airport_challenge_spec.rb
+++ airport_challenge_spec.rb
@@ -1,25 +1,68 @@
 describe "Airport Challenge" do
-# As an air traffic controller
-# So I can get passengers to a destination
-# I want to instruct a plane to land at an airport and confirm that it has landed
 
-# As an air traffic controller
-# So I can get passengers on the way to their destination
-# I want to instruct a plane to take off from an airport and confirm that it is no longer in the airport
+  # As an air traffic controller
+  # So I can get passengers to a destination
+  # I want to instruct a plane to land at an airport and confirm that it has landed
 
-# As an air traffic controller
-# To ensure safety
-# I want to prevent takeoff when weather is stormy
+  describe "Planes can land at airports and confirm that they have landed" do
+    #GIVEN
+    airport = Airport.new
+    plane = Plane.new
+    #WHEN
+    plane.land
+    airport.accept plane
+    #THEN
+    expect(plane).not_to be_flying
+    expect(airport.planes).to include plane
+  end
 
-# As an air traffic controller
-# To ensure safety
-# I want to prevent landing when weather is stormy
+  # As an air traffic controller
+  # So I can get passengers on the way to their destination
+  # I want to instruct a plane to take off from an airport and confirm that it is no longer in the airport
 
-# As an air traffic controller
-# To ensure safety
-# I want to prevent landing when the airport is full
+  describe "Planes can take off from airports and confirm that they have left the airport" do
+    #GIVEN
+    #WHEN
+    #THEN
+  end
 
-# As the system designer
-# So that the software can be used for many different airports
-# I would like a default airport capacity that can be overridden as appropriate
+  # As an air traffic controller
+  # To ensure safety
+  # I want to prevent takeoff when weather is stormy
+
+  describe "Planes are prevented from taking off when the weather is stormy" do
+    #GIVEN
+    #WHEN
+    #THEN
+  end
+
+  # As an air traffic controller
+  # To ensure safety
+  # I want to prevent landing when weather is stormy
+
+  describe "Planes are prevented from landing when the weather is stormy" do
+    #GIVEN
+    #WHEN
+    #THEN
+  end
+
+  # As the system designer
+  # So that the software can be used for many different airports
+  # I would like a default airport capacity that can be overridden as appropriate
+
+  describe "Airports have a default capacity that can be overridden" do
+    #GIVEN
+    #WHEN
+    #THEN
+  end
+
+  # As an air traffic controller
+  # To ensure safety
+  # I want to prevent landing when the airport is full
+
+  describe "Planes are prevented from landing when the airport is full" do
+    #GIVEN
+    #WHEN
+    #THEN
+  end
 end
