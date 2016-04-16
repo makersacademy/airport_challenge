dd7369d6a0fbecf1d25ccfb2b7d97bb4e57f6aeb
Jackrim1 <jriminton@gmail.com>
2016-04-16 16:40:07 +0100

    attr_reader to private - testing coverage

--- lib/airport.rb
+++ lib/airport.rb
@@ -2,7 +2,7 @@ require_relative 'plane'
 
 class Airport
 
-attr_reader :planes
+
 
 def initialize
   @planes = []
@@ -16,5 +16,8 @@ def take_off(plane)
   @planes.pop
 end
 
+private
+
+attr_reader :planes
 
 end
