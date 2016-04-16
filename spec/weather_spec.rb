33e2b7aa8a2041bd624d520b6a541b7a8ca07491
gerauf <fo4786@gmail.com>
2016-04-16 20:25:42 +0100

    added read me

--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -3,12 +3,9 @@ require 'weather'
 describe Weather do
 
   describe '.is_sunny?' do
-    it 'returns true when weather_severity < 5' do
+    it 'when weather_severity < 5' do
       allow(Weather).to receive(:rand).and_return(3)
       expect(Weather).to be_sunny
-    end
-
-    it 'returns true when weather_severity > 5' do
       allow(Weather).to receive(:rand).and_return(6)
       expect(Weather).not_to be_sunny
     end
