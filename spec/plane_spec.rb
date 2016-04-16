6f973431a998ee93e97956bf04b4851bbc8ad883
ollieh-m <ollie.hm@cantab.net>
2016-04-16 12:12:23 +0100

    Make tests pass by initializing plane in the tests with the weather double expected to be passed to the airport with the receive method

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -3,27 +3,28 @@ require 'plane'
 describe Plane do
 	let(:airport){ spy(:airport) }
 	let(:weather){ double(:weather) }
+	let(:plane){ Plane.new(weather) }
 
 	context 'when trying to land' do
 		it 'should tell the airport we want it to receive the plane if plane is not already landed' do
-			subject.try_to_land(airport)
-			expect(airport).to have_received(:receive).with(subject,weather)
+			plane.try_to_land(airport)
+			expect(airport).to have_received(:receive).with(plane,weather)
 		end
 		it 'should not tell the airport we want it to receive the plane if plan is already landed' do
-			subject.land(airport)
-			subject.try_to_land(airport)
-			expect(airport).not_to have_received(:receive).with(subject,weather)
+			plane.land(airport)
+			plane.try_to_land(airport)
+			expect(airport).not_to have_received(:receive).with(plane,weather)
 		end
 		it '#land should set a landed attribute to true' do
-			subject.land(airport)
-			expect(subject.landed?).to eq true
+			plane.land(airport)
+			expect(plane.landed?).to eq true
 		end
 		it '#land should store the plan\'s location' do
-			subject.land(airport)
-			expect(subject.location).to eq airport
+			plane.land(airport)
+			expect(plane.location).to eq airport
 		end
 		it '#land should return a status update explaining that the plan landed successfully' do
-			expect(subject.land(airport)).to eq "Plane landed successfully..."
+			expect(plane.land(airport)).to eq "Plane landed successfully..."
 		end
 	end
 
