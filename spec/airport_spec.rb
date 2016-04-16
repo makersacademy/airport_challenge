353cef75a2ddb060f3646bb4739164b837baa32a
ollieh-m <ollie.hm@cantab.net>
2016-04-16 11:51:27 +0100

    Change airport tests too diagnose weather through a separate argument passed to the receive method - so planes aren't weather forecasters

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,54 +1,53 @@
 require 'airport'
 
 describe Airport do
-	let(:plane_ingoodweather){ double(:plane_ingoodweather, :stormy? => false, :land => nil, :land_fails => nil) }
-	let(:plane_inbadweather){ double(:plane_inbadweather, :stormy? => true, :land_fails => nil) }
-	
+	let(:goodweather){ double(:goodweather, :stormy? => false) }
+	let(:badweather){ double(:badweather, :stormy? => true) }
+	let(:plane){ spy(:plane) }
 
 		context 'when receiving a plane' do
 
 			context 'when weather is good' do
 				context 'when docking station is not full' do
 					it '#receive should store the plane' do
-						subject.receive(plane_ingoodweather)
-						expect(subject.landed).to include plane_ingoodweather
+						subject.receive(plane,goodweather)
+						expect(subject.landed).to include plane
 					end
 					it '#receive should tell the plane it has landed' do
-						expect(plane_ingoodweather).to receive(:land).with(subject)
-						subject.receive(plane_ingoodweather)
+						subject.receive(plane,goodweather)
+						expect(plane).to have_received(:land).with(subject)
 					end
 				end
 				context 'when docking station is full' do
 					let(:airport) do
 						airport = Airport.new(5)
-						5.times { airport.receive(plane_ingoodweather) }
+						5.times { airport.receive(plane,goodweather) }
 						airport
 					end
 					it '#receive should not store the plane' do
-						airport.receive(plane_ingoodweather)
+						airport.receive(plane,goodweather)
 						expect(airport.landed.length).to eq 5
 					end
 					it '#receive should tell the plane landing failed' do
-						expect(plane_ingoodweather).to receive(:land_fails).with("full")
-						airport.receive(plane_ingoodweather)
+						airport.receive(plane,goodweather)
+						expect(plane).to have_received(:land_fails).with("full")
 					end
 				end
-
 			end
 
 			context 'when weather is bad' do
 				context 'when docking station is not full' do
 					it '#receive should not store the plane' do
-						subject.receive(plane_inbadweather)
-						expect(subject.landed).not_to include plane_inbadweather
+						subject.receive(plane,badweather)
+						expect(subject.landed).not_to include plane
 					end
 					it '#receive should tell the plane landing failed' do
-						expect(plane_inbadweather).to receive(:land_fails).with("weather")
-						subject.receive(plane_inbadweather)
+						subject.receive(plane,badweather)
+						expect(plane).to have_received(:land_fails).with("weather")
 					end
 				end
-
 			end
+
 		end
 
 end
