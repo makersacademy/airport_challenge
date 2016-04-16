47d15065075d3de6ed209e4ec043237c4a1f645b
Lucy Clarke <l.l.c.clarke@gmail.com>
2016-04-16 17:43:24 +0100

    rspec test to allow multiple planes to land

--- lib/airport.rb
+++ lib/airport.rb
@@ -2,7 +2,8 @@ require_relative 'plane'
 
 class Airport
 
-def land
+def land(plane)
+  plane
 end
 
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,7 +2,13 @@ require 'airport'
 
 describe Airport do
 
-it { is_expected.to respond_to(:land).with(1).argument }
+  it { is_expected.to respond_to(:land).with(1).argument }
+
+  it "allows multiple planes to land" do
+    plane = Plane.new
+    expect(subject.planes[0]).to eq plane
+  end
+
 
 end
 
