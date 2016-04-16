5b27ab823403e62021da79444a2387d1eaca3b4c
ollieh-m <ollie.hm@cantab.net>
2016-04-16 11:13:56 +0100

    Test airport.receive(plane) in good conditions and not full lands and stores plane

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,4 +1,21 @@
 require 'airport'
 
 describe Airport do
+	let(:plane_ingoodweather){ double(:plane_ingoodweather, :stormy? => false, :land => nil) }
+		
+		context 'when receiving a plane' do
+			context 'when weather is good' do
+				context 'when docking station is not full' do
+					it '#receive should store the plan' do
+						subject.receive(plane_ingoodweather)
+						expect(subject.landed).to include plane_ingoodweather
+					end
+					it '#receive should tell the plane it has landed' do
+						expect(plane_ingoodweather).to receive(:land).with(subject)
+						subject.receive(plane_ingoodweather)
+					end
+				end
+			end
+		end
+
 end
