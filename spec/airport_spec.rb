8777f318c07a7929b7f259528ff63be2cec1de23
gerauf <fo4786@gmail.com>
2016-04-16 20:07:05 +0100

    clean up specs

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,25 +1,5 @@
 require 'airport'
 
-# As an air traffic controller
-# So I can get passengers to a destination
-# I want to instruct a plane to land at an airport and confirm that it has landed
-
-# As an air traffic controller
-# So I can get passengers on the way to their destination
-# I want to instruct a plane to take off from an airport and confirm that it is no longer in the airport
-
-# As an air traffic controller
-# To ensure safety
-# I want to prevent takeoff when weather is stormy
-#
-# As an air traffic controller
-# To ensure safety
-# I want to prevent landing when weather is stormy
-
-# As an air traffic controller
-# To ensure safety
-# I want to prevent landing when the airport is full
-
 describe Airport do
 
   it {is_expected.to respond_to(:land).with(1).argument}
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -1,13 +1,5 @@
 require 'plane'
 
-# As an air traffic controller
-# So I can get passengers to a destination
-# I want to instruct a plane to land at an airport and confirm that it has landed
-
-# As an air traffic controller
-# So I can get passengers on the way to their destination
-# I want to instruct a plane to take off from an airport and confirm that it is no longer in the airport
-
 describe Plane do
   it {is_expected.to respond_to(:broadcast_location)}
   it {is_expected.to respond_to(:land)}
