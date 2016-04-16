391b9b25bf56e30516c11e561011744e9ff4a1f4
ollieh-m <ollie.hm@cantab.net>
2016-04-16 10:44:17 +0100

    Make tests pass

--- lib/plane.rb
+++ lib/plane.rb
@@ -1,7 +1,19 @@
 class Plane
 
+	attr_reader :location
+
 	def try_to_land(airport)
 		airport.receive(self)
+	end
+
+	def land(airport)
+		@landed = true
+		@location = airport
+		"Plane landed successfully..."
+	end
+
+	def landed?
+		@landed
 	end
 
 end
