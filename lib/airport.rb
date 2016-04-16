d9cf9ea7a2fa1e54899bbf187e2acc86026fb078
ollieh-m <ollie.hm@cantab.net>
2016-04-16 11:43:39 +0100

    Make tests pass

--- lib/airport.rb
+++ lib/airport.rb
@@ -2,17 +2,26 @@ class Airport
 
 	attr_reader :landed
 
-	def initialize
+	def initialize(capacity=20)
 		@landed = []
+		@capacity = capacity
 	end
 
 	def receive(plane)
 		if plane.stormy?
 			plane.land_fails("weather")
+		elsif full?
+			plane.land_fails("full")
 		else
 			@landed << plane
 			plane.land(self)
 		end
+	end
+
+	private
+
+	def full?
+		landed.length == @capacity
 	end
 
 end
