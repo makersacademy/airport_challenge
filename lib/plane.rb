ff21e5c05d94a3c95935e48f3e9cca98bf6266da
Sity <sity@Sitaras-MacBook-Air.local>
2016-04-16 19:31:15 +0100

    Added an initial landed status for Plane

--- lib/plane.rb
+++ lib/plane.rb
@@ -1,2 +1,14 @@
+
 class Plane
+
+  attr_reader :landed_status
+
+  def initialize
+    @landed_status = false
+  end
+
+  def landed?
+    @landed_status = true
+  end
+
 end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -2,8 +2,8 @@ require 'plane'
 
 describe Plane do
 
-  it "has a landed status" do
-
+  it "has an initial landed_status set to false" do
+    expect(subject.landed_status).to eq false
   end
 
 end
