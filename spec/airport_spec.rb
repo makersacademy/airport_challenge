5f8073e1f404076a4bfb724b7e59760345ada73f
Lucy Clarke <l.l.c.clarke@gmail.com>
2016-04-16 18:44:03 +0100

    rspec test for airport responding to take_off

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -3,6 +3,7 @@ require 'airport'
 describe Airport do
 
   it { is_expected.to respond_to(:land).with(1).argument }
+  it { is_expected.to respond_to(:take_off) }
 
   it "allows multiple planes to land" do
     plane = Plane.new
