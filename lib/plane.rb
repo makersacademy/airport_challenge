dddec66a79ff20c0e5613c4e80adb234e4900959
AdamSkuse <a.t.skuse@gmail.com>
2016-04-16 22:04:20 +0100

    Return false to #landed? if plane takes off by adding #take_off method to Plane class.

--- lib/plane.rb
+++ lib/plane.rb
@@ -1,5 +1,10 @@
 class Plane
 attr_accessor :landed
+DEFAULT_LANDED = true
+
+def initialize(landed = DEFAULT_LANDED)
+  @landed = landed
+end
 
   def land
     @landed = true
@@ -7,6 +12,10 @@ attr_accessor :landed
 
   def landed?
     @landed
+  end
+
+  def take_off
+    @landed = false
   end
 
 end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -2,18 +2,25 @@ require 'plane'
 
 describe Plane do
 
-  describe "#land" do
+  describe '#land' do
 
-    it 'is expected to respond to #landed with true if landed' do
+    it 'is expected to respond to #landed? with true if landed' do
       subject.land
       expect(subject.landed?).to eq true
     end
 
-    it 'is expected to respond to #landed with false if not landed' do
+    it 'is expected to respond to #landed? with false if not landed' do
       subject.landed = false
       expect(subject.landed?).to eq false
     end
 end
 
+  describe '#take_off' do
+
+  it 'is expected to respond to #landed? with false when it takes off' do
+    subject.take_off
+    expect(subject.landed?).to eq false
+  end
+end
 
 end
