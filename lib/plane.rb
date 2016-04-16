b6cdaf2d954a71d8b196ecdb2febc0bd5fc818da
AdamSkuse <a.t.skuse@gmail.com>
2016-04-16 21:03:39 +0100

    make plane respond to .landed? with true if landed

--- lib/plane.rb
+++ lib/plane.rb
@@ -1,6 +1,11 @@
 class Plane
 
   def land
+    @landed = true
   end
-  
+
+  def landed?
+    @landed
+  end
+
 end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -2,4 +2,13 @@ require 'plane'
 
 describe Plane do
     it { is_expected.to respond_to :land }
+
+    it { is_expected.to respond_to :landed? }
+
+    it 'is expected to respond to #landed with true if landed' do
+subject.land
+      expect(subject.landed?).to eq true
+    end
+
+
 end
