23276d0011e5aea2d50ccc317b9e6fea5acc5310
ollieh-m <ollie.hm@cantab.net>
2016-04-16 14:47:30 +0100

    Test that stormy? returns false more than true

--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -2,8 +2,15 @@ require 'weather'
 
 describe Weather do
 
-	it '#stormy? should return true or false' do
-		expect([true,false]).to include subject.stormy?
+	context '#stormy?' do
+		it 'should return true or false' do
+			expect([true,false]).to include subject.stormy?
+		end
+		it 'should return false more than true' do
+			array = []
+			100.times { array << subject.stormy? }
+			expect(array.count(false) > array.count(true))
+		end
 	end
-	
+
 end
