6d072dc72fe0e57fc06a04febf0db31fc9bade6e
ollieh-m <ollie.hm@cantab.net>
2016-04-16 10:20:42 +0100

    Test plane.try_to_land(airport) tells airport to receive plane

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -1,5 +1,13 @@
 require 'plane'
 
 describe Plane do
-	
+	let(:airport){ spy(:airport) }
+
+	context 'when trying to land' do
+		it 'should tell the airport we want it to receive the plane' do
+			subject.try_to_land(airport)
+			expect(airport).to have_received(:receive).with(subject)
+		end
+	end
+
 end
