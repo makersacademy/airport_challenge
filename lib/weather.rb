fd21603ef58d832324d1bb695f9cae656e768da6
Lucy Clarke <l.l.c.clarke@gmail.com>
2016-04-16 20:37:51 +0100

    changed is_stormy? to stormy?

--- lib/weather.rb
+++ lib/weather.rb
@@ -1,6 +1,6 @@
 class Weather
 
-def is_stormy?
+def stormy?
 end
 
 end
--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -2,7 +2,7 @@ require 'weather'
 
 describe Weather do
 
-  it { is_expected.to respond_to(:is_stormy?)}
+  it { is_expected.to respond_to(:stormy?)}
 
 
 end
