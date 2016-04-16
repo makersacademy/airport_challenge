fa286ba45c36cfbeb16e1939d42409313b1b5e14
Jackrim1 <jriminton@gmail.com>
2016-04-16 14:39:55 +0100

    First basic test completed - airport can land a plane

---
+++ lib/airport.rb
@@ -0,0 +1,12 @@
+require_relative 'plane'
+
+class Airport
+
+def land(plane)
+
+end
+
+
+
+
+end
---
+++ spec/airport_spec.rb
@@ -0,0 +1,16 @@
+load 'airport.rb'
+
+describe Airport do
+
+  subject(:airport) { described_class.new }
+  let (:plane) { spy :plane}
+
+  it 'can land a plane' do
+    is_expected.to respond_to(:land)
+  end
+
+
+
+
+
+end
