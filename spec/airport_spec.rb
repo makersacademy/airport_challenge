6b94fd676466c8cdeac45c9e3fbc62e84b4b6ad4
ollieh-m <ollie.hm@cantab.net>
2016-04-16 11:41:02 +0100

    Test airport.receive(plan) if weather is good but capacity reached airport shouldn't store plan and should report failed landing

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,16 +1,15 @@
 require 'airport'
 
 describe Airport do
-	let(:plane_ingoodweather){ double(:plane_ingoodweather, :stormy? => false, :land => nil) }
+	let(:plane_ingoodweather){ double(:plane_ingoodweather, :stormy? => false, :land => nil, :land_fails => nil) }
 	let(:plane_inbadweather){ double(:plane_inbadweather, :stormy? => true, :land_fails => nil) }
 	
 
 		context 'when receiving a plane' do
 
 			context 'when weather is good' do
-
 				context 'when docking station is not full' do
-					it '#receive should store the plan' do
+					it '#receive should store the plane' do
 						subject.receive(plane_ingoodweather)
 						expect(subject.landed).to include plane_ingoodweather
 					end
@@ -19,11 +18,25 @@ describe Airport do
 						subject.receive(plane_ingoodweather)
 					end
 				end
+				context 'when docking station is full' do
+					let(:airport) do
+						airport = Airport.new(5)
+						5.times { airport.receive(plane_ingoodweather) }
+						airport
+					end
+					it '#receive should not store the plane' do
+						airport.receive(plane_ingoodweather)
+						expect(airport.landed.length).to eq 5
+					end
+					it '#receive should tell the plane landing failed' do
+						expect(plane_ingoodweather).to receive(:land_fails).with("full")
+						airport.receive(plane_ingoodweather)
+					end
+				end
 
 			end
 
 			context 'when weather is bad' do
-
 				context 'when docking station is not full' do
 					it '#receive should not store the plane' do
 						subject.receive(plane_inbadweather)
