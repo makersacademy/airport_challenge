68fb1e7382a75c53ca6cc07358cd541d3391e8d1
letianw <tmac4ever0824@gmail.com>
2016-04-17 02:24:10 +1000

    user stories 3-5 have been implemented for airport class

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,18 +1,23 @@
 class Airport
+  DEFAULT_CAPACITY = 20
 
   attr_reader :hangar
 
-  def initialize
+  def initialize(capacity = DEFAULT_CAPACITY,weather)
     @hangar = []
+    @capacity = capacity
+    @weather = weather
   end
 
   def land(plane)
     raise 'Stormy weather! Do not land!' if stormy?
+    raise 'Hangar full! Do not land!' if full?
     plane.land
     @hangar << plane
   end
 
   def take_off(plane)
+    raise 'Stomy weather! Do not take off!' if stormy?
     plane.take_off
     @hangar.delete(plane)
   end
@@ -20,7 +25,12 @@ class Airport
     private
 
     def stormy?
-
+      weather.stormy?
     end
+
+    def full?
+      @capacity == @hangar.length
+    end
+
 
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,63 +1,73 @@
 require 'airport'
 
 describe Airport do
+  subject {described_class.new}
 
-let(:airport) { subject }
-let(:plane) { double(:plane, land: nil, take_off: nil) }
-let(:weather) { double(:weather) }
+  let(:airport) { subject }
+  let(:plane) { spy(:plane) }
+  let(:plane1) { spy(:plane) }
 
-  context 'When weather is not stormy' do
+  describe '#land' do
 
-    context 'when a plane trying to land' do
+    context 'if the weather is not stormy' do
 
-      it 'instruct the plane to land' do
-        expect(plane).to receive(:land)
+      it 'instructs the plane to land if hangar is not full' do
+        allow(airport).to receive(:stormy?).and_return false
         airport.land(plane)
+        expect(plane).to have_received(:land)
       end
 
-      it 'confirm the plane has landed' do
+      it 'confirms the plane has landed' do
+        allow(airport).to receive(:stormy?).and_return false
         airport.land(plane)
         expect(airport.hangar).to include plane
       end
 
-    end
-
-    context 'when a plane trying to taking off' do
-
-      it 'instruct the plane to take off' do
-        airport.land(plane)
-        expect(plane).to receive(:take_off)
-        airport.take_off(plane)
-      end
-
-      it 'confirm the plane has left' do
-        airport.land(plane)
-        airport.take_off(plane)
-        expect(airport.hangar).not_to include plane
+      it 'raise an error if the hangar is full' do
+        tiny_airport = Airport.new(1)
+        allow(tiny_airport).to receive(:stormy?).and_return false
+        tiny_airport.land(plane)
+        expect{tiny_airport.land(plane1)}.to raise_error 'Hangar full! Do not land!'
       end
 
     end
 
-  end
+    context 'if the weather is stormy' do
 
-  context 'When the whether is stormy' do
-
-    context 'when a plane trying to land' do
-
-      it 'raise an error' do
-        allow(weather).to receive(:stormy?).and_return true
+      it 'raises an error and prevents landing' do
+        allow(airport).to receive(:stormy?).and_return true
         expect{airport.land(plane)}.to raise_error 'Stormy weather! Do not land!'
       end
 
-      it 'does not let the plane land' do
-        allow(weather).to receive(:stormy?).and_return true
+    end
+
+  end
+
+  describe '#take_off' do
+
+    context 'if the weather is not stormy' do
+
+      it 'instructs the plane to take off' do
+        allow(airport).to receive(:stormy?).and_return false
         airport.land(plane)
-        expect(airport.hangar).not_to include plane
+        allow(airport).to receive(:stormy?).and_return false
+        airport.take_off(plane)
+        expect(plane).to have_received(:take_off)
+      end
+
+    end
+
+    context 'if the weather is stormy' do
+
+      it 'raises an error to tell operator it is stormy' do
+        allow(airport).to receive(:stormy?).and_return false
+        airport.land(plane)
+        allow(airport).to receive(:stormy?).and_return true
+        expect{airport.take_off(plane)}.to raise_error 'Stomy weather! Do not take off!'
       end
 
     end
 
   end
-
 
 end
