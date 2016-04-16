e256d2530003f4c86004026092767271e3a28c25
gerauf <fo4786@gmail.com>
2016-04-16 20:03:07 +0100

    remove dependency on private weather method

--- lib/weather.rb
+++ lib/weather.rb
@@ -1,10 +1,7 @@
 class Weather
+  
   def self.sunny?
-    self.weather_severity < 5
+    rand(10) < 5
   end
 
-  private
-  def self.weather_severity
-    rand(10)
-  end
 end
--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -4,12 +4,12 @@ describe Weather do
 
   describe '.is_sunny?' do
     it 'returns true when weather_severity < 5' do
-      allow(Weather).to receive(:weather_severity).and_return(3)
+      allow(Weather).to receive(:rand).and_return(3)
       expect(Weather).to be_sunny
     end
 
     it 'returns true when weather_severity > 5' do
-      allow(Weather).to receive(:weather_severity).and_return(6)
+      allow(Weather).to receive(:rand).and_return(6)
       expect(Weather).not_to be_sunny
     end
   end
