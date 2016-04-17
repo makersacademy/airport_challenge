87f463d6bded4847a98cc6728e5932aed01538e5
Sity <sity@Sitaras-MacBook-Air.local>
2016-04-17 19:17:53 +0100

    When I remove the airport double, all tests pass for plane - must work out how to use double and make all tests pass

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -3,30 +3,33 @@ require 'plane'
 describe Plane do
 
   subject(:plane) { described_class.new }
-  let(:airport) { double(:airport) }
+  #let(:airport) { double(:airport) }
 
   context 'when weather is not stormy' do
     describe '#landed_status' do
-      before { allow(airport).to receive(:stormy?).and_return false }
-      before { allow(airport).to receive(:land) }
+      # before { allow(airport).to receive(:stormy?).and_return false }
+      # before { allow(airport).to receive(:land) }
 
       it 'set to false upon initialization' do
         expect(plane.landed_status).to eq false
       end
 
       it 'set to true upon landing' do
+        airport = Airport.new
+        allow(airport).to receive(:stormy?).and_return false
         airport.land(plane)
         expect(plane.landed_status).to eq true
       end
 
       it 'set to false upon take off' do
-      allow(airport).to receive(:take_off)
+      #allow(airport).to receive(:take_off)
+      airport = Airport.new
+      allow(airport).to receive(:stormy?).and_return false
       airport.land(plane)
       airport.take_off(plane)
       expect(plane.landed_status).to eq false
       end
     end
-
   end
 
 end
