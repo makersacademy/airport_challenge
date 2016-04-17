881c0077b23ba83c94b92b820b8865728aa376ac
Hanna <hannaeberhoefer@yahoo.com>
2016-04-17 19:44:17 +0100

    Added plane_spec

--- lib/plane.rb
+++ lib/plane.rb
@@ -1,21 +1,20 @@
 class Plane
 
+  def initialize
+    @flying = true
+  end
 
+  def land
+    fail 'Plane not flying' unless flying?
+    @flying = false
+  end
 
+  def take_off
+    fail 'Plane already flying' if flying?
+    @flying = true
+  end
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  def flying?
+    @flying
+  end
 end
