a9285d2355f7d6ef2474de656f36cc0bc2e85b9e
pixelandpage <mags@pixelandpage.co.uk>
2016-04-17 11:55:18 +0100

    planes cannot land when the weather is stormy ; weather is not yet randomised

--- lib/airport.rb
+++ lib/airport.rb
@@ -7,12 +7,6 @@ class Airport
     @planes = []
   end
 
-  def land(plane)
-    plane.location = self
-    @planes << plane
-    plane
-  end
-
   def safe_to_fly
     weather = Weather.new.weather_now
     if weather == 'stormy'
@@ -21,10 +15,16 @@ class Airport
       true
     end
   end
-
+  
+  def land(plane)
+    fail "Can't land when stormy" if safe_to_fly == false
+    plane.location = self
+    @planes << plane
+    plane
+  end
 
   def take_off(plane)
-      fail "Planes cannot take off if the weather is stormy" if safe_to_fly == false
+      fail "Can't take off when stormy" if safe_to_fly == false
       plane.location = "in_the_air"
       @planes.delete(plane)
   end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -7,16 +7,19 @@ let(:plane) {double :plane}
   it{is_expected.to respond_to(:land).with(1).argument}
 
   it 'can land a plane' do
+    allow(subject).to receive(:safe_to_fly).and_return(true)
     allow(plane).to receive(:location=).with(subject)
     expect(subject.land(plane)).to eq(plane)
   end
 
   it 'landing a plane sets its location to current airport' do
+    allow(subject).to receive(:safe_to_fly).and_return(true)
     expect(plane).to receive(:location=).with(subject)
     subject.land(plane)
   end
 
   it 'landed planes should be stored in the airport' do
+    allow(subject).to receive(:safe_to_fly).and_return(true)
     allow(plane).to receive(:location=).with(subject)
     subject.land(plane)
     expect(subject.planes).to include(plane)
@@ -24,23 +27,29 @@ let(:plane) {double :plane}
 
   it{is_expected.to respond_to(:take_off).with(1).argument}
 
-  # it 'taking off should set the plane\'s location to in the air' do
-  #   expect(plane).to receive(:location=).with("in_the_air")
-  #   subject.take_off(plane)
-  # end
-  #
-  # it 'planes which have taken off should no longer be in the airport' do
-  #   allow(plane).to receive(:location=).with("in_the_air")
-  #   subject.planes << plane
-  #   expect(subject.planes).to include(plane)
-  #   subject.take_off(plane)
-  #   expect(subject.planes).not_to include(plane)
-  # end
+  it 'taking off should set the plane\'s location to in the air' do
+    allow(subject).to receive(:safe_to_fly).and_return(true)
+    expect(plane).to receive(:location=).with("in_the_air")
+    subject.take_off(plane)
+  end
+
+  it 'planes which have taken off should no longer be in the airport' do
+    allow(subject).to receive(:safe_to_fly).and_return(true)
+    allow(plane).to receive(:location=).with("in_the_air")
+    subject.planes << plane
+    expect(subject.planes).to include(plane)
+    subject.take_off(plane)
+    expect(subject.planes).not_to include(plane)
+  end
 
 let(:safe_to_fly) {double :safe_to_fly}
   it 'planes should not be able to take off if it is not safe to fly' do
     allow(subject).to receive(:safe_to_fly).and_return(false)
-    expect{subject.take_off(plane)}.to raise_error("Planes cannot take off if the weather is stormy")
+    expect{subject.take_off(plane)}.to raise_error("Can't take off when stormy")
   end
 
+  it 'planes should not be able to land if it is not safe to fly' do
+    allow(subject).to receive(:safe_to_fly).and_return(false)
+    expect{subject.land(plane)}.to raise_error("Can't land when stormy")
+  end
 end
