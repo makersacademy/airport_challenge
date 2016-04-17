348b46aab4b88e1a2b9d653d21c5452ceb7639b6
Alex Robertson <lex1989@gmail.com>
2016-04-17 13:30:38 +0100

    Updated README.md

--- lib/weather.rb
+++ lib/weather.rb
@@ -5,7 +5,7 @@ class Weather
   def weather_calc(plane)
     if rand(20) > 17
       fail "Planes cannot take off due to stormy weather" if plane.ground
-      fail "Planes cannot land due to stormy weather" if !plane.ground
+      fail "Planes cannot land due to stormy weather" unless plane.ground
     end
   end
 end
