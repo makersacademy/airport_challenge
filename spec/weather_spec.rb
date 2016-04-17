b892aae3d6c13b3c10b0d3c7da3961bb9b242aab
Sity <sity@Sitaras-MacBook-Air.local>
2016-04-17 19:12:59 +0100

    removed additional end

--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -5,12 +5,12 @@ describe Weather do
   subject(:weather) { described_class.new }
 
     describe '#stormy?' do
+
         it 'returns true when rand(10) >= 7' do
           allow(weather).to receive(:rand).and_return 8
           expect(weather.stormy?). to eq true
-       end
+        end
+
     end
-    
-  end
 
 end
