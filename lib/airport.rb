eeccde9fcb2e7a3854da018d87c461342eb0acd1
hnamitha1 <namitha.h@gmail.com>
2016-04-17 19:25:39 +0100

    stormy weather

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,16 +1,19 @@
 require_relative 'plane'
+require_relative 'weather'
 
 class Airport
 	DEFAULT_CAPACITY = 20
-    attr_reader :planes, :capacity
+    attr_reader :planes, :capacity,:weather
     
-    def initialize(capacity = DEFAULT_CAPACITY)
+    def initialize(weather = Weather.new,capacity = DEFAULT_CAPACITY)
     	@planes = []
     	@capacity = capacity
+    	@weather = weather
     end	
 	
 	def landing(plane)
 		raise "Airport is full" if full?
+		raise "Unable to land due to stormy weather" if weather.stormy?
 		@planes << plane if plane.landed?
 	end	
 
@@ -23,5 +26,4 @@ class Airport
 	def full?
 		@planes.count >= capacity
 	end	
-
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,30 +1,40 @@
 require 'airport'
 
 describe Airport do 
-	subject(:airport) { described_class.new }
 	
 	it { is_expected.to respond_to(:landing).with(1).argument }
 	it { is_expected.to respond_to(:departure) }
-	let(:plane) {double :plane }
+	let(:weather){ double(:weather, :stormy? => true) }
+	let(:weather1){ double(:weather, :stormy? => false) }
+	let(:plane){ double(:plane) }
+	let(:airport){ Airport.new(weather,15) }
+	let(:airport1) {Airport.new(weather1,5)}
 	
 	describe '#land' do
         it 'confirms landed or not' do
     		allow(plane).to receive(:landed?).and_return(true)
-    		expect(subject.landing(plane)).to include plane
+    		expect(airport1.landing(plane)).to include plane
     	end	
 
     	it "raises error when airport is full" do 
     		allow(plane).to receive(:landed?).and_return(true)
-			airport.capacity.times { subject.landing(plane) }
-			expect{subject.landing(plane)}.to raise_error("Airport is full")
+			airport1.capacity.times { airport1.landing(plane) }
+			expect{airport1.landing(plane)}.to raise_error "Airport is full"
 		end
+
+		it "raises error with stormy weather" do 
+			allow(weather).to receive(:stormy?).and_return true
+			message = 'Unable to land due to stormy weather'
+  			expect { airport.landing(plane) }.to raise_error message    		
+		end
+
 	end
 
 	describe '#departure' do
 		it 'take off from the airport' do
 			plane = Plane.new
-			subject.landing(plane)
-			expect(subject.departure(plane)).to eq plane
+			airport1.landing(plane)
+			expect(airport1.departure(plane)).to eq plane
 		end	
 	end	
 end
