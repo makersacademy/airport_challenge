7740284ababc9694d80561e9a54e8f9d11075d63
chriselevate <coates.c@hotmail.com>
2016-04-17 21:01:21 +0100

    final errors fixed and submitting user story 6

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,23 +1,44 @@
+require "plane"
+
 class Airport
-  def land(_plane)
+
+  attr_reader :capacity
+
+  DEFAULT_CAPACITY = 100
+
+  def initialize(capacity=DEFAULT_CAPACITY)
+    @capacity = capacity
+    @planes= []
+  end
+
+  def land(plane)
+    fail "Airport full" if full?
     fail "Stormy cannot land" if stormy?
-    true
+    planes << plane
+    landed?
   end
 
-  def take_off(_plane)
+  def take_off
     fail "Cannot take off" if stormy?
+    planes.pop
+  end
+
+  def landed?
     true
   end
 
-  def landed?(_plane)
+  def departed?
     true
   end
 
-  def departed?(_plane)
-    true
+  def full?
+    planes.count >= capacity
   end
 
-   def stormy?(_plane)
+   def stormy?
     rand(1..2) == 1
    end
+
+  private
+   attr_reader :planes
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,41 +1,60 @@
 require "airport"
+require "plane"
 
 describe Airport do
-  subject(:airport) {described_class.new}
+  subject(:airport) {described_class.new(100)}
+  let(:plane) {double(:plane)}
+
+    describe "#initialize" do
+      it "allow the cpapcity to be set" do
+        newairport = Airport.new(20)
+        expect(newairport.capacity).to eq 20
+      end
+    end
+
 
     describe "#land" do
       it "confirms plane has landed" do
-       expect(airport.landed?(:plane)).to be true
+       expect(airport.landed?).to be true
       end
 
       context "weather" do
         it "raises an error if weather is stormy" do
           allow(airport).to receive(:stormy?).and_return true
-          expect { airport.land(:plane) }.to raise_error "Stormy cannot land"
+          expect { airport.land(plane) }.to raise_error "Stormy cannot land"
         end
 
         it "can land if not stormy" do
           allow(airport).to receive(:stormy?).and_return false
-          expect(airport.land(:plane)).to be true
+          expect(airport.land(plane)).to be true
+        end
+      end
+
+      context "full" do
+        it "raises an error" do
+          allow(airport).to receive(:stormy?).and_return false
+          100.times { airport.land(plane) }
+          full = "Airport full"
+          expect { airport.land(plane) }.to raise_error full
         end
       end
     end
 
     describe "#take_off" do
-
       it "confirms plane no longer at airport" do
-       expect(airport.departed?(:plane)).to be true
+       expect(airport.departed?).to be true
       end
 
       context "weather" do
         it "raises an error if weather is stormy" do
           allow(airport).to receive(:stormy?).and_return true
-          expect { airport.take_off(:plane) }.to raise_error "Cannot take off"
+          expect { airport.take_off }.to raise_error "Cannot take off"
         end
 
         it "can take off if not stormy" do
           allow(airport).to receive(:stormy?).and_return false
-          expect(airport.take_off(:plane)).to be true
+          airport.land(plane)
+          expect(airport.take_off).to be plane
         end
       end
     end
@@ -43,12 +62,12 @@ describe Airport do
     describe "#stormy" do
       it "can be stormy" do
       allow(airport).to receive(:stormy?).and_return true
-      expect(airport.stormy?(:plane)).to be true
+      expect(airport.stormy?(plane)).to be true
       end
 
       it "can be not stormy" do
       allow(airport).to receive(:stormy?).and_return false
-      expect(airport.stormy?(:plane)).to be false
+      expect(airport.stormy?(plane)).to be false
       end
     end
 end
