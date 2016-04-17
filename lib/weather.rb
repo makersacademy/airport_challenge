e7fd7d5d07616217be47729522df9bea39c5972c
Wayne Rumble <rumblewayne@gmail.com>
2016-04-17 18:47:20 +0100

    refactored

---
+++ lib/weather.rb
@@ -0,0 +1,7 @@
+class Weather
+
+  def stormy?
+    rand > 0.85
+  end
+
+end
---
+++ spec/weather_spec.rb
@@ -0,0 +1,21 @@
+require "weather"
+
+describe Weather do
+
+  subject(:weather) {described_class.new}
+
+  describe "#stormy?" do
+
+    it "returns false when weather isnt stormy" do
+      allow(weather).to receive(:stormy?) {false}
+      expect(subject.stormy?).to be false
+    end
+
+    it "returns true when weather is stormy" do
+      allow(weather).to receive(:stormy?) {true}
+      expect(subject.stormy?).to be true
+    end
+
+  end
+
+end
