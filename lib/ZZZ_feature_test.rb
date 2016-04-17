72b9f4c29db01e6825662266b14627d0da43b358
omajul <omajul2@hotmail.com>
2016-04-17 13:47:22 +0000

    Update feature test file with more interactions

--- lib/ZZZ_feature_test.rb
+++ lib/ZZZ_feature_test.rb
@@ -8,19 +8,14 @@ puts "Planes at #{cdg.name}:\n#{cdg.planes}"
 puts "Planes at #{lhr.name}:\n#{lhr.planes}"
 puts
 
-puts "A plane is ready for taking off if the weather is sunny"
+puts "A plane from CDG is ready for taking off if the weather is sunny"
 plane1 = cdg.planes.first
-puts plane1.take_off(cdg)
+plane1.take_off(cdg)
 puts
 
-puts "CDG has now only 1 plane"
+puts "If weather was fine, CDG must have only 1 plane (2 if stormy)"
 puts "Planes at #{cdg.name}:\n#{cdg.planes}"
-puts
-
-# After releasing the plane, the plane status is :flying and is not anymore
-# on the available planes for that airport
 puts "Status of plane #{plane1}: #{plane1.status}"
-puts "Planes at #{cdg.name}:\n#{cdg.planes}"
 puts
 
 puts "At this point, LHR still has 2 planes landed"
@@ -28,11 +23,10 @@ puts "Planes at #{lhr.name}:\n#{lhr.planes}"
 puts
 
 puts "The plane that left CDG is about to land at LHR if the weather is sunny"
-puts plane1.land(lhr)
+plane1.land(lhr)
 puts "Status of plane #{plane1}: #{plane1.status}"
 puts "Planes at #{lhr.name}:\n#{lhr.planes}"
 
-
-
-
-
+# puts "A plane at CDG airport cannot take off from LHR"
+p plane2 = cdg.planes.first
+plane2.take_off(lhr)
