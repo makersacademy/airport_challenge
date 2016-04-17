e7d57577bd2f6eb817d72bbeead761c66020e859
Macey <macebake@gmail.com>
2016-04-17 20:42:28 +0100

    Fixed landing issue

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -3,23 +3,17 @@ require 'airport'
 describe Airport do
   let(:plane) {double(:plane)}
 
-  it 'checks whether a plane is in the airport after landing' do
-    allow(plane).to receive(:land).with(subject)
-  #  allow(subject).to receive(:cleared_for_landing).with(plane)
-    plane.land(subject)
+  it 'checks whether or not a plane is in the airport after landing and takeoff' do
+    plane = Plane.new
+    (plane).land(subject)
     expect(subject.check_plane_status(plane)).to eq "Plane in airport"
-  end
-
-  it 'checks plane after takeoff' do
-  #  allow(subject).to receive(:cleared_for_takeoff).with(plane)
-    allow(plane).to receive(:takeoff).with(subject)
-    plane.takeoff(subject)
+    (plane).takeoff(subject)
     expect(subject.check_plane_status(plane)).to eq "Plane not in airport"
   end
 
   it 'raises an error if a plane tries to land when the airport is full' do
     allow(subject).to receive(:stormy?) {false}
-    allow(plane).to receive(:land).with(subject)
+    #allow(plane).to receive(:land).with(subject)
     Airport::DEFAULT_CAPACITY.times {(Plane.new).land(subject)}
     expect{subject.cleared_for_landing(plane)}.to raise_error("The airport is full.")
   end
