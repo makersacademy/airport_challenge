6272be0675c303e8a42d682c717401527e9857d8
Lucy Clarke <l.l.c.clarke@gmail.com>
2016-04-17 12:11:52 +0100

    rspec tests for when weather not stormy

--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -15,7 +15,7 @@ describe Weather do
 
   end
 
-  describe '#weather_setter_generator' do
+  describe '#weather_setter_generator when stormy' do
     before :each do
     srand(9)
 
@@ -31,6 +31,25 @@ describe Weather do
       expect(weather.stormy?).to eq true
     end
     end
+
+  describe '#weather_setter_generator when not stormy' do
+    before :each do
+    srand(8)
+
+    it "produces a random number value between 1 - 10" do
+      weather = Weather.new
+      expect(weather.weather_setter_generator).to be < 9
+      expect(weather.weather_setter_generator).to be >= 0
+    end
+
+    it "changes to true if weather_setter_generator is >= 9" do
+      weather = Weather.new
+      weather.weather_setter_generator
+      expect(weather.stormy?).to eq false
+    end
+    end
+  end
+
   end
 
 
