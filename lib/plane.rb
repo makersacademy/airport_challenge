28f1e68a196f6d6022ff520ce28e0f72c36729cb
Lexi Tran <hazukitran@gmail.com>
2016-04-17 22:49:34 +0700

    Tested and wrote code for happy path

--- lib/plane.rb
+++ lib/plane.rb
@@ -1,6 +1,29 @@
 class Plane
 
-  def landing
+  def initialize(weather, airport)
+    @weather = weather
+    @airport = airport
+  end
+
+  def land
+
+  end
+
+  def landed?
+    return false if weather.stormy? || airport.full?
     true
   end
+
+  def take_off(weather)
+
+  end
+
+  def taken_off?
+    weather.stormy? ? false : true
+  end
+
+  private
+
+    attr_reader :weather, :airport
+  
 end
--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -1,1 +1,16 @@
 require 'weather'
+
+describe Weather do 
+
+  it 'can be sunny' do
+    allow(subject).to receive(:number).and_return 1
+    expect(subject.stormy?).to be false
+    expect(subject.sunny?).to be true
+  end
+
+  it 'can be stormy' do
+    allow(subject).to receive(:number).and_return 5
+    expect(subject.stormy?).to be true
+    expect(subject.sunny?).to be false
+  end
+end
