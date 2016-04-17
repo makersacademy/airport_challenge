b908d3218150482fc8d3135f6d798b7aef900f89
Lucy Clarke <l.l.c.clarke@gmail.com>
2016-04-17 12:42:11 +0100

    changed rspec tests

--- lib/weather.rb
+++ lib/weather.rb
@@ -6,19 +6,20 @@ def initialize
   @stormy = false
 end
 
-  def stormy?
-    @stormy
-  end
+def stormy?
+  @stormy
+end
 
-
-
-  def weather_setter_generator
-    storm = rand(10)
-      if storm >= 9
+def weather_setter
+  if weather_setter_generator >= 9
         @stormy = true
       else
         @stormy = false
       end
+end
+ private
+  def weather_setter_generator
+    rand(10)
 
   end
 
--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -4,7 +4,7 @@ describe Weather do
 
 
   it { is_expected.to respond_to(:stormy?) }
-  it { is_expected.to respond_to(:weather_setter_generator) }
+  it { is_expected.to respond_to(:weather_setter) }
 
   describe '#stormy?' do
     it "is equal to false" do
@@ -15,31 +15,31 @@ describe Weather do
 
   end
 
-  #describe '#weather_setter_generator when stormy' do
-   # before :each do
-    #srand(9)
+  describe '#weather_setter_generator when stormy' do
+   before :each do
+    srand(9)
 
-    #it "changes to true if weather_setter_generator is >= 9" do
-    #  weather = Weather.new
-     # weather.weather_setter_generator
-     # expect(weather.stormy?).to eq true
-    #end
-   # end
+    it "changes to true if weather_setter_generator is >= 9" do
+      weather = Weather.new
+      weather.weather_setter
+      expect(weather.stormy?).to eq true
+    end
+    end
 
-  #describe '#weather_setter_generator when not stormy' do
-   # before :each do
-    #srand(0..8)
+  describe '#weather_setter_generator when not stormy' do
+    before :each do
+    srand(0..8)
 
 
-    #it "changes to true if weather_setter_generator is >= 9" do
-     # weather = Weather.new
-      #weather.weather_setter_generator
-      #expect(weather.stormy?).to eq false
-    #end
-    #end
-  #rend
+    it "changes to true if weather_setter_generator is >= 9" do
+      weather = Weather.new
+      weather.weather_setter
+      expect(weather.stormy?).to eq false
+    end
+    end
+  end
 
-  #end
+  end
 
 
 
