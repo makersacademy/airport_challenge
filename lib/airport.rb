46175c14364c1362a5269d9f820afcc2567a64c8
chriselevate <coates.c@hotmail.com>
2016-04-17 11:07:46 +0100

    user story 2

--- lib/airport.rb
+++ lib/airport.rb
@@ -3,7 +3,15 @@ class Airport
     true
   end
 
+  def take_off(_plane)
+    true
+  end
+
   def landed?(_plane)
+    true
+  end
+
+  def departed?(_plane)
     true
   end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -3,13 +3,23 @@ require "airport"
 describe Airport do
   subject(:airport) {described_class.new}
 
-    describe "Landing Plane" do
+    describe "landing" do
       it "enables a plane to land" do
-        expect(airport.land(:plane)).to eq true
+        expect(airport.land(:plane)).to be true
       end
 
       it "confirms plane has landed" do
-       expect(airport.landed?(:plane)).to eq true
+       expect(airport.landed?(:plane)).to be true
+      end
+    end
+
+    describe "take off" do
+      it "enables a plane to take off" do
+        expect(airport.take_off(:plane)).to be true
+      end
+
+      it "confirms plane no longer at airport" do
+       expect(airport.departed?(:plane)).to be true
       end
     end
 end
