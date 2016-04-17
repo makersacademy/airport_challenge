321e0edeec27ee10994b599d1399df8b249b25e2
RichardWatkins1 <richardwatkins1@gmail.com>
2016-04-17 15:58:38 +0100

    User stories satisfied (not elegant)....yet

--- lib/Airport.rb
+++ lib/Airport.rb
@@ -3,14 +3,45 @@ require_relative 'Plane'
 
 class Airport
 
-	attr_reader :plane
+	attr_reader :plane, :capacity
+
+	DEFAULT_CAPACITY = 50
+
+	def initialize(capacity = DEFAULT_CAPACITY)
+		@capacity = capacity
+		@planes = []
+	end
 
 	def land(plane)
-		true
+		fail 'no spaces available' if full?
+		fail 'the weather is stormy' if stormy?
+		@planes << plane
+	end
+
+
+	def release_planes
+		fail 'the weather is stormy' if stormy?
+		@planes
 	end
 
 	def take_off(plane)
-		true
+		if plane == []
+			'the planes taken off'
+		else
+			plane
+		end
 	end
+
+	def stormy?
+		rand(1..6) < 6? false : true
+	end
+
+	private
+
+
+    def full?
+        @planes.count >= capacity
+    end
+
 
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -9,16 +9,45 @@ describe Airport do
 	end
 
 	it 'says that it has landed' do 
-	expect(subject.land(plane)).to eq true
+	allow(subject).to receive(:stormy?).and_return(false)
+	expect(subject.land(plane)).to include plane
 	end
+
+
+	it 'stops the plane landing' do 
+	allow(subject).to receive(:stormy?).and_return(true)
+	expect{subject.release_planes}.to raise_error 'the weather is stormy'
+	end
+
+
 
 	it 'responds to has take off' do
 	expect {is_expected.to respond_to(:take_off).with(1).argument }
 	end
 
-	it 'tells you the plane has taken off' do
-	expect(subject.take_off(plane)).to eq true
+
+	it 'responds to release_planes' do
+	expect(subject).to respond_to 'release_planes'
 	end
+
+	it 'fails if it is stormy' do
+	allow(subject).to receive(:stormy?).and_return(true)
+	expect{subject.release_planes}.to raise_error 'the weather is stormy'
+	end
+
+	it 'fails if it is stormy' do
+	allow(subject).to receive(:stormy?).and_return(false)
+	expect(subject.release_planes).to eq []
+	end
+
+	describe '#land' do
+        it 'raises an error when exceding DEFAULT_CAPACITY' do
+      	allow(subject).to receive(:stormy?).and_return(false)
+        subject.capacity.times { subject.land plane }
+        expect { subject.land plane }.to raise_error 'no spaces available'
+        end
+    end
+
 
 end
 	
