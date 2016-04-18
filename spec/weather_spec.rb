fe2adba0c1fb0b26e4edcea6d1103ee2558b6f74
Nick <nm.richards@yahoo.com>
2016-04-18 09:12:58 -0400

    Took out some possible unnecessary tests

--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -2,8 +2,8 @@ require 'weather'
 
 describe Weather do
   subject(:weather){described_class.new}
-  it {is_expected.to respond_to :stormy?}
-  it {is_expected.to respond_to :random_weather}
+  # it {is_expected.to respond_to :stormy?}
+  # it {is_expected.to respond_to :random_weather}
 
   it 'generates a boolean for stormy?' do
     expect([true, false]).to include weather.stormy?
