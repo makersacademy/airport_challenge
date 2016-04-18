fbcc6a0019e6416f1a03631a0d9dc4d08cf106d4
Noah Pollock <pollocknoah@gmail.com>
2016-04-18 02:02:48 +0100

    weather coverage update

--- lib/weather.rb
+++ lib/weather.rb
@@ -1,11 +1,8 @@
 class Weather
   attr_accessor :condition
 
-  def initialize(condition = rand(50..100))
-    @condition = condition
+  def initialize
+    @condition = rand(50..100)
   end
 
 end
-
-
-#allow(Kernel).to receive(:rand).and_return(4)
--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -3,14 +3,8 @@ require 'weather'
 describe Weather do
 
   describe "#initialize" do
-    let (:condition) { double :condition}
-
-    it "expects condition to be set" do
-      expect(condition).to eq(condition)
-    end
-
     it "creates a random weather condition" do
-      weather = double("weather", :condition => 88)
+      weather = instance_double("weather", :condition => 88)
       expect(weather.condition).to eq(88)
     end
   end
