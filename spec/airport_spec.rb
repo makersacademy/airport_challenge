793b87abbfccf4ee5fa4a8cfc0a74e161025552a
Harry Wynn-Williams <harrywynnwilliams@googlemail.com>
2016-04-18 07:25:07 +0100

    refactored specs

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -12,64 +12,36 @@ describe Airport do
   end
 
   describe '#land' do
-    # it 'can land a plane' do
-    #   subject.land(plane)
-    #   expect(subject.plane_holder).to eq [plane]
-    # end
-    it 'can land in clear weather' do
-        expect(subject.plane_holder).to eq []
-        subject.land(plane_fine)
-        expect(subject.plane_holder).to eq [plane_fine]
-    end
-     it 'does not land in stormy weather' do
-        #allow(subject).to receive(:land).and_raise("can't land in a storm")
-        expect{subject.land(plane_storm)}.to raise_error("can't land in a storm")
-        expect(subject.plane_holder).to eq []
-    end
-     it 'does not land when the airport is full' do
-        #allow(subject).to receive(:land).and_return(false)
-        #allow(subject).to receive(:land).and_return(false)
-        10.times {(subject.land(plane_fine))}
 
-        expect {subject.land(plane_fine)}.to raise_error("can't airport full")
-        expect(subject.plane_holder).to eq [plane_fine,plane_fine,plane_fine,plane_fine,plane_fine,plane_fine,plane_fine,plane_fine,plane_fine,plane_fine]
+  it 'can land in clear weather' do
+    expect(subject.plane_holder).to eq []
+    subject.land(plane_fine)
+    expect(subject.plane_holder).to eq [plane_fine]
+   end
+  it 'does not land in stormy weather' do
+    expect{subject.land(plane_storm)}.to raise_error("can't land in a storm")
+    expect(subject.plane_holder).to eq []
+   end
+  it 'does not land when the airport is full' do
+    10.times {(subject.land(plane_fine))}
+    expect {subject.land(plane_fine)}.to raise_error("can't airport full")
+    expect(subject.plane_holder).to eq [plane_fine,plane_fine,plane_fine,plane_fine,
+      plane_fine,plane_fine,plane_fine,plane_fine,plane_fine,plane_fine]
     end
-
-# decribe "#full?" do
-#   it 'will return a boolean true if the plane plane_holder array is bigger than the capacity' do
-#      10.times (subject.land(plane))
-  # end
    end
    describe "#take_off" do
-      it 'plane can take off' do
-        #allow(subject).to receive(:land).and_return(false)
+    it 'plane can take off' do
       subject.land(plane_fine)
-         #allow(subject).to receive(:take_off).and_return(false)
       subject.take_off(plane_fine)
       expect(subject.plane_holder).to eq []
-      end
-
-
-
-      it 'takes off in clear weather' do
-      # allow(subject).to receive(:take_off).and_return(false)
-      expect {subject.take_off(plane_fine)}.not_to raise_error
-      end
-      it 'does not takes off if weather is stormy' do
-      #allow(subject).to receive(:take_off).and_raise("can't fly in storm")
-      # expect {subject.take_off(plane)}.to raise_error("can't fly in storm")
-       #expect {subject.take_off(plane_storm)}.to raise_error
-       subject.land(plane_fine)
-       expect {subject.take_off(plane_storm)}.to raise_error
-       expect(subject.plane_holder).to eq [plane_fine]
-
-
     end
-end
-  #  describe '#landed?' do
-  #   it 'can check a plane has landed' do
-  #      expect(airport).to eq(plane)
-  #   end
-  # end
-
+     it 'takes off in clear weather' do
+      expect {subject.take_off(plane_fine)}.not_to raise_error
+    end
+    it 'does not takes off if weather is stormy' do
+     subject.land(plane_fine)
+     expect {subject.take_off(plane_storm)}.to raise_error
+     expect(subject.plane_holder).to eq [plane_fine]
+    end
+  end
 end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -1,55 +1,17 @@
 require 'plane'
-#require 'mocha/setup'
 
 describe Plane do
 
  let(:plane) {Plane.new}
  let(:airport) {double("airport")}
-# let(:take_off_storm) {allow(subject).to receive(:take_off).with("Heathrow") {"can't fly in storm"}}
-# let(:take_off_clear) {allow(plane).to receive(:take_off).with("Heathrow") {@landed=false}}
 
     it 'can create a plane object' do
       expect(plane).to be_an_instance_of Plane
     end
-    # it 'plane object can call land method' do
-    #   expect(plane).to respond_to(:land).with(1).argument
-    # end
-    # it 'the plane object can take off' do
-    #  expect(plane).to respond_to(:take_off).with(1).argument
-    # end
-    # it 'check status of landing' do
-    # expect(plane).to respond_to(:landed)
-    # end
 
-
-
-
-  #   describe "#land" do
-  #     it "plane lands at the airport in the parameter" do
-  #     expect(subject.land airport).to eq airport
-  #   end
-  # end
-
-  #  describe "#take_off" do
-  #     it 'takes off in clear weather' do
-  #     allow(subject).to receive(:take_off).and_return(false)
-  #     expect {subject.take_off("Heathrow")}.not_to raise_error
-  #     end
-  #     it 'does not takes off if weather is stormy' do
-  #       allow(subject).to receive(:take_off).and_raise("can't fly in storm")
-  #       expect {subject.take_off("Heathrow")}.to raise_error("can't fly in storm")
-  #   end
-  # end
-
-
-    describe "#storm" do
-      it 'responds to weather' do
+  describe "#storm" do
+    it 'responds to weather' do
       expect(subject).to respond_to(:storm)
     end
-  #    it "returns true if storm" do
-  #     #it 'produces a random outcome on weather weighted 10% to storm' do
-  #     allow(subject).to receive(:storm) {true}
-  #     expect(subject.storm).to be true
-  #   end
   end
 end
