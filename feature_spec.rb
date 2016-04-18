0bf0999c0220e7fb9a3eb28299c189b50e8b47e0
Ruta <ruta@baltiejus.com>
2016-04-18 11:42:59 +0100

    got rid of comments to fail all my feature tests

--- feature_spec.rb
+++ feature_spec.rb
@@ -5,21 +5,21 @@ require "./lib/weather"
 airport = Airport.new
 plane = Plane.new
 
-# fail "plane did not land" unless airport.land(plane) == plane
+fail "plane did not land" unless airport.land(plane) == plane
 fail "plane landing wasn't confirmed" unless airport.land(plane) == [plane]
-# fail "plane did not take off" unless airport.take_off(plane) == plane
+fail "plane did not take off" unless airport.take_off(plane) == plane
 
-# # airport.take_off(plane)
-# fail "plane took off but is still in the airport" unless airport.planes == []
+airport.take_off(plane)
+fail "plane took off but is still in the airport" unless airport.planes == []
 
-# empty_airport = Airport.new
-# fail "airport does not start empty" unless empty_airport.planes == []
-# empty_airport.take_off(plane)
-# fail "Airport allows non-existent plane to take off" if empty_airport.planes == []
+empty_airport = Airport.new
+fail "airport does not start empty" unless empty_airport.planes == []
+empty_airport.take_off(plane)
+fail "Airport allows non-existent plane to take off" if empty_airport.planes == []
 
 weather = Weather.new
-# fail "Object responded weirdly" unless weather.new_day == "Stormy" || "Sunny"
-# fail "Plane took off in stormy weather" if airport.take_off(plane) == []
+fail "Object responded weirdly" unless weather.new_day == "Stormy" || "Sunny"
+fail "Plane took off in stormy weather" if airport.take_off(plane) == []
 fail "Returned false when sunny" if airport.safe == false
 
 plane = Plane.new
