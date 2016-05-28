81902185b8cd1c21b8896c1f3dcf869e0bfc043b
Kenneth Barrett <barrett.kenneth@gmail.com>
2016-05-28 15:07:43 +0100

    Feature test placeholder added

--- spec/feature_tests_spec.rb
+++ spec/feature_tests_spec.rb
@@ -1,12 +1,11 @@
 require 'airport'
 
-feature 'Air Traffic control' do
+describe 'Air Traffic control' do
   let(:weather) { double(:weather, stormy?: false) }
   subject(:airport) { described_class.new(10,weather) }
   let(:plane) { double(:plane, land: nil, take_off:nil, landed?: false) }
 
-  scenario 'Airport reaches capacity, then stops landings' do
-
+  it 'Airport reaches capacity, then stops landings' do
   end
 
 end
