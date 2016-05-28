3309717f7a1ba364ba221b3d256c973007a62191
Matt Butler <mattb-w@hotmail.co.uk>
2016-05-28 20:36:06 +0100

    fleshed out feature test

--- spec/feature_test_spec.rb
+++ spec/feature_test_spec.rb
@@ -3,12 +3,18 @@ require 'airport'
 require 'plane'
 
 describe 'feature test' do
-  it 'good weather' do
+  it 'Planes land and take off during good weather' do
     weather1 = Weather.new
     allow(weather1).to receive(:is_stormy?) {false}
     plane1 = Plane.new
+    plane2 = Plane.new
+    plane3 = Plane.new
     airport1 = Airport.new(weather1,10)
     airport1.dock(plane1)
-    airport1.release
+    airport1.dock(plane2)
+    airport1.dock(plane3)
+    plane3 = airport1.release
+    plane2 = airport1.release
+    airport1.dock(plane2)
   end
 end
