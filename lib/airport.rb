b9b92befcb4fe121bf1d3ec2e7637ac85462ac1f
Anna <anna.hollandsmith3@gmail.com>
2016-05-28 17:39:22 +0100

    minor ammendments to pass tests

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,7 +1,4 @@
-require_relative 'Plane'
-
 class Airport
 	def land_plane
-		true
 	end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,12 +1,7 @@
 require 'Airport'
 
 describe Airport do 
-
-  describe '#land_plane' do
-	  it 'tells the plane to land' do
-	  expect{defined_class.to respond_to(:land_plane)}
-	  end
-  end
+  it { is_expected.to respond_to(:land_plane)}
 end
 
 
