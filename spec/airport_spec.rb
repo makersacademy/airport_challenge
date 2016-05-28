882ce94b165d1174b6cd2a5184e422383c343c15
a-mellor <alexander.mellor@gmail.com>
2016-05-28 19:49:41 +0100

    created the plane and airport spec and lib files

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,3 +1,18 @@
+require 'airport'
+
 describe Airport do
-  
+  subject(:airport) { described_class.new}
 end
+# describe Airport do
+#
+#   # describe '#land' do
+#   #   it 'responds to plane_to_land' do
+#   #     expect(subject).to respond_to :land
+#   #   end
+#   #
+#   #   it 'lands a plane'
+#
+#
+#   end
+#
+# end
