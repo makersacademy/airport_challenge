47b4cf218a3f1f7d49f8c6e171356740109b3149
Toby Wilkins <tobywilkins@Tobys-MacBook-Pro-3.local>
2016-05-28 18:02:49 +0100

    delivered first 4 user stories. Planes take off & land only if weather is good and airport is not full

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,6 +1,31 @@
 class Airport
+attr_reader :planes
+DEFAULT_CAPACITY = 20
 
-def instruct_plane_to_land(plane)
-end
+  def initialize(capacity = DEFAULT_CAPACITY)
+    @planes = []
+    @capacity = capacity
+  end
+
+  def instruct_plane_to_land(plane)
+    raise 'Cannot land - airport is full' if is_full?
+    raise 'Too stormy to land' if is_stormy?
+    @planes << plane
+  end
+
+  def instruct_plane_to_take_off(plane)
+    raise 'Too stormy to take off' if is_stormy?
+    @planes.delete_at(@planes.find_index(plane))
+  end
+
+private
+
+  def is_stormy?
+     rand(6) >= 4 ? true : false
+  end
+
+  def is_full?
+    @planes.count >= @capacity ? true : false
+  end
 
 end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -4,7 +4,6 @@ describe Plane do
 
   describe '::new' do
     it {should be_instance_of(Plane)}
-    it {should initially be grounded}
   end
 
 end
