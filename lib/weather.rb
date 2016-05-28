f855ac5ddc7e73c2a8b3847de97c87098b8625e9
Luke <lukeaveil@yahoo.com>
2016-05-28 21:04:13 +0100

    test for random weather generator method, initial random weather method. Both need some working on

--- lib/weather.rb
+++ lib/weather.rb
@@ -1,3 +1,13 @@
 class Weather
 
+	def stormy
+		random == :storm
+	end
+
+	outlook = [:fine, :fine, :fine, :storm]
+
+	def random(outlook)
+		outlook.sample
+	end
+
 end
--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -2,4 +2,8 @@ require 'weather'
 
 describe Weather do
 	
+	it 'generates random weather patterns' do
+		outlook = [:fine, :fine, :fine, :storm]
+		expect(subject.random(outlook)).to eq :fine
+	end
 end
