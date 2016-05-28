7267a841fbe2d8593ec1755020e32023ab65882b
Anna <anna.hollandsmith3@gmail.com>
2016-05-29 00:56:56 +0100

    refactored unit tests for airport

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -5,13 +5,15 @@ describe Airport do
   it { is_expected.to respond_to(:land).with(1).argument }
   it { is_expected.to respond_to(:take_off).with(1).argument }
 
+  let(:plane) { double(:plane) }
+
   it 'allows a flying plane to land' do
-    plane = Plane.new
+    #plane = double(:plane, flying?: true)
     expect(subject.land(plane)).to eq "The plane has landed"
   end
   it 'allows a plane to take-off' do
-    plane = Plane.new
+    #plane = double(:bike, landed?: true)
     expect(subject.take_off(plane)).to eq "The plane has taken off"
   end
-  
+
 end
