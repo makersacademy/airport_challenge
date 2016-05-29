cbb604612ef798204b9987f146fc01fceadb66e5
Randy Marmer <rmarmer1@yahoo.com>
2016-05-29 18:07:25 +0100

    initial commit - 'uninitialized constants...'

---
+++ spec/plane_spec.rb
@@ -0,0 +1,6 @@
+require 'spec_helper'
+require 'plane'
+
+describe Plane do
+	subject(:plane) { described_class.new }
+end
---
+++ spec/weather_spec.rb
@@ -0,0 +1,6 @@
+require 'spec_helper'
+require 'weather'
+
+describe Weather do
+	subject(:weather) { described_class.new }
+end
