c2c13cf83c4b6166988a858092b4a86d1b35110a
Carlos <chdezmar@gmail.com>
2016-05-29 17:43:53 +0100

    Added tests for plane

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -2,5 +2,24 @@ require 'plane.rb'
 
 describe Plane do
 
+  it "has a default capacity" do
+    expect(subject.capacity).to eq Plane::DEFAULT_CAPACITY
+  end
+  it "is flying when started" do
+    expect(subject.flying?).to be true
+  end
+
+  it "has an unknown origin" do
+    expect(subject.origin).to be_nil
+  end
+
+  it "has an unknown destination" do
+    expect(subject.destination).to be_nil
+  end
+
+  it "allows us to change destination" do
+    subject.destination = "MAD"
+    expect(subject.destination).to eq "MAD"
+  end
 
 end
