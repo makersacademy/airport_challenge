04f7cd3f9e4190d0344f7a82aa2423b53e4aeb6a
Elena <elena.vilimaite@gmail.com>
2016-05-29 13:00:01 +0100

    bad weather added

--- lib/airport.rb
+++ lib/airport.rb
@@ -6,16 +6,24 @@ class Airport
 	def initialize(capacity)
 		@capacity = capacity
 		@planes = 0
+		@good_weather = true 
 	end
 
-	def arriving
+	def arrival
+		fail "Can't land during stormy weather" if !@good_weather
 		@planes += 1
 	end
-	
-	def departing 
+
+	def departure
+		fail "Can't take off during stormy weather" if !@good_weather
 		@planes -= 1
 	end
 
+	def change_weather
+		@good_weather = !@good_weather
+	end
+
+	private
 	def full?
 		@planes >= @capacity
 	end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -4,10 +4,23 @@ describe Airport do
 
 	subject {Airport.new(3)}
 
-	it {is_expected.to respond_to(:arriving)}
+	it {is_expected.to respond_to(:arrival)}
 
-	it {is_expected.to respond_to(:departing)}
+	it {is_expected.to respond_to(:departure)}
 
-	it {is_expected.to respond_to(:full?)}
+	it {is_expected.to respond_to(:change_weather)}
 
+	it 'changes the weather and checks it' do
+		expect(subject.change_weather).to eq false
+	end
+
+	it 'tries to accept and arriving plane in bad weather' do
+		subject.change_weather
+		expect{subject.arrival}.to raise_error("Can't land during stormy weather")
+	end
+
+	it 'tries to depart a plane in bad weather' do
+		subject.change_weather
+		expect{subject.departure}.to raise_error("Can't take off during stormy weather")
+	end
 end
