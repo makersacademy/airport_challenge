7eb2574e94626b46790d8026afaba9d1def4216d
Andrea Mazzarella <andrea.mazzarella89@gmail.com>
2016-05-29 14:28:24 +0100

    Add feature test for checking plane parking status

---
+++ spec/features/check_plane_parking_status_spec.rb
@@ -0,0 +1,24 @@
+require 'spec_helper.rb'
+require 'plane'
+require 'airport'
+ 
+feature 'Checking parking status for a plane' do
+
+  context 'When a plane has not landed' do
+    scenario 'Air traffic controller can check the plane is not in the airport' do
+      plane = Plane.new
+      airport = Airport.new
+      expect(airport.parked?(plane)).to eq false
+    end
+  end
+ 
+  context 'When a plane has landed' do 
+    scenario 'Air traffic controller can check the plane has landed' do
+      allow_any_instance_of(Weather).to receive(:stormy?).and_return(false)
+      plane = Plane.new
+      airport = Airport.new
+      airport.receive(plane)
+      expect(airport.parked?(plane)).to eq true
+    end
+  end
+end
--- spec/features/land_plane_into_airport_spec.rb
+++ spec/features/land_plane_into_airport_spec.rb
@@ -12,6 +12,14 @@ feature 'Landing a plane into an airport' do
         airport = Airport.new
         expect{airport.receive(plane)}.not_to raise_error
       end
+      
+      scenario 'Air traffic controller can check the plane has landed' do
+        allow_any_instance_of(Weather).to receive(:stormy?).and_return(false)
+        plane = Plane.new
+        airport = Airport.new
+        airport.receive(plane)
+        expect(airport.parked?(plane)).to eq true
+      end
     end
   end 
 
@@ -22,6 +30,13 @@ feature 'Landing a plane into an airport' do
         plane = Plane.new
         airport = Airport.new
         expect{airport.receive(plane)}.to raise_error
+      end
+
+      scenario 'Air traffic controller can check the plane was not landed' do
+        allow_any_instance_of(Weather).to receive(:stormy?).and_return(true)
+        plane = Plane.new
+        airport = Airport.new
+        expect(airport.parked?(plane)).to eq false
       end
     end 
   end 
