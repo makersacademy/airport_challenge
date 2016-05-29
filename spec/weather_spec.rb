0b87c493384e63989a62d58ff777048bb8281329
Apoorva Saxena <apsaxena24@gmail.com>
2016-05-30 00:40:31 +0100

    Tests for storm generator

--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -12,4 +12,14 @@ describe Weather do
       expect(subject.stormy?).to eq true
     end
   end
+  describe "#storm_generator" do
+    it 'returns a number 0' do
+      subject.stub(:storm_generator).and_return 0
+      expect(subject.storm_generator).to eq 0
+    end
+    it 'returns a number 1' do
+      subject.stub(:storm_generator).and_return 1
+      expect(subject.storm_generator).to eq 1
+    end
+  end
 end
