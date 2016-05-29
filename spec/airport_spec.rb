51a59c7c4459dddb831d6d011695d507d73f7d6b
Luke <lukeaveil@yahoo.com>
2016-05-29 10:30:31 +0100

    arranged unit tests in airport_spec to be more readable

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -7,37 +7,39 @@ describe Airport do
 	subject(:airport) {described_class.new}
 	let(:plane) {double(:plane)}
 
-	########### BEFORE LANDING ##########
-
-	it 'accepts flying plane' do
-		expect(airport.land(plane)).to eq "Welcome to London"
-	end
-
 	describe '#land' do
+
+		it 'accepts flying plane' do
+			expect(airport.land(plane)).to eq "Welcome to London"
+		end
+
+		it 'has the plane after landing' do
+			airport.land(plane)
+			allow(plane).to receive(:landed?).and_return(true)
+			expect(airport.in_airport?(plane)).to eq true
+		end
+
 		it 'raises an error when full' do
 			20.times {airport.land(plane)}
 			expect {airport.land(plane)}.to raise_error "Airport full"
 		end
+
 	end
 
-	it 'allows landed plane to take off again' do
-		airport.land(plane)
-		expect(airport.take_off(plane)).to eq "Have a pleasant flight"
-	end
+	describe '#take_off' do
 
-	########### AFTER LANDING ##########
+		it 'allows landed plane to take off again' do
+				airport.land(plane)
+				expect(airport.take_off(plane)).to eq "Have a pleasant flight"
+			end
 
-	it 'has the plane after landing' do
-		airport.land(plane)
-		allow(plane).to receive(:landed?).and_return(true)
-		expect(airport.in_airport?(plane)).to eq true
-	end
+		it 'does not have plane after takeoff' do
+			airport.land(plane)
+			airport.take_off(plane)
+			allow(plane).to receive(:landed?).and_return(false)
+			expect(airport.in_airport?(plane)).to eq false
+		end
 
-	it 'does not have plane after takeoff' do
-		airport.land(plane)
-		airport.take_off(plane)
-		allow(plane).to receive(:landed?).and_return(false)
-		expect(airport.in_airport?(plane)).to eq false
 	end
 
 end
