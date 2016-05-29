355c1eddeeb48006f696eae309ce76cb536d345d
Van Le <singularity@vannio.co.uk>
2016-05-29 11:58:24 +0100

    Added some tests for weather-related exceptions

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,22 +1,28 @@
+require "weather"
+
 class Airport
   DEFAULT_CAPACITY = 20
   ERROR_FULL = "Airport is full. Please circle the airport awkwardly.".freeze
   ERROR_NO_PLANE = "That plane doesn't exist!".freeze
+  ERROR_STORMY = "No can do. It's too stormy, cap'n!".freeze
 
   attr_reader :capacity
 
-  def initialize(capacity = DEFAULT_CAPACITY)
+  def initialize(capacity = DEFAULT_CAPACITY, weather = Weather.new)
     @capacity = capacity
+    @weather = weather
     @planes = []
   end
 
   def land_plane(plane)
+    fail ERROR_STORMY if @weather.stormy?
     fail ERROR_FULL if full?
     @planes << plane
   end
 
   def take_off(plane)
-    fail ERROR_NO_PLANE unless @planes.include?(plane)
+    fail ERROR_STORMY if @weather.stormy?
+    fail ERROR_NO_PLANE unless docked?(plane)
     @planes.delete(plane)
   end
 
@@ -26,7 +32,7 @@ class Airport
 
   private
     attr_reader :planes
-    
+
     def empty?
       @planes.size == 0
     end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -5,46 +5,65 @@ describe Airport do
   let(:weather_bad) { double(:weather, :stormy? => true) }
   let(:plane) { double(:plane) }
 
-  describe "#land_plane" do
-    # p weather_bad
-    it "instructs the plane to land" do
-      expect(subject).to receive(:land_plane)
-      subject.land_plane
+  context "in good weather" do
+    subject { described_class.new(described_class::DEFAULT_CAPACITY, weather_good) }
+
+    describe "#land_plane" do
+      it "instructs the plane to land" do
+        expect(subject).to receive(:land_plane)
+        subject.land_plane
+      end
+
+      it "confirms the plane has landed" do
+        subject.land_plane(plane)
+        expect(subject.docked?(plane)).to be_truthy
+      end
+
+      it "raises an error if the airport is at capacity" do
+        described_class::DEFAULT_CAPACITY.times { subject.land_plane(plane) }
+        expect { subject.land_plane(plane) }.to raise_error(described_class::ERROR_FULL)
+      end
     end
 
-    it "confirms the plane has landed" do
-      subject.land_plane(plane)
-      expect(subject.docked?(plane)).to be_truthy
-    end
+    describe "#take_off" do
+      it "raises an error if the plane isn't at the airport" do
+        expect { subject.take_off(plane) }.to raise_error(described_class::ERROR_NO_PLANE)
+      end
 
-    it "raises an error if the airport is at capacity" do
-      Airport::DEFAULT_CAPACITY.times { subject.land_plane(plane) }
-      expect { subject.land_plane(plane) }.to raise_error(Airport::ERROR_FULL)
+      it "instructs a plane to take off" do
+        expect(subject).to receive(:take_off)
+        subject.take_off(plane)
+      end
+
+      it "confirms the plane is not at the airport anymore" do
+        # Assuming planes need to have landed to exist at the airport.
+        # This test depends on another method working, needs refactoring :/
+        subject.land_plane(plane)
+        subject.take_off(plane)
+        expect(subject.docked?(plane)).to be_falsey
+      end
     end
   end
 
-  describe "#take_off" do
-    it "raises an error if the plane isn't at the airport" do
-      expect { subject.take_off(plane) }.to raise_error(Airport::ERROR_NO_PLANE)
+  context "in bad weather" do
+    subject { described_class.new(described_class::DEFAULT_CAPACITY, weather_bad) }
+
+    describe "#land_plane" do
+      it "does not allow a plane to land" do
+        expect { subject.land_plane(plane) }.to raise_error described_class::ERROR_STORMY
+      end
     end
 
-    it "instructs a plane to take off" do
-      expect(subject).to receive(:take_off)
-      subject.take_off(plane)
-    end
-
-    it "confirms the plane is not at the airport anymore" do
-      # Assuming planes need to have landed to exist at the airport.
-      # This test depends on another method working, needs refactoring :/
-      subject.land_plane(plane)
-      subject.take_off(plane)
-      expect(subject.docked?(plane)).to be_falsey
+    describe "#take_off" do
+      it "does not allow a plane to take off" do
+        expect { subject.take_off(plane) }.to raise_error described_class::ERROR_STORMY
+      end
     end
   end
 
   describe "#capacity" do
     it "has a default capacity" do
-      expect(subject.capacity).to eq Airport::DEFAULT_CAPACITY
+      expect(subject.capacity).to eq described_class::DEFAULT_CAPACITY
     end
 
     describe "can take an argument to set capacity" do
