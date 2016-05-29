6ef8e92f1c76b13a4f719694f19d127fc962b444
a-mellor <alexander.mellor@gmail.com>
2016-05-29 21:58:34 +0100

    removed comments

--- lib/plane.rb
+++ lib/plane.rb
@@ -1,15 +1,15 @@
 class Plane
 
   def landed?
-    true
+
   end
 
   def take_off?
-    true
+
   end
 
   def flying?
-    true
+    
   end
 
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,5 +1,5 @@
 require 'airport'
-# require_relative 'plane'
+require 'plane'
 
 describe Airport do
   subject(:airport) { described_class.new }
@@ -15,22 +15,12 @@ describe Airport do
       end
 
       it 'does not allow planes to land if the aiport is full' do
-        plane = double(:plane, :landed? => false)
+        plane = double(:plane, landed: false)
         error = 'Sorry, we\'re full. Try somewhere else'
         Airport::DEFAULT_CAPACITY.times { subject.land(plane) }
         expect { subject.land(plane) }.to raise_error error
       end
     end
-
-
-      # ir 'only lets planes land if it is not stormy' do
-      #   plane = double(:plane, :)
-      # end
-      # it 'only lets planes that are flying to land' do
-      #   message = 'That plane is already on the gound'
-      #   plane = double(:plane, :flying? => true)
-      #   expect(subject.flying?(plane)).to raise_error message
-      # end
 
     describe '#take_off'
       it 'responds to take_off' do
@@ -38,7 +28,7 @@ describe Airport do
       end
 
       it 'confirms a plane has taken off' do
-        plane = double(:plane, :take_off? => true)
+        plane = double(:plane, take_off?: true)
         expect(subject.take_off(plane)).to eq plane
       end
     end
