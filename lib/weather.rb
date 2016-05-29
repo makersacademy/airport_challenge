d1a4d5c4fcd7823d70e2eacff3d2ec85c4db4172
Van Le <singularity@vannio.co.uk>
2016-05-29 08:20:23 +0100

    Added some random weather sampling

--- lib/weather.rb
+++ lib/weather.rb
@@ -1,2 +1,12 @@
 class Weather
+  def stormy?
+    outlook_sample == :stormy
+  end
+
+  private
+    OUTLOOK = [:stormy, :sunny, :sunny, :overcast, :overcast].freeze
+
+    def outlook_sample
+      OUTLOOK.sample
+    end
 end
--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -1,4 +1,15 @@
 require "weather"
 
 describe Weather do
+  describe "#stormy?" do
+    it "returns true if stormy" do
+      allow(subject).to receive(:stormy?).and_return(true)
+      expect(subject.stormy?).to eq(true)
+    end
+
+    it "returns false if not stormy" do
+      allow(subject).to receive(:stormy?).and_return(false)
+      expect(subject.stormy?).to eq(false)
+    end
+  end
 end
