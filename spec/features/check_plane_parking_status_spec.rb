55ff0b61edf75243a2447848ecf5f65b9b3cd1bf
Andrea Mazzarella <andrea.mazzarella89@gmail.com>
2016-05-29 19:03:04 +0100

    Add a few extra feature tests

--- spec/features/check_plane_parking_status_spec.rb
+++ spec/features/check_plane_parking_status_spec.rb
@@ -21,4 +21,15 @@ feature 'Checking parking status for a plane' do
       expect(airport.parked?(plane)).to eq true
     end
   end
+
+  context 'When a plane has just taken off' do
+    scenario 'Air traffic controller can check the plane is not in the airport' do
+      allow_any_instance_of(Weather).to receive(:stormy?).and_return(false)
+      plane = Plane.new
+      airport = Airport.new
+      airport.receive(plane)
+      airport.release(plane)
+      expect(airport.parked?(plane)).to eq false
+    end
+  end
 end
--- spec/features/land_plane_into_airport_spec.rb
+++ spec/features/land_plane_into_airport_spec.rb
@@ -5,6 +5,7 @@ require 'airport'
 feature 'Instructing a plane to land into an airport' do
 
   context 'When weather conditions are good' do
+
     context 'and airport has free space' do
       scenario 'Air traffic controller can land a plane without error' do
         allow_any_instance_of(Weather).to receive(:stormy?).and_return(false)
@@ -13,9 +14,20 @@ feature 'Instructing a plane to land into an airport' do
         expect{airport.receive(plane)}.not_to raise_error
       end
     end
+
+    context 'and airport is full' do
+      scenario 'Air traffic controller cannot land a plane(error is raised)' do
+        allow_any_instance_of(Weather).to receive(:stormy?).and_return(false)
+        airport = Airport.new
+        (Airport::DEFAULT_CAPACITY).times{airport.receive(Plane.new)}
+        expect{airport.receive(plane)}.to raise_error
+      end
+    end
   end 
 
+
   context 'When weather conditions are bad' do
+
     context 'and airport has free space' do
       scenario 'Air traffic controller cannot land a plane (error is raised)' do
         allow_any_instance_of(Weather).to receive(:stormy?).and_return(true)
@@ -24,5 +36,17 @@ feature 'Instructing a plane to land into an airport' do
         expect{airport.receive(plane)}.to raise_error
       end
     end 
+
+    context 'and airport is full' do
+      scenario 'Air traffic controller cannot land a plane (error is raised)' do
+        #weather is good fist to allow the airport to fill up
+        allow_any_instance_of(Weather).to receive(:stormy?).and_return(false)
+        airport = Airport.new
+        (Airport::DEFAULT_CAPACITY).times{airport.receive(Plane.new)}
+        #back to bad weather, we are in England after all :P 
+        allow_any_instance_of(Weather).to receive(:stormy?).and_return(true)
+        expect{airport.receive(plane)}.to raise_error
+      end
+    end
   end 
 end
