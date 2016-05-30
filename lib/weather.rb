d153976f9518f935a832658488cb095232549d4e
Randy Marmer <rmarmer1@yahoo.com>
2016-05-30 23:39:12 +0100

    randomized weather test (stub not yet implimented) (usually ;) passes

--- lib/weather.rb
+++ lib/weather.rb
@@ -1,3 +1,14 @@
 class Weather
+
+	def initialize
+		@stormy = false
+		@stormy = true if rand(1..10) == 5
+	end
 	
+
+	def stormy?
+		@stormy
+
+	end
+
 end
--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -4,5 +4,8 @@ require 'weather'
 describe Weather do
 	subject(:weather) { described_class.new }
 
-	
+	it 'indicates whether or not the weather is stormy' do
+		weather = Weather.new
+		expect(weather.stormy?).to eq false
+	end
 end
