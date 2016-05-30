680af42402a0c995b396703999ea01fbcf5926fe
Jonathan <j_passe@live.fr>
2016-05-30 08:36:09 +0100

    refactor 1

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,42 +1,40 @@
 require_relative 'plane'
 class Airport
 
-  attr_reader :stormy
-  attr_reader :plane
-  attr_reader :capacity
+
+  attr_reader :stormy , :plane , :capacity
+
 
   DEFAULT_CAPACITY = 1
 
   def initialize capacity = DEFAULT_CAPACITY
     rand(2) == 1 ? @stormy = true : @stormy = false
-    @planes = Array.new
+    @planes = []
     @capacity = capacity
   end
 
   def land plane
-    raise 'The plane is on the ground Fool!' if plane.landed
-    raise 'Sorry, the airport is full. Good luck finding another airport' if self.full
-    raise 'Sorry bad weather, find another airport' if self.stormy
+    fail 'The plane is on the ground Fool!' if plane.landed
+    fail 'Sorry, the airport is full. Good luck finding another airport' if self.full
+    fail 'Sorry bad weather, find another airport' if self.stormy
     plane.landed = true
     @planes << plane
-    return self
+    self
   end
 
   def take_off plane
-    raise 'The plane is in the air Fool!' unless plane.landed
-    raise 'Sorry bad weather, wait a moment' if self.stormy
+    fail 'The plane is in the air Fool!' unless plane.landed
+    fail 'Sorry bad weather, wait a moment' if self.stormy
     plane.landed = false
     @planes.pop
-    return self
+    self
   end
 
   def full
     @planes.count >= DEFAULT_CAPACITY
   end
 
-
-
-
+private
 
 
 end
