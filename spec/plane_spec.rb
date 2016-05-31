e26212d5dcc318a7d85d68759124f14a9ec111a4
Marlon Date-Chong <mdatechong@gmail.com>
2016-05-31 11:35:53 +0100

    updated plane_spec.rb

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -2,19 +2,15 @@ require 'airport'
 
 describe "Plane" do 
 	
-	# it "checks if plane hass parked" do 
-	# 	expect(Plane.new).to respond_to :parked
- #    end
-  # it "checks if planes are in the airport" do
-  #   expect()
+  subject(:plane) { Plane.new }
 
   it "checks if plane has landed or taken off" do
-    expect(Plane.new).to respond_to :landed
-    expect(Plane.new).to respond_to :taken_off
+    expect(plane).to respond_to :landed
+    expect(plane).to respond_to :taken_off
   end
 
   it "checks landing state" do
-  	expect(Plane.new).to respond_to :airport
+  	expect(plane).to respond_to :airport
   end
 
 end
