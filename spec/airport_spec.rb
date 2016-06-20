929496eb35f54669f1363881b207a6452ecb4b17
fareedpatel <fareed_patel@hotmail.co.uk>
2016-06-20 18:38:27 +0100

    user story3 finished off

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -8,19 +8,18 @@ describe Airport do
 	subject { described_class.new(capacity: 20)}
 	let(:plane) {Plane.new}
 
-	it 'lets plane land' do
+	it 'Lets plane land' do
 	  plane.land?
 	  expect { subject.land(plane) }.to change {subject.count_plane}.by 1
 	end
 
-	it 'does not let plane land and raises error when full' do
+	it 'Does not let planes land and raises error when full' do
 	  20.times { subject.land(plane) }
-	  expect { subject.land(plane) }.to raise_error('does not let plane land' )	
+	  expect { subject.land(plane) }.to raise_error('does not let plane land')
 	end
 
-    it 'once plane has taken off it is no longer in the airport' do
-      subject.land(plane)
-      expect { subject.taking_off(plane) }.to change {subject.count_plane}.by (-1)
-    end
-
+  it 'Once plane has taken off it is no longer in the airport' do
+    subject.land(plane)
+    expect { subject.taking_off(plane) }.to change {subject.count_plane}.by (-1)
+  end
 end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -6,12 +6,17 @@ describe Plane do
 
 subject { described_class.new }
 
-	it 'If I want a plane to land' do
+	it 'If I want a plane to land it must have a flying status' do
 		expect(subject).to be_flying
 	end
 
 	it 'Once landed it does not have a flying status' do
 		subject.land?
 		expect(subject).not_to be_flying
+	end
+
+	it 'Once taken off it has a flying status' do
+    subject.flying?
+    expect(subject).to be_flying
 	end
 end
