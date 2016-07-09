bce06dc6af8d7819d7df331cb11224697865194a
Nomi Vos <nvoswork@gmail.com>
2016-07-09 14:00:59 -0500

    creates local variable 'plane' to refer to the class 'Plane' instead of using subject to refer to 'Plane' in plane_spec.rb

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -3,10 +3,11 @@ require 'plane'
 describe Plane do
   context '#land' do
 
+    subject(:plane) { described_class.new }
     it { is_expected.to respond_to(:land).with(1).arguments }
     it 'checks if plane has landed' do
       airport = AirPort.new
-      expect(subject.land(airport)).to eq 'The plane has landed'
+      expect(plane.land(airport)).to eq 'The plane has landed'
 
     end
   end
@@ -17,6 +18,7 @@ describe Plane do
     it 'checks if plane has taken off' do
       airport = AirPort.new
       expect(subject.take_off(airport)).to eq 'The plane has taken off'
+
     end
   end
 
