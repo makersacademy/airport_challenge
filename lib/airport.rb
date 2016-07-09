427fa8902f269603c2fda6ea0df5eb53a8efae1a
Rebecca Pearce <becciepearce@hotmail.co.uk>
2016-07-09 17:29:16 +0100

    Landing creates a new plane

--- lib/airport.rb
+++ lib/airport.rb
@@ -3,6 +3,6 @@ require 'plane'
 class Airport
 
   def land
-
+      plane = Plane.new
   end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,4 +1,13 @@
 require 'airport'
+require 'plane'
 
 describe Airport do
   it {is_expected.to respond_to(:land)}
+
+
+  describe '#land' do
+    it 'Lands a plane' do
+      expect(subject.land).to be_instance_of Plane
+    end
+  end
+end
