1738ca078d4e7a6757156a6f06147cac61441c04
Rylan Gooch <rylangooch@gmail.com>
2016-07-09 20:49:05 +0100

    Added tests and preliminary code for airport class with 3 methods (land, take_off and plane, the latter of which will become an attr_reader)

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,3 +1,12 @@
 class Airport
 
+  def land
+  end
+
+  def plane
+  end
+
+  def take_off
+  end
+
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -4,4 +4,8 @@ describe Airport do
 
 it { is_expected.to respond_to(:land)}
 
+it { is_expected.to respond_to(:plane)}
+
+it { is_expected.to respond_to(:take_off)}
+
 end
