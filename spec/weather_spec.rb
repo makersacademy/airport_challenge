5fc1c2040b03be24055740be1b65dbd5505cb713
Thady Condon <thadycondon@hotmail.com>
2016-07-09 21:16:04 +0100

    Created and passed feature and unit test for weather guage to check it returns a random number. Also created a stub for weather guage which sets it to five which will allow to test for stormy conditions.

--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -1,16 +1,19 @@
 require 'weather'
 
 describe WeatherReport do
-  #let(:weather_report) {double :weathergauge}
+  #let(:gauge) {double :gauge}
   it {is_expected.to respond_to(:weather_conditions)}
   it {is_expected.to respond_to(:weather_gauge)}
 
   it 'is stormy under certain conditions' do
-    #weather_report = double(:weather_gauge => 5)
+
+    #weather_gauge = double(:weather_gauge => 5)
+    allow(subject).to receive(:weather_gauge) { 5 }
+    expect(subject.weather_gauge).to eq(5)
     expect(subject.weather_conditions).to eq("stormy")
   end
 
-  it 'exects weather gauge to return a fixnum' do
+  it 'exects weather gauge to return a number between 0 and 5' do
     expect(subject.weather_gauge).to be_between(0,5).inclusive
   end
 
