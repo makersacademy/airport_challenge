f291c2858868b828196cef148371078096aa909e
Nomi Vos <nvoswork@gmail.com>
2016-07-09 16:48:43 -0500

    removes the tests to check if the plane has landed and taken off in the plane_spec.rb file

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -3,22 +3,4 @@ require 'plane'
 describe Plane do
   subject(:plane) { described_class.new }
 
-  # context '#land' do
-  #
-  #   it { is_expected.to respond_to :land }
-  #   it 'checks if plane has landed' do
-  #     expect(plane.land).to eq 'The plane has landed'
-  #
-  #   end
-  #end
-
-  context '#take_off' do
-
-    it { is_expected.to respond_to :take_off }
-    it 'checks if plane has taken off' do
-      expect(plane.take_off).to eq 'The plane has taken off'
-
-    end
-  end
-
 end
