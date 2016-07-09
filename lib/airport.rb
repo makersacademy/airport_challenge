c968bf93bc10a0be60b689100b184c8946ca8e16
Thady Condon <thadycondon@hotmail.com>
2016-07-09 11:35:52 +0100

    Created feature and unit tests for weather

--- lib/airport.rb
+++ lib/airport.rb
@@ -6,7 +6,7 @@ class Airport
 
 def land_plane(plane)
 @plane = plane
-@plane
+@plane.landed?
 end
 
 
--- lib/feature_test.rb
+++ lib/feature_test.rb
@@ -6,8 +6,13 @@ puts airport = Airport.new
 puts 'create a new plane'
 puts plane = Plane.new
 
+puts 'create weather report'
+puts weather_report = Weather.new
+
+#puts 'check the weather conditions'
+
 puts 'land plane'
 puts airport.land_plane(plane)
 
-puts 'plane confirms landing'
-puts plane.landed?
+#puts 'plane confirms landing'
+#puts plane.landed?
