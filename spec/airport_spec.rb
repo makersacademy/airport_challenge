5173d8cebe568aeef0f8bb8415c4f8e772ea534a
nikklein <objcoding@gmail.com>
2016-07-09 13:01:09 +0100

    Add test for land method with 1 argument and correct the next test message

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -3,7 +3,8 @@ require 'plane'
 
 describe Airport do
 it {is_expected.to respond_to :land}
-it 'plane lands and airport confirms landing' do
+it {is_expected.to respond_to(:land).with(1).argument }
+it 'instruct a plane to land and airport confirms landing' do
 airport = Airport.new
 plane = Plane.new
 expect(subject.land(plane)).to eq "The plane #{plane} has landed"
