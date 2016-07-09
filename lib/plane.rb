93394cf3a3e9ac7c1aefa6bf466aacb2e0f22ee6
Nomi Vos <nvoswork@gmail.com>
2016-07-09 13:56:30 -0500

    adds a test to check if plane has taken off.  Adds method called take_off that returns the string 'The plane has taken off'

--- lib/plane.rb
+++ lib/plane.rb
@@ -6,4 +6,8 @@ class Plane
     'The plane has landed'
   end
 
+  def take_off(airport)
+    'The plane has taken off'
+  end
+
 end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -1,10 +1,23 @@
 require 'plane'
 
 describe Plane do
-  it { is_expected.to respond_to(:land).with(1).arguments }
-  it 'checks if plane has landed' do
-    airport = AirPort.new
-    expect(subject.land(airport)).to eq 'The plane has landed'
+  context '#land' do
+
+    it { is_expected.to respond_to(:land).with(1).arguments }
+    it 'checks if plane has landed' do
+      airport = AirPort.new
+      expect(subject.land(airport)).to eq 'The plane has landed'
+
+    end
+  end
+
+  context '#take_off' do
+
+    it { is_expected.to respond_to(:take_off).with(1).arguments }
+    it 'checks if plane has taken off' do
+      airport = AirPort.new
+      expect(subject.take_off(airport)).to eq 'The plane has taken off'
+    end
   end
 
 end
