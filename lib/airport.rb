e2cdab389736be91197901906938584ca1a38e56
Nomi Vos <nvoswork@gmail.com>
2016-07-09 16:08:07 -0500

    Adds test in airport_spec.rb to check if airport capacity is full.  Adds method called capacity which returns string 'full'

--- lib/airport.rb
+++ lib/airport.rb
@@ -3,4 +3,8 @@ class AirPort
   def confirm_landed
 
   end
+
+  def capacity
+    'full'
+  end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,9 +1,21 @@
 require 'airport'
 
 describe AirPort do
+  subject(:airport) {described_class.new }
 
   context '#confirm_landed' do
     it { is_expected.to respond_to :confirm_landed }
+    airport = AirPort.new
+
+  end
+
+  context '#capacity' do
+    it { is_expected.to respond_to :capacity }
+    airport = AirPort.new
+    it 'checks to see if capacity is full' do
+      expect(airport.capacity).to eq 'full'
+    end
+
   end
 
 end
