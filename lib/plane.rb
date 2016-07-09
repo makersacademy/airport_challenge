86b061cdf5a23542829ed3939f038937eecc0ed5
JAstbury <jess.astbury@hotmail.com>
2016-07-09 15:22:58 +0100

    Adds basic methods for take_off and land

--- lib/plane.rb
+++ lib/plane.rb
@@ -1,3 +1,18 @@
+require_relative 'airport'
+
 class Plane
+  attr_reader :landed
+
+  def initialize
+    @landed = false
+  end
+
+  def land
+    @landed = true
+  end
+
+  def take_off
+    @landed = false
+  end
 
 end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -2,4 +2,23 @@ require 'plane'
 
 describe Plane do
 
+  it { is_expected.to respond_to :land }
+  it { is_expected.to respond_to :take_off}
+
+  describe '#land' do
+    it 'changes plane to landed' do
+      plane = Plane.new
+      plane.land
+      expect(plane.land).to eq true
+    end
+  end
+
+  describe '#take_off' do
+    it 'changes plane to not landed' do
+      plane = Plane.new
+      plane.land
+      expect(plane.take_off).to eq false
+    end
+  end
+
 end
