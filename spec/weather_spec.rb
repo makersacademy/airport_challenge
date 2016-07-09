99a340ed688aec67f0ba200484590b587c4174ed
Thady Condon <thadycondon@hotmail.com>
2016-07-09 11:53:55 +0100

    Created unit test to check if weather gauge is responding, this isn't picked up in the feature test as weather gauge will be passed into weather conditions which is already working in feature test.

--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -2,4 +2,8 @@ require 'weather'
 
 describe WeatherReport do
 it {is_expected.to respond_to(:weather_conditions)}
+it {is_expected.to respond_to(:weather_gauge)}
+#it 'is stormy under certain conditions' do
+#end
+
 end
