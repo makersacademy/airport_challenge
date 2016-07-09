2d7bb5775c5b18e72c85d500ba4afc54b5d91016
Nomi Vos <nvoswork@gmail.com>
2016-07-09 16:46:47 -0500

    Adds method called request_take_off that calls the method take_off in the plane.rb file. Adds tests to see if the plane takes off in airport_spec.rb file

--- lib/airport.rb
+++ lib/airport.rb
@@ -6,6 +6,10 @@ class Airport
     plane.land
   end
 
+  def request_take_off(plane)
+    plane.take_off
+  end
+
   def full?
     'full'
   end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -3,15 +3,24 @@ require 'plane'
 
 describe Airport do
   subject(:airport) {described_class.new }
+  airport = Airport.new
+  plane = Plane.new
 
   context '#request_landing' do
     it { is_expected.to respond_to(:request_landing).with(1).arguments }
     it 'tells plane to land' do
-      airport = Airport.new
-      plane = Plane.new
+      # airport = Airport.new
+      # plane = Plane.new
       expect(airport.request_landing(plane)).to eq 'The plane has landed'
     end
 
+  end
+
+  context '#request_take_off' do
+    it { is_expected.to respond_to(:request_take_off).with(1).arguments }
+    it 'tells plane to take off' do
+      expect(airport.request_take_off(plane)).to eq 'The plane has taken off'
+    end
   end
 
   context '#full?' do
