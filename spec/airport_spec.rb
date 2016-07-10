04ccf35f77c34e6ed1100e70e8547d63ee8f0a68
Jose <jcopoviking@outlook.com>
2016-07-10 16:28:18 +0100

    replaces :plane symbol with double for plane obejcts in airport_spec

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -4,23 +4,26 @@ require 'plane'
 describe Airport do
 
   let(:airport1) {Airport.new}
-  let(:plane1) {Plane.new}
+
 
   context "planes landing and take-off" do
-
     it "a plane can be instructed to land" do
+      plane1 = double(:plane1)
       airport1.land_plane(plane1)
     end
 
     it "a plane can be instructed to take-off" do
+      plane1 = double(:plane1)
       airport1.take_off_plane(plane1)
     end
 
     it "confirms that the plane has landed" do
+      plane1 = double(:plane1)
       expect( airport1.land_plane( plane1 ) ).to eq "#{plane1} has successfully landed"
     end
 
     it "confirms that the plane has taken-off" do
+      plane1 = double(:plane1)
       expect( airport1.take_off_plane( plane1 ) ).to eq "#{plane1} has successfully taken-off"
     end
   end
