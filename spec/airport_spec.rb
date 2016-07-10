64ba395709ab137dc5ba2ea9354386ad55dce8b6
Rylan Gooch <rylangooch@gmail.com>
2016-07-10 18:53:45 +0100

    Refactored and removed unnecessary comments

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -4,49 +4,30 @@ describe Airport do
 subject { described_class.new(20) }
   let(:plane) { double(:plane) }
 
-  it 'does not allow landing when it is full' do
-    20.times { subject.land(:plane) }
-    expect { subject.land(:plane) }.to raise_error "Cannot land plane as airport is full"
-  end
-
   describe'#land' do
-#  let(:plane) { double(:plane) }
 
     it 'instructs a plane to land' do
       expect(subject).to respond_to(:land).with(1).argument
     end
 
-#    it 'confirms that a plane has landed' do
-#      expect(subject.land(:plane)).to eq(:plane)
-#    end
-
+    it 'does not allow landing when it is full' do
+      20.times { subject.land(plane) }
+      expect { subject.land(plane) }.to raise_error "Cannot land plane as airport is full"
+    end
   end
 
-
-# it { is_expected.to respond_to(:plane)}
-# this is now taken care of in example below
-
   describe '#plane' do
-#  subject { described_class.new(20) }
-# let(:plane) { double(:plane) }
 
       it 'confirms what planes are in an airport' do
-        expect{ subject.plane }.to eq(@planes)
+        expect(subject.plane).to eq(@planes)
       end
     end
 
   describe '#take_off' do
-  #  subject { described_class.new }
-#    let(:plane) { double(:plane) }
 
     it 'instructs a plane to take off' do
       expect(subject).to respond_to(:take_off).with(1).argument
     end
-
-#    it 'confirms that a plane has taken off' do
-#      subject.land(:plane)
-#      expect(subject.take_off(:plane)).to eq(:plane)
-#    end
 
   end
 end
