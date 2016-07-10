8e6ba1d4ad65d27cd3c00f25fd6599767d46cb37
yurizhuravel <zhuravel@gmail.com>
2016-07-10 18:06:58 +0100

    Airport now denies landing in stormy weather

--- lib/airport.rb
+++ lib/airport.rb
@@ -6,13 +6,17 @@ class Airport
   #  Plane.new
   #end
 
+  def weather_stormy?
+    #The weather generator: it will return true 10% of the time
+    rand(101) <= 10
+  end
+
   def deny_takeoff
     'The weather is stormy, takeoff denied' if weather_stormy?
   end
 
-  def weather_stormy?
-    #The weather generator: it will return true 10% of the time
-    rand(101) <= 10
+  def deny_landing
+    'The weather is stormy, landing denied' if weather_stormy?
   end
 
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -9,8 +9,12 @@ describe Airport do
 
   it {is_expected.to respond_to(:weather_stormy?)}
 
-  it 'prevents takeoffs in stormy weather' do
+  it 'prevents takeoff in stormy weather' do
     allow(subject.deny_takeoff).to receive(:weather_stormy?){1}.and_return ('The weather is stormy, takeoff denied')
+  end
+
+  it 'prevents landing in stormy weather' do
+    allow(subject.deny_landing).to receive(:weather_stormy?){1}.and_return ('The weather is stormy, landing denied')
   end
 
 end
