20694453b409c67c75b25ca1f664cf944ca71e0b
Richard Bates <richo225@gmail.com>
2016-07-10 13:09:51 +0100

    First user story. Pass unit test to land plane

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,3 +1,11 @@
+require_relative "plane.rb"
+
 class Airport
-  
+  def initialize
+    @planes = []
+  end
+
+  def land(plane)
+    @planes << plane
+  end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,10 +2,9 @@ require "airport"
 require "plane"
 
 describe Airport do
-  subject(:airport) {Airport.new} #give new instance name airport
 
   describe "#land" do
-    subject(:airport) {Airport.new}
+    subject(:airport) {Airport.new} #give new instance name airport
     it "lands a plane" do
       expect(airport).to respond_to(:land).with(1).argument
     end
