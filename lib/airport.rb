a89fca0f1438b3d7c5936bf4960643c8bcb47fd6
Sophie Gill <sophie-gill@hotmail.com>
2016-07-10 16:07:26 +0100

    added a land method to plane which accepts one argument

--- lib/airport.rb
+++ lib/airport.rb
@@ -2,4 +2,8 @@ require_relative "plane"
 
 class Airport
 
+  def land(plane)
+
+  end
+
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,5 +2,8 @@ require "airport"
 
 describe Airport do
   subject(:airport) { described_class.new }
+  let(:plane) { double :plane }
+
+  it { is_expected.to(respond_to(:land).with(1).argument) }
 
 end
