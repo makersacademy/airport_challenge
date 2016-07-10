68f2a2fe0a096c6ca5998e236f7f1ebe8fe717fe
Laurence kite <laurencekite@Laurences-MBP.home>
2016-07-10 23:26:45 +0100

    added test for take off it responds to it

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -6,5 +6,7 @@ describe Plane do
   it 'confirms that it has landed' do
     expect(subject.confirm_landing).to eq 'plane has landed'
   end
-
+  it 'resoponds to take_off' do
+    expect(subject).to respond_to :take_off
+  end
 end
