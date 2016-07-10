96bd004e564f74823c91b95cce5c1c02e313a684
Titus Iudean <iudeant@gmail.com>
2016-07-11 00:02:50 +0300

    Airport accepts landing plane and confirms landing

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,2 +1,7 @@
+require_relative "plane.rb"
+
 class Airport
+  def accepts_landing(plane)
+    "Plane landed"
+  end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,5 +1,8 @@
 require "airport"
 
 describe Airport do
-
+  it 'accepts airplanes to land' do
+    plane = Plane.new
+    expect(subject.accepts_landing(plane)).to eq "Plane landed"
+  end
 end
