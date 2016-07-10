64f511f05c9911b9a76b7dd5ea687cdd8cd7b950
Rylan Gooch <rylangooch@gmail.com>
2016-07-10 18:38:55 +0100

    User stories 1 and 2 covered

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,12 +1,30 @@
+require_relative 'plane'
+
 class Airport
 
-  def land
+  def initialize(capacity)
+    @capacity = capacity
+    @planes = []
+  end
+
+
+  def land(plane)
+    raise "Cannot land plane as airport is full" if full?
+    @planes << plane
   end
 
   def plane
+    @planes
   end
 
-  def take_off
+  def take_off(plane)
+    @planes.pop
+  end
+
+  private
+
+  def full?
+    @planes.length >= @capacity
   end
 
 end
---
+++ spec/plane_spec.rb
@@ -0,0 +1,8 @@
+require 'plane'
+
+describe Plane do
+
+  it { is_expected.to respond_to :landed? }
+
+
+end
