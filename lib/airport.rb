8bafa011fd16c6e13024927a71c7bea2c91c4c66
Sophie Gill <sophie-gill@hotmail.com>
2016-07-10 16:19:53 +0100

    added a plane flying check to the land method in airport

--- lib/airport.rb
+++ lib/airport.rb
@@ -3,11 +3,11 @@ require_relative "plane"
 class Airport
 
   def land(plane)
-
+    is_flying?(plane)
   end
 
   def is_flying?(plane)
-
+    plane.landed?
   end
 
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -7,4 +7,18 @@ describe Airport do
   it { is_expected.to(respond_to(:land).with(1).argument) }
   it { is_expected.to(respond_to(:is_flying?).with(1).argument) }
 
+  describe '#land' do
+    it 'checks to see if the plane is flying' do
+      expect(subject).to(receive(:is_flying?))
+      subject.land(plane)
+    end
+  end
+
+  describe '#is_flying?' do
+    it 'returns the landed? status of the plane' do
+      expect(plane).to(receive(:landed?))
+      subject.is_flying?(plane)
+    end
+  end
+
 end
