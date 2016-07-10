beac7b78c824a793c3aa0fa65614656167136051
Arunas Skirius <arukomp@gmail.com>
2016-07-10 13:20:52 +0100

    A couple more changes to pass Hound check

--- lib/plane.rb
+++ lib/plane.rb
@@ -5,12 +5,12 @@ class Plane
   end
 
   def land
-    raise 'Plane already landed' if landed?
+    fail 'Plane already landed' if landed?
     @landed = true
   end
 
   def take_off
-    raise 'Plane already in air' unless landed?
+    fail 'Plane already in air' unless landed?
     @landed = false
   end
 
