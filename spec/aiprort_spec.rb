89c45859d4bc547ed159638ac2dfb551e0fbddf4
Jonny Moore <jonnymoore121212@gmail.com>
2016-07-10 21:06:00 +0100

    Refactors clear weather tests

--- spec/aiprort_spec.rb
+++ spec/aiprort_spec.rb
@@ -10,46 +10,48 @@ describe Airport do
     expect(subject.capacity).to eq Airport::DEFAULT_CAPACITY
   end
 
-  describe '#land' do
-    it 'instructs a plane to land at airport and confirms it has landed' do
-      plane = Plane.new
+  context 'clear weather' do
+    before do
       allow(subject).to receive(:stormy?).and_return(false)
-      expect(subject.land(plane)).to eq("Plane #{plane} has landed.")
     end
-    it 'raises an error when we try to land a plane at an airport already at capacity' do
-      allow(subject).to receive(:stormy?).and_return(false)
-      subject.capacity.times {subject.land(Plane.new)}
-      expect{subject.land(Plane.new)}.to raise_error("Plane cannot land. Airport full.")
-    end
-    it 'cannot land a plane which has already landed' do
-      plane = Plane.new
-      allow(subject).to receive(:stormy?).and_return(false)
-      subject.land(plane)
-      expect{subject.land(plane)}.to raise_error("Plane already landed!")
-    end
-  end
 
-  describe '#take_off' do
-    it 'instructs a plane to take off from airport and confirms it is in the air' do
-      plane = Plane.new
-      allow(subject).to receive(:stormy?).and_return(false)
-      subject.land(plane)
-      expect(subject.take_off(plane)).to eq ("Plane #{plane} has left the airport and is in the air.")
+    describe '#land' do
+      it 'instructs a plane to land at airport and confirms it has landed' do
+        plane = Plane.new
+        expect(subject.land(plane)).to eq("Plane #{plane} has landed.")
+      end
+      it 'cannot land a plane at a full capacity airport' do
+        subject.capacity.times {subject.land(Plane.new)}
+        expect{subject.land(Plane.new)}.to raise_error("Plane cannot land. Airport full.")
+      end
+      it 'cannot land a plane which has already landed' do
+        plane = Plane.new
+        subject.land(plane)
+        expect{subject.land(plane)}.to raise_error("Plane already landed!")
+      end
     end
-    it 'plane which is flying cannot take off' do
-      expect{subject.take_off(Plane.new)}.to raise_error("Plane already flying!")
-    end
-    it 'planes can only take off from airports they are in' do
-      plane = Plane.new
-      allow(subject).to receive(:stormy?).and_return(false)
-      subject.land(plane)
-      expect{Airport.new.take_off(plane)}.to raise_error("Plane not in that airport!")
-    end
-  end
 
-  context 'when stormy' do
     describe '#take_off' do
-      it 'prevents take off when weather is stormy' do
+      it 'instructs a plane to take off from airport and confirms it is in the air' do
+        plane = Plane.new
+        subject.land(plane)
+        expect(subject.take_off(plane)).to eq ("Plane #{plane} has left the airport and is in the air.")
+      end
+      it 'plane which is flying cannot take off' do
+        expect{subject.take_off(Plane.new)}.to raise_error("Plane already flying!")
+      end
+      it 'planes can only take off from airports they are in' do
+        plane = Plane.new
+        subject.land(plane)
+        expect{Airport.new.take_off(plane)}.to raise_error("Plane not in that airport!")
+      end
+    end
+
+  end
+
+  context 'stormy weather' do
+    describe '#take_off' do
+      it 'prevents take off if weather is stormy' do
         plane = Plane.new
         allow(subject).to receive(:stormy?).and_return(false)
         subject.land(plane)
