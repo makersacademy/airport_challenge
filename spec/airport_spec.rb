b2fe0f0d7451015ae77af8ddb42da1a1de64cf50
nikklein <objcoding@gmail.com>
2016-07-11 00:54:47 +0100

    Changes according to Hound test

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -6,51 +6,51 @@ describe Airport do
 it {is_expected.to respond_to :land}
 it {is_expected.to respond_to(:land).with(2).argument }
 it 'instructs a plane to land and airport confirms landing' do
-plane = double()
-weather = double()
+plane = double(Plane)
+weather = double(Weather)
 allow(weather).to receive(:stormy?).and_return(false)
-expect(subject.land plane, weather).to eq "The plane #{plane} has landed"
+expect(subject.land(plane, weather)).to eq "The plane #{plane} has landed"
 end
 
 
 it {is_expected.to respond_to :take_off}
 it {is_expected.to respond_to(:take_off).with(2).argument}
 it 'instructs a plane to take off and confirms it' do
-plane = double()
-weather = double()
+plane = double(Plane)
+weather = double(Weather)
 allow(weather).to receive(:stormy?).and_return(false)
 subject.land plane, weather
-weather = double()
+weather = double(Weather)
 allow(weather).to receive(:stormy?).and_return(false)
-expect(subject.take_off plane, weather).to eq "The plane #{plane} has taken off"
+expect(subject.take_off(plane, weather)).to eq "The plane #{plane} has taken off"
 end
 
 it 'when a plane took off from airport it confirms that plane is no longer in airport' do
-plane = double()
-weather = double()
+plane = double(Plane)
+weather = double(Weather)
 allow(weather).to receive(:stormy?).and_return(false)
 subject.land plane, weather
-weather = double()
+weather = double(Weather)
 allow(weather).to receive(:stormy?).and_return(false)
 subject.take_off plane, weather
 expect(subject.planes.map{|x| x if x == plane}.empty?).to be true
 end
 
 it 'prevents take off if weather is stormy' do
- plane = double()
- weather = double()
+ plane = double(Plane)
+ weather = double(Weather)
  allow(weather).to receive(:stormy?).and_return(false)
  subject.land plane, weather
- weather = double()
+ weather = double(Weather)
  allow(weather).to receive(:stormy?).and_return(true)
- expect{subject.take_off plane, weather}.to raise_error('Weather condition is bad, no take offs')
+ expect{subject.take_off(plane, weather)}.to raise_error('Weather condition is bad, no take offs')
  end
 
 it 'prevents landing if weather is stormy' do
-  plane = double()
-  weather = double()
+  plane = double(Plane)
+  weather = double(Weather)
   allow(weather).to receive(:stormy?).and_return(true)
-  expect{subject.land plane, weather}.to raise_error('Weather condition is bad, no landing')
+  expect{subject.land(plane, weather)}.to raise_error('Weather condition is bad, no landing')
 end
 it 'prevents landing if airport is full' do
   expect(subject.planes.size).to be <= Airport::DEFAULT_CAPACITY
@@ -59,4 +59,5 @@ it 'default airport capacity could be overridden as appropriate' do
 airport = Airport.new(30)
   expect(airport.capacity).to eq 30
 end
+
 end
--- spec/feature_spec.rb
+++ spec/feature_spec.rb
@@ -1,11 +1,12 @@
 require './lib/airport'
 require './lib/plane'
 require './lib/weather'
-
+=begin
 puts airport = Airport.new
 puts plane = Plane.new
-puts airport.land plane
+puts airport.land plane, weather
 puts airport.planes
 puts airport.take_off plane
 puts airport.planes
 puts weather = Weather.new
+=end
