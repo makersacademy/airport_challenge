caaefabed6888336e1029e10d139e52f7865f2c7
Sophie Gill <sophie-gill@hotmail.com>
2016-07-10 17:40:22 +0100

    airport now lands planes and returns an array of landed planes

--- lib/airport.rb
+++ lib/airport.rb
@@ -2,13 +2,18 @@ require_relative "plane"
 
 class Airport
 
+  def initialize
+    @planes_array = []
+  end
+
   def planes
-    
+    @planes_array
   end
 
   def land(plane)
     fail "Plane has already landed" if is_flying?(plane)
     plane.land_plane
+    @planes_array.push(plane)
   end
 
   def is_flying?(plane)
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -8,16 +8,32 @@ describe Airport do
   it { is_expected.to(respond_to(:is_flying?).with(1).argument) }
   it { is_expected.to(respond_to(:planes))}
 
+  describe '#planes' do
+    it 'returns landed planes' do
+      allow(plane).to(receive(:landed?))
+      allow(plane).to(receive(:land_plane))
+      subject.land(plane)
+      expect(subject.planes).to(eq([plane]))
+    end
+  end
+
+
   describe '#land' do
     it 'returns an error if the plane is already landed' do
-      expect(plane).to(receive(:landed?).and_return(true))
+      allow(plane).to(receive(:landed?).and_return(true))
       expect{ subject.land(plane) }.to(raise_error("Plane has already landed"))
     end
     it 'lands a plane' do
-      expect(plane).to(receive(:landed?).and_return(false))
+      allow(plane).to(receive(:landed?).and_return(false))
       expect(plane).to(receive(:land_plane))
       subject.land(plane)
     end
+    #it 'confirms that a plane as landed' do
+    #  allow(plane).to(receive(:landed?).and_return(false))
+    #  allow(plane).to(receive(:land_plane))
+    #  allow(plane).to(receive(:landed?).and_return(false))
+    #  expect{ subject.land(plane) }.to(raise_error("Error - plane did not land"))
+    #end
   end
 
   describe '#is_flying?' do
