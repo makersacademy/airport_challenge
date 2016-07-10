26550f6fef930f52f6c214b6b75ac1581c2f8e54
Tommy Williams <tom@teow.co.uk>
2016-07-10 20:26:57 +0100

    prevent landing when the airport is full

--- lib/airport.rb
+++ lib/airport.rb
@@ -2,20 +2,33 @@ require_relative 'plane'
 require_relative 'weather'
 
 class Airport
+  DEFAULT_CAPACITY = 5
 
-  def initialize
-    @planes = []
+  def initialize(capacity = DEFAULT_CAPACITY)
+    @capacity = capacity
+    @landed_planes = []
+    @airborn_planes = []
   end
 
   def land(plane)
     fail "airport experiencing stormy weather" unless Weather.clear?
-    @planes << plane
-    @planes.last
+    fail "airport full" if full?
+    @landed_planes << plane
+    @landed_planes.last
   end
 
-  def take_off
+  def take_off(plane)
     fail "airport experiencing stormy weather" unless Weather.clear?
-    @planes.pop
+    @landed_planes.pop
+    @airborn_planes << plane
+  end
+
+attr_reader :capacity
+
+private
+
+  def full?
+    @landed_planes.count >= @capacity
   end
 
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -15,16 +15,22 @@ describe '#land' do
     allow(Weather).to receive(:clear?).and_return(false)
     expect { subject.land(Plane.new)}.to raise_error("airport experiencing stormy weather")
   end
+
+  it 'raises an error when airport full' do
+    allow(Weather).to receive(:clear?) {true}
+    subject.capacity.times { subject.land(Plane.new) }
+    expect { subject.land Plane.new }.to raise_error("airport full")
+  end
 end
 
 describe '#take_off' do
-  it {is_expected.to respond_to(:take_off)}
+  it {is_expected.to respond_to(:take_off).with(1).argument}
 
   it 'raises an error when stormy weather' do
     allow(Weather).to receive(:clear?) {true}
     subject.land(Plane.new)
     allow(Weather).to receive(:clear?).and_return(false)
-    expect { subject.take_off }.to raise_error("airport experiencing stormy weather")
+    expect { subject.take_off(Plane.new) }.to raise_error("airport experiencing stormy weather")
   end
 
   # it 'takes off a plane and returns confirmation'
