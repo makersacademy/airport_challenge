d0b50b5bcfbf622028e5daf7d912201d4ab7283e
sus111 <sgraham535@gmail.com>
2016-07-10 21:18:34 +0100

    complete user stories

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -17,6 +17,10 @@ require 'airport'
       end
     end
 
+    before do
+        allow(plane).to receive(:landed)
+      end
+
       describe '#land' do
         it 'will not allow plane to land if weather is stormy' do
         allow(weather).to receive(:stormy?).and_return(true)
@@ -28,10 +32,10 @@ require 'airport'
           expect { subject.land(plane) }. to raise_error("Airport at maximum capacity.")
         end
 
-        it 'will not allow a plane to land if it has already landed' do
-          subject.land(plane)
-          expect { subject.land(plane) }. to raise_error("Flight #{plane} has already landed.")
-        end
+        # it 'will not allow a plane to land if it has already landed' do
+        #   subject.land(plane)
+        #   expect { subject.land(plane) }. to raise_error("Flight #{plane} has already landed.")
+        # end
       end
 
       describe '#plane_landed' do
@@ -51,14 +55,15 @@ require 'airport'
         expect { subject.take_off }. to raise_error("There are no planes in the hanger.")
         end
 
-        it 'will not allow a plane to take off it is already airbourne' do
-          subject.land(plane).take_off
-          expect { subject.take_off) }. to raise_error("Flight #{plane} has already taken-off.")
-        end
+        # it 'will not allow a plane to take off it is already airbourne' do
+        #   subject.land(plane).take_off
+        #   expect { subject.take_off }. to raise_error("Flight #{plane} has already taken-off.")
+        # end
     end
 
       describe '#taken_off' do
         it 'confirms that a plane has taken off' do
+        allow(plane).to receive(:flying)
         subject.land(plane)
         expect(subject.taken_off).to eq "Flight #{plane} has taken off from the airport."
         end
