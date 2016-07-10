c5ff376b9309e0970329d0cb3390d8cfb9b4052d
Thady Condon <thadycondon@hotmail.com>
2016-07-10 08:42:57 +0100

    Created unit test to check safe conditions in clear weather

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,19 +1,27 @@
 require 'airport'
 
 describe Airport do
-it { is_expected.to respond_to :land_plane }
+  let(:weather_report) {double :WeatherReport}
+  it { is_expected.to respond_to :land_plane }
 
-it 'Land plane receives a plane'do
-  plane = Plane.new
-  subject.land_plane(plane)
-end
+  it 'Land plane receives a plane'do
+    plane = Plane.new
+    subject.land_plane(plane)
+  end
 
-it "checks the weather report" do
-  weather_report = double(:weather_conditions => "clear")
-  allow(weather_report).to receive(:weather_conditions) { "stormy" }
-  expect(subject.safe_conditions?(weather_report.weather_conditions)).to eq(false)
-  #expect{subject.safe_to_land?(weather_report.weather_conditions)}.to raise_error "Unable to land due to stormy conditions"
-end
+  it "checks the weather report result when stormy" do
+    #weather_report = double(:weather_conditions => "clear")
+    allow(weather_report).to receive(:weather_conditions) { "stormy" }
+    expect(subject.safe_conditions?(weather_report.weather_conditions)).to eq(false)
+    #expect{subject.safe_to_land?(weather_report.weather_conditions)}.to raise_error "Unable to land due to stormy conditions"
+  end
+
+  it "cecks the weather report result when clear" do
+    allow(weather_report).to receive(:weather_conditions) { "clear"}
+    expect(subject.safe_conditions?(weather_report.weather_conditions)).to eq(true)
+  end
+
+
 
 #it "ensures the plane lands in clear weather" do
 #allow(weather_report).to receive(:weather_conditions) { "clear" }
