ff2c66b9e4abb56bbbf6bfb69feb95d63c7799ad
Richard Bates <richo225@gmail.com>
2016-07-10 12:55:15 +0100

    Implement first user story. First failing test for landing plane

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,5 +1,13 @@
 require "airport"
+require "plane"
 
 describe Airport do
   subject(:airport) {Airport.new} #give new instance name airport
+
+  describe "#land" do
+    subject(:airport) {Airport.new}
+    it "lands a plane" do
+      expect(airport).to respond_to(:land).with(1).argument
+    end
+  end
 end
---
+++ spec/plane_spec.rb
@@ -0,0 +1,5 @@
+require "plane"
+
+describe Plane do
+  subject(:plane) {Plane.new} #give new instance name plane
+end
