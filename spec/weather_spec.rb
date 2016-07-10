926557abd89982961455bbd9d8ac4fa425af7c99
Jack Oddy <jackoddy91@gmail.com>
2016-07-10 22:12:15 +0100

    restores spec_helper again

--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -2,7 +2,7 @@ require 'weather'
 
 describe Weather do
   describe '#fair?' do
-        it 'responds to the method' do
+        it 'responds to the method with true or false' do
             expect(Weather.fair?).to satisfy {|value| (value == true) || (value == false) }
         end
     end
