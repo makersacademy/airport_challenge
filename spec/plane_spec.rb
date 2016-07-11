1fada8c22eaad57bae849aee857fcad7e51a8588
Laurence kite <laurencekite@Laurences-MacBook-Pro-2.local>
2016-07-11 10:02:38 +0100

    updated spec file a bit"

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -9,4 +9,12 @@ describe Plane do
   it 'resoponds to take_off' do
     expect(subject).to respond_to :take_off
   end
+  it 'responds to deny_take_off for stormy weather' do
+     # allow(die).to receive(:roll) { 3 } using this
+     allow(subject.deny_take_off).to receive(:weather_stormy?){1}.and_return ('Sorry no flights today weather is stormy')
+  end
+  it 'it responds to deny_land for stormy weeath' do
+    
+  end
+  end
 end
