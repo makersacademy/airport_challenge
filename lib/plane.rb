23fb469bdc029f894b6f4fdbfa139976280f13aa
liskowsky <karollebiedzinski@gmail.com>
2016-07-11 20:15:23 +0100

    More refactoring done

--- lib/plane.rb
+++ lib/plane.rb
@@ -1,4 +1,10 @@
 class Plane
+  attr_reader :landed, :flying
+
+  def initialize
+    @landed = true
+    @flying = false
+  end
 
   def flying?
 
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -4,33 +4,55 @@ describe Airport do
   subject(:airport) { described_class.new }
   let(:plane) {double :plane}
 
-  it "instructs planes to land and confirms it has landed" do
-    allow(subject).to receive(:stormy?).and_return(false)
-    expect(subject.land(plane)).to eq "#{plane} has landed"
-  end
-  it "instructs planes to take off" do
-    allow(subject).to receive(:stormy?).and_return(false)
-    subject.land(plane)
-    expect(subject.takeoff(plane)).to eq "#{plane} is up in the air"
+
+  describe "#Land" do
+    context "when weather is sunny" do
+      before do
+        allow(subject).to receive(:stormy?).and_return false
+      end
+      it "instructs planes to land" do
+        expect(subject.land(plane)).to eq "#{plane} has landed"
+      end
+      context "when the airport is full" do
+        it "prevents planes form landing" do
+          subject.instance_variable_get(:@capacity).times { subject.land(plane) }
+          expect { subject.land(plane) }.to raise_error "There is no space"
+        end
+      end
+    end
+    context "when weather is stormy" do
+      before do
+        allow(subject).to receive(:stormy?).and_return true
+      end
+      it "prevents planes form landing" do
+        expect { subject.land(plane) }.to raise_error "Stormy weather!"
+      end
+    end
   end
 
-  describe "#Stormy weather" do
-    it "prevents planes form taking off if the weather is stormy" do
-      allow(subject).to receive(:stormy?).and_return(true)
-      expect { subject.takeoff(plane) }.to raise_error "Stormy weather!"
+
+  describe "#Take off" do
+    context "when weather is sunny" do
+      before do
+        allow(subject).to receive(:stormy?).and_return false
+      end
+      it "instructs planes to take off" do
+        subject.land(plane)
+        expect(subject.takeoff(plane)).to eq "#{plane} is up in the air"
+      end
     end
-    it "prevents planes form landing if the weather is stormy" do
-      allow(subject).to receive(:stormy?).and_return(true)
-      expect { subject.land(plane) }.to raise_error "Stormy weather!"
+    context "when weather is stormy" do
+      before do
+        allow(subject).to receive(:stormy?).and_return true
+      end
+      it "prevents planes form taking off" do
+        expect { subject.takeoff(plane) }.to raise_error "Stormy weather!"
+      end
     end
   end
+
 
   describe "#Capacity" do
-    it "prevents planes form landing if the airport if full" do
-      allow(subject).to receive(:stormy?).and_return(false)
-      subject.instance_variable_get(:@capacity).times { subject.land(plane) }
-      expect { subject.land(plane) }.to raise_error "There is no space"
-    end
     it "can be overridden" do
       airport1 = Airport.new(66)
       expect(airport1.instance_variable_get(:@capacity)).to eq 66
