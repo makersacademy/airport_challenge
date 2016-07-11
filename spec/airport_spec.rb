757b47842c8c0f75db7d2464f4863e37354f96cd
Frunez <francesco.nunez@gmail.com>
2016-07-11 09:14:40 +0100

    docking test

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,7 +1,7 @@
 require 'airport'
 
 describe Airport do
-  describe '#cap'
+  describe '#cap' do
     it 'has a set capacity' do
       airport = Airport.new(28)
       expect(airport.cap).to eq 28
@@ -9,9 +9,11 @@ describe Airport do
     it 'has a default capacity if none is   predefined' do
       expect(subject.cap).to eq Airport::DEF_CAP
     end
+  end
 
   describe '#docked(plane)' do
     it 'makes a record of all currently landed planes' do
       plane = double(:plane, landed: true)
     end
+  end
 end
