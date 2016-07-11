33e72624aeb2832a62f396387b2a9ffeb2e22bcc
Sara <sarah.alalaya@gmail.com>
2016-07-11 09:27:41 +0300

    Planes that are landed cannot land again

--- lib/airport.rb
+++ lib/airport.rb
@@ -7,12 +7,20 @@ class Airport
   end
 
   def land(plane)
+    message = 'Unable to land a plane that has already landed before'
+    raise message if plane_has_landed_before(plane)
     planes << plane
+    plane.has_landed
   end
 
 
   private
+
   attr_reader :planes
+
+  def plane_has_landed_before(plane)
+    planes.include?(plane)
+  end
 
 
 end
--- spec/airporte_spec.rb
+++ spec/airporte_spec.rb
@@ -1,15 +1,23 @@
 require 'airport'
 
 describe Airport do
-  it {is_expected.to respond_to :land }
+  it {is_expected.to respond_to(:land).with(1).argument }
 
   describe '#land' do
     subject(:airport) { described_class.new }
     let(:plane) { double :plane }
-    it "Lands a plane" do
+
+    it "Lands a plane and has it in after landing" do
       allow(plane).to receive(:land)
       airport.land(plane)
       expect(airport.send(:planes)).to include plane
     end
+
+    it "Raises an error when instructed to land a plane that has already landed"  do
+      message = 'Unable to land a plane that has already landed before'
+      airport.land(plane)
+      expect {airport.land(plane)}.to raise_error message
+    end
+
   end
 end
