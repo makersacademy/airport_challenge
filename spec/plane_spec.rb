fc2754d38b4d8331a40997750a642a82731b9385
liskowsky <karollebiedzinski@gmail.com>
2016-07-11 23:23:18 +0100

    Partial solution 2

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -9,7 +9,6 @@ describe Plane do
     allow(subject).to receive(:flying?).and_return(true)
     expect { subject.takeoff }.to raise_error "Plane is already flying"
   end
-
   it "cannot land if it's already on the ground" do
     allow(subject).to receive(:flying?).and_return(false)
     expect { subject.land }.to raise_error "Plane is on the ground"
