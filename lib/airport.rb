b4006cc669d07bd2bb012c27ead30dcb790b8e88
Titus Iudean <iudeant@gmail.com>
2016-07-11 10:28:08 +0300

    Adds array to Airport to hold landed airplanes

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,7 +1,15 @@
 require_relative "plane.rb"
 
 class Airport
-  def accepts_landing(plane)
-    "Plane landed"
+  attr_accessor :planes
+  def initialize()
+    @planes = []
+  end
+  def allows_landing(plane)
+    planes << plane
+    "Aircraft #{plane} has landed"
+  end
+  def allows_takeoff
+    "Aircraft #{@planes.pop} has departed"
   end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,8 +1,14 @@
 require "airport"
 
 describe Airport do
-  it 'accepts airplanes to land' do
+  
+  it 'allows airplanes to land and confirms arrival' do
     plane = Plane.new
-    expect(subject.accepts_landing(plane)).to eq "Plane landed"
+    expect(subject.allows_landing(plane)).to eq "Aircraft #{plane} has landed"
+  end
+  it "allows airplanes to take off and confirms departure" do
+    plane = Plane.new
+    subject.allows_landing(plane)
+    expect(subject.allows_takeoff).to eq "Aircraft #{plane} has departed"
   end
 end
