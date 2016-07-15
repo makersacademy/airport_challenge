8c57d47fb4de681f5d4b80f74a72a816cf5f4f06
cyberplanner <saleh.gfx@gmail.com>
2016-07-15 20:10:33 +0100

    User story#2 As an air traffic controller So I can get passengers on the way to their destination I want to instruct a plane to take off from an airport and confirm that it is no longer in the airport

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -3,7 +3,11 @@ require "airport"
 describe Airport do
   subject(:airport) { described_class.new }
 
-  it "instructs a plan to land" do
+  it "instructs a plane to land" do
     expect(airport).to respond_to(:land).with(1).argument
+  end
+
+  it "instructs a plane to take off" do
+    expect(airport).to respond_to(:take_off).with(1).argument
   end
 end
--- spec/features/user_stories_spec.rb
+++ spec/features/user_stories_spec.rb
@@ -1,4 +1,3 @@
-#require "airport"
 describe "user stories" do
   # As an air traffic controller
   # So I can get passengers to a destination
@@ -9,6 +8,30 @@ describe "user stories" do
     plane = Plane.new
     expect { airport.land(plane) }.not_to raise_error
   end
+
+  # As an air traffic controller
+  # So I can get passengers on the way to their destination
+  # I want to instruct a plane to take off from an airport and confirm that it is no longer in the airport
+  it 'so plans can take off from an airport, instruct planes to take off' do
+    airport = Airport.new
+    plane = Plane.new
+    expect { airport.take_off(plane) }.not_to raise_error
+  end
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 end
