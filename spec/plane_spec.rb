6b2f662c6a74241e0d31b4abd9f591a82ff21898
Matthew Perkins <mfperkins@gmail.com>
2016-08-20 16:00:38 +0100

    added tests for Plane methods

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -1,1 +1,19 @@
 require 'plane'
+
+describe Plane do
+
+  describe '#is_flying' do
+
+    it "should turn the plane's flying status to true" do
+      expect(subject.is_flying).to eq true
+    end
+  end
+
+  describe '#has_landed' do
+    it "should set the plane's landed status to true" do
+      expect(subject.has_landed).to eq true
+    end
+  end
+
+
+end
