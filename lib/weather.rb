34ca64ab2e39db4e2d714d3bf0962af44b6a027d
lsewilson <laurawilson@hotmail.co.uk>
2016-08-20 16:08:49 +0100

    adds weather class and stormy method

---
+++ lib/weather.rb
@@ -0,0 +1,8 @@
+
+class Weather
+
+  def stormy?
+    true 
+  end
+
+end
---
+++ spec/weather_spec.rb
@@ -0,0 +1,12 @@
+require 'weather'
+
+describe Weather do
+
+# test 1 does weather respond to stormy?
+
+  it { is_expected.to respond_to(:stormy?) }
+
+# test 2 does takeoff fail if stormy
+
+
+end
