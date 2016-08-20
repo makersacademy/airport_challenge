187644012164841209af08399f1386a02a310a59
lsewilson <laurawilson@hotmail.co.uk>
2016-08-20 15:51:25 +0100

    refactors airport_spec file

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,21 +2,19 @@ require 'airport'
 
 describe Airport do
 
+let(:plane) { double :plane }
+
   describe '#land' do
-    it { is_expected.to respond_to(:land).with(1).argument }
 
     it 'lands a plane at the airport' do
-      plane = Plane.new
       expect(subject.land(plane)).to eq subject
     end
 
   end
 
   describe '#takeoff' do
-    it { is_expected.to respond_to(:takeoff).with(1).argument }
 
     it 'makes a plane take off from the airport' do
-      plane = Plane.new
       subject.land(plane)
       expect(subject.takeoff(plane)).to eq plane
     end
@@ -26,13 +24,11 @@ describe Airport do
   describe '#planes' do
 
     it 'has registered landed plane at airport' do
-      plane = Plane.new
       subject.land(plane)
       expect(subject.planes).to eq [plane]
     end
 
     it 'confirms that plane is no longer at airport after takeoff' do
-      plane = Plane.new
       subject.land(plane)
       subject.takeoff(plane)
       expect(subject.planes).not_to include plane
