7018ff05d4e40adcb314cc7d6975ca7777f51630
BenRoss92 <benjaminjackross@gmail.com>
2016-08-20 18:48:43 +0100

    wrote test for checking landing status after landing

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -4,4 +4,10 @@ describe Plane do
 
   it { is_expected.to respond_to (:landed?) }
 
+  it 'confirms it has landed after landing' do
+    airport = Airport.new
+    airport.land(subject)
+    expect(subject.landed?).to be(true)
+  end
+
 end
