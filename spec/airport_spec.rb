1e235e4de7c1799e0a63a6757845a999e2450dff
johnnydee8 <j.dworzynski@me.com>
2016-08-20 17:57:45 +0100

    added test for land method

---
+++ spec/airport_spec.rb
@@ -0,0 +1,11 @@
+
+
+describe Airport do
+  let (:plane) {double :plane}
+
+  it "allows planes to land to an airport and confirms that it has landed" do
+    expect(subject.land(plane)).to eq true
+  end
+
+
+end
