eb5623850ee00a9b5d4d63e6429bcdd479ba009d
Lawrence Dawson <lawrencedawson@hotmail.co.uk>
2016-08-20 20:44:57 +0100

    test for land method passed

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,2 +1,5 @@
 class Airport
+
+  def land
+  end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,4 +2,8 @@ require 'airport.rb'
 
 describe Airport do
 
+  it "responds to 'land'" do
+    expect(subject).to respond_to :land
+  end 
+
 end
