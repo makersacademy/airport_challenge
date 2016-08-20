5c59eae86c28ee5268128c437087970f674da9d3
James Stonehill <jamesstonehill@gmail.com>
2016-08-20 16:59:56 +0100

    the airport capacity can now be set to a user defined value

--- lib/Airport.rb
+++ lib/Airport.rb
@@ -1,7 +1,8 @@
 class Airport
   attr_reader :capacity
-  
-  def initialize(capacity = 20)
+  CAPACITY = 40
+
+  def initialize(capacity = CAPACITY)
     @capacity = capacity #capacity refers to the number of planes in the airport
   end
 end
--- spec/Airport_spec.rb
+++ spec/Airport_spec.rb
@@ -2,6 +2,15 @@ require 'Airport.rb'
 
 describe Airport do
 
+  it 'is possible to set the airport capacity to a user defined number' do
+    plane_capacity = 6666
+    big_airport = Airport.new(plane_capacity)
+    expect(big_airport.capacity).to eq(plane_capacity)
+  end
+
+  it 'has a default capaicty of 40' do
+    expect(subject.capacity).to eq(40)
+  end
 
 
 end
