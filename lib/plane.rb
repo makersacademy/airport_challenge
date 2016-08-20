32727be0dadb8d6f54649bd64c06d12e60182adb
Thao Vo <ttthao.vo@gmail.com>
2016-08-20 19:47:28 +0100

    Implemented land method in plane.rb and added unit test

--- lib/plane.rb
+++ lib/plane.rb
@@ -1,3 +1,6 @@
 class Plane
 
+  def land
+    print 'Plane has landed.'
+  end
 end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -1,5 +1,22 @@
 require 'plane'
 
 describe Plane do
+  describe "#land" do
+    before do
+      @plane = Plane.new
+    end
 
+    it "confirms landing status" do
+      expect do
+        @plane.land
+      end.to output('Plane has landed.').to_stdout
+    end
+
+    it "does not print something else apart from 'Plane has landed'" do
+      expect do
+        @plane.land
+      end.to_not output('Plane has crashed').to_stdout
+    end
+
+  end
 end
