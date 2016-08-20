cca7f816ee791ca641078b3ae62fbfbd3b3224e3
Abigail <abigail.mcphillips@gmail.com>
2016-08-20 11:23:41 +0100

    creates plane class and passes test for landing plane in airport

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,6 +1,9 @@
+require_relative 'plane'
+
 class Airport
 
   def land(plane)
+    plane
   end
 
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -4,4 +4,9 @@ describe Airport do
 
   it {is_expected.to respond_to(:land).with(1).argument}
 
+  it "lands plane" do
+    plane = Plane.new
+    expect(subject.land(plane)).to eq plane
+  end
+
 end
