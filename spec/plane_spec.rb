e619cc3aec9ef98d53a2bc644f2ad8a6f94f49ed
lsewilson <laurawilson@hotmail.co.uk>
2016-08-20 21:16:05 +0100

    refactors good and bad weather settings

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -2,6 +2,21 @@ require 'plane'
 
 describe Plane do
 
+subject(:plane) { described_class.new }
 
+  it {is_expected.to respond_to(:landed)}
+  it {is_expected.to respond_to(:taken_off)}
+
+  describe '#landed' do
+    it 'should return true if the plane has landed' do
+      expect(plane.landed).to eq true
+    end
+  end
+
+  describe '#taken_off' do
+    it 'should return false if the plane has taken off' do
+      expect(plane.taken_off).to eq false
+    end
+  end
 
 end
