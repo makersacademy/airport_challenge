1af54a6f671139878a4e8fa82f075f6ebbf8acac
Joseph <joseph.jr@gmail.com>
2016-08-21 13:07:16 +0100

    added test and methods for landing and taking off planes

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,15 +1,23 @@
+require_relative 'plane'
+
 class Airport
 
 def initialize
+  @weather = true
+  @planes = []
+  @sky = []
 end
 
-def weather
+def stormy
+  @weather = false
 end
 
 def take_off(plane)
+  @sky << plane
 end
 
 def landing(plane)
+  @planes << plane
 end
 
 
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -4,6 +4,23 @@ describe Airport do
 
   it {is_expected.to respond_to(:take_off).with(1).argument}
   it {is_expected.to respond_to(:landing).with(1).argument}
-  it {is_expected.to respond_to :weather}
+  it {is_expected.to respond_to :stormy}
+
+  it 'lands multiple planes' do
+    plane = Plane.new
+    plane1 = Plane.new
+    subject.landing(plane1)
+    expect(subject.landing(plane)).to eq [plane1, plane]
+  end
+
+  it 'take off a plane' do
+    plane = Plane.new
+    expect(subject.take_off(plane)).to eq [plane]
+  end
+
+  it 'the airport becomes stormy' do
+    subject.stormy
+    expect(subject.instance_variable_get(:@weather)).to eq false
+  end
 
 end
