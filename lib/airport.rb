a230717f84f2dcf8153af79658f4e40d0d72cb66
Thao Vo <ttthao.vo@gmail.com>
2016-08-21 23:17:00 +0100

    Implementation of airport and unit tests

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,15 +1,43 @@
+require_relative "plane"
+
 class Airport
 
-  def initialize(weather, capacity)
-    @weather = weather
+  def initialize(capacity = 50)
+    @weather = "sunny"
     @capacity = capacity
+    @planes = []
   end
 
-  def weather
-    @weather
+  def set_weather(weather)
+    @weather = weather
   end
 
-  def capacity
-    @capacity
+  def prevent_take_off
+    fail 'Take-off not permitted due to stormy weather. Await for further instructions!' if stormy?
+    fail 'Error! No plane to take-off!' if empty?
+    plane = planes.pop
+    plane.take_off
+  end
+
+  def prevent_landing(plane)
+    fail 'Landing not permitted due to stormy weather. Await for further instructions!' if stormy?
+    fail 'Landing not permitted - airport full. Await for further instructions!' if full?
+    planes << plane
+    plane.land
+  end
+
+private
+
+attr_reader :planes
+  def empty?
+    @planes.empty?
+  end
+
+  def full?
+    @planes.length >= @capacity
+  end
+
+  def stormy?
+    @weather == "stormy"
   end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,15 +1,33 @@
 require 'airport'
+require 'plane'
 
 describe Airport do
   before do
-    @airport = Airport.new("good weather", 5)
+    subject() { described_class.new }
+    subject.set_weather("stormy")
   end
 
-  it "returns weather" do
-    expect(@airport.weather).to eq("good weather")
+  describe "#prevent_take_off" do
+    it "raises an error when the weather is bad to take off" do
+      expect { subject.prevent_take_off }.to raise_error "Take-off not permitted due to stormy weather. Await for further instructions!"
+    end
+
+    it "raises an error when there is no plane at the airport to take off" do
+      subject.set_weather("sunny")
+      expect { subject.prevent_take_off }.to raise_error "Error! No plane to take-off!"
+    end
   end
 
-  it "returns capacity" do
-    expect(@airport.capacity).to eq(5)
-  end
+  describe "#prevent_landing" do
+    it "raises an error when the weather is bad for landing" do
+      expect { subject.prevent_landing(Plane.new) }.to raise_error "Landing not permitted due to stormy weather. Await for further instructions!"
+    end
+
+    it "raises an error when the airport is full and no further plane can land" do
+      subject = Airport.new(0)
+      expect { subject.prevent_landing(Plane.new) }.to raise_error "Landing not permitted - airport full. Await for further instructions!"
+    end
+end
+
+
 end
