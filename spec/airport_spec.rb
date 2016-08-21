8fd03febb151aa4895ff2629ece0a7c11cf1c1e8
Abigail <abigail.mcphillips@gmail.com>
2016-08-21 11:13:27 +0100

    spec files use named subject with described_class

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,35 +2,36 @@ require 'airport'
 
 describe Airport do
 
-  let(:plane) {double :plane}
-  let(:plane2) {double :plane2}
+  subject(:airport) { described_class.new }
+  let(:plane) { double :plane }
+  let(:plane2) { double :plane2 }
 
   describe '#land' do
 
     before(:each) do
       allow(plane).to receive(:ground)
       allow(plane).to receive(:in_flight).and_return true
-      allow(subject).to receive(:bad_weather?).and_return false
+      allow(airport).to receive(:bad_weather?).and_return false
     end
 
     it 'lands plane' do
-      subject.land(plane)
-      expect(subject.planes).to eq [plane]
+      airport.land(plane)
+      expect(airport.planes).to eq [plane]
     end
 
     it 'prevents landing when weather is stormy' do
-      allow(subject).to receive(:bad_weather?).and_return true
-      expect{subject.land(plane)}.to raise_error "Ach no, there's a storm a-brewin'!"
+      allow(airport).to receive(:bad_weather?).and_return true
+      expect{airport.land(plane)}.to raise_error "Ach no, there's a storm a-brewin'!"
     end
 
     it 'prevents landing when airport is full' do
-      Airport::DEFAULT_CAPACITY.times {subject.land(plane)}
-      expect{subject.land(plane)}.to raise_error "Sorry, we've got more planes than you can shake a stick at."
+      Airport::DEFAULT_CAPACITY.times {airport.land(plane)}
+      expect{airport.land(plane)}.to raise_error "Sorry, we've got more planes than you can shake a stick at."
     end
 
     it 'prevents landing if plane is grounded' do
       allow(plane).to receive(:in_flight).and_return false
-      expect{subject.land(plane)}.to raise_error "Plane's aleady down, Sir."
+      expect{airport.land(plane)}.to raise_error "Plane's aleady down, Sir."
     end
 
   end
@@ -38,7 +39,7 @@ describe Airport do
   describe '#take_off' do
 
     before (:each) do
-      allow(subject).to receive(:bad_weather?).and_return false
+      allow(airport).to receive(:bad_weather?).and_return false
     end
 
     it 'allows planes to take off and leave airport' do
@@ -46,23 +47,23 @@ describe Airport do
       allow(plane).to receive(:in_flight)
       plane2 = double(:plane2, :ground => false)
       allow(plane2).to receive(:in_flight)
-      subject.land(plane)
-      subject.land(plane2)
-      subject.take_off(plane)
-      expect(subject.planes).to eq [plane2]
+      airport.land(plane)
+      airport.land(plane2)
+      airport.take_off(plane)
+      expect(airport.planes).to eq [plane2]
     end
 
     it 'it prevents take off when weather is stormy' do
       plane = double(:plane, :ground => false, :fly => true)
       allow(plane).to receive(:in_flight)
-      subject.land(plane)
-      allow(subject).to receive(:bad_weather?).and_return true
-      expect{subject.take_off(plane)}.to raise_error "Ach no, there's a storm a-brewin'!"
+      airport.land(plane)
+      allow(airport).to receive(:bad_weather?).and_return true
+      expect{airport.take_off(plane)}.to raise_error "Ach no, there's a storm a-brewin'!"
     end
 
     it 'prevents take off if plane is flying' do
       allow(plane).to receive(:in_flight).and_return true
-      expect{subject.take_off(plane)}.to raise_error "Plane's already up there, Sir."
+      expect{airport.take_off(plane)}.to raise_error "Plane's already up there, Sir."
     end
 
     it 'only allows plane to take off from airport they are in' do
@@ -71,13 +72,13 @@ describe Airport do
       allow(airport2).to receive(:bad_weather?).and_return false
       airport2.land(plane)
       plane = double(:plane, :fly => false, :in_flight => false)
-      expect{subject.take_off(plane)}.to raise_error "Plane's in another airport, Sir."
+      expect{airport.take_off(plane)}.to raise_error "Plane's in another airport, Sir."
     end
 
   end
 
   it 'sets airport capacity' do
-    allow(subject).to receive(:bad_weather?).and_return false
+    allow(airport).to receive(:bad_weather?).and_return false
     airport = Airport.new(2)
     expect(airport.capacity).to eq 2
   end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -2,18 +2,20 @@ require 'plane'
 
 describe Plane do
 
+  subject(:plane) { described_class.new }
+
   it {is_expected.to respond_to(:ground)}
   it {is_expected.to respond_to(:fly)}
   it {is_expected.to respond_to(:in_flight)}
 
   it "is not in flight if already landed" do
-    subject.ground
-    expect(subject.in_flight).to eq(false)
+    plane.ground
+    expect(plane.in_flight).to eq(false)
   end
 
   it "is in flight if taken off" do
-    subject.fly
-    expect(subject.in_flight).to eq(true)
+    plane.fly
+    expect(plane.in_flight).to eq(true)
   end
 
 end
