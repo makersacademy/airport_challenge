29f1a9a642cfc1774e9b07976d060c1fb3bfef58
Rosie Allott <rosie.allott@gmail.com>
2016-08-21 16:01:15 +0100

    Add tests for taking off

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,6 +1,13 @@
 require './lib/plane'
 
 class Airport
+  attr_reader :planes
+
+  def initialize
+    @planes = []
+  end
+
   def land(plane)
+    @planes << plane
   end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -4,11 +4,24 @@ describe Airport do
   subject(:airport) { described_class.new }
   let(:plane) { double :plane }
 
-  it {expect(airport).to respond_to(:land).with(1).argument}
-
-  it 'confirms the plane is at that airport once landed' do
-    airport.land(plane)
-    expect(airport.planes).to include plane
+  #instructs the plane to land and confirm it has landed
+  before do
+    allow(plane).to receive(:land)
   end
+    it { expect(airport).to respond_to(:land).with(1).argument }
+    it 'confirms the plane is at that airport once landed' do
+      airport.land(plane)
+      expect(airport.planes).to include plane
+    end
+
+  #instructs the plane to take off and confirm no longer at airport
+  before do
+    allow(plane).to receive(:take_off)
+  end
+    it { expect(airport).to respond_to(:take_off).with(1).argument }
+    it 'confirms the plane is not at that airport once taken off' do
+      airport.take_off(plane)
+      expect(airport.planes).to_not include plane
+    end
 
 end
