fc91e256c4e03c22e09e8772d9ffb59d9caf85ff
Ewan Sheldon <ewansheldon@gmail.com>
2016-08-21 13:00:48 +0100

    airport lands planes, confirms landing and stores

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,7 +1,14 @@
 class Airport
 
+  attr_reader :landed_planes
+
+  def initialize
+    @landed_planes = []
+  end
+
   def land(plane)
-    plane
+    landed_planes << plane
+    "#{plane} has landed"
   end
 
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -3,10 +3,16 @@ require 'airport'
 describe Airport do
 
   describe 'land(plane)' do
-    it 'lands a plane' do
+    it 'confirms a plane has landed' do
       plane = double(:plane)
-      expect(subject.land(plane)).to eq plane
+      expect(subject.land(plane)).to eq "#{plane} has landed"
     end
+  end
+
+  it 'stores a plane at airport' do
+    plane = double(:plane)
+    subject.land(plane)
+    expect(subject.landed_planes).to include plane
   end
 
 end
