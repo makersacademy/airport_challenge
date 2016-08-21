3c915a0cd56f469089e6ae1a45fc7e28babdc16f
matt vickers <matt.vickers@sky.com>
2016-08-21 16:35:28 +0100

    added Plane methods to Airport and confirmation messages

--- lib/airport.rb
+++ lib/airport.rb
@@ -9,11 +9,23 @@ class Airport
   end
 
   def land(plane)
+    plane.landed
+    confirm_landing
     hangar << plane
   end
 
   def take_off(plane)
+    plane.flying
+    confirm_take_off
     hangar.delete(plane)
+  end
+
+  def confirm_take_off
+    puts "Take-off successful"
+  end
+
+  def confirm_landing
+    puts "Landing successful"
   end
 
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,20 +2,27 @@ require 'airport'
 
 describe Airport do
 
-  let(:plane) {double :plane}
+  subject(:airport) { described_class.new }
+  let(:plane) { double :plane }
 
   it { is_expected.to respond_to(:land).with(1).argument }
   it { is_expected.to respond_to(:take_off).with(1).argument }
 
-  it "instructs plane to land" do
+  it 'instructs plane to land' do
+    plane = double(:plane, :landed => false)
     expect(subject.land(plane)).to eq [plane]
-
   end
 
-  it "instructs plane to take off" do
+  it 'instructs plane to take off' do
+    plane = double(:plane, :flying => false, :landed => true)
     subject.land(plane)
     expect(subject.take_off(plane)).to eq plane
+  end
 
+  it 'docks plane in hangar' do
+    plane = double(:plane, :flying => false, :landed => true)
+    subject.land(plane)
+    expect(subject.hangar).to include plane
   end
 
 end
