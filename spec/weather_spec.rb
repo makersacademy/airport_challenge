fb198a993cb03428f825b77c58786aa951b5d504
Matthew Perkins <mfperkins@gmail.com>
2016-08-21 20:50:09 +0100

    refactoring for CI tests

--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -2,8 +2,6 @@ require 'weather'
 
 describe Weather do
 
-  let(:random_weather) { double :random_weather}
-
   describe '#initialize' do
     it 'should set "sunny" to true' do
       expect(subject.sunny).to eq true
@@ -26,13 +24,5 @@ describe Weather do
       expect(subject.stormy?).to eq true
     end
   end
-
-  describe '#random_weather' do
-    it 'should set weather to stormy randomly less than 10% of the time' do
-      10.times { subject.random_weather }
-      expect(subject.stormy).to eq true
-    end
-  end
-
 
 end
