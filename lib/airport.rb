f572f51c0c38feab14c9a46adacc7dbb13cbb39b
matt vickers <matt.vickers@sky.com>
2016-08-21 12:57:00 +0100

    Plane class created and Airport lands planes

---
+++ lib/airport.rb
@@ -0,0 +1,9 @@
+require_relative 'plane'
+
+class Airport
+
+  def land(plane)
+    plane
+  end
+
+end
---
+++ spec/airport_spec.rb
@@ -0,0 +1,14 @@
+require 'airport'
+
+describe Airport do
+
+  it { is_expected.to respond_to(:land).with(1).argument }
+
+it "lands plane" do
+
+plane = Plane.new
+expect(subject.land(plane)).to eq plane
+
+end
+
+end
