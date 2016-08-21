01f0d9a1c89e31ba7a6149eb34805270780769ea
mrmurtz <murtaza.s.abidi@gmail.com>
2016-08-22 00:06:06 +0100

    Updated weather to randomly create stormy true or false when airport instantiated

---
+++ spec/weather_spec.rb
@@ -0,0 +1,14 @@
+require 'weather'
+
+describe Weather do
+
+  # it "is sunny" do
+  #   expect(subject.weather).to include "sunny"
+  # end
+  # #
+  # it "creates random weather when instantiated" do
+  #   expect(subject.weather).to eq("sunny" || "stormy")
+  # end
+
+
+end
