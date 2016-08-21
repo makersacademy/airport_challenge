89d78a6e50bff33faa825d1670bc043bb29a4052
lsewilson <laurawilson@hotmail.co.uk>
2016-08-21 23:39:38 +0100

    refactors airport code and spec

--- lib/airport.rb
+++ lib/airport.rb
@@ -3,7 +3,7 @@ require_relative './weather.rb'
 
 class Airport
 
-  attr_reader :planes , :weather
+  attr_reader :weather
 
   DEFAULT_CAPACITY = 20
 
@@ -14,7 +14,7 @@ class Airport
   end
 
   def land(plane)
-    fail "Planes cannot land in stormy weather" if @weather.stormy?
+    fail "Planes cannot land in stormy weather" if bad_weather?
     fail "Planes cannot land when airport is full" if full?
     fail "This plane is already at an airport" if plane.grounded == true
     plane.landed
@@ -22,16 +22,23 @@ class Airport
   end
 
   def take_off(plane)
-    fail "Planes cannot take off in stormy weather" if @weather.stormy?
+    fail "Planes cannot take off in stormy weather" if bad_weather?
     fail "This plane is already flying" if plane.grounded == false
     fail "This plane is not at this airport" unless @planes.include? plane
     plane.taken_off
     @planes.delete(plane)
   end
 
+  private
 
-  def full?
-    planes.count >= @capacity
-  end
+  attr_reader :planes ,  :weather
+
+    def full?
+      planes.count >= @capacity
+    end
+
+    def bad_weather?
+      @weather.stormy?
+    end
 
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -6,66 +6,73 @@ subject(:airport) { described_class.new }
 subject(:airport_1) { described_class.new }
 let(:plane) { double :plane }
 
+before(:each) do
+  allow(airport).to receive(:bad_weather?).and_return false
+  allow(airport_1).to receive(:bad_weather?).and_return false
+end
+
   describe '#land' do
 
     it 'lands a plane at the airport in good weather' do
-      good_weather
-      plane_in_flight
+      allow(plane).to receive(:grounded).and_return(false)
+      allow(plane).to receive(:landed)
       expect(airport.land(plane)).to eq [plane]
     end
 
+    it 'prevents landing when the airport is full' do
+      allow(plane).to receive(:grounded).and_return(false)
+      allow(plane).to receive(:landed)
+      Airport::DEFAULT_CAPACITY.times { airport.land(plane) }
+      expect{airport.land(plane)}.to raise_error "Planes cannot land when airport is full"
+    end
+
     it 'raises error if landing attempted in stormy weather' do
-      bad_weather
+      allow(airport).to receive(:bad_weather?).and_return true
       msg = "Planes cannot land in stormy weather"
       expect{airport.land(plane)}.to raise_error msg
     end
 
     it 'should not allow a grounded plane to land again' do
-      good_weather
-      plane_landed
+      allow(plane).to receive(:grounded).and_return true
+      allow(plane).to receive(:taken_off)
       msg =  "This plane is already at an airport"
       expect{airport.land(plane)}.to raise_error msg
-    end
-
-    it 'prevents landing when the airport is full' do
-      good_weather
-      plane_in_flight
-      Airport::DEFAULT_CAPACITY.times { airport.land(plane) }
-      expect{airport.land(plane)}.to raise_error "Planes cannot land when airport is full"
     end
 
   end
 
   describe '#take_off' do
 
+    before(:each) do
+      allow(plane).to receive(:grounded).and_return(false)
+      allow(plane).to receive(:landed).and_return(true)
+    end
+
     it 'allows a plane to take off from the airport in good weather' do
-      good_weather
-      plane_in_flight
       airport.land(plane)
-      plane_landed
+      allow(plane).to receive(:grounded).and_return true
+      allow(plane).to receive(:taken_off).and_return false
       airport.take_off(plane)
-      expect(airport.planes).to eq []
+      expect(airport.instance_variable_get(:@planes)).to eq []
     end
 
     it 'raises error if takeoff attempted in stormy weather' do
-      bad_weather
+      allow(airport).to receive(:bad_weather?).and_return true
       msg = "Planes cannot take off in stormy weather"
       expect{airport.take_off(plane)}.to raise_error
     end
 
     it 'should not allow a flying plane to take off again' do
-      good_weather
-      plane_in_flight
+      allow(plane).to receive(:grounded).and_return(false)
+      allow(plane).to receive(:landed).and_return(false)
       msg =  "This plane is already flying"
       expect{airport.take_off(plane)}.to raise_error msg
     end
 
     it 'should not allow a plane to take off unless it is at that airport' do
-      good_weather
-      plane_in_flight
       airport.land(plane)
-      allow(airport_1.weather).to receive(:stormy?).and_return(false)
-      plane_landed
+      allow(plane).to receive(:grounded).and_return true
+      allow(plane).to receive(:taken_off)
       msg = "This plane is not at this airport"
       expect{airport_1.take_off(plane)}.to raise_error msg
     end
@@ -74,42 +81,23 @@ let(:plane) { double :plane }
 
   describe '#planes' do
 
-    it 'has registered landed plane at airport' do
-      good_weather
-      plane_in_flight
+    before(:each) do
+      allow(plane).to receive(:grounded).and_return(false)
+      allow(plane).to receive(:landed)
       airport.land(plane)
-      expect(airport.planes).to eq [plane]
+    end
+
+    it 'has registered landed plane at airport' do
+      expect(airport.instance_variable_get(:@planes)).to eq [plane]
     end
 
     it 'confirms that plane is no longer at airport after takeoff' do
-      good_weather
-      plane_in_flight
-      airport.land(plane)
-      plane_landed
+      allow(plane).to receive(:grounded).and_return true
+      allow(plane).to receive(:taken_off)
       airport.take_off(plane)
-      expect(airport.planes).not_to include plane
+      expect(airport.instance_variable_get(:@planes)).not_to include plane
     end
 
   end
 
 end
-
-private
-
-  def bad_weather
-    allow(airport.weather).to receive(:stormy?).and_return(true)
-  end
-
-  def good_weather
-    allow(airport.weather).to receive(:stormy?).and_return(false)
-  end
-
-  def plane_in_flight
-    allow(plane).to receive(:grounded).and_return(false)
-    allow(plane).to receive(:landed)
-  end
-
-  def plane_landed
-    allow(plane).to receive(:grounded).and_return true
-    allow(plane).to receive(:taken_off)
-  end
