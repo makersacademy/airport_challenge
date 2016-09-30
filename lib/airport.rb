ee6d085c88294964caf48f040c583c8342e68194
Darrenf <darrenf54321@gmail.com>
2016-09-30 23:52:10 +0100

    files and folders setup

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,2 +1,8 @@
 class Airport
+
+  def land_plane(plane)
+
+  end
+
+
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,5 +1,15 @@
+
+require 'spec_helper'
 require 'airport'
+require 'plane'
 
 describe Airport do
+
   it { is_expected.to respond_to :land_plane}
+
+  describe "instructs a plane to land" do
+   plane = Plane.new
+   expect(subject.land(plane)).to eq plane
+  end
+
 end
