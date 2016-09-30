49813cfa825a1dd685abdab145b72ee70d829242
antonyCastineiras <antonycastineiras80@gmail.com>
2016-10-01 00:49:03 +0100

    added tests for land and take off methods and made them pass

--- lib/plane.rb
+++ lib/plane.rb
@@ -1,10 +1,25 @@
 class Plane
-	
+	attr_accessor :current_airport
+
+	def initialize
+		@current_airport
+	end
+
 	def land(airport)
-		airport.landed_planes << self	
+		if airport.weather == "stormy"
+			"Stormy weather. Unable to land."
+		else
+			@current_airport = airport
+			airport.landed_planes << self
+		end	
 	end
 
 	def take_off
-
+		if @current_airport.weather == "stormy"
+			"Stormy weather. Unable to take off."
+		else
+			@current_airport.landed_planes.delete(self)
+			@current_airport = nil
+		end
 	end
 end
