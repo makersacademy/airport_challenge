faa6a27115fa82448b9b5ef4b616519b945935a7
david anderson <d.a.m.anderson@gmail.com>
2016-10-01 21:01:48 +0100

    Working model, limited tests, not refactored'
    q

--- lib/airport.rb
+++ lib/airport.rb
@@ -3,11 +3,39 @@ require_relative 'plane.rb'
 class Airport
 
   attr_reader :weather
+  attr_accessor :capacity
 
   CHANCE_OF_SUN = 0.9
+  DEFAULT_CAPACITY = 200
 
-  def initialize
+  def initialize(capacity = DEFAULT_CAPACITY)
     @weather = 'sunny'
+    @capacity = capacity
+    @planes = []
+  end
+
+  def land(plane)
+    check_weather
+    fail "Plane not in flight" if plane.on_ground
+    fail "Can't land when stormy" if @weather == 'stormy'
+    fail "Airport full" if @planes.count >= @capacity
+    plane.on_ground = true
+    @planes << plane
+  end
+
+  def takeoff(plane)
+    check_weather
+    fail "Plane already in air" if plane.on_ground == false
+    fail "Can't takeoff when stormy" if @weather == 'stormy'
+    fail "Plane not in airport" if @planes.include?(plane) == false
+    plane.on_ground = false
+    @planes.delete(plane)
+  end
+
+  def add_new_plane(plane)
+    fail "Plane already in airport" if @planes.include?(plane)
+    fail "Plane already in air" if plane.on_ground == false
+    @planes << plane
   end
 
   def check_weather
@@ -19,16 +47,12 @@ class Airport
     @weather
   end
 
-  def land(plane)
-    check_weather
-    fail "Can't land when stormy" if @weather == 'stormy'
-    plane.on_ground = true
+  def check_planes
+    puts @planes
   end
 
-  def takeoff(plane)
-    check_weather
-    fail "Can't takeoff when stormy" if @weather == 'stormy'
-    plane.on_ground = false
+  def find_plane(plane)
+    fail "Plane not in airport" if @planes.include?(plane) == false
+    puts "Plane in airport"
   end
-
 end
