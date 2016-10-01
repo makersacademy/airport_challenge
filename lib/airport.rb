68bf87d816029d65af6a1c7d879ef6f7d62f48a4
antonyCastineiras <antonycastineiras80@gmail.com>
2016-10-01 12:58:49 +0100

    added a DEFAULT_CAPACITY constant for the airport class and wrote a test for it

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,3 +1,3 @@
 class Airport
-	
+	DEFAULT_CAPACITY = 20
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,4 +2,8 @@ require 'airport.rb'
 
 describe Airport do
  	
+	it 'has a default capacity' do 
+		expect(Airport::DEFAULT_CAPACITY).to be > 0
+	end
+
 end
