71901ea2d70674d24d8d65eefd2ad3875b4d4f92
elizabethvenner <elizabethvenner@googlemail.com>
2016-10-01 17:31:21 +0100

    test and write land method

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -5,7 +5,14 @@ describe Airport do
   it {is_expected.to respond_to(:land).with(1).argument}
 
   it 'instructs a plane to land at an airport' do
-    expect(subject.land).to eq @plane
+    plane1 = Plane.new
+    expect(subject.land(plane1)).to eq plane1
+  end
+
+  it 'confirms that the plane has landed' do
+    plane1 = Plane.new
+    subject.land(plane1)
+    expect(subject.plane).to eq plane1
   end
 
 end
