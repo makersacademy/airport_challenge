2cce7d0de4cbaafabfdafee0ffa06933a534de3f
Rosie <r.gollancz@gmail.com>
2016-10-01 13:05:19 +0100

    Adds first planes methods and tests

--- lib/plane.rb
+++ lib/plane.rb
@@ -1,5 +1,14 @@
 require_relative 'airport'
 
 class Plane
+  attr_reader :landed
+
+  def initialize
+    @landed = true
+  end
+
+  def land
+    @landed = true
+  end
 
 end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -3,4 +3,11 @@ require './lib/airport'
 
 describe Plane do
 
+  let(:airport) { double() }
+
+    it 'can land', focus: :true do
+      subject.land
+      expect(subject.landed).to eq true
+    end
+
 end
