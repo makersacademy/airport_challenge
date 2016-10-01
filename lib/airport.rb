61a35dd126ce485761893129576873019a0cf11b
Darrenf <darrenf54321@gmail.com>
2016-10-01 18:56:59 +0100

    added loaded random method so as random weather can be loaded

--- lib/airport.rb
+++ lib/airport.rb
@@ -10,7 +10,8 @@ class Airport
   end
 
   def random_weather
-    ["stormy", "sunny"].sample
+    weather = { "stormy" => 20, "sunny" => 80 }
+    choose_weighted(weather)
   end
 
   def land_plane(plane)
@@ -44,4 +45,15 @@ class Airport
       "plane cannot take off, it's stormy"
     end
   end
+
+  def choose_weighted(weighted)
+    sum = weighted.inject(0) do |sum, item_and_weight|
+    sum += item_and_weight[1]
+  end
+    target = rand(sum)
+    weighted.each do |item, weight|
+    return item if target <= weight
+    target -= weight
+  end
+end
 end
