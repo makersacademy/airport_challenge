6033423c4e34d0c09af6da99f85ed98bd955d597
Julio Gonzalez <julio.gteran@gmail.com>
2016-10-01 13:42:33 +0100

    Planes take-off

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,8 +1,21 @@
 require_relative 'plane'
 
 class Airport
+attr_reader :landed
+
+  def initialize
+    @landed = []
+  end
 
   def land(plane)
-    plane
+    @landed << plane
+    "The plane has already landed"
   end
+
+  def take_off
+    @landed.pop
+    "The plane has taken off and it is no longer in the airport"
+  end
+
+
 end
--- spec/ariport_spec.rb
+++ spec/ariport_spec.rb
@@ -2,9 +2,35 @@ require 'airport'
 require 'plane'
 
 describe Airport do
-  subject(:airport) {described_class.new}
+    subject(:airport) {described_class.new}
+
+    before(:each) do
+      @landed = []
+      plane = Plane.new
+    end
 
   describe "#land" do
     it {is_expected.to respond_to(:land).with(1).arguments}
+
+    it "allows the plane to land" do
+      plane = Plane.new
+      airport.land(plane)
+      expect(@landed).to eq @landed << plane
+    end
+
+    it "confirms that the plane has landed" do
+      plane = Plane.new
+      airport.land(plane)
+      expect(airport.land(plane)).to eq "The plane has already landed"
+    end
+  end
+
+  describe "#take off" do
+    it {is_expected.to respond_to(:take_off)}
+
+    it "confirms that the plane has taken off and it is no longer in the airport" do
+      airport.take_off
+      expect(airport.take_off).to eq "The plane has taken off and it is no longer in the airport"
+    end
   end
 end
