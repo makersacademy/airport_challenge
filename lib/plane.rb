d8cc37728a174bfba18f0257316d34b9242b1cbd
Chet Sanghani <crsanghani@gmail.com>
2016-10-01 10:16:35 +0000

    starting to feature test edge cases for launchs and landings

--- lib/plane.rb
+++ lib/plane.rb
@@ -3,7 +3,7 @@ class Plane
     attr_accessor :flying
     
     def initialize
-        @flying = true
+        flying = true
     end
    
     def flying?
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,23 +2,33 @@ require 'spec_helper'
 
 describe Airport do
     
+    
     let(:flying_plane) {double :plane, flying?: true}
     let(:landed_plane) {double :plane, flying?: false}
     
     it 'lets a flying plane land' do
         plane = Plane.new
+        subject.launch(plane)
         subject.land(plane)
         expect(plane).not_to be_flying
     end
 
     it "allows a grounded plane to takeoff" do
         plane = Plane.new
-        plane.flying = false
         subject.launch(plane)
         expect(plane).to be_flying
+    end
+
+    it "won't allow a flying plane to take off" do
+        expect { subject.launch(flying_plane) }.to raise_error "Cant take off. Plane is flying."
     end
     
     it "has a default capacity" do
         expect(subject.capacity).to eq described_class::DEFAULT_CAPACITY
+    end
+    
+    it "has a variable capacity" do
+        subject.capacity = 10 
+        expect(subject.capacity).to eq 10
     end
 end
