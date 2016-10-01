8b72cccd97090b5c165ff907557f8d60ded88047
Katy McCann <katy.alice.mccann@gmail.com>
2016-10-01 23:41:07 +0100

    air_traffic_controll repons to the new weather class

--- spec/air_traffic_controller_spec.rb
+++ spec/air_traffic_controller_spec.rb
@@ -3,12 +3,22 @@ require 'air_traffic_controller'
 
 describe AirTrafficController do 
 
-	it 'recognises when weather is stormy' do
-		@air_traffic_controller = AirTrafficController.new
+before :each do 
+  @weather = Weather.new
+  @air_traffic_controller = AirTrafficController.new
+end 
 
+	it 'recognises when weather is stormy' do
+	
 		expect(@air_traffic_controller).to respond_to :weather
 	end 
+=begin
+		it 'prevents landing when the weather is stormy' do
 
+			expect(air_traffic_controller.stop_landing).to eq 
+
+		end  
+=end
 end 
 
 
