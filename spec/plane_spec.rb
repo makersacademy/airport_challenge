31eac3ba95770d80a727c0ae808bbe2a8b6d1fa2
Amaal Ali <amq.ali@gmail.com>
2016-10-02 00:37:08 +0100

    Fixed error with consistency in method naming

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -1,21 +1,19 @@
-require 'plane'
+require 'plane_file'
 
-describe plane do
+describe Plane do
 
-  describe '#landing_status?' do
+  describe '#plane_is_landed?' do
 
     it {is_expected.to respond_to (:plane_is_landed?)}
 
     it 'returns the landing status of landed plane as "true" ' do
       plane = instance_double("plane", :plane_is_landed? => true)
-      expect(plane.plane_is_landed).to eq true
+      expect(plane.plane_is_landed?).to eq true
     end
 
     it 'returns the landing status of non-landed plane as "false" ' do
       plane = instance_double("plane", :plane_is_landed? => false)
-      expect(plane.plane_is_landed).to eq false
+      expect(plane.plane_is_landed?).to eq false
     end
-
   end
-
 end
