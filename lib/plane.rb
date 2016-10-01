c4867d29962b1ee7dd8aa60a714113bca34fc591
Lawrence Hunt <lawrence.hunt@hotmail.com>
2016-10-01 14:04:49 +0100

    Planes do not land while stormy.

--- lib/plane.rb
+++ lib/plane.rb
@@ -3,34 +3,39 @@ require_relative 'airport'
 
 class Plane
 
-attr_accessor :landed, :stormy
+  attr_accessor :landed, :stormy
 
-def initialize
-  @landed = true
-  @stormy = false
-end
-
-def chance
-rand(10)
-end
-
-def weather
-  @stormy = true  if chance >= 7
-end
-
-def take_off
-  if @stormy == true
-  @landed = true
-  'Traffic control we are grounded. It is STORMY!'
-else @landed = false
-  'Traffic control this is niner-niner-three. We have taken off!'
+  def initialize
+    @landed = true
+    @stormy = false
   end
-end
 
-def land(airport)
-  @landed = true
-  'Traffic control this is niner-niner-three. We have landed!'
-end
+  def chance
+    rand(10)
+  end
+
+  def weather
+    @stormy = true  if chance >= 7
+  end
+
+  def take_off
+    if @stormy == true
+       @landed = true
+       'Traffic control we are grounded. It is STORMY!'
+    else @landed = false
+        'Traffic control this is niner-niner-three. We have taken off!'
+    end
+  end
+
+  def land(airport)
+    if @stormy == true
+      @landed = false
+      'Traffic control WTF do we do it is MAJORLY STORMY'
+    else
+    @landed = true
+    'Traffic control this is niner-niner-three. We have landed!'
+    end
+  end
 
 
 end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -18,16 +18,17 @@ let (:airport2) {Airport.new}
     expect(subject.stormy).to eq true
   end
 
-  it 'takes off if weather clear' do
+  it 'takes off and announces itself if weather clear' do
     subject.stormy = false
     expect(subject.take_off).to eq 'Traffic control this is niner-niner-three. We have taken off!'
     expect(subject.landed).to eq false
   end
 
-  it 'announces itself when taking off' do
-    expect(subject.take_off).to eq 'Traffic control this is niner-niner-three. We have taken off!'
+  it 'cannot land while stormy' do
+    subject.stormy = true
+    expect(subject.land(airport1)).to eq 'Traffic control WTF do we do it is MAJORLY STORMY'
+    expect(subject.landed).to eq false
   end
-
 
 
 end
