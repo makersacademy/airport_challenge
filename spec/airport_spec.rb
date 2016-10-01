d41962a8bc194bab4993b55712b794752908dfb4
Chet Sanghani <crsanghani@gmail.com>
2016-10-01 10:59:10 +0000

    worked out how to get resolve an rspec error based around stubs

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe Airport do
-    
+    attr_accessor :flying    
     
     let(:flying_plane) {double :plane, flying?: true}
     let(:landed_plane) {double :plane, flying?: false}
@@ -12,15 +12,20 @@ describe Airport do
         expect(plane).not_to be_flying
     end
 
-    it "allows a grounded plane to takeoff" do
+    it "let a grounded plane launch" do
         plane = Plane.new
         subject.land(plane)
         subject.launch(plane)
         expect(plane).to be_flying
     end
 
-    it "won't allow a flying plane to take off" do
+    it "wont let a flying plane launch" do
         expect { subject.launch(flying_plane) }.to raise_error "Cant take off. Plane is flying."
+    end
+    
+    it "wont let a grounded plane land" do
+        allow(landed_plane).to receive(:flying).with(no_args)
+        expect { subject.land(landed_plane) }.to raise_error "Cant land, the plane isnt flying"
     end
     
     it "has a default capacity" do
