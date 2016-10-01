ded33a886c5776718440b1cb310f740c19d3ea67
naomi <naomi.emma.spencer@gmail.com>
2016-10-01 21:07:13 +0100

    test and module for plane taking off

--- lib/airport.rb
+++ lib/airport.rb
@@ -4,4 +4,8 @@ class Airport
   def land(plane)
     @plane = plane
   end
+
+  def take_off(plane)
+    @plane = plane
+  end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,8 +2,15 @@ require 'airport'
 
 describe Airport do
     it { is_expected.to respond_to(:land) }
+    it { is_expected.to respond_to(:take_off) }
+
     it 'lands the plane' do
       plane = Plane.new
       expect(subject.land(plane)).to eq(plane)
+    end
+
+    it 'allows plane to take off' do
+      plane = Plane.new
+      expect(subject.take_off(plane)).to eq(plane)
     end
 end
