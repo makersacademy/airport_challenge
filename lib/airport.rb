a3f5d3d5a3e4b9ed1ea0634e277f4476f8c0b57d
david anderson <d.a.m.anderson@gmail.com>
2016-10-01 20:02:26 +0100

    Weather added, planes wont land or takeoff when stormy

--- lib/airport.rb
+++ lib/airport.rb
@@ -2,8 +2,33 @@ require_relative 'plane.rb'
 
 class Airport
 
+  attr_reader :weather
+
+  CHANCE_OF_SUN = 0.9
+
+  def initialize
+    @weather = 'sunny'
+  end
+
+  def check_weather
+    if rand < Airport::CHANCE_OF_SUN
+      @weather = 'sunny'
+    else
+      @weather = 'stormy'
+    end
+    @weather
+  end
+
   def land(plane)
+    check_weather
+    fail "Can't land when stormy" if @weather == 'stormy'
     plane.on_ground = true
+  end
+
+  def takeoff(plane)
+    check_weather
+    fail "Can't takeoff when stormy" if @weather == 'stormy'
+    plane.on_ground = false
   end
 
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,6 +2,26 @@ require 'airport'
 
 describe Airport do
 
-  it { is_expected.to respond_to(:land).with(1).argument }
+  describe 'land' do
+
+    it { is_expected.to respond_to(:land).with(1).argument }
+
+    it 'raises an error when weather is stormy' do
+      #to fix: will currently only work when whether is randomly stormy
+      expect { subject.land(Plane.new) }.to raise_error "Can't land when stormy"
+    end
+  end
+
+  describe 'takeoff' do
+
+    it { is_expected.to respond_to(:takeoff).with(1).argument }
+
+    it 'raises an error when weather is stormy' do
+      #to fix: will currently only work when whether is randomly stormy
+      expect { subject.takeoff(Plane.new) }.to raise_error "Can't take off when stormy"
+    end
+  end
+
+
 
 end
