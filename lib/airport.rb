a049250822dcaeba5459a2e40226ce0ab84e37f9
Laszlo Bogacsi <bogacsi.laszlo@gmail.com>
2016-10-01 23:58:35 +0100

    initialize method in airport for planes array, grouped the tests in airport spec

--- lib/airport.rb
+++ lib/airport.rb
@@ -5,12 +5,15 @@ require_relative 'plane'
 class Airport
   attr_reader :planes
 
+  def initialize
+    @planes = []
+  end
+
   def take_off
     @planes.pop
   end
 
   def land(plane)
-    @planes = []
     @planes << plane
   end
 
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -5,21 +5,24 @@ describe Airport do
     @plane = Plane.new
   end
 
-  it 'instructs a plane to land at an airport' do
-    expect(subject.land(@plane)).to eq [@plane]
-  end
+  describe 'landing and take off' do
 
-  it 'confirms that a plane has landed' do
-    subject.land(@plane)
-    expect(subject.planes).to eq [@plane]
-  end
+    it 'instructs a plane to land at an airport' do
+      expect(subject.land(@plane)).to eq [@plane]
+    end
 
-  it 'instructs a plane to take off' do
-    expect(subject).to respond_to(:take_off)
-  end
+    it 'confirms that a plane has landed' do
+      subject.land(@plane)
+      expect(subject.planes).to eq [@plane]
+    end
 
-  it 'confirms that a plane has took off' do
-    subject.land(@plane)
-    expect(subject.take_off).to eq @plane
+    it 'instructs a plane to take off' do
+      expect(subject).to respond_to(:take_off)
+    end
+
+    it 'confirms that a plane has took off' do
+      subject.land(@plane)
+      expect(subject.take_off).to eq @plane
+    end
   end
 end
