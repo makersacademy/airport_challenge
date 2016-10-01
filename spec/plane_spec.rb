4630832c019f127cb2489585154730d0e786cac4
Amaal Ali <amq.ali@gmail.com>
2016-10-02 00:24:25 +0100

    Fixed sytax error, mismatched brackets

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -1,3 +1,5 @@
+require 'plane'
+
 describe plane do
 
   describe '#landing_status?' do
@@ -5,12 +7,12 @@ describe plane do
     it {is_expected.to respond_to (:plane_is_landed?)}
 
     it 'returns the landing status of landed plane as "true" ' do
-      plane = instance_double("plane"), :plane_is_landed? => true)
+      plane = instance_double("plane", :plane_is_landed? => true)
       expect(plane.plane_is_landed).to eq true
     end
 
     it 'returns the landing status of non-landed plane as "false" ' do
-      plane = instance_double("plane"), :plane_is_landed? => false)
+      plane = instance_double("plane", :plane_is_landed? => false)
       expect(plane.plane_is_landed).to eq false
     end
 
