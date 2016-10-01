81b1f5222acdb6d8d5fbe2eb7c82f6a93267a43e
Tadas Majeris <tadas.majeris@gmail.com>
2016-10-01 20:14:30 +0100

    add(plane) method - to add landed planes

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,8 +1,14 @@
 class Airport
-  attr_reader :capacity
+  attr_reader :capacity, :planes
   DEFAULT_CAPACITY = 50
 
   def initialize(capacity = DEFAULT_CAPACITY)
     @capacity = capacity
+    @planes = []
+  end
+
+  def add(plane)
+    fail "Cannot add flying planes to airport" if plane.flying?
+    planes << plane
   end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -7,5 +7,20 @@ describe Airport do
     it "can be set a different capacity" do
       expect(Airport.new(100).capacity).to eq 100
     end
+    it "should have no planes" do
+      expect(subject.planes).to eq []
+    end
+  end
+
+  let(:plane)        { double(:plane, flying?: false)}
+  let(:flying_plane) { double(:plane, flying?: true)}
+
+  describe "#add" do
+    it "should raise an error when adding a flying plane to airport" do
+      expect {subject.add(flying_plane)}.to raise_error "Cannot add flying planes to airport"
+    end
+    it "should add a landed plane to airport" do
+      expect(subject.add(plane)).to eq [plane]
+    end
   end
 end
