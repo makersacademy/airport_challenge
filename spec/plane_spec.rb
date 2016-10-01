cc7306c48ca9e1d2a829b654ac2d696847e39f8d
Glenn <glynester@hotmail.com>
2016-10-01 10:16:51 +0000

    Tests check status of plane after instruction to land or take-off.

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -2,10 +2,24 @@ require 'plane'
 
 describe Plane do
     
-     it { is_expected.to respond_to :land_plane}
+    describe "Plane instructed to land." do
+        
+        it { is_expected.to respond_to :land_plane}
+        
+        it "Plane returns a status of 'down' when it is instructed to land." do
+            expect(subject.land_plane).to eq "down"
+        end    
+    
+    end
+    
+    describe "Plane instructed to take-off." do
 
-     it {is_expected.to respond_to :take_off}
-     
+        it {is_expected.to respond_to :take_off}
+        
+        it "Plane returns a status of 'up' when it is instructed to take-off." do
+            expect(subject.take_off).to eq "up"
+        end
+    end  
      
      
 end
