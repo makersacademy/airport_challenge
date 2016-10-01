bb7a4246725fa5b936c6b1164a6632cacc7b30db
Chet Sanghani <crsanghani@gmail.com>
2016-10-01 11:12:02 +0000

    three user stories commited and aiport_spec.rb restructured to ease modifying tests

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,39 +1,53 @@
 require 'spec_helper'
 
 describe Airport do
-    attr_accessor :flying    
     
     let(:flying_plane) {double :plane, flying?: true}
     let(:landed_plane) {double :plane, flying?: false}
     
-    it 'lets a flying plane land' do
-        plane = Plane.new
-        subject.land(plane)
-        expect(plane).not_to be_flying
+    
+    
+    context "Launch tests" do
+        
+        it "let a grounded plane launch:" do
+            plane = Plane.new
+            subject.land(plane)
+            subject.launch(plane)
+            expect(plane).to be_flying
+        end
+        
+        it "wont let a flying plane launch" do
+            expect { subject.launch(flying_plane) }.to raise_error "Cant take off. Plane is flying."
+        end    
     end
 
-    it "let a grounded plane launch" do
-        plane = Plane.new
-        subject.land(plane)
-        subject.launch(plane)
-        expect(plane).to be_flying
+    context "Landing tests:" do
+        
+        it 'lets a flying plane land' do
+            plane = Plane.new
+            subject.land(plane)
+            expect(plane).not_to be_flying
+        end
+        
+        it "wont let a grounded plane land" do
+            allow(landed_plane).to receive(:flying).with(no_args)
+            expect { subject.land(landed_plane) }.to raise_error "Cant land, the plane isnt flying"
+        end
     end
 
-    it "wont let a flying plane launch" do
-        expect { subject.launch(flying_plane) }.to raise_error "Cant take off. Plane is flying."
+    context "Capacity tests:" do
+        
+        it "has a default capacity" do
+            expect(subject.capacity).to eq described_class::DEFAULT_CAPACITY
+        end
+    
+        it "has a variable capacity" do
+            subject.capacity = 20 
+            expect(subject.capacity).to eq 20
+        end
     end
     
-    it "wont let a grounded plane land" do
-        allow(landed_plane).to receive(:flying).with(no_args)
-        expect { subject.land(landed_plane) }.to raise_error "Cant land, the plane isnt flying"
-    end
+
     
-    it "has a default capacity" do
-        expect(subject.capacity).to eq described_class::DEFAULT_CAPACITY
-    end
-    
-    it "has a variable capacity" do
-        subject.capacity = 10 
-        expect(subject.capacity).to eq 10
-    end
+
 end
