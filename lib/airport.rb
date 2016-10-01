5465624ec28af39b007971ae1b0ebc2006854f0f
Dionysis <Dionysis.k22@gmail.com>
2016-10-01 18:05:26 +0100

    corrected land method from plane to airport file and tested successfully with rspec

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,2 +1,8 @@
+require_relative 'plane.rb'
+
 class Airport
+
+  def land(plane)
+  end
+
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,10 +2,16 @@ require "airport"
 
 describe Airport do
 
-  # 1
+  #1
   it "checks existance of the class Airport" do
     expect(subject.class).to eq Airport
   end
+
+  #2
+  it "plane can land at airport - land method" do
+    expect(subject).to respond_to(:land).with(1).argument
+  end
+
 
 
 end
