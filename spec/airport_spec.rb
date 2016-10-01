b76c957b903111437dca1ee9b5b6cd23351d5515
Tadas Majeris <tadas.majeris@gmail.com>
2016-10-01 17:39:46 +0100

    Can be set a different capacity

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -4,5 +4,8 @@ describe Airport do
     it "defaults capacity" do
       expect(subject.capacity).to eq Airport::DEFAULT_CAPACITY
     end
+    it "can be set a different capacity" do
+      expect(Airport.new(100).capacity).to eq 100
+    end
   end
 end
