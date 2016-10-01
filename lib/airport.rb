761fe191b5c4700a1ad8f4f89e2898fb7dbd01cb
naomi <naomi.emma.spencer@gmail.com>
2016-10-01 20:59:32 +0100

    test and modules for landing a plane

---
+++ lib/airport.rb
@@ -0,0 +1,7 @@
+
+class Airport
+
+  def land(plane)
+    @plane = plane
+  end
+end
---
+++ spec/plane_spec.rb
@@ -0,0 +1,7 @@
+require 'plane'
+
+describe Plane do
+  it 'returns a new plane' do
+    expect(subject).equal? Plane.new
+  end
+end
