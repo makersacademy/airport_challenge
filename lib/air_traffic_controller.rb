d53f1332c951b95079717f7ed3c1f3be6347a513
Katy McCann <katy.alice.mccann@gmail.com>
2016-10-01 22:30:43 +0100

    recognises when weather is storym

--- lib/air_traffic_controller.rb
+++ lib/air_traffic_controller.rb
@@ -1,4 +1,10 @@
 
 class AirTrafficController
+	def initialize (weather = true)
+	    @weather = weather
+	end
 
+	def weather
+	 weather = false
+	end 
 end
--- spec/air_traffic_controller_spec.rb
+++ spec/air_traffic_controller_spec.rb
@@ -1,10 +1,15 @@
 
-require 'air_traffic_controller.rb'
+require 'air_traffic_controller'
 
 describe AirTrafficController do 
-skip
-end 
 
+	it 'recognises when weather is stormy' do
+		@air_traffic_controller = AirTrafficController.new
+
+		expect(@air_traffic_controller).to respond_to :weather
+	end 
+
+end 
 
 
 
