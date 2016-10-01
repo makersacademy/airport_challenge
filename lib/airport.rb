3e78af2085abaf42c79b5733d2b3f7f5f21aa9a4
Darrenf <darrenf54321@gmail.com>
2016-10-01 11:32:47 +0100

    passing test with landing prevented if stormy

--- lib/airport.rb
+++ lib/airport.rb
@@ -7,9 +7,13 @@ class Airport
   end
 
   def land_plane(plane)
-    @planes << plane
-    #@planes.last
-    "Plane has landed"
+    if random_weather == "sunny"
+      @planes << plane
+      #@planes.last
+      "Plane has landed"
+    else
+      "Plane cannot land as it is stormy"
+    end
   end
 
   def take_off(plane)
@@ -20,7 +24,5 @@ class Airport
       "plane cannot take off, it's stormy"
     end
   end
-
-
 
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -3,31 +3,33 @@ require 'spec_helper'
 require 'airport'
 require 'plane'
 
-describe Airport do
+  describe Airport do
 
-  let(:plane) { Plane.new }
-  let(:airport) { Airport.new }
+    let(:plane) { Plane.new }
+    let(:airport) { Airport.new }
 
-  it { is_expected.to respond_to :land_plane}
+    it { is_expected.to respond_to :land_plane}
 
-  it "instructs a plane to land" do
-    expect(airport.land_plane(plane)).to eq "Plane has landed"
-  end
-
-  it "will not allow a plane to land if it is stormy" do
-    allow(airport.land_plane(plane)).to eq "The plane cannot land as it is stormy"
-  end
-
-    it "instructs a plane to take off and confirms it is no longer at the airport" do
+    it "instructs a plane to land" do
       allow(airport).to receive(:random_weather) { 'sunny' }
-      expect(airport.take_off(plane)).to eq "the plane has taken off"
+     expect(airport.land_plane(plane)).to eq "Plane has landed"
     end
 
-    it "will not allow a plane to take off if it is stormy" do
+    it "will not allow a plane to land if it is stormy" do
       allow(airport).to receive(:random_weather) { 'stormy' }
-      expect(airport.take_off(plane)).to eq "plane cannot take off, it's stormy"
-    end
+      expect(airport.land_plane(plane)).to eq   "Plane cannot land as it is stormy"
+   end
+
+      it "instructs a plane to take off and confirms it is no longer at the airport" do
+        allow(airport).to receive(:random_weather) { 'sunny' }
+        expect(airport.take_off(plane)).to eq "the plane has taken off"
+      end
+
+      it "will not allow a plane to take off if it is stormy" do
+        allow(airport).to receive(:random_weather) { 'stormy' }
+        expect(airport.take_off(plane)).to eq "plane cannot take off, it's stormy"
+      end
 
 
 
-end
+  end
