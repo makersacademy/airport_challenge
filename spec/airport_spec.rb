188206d119878a441b506e12ec341a12495c0628
Tom Nunn <t@tomnunn.uk>
2016-10-02 13:33:31 +0100

    instruct a plane to land at an airport

---
+++ spec/airport_spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+
+describe Airport do
+  subject(:airport) { described_class.new }
+
+  it 'should instruct a plane to land' do
+    expect(airport).to respond_to(:land).with(1).argument
+  end
+end
