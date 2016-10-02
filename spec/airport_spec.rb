2bfafd904a35623d054e10b1feedf0607a29a898
Tom Nunn <t@tomnunn.uk>
2016-10-02 16:40:07 +0100

    refactor airport Examples

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,24 +2,28 @@ require 'spec_helper'
 
 describe Airport do
   subject(:airport) { described_class.new }
-  subject(:plane) { described_class.new }
+  let(:plane) { double :plane }
 
-  it 'should instruct a plane to land' do
-    expect(airport).to respond_to(:land).with(1).argument
+  describe '#land' do
+    it 'should instruct a plane to land' do
+      expect(airport).to respond_to(:land).with(1).argument
+    end
+
+    it 'should confirm a plane has landed' do
+      airport.land(plane)
+      expect(airport.planes).to include plane
+    end
   end
 
-  it 'should confirm a plane has landed' do
-    airport.land(plane)
-    expect(airport.planes).to include plane
-  end
+  describe '#take_off' do
+    it 'should instruct a plane to take_off' do
+      expect(airport).to respond_to(:take_off).with(1).argument
+    end
 
-  it 'should instruct a plane to take_off' do
-    expect(airport).to respond_to(:take_off).with(1).argument
-  end
-
-  it 'should confirm that the plane in no longer at the Airport' do
-    airport.land(plane)
-    airport.take_off(plane)
-    expect(airport.planes).not_to include plane
+    it 'should confirm that the plane in no longer at the Airport' do
+      airport.land(plane)
+      airport.take_off(plane)
+      expect(airport.planes).not_to include plane
+    end
   end
 end
