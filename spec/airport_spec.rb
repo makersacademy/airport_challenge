c147c4a51670045d3138b46c35561b37b91bf98b
jinimcoroneo <jinim1983@gmail.com>
2016-10-02 12:33:56 +0100

    second user story completed, moved features file to spec folder so that rspec could see it

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -6,4 +6,12 @@ describe Airport do
   it "airport can instruct the plane to land" do
     expect(airport).to respond_to(:land).with(1).argument
   end
+
+  it 'airport can instruct plane to take off' do
+    expect(airport).to respond_to(:take_off).with(1).argument
+  end
+
+  it 'airport can confirm that the plane is no longer in the airport' do
+    expect(airport).to respond_to(:take_off).with(1).argument
+  end
 end
