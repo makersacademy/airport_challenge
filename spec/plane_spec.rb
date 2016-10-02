1c11f589aea100899a84b55e937806fc1d00a108
Laura Weston <laura.weston@outlook.com>
2016-10-02 10:23:49 +0100

    Plane cannot land unless instructed to land

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -3,15 +3,29 @@
 # I want to instruct a plane to land at an airport and confirm that it has landed 
 
 class Plane
-  def landed?
-    true
+  
+  def initialize
+    @landed = false
   end
+
+  def land
+    @landed = true
+  end
+
+  def landed?
+    @landed
+  end
+
 end
 
 describe 'plane' do
   it 'can land at an airport and confirms it has landed' do
     plane = Plane.new
-    expect(plane.landed?).to be true
-     
+    plane.land
+    expect(plane.landed?).to be true 
+  end
+  it 'does not land unless instructed to land' do
+    plane = Plane.new
+    expect(plane.landed?).to be false
   end
 end
