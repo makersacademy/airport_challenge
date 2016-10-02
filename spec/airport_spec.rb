bbbd8d933686c777beda6f22baee0f5ad91baa04
James Gardiner <jamesgardiner075@gmail.com>
2016-10-02 15:58:53 +0100

    Refactor spec file

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -8,9 +8,34 @@ describe Airport do
     @airport = Airport.new(@weather)
   end
 
-  context "land plane" do
+  describe "#take_off" do
+    context "given a plane" do
+      it "calls #land on the plane" do
+        expect(@plane).to receive(:take_off)
+        @airport.take_off(@plane)
+      end
+    end
 
-    describe "#land" do
+    context "once instructed a plane to takeoff" do
+      it "removes the plane from the array" do
+        allow(@plane).to receive(:take_off)
+        @airport.planes << @plane
+        @airport.take_off(@plane)
+        expect(@airport.planes).not_to include @plane
+      end
+    end
+
+    context "when #stormy? is true" do
+      it "raises an error" do
+        allow(@plane).to receive(:take_off)
+        allow(@weather).to receive(:stormy?).and_return true
+        expect{@airport.take_off(@plane)}.to raise_error "Cannot takeoff due to stormy weather"
+      end
+    end
+  end
+
+  describe "#land" do
+    context "when given a plane"
       it "instructs the plane to land" do
         # expects plane to have its land method called
         expect(@plane).to receive(:land)
@@ -19,47 +44,21 @@ describe Airport do
       end
     end
 
-    describe "#land" do
-      it "has the plane after it has landed" do
+    context "once instructed the plane to land" do
+      it "adds the plane to the array" do
         allow(@plane).to receive(:land)
         @airport.land @plane
         expect(@airport.planes).to include @plane
       end
     end
-
-  end
-
-  context "takeoff" do
-
-    describe "#take_off" do
-      it "instruct plane to takeoff" do
-        expect(@plane).to receive(:take_off)
-        @airport.take_off(@plane)
-      end
-    end
-
-    describe "#take_off" do
-      it "doesn't have the plane after takeoff" do
-        allow(@plane).to receive(:take_off)
-        @airport.planes << @plane
-        @airport.take_off(@plane)
-        expect(@airport.planes).not_to include @plane
-      end
-    end
-
-    it "doesn't allow takeoff if weather is stormy" do
-      allow(@plane).to receive(:take_off)
-      allow(@weather).to receive(:stormy?).and_return true
-      expect{@airport.take_off(@plane)}.to raise_error "Cannot takeoff due to stormy weather"
-    end
-
   end
 
   describe "#stormy?" do
-    it "checks if weather is stormy" do
-      allow(@weather).to receive(:stormy?).and_return true
-      expect(@airport.stormy?).to eq true
-    end
+    context "when @weather.stormy? is true" do
+      it "returns true" do
+        allow(@weather).to receive(:stormy?).and_return true
+        expect(@airport.stormy?).to eq true
+      end
   end
 
 end
