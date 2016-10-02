b4ef9728bb6b35e6d031d5756502a57c74657088
Amaal Ali <amq.ali@gmail.com>
2016-10-02 01:04:59 +0100

    Method written to land plane

--- lib/plane_file.rb
+++ lib/plane_file.rb
@@ -1,14 +1,14 @@
 class Plane
 
-  def initialize(plane_location = false)
-    @plane_location = plane_location
+  def initialize(plane_is_landed = false)
+    @plane_is_landed = plane_is_landed
   end
 
   def plane_is_landed?
-    @plane_location
+    @plane_is_landed
   end
 
   def proceed_to_land
-
+    @plane_is_landed = true
   end
 end
