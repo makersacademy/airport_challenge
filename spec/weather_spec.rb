6b3cf151635bf7f87edb24730e68ae2557963eca
Lilian Breidenbach <liliancbreidenbach@gmail.com>
2016-10-02 15:18:28 +0100

    all tests should pass

--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -2,9 +2,5 @@ require 'weather.rb'
 
 describe Weather do
   subject(:condition) { described_class.new }
-
-  it 'should equal stormy if random_weather is greater than 7' do
-    subject.stub(:random_weather).and_return(true)
-    expect(condition.stormy).to eq true
-  end
+  it { is_expected.to respond_to(:stormy) }
 end
