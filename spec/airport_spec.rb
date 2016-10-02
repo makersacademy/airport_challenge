a2a71f7234cc30dfdc230357320ea50f4246bcb1
James Gardiner <jamesgardiner075@gmail.com>
2016-10-02 19:25:16 +0100

    Refactor tests to use let instead of before :each do

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,34 +2,32 @@ require "airport"
 
 describe Airport do
 
-  before :each do
-    @plane = double(:plane)
-    @weather = double(:weather, :stormy? => false)
-    @airport = Airport.new(@weather)
-  end
+  let(:airport) { described_class.new(weather) }
+  let(:weather) { double(:weather, :stormy? => false) }
+  let(:plane) { double(:plane) }
 
   describe "#take_off" do
     context "given a plane" do
       it "calls #land on the plane" do
-        expect(@plane).to receive(:take_off)
-        @airport.take_off(@plane)
+        expect(plane).to receive(:take_off)
+        airport.take_off(plane)
       end
     end
 
     context "once instructed a plane to takeoff" do
       it "removes the plane from the array" do
-        allow(@plane).to receive(:take_off)
-        @airport.planes << @plane
-        @airport.take_off(@plane)
-        expect(@airport.planes).not_to include @plane
+        allow(plane).to receive(:take_off)
+        airport.planes << plane
+        airport.take_off(plane)
+        expect(airport.planes).not_to include plane
       end
     end
 
     context "when #stormy? is true" do
       it "raises an error" do
-        allow(@plane).to receive(:take_off)
-        allow(@weather).to receive(:stormy?).and_return true
-        expect{@airport.take_off(@plane)}.to raise_error "Cannot takeoff due to stormy weather"
+        allow(plane).to receive(:take_off)
+        allow(weather).to receive(:stormy?).and_return true
+        expect{airport.take_off(plane)}.to raise_error "Cannot takeoff due to stormy weather"
       end
     end
   end
@@ -38,33 +36,33 @@ describe Airport do
     context "when given a plane" do
       it "instructs the plane to land" do
         # expects plane to have its land method called
-        expect(@plane).to receive(:land)
-        # call the airport.land(@plane) to call plane.land
-        @airport.land @plane
+        expect(plane).to receive(:land)
+        # call the airport.land(plane) to call plane.land
+        airport.land plane
       end
     end
 
     context "once instructed the plane to land" do
       it "adds the plane to the array" do
-        allow(@plane).to receive(:land)
-        @airport.land @plane
-        expect(@airport.planes).to include @plane
+        allow(plane).to receive(:land)
+        airport.land plane
+        expect(airport.planes).to include plane
       end
     end
 
     context "when #stormy? is true" do
       it "raises an error" do
-        allow(@plane).to receive(:land)
-        allow(@weather).to receive(:stormy?).and_return true
-        expect{@airport.land(@plane)}.to raise_error "Cannot land due to stormy weather"
+        allow(plane).to receive(:land)
+        allow(weather).to receive(:stormy?).and_return true
+        expect{airport.land(plane)}.to raise_error "Cannot land due to stormy weather"
       end
     end
 
     context "when #full? is true" do
       it "raises an error" do
-        allow(@plane).to receive(:land)
-        10.times {@airport.land(@plane)}
-        expect{@airport.land(@plane)}.to raise_error "Cannot land plane as airport is full"
+        allow(plane).to receive(:land)
+        10.times {airport.land(plane)}
+        expect{airport.land(plane)}.to raise_error "Cannot land plane as airport is full"
       end
     end
   end
--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -2,22 +2,20 @@ require "weather"
 
 describe Weather do
 
-  before :each do
-    @weather = Weather.new
-  end
+  let(:weather) { described_class.new }
 
   describe "#stormy?" do
     context "when #random_outlook is :stormy" do
       it "can produce true as an output" do
-        allow(@weather).to receive(:random_outlook).and_return :stormy
-        expect(@weather.stormy?).to eq true
+        allow(weather).to receive(:random_outlook).and_return :stormy
+        expect(weather.stormy?).to eq true
       end
     end
 
     context "when #random_outlook is :fine" do
       it "can produce false as an output" do
-        allow(@weather).to receive(:random_outlook).and_return :fine
-        expect(@weather.stormy?).to eq false
+        allow(weather).to receive(:random_outlook).and_return :fine
+        expect(weather.stormy?).to eq false
       end
     end
   end
