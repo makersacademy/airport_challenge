11b29f875993e1df9774055eff97261228abbb0a
Anna Lucking <annavlucking@hotmail.co.uk>
2016-10-02 13:34:37 +0100

    Added ability to land plane

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,6 +1,8 @@
 class Airport
+attr_reader :plane
 
-  def land_plane
+  def land(plane)
+    @plane = plane 
   end
 
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,7 +2,25 @@ require 'airport'
 
 describe Airport do
 airport = Airport.new
-  it 'lands plane' do
-    expect(airport).to respond_to (:land_plane)
+  it 'reads land method' do
+    expect(airport).to respond_to (:land)
+  end
+
+  it 'reads land argument' do
+    expect(airport).to respond_to(:land).with(1).argument
+  end
+
+  it 'reads attr_reader' do
+    expect(airport).to respond_to(:plane)
+  end
+
+  it 'lands a plane' do
+    plane = Plane.new
+    expect(subject.land(plane)).to eq(plane)
+  end
+
+  it 'confirms plane has landed' do
+    expect()
+    #expect successful plane landing to output the string 'Plane has landed!'
   end
 end
