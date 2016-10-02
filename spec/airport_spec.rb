5b056cccf96370cd751d15ced8544282fc29b80f
Michelle Epstein <epstein.michelle@gmail.com>
2016-10-02 20:34:12 +0100

    added weather lib and spec

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -3,16 +3,45 @@ require 'plane'
 
 describe Airport do
 
-before do
-  @airport = Airport.new
+subject(:airport) {described_class.new}
+let(:plane) {double :plane}
+
+describe "#initialize" do
+  context "the airtraffic controller set a capacity" do
+    it "should allow the airtraffic controller to set a capacity" do
+      airport.instance_variable_get("@capacity")
+    end
+  end
+
+  context "use default capacity" do
+    it "should use the default capacity of 50 unless stated" do
+      expect(airport.capacity).to  eq 50
+    end
+  end
 end
 
-  it "should instruct a plane to land and confirm it has landed" do
-     expect(@airport).to respond_to(:land)
-   end
+desribe '#land'
 
-   it "should instruct a plane to take off and confirm it's no longer at the airport" do
-     expect(@airport).to respond_to(:take_off)
+it "instructs the plane to land" do
+  allow(plane).to receive(:land)
+  airport.land(plane)
+end
+
+it "knows that the plane has landed" do
+  allow(plane).to receive(:land)
+  airport.land(plane)
+  expect(airport.planes).to include plane
+
+  airport.land(plane)
+end
+
+it "instructs a plane to take off" do
+  allow(plane).to receive(:take_off)
+  airport.take_off
+end
+
+  it "should instruct a plane to take off" do
+     allow(plane).to receive(:take_off)
    end
 
    it "should prevent landing when the airport is full" do
