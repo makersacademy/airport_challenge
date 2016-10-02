c1b9700597f23d81a8b42fe65f08b6acfad3d7a2
Laura Weston <laura.weston@outlook.com>
2016-10-02 16:57:53 +0100

    Add extra test cases to ensure Plane#landed? and Plane#airborne? are mutually exclusive

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -7,34 +7,45 @@ describe Plane do
     it 'is airborne' do
       expect(plane.airborne?).to be true
     end
+    it 'has not landed' do
+      expect(plane.landed?).to be false
+    end
   end
-
+  
   context 'when instructed to land' do
-    it 'can land at an airport and confirm that it has landed' do
+    it 'can land and confirm that it has landed' do
       plane.land
       expect(plane.landed?).to be true 
+    end
+    it 'can land and confirm it is no longer airborne' do
+      plane.land
+      expect(plane.airborne?).to be false
     end
     it 'cannot land if it has already landed' do
       plane.land
       expect { plane.land }.to raise_error("This plane has already landed!")
-    end
+    end 
   end
   
   # As an air traffic controller 
   # So I can get passengers on the way to their destination 
   # I want to instruct a plane to take off from an airport and confirm that it is no longer in the airport
   context 'when instructed to take off' do
-    it 'can take off from an airport and confirm that it is no longer in the airport' do
+    it 'can take off and confirm that it is airborne' do
       plane.land
       plane.take_off
       expect(plane.airborne?).to be true
     end
-
+    it 'can take off and confirm that it has not landed' do
+      plane.land
+      plane.take_off
+      expect(plane.landed?).to be false
+    end
     it 'cannot take off if it is already airborne' do
       expect { plane.take_off }.to raise_error("This plane is no longer in the airport!")
     end
   end
-
+  
   # As an air traffic controller 
   # To ensure safety 
   # I want to prevent takeoff when weather is stormy
