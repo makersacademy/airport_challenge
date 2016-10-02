39d546b0de23fd7e76327ae59449e22f9f035fb6
elizabethvenner <elizabethvenner@googlemail.com>
2016-10-02 18:22:34 +0100

    Refactor plane_spec.rb to introduce doubles

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,14 +2,15 @@ require 'airport'
 
 describe Airport do
   subject(:airport) {described_class.new}
+  let(:plane1) {double :plane}
 
   it 'instructs a plane to land at an airport' do
-    plane1 = Plane.new
+    allow(plane1).to receive(:land).and_return(plane1)
     expect(airport.land(plane1)).to eq plane1
   end
 
   it 'confirms that the plane has landed' do
-    plane1 = Plane.new
+    allow(plane1).to receive(:land).and_return(plane1)
     airport.land(plane1)
     expect(airport.plane).to eq plane1
   end
@@ -17,13 +18,13 @@ describe Airport do
   it {is_expected.to respond_to(:take_off).with(1).argument}
 
   it 'instructs plane to take off from airport' do
-    plane1 = Plane.new
+    allow(plane1).to receive(:take_off).and_return(nil)
     airport.land(plane1)
     expect(airport.take_off(plane1)).to eq nil
   end
 
   it 'confirms that plane is no longer in the airport' do
-    plane1 = Plane.new
+    allow(plane1).to receive(:take_off).and_return(nil)
     airport.land(plane1)
     airport.take_off(plane1)
     expect(airport.plane).to eq nil
