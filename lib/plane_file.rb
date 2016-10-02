04c923320b03e12bba43aea74a8aa9cd3f6e8293
Amaal Ali <amq.ali@gmail.com>
2016-10-02 16:30:12 +0100

    Defined initialization of a plane and varaible to store plane landing status.

--- lib/plane_file.rb
+++ lib/plane_file.rb
@@ -1,8 +1,18 @@
 require 'airport'
 
 class Plane
+
+  def initialize(plane_is_landed = false)
+    @plane_is_landed = plane_is_landed
+  end
+
+  def plane_is_landed?
+    @plane_is_landed
+  end
+
+
   def proceed_to_land(airport_name)
     airport_name.accept_plane(self)
-    @plane_is_landed = true
+    @plane_is_landed = airport_name.landed?(self)
   end
 end
