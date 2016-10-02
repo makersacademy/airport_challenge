13297713d62d72608297875f03564d6d760575b0
Michelle Epstein <epstein.michelle@gmail.com>
2016-10-02 15:13:56 +0100

    Initial RSpec and ruby files added

---
+++ lib/airport.rb
@@ -0,0 +1,12 @@
+require_relative "plane.rb"
+
+class Airport
+
+  # def initalize
+  #   @planes = []
+  # end
+
+  def land(plane)
+    # @planes << plane
+  end
+end
---
+++ spec/airport_spec.rb
@@ -0,0 +1,6 @@
+require "airport.rb"
+
+describe Airport do
+  it { is_expected.to respond_to :land}
+
+end
