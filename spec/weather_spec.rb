441e04ed0031692a191e3f2c91951daa2857c893
James Gardiner <jamesgardiner075@gmail.com>
2016-10-02 13:44:47 +0100

    Weather will give random weather output

--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -1,1 +1,17 @@
 require "weather"
+
+describe Weather do
+
+  before :each do
+    @test_arr = []
+    1000.times { @test_arr << subject.stormy? }
+  end
+
+  it "can produce true as an output" do
+    expect(@test_arr).to include true
+  end
+
+  it "can produce false as an output" do
+    expect(@test_arr).to include false
+  end
+end
