fe8a7d20f0d447244ad880dfbcfdd81fa6084fc5
Michelle Epstein <epstein.michelle@gmail.com>
2016-10-02 20:04:40 +0100

    Added airport capacity

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,15 +1,27 @@
 require_relative "plane.rb"
 
 class Airport
-  def initalize
+    DEFAULT_CAPACITY = 50
+    attr_reader :planes, :capacity, :stormy
+
+  def initalize(capacity = DEFAULT_CAPACITY)
     @planes = []
+    @capacity = capacity
+    @stormy = []
   end
 
   def land(plane)
+    raise "There is no room to land!" if full?
     @planes << plane
+      "Confirm the plane has landed"
   end
 
   def take_off(plane)
     @planes.pop
+  end
+
+  private
+  def full?
+    @planes.count == @capacity
   end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -14,4 +14,9 @@ end
    it "should instruct a plane to take off and confirm it's no longer at the airport" do
      expect(@airport).to respond_to(:take_off)
    end
+
+   it "should prevent landing when the airport is full" do
+     Airport::DEFAULT_CAPACITY.times {airport.land(plane)}
+     expect{airport.land(plane)}.to raise_error "Sorry, the airport is full so you cannot land"
+   end
  end
