46da74909ef27f34735df7fca78bbbbb8a14555a
Blanca Mendizabal Perello <blanca.mendizabal.perello@gmail.com>
2016-10-02 13:02:05 +0100

    Prevent invalid states
    Raise error when landing when plane already landed.
    Raise error when taking off if plane is not in the airport.

--- lib/airport.rb
+++ lib/airport.rb
@@ -6,11 +6,13 @@ class Airport
   end
 
   def land(plane)
+    fail 'This plane has already landed' if @planes.include?(plane)
     plane.land
     @planes.push(plane)
   end
 
   def take_off(plane)
+    fail 'This plane has not landed yet' unless @planes.include?(plane)
     plane.take_off
     @planes.delete(plane)
   end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -8,27 +8,33 @@ describe Airport do
   it { is_expected.to respond_to :take_off }
 
   describe '#land' do
-    it 'instructs the plane to land' do
+    it 'instructs plane to land' do
       expect(plane).to receive(:land)
       subject.land(plane)
     end
 
-    it 'has the plane after it has landed' do
+    it 'has plane after it has landed' do
       allow(plane).to receive(:land)
       airport.land(plane)
       expect(airport.planes).to include plane
     end
+
+    it 'prevents plane from landing if already in airport' do
+      allow(plane).to receive(:land)
+      airport.land(plane)
+      expect { airport.land(plane) }.to raise_error 'This plane has already landed'
+    end
   end
 
   describe '#take_off' do
-    it 'instructs a plane to take off' do
+    it 'instructs plane to take off' do
       allow(plane).to receive(:land)
       expect(plane).to receive(:take_off)
       airport.land(plane)
       airport.take_off(plane)
     end
 
-    it 'don\'t have the plane after take off' do
+    it 'doesn\'t have plane after take off' do
       allow(plane).to receive(:land)
       allow(plane).to receive(:take_off)
       airport.land(plane)
@@ -36,5 +42,9 @@ describe Airport do
       expect(airport.planes).not_to include plane
     end
 
+    it 'prevents plane from taking off if not in airport' do
+      allow(plane).to receive(:take_off)
+      expect { airport.take_off(plane) }.to raise_error 'This plane has not landed yet'
+    end
   end
 end
