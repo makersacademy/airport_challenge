ce9589c48fe5e0da0230c0c7a42e6c7313d6d4f1
Anna Beddow <anna.beddow@gmail.com>
2016-10-02 17:30:58 +0100

    added planes depart and arrive at airport and confirm depature and arrival

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,17 +1,38 @@
 class Airport
-
-attr_reader :plane
+attr_reader :status, :take_off, :land, :departure_arr, :arrivals
 
 def initialize
+  @departure_arr =[]
+  @arrivals=[]
+
 end
 
 def land(plane)
-  @plane = plane
-  print "Plane has landed" if defined? @plane
+  @landed_plane = plane
+  @landed_plane.landed
+  @arrivals << @landed_plane
+
+  confirm_landed
 end
 
-def confirm_landing
-
+def confirm_landed
+  print  @arrivals.length >= 1 ? "Plane has landed" : "Landing failed!"
 end
+
+def departures(plane)
+  @departure_arr << plane
+end
+
+def take_off
+  @inflight_plane = @departure_arr.pop
+  @inflight_plane.airborne
+
+  confirm_take_off
+end
+
+def confirm_take_off
+  print @departure_arr.length <= 0 ? "Plane has taken off" : "Take_off failed!"
+end
+
 
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -9,6 +9,14 @@ describe "Airport" do
   expect(airport.class).to eq Airport
  end
 
+ context 'plane is airborne' do
+   it "changes plane flight status to 'flying'"do
+    airport.departures(plane)
+    airport.take_off
+     expect(plane.status).to eq('flying')
+   end
+ end
+
 it 'lands a plane' do
   expect(airport).to respond_to(:land).with(1).argument
 end
@@ -16,5 +24,9 @@ end
 it 'confirms plane has landed' do
   expect{airport.land(plane)}.to output("Plane has landed").to_stdout
 end
+
+
+
+
 
 end
