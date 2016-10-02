240ddfaafe810c505f9dec17bb93fd0a55174371
Chris Lawrence <chris.lawrence@gmx.com>
2016-10-02 10:18:13 +0100

    Plane and Weather class re written to better follow TDD

--- lib/plane.rb
+++ lib/plane.rb
@@ -1,14 +1,3 @@
-require_relative './airport.rb'
-
 class Plane
-  attr_accessor :airborne, :landed
-
-  def initialise
-    @landed = false
-  end
-
-  def airborne?
-    landed ? false : true
-  end
 
 end
--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -2,8 +2,12 @@ require 'weather'
 
 describe Weather do
 
+  it "is an instance of the Weather class" do
+    expect(subject.is_a?(Weather)).to eq true
+  end
+
   it "stormy? should return true if it is stormy" do
-    expect(subject.stormy?).to eq(false)
+    expect(subject.stormy?).to eq false
   end
 
 end
