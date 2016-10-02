c7312e809e14dbfc35b45c439e7e75dfbe783268
Royston <rlqhenson@hotmail.com>
2016-10-02 14:43:24 +0100

    added some tests and code

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,1 +1,20 @@
- require 'plane'
+ require_relative 'plane'
+
+class Airport
+attr_reader :planes
+
+  def initialize
+      @planes = []
+  end
+
+  def land(plane)
+    @planes << plane
+    #confirm_landing
+  end
+
+private
+
+  def confirm_landing
+      "The plane has landed"
+  end
+end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,1 +1,17 @@
 require 'airport'
+
+describe Airport do
+  let(:airport) { Airport.new }
+  let(:plane) { Plane.new }
+  it "responds to lands" do
+    expect(airport).to respond_to(:land).with(1).argument
+  end
+  it "lands plane in airport" do
+    planes = []
+    planes << Plane.new
+    expect(airport.land(plane)).to include(plane)
+  end
+  # it "confirms landing" do
+  #   expect(confirm_landing).to include"The plane has landed"
+  # end
+end
