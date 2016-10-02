491e3cd7c06fdd0af7e784eca77d7f77e6c17281
Stephen Gregory <stejgregory@gmail.com>
2016-10-03 00:59:45 +0100

    Full airport won't allow plans

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,10 +1,20 @@
 class Airport
 
   DEFAULT_CAPACITY = 25
-  attr_reader :capacity
+  attr_reader :capacity, :grounded_planes
+
+  def initialize (capacity = DEFAULT_CAPACITY)
+    @capacity = capacity
+    @grounded_planes = []
+  end
 
   def land(plane)
-    plane
+      fail "Airport is full. Unable to land." if full?
+      @grounded_planes << plane
+  end
+
+  def full?
+    @grounded_planes.count == @capacity
   end
 
   def takeoff(plane)
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -5,7 +5,12 @@ describe Airport do
   let(:plane) { double :plane }
 
   it "accepts planes landing" do
-    expect(subject.land(plane)).to eq plane
+    expect(subject.land(plane)).to eq [plane]
+  end
+
+  it "won't allow landing plane if airport full" do
+    subject.capacity.times{ subject.land(plane) }
+    expect{ subject.land(plane) }.to raise_error "Airport is full. Unable to land."
   end
 
   it "accepts planes taking off" do
