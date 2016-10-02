1cb327e0972f5fd976547ff59588319efb5eba84
Chris Lawrence <chris.lawrence@gmx.com>
2016-10-02 11:10:11 +0100

    Plane class given take off and landing methods, tests passing

--- lib/plane.rb
+++ lib/plane.rb
@@ -1,3 +1,13 @@
 class Plane
 
+  def land
+    true
+    "#{self} landed"
+  end
+
+  def take_off
+    true
+    "#{self} has taken off"
+  end
+
 end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -5,4 +5,22 @@ describe Plane do
   it "is an instance of the Plane class" do
     expect(subject.is_a?(Plane)).to eq(true)
   end
+
+  xit "can be instructed to land" do
+    expect(subject.land).to eq true
+  end
+
+  it "informs me it has landed" do
+    expect(subject.land).to eq "#{subject} landed"
+  end
+
+  xit "can be instructed to takeoff" do
+    expect(subject.take_off).to eq true
+  end
+
+  it "informs me it has taken off" do
+    expect(subject.take_off).to eq "#{subject} has taken off"
+  end
+
+
 end
