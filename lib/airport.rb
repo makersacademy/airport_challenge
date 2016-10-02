3eca0c4815fa4f340b1b3bd0f87b246d7ae4a5a7
Tom Nunn <t@tomnunn.uk>
2016-10-02 14:41:12 +0100

    instruct a plane to take off from an airport

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,5 +1,7 @@
 class Airport
   def land(plane)
-    true
+  end
+
+  def take_off(plane)
   end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -6,4 +6,8 @@ describe Airport do
   it 'should instruct a plane to land' do
     expect(airport).to respond_to(:land).with(1).argument
   end
+
+  it 'should instruct a plane to take_off' do
+    expect(airport).to respond_to(:take_off).with(1).argument
+  end
 end
