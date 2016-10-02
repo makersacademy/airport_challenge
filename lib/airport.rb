ff4904d533acdd44dcbe76df3e5eb20a569a538d
Anna Beddow <anna.beddow@gmail.com>
2016-10-02 17:50:51 +0100

    added random number weather

--- lib/airport.rb
+++ lib/airport.rb
@@ -4,15 +4,19 @@ attr_reader :status, :take_off, :land, :departure_arr, :arrivals
 def initialize
   @departure_arr =[]
   @arrivals=[]
+  @conditions = rand(10)
 
 end
 
 def land(plane)
+  if @conditions >=3
   @landed_plane = plane
   @landed_plane.landed
   @arrivals << @landed_plane
-
   confirm_landed
+  else
+  print "Sorry it's too stormy to land right now"
+  end
 end
 
 def confirm_landed
@@ -24,10 +28,13 @@ def departures(plane)
 end
 
 def take_off
+  if @conditions >= 3
   @inflight_plane = @departure_arr.pop
   @inflight_plane.airborne
-
   confirm_take_off
+  else
+    print "sorry it's too stormy to fly today."
+  end
 end
 
 def confirm_take_off
