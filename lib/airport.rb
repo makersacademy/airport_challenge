c3a054a4b424b48b8aaf3956bf8c25bb6d1f71af
alexanderwjrussell <awjrussell@live.co.uk>
2016-10-02 10:36:17 +0100

    DRY up the code and make it more readable

--- lib/airport.rb
+++ lib/airport.rb
@@ -15,18 +15,13 @@ DEFAULT_CAPACITY = 5
   end
 
   def land(plane)
-    fail "Airport is at capacity" if full?
-    fail "Can't land as already landed" if
-    plane.flying == false
-    fail "Can't land in stormy weather" if weather.conditions == "stormy"
+    check_can_land(plane)
     plane.flying = false
     @planes << plane
   end
 
   def take_off(plane)
-    fail "Can't take off as already flying" if plane.flying?
-    fail "Can't take off as plane doesn't exist" if self.exists(plane) == false
-    fail "Can't take off in stormy weather" if weather.conditions == "stormy"
+    check_can_take_off(plane)
     plane.flying = true
     @planes -= [plane]
   end
@@ -41,5 +36,22 @@ DEFAULT_CAPACITY = 5
 
   def exists(plane)
     @planes.include? plane
+  end
+
+  def stormy_weather
+    weather.conditions == "stormy"
+  end
+
+  def check_can_take_off(plane)
+    fail "Can't take off as already flying" if plane.flying?
+    fail "Can't take off as plane doesn't exist" if self.exists(plane) == false
+    fail "Can't take off in stormy weather" if stormy_weather
+  end
+
+  def check_can_land(plane)
+    fail "Airport is at capacity" if full?
+    fail "Can't land as already landed" if
+    plane.flying == false
+    fail "Can't land in stormy weather" if stormy_weather
   end
 end
