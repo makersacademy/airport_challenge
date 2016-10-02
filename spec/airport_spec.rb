d7e6f8fb983cc6231124844d9facc553dc669abf
elizabethvenner <elizabethvenner@googlemail.com>
2016-10-02 18:28:35 +0100

    Refactor airport_spec.rb again to take make use of before blocks

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -4,30 +4,40 @@ describe Airport do
   subject(:airport) {described_class.new}
   let(:plane1) {double :plane}
 
-  it 'instructs a plane to land at an airport' do
-    allow(plane1).to receive(:land).and_return(plane1)
-    expect(airport.land(plane1)).to eq plane1
-  end
+  describe 'tests that need to call #land on a plane double' do
+    before do
+      allow(plane1).to receive(:land).and_return(plane1)
+    end
 
-  it 'confirms that the plane has landed' do
-    allow(plane1).to receive(:land).and_return(plane1)
-    airport.land(plane1)
-    expect(airport.plane).to eq plane1
+    it 'instructs a plane to land at an airport' do
+      expect(airport.land(plane1)).to eq plane1
+    end
+
+    it 'confirms that the plane has landed' do
+      airport.land(plane1)
+      expect(airport.plane).to eq plane1
+    end
+
   end
 
   it {is_expected.to respond_to(:take_off).with(1).argument}
 
-  it 'instructs plane to take off from airport' do
-    allow(plane1).to receive(:take_off).and_return(nil)
-    airport.land(plane1)
-    expect(airport.take_off(plane1)).to eq nil
-  end
+  describe 'tests that need to call #take_off on a plane double' do
+    before do
+      allow(plane1).to receive(:take_off).and_return(nil)
+    end
 
-  it 'confirms that plane is no longer in the airport' do
-    allow(plane1).to receive(:take_off).and_return(nil)
-    airport.land(plane1)
-    airport.take_off(plane1)
-    expect(airport.plane).to eq nil
+    it 'instructs plane to take off from airport' do
+      airport.land(plane1)
+      expect(airport.take_off(plane1)).to eq nil
+    end
+
+    it 'confirms that plane is no longer in the airport' do
+      airport.land(plane1)
+      airport.take_off(plane1)
+      expect(airport.plane).to eq nil
+    end
+
   end
 
 end
