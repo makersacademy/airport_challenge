d73e4e1fb4aec88ec1e82c53572616d8f18d67ef
antonyCastineiras <antonycastineiras80@gmail.com>
2016-10-02 17:28:59 +0100

    added a chance of chance_of_storm variable, a stormy method and tests to check these to the airport class

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,3 +1,18 @@
 class Airport
 	DEFAULT_CAPACITY = 20
+
+	attr_reader :landed_planes, :capacity 
+	attr_accessor :weather, :chance_of_storm
+
+	def initialize(capacity=DEFAULT_CAPACITY,weather = "clear")
+		@landed_planes = []
+		@capacity = capacity
+		@weather = weather
+		@chance_of_storm = rand(10)
+	end
+
+	def stormy
+		if @chance_of_storm == 10 then @weather = "stormy" end
+	end
 end
+
