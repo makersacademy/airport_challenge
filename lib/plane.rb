b98b216cd735ca8d633e8269ba1811168192508c
B-raw <michaelbarry1989@hotmail.com>
2016-10-02 09:55:53 +0100

    Add landed logic to plane

--- lib/plane.rb
+++ lib/plane.rb
@@ -1,11 +1,16 @@
 class Plane
+  attr_reader :landed
+  alias_method :landed?, :landed
 
-  def landed?
-    true
+  def initialize
+    @landed = true
   end
 
   def land airport
     airport.landed_planes << self
   end
 
+  def take_off airport
+    @landed = false
+  end
 end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -8,9 +8,31 @@ describe Plane do
   it { is_expected.to respond_to :land }
 
   describe "#land" do
-    it "should land at an airport" do
-      
+    before(:each) do
+      @airport = Airport.new
     end
+
+    it "should land at an airport" do
+      subject.land(@airport)
+    end
+
+    it "should know it is landed at an airport" do
+      subject.land(@airport)
+      expect(subject.landed?).to eq true
+    end
+  end
+
+  describe "#take_off" do
+    before(:each) do
+      @airport = Airport.new
+    end
+    it { is_expected.to respond_to :take_off}
+
+    it "should confirm it is not landed" do
+      subject.take_off(@airport)
+      expect(subject.landed?).to eq false
+    end
+
   end
 
 end
