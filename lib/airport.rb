139ea16fdc1f50b6fd8f4a4b79254d113f1440c9
Royston <rlqhenson@hotmail.com>
2016-10-02 18:02:19 +0100

    added capacities test and code

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,23 +1,30 @@
- require_relative 'plane'
+require_relative 'plane'
 
 class Airport
-attr_reader :planes
+  attr_reader :planes
+  attr_accessor :capacity
+  DEFAULT_CAPACITY = 60
 
-  def initialize
-      @planes = []
+  def initialize(capacity=DEFAULT_CAPACITY)
+    @planes = []
+    @capacity = capacity
   end
 
   def land(plane)
+
+    raise "That plane has already landed!" unless !@planes.include?(plane)
     confirm_landing
     @planes << plane
   end
 
   def take_off(plane)
+    raise "You cannot take off in a storm!" unless @weather == "Sunny"
+    raise "That plane is already flying!" unless @planes.include?(plane)
     confirm_take_off
     @planes.pop
   end
 
-private
+  private
 
   def confirm_landing
     puts "The plane has landed!"
@@ -26,4 +33,8 @@ private
   def confirm_take_off
     puts "The plane has taken-off!"
   end
+
+  def weather_today
+  end
+
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -12,4 +12,17 @@ describe Airport do
   it "responds to take-off" do
     expect(airport).to respond_to(:take_off).with(1).argument
   end
+  it "can't land when already landed" do
+    airport.land(plane)
+    expect {airport.land(plane)}.to raise_error("That plane has already landed!")
+  end
+  it "sets default capacity" do
+    DEFAULT_CAPACITY = 60
+    expect(subject.capacity).to eq 60
+  end
+  it "set user-require capacity" do
+    airport = Airport.new(34)
+    expect(airport.capacity).to eq 34
+  end
+
 end
