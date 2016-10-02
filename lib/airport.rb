c19452886771388910f06bbb2764f12bf875d6a2
Tomy8s <tomy8s@hotmail.com>
2016-10-02 23:48:52 +0100

    airports now have weather

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,3 +1,6 @@
 class Airport
   attr_accessor :weather
+  def change_weather
+    @weather = rand(5) < 5 ? 'clear' : 'stormy'
+  end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,7 +1,11 @@
 require 'airport'
 
 describe Airport do
-  it 'has weather conditions' do
+  it 'has weather' do
     expect(subject).to respond_to :weather
   end
+  it 'has differing weather conditions'
+    weather = []
+    100.times{ weather << subject.change_weather }
+    expect(weather).to include 'clear'
 end
