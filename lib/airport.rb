9fa93cdb030ce7f796ced55d993f5fc3e70c338d
iaink <iainkinnish@googlemail.com>
2016-10-02 20:16:00 +0000

    Added a function which allows planes to land.

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,3 +1,17 @@
+require_relative 'plane'
+
 class Airport
+
+def initialize 
+   @planes = [] 
+end
+
+def planes
+    @planes
+end
     
+def land(plane)
+     @planes.push(plane)
+end
+
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,4 +2,19 @@ require 'airport'
 
 describe Airport do
     
+    describe 'initialize' do
+    end
+        
+    describe '#land' do
+    it { is_expected.to respond_to (:land) }
+
+    describe '#land()' do
+    it 'should know when a plane has landed' do
+    liverpool = Airport.new
+    etihad = Plane.new
+    liverpool.land(etihad)
+    expect(subject.land(etihad).pop).to eq etihad
+    end
+    end
+    end
 end
