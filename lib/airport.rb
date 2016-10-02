d79203641cf2c30ccc9e65ce513077afea89dff5
Tom Nunn <t@tomnunn.uk>
2016-10-02 15:38:56 +0100

    confirm plane has landed or is no longer at the airport

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,7 +1,15 @@
 class Airport
+  attr_reader :planes
+
+  def initialize
+    @planes = []
+  end
+
   def land(plane)
+    @planes.push(plane)
   end
 
   def take_off(plane)
+    @planes.delete(plane)
   end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,12 +2,24 @@ require 'spec_helper'
 
 describe Airport do
   subject(:airport) { described_class.new }
+  subject(:plane) { described_class.new }
 
   it 'should instruct a plane to land' do
     expect(airport).to respond_to(:land).with(1).argument
   end
 
+  it 'should confirm a plane has landed' do
+    airport.land(plane)
+    expect(airport.planes).to include plane
+  end
+
   it 'should instruct a plane to take_off' do
     expect(airport).to respond_to(:take_off).with(1).argument
+  end
+
+  it 'should confirm that the plane in no longer at the Airport' do
+    airport.land(plane)
+    airport.take_off(plane)
+    expect(airport.planes).not_to include plane
   end
 end
