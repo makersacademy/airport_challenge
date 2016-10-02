9b8fd5793203f79a17f8e11a531a1ee6b867b74c
Stevie Winston-Gore <Floki@Stevies-MBP.home>
2016-10-02 18:37:19 +0100

    specs for plane status added

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -1,7 +1,18 @@
 require 'plane'
+require 'airport'
 
 describe Plane do
+  it 'has a status' do
+    expect(subject.status).to eq 'in flight'
+  end
+
   it 'instructs a plane to land' do
     expect(subject).to respond_to :land
+  end
+
+  it 'confirms when it has landed' do
+    airport = Airport.new
+    subject.land(airport)
+    expect(subject.status).to eq 'landed'
   end
 end
