5874efc5a181b8d834ac88a72a578fd2abe5ee25
Nicole <nicoleshasha@hotmail.co.uk>
2016-10-02 18:17:41 +0100

    Add argument to airport spec for second user story

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,7 +2,8 @@ require 'airport'
 
 describe Airport do
     it { is_expected.to respond_to(:land).with(1).argument }
-
+    it { is_expected.to respond_to(:take_off).with(1).argument}
+    
     it 'lands a plane at the airport' do
     plane = Plane.new
     expect(subject.land(plane)).to be plane
