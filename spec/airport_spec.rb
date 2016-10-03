169259e4b64455e36676ecec24e3bdb2974aae62
david anderson <d.a.m.anderson@gmail.com>
2016-10-03 08:59:51 +0100

    Doubles added

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -3,22 +3,20 @@ require 'plane'
 
 describe Airport do
 
-  describe '#land' do
+  let(:plane) { double :plane }
 
-    #let(:plane) { double :plane }
+  describe '#land' do
 
     it { is_expected.to respond_to(:land).with(1).argument }
 
     it 'raises an error when weather is stormy' do
         #allow(subject).to receive(:check_weather).and_return(true)
-        plane = Plane.new
-        plane.on_ground = false
+        allow(plane).to receive(:on_ground).and_return(false)
         expect{subject.land(plane)}.to raise_error "Can't land when stormy"
     end
 
     it 'raises an error when plane on ground' do
-      plane = Plane.new
-      subject.add_new_plane(plane)
+      allow(plane).to receive(:on_ground).and_return(true)
       expect { subject.land(plane) }.to raise_error "Plane not in flight"
     end
 
@@ -42,14 +40,12 @@ describe Airport do
 
     it 'raises an error when weather is stormy' do
       #allow(subject).to receive(:check_weather).and_return('stormy')
-      plane = Plane.new
-      subject.add_new_plane(plane)
+      allow(plane).to receive(:on_ground).and_return(true)
       expect { subject.takeoff(plane) }.to raise_error "Can't takeoff when stormy"
     end
 
     it 'raises an error when plane already in flight' do
-      plane = Plane.new
-      plane.on_ground = false
+      allow(plane).to receive(:on_ground).and_return(false)
       expect { subject.takeoff(plane) }.to raise_error "Plane already in air"
     end
 
@@ -60,13 +56,13 @@ describe Airport do
     it { is_expected.to respond_to(:find_plane).with(1).argument }
 
     it 'finds a plane in an airport' do
-      plane = Plane.new
+      allow(plane).to receive(:on_ground).and_return(true)
       subject.add_new_plane(plane)
       expect(subject.find_plane(plane)).to eq plane
     end
 
     it 'raises an error when plane not in airport' do
-      expect { subject.find_plane(Plane.new) }.to raise_error "Plane not in airport"
+      expect { subject.find_plane(plane) }.to raise_error "Plane not in airport"
     end
 
   describe '#initilize' do
