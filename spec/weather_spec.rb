9acbc83488acdf430635ee79c100cea7a962f15e
Lawrence Hunt <lawrence.hunt@hotmail.com>
2016-10-03 10:31:49 +0100

    final

--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -8,7 +8,8 @@ describe Weather do
 subject(:weather) {described_class.new}
 
     it 'is stormy if chance occurs' do
-    subject.stub(:chance) {8}
+      allow(subject).to receive(:chance).and_return(8)
+    # subject.stub(:chance) {8}
     subject.weather
     expect(subject.stormy).to eq true
     end
