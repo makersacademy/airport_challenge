fb5af5dd9b75c7ad987ebabeea8559663efe3eda
souljuse <oppo.irene@gmail.com>
2016-10-03 09:54:43 +0200

    added weather condition statement - not working

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,15 +1,16 @@
 require_relative 'plane'
+require_relative 'weather'
 class Airport
   DEFAULT_CAPACITY = 20
-  attr_reader :capacity, :stormy
+  attr_reader :capacity
   def initialize(capacity = DEFAULT_CAPACITY)
     @planes = []
     @capacity = capacity
-    @stormy = true
+    @weather = Weather.new
   end
 	def take_off
     #raise "Oh no! No bikes available!" if empty?
-#    fail 'The plane cannot fly due to bad weather conditions' if stormy?
+    fail 'The plane cannot fly due to bad weather conditions' if @weather.stormy?
     @planes.pop
 	end
   def empty?
@@ -20,12 +21,10 @@ class Airport
   end
 	def land(plane)
     fail 'Airport is full. The plane cannot land' if full?
-    #fail 'Airport is full. The plane cannot land' if full?
+    fail 'The plane cannot land due to bad weather conditions' if @weather.stormy?
     @planes << plane
+    #puts "The plane has landed"
 	end
-    # def stormy?
-    #   @stormy
-    # end
 	attr_reader :plane
   private :empty?, :full?
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -3,7 +3,6 @@ DEFAULT_CAPACITY = 20
 describe Airport do
   it { is_expected.to respond_to :take_off }
   it { is_expected.to respond_to(:land).with(1).argument }
-  puts 'ciao' if @stormy
   #mocking behaviour of the double
   #we use the plane.new very often so we store it with a let statement
   let(:plane) { double :plane }
@@ -16,22 +15,11 @@ describe Airport do
       subject.capacity.times { subject.land plane }
       expect { subject.land plane }.to raise_error ("Airport is full. The plane cannot land")
     end
-      # it "should not let plane fly if the weather is stormy" do
-      #   subject.capacity.times { subject.land plane }
-      #   expect { subject.land plane }.to raise_error ("landing station full")
-      # end
+    it "should not let plane land if the weather is stormy" do
+      weather = double(:weather, stormy?: true)
+      expect { subject.land plane }.to raise_error ("The plane cannot land due to bad weather conditions")
+    end
   end
-
-    # describe 'initialization' do
-    #   it 'defaults capacity' do
-    #     #described_class just refers to the class
-    #     #it's the same as writing DockingStation::DEFAULT_CAPACITY
-    #     described_class::DEFAULT_CAPACITY.times do
-    #       subject.land(plane)
-    #     end
-    #     expect{ subject.land(plane) }.to raise_error 'Airport is full. The plane cannot land'
-    #   end
-    # end
 
   describe '#take_off' do
     it "the airport should let planes take off" do
@@ -41,11 +29,10 @@ describe Airport do
       # it "should not release infinite planes" do
       #   expect{ subject.take_off }.to raise_error ("There are no planes in the airport")
       # end
-      # it "should not let plane fly if the weather is stormy" do
-      #   plane = double(:plane)
-      #   subject.land plane
-      #   expect{ subject.take_off }.to raise_error ('The plane cannot fly due to bad weather conditions')
-      # end
+    it "should not let plane fly if the weather is stormy" do
+      weather = double(:weather, stormy?: true)
+      expect{ subject.take_off }.to raise_error ('The plane cannot fly due to bad weather conditions')
+    end
   end
 
 end
