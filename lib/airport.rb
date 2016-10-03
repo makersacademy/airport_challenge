ad84dccdb04a8fd0025b548cb55d5cb8673e2e06
B-raw <michaelbarry1989@hotmail.com>
2016-10-03 08:53:28 +0100

    Added feature spec test

--- lib/airport.rb
+++ lib/airport.rb
@@ -2,14 +2,18 @@ require_relative 'plane'
 require_relative 'weather'
 
 class Airport
-  attr_reader :landed_planes, :capacity, :stormy_weather
+  attr_reader :landed_planes, :capacity
 
   DEFAULT_CAPACITY = 40
 
   def initialize(capacity=DEFAULT_CAPACITY)
     @landed_planes = []
     @capacity = capacity
-    @stormy_weather = (Weather.new).stormy_weather
+    @stormy_weather = stormy_weather
+  end
+
+  def stormy_weather
+    (Weather.new).stormy_weather
   end
 
   def full
