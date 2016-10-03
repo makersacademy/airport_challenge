ce767ae554ef7315a1c0897caae59f96c77e7179
Amaal Ali <amq.ali@gmail.com>
2016-10-03 01:28:52 +0100

    Added test to check if output of weather.stormy? is Boolean

--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -1,14 +1,20 @@
 require 'weather'
 
+module Boolean; end
+class TrueClass; include Boolean; end
+class FalseClass; include Boolean; end
+
+
 describe Weather do
   describe '#stormy?' do
     it {is_expected.to respond_to (:stormy?)}
 
-    # it 'output should be true/false' do
-    #   weather_output
-    #   100.times {weather_output << subject.stormy?}
-    #   weather_output.each do |current_weather|
-    #     expect(current_weather).to eq true || false)
-    #   end
+    it 'output should be true/false' do
+       weather_output = Array.new
+      1000.times {weather_output << subject.stormy?}
+      weather_output.each do |current_weather|
+        expect(current_weather.is_a?(Boolean)).to eq true
+      end
+    end
   end
 end
