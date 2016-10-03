a59fcf29d47c5032a8204bd6b4b945b486aa714e
Felix Harrison <fmlharrison@gmail.com>
2016-10-03 11:57:45 +0100

    cleaning weather rspec file

--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -2,16 +2,16 @@ require 'weather'
 
 describe Weather do
   subject(:weather) {described_class.new}
+  let(:stormy_weather) {double :stormy_weather, :stormy? => true }
+  let(:sunny_weather) {double :sunny_weather, :stormy? => false }
 
   it {is_expected.to respond_to(:stormy?)}
 
   it 'returns true when the weather is stormy' do
-    allow(weather).to receive(:stormy?).and_return(true)
-    expect(weather.stormy?).to be true
+    expect(stormy_weather.stormy?).to be true
   end
 
   it 'returns false when the weather is sunny' do
-    allow(weather).to receive(:stormy?).and_return(false)
-    expect(weather.stormy?).to be false
+    expect(sunny_weather.stormy?).to be false
   end
 end
