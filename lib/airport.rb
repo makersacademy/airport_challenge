fdc79bc94fc452b26e415657d2156cb6d5f5ab0f
Ed Powderham <edpowderham@outlook.com>
2016-10-03 10:56:32 +0100

    Refactoring

--- lib/airport.rb
+++ lib/airport.rb
@@ -3,7 +3,9 @@ require_relative 'plane'
 class Airport
   attr_accessor :planes, :flight_log, :weather, :capacity
 
-  def initialize(capacity = 10)
+  DEFAULT_CAPACITY = 10
+
+  def initialize(capacity = DEFAULT_CAPACITY)
     @stormy = set_weather
     @planes = []
     @flight_log = []
@@ -14,39 +16,51 @@ class Airport
     raise 'Cannot land due to no capacity' if at_capacity?
     raise 'Cannot land due to bad weather' if stormy?
     raise 'Plane has already landed' if plane_exists?(plane)
-    @planes << plane
-    @flight_log << "Plane #{plane} landed"
+    add_plane(plane)
+    update_log("Plane #{plane} landed")
     @flight_log.last
   end
 
   def take_off(plane)
     raise 'Cannot take off due to bad weather' if stormy?
     raise 'Plane is not in the airport' unless plane_exists?(plane)
-
-    runway = plane
-    @planes.delete_if {|plane| plane === runway}
-    @flight_log << "Plane #{plane} took off"
+    remove_plane(plane)
+    update_log("Plane #{plane} took off")
     @flight_log.last
   end
 
-  def flight_log
+  def show_log
     @flight_log
-  end
-
-  def stormy?
-    @stormy == true
-  end
-
-  def at_capacity?
-    @planes.length == @capacity
-  end
-
-  def plane_exists?(plane)
-    @planes.include? plane
   end
 
   private
     def set_weather
-      (rand > 0.95) ? @stormy = true : @stormy = false
+      rand > 0.95
     end
+
+    def stormy?
+      @stormy == true
+    end
+
+    def at_capacity?
+      @planes.length == @capacity
+    end
+
+    def update_log(message)
+      @flight_log << message
+    end
+
+    def remove_plane(plane)
+      runway = plane
+      @planes.delete_if {|plane| plane === runway}
+    end
+
+    def add_plane(plane)
+      @planes << plane
+    end
+
+    def plane_exists?(plane)
+      @planes.include? plane
+    end
+
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -13,58 +13,68 @@ describe Airport do
     expect(subject).to be_a Airport
   end
 
-  it "Should allow a plane to land" do
-    expect(@airport).to respond_to :land_plane
-    @airport.land_plane(@plane)
-    expect(@airport.planes).to eq [@plane]
-    dumbo = Plane.new
-    expect(@airport.land_plane(dumbo)).to eq "Plane #{dumbo} landed"
+  describe "Landing tests" do
+    it "Should allow a plane to land" do
+      expect(@airport).to respond_to :land_plane
+      @airport.land_plane(@plane)
+      expect(@airport.planes).to eq [@plane]
+    end
+    it "Planes should not be able to land twice" do
+      @airport.land_plane(@plane)
+      expect{@airport.land_plane(@plane)}.to raise_error('Plane has already landed')
+    end
   end
 
-  it "Should allow planes to take off" do
-    expect(@airport).to respond_to :take_off
-    @airport.land_plane(@plane)
-    @airport.take_off(@plane)
-    expect(@airport.planes).to_not include(@plane)
-    dumbo = Plane.new
-    @airport.land_plane(dumbo)
-    expect(@airport.take_off(dumbo)).to eq "#{@airport.flight_log.last}"
+  describe "Take off tests" do
+    it "Should allow planes to take off" do
+      @airport.land_plane(@plane)
+      expect(@airport.take_off(@plane)).to eq "#{@airport.flight_log.last}"
+    end
+
+    it "Planes should not be able to take off if they are not in the airport" do
+      expect{@airport.take_off(@plane)}.to raise_error('Plane is not in the airport')
+    end
+
+    it "Remove plane should remove plane" do
+      expect(@airport).to respond_to :take_off
+      @airport.land_plane(@plane)
+      @airport.take_off(@plane)
+      expect(@airport.planes).to_not include(@plane)
+    end
+
+    it "Update log should update log" do
+      @airport.land_plane(@plane)
+      @airport.take_off(@plane)
+      expect(@airport.show_log).to eq ["Plane #{@plane} landed","Plane #{@plane} took off"]
+    end
   end
 
-  it "Should not allow take off of landing during bad weather" do
-    #Forcing :stormy to return true
-    allow(@airport).to receive_messages(:stormy? => true)
-    expect{@airport.land_plane(@plane)}.to raise_error("Cannot land due to bad weather")
-    expect{@airport.take_off(@plane)}.to raise_error('Cannot take off due to bad weather')
+  describe "Weather tests" do
+    it "Should not allow take off of landing during bad weather" do
+      #Forcing :stormy to return true
+      allow(@airport).to receive_messages(:stormy? => true)
+      expect{@airport.land_plane(@plane)}.to raise_error("Cannot land due to bad weather")
+      expect{@airport.take_off(@plane)}.to raise_error('Cannot take off due to bad weather')
+    end
   end
 
-  it "Airport should have definable capacity" do
-    #Testing default capacity
-    capacity_airport = Airport.new
-    expect(capacity_airport).to have_attributes(:capacity => 10)
+  describe "Capacity tests" do
+    it "Airport should have a default capacity" do
+      capacity_airport = Airport.new
+      expect(capacity_airport).to have_attributes(:capacity => 10)
+    end
+    it "Airport should have a definable capacity" do
+      capacity_airport = Airport.new(15)
+      expect(capacity_airport).to have_attributes(:capacity => 15)
+    end
 
-    #Testing definable capacity
-    capacity_airport = Airport.new(15)
-    expect(capacity_airport).to have_attributes(:capacity => 15)
+    it "Planes should not be able to land if capacity is full" do
+      10.times do @airport.land_plane(Plane.new) end
+      expect{@airport.land_plane(@plane)}.to raise_error('Cannot land due to no capacity')
+    end
+
+    it "Airport should stand up to rush hour" do
+      expect{10.times do @airport.land_plane(Plane.new) end}.not_to raise_error
+      end
   end
-
-  it "Planes should not be able to land if capacity is full" do
-    10.times do @airport.land_plane(Plane.new) end
-    expect{@airport.land_plane(@plane)}.to raise_error('Cannot land due to no capacity')
-  end
-
-  it "Planes should not be able to take off if they are not in the airport" do
-    expect{@airport.take_off(@plane)}.to raise_error('Plane is not in the airport')
-  end
-
-  it "Planes should not be able to land twice" do
-    expect{@airport.take_off(@plane)}.to raise_error('Plane is not in the airport')
-    @airport.land_plane(@plane)
-    expect{@airport.land_plane(@plane)}.to raise_error('Plane has already landed')
-  end
-
-it "Airport should stand up to rush hour" do
-  expect{10.times do @airport.land_plane(Plane.new) end}.not_to raise_error
-end
-
 end
