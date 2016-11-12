33f991dcac7cefd43f463fc60449987d44b53303
Chris Cooper <cjcooper252@gmail.com>
2016-11-12 13:37:27 +0000

    airport land and takes off

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,4 +1,18 @@
+require_relative 'plane.rb'
+
 class Airport
+
+  attr_reader :plane
+
+  def land(plane)
+    fail "Airport is full" if @plane
+    @plane = plane
+  end
+
+  def take_off(plane)
+    fail "Plane is not in airport" unless @plane
+    @plane
+  end
 
 
 
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,8 +2,45 @@ require 'airport.rb'
 
 describe Airport do
 
-  it { is_expected.to respond_to :take_off }
+  it { is_expected.to respond_to(:take_off).with(1).argument }
 
   it { is_expected.to respond_to(:land).with(1).argument }
+
+  it { is_expected.to respond_to(:plane) }
+
+  it 'returns planes which have landed' do
+    plane = Plane.new
+    subject.land(plane)
+    expect(subject.plane).to eq plane
+  end
+
+  describe '#land' do
+
+    it 'lands a plane' do
+      plane = Plane.new
+      expect(subject.land(plane)).to eq plane
+    end
+
+    it 'raises an error if airport is full' do
+      subject.land(Plane.new)
+      expect {subject.land(Plane.new)}.to raise_error 'Airport is full'
+    end
+
+  end
+
+  describe '#take_off' do
+
+    it 'lets a plane take off' do
+      plane = Plane.new
+      subject.land(plane)
+      expect(subject.take_off(plane)).to eq plane
+    end
+
+    it 'raises an error if plane is not in airport' do
+      plane = Plane.new
+      expect {subject.take_off(plane)}.to raise_error 'Plane is not in airport'
+    end
+
+  end
 
 end
