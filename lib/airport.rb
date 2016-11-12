cd868625f9365f13e3af575f74e7ca634be9c1fd
pflarcology <rhrg500@gmail.com>
2016-11-12 19:48:18 +0000

    implemented weather method

--- lib/airport.rb
+++ lib/airport.rb
@@ -6,6 +6,7 @@ class Airport
 
   def initialize
     @planes = []
+    @stormy = false
   end
 
   def land(plane)
@@ -14,10 +15,16 @@ class Airport
   end
 
   def take_off
+    raise "Due to inclement weather no planes can take off" if @stormy
     @planes.pop
   end
 
-  def is_it_stormy?
+  def is_it_stormy?(number)
+    @stormy = number > 4? true : false
+  end
+
+  def number_generator
+    rand(1..5)
   end
 
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,12 +1,11 @@
 require 'airport'
-require 'plane'
 
 describe Airport do
+  let(:plane) { double :plane }
 
   it { is_expected.to respond_to :land}
 
   it 'is expected to land a plane' do
-    plane = Plane.new
     airport = Airport.new
     expect(airport.land(plane)).to eq "Flight #{plane} has landed."
   end
@@ -14,7 +13,6 @@ describe Airport do
   it { is_expected.to respond_to :planes}
 
   it 'supposed to store a plane' do
-    plane = Plane.new
     airport = Airport.new
     airport.land(plane)
     expect(airport.planes).to eq [plane]
@@ -24,19 +22,49 @@ describe Airport do
 
   it 'is expected to allow a plane to take off' do
     plane = Plane.new
-    airport = Airport.new
-    airport.land(plane)
-    expect(airport.take_off).to eq plane
+    subject.land(plane)
+    subject.is_it_stormy?(4)
+    expect(subject.take_off).to eq plane
   end
 
   it 'is expected to show that the plane is no longer in the airport' do
+    weather = Weather.new
     plane = Plane.new
-    airport = Airport.new
-    airport.land(plane)
-    airport.take_off
-    expect(airport.planes).to eq []
+    subject.land(plane)
+    subject.take_off
+    expect(subject.planes).to eq []
   end
 
   it { is_expected.to respond_to :is_it_stormy?}
+
+  it 'is expected to prevent planes from taking off' do
+    subject.land(plane)
+    subject.is_it_stormy?(5)
+    expect{ subject.take_off }.to raise_error(RuntimeError, 'Due to inclement weather no planes can take off')
+  end
+
+  it 'expected to return true if number is 5' do
+    weather = Weather.new
+    plane = Plane.new
+    expect(subject.is_it_stormy?(5)).to eq true
+  end
+
+  it 'expect to return true if number is 4' do
+    weather = Weather.new
+    plane = Plane.new
+    expect(subject.is_it_stormy?(4)).to eq false
+  end
+
+  it 'should return a random value' do
+    uses = []
+    100.times do
+      uses << subject.number_generator
+    end
+    expect(uses).to include(1, 2, 3, 4, 5)
+    expect(uses).not_to include(0, 6)
+  end
+
+
+
 
 end
