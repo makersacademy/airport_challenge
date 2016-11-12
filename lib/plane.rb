ffa3a0e775b566c69e7cfb2637511738838eeeed
DarkChocLatte <darkchoclatte@gmail.com>
2016-11-12 12:51:34 +0000

    Plane now knows that it is flying when it takes off

--- lib/plane.rb
+++ lib/plane.rb
@@ -6,9 +6,15 @@ attr_reader :flying
 
 
   def land_at(airport)
-    puts "plane has now landed at #{airport}"
+    puts "plane has now landed at the airport"
     @flying = false
     return airport.store_plane(self)
+  end
+
+  def take_off_from(airport)
+    puts "The plane has now taken to the air"
+    @flying = true
+    return  airport.landing_bay.delete(self) #delete method deletes froma array and returns
   end
 
 
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -8,17 +8,31 @@ require './lib/plane.rb'
      it "expects plane to respond to land_at method" do
        expect(subject).to respond_to(:land_at).with(1).argument
      end
+
+     it "expects plane to respond to take_off method" do
+       expect(subject).to respond_to(:take_off_from).with(1).argument
+     end
    end
 
    context "expects plane land_at method will land the plane at the airport" do
 
-     it "expects a flying to return false when subject has landed" do
+     it "expects flying to return false when subject has landed" do
 
        heathrow = Airport.new
        subject.land_at(heathrow)
 
        expect(subject.flying).to eq false
      end
+   end
+
+   context "expects take_off_from method will fly the plane " do
+
+       it "subject should be flying if it has taken off" do
+         heathrow = Airport.new
+         subject.take_off_from(heathrow)
+
+         expect(subject.flying).to eq true
+       end
    end
 
  end
