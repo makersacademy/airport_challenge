0393cc9fe6acfd06f2945ee9d45b5342d61e5957
Chris Cooper <cjcooper252@gmail.com>
2016-11-12 14:57:52 +0000

    airport private methods

--- lib/airport.rb
+++ lib/airport.rb
@@ -9,18 +9,26 @@ class Airport
   end
 
   def land(plane)
-    fail "Airport is full" if @planes.count >= 50
+    fail "Airport is full" if full?
     plane.land
     @planes << plane
   end
 
   def take_off(plane)
-    fail "Plane is not in airport" unless @planes.include?(plane)
+    fail "Plane is not in airport" unless in_airport?(plane)
     plane.take_off
     @planes.delete(plane)
     plane
   end
 
+  private
 
+  def full?
+    @planes.count >= 50
+  end
+
+  def in_airport?(plane)
+    @planes.include?(plane)
+  end
 
 end
