4980de642c9b4f30bb95e49ecc283170eea32f22
Pea M. Tyczynska <CrystalPea@github.com>
2016-11-12 19:05:45 +0000

    Grouped unit tests by method

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,47 +2,53 @@ require 'airport'
 describe Airport do
 
   it { is_expected.to be_instance_of(Airport) }
+  describe "#land" do
+    it { is_expected.to respond_to(:land).with(1).argument }
 
-  it { is_expected.to respond_to(:land).with(1).argument }
+    it "tells a plane to land" do
+      plane = Plane.new
+      weather = "good"
+      subject.land(plane)
+      expect(plane).to eq subject.planes.last
+    end
 
-  it "tells a plane to land" do
-    plane = Plane.new
-    weather = "good"
-    subject.land(plane)
-    expect(plane).to eq subject.planes.last
+    it "doesn't allow plane to land if weather stormy" do
+      plane = Plane.new
+      subject.weather = "stormy"
+      expect { subject.land(plane) }.to raise_error("Landing impossible due to stormy weather")
+    end
   end
 
-  it "confirms that a plane is landed there/has not taken off" do
-    plane = Plane.new
-    weather = "good"
-    subject.land(plane)
-    expect(subject.on_airport?(plane)).to eq true
+  describe "#on_airport?" do
+    it "confirms that a plane is landed there/has not taken off" do
+      plane = Plane.new
+      weather = "good"
+      subject.land(plane)
+      expect(subject.on_airport?(plane)).to eq true
+    end
+
+    it "informs if the plane is not landed at this airport/has taken off" do
+      expect(subject.on_airport?(Plane.new)).to eq false
+    end
   end
 
-  it "informs if the plane is not landed at this airport/has taken off" do
-    expect(subject.on_airport?(Plane.new)).to eq false
+  describe "#take_off" do
+    it { is_expected.to respond_to(:take_off).with(1).argument }
+
+    it "instructs a plane to take_off" do
+      plane = Plane.new
+      subject.weather = "good"
+      subject.land(plane)
+      subject.take_off(plane)
+      expect(subject.planes).not_to include plane
+    end
+
+    it "doesn't allow plane to take off if weather stormy" do
+      plane = Plane.new
+      subject.land(plane)
+      subject.weather = "stormy"
+      expect { subject.take_off(plane) }.to raise_error("Take-off impossible due to stormy weather")
+    end
   end
 
-  it { is_expected.to respond_to(:take_off).with(1).argument }
-
-  it "instructs a plane to take_off" do
-    plane = Plane.new
-    subject.weather = "good"
-    subject.land(plane)
-    subject.take_off(plane)
-    expect(subject.planes).not_to include plane
-  end
-
-  it "doesn't allow plane to take off if weather stormy" do
-    plane = Plane.new
-    subject.land(plane)
-    subject.weather = "stormy"
-    expect { subject.take_off(plane) }.to raise_error("Take-off impossible due to stormy weather")
-  end
-
-  it "doesn't allow plane to land if weather stormy" do
-    plane = Plane.new
-    subject.weather = "stormy"
-    expect { subject.land(plane) }.to raise_error("Landing impossible due to stormy weather")
-  end
 end
