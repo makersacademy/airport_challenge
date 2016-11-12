8c79703d58197c81fb3a6e4ac64e221bb6b81025
Russell <rusyeo@gmail.com>
2016-11-12 15:35:52 +0000

    land_plane returns the landed plane

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,13 +1,18 @@
 require_relative 'plane'
 
+
 class Airport
 
+  attr_reader :plane
+
   def land_plane(plane)
-    true
+    @plane = plane
   end
 
   def landed?(plane)
     true
   end
+
+
 
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,24 +2,33 @@ require 'airport'
 
 describe Airport do
 
-  it "responds to land_plane" do
-    expect(subject).to respond_to :land_plane
+  describe "#land_plane" do
+    it "responds to land_plane" do
+      expect(subject).to respond_to :land_plane
+    end
+    it "allows controller to land a plane" do
+      plane = Plane.new
+      expect(subject.land_plane(plane))
+    end
+    it "returns the landed plane" do
+      plane = Plane.new
+      expect(subject.land_plane(plane)).to eq plane
+    end
   end
 
-  it "allows controller to land a plane" do
-    plane = Plane.new
-    expect(subject.land_plane(plane))
+  describe "#landed?" do
+    it "responds to landed?" do
+      expect(subject).to respond_to :landed?
+    end
+    it "returns if a plane has landed" do
+      plane = Plane.new
+      expect(subject.landed?(plane)).to eq true
+    end
   end
 
-  it "responds to landed?" do
-    expect(subject).to respond_to :landed?
-  end
 
-  it "returns if a plane has landed" do
-    plane = Plane.new
-    expect(subject.landed?(plane)).to eq true
-  end
 
-  
+
+
 
 end
