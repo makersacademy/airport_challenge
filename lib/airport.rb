940671bc965a05dd0993098177cae3c24c88d36f
Russell <rusyeo@gmail.com>
2016-11-12 13:28:36 +0000

    airport responds to land_plane

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,4 +1,8 @@
 require_relative 'plane'
 
 class Airport
+
+  def land_plane
+  end
+  
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,4 +2,8 @@ require 'airport'
 
 describe Airport do
 
+  it "responds to land_plane" do
+    expect(subject).to respond_to :land_plane
+  end
+
 end
