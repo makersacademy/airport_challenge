f302862ad51bb201ad6832cee58b53564f2d89a3
Dan Pelensky <dan@pelensky.com>
2016-11-12 13:10:40 +0000

    all tests passing

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,18 +1,28 @@
 class Airport
+  #SETUP
+  attr_reader :airport_capacity, :airplanes_on_ground
+  AIRCRAFT_HANGERS = 10
 
-  #TAKE OFF
-  def take_off
-
+  def initialize(capacity=10)
+    @airport_capacity = capacity
+    @airplanes_on_ground = []
   end
 
   #LANDING
 
-  def land_at_airport
+  def land_at_airport(plane)
 
   end
 
   def landed?
 
   end
+
+
+  #TAKE OFF
+  def take_off
+
+  end
+
 
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -3,32 +3,46 @@ require "airport"
 describe Airport do
 
   context "Setup" do
-    it "expects an Airport class to exist" do
+    it "expects an airport class to exist" do
       expect(subject.class).to eq(Airport)
     end
-  end
 
-
-  context "Take Off" do
-    it "expects Airport to respond to a take_off method" do
-      expect(subject).to respond_to(:take_off)
+    it "expects airport to only allow 10 airplanes to land by default" do
+      expect(subject.airport_capacity).to eq Airport::AIRCRAFT_HANGERS
     end
 
+    it "expects airport to allow as many airplanes to land as the system maintainer sets" do
+      airport = Airport.new(40)
+      expect(airport.airport_capacity).to eq(40)
   end
+
+  it "expects no airplanes to be at the airport by default" do
+    expect(subject.airplanes_on_ground).to be_empty
+  end
+end
+
 
   context "Landing" do
 
-    it "expects Airport to respond to a land_at_airport method" do
-      expect(subject).to respond_to(:land_at_airport)
+    it "expects airport to respond to a land_at_airport method and pass in a plane" do
+      expect(subject).to respond_to(:land_at_airport).with(1).argument
     end
 
-    it "expects Airport to respond to a landed? method" do
+    it "expects airport to respond to a landed? method" do
       expect(subject).to respond_to(:landed?)
     end
 
     # it "expects landed? to be true if the plane has landed" do
     #   expect(subject).to be_landed
     # end
+
+  end
+
+  context "Take Off" do
+
+    it "expects airport to respond to a take_off method" do
+      expect(subject).to respond_to(:take_off)
+    end
 
   end
 end
