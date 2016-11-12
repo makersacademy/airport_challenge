92a5c77b92839771cdeef65b77e0915d03c008bc
Mike Hurl <mikehurl@gmail.com>
2016-11-12 19:02:23 +0000

    seventh test passed

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -10,4 +10,8 @@ describe Airport do
     expect(subject.prevent_landing).to eq true
   end
 
+  it "should prevent planes from landing there when it is at full capacity" do
+    (subject.prevent_landing).should eq(subject.full_capacity?)
+  end
+
 end
