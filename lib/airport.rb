b0ea8f6de7875389de2a2d6789cffa90de81d973
Mike Hurl <mikehurl@gmail.com>
2016-11-12 20:41:01 +0000

    eighth test passed and airport_spec and airport.rb refactored

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,11 +1,16 @@
 class Airport
+  attr_accessor :full_capacity
 
-  def full_capacity?
-    true
+  def initialize
+    @full_capacity = 20
   end
 
-  def prevent_landing
-    true
+  def prevent_landing(capacity)
+    if capacity > @full_capacity
+      true
+    else
+      false
+    end
   end
 
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,16 +2,12 @@ require "airport"
 
 describe Airport do
 
-  it "should know if it is at full capacity" do
-    expect(subject.full_capacity?).to eq true
-  end
-
-  it "should be able to prevent planes landing there" do
-    expect(subject.prevent_landing).to eq true
-  end
-
   it "should prevent planes from landing there when it is at full capacity" do
-    (subject.prevent_landing).should eq(subject.full_capacity?)
+    expect(subject.prevent_landing(21)).to eq true
+  end
+
+  it "should allow planes to land when it is not at full capacity" do
+    expect(subject.prevent_landing(19)).to eq false
   end
 
 end
