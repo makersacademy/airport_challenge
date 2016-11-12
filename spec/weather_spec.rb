4f7f4a50b9766cb91fc412e4ffa0d49ae8f7632b
George Seeger <georgeseeger@hotmail.com>
2016-11-12 09:08:01 +0000

    Weather object added and passing all tests

---
+++ spec/weather_spec.rb
@@ -0,0 +1,17 @@
+require 'weather'
+
+describe Weather do
+  it "should respond to condiiton" do
+    expect(subject).to respond_to(:condition)
+  end
+
+  it "should be either Sunny or Stormy" do
+    expect(["Sunny", "Stormy"]).to include subject.condition
+  end
+
+  it "should be either kinds of weather at random" do
+    weathers = Array.new(100){Weather.new.condition}
+    expect(weathers).to include "Stormy"
+    expect(weathers).to include "Sunny"
+  end
+end
