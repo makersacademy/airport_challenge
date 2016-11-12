c438f3f8ca0eef7e4baf1701549b792334e8c4cf
DarkChocLatte <darkchoclatte@gmail.com>
2016-11-12 12:00:55 +0000

    feature -Plane knows that it is not flying when it lands

--- lib/plane.rb
+++ lib/plane.rb
@@ -2,9 +2,11 @@ require_relative 'airport'
 
 class Plane
 
+  attr_reader :flying
+
   def land_at(airport)
     puts "plane has now landed at #{airport}"
-
+    @flying = false
 
   end
 
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -2,13 +2,32 @@ require './lib/plane.rb'
 
  describe Plane do
 
+   let(:plane) {double :plane}
+   let(:airport) {double :airport}
+
    context "Expects that plane will respond to plane methods" do
 
      it "expects plane to respond to land_at method" do
        expect(subject).to respond_to(:land_at).with(1).argument
      end
 
-   end 
+     it "expects plane to respond to flying" do
+       expect(subject).to respond_to(:flying)
+     end
+
+   end
+
+   context "Expects that plane will change to different states when methods are called" do
+
+     it "expects flying to equal false when the plane lands" do
+       subject.land_at(:airport)
+       expect(subject.flying).to eq false
+     end
+   end
+
+ end
+
+
 
 
 
@@ -19,14 +38,3 @@ require './lib/plane.rb'
     #  it "expects plane to respond to take_off method" do
     #    expect(subject).to respond_to(:take_off)
     #  end
-
-
-
-
-
-
-
-
-
-
-end
