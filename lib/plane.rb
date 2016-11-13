9d72fe554fcc7c781568bd60ac0b6084db4a9b61
unicornelia <ez.kornelia@gmail.com>
2016-11-13 14:51:25 +0000

    Created plane object, first tests for  checking if plane responds to landed? and it the method can be called on airport

--- lib/plane.rb
+++ lib/plane.rb
@@ -4,4 +4,6 @@ def initialize
   @status
 end
 
+
+
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,7 +2,11 @@ require "airport"
 
 describe Airport do
   plane = Plane.new
-  it 'instructs planes to land' do
+  it 'Checks if airport responds to landing message' do
+    expect(subject).to respond_to(:landed?)
+  end
+
+  it 'Checks if plane has landed?' do
     expect(subject.landed?(plane)).to eq true
   end
 
