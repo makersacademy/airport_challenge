2fd700dc27d8e7a69dbb4b3eba4258b6f36d55a4
unicornelia <ez.kornelia@gmail.com>
2016-11-13 20:34:07 +0000

    Airport status updated with method"
    q
    exit

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -13,7 +13,19 @@ describe Airport do
     it 'Returns true if the airport is empty' do
       expect(subject.is_empty?).to eq true
     end
+
+    it 'Returns false if the airport is not empty' do
+      subject.receive_plane(Plane.new)
+      expect(subject.is_empty?).to eq false
+    end
   end
+  # 
+  # describe 'inventory' do
+  #   it 'Returns how many planes are at the airport' do
+  #     plane = Plane.new
+  #     expect(subject.planes_at_airport).to eq 0
+  #   end
+  # end
 
 
 
