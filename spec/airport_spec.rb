bc0ae0d7f15e875b4fb6f19c0e4f414039b789c8
unicornelia <ez.kornelia@gmail.com>
2016-11-13 16:17:59 +0000

    Changed tests, added plane spec and realized that landing is the feature of plane.

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,13 +1,4 @@
 require "airport"
 
 describe Airport do
-  plane = Plane.new
-  it 'Checks if airport responds to landing message' do
-    expect(subject).to respond_to(:landed?)
-  end
-
-  it 'Checks if plane has landed?' do
-    expect(subject.landed?(plane)).to eq true
-  end
-
 end
---
+++ spec/plane_spec.rb
@@ -0,0 +1,8 @@
+require 'plane'
+
+describe Plane do
+
+  describe '#landed?' do
+    it { is_expected.to respond_to 'landed?' }
+  end
+end
