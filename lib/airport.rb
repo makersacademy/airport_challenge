ea3626c2dc8336ec00b663d735f152febf1c8e25
TudorTacal <tudortacal@gmail.com>
2016-11-13 11:37:21 +0000

    Tested if the airport keeps track on landed planes and passed the test

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,4 +1,14 @@
+require_relative "plane.rb"
+
 class Airport
+  attr_reader :planes
+
+  def initialize
+    @planes = []
+  end
+
   def land(plane)
+    @planes << plane
+    true
   end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,8 +1,25 @@
 require  "airport"
+require  "plane"
 
 describe Airport do
-  it {is_expected.to be_a(Object)}
-  it {is_expected.to respond_to(:land)}
-  it {is_expected.to respond_to(:land).with(1).argument}
+  context "when created it" do
+    it {is_expected.to be_a(Object)}
+    it {is_expected.to respond_to(:land)}
+    it {is_expected.to respond_to(:land).with(1).argument}
+  end
+  describe "when landing a plane" do
+      before(:each) do
+        @plane = Plane.new
+        @airport = Airport.new
+        @airport.land(@plane)
+      end
+      it "should have a 'planes' getter method" do
+        expect(@airport).to respond_to(:planes)
+      end
+      it "should keep track of the planes" do
+        expect(@airport.planes.count).to be 1
+      end
+  end
+
 
 end
