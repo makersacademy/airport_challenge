6e9ca3b3db1d0a0fc6c7c683587f0dcaa7ab5389
Kim Wilson <k.wilson541@gmail.com>
2016-11-13 00:10:29 +0000

    Refactored weather tests

--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -5,12 +5,12 @@ describe Weather do
 	let(:airport) { (Class.new {include Weather}).new }
 	
 	it 'expects stormy? to be true when given random number 0' do
-		allow(airport).to receive(:random_number) { 0 }
+		srand(2) # seed means next call to rand will return 0
 		expect(airport.stormy?).to eq true
 	end
 	
-	it 'expects stormy? to be false when given random number 1' do
-		allow(airport).to receive(:random_number) { 1 }
+	it 'expects stormy? to be false when given random number 3' do
+		srand(1) #seed means next call to rand will return 3
 		expect(airport.stormy?).to eq false
 	end
 
