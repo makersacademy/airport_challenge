3e9a318a2bcd51d46d28053bed1392585791a7aa
Russell <rusyeo@gmail.com>
2016-11-13 17:44:35 +0000

    stormy responds true

--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -2,6 +2,13 @@ require 'weather'
 
 describe Weather do
 
-  it { is_expected.to respond_to(:stormy?) }
+
+
+  describe "outputting the weather" do
+    it { is_expected.to respond_to(:stormy?) }
+    it "returns true" do
+      expect(subject.stormy?).to be true
+    end
+  end
 
 end
