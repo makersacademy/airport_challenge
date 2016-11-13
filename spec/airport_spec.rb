8afcd99be638ee20fc7efe97d42cbf8e9cd9b5a8
unicornelia <ez.kornelia@gmail.com>
2016-11-13 19:54:46 +0000

    Method to check whether airport is empty

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -3,20 +3,18 @@ require "airport"
 describe Airport do
 
   describe '#take_off' do
-    plane = Plane.new
     it 'Checks whether plane has taken off and left airport' do
+      plane = Plane.new
       expect(subject.take_off(plane)).to eq true
     end
   end
 
-  describe '#inventory' do
-    it 'Shows the landed plane at the airport' do
-      @plane = Plane.new
-      @plane2 = Plane.new
-      subject.inventory(@plane)
-      subject.inventory(@plane2)
-      expect(subject.planes.push).to include(@plane, @plane2)
+  describe '#empty?' do
+    it 'Returns true if the airport is empty' do
+      expect(subject.is_empty?).to eq true
     end
   end
+
+
 
 end
