e44c64c8b6ba0bceea40783f904510b4ac48523d
Teako Warfield-Graham <teakowg@gmail.com>
2016-11-13 15:35:26 +0000

    Testing out plane confirmations

--- lib/airport_challenge.rb
+++ lib/airport_challenge.rb
@@ -1,15 +1,26 @@
 
 class Airport
 
-  def land_plane
+  def initialize
+    @confirm = true
+
+
+  def land_plane(plane)
     puts "This plane has landed"
+    @confirm = false
   end
 
   def take_off(plane)
     puts "All clear for take off!"
+    @confirm
+  end
+
+  def confirm(plane)
+    true
   end
 
 end
 
 class Plane
+
 end
--- spec/airport_challenge_spec.rb
+++ spec/airport_challenge_spec.rb
@@ -5,10 +5,22 @@ describe Airport do
     expect(airport).to respond_to(:land_plane)
   end
 
+  it "Can confirm a plane has landed" do
+    plane = Plane.new
+    airport.land_plane(plane)
+    expect(airport.take_off(plane)).to eq false
+  end
+
   it "Can give a command for planes to take off" do
     plane = Plane.new
     airport.take_off(plane)
-    expect(airport).to respond_to(:take_off).with(1).arguement
+    expect(airport).to respond_to(:take_off).with(1).argument
+  end
+
+  it "Can talk to the plane to confirm it's taken off" do
+    plane = Plane.new
+    airport.take_off(plane)
+    expect(airport.confirm?(plane)).to eq true
   end
 
 end
