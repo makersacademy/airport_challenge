9159763d6f95bcb1bfb8e1256a6fb987c2da9322
Russell <rusyeo@gmail.com>
2016-11-13 11:17:44 +0000

    explicit subject in rspec file

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -5,63 +5,63 @@ describe Airport do
 
   context "#land_plane" do
     it "responds to #land_plane" do
-      expect(subject).to respond_to :land_plane
+      expect(airport).to respond_to :land_plane
     end
     it "returns a landed plane" do
       plane = Plane.new
-      expect(subject.land_plane(plane)).to eq plane
+      expect(airport.land_plane(plane)).to eq plane
     end
   end
 
   context "#planes" do
     it "responds to #planes" do
-      expect(subject).to respond_to :planes
+      expect(airport).to respond_to :planes
     end
     it "saves one plane that is landed" do
       plane = Plane.new
-      subject.land_plane(plane)
-      expect(subject.planes).to eq [plane]
+      airport.land_plane(plane)
+      expect(airport.planes).to eq [plane]
     end
     it "can store more than one plane" do
       plane1 = Plane.new
-      subject.land_plane(plane1)
+      airport.land_plane(plane1)
       plane2 = Plane.new
-      subject.land_plane(plane2)
-      expect(subject.planes).to eq [plane1, plane2]
+      airport.land_plane(plane2)
+      expect(airport.planes).to eq [plane1, plane2]
     end
   end
 
   context "#in_airport?" do
     it "responds to #in_airport?" do
-      expect(subject).to respond_to :in_airport?
+      expect(airport).to respond_to :in_airport?
     end
     it "returns false if plane is not in the airport" do
       plane = Plane.new
-      expect(subject.in_airport?(plane)).not_to be true
+      expect(airport.in_airport?(plane)).not_to be true
     end
     it "returns true if plane is in the airport" do
       plane = Plane.new
-      subject.land_plane(plane)
-      expect(subject.in_airport?(plane)).to be true
+      airport.land_plane(plane)
+      expect(airport.in_airport?(plane)).to be true
     end
   end
 
   context "#takeoff" do
     it "responds to #takeoff" do
-      expect(subject).to respond_to :takeoff
+      expect(airport).to respond_to :takeoff
     end
     it "instructs a plane to takeoff" do
       plane = Plane.new
-      expect(subject.takeoff(plane)).to eq plane
+      expect(airport.takeoff(plane)).to eq plane
     end
     it "removes plane from airport after takeoff" do
       plane = Plane.new
-      subject.land_plane(plane)
+      airport.land_plane(plane)
       puts "planes"
-      puts subject.planes
-      subject.takeoff(plane)
-      puts subject.planes
-      expect(subject.planes).not_to include(plane)
+      puts airport.planes
+      airport.takeoff(plane)
+      puts airport.planes
+      expect(airport.planes).not_to include(plane)
     end
   end
 
