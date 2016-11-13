527205bdcba3d77fe9ebaa8610af0d89380ce4a1
unicornelia <ez.kornelia@gmail.com>
2016-11-13 17:54:54 +0000

    Shows airport inventory, with plane status, at the airport, aka landed or not there, aka flying

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -8,4 +8,15 @@ describe Airport do
       expect(subject.take_off(plane)).to eq true
     end
   end
+
+  describe '#inventory' do
+    it 'Shows the landed plane at the airport' do
+      @plane = Plane.new
+      @plane2 = Plane.new
+      subject.inventory(@plane)
+      subject.inventory(@plane2)
+      expect(subject.planes.push).to include(@plane, @plane2)
+    end
+  end
+
 end
