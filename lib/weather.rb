27848248ad35163726c6363e8deafa1014d1f60d
Louisa <louisaspicer@gmail.com>
2016-11-13 12:19:08 +0000

    randomised todays weather with 4 options

--- lib/weather.rb
+++ lib/weather.rb
@@ -1,10 +1,19 @@
 class Weather
 
-attr_accessor :today
+attr_accessor :today, :weathers
 
-def initialize
-  @today = "sunny"
-end
+  def initialize
+    @today = "sunny"
+    @weathers = ["Sunny", "Stormy", "Sunny", "Sunny"]
+  end
+
+  def randomise
+    rand(4)
+  end
+
+  def todays_weather
+    @weathers[randomise]
+  end
 
 
 end
