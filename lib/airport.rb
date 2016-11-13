2647258673202204191fcecf5c25a4abebef5332
Asuka Ochi <asukaochi@gmail.com>
2016-11-13 09:49:12 +0000

    Refactoring

--- lib/airport.rb
+++ lib/airport.rb
@@ -13,7 +13,7 @@ class Airport
 
   def land(plane)
     fail "Due to bad weather, aircraft are unable to land." if check_weather == "stormy"
-    fail "Airport at full capacity, no space available." if @plane.size == @capacity
+    fail "Airport at full capacity, no space available." if full?
     @plane.include?(plane) ? false : @plane << plane
   end
 
@@ -31,6 +31,12 @@ class Airport
 
   def check_weather
     Weather.new.today
+  end
+
+  private
+
+  def full?
+    @plane.size >= @capacity
   end
 
 end
