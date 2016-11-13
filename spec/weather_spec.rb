2f15873e21400e25ec7cba28e20c05d937b69d5f
Russell <rusyeo@gmail.com>
2016-11-13 18:34:23 +0000

    randomized #stormy? returns false

--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -1,25 +1,24 @@
 require 'weather'
 
 describe Weather do
-
   describe "outputting the weather" do
-
     it "returns stormy" do
       allow(subject).to receive(:randomize).and_return(:stormy)
       expect(subject.stormy?).to be true
     end
-
+    it "returns clear" do
+      allow(subject).to receive(:randomize).and_return(:clear)
+      expect(subject.stormy?).not_to be true
+    end
   end
-
   describe "randomizing the weather" do
-    it "outputs stormy" do
+    it "outputs :stormy" do
       allow(subject).to receive(:randomize).and_return(:stormy)
       expect(subject.randomize).to be :stormy
     end
-    it "outputs clear" do
+    it "outputs :clear" do
       allow(subject).to receive(:randomize).and_return(:clear)
       expect(subject.randomize).to be :clear
     end
   end
-
 end
