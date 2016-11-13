9287b1000f100151e233545e2b6105b451c99511
pflarcology <rhrg500@gmail.com>
2016-11-13 16:51:37 +0000

    added more tests for stormy

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -6,23 +6,20 @@ describe Airport do
   it { is_expected.to respond_to :land}
 
   it 'is expected to land a plane' do
-    airport = Airport.new
-    expect(airport.land(plane)).to eq "Flight #{plane} has landed."
+    expect(subject.land(plane)).to eq "Flight #{plane} has landed."
   end
 
   it { is_expected.to respond_to :planes}
 
   it 'supposed to store a plane' do
-    airport = Airport.new
-    airport.land(plane)
-    expect(airport.planes).to eq [plane]
+    subject.land(plane)
+    expect(subject.planes).to eq [plane]
   end
 
   it { is_expected.to respond_to :take_off}
 
   it 'is expected to allow a plane to take off' do
     subject.land(plane)
-    subject.is_it_stormy?(4)
     expect(subject.take_off).to eq plane
   end
 
@@ -49,7 +46,10 @@ describe Airport do
     expect(subject.is_it_stormy?(5)).to eq true
   end
 
-  it 'expect to return true if number is 4' do
+  it 'expect to return true if number is between 1 and 4' do
+    expect(subject.is_it_stormy?(1)).to eq false
+    expect(subject.is_it_stormy?(2)).to eq false
+    expect(subject.is_it_stormy?(3)).to eq false
     expect(subject.is_it_stormy?(4)).to eq false
   end
 
@@ -83,8 +83,5 @@ describe Airport do
   it 'check that capacity is equal to DEFAULT_CAPACITY when nothing else set' do
     expect(Airport::DEFAULT_CAPACITY).to eq Airport::DEFAULT_CAPACITY
   end
-
-
-
 
 end
