dde27dcbbc820d52a7934ba8553d79da68b7dad4
unicornelia <ez.kornelia@gmail.com>
2016-11-13 17:05:33 +0000

    Plane has initial status of being landed, this status is false when not landed  and test checks these

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -5,4 +5,15 @@ describe Plane do
   describe '#landed?' do
     it { is_expected.to respond_to 'landed?' }
   end
+
+  it 'returns true when plane has landed' do
+    subject.land=(true)
+    expect(subject.landed?).to eq true
+  end
+
+  it 'returns false when plane has not landed' do
+    subject.land=(false)
+    expect(subject.landed?).to eq false
+  end
+
 end
