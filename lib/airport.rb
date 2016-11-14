5af89a2e6527df4315abc7fa90037e30f4813677
sara <sara@huhbub.com>
2016-11-14 14:51:45 +0000

    I want to instruct a plane to take off from an airport and confirm that it is no longer in the airport

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,10 +1,18 @@
 class Airport
 
-  def land(plane)
+  def instructs_landing(plane)
     true
   end
 
   def confirms_landing(plane)
+    true
+  end
+
+  def instructs_takeoff(plane)
+    true
+  end
+
+  def confirms_takeoff(plane)
     true
   end
 
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -10,7 +10,7 @@ describe Airport do
   it 'instructs a plane to land' do
     airport = Airport.new
     plane = Plane.new
-    expect(airport.land(plane)).to eq true
+    expect(airport.instructs_landing(plane)).to eq true
     end
 
   it 'confirms a plane has landed' do
@@ -19,23 +19,41 @@ describe Airport do
     expect(airport.confirms_landing(plane)).to eq true
     end
 
-
   # As an air traffic controller
   # So I can get passengers on the way to their destination
   # I want to instruct a plane to take off from an airport and confirm that it is no longer in the airport
-  #
+
+  it 'instructs a plane to take off' do
+    airport = Airport.new
+    plane = Plane.new
+    expect(airport.instructs_takeoff(plane)).to eq true
+    end
+
+  it 'confirms that a plane has taken off' do
+    airport = Airport.new
+    plane = Plane.new
+    expect(airport.confirms_takeoff(plane)).to eq true
+    end
+
+
   # As an air traffic controller
   # To ensure safety
   # I want to prevent takeoff when weather is stormy
-  #
+
+
+
   # As an air traffic controller
   # To ensure safety
   # I want to prevent landing when weather is stormy
-  #
+
+
+
   # As an air traffic controller
   # To ensure safety
   # I want to prevent landing when the airport is full
-  #
+
+
+
   # As the system designer
   # So that the software can be used for many different airports
   # I would like a default airport capacity that can be overridden as appropriate
