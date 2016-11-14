dd8063261e3292a3713937cd2bf2cd9163be3929
Jenna Ramdenee <jenna_ramdenee@hotmail.co.uk>
2016-11-14 18:24:44 +0000

    Used Kernel in weather clas

--- lib/weather.rb
+++ lib/weather.rb
@@ -2,12 +2,8 @@ class Weather
 
   attr_accessor :weather_code
 
-  def initialize
-    @weather_code = rand
-  end
-
   def stormy?
-    @weather_code < 0.3
+    Kernel.rand < 0.3
   end
 
 end
--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -3,12 +3,12 @@ require './lib/weather'
 describe Weather do
 
   it 'returns "true" when it is stormy' do
-    subject.weather_code = 0.1
+    allow(Kernel).to receive(:rand).and_return 0.1
     expect(subject.stormy?).to eq true
   end
 
   it 'returns "false" when it is not stormy' do
-    subject.weather_code = 0.5
+    allow(Kernel).to receive(:rand).and_return 0.5
     expect(subject.stormy?).to eq false
   end
 
