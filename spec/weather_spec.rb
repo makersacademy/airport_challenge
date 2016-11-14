b20b82ec7358257f83ec353fa72d3fba05bc408d
sara <sara@huhbub.com>
2016-11-14 17:02:24 +0000

    overrode weather randomisation with stub

--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -1,13 +1,15 @@
 require 'weather'
 
 describe Weather do
+    subject(:weather) {described_class.new}
 
 # As an air traffic controller
 # To ensure safety
 # I want to prevent takeoff when weather is stormy
 
 it 'prevents planes from taking off when stormy' do
-  expect { subject.prevents_takeoff }. to raise_error 'Cannot take off due to stormy weather'
+  allow(weather).to receive(:stormy?).and_return true
+  expect { weather.prevents_takeoff }. to raise_error 'Cannot take off due to stormy weather'
   end
 
 # As an air traffic controller
