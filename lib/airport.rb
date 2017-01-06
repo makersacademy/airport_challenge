1b360b15e86c6e33dcaa2dc1c6124f72073e91fc
James Turner <mail.jt@gmail.com>
2017-01-06 23:28:21 +0000

    added weather to airport

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,5 +1,6 @@
 require_relative "weather_station"
 class Airport
+  attr_reader :weather_station
   def initialize (station = WeatherStation.new)
     @weather_station = station
   end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,21 +1,19 @@
 require "airport"
 describe Airport do
   let(:good_weather) {double :good_weather, stormy?: false}
-  let(:bad_weather)  {double :bad_weather , stormy?: true}
+  let(:bad_weather)  {double :bad_weather,  stormy?: true}
   describe "#stormy?" do
     it{is_expected.to respond_to :stormy?}
   end
   describe "#initialize" do
-    it "it will create its own weather station if not given one" do
+    it "should create its own weather station if not given one" do
       expect{Airport.new;subject}.not_to raise_error
     end
-    it "it will say false if the station says false" do
-      Airport.new good_weather
-      is_expected.not_to be_stormy
+    it "should say false if the station says false" do
+      expect((Airport.new good_weather).stormy?).to eq false
     end
-    it "it will say true if the station says true" do
-      Airport.new bad_weather
-      is_expected.to be_stormy
+    it "should say true if the station says true" do
+      expect((Airport.new bad_weather).stormy?).to eq true
     end
   end
 end
