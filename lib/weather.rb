bd0b97164c06078c4c22e4d386dd1f196be614ac
Sam Jones <sam.jones494@googlemail.com>
2017-01-06 20:25:59 +0000

    adds return value to generate_num method

--- lib/weather.rb
+++ lib/weather.rb
@@ -1,7 +1,7 @@
 class Weather
 
 	def generate_num
-
+		rand(10)
 	end
 
 end
--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -4,5 +4,7 @@ describe Weather do
 
 	it {is_expected.to respond_to :generate_num}
 
-	
+	it 'generates a random number' do
+		expect(subject.generate_num).to be_an_instance_of(Fixnum)
+	end
 end
