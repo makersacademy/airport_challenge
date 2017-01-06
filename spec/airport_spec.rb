19e9ca5b9b91d26263dcc49db4eaa4ee912344ec
James Turner <mail.jt@gmail.com>
2017-01-06 22:33:28 +0000

    sober backup

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,7 +2,9 @@ require "airport"
 describe Airport do
   let(:good_weather) {double :good_weather, stormy?: false}
   let(:bad_weather)  {double :bad_weather , stormy?: true}
-  it{is_expected.to respond_to :stormy?}
+  describe "#stormy?" do
+    it{is_expected.to respond_to :stormy?}
+  end
   describe "#initialize" do
     it "it will create its own weather station if not given one" do
       expect{Airport.new;subject}.not_to raise_error
@@ -12,7 +14,7 @@ describe Airport do
       is_expected.not_to be_stormy
     end
     it "it will say true if the station says true" do
-      Airport.new good_weather
+      Airport.new bad_weather
       is_expected.to be_stormy
     end
   end
