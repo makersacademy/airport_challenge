07aaefba51732ce9d75f664467e62282764b4e65
James Turner <mail.jt@gmail.com>
2017-01-06 19:09:19 +0000

    added taking off

--- lib/plane.rb
+++ lib/plane.rb
@@ -1,4 +1,17 @@
 class Plane
+  def initialize
+    @landed = true
+  end
+
   def land airport
+    #check weather
+  end
+
+  def take_off
+    @landed = false
+  end
+
+  def landed?
+    @landed
   end
 end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -5,7 +5,14 @@ describe Plane do
     it{is_expected.to respond_to(:land).with(1).argument}
   end
   describe("#landed?") do
-    it{is_expected.to respond_to:landed?}
+    it{is_expected.to respond_to :landed?}
+    it{is_expected.to be_landed}
+    it "is expected to not be landed after taking off" do
+      subject.take_off
+      is_expected.not_to be_landed
+    end
   end
-  #describe(
+  describe("#take_off") do
+    it{is_expected.to respond_to :take_off}
+  end
 end
