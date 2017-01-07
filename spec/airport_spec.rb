2c4f03ea390ad684b8dbe9a5c0a3e63d0e94ea6b
Rob Brentnall <rob.brentnall@gmail.com>
2017-01-07 16:40:08 +0000

    Added test to see if same plane can land when already in the airport - fail

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -13,4 +13,13 @@ describe Airport do
     airport = Airport.new(50)
     expect(airport.capacity).to eq 50
   end
+  it 'ensures that the same plane cannot land if it is already in the airport' do
+    airport = Airport.new
+    plane = Plane.new
+    atc = AirTrafficController.new(plane)
+    atc.instruct_to_land
+    plane.land(airport)
+    expect{ plane.land(airport) }.to raise_error "Plane is already on the ground"
+  end
+
 end
