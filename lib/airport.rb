9d11697e08763091341e794057d60bb3b558004a
bvjones <benvaughanjones@gmail.com>
2017-01-07 14:25:38 +0000

    Ability to release a plane

--- lib/airport.rb
+++ lib/airport.rb
@@ -3,9 +3,16 @@ require_relative 'plane'
 class Airport
   attr_reader :plane
 
-  def landing(plane)
-    @plane = plane
+  def initialize
+    @plane = []
   end
 
+  def landing(plane)
+    @plane << plane
+  end
+
+  def release_plane
+    @plane.pop
+  end
 
 end
