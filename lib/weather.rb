114b2b1993b9da5b12bb2bdebd715f4cf5906ca1
Rob Brentnall <rob.brentnall@gmail.com>
2017-01-07 15:19:42 +0000

    Better test for random weather conditions

--- lib/weather.rb
+++ lib/weather.rb
@@ -1,7 +1,10 @@
 
 class Weather
 
+  WEATHER_CONDITIONS = %w(Sunny Stormy)
+
   def current_weather
+    Random.new.rand(5) <= 3 ? WEATHER_CONDITIONS[0] : WEATHER_CONDITIONS[1]
   end
 
 end
--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -3,6 +3,6 @@ require 'weather'
 describe Weather do
   it 'determines the current weather' do
     expect(subject).to respond_to :current_weather
-    expect(subject.current_weather).to eq "Stormy" || "Sunny"
+    expect(Weather::WEATHER_CONDITIONS.include?(subject.current_weather)).to eq true
   end
 end
