3c3e2d9985d462206746336592811757b39dfde3
Rick Clark <rkingsleyclark@gmail.com>
2017-01-07 11:13:17 +0000

    Initial land and take_off methods

--- lib/plane.rb
+++ lib/plane.rb
@@ -1,4 +1,11 @@
 class Plane
 
+  def land
+    true
+  end
 
+  def take_off
+    true
+  end
+  
 end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -1,7 +1,23 @@
 require "plane.rb"
 
 describe Plane do
-  
 
+  context "LANDING TESTS" do
+    it "can be instructed to land" do
+      expect(subject).to respond_to(:land)
+    end
+    it "confirms it has landed" do
+      expect(subject.land).to eq true
+    end
+  end
+
+  context "TAKE OFF TESTS" do
+    it "can be instructed to take off" do
+      expect(subject).to respond_to(:take_off)
+    end
+    it "confirms it has taken off" do
+      expect(subject.take_off).to eq true
+    end
+  end
 
 end
