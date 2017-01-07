64a993a9d02815f439ca0685c3375b112acc7882
Kat Hicks <kmhicks92@gmail.com>
2017-01-07 16:03:23 +0000

    Added an a new plane instance variable @landing_status to pass previous test. Note two other tests are now failing.

--- lib/plane.rb
+++ lib/plane.rb
@@ -1,17 +1,24 @@
 class Plane
+  attr_reader :landing_status
+
+  def initialize
+    @landing_status = nil
+  end
 
   def land(airport)
     fail "Plane is already landed at #{airport}." if airport.planes.include? (self)
     airport.store_plane(self)
+    @landing_status = true
   end
 
   def take_off(airport)
     fail "Plane is not currently in #{airport} so cannot take off." unless airport.planes.include? (self)
     airport.release_plane(self)
+    @landing_status = false
   end
 
   def confirm_landed?
-    true
+    landing_status
   end
 
   def confirm_take_off?
