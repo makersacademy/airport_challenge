9309aa7de0d2dba22995aa21f680c73781be18d4
Kat Hicks <kmhicks92@gmail.com>
2017-01-07 13:46:23 +0000

    Added third test to plane spec detailing that it should respond to the method confirm_landed?

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -8,4 +8,9 @@ describe Plane do
   it 'should respond to method take_off' do
     expect(subject).to respond_to :take_off
   end
+
+  it 'should respond to method confirm_landed?' do
+    expect(subject).to respond_to :confirm_landed?
+  end
+
 end
