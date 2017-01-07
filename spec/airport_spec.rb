74cf9fc2542c20cfeb0052b1a6fdd1b2349fee55
Rob Brentnall <rob.brentnall@gmail.com>
2017-01-07 16:16:15 +0000

    Added test for airport capacity - fail

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -4,4 +4,7 @@ describe Airport do
   it 'Can hold planes' do
     expect(subject).to respond_to :planes
   end
+  it 'Has a maximum capacity of planes that it can hold' do
+    expect(subject).to respond_to :capacity
+  end
 end
