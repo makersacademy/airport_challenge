0bff3495c82cd6b82b2c372a02414f25c4cbf9a8
bvjones <benvaughanjones@gmail.com>
2017-01-07 23:44:28 +0000

    Error messages and take off

--- lib/plane.rb
+++ lib/plane.rb
@@ -1,11 +1,15 @@
 class Plane
-attr_accessor :landed
-
-  def initialize(landed = false)
-    @landed = landed
-  end
+  attr_accessor :landed
 
   def landing(landed)
+    raise "Cannot land! Plane is already grounded" if landed
     landed = true
+    "Plane has landed!"
+  end
+
+  def take_off(landed)
+    raise "Cannot take off! Plane is already in the air" unless landed
+    landed = false
+    "Plane has sucessfully taken off!"
   end
 end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -4,11 +4,26 @@ describe Plane do
   plane = Plane.new
 
   describe '#landing' do
-      it 'landing a plane in the airport' do
+    it 'landing a plane in the airport' do
       should respond_to(:landing)
     end
-      it 'checking if plane is grounded' do
-      expect(subject.landing(false)).to eq true
+    it 'checking if plane is grounded' do
+      expect(subject.landing(false)).to eq "Plane has landed!"
+    end
+    it 'error when plane is already grounded' do
+      expect{ subject.landing(true) }.to raise_error("Cannot land! Plane is already grounded")
+    end
+  end
+
+  describe '#take_off' do
+    it 'testing plane has taken off' do
+      should respond_to(:take_off)
+    end
+    it 'checking if plane has left' do
+      expect(subject.take_off(true)).to eq "Plane has sucessfully taken off!"
+    end
+    it 'error when plane is already grounded' do
+      expect{ subject.take_off(false) }.to raise_error("Cannot take off! Plane is already in the air")
     end
   end
 end
