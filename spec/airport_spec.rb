64235678cc65337d4ee588d481f05084caa5b36d
Kat Hicks <kmhicks92@gmail.com>
2017-01-07 13:17:49 +0000

    Amended first test for new rspec 3.2.2 syntax

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,7 +2,6 @@ require 'airport'
 
 describe Airport do
   it 'should assign a default capacity when initialised' do
-    get :initialize
-    assigns(:capacity).should_not be_nil
+    expect(subject.capacity).not_to be_nil
   end
 end
