7b62a93963064549c6e012e58a5191659362e0c4
Kat Hicks <kmhicks92@gmail.com>
2017-01-07 13:43:20 +0000

    Added a second test to plane spec detailing that it should respond to the method take_off

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -4,4 +4,8 @@ describe Plane do
   it 'should respond to method land' do
     expect(subject).to respond_to :land
   end
+
+  it 'should respond to method take_off' do
+    expect(subject).to respond_to :take_off
+  end
 end
