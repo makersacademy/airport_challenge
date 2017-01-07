28e21c8512d56c1b66bb689ba2903d835fc50c29
Basile Koko <basile_koko@hotmail.com>
2017-01-07 11:54:59 +0000

    class plane and method land and take_off added

---
+++ lib/plane.rb
@@ -0,0 +1,7 @@
+class Plane
+  def land
+  end
+
+  def take_off
+  end
+end
---
+++ spec/plane_spec.rb
@@ -0,0 +1,5 @@
+require 'plane'
+describe Plane do
+  it {is_expected.to respond_to(:land)}
+  it {is_expected.to respond_to(:take_off)}
+end
