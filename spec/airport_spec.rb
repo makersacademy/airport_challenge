d9662f3f4e6d84d65e7f4b616e64ea801d0072b6
Kat Hicks <kmhicks92@gmail.com>
2017-01-07 13:20:57 +0000

    Added another test confirming that default value of capacity is as expected

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -4,4 +4,8 @@ describe Airport do
   it 'should assign a default capacity when initialised' do
     expect(subject.capacity).not_to be_nil
   end
+
+  it 'should have a default capacity of 1' do
+    expect(subject.capacity).to eq 1
+  end
 end
