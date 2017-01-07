19901ed91e5f34ee05246411477199a1a4be96e2
Kat Hicks <kmhicks92@gmail.com>
2017-01-07 14:03:07 +0000

    Changed syntax style of tests to be consistent - added brackets around method names

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -2,7 +2,7 @@ require 'plane'
 
 describe Plane do
   it 'should respond to method land' do
-    expect(subject).to respond_to :land
+    expect(subject).to respond_to(:land)
   end
 
   it 'should land at an airport' do
@@ -10,15 +10,15 @@ describe Plane do
   end
 
   it 'should respond to method take_off' do
-    expect(subject).to respond_to :take_off
+    expect(subject).to respond_to(:take_off)
   end
 
   it 'should respond to method confirm_landed?' do
-    expect(subject).to respond_to :confirm_landed?
+    expect(subject).to respond_to(:confirm_landed?)
   end
 
   it 'should respond to method confirm_take_off?' do
-    expect(subject).to respond_to :confirm_take_off?
+    expect(subject).to respond_to(:confirm_take_off?)
   end
 
 end
