185dd22d9e91237e2a6edc9dd366231806cdb167
herecomesjaycee <jaycee.cheong04@gmail.com>
2017-01-07 15:04:59 +0000

    Review test cases

--- lib/airport.rb
+++ lib/airport.rb
@@ -2,23 +2,36 @@
 
 class Airport
 
-DEFAULT_CAPACITY = 0 
-	def initiliaze(capacity = DEFAULT_CAPACITY)
+DEFAULT_CAPACITY = 0
+	def initiliaze(capacity =DEFAULT_CAPACITY)
 	@capacity =  capacity
+	@planes = 0
 	end
 
 	def land(plane)
+		raise 'Airport is full' if full?
 		raise 'Weather is stormy, cannot land' if stormy?
 		plane.landed?
+		#@planes += 1
 	end
 
 	def take_off(plane)
 		raise 'Weather is stormy, cannot take off' if stormy?
 		plane.landed = false
+		@planes -= 2
 	end
 
+	private 
+
 	def stormy?
-		true if rand(1..100) > 50
+		 rand(1..100) > 50? true : false
 	end	
+
+	def full?
+		@capacity == 0
+	end
+
+	
+
 
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -9,12 +9,24 @@ describe Airport do
 		it 'should return plane is landed if weather is sunny' do
 			allow(subject).to receive(:stormy?) {false}
 			expect{subject.land(plane).to eq true}
-			end
-
-		it 'should raise error before landing about stormy weather ' do
+		end
+		
+		it 'should add plane to planes' do
+			allow(subject).to receive(:stormy?) {false}
+			subject.land(plane)
+			/to add a line to test array or count once a plane is landed/
+		end
+		
+		it 'should return plane is not landed if weather is stormy' do
 			allow(subject).to receive(:stormy?) {true}
-			expect{subject.land(plane)}.to raise_error('Weather is stormy, cannot land')
-			end
+			expect{subject.land(plane).to eq false}
+		end
+
+		it 'should not allow plane to land if capacity is zero' do
+			allow(subject).to receive(:full?) {true}
+			expect{subject.land(plane)}.to raise_error('Airport is full')
+		end
+
 
 	end
 
@@ -23,13 +35,28 @@ describe Airport do
 		it 'should return plane is taken off if weather is sunny' do
 			allow(subject).to receive(:stormy?) {false}
 			expect{subject.take_off(plane).to eq false}
-			end	
+		end
+
+		it 'should remove plane from planes' do
+			allow(subject).to receive(:stormy?) {false}
+			subject.land(plane)
+			/to add a line to test array or count once a plane is has taken off/
+		end
 	
 		it 'should raise error before take off about stormy weather 'do
 			allow(subject).to receive(:stormy?) {true}
 			expect{subject.take_off(plane)}.to raise_error('Weather is stormy, cannot take off')
-			end
+		end
 
+	end
+
+	describe '#new_instance' do
+		it 'should return 0 if no capacity is given' do
+			expect{(subject::capacity).to eq 0}
+		end
+		it 'shoud return the given value if capacity is given' do
+			expect{(subject(12)::capacity).to eq 12}
+		end
 	end
 
 end
