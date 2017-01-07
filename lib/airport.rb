74038336cb3d6bd85f10113106a45832700003db
Edyta Wrobel <edyta.wrobel.willdoit@gmail.com>
2017-01-07 17:47:02 +0000

    UserStory(2): lets plane to take off

--- lib/airport.rb
+++ lib/airport.rb
@@ -3,4 +3,8 @@ class Airport
   def land(plane)
     puts "landed!"
   end
+
+  def take_off
+    puts "plane taken off!"
+  end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -4,6 +4,9 @@ require "plane"
 describe Airport do
   describe '#land' do
     it { is_expected.to respond_to(:land).with(1).argument }
+  end
 
+  describe '#take_off' do
+    it { is_expected.to respond_to(:take_off) }
   end
 end
