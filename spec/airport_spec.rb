b17b5eff2ffdd08f3ba95072e550db2c5381d2e7
Jack <92jackhenderson@gmail.com>
2017-01-08 19:17:53 +0000

    minus comments, first 4 test passed

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -6,14 +6,10 @@ describe Airport do
 
   describe "#land" do
     it "respond to #land" do
-      #raise exception when stormy!
-      #raise exception when full!
       expect(subject).to respond_to(:land).with(1).argument
     end
 
     it "landing confirmation" do
-      #later i will raise an exception so that this doesn't happen
-      # the method land won't even run if airport is full.
         allow(subject).to receive(:weather_check) {"balmy"}
       expect(subject.land(plane)).to eq("plane landed")
     end
@@ -26,7 +22,6 @@ describe Airport do
 
   describe "#takeoff" do
     it "responds to #takeoff" do
-      #raise exception when stormy!
       expect(subject).to respond_to(:takeoff).with(1).argument
     end
 
@@ -47,8 +42,6 @@ describe Airport do
     end
 
     it "returns the state of the new Weather class" do
-      # insert a stub?
-      # allow(die).to receive(:roll) { 3 }
       allow(subject).to receive(:weather_check) {"balmy"}
       expect(subject.weather_check).to eq("balmy")
     end
