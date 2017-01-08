03e8e986ec53070c1ee1a69ee2ea3fb1157060e8
Kat Hicks <kmhicks92@gmail.com>
2017-01-08 10:22:16 +0000

    Added another test detailing that check_weather should also return 'stormy'

--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -4,7 +4,12 @@ describe Weather do
   it 'responds to the method check_weather' do
     expect(subject).to respond_to :check_weather
   end
+
   it 'should return "sunny" as an outcome of check_weather' do
     expect(subject.check_weather).to eq("sunny")
+  end
+
+  it 'should return "stormy" as an outcome of check_weather' do
+    expect(subject.check_weather).to eq("stormy")
   end
 end
