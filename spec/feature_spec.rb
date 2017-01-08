0866d85fca12c609ccf48ea55bb2948f484a1f41
James Turner <mail.jt@gmail.com>
2017-01-08 10:50:44 +0000

    written feature_spec.rb, finished unless  more tests are added to this

--- spec/feature_spec.rb
+++ spec/feature_spec.rb
@@ -3,20 +3,41 @@ require "./lib/plane"
 require "./lib/weather_station"
 require "./lib/air_traffic_control"
 
-control = AirTrafficControl.new
+describe "full feature test" do
+  let(:good_weather) {double :good_weather, stormy?: false}
+  let(:bad_weather)  {double :bad_weather,  stormy?: true}
+  before :each do
+    @plane = Plane.new
+    @control = AirTrafficControl.new
+    @capacity = 5
+  end
 
-leeds_bradford = Airport.new
-heathrow = Airport.new
+  context "weather is good" do
+    before(:each){@airport = Airport.new @capacity, good_weather}
 
-plane_1 = Plane.new
-plane_2 = Plane.new
-plane_3 = Plane.new
-plane_4 = Plane.new
-plane_5 = Plane.new
-plane_6 = Plane.new
-plane_7 = Plane.new
-plane_8 = Plane.new
-plane_9 = Plane.new
-plane_10 = Plane.new
+    it "will accept planes landing" do
+      expect(@control.land_plane @plane, @airport).to eq true
+    end
+    it "won't accept the same plane landing twice" do
+      @control.land_plane @plane, @airport
+      expect(@control.land_plane @plane, @airport).to eq false
+    end
+    it "won't land plane if airport is full" do
+      @capacity.times{(@control.land_plane Plane.new, @airport)}
+      expect(@control.land_plane @plane, @airport).to eq false
+    end
+  end
 
-#control.land_plane(plane_1, heathrow)
+  context "weather is bad" do
+    before(:each){@airport = Airport.new 5, bad_weather}
+
+    it "will not accept planes landing" do
+      expect(@control.land_plane @plane, @airport).to eq false
+    end
+    it "will not accept planes taking off" do
+      subject.instance_variable_set(:@landed, true)
+      subject.instance_variable_set(:@airport, @airport)
+      expect(@control.take_off_plane @plane, @airport).to eq false
+    end
+  end
+end
