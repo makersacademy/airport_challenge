41d12c4789c37dc9aff4aa0d246e4acefb9d726f
Kat Hicks <kmhicks92@gmail.com>
2017-01-08 11:18:26 +0000

    Realised that stubs need to be cascaded throughout the tests for all previous tests to work as expected. Think that should be able to declare a let statement to set a default state for whole test environment. However, cannot figure out how. Currently tests are not very DRY.

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -19,19 +19,23 @@ describe Airport do
 
   it 'should store a plane that has landed' do
     plane = Plane.new
+    allow(plane).to receive(:weather) { "sunny" }
     expect{plane.land(subject)}.to change{ subject.planes.length }.by(1)
   end
 
   it 'should release a plane that has taken off' do
     plane = Plane.new
+    allow(plane).to receive(:weather) { "sunny" }
     plane.land(subject)
     expect{plane.take_off(subject)}.to change{ subject.planes.length }.by(-1)
   end
 
   it 'should raise an error if plane attempts to land when full' do
     first_plane = Plane.new
+    allow(first_plane).to receive(:weather) { "sunny" }
     subject.store_plane(first_plane)
     second_plane = Plane.new
+    allow(second_plane).to receive(:weather) { "sunny" }
 
     expect{ subject.store_plane(second_plane) }.to raise_error("Airport is currently full.")
   end
