4441792a7436d02b22c04355f8c65f22bb93a8a6
Mike Field-May <mfieldmay@gmail.com>
2017-01-08 10:45:02 +0000

    Wrote a failing test that expects an error to be raised when planes try to take off in bad weather

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -16,12 +16,15 @@ describe Plane do
 
   describe "#take_off" do
     it {expect(subject).to respond_to(:take_off).with(1).argument}
+
     before do
+
       @airport = Airport.new
       @plane = Plane.new
       @plane.land(@airport)
 
     end
+
 
     context "if a plane takes off"
     it { expect{subject.take_off(@airport)}.to change{@airport.planes}}
@@ -29,8 +32,21 @@ describe Plane do
 
     context "if the weather is not stormy" do
       it "will take_off" do
-        allow(@plane).to receive(:safe?) {true}
-      #it { expect{subject.take_off(@airport)}.to change{@airport.planes}}
+        @plane = Plane.new
+        @airport = Airport.new
+        @plane.land(@airport)
+        allow(@plane).to receive(:safe?).and_return( true )
+        expect{@plane.take_off(@airport)}.to change{@airport.planes}
+      end
+    end
+
+    context "if the weather is stormy" do
+      it "will not take off" do
+        @plane = Plane.new
+        @airport = Airport.new
+        @plane.land(@airport)
+        allow(@plane).to receive(:safe?) {false}
+        expect{@plane.take_off(@airport)}.to raise_error
       end
     end
 
