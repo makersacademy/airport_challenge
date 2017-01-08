58d2d8eeb560b26097bf2792ade488052ce7a165
Shereen Finney <shereenfinney@gmail.com>
2017-01-08 16:21:32 +0000

    Added new unit test to plane_spec to test that a plane instance will respond to a method confirm_not_in_airport

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -1,15 +1,17 @@
 # /Users/shereen/Google Drive/Projects/airport_challenge/spec/plane_spec.rb
-
 require "plane"
 
 describe Plane do
-
   #1st story / 1st test
   it {is_expected.to respond_to :confirm_landed?}
 
-  #1st story / Second test
+  #1st story / 4th test
   it "confirms to the airport it has landed" do
     expect(subject.confirm_landed?).to eq("I've landed!")
   end
+end
 
+describe Plane do
+  #Second story / 1st test
+  it {is_expected.to respond_to :confirm_not_in_airport?}
 end
