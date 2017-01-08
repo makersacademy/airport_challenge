9fa472581f72c69301b029ba5d2617d0c7659632
Sam Jones <sam.jones494@googlemail.com>
2017-01-08 11:05:58 +0000

    updates tests for weather class

--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -2,13 +2,17 @@ require 'weather'
 
 describe Weather do 
 
-	describe '#stormy?' do
-		it 'reports whether the weather is stormy' do
-			weather = double("weather")
-			allow(weather).to receive(:stormy?).and_return true
-			expect(weather.stormy?).to eq true
-			allow(weather).to receive(:stormy?).and_return false
-			expect(weather.stormy?).to eq false
-		end
-	end
+	 describe '#stormy?' do
+	 	it 'returns truw when the weather is stormy' do
+	 		weather = double(:weather)
+	 		allow(weather).to receive(:stormy?).and_return true
+	 		expect(weather.stormy?).to eq true
+	 	end
+
+	 	it 'returns false when the weather is good' do
+	 		weather = double(:weather)
+	 		allow(weather).to receive(:stormy?).and_return false
+	 		expect(weather.stormy?).to eq false
+	 	end
+	 end
 end
