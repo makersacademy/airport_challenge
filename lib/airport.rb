3d8bd45ab395221a52ac6d56bf0b06e06dffab43
Edyta Wrobel <edyta.wrobel.willdoit@gmail.com>
2017-01-08 13:37:22 +0000

    Confrim when a plane's taken off; raises error - capacity full

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,13 +1,26 @@
 class Airport
+  DEFAULT_CAPACITY = 200
+  attr_reader :planes, :capacity
 
-  attr_reader :plane
+  def initialize(capacity=DEFAULT_CAPACITY)
+    @planes = []
+    @capacity = capacity
+  end
 
   def land(plane)
-    fail 'No slots available' if @plane
-    @plane = plane
+    fail "No landing slots available!" if full?
+    @planes << plane
+    @planes[0]
   end
 
-  def take_off
-    puts "plane taken off!"
+  def take_off(plane)
+    @planes.pop
   end
+
+  private
+
+  def full?
+    @planes.count >= capacity
+  end
+
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -5,21 +5,29 @@ require "weather"
 plane = Plane.new
 
 describe Airport do
+
+  it 'assigns default airport capacity' do
+      expect(subject.capacity).to eq 200
+  end
+
   describe '#land' do
     it { is_expected.to respond_to(:land).with(1).argument }
 
     it 'lets plane to land' do
-      subject.land(plane)
-      expect(subject.plane).to eq plane
+      expect(subject.land(plane)).to eq plane
     end
 
     it 'raises an error when the capacity is full' do
-      subject.land(plane)
-      expect { subject.land(plane) }.to raise_error 'No slots available'
+      200.times { subject.land(plane) }
+      expect { subject.land(plane) }.to raise_error 'No landing slots available!'
     end
   end
 
   describe '#take_off' do
     it { is_expected.to respond_to(:take_off) }
+
+    it 'confirms that the plane is no longer in the airport' do
+      expect(subject.planes).to be_empty
+    end
   end
 end
