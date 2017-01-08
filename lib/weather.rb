f8cf9cb850f4b5124dc39c19bf3d5b6dbb617c57
Dylan Rhodius <dylanrhodius@hotmail.com>
2017-01-08 13:46:04 +0000

    Created instance variable 'status'. Value for 'status' randomly generated with initialized method

--- lib/weather.rb
+++ lib/weather.rb
@@ -1,4 +1,11 @@
 class Weather
 
+attr_reader :status
+
+
+  def initialize
+    rand(100) <= 5 ? @status = "stormy" : @status = "sunny"
+  end
+
 
 end
--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -2,4 +2,6 @@ require './lib/weather'
 
 describe Weather do
 
+  it {is_expected.to respond_to :status}
+
 end
