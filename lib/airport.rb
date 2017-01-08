0c2c68e679f833a404c509952144bc7b65531926
Jack <92jackhenderson@gmail.com>
2017-01-08 19:17:08 +0000

    first 4 tests passed

--- lib/airport.rb
+++ lib/airport.rb
@@ -5,11 +5,12 @@ class Airport
   attr_reader :plane
 
   def land(plane)
+    fail "too inclement for landing!" unless weather_check == "balmy"
     "plane landed"
   end
 
   def takeoff(plane)
-    fail "too inclement for flight!" unless weather_check=="balmy"
+    fail "too inclement for flight!" unless weather_check == "balmy"
     "plane took off!"
   end
 
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -14,7 +14,13 @@ describe Airport do
     it "landing confirmation" do
       #later i will raise an exception so that this doesn't happen
       # the method land won't even run if airport is full.
+        allow(subject).to receive(:weather_check) {"balmy"}
       expect(subject.land(plane)).to eq("plane landed")
+    end
+
+    it "raise an error when the weather is stormy" do
+      allow(subject).to receive(:weather_check) {"stormy"}
+      expect {subject.land(plane)}.to raise_error("too inclement for landing!")
     end
   end
 
@@ -24,26 +30,27 @@ describe Airport do
       expect(subject).to respond_to(:takeoff).with(1).argument
     end
 
-    it "takeoff confirmation" do
+    it "takeoff confirmation when weather is fine" do
+      allow(subject).to receive(:weather_check) {"balmy"}
       expect(subject.takeoff(plane)).to eq("plane took off!")
+    end
+
+    it "raises an error when the weather is stormy" do
+      allow(subject).to receive(:weather_check) {"stormy"}
+      expect {subject.takeoff(plane)}.to raise_error("too inclement for flight!")
     end
   end
 
   describe "#weather" do
     it "responds to #weather method" do
-      # write new class Weather, instigate a new Weather object each time
-      # put a random num generator in it.
-      # attr_accessor it, so that airport can run weathercheck method,
-      # then if it's good, run land/takeoff methods
       expect(subject).to respond_to(:weather_check)
-      # weather_check returns the state of the new Weather object
     end
 
     it "returns the state of the new Weather class" do
       # insert a stub?
       # allow(die).to receive(:roll) { 3 }
-      allow(subject).to receive(:weather_check) {"stormy"}
-      expect(subject.weather_check).to eq("stormy")
+      allow(subject).to receive(:weather_check) {"balmy"}
+      expect(subject.weather_check).to eq("balmy")
     end
 
   end
