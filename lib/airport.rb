55f0cc0ec6137679c1940439f1c663e08e1ff65c
Kat Hicks <kmhicks92@gmail.com>
2017-01-08 12:18:06 +0000

    Refactored and removed redundant return values for store_plane and release_plane methods

--- lib/airport.rb
+++ lib/airport.rb
@@ -13,12 +13,10 @@ class Airport
   def store_plane(plane)
     fail "Airport is currently full." unless planes.length < capacity
     planes.push(plane)
-    return planes
   end
 
   def release_plane(plane)
     planes.delete(plane)
-    return planes
   end
 
 end
