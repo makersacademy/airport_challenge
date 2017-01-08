14f27a0705be7d00ca241c7a85f747e2b6bfbb53
Joe Marriott <marriott90@gmail.com>
2017-01-08 18:18:20 +0000

    Set default capacity and added empty? private method for Airport class

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,27 +1,41 @@
 require_relative 'plane'
 
 class Airport
+  attr_accessor :capacity
+  DEFAULT_CAPACITY = 5
 
-  def initialize
+  def initialize(capacity=DEFAULT_CAPACITY)
     @planes = []
+    @capacity = capacity
   end
 
   def land(plane)
     raise "Stormy weather preventing landing" if stormy?
+    raise "Airport is full" if full?
     @planes << plane
     plane.airborne = false
     "#{plane} has landed"
   end
 
   def take_off(plane)
-    fail 'Stormy weather preventing take off' if stormy?
-    fail 'No planes available' if @planes.empty?
+    raise 'Stormy weather preventing take off' if stormy?
+    raise 'No planes available' if empty?
     plane.airborne = true
     "#{@planes.pop} has taken off"
   end
 
+  private
+
   def stormy?
     rand > 0.5? true : false
+  end
+
+  def full?
+    @planes.count >= capacity
+  end
+
+  def empty?
+    @planes.empty?
   end
 
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -3,30 +3,49 @@ require 'airport'
 describe Airport do
 
   describe '#land' do
+
     it 'lands a plane in the airport' do
       allow(subject).to receive(:stormy?) {false}
       plane = Plane.new
       expect(subject.land plane).to eq "#{plane} has landed"
     end
-    it 'fails to lands a plane in stormy weather' do
+
+    it 'fails to land a plane in stormy weather' do
       allow(subject).to receive(:stormy?) {true}
       plane = Plane.new
       expect{subject.land plane}.to raise_error "Stormy weather preventing landing"
     end
+
+    it 'fails to land a plane if airport is full' do
+      allow(subject).to receive(:stormy?) {false}
+      allow(subject).to receive(:full?) {true}
+      plane = Plane.new
+      expect{subject.land plane}.to raise_error "Airport is full"
+    end
+
   end
 
   describe '#take_off' do
+
     it 'instructs plane to take off' do
       allow(subject).to receive(:stormy?) {false}
       plane = Plane.new
       subject.land plane
       expect(subject.take_off plane).to eq "#{plane} has taken off"
     end
+
+    it 'fails to allow plane take off if no planes at airport' do
+      allow(subject).to receive(:stormy?) {false}
+      plane = Plane.new
+      expect{subject.take_off plane}.to raise_error 'No planes available'
+    end
+
     it 'fails to allow plane take off in stormy weather' do
       allow(subject).to receive(:stormy?) {true}
       plane = Plane.new
       expect{subject.take_off plane}.to raise_error "Stormy weather preventing take off"
     end
+
   end
 
 end
