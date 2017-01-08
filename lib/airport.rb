2c8ba8f261f66e08818fd1e270db8d41067d175c
Edyta Wrobel <edyta.wrobel.willdoit@gmail.com>
2017-01-08 13:16:09 +0000

    Revert "raise error when airport full; confirm when plane took off"
    This reverts commit d5246d7491cbc2f05e8e86e2ff8c324be59eec25.

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,46 +1,13 @@
 class Airport
-  DEFAULT_CAPACITY = 200
-  attr_reader :planes, :capacity
 
-  def initialize(capacity=DEFAULT_CAPACITY)
-    @planes = []
-    @capacity = capacity
-  end
+  attr_reader :plane
 
   def land(plane)
-    fail "No landing slots available!" if full?
-    @planes << plane
-    @planes[0]
+    fail 'No slots available' if @plane
+    @plane = plane
   end
 
-  def take_off(plane)
-    @planes.pop
+  def take_off
+    puts "plane taken off!"
   end
-
-  private
-
-  def full?
-    @planes.count >= capacity
-  end
-
 end
-
-# it 'instructs a plane to land and confirm that it landed' do
-#     plane = Plane.new
-#     expect(subject.land(plane)).to eq plane
-#   end
-#
-#  it 'instructs a plane to take off' do
-#     plane = Plane.new
-#     subject.land(plane)
-#     expect(subject.take_off(plane)).to eq plane
-#   end
-#
-#   it 'confirms that the plane is no longer in the airport' do
-#     expect(subject.planes).to be_empty
-#   end
-#
-#   it 'prevents landing when the airport is full' do
-#     Airport::DEF_CAPACITY.times {subject.land(Plane.new)}
-#     expect{subject.land(Plane.new)}.to raise_error
-#   end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -5,34 +5,21 @@ require "weather"
 plane = Plane.new
 
 describe Airport do
-
-  it 'assigns default airport capacity' do
-    expect(subject.capacity).to eq 200
-  end
-
   describe '#land' do
     it { is_expected.to respond_to(:land).with(1).argument }
 
     it 'lets plane to land' do
-      expect(subject.land(plane)).to eq plane
+      subject.land(plane)
+      expect(subject.plane).to eq plane
     end
 
-    it 'raises an error when the airport is full' do
-      200.times { subject.land plane }
-      expect { subject.land plane }.to raise_error "No landing slots available!"
+    it 'raises an error when the capacity is full' do
+      subject.land(plane)
+      expect { subject.land(plane) }.to raise_error 'No slots available'
     end
   end
 
   describe '#take_off' do
-    it { is_expected.to respond_to(:take_off).with(1).argument }
-
-    it 'lets plane to take off' do
-      subject.land(plane)
-      expect(subject.take_off(plane)).to eq plane
-    end
-
-    it 'confirms that the plane is no longer at the airport' do
-      expect(subject.planes).to be_empty
-    end
+    it { is_expected.to respond_to(:take_off) }
   end
 end
