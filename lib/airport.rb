7b7c27841046e9dd74698295109753badce0a0eb
Gurminder Bhachu <gurminderbhachu@hotmail.com>
2017-01-08 14:03:04 +0000

    planes ca now take off from airport

--- lib/airport.rb
+++ lib/airport.rb
@@ -2,4 +2,8 @@ class Airport
 
     def land(plane)
     end
+
+    def take_off(plane)
+    end
+
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -6,4 +6,7 @@ describe Airport do
   #it {is expected.to respond_to(:land).with(1).argument}
     expect(subject).to respond_to(:land).with(1).argument
   end
+  it 'instructs plane to take off' do
+    expect(subject).to respond_to(:take_off).with(1).argument
+  end
 end
