c22cc31207bbf8b36b0b3f4f248562bbe6024355
Sam Jones <sam.jones494@googlemail.com>
2017-01-08 12:55:03 +0000

    further refactors tests

--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -2,15 +2,15 @@ require 'weather'
 
 describe Weather do 
 
+let(:weather) {double :weather}
+
 	 describe '#stormy?' do
 	 	it 'returns true when the weather is stormy' do
-	 		weather = double(:weather)
 	 		allow(weather).to receive(:stormy?).and_return true
 	 		expect(weather.stormy?).to eq true
 	 	end
 
 	 	it 'returns false when the weather is good' do
-	 		weather = double(:weather)
 	 		allow(weather).to receive(:stormy?).and_return false
 	 		expect(weather.stormy?).to eq false
 	 	end
