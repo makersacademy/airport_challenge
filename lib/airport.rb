619300190b38853dff6df8b96e5eb2a8ebd93ad6
Ross Benzie <rossbenzie@Ross-MacBook-Pro.local>
2017-01-08 20:43:09 +0000

    airport.rb access methods working

--- lib/airport.rb
+++ lib/airport.rb
@@ -11,25 +11,25 @@ class Airport
   def ask_to_land(plane)
     raise "Unable to land due to airport being full" if is_full?
     raise "Unable to land due to stormy weather" if is_stormy?
-    @planes << plane
+    planes << plane
   end
   def ask_to_take_off(plane)
     raise "Unable to take off due to stormy weather" if is_stormy?
-    @planes.delete(plane)
+    planes.delete(plane)
   end
   def has_landed?(plane)
-    @planes.include?(plane)
+    planes.include?(plane)
   end
   def has_taken_off?(plane)
-    @planes.include?(plane) == false
+    planes.include?(plane) == false
   end
 
   private
 
   def is_stormy?
-    @weather_report == 'stormy'
+    weather_report == 'stormy'
   end
   def is_full?
-    @planes.size == @capacity
+    planes.size == capacity
   end
 end
