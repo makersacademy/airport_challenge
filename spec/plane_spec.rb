3314d5437c9f246aed1700f9a6a9b7d2baedd6ac
Rob Brentnall <rob.brentnall@gmail.com>
2017-01-08 15:19:47 +0000

    Retrospective tests added for edge cases

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -11,6 +11,11 @@ describe Plane do
     @atc.instruct_to_land(@plane, "Sunny")
     expect(@plane).to be_clear_to_land
   end
+  it 'is not able to land if not cleared to' do
+    @atc.instruct_to_take_off(@plane, "Sunny")
+    @plane.take_off(@airport)
+    expect { expect(@plane.land(@airport)) }.to raise_error "Not cleared for landing"
+  end
   it 'is able to land' do
     expect(subject).to respond_to :land
     @atc.instruct_to_land(subject, "Sunny")
@@ -20,6 +25,9 @@ describe Plane do
     expect(subject).to respond_to :clear_for_take_off?
     @atc.instruct_to_take_off(@plane, "Sunny")
     expect(@plane).to be_clear_for_take_off
+  end
+  it 'is not able to take off if not cleared to' do
+    expect { expect(subject.take_off(@airport)) }.to raise_error "Not cleared for takeoff"
   end
   it 'is able to take off' do
     expect(subject).to respond_to :take_off
