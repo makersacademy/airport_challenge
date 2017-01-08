97aa078eee16cf88717e2decb749715783d8548d
Ekaterina Loshchinina <KateLoschinina@ekaterinas-mbp.home>
2017-01-08 17:14:25 +0000

    Adding initialisation with capacity to airport class

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,2 +1,11 @@
 class Airport
+
+  CAPACITY = 10
+  def initialize(capacity = CAPACITY)
+    @planes = []
+    @capacity = capacity
+  end
+
+  attr_accessor :planes
+  attr_accessor :capacity
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,5 +1,31 @@
 require 'airport'
 
 describe Airport do
-  
+
+  describe 'entity' do
+    it 'when created has an empty array of planes' do
+      expect(subject.planes).to eq([])
+    end
+  end
+
+  describe 'traffic management procedures' do
+    it 'can land a plane' do
+
+    end
+    it 'can takeoff a plane' do
+
+    end
+  end
+
+  describe 'capacity' do
+    it 'by default capacity is constant' do
+      expect(subject.capacity).to eq(Airport::CAPACITY)
+    end
+    it 'capacity can be overridden' do
+      number = 20
+      port = Airport.new(number)
+      expect(port.capacity).to eq(number)
+    end
+  end
+
 end
