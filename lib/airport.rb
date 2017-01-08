6192940364bf437ef8a0d96d5c03986343bc242c
Joe Marriott <marriott90@gmail.com>
2017-01-08 16:36:55 +0000

    added fails for take_off and land methods in stormy weather

--- lib/airport.rb
+++ lib/airport.rb
@@ -7,12 +7,14 @@ class Airport
   end
 
   def land(plane)
+    fail 'Stormy weather preventing landing' if stormy?
     @planes << plane
     plane.airborne = false
     "#{plane} has landed"
   end
 
   def take_off(plane)
+    fail 'Stormy weather preventing take off' if stormy?
     fail 'No planes available' if @planes.empty?
     plane.airborne = true
     "#{@planes.pop} has taken off"
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -4,6 +4,7 @@ describe Airport do
 
   describe '#land' do
     it 'lands a plane in the airport' do
+      allow(subject).to receive(:stormy) {false}
       plane = Plane.new
       expect(subject.land plane).to eq "#{plane} has landed"
     end
@@ -11,6 +12,7 @@ describe Airport do
 
   describe '#take_off' do
     it 'instructs plane to take off' do
+      allow(subject).to receive(:stormy?) {false}
       plane = Plane.new
       subject.land plane
       expect(subject.take_off plane).to eq "#{plane} has taken off"
