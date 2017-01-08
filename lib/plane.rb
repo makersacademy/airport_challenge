f14ce3750c38ab00d70d03d2bfae477044b9b6d1
Varvara Shinkarenko <shinkarenko.varvara@gmail.com>
2017-01-08 17:43:50 +0000

    Changed plane files - checking the status of the plane

--- lib/plane.rb
+++ lib/plane.rb
@@ -1,12 +1,7 @@
 class Plane
-  attr_accessor :status # we should be able to read and change(write) the status
-
-  def land
-    @status = 'landed'
-  end
-
-  def take_off
-    @status = 'took off'
+#  attr_accessor :status # we should be able to read and change(write) the status
+  def in_air?(status = true)
+    status
   end
 
 end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -1,22 +1,14 @@
 require 'plane'
 
 describe Plane do
-  context 'when landing the plane' do
-    it 'instruct to land' do
-    expect(subject).to respond_to(:land)
-    end
-    it 'can confirm its status' do
-      expect(subject.land).to eq 'landed'
-    end
+
+  it 'checks if plane can fly' do
+    expect(subject.in_air?).to eq(true)
   end
 
-
-  context 'when taking off' do
-    it 'instruct to take off' do
-    expect(subject).to respond_to(:take_off)
-    end
-    it 'can confirm its status' do
-      expect(subject.take_off).to eq 'took off'
-    end
+  it 'checks for plane status through received parameters' do
+    expect(subject.in_air?(true)).to eq(true)
+    expect(subject.in_air?(false)).to eq(false)
   end
+
 end
