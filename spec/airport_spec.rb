4f70866fb9fc1eb4a043d8bada4f5ad08936053c
Shereen Finney <shereenfinney@gmail.com>
2017-01-08 16:11:58 +0000

    Added a new unit test in airport_spec to test that an airport instance will respond to plane take off

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -3,20 +3,18 @@
 require "airport"
 
 describe Airport do
-
   #1st story / 1st test
   it {is_expected.to respond_to :plane_landed?}
 
-  #1st story / 2nd test
+  #1st story / 3rd test
   it 'receives a plane' do
     plane = subject.plane_landed?
     # expect(plane).to be_confirm_landed?
     expect(plane).to respond_to :confirm_landed?
   end
+end
 
+describe Airport do
   #2nd story / 1st test
   it {is_expected.to respond_to :plane_take_off}
-
-
-
 end
