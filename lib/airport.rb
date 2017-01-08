67abb10695380c9017a8dc1bbb570f286ccc1d76
Rick Clark <rkingsleyclark@gmail.com>
2017-01-08 09:59:29 +0000

    Added unit test for airport_name

--- lib/airport.rb
+++ lib/airport.rb
@@ -2,7 +2,7 @@ class Airport
   attr_reader :airport_name
 
   def initialize(name)
-
+    @airport_name = name
   end
 
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,15 +1,22 @@
 require "airport.rb"
 
 describe Airport do
+  test_airport = Airport.new("Paris")
 
   describe ".new" do
     it "takes an argument" do
-      expect{Airport.new("Paris")}.not_to raise_error
+      expect{test_airport}.not_to raise_error
+    end
+    it "saves argument to @airport_name" do
+      expect(test_airport.airport_name).to eq "Paris"
     end
   end
 
-  subject(:airport) { Airport.new("Paris") }
-  it { is_expected.to respond_to(:airport_name) }
+  describe ":airport_name" do
+    subject(:airport) { test_airport }
+    it { is_expected.to respond_to(:airport_name) }
+  end
+
 
 
 end
