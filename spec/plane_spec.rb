3978b1976ffa6fff95263b0f2c3618ff640c36d7
Shereen Finney <shereenfinney@gmail.com>
2017-01-08 17:16:18 +0000

    Added test to plane_spec to test that a plane instance will confirm it is not in the airport anymore

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -12,6 +12,15 @@ describe Plane do
 end
 
 describe Plane do
-  #Second story / 2nd test
-  it {is_expected.to respond_to :confirm_not_in_airport?}
+    #Second story / 2nd test
+    it {is_expected.to respond_to :confirm_not_in_airport?}
+
+  #2nd story / 4th test
+  it "confirms to the airport it is not in the airport" do
+    expect(subject.confirm_not_in_airport).to eq("I'm not in the airport")
+  end
+
+
+
+
 end
