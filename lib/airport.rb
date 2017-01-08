d5246d7491cbc2f05e8e86e2ff8c324be59eec25
Edyta Wrobel <edyta.wrobel.willdoit@gmail.com>
2017-01-08 13:13:32 +0000

    raise error when airport full; confirm when plane took off

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,13 +1,46 @@
 class Airport
+  DEFAULT_CAPACITY = 200
+  attr_reader :planes, :capacity
 
-  attr_reader :plane
+  def initialize(capacity=DEFAULT_CAPACITY)
+    @planes = []
+    @capacity = capacity
+  end
 
   def land(plane)
-    fail 'No slots available' if @plane
-    @plane = plane
+    fail "No landing slots available!" if full?
+    @planes << plane
+    @planes[0]
   end
 
-  def take_off
-    puts "plane taken off!"
+  def take_off(plane)
+    @planes.pop
   end
+
+  private
+
+  def full?
+    @planes.count >= capacity
+  end
+
 end
+
+# it 'instructs a plane to land and confirm that it landed' do
+#     plane = Plane.new
+#     expect(subject.land(plane)).to eq plane
+#   end
+#
+#  it 'instructs a plane to take off' do
+#     plane = Plane.new
+#     subject.land(plane)
+#     expect(subject.take_off(plane)).to eq plane
+#   end
+#
+#   it 'confirms that the plane is no longer in the airport' do
+#     expect(subject.planes).to be_empty
+#   end
+#
+#   it 'prevents landing when the airport is full' do
+#     Airport::DEF_CAPACITY.times {subject.land(Plane.new)}
+#     expect{subject.land(Plane.new)}.to raise_error
+#   end
