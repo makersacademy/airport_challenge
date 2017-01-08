237363a4d2e2a5ff8ae8e69977052687ad321b21
Kat Hicks <kmhicks92@gmail.com>
2017-01-08 10:35:46 +0000

    Developed tests so that they make use of stubs/mocks. Realised that stubs are now depreciated and should use mocks

--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -6,10 +6,12 @@ describe Weather do
   end
 
   it 'should return "sunny" as an outcome of check_weather' do
+    allow(subject).to receive(:chance) { 84 }
     expect(subject.check_weather).to eq("sunny")
   end
 
   it 'should return "stormy" as an outcome of check_weather' do
+    allow(subject).to receive(:chance) { 95 }
     expect(subject.check_weather).to eq("stormy")
   end
 end
