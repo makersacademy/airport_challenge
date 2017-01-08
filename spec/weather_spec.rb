778cfefdd3a1185c632522491481439cc8b7c8b6
Kat Hicks <kmhicks92@gmail.com>
2017-01-08 10:19:52 +0000

    Set another test for weather expecting check_weather to return 'sunny'

--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -4,4 +4,7 @@ describe Weather do
   it 'responds to the method check_weather' do
     expect(subject).to respond_to :check_weather
   end
+  it 'should return "sunny" as an outcome of check_weather' do
+    expect(subject.check_weather).to eq("sunny")
+  end
 end
