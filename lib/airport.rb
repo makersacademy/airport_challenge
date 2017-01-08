b631cb630a464e81c54c62a136622e6777eede56
Justinio14 <jbutwell@hotmail.com>
2017-01-08 20:51:10 +0000

    Error if plane attempts to land at full airport

--- lib/airport.rb
+++ lib/airport.rb
@@ -3,30 +3,34 @@ require_relative 'weather'
 
 class Airport
 
-  attr_accessor :plane, :weather, :sunny
+  DEFAULT_CAPACITY = 20
 
-  def initialize(weather = Weather.new)
+  attr_accessor :plane, :weather, :sunny, :arrivals
+
+  def initialize(capacity = DEFAULT_CAPACITY, weather = Weather.new)
     @plane = plane
     @Weather = weather
     @sunny = weather.sunshine
-
+    @arrivals = []
+    @capacity = capacity
   end
 
   def landed(plane)
       fail "Unable to depart due to stormy weather" unless @sunny
-
-      @plane = plane
+      fail "Airport full" if full?
+      @arrivals << plane
 
   end
 
   def departure
     fail "Unable to depart due to stormy weather" unless @sunny
-      @plane
+
+      @arrivals.shift
 
   end
 
   def full?
-
+    @arrivals.count >= @capacity
   end
 
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -6,7 +6,7 @@ describe Airport do
   let(:weather) { double :weather, :sunshine= => true, sunshine: true}
   let(:plane) { double :plane }
 
-  it { is_expected.to respond_to :landed}
+  it { is_expected.to respond_to(:landed).with(1).argument }
 
   it 'confirms a plane has landed' do
     allow(weather).to receive(:sunshine).and_return(true)
@@ -31,6 +31,12 @@ describe Airport do
   end
 
   it { is_expected.to respond_to :full?}
+
+
+  it "gives an error when airport is full" do
+    Airport::DEFAULT_CAPACITY.times { subject.landed(plane) }
+    expect{subject.landed(plane)}.to raise_error 'Airport full'
+  end
   
   #it 'prevents planes from departing if stormy' do
   #expect {subject.stormy}.to raise_error 'Unable to depart due to stormy weather'
