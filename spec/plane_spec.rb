ae96315d538878fb67d9191bc04f82ed241ba13c
Dylan Rhodius <dylanrhodius@hotmail.com>
2017-01-09 09:37:03 +0000

    Add plane departed test.

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -1,8 +1,14 @@
 require './lib/plane'
 
 describe Plane do
+
   it {is_expected.to respond_to :status?}
   it {is_expected.to respond_to :landed}
   it {is_expected.to respond_to :departed}
+
+  it 'sets the plane Object\'s status to departed' do
+    expect(subject.departed).to eq("Plane departed.")
+  end
+
 
 end
