2fe8aff1577bfe1ea81e400364719edcd9dfdaff
Rob Brentnall <rob.brentnall@gmail.com>
2017-01-09 08:31:58 +0000

    Added on_ground and airbourne private methods

--- lib/airport.rb
+++ lib/airport.rb
@@ -16,17 +16,25 @@ class Airport
   def fleet(plane)
     if plane.landing || plane.landed
       raise 'Airport is full' if full?
-      raise 'Plane is already on the ground' if @planes.include?(plane)
+      raise 'Plane is already on the ground' if on_ground?(plane)
       @airbourne_planes.delete(plane)
       @planes << plane
     elsif plane.taking_off || plane.taken_off
-      raise 'Plane is already in the air' unless @planes.include?(plane)
+      raise 'Plane is already in the air' unless airbourne?(plane)
       @planes.delete(plane)
       @airbourne_planes << plane
     end
   end
 
   private
+
+  def on_ground?(plane)
+    @planes.include?(plane)
+  end
+
+  def airbourne?(plane)
+    @planes.include?(plane)
+  end
 
   def full?
     @planes.size >= @capacity
