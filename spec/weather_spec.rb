58e7acdace76b152af19a18a6153093ea3b832f0
Pedro Castanheira <josepedrocastanheira@gmail.com>
2017-01-09 10:05:18 +0000

    weather test added

--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -2,7 +2,7 @@ require 'weather'
 
 describe Weather do
 
-  #it {expect(subject.class).to eq(Weather)}
+  it {expect(subject.class).to eq(Weather)}
   it {expect(Weather).to respond_to(:stormy_weather?)}
 
 end
