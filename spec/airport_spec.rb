1fe097802e440d20077b307bdbc3aee9224caea7
Noora Al Qasimi <n.k.alqasimi@gmail.com>
2017-02-18 15:50:40 +0000

    passed test for landing when airport is full

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -11,6 +11,12 @@ let(:plane) { Plane.new }
     it 'lands a plane when instructed' do
       expect(subject.land(plane)).to eq plane
     end
+
+    it 'doesn\'t land a plane when airport is full' do
+      full_error = 'Cannot land, airport full'
+      subject.land(plane)
+      expect{ subject.land(plane) }.to raise_error full_error
+    end
   end
 
   describe '#take_off' do
