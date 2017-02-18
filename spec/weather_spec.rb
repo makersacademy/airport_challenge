52bd67b4098f89853032b6da26e5e96521331347
Ashwini <ashie.oh@gmail.com>
2017-02-18 13:36:50 +0000

    weather returns false if not stormy

--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -8,4 +8,9 @@ describe Weather do
     allow(@weather).to receive(:is_stormy?)
     expect(@weather).to respond_to(:is_stormy?)
   end
+
+  it "returns false when not stormy" do
+    allow(@weather).to receive(:is_stormy?){false}
+    expect(@weather.is_stormy?).to eq false
+  end
 end
