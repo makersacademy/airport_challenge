7dac96532880e68b303a97362a142129e2b3dd9b
Vicky Ledsom <victorialedsom@gmail.com>
2017-02-18 16:10:13 +0000

    First test complete- Instructs a plane to land

---
+++ lib/airport.rb
@@ -0,0 +1,5 @@
+class Airport
+  def landing
+    true
+  end
+end
---
+++ spec/airport_spec.rb
@@ -0,0 +1,8 @@
+require 'airport'
+
+describe Airport do
+
+  it "instructs a plane to land" do
+  expect(subject.landing).to eq true
+  end
+end
