b300211fcad029a658c0190c06f5caba860f801f
Ashwini <ashie.oh@gmail.com>
2017-02-18 13:58:19 +0000

    airport object has a default capacity

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,4 +1,11 @@
 class Airport
+  attr_reader :capacity
+
+  def initialize
+    @capacity = 10
+  end
+
   def is_full?
+    # true or false
   end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -4,8 +4,12 @@ describe Airport do
   before(:each) do
     @airport = Airport.new
   end
-  
+
   it "it responds to is_full?" do
     expect(@airport).to respond_to(:is_full?)
+  end
+
+  it "has a default capacity" do
+    expect(@airport.capacity).to eq 10
   end
 end
