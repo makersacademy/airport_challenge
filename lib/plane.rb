58ae87c989882c4417a38d88a4bd63656098f06d
Ashwini <ashie.oh@gmail.com>
2017-02-18 12:42:59 +0000

    plane accepts only strings as arg, raises error otherwise and status is unchanged

--- lib/plane.rb
+++ lib/plane.rb
@@ -2,7 +2,8 @@ class Plane
   attr_accessor :status
 
   def land(airport)
-
+    fail "Invalid airport" unless airport.respond_to?(:chars) # duck typing
+    self.status = "Landed at #{airport}"
   end
 
 end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -3,11 +3,25 @@
 require 'plane'
 
 describe Plane do
+  before(:each) do
+    @plane = Plane.new
+  end
+
   it "lands when instructed to land" do
-    expect(subject).to respond_to(:land).with(1).argument
+    expect(@plane).to respond_to(:land).with(1).argument
   end
 
   it "confirms that it has landed" do
-    expect(subject).to respond_to(:status)
+    expect(@plane).to respond_to(:status)
+  end
+
+  it "status changes to landed after landing" do
+    @plane.land("LHR")
+    expect(@plane.status).to eq "Landed at LHR"
+  end
+
+  it "raises an error if invalid airport name passed, status remains unchanged" do
+    expect {@plane.land(123)}.to raise_error("Invalid airport")
+    expect(@plane.status).to eq nil
   end
 end
