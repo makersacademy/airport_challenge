1bc5a0c158c736268aeaefcc62bbfe747792cceb
Vicky Ledsom <victorialedsom@gmail.com>
2017-02-18 18:20:43 +0000

    Refactor to use arrays to see when plane is in the air or at the airport

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,11 +1,24 @@
-class Airport
-  attr_reader :plane
+require_relative 'plane'
 
-  def instruct_to_land(plane)
-    @plane = plane
+class Airport
+  attr_reader :plane, :in_air, :in_airport
+
+  def initialize
+    @in_airport = []
+    @in_air = []
   end
 
-  def landed?(plane)
-    @plane = plane
+  def land(plane)
+    #adds plane to the in_airport array
+    @in_airport.push(plane)
+  end
+
+  # def landed?(plane)
+  #   @plane = plane
+  # end
+
+  def take_off(plane)
+    #adds plane to the in_air array
+    @in_air.push(plane)
   end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -4,10 +4,23 @@ describe Airport do
   let(:plane) {double(:plane)}
 
   it "instructs a plane to land" do
-    expect(subject.instruct_to_land(plane)).to eq plane
+    expect(subject).to respond_to(:land).with(1).argument
   end
 
-  it "confirms when plane has landed" do
-    expect(subject.landed?(plane)).to eq plane
+  it "confirms the plane is in the airport" do
+    # expect(subject.landed?(plane)).to eq plane
+    new_plane = Plane.new
+    subject.land(new_plane)
+    expect(subject.in_airport).to include(new_plane)
+  end
+
+  it "instructs a plane to take off" do
+    expect(subject).to respond_to(:take_off).with(1).argument
+  end
+
+  it "confirms the plane is airborne"do
+    new_plane = Plane.new
+    subject.take_off(new_plane)
+    expect(subject.in_air).to include(new_plane)
   end
 end
