95eaa21d75bd7511efcc2430633a779b88754b3f
Albert <ayanit1@googlemail.com>
2017-02-18 15:12:15 +0000

    add spec for take_off?

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -2,8 +2,12 @@ require 'plane'
 
 describe Plane do
 
-  it 'responds to land?' do
+  it 'land?' do
     expect(subject).to respond_to(:land?)
+  end
+
+  it 'take_off?' do
+    expect(subject).to respond_to(:take_off?)
   end
 
 end
