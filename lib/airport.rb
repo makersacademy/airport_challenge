de47c9fe7363d646f61f4d1a5a407a0edba36876
Rory McGuinness <rorymcguinness@hotmail.co.uk>
2017-02-18 13:54:13 +0000

    confirm landed now takes argument to check specific plane object

--- lib/airport.rb
+++ lib/airport.rb
@@ -14,14 +14,20 @@ class Airport
     @plane = plane
   end
 
-  def confirm_landed
-    fail "No plane has landed" if empty?
-    "#{@plane} has landed"
+  def confirm_landed(plane)
+    @plane = plane
+    fail "There are no planes at the airport." if empty?
+    fail "#{@plane} not in airport." unless landed?
+    "#{@plane} has landed."
   end
 
   private
   def empty?
     @plane_arr.empty? ? true : false
+  end
+
+  def landed?
+    @plane_arr.include?(@plane) ? true : false
   end
 
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,18 +1,38 @@
 require 'airport'
 
 describe Airport do
-  it "instructs a plane to land" do
-    plane = Plane.new
-    expect(subject.instruct_to_land(plane)).to eq plane
+  let(:airport) {described_class.new}
+
+  describe "#landing", :landing do
+    let(:plane) {Plane.new}
+    it "instructs a plane to land" do
+      expect(airport.instruct_to_land(plane)).to eq plane
+    end
+
+    it "confirms the plane has landed" do
+      airport.instruct_to_land(plane)
+      expect(airport.confirm_landed(plane)).to eq "#{plane} has landed."
+    end
+
+    it "reports the plane hasn't landed when it hasn't" do
+      expect{airport.confirm_landed(plane)}.to raise_error "There are no planes at the airport."
+    end
   end
 
-  it "confirms the plane has landed" do
-    plane = Plane.new
-    subject.instruct_to_land(plane)
-    expect(subject.confirm_landed).to eq "#{plane} has landed"
-  end
+  # describe "#take_off", :takeoff do
+  #   it "instructs a plane to take off" do
+  #     expect(airport.take_off(plane)).to eq plane
+  #   end
+  #
+  #   it "confirms the plane has taken off" do
+  #     expect(airport.confirm_takeoff).to eq "#{plane} has taken off."
+  #   end
+  #
+  #   it "reports the plane hasn't taken off when it hasn't" do
+  #     expect{airport.confirm_takeoff}.to raise_error "Plane has not taken off"
+  #   end
+  #
+  # end
 
-  it "reports the plane hasn't landed when it hasn't" do
-    expect{subject.confirm_landed}.to raise_error "No plane has landed"
-  end
+
 end
