c23335d585bdbf4c2cc4f4a45a3c2087d2157282
Bernard Malhame <bernard.malhame@gmail.com>
2017-02-18 16:59:30 +0000

    added default capacity

--- lib/airport.rb
+++ lib/airport.rb
@@ -2,12 +2,30 @@ require_relative 'plane'
 
 class Airport
 
+DEFAULT_CAPACITY = 20
+
+attr_reader :capacity
+
+def initialize(capacity = DEFAULT_CAPACITY)
+  @capacity = capacity
+  @planes =[]
+end
+
 def stormy?
   false
 end
 
+def full?
+  if @planes.count >= capacity
+    true
+  else
+    false
+  end
+end
+
 def land(plane)
-  fail 'Cannot land plane because of stormy weather' if self.stormy?
+  fail 'Cannot land plane because of stormy weather or at full capacity' if self.stormy? || self.full?
+  @planes << plane
   plane.landed
 end
 
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -15,6 +15,10 @@ describe Airport do
 
   it { is_expected.to respond_to :take_off}
 
+  it 'has a default capacity' do
+    expect(subject.capacity).to eq Airport::DEFAULT_CAPACITY
+  end
+
   describe '#land' do
     it 'lands a plane' do
     plane = Plane.new
@@ -34,8 +38,19 @@ describe Airport do
     airport = Airport.new
     plane = Plane.new
     airport.stormy?
-    expect { airport.land(plane) }.to raise_error 'Cannot land plane because of stormy weather'
+    expect { airport.land(plane) }.to raise_error 'Cannot land plane because of stormy weather or at full capacity'
    end
  end
+
+ describe '#land' do
+   it 'raises an error when airport full' do
+    subject.capacity.times { subject.land Plane.new }
+    expect { subject.land Plane.new }.to raise_error 'Cannot land plane because of stormy weather or at full capacity'
+    # airport = Airport.new
+    # airport.full?
+    # expect { airport.land(plane) }.to raise_error 'Cannot land plane because of stormy weather or at full capacity'
+   end
+ end
+
 
 end
