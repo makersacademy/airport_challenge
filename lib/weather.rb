3caba2285f3a5373b705748a2ee3a154719d6e85
Ashwini <ashie.oh@gmail.com>
2017-02-18 13:34:40 +0000

    used a double instead of weather object

--- lib/weather.rb
+++ lib/weather.rb
@@ -1,4 +1,5 @@
 class Weather
   def is_stormy?
+    [0,1].sample == 0 ? false : true
   end
 end
--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -2,9 +2,10 @@ require 'weather'
 
 describe Weather do
   before do
-    @weather = Weather.new
+    @weather = double(:weather)
   end
   it "has a is_stormy method" do
+    allow(@weather).to receive(:is_stormy?)
     expect(@weather).to respond_to(:is_stormy?)
   end
 end
