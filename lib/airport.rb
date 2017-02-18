1177f5169ae76cf9e0ddf037b3631d3bcb34d747
Vicky Roberts <vicki_uk@hotmail.com>
2017-02-18 21:06:52 +0000

    refactored to private methods

--- lib/airport.rb
+++ lib/airport.rb
@@ -9,13 +9,22 @@ attr_reader :planes
   end
 
   def land(plane)
-    raise "Airport is full" unless @planes.count < 20
+    raise "Airport is full" if airport_full?
     @planes << plane
   end
 
   def take_off
-    raise "Airport is empty" if @planes.empty?
+    raise "Airport is empty" if airport_empty?
     @planes.pop
+  end
+
+private
+  def airport_full?
+    @planes.count >= 20
+  end
+
+  def airport_empty?
+    @planes.empty?
   end
 
 end
