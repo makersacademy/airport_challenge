99ff67c67ab236181bc6e99b3626eab72e92e493
Bernard Malhame <bernard.malhame@gmail.com>
2017-02-18 15:02:41 +0000

    added manually-entered stormy weather condition

--- lib/airport.rb
+++ lib/airport.rb
@@ -2,11 +2,17 @@ require_relative 'plane'
 
 class Airport
 
+def stormy?
+  false
+end
+
 def land(plane)
+  fail 'Cannot land plane because of stormy weather' if self.stormy?
   plane.landed
 end
 
 def take_off(plane)
+  fail 'Cannot let plane take off because of stormy weather' if self.stormy?
   plane.taken_off
 end
 
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,5 +1,5 @@
 require 'airport'
-
+require 'plane'
 
 describe Airport do
 
@@ -14,5 +14,28 @@ describe Airport do
   end
 
   it { is_expected.to respond_to :take_off}
+
+  describe '#land' do
+    it 'lands a plane' do
+    plane = Plane.new
+    expect(subject.land(plane)).to eq true
+  end
+ end
+
+ describe '#take_off' do
+   it 'lets a plane take off' do
+   plane = Plane.new
+   expect(subject.take_off(plane)).to eq false
+  end
+ end
+
+ describe '#land' do
+   it 'raises an error when stormy weather' do
+    airport = Airport.new
+    plane = Plane.new
+    airport.stormy?
+    expect { airport.land(plane) }.to raise_error 'Cannot land plane because of stormy weather'
+   end
+ end
 
 end
