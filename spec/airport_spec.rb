b8e56b2a414c18b91e7d6586c6e0a92a285634c4
Ashwini <ashie.oh@gmail.com>
2017-02-19 09:42:19 +0000

    Airport checks conditions, if met, instructs a plane to land or take off & confirms

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -12,7 +12,7 @@ describe Airport do
   let(:new_york_plane)  {Plane.new}
 
   it "has a default capacity" do
-    expect(@airport.capacity).to eq Airport::DEFAULT_CAPACITY
+    expect(Airport.new("BHR").capacity).to eq Airport::DEFAULT_CAPACITY
   end
 
   it "default capacity can be overriden" do
@@ -21,14 +21,29 @@ describe Airport do
 
 
   it "gets a request from a landed plane to take-off, and allows it to take-off", :focus => true do
-    @airport.permission_to_land(@plane)
-    expect(@airport.permission_to_take_off(@plane)).to be true
+    london_airport.permission_to_land(london_plane)
+    expect(london_airport.permission_to_take_off(london_plane)).to be true
+  end
+
+  it "confirms that a flight has taken off" do
+    london_airport.permission_to_land(london_plane)
+    london_airport.permission_to_take_off(london_plane)
+    expect(london_plane.status).to eq "In-Flight"
+    expect(london_airport.planes).to_not include(london_plane)
   end
 
   it "gets a request from a in-flight plane to land, and allows it to land" do
-    @airport.permission_to_land(@plane)
-    @airport.permission_to_take_off(@plane)
-    expect(@airport.permission_to_land(@plane)).to be true
+    london_airport.permission_to_land(london_plane)
+    london_airport.permission_to_take_off(london_plane)
+    expect(london_airport.permission_to_land(london_plane)).to eq true
+  end
+
+  it "confirms that a flight has landed" do
+    london_airport.permission_to_land(london_plane)
+    london_airport.permission_to_take_off(london_plane)
+    london_airport.permission_to_land(london_plane)
+    expect(london_plane.status).to eq "Landed"
+    expect(london_airport.planes).to include(london_plane)
   end
 
   it "gets a request from a landed plane to land, and it raises a status error" do
