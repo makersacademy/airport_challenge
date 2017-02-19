29407de174d85fd04217b04ebdfa4b73a12d2e05
Ashwini <ashie.oh@gmail.com>
2017-02-19 14:07:17 +0000

    tested for code coverage

--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -9,6 +9,10 @@ describe Weather do
     expect(@weather).to respond_to(:is_stormy?)
   end
 
+  it "it's is_stormy? method returns only boolean" do
+    expect(@weather.is_stormy?).to be(true).or be(false)
+  end
+
   it "returns false when not stormy" do
     allow(@weather).to receive(:is_stormy?){false}
     expect(@weather.is_stormy?).to eq false
