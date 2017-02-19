890cdd1e0bfc128e547d929946817a21b58f97b3
allthatilk <imogenkutz@hotmail.com>
2017-02-19 00:38:31 +0000

    Fulfilled 2nd user story

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,13 +1,30 @@
-require 'plane'
+#require_relative 'plane'
 
 class Airport
 
+  def initialize
+    @gateways = []
+  end
+
   def land(plane)
     @plane = plane
+    @gateways << @plane
+    @gateways[-1]
   end
 
   def confirm_landing
-    "The #{@plane} has landed at #{self}."
+    "The #{@gateways[-1]} has landed at #{self}."
+  end
+
+  def take_off(plane)
+    @plane = plane
+    @gateways -=[plane]
+    @gateways
+  end
+
+  def empty_gateway(plane) # Consider using empty_gateway? instead
+    message = "The gateway is empty as #{plane} has taken off."
+    return message unless @gateways.include?(plane)
   end
 
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,15 +2,28 @@ require 'airport'
 
 describe Airport do
   subject(:airport) {described_class.new}
+  let(:plane) {double :plane}
 
-  it "the plane lands in the airport" do
-    plane = double("This Plane")
+  it "lands the plane in the airport" do
     expect(subject.land(plane)).to eq plane
   end
 
   it "confirms the plane has landed" do
-    plane = subject.land(Plane.new)
+    plane = subject.land(plane)
     expect(subject.confirm_landing).to eq "The #{plane} has landed at #{subject}."
+  end
+
+  it "gets the plane to take off from the airport" do
+    plane = subject.land(plane)
+    gateways = subject.take_off(plane)
+    expect(subject.take_off(plane)).not_to include(plane)
+  end
+
+  it {is_expected.to respond_to :empty_gateway}
+
+  it "shows plane has left the airport" do
+    message = "The gateway is empty as #{plane} has taken off."
+    expect(subject.empty_gateway(plane)).to eq message
   end
 
 end
