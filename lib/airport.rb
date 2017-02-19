3622ab0720167b062d259c0e2e99e2919613535b
Christos <chris.paraskeva10@gmail.com>
2017-02-19 10:27:53 +0000

    added information that will diplay current plane status in airport / cleaned up code and added some comments

--- lib/airport.rb
+++ lib/airport.rb
@@ -8,17 +8,13 @@ class Airport
   def initialize
     @landed = []
     @taken_off = []
-    #@latest_plane
-    @landing_plane
-    #@flying
+    #@landing_plane
     @current_plane = []
     @weather = 1
-    #@plane = []
-    #@new_plane_landed = Plane.new #false
   end
 
   def confirm_landed?
-    if (@landing_plane == @landed.pop)
+    if (@current_plane == @landed[-1])
       true
     else
       false
@@ -32,7 +28,7 @@ class Airport
       false
     end
   end
-
+# will randomly decide whether it is stormy or not
   def stormy?(weather=@weather)
     if (weather == 3)
       true
@@ -44,20 +40,22 @@ class Airport
   def weather_generator
     @weather = [1,1,2,2,2,2,3,4,5].sample
   end
-
+# will land plane if weather is not stormy and there is space available at airport
   def land_plane
     fail 'Airport full' if airport_full?
     weather_generator
     if stormy?
       fail 'Stormy weather preventing landing'
     else
-      @landing_plane = Plane.new # false
-      @landed << @landing_plane
+      #@landing_plane = Plane.new
+      @current_plane = Plane.new
+      @landed << @current_plane
+      display_airport_information
     end
   end
-# consider removing this one and the test
+# checks to confirm that the same plane that was instructed to take off is the one that left the airport
   def plane_left_airport?
-      if (@current_plane == @taken_off.pop) && !(@current_plane == nil)
+      if (@current_plane == @taken_off[-1]) && !(@current_plane == nil)
         true
       else
         false
@@ -68,6 +66,21 @@ class Airport
     @landed.count
   end
 
+  def display_airport_information
+    singular = "plane that has"
+    singular_is = "is currently"
+    plural = "planes that have"
+    plural_are = "are currently"
+
+    if (@landed.count == 1)
+      puts "There #{singular_is} #{@landed.count} #{singular} landed."
+    elsif (@landed.count == 0)
+      puts "There #{plural_are} no #{plural} landed."
+    else
+      puts  "There #{plural_are} #{@landed.count} #{plural} landed."
+    end
+  end
+
   def available_plane?
     if (landed_planes > 0)
       true
@@ -75,7 +88,7 @@ class Airport
       false
     end
   end
-
+# will allow plane to take off if the weather is not stormy and there is an available plane
   def take_off
     fail 'No available planes' if !available_plane?
     weather_generator
@@ -84,7 +97,7 @@ class Airport
       else
         @current_plane = landed.pop
         @taken_off.push(@current_plane)
-        plane_left_airport?
+        display_airport_information
       end
     end
   end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -10,6 +10,34 @@ describe Airport do
       plane = subject.land_plane
       expect(subject.land_plane).to eq plane
     end
+
+    it 'raises an error when trying to land when airport is full' do
+      allow(subject).to receive(:stormy?).and_return(false)
+      DefaultCapacity::MEDIUM.times { subject.land_plane }
+      expect { subject.land_plane }.to raise_error 'Airport full'
+    end
+
+  # cannot work out how to test for a puts statement output.
+  # use something like capybara?
+
+    # it 'tests to see whether the correct landing information is displayed after land command' do
+    #   allow(subject).to receive(:stormy?).and_return(false)
+    #   subject.land_plane
+    #   #allow(subject).to receive(:puts).and_return('There is currently 1 plane that has landed.')
+    #   expect(subject.land_plane).to eq('There is currently 1 plane that has landed.')
+    # end
+    #   it 'tests to see whether the correct landing information is displayed after land command' do
+    #
+    #   5.times { subject.land_plane }
+    #   #allow(subject).to receive(:puts).and_return('There is currently 1 plane that has landed.')
+    #   #expect(subject.land_plane).to output ('There is currently 1 plane that has landed.')
+    #   specify { expect { subject.land_plane }.to output('foo').to_stdout }
+    # end
+
+    it 'raises an error when trying to land plane in stormy weather' do
+      allow(subject).to receive(:stormy?).and_return(true)
+      expect { subject.land_plane }.to raise_error 'Stormy weather preventing landing'
+    end
   end
 
   describe '#confirm_landed?' do
@@ -22,13 +50,6 @@ describe Airport do
     end
   end
 
-  describe '#land_plane' do
-    it 'raises an error when trying to land when airport is full' do
-      allow(subject).to receive(:stormy?).and_return(false)
-      DefaultCapacity::MEDIUM.times { subject.land_plane }
-      expect { subject.land_plane }.to raise_error 'Airport full'
-    end
-  end
 
   describe '#take_off', :focus => true do
     it 'confirm take off / confirm correct plane takes off' do
@@ -38,24 +59,17 @@ describe Airport do
       allow(subject).to receive(:stormy?).and_return(false)
       expect(subject.take_off).to eq plane
     end
-  end
 
-  # describe '#plane_left_airport?' do
-  #   it 'confirms the correct plane left airport' do
-  #     plane = Plane.new
-  #     planes = []
-  #     allow(subject).to receive(:available_plane?).and_return(true)
-  #     allow(subject).to receive(:stormy?).and_return(false)
-  #     #allow(subject).to receive(:take_off).and_return(planes.push(plane))
-  #     expect(subject.plane_left_airport?).to eq true
-  #   end
-  # end
-
-  describe '#take_off', :focus_stormy => true do
     it 'raises an error when trying to take off in stormy weather' do
       allow(subject).to receive(:available_plane?).and_return(true)
       allow(subject).to receive(:stormy?).and_return(true)
       expect { subject.take_off }.to raise_error 'Stormy weather preventing take-off'
+    end
+
+    it 'raises an error when trying to take off when there are no planes that have landed' do
+      allow(subject).to receive(:available_plane?).and_return(false)
+      allow(subject).to receive(:stormy?).and_return(false)
+      expect { subject.take_off }.to raise_error 'No available planes'
     end
   end
 
@@ -63,21 +77,6 @@ describe Airport do
     it 'confirms if there is a plane available for take-off' do
       allow(subject).to receive(:landed_planes).and_return(1)
       expect(subject.available_plane?).to eq true
-    end
-  end
-
-  describe '#land_plane', :focus_stormy_landing => true do
-    it 'raises an error when trying to land plane in stormy weather' do
-      allow(subject).to receive(:stormy?).and_return(true)
-      expect { subject.land_plane }.to raise_error 'Stormy weather preventing landing'
-    end
-  end
-
-  describe '#take_off', :takeoff_no_planes => true do
-    it 'raises an error when trying to take off when there are no planes that have landed' do
-      allow(subject).to receive(:available_plane?).and_return(false)
-      allow(subject).to receive(:stormy?).and_return(false)
-      expect { subject.take_off }.to raise_error 'No available planes'
     end
   end
 
