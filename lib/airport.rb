ec08366877efcbb0c7eaaed80190cbb673001fe7
Vicky Roberts <vicki_uk@hotmail.com>
2017-02-19 17:54:49 +0000

    extracted take-off/landing conditions to separate methods and added confirmation messages

--- lib/airport.rb
+++ lib/airport.rb
@@ -12,23 +12,41 @@ class Airport
   end
 
   def land(plane)
-    raise "Airport is full" if airport_full?
-    raise "Too stormy for landing" if stormy?
-    raise "Plane already landed" if @planes.include?(plane)
+    landing_conditions(plane)
     plane.landed
+    confirm_landing
     @planes << plane
   end
 
   def take_off(plane)
-    raise "Plane is already flying" if plane.in_flight
-    raise "Airport is empty" if airport_empty?
-    raise "Too stormy for take off" if stormy?
+    take_off_conditions(plane)
     plane.flying
+    confirm_take_off
     @planes.delete(plane)
   end
 
 
   private
+
+  def landing_conditions(plane)
+    raise "Plane already landed" if @planes.include?(plane)
+    raise "Airport is full" if airport_full?
+    raise "Too stormy for landing" if stormy?
+  end
+
+  def take_off_conditions(plane)
+    raise "Plane is already flying" if plane.in_flight
+    raise "Airport is empty" if airport_empty?
+    raise "Too stormy for take off" if stormy?
+  end
+
+  def confirm_landing
+    "Landing successful."
+  end
+
+  def confirm_take_off
+    "Take off successful."
+  end
 
   def stormy?
     Weather.new.randomize
