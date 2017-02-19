80ee1882a330d4fd9c0dc07ec2720df7fd724440
Vicky Ledsom <victorialedsom@gmail.com>
2017-02-19 21:25:50 +0000

    Make capacity adjustable and refactor (complete user story 5 & 6)

--- lib/airport.rb
+++ lib/airport.rb
@@ -3,10 +3,11 @@ require_relative 'weather'
 
 class Airport
   attr_reader :planes, :take_off, :stormy, :full
+  attr_accessor :capacity
 
   DEFAULT_CAPACITY = 10
 
-  def initialize
+  def initialize(capacity = DEFAULT_CAPACITY)
     @planes = [] #in airport
     @weather = Weather.new.todays_forecast
     @capacity = DEFAULT_CAPACITY
@@ -17,17 +18,17 @@ class Airport
   end
 
   def full?
-     @planes.size == @capacity
+     planes.size == @capacity
   end
 
   def land(plane)
     raise "It isn't safe to land." if stormy?
     raise "The airport is full, you must wait." if full?
-    @planes << plane
+    planes << plane
   end
 
   def take_off(plane)
     raise "It isn't safe to fly." if stormy?
-    @planes.delete(plane)
+    planes.delete(plane)
   end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -4,15 +4,11 @@ require 'airport'
 describe Airport do
   let(:plane) {double(:plane)}
 
-  it "gives current airport capacity" do
-    expect(subject).to respond_to(:full)
-  end
-
   it "checks for stormy weather" do
     expect(subject).to respond_to(:stormy?)
   end
 
-  describe '#land' do
+  describe "#land" do
     it "instructs a plane to land" do
       expect(subject).to respond_to(:land).with(1).argument
     end
@@ -32,7 +28,7 @@ describe Airport do
       expect{subject.land(plane)}.to raise_error "The airport is full, you must wait."
     end
 
-    context 'when stormy' do
+    context "when stormy" do
       it "won't allow a plane to land when stormy" do
         allow(subject).to receive(:stormy?).and_return(true)
         expect{subject.land(plane)}.to raise_error("It isn't safe to land.")
@@ -40,7 +36,7 @@ describe Airport do
     end
   end
 
-  describe '#take-off' do
+  describe "#take-off" do
     it "instructs a plane to take off" do
       expect(subject).to respond_to(:take_off).with(1).argument
     end
@@ -51,11 +47,26 @@ describe Airport do
       expect(subject.planes).not_to include(plane)
     end
 
-    context 'when stormy' do
+    context "when stormy" do
       it "won't allow a plane to take off when stormy" do
         allow(subject).to receive(:stormy?).and_return(true)
         expect{subject.take_off(plane)}.to raise_error("It isn't safe to fly.")
       end
+    end
+  end
+
+  describe "capacity" do
+    it "has a default capacity" do
+      expect(subject.capacity).to eq Airport::DEFAULT_CAPACITY
+    end
+
+    it "has an adjustable capacity" do
+      airport = Airport.new(30)
+      Airport::DEFAULT_CAPACITY.times do
+        allow(subject).to receive(:stormy?).and_return(false)
+        subject.land(plane)
+      end
+      expect{subject.land(plane)}.to raise_error("The airport is full, you must wait.")
     end
   end
 end
