0194856221cbab7e41d339fbd7476091cdd50143
Flora Harvey <fharvey555@gmail.com>
2017-02-19 16:50:34 +0000

    Added test for random weather generator

--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -1,21 +1,18 @@
+require 'spec_helper'
 require 'weather'
 
 class DummyWeatherChecker
 end
 
 describe 'Weather' do
+
   before :each do
     @weather_checker = DummyWeatherChecker.new
     @weather_checker.extend(Weather)
   end
 
-  it "returns true when stormy" do
-    allow(@weather_checker).to receive(:stormy?).and_return(true)
+  it "returns stormy weather when rand is greater than 0.7" do
     expect(@weather_checker.stormy?).to eq true
   end
 
-  it "returns false when not stormy" do
-    allow(@weather_checker).to receive(:stormy?).and_return(false)
-    expect(@weather_checker.stormy?).to eq false
-  end
 end
