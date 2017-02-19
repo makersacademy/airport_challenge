ec896d102839eb12251146cea8e9903a6921ad35
Vicky Ledsom <victorialedsom@gmail.com>
2017-02-19 23:17:53 +0000

    Add before methods to keep tests DRY

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -3,24 +3,27 @@ require 'airport'
 describe Airport do
   let(:plane) {double(:plane)}
 
+  before(:each) do
+      allow(subject).to receive(:stormy?).and_return(false)
+  end
+
   it "checks for stormy weather" do
     expect(subject).to respond_to(:stormy?)
   end
 
   describe "#land" do
+
     it "instructs a plane to land" do
       expect(subject).to respond_to(:land).with(1).argument
     end
 
     it "confirms the plane is in the airport" do
-      allow(subject).to receive(:stormy?).and_return(false)
       allow(plane).to receive(:land)
       subject.land(plane)
       expect(subject.planes).to include(plane)
     end
 
     it "wont allow a plane to land when the airport is full" do
-      allow(subject).to receive(:stormy?).and_return(false)
       Airport::DEFAULT_CAPACITY.times do
         subject.land(plane)
       end
@@ -28,6 +31,7 @@ describe Airport do
     end
 
     context "when stormy" do
+
       it "won't allow a plane to land when stormy" do
         allow(subject).to receive(:stormy?).and_return(true)
         expect{subject.land(plane)}.to raise_error("It isn't safe to land.")
@@ -36,12 +40,13 @@ describe Airport do
   end
 
   describe "#take-off" do
+
     it "instructs a plane to take off" do
       expect(subject).to respond_to(:take_off).with(1).argument
     end
 
     it "confirms the plane is airborne" do
-      allow(subject).to receive(:stormy?).and_return(false)
+      # allow(subject).to receive(:stormy?).and_return(false)
       subject.take_off(plane)
       expect(subject.planes).not_to include(plane)
     end
@@ -62,7 +67,6 @@ describe Airport do
     it "has an adjustable capacity" do
       airport = Airport.new(30)
       Airport::DEFAULT_CAPACITY.times do
-        allow(subject).to receive(:stormy?).and_return(false)
         subject.land(plane)
       end
       expect{subject.land(plane)}.to raise_error("The airport is full, you must wait.")
