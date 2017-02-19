90e6efa406f28b12df9114522faf7d27f54401ec
Ryan <Ryanchu.ws@gmail.com>
2017-02-19 12:34:46 +0000

    Basic structure of airport constructed; weather functionality yet to be added

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,9 +1,17 @@
 require_relative 'plane'
 
 class Airport
+attr_reader :avail_planes
+
+  def initialize
+    @avail_planes = []
+  end
+
   def plane_landing(plane)
+    @avail_planes << plane
   end
 
   def plane_takeoff
+    @avail_planes.pop
   end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -10,4 +10,15 @@ describe Airport do
   it "allows planes to take_off" do
     expect(subject).to respond_to :plane_takeoff
   end
+
+  it "can confirm whether an airplane has landed" do
+    plane1 = Plane.new
+    expect(subject.plane_landing(plane1)).to eq subject.avail_planes
+  end
+
+  it "allows planes that have landed to take_off" do
+    plane1 = Plane.new
+    subject.plane_landing(plane1)
+    expect(subject.plane_takeoff).to eq plane1
+  end
 end
