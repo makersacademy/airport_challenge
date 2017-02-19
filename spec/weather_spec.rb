ef0cef493f10882a9cc7a35979dd09935badf2b2
Natalia Stafeeva <n.stafeeva@gmail.com>
2017-02-19 20:01:47 +0000

    Changed test for weather class

--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -5,7 +5,8 @@ describe Weather do
   it { is_expected.to respond_to :sunny? }
 
   it "should always return true or false" do
-    expect(Weather.new.sunny?).to eq ( true || false )
+    w = Weather.new.sunny?
+    expect([true, false].include? w).to eq true
   end
 
 end
