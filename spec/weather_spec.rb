7e9ef206c0f25412fd4b23fe7ad9b656b91dcfc7
Flora Harvey <fharvey555@gmail.com>
2017-02-19 16:18:32 +0000

    Added tests for weather module

--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -1,1 +1,21 @@
+require 'weather'
 
+class DummyWeatherChecker
+end
+
+describe 'Weather' do
+  before :each do
+    @weather_checker = DummyWeatherChecker.new
+    @weather_checker.extend(Weather)
+  end
+
+  it "returns true when stormy" do
+    allow(@weather_checker).to receive(:stormy?).and_return(true)
+    expect(@weather_checker.stormy?).to eq true
+  end
+
+  it "returns false when not stormy" do
+    allow(@weather_checker).to receive(:stormy?).and_return(false)
+    expect(@weather_checker.stormy?).to eq false
+  end
+end
