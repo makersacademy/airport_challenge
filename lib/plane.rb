6c6f7256f7560d312ae2cae4ce55ba23a4d652fe
Natalia Stafeeva <n.stafeeva@gmail.com>
2017-02-19 10:05:13 +0000

    Rewrote tests for plane using doubles for Airport class

--- lib/plane.rb
+++ lib/plane.rb
@@ -18,4 +18,5 @@ class Plane
     end
   end
 
+
 end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -1,23 +1,27 @@
 require 'plane'
 
 describe Plane do
-
+  
   it "should land at an airport" do
     plane = Plane.new
-    airport = Airport.new
+    airport = double(:airport)
+    allow(airport).to receive(:request_to_land).and_return(true)
     plane.land(airport)
     expect(plane.airport).to eq airport
   end
 
   it "should confirm it's landed" do
     plane = Plane.new
-    airport = Airport.new
+    airport = double(:airport)
+    allow(airport).to receive(:request_to_land).and_return(true)
     expect(plane.land(airport)).to eq true
   end
 
   it "should take off from an airport" do
     plane = Plane.new
-    airport = Airport.new
+    airport = double(:airport)
+    allow(airport).to receive(:request_to_land).and_return(true)
+    allow(airport).to receive(:request_take_off).and_return(true)
     plane.land(airport)
     plane.take_off(airport)
     expect(plane.airport).to eq nil
@@ -25,9 +29,11 @@ describe Plane do
 
   it "should confirm it took off" do
     plane = Plane.new
-    airport = Airport.new
+    airport = double(:airport)
+    allow(airport).to receive(:request_to_land).and_return(true)
+    allow(airport).to receive(:request_take_off).and_return(true)
     plane.land(airport)
     expect(plane.take_off(airport)).to eq true
   end
-  
+
 end
