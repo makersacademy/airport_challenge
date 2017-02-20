306004a46619978d8f7942e91d6decdb75e92462
Eva Balog <evabalog@Evas-MacBook-Air.local>
2017-02-20 03:42:36 +0000

    Checked weather randomness

--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -5,5 +5,10 @@ describe Weather do
   it 'Checks current weather status' do
     expect(subject).to respond_to :current_weather
   end
-  
+
+  it 'Checks if weather is stormy' do
+    allow(subject).to receive(:stormy?).and_return true
+    expect(subject.stormy?).to eq true
+  end
+
 end
