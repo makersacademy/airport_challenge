8606e763595a68a2e882996a4a5e51f0dfa34289
Eva Balog <evabalog@Evas-MacBook-Air.local>
2017-02-20 02:02:52 +0000

    Raises error if airport is full and removed direct access to instance variables

--- lib/airport.rb
+++ lib/airport.rb
@@ -3,16 +3,29 @@ require_relative 'weather'
 
 class Airport
 
-attr_reader :plane
+  DEFAULT_CAPACITY = 1
+
+attr_reader :planes, :capacity
+
+  def initialize(capacity=DEFAULT_CAPACITY)
+      @planes = []
+      @capacity = capacity
+    end
 
   def lands_plane(plane, weather)
       raise "Cannot land at airport due to adverse weather conditions" if weather == :stormy
+      raise "Airport is full, no landing" if is_full?
       plane.land
+      planes << plane
     end
 
   def take_off(plane, weather)
       raise "Cannot take off from airport due to adverse weather conditions" if weather == :stormy
       plane.flying
+      planes.pop(plane)
     end
 
+  def is_full?
+    planes.count >= capacity
+  end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -10,13 +10,15 @@ describe Airport do
     @plane = Plane.new
   end
 
+    it { is_expected.to respond_to :take_off }
+
     it 'Lands a plane' do
-      expect(subject.lands_plane(@plane, weather.current_weather)).to eq true
+      expect(subject.lands_plane(@plane, weather.current_weather)).to eq subject.planes
     end
 
-    it 'Lets plane to take off' do
-      expect(subject.take_off(@plane, weather.current_weather)).to eq false #take_off is actually self.land hence equals false here
-    end
+    # it 'Lets plane to take off' do
+    #   expect(subject.take_off(@plane, weather.current_weather)).to eq false #take_off is actually self.land hence equals false here
+    # end
   end
 
   context 'bad weather' do
@@ -35,4 +37,21 @@ describe Airport do
     end
   end
 
+    context 'capacity' do
+
+      let(:weather) { double('weather', :current_weather => :fine)}
+      before(:each) do
+        @plane = Plane.new
+      end
+
+      it 'Returns true if airport is full' do
+        subject.capacity.times {subject.lands_plane(@plane, weather.current_weather)}
+        expect(subject.is_full?).to eq true
+      end
+
+      it 'Raises an error if landing is requested when airport is full' do
+        subject.capacity.times {subject.lands_plane(@plane, weather.current_weather)}
+        expect { subject.lands_plane(@plane, weather.current_weather) }.to raise_error("Airport is full, no landing")
+    end
+  end
 end
