7c8e65f8e689900cc31c6aa47a0f144abec838ba
Jessica Rodriguez <jessicarodriguez@Jessicas-MacBook-Pro.local>
2017-03-18 15:51:55 +0000

    create test for plane docking at airport

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -4,9 +4,10 @@ require 'airport'
 describe Airport do
 	it {is_expected.to respond_to(:allow_landing).with(1).argument }
 	it {is_expected.to respond_to(:confirm_landing).with(1).argument }
+	it {is_expected.to respond_to(:dock).with(1).argument }
 	it {is_expected.to respond_to(:allow_takeoff).with(1).argument }
   
-	describe "#land_at_airport" do 
+	describe "#allow_landing" do 
 	  it "instructs plane to land at airport" do
 	    plane = double("plane")
 	    expect(subject.allow_landing(plane)).to eq plane
@@ -19,11 +20,25 @@ describe Airport do
 	  end
 	end
 
-	describe "#takeoff_from_airport" do   
+	describe "#dock" do 
+    it "docks plane at airport" do
+      plane = Plane.new
+      expect(subject.dock(plane)).to eq plane  
+    end	
+
+	end
+
+	describe "#allow_takeoff" do   
 	  it "instructs plane to take off from airport" do
       plane = double("plane")
       expect(subject.allow_takeoff(plane)).to eq plane
 	  end
+	  
+	  it "confirms that the plane has left the airport" do 
+      plane = double("plane", :has_taken_off? => true)
+      expect(plane).to have_taken_off 
+	  end	
+
 	end
 
 end
