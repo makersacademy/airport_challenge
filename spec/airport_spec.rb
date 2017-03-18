d4a339d344f3d17e7ac16ff79a35843d8bf19ad9
emmpak <eiliev05@yahoo.com>
2017-03-18 13:30:20 +0000

    Add lands the plane failing test

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -3,6 +3,11 @@ require 'airport'
 describe Airport do
   describe '#land' do
     it { is_expected.to respond_to(:land).with(1).argument }
+
+    it 'lands the plane' do
+      plane = Plane.new
+      expect(subject.land(plane)).to eq plane
+    end
   end
 
 end
