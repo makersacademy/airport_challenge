e45e691f68f02628daafb836584558c2be31c294
Eugenia Guerrero <eugenia.guerrero@me.com>
2017-03-18 11:13:41 +0000

    User story 1: adds unit test confirming plane has landed

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,3 +1,16 @@
+require 'airport'
+require 'plane'
+
 describe Airport do
   it { is_expected.to respond_to :land}
+
+  describe '#land(plane)' do
+    let (:plane) { double :plane}
+    it 'lands planes' do
+      plane = double :plane
+      allow(plane).to receive(:landed?).and_return(true)
+      subject.land(plane)
+    end
+
+  end
 end
