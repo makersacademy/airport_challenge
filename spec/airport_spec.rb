7087ad56e77158eb63c67ff3946ed3ca3afffa59
emmpak <eiliev05@yahoo.com>
2017-03-18 15:43:24 +0000

    Refactor airport_spec to use named subject

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,17 +1,18 @@
 require 'airport'
 
 describe Airport do
+  subject(:airport) { described_class.new }
 
   describe '#land' do
     it { is_expected.to respond_to(:land).with(1).argument }
     it 'lands the plane' do
       plane = Plane.new
       subject.land(plane)
-      expect(subject.planes.pop).to eq plane
+      expect(airport.planes.pop).to eq plane
     end
     it 'raises error when there is no more space at the airport' do
       subject.land(Plane.new)
-      expect { subject.land(Plane.new)}.to raise_error "The airport is full"
+      expect { airport.land(Plane.new)}.to raise_error "The airport is full"
     end
   end
 
@@ -19,8 +20,8 @@ describe Airport do
     it { is_expected.to respond_to(:planes) }
     it 'returns the landed plane' do
       plane = Plane.new
-      subject.land(plane)
-      expect(subject.planes.pop).to eq plane
+      airport.land(plane)
+      expect(airport.planes.pop).to eq plane
     end
   end
 
@@ -28,11 +29,11 @@ describe Airport do
     it { is_expected.to respond_to(:take_off) }
     it 'instruct a landed plane to take_off' do
       plane = Plane.new
-      subject.land(plane)
-      expect(subject.take_off).to eq plane
+      airport.land(plane)
+      expect(airport.take_off).to eq plane
     end
     it 'raises an error if there are no planes' do
-      expect{subject.take_off}.to raise_error "No planes at the airport"
+      expect{airport.take_off}.to raise_error "No planes at the airport"
     end
   end
 
