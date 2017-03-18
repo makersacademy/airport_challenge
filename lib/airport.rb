83ea83c6035533802c7e63caab2328d4b4f19a77
Ruan Odendaal <ruanodendaal@gmail.com>
2017-03-18 09:45:00 +0000

    create array of planes to hold more than one

--- lib/airport.rb
+++ lib/airport.rb
@@ -2,9 +2,18 @@ require_relative 'plane'
 
 class Airport
 
+  attr_reader :planes
+
+  def initialize
+    @planes = []
+  end
+
   def land(plane)
     plane.landed?
   end
 
+  def add_plane(plane)
+    planes << plane
+  end
 
 end
