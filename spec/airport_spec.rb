872057be1ff671ffc1fe469126347d937c76de14
Pete Wilkins <pete.p.wilkins@gmail.com>
2017-03-18 12:18:36 +0000

    land method takes 1 argument

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,7 +1,15 @@
 require 'airport'
 
 describe Airport do
-  it 'responds to land' do
-    expect(subject).to respond_to :land
+
+  describe "#land" do
+
+    it 'responds to land' do
+      expect(subject).to respond_to :land
+    end
+
+    it 'land takes 1 argument' do
+      expect(subject).to respond_to(:land).with(1).argument
+    end
   end
 end
