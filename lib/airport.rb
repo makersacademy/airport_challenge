92129f3c9ff3e96a65399cdd9c494d7800bebc6d
Ruan Odendaal <ruanodendaal@gmail.com>
2017-03-18 10:26:54 +0000

    create take_off method

--- lib/airport.rb
+++ lib/airport.rb
@@ -9,11 +9,17 @@ class Airport
   end
 
   def land(plane)
+    add_plane(plane)
     plane.landed?
   end
 
   def add_plane(plane)
     planes << plane
+  end
+
+  def take_off
+    planes.pop
+    planes
   end
 
 end
