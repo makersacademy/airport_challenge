f9910bfa4429c389fc3bfe671c0d14b7ae5a257c
Eugenia Guerrero <eugenia.guerrero@me.com>
2017-03-18 13:05:44 +0000

    adds unit test asserting that plane is able to take-off from airport

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,15 +2,25 @@ require 'airport'
 require 'plane'
 
 describe Airport do
-  it { is_expected.to respond_to :land}
+  it { is_expected.to respond_to(:land).with(1).argument }
 
   describe '#land(plane)' do
-    let (:plane) { double :plane}
     it 'lands planes' do
       plane = double :plane
-      allow(plane).to receive(:landed?).and_return(true)
-      subject.land(plane)
+      expect(subject.land(plane)).to eq [plane]
     end
-
   end
+
+  describe '#take_off(plane)' do
+    let (:plane) { double :plane}
+    it 'takes-off planes' do
+      plane = double :plane
+      allow(plane).to receive(:left?).and_return(true)
+      subject.land(plane)
+      taken_off = subject.take_off
+      expect(taken_off).to be_left
+    end
+  end
+
+
 end
