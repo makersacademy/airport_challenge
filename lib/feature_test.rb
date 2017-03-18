dd612000858401c4768f6ed5b644515f25d85d58
Eugenia Guerrero <eugenia.guerrero@me.com>
2017-03-18 21:46:52 +0000

    adds feature test which uses rand method to select weather

--- lib/feature_test.rb
+++ lib/feature_test.rb
@@ -8,3 +8,16 @@ airport = Airport.new
 plane = Plane.new
 airport.land(plane)
 airport.take_off
+
+
+require './lib/weather'
+def forecast
+weather = []
+weather_select = Random.new
+weather_select = weather_select.rand(2)
+if weather_select == 0
+  weather << 'stormy'
+else
+  weather << 'sunny'
+end
+end
