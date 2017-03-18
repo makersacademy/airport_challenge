6c5e5061fa34f865108ceecf82717a9eeb4ec34b
Jack Bittiner <jack@Jacks-MacBook-Pro.local>
2017-03-18 13:11:48 +0000

    Airport test passed - airport stores plane

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,6 +1,17 @@
+require './lib/plane.rb'
+
 class Airport
 
-def release_plane
-end
+  def initialize
+    @plane = []
+  end
+
+  def release_plane(plane)
+    plane
+  end
+
+  def accept_plane(plane)
+    @plane << plane
+  end
 
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,8 +1,26 @@
 require './lib/airport.rb'
 
 describe Airport do
-  it 'responds to "release plane"' do
-    expect(subject).to respond_to :release_plane
+
+  describe '#release_plane' do
+
+    it 'responds to "release_plane"' do
+      expect(subject).to respond_to :release_plane
+    end
+
+    it 'releases an instance of plane when method called with argument' do
+      aeroplane = Plane.new
+      expect(subject.release_plane(aeroplane)).to eq aeroplane
+    end
   end
+
+  describe '#accepts_plane' do
+
+    it 'stores a plane when it is passed as an argument' do
+      aeroplane = Plane.new
+      expect(subject.accept_plane(aeroplane)).to eq [aeroplane]
+    end
+  end
+
 
 end
