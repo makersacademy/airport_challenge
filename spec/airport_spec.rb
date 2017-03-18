175dc5655a7871fd9a1b886264bcd13e407f24d1
Jessica Rodriguez <jessicarodriguez@Jessicas-MacBook-Pro.local>
2017-03-18 14:51:23 +0000

    refactor and reorganise tests

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,26 +2,28 @@ require 'airport'
 
 
 describe Airport do
-	it {is_expected.to respond_to(:grant_landing_permission).with(1).argument }
+	it {is_expected.to respond_to(:land_at_airport).with(1).argument }
 	it {is_expected.to respond_to(:confirm_landing).with(1).argument }
-	it {is_expected.to respond_to(:grant_takeoff_permission).with(1).argument }
+	it {is_expected.to respond_to(:takeoff_from_airport).with(1).argument }
   
-	describe 
-	it "instructs plane to land at airport" do
-	  plane = double("plane")
-	  expect(subject.grant_landing_permission(plane)).to eq plane
+	describe "#land_at_airport" do 
+	  it "instructs plane to land at airport" do
+	    plane = double("plane")
+	    expect(subject.land_at_airport(plane)).to eq plane
+	  end
+
+    it "confirms that plane has landed" do
+	    plane = Plane.new
+	    subject.land_at_airport(plane)
+	    expect(plane).to have_landed	
+	  end
 	end
 
-  it "confirms that plane has landed" do
-	  plane = Plane.new
-	  subject.grant_landing_permission(plane)
-	  expect(plane).to have_landed	
+	describe "#takeoff_from_airport" do   
+	  it "instructs plane to take off from airport" do
+      plane = double("plane")
+      expect(subject.grant_takeoff_permission(plane)).to eq plane
+	  end
 	end
-
-	it "instructs plane to take off from airport" do
-    plane = double("plane")
-    expect(subject.grant_takeoff_permission(plane)).to eq plane
-	end
-	
 
 end
