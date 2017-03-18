15d4714a0a752ced088fd34afd8956cb1e0a4c14
Jessica Rodriguez <jessicarodriguez@Jessicas-MacBook-Pro.local>
2017-03-18 19:21:57 +0000

    create instruct plane functionality to manage planes

--- lib/airport.rb
+++ lib/airport.rb
@@ -2,22 +2,31 @@ require_relative "plane"
 
 class Airport
 
-	attr_reader :plane
+	attr_reader :planes
+
+	def initialize
+      @planes = []
+	end
+
+	def instruct(plane)
+	  planes << plane
+	end
 
 	def allow_landing(plane)
-	  plane
+	  planes
 	end
 
     def confirm_landing(plane)
-	  plane	
+	  planes	
 	end	
 
 	def dock(plane)
-	  plane	
+	  raise "there are no docked planes" if planes.empty?
+	  planes
 	end
 
 	def allow_takeoff(plane)
-	  plane	
+	  planes.pop	
 	end
 
 end
