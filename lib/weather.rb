841ffb87fb72766702152b0019c3c1581dce67bb
Naz <nazmalik08@gmail.com>
2017-03-18 17:37:42 +0000

    added rand to stormy? method to set stormy weather 20% of the time

--- lib/weather.rb
+++ lib/weather.rb
@@ -1,10 +1,9 @@
 require_relative 'airport'
-#The planes can land and take off provided that the weather is sunny. Occasionally it may be stormy, in which case no planes can land or take off.
 #You will need to use a random number generator to set the weather (it is normally sunny but on rare occasions it may be stormy)
 class Weather
 
   def stormy?
-    true
+    true if rand > 0.8
   end
 
 end
