e92430e5940de3df0781a130a4098a661f356593
Jessica Rodriguez <jessicarodriguez@Jessicas-MacBook-Pro.local>
2017-03-18 14:10:13 +0000

    create test to confirm that the plane has landed

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -8,6 +8,10 @@ describe Airport do
 	  expect(subject.grant_landing_permission(plane)).to eq plane
 	end
 
-
+  it "confirms that plane has landed" do
+	  plane = Plane.new
+	  subject.grant_landing_permission(plane)
+	  expect(plane).to have_landed	
+	end
 	
 end
