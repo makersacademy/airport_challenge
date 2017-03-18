0e45d2f9bcbbe81e51e1b627925a05d62e28d964
Jessica Rodriguez <jessicarodriguez@Jessicas-MacBook-Pro.local>
2017-03-18 23:25:47 +0000

    update Gemfile issue with ruby version

--- Gemfile
+++ Gemfile
@@ -1,7 +1,5 @@
 source 'https://rubygems.org'
 
-ruby '2.4.0'
-
 gem 'rspec'
 gem 'capybara'
 gem 'coveralls', require: false
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -1,5 +1,6 @@
 require 'plane'
 
 describe Plane do
-  it { is_expected.to respond_to :has_landed? }	
+  it { is_expected.to respond_to :has_landed? }
+  it { is_expected.to respond_to :has_taken_off? }	
 end
