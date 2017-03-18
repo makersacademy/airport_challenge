e77208cdaa6df525eaa44753e4a7e52f9ec7c81f
Jessica Rodriguez <jessicarodriguez@Jessicas-MacBook-Pro.local>
2017-03-18 14:32:42 +0000

    refactor getter method in class

--- lib/airport.rb
+++ lib/airport.rb
@@ -2,11 +2,17 @@ require_relative "plane"
 
 class Airport
 
+	attr_reader :plane
+
 	def grant_landing_permission(plane)
-	  @plane = plane
+	  plane
 	end
 
     def confirm_landing(plane)
 	  plane	
 	end	
+
+	def grant_takeoff_permission(plane)
+	  plane	
+	end
 end
