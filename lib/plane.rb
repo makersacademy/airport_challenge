22fb7665075cb4694e351e9941934ebc17fc895d
Ruan Odendaal <ruanodendaal@gmail.com>
2017-03-19 19:00:10 +0000

    create variable to track airplane airborne status

--- lib/plane.rb
+++ lib/plane.rb
@@ -1,7 +1,21 @@
 class Plane
 
+  attr_reader :airborne
+
+  def initialize
+    @airborne = false
+  end
+
   def landed?
     true
+  end
+
+  def plane_status(status)
+    status == "airborne" ? @airborne = true : "landed"
+  end
+
+  def airborne?
+    @airborne
   end
 
 end
