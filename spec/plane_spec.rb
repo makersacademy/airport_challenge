7342a0e61f45fe053498687fd9caec2cfda3d63c
Jean-Jacques Calbayrac <calbayracjj@gmail.com>
2017-03-19 13:09:07 +0000

    Unfactoring, renaming tests, stormy landing

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -8,15 +8,18 @@ describe Plane do
     expect(plane).to be_landed
   end
 
-  it "instruct a plane to land" do
+  it "instruct a plane to land when good weather" do
     plane = Plane.new
     expect(plane).to respond_to :start_landing
+    allow(plane).to receive(:weather) { 1 }
     expect(plane.start_landing).to eq true
+    expect(plane.landed?).to eq true
   end
 
-  it "instruct to take off" do
+  it "instruct to take off when good weather" do
     plane = Plane.new
     expect(plane).to respond_to :take_off
+    allow(plane).to receive(:weather) { 1 }
     expect(plane.take_off).to eq true
     expect(plane.landed?).to_not eq true
   end
@@ -27,6 +30,13 @@ describe Plane do
     allow(plane).to receive(:weather) { 6 }
     expect(plane).to respond_to :take_off
     expect(plane.take_off).to eq false
+  end
+
+  it "doesn't land if stormy weather" do
+    plane = Plane.new
+    allow(plane).to receive(:weather) { 6 }
+    expect(plane.start_landing).to eq false
+    expect(plane.landed?).to eq false
   end
 
 end
