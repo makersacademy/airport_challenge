290007bdd0227a8009310ea78a6daf73b9bf5d72
Emily Chai <echai93@gmail.com>
2017-03-19 21:26:50 +0000

    Added guard conditions to stop plane from landning if stormy and if capacity is full

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,23 +1,35 @@
+require_relative 'weather'
+require_relative 'plane'
+
 class Airport
 
-    attr_reader :all_planes, :capacity
+    attr_reader :all_planes, :capacity, :weather
 
-    DEFAULT_CAPACITY = 10
+    DEFAULT_CAPACITY = 50
 
-    def initialize
+    def initialize(capacity = DEFAULT_CAPACITY)
       @all_planes = []
-      @capacity = DEFAULT_CAPACITY
+      @capacity = capacity
+      @weather = weather
     end
 
     def land(boeing)
-      boeing.land_plane
+      fail "Not able to land, plane has already landed" if plane_landed?
+      fail "Too stormy to land the plane!" if weather == "Stormy"
+      fail "Not able to land, airport has reached its capacity" if full?
       all_planes << boeing
     end
 
     def take_off(boeing)
-      boeing.take_off_plane
+      fail "Too stormy for the plane to take off!" if weather == "Stormy"
       all_planes.delete(boeing)
     end
+
+    def weather_condition(weather)
+      @weather = weather.generate_weather
+    end
+
+private
 
     def full?
       all_planes.count >= capacity
