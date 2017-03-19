e4f4f4a382a8d9e5efde407eb1e3c566f7d601ca
Emily Chai <echai93@gmail.com>
2017-03-19 16:53:54 +0000

    Tests pass for generating random weather

---
+++ spec/weather_spec.rb
@@ -0,0 +1,16 @@
+require 'weather'
+
+describe Weather do
+
+  describe '#generate_weather' do
+    it 'Has a method that generates random weather' do
+      expect(subject).to respond_to(:generate_weather)
+    end
+
+    it 'Randomly generates stormy weather' do
+      allow_any_instance_of(Random).to receive(:rand).and_return(1)
+      expect(subject.generate_weather).to eq "Stormy"
+    end
+  end
+
+end
