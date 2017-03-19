224ae3c2cb2bd5cc6e549834505060c2f08d08eb
Colin Turner <colinturner34@gmail.com>
2017-03-19 11:37:27 +0000

    Confirm plane landed at airport

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,9 +1,18 @@
 require 'airport'
 
 describe Airport do
+
   describe '#land' do
     it 'should respond to land with one argument' do
       expect(subject).to respond_to(:land).with(1).argument
+    end
+  end
+
+  describe 'planes' do
+    it 'should return the planes at the airport' do
+      plane = double(:plane)
+      subject.land(plane)
+      expect(subject.planes).to eq [plane]
     end
   end
 end
