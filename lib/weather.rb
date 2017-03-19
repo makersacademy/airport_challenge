529945d119db2e944311812307c11196b2211631
Ruan Odendaal <ruanodendaal@gmail.com>
2017-03-19 14:06:59 +0000

    split out random number generator

--- lib/weather.rb
+++ lib/weather.rb
@@ -2,8 +2,13 @@ class Weather
 
 # change to random sunny or stormy generator later
   def stormy?
+    @stormy = random.even? ? true : false
+  end
+
+  private
+
+  def random
     num = rand(10)
-    @stormy = num.even? ? true : false
   end
 
 end
