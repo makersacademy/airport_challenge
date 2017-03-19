ac734d2565ee5c73b17fb91f9f85cfee6e7f922b
Eugenia Guerrero <eugenia.guerrero@me.com>
2017-03-19 19:58:35 +0000

    adds unit test which tests for default capacity, adds functionality for default capacity

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -21,6 +21,9 @@ describe Airport do
       Airport::DEFAULT_CAPACITY.times { subject.land double :plane}
       expect { subject.land double(:plane) }.to raise_error 'Airport full'
     end
+    it 'has a default capacity' do
+      expect(subject.capacity).to eq Airport::DEFAULT_CAPACITY
+    end
   end
 
   describe '#take_off' do
