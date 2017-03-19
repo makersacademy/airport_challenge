ac3d9dd256d8e29f6f6d8dcf469b93f1f18fca10
Ruan Odendaal <ruanodendaal@gmail.com>
2017-03-19 13:58:46 +0000

    remove comments and reintroduce tests

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -2,9 +2,9 @@ require 'plane'
 
 describe Plane do
 
-  # it 'landed' do
-  #   expect(subject.landed?).to eq true
-  # end
+  it 'landed' do
+    expect(subject.landed?).to eq true
+  end
 
 
 end
