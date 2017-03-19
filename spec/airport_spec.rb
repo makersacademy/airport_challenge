4fa44edad74a98e713be9512dbd56a14db4d8869
Jessica Rodriguez <jessicarodriguez@Jessicas-MacBook-Pro.local>
2017-03-19 07:51:29 +0000

    refactor tests to remove docking capabilities for planes

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -4,7 +4,7 @@ require 'airport'
 describe Airport do
 	it {is_expected.to respond_to(:allow_landing).with(1).argument }
 	it {is_expected.to respond_to(:confirm_landing).with(1).argument }
-	it {is_expected.to respond_to(:dock).with(1).argument }
+	# it {is_expected.to respond_to(:dock).with(1).argument }
 	it {is_expected.to respond_to(:allow_takeoff).with(1).argument }
   
 	describe "#instruct" do
@@ -23,32 +23,32 @@ describe Airport do
 	  end
 
     it "confirms that plane has landed" do
-	    plane = Plane.new
-	    subject.confirm_landing(plane)
-	    expect(plane).to have_landed	
+    	plane = double("plane")
+	    landed_plane = subject.allow_landing(plane)
+	    expect(subject.confirm_landing(landed_plane)).to eq landed_plane	
 	  end
 	end
 
-	describe "#dock" do 
-    it "docks plane at airport" do
-      plane = double("plane")
-      subject.instruct(plane)
-      expect(subject.dock(plane)).to eq [plane]  
-    end	
-
-    it "raises error when plane has left airport" do
-      plane = double("bike")
-      
-      expect {subject.dock(plane)}.to raise_error "there are no docked planes"
-    
-    end	
-
-	end
+	# describe "#dock" do 
+ #    it "docks plane at airport" do
+ #    	plane = double("plane")
+ #    	subject.instruct(plane)
+ #    	landed_plane = subject.allow_landing(plane)
+ #      subject.confirm_landing(landed_plane)
+ #      expect(subject.dock(landed_plane)).to eq landed_plane
+ #    end	
+ 
+    # it "raises error when plane has left airport" do
+    #   landed_plane = double("landed_plane")
+    #   expect {subject.dock(landed_plane)}.to raise_error "there are no docked planes"    
+    # end	
+	# end
 
 	describe "#allow_takeoff" do   
-	  it "instructs plane to take off from airport" do
-      plane = double("plane")
-      subject.instruct(plane)
+	  it "instructs a docked plane to take off from airport" do
+    	plane = double("plane")
+    	subject.instruct(plane)
+    	subject.confirm_landing(plane)
       expect(subject.allow_takeoff(plane)).to eq plane
 	  end
 	  
