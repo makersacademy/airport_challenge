d56412292918aefc5c30dc964c3139550d860fd6
Ruan Odendaal <ruanodendaal@gmail.com>
2017-03-19 15:39:04 +0000

    refactor to mock airport instead of using subject

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -3,16 +3,14 @@ require 'airport'
 describe Airport do
  let(:plane)   { Plane.new }
  let(:weather) { Weather.new }
+ let(:airport) { subject }
 
   describe '#initialization' do
-    # it 'creates a empty array for planes' do
-    #   expect(subject.planes).to eq planes
-    # end
 
     it 'has sets default capacity as 30' do
-      allow(subject).to receive(:stormy?) { false }
-      30.times { subject.land(plane) }
-      expect { subject.land(plane) }.to raise_error "Airport full...jog on"
+      allow(airport).to receive(:stormy?) { false }
+      30.times { airport.land(plane) }
+      expect { airport.land(plane) }.to raise_error "Airport full...jog on"
     end
 
     it 'has a variable capacity' do
@@ -29,42 +27,42 @@ describe Airport do
     it { is_expected.to respond_to(:land) }
 
     it 'instructs a plane to land at a specific airport and confirm it has landed' do
-      allow(subject).to receive(:stormy?) { false }
-      expect(subject.land(plane)).to eq true
+      allow(airport).to receive(:stormy?) { false }
+      expect(airport.land(plane)).to eq true
     end
 
     it 'contains plane that has landed' do
-      allow(subject).to receive(:stormy?) { false }
-      subject.land(plane)
-      expect(subject.planes).to include plane
+      allow(airport).to receive(:stormy?) { false }
+      airport.land(plane)
+      expect(airport.planes).to include plane
     end
 
     it 'should prevent landing in stormy weather' do
-      allow(subject).to receive(:stormy?) { true }
-      expect { subject.land(plane) }.to raise_error "Cannot land due to bad weather"
+      allow(airport).to receive(:stormy?) { true }
+      expect { airport.land(plane) }.to raise_error "Cannot land due to bad weather"
     end
 
   end
 
   describe '#add_plane' do
     it 'should add a plane' do
-      expect(subject.add_plane(plane)).to eq [plane]
+      expect(airport.add_plane(plane)).to eq [plane]
     end
   end
 
 
   describe '#take_off' do
     it 'should take_off' do
-      allow(subject).to receive(:stormy?) { false }
-      subject.land(plane)
-      expect(subject.take_off(plane)).to eq plane
+      allow(airport).to receive(:stormy?) { false }
+      airport.land(plane)
+      expect(airport.take_off(plane)).to eq plane
     end
 
     it 'should prevent take_off if weather stormy' do
-      allow(subject).to receive(:stormy?) { false }
-      subject.land(plane)
-      allow(subject).to receive(:stormy?) { true }
-      expect { subject.take_off(plane) }.to raise_error "Cannot take off due to bad weather"
+      allow(airport).to receive(:stormy?) { false }
+      airport.land(plane)
+      allow(airport).to receive(:stormy?) { true }
+      expect { airport.take_off(plane) }.to raise_error "Cannot take off due to bad weather"
     end
 
 
