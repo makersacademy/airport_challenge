99ac1163ef85f1e0d9eb6c0f0caa070861715c1a
Ruan Odendaal <ruanodendaal@gmail.com>
2017-03-19 14:39:42 +0000

    do not let planes land if airport full

--- lib/airport.rb
+++ lib/airport.rb
@@ -3,16 +3,21 @@ require_relative 'weather'
 
 class Airport
 
+  DEFAULT_CAPACITY = 30
+
   attr_reader :planes
   attr_reader :weather
+  attr_reader :capacity
 
-  def initialize#weather: Weather.new
+  def initialize(capacity=DEFAULT_CAPACITY)
     @planes = []
     @weather = Weather.new
+    @capacity = capacity
   end
 
   def land(plane)
     raise "Cannot land due to bad weather" if stormy?
+    raise "Airport full...jog on" if full?
     add_plane(plane)
     plane.landed?
   end
@@ -30,6 +35,10 @@ class Airport
 
   def stormy?
     @weather.stormy?
+  end
+
+  def full?
+     planes.size >= capacity
   end
 
 end
