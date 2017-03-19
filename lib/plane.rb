c2c2ad65ab07553e73cf247ce92a980a2dde46f3
Kate <allbecauseyoutoldmeso@yahoo.co.uk>
2017-03-19 16:14:04 +0000

    Add fly and land functions to plane class

--- lib/plane.rb
+++ lib/plane.rb
@@ -1,19 +1,19 @@
 class Plane
 
-  # def initialize
-  #   @flying = true
-  # end
-  #
-  # def flying?
-  #   @flying
-  # end
-  #
-  # def land
-  #   @flying = false
-  # end
-  #
-  # def fly
-  #   @flying = true
-  # end
+  def initialize
+    @flying = true
+  end
+
+  def flying?
+    @flying
+  end
+
+  def land
+    @flying = false
+  end
+
+  def fly
+    @flying = true
+  end
 
 end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -2,22 +2,22 @@ require 'plane'
 
   describe Plane do
 
-    # it { is_expected.to respond_to :flying? }
-    # it { is_expected.to respond_to :fly }
-    # it { is_expected.to respond_to :land }
-    #
-    # describe '#fly' do
-    #   it 'makes the plane be flying' do
-    #     subject.fly
-    #     expect(subject).to be_flying
-    #   end
-    # end
-    #
-    # describe '#land' do
-    #   it 'stops the plane flying' do
-    #     subject.land
-    #     expect(subject).not_to be_flying
-    #   end
-    # end
+    it { is_expected.to respond_to :flying? }
+    it { is_expected.to respond_to :fly }
+    it { is_expected.to respond_to :land }
+
+    describe '#fly' do
+      it 'makes the plane be flying' do
+        subject.fly
+        expect(subject).to be_flying
+      end
+    end
+
+    describe '#land' do
+      it 'stops the plane flying' do
+        subject.land
+        expect(subject).not_to be_flying
+      end
+    end
 
   end
