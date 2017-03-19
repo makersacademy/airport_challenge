1e06c41fb594792c3af4e287fe0ab6c3e36ea5f0
Naz <nazmalik08@gmail.com>
2017-03-19 18:14:22 +0000

    added plane_in_airport? method and encapsulated

--- lib/airport.rb
+++ lib/airport.rb
@@ -12,17 +12,19 @@ DEFAULT_CAPACITY = 20
   end
 
   def land(plane)
+
     fail "Plane is on the ground" unless plane.flying?
     fail "Airport is full" if full?
-    fail "Too stormy to land" if self.stormy?
+    fail "Too stormy to land" if stormy?
     plane.plane_lands
     @planes_in_airport << plane
   end
 
   def take_off(plane)
+
     fail "Plane is in the air" if plane.flying?
-    fail "Plane not in airport" unless self.planes_in_airport.include?(plane)
-    fail "Too stormy to take off" if self.stormy?
+    fail "Plane not in airport" unless plane_in_airport?(plane)
+    fail "Too stormy to take off" if stormy?
     plane.plane_takes_off
     @planes_in_airport.delete(plane)
   end
@@ -31,6 +33,10 @@ DEFAULT_CAPACITY = 20
 
   def full?
     @planes_in_airport.length >= @capacity
+  end
+
+  def plane_in_airport?(plane)
+    @planes_in_airport.include?(plane)
   end
 
 end
