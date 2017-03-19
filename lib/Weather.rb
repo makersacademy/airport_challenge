884d0ffd46c1991ae6436060594ea0522c3c9424
Nick Rupp <nickrupp95@gmail.com>
2017-03-19 11:51:53 +0000

    Refactor Weather#stormy? method

--- lib/Weather.rb
+++ lib/Weather.rb
@@ -1,18 +1,16 @@
 class Weather
 
   def number_between_1_and_x(x=100)
+
       1 + rand(x)
+      
   end
 
 
   def stormy?
-    p = number_between_1_and_x(100)
-    if p > 90
-      true
-    else
-      false
-    end
 
+    p = number_between_1_and_x(100)
+    return p > 90
 
   end
 
