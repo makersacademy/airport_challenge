01b3423281262cd50a677ce042e66f70decf8f81
Ruan Odendaal <ruanodendaal@gmail.com>
2017-03-19 15:36:08 +0000

    refactor to use mock

--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -1,16 +1,17 @@
 require 'weather'
 
 describe Weather do
+  let(:weather) { Weather.new }
 
   describe '#stormy?' do
     it 'is stormy if even number' do
-    allow(subject).to receive(:random) { 2 }
-    expect(subject.stormy?).to eq true
+    allow(weather).to receive(:random) { 2 }
+    expect(weather.stormy?).to eq true
     end
 
     it 'is NOT stormy if odd number' do
-    allow(subject).to receive(:random) { 5 }
-    expect(subject.stormy?).to eq false
+    allow(weather).to receive(:random) { 5 }
+    expect(weather.stormy?).to eq false
     end
 
   end
