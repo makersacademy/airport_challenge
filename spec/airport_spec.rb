a5618df8645113169e3d866098569adaa8fc2ad3
Kate <allbecauseyoutoldmeso@yahoo.co.uk>
2017-03-20 13:54:43 +0000

    Apply before hook to tests for launch and land

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,77 +2,73 @@ require 'airport'
 
 describe Airport do
 
+  before do
+  @plane = double('plane', :descend => @plane, :ascend => @plane, :flying? => true)
+  @weather = double('weather', :stormy? => false)
+  end
+
   describe '#land' do
 
-    before(:example) do
-    @plane = double('plane', :descend => @plane, :flying? => true)
-    @weather = double('weather', :stormy? => false)
-    end
-
-    it 'tells the plane to descend' do
-      subject.land(@plane, @weather)
-      expect(@plane).to have_received(:descend)
-    end
-    it 'puts the plane in the airport' do
-      expect(subject.land(@plane, @weather)).to eq [@plane]
-    end
-    it 'raises an error if weather is stormy' do
-      weather = double('weather', :stormy? => true)
-      expect { subject.land(@plane, weather) }.to raise_error 'Weather warning'
-    end
-    it 'raises an error if airport is at full capacity' do
-      Airport::DEFAULT_CAPACITY.times do
-        plane = double('plane', :descend => plane, :flying? => true)
-        subject.land(plane, @weather)
+    context 'when fair weather' do
+      it 'tells the plane to descend' do
+        subject.land(@plane, @weather)
+        expect(@plane).to have_received(:descend)
+      end
+      it 'puts the plane in the airport' do
+        expect(subject.land(@plane, @weather)).to eq [@plane]
+      end
+      it 'raises an error if airport is at full capacity' do
+        Airport::DEFAULT_CAPACITY.times do
+          plane = double('plane', :descend => plane, :flying? => true)
+          subject.land(plane, @weather)
       end
       plane = double('plane', :descend => plane, :flying? => true)
       expect { subject.land(plane, @weather) }.to raise_error 'Airport is full'
      end
      it 'raises an error if this plane has already landed' do
-       plane = double('plane', :descend => plane, :flying? =>false)
-       expect { subject.land(plane, @weather)}.to raise_error 'This plane has already landed'
+       allow(@plane).to receive(:flying?).and_return false
+       expect { subject.land(@plane, @weather)}.to raise_error 'This plane has already landed'
      end
+   end
+
+   context 'when stormy' do
+     it 'raises an error if weather is stormy' do
+       allow(@weather).to receive(:stormy?).and_return true
+       expect { subject.land(@plane, @weather) }.to raise_error 'Weather warning'
+     end
+   end
+
   end
 
   describe '#launch' do
     it 'launches a plane' do
-      plane = double('plane', :descend => plane, :ascend => plane, :flying? => true)
-      weather = double('weather', :stormy? => false)
-      subject.land(plane, weather)
-      expect(subject.launch(plane, weather)).to eq plane
+      subject.land(@plane, @weather)
+      expect(subject.launch(@plane, @weather)).to eq @plane
     end
     it 'raises an error if weather is stormy' do
-      plane = double('plane', :descend => plane, :flying? => true)
-      weather = double('weather', :stormy? => false)
-      subject.land(plane, weather)
-      weather = double('weather', :stormy? => true)
-      expect { subject.launch(plane, weather) }.to raise_error 'Weather warning'
+      subject.land(@plane, @weather)
+      allow(@weather).to receive(:stormy?).and_return true
+      expect { subject.launch(@plane, @weather) }.to raise_error 'Weather warning'
     end
     it 'raises an error if there are no planes' do
-      weather = double('weather', :stormy? => false)
-      expect { subject.launch(weather) }. to raise_error 'Airport empty'
+      expect { subject.launch(@weather) }. to raise_error 'Airport empty'
     end
     it 'tells the plane to ascend' do
-      plane = double('plane', :descend => plane, :ascend => plane, :flying? => true)
-      weather = double('weather', :stormy? => false)
-      subject.land(plane, weather)
-      subject.launch(plane, weather)
-      expect(plane).to have_received(:ascend)
+      subject.land(@plane, @weather)
+      subject.launch(@plane, @weather)
+      expect(@plane).to have_received(:ascend)
     end
     it 'launches a specific plane' do
       plane1 = double('plane1', :descend => plane1, :ascend => plane1, :flying? => true)
       plane2 = double('plane2', :descend => plane2, :ascend => plane2, :flying? => true)
-      weather = double('weather', :stormy? => false)
-      subject.land(plane1, weather)
-      subject.land(plane2, weather)
-      expect(subject.launch(plane1, weather)).to eq plane1
+      subject.land(plane1, @weather)
+      subject.land(plane2, @weather)
+      expect(subject.launch(plane1, @weather)).to eq plane1
     end
     it 'raises an error if the plane is not at the airport' do
-      plane = double('plane', :descend => plane, :flying? => true)
       fakeplane = double('fakeplane', :ascend => fakeplane)
-      weather = double('weather', :stormy? => false)
-      subject.land(plane, weather)
-      expect { subject.launch(fakeplane, weather) }.to raise_error 'That plane is not here'
+      subject.land(@plane, @weather)
+      expect { subject.launch(fakeplane, @weather) }.to raise_error 'That plane is not here'
     end
   end
 
