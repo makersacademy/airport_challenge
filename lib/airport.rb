97a9b1364bdb7d2b5f38a5ef94a8d502778c64db
Jessica Rodriguez <jessicarodriguez@Jessicas-MacBook-Pro.local>
2017-03-21 08:32:20 +0000

    debug landing & departing planes when stormy weather

--- lib/airport.rb
+++ lib/airport.rb
@@ -7,54 +7,53 @@ class Airport
   attr_accessor :capacity
 
   def initialize(capacity=DEFAULT_CAPACITY )
-      @planes = []
-      @landed_planes = []
-      @weather = weather
-      @capacity = capacity
+    @planes = []
+    # @landed_planes = []
+    # @weather = weather
+    @capacity = capacity
   end
 
-  def weather_report
-    weather = rand(1..10)
-    if weather <= 4
-      "stormy"
-      else
-      "sunny"
-      end
-  end
+  # def weather_report
+  #   weather = rand(1..10)
+  #   if weather <= 4
+  #     "stormy"
+  #     else
+  #     "sunny"
+  #     end
+  # end
 
   def land(plane)
     fail "Landing denied, Airport is full" if full?
-    fail "Landing denied, weather report stormy" if weather_report == "stormy"
+    fail "Landing denied, weather report stormy" if stormy?
     puts "Fasten seatbelts, plane landing..."
     planes << plane
   end
 
-  def confirm_landing(plane)
-    planes.each do |plane|
-      landed_planes << plane	
-    end
-    puts "Plane has landed! Docked at bridge for passengers."
-    landed_planes
-    end	
-    
-  def takeoff(landed_plane)
-    fail "the flight has already departed" if empty?
-    fail "Take off denied, weather report stormy" if stormy? 
+  # def confirm_landing(plane)
+  #   planes.each do |plane|
+  #     landed_planes << plane
+  #   end
+  #   puts "Plane has landed! Docked at bridge for passengers."
+  #   landed_planes
+  #   end
+
+  def takeoff(plane)
+    fail "the plane has already departed" if empty?
+    fail "Take off denied, weather report stormy" if stormy?
     puts "Fasten seatbelts, plane taking off..."
-    landed_planes.pop
+    planes.pop
   end
 
   def plane_status
     puts "Number of Planes: #{planes}, #{planes.count} total."
-    puts "Planes on ground: #{landed_planes}, #{landed_planes.count} total."
   end
 
-  private 
+  private
 
-    attr_reader :planes, :landed_planes, :weather
+    attr_reader :planes, :weather
 
     def empty?
-      landed_planes.empty?
+      planes.empty?
     end
 
     def full?
@@ -62,6 +61,6 @@ class Airport
     end
 
     def stormy?
-      weather_report == "stormy"
+      rand(1..6) > 4
     end
 end
