7c69322fa2b868354d784d68dea22ffd3d6e15ff
Freddy Fallon <freddyfallon@gmail.com>
2017-03-21 13:41:51 +0000

    Add before blocks to remove repetition - and this time it works

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -6,36 +6,40 @@ describe Airport do
 
   describe '#land' do
     context 'when not stormy' do
-      before :each do
-        allow(airport.weather).to receive(:stormy_weather?).and_return(false)
-        allow(plane).to receive(:land_plane)
-      end
-      it 'should tell the plane to land' do
-        plane = double(:plane, airborne: true)
-        expect(airport).to receive(:land)
-        airport.land(plane)
-      end
-
-      it 'should have plane in the Airport' do
-        plane = double(:plane, airborne: true)
-        allow(plane).to receive(:land_plane)
-        airport.land(plane)
-        expect(airport.planes_in_airport).to include plane
-      end
-
-      it 'should raise error when trying to land a landed plane' do
-        plane = double(:plane, airborne: false)
-        message = "This plane is already landed."
-        expect{ airport.land(plane) }.to raise_error message
-      end
-
-      context 'when full' do
-        it 'should raise an error' do
-          plane = double(:plane, airborne: true)
+      context 'when plane is airborne' do
+        before :each do
+          allow(plane).to receive(:airborne).and_return(true)
           allow(plane).to receive(:land_plane)
-          airport.capacity.times {airport.land(plane)}
-          message = "Sorry. Airport full. Go away."
-          expect{airport.land(plane)}.to raise_error message
+          allow(airport.weather).to receive(:stormy_weather?).and_return(false)
+        end
+
+        it 'should tell the plane to land' do
+          expect(airport).to receive(:land)
+          airport.land(plane)
+        end
+
+        it 'should have plane in the Airport' do
+          airport.land(plane)
+          expect(airport.planes_in_airport).to include plane
+        end
+        context 'when airport is full' do
+          it 'should raise an error' do
+            airport.capacity.times {airport.land(plane)}
+            message = "Sorry. Airport full. Go away."
+            expect{airport.land(plane)}.to raise_error message
+          end
+      end
+      end
+      context 'when plane is not airborne' do
+        before :each do
+          allow(plane).to receive(:airborne).and_return(false)
+          allow(airport.weather).to receive(:stormy_weather?).and_return(false)
+          allow(plane).to receive(:land_plane)
+        end
+
+        it 'should raise error when trying to land a landed plane' do
+          message = "This plane is already landed."
+          expect{ airport.land(plane) }.to raise_error message
         end
       end
     end
@@ -43,27 +47,23 @@ describe Airport do
   end
 
   describe '#take_off' do
-    context 'when not stormy'
     before do
+      allow(plane).to receive(:airborne).and_return(false)
       allow(airport.weather).to receive(:stormy_weather?).and_return(false)
       allow(plane).to receive(:take_off_plane)
     end
     it 'should tell the airport to take off the plane' do
-      plane = double(:plane, airborne: false)
       expect(airport).to receive(:take_off)
       airport.take_off(plane)
     end
 
     it 'should remove plane from Airport' do
-      plane = double(:plane, airborne: false)
-      allow(plane).to receive(:take_off_plane)
       airport.planes_in_airport.push(plane)
       airport.take_off(plane)
       expect(airport.planes_in_airport).not_to include(plane)
     end
 
     it 'should raise an error when plane is not in airport' do
-      plane = double(:plane, airborne: false)
       airport.planes_in_airport.delete(plane)
       message = "Plane is not in this airport"
       expect { airport.take_off(plane) }.to raise_error message
