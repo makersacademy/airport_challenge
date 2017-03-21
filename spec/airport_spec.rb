fbe5e48a7edad3ef8af82fa41e6cdcb3d9ea04b0
Freddy Fallon <freddyfallon@gmail.com>
2017-03-21 13:10:44 +0000

    Fix before blocks and remove unneccessary repetition in airport_spec

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,41 +2,39 @@ require 'airport'
 describe Airport do
 
   subject(:airport) { described_class.new }
-  let(:plane) {double :plane} # not sure I completely get why this goes here
+  let(:plane) {double :plane}
 
   describe '#land' do
     context 'when not stormy' do
-      before do
+      before :each do
         allow(airport.weather).to receive(:stormy_weather?).and_return(false)
-      end
-      it 'should tell the plane to land' do
-        plane = double(:plane, airborne: true)
-        allow(plane).to receive(:land_plane) # can't put this into before block?
-        expect(airport).to receive(:land)
-        airport.land(plane)
-      end
-
-      it 'should have plane in the Airport' do
-        plane = double(:plane, airborne: true)
         allow(plane).to receive(:land_plane)
-        airport.land(plane)
-        expect(airport.planes_in_airport).to include plane
-      end
 
-      it 'should raise error when trying to land a landed plane' do
-        plane = double(:plane, airborne: false)
-        allow(plane).to receive(:land_plane)
-        message = "This plane is already landed."
-        expect{ airport.land(plane) }.to raise_error message
-      end
-
-      context 'when full' do
-        it 'should raise an error' do
+        it 'should tell the plane to land' do
           plane = double(:plane, airborne: true)
-          allow(plane).to receive(:land_plane)
-          airport.capacity.times {airport.land(plane)}
-          message = "Sorry. Airport full. Go away."
-          expect{airport.land(plane)}.to raise_error message
+          expect(airport).to receive(:land)
+          airport.land(plane)
+        end
+
+        it 'should have plane in the Airport' do
+          plane = double(:plane, airborne: true)
+          airport.land(plane)
+          expect(airport.planes_in_airport).to include plane
+        end
+
+        it 'should raise error when trying to land a landed plane' do
+          plane = double(:plane, airborne: false)
+          message = "This plane is already landed."
+          expect{ airport.land(plane) }.to raise_error message
+        end
+
+        context 'when full' do
+          it 'should raise an error' do
+            plane = double(:plane, airborne: true)
+            airport.capacity.times {airport.land(plane)}
+            message = "Sorry. Airport full. Go away."
+            expect{airport.land(plane)}.to raise_error message
+          end
         end
       end
     end
@@ -47,27 +45,26 @@ describe Airport do
     before do
       allow(airport.weather).to receive(:stormy_weather?).and_return(false)
       allow(plane).to receive(:take_off_plane)
-    end
 
-    it 'should tell the airport to take off the plane' do
-      plane = double(:plane, airborne: false)
-      expect(airport).to receive(:take_off)
-      airport.take_off(plane)
-    end
+      it 'should tell the airport to take off the plane' do
+        plane = double(:plane, airborne: false)
+        expect(airport).to receive(:take_off)
+        airport.take_off(plane)
+      end
 
-    it 'should remove plane from Airport' do
-      plane = double(:plane, airborne: false)
-      allow(plane).to receive(:take_off_plane)
-      airport.planes_in_airport.push(plane)
-      airport.take_off(plane)
-      expect(airport.planes_in_airport).not_to include(plane)
-    end
+      it 'should remove plane from Airport' do
+        plane = double(:plane, airborne: false)
+        airport.planes_in_airport.push(plane)
+        airport.take_off(plane)
+        expect(airport.planes_in_airport).not_to include(plane)
+      end
 
-    it 'should raise an error when plane is not in airport' do
-      plane = double(:plane, airborne: false)
-      airport.planes_in_airport.delete(plane)
-      message = "Plane is not in this airport"
-      expect { airport.take_off(plane) }.to raise_error message
+      it 'should raise an error when plane is not in airport' do
+        plane = double(:plane, airborne: false)
+        airport.planes_in_airport.delete(plane)
+        message = "Plane is not in this airport"
+        expect { airport.take_off(plane) }.to raise_error message
+      end
     end
   end
 
@@ -87,8 +84,8 @@ describe Airport do
 
     it 'should change the capacity to a different number' do
       number = double(:number)
-      airport.change_capacity(number)
-      expect(airport.capacity).to eq number
+      airport.change_capacity(10)
+      expect(airport.capacity).to eq 10
     end
   end
 
