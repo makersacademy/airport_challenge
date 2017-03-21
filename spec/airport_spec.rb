e602cac7ed835ac2176d2ae6052213fc75ce2e50
Freddy Fallon <freddyfallon@gmail.com>
2017-03-21 13:17:53 +0000

    Revert do blocks to replace tests

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -9,35 +9,37 @@ describe Airport do
       before :each do
         allow(airport.weather).to receive(:stormy_weather?).and_return(false)
         allow(plane).to receive(:land_plane)
+      end
+      it 'should tell the plane to land' do
+        plane = double(:plane, airborne: true)
+        expect(airport).to receive(:land)
+        airport.land(plane)
+      end
 
-        it 'should tell the plane to land' do
+      it 'should have plane in the Airport' do
+        plane = double(:plane, airborne: true)
+        allow(plane).to receive(:land_plane)
+        airport.land(plane)
+        expect(airport.planes_in_airport).to include plane
+      end
+
+      it 'should raise error when trying to land a landed plane' do
+        plane = double(:plane, airborne: false)
+        message = "This plane is already landed."
+        expect{ airport.land(plane) }.to raise_error message
+      end
+
+      context 'when full' do
+        it 'should raise an error' do
           plane = double(:plane, airborne: true)
-          expect(airport).to receive(:land)
-          airport.land(plane)
-        end
-
-        it 'should have plane in the Airport' do
-          plane = double(:plane, airborne: true)
-          airport.land(plane)
-          expect(airport.planes_in_airport).to include plane
-        end
-
-        it 'should raise error when trying to land a landed plane' do
-          plane = double(:plane, airborne: false)
-          message = "This plane is already landed."
-          expect{ airport.land(plane) }.to raise_error message
-        end
-
-        context 'when full' do
-          it 'should raise an error' do
-            plane = double(:plane, airborne: true)
-            airport.capacity.times {airport.land(plane)}
-            message = "Sorry. Airport full. Go away."
-            expect{airport.land(plane)}.to raise_error message
-          end
+          allow(plane).to receive(:land_plane)
+          airport.capacity.times {airport.land(plane)}
+          message = "Sorry. Airport full. Go away."
+          expect{airport.land(plane)}.to raise_error message
         end
       end
     end
+
   end
 
   describe '#take_off' do
@@ -45,27 +47,28 @@ describe Airport do
     before do
       allow(airport.weather).to receive(:stormy_weather?).and_return(false)
       allow(plane).to receive(:take_off_plane)
-
-      it 'should tell the airport to take off the plane' do
-        plane = double(:plane, airborne: false)
-        expect(airport).to receive(:take_off)
-        airport.take_off(plane)
-      end
-
-      it 'should remove plane from Airport' do 
-        plane = double(:plane, airborne: false)
-        airport.planes_in_airport.push(plane)
-        airport.take_off(plane)
-        expect(airport.planes_in_airport).not_to include(plane)
-      end
-
-      it 'should raise an error when plane is not in airport' do
-        plane = double(:plane, airborne: false)
-        airport.planes_in_airport.delete(plane)
-        message = "Plane is not in this airport"
-        expect { airport.take_off(plane) }.to raise_error message
-      end
     end
+    it 'should tell the airport to take off the plane' do
+      plane = double(:plane, airborne: false)
+      expect(airport).to receive(:take_off)
+      airport.take_off(plane)
+    end
+
+    it 'should remove plane from Airport' do
+      plane = double(:plane, airborne: false)
+      allow(plane).to receive(:take_off_plane)
+      airport.planes_in_airport.push(plane)
+      airport.take_off(plane)
+      expect(airport.planes_in_airport).not_to include(plane)
+    end
+
+    it 'should raise an error when plane is not in airport' do
+      plane = double(:plane, airborne: false)
+      airport.planes_in_airport.delete(plane)
+      message = "Plane is not in this airport"
+      expect { airport.take_off(plane) }.to raise_error message
+    end
+
   end
 
   context 'when stormy' do
