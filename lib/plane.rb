5578c1578e5592f40eeaf95bc98da459f1492e8f
Sophie Mann <sklm@live.co.uk>
2017-04-14 16:58:40 +0100

    update landed method and test to default planes as landed

--- lib/plane.rb
+++ lib/plane.rb
@@ -1,4 +1,5 @@
 class Plane
   def landed?
+    true
   end
 end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -2,4 +2,5 @@ require 'plane'
 
 describe Plane do
   it {is_expected.to respond_to :landed?}
+  it {is_expected.to be_landed}
 end
