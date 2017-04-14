aec76ac68870013ce12d4ade92880e3667282d32
Kay Lovelace <kay@herostrat.us>
2017-04-14 13:59:08 +0100

    Add local code coverage with SimpleCov

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,9 @@
-require 'coveralls'
-Coveralls.wear!
+require 'simplecov'
+require 'simplecov-console'
+
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new(
+  SimpleCov::Formatter::Console,
+  # Want a nice code coverage website? Uncomment this next line!
+  # SimpleCov::Formatter::HTMLFormatter
+)
+SimpleCov.start
