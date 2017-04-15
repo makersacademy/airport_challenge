68c5d67135460ce1dc3848d0aac898086520f969
Sophie Mann <sklm@live.co.uk>
2017-04-15 20:20:17 +0100

    updates weather tests and class

--- lib/weather.rb
+++ lib/weather.rb
@@ -1,9 +1,12 @@
 class Weather
+
+  STORM_RATE = 0.1
+
   def stormy?
     @stormy
   end
 
-  def stormy
-    @stormy = true
+  def generate_weather(rate = STORM_RATE)
+    @stormy = Random.rand >= 1.0 - rate
   end
 end
--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -1,10 +1,29 @@
 require 'weather'
 
 describe Weather do
+  let(:weather) {double :weather}
   it {is_expected.to respond_to :stormy?}
-
   it 'can be stormy' do
-    subject.stormy
+    allow(weather).to receive(:stormy?).and_return(true)
+    expect(weather).to be_stormy
+  end
+
+  it 'can be not stormy' do
+    allow(weather).to receive(:stormy?).and_return(false)
+    expect(subject).not_to be_stormy
+  end
+
+  it 'generates storms when storm rate 1' do
+    allow(weather).to receive(:generate_weather).with(1)
+    allow(weather).to receive(:stormy?)
+    subject.generate_weather(1)
     expect(subject).to be_stormy
+  end
+
+  it 'responds to storm rate of zero with no storm' do
+    allow(weather).to receive(:generate_weather).with(0)
+    allow(weather).to receive(:stormy?)
+    subject.generate_weather(0)
+    expect(subject).not_to be_stormy
   end
 end
