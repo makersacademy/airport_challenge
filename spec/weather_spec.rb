12d2324a567b898ed7c1b0a238cdc7c03b70adcf
hendrik <g9a2hvk9@me.com>
2017-04-15 11:51:58 +0200

    Adds rspec test for Weather class and passes

--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -1,4 +1,9 @@
 require "weather"
 
 describe Weather do
+  describe ".new" do
+    it "should be an instance of the Weather class" do
+      expect(subject).to be_an_instance_of Weather
+    end
+  end
 end
