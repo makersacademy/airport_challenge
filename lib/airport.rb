f5e977b8624ddbff44d9ce32741b5e4bac545143
Jessica Barclay <jessicabarclay.net@gmail.com>
2017-04-15 10:47:34 +0100

    Added initial land_plane feature. Passes all tests

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,2 +1,14 @@
+require './lib/plane'
+
 class Airport
+  attr_reader :hangar
+
+  def initialize
+    @hangar = []
+  end
+
+  def land_plane(plane)
+    @hangar << plane
+  end
+
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,7 +1,15 @@
 require './lib/airport'
+require './lib/plane'
 
 describe Airport do
-  it 'should hold, land and allow planes to take off safely' do
-    expect(subject).to be_instance_of Airport
+  it { is_expected.to be_instance_of(Airport) }
+  it { is_expected.to respond_to(:land_plane).with(1).argument }
+  it { is_expected.to respond_to(:hangar) }
+  describe '#land_plane' do
+    it "lands plane safely" do
+    plane = double(:plane)
+    subject.land_plane(plane)
+    expect(subject.hangar).to include plane
+    end
   end
 end
