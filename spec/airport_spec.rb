cb4c74d94ebbd76197840caccc672bcc29c26254
Alex Chalk <alexchalk17@gmail.com>
2017-04-15 14:17:35 -0400

    Expects Airport#dock_plane(plane) to add plane to airport's @planes

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -5,4 +5,12 @@ describe Airport do
   it { is_expected.to respond_to(:planes) }
   it { is_expected.to respond_to(:dock_plane).with(1).argument }
 
+  describe '#dock_plane' do
+    it 'adds plane to Airport\'s @planes array' do
+      plane = double(:plane)
+      subject.dock_plane(plane)
+      expect(subject.planes).to include plane 
+    end
+  end
+
 end
