8b3d975e614864deb61c4d918b45ee15f78748f5
Paula <pmuldoon@umich.edu>
2017-04-15 20:16:43 +0100

    refactored tests to nest describe

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -1,19 +1,25 @@
 require 'plane'
 
 describe 'plane' do
-  plane = Plane.new
-  it 'tests whether an instance of plane responds to land method' do
-    expect(plane).to respond_to :land
+  describe ':land' do
+    plane = Plane.new
+    it 'tests whether an instance of plane responds to land method' do
+      expect(plane).to respond_to :land
+    end
+
+    it 'tests whether a landed plan has been added to the array' do
+      plane.land(plane)
+      expect($planes_landed). to include(plane)
+    end
   end
 
-  it 'tests whether a landed plan has been added to the array' do
-    plane.land(plane)
-    expect($planes_landed). to include(plane)
-  end
+  describe 'has_landed?' do
+    plane = Plane.new
+    it 'confirms that a plane has landed' do
+      plane.land(plane)
+      expect(plane). to respond_to :has_landed?
+    end
 
-  it 'confirms that a plane has landed' do
-    plane.land(plane)
-    expect(plane). to respond_to :has_landed?
   end
 
 end
