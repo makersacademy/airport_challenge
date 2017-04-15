0b59c75592b2ceb876984e3bb44117185f0f0108
BenNoonan1991 <ben.noonan2@gmail.com>
2017-04-15 20:34:32 +0100

    Adds and passes test for land method in plane class

--- lib/plane.rb
+++ lib/plane.rb
@@ -1,3 +1,7 @@
 class Plane
 
+  def land(destination_airport)
+
+  end
+
 end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -2,4 +2,6 @@ require 'plane'
 
 describe Plane do
 
+  it { is_expected.to respond_to(:land).with(1).argument }
+
 end
