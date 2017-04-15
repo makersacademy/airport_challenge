ab58867b3fc54347cbdf19b579b82ed8ef472a23
Alex Chalk <alexchalk17@gmail.com>
2017-04-15 15:06:08 -0400

    Plane expected to receive has_landed? when airport.dock_plane(plane)

--- spec/feature_spec.rb
+++ spec/feature_spec.rb
@@ -6,7 +6,7 @@ feature 'Air_traffic_control', :type => :feature do
     and_there_is_a_plane
     when_i_instruct_a_plane_to_land_at_an_airport
     then_the_plane_should_land_at_the_airport
-    and_the_plane_should_confirm_that_it_has_landed
+    and_the_airport_should_confirm_with_the_plane_that_it_has_landed
   end
   def given_there_is_an_airport
     @airport = Airport.new
@@ -24,10 +24,11 @@ feature 'Air_traffic_control', :type => :feature do
     expect(@airport.planes).to include @plane
   end
 
-  def and_the_plane_should_confirm_that_it_has_landed
-    expect(@plane).to receive(:docked)
-    @plane.docked
-    expect(@plane).to have_confirmed_landing
+  def and_the_airport_should_confirm_with_the_plane_that_it_has_landed
+    spy_plane = spy(:plane)
+    @airport.dock_plane(spy_plane)
+    expect(spy_plane).to have_received(:has_landed?)
+    expect(@plane.has_landed?).to eq true
   end
 
 end
