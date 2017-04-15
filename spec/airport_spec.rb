26664b035332d58bdeecbda08a04c96ee0eb6560
Bertie Wooles <bertie.wooles@hotmail.co.uk>
2017-04-15 12:59:00 +0100

    Adds new tests to airport_spec

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -12,4 +12,19 @@ describe '#land'do
     expect(subject.land(plane)).to eq [plane]
   end
 end
+describe '#take_off'do
+
+  it 'takes off a plane'do
+  plane = Plane.new
+  subject.land(plane)
+  expect(plane).to receive (:took_off)
+  subject.take_off(plane)
+end
+it 'is expected to remove the plane from the @planes array'do
+  plane = Plane.new
+  subject.land(plane)
+  subject.take_off(plane)
+  expect(subject.planes).not_to eq [plane]
+end
+end
 end
