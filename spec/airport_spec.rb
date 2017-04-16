f5471b06dfa17a0334b9b5d995acbd7efb6141ba
milesillsley <milesillsley@hotmail.co.uk>
2017-04-16 20:25:50 +0100

    Add edge case tests for landing and takeoffs

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -10,10 +10,22 @@ describe Airport do
     expect(subject.land(Plane.new)).to eq("Plane has landed.")
   end
 
+  it "Won't land a plane that has already landed" do
+    plane = Plane.new
+    subject.land(plane)
+    expect { subject.land(plane) }.to raise_error("Plane has aleady landed.")    
+  end
+
   it { expect(subject).to respond_to(:takeoff).with(1).argument }
 
   it "Instructs a plane to take off" do
     expect(subject.takeoff(Plane.new)).to eq("Plane has taken off.")
+  end
+
+  it "won't instruct a plane to take off if it is already flying" do
+    plane = Plane.new
+    subject.takeoff(plane)
+    expect { subject.takeoff(plane) }.to raise_error("Plane is already flying.")
   end
 
   it "prevents takeoff when the weather is stromy" do
