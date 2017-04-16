d6ebeab1c193be924667298e0f225d6cfd313303
Sara Riley <saralynnaz@gmail.com>
2017-04-16 08:05:05 +0100

    First tests - planes can land

--- lib/airport.rb
+++ lib/airport.rb
@@ -2,8 +2,8 @@ require_relative 'plane'
 
 class Airport
 
-  def land
-  
+  def land(plane)
+    plane
   end
 
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,5 +1,14 @@
 require 'airport'
 
 describe Airport do
+  it { is_expected.to respond_to :land }
+
+  it { is_expected.to respond_to(:land).with(1).argument }
+
+  it 'tells a plane to land and confirms that it has landed'  do
+    plane = Plane.new
+    expect(subject.land(plane)).to eq plane
+    expect(plane).to be_landed
+  end
 
 end
