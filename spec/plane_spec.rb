114992dbc64af1aa57804cb9a67b31c61c3ce764
Robert Jones <robert.g.jones@gmail.com>
2017-04-16 21:55:39 +0100

    Plane class responds to single argument of airport

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -1,5 +1,9 @@
 require 'plane'
 
 describe Plane do
-  it { is_expected.to respond_to :land }
+  describe "#land" do
+    it "Expects a plane to respond to #land with an argument" do
+    expect(subject).to respond_to(:land).with(1).argument
+    end
+  end
 end
