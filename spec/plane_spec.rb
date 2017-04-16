f65e22583a6789e59927824396a0d68309ea4648
Nigel Said <nigelsaid@gmail.com>
2017-04-16 10:38:22 +0100

    adds to plane_spec.rb

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -25,6 +25,14 @@ describe Plane do
     expect(plane1.status == 'airborne').to eq true
     expect(airport1.planes == []).to eq true
   end
+
+  it 'expects not to land in airport' do
+    plane1 = Plane.new("Test_plane")
+    airport1 = Airport.new("Test_port")
+    plane1.land(airport1)
+    expect(plane1.land(airport1) ==  "Test_plane already on the ground").to eq true
+  end
+
 end
 
 describe Plane do
@@ -39,4 +47,6 @@ describe Plane do
     plane1.land(airport1)
     expect(plane1.status == 'grounded').to eq false
   end
+
+
 end
--- spec/safety_spec.rb
+++ spec/safety_spec.rb
@@ -1,8 +1,1 @@
 require 'safety'
-describe Safety do
-
-  before do
-    allow_any_instance_of(Weather).to receive(:status) { "sunny" }
-  end
-
-end  
