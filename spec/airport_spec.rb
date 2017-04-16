456339404c448e5f04585ebfa337b4a69ac69065
JayWebDevCom <jaiye.s@gmail.com>
2017-04-16 13:00:28 +0100

    Add code refactor

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -18,10 +18,6 @@ describe Airport do
     expect(airport.full?).to eq true
   end
 
-  it 'has a planes array' do
-    expect(airport).to respond_to :planes
-  end
-
   it 'responds to capacity' do
     expect(airport).to respond_to :capacity
   end
@@ -43,27 +39,17 @@ describe Airport do
     expect(airport.capacity).to eq 30
   end
 
-  it 'receives landing planes into its planes array' do
-    allow(plane).to receive_messages(:land => :landed)
-    allow(weather).to receive_messages(:status => :sunny)
-    plane.land(weather, airport)
-    airport.receive(plane)
-    expect(airport.planes.count).to eq 1
-  end
-
-  it 'releases departing planes from its planes array' do
-    allow(plane).to receive_messages(:take_off => :in_air, :land => true)
-    allow(weather).to receive_messages(:status => :sunny)
-    airport.receive(plane)
-    expect(airport.planes.count).to eq 1
-    plane.take_off(weather, airport)
-    airport.release(plane)
-    expect(airport.planes.count).to eq 0
-  end
-
   it 'cannot release planes that it does not have' do
     message = "Plane is not at that airport"
     expect { airport_2.release(plane) }.to raise_error message
+  end
+
+  it 'planes are released from the planes array' do
+    allow(weather).to receive_messages(:status => :sunny)
+    allow(plane).to receive(:land)
+    plane.land(weather, airport)
+    airport.receive(plane)
+    expect(airport.release(plane)).to eq plane
   end
 
 end
