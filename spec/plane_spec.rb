7ca9e776514e244a8e5cd81f8fb1fb0704e86218
Sophie Mann <sklm@live.co.uk>
2017-04-16 13:29:44 +0100

    updates plane spec using double

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -2,20 +2,26 @@ require 'plane'
 require 'airport'
 
 describe Plane do
-  plane = Plane.new
+  let(:airport) {double :airport}
   it {is_expected.to respond_to :landed?}
   it {is_expected.not_to be_landed}
 
   it 'has landed status when landed' do
-    a = Airport.new
-    a.land(subject)
+    allow(subject).to receive(:stormy?).and_return(false)
+    allow(airport).to receive(:land).with(subject)
+    allow(subject).to receive(:landed?).and_return(true)
+    airport.land(subject)
     expect(subject).to be_landed
   end
 
   it 'is not landed after take_off' do
-    a = Airport.new
-    a.land(subject)
-    a.take_off
+    allow(subject).to receive(:stormy?).and_return(false)
+    allow(airport).to receive(:land).with(subject)
+    allow(airport).to receive(:take_off)
+    allow(subject).to receive(:landed?).and_return(true)
+    airport.land(subject)
+    airport.take_off
+    allow(subject).to receive(:landed?).and_return(false)
     expect(subject).not_to be_landed
   end
 end
