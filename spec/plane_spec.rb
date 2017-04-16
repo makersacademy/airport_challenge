736aff33a50f29b8274f99b9bf50ed5de1a32fbc
bruxelles86 <w_gant@yahoo.co.uk>
2017-04-16 15:31:45 +0100

    Challenge finished, new files included! (forgotten before)

---
+++ spec/plane_spec.rb
@@ -0,0 +1,8 @@
+require 'plane'
+
+describe Plane do
+  # simple test to check that our planes really are from our Plane class
+  it 'Creates planes' do
+    expect(subject.class).to eq Plane
+  end
+end
