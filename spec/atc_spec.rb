ae91939adbfb623c3262594e8ba913658fc17629
Sandy Cochrane <a.x.cochrane@gmail.com>
2017-04-16 16:30:49 +0100

    added tests for existence of Atc methods instruct_landing & check_location

--- spec/atc_spec.rb
+++ spec/atc_spec.rb
@@ -8,6 +8,14 @@ describe Atc do
     expect(atc).to respond_to(:check_location).with(1).arguments
     end
   end
+
+  describe "#instruct_landing" do
+    it "instructs a plane to land at airport" do
+    atc = Atc.new
+    expect(atc).to respond_to(:instruct_landing).with(2).arguments
+    end
+  end
   
+
 end
 
