e6456480d92b19e3a0823d98a87ab0f5ff6c090b
Sara Riley <saralynnaz@gmail.com>
2017-04-16 10:05:03 +0100

    First User Story complete

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,14 +1,23 @@
 require 'airport'
 
 describe Airport do
+
   it { is_expected.to respond_to :land }
 
   it { is_expected.to respond_to(:land).with(1).argument }
 
-  it 'tells a plane to land and confirms that it has landed'  do
+  it { is_expected.to respond_to :plane }
+
+  it 'tells a plane to land' do
     plane = Plane.new
     expect(subject.land(plane)).to eq plane
     expect(plane).to be_landed
+  end
+
+  it 'has the landed plane' do
+    plane = Plane.new
+    subject.land(plane)
+    expect(subject.plane).to eq plane
   end
 
 end
