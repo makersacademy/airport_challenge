052e6d1f40a1dcb4d82db14154d6190fa6eb9cfe
Robert Jones <robert.g.jones@gmail.com>
2017-04-17 21:23:31 +0100

    Tests that Weather instances respond to query #stormy and expect it to be either true or false

--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -2,5 +2,11 @@ require './lib/weather.rb'
 
 describe Weather do
   subject(:weather) { described_class.new }
-  it { is_expected.to respond_to :generate_weather }
+  it { is_expected.to respond_to :stormy }
+
+  it "Tests weather generation" do
+      expect(weather.stormy).to be(true).or be(false)
+    end
+
+
 end
