fa0534e1487c4ba9372c0e4067aa70bcba986ee3
HENRY HOBHOUSE <henryhobhouse@gmail.com>
2017-04-17 08:11:41 +0100

    updated README.md and unit tests

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,3 +1,5 @@
+require 'plane.rb'
+
 class Airport
 
   DEFAULT_CAPACITY = 10
@@ -5,32 +7,35 @@ class Airport
   attr_reader :planes
   attr_accessor :weather
 
-  def initialize(x = 1, y = DEFAULT_CAPACITY)
+  def initialize(y = DEFAULT_CAPACITY)
     @planes = []
     # Weather is good when set to '1' and stormy when '0'
-    @weather = x
+    @weather = rand_weather
     @capacity = y
   end
 
-  def land_plane(plane)
-    full?
+  def land_plane(plane = Plane.new)
+    raise ArgumentError, "Airport Full!", caller if @planes.count == @capacity
     weather?
     @planes << plane
   end
 
   def plane_take_off
+    raise ArgumentError, "Airport Empty!", caller if @planes.count.zero?
     weather?
     @planes.pop
   end
 
   def weather?
-    raise ArgumentError, "Weather Stormy!", caller if @weather == 0
+    raise ArgumentError, "Weather Stormy!", caller if @weather.zero?
   end
 
-  def full?
-    raise ArgumentError, "Airport Full!", caller if @planes.count == @capacity
+  def rand_weather
+    if rand(1..5) == 1; 0
+    else 1
+    end
   end
 
-  private :weather?, :full?
+  private :rand_weather, :weather?
 
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -3,41 +3,51 @@ require 'plane.rb'
 
 describe Airport do
 
-
   it 'allows planes to land' do
     expect(subject).to respond_to :land_plane
   end
 
   it 'remembers that a plane has landed' do
     plane = Plane.new
+    subject.weather = 1
     subject.land_plane(plane)
     expect(subject.planes[0]).to eq plane
   end
 
   it 'allows planes to take off' do
+    subject.weather = 1
     expect(subject).to respond_to :plane_take_off
   end
 
   it 'remembers that a plane has taken off' do
-  subject.land_plane("test")
-  subject.plane_take_off
-  expect(subject.planes[0]).to eq nil
+    subject.weather = 1
+    subject.land_plane
+    subject.plane_take_off
+    expect(subject.planes[0]).to eq nil
   end
 
   it 'prevents takeoff if weather is stormy' do
-    subject.land_plane("test")
+    subject.weather = 1
+    subject.land_plane
     subject.weather = 0
     expect { subject.plane_take_off }.to raise_error "Weather Stormy!"
   end
 
   it 'prevents landing if weather is stormy' do
     subject.weather = 0
-    expect { subject.land_plane("test") }.to raise_error "Weather Stormy!"
+    expect { subject.land_plane }.to raise_error "Weather Stormy!"
   end
 
   it 'prevents landing if airport is full' do
-    10.times { |plane| subject.land_plane(plane) }
-    expect { subject.land_plane("test") }.to raise_error "Airport Full!"
+    subject.weather = 1
+    10.times { subject.land_plane }
+    expect { subject.land_plane }.to raise_error "Airport Full!"
+  end
+
+  it 'prevents plane_take_off if there are no planes' do
+    subject.weather = 1
+    10.times { subject.land_plane }
+    expect { 11.times { subject.plane_take_off } }.to raise_error "Airport Empty!"
   end
 
 end
