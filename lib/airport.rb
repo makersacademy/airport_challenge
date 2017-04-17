ef02c026b5d68956a3812429740347b95fdb71ac
hendrik <g9a2hvk9@me.com>
2017-04-17 18:20:40 +0200

    refactors airport and airport_spec; Especially the .takeoff and .land functions. RSpec now passes. Coverage is still poor.

--- lib/airport.rb
+++ lib/airport.rb
@@ -11,24 +11,28 @@ class Airport
   end
 
   def takeoff
-    raise "Takeoff not possible due to stormy weather" if !empty? && Weather.new.instance_variable_get(:@weather) == "Stormy"
-    raise "Takeoff not possible, as no planes present" if empty? && Weather.new.instance_variable_get(:@weather) == "Sunny"
+    @weather = Weather.new.instance_variable_get(:@weather)
+    fail "Takeoff not possible due to stormy weather" if !empty? && @weather == "Stormy"
+    fail "Takeoff not possible, as no planes present" if empty? && @weather == "Sunny"
     @planes.pop
   end
 
   def land(plane)
-    raise "Landing not possible due to stormy weather" if Airport.full? == false && Weather.new.instance_variable_get(:@weather) == "Stormy"
-    raise "Landing not possible. This airport is full." if Airport.full? == true && Weather.new.instance_variable_get(:@weather) == "Sunny"
+    @weather = Weather.new.instance_variable_get(:@weather)
+    fail "Landing not possible due to stormy weather" if full? == false && @weather == "Stormy"
+    fail "Landing not possible. This airport is full." if full? == true && @weather == "Sunny"
     @planes << plane
   end
-
-  private
 
   def empty?
     @planes.empty?
   end
 
-  def full?
-    @planes.count >= @capacity
+  def full
+    if @planes.count >= @capacity
+      true
+    else
+      false
+    end
   end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -18,7 +18,14 @@ describe Airport do
       expect(subject).to respond_to(:takeoff)
     end
     it "should fail when @planes is empty" do
-      expect(subject.takeoff).to raise RuntimeError, "Takeoff not possible, as no planes present" if subject.instance_variable_get(:@planes).empty? && Weather.new.instance_variable_get(:@weather) == "Sunny"
+      @weather = Weather.new(0)
+      @planes = Airport.new.instance_variable_get(:@planes)
+      expect(subject.takeoff).to raise RuntimeError, "Takeoff not possible, as no planes present" if @planes.empty? && @weather == "Sunny"
+    end
+    it "should fail when weather is Stormy" do
+      @weather = Weather.new(1)
+      @planes = Airport.new.instance_variable_get(:@planes)
+      expect(subject.takeoff).to raise RuntimeError, "Takeoff not possible. Weather is stormy" if !@planes.empty? && @weather == "Stormy"
     end
   end
 
@@ -27,10 +34,12 @@ describe Airport do
       expect(subject).to respond_to(:land).with(1).argument
     end
     it "should fail when airport is full" do
-      expect(subject.land).to fail "Landing not possible. This airport is full." if Airport.send(:full?) && Weather.new.instance_variable_get(:@weather) == "Sunny"
+      @weather = Weather.new(0)
+      expect(subject.land).to fail "Landing not possible. This airport is full." if :full == true && @weather == "Sunny"
     end
     it "should fail when weather is Stormy" do
-      expect(subject.land).to fail "Landing not possible due to stormy weather"  if Airport.full? && Weather.new.instance_variable_get(:@weather) == "Stormy"
+      @weather = Weather.new(1)
+      expect(subject.land).to fail "Landing not possible due to stormy weather" if :full == false && @weather == "Stormy"
     end
   end
 
