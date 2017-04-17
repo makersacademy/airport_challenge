73bdd40e102e6858709520b88dbaade3063abe82
Adam Erdem Er <adamerdemer@gmail.com>
2017-04-18 00:37:52 +0100

    lands plane

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,4 +1,10 @@
 require 'airport'
-describe Airport do
+#require 'plane'
 
+describe Airport do
+  subject(:airport) { described_class.new}
+
+  it 'lands plane' do
+    expect(airport).to respond_to(:land).with(1).argument
+  end
 end
