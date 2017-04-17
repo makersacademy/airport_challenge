eb667a2b7a3c4a527d3d8f012a8a1af5276a4194
Robert Jones <robert.g.jones@gmail.com>
2017-04-17 18:17:31 +0100

    Tests whether #stormy? can return a true or false value

--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -1,8 +1,12 @@
 require './lib/weather.rb'
 
 describe Weather do
+  subject(:weather) { described_class.new }
   context "Stormy" do
-    it {is_expected.to respond_to :stormy? }
-  end
+    it { is_expected.to respond_to :stormy? }
 
+    it "Expects a true or false from the #stormy? " do
+      expect(weather.stormy?).to eq(true || false)
+    end
+  end
 end
