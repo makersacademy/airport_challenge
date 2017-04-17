a2494c9fc0f4d211cefc3500c33d5b58de444302
milesillsley <milesillsley@hotmail.co.uk>
2017-04-17 16:31:09 +0100

    refactor airport_spec

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,16 +2,16 @@ require 'airport'
 
 describe Airport do
   let(:weather) { double :weather, stormy?: false }
+  let(:plane) { double :plane }
   subject { Airport.new weather }
 
   it { expect(subject).to respond_to(:land).with(1).argument }
 
   it "Instructs a plane to land" do
-    expect(subject.land(Plane.new)).to eq("Plane has landed.")
+    expect(subject.land(plane)).to eq("Plane has landed.")
   end
 
   it "Won't land a plane that has already landed" do
-    plane = Plane.new
     subject.land(plane)
     expect { subject.land(plane) }.to raise_error("Plane has aleady landed.")
   end
@@ -19,20 +19,17 @@ describe Airport do
   it { expect(subject).to respond_to(:takeoff).with(1).argument }
 
   it "Instructs a plane to take off" do
-    plane = Plane.new
     plane.flying = false
     expect(subject.takeoff(plane)).to eq("Plane has taken off.")
   end
 
   it "won't instruct a plane to take off if it is already flying" do
-    plane = Plane.new
     plane.flying = false
     subject.takeoff(plane)
     expect { subject.takeoff(plane) }.to raise_error("Plane is already flying.")
   end
 
   it "prevents takeoff when the weather is stromy" do
-    plane = Plane.new
     plane.flying = false
     allow(weather).to receive(:stormy?).and_return(true)
     expect { subject.takeoff(plane) }.to raise_error("Too stormy for takeoff.")
