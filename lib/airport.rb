435c5ea42cc9d298e1d5acfc343d41baa45c17a9
Alex Law <achlawuk@gmail.com>
2017-04-17 01:21:41 +0100

    Refactor parts of airport_spec.rb and airport.rb

--- lib/airport.rb
+++ lib/airport.rb
@@ -2,14 +2,30 @@ require_relative 'plane'
 
 class Airport
 
-attr_reader :plane
+	DEFAULT_CAPACITY = 5
+
+	def initialize(capacity = DEFAULT_CAPACITY)
+		@capacity = capacity
+		@planes = []
+	end
 
 	def land(plane)
-		@plane = plane
+		fail 'Airport is full' if full?
+		# plane.land?
+		planes << plane
 	end
 
-	def take_off
-		@plane
+	def take_off(plane)
+		plane.take_off?
+		planes.pop
 	end
+
+	private
+
+	def full?
+		planes.length >= capacity
+	end
+
+	attr_accessor :planes, :capacity
 
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,18 +2,43 @@ require 'airport'
 require 'plane'
 
 describe Airport do
-	
-	describe '#land'
-	it 'instructs plane to land' do
-		plane = Plane.new
-		expect(subject.land(plane)).to eq plane
+
+	subject(:airport) { described_class.new(20) }
+	let(:plane) { double :plane }
+
+	describe '#land' do
+
+		it 'instructs plane to land' do
+		expect(airport).to respond_to(:land).with(1).argument
 	end
 
-	describe '#take_off'
-	it 'instructs plane to take off' do
-		plane = Plane.new
-		subject.land(plane)
-		expect(subject.take_off).to eq plane
+	context 'when airport is full' do
+	it 'raises an error' do
+		20.times do
+			airport.land(plane)
+		end
+		expect { airport.land(plane) }.to raise_error 'Airport is full'
+		end
+		end
+	end
+
+	describe '#take_off' do
+		it 'instructs plane to take off' do
+			plane = Plane.new
+			subject.land(plane)
+			expect(subject.take_off(plane)).to eq plane
+		end
+	end
+
+	describe 'default capacity' do
+		subject { Airport.new }
+		let(:plane) { Plane.new }
+		it 'overrides dafault capacity' do
+			described_class::DEFAULT_CAPACITY.times do
+			subject.land(plane)
+		end
+			expect{ subject.land(plane) }.to raise_error 'Airport full'
+		end
 	end
 
 end
