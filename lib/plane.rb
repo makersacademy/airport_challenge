2dcdb0a724a2fb21a7a6032a16f7bdb394587b27
Robert Jones <robert.g.jones@gmail.com>
2017-04-17 14:20:07 +0100

    Plane runs a private #landing_schedule when asked to land. #landed attr writes to true

--- lib/plane.rb
+++ lib/plane.rb
@@ -1,7 +1,19 @@
+require './lib/airport.rb'
+
 class Plane
   attr_reader :landed
-  
+
+
   def land(airport)
+    landing_schedule(airport)
   end
+
+  private
+  attr_writer :landed
+  def landing_schedule(airport)
+    airport.land(self)
+    self.landed = true
+  end
+
 
 end
