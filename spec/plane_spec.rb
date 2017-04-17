d429174817d18b1b4d8207608abd9cf975d3d8da
Robert Jones <robert.g.jones@gmail.com>
2017-04-17 14:23:09 +0100

    Refactor plane.rb and plane_spec with Rubocop and additional context

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -9,16 +9,18 @@ describe Plane do
       expect(plane).to respond_to(:land).with(1).argument
     end
 
-    it { is_expected.to respond_to :landed }
-
     it "Expects plane to run a landing schedule after landing call" do
       expect(plane.land(airport)).to eq true
     end
-
+    
     it "Expects plane to have true #landed attribute once landing_schedule" do
       plane.land(airport)
       expect(plane.landed).to eq true
     end
-
   end
+
+  context "#landed" do
+    it { is_expected.to respond_to :landed }
+  end
+
 end
