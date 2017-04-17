d2e24f20e3684afa9afd5753511dfe0d9427a4ca
anaalta <suciuanamaria@yahoo.com>
2017-04-17 15:13:19 +0200

    added unit testing for confirm takeoff - taking a landed plane out of the array of planse if takeoff confirmed

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -8,7 +8,7 @@ it { is_expected.to respond_to(:confirm_takeoff).with(1).argument}
 end
 
 describe '#confirm_landing' do
-  it "should confirm landing by adding a new plane  -the argument- to an array" do
+  it "should confirm landing by adding a new plane  -the argument- to the array of planes in the airport" do
     plane = Plane.new
     airport = Airport.new
     expect(airport.confirm_landing(plane)).to eq [plane]
@@ -16,5 +16,10 @@ end
 end
 
 describe '#confirm_takeoff' do
-
+it "should confirm takeoff by popping a plane from the array of planes in the airport" do
+  plane = Plane.new
+  airport = Airport.new
+  airport.confirm_landing(plane)
+  expect(airport.confirm_takeoff(plane)).to eq plane
+end
 end
