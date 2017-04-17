4f5e4d2fc55367610f5118c3f6f0db78b65bfc6e
Robert Jones <robert.g.jones@gmail.com>
2017-04-17 14:18:42 +0100

    Tests that plane will message airport to land and sets its landed status to true

--- spec/feature_test.rb
+++ spec/feature_test.rb
@@ -5,6 +5,10 @@ p airport = Airport.new
 p plane = Plane.new
 # asks a plane to respond to the call to land with a single argument
 p plane.land(airport)
-# Checks that the landed plane is stored in a
+# Checks that the landed plane is stored in an attribute of airport
 p airport.land(plane)
 p airport.aircraft
+
+# plane responds to landed? and responds 'true' if has landed in airport
+p plane.landed
+p plane.landed == true
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -2,11 +2,23 @@ require 'plane'
 
 describe Plane do
   subject(:plane) { described_class.new }
+  let(:airport) { Airport.new }
 
   context "#land" do
     it "Expects a plane to respond to #land with an argument" do
       expect(plane).to respond_to(:land).with(1).argument
     end
+
     it { is_expected.to respond_to :landed }
+
+    it "Expects plane to run a landing schedule after landing call" do
+      expect(plane.land(airport)).to eq true
+    end
+
+    it "Expects plane to have true #landed attribute once landing_schedule" do
+      plane.land(airport)
+      expect(plane.landed).to eq true
+    end
+
   end
 end
