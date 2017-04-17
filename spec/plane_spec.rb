8a1aed9b1b76e65d75906e7f74e362306bb6eec1
Chris Mitchell <chrisjmit@gmail.com>
2017-04-17 20:40:34 +0100

    Test formatting updated

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -3,14 +3,14 @@ require './lib/plane.rb'
 describe Plane do
   subject(:plane) { described_class.new }
 
-  context "#confirm the plane has landed" do
+  context "Confirm the plane has landed" do
     it '#able to land a flying plane' do
       subject.land_plane
       expect(subject.flying).to be false
     end
   end
 
-  context "#confirm that a plane has taken off" do
+  context "Confirm that a plane has taken off" do
     it "#the plane has left the airport" do
       subject.plane_take_off
       expect(subject.flying).to be true
