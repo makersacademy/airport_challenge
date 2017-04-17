ff7358b4ddd7b8c3b02a241327e77780367f2adb
Robert Jones <robert.g.jones@gmail.com>
2017-04-17 16:16:21 +0100

    Tests that airport will respond to #take_off

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,16 +2,21 @@ require './lib/airport.rb'
 
 describe Airport do
   subject(:airport) { described_class.new }
-  subject(:plane) { double(:plane) }
-  subject(:aircraft) { double(:aircraft) }
+  let(:plane) { double(:plane) }
+  let(:aircraft) { double(:aircraft) }
 
   context "#landing aircraft" do
-    it "expects an airport to accept a landing aircraft" do
+    it "Expects an airport to accept a landing aircraft" do
       expect(airport).to respond_to(:land).with(1).argument
     end
 
-    it "has a attribute set to an instance of Plane post landing" do
+    it "Has a attribute set to an instance of Plane post landing" do
       expect(airport.land(plane)).to eq airport.aircraft
     end
   end
+
+  context "#take_off aircraft" do
+    it { is_expected.to respond_to :take_off }
+  end
+
 end
