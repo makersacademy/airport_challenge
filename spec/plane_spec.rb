83f72752012dbe1579fe3129f2f6db2088387562
Paula <pmuldoon@umich.edu>
2017-04-17 13:05:46 +0100

    apparently both stubs working

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -7,10 +7,10 @@ describe Plane do
       expect(plane).to respond_to :land
     end
 
-    # it 'tests whether a landed plan has been added to the array' do
-    #   plane.land(plane)
-    #   expect($planes_landed).to include(plane)
-    # end
+    it 'tests whether a landed plan has been added to the array' do
+      plane.land(plane)
+      expect($planes_landed).to include(plane)
+    end
 
     # it 'raises an error if a plane tries to land and the airport is full' do
     #   Airport::DEFAULT_CAPACITY.times { Plane.new.land(Plane.new) }
@@ -31,7 +31,7 @@ describe Plane do
     it 'tests whether the plane responds to :take_off' do
       weather = Weather.new
       allow(weather).to receive(:stormy?).and_return(true)
-      expect(plane). to respond_to :take_off
+      expect(plane).to respond_to :take_off
     end
 
     it 'moves the plane from the landed array to the in air array' do
@@ -43,7 +43,7 @@ describe Plane do
 
     # it 'raises an error if the plane tries to take off in stormy weather' do
     #   weather = Weather.new
-    #   allow(weather).to receive(:stormy?) { true }
+    #   allow(weather).to receive(:stormy?).and_return(true)
     #   expect { plane.take_off(plane) }.to raise_error 'Weather is stormy'
     # end
   end
