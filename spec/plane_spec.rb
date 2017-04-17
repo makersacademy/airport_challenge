91e2eb8867d9a648204b54b0bb902f8fe350fd65
Robert Jones <robert.g.jones@gmail.com>
2017-04-17 12:39:52 +0100

    Refactor to use attr_reader for #landed method in Plane class

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -7,6 +7,6 @@ describe Plane do
     it "Expects a plane to respond to #land with an argument" do
       expect(plane).to respond_to(:land).with(1).argument
     end
-    it { is_expected.to respond_to :landed? }
+    it { is_expected.to respond_to :landed }
   end
 end
