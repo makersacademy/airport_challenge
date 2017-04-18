2380629f8a66f1bd6fcc02d2f35fe92179cdea5b
Adam Erdem Er <adamerdemer@gmail.com>
2017-04-18 03:08:06 +0100

    added in confirmations and refactored code a little

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,16 +1,40 @@
 class Airport
 
+  attr_reader :landed_planes
+
+
   def initialize(capacity)
+    @landed_planes = []
     @capacity = capacity
-    @landed_planes = 0
+
+
   end
 
   def land(plane)
-    raise 'Landing denied: airport full' if @landed_planes >= @capacity
-    @landed_planes += 1
+    raise 'Landing denied: airport full' if @landed_planes.count >= @capacity
+    @landed_planes << plane
 
   end
 
-  def take_off
+  def landed?(plane)
+    if landed_planes.include? plane
+      return true
+    else
+      return false
+    end
   end
+
+  def take_off(plane)
+    @landed_planes.delete(plane)
+  end
+
+  def taken_off?(plane)
+     return !@landed_planes.include?(plane)
+    # if @landed_planes.include? plane
+    #   return false
+    # else
+    #   return true
+    #  end
+  end
+
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -8,14 +8,25 @@ describe Airport do
     expect(airport).to respond_to(:land).with(1).argument
   end
 
+  it 'Confirms plane has landed' do
+    airport.land('plane')
+    expect(airport.landed? ('plane')).to eq true
+  end
+
   it 'Instructs plane to take off' do
     expect(airport).to respond_to(:take_off)
   end
 
+  it 'Confirms plane has taken off' do
+    airport.land('plane')
+    airport.take_off('plane')
+    expect(airport.taken_off? 'plane').to eq true
+  end
+
   it "Landing denied: airport full" do
     15.times do
-      airport.land(:plane)
+      airport.land('plane')
     end
-    expect { airport.land(:plane) }.to raise_error 'Landing denied: airport full'
+    expect { airport.land('plane') }.to raise_error 'Landing denied: airport full'
   end
 end
