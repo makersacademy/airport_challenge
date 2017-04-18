d10fd50d34cdfc60f4908d90fe735d119955594e
Adam Snow <adamjohnsnow@icloud.com>
2017-04-18 11:40:30 +0100

    refactoring airport_spec

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -1,58 +1,59 @@
 require './lib/plane'
 
 describe Plane do
-  let(:airport) { double :airport, weather: "Stormy", name: "Test", full?: false, runway: [], land_plane: '', takeoff_plane: '' }
 
-  it "#planes can take off" do
-    expect(subject).to respond_to(:takeoff)
-  end
+  context 'when airport is sunny' do
+    let(:airport) { double :airport, weather: "Sunny", name: "Test", full?: false, runway: [], land_plane: '', takeoff_plane: '' }
 
-  it "#planes can land" do
-    expect(subject).to respond_to(:land_at)
-  end
+      describe '#land' do
 
-  it "#planes can land at an #airport" do
-    allow(airport).to receive(:weather).and_return("Sunny")
-    subject.land_at(airport)
-    expect(subject.landed_at).to eq airport
-  end
+        it "#planes can land at an #airport" do
+          subject.land_at(airport)
+          expect(subject.landed_at).to eq airport
+        end
 
-  it "#planes can leave an #airport" do
-    allow(airport).to receive(:weather).and_return("Sunny")
-    subject.landed_at = airport
-    subject.takeoff
-    expect(subject.landed_at).to be nil
-  end
+        it "cannot #land if already in #landed_at an #airport" do
+          subject.landed_at = airport
+          expect { subject.land_at(airport) }.to raise_error "Already landed at Test"
+        end
 
-  it "cannot #takeoff if already in flight" do
-    expect { subject.takeoff }.to raise_error "Already in flight"
-  end
+        it "cannot #land_at a #full #airport" do
+          allow(airport).to receive(:full?).and_return(true)
+          expect { subject.land_at(airport) }.to raise_error "Cannot land, airport full"
+        end
 
-  it "cannot #land if already in #landed_at an #airport" do
-    allow(airport).to receive(:weather).and_return("Sunny")
-    subject.landed_at = airport
-    expect { subject.land_at(airport) }.to raise_error "Already landed at Test"
-  end
+      end
 
-  it "cannot #land if #weather is #stormy #airport" do
-    expect { subject.land_at(airport) }.to raise_error "Cannot land, weather at Test stormy"
-  end
+      describe '#takeoff' do
 
-  it "cannot #takeoff if #weather is #stormy #airport" do
-    subject.landed_at = airport
-    expect { subject.takeoff }.to raise_error "Cannot takeoff, weather at Test stormy"
-  end
+        it "#planes can leave an #airport" do
+          subject.landed_at = airport
+          subject.takeoff
+          expect(subject.landed_at).to be nil
+        end
 
-  it "cannot #land_at a #full #airport" do
-    allow(airport).to receive(:weather).and_return("Sunny")
-    allow(airport).to receive(:full?).and_return(true)
-    expect { subject.land_at(airport) }.to raise_error "Cannot land, airport full"
-  end
+        it "cannot #takeoff if already in flight" do
+          expect { subject.takeoff }.to raise_error "Already in flight"
+        end
 
-  it "#lands_at a #airport and enters #runway" do
-    allow(airport).to receive(:weather).and_return("Sunny")
-    airport.runway << subject
-    expect(airport.runway).to eq([subject])
-  end
+      end
+
+    end
+
+    context 'when #airport is #stormy' do
+
+    let(:airport) { double :airport, weather: "Stormy", name: "Test", full?: false, runway: [], land_plane: '', takeoff_plane: '' }
+
+      it "cannot #land if #weather is #stormy #airport" do
+        expect { subject.land_at(airport) }.to raise_error "Cannot land, weather at Test stormy"
+      end
+
+      it "cannot #takeoff if #weather is #stormy #airport" do
+        subject.landed_at = airport
+        expect { subject.takeoff }.to raise_error "Cannot takeoff, weather at Test stormy"
+      end
+
+    end
+
 
 end
