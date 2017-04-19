3ffcb862b086f1ce86f37693a1a843659434ab95
Timothy Robinson <timmyrobby@gmail.com>
2017-04-19 11:39:37 +0100

    Adds emergency landing messages

--- lib/plane.rb
+++ lib/plane.rb
@@ -10,10 +10,12 @@ class Plane
   def takeoff
     raise 'Already flying' unless landed?
     @landed = false
+    "Emergency takeoff."
   end
 
   def land
     raise 'Already landed' if landed?
     @landed = true
+    "Emergency landing outside airport."
   end
 end
