8cf38c9b539923ab174797ffee3257924da6b943
JayWebDevCom <jaiye.s@gmail.com>
2017-04-22 09:22:16 +0100

    fix feature test and add capybara to gem file

--- spec/rspecfeaturetest_spec.rb
+++ spec/rspecfeaturetest_spec.rb
@@ -1,45 +1,45 @@
+require 'spec_helper'
 require 'airport'
 require 'weather'
 require 'plane'
 
-#
-# feature 'planes take off and land' do
-#
-# scenario 'given sunny weather' do
-#   create_entities
-#   a_plane_can_land_at_an_airport
-#   a_plane_can_report_its_correct_landed_status
-#   a_plane_cannot_take_off_from_a_different_airport
-#   a_plane_can_take_off_from_that_airport
-#   a_plane_can_report_its_correct_takeoff_status
-#   end
-#
-# def create_entities
-#   klm = Plane.new
-# 	jfk = Airport.new
-#   lax = Airport.new
-# 	weather = Weather.new
-# end
-#
-#   def a_plane_can_land_at_an_airport
-#   	expect(klm.land(weather, jfk)).to eq :landed
-#   end
-#
-#   def a_plane_can_report_its_correct_landed_status
-#     expect(klm.confirm_status).to eq :landed
-#   end
-#
-#   def a_plane_cannot_take_off_from_a_different_airport
-#     message = "Plane is not at that airport"
-#     expect { klm.take_off(weather, lax)  }. to raise_error message
-#   end
-#
-#   def a_plane_can_take_off_from_that_airport
-#   	expect(klm.takeoff(weather, jfk)).to eq :in_air
-#   end
-#
-#   def a_plane_can_report_its_correct_takeoff_status
-#   	expect(klm.confirm_status_.to eq :in_air
-#   end
-#
-# end
+feature 'planes take off and land' do
+
+scenario 'given varying weather' do
+  create_entities
+  a_plane_can_land_at_an_airport
+  a_plane_can_report_its_correct_landed_status
+  a_plane_cannot_take_off_from_a_different_airport
+  a_plane_can_take_off_from_that_airport
+  a_plane_can_report_its_correct_takeoff_status
+  end
+
+def create_entities
+  @klm = Plane.new
+	@jfk = Airport.new
+  @lax = Airport.new
+	@weather = Weather.new
+end
+
+  def a_plane_can_land_at_an_airport
+  	expect(@klm.land(@weather, @jfk)).to eq :landed
+  end
+
+  def a_plane_can_report_its_correct_landed_status
+    expect(@klm.confirm_status).to eq :landed
+  end
+
+  def a_plane_cannot_take_off_from_a_different_airport
+    message = "Plane is not at that airport"
+    expect { @klm.take_off(@weather, @lax)  }. to raise_error message
+  end
+
+  def a_plane_can_take_off_from_that_airport
+  	expect(@klm.take_off(@weather, @jfk)).to eq :in_air
+  end
+
+  def a_plane_can_report_its_correct_takeoff_status
+  	expect(@klm.confirm_status).to eq :in_air
+  end
+
+end
