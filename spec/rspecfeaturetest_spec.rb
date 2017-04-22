ef1854a73f96afc89cb835dd8c346576572989d7
JayWebDevCom <jaiye.s@gmail.com>
2017-04-22 09:32:07 +0100

    add another test to feature test

--- spec/rspecfeaturetest_spec.rb
+++ spec/rspecfeaturetest_spec.rb
@@ -11,6 +11,7 @@ scenario 'given varying weather' do
   a_plane_can_report_its_correct_landed_status
   a_plane_cannot_take_off_from_a_different_airport
   a_plane_can_take_off_from_that_airport
+  a_flying_plane_cannot_take_off_again
   a_plane_can_report_its_correct_takeoff_status
   end
 
@@ -36,6 +37,11 @@ end
 
   def a_plane_can_take_off_from_that_airport
   	expect(@klm.take_off(@weather, @jfk)).to eq :in_air
+  end
+
+  def a_flying_plane_cannot_take_off_again
+    message = "I am already in the air"
+  	expect { @klm.take_off(@weather, @jfk) }. to raise_error message
   end
 
   def a_plane_can_report_its_correct_takeoff_status
