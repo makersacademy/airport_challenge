e1d98d36e0a00a2542373f8b91db7c825545fd94
Mário Ribeiro <mail@marioribeiro.com>
2017-05-12 23:50:56 +0100

    Refine tests and code that controls the status update after the plane lands

--- spec/airport_feature_spec.rb
+++ spec/airport_feature_spec.rb
@@ -5,6 +5,7 @@ feature 'Air Traffic Control' do
     given_there_is_an_airport
     and_a_plane
     and_i_want_to_instruct_the_plane_to_land_at(@airport)
+    and_confirm_that_it_has_landed 
   end
 
   scenario 'Instructs a plane to take off' do
@@ -30,6 +31,11 @@ feature 'Air Traffic Control' do
   end
 
   def and_i_want_to_instruct_the_plane_to_land_at(airport)
+    @plane.land(@airport)
+  end
+
+  def and_confirm_that_it_has_landed 
+    expect(@plane.on_ground).to eq true
   end
 
   def i_want_to_instruct_a_plane_to_takeoff_to(destination)
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -2,6 +2,7 @@ require 'plane'
 
 describe Plane do
   it { is_expected.to respond_to(:land).with(1).argument }
+  it { is_expected.to respond_to(:on_ground)}
   
   describe '#land' do
     it 'can land on airport' do
