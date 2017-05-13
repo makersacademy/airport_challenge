085e9d80b299b1b33c4749ad08149e5810f5c834
Simon Ashbery <si.ashbery@gmail.com>
2017-05-13 13:42:04 +0100

    created a test to check that the show_capacity method returns a value

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -4,4 +4,7 @@ describe Airport do
   it "responds to the show capacity method" do
     expect(subject).to respond_to :show_capacity
   end
+  it "returns a value when the show capacity method is called" do
+    expect(subject.show_capacity).to eq 20
+  end
 end
