644ce65235091108a1170b7ef0ffad0bf603a2ad
Lubos Michalic <lnmichalic@gmail.com>
2017-05-13 20:22:44 +0100

    Write second feature test to instruct a plane to take-off

--- spec/feature_tests_spec.rb
+++ spec/feature_tests_spec.rb
@@ -9,3 +9,8 @@ describe 'Landing' do
     expect(airport.land(plane)).to eq "Plane has landed"
   end
 end
+describe 'Taking Off' do
+  it'ATC instructs a plane to landed gets a confirmation' do
+    expect(airport.plane_takeoff).to eq "Plane has taken off"
+  end
+end
