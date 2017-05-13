6666acfd2aac4d629c7cafe1888c1fa3ec2438db
Simon Ashbery <si.ashbery@gmail.com>
2017-05-13 14:15:12 +0100

    Created and failed test to check that the show_current status method returns airborne

--- spec/airplane_spec.rb
+++ spec/airplane_spec.rb
@@ -3,4 +3,7 @@ describe Airplane do
   it "Shows that an instance of Airplane responds to the current_status method" do
     expect(subject).to respond_to :show_current_status
   end
+  it "Returns airborne when show_current_status is called" do
+    expect(subject.show_current_status).to eq "airborne"
+  end
 end
