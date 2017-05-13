4fee5c93dfe79ea849141b5767362e923fccf7f8
Simon Ashbery <si.ashbery@gmail.com>
2017-05-13 20:22:38 +0100

    Created and failed test to show that the safe_to_land? method will return false if weather is stormy or true if it is sunny

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -7,6 +7,13 @@ describe Airport do
   it "Shows that safe_to_land? returns true" do
     expect(subject.safe_to_land?).to eq true
   end
+  it "Shows that safe_to_land will return true if the weather is sunny or false if it is stormy" do
+    weather = double(:weather)
+    allow(weather).to receive(:show_weather_condition).and_return ("sunny")
+    expect(subject.safe_to_land?).to eq true
+    allow(weather).to receive(:show_weather_condition).and_return ("stormy")
+    expect(subject.safe_to_land?).to eq false
+  end
   it "responds to the show capacity method" do
     expect(subject).to respond_to :show_capacity
   end
