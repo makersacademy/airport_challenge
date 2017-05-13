bdb537fedf7b5aa7498b55e9f0466960bc0e1c92
“rogrenke” <roland@rolandgrenke.com>
2017-05-14 00:41:08 +0100

    create test to check if weather of new Weather instance is either :good or :stormy, create simplified initialize method that always set weather as :good, randomized weather creator to be added later

--- lib/weather.rb
+++ lib/weather.rb
@@ -1,5 +1,9 @@
 class Weather
 
   attr_reader :current_state
+  #TO BE BUILT LATER: Random State creater
+  def initialize
+    @current_state = :good
+  end
 
 end
--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -4,4 +4,7 @@ describe Weather do
   it 'should respond to #current_state' do
     expect(subject).to respond_to(:current_state)
   end
+  it 'should return either "good" or "stormy" when #current_state is called on it' do
+    expect([:good, :stormy].include?(subject.current_state)).to eq true
+  end
 end
