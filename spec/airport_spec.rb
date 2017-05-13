e44dfda0e332756cce3bcc10fc7f261ba492a766
Lubos Michalic <lnmichalic@gmail.com>
2017-05-13 20:15:46 +0100

    Write test to store landed planes in airport

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,10 +1,16 @@
 require 'airport'
 
 describe Airport do
+  let(:plane) {double('plane')}
+  let(:airport) {Airport.new}
   it 'responds to #land' do
     is_expected.to respond_to(:land)
   end
   it 'responds to #land with the arguement "plane"' do
     is_expected.to respond_to(:land).with(1).argument
+  end
+  it 'lands an airplane and stores it in airport' do
+    airport.land(plane)
+    expect(airport.landed_planes).to eq [plane]
   end
 end
