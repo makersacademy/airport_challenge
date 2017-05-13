aad25d61ab6dc750a28d920d2c01da882895e189
Aaron Rodrigues <aaronrodrigues@Aarons-MBP.home>
2017-05-13 23:08:33 +0100

    Added test to respond to the take_off method and added the method

--- lib/airport.rb
+++ lib/airport.rb
@@ -2,10 +2,15 @@ require_relative 'plane'
 
 class Airport
   attr_reader :plane
-  
+
   def land plane
   	@plane = plane
   end
+
+  def take_off plane
+
+  end
+
 
 end
 
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -3,11 +3,13 @@ describe Airport do
 
   # subject(:airport) { Airport.new }
   it { is_expected.to respond_to(:land).with(1).argument  }
-  
+  it { is_expected.to respond_to(:take_off).with(1).argument}
+
   it 'returns landed planes' do
  	 plane = Plane.new
  	 subject.land(plane)
  	 expect(subject.plane).to eq plane
   end
-
+  
+  
 end
