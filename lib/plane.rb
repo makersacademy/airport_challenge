a5397400eb4ccd6ff05acbb22997edbf262f9d7e
Elena Morton <elmo@Elenas-MacBook-Pro.local>
2017-05-13 11:18:16 +0100

    Added landing/taken_off behaviour to the plane.

--- lib/plane.rb
+++ lib/plane.rb
@@ -2,15 +2,23 @@ class Plane
 
   def initialize
     @taken_off = false
-    @landed = true
+    @landed = false
   end
 
   def suitable_for_flying?
     true
   end
 
+  def report_landed
+    @landed = true
+  end
+
   def landed?
     @landed
+  end
+
+  def report_takenoff
+    @taken_off = true
   end
 
   def taken_off?
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -7,7 +7,14 @@ describe Plane do
   it { expect(subject).to respond_to :landed? }
   it { expect(subject).to respond_to :taken_off? }
 
+  it 'Allow reporting a plane has landed' do
+    subject.report_landed
+    expect(subject).to be_landed
+  end
 
-  
+  it 'Allow reporting a plane has taken off' do
+    subject.report_takenoff
+    expect(subject).to be_taken_off
+  end
 
 end
