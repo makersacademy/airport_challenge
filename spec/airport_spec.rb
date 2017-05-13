20216db07f851515e9ddd173c0d22042b74421c7
Michael Jacobson <michael.jacobson89@gmail.com>
2017-05-13 11:09:04 +0100

    Add test in airport_spec file to check that an Airport instance's default capacity can be overridden on initialization.

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -4,5 +4,10 @@ describe Airport do
   it "has a default capacity" do
     expect(subject.airport_capacity).to eq Airport::DEFAULT_CAPACITY
   end
+  it "capacity can be overridden on initialization" do
+    test_capacity = 100
+    airport = Airport.new(test_capacity)
+    expect(airport.airport_capacity).to eq test_capacity
+  end
 
 end
