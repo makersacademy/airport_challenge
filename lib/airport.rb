1e7cbaf5c02c4c0bd2473ddc61deec1f8fcadd54
Aaron Rodrigues <aaronrodrigues@Aarons-MBP.home>
2017-05-13 22:57:49 +0100

    Added instance variable @plan in method land and in the function plane. Added test to check if a plane is returned when landed

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,5 +1,20 @@
+require_relative 'plane'
+
 class Airport
+  
   def land plane
-  	
+  	@plane = plane
   end
+
+  def plane
+  	@plane 
+  end
+
 end
+
+
+
+# p plane = Plane.new
+# p airport = Airport.new
+# p airport.land(plane)
+# p airport.plane
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,11 +1,13 @@
 require 'airport'
 describe Airport do
 
-  subject(:airport) { Airport.new }
+  # subject(:airport) { Airport.new }
   it { is_expected.to respond_to(:land).with(1).argument  }
   
-  it 'should be able let a plane land' do
-  	 
+  it 'returns landed planes' do
+ 	 plane = Plane.new
+ 	 subject.land(plane)
+ 	 expect(subject.plane).to eq plane
   end
 
 end
