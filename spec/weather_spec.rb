e4ab73a984bf908fb51e4864a22b791699304820
Simon Ashbery <si.ashbery@gmail.com>
2017-05-13 16:10:34 +0100

    Created and failed unit test to show that the show_current_weather_condition returns sunny

--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -4,5 +4,7 @@ describe Weather do
   it "Shows that weather responds to the show_current_weather_condition method" do
     expect(subject).to respond_to :show_current_weather_condition
   end
-
+  it "Shows that the show_current_weather_condition method returns sunny" do
+    expect(subject.show_current_weather_condition).to eq "sunny"
+  end
 end
