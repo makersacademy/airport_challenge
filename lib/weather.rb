1727c68b0c316fda994b2765661ddd29c02a1aa6
Simon Ashbery <si.ashbery@gmail.com>
2017-05-13 19:54:26 +0100

    Moved the change_weather_chance and change_weather_condition methods into private. Created and passed tests to show that show_weather_value will return stormy 15% of the tim, otherwise it will return sunny

--- lib/weather.rb
+++ lib/weather.rb
@@ -1,20 +1,27 @@
 class Weather
+
+  attr_reader(:change_weather_chance, :change_weather_condition)
+
   def initialize
     @current_weather_conditon = "sunny"
-    @weather_chance = 50
   end
 
   def show_current_weather_condition
+    change_weather_condition
     @current_weather_conditon
   end
 
-  def change_weather_condition
-    @current_weather_conditon = "stormy" if @weather_chance == 50
-  end
-end
-
 private
 
-def change_weather_chance
-  @weather_chance = 50
+  def change_weather_chance
+    rand(1...100)
+  end
+
+  def change_weather_condition
+    if change_weather_chance >= 85
+      @current_weather_conditon = "stormy"
+    else
+      @current_weather_conditon = "sunny"
+    end
+  end
 end
--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -4,28 +4,12 @@ describe Weather do
   it "Shows that weather responds to the show_current_weather_condition method" do
     expect(subject).to respond_to :show_current_weather_condition
   end
-  it "Shows that an instance of the Weather class responds to a change_weather_condition method" do
-    expect(subject).to respond_to :change_weather_condition
-  end
-  it "shows that by default the current_weather_conditon instance variable is set to sunny" do
-    expect(subject.show_current_weather_condition).to eq "sunny"
-  end
-  # it "shows that instances of weather have a change weather chance method" do
-  #   expect(subject).to respond_to :change_weather_chance
-  # end
-  # it "shows that the change_weather_chance method returns an instance variable called" do
-  #   expect(subject.change_weather_chance).to eq 50
-  # end
-  it "shows that the change_weather_condition method will change the current_weather_conditon instance variable to stormy based on the value of weather_chance instance variable" do
-    subject.change_weather_condition
-    expect(subject.show_current_weather_condition).to eq "stormy"
-  end
-  it "shows that the change_weather_condition method will return stormy if weather_chance > 85 and sunny otherwise" do
-    20.times do
-      subject.stub(:change_weather_chance) {rand(85...100)}
-      expect(subject.change_weather_condition).to eq "stormy"
-      subject.stub(:change_weather_chance) {rand(1..85)}
-      expect(subject.change_weather_condition).to eq "sunny"
+  it "shows that the show_weather_condition method will return stormy 15% of the time and sunny otherwise" do
+    1000.times do
+      subject.stub(:change_weather_chance) { rand(85..100) }
+      expect(subject.show_current_weather_condition).to eq "stormy"
+      subject.stub(:change_weather_chance) { rand(1...85) }
+      expect(subject.show_current_weather_condition).to eq "sunny"
     end
   end
 end
