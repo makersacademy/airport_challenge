85e0a2039bc16cdf8375afa04f6c305927d1d6c2
jade <jadealvares@hotmail.co.uk>
2017-05-13 21:27:20 +0100

    add comments on each unit test to state which user stories it is testing

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,24 +2,24 @@ require "airport.rb"
 
 describe Airport do
 
-  it "should have a default capacity" do
+  it "should have a default capacity" do # <--user story 6 (needed to set default capacity to state airport is full and do user story 5)
     airport = Airport.new
     expect(subject.capacity).to eq Airport::DEFAULT_CAPACITY
   end
 
-  it "should have a default capacity that can be reset" do
-    airport = Airport.new(250)
-    expect(airport.airport_capacity(250)).to eq 250
-  end
-
   describe "#land" do
-  it "raises an error when attempting to land a plane in an airport of full capacity" do
+  it "raises an error when attempting to land a plane in an airport of full capacity" do # <--user story 5
     subject.capacity.times { subject.land Plane.new}
       expect { subject.land Plane.new }.to raise_error "Airport is full"
     end
   end
 
-  it "should not land the plane when an airport is full" do
+  it "should not land the plane when an airport is full" do  # <--user story 5
     expect(subject).to respond_to(:full?)
+  end
+
+  it "should have a default capacity that can be reset" do # <--user story 6
+    airport = Airport.new(250)
+    expect(airport.airport_capacity(250)).to eq 250
   end
 end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -1,20 +1,20 @@
 require "plane.rb"
 
 describe Plane do
-it "should instruct a plane to land at the airport" do
+it "should instruct a plane to land at the airport" do  # <--user story 1
   expect(subject).to respond_to(:land)
   end
 
-it "should confirm that it is in the airport" do
+it "should confirm that it is in the airport" do  # <--user story 1
   subject.land
   expect(subject.status_of_plane).to eq "in airport"
 end
 
-it "should instruct a plane to take off from an airport" do
+it "should instruct a plane to take off from an airport" do  # <--user story 2
   expect(subject).to respond_to(:take_off)
   end
 
-it "should confirm that it is no longer in the airport" do
+it "should confirm that it is no longer in the airport" do  # <--user story 2
   subject.take_off
   expect(subject.status_of_plane).to eq "not in airport"
 
