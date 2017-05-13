5efc9c1791f9bddcb5afc9df72555597edbc9616
wemmm <jenny.wem@gmail.com>
2017-05-13 22:56:46 +0100

    Added failing test for the lands_plane method giving the user a message when a plane lands
    - Edited airport_spec.rb

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -4,4 +4,10 @@ describe Airport do
 
   it { is_expected.to respond_to(:lands_plane).with(1).argument }
 
+  describe '#lands_plane' do
+
+    it { is_expected.to eq "The plane has landed!" }
+
+  end
+
 end
