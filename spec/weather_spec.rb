fdd21e922c9840558ce5b17de0c73ef60bd5d950
Simon Ashbery <si.ashbery@gmail.com>
2017-05-13 16:17:52 +0100

    Created and failed test to show that Inatances of the Weather class respond to a change_weather_condition method

--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -7,4 +7,7 @@ describe Weather do
   it "Shows that the show_current_weather_condition method returns sunny" do
     expect(subject.show_current_weather_condition).to eq @current_weather_conditon
   end
+  it "Shows that an instance of the Weather class responds to a change_weather_condition method" do
+    expect(subject).to respond_to :change_weather_condition
+  end
 end
