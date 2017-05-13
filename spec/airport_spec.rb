9320c931b730536b75371df15c481c7ada4590d3
Kynosaur <kye.bracey@gmail.com>
2017-05-13 12:16:51 +0100

    Add test for allowing user to override airport capacity

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -12,4 +12,8 @@ describe Airport do
     expect(airport.capacity).to eq 20
   end
 
+  it 'should allow user to override airport capacity' do
+    expect(airport.capacity = 30).to eq 30
+  end
+
 end
