020708aba737cda2310e60705b9386118d5f4939
“rogrenke” <roland@rolandgrenke.com>
2017-05-13 20:48:47 +0100

    create test to check if plane reports correctly that it's landed and at which airport.

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -4,4 +4,10 @@ describe Plane do
   it 'responds to #report_landed' do
     expect(subject).to respond_to(:report_landed)
   end
+  it 'should report that it is landed as soon as it landed on an airport, including its ID and the ID of the airport' do
+    airport = Airport.new
+    plane = subject
+    airport.instruct_to_land(plane)
+    expect(plane.report_landed).to eq "This is plane #{plane.object_id} speaking. We report that we landed at airport #{airport.object_id}"
+  end
 end
