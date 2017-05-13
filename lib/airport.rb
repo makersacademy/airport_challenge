65b1a0574bce1dfebbc254ca007ee155cea486ef
wemmm <jenny.wem@gmail.com>
2017-05-13 23:35:35 +0100

    Passed failing test, added Plane class
    - Created plane.rb

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,7 +1,9 @@
+require_relative 'plane'
+
 class Airport
 
   def lands_plane(plane)
-
+    return "The plane has landed!"
   end
 
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -5,9 +5,10 @@ describe Airport do
   it { is_expected.to respond_to(:lands_plane).with(1).argument }
 
   describe '#lands_plane' do
-
-    it { is_expected.to eq "The plane has landed!" }
-
+    it 'gives the user a message when a plane lands' do
+      plane = Plane.new
+      airport = Airport.new
+      expect(airport.lands_plane(plane)).to eq "The plane has landed!"
+    end
   end
-
 end
