2af162057f40a2359a8234374dd7f91aede6209d
RSijelmass <R.Sijelmass@gmail.com>
2017-05-13 16:28:01 +0100

    Adds condition: only releases / lands plane if within capacity - Standard capacity set at 20, can be changed by maintainer - private method #full? created, checking if airport is at capacity - private method #empty? created, checking if airport is empty

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,19 +1,22 @@
 require_relative 'plane'
 
 class Airport
-  attr_reader :planes, :weather_grade
+  attr_reader :planes, :weather_grade, :capacity
 
-  def initialize
+  def initialize(capacity = 20)
     @planes = []
+    @capacity = capacity
     @weather_grade = rand(10) # bad weather: < 4
   end
 
   def land(plane)
+    full?
     (@planes << plane; return plane) unless stormy?
 
   end
 
   def plane_take_off
+    empty?
     return @planes.pop unless stormy?
   end
 
@@ -21,5 +24,13 @@ class Airport
 
   def stormy?
     raise 'Sorry, the storm is too great!' if @weather_grade < 4
+  end
+
+  def full?
+    raise 'The airport is currently full.' if @planes.length >= @capacity
+  end
+
+  def empty?
+    raise 'There currently are no planes available.'if @planes.empty?
   end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -16,15 +16,18 @@ describe Airport do
       airport.instance_variable_set(:@weather_grade, 3)
       expect { airport.land(plane) }.to raise_error 'Sorry, the storm is too great!'
     end
+    it 'raises an error if the airport is full' do
+      airport.instance_variable_set(:@weather_grade, 6)
+      airport::capacity.times {airport.land(plane) }
+      expect { airport.land(plane) }.to raise_error 'The airport is currently full.'
+    end
   end
 
   describe '#plane_take_off' do
     it 'removes one plane from the planes array' do
       airport.instance_variable_set(:@weather_grade, 6)
-      2.times { airport.land(plane) }
+      airport.land(plane)
       expect(airport.plane_take_off).to eq plane
-      airport.plane_take_off
-      expect(airport.plane_take_off).to eq nil
     end
 
     it 'raises an error if stormy' do
@@ -32,6 +35,10 @@ describe Airport do
       airport.land(plane)
       airport.instance_variable_set(:@weather_grade, 3)
       expect { airport.plane_take_off }.to raise_error 'Sorry, the storm is too great!'
+    end
+    it 'raises an error if the airport is empty' do
+      airport.instance_variable_set(:@weather_grade, 6)
+      expect { airport.plane_take_off }.to raise_error 'There currently are no planes available.'
     end
   end
 end
