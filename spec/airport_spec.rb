8502b949a695808e055535aaa4c38a7af710b82a
“rogrenke” <roland@rolandgrenke.com>
2017-05-13 19:52:01 +0100

    add test to check that airport is initialized empty

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -7,7 +7,10 @@ describe Airport do
   it 'responds to landed_planes method' do
     expect(subject).to respond_to(:landed_planes)
   end
-  it 'should should return array if land_planes method is called on it' do
+  it 'should return array if land_planes method is called on it' do
     expect(subject.landed_planes.class).to eq Array
+  end
+  it 'should be initialized as an empty airport, i.e. landed_planes is empty' do
+    expect(subject.landed_planes.empty?).to eq true
   end
 end
