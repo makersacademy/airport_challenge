d1de691f979738ab1f3d769e6653956c3ad58fcb
Kynosaur <kye.bracey@gmail.com>
2017-05-13 21:09:21 +0100

    Add Weather class to airport.rb with stormy? method to return true 30% of the time.  Add @weather instance variable to Airport class, and update Airport#land to raise exception if weather is stormy

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,5 +1,11 @@
 require_relative 'plane'
 
+class Weather
+  def stormy?
+    rand(10) < 3 ? true : false
+  end
+end
+
 class Airport
 
   attr_accessor :capacity
@@ -7,10 +13,12 @@ class Airport
 
   def initialize(capacity = DEFAULT_CAPACITY)
     @capacity = capacity
+    @weather = Weather.new
     @planes = []
   end
 
   def land(plane)
+    raise "Unable to land plane - weather is stormy" if @weather.stormy?
     @planes << plane
   end
 
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -3,7 +3,6 @@ require 'airport'
 describe Airport do
   subject(:airport) { described_class.new }
   let(:plane) { double :plane }
-  let(:weather) { double :weather }
 
   it 'should have a default capacity of 10 when instantiated with no arguments' do
     expect(airport.capacity).to eq Airport::DEFAULT_CAPACITY
@@ -18,13 +17,19 @@ describe Airport do
     expect(airport.capacity = 30).to eq 30
   end
 
+  it 'should indicate that airport is full' do
+    allow(airport.instance_variable_get(:@weather)).to receive(:stormy?).and_return false
+    Airport::DEFAULT_CAPACITY.times { airport.land(Plane.new) }
+    expect(airport.full?).to eq true
+  end
+
   it 'should prevent a plane from landing if the weather is stormy' do
-    allow(weather).to receive(:stormy?).and_return true
-    expect{airport.land(plane)}.to raise_error "Unable to land - weather is stormy"
+    allow(airport.instance_variable_get(:@weather)).to receive(:stormy?).and_return true
+    expect{airport.land(plane)}.to raise_error "Unable to land plane - weather is stormy"
   end
 
   it 'should allow a plane to land if the weather is clear' do
-    allow(weather).to receive(:stormy?).and_return false
+    allow(airport.instance_variable_get(:@weather)).to receive(:stormy?).and_return false
     airport.land(plane)
     expect(airport.instance_variable_get(:@planes)).to eq [plane]
   end
