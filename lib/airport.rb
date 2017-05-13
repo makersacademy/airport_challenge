ddf6fafa62f25ced7f48d04a9d01353d61896c29
“rogrenke” <roland@rolandgrenke.com>
2017-05-13 13:43:33 +0100

    adjust previous test to see if instance of Airport accepts method with one argument, adjust instruct_to_land method to accept one argument.

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,4 +1,4 @@
 class Airport
-  def instruct_to_land
+  def instruct_to_land(plane)
   end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,6 +2,6 @@ require 'airport'
 
 describe Airport do
   it 'responds to instruct_to_land method' do
-    expect(subject).to respond_to(:instruct_to_land)
+    expect(subject).to respond_to(:instruct_to_land).with(1).argument
   end
 end
