56344c9ab3e797214fffc3dbf453c5bff22f7570
jade <jadealvares@hotmail.co.uk>
2017-05-13 17:45:54 +0100

    instruct plane to take off and that it is no longer in the airport

--- lib/plane.rb
+++ lib/plane.rb
@@ -3,4 +3,7 @@
 class Plane
   def landing
   end
+
+  def take_off
+  end
 end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -1,8 +1,12 @@
 require "plane.rb"
 
 describe Plane do
-it "should instruct a plane to land at the airport" do
+it "should instruct a plane to land at the airport and confirm it has" do
   plane = Plane.new
   expect(subject).to respond_to(:landing)
+  end
+
+it "should instruct a plane to take off from an airport and confirm that it is no longer in the airport" do
+  expect(subject).to respond_to(:take_off)
   end
 end
