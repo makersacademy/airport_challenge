d3cb3b22a7eb0996fc427603c0bec07bd3af7a4a
bannastre <bannastre@gmail.com>
2017-05-13 09:47:55 +0100

    First tests written - Red RSPEC test for .land_plane written - Feature Test updated and written for .land_plane

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,5 +1,7 @@
 require 'airport'
 
 describe Airport do
+
+  it { is_expected.to respond_to :land_plane}
   
 end
--- spec/feature_tests.rb
+++ spec/feature_tests.rb
@@ -3,3 +3,15 @@
 require './lib/airport.rb'
 
 airport = Airport.new
+
+# It instructs a plane to land
+feature_test("plane landed", airport.land_plane)
+
+## Test ->
+def feature_test(expected_result, test_statement)
+  if expected_result == test_statement
+    p "Pass"
+  else
+    p "FAIL -> expected #{expected_result}, got #{test_statement}"
+  end
+end
