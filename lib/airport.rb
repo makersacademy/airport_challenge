e56ddb53f8630ae73f2bfcb11f2e0e343c9e5565
Lubos Michalic <lnmichalic@gmail.com>
2017-05-13 20:18:37 +0100

    Write prod code to pass test for storing planes in airport

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,6 +1,14 @@
 class Airport
+
+  attr_reader :landed_planes
+
+  def initialize
+    @landed_planes = []
+  end
+
   def land(plane)
-    plane
+    @landed_planes << plane
     "Plane has landed"
   end
+
 end
