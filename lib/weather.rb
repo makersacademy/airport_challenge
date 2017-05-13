d2df9eb6f3e70a1a64b09aa1f591113f1fd5dde1
Simon Ashbery <si.ashbery@gmail.com>
2017-05-13 16:38:54 +0100

    Defined an initialize variable for the Weather class that sets the current_weather_conidtion instance variable to sunny, altered tests to reflect that the change_weather_condition method should change the weather conditoin to stormy and removed test that tested for show_current_weather_conditon returning an empty variable

--- lib/weather.rb
+++ lib/weather.rb
@@ -1,9 +1,13 @@
 class Weather
+  def initialize
+    @current_weather_conditon = "sunny"
+  end
+
   def show_current_weather_condition
     @current_weather_conditon
   end
 
   def change_weather_condition
-    @current_weather_conditon = "sunny"
+    @current_weather_conditon = "stormy"
   end
 end
--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -4,17 +4,14 @@ describe Weather do
   it "Shows that weather responds to the show_current_weather_condition method" do
     expect(subject).to respond_to :show_current_weather_condition
   end
-  it "Shows that the show_current_weather_condition method returns sunny" do
-    expect(subject.show_current_weather_condition).to eq @current_weather_conditon
-  end
   it "Shows that an instance of the Weather class responds to a change_weather_condition method" do
     expect(subject).to respond_to :change_weather_condition
   end
   it "shows that by default the current_weather_conditon instance variable is set to sunny" do
-    expect(subject.current_weather_conditon).to eq "sunny"
-  end
-  it "shows that the change_weather_condition method will change the current_weather_conditon instance variable to sunny" do
-    subject.change_weather_condition
     expect(subject.show_current_weather_condition).to eq "sunny"
+  end
+  it "shows that the change_weather_condition method will change the current_weather_conditon instance variable to stormy" do
+    subject.change_weather_condition
+    expect(subject.show_current_weather_condition).to eq "stormy"
   end
 end
