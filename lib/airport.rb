11e80269943f41244cdb82333c2c88fa137fe187
charliefea <charlotte.feather@yahoo.com>
2017-05-13 12:46:27 +0100

    Add tests for #takes_off and create the method, completed US 1 & 2

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,6 +1,18 @@
 class Airport
-  #Method for plane landing at airport
-  def lands_plane(plane)
+  attr_reader :planes
 
+  def initialize
+    #An array to hold the planes that take off and land at the airport
+    @planes=[]
+  end
+  #Method for a plane landing at an airport
+  def lands_plane(plane)
+    planes << plane
+    "#{plane} has landed"
+  end
+  #Method for a plane landing at an airport
+  def takes_off
+    plane_take_off = planes.pop
+    "#{plane_take_off} has taken off"
   end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,8 +1,34 @@
 require 'airport'
+require 'plane'
 
 describe Airport do
+let (:plane) { double( :plane ) }
   describe '#lands_planes' do
     it { is_expected.to respond_to(:lands_plane).with(1).argument }
-
+    it 'returns the value of the plane which has landed' do
+      plane = double( 'plane', :lands_plane => plane)
+      expect(subject.lands_plane(plane)).to eq "#{plane} has landed"
+    end
+    it 'adds the plane to the plans array' do
+      plane = double( 'plane')
+      subject.lands_plane(plane)
+      expect(subject.planes).to eq [plane]
+    end
+  end
+  describe '#planes' do
+    it { is_expected.to respond_to(:planes) }
+  end
+  describe '#takes_off' do
+    it { is_expected.to respond_to(:takes_off)}
+    it 'returns the value of the plane which has taken off' do
+      plane = double( 'plane')
+      subject.lands_plane(plane)
+      expect(subject.takes_off).to eq "#{plane} has taken off"
+    end
+    it 'removes the plane to the plans array' do
+      plane = double( 'plane', :lands_plane => plane)
+      subject.takes_off
+      expect(subject.planes).to eq []
+    end
   end
 end
