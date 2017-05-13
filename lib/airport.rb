bafb1e2a29ef55c3ff90de2a4180cba7d1b73a59
Kavita <kkavita92@gmail.com>
2017-05-13 16:27:48 +0100

    refactor code

--- lib/airport.rb
+++ lib/airport.rb
@@ -10,14 +10,23 @@ class Airport
   def land_plane(plane)
     fail("Plane is already landed") unless plane.airborne?
     fail("Airport is full!") if @capacity == 0
+    complete_landing_procedure(plane)
+  end
+
+  def takeoff_plane(plane)
+    fail("Plane is already landed") if plane.airborne?
+    complete_takeoff_procedure(plane)
+  end
+
+  def complete_landing_procedure(plane)
     plane.landed
     @capacity -= 1
     return "#{plane} has completed landing"
   end
 
-  def takeoff_plane(plane)
-    fail("Plane is already landed") if plane.airborne?
+  def complete_takeoff_procedure(plane)
     plane.takeoff
+    @capacity += 1
     return "#{plane} has taken off"
   end
 
