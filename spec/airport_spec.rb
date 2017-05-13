c1e47ad5b8f9f89079b0a620d210b7edbd8926df
wemmm <jenny.wem@gmail.com>
2017-05-13 23:42:31 +0100

    Added failing test for Airport method .launches_plane

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -4,6 +4,8 @@ describe Airport do
 
   it { is_expected.to respond_to(:lands_plane).with(1).argument }
 
+  it { is_expected.to respond_to(:launches_plane).with(1).argument }
+
   describe '#lands_plane' do
     it 'gives the user a message when a plane lands' do
       plane = Plane.new
