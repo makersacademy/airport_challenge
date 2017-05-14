700560b3ce67bacdd87e60e0aed05220997861d6
Panteha <panteha.ahmadi@gmail.com>
2017-05-14 21:18:54 +0100

    Red:plane can not land in another airport when already landed

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -2,4 +2,12 @@ require 'plane.rb'
 
 describe Plane do
 
+  let(:airplane){double(:airplane)}
+  it "can not land in another airport when already landed" do
+    airport.land(subject)
+    airport2 = double(airport2)
+    expect{airport2.land(subject)}.to raise_error "The plane is already landed in another airport"
+
+  end
+
 end
