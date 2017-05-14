1dbc985727236120171f743a8fefe873b845b1e8
charliefea <charlotte.feather@yahoo.com>
2017-05-14 20:30:13 +0100

    Final Version

--- lib/plane.rb
+++ lib/plane.rb
@@ -2,9 +2,9 @@ class Plane
   attr_accessor :flying
 
   def initialize
-    @flying = true # Represents if the plane is airborne or not
+    @flying = true # Represents if the plane is airborn or not
   end
-  
+
 # Changes the flying instance variable to false representing it is no longer flying
 
   def arrives
