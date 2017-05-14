68f351c99c5d9cdb4116b2f90d6be937afb51aaa
Dom <dom.mcdonnell@gmail.com>
2017-05-14 17:56:51 +0100

    Add weather class and inital test for #random_weather method

---
+++ lib/weather.rb
@@ -0,0 +1,5 @@
+class Weather
+  def random_weather
+
+  end
+end
---
+++ spec/weather_spec.rb
@@ -0,0 +1,9 @@
+require 'weather'
+describe Weather do
+  it 'should be able to generate weather conditions' do
+    expect(subject).to respond_to :random_weather
+  end
+  it '' do
+    
+  end
+end
