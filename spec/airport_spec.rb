e1a0cee913c795ceb5088f7fdd55348d974f2151
IAN SIMPSON <ianconradsimpson@gmail.com>
2017-05-14 14:52:33 +0100

    Failing test for when capacity reached

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,6 +2,7 @@ require 'airport'
 
 describe Airport do
   it { is_expected.to respond_to(:landing_plane).with(1).argument }
+  it { is_expected.to respond_to :planes }
 
   it "Set capacity of planes when new airport made"do
     expect(Airport).to respond_to(:new).with(1).argument
@@ -14,6 +15,14 @@ describe Airport do
 
   it "doesn't allow planes to land or takeoff in bad weather" do
     expect(subject.weather).to eq "Apologies due to bad weather you'll not be able to land!"
+  end
+
+  describe 'landing_plane' do
+    let(:plane) {double :plane}
+    it "Raises an error if at capacity" do
+      Airport::Default_capacity.times {subject.landing_plane double(:plane) }
+      expect {subject.landing_plane double(:plane) }.to raise_error "No space available"
+    end
   end
 
 end
