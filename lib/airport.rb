85d91bd67acb992828a378640564d9fb4026c54f
Sam Pritchard <sampritchard@msn.com>
2017-05-14 11:02:39 +0100

    test to fail if trying to take off a plane which has not landed at the airport

--- lib/airport.rb
+++ lib/airport.rb
@@ -13,10 +13,17 @@ class Airport
 
   def take_off(plane)
     fail "This plane is not at the airport!" if empty?
+    #refactor below here??
+    if @plane.include? plane
     @plane.pop
+    else
+    fail "This plane is not at the airport!"
+  end
   end
 
   def empty?
     @plane.count == 0
   end
+
+
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -21,9 +21,11 @@ describe Airport do
     it { is_expected.to respond_to(:take_off).with(1).argument}
 
     describe '#take_off' do
-      it 'raises an error when attempting to take off a plane at an empty airport' do
+      it 'raises an error when attempting to take off a plane which is not at the airport' do
         plane = double(:plane)
-        expect { subject.take_off(plane) }.to raise_error "This plane is not at the airport!"
+        plane2 = double(:plane)
+        subject.land_plane(plane)
+        expect { subject.take_off(plane2) }.to raise_error "This plane is not at the airport!"
       end
     end
 end
