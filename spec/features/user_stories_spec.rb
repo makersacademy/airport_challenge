c8b449696cfe50de76580be3a5fc879e56816c7a
Alex <alex.satur89@gmail.com>
2017-05-14 14:03:16 +0100

    FInal user story

--- spec/features/user_stories_spec.rb
+++ spec/features/user_stories_spec.rb
@@ -1,7 +1,5 @@
 describe 'User Stories' do
-  #As an air traffic controller
-  #So I can get passengers to a destination
-  #I want to instruct a plane to land at an airport and confirm that it has landed
+
   it "Allows ATC to instruct plane to land at airport" do
     airport = Airport.new(2)
     plane = Plane.new
@@ -9,19 +7,12 @@ describe 'User Stories' do
     expect { airport.land(plane) }.not_to raise_error
   end
 
-  #As an air traffic controller
-  #So I can get passengers on the way to their destination
-  #I want to instruct a plane to take off from an airport and confirm that it is no longer in the airport
-
   it "Allows ATC to instruct a plane to take off" do
     airport = Airport.new(2)
     plane = Plane.new
     expect { airport.takeoff(plane) }.not_to raise_error
   end
 
-  #As an air traffic controller
-  #To ensure safety
-  #I want to prevent takeoff and landing when weather is stormy
   context "When airport is stormy"
   it "Does not allow planes to land" do
     airport = Airport.new(2)
@@ -30,15 +21,6 @@ describe 'User Stories' do
     expect { airport.land(plane) }.to raise_error "Cannot land - Stormy"
   end
 end
-
-
-
-
-
-  #As an air traffic controller
-  #To ensure safety
-  #I want to prevent landing when the airport is full
-
   context "When airport is full" do
     it "Does not allow planes to land" do
       airport = Airport.new(2)
@@ -50,7 +32,3 @@ end
       expect { airport.land(plane) }.to raise_error "Cannot land - Airport full!"
     end
 end
-
-  #As the system designer
-  #So that the software can be used for many different airports
-  #I would like a default airport capacity that can be overridden as appropriate
