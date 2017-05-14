13c75ad1931ca9e5c28d1924907c141b4ed84cb6
jade <jadealvares@hotmail.co.uk>
2017-05-14 15:15:58 +0100

    changed weather from boolean to string 'stormy' or 'ok weather' and instance variable @weather_conditions

--- lib/airport.rb
+++ lib/airport.rb
@@ -2,11 +2,13 @@ require_relative "plane"
 
 class Airport
   attr_accessor :capacity, :planes
+  attr_reader :weather_conditions
   DEFAULT_CAPACITY = 500
 
   def initialize(capacity=DEFAULT_CAPACITY)
     @capacity = capacity
     @planes = []
+    @weather_conditions = "ok weather"
   end
 
   def airport_capacity(capacity)
@@ -15,6 +17,7 @@ class Airport
 
   def land_in_airport(plane)
     fail "Airport is full" if full?
+    fail "Due to stormy weather, plane cannot currently land in airport" unless true
       @planes << plane
     end
 
@@ -30,7 +33,9 @@ class Airport
   def weather?
     @weather_conditions = 1 + rand(10)
     if @weather_conditions <= 9
-      true
+      "ok weather"
+    else
+      "stormy"
     end
   end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -24,16 +24,28 @@ describe Airport do
   end
 
   describe "#weather?" do     # <--user story 3
-  it "Should be able to set the weather" do
-    if @weather_conditions == true
+  it "should allow the plane to leave an airport if the weather is not stormy" do
+    if @weather_conditions == "ok weather"
       expect(subject.weather?).to respond_to(:leave_airport)
       end
     end
   end
 
-  it "raises an error when there is stormy weather, so that plane does not leave an airport" do # <--user story 3
-    if @weather_conditions == false
+  it "should instruct a plane that there is stormy weather, so that plane does not leave an airport" do # <--user story 3
+    if @weather_conditions == "stormy"
       expect { subject.leave_airport Plane.new }.to raise_error "Due to stormy weather, plane cannot take off"
+    end
+  end
+
+  it "should allow a plane to land if the weather is not stormy" do    # <--user story 4
+    if @weather_conditions == "ok weather"
+      expect(subject.weather?).to respond_to(:land_in_airport).with(1).argument
+    end
+  end
+
+  it "should instruct a plane to not land in the airport if there is stormy weather conditions" do # <--user story 4
+    if @weather_conditions == "stormy"
+      expect { subject.land_in_airport Plane.new }.to raise_error "Due to stormy weather, plane cannot currently land in airport"
     end
   end
 end
