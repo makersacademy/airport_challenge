eb21350bd22fae56c9be9c8081816a11d4a698ea
Aaron Rodrigues <aaronrodrigues@Aarons-MBP.home>
2017-05-14 14:44:46 +0100

    Adds test to check if sunny
    Added double :weather sunny?:true

--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -2,9 +2,13 @@ require 'weather'
 
 describe Weather do
 
+	let(:weather) {double :weather, sunny?: true}
+
 	it {is_expected.to respond_to :sunny?}
 
 	it 'checks if it is sunny' do
-		expect(Weather.new).to be_sunny
+		expect(weather).to be_sunny
 	end
+
+	
 end
