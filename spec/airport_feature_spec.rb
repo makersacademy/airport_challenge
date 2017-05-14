9b4fddaa94fb9a1721b598cb8bb37049e3670e50
Mário Ribeiro <mail@marioribeiro.com>
2017-05-14 17:20:51 +0100

    Organize structure of the feature test file

--- spec/airport_feature_spec.rb
+++ spec/airport_feature_spec.rb
@@ -1,13 +1,16 @@
 require 'airport'
 
-feature 'Air Traffic Control' do
-  scenario 'Instructs a plane to land when its not stormy' do
+feature 'Air Traffic Controller' do
+
+  # LANDING
+
+  scenario 'Instructs a plane to land when it is not stormy' do
     given_there_is_an_airport
     and_a_plane
     and_weather_is_not_stormy
     i_instruct_the_plane_to_land_at(@airport)
-    and_confirm_that_it_has_landed
-    and_the_plane_location_is_set_to_arrival_airport
+    and_planes_confirms_that_it_has_landed
+    and_the_plane_location_is_set_to_the_arrival_airport
   end
 
   scenario 'Instructs a plane to land when stormy' do
@@ -17,6 +20,24 @@ feature 'Air Traffic Control' do
     i_instruct_the_plane_to_land_with_stormy_weather_at(@airport)
     and_expect_an_error
   end
+
+  scenario 'After a plane landed the airport, it is added to the list of planes on ground at that airport' do
+    given_there_is_an_airport
+    and_a_plane
+    and_weather_is_not_stormy
+    i_instruct_the_plane_to_land_at(@airport)
+    the_list_of_planes_on_ground_now_contains_the_plane_that_landed
+  end
+
+  scenario 'Instructs plane to land on a full airport' do
+    given_there_is_an_airport
+    and_a_plane
+    and_weather_is_not_stormy
+    i_instruct_the_plane_to_land_on_a_full(@airport)
+    and_expect_an_error
+  end
+
+  # TAKEOFF
 
   scenario 'Instructs a plane to takeoff when stormy' do
     given_there_is_an_airport
@@ -39,53 +60,60 @@ feature 'Air Traffic Control' do
     and_the_plane_location_is_on_air
   end
 
-  scenario 'After a plane landed the airport, it is added to the list of planes on ground at that airport' do
-    given_there_is_an_airport
-    and_a_plane
-    and_weather_is_not_stormy
-    i_instruct_the_plane_to_land_at(@airport)
-    the_list_of_planes_on_ground_now_contains_the_plane_that_landed
-  end
+  
+  # INSTRUCTIONS ########################
 
-  scenario 'Instructs plane to land on a full airport' do
-    given_there_is_an_airport
-    and_a_plane
-    and_weather_is_not_stormy
-    i_instruct_the_plane_to_land_on_a_full(@airport)
-    and_expect_an_error
-  end
+
+  # Create an Airport
 
   def given_there_is_an_airport
     @airport = Airport.new("LHR",1)
   end
   
+   # Create a Plane
+
   def and_a_plane
     @plane = Plane.new("G-ZBKP")
   end
+
+  # Land the plane to make sure it on ground
 
   def and_the_plane_is_in_the_airport
     allow(@airport).to receive(:stormy?).and_return false
     @airport.land_plane(@plane)
   end
 
+  # Set airport.stormy? to false
+
   def and_weather_is_not_stormy
     allow(@airport).to receive(:stormy?).and_return false
   end
 
+  # Empty. Right now is just to make the story flow.. :)
   def and_expect_an_error
   end
 
+  # Set weather to stormy
   def and_weather_is_stormy
     allow(@airport).to receive(:stormy?).and_return true
   end
+
+  # Create a destination airport
 
   def and_the_destination_airport_exists
     @destination = Airport.new("LIS")
   end
 
+
+  # LANDING INSTRUCTIONS ########################
+
+  # Not stormy
+
   def i_instruct_the_plane_to_land_at(airport)
     @airport.land_plane(@plane)
   end
+
+  # Landing instructions to a full airport 
 
   def i_instruct_the_plane_to_land_on_a_full(airport)
     rejected_plane = Plane.new("Z-BJTR")
@@ -94,29 +122,48 @@ feature 'Air Traffic Control' do
     expect { @airport.land_plane(@rejected_plane) }.to raise_error 'Operation aborted: Airport is full'
   end
 
+  # Stormy
+
   def i_instruct_the_plane_to_land_with_stormy_weather_at(airport)
     expect { @airport.land_plane(@plane) }.to raise_error 'Operation aborted: Weather is stormy'
   end
 
-   def i_instruct_a_plane_to_takeoff_with_stormy_weather_at(plane, airport)
-    expect { @airport.takeoff_plane(@plane, @destination) }.to raise_error 'Operation aborted: Weather is stormy'
-  end
+  # Plane confirms to be on ground after landing
 
-  def and_confirm_that_it_has_landed 
+  def and_planes_confirms_that_it_has_landed 
     expect(@plane.on_ground).to eq true
   end
 
-  def and_the_plane_location_is_set_to_arrival_airport
+  # Update plane location to the arrival airport
+
+  def and_the_plane_location_is_set_to_the_arrival_airport
     expect(@plane.location).to eq @airport.iata_code
   end
+
+  # Checks if the plane location is the arrival airport
 
   def the_list_of_planes_on_ground_now_contains_the_plane_that_landed
     expect(@airport.planes_on_ground).to eq ["G-ZBKP"]
   end
 
+
+
+  # TAKEOFF INSTRUCTIONS ########################
+
+  # Not stormy
+
   def i_instruct_a_plane_to_takeoff(plane, to_destination)
     @airport.takeoff_plane(@plane, @destination)
   end
+
+  # Stormy
+
+  def i_instruct_a_plane_to_takeoff_with_stormy_weather_at(plane, airport)
+    expect { @airport.takeoff_plane(@plane, @destination) }.to raise_error 'Operation aborted: Weather is stormy'
+  end
+
+
+  # Confirmations
 
   def and_the_plane_confirms_departure
     expect(@plane.on_ground).to eq false
@@ -124,5 +171,6 @@ feature 'Air Traffic Control' do
 
   def and_the_plane_location_is_on_air
     expect(@plane.location).to eq "air"
-  end
+  end 
+  
 end
