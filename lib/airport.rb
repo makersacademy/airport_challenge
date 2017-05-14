529b33d5197efcdaf5364deda374ceec37f63af3
IAN SIMPSON <ianconradsimpson@gmail.com>
2017-05-14 14:20:05 +0100

    Added weather method for landing

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,4 +1,4 @@
-require 'plane'
+require_relative 'plane.rb'
 
 class Airport
 
@@ -7,6 +7,11 @@ class Airport
   end
 
   def weather
-    false
+    bad_weather = "Apologies due to bad weather you'll not be able to land!"
+    good_weather = "Welcome, you're clear to land"
+    random_number = rand(11)
+    weather_result = random_number == 1 || random_number == 4 ? bad_weather : good_weather
+    weather_result
   end
+
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -9,7 +9,7 @@ describe Airport do
   end
 
   it "doesn't allow planes to land or takeoff in bad weather" do
-    expect(subject.weather).to eq false
+    expect(subject.weather).to eq "Apologies due to bad weather you'll not be able to land!"
   end
 
 end
