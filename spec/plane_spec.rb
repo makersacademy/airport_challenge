267e3b351cb6e144320ed2b0bcd79d7ebe98e801
Kynosaur <kye.bracey@gmail.com>
2017-05-14 11:56:10 +0100

    Add test to plane_spec for raising an exception when trying to take_off from the wrong airport

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -4,17 +4,9 @@ describe Plane do
   subject(:plane) { described_class.new }
   let(:airport) {double :airport}
 
-  it 'should allow user to instruct a plane to take off' do
-    expect(plane).to respond_to(:take_off)
-  end
-
   it 'should confirm that it is no longer in the airport' do
     plane.take_off
     expect(plane).not_to be_in_airport
-  end
-
-  it 'should allow user to instruct a plane to land' do
-    expect(plane).to respond_to(:land)
   end
 
   it 'should confirm that it has landed' do
@@ -31,4 +23,8 @@ describe Plane do
     expect{ plane.land(airport) }.to raise_error "Unable to land - airport full"
   end
 
+  it 'should raise an exception when trying to take off from an airport it is not at' do
+      allow(airport).to receive(:planes).and_return []
+      expect{ plane.take_off(airport) }.to raise_error "Plane is not at that airport"
+  end
 end
