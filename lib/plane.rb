804ab7bf2a03b82beea842d5f39534f8a8daa0ea
Colin Scally <colin.scally@gmail.com>
2017-05-15 00:02:35 +0100

    minor refactoring

--- lib/plane.rb
+++ lib/plane.rb
@@ -9,20 +9,28 @@ class Plane
   end
 
   def land_at(airport)
-    fail "There's no space to land" if airport.planes.count == airport.capacity
-    fail "Looks like I picked the wrong week to quit sniffing glue..." if airport.weather == "storm"
-    fail "This plane is already landed" if flying == "landed"
+    land_guard_conditions(airport)
     puts "#{self} is coming in to land at #{airport.airport_name}"
     airport.planes << self
     flying_switch
   end
 
+  def land_guard_conditions(airport)
+    fail "There's no space to land" if airport.planes.count == airport.capacity
+    fail "Looks like I picked the wrong week to quit sniffing glue..." if airport.weather == "storm"
+    fail "This plane is already landed" if flying == "landed"
+  end
+
   def depart_from(airport)
-    fail "This plane isn't at this airport" unless airport.planes.include?(self)
-    fail "We can't take off in a storm" if airport.weather == "storm"
+    depart_guard_conditions(airport)
     puts "#{self} has taken off from #{airport.airport_name}"
     airport.planes.delete(self)
     flying_switch
+  end
+
+  def depart_guard_conditions(airport)
+    fail "This plane isn't at this airport" unless airport.planes.include?(self)
+    fail "We can't take off in a storm" if airport.weather == "storm"
   end
 
   def flying_switch
