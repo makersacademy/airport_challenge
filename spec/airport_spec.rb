fb837c3eab315d35af382f94c85ffa0d37d9d14d
Andrew Clarke <andrewclarke982@gmail.com>
2017-05-14 13:32:10 +0100

    UT(fail) - Test to see if plane_landed in Airport

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,7 +2,12 @@ require 'airport'
 
 
 describe Airport do
-  
+
   it {is_expected.to respond_to :land_plane}
+
+  it 'lands plane' do
+    plane = subject.land_plane
+    expect(plane).to be_landed
+  end
 
 end
