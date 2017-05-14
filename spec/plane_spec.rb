e909fb0075b3070e2d6eedbedeb931d2c13bc270
Mário Ribeiro <mail@marioribeiro.com>
2017-05-14 18:50:57 +0100

    Refactor code to remove subject

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -1,7 +1,7 @@
 require 'plane'
 
 describe Plane do
-  subject { described_class.new("G-ZBKP") }
+  subject(:plane) { described_class.new("G-ZBKP") }
   it { is_expected.to respond_to(:land).with(1).argument }
   it { is_expected.to respond_to(:takeoff).with(1).argument }
   it { is_expected.to respond_to(:on_ground) }
@@ -12,35 +12,35 @@ describe Plane do
   describe '#land' do
     it 'can land on airport' do
       airport = Airport.new("LHR")
-      expect(subject.land(airport))
+      expect(plane.land(airport))
     end
 
     it 'confirms when lands' do
       airport = Airport.new("LHR")
-      subject.land(airport)
-      expect(subject.on_ground).to be true
+      plane.land(airport)
+      expect(plane.on_ground).to be true
     end
 
     it 'sets location to the airport it has landed' do
       airport = Airport.new("LHR")
-      subject.land(airport)
-      expect(subject.location).to eq airport.iata_code
+      plane.land(airport)
+      expect(plane.location).to eq airport.iata_code
     end
   end
 
   describe '#takeoff' do
     it 'can takeoff from airport to a destination' do
-      expect(subject.takeoff(:destination))
+      expect(plane.takeoff(:destination))
     end
 
     it 'confirms departure' do
-      subject.takeoff(:destination)
-      expect(subject.on_ground).to be false
+      plane.takeoff(:destination)
+      expect(plane.on_ground).to be false
     end
 
     it 'sets location to flying after takeoff' do
-      subject.takeoff(:destination)
-      expect(subject.location).to eq "air"
+      plane.takeoff(:destination)
+      expect(plane.location).to eq "air"
     end
   end
 end
