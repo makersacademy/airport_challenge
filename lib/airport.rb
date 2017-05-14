a0070a6e6657cc66222356181247599c3c451d16
Andrew Clarke <andrewclarke982@gmail.com>
2017-05-14 14:47:34 +0100

    UT(pass) - Does Airport remember plane to take_off ?

--- lib/airport.rb
+++ lib/airport.rb
@@ -7,11 +7,13 @@ class Airport
   end
 
   def take_off(plane)
-    # let plane take off that landed
-    plane
+    # stores the plane in the 'state' of this instance
+    @plane = plane
   end
 
   def plane
+    # returns plane 
+    @plane
   end
 
 end
