a5b3173cbeaca6544226a2fa82011998f5e7f195
Kavita <kkavita92@gmail.com>
2017-05-14 11:40:27 +0100

    increase test coverage for plane_spec

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -5,4 +5,19 @@ describe Plane do
   it { is_expected.to respond_to(:airborne) }
   it { is_expected.to respond_to(:takeoff) }
 
+  describe '#landed' do
+    it 'should change flight airborne status upon landing' do
+      subject.landed
+      expect(subject.airborne?).to eq false
+    end
+  end
+
+  describe '#takeoff' do
+    it 'should change flight airborne status upon takeoff' do
+      subject.landed
+      subject.takeoff
+      expect(subject).to be_airborne
+    end
+  end
+
 end
