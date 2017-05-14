0a93fbf7c8bc618cb8faf32832f3cd13146ff188
Mário Ribeiro <mail@marioribeiro.com>
2017-05-14 12:07:39 +0100

    Modify Plane to set its location after landing / takeoff

--- spec/airport_feature_spec.rb
+++ spec/airport_feature_spec.rb
@@ -6,14 +6,16 @@ feature 'Air Traffic Control' do
     and_a_plane
     i_instruct_the_plane_to_land_at(@airport)
     and_confirm_that_it_has_landed
+    and_the_plane_location_is_set_to_arrival_airport
   end
 
   scenario 'Instructs a plane to take off' do
     given_there_is_an_airport
     and_a_plane
     and_the_destination_airport_exists
-    i_want_to_instruct_a_plane_to_takeoff(@plane, @destination)
-    and_the_plane_confirms_departure 
+    i_instruct_a_plane_to_takeoff(@plane, @destination)
+    and_the_plane_confirms_departure
+    and_the_plane_location_is_on_air
   end
 
   def given_there_is_an_airport
@@ -32,15 +34,24 @@ feature 'Air Traffic Control' do
     @airport.land_plane(@plane)
   end
 
+
   def and_confirm_that_it_has_landed 
     expect(@plane.on_ground).to eq true
   end
 
-  def i_want_to_instruct_a_plane_to_takeoff(plane, to_destination)
+  def and_the_plane_location_is_set_to_arrival_airport
+    expect(@plane.location).to eq @airport
+  end
+
+  def i_instruct_a_plane_to_takeoff(plane, to_destination)
     @airport.takeoff_plane(@plane, @destination)
   end
 
   def and_the_plane_confirms_departure
     expect(@plane.on_ground).to eq false
+  end
+
+  def and_the_plane_location_is_on_air
+    expect(@plane.location).to eq "air"
   end
 end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -4,6 +4,8 @@ describe Plane do
   it { is_expected.to respond_to(:land).with(1).argument }
   it { is_expected.to respond_to(:takeoff).with(1).argument }
   it { is_expected.to respond_to(:on_ground)}
+  it { is_expected.to respond_to(:location)}
+
   
   describe '#land' do
     it 'can land on airport' do
@@ -13,6 +15,11 @@ describe Plane do
     it 'confirms when lands' do
       subject.land(:airport)
       expect(subject.on_ground).to be true
+    end
+
+    it 'sets location to the airport it has landed' do
+      subject.land(:airport)
+      expect(subject.location).to eq :airport 
     end
   end
 
@@ -24,6 +31,11 @@ describe Plane do
     it 'confirms departure' do
       subject.takeoff(:destination)
       expect(subject.on_ground).to be false
+    end
+
+    it 'sets location to flying after takeoff' do
+      subject.takeoff(:destination)
+      expect(subject.location).to eq "air"
     end
   end
 end
