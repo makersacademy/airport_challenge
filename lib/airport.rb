d2370afc8c6d7c48e80d073c5f579b3a2e87301e
Charlotte Fereday <cbrfereday@gmail.com>
2017-05-15 10:32:51 +0100

    Refactors from code review to include a private method, require relative paths.Comments added for extra refactoring that I could do to add more specific raise messages for land & takeoff

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,11 +1,11 @@
-require './lib/plane'
-require './lib/weather'
+require_relative 'plane'
+require_relative 'weather'
 
 class Airport
 
   MAX_CAPACITY = 10
 
-  attr_reader :landed_planes
+  attr_reader :landed_planes, :capacity, :station
 
   def initialize(station = WeatherStation.new, capacity = MAX_CAPACITY)
     @landed_planes = []
@@ -18,12 +18,22 @@ class Airport
     plane.flight_status = :landed
     @landed_planes << plane
   end
+  # would be good to have more specific raise messages about why the plane can't land
+  # (e.g. already landed, already taken off, stormy weather)
 
   def takeoff(plane)
     raise "Can't take off plane!" if should_not_take_off?(plane)
     plane.flight_status = :taken_off
     @landed_planes.delete(plane)
   end
+
+  # would be good to have more specific raise messages about why the plane can't takeoff
+  # (e.g. already landed, already taken off, stormy weather)
+  def present?(plane)
+    @landed_planes.include?(plane)
+  end
+
+  private
 
   def should_not_land?(plane)
     plane.landed? || plane.taken_off? || stormy? || at_capacity?
@@ -39,10 +49,6 @@ class Airport
 
   def at_capacity?
     @landed_planes.length >= @capacity
-  end
-
-  def present?(plane)
-    @landed_planes.include?(plane)
   end
 
 end
