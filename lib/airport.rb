b014d6440967e06363c32092fee2f27e4338f027
Mário Ribeiro <mail@marioribeiro.com>
2017-05-15 11:14:38 +0100

    Refactor land_plane and takeoff_plane

--- lib/airport.rb
+++ lib/airport.rb
@@ -15,16 +15,16 @@ class Airport
   end
 
   def land_plane(plane)
-    raise 'Operation aborted: Weather is stormy' if stormy?
-    raise 'Operation aborted: Airport is full' if full?
-    raise 'The plane is already at #{@iata_code}' if plane.location == @iata_code
-    raise 'Operation aborted: Plane is on ground' if plane.on_ground == true
+    raise 'Operation aborted: Weather is stormy'                     if stormy?
+    raise 'Operation aborted: Airport is full'                       if full?
+    raise 'Operation aborted: The plane is already at #{@iata_code}' if plane.location == @iata_code
+    raise 'Operation aborted: Plane is on ground'                    if plane.on_ground == true
     @planes_on_ground << plane.land(self)
   end
 
   def takeoff_plane(plane, destination)
-    raise 'Operation aborted: Weather is stormy' if stormy?
-    raise 'The plane is not at #{@iata_code}' if plane.location != @iata_code
+    raise 'Operation aborted: Weather is stormy'                     if stormy?
+    raise 'Operation aborted: The plane is not at #{@iata_code}'     if plane.location != @iata_code
     @planes_on_ground.delete(plane.takeoff(destination))
   end
 
