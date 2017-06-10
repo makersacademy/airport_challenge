79bc35ee9ab78fe56287a61f38dfb2c1316e873e
Verity <vhonebon@gmail.com>
2017-06-10 17:54:25 +0100

    land method and confirm land methods added

--- docs/airport.rb
+++ docs/airport.rb
@@ -1,2 +1,12 @@
+require './docs/plane.rb'
 class Airport
+
+def land(plane)
+  plane
+end
+
+def confirm_land(plane)
+  land(plane) == plane
+end
+
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,5 +1,16 @@
 require './docs/airport.rb'
 
 describe Airport do
+	it { is_expected.to respond_to :land }
 
+	it "will return plane if land method is called" do
+	      plane = Plane.new
+	      expect(subject.land(plane)).to eq(plane)
+	    end
+
+	it "will confirm if a plane has landed" do
+	      plane = Plane.new
+				subject.land(plane)
+	      expect(subject.confirm_land(plane)).to eq(true)
+	    end
 end
