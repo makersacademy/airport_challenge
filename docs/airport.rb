25ecf95ee6a2712940ee52155cf7da687df699a0
Verity <vhonebon@gmail.com>
2017-06-10 19:10:47 +0100

    Added take off method and way of recording planes currently at airport

--- docs/airport.rb
+++ docs/airport.rb
@@ -1,12 +1,26 @@
 require './docs/plane.rb'
 class Airport
 
+attr_accessor :planes
+
+def initialize
+  @planes = []
+end
+
 def land(plane)
-  plane
+  @planes << plane
 end
 
 def confirm_land(plane)
-  land(plane) == plane
+  @planes.include?(plane)
+end
+
+def take_off(plane)
+  @planes.pop
+end
+
+def confirm_take_off(plane)
+  !@planes.include?(plane)
 end
 
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -3,9 +3,9 @@ require './docs/airport.rb'
 describe Airport do
 	it { is_expected.to respond_to :land }
 
-	it "will return plane if land method is called" do
+	it "will record that a plane is at airport if land method is called" do
 	      plane = Plane.new
-	      expect(subject.land(plane)).to eq(plane)
+	      expect(subject.land(plane)).to eq([plane])
 	    end
 
 	it "will confirm if a plane has landed" do
@@ -13,4 +13,17 @@ describe Airport do
 				subject.land(plane)
 	      expect(subject.confirm_land(plane)).to eq(true)
 	    end
+
+	it { is_expected.to respond_to :take_off }
+
+	it "will record that a plane has taken off when the take off method is called" do
+				plane = Plane.new
+				subject.land(plane)
+				subject.take_off(plane)
+				expect(subject.planes).not_to include(plane)
+			end
+
+	it "has a default setting of empty (no planes)" do
+				expect(subject.planes).to eq []
+			end
 end
