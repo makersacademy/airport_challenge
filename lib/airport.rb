946d3bb94cee265a8b503070cb11285cd2a1a7f1
Jasper King-Harman <j.kingharman@googlemail.com>
2017-06-10 22:25:14 +0100

    Building out airport and writing specs with mock plane and weather

--- lib/airport.rb
+++ lib/airport.rb
@@ -2,6 +2,7 @@
 
 # SPP: understands how to land and take off aircraft
 
+# Do you need some way to start with planes?
 
 class Airport
 
@@ -22,20 +23,57 @@ def is_full?
   hanger.size == capacity
 end
 
-def check_air_space_for(aircraft)
+def check_airspace_for(aircraft)
   fail "The plane is not flying" unless aircraft.flying?
 end
 
 def check_hanger_for(aircraft)
-  fail "The plane is not here" unless hanger.include(aircraft)
+  fail "The plane is not here" unless hanger.include?(aircraft)
 end
 
 def check_capacity
-  fail "There's no space to land here." if self.is_full?
+  fail "There's no space to land here." if is_full?
 end
 
 def check_weather
-   fail "It's too stormy to fly" if weather.stormy?
+   fail "It's too stormy" if weather.stormy?
+end
+
+def prepare_for_landing(aircraft)
+  check_weather
+  check_airspace_for(aircraft)
+  check_capacity
+end
+
+def prepare_for_takeoff(aircraft)
+  check_weather
+  check_hanger_for(aircraft)
+end
+
+def dock(aircraft)
+  hanger << aircraft
+end
+
+def dedock(aircraft)
+  hanger.delete(aircraft)
+end
+
+attr_reader :capacity, :weather
+
+public
+
+attr_reader :hanger
+
+def land(aircraft)
+  prepare_for_landing(aircraft)
+  aircraft.land
+  dock(aircraft)
+end
+
+def takeoff(aircraft)
+  prepare_for_takeoff(aircraft)
+  dedock(aircraft)
+  aircraft.fly
 end
 
 
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -7,8 +7,121 @@ require_relative "../lib/airport"
 
 describe Airport do
 
-  
+  let(:plane) { double("Plane") }
+  let(:weather) { double("Weather")}
+
+  subject(:airport) { described_class.new({}, weather) }
+
+  describe "#land" do
+
+    before do
+        allow(weather).to receive(:stormy?).and_return(true)
+    end
+
+    context "when stormy" do
+
+      it "raises an error with descriptive message" do
+        expect{airport.land(plane)}.to raise_error("It's too stormy")
+      end
+
+    end
+
+    context "when plane not in air" do
+
+      before do
+          allow(weather).to receive(:stormy?).and_return(false)
+          allow(plane).to receive(:flying?).and_return(false)
+      end
+
+      it "raises an error with descriptive message" do
+        expect{airport.land(plane)}.to raise_error("The plane is not flying")
+      end
+
+    end
+
+    context "when airport full" do
+
+      before do
+          allow(weather).to receive(:stormy?).and_return(false)
+          allow(plane).to receive(:flying?).and_return(true)
+          allow(airport).to receive(:is_full?).and_return(true)
+      end
+
+      it "raises an error with descriptive message" do
+        expect{airport.land(plane)}.to raise_error("There's no space to land here.")
+      end
+
+    end
+
+    context "when checks are all passed" do
+
+      before do
+          allow(weather).to receive(:stormy?).and_return(false)
+          allow(plane).to receive(:flying?).and_return(true)
+          allow(plane).to receive(:land)
+          allow(airport).to receive(:is_full?).and_return(false)
+      end
+
+      it "docks the plane" do
+        airport.land(plane)
+        expect(airport.hanger).to include(plane)
+      end
+
+# You need to write a test for physical act of landing the plane.
+
+    end
+
+  end
 
 
+    describe "takeoff" do
 
-end
+
+          before do
+              allow(weather).to receive(:stormy?).and_return(true)
+          end
+
+          context "when stormy" do
+
+            it "raises an error with descriptive message" do
+              expect{airport.takeoff(plane)}.to raise_error("It's too stormy")
+            end
+
+          end
+
+          context "when plane not on ground" do
+
+            before do
+                allow(weather).to receive(:stormy?).and_return(false)
+            end
+
+            it "raises an error with descriptive message" do
+              expect{airport.takeoff(plane)}.to raise_error("The plane is not here")
+            end
+
+          end
+
+          context "when checks passed" do
+
+              before do
+                  allow(weather).to receive(:stormy?).and_return(false)
+                  allow(plane).to receive(:flying?).and_return(true)
+                  allow(plane).to receive(:land)
+                  allow(plane).to receive(:fly)
+                  allow(airport).to receive(:is_full?).and_return(false)
+                  airport.land(plane)
+               end
+
+
+            it "dedocks the plane" do
+               expect{airport.takeoff(plane)}.to change{airport.hanger}.from([plane]).to([])
+            end
+
+            # You need to write a test to change physical flight
+
+          end
+
+    end
+
+
+  end
