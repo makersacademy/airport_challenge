b25feddffc69ea7e22513357cf2367ae6661675f
Simon Hempenstall <simonjhemp@hotmail.com>
2017-06-11 16:44:10 +0100

    Add a random weather generator and update tests

--- lib/weather.rb
+++ lib/weather.rb
@@ -1,6 +1,12 @@
 class Weather
 
-  def stormy
+  def stormy?
+    change_weather == :stormy
+  end
+
+  def change_weather
+    forecast = [:stormy, :sunny, :sunny, :sunny, :sunny]
+    forecast.sample
   end
 
 end
--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -4,8 +4,22 @@ describe Weather do
 
 let(:weather) {Weather.new}
 
-  it "responds to stormy method" do
-  expect(weather).to respond_to :stormy
+describe "#stormy?" do
+    it "responds to stormy method" do
+    expect(weather).to respond_to :stormy?
+    end
+  end
+
+describe "#change_weather" do
+    it "can randomly change the forecast to stormy" do
+    allow(weather).to receive(:change_weather) {:stormy}
+    expect(weather.change_weather).to eq :stormy
+    end
+
+    it "can randomly change the forecast to sunny" do
+    allow(weather).to receive(:change_weather) {:sunny}
+    expect(weather.change_weather).to eq :sunny
+    end
   end
 
 end
