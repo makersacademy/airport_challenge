2d1c679b0cdaba62e59f0767682e40e20a7e0ae6
paulmillen <pwmillen@gmail.com>
2017-07-07 18:30:58 +0100

    tested airport#stormy?

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -5,6 +5,14 @@ describe Airport do
   it { is_expected.to respond_to :stormy? }
   it { is_expected.to respond_to :full? }
 
+  describe '#stormy?' do
+
+    it 'is sometimes stormy' do
+      weather = Airport.new.stormy?
+      expect(weather).to be_(true).or be(false)
+    end
+  end
+
   describe '#initialize' do
 
     it 'initializes' do
@@ -15,6 +23,11 @@ describe Airport do
     it 'has a default capacity' do
       airport = Airport.new
       expect(airport.capacity).to eq Airport::DEFAULT_CAPACITY
+    end
+
+    it 'has a default capacity that can be overridden' do
+      airport = Airport.new(rand(10))
+      expect(airport.capacity).to eq airport.capacity
     end
   end
 
