f8e0d631d6f4a1613a056db5a0e412ec48dd04bd
Petra Chocholova <petra.chocholova@gmail.com>
2017-07-08 00:48:33 +0200

    Weather class added, Airport has capacity and lands planes

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,9 +1,29 @@
 class Airport
+  CAPACITY = 10
+
+  attr_reader :capacity
+
+  def initialize(capacity = CAPACITY)
+    @capacity = capacity
+    @planes = []
+  end
 
   def take_off
   end
 
   def land(plane)
+    raise "#{self.class.name} full" if full?
+    planes << plane
   end
 
+  def full?
+    planes.count >= capacity
+  end
+
+  def empty?
+    planes.empty?
+  end
+
+  private
+  attr_reader :planes
 end
