43b9c7e68aa3270f7dbe94addaa1880cf663018d
oleglukyanov <oleg.s.lukyanov@gmail.com>
2017-07-08 18:51:53 +0100

    Update README

--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -10,7 +10,7 @@ describe WeatherTest do
   it 'is expected to be stormy on rare occasions' do
     rare = (0.01..0.1)
     stats = []
-    100.times { stats << subject.stormy? }
+    1000.times { stats << subject.stormy? }
     stormy_ratio = stats.select { |n| n == true }.count / stats.count.to_f
     expect(rare.include? stormy_ratio).to be true
   end
