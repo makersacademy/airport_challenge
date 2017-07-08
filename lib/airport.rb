90a781ceeefda2987b71cd14a2126518940b1acf
oleglukyanov <oleg.s.lukyanov@gmail.com>
2017-07-08 22:57:24 +0100

    Do minor tweaks

--- lib/airport.rb
+++ lib/airport.rb
@@ -3,7 +3,7 @@ require_relative 'weather'
 class Airport
   include Weather
   DEFAULT_CAPACITY = 50
-  attr_reader :capacity, :planes
+  attr_reader :capacity
 
   def initialize(capacity = DEFAULT_CAPACITY)
     @capacity = capacity
