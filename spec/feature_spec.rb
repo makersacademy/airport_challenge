9a66fec4e2fe9bbd94cdd02875ae1e202c7d40b0
Phil Lang <plangphoto@gmail.com>
2017-07-08 16:52:35 +0100

    testing my work with features

--- spec/feature_spec.rb
+++ spec/feature_spec.rb
@@ -3,6 +3,14 @@ require './lib/airport'
 airport = Airport.new
 plane = Plane.new
 weather = Weather.new
+
+weather.storms?
+p weather
+
+airport.request_landing(plane, weather)
 p airport
 p plane
-p weather
+
+airport.request_takeoff(plane, weather)
+p airport
+p plane
