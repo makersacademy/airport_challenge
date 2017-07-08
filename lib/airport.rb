f80134c74b3ea640e0e41574ca5ac48103cc8074
Eoin O Sullivan <esully666@gmail.com>
2017-07-08 12:11:47 +0000

    The airport stops planes from landing and taking off when the weather is too bad.

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,15 +1,48 @@
 require_relative 'plane.rb'
+require_relative 'weather.rb'
+
 class Airport
+  
+  attr_reader :hanger
+  
   def initialize
       @hanger = []
   end
   
-  def land(airplane)
-      airplane.the_airplane_has_landed_or_taken_off
-      @hanger << airplane
+  def permission_from_traffic_control(weather_report)
+     if weather_report == "Sunny"
+       @permission = true
+    else
+       @permission = false
+     end
+  end
+  
+
+   def permission_to_land(incoming_airplane)
+      if @permission
+        land(incoming_airplane)
+      else
+        'No permission to land due to poor weather!'
+      end
+   end
+     
+   def permission_to_take_off
+     if @permission
+       take_off
+     else
+       'No permission to take off due to poor weather!'
+     end
+   end
+    
+ 
+  def land(incoming_airplane)
+    
+      incoming_airplane.the_airplane_has_landed_or_taken_off
+      @hanger << incoming_airplane
   end
   
   def take_off
+      @hanger[0].the_airplane_has_landed_or_taken_off
       @hanger.shift
   end
   
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,18 +1,82 @@
 require 'airport'
+
 describe Airport do
     it {is_expected.to respond_to(:land)}
     it {is_expected.to respond_to(:take_off)}
+    it {is_expected.to respond_to(:hanger)}
+    it {is_expected.to respond_to(:permission_from_traffic_control)}
+    it {is_expected.to respond_to(:permission_to_take_off)}
+    it {is_expected.to respond_to(:permission_to_land)}
     
-    describe '#take_off' do
-        it 'tells a plane take_off' do
-            boeing = Plane.new
-            subject.land(boeing)
-            expect(subject.take_off).to eq boeing
+    describe '#traffic_control' do
+        it 'takes a random weather report and returns true or false' do
+        weather = Weather.new
+        weather_report = weather.random_weather_generator
+        expect(subject.permission_from_traffic_control(weather_report)).to eq(true).or(eq(false))
+        end
+        
+        it 'takes a weather report of Sunny and retuns true' do
+            weather_report = "Sunny"
+            expect(subject.permission_from_traffic_control(weather_report)).to eq(true)
+        end
+        
+        it 'takes a weather report of Stormy and retuns false' do
+            weather_report = "Stormy"
+            expect(subject.permission_from_traffic_control(weather_report)).to eq(false)
+        end
+        
+    end
+    
+    describe '#permission_to_land()' do
+        
+        it 'gives permission and lands a plane when its sunny' do
+        boeing = double(:airplane)
+        allow(boeing).to receive(:the_airplane_has_landed_or_taken_off)
+        subject.permission_from_traffic_control("Sunny")
+        expect(subject.permission_to_land(boeing)).to eq [boeing]
+        end
+        
+        it 'Stops plans from landing when its stormy' do
+        boeing = double(:airplane)
+        allow(boeing).to receive(:the_airplane_has_landed_or_taken_off)
+        subject.permission_from_traffic_control("Stromy")
+        expect(subject.permission_to_land(boeing)).to eq 'No permission to land due to poor weather!'
         end
     end
     
+    describe '#permission_take_off' do
+        
+        it 'gives permission to take off if the weather is Sunny' do
+            boeing = double(:airplane)
+            allow(boeing).to receive(:the_airplane_has_landed_or_taken_off)
+            subject.land(boeing)
+            subject.permission_from_traffic_control("Sunny")
+            expect(subject.permission_to_take_off).to eq boeing
+            
+        end
+        
+        it 'Stops planes from taking off and issues a failure when the weather is too bad' do
+            boeing = double(:airplane)
+            allow(boeing).to receive(:the_airplane_has_landed_or_taken_off)
+            subject.land(boeing)
+            subject.permission_from_traffic_control("Stormy")
+            expect(subject.permission_to_take_off).to eq 'No permission to take off due to poor weather!'
+            
+        end
+    end
+    
+    describe '#take_off' do
+
+            it 'enables a plane to take_off' do
+            boeing = Plane.new
+            subject.land(boeing)
+            expect(subject.take_off).to eq boeing
+            expect(subject.hanger).to eq []
+            end
+    end
+    
     describe '#land' do
-        it 'tells a plane to land' do
+        it 'enables a plane to land' do
             boeing = Plane.new
             expect(subject.land(boeing)).to eq [boeing]
         end
