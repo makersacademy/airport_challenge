da859c89b5c93f0f09f390fd35414973695d64fc
Phil Lang <plangphoto@gmail.com>
2017-07-08 15:15:48 +0100

    cleaned up spec files, added describe and broke out repeated variable representations

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,28 +1,33 @@
 require 'airport'
 
 describe Airport do
-  let(:plane) { double(:plane) }
+  let(:plane_true) { double(:plane, status?: true) }
+  let(:plane_false) { double(:plane, status?: false) }
   subject(:airport) { described_class.new }
 
   it { is_expected.to respond_to(:request_landing).with(1).argument }
   it { is_expected.to respond_to(:request_takeoff).with(1).argument }
 
   describe 'Airport landing request' do
+    before do
+      allow(plane_false).to receive(:landing)
+    end
+
     it 'requests a plane to land at the airport' do
-      plane = double(:plane, status?: false)
-      allow(plane).to receive(:landing)
-      request = airport.request_landing(plane)
-      expect(airport.request_landing(plane)).to eq request
+      request = airport.request_landing(plane_false)
+      expect(airport.request_landing(plane_false)).to eq request
     end
   end
 
   describe 'Airport take off request' do
+    before do
+      allow(plane_true).to receive(:landing)
+      allow(plane_true).to receive(:take_off)
+    end
+
     it 'requests a plane to take off from the airport' do
-      plane = double(:plane, status?: true)
-      allow(plane).to receive(:take_off)
-      allow(plane).to receive(:landing)
-      airport.request_landing(plane)
-      expect(airport.request_takeoff(plane)).to be plane
+      airport.request_landing(plane_true)
+      expect(airport.request_takeoff(plane_true)).to be plane_true
     end
   end
 end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -1,20 +1,21 @@
 require 'plane'
 
 describe Plane do
-  let(:plane) { double(:plane, landed?: true) }
+  subject(:plane) { described_class.new }
+
   it { is_expected.to respond_to(:status?) }
   it { is_expected.to respond_to(:landing) }
   it { is_expected.to respond_to(:take_off) }
 
   describe 'Plane status' do
     it 'set plane status to landed = true' do
-      subject.landing
-      expect(subject.status?).to eq true
+      plane.landing
+      expect(plane.status?).to eq true
     end
 
     it 'set plane status to landed = false' do
       subject.take_off
-      expect(subject.status?).to eq false
+      expect(plane.status?).to eq false
     end
   end
 end
