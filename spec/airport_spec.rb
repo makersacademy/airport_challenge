b8c5e708b57cbc94f28f6d188e22542183c875d9
Sapphire Mason-Brown <saphmb@gmail.com>
2017-07-09 12:21:54 +0100

    Test to show planes in airport

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -3,10 +3,20 @@ require 'plane'
 
 describe Airport do
   subject(:airport) { described_class.new }
+  plane = Plane.new
 
   it 'allows a plane to land' do
-    plane = Plane.new
     expect(airport.land(plane)).to eq [plane]
   end
+
+
+  it 'shows planes in airport' do
+    airport.land(plane)
+    expect(airport.planes).to eq [plane]
+  end
+
+  # it 'checks if a plane has landed' do
+  #
+  # end
 
 end
