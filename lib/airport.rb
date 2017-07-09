1f7f5f6841979ef7b93ebf360516b7d91065a6cd
annalaise <annalaise.gibbons@gmail.com>
2017-07-09 15:26:28 +0100

    landing and take off features added and passing green

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,26 +1,30 @@
 require_relative 'plane'
-# require_relative 'weather'
+require_relative 'weather'
 
 class Airport
   DEFAULT_CAPACITY = 10
 
-    def initialize
+    def initialize(capacity=DEFAULT_CAPACITY)
       @planes = []
       @capacity = DEFAULT_CAPACITY
+      @weather = Weather.new
     end
 
     def confirm_land(plane)
       fail "Landing denied: Airport full" if full?
+      fail "Landing denied: Adverse weather" if stormy?
       land(plane)
     end
 
     def land(plane)
+      plane.landed?
       @planes << plane
     end
 
     def take_off(plane)
+      fail "Take-off denied: Adverse weather" if stormy?
       @planes.delete(plane)
-      return @planes
+      plane.departed?
     end
 
     # private
@@ -31,6 +35,10 @@ class Airport
 
     def full?
       @planes.length >= @capacity
+    end
+
+    def stormy?
+      @weather.stormy?
     end
 
 end
