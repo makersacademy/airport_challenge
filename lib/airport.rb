8a5558a96059786ee3bbe93bded5f88a4cc66ac6
BDCraven <bencraven@hotmail.com>
2017-07-09 18:01:05 +0100

    Refactoring airport.rb

--- lib/airport.rb
+++ lib/airport.rb
@@ -14,24 +14,32 @@ class Airport
     fail 'Airport full' if full?
     fail "#{plane} has already landed" if @planes.include?(plane)
     fail 'Storms prevent landing' if weather.stormy?
-    plane.flying = false
-    plane.landed = true
     @plane = plane
-    @planes << plane
+    park_plane
   end
 
   def take_off(plane)
     fail 'No planes currently at the airport' if empty?
     fail 'Storms prevent take off' if weather.stormy?
     fail "Cannot take off. #{plane} is flying" if plane.flying == true
-    plane.flying = true
-    plane.landed = false
-    @planes.delete(plane)
-    @plane = nil
+    plane_leaves
     "#{plane} has taken off"
   end
 
   private
+  def park_plane
+    plane.flying = false
+    plane.landed = true
+    @planes << plane
+  end
+
+  def plane_leaves
+    plane.flying = true
+    plane.landed = false
+    @planes.delete(plane)
+    @plane = nil
+  end
+
   def full?
     @planes.count >= capacity
   end
