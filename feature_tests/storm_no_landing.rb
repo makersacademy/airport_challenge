df680b8267a1b1d0af79715f3b870e89fb45c883
annalaise <annalaise.gibbons@gmail.com>
2017-07-09 22:32:22 +0100

    removing redundant files

--- feature_tests/storm_no_landing.rb
+++
@@ -1,3 +0,0 @@
-# As an air traffic controller
-# To ensure safety
-# I want to prevent landing when weather is stormy 
--- feature_tests/storm_no_takeoff.rb
+++
@@ -1,3 +0,0 @@
-# As an air traffic controller
-# To ensure safety
-# I want to prevent takeoff when weather is stormy 
