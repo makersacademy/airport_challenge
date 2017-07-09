8f525abc07d033e1cd46da0df947bbfb292cbbee
James Ransome <jamesransome@msn.com>
2017-07-09 21:28:28 +0100

    stormy weather prevents take off and landing

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,22 +1,28 @@
+require 'weather'
+
 class Airport
   attr_reader :planes
   attr_reader :capacity
+  attr_reader :weather
 
   DEFAULT_CAPACITY = 30
 
   def initialize capacity = DEFAULT_CAPACITY
     @capacity = capacity
     @planes = []
+    @weather = Weather.new
   end
 
   def land plane
     fail("No space to land at this airport!") if full?
+    fail("Weather conditions not safe for landing!") if @weather.is_stormy?
     plane.land_at(self)
     @planes << plane
   end
 
   def take_off plane
     fail("Plane is not at this airport!") unless @planes.include?(plane)
+    fail("Weather conditions not safe for take off!") if @weather.is_stormy?
     plane.take_off
     @planes -= [plane]
   end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -3,9 +3,25 @@ require 'airport'
 describe Airport do
   subject(:airport) { described_class.new }
   let(:plane) { instance_double("plane") }
-  let(:weather) { double :weather }
+  let(:other_plane) { instance_double("plane") }
+  let(:weather) { airport.weather }
+
+  describe '#initialize' do
+    it 'initialises with a default capacity' do
+      expect(airport.capacity).to eq Airport::DEFAULT_CAPACITY
+    end
+
+    it 'can be initialized with a custom capacity' do
+      custom_capacity = 15
+      expect(Airport.new(custom_capacity).capacity).to eq custom_capacity
+    end
+  end
 
   describe '#land' do
+    before do
+      allow(weather).to receive(:is_stormy?) { false }
+    end
+
     it 'instructs a plane to land at the airport itself' do
       expect(plane).to receive(:land_at).with(airport)
       airport.land plane
@@ -19,9 +35,8 @@ describe Airport do
   end
 
   describe '#take_off' do
-    let(:other_plane) { double :plane }
-
     before do
+      allow(weather).to receive(:is_stormy?) { false }
       allow(plane).to receive(:land_at).with(airport)
       allow(plane).to receive(:landed?) { true }
       airport.land plane
@@ -44,23 +59,11 @@ describe Airport do
     end
   end
 
-  describe '#initialize' do
-    it 'initialises with a default capacity' do
-      expect(airport.capacity).to eq Airport::DEFAULT_CAPACITY
-    end
-
-    it 'can be initialized with a custom capacity' do
-      custom_capacity = 15
-      expect(Airport.new(custom_capacity).capacity).to eq custom_capacity
-    end
-  end
-
   context 'airport is full' do
-    let(:other_plane) { instance_double("plane") }
-
     before do
       Airport::DEFAULT_CAPACITY.times do
         allow(other_plane).to receive(:land_at).with(airport)
+        allow(weather).to receive(:is_stormy?) { false }
         airport.land(other_plane)
       end
     end
@@ -68,6 +71,26 @@ describe Airport do
     it 'does not allow planes to land' do
       allow(plane).to receive(:land_at).with(airport)
       expect { airport.land(plane) }.to raise_error("No space to land at this airport!")
+    end
+  end
+
+  context 'stormy weather' do
+    before do
+      allow(weather).to receive(:is_stormy?) { false }
+      allow(plane).to receive(:land_at).with(airport)
+      allow(plane).to receive(:landed?) { true }
+      airport.land(plane)
+      allow(weather).to receive(:is_stormy?) { true }
+    end
+
+    it 'prevents take off' do
+      allow(plane).to receive(:take_off)
+      expect { airport.take_off(plane) }.to raise_error("Weather conditions not safe for take off!")
+    end
+
+    it 'prevents landing' do
+      allow(plane).to receive(:land_at).with(airport)
+      expect { airport.land(plane) }.to raise_error("Weather conditions not safe for landing!")
     end
   end
 
