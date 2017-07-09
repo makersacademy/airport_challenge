f200976647dd6f79c519a3f8657d68dfbfedab9c
Kat Downes <katdownes@kats-mbp.home>
2017-07-09 18:38:37 +0100

    instructs a plane to land and confirms landing

--- lib/plane.rb
+++ lib/plane.rb
@@ -1,6 +1,10 @@
 class Plane
   def instruct_to_land
-    
+    "You have clearance to land the plane at the airport"
+  end
+
+  def landed?
+    true
   end
 
 end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -1,6 +1,21 @@
 require 'plane'
 
 describe Plane do
+  subject(:plane) {described_class.new}
+
   it {should respond_to (:instruct_to_land)}
+  it {should respond_to (:landed?)}
+
+  describe '#instruct_to_land' do
+    it "tells the plane to land at the airport" do
+      expect(plane.instruct_to_land).to eq "You have clearance to land the plane at the airport"
+  end
+end
+
+  describe '#landed?' do
+    it "confirms that the plane has landed" do
+      expect(plane.landed?).to eq true
+    end
+  end
 
 end
