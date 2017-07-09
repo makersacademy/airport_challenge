9aa30338235e1c563a9bede5fe88fcf7d6bf7ffa
oleglukyanov <oleg.s.lukyanov@gmail.com>
2017-07-10 00:58:21 +0100

    Add feature test for routine operations

--- spec/features/routine_operations_spec.rb
+++ spec/features/routine_operations_spec.rb
@@ -1,23 +1,47 @@
 feature 'Routine operations' do
+  before { @airport = make_airport }
+  before { @planes = make_planes(@airport.capacity) }
+  before { @landings, @takeoffs, @weather_rejects = 0, 0, 0 }
+
   scenario 'Lands and takes-off a number of planes' do
-    @airport = create_airport
-    @planes = create_planes
-
-    rand(100).times do
-      plane = @planes[rand(@planes.count - 1)]
-
+    100.times do
+      plane = random_plane
+      expect { give_order(plane, @airport) }.not_to raise_error
     end
-    # randomly choose a plane
-    # give it an order
-    # rinse & repeat
+    puts "Made #{@landings} landings and #{@takeoffs} take-offs. All good."
+    puts "Received #{@weather_rejects} weather rejects."
   end
 
   private
-  def create_airport
+
+  def make_airport
     Airport.new
   end
 
-  def create_planes
-    Array.new(20, Plane.new)
+  def make_planes(n = 20)
+    Array.new(n, Plane.new)
+  end
+
+  def random_plane
+    @planes[rand(@planes.count - 1)]
+  end
+
+  def give_order(plane, airport)
+    begin
+      plane.landed_at?(airport) ? test_takeoff(plane, airport) : test_land(plane, airport)
+    rescue StandardError => e
+      raise e.message unless e.message.include?("weather conditions")
+      @weather_rejects += 1
+    end
+  end
+
+  def test_takeoff(plane, airport)
+    plane.takeoff_from(airport)
+    @takeoffs += 1
+  end
+
+  def test_land(plane, airport)
+    plane.land_at(airport)
+    @landings += 1
   end
 end
