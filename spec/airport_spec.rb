b876e13c611f4035f08a2f41be79727e222ec517
Ryan Davidson <daviryan@aol.com>
2017-07-09 18:39:02 +0100

    added weather contexts to airport_spec

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -5,23 +5,43 @@ describe Airport do
   let(:airport) { Airport.new }
   let(:plane) { Plane.new }
 
-  it 'should instruct a plane to land and add to planes in airport' do
-    allow(airport).to receive(:stormy?).and_return(false)
-    airport.land(plane)
-    expect(airport.plane_count).to eq 1
+  context 'weather is good' do
+    before do
+      allow(airport).to receive(:stormy?).and_return(false)
+    end
+
+    it 'should instruct a plane to land and add to planes in airport' do
+      airport.land(plane)
+      expect(airport.plane_count).to eq 1
+    end
+
+    it 'should instruct a plane to land and add to planes in airport' do
+      airport.land(plane)
+      expect(airport.plane_count).to eq 1
+    end
+
+    it 'should instruct a plane to take off and remove from planes in airport' do
+      allow(airport).to receive(:stormy?).and_return(false)
+      airport.land(plane)
+      expect(airport.plane_count).to eq 1
+      airport.take_off(plane)
+      expect(airport.plane_count).to eq 0
+    end
+
+    it 'should prevent land(plane) if airport full' do
+      Airport::DEFAULT_CAPACITY.times { airport.land(Plane.new) }
+      expect { airport.land(plane) } .to raise_error 'Airport is full'
+    end
+
+  end
+
+context 'stormy weather' do
+  before do
+    allow(airport).to receive(:stormy?).and_return(true)
   end
 
   it 'should raise error and prevent plane landing if stormy weather' do
-    allow(airport).to receive(:stormy?).and_return(true)
     expect { airport.land(plane) }.to raise_error "All flights cancelled due to stormy weather"
-  end
-
-  it 'should instruct a plane to take off and remove from planes in airport' do
-    allow(airport).to receive(:stormy?).and_return(false)
-    airport.land(plane)
-    expect(airport.plane_count).to eq 1
-    airport.take_off(plane)
-    expect(airport.plane_count).to eq 0
   end
 
   it 'should raise error and prevent take_off(plane) if stormy weather' do
@@ -30,6 +50,7 @@ describe Airport do
     allow(airport).to receive(:stormy?).and_return(true)
     expect { airport.take_off(plane) }.to raise_error "All flights cancelled due to stormy weather"
   end
+end
 
   it 'should have a default capacity that can be overridden' do
     expect(airport.capacity).to eq Airport::DEFAULT_CAPACITY
@@ -37,12 +58,6 @@ describe Airport do
     expect(airport.capacity).to eq 25
     airport = Airport.new(30)
     expect(airport.capacity).to eq 30
-  end
-
-  it 'should prevent land(plane) if airport full' do
-    allow(airport).to receive(:stormy?).and_return(false)
-    Airport::DEFAULT_CAPACITY.times { airport.land(Plane.new) }
-    expect { airport.land(plane) } .to raise_error 'Airport is full'
   end
 
 end
