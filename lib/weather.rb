436fed20467a685db9c3ed66ff59fd6420a741fb
chuk-chuk <yulia.v.litvin@gmail.com>
2017-07-09 13:45:28 +0100

    add random weather selector and write unit test

--- lib/weather.rb
+++ lib/weather.rb
@@ -1,4 +1,5 @@
 class Weather
   def stormy?
+    rand(101) > 50
   end
 end
--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -3,4 +3,10 @@ require 'weather'
 describe Weather do
   subject(:weather) {described_class.new}
   it { is_expected.to respond_to(:stormy?) }
+
+  it "should return true or false" do
+    today_weather = weather.stormy?
+    expect([true, false].include? today_weather).to eq true
+  end
+
 end
