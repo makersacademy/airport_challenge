1ba7aa493e40fd59873d189e11131468f39c5aac
Akshatha Ballal <akshatha.ballal@gmail.com>
2017-07-09 13:54:46 +0100

    Completed feature test 'prevent landing when weather is stormy' in feature4_spec.rb

--- feature_spec/feature4_spec.rb
+++ feature_spec/feature4_spec.rb
@@ -1,3 +1,22 @@
 # As an air traffic controller
 # To ensure safety
 # I want to prevent landing when weather is stormy
+
+require_relative '../lib/plane.rb'
+require_relative '../lib/airport.rb'
+require_relative '../lib/weather.rb'
+
+plane = Plane.new
+
+2.times do |i|
+  weather = Weather.new(i.zero? ? :stormy : :sunny)
+  airport = Airport.new("London Heathrow", weather)
+
+  puts plane.status
+  begin
+    plane.land(airport)
+    puts plane.status
+  rescue => exception
+    puts exception.inspect
+  end
+end
