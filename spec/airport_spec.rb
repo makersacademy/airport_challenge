114f5d140aad48fa1d476cfabadada4695224a10
Sapphire Mason-Brown <saphmb@gmail.com>
2017-07-10 00:53:09 +0100

    Clarify methods being tested

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -6,45 +6,55 @@ describe Airport do
   plane1 = Plane.new
   plane2 = Plane.new
 
-  it 'allows a plane to land' do
-    expect { airport.land double(:bike).to eq :bike }
+  describe '#land' do
+    it 'allows a plane to land' do
+      expect { airport.land double(:bike).to eq :bike }
+    end
   end
 
-  it 'shows planes in airport' do
-    airport.land(plane1)
-    airport.land(plane2)
-    expect(airport.planes).to eq [plane1, plane2]
+  describe '#planes' do
+    it 'shows planes in airport' do
+      airport.land(plane1)
+      airport.land(plane2)
+      expect(airport.planes).to eq [plane1, plane2]
+    end
   end
 
-  it 'checks if a plane has landed' do
-    airport.land(plane1)
-    expect(airport.in_airport?(plane1)).to be_truthy
-    expect(airport.in_airport?(plane2)).to be_falsy
+  describe '#in_airport' do
+    it 'checks if a plane has landed' do
+      airport.land(plane1)
+      expect(airport.in_airport?(plane1)).to be_truthy
+      expect(airport.in_airport?(plane2)).to be_falsy
+    end
   end
 
-  it 'shows plane is no longer in airport after takeoff' do
-    airport.planes = [plane1]
-    airport.takeoff(plane1)
-    expect(airport.in_airport?(plane1)).to be_falsy
+  describe '#takeoff' do
+    it 'shows plane is no longer in airport after takeoff' do
+      airport.planes = [plane1]
+      airport.takeoff(plane1)
+      expect(airport.in_airport?(plane1)).to be_falsy
+    end
+
+    it "cannot takeoff if it is stormy" do
+      airport.planes = [plane1]
+      airport.weather = "stormy"
+      expect { airport.takeoff(plane1) }.to raise_error "cannot takeoff due to weather"
+      expect(airport.in_airport?(plane1)).to be_truthy
+    end
+
+    it "can takeoff if it is sunny" do
+      airport.planes = [plane1]
+      airport.weather = "sunny"
+      airport.takeoff(plane1)
+      expect(airport.in_airport?(plane1)).to be_falsy
+    end
   end
 
-  it "cannot takeoff if it is stormy" do
-    airport.planes = [plane1]
-    airport.weather = "stormy"
-    expect { airport.takeoff(plane1) }.to raise_error "cannot takeoff due to weather"
-    expect(airport.in_airport?(plane1)).to be_truthy
-  end
-
-  it "can takeoff if it is sunny" do
-    airport.planes = [plane1]
-    airport.weather = "sunny"
-    airport.takeoff(plane1)
-    expect(airport.in_airport?(plane1)).to be_falsy
-  end
-
-  it 'checks whether an airport is full' do
-    airport.capacity.times { airport.land(Plane.new) }
-    expect(airport.full?).to eq true
+  describe '#full?' do
+    it 'checks whether an airport is full' do
+      airport.capacity.times { airport.land(Plane.new) }
+      expect(airport.full?).to eq true
+    end
   end
 
 end
