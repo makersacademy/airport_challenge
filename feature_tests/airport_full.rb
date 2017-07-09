8c81c012c793898f25715c9114ece37a8acd086d
annalaise <annalaise.gibbons@gmail.com>
2017-07-09 15:04:14 +0100

    feature test for full airport: landing denial

--- feature_tests/airport_full.rb
+++ feature_tests/airport_full.rb
@@ -1,3 +1,13 @@
+require '../lib/airport.rb'
+require '../lib/plane.rb'
+require '../lib/weather.rb'
+
+# USER STORY
 # As an air traffic controller
 # To ensure safety
-# I want to prevent landing when the airport is full 
+# I want to prevent landing when the airport is full
+
+airport = Airport.new
+
+(Airport::DEFAULT_CAPACITY).times {airport.land(Plane.new)} # fills airport up to default capacity with planes.
+airport.confirm_land(Plane.new) # Trying to land a further plane will now return an error message saying the airport is full. 
