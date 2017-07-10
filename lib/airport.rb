7598efc9470e1c2aabcc54b2b8b3bf67b570f95f
Costas Kiteou <ckiteou22@gmail.com>
2017-07-10 01:03:55 +0100

    Unit test passed. Added guard and warning message to prevent landing when airport is at capacity

--- lib/airport.rb
+++ lib/airport.rb
@@ -2,14 +2,19 @@ require_relative 'plane'
 require_relative 'weather'
 
 class Airport
+  DEFAULT_CAPACITY = 50
   attr_reader :planes
+  attr_reader :capacity
+
   def initialize(weather: Weather.new)
+    @capacity = DEFAULT_CAPACITY
     @planes = []
     @weather = weather
   end
 
   def land(plane)
-    fail "Unable to land due to weather" if weather.stormy?
+    fail "Cannot land since airport is full" if full?
+    #fail "Unable to land due to weather" if weather.stormy?
     @planes << plane
   end
 
@@ -20,4 +25,8 @@ class Airport
 
   private
   attr_reader :weather
+
+  def full?
+    planes.count >= capacity
+  end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -5,29 +5,34 @@ describe Airport do
   let(:plane) { double :plane }
   let(:weather) { double :weather }
 
-
   describe 'landing planes' do
     it { is_expected.to respond_to(:land).with(1).argument }
+
+    it "defaults capacity" do
+      described_class::DEFAULT_CAPACITY.times { subject.land(plane) }
+      expect{ subject.land(plane) }.to raise_error "Cannot land since airport is full"
+    end
 
     it "raises an error when the weather is stormy and prevents landing" do
       allow(weather).to receive(:stormy?).and_return(true)
       message = "Unable to land due to weather"
       expect { subject.land(plane) }.to raise_error message
     end
+  end
+
+    it "does not accept a plane if over the airport capacity" do
+      subject.land(plane)
+      message = "Cannot land since airport is full"
+      expect { subject.land(double :plane) }.to raise_error message
+    end
 
     it 'has the plane after it has landed' do
       subject.land(plane)
       expect(subject.planes).to include plane
     end
-  end
 
   describe 'planes taking off' do
     it { is_expected.to respond_to(:take_off) }
-
-    it 'raises an error when the weather is stormy and prevents takeoff' do
-      message = "Unable to take off due to weather"
-      expect { subject.take_off }.to raise_error message
-    end
 
     it 'does not have the plane after it has taken off' do
       subject.take_off
