9dcb2b254ff9e7bf954f5822a43a9e2418f76ac0
oleglukyanov <oleg.s.lukyanov@gmail.com>
2017-07-10 21:07:18 +0100

    Do minor refactoring

--- spec/features/routine_operations_spec.rb
+++ spec/features/routine_operations_spec.rb
@@ -23,7 +23,7 @@ feature 'Routine operations' do
   end
 
   def given_a_random_plane
-    @plane = @planes[rand(@planes.count)]
+    @plane = @planes.sample
   end
 
   def when_received_an_order_then_should_execute_successfully
