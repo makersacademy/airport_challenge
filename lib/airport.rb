358b971ae5363bb23f2284db820b7c6fa6d39b7f
Nick Otter <nicholas.a.otter@gmail.com>
2017-08-05 22:45:22 +0100

    User story 2, passed (96.00% coverage)

--- lib/airport.rb
+++ lib/airport.rb
@@ -8,6 +8,16 @@ class Airport
   def land(plane)
     @planes << plane
   end
+
+  def take_off(plane)
+    @planes.delete(plane)
+    return if @planes.empty? raise 'No planes present'
+  end
+
+  private
+  def empty?
+    @planes.size.zero?
+  end
 end
 
 class Plane
