e93a43c4766900a122a0b0bcb9d07d231993e5dc
Kay Lovelace <kay@herostrat.us>
2017-08-05 15:41:21 +0100

    Anon 5

---
+++ lib/plane.rb
@@ -0,0 +1,14 @@
+
+require_relative 'airport.rb'
+
+class Plane
+
+  attr_accessor :landed
+
+  def initialize
+    @landed = false
+  end
+
+
+
+end
---
+++ spec/plane_spec.rb
@@ -0,0 +1,9 @@
+require 'plane.rb'
+
+describe Plane do
+
+  it {is_expected.to respond_to(:landed)}
+  it {is_expected.to respond_to(:landed=)}
+
+
+end
