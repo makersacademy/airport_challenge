77331c89f4a7dc6b34dfaf02c2bedfa8c0cef35a
Nick Otter <nicholas.a.otter@gmail.com>
2017-08-05 22:44:12 +0100

    User story 2, unit & feature tests

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,7 +1,18 @@
-require 'airport.rb'
+require './lib/airport.rb'
 
 describe Airport do
   it "land a plane" do
-    expect(subject).to respond_to(:land).with(Plane.new).argument
+    expect(subject.land(:plane)).to eq subject.instance_variable_get(:@planes)
+  end
+  it "plane takes off" do
+    plane = Plane.new
+    expect(subject.take_off(plane)).to eq subject.instance_variable_get(:@planes.empty?)
+  end
+  it "plane takes off and no more planes left" do
+    plane = Plane.new
+    expect(subject.take_off(plane)).to eq subject.send(:empty? == true)
+  end
+  it "plane is no longer in the airport" do
+    expect { subject.take_off(:plane) }.to raise_error('No planes present')
   end
 end
--- spec/feature_test.rb
+++ spec/feature_test.rb
@@ -1,4 +1,4 @@
-require './lib/airport.rb'
+require '../lib/airport.rb'
 
 airport = Airport.new
 plane = Plane.new
@@ -9,4 +9,12 @@ p "#{plane}"
 
 puts "plane landing"
 p "#{airport.land(plane)}"
-puts "#{airport}"
+puts "#{airport.instance_variable_get(:@planes)}"
+
+puts "plane take-off"
+p "#{airport.take_off(plane)}"
+p "#{airport.instance_variable_get(:@planes)}"
+
+puts "confirm that plane is no longer in the airport"
+p "#{airport.take_off(plane)}"
+p "#{airport.instance_variable_get(:@planes)}"
