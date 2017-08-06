940cdcb8eb531b4a8920feee416b7f7c711acb0b
Nick Otter <nicholas.a.otter@gmail.com>
2017-08-06 01:44:30 +0100

    User story 3, failed by 1 (95.00% coverage)

--- lib/airport.rb
+++ lib/airport.rb
@@ -3,6 +3,7 @@ class Airport
 
   def initialize
     @planes = []
+    @climate = Weather.new.instance_variable_get(:@climate)
   end
 
   def land(plane)
@@ -10,13 +11,25 @@ class Airport
   end
 
   def take_off(plane)
-    @planes.delete(plane)
-    return if @planes.empty? raise 'No planes present'
+    if @planes.empty?
+      raise 'No planes present'
+    elsif @climate > 3
+      raise 'Take-off prevented, inclement weather'
+    else
+      @planes.pop && "#{plane} has taken off"
+    end
   end
 
   private
   def empty?
     @planes.size.zero?
+  end
+end
+
+class Weather
+
+  def initialize
+    @climate = rand(0..5)
   end
 end
 
