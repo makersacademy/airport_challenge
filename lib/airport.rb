9241742c9d1d87ce3440217113b58598bfb365fd
Will Porter <willjsporter@hotmail.co.uk>
2017-08-06 23:47:06 +0100

    post testing cycle for status alterations between planes (Sunday night week1

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,8 +1,18 @@
+module Weather
+
+  def current_weather(determiner = rand)
+    determiner > 0.8 ? "bad" : "good"
+  end
+
+end
+
 class Airport
+
+  include Weather
 
   DEFAULT_CAPACITY = 100
   def initialize(code, planes, capacity = DEFAULT_CAPACITY)
-    @code = code
+    @code = code.intern
     @capacity = capacity
     @planes = planes
     @plane_list = Array.new(capacity)
@@ -11,25 +21,27 @@ class Airport
 
   attr_reader :code, :capacity, :planes, :plane_list
 
-  def call_to_land(plane)
-    plane.location == "flying" ? (puts "Time to land") : (raise "Plane is grounded")
-    #want to add break clause if bad weather
-    #also want plane to register this call, possibly by registering airport code in action_status
+  def call_to_land(plane, weather = current_weather)
+    raise "Plane is grounded" if plane.location != "flying"
+    raise "Bad weather - landing not safe" unless weather == "good"
+    plane.clear_for_action("land", @code)
+    puts "Time to land"
   end
 
-  def call_to_take_off(plane)
-    plane.airport == code ? (puts "Time to take-off") : (raise "Plane is not at this airport")
-    #want to add break clause if bad weather
-    #also want plane to register this call if sent
+  def call_takeoff(plane, weather = current_weather)
+    raise "Plane is not at this airport" if plane.airport != code
+    raise "Bad weather - takeoff not safe" unless weather == "good"
+    plane.clear_for_action("takeoff")
+    puts "Time to take-off"
   end
   #
-  # def dock_plane
+  # def dock_plane(plane)
   #   #to be called from plane if it lands
   #   #will add to plane_list
   # end
   #
-  # def release_plane
-  #   #to be called from plane if it takes off
+  # def release_plane(plane)
+  #   #to be called once plane's clear for action is true
   #   #will remove plane from plane list
   # end
 
@@ -43,20 +55,34 @@ class Plane
     @action_status = nil
   end
   attr_accessor :airport, :location
+  attr_reader :action_status
 
-  # def clear_for_action
-  #   #allow action status to be true which enables landing
-  # end
-  #
-  # def land
-  #   #brings plane down safely if clear for action, changes location to grounded and switches back action status
-  # end
-  #
-  # def take_off
-  #   #plane takes off if clear for action, changes location to flying and switches back action status to false
-  # end
+  def clear_for_action(command, location = nil)
+    if command == "land" && location != nil
+      @action_status = location
+    elsif command == "takeoff"
+      @action_status = command
+    else raise "Not a valid clearance command"
+    end
+    # allow action status to be "takeoff" or landing plus airport_id which enables landing
+  end
+
+  def land
+    raise "Not clear for landing" unless @action_status != nil && @action_status.is_a?(Symbol)
+    @airport = @action_status; @location = "grounded"; @action_status = nil
+    # brings plane down safely if clear for action, changes location to grounded,
+    # airport to airport and switches back action status
+  end
+
+  def take_off
+    raise "Not clear for takeoff" unless @action_status == "takeoff"
+    @airport = nil; @location = "flying"; @action_status = nil
+    # plane takes off if clear for action, changes location to flying
+    # and switches back action status to false
+  end
 
 end
+
 #
 # asdf=Airport.new(1)
 # p asdf.singleton_class
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,7 +2,6 @@ require 'airport'
 
 describe Airport do
   airport = Airport.new(:LHR, 50)
-  p airport.plane_list[50..99]
   let(:grounded_plane) { double :grounded_plane }
   let(:flying_plane) { double :flying_plane }
 
@@ -22,30 +21,46 @@ describe Airport do
     expect(airport.plane_list[50..99].all? { |plane| plane.nil? }).to eq true
   end
 
-  it "calls a plane to land" do
+  it "calls a plane to land if weather is fine" do
     allow(flying_plane).to receive(:location).and_return("flying")
-    expect { airport.call_to_land(flying_plane) }.to output("Time to land\n").to_stdout
+    allow(flying_plane).to receive(:clear_for_action)
+    expect { airport.call_to_land(flying_plane, "good") }.to output("Time to land\n").to_stdout
   end
 
-  it "calls a plane to take-off" do
+  it "calls a plane to take-off if weather is fine" do
     allow(grounded_plane).to receive(:airport).and_return(:LHR)
-    expect { airport.call_to_take_off(grounded_plane) }.to output("Time to take-off\n").to_stdout
+    allow(grounded_plane).to receive(:clear_for_action)
+    expect { airport.call_takeoff(grounded_plane, "good") }.to output("Time to take-off\n").to_stdout
   end
 
   it "raises an error if call_to_land called on a plane on the ground" do
-    allow(flying_plane).to receive(:location).and_return("grounded")
-    expect { airport.call_to_land(flying_plane) }.to raise_exception "Plane is grounded"
+    allow(grounded_plane).to receive(:location).and_return("grounded")
+    expect { airport.call_to_land(grounded_plane) }.to raise_exception "Plane is grounded"
   end
 
-  it "raises an error if call_to_take_off called and plane not at airport" do
-    allow(grounded_plane).to receive(:airport).and_return(nil)
-    expect { airport.call_to_take_off(grounded_plane) }.to raise_exception "Plane is not at this airport"
+  it "raises an error if call_takeoff called and plane not at airport" do
+    allow(flying_plane).to receive(:airport).and_return(nil)
+    expect { airport.call_takeoff(flying_plane) }.to raise_exception "Plane is not at this airport"
   end
 
+  it "call_to_land raises an exception if it is bad weather" do
+    allow(flying_plane).to receive(:location).and_return("flying")
+    error_message = "Bad weather - landing not safe"
+    expect { airport.call_to_land(flying_plane, "bad") }.to raise_exception error_message.to_s
+  end
+
+  it "call_takeoff raises an exception if it is bad weather" do
+    allow(grounded_plane).to receive(:airport).and_return(:LHR)
+    error_message1 = "Bad weather - takeoff not safe"
+    expect { airport.call_takeoff(grounded_plane, "bad") }.to raise_exception error_message1.to_s
+  end
+
+#   it "if plane lands then it sends a signal "
 end
 
 describe Plane do
   plane = Plane.new
+  airport1 = Airport.new(:LHR, 50)
 
   it "has default value of airport = nil" do
     expect(plane.airport).to eq nil
@@ -53,6 +68,47 @@ describe Plane do
 
   it "has default value of status = grounded" do
     expect(plane.location). to eq "grounded"
+  end
+
+  it "will only allow call to land to respond to 'takeoff' or 'landing' with airport_id" do
+    expect { plane.clear_for_action("blah") }.to raise_exception "Not a valid clearance command"
+  end
+
+  plane1 = Plane.new
+  plane1.location = "flying"
+
+  it "if weather ok plane registers call to land" do
+    airport1.call_to_land(plane1, "good")
+    expect(plane1.action_status).to eq :LHR
+  end
+
+  it "if weather ok plane registers call to take off" do
+    airport1.call_to_land(plane1, "good")
+    plane1.land
+    airport1.call_takeoff(plane1, "good")
+    expect(plane1.action_status).to eq "takeoff"
+  end
+
+  it "registered as grounded at airport once landed and action_status is reset" do
+    plane.clear_for_action("land", :LHR)
+    plane.land
+    a_s = plane.action_status
+    expect(plane.airport == :LHR && plane.location == "grounded" && a_s.nil?).to eq true
+  end
+
+  it "after take off sets airport to nil, location to 'flying' and action_status resets" do
+    plane.clear_for_action("takeoff")
+    plane.take_off
+    a_s = plane.action_status
+    expect(plane.airport.nil? && plane.location == "flying" && a_s.nil?).to eq true
+  end
+
+end
+
+describe Weather do
+
+  it "can be set to a specific value (default random) and will return good if under 0.8" do
+    expect(current_weather(0.1)).to eq "good"
   end
 
 end
