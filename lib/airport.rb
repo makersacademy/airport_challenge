c89b9c06fce93495a99d90a1aa1bfa287bf3dbc0
David Divilly <d.divilly@gmail.com>
2017-08-06 14:35:28 +0100

    adds to airport class

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,13 +1,30 @@
-
+require_relative 'plane.rb'
+require_relative 'weather.rb'
 
 class Airport
-  DEFAULT_CAPACTIY = 20
+  DEFAULT_CAPACTIY = 2
   attr_reader :onground, :inair, :capacity
 
   def initialize(capacity = DEFAULT_CAPACTIY)
-    @ongound = []
+    @onground = []
     @inair = []
     @capacity = capacity
+  end
+
+  def land(plane, sunny = true)
+    raise "it's too stormy to land!" if sunny != true
+    raise "sorry, the airport is full" if full?
+    @onground << plane
+  end
+
+  def take_off(sunny = true)
+    raise "it's too stormy to take off!" if sunny != true
+    raise "there are no planes to take off!" if @onground.count.zero?
+    @inair << @onground.pop
+  end
+
+  def full?
+    @onground.length >= @capacity
   end
 
 end
