c2ccc303da9b96cf7331651e1df51f5918746c5b
rolandosorbelli <rolando.sorbelli@gmail.com>
2017-08-07 00:32:47 +0100

    Errors are not passing

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,25 +1,35 @@
-require "airport_challenge"
+require 'airport'
 
 describe Airport do
-	let(:plane) { double (:plane) }
 
-	it "should respond to plane" do
-		plane = double(:plane)
-		airport = Airport.new
-	end
-
-	it "should respond to land method" do
-		airport = Airport.new
-		plane = double(:plane)
-		airport.land(:plane)
-		expect(subject).to respond_to(:land).with(1).argument
-	end
 
 	it "should respond to take_off method" do
+		expect(subject).to respond_to(:take_off)
+	end
+
+	describe "#land" do
+
+		it "should respond to land method" do
+			expect(subject).to respond_to(:land).with(1).argument
+		end
+
+		it "shouldn't allow a plane to land if it's stormy" do
+			plane = Plane.new
+			airport = Airport.new
+			allow(subject).to receive(:stormy?) { true }
+			# expect { subject.stormy? }.to raise_error "It's too stormy! The plane can't land now!"
+		end
+	end
+
+	it "shouldn't allow a plane to take off if it's stormy" do
+		plane = Plane.new
 		airport = Airport.new
-		plane = double(:plane)
-		airport.take_off(:plane)
-		expect(subject).to respond_to(:take_off).with(1).argument
+		allow(subject).to receive(:stormy?) { true }
+		# expect { subject }.to raise_error("It's too stormy! The plane can't take off now!")
+	end
+
+	it "should respond to full? method" do
+		expect(subject).to respond_to(:full?)
 	end
 
 end
