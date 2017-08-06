7f3ebfdd781adb5e3b1a9fd77cacbea3e3a4ea14
pyan83 <puyanwei@gmail.com>
2017-08-06 12:19:17 +0100

    Added all tests apart from edge cases

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,45 +1,57 @@
 require './docs/airport'
 
 describe Airport do
-  context "Initialize"
+  let(:airport) { Airport.new }
 
+  context "Initialize"
   it "Capacity of airport should be 20 by default if no arguments passed in" do
-    expect(subject.capacity).to eq(20)
+    expect(airport.capacity).to eq(20)
   end
 
   it "Capacity of airport should be able to be customized" do
-    airport = Airport.new(10)
+    airport.capacity = 10
     expect(airport.capacity).to eq(10)
   end
 
   it "Planes array should be an empty array by default" do
-    expect(subject.planes).to eql([])
+    expect(airport.planes).to eql([])
   end
 
   context "Plane landing"
-
   it "Plane should be able to land and be added to @planes array" do
-    expect(subject.landing).to be_instance_of(Array)
+    expect(airport.landing).to be_instance_of(Array)
   end
 
   it "Plane should not be able to land because airport is full" do
-    airport = Airport.new(4)
+    airport.capacity = 4
     4.times { airport.landing }
     expect { airport.landing }.to raise_error "Airport is full!"
   end
 
+  #let(:weather) {double(:weather)}
+
   context "Plane taking off"
   it "Plane should be able to take off" do
-    expect(subject.landing).to be_instance_of(Array)
+    allow(airport).to receive_messages(:weather => "Clear")
+    expect(airport.landing).to be_instance_of(Array)
   end
-
-  let (:Weather) { double :Weather }
 
   it "Plane should not be able to take off due to stormy weather" do
-    Weather.status = "Stormy"
-    expect { subject.take_off }.to raise_error "Plane cannot take off due to stormy weather"
+    allow(airport).to receive_messages(:weather => "Stormy")
+    expect { airport.take_off }.to raise_error "Plane cannot take off due to stormy weather"
   end
 
-  it "Plane should be taken off the @planes array once it takes off" do
+  it "A plane should be taken off the @planes array once it takes off" do
+    allow(airport).to receive_messages(:weather => "Clear")
+    2.times { airport.landing }
+    airport.take_off
+    expect(airport.planes.size).to eq(1)
   end
+
+  context "Edge cases"
+it "A plane can only take off from an airport they are in"
+it "A plane cannot be in an airport if they are flying"
+it "A plane cannot take off if they are flying"
+it "A plane cannot land again if they have landed already"
+
 end
