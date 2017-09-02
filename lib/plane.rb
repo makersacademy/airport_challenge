c729a82b25ec5e4a1dc46fe22d53ca00892c1c11
James Hughes <hugjimbo77@gmail.com>
2017-09-02 23:17:49 +0100

    plane methods and plane_spec tests

--- lib/plane.rb
+++ lib/plane.rb
@@ -1,1 +1,18 @@
+class Plane
+  def intitialize
+    @flying = true
+  end
 
+  def flying?
+    @flying = true
+  end
+
+  def take_off?
+    @flying = true
+  end
+
+  def landed?
+    @flying = false
+  end
+
+end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -1,1 +1,17 @@
+require 'plane'
 
+describe Plane do
+  subject(:plane) { described_class.new }
+
+  it "should have flying status when flying" do
+    expect(subject).to be_flying
+  end
+
+  it "should have flying status when plane has taken off" do
+    expect(subject).to be_flying
+  end
+
+  it "should not have flying status when landed" do
+    expect(subject).not_to be_landed
+  end
+end
