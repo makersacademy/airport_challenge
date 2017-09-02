54bb3fffabc9c1f248fde644c76c950c5e27a675
Rory Hodgson <29726065+rorybot@users.noreply.github.com>
2017-09-02 11:11:25 +0100

    runway of airport can contain a plane

---
+++ lib/airport.rb
@@ -0,0 +1,17 @@
+require_relative 'plane'
+
+class Airport
+  # def runway
+  #   runway = []
+  # end
+  #
+  # def land
+  #   runway << plane
+  # end
+
+
+    def runway
+    @runway = ["plane"]
+    end
+    
+end
---
+++ spec/airport_spec.rb
@@ -0,0 +1,14 @@
+
+require 'airport.rb'
+require 'spec_helper'
+
+describe Airport do
+  subject(:airport) {described_class.new}
+  let(:plane) {double :plane}
+  context 'airport runway/terminals' do
+    it 'should land a plane in a runway' do
+      expect(subject.runway).to include "plane"
+    end
+  end
+
+end
