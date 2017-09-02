1f44100455686c5e04d76a544c2e011e28c32925
Joe Ingram <josephcingram@hotmail.co.uk>
2017-09-02 18:25:07 +0100

    method and working test for plane status

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -1,5 +1,11 @@
 require 'plane'
 
-class Plane
+describe Plane do
 
+  describe '#status' do
+    it 'confirms that plane has taken off safely' do
+      plane = Plane.new
+    expect(plane.status).to eq "plane taken off safely"
+    end
+  end
 end
