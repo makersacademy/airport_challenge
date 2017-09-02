80c1b9e044fc9a583131529591f181a63c5fe2f7
Rory Hodgson <29726065+rorybot@users.noreply.github.com>
2017-09-02 21:54:49 +0100

    more to do

--- lib/runway.rb
+++ lib/runway.rb
@@ -1,11 +1,26 @@
+require_relative 'airport'
 
 module Runway
 
-  @@runway = []
+  def new_runway
+    @runway_traffic = []
+  end
 
-  def stormy?
-    poss_weather_conditions = [true, false]
-    @@stormy = poss_weather_conditions.sample
+  def runway_traffic
+    @runway_traffic
   end
 
 end
+
+module Weather
+
+    def stormy?
+      poss_weather_conditions = [true, false]
+      @@stormy = poss_weather_conditions.sample
+    end
+
+end
+
+
+# attr_reader :AIRPORT_CAPACITY
+# @AIRPORT_CAPACITY = 1
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -3,13 +3,15 @@ require 'airport.rb'
 require 'spec_helper'
 require 'plane.rb'
 require 'runway'
+
 describe Airport do
 
   subject(:airport) { described_class.new }
   let(:plane) { double :plane }
   context 'runway/terminal basic operation' do
 
-    it 'allow a plane should be able to #land on a runway' do
+    it 'allow a plane to #land on a runway' do
+      allow(airport).to receive(:stormy?).and_return false
       subject.land plane
       expect(subject.runway_traffic).to include plane
 
@@ -24,18 +26,51 @@ describe Airport do
   end
 
   context 'stormy weather for landing' do
-    it 'disallow a plane to #take_off if it is stormy' do
+    it 'DISallow a plane to #take_off if it IS stormy' do
       allow(airport).to receive(:stormy?).and_return true
       storm_warning = "Storm! Do not take off!"
+      subject.runway_traffic << plane
       expect { subject.take_off(plane) }.to raise_error storm_warning
     end
 
-    it 'disallow a plane to #take_off if it is stormy' do
+    #Consider refactoring the above for more elegant way to allow runway to contain a plane before takeoff
+
+    it 'allow a plane to #take_off if it is NOT stormy' do
       allow(airport).to receive(:stormy?).and_return false
       storm_warning = "Storm! Do not take off!"
       expect { subject.take_off(plane) }.not_to raise_error storm_warning
     end
 
+    it 'DISallow a plane to #land if it IS stormy' do
+      allow(airport).to receive(:stormy?).and_return true
+      storm_warning = "Storm! Do not land here!"
+      expect { subject.land(plane) }.to raise_error storm_warning
+    end
+
+    it 'allow a plane to #land if it is NOT stormy' do
+      allow(airport).to receive(:stormy?).and_return false
+      storm_warning = "Storm! Do not land here!"
+      expect { subject.take_off(plane) }.not_to raise_error storm_warning
+    end
+
   end
+
+
+  context 'airport runway approach/depart' do
+
+    it 'a plane should respond that it is on the ground when on the runway' do
+      subject.runway_traffic << plane
+      # Places subject plane in runway
+      expect(subject.report_status plane).to eq "I'm on the ground!"
+    end
+
+    it 'a plane should respond that it is in the air when not on the runway' do
+
+      # Tests an empty runway
+      expect(subject.report_status plane).to eq "I'm in the air!"
+    end
+
+  end
+
 
 end
