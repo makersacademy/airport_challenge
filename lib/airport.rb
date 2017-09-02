67c5dc2d1621db70cc9adf7b24f473065f64cbdb
Rory Hodgson <29726065+rorybot@users.noreply.github.com>
2017-09-02 11:13:40 +0100

    Rubocop cleanup

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,17 +1,9 @@
 require_relative 'plane'
 
 class Airport
-  # def runway
-  #   runway = []
-  # end
-  #
-  # def land
-  #   runway << plane
-  # end
 
-
-    def runway
+  def runway
     @runway = ["plane"]
-    end
-    
+  end
+
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -3,12 +3,16 @@ require 'airport.rb'
 require 'spec_helper'
 
 describe Airport do
-  subject(:airport) {described_class.new}
-  let(:plane) {double :plane}
+
+  subject(:airport) { described_class.new }
+  let(:plane) { double :plane }
+
   context 'airport runway/terminals' do
+
     it 'should land a plane in a runway' do
       expect(subject.runway).to include "plane"
     end
+
   end
 
 end
