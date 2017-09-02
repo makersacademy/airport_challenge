3d7474984057777140c095c644a1292a4ad0e210
Zoe Hopkins <zoe.hopkins@gmail.com>
2017-09-02 16:13:46 +0100

    Create unit test for landing planes in airport_spec.rb and land method in airport.rb

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,5 +1,7 @@
 require_relative 'plane'
 
 class Airport
-
+  def land(plane)
+    
+  end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,5 +1,9 @@
 require 'airport'
 
 describe Airport do
+  subject(:airport) { described_class.new }
 
+  describe 'landing planes' do
+    it { is_expected.to respond_to(:land).with(1).argument }
+  end
 end
