8b5c3496401e6ab4082d4ca41d7d0006a40679db
robincollins <racollins1@me.com>
2017-09-02 16:03:34 +0100

    empty plane_take_off method

---
+++ lib/plane.rb
@@ -0,0 +1,13 @@
+class Plane
+
+
+  def plane_land
+
+  end
+
+  def plane_take_off
+
+  end
+
+
+end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -1,12 +1,16 @@
-require 'airport'
+require 'plane'
 
-describe Airport do
+describe Plane do
   # p described_class
   # subject(:airport) { described_class.new}
 
   it 'responds to #plane_land' do
 
     expect(subject).to respond_to(:plane_land)
+  end
+
+  it 'responds to #plane_take_off' do
+    expect(subject).to respond_to(:plane_take_off)
   end
 
 
