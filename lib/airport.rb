3ed0a8746bcd072e44c357b0aac5ce2b0b5c342e
mengchenwang <mengchenwang@hotmail.com>
2017-09-02 23:35:47 +0100

    updated land and takeoff spec with mock

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,14 +1,19 @@
 require_relative 'airplane'
 
 class Airport
+  def initialize
+    @airplanes = []
+  end
+
   attr_reader :airplanes
 
   def land(airplane)
     airplane.landed=(true)
-    @airplanes = airplane
+    @airplanes << airplane
   end
 
   def takeoff(airplane)
+    fail 'No airplanes in the airport' if @airplanes.empty?
     airplane.landed=(false)
     airplane
   end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,26 +1,41 @@
 # spec/airport_spec.rb
 
 require 'airport'
-require 'airplane'
 
 RSpec.describe Airport do
   subject(:airport) { described_class.new }
+  let(:airplane) { double :airplane }
 
   describe '#land' do
-    it 'lands a plane in the airport' do
-      airplane = Airplane.new
-      airport.land(airplane)
-      expect(airplane.landed).to be true
-      expect(airport.airplanes).to eq(airplane)
+    before do
+      allow(airplane).to receive(:land)
+      allow(airplane).to receive(:landed=)
     end
 
-  describe '#takeoff' do
-    it 'instruct a plane to take off' do
-      airplane = Airplane.new
-      airport.takeoff(airplane)
-      expect(airplane.landed).to be false
+    it 'lands a plane in the airport' do
+      subject.land airplane
+    end
+
+    it 'has the plane after it has landed' do
+      subject.land airplane
+      expect(airport.airplanes).to include airplane
     end
   end
-  
-end
+
+  describe '#takeoff' do
+    before do
+      allow(airplane).to receive(:takeoff)
+      allow(airplane).to receive(:landed=)
+    end
+
+    it 'instruct a plane to take off' do
+      subject.land airplane
+      subject.takeoff airplane
+    end
+
+    it 'raise an error when there are no planes in the airport' do
+      expect { subject.takeoff(airplane) }.to raise_error 'No airplanes in the airport'
+    end
+  end
+
 end
