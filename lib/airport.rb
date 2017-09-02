79d7bac1e9f6ca5cb115c3d46db15e76af4ccacd
mengchenwang <mengchenwang@hotmail.com>
2017-09-02 23:53:37 +0100

    add airport capacity spec and variable

--- lib/airport.rb
+++ lib/airport.rb
@@ -3,11 +3,17 @@ require_relative 'airplane'
 class Airport
   def initialize
     @airplanes = []
+    @capacity = 1
   end
 
   attr_reader :airplanes
+  attr_writer :airplanes
+
+  attr_reader :capacity
+  attr_writer :capacity
 
   def land(airplane)
+    fail 'Airport is full' if @airplanes.count == @capacity
     airplane.landed=(true)
     @airplanes << airplane
   end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -13,12 +13,17 @@ RSpec.describe Airport do
     end
 
     it 'lands a plane in the airport' do
-      subject.land airplane
+      subject.land(airplane)
     end
 
     it 'has the plane after it has landed' do
-      subject.land airplane
+      subject.land(airplane)
       expect(airport.airplanes).to include airplane
+    end
+
+    it 'raise an error when the airport is full' do
+      subject.land(airplane)
+      expect { subject.land(airplane) }.to raise_error 'Airport is full'
     end
   end
 
@@ -29,8 +34,8 @@ RSpec.describe Airport do
     end
 
     it 'instruct a plane to take off' do
-      subject.land airplane
-      subject.takeoff airplane
+      subject.land(airplane)
+      subject.takeoff(airplane)
     end
 
     it 'raise an error when there are no planes in the airport' do
