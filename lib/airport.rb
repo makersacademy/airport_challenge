3f1993843692802f8a23f400bb3cae4a6a37a825
Joe Ingram <josephcingram@hotmail.co.uk>
2017-09-03 18:58:38 +0100

    dont land a plane when it is stormy, with comments

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,9 +1,23 @@
 class Airport
+  attr_reader :conditions
+
+  def initialize(weatherConditions)
+    @conditions = weatherConditions
+  end
+
+  # def conditions
+  #   @conditions
+  # end
+
   def land(plane)
       plane
   end
 
   def take_off(plane)
+    if @conditions.stormy
+      fail "Cannot take off due to stormy weather"
+    else
       plane
+    end
   end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,25 +1,44 @@
 require 'Airport'
+require 'Conditions'
 
 describe Airport do
 
   describe '#land' do
     it 'instructs a plane to land' do
+      # Arrange
       plane = Plane.new
-    expect(subject.land(plane)).to eq(plane)
+      conditions = Conditions.new
+      subject = Airport.new(conditions)
+      # Act
+      # Assert
+      expect(subject.land(plane)).to eq(plane)
     end
   end
 
-  describe '#take_off' do
+  describe'#take_off' do
     it 'instructs a plane to take off' do
+      # Arrange
       plane = Plane.new
-      subject.take_off(plane)
+      conditions = Conditions.new
+      subject = Airport.new(conditions)
+      allow(conditions).to receive(:stormy).and_return false
+
+      # Action incorporated in assertion below
+      # Assertion
       expect(subject.take_off(plane)).to eq(plane)
     end
   end
-end
-#A) look up how respond_to matchers work
-#B) think about your return value. What do you want subject.land(plane) to return?
-
-
-#[4:18]
-#It might be a good idea as much as possible for the moment use the to eq matchers
+    it 'fails if weather is stormy' do
+      # create a new variable equal to a new conditions class (stormy by default)
+      conditions = Conditions.new
+      # make the stormy method in the above variable evaluate to true
+      allow(conditions).to receive(:stormy).and_return true
+      # new plane please
+      plane = Plane.new
+      # set subject to equal a new airport with our defined conditions
+      subject = Airport.new(conditions)
+      # expect that new airport to raise error and return string
+      # because you have set conditions to stormy
+      expect{subject.take_off(plane)}.to raise_error "Cannot take off due to stormy weather"
+    end
+  end
