88da71db372dd8ea6444c02aa0ec06ea7bbb2627
Zoe Hopkins <zoe.hopkins@gmail.com>
2017-09-03 11:23:03 +0100

    Updates tests and code in plane.rb that controls the status update after the plane lands

--- lib/plane.rb
+++ lib/plane.rb
@@ -1,7 +1,10 @@
 class Plane
 
+  attr_reader :on_ground
+
   def land(airport)
     @landed = true
+    @on_ground = true
   end
 
 end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -3,6 +3,9 @@ require 'plane'
 describe Plane do
   subject(:plane) { described_class.new }
 
+  it { is_expected.to respond_to(:land).with(1).argument}
+  it { is_expected.to respond_to(:on_ground) }
+
   describe '#land' do
     it 'can land at airport' do
       expect(plane.land(Airport.new))
