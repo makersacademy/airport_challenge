66f7d87e472c65de6e5339cedb31c58ac89931fa
keith(mac) <ka33145@googlemail.com>
2017-09-03 21:54:22 +0100

    Fixed typo in airport spec & class

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,6 +1,6 @@
 require 'airport'
 describe Airport do
   it 'has method land and takes on argument' do
-    is_expected.to respond_to(:land)
+    is_expected.to respond_to(:land).with(1).argument
   end
 end
