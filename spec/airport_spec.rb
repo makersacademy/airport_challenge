695dedd5f071801360cc3532eaa0ba96c0f134d6
keith(mac) <ka33145@googlemail.com>
2017-09-03 22:08:43 +0100

    Added test to check if airport class has a  method takeoff

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -3,4 +3,7 @@ describe Airport do
   it 'has method land and takes on argument' do
     is_expected.to respond_to(:land).with(1).argument
   end
+  it 'has method takeoff' do
+    is_expected.to respond_to(:takeoff)
+  end
 end
