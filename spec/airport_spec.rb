a9fb369ed6cbc487450db940b58e03a79ba123d3
Rory Hodgson <29726065+rorybot@users.noreply.github.com>
2017-09-03 12:38:16 +0100

    Refactored rspec tests to conform to best practice regarding not_to raise_error, so that it doesn't run afoul of false positives

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -8,6 +8,13 @@ describe Airport do
 
   subject(:airport) { described_class.new }
   let(:plane) { double :plane }
+
+  context 'runway/terminal initialisation' do
+    it 'gives a fixed capacity to how many planes can sit on the runway' do
+
+    end
+  end
+
   context 'runway/terminal basic operation' do
 
     it 'allow a plane to #land on a runway' do
@@ -26,6 +33,7 @@ describe Airport do
   end
 
   context 'stormy weather for landing' do
+
     it 'DISallow a plane to #take_off if it IS stormy' do
       allow(airport).to receive(:stormy?).and_return true
       storm_warning = "Storm! Do not take off!"
@@ -37,8 +45,7 @@ describe Airport do
 
     it 'allow a plane to #take_off if it is NOT stormy' do
       allow(airport).to receive(:stormy?).and_return false
-      storm_warning = "Storm! Do not take off!"
-      expect { subject.take_off(plane) }.not_to raise_error storm_warning
+      expect { subject.take_off(plane) }.not_to raise_error
     end
 
     it 'DISallow a plane to #land if it IS stormy' do
@@ -49,8 +56,7 @@ describe Airport do
 
     it 'allow a plane to #land if it is NOT stormy' do
       allow(airport).to receive(:stormy?).and_return false
-      storm_warning = "Storm! Do not land here!"
-      expect { subject.take_off(plane) }.not_to raise_error storm_warning
+      expect { subject.take_off(plane) }.not_to raise_error
     end
 
   end
@@ -64,7 +70,6 @@ describe Airport do
     end
 
     it 'a plane should respond that it is in the air when not on the runway' do
-
       # Tests an empty runway
       expect(subject.report_status plane).to eq "I'm in the air!"
     end
