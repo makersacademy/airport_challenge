d3dbe7161219a99ccd952692ebff6586614a05a0
David Halewood <davidnhalewood@hotmail.com>
2017-09-29 23:01:57 +0100

    adding argument to land_plane method

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,5 +1,7 @@
 require 'airport'
 
 describe Airport do
-  it { is_expected.to respond_to :land_plane }
+  it "shoule be able to land a plane" do
+  expect(subject).to respond_to(:land_plane).with(1).argument
+  end
 end
