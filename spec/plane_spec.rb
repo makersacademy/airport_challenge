bb41cb569dbdd19273fd17a3d0883a9e8af0c128
Ainsley <ab.chang@me.com>
2017-09-29 21:56:03 +0100

    add land_at method

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -2,4 +2,13 @@ require 'plane'
 
 describe Plane do
 
+  describe '#land_at' do
+
+    it 'responds to land_at method' do
+      expect(Plane.new).to respond_to :land_at
+    end
+
+  end
+    
+
 end
