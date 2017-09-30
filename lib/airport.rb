1589fb75129358177c2bf950325b40dd4027aa51
Ainsley <ab.chang@me.com>
2017-09-30 19:51:21 +0100

    initialize weather instance along with airport

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,7 +1,19 @@
+require_relative './weather'
+
 class Airport
+
+  attr_reader :weather
+
+  def initialize
+    @weather = Weather.new
+  end
 
   def clear?
 
   end
-  
+
+  # def weather
+  #   Weather.new
+  # end
+
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,9 +1,25 @@
 require 'airport'
+require 'weather'
 
 describe Airport do
 
   it 'clears plane for take off' do
     expect(Airport.new).to respond_to :clear?
   end
+  it 'creates a new weather instance' do
+    expect(Airport.new.weather).to be_instance_of Weather
+  end
+  it 'returns the weather' do
+    airport = Airport.new
+    allow(airport.weather).to receive(:rand).with(0..9) {9}
+    airport.weather.randomize
+    expect(airport.weather.stormy).to be_truthy
+  end
 
+  # it 'checks the weather' do
+  #   weather = Weather.new
+  #   allow(weather).to receive(:rand).with(0..9) {9}
+  #   weather.randomize
+  #   expect(Airport.new.clear?).to be_truthy
+  
 end
