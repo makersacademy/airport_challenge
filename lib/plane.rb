610af57ca751443270bf96246f2a391cd80e0fba
Christine <splogy2001@hotmail.com>
2017-09-30 13:37:22 +0100

    Plane class and method created

---
+++ lib/plane.rb
@@ -0,0 +1,9 @@
+
+
+class Plane
+
+  def plane
+  end
+
+  
+end
---
+++ spec/plane_spec.rb
@@ -0,0 +1,8 @@
+require 'plane'
+
+describe Plane do
+  it 'responds the plane method' do
+    expect(subject).to respond_to :plane
+  end
+
+end
