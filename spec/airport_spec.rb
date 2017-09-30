43d5088595c42d2d01de9ea5508d9a5d33e8ab43
Lucy Borthwick <lucyborthwick95@gmail.com>
2017-09-30 11:58:50 +0100

    adding a green rspec and a method to rb to allow a plane to take off from the airport and no longer be seen there

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,13 +1,17 @@
 require 'airport'
-require 'aeroplane'
+
+class FakeAeroplane
+
+end
 
 describe Airport do
 
   let(:subject)   {Airport.new}
-  # let(:aeroplane) {Aeroplane.new}
-  
+
   it 'allows a plane to land in the airport' do
-    plane = Aeroplane.new
+    plane = FakeAeroplane.new
     expect(subject.land(plane)).to eq plane
   end
+
+  it { is_expected.to respond_to(:take_off).with(1).argument }
 end
