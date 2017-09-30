83569a73ded7454516baa4b20a12728994ee0405
mormolis <george.lamprakis@gmail.com>
2017-09-30 10:08:23 +0100

    tests and implements 2nd user's story Plane<--take_off-->false | Plane<--at_the_airport?-->true/false

--- lib/plane.rb
+++ lib/plane.rb
@@ -1,5 +1,17 @@
 class Plane
+  def initialize
+    @at_the_airport = true
+  end
+
   def land_at(airport)
     airport
+  end
+  
+  def take_off
+    @at_the_airport = false
+  end
+
+  def at_the_airport?
+    @at_the_airport
   end
 end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -6,6 +6,10 @@ describe Plane do
   it "should be able to land at an airport" do
     expect(subject.land_at(airport)).to eq airport # Airport.new
   end
+  
+  it "should confirm that is no longer at the airport after take off" do
+    subject.take_off
+    expect(subject.at_the_airport?).to eq false
+  end
 
 end
-
