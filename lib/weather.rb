58d771d68747fa228d6c73eb523156997ae40f78
David Halewood <davidnhalewood@hotmail.com>
2017-09-30 16:20:55 +0100

    adding stormy? method to weather class

--- lib/weather.rb
+++ lib/weather.rb
@@ -1,2 +1,6 @@
 class Weather
+  
+  def stormy?
+  end
+
 end
--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -1,5 +1,9 @@
 require 'weather'
 
 describe Weather do
-
+  describe '#stormy?' do
+    it 'generates mostly sunny weather with occasional storms' do
+      expect(subject).to respond_to(:stormy?)
+    end
+  end
 end
