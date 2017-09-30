6f0c3b5989fcc1fa81cb07f47cbb807515fc8369
Ainsley <ab.chang@me.com>
2017-09-30 12:16:30 +0100

    further tests & refactoring for weather.rb

--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -4,18 +4,31 @@ describe Weather do
 
   subject(:weather) {Weather.new}
 
-  it 'responds to stormy?' do
-    expect(weather).to respond_to :stormy?
-  end
-  it 'returns true for stormy weather' do
-    # use a stub to fake a result from :rand
-    allow(weather).to receive(:rand) {9}
-    expect(weather).to be_stormy
-  end
-  it 'returns false for sunny weather' do
-    # use a stub to fake a result from :rand
-    allow(weather).to receive(:rand) {1}
-    expect(weather).to_not be_stormy
+  describe '#stormy?' do
+
+    context 'Stormy Weather' do   
+      it 'returns true for stormy weather' do
+        # use a stub to fake a result from :rand
+        allow(weather).to receive(:rand) {9}
+        expect(weather).to be_stormy
+      end
+    end
+    context 'Sunny weather' do
+      it 'returns false for sunny weather' do
+        # use a stub to fake a result from :rand
+        allow(weather).to receive(:rand) {1}
+        expect(weather).to_not be_stormy
+        allow(weather).to receive(:rand) {3}
+        expect(weather).to_not be_stormy
+        allow(weather).to receive(:rand) {6}
+        expect(weather).to_not be_stormy
+        allow(weather).to receive(:rand) {8}
+        expect(weather).to_not be_stormy
+        allow(weather).to receive(:rand) {0}
+        expect(weather).to_not be_stormy
+      end
+    end
+
   end
 
 end
