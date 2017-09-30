f8975f0a2b6d8c7e1c6d96f1b2731dacac889054
Jon Sanders <jonsanders101@aol.com>
2017-09-30 14:25:37 +0100

    Airport stores planes

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,2 +1,8 @@
 class Airport
+
+attr_reader :planes
+
+  def initialize(planes = [])
+    @planes = planes
+  end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,4 +1,7 @@
 require 'airport'
 
 describe Airport do
+  it "stores landed planes" do
+    expect(subject.planes).to eq([])
+  end
 end
