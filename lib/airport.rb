79a544d69c274982df3bf9d79410a26d71d5b1e6
Chris <cdunham1989@gmail.com>
2017-09-30 13:14:53 +0100

    unit tests checking airport tracks and stores planes as they take off and land

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,15 +1,20 @@
 require './lib/plane'
 
 class Airport
+  attr_reader :planes
+
+  def initialize
+    @planes = []
+  end
 
   def land(plane)
+    @planes << plane
     plane.landed = true
-    plane
   end
 
   def takeoff(plane)
+    planes.drop(1)
     plane.landed = false
-    plane
   end
 
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,21 +2,37 @@ require 'airport'
 
 describe Airport do
   subject(:airport) { Airport.new }
+  let(:planes) { double :planes }
 
-  describe '#landing' do
-    it 'lands a plane at the airport' do
-      plane = Plane.new
-      airport.land(plane)
-      expect(plane.landed).to be true
+  context 'Landing' do
+    describe '#landing' do
+      it 'lands a plane at the airport' do
+        plane = Plane.new
+        airport.land(plane)
+        expect(plane.landed).to be true
+      end
+
+      it 'contains the plane once landed' do
+        plane = Plane.new
+        airport.land(plane)
+        expect(airport.planes).to include plane
+      end
     end
   end
 
-  describe '#taking off' do
-    it 'lets a plane takeoff from the airport' do
-      plane = Plane.new
-      airport.takeoff(plane)
-      expect(plane.landed).to be false
+  context 'Takeoff' do
+    describe '#taking off' do
+      it 'lets a plane takeoff from the airport' do
+        plane = Plane.new
+        airport.takeoff(plane)
+        expect(plane.landed).to be false
+      end
+
+      it 'removes plane after takeoff' do
+        plane = Plane.new
+        airport.takeoff(plane)
+        expect(airport.planes).not_to include plane
+      end
     end
   end
-
 end
