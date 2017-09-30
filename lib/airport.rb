2fbed84bff4a357c67ec77823cb21f056cd9b28a
Christine <splogy2001@hotmail.com>
2017-09-30 15:43:40 +0100

    With instance variables and attr reader

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,14 +1,14 @@
 require_relative 'plane'
 
 class Airport
-# attr_reader :plane
+ attr_reader :plane
 
   # def initailize(plane)
     # @plane = plane
   # end
 
   def land(plane)
-    plane
+    @plane = plane
   end
 
 
