ef8c90afd914738a4efd10af1fa5937c509fe8b7
RMCollins175 <rorycollins@me.com>
2017-09-30 12:50:21 +0100

    new unit tests and production code for returning a landed plane

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,6 +1,7 @@
 class Airport
 
   def land(plane)
+    plane 
   end
 
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -4,4 +4,10 @@ describe Airport do
   describe "landing plane tests" do
     it { is_expected.to respond_to(:land).with(1).argument }
   end
+
+  it "returns the landed plane" do
+    plane = Plane.new
+    expect(subject.land(plane)).to eq plane
+  end
+
 end
