0401fbe49e51ea39ce086d556ee14f12a2137697
Ainsley <ab.chang@me.com>
2017-09-30 12:48:52 +0100

    fix tests & code for weather.rb (stub does not verify rand argument)

--- lib/weather.rb
+++ lib/weather.rb
@@ -1,7 +1,7 @@
 class Weather
 
   def stormy?
-    rand(9) == 9 ? true : false
+    rand(0..9) == 9 ? true : false
   end
 
 end
--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -9,22 +9,22 @@ describe Weather do
     context 'Stormy Weather' do   
       it 'returns true for stormy weather' do
         # use a stub to fake a result from :rand
-        allow(weather).to receive(:rand) {9}
+        allow(weather).to receive(:rand).with(0..9) {9}
         expect(weather).to be_stormy
       end
     end
     context 'Sunny weather' do
       it 'returns false for sunny weather' do
         # use a stub to fake a result from :rand
-        allow(weather).to receive(:rand) {1}
+        allow(weather).to receive(:rand).with(0..9) {1}
         expect(weather).to_not be_stormy
-        allow(weather).to receive(:rand) {3}
+        allow(weather).to receive(:rand).with(0..9) {3}
         expect(weather).to_not be_stormy
-        allow(weather).to receive(:rand) {6}
+        allow(weather).to receive(:rand).with(0..9) {6}
         expect(weather).to_not be_stormy
-        allow(weather).to receive(:rand) {8}
+        allow(weather).to receive(:rand).with(0..9) {8}
         expect(weather).to_not be_stormy
-        allow(weather).to receive(:rand) {0}
+        allow(weather).to receive(:rand).with(0..9) {0}
         expect(weather).to_not be_stormy
       end
     end
