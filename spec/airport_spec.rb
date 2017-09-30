6b4970be423cba55a0e4b70bd155f06007ce6a31
lucasasalmins <lucasasalmins@gmail.com>
2017-09-30 12:26:25 +0100

    added argument to land method

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,6 +2,8 @@ require 'airport.rb'
 
 describe Airport do
 
-  it { is_expected.to respond_to :land }
+  it { is_expected.to respond_to(:land).with(1).argument }
+
+  it { is_expected.to respond_to :take_off }
 
 end
