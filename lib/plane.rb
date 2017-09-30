35e33916275d4bbc700c7de317f658ce1c6bcced
Pablo <pabloandresvdl@gmail.com>
2017-09-30 17:14:17 +0100

    add take_off test and code, add in_the_air variable

--- lib/plane.rb
+++ lib/plane.rb
@@ -1,7 +1,18 @@
 require './lib/airport.rb'
-
 class Plane
+  def initialize
+    @in_the_air = false
+  end
+
   def land(airport)
     true if airport.class == Airport
+  end
+
+  def take_off(airport)
+    @in_the_air = true if airport.class == Airport
+  end
+  
+  def in_the_air?
+    @in_the_air
   end
 end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -10,4 +10,12 @@ describe Plane do
       expect(subject.land(airport)).to eq true
     end
   end
+  describe "#take_off" do
+    it "expects the plane to take off and set the in_the_air variable to true" do
+      airport = Airport.new
+      plane = Plane.new
+      plane.take_off(airport)
+      expect(plane.in_the_air?).to eq true
+    end
+  end
 end
