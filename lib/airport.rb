24325e3ef9eb7ba185af65ad8695437813c310a0
Tim Jones <timjones10@hotmail.com>
2017-09-30 11:19:57 +0100

    Takeoff method added

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,3 +1,6 @@
 class Airport
 
+def takeoff
+end
+
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,4 +2,8 @@ require "Airport"
 
 describe Airport do
 
+  it "responds to takeoff" do
+  expect(subject).to respond_to :takeoff
+  end
+
 end
