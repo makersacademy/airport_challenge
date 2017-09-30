681b9b67747e18e732ae98eb6cb20749ad7af047
Christine <splogy2001@hotmail.com>
2017-09-30 16:05:26 +0100

    Tells the user if all the planes have gone

--- lib/airport.rb
+++ lib/airport.rb
@@ -3,15 +3,16 @@ require_relative 'plane'
 class Airport
  attr_reader :plane
 
-  # def initailize(plane)
-    # @plane = plane
-  # end
+#  def initailize(plane)
+    #  @plane = []
+  #  end
 
   def land(plane)
     @plane = plane
   end
 
   def take_off
+    raise 'All planes have gone'
   end
 
 
--- spec/airpot_spec.rb
+++ spec/airpot_spec.rb
@@ -14,5 +14,9 @@ describe Airport do
       expect(subject).to respond_to :take_off
     end
 
+    it 'raises an error when you to run take_off method when no planes present' do
+      expect {subject.take_off}.to raise_error 'All planes have gone'
+    end
+
 
 end
