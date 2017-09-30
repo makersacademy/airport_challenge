6c30adb4db5f6f61805942a445f01237c9866508
Chris <cdunham1989@gmail.com>
2017-09-30 14:59:33 +0100

    Weather class created

---
+++ lib/weather.rb
@@ -0,0 +1,7 @@
+class Weather
+
+  def forecast
+
+  end
+  
+end
---
+++ spec/weather_spec.rb
@@ -0,0 +1,12 @@
+require 'weather'
+
+describe Weather do
+  subject(:weather) { Weather.new }
+
+  describe 'forecast' do
+    it 'generates a weather forecast' do
+      
+    end
+  end
+
+end
