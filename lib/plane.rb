febf3c1cf712b8ab245d8f87f2bc8e72e8b9fa34
Ed Lowther <ed.lowther[at]gmail.com>
2017-09-30 12:28:38 +0100

    Plane reports whether airborne, and does/does not land based on whether it is already in the air or not

--- lib/plane.rb
+++ lib/plane.rb
@@ -1,13 +1,23 @@
 class Plane
-  attr_accessor :destination
+  attr_accessor :destination, :airborne
+
+  def initialize
+    @airborne = false
+  end
+
   def fly_to airport
+    raise 'Already at airport' if airport.check_for_plane(self)
+    raise 'Not in the air' if !@airborne
     @destination = airport
+    @airborne = false
     airport.land self
   end
 
   def take_off airport
     raise 'Not parked at airport' if !airport.check_for_plane(self)
+    raise 'Already in the air' if @airborne
     @destination = nil
+    @airborne = true
     airport.leave self
   end
 end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -1,13 +1,36 @@
 require 'plane'
 
 describe Plane do
-  let(:airport) { double(:airport, :land => true, :leave => true) }
   let(:airport_with_plane) { double(:airport, :leave => true, :check_for_plane => true) }
-  let(:airport_without_plane) { double(:airport, :check_for_plane => false) }
+  let(:airport_without_plane) { double(:airport, :land => true, :check_for_plane => false) }
+
+  it 'can is not airborne at first' do
+    expect(subject.airborne).to eq false
+  end
+
+  it 'reports that it is not airborne after being instructed to land at an airport' do
+    subject.take_off airport_with_plane
+    subject.fly_to airport_without_plane
+    expect(subject.airborne).to eq false
+  end
+
+  it 'reports that it is airborne after being told to take off' do
+    subject.take_off airport_with_plane
+    expect(subject.airborne).to eq true
+  end
+
+  it 'can is not airborne at first' do
+    expect(subject.airborne).to eq false
+  end
 
   it 'can be told to fly to an airport' do
-    subject.fly_to airport
-    expect(subject.destination).to eq airport
+    subject.take_off airport_with_plane
+    subject.fly_to airport_without_plane
+    expect(subject.destination).to eq airport_without_plane
+  end
+
+  it 'won\'t fly to an airport that it is already at' do
+    expect{subject.fly_to airport_with_plane}.to raise_error 'Already at airport'
   end
 
   it 'can be told to take off from an airport' do
@@ -17,6 +40,15 @@ describe Plane do
 
   it 'won\'t take off from an airport that it is not parked at' do
     expect{subject.take_off airport_without_plane}.to raise_error 'Not parked at airport'
+  end
+
+  it 'can\'t be told to take off if it is already airborne' do
+    subject.take_off airport_with_plane
+    expect{subject.take_off airport_with_plane}.to raise_error 'Already in the air'
+  end
+
+  it 'can\'t be told to land if it is not airborne' do
+    expect{subject.fly_to airport_without_plane}.to raise_error 'Not in the air'
   end
 
 end
