70ddabec568de88b18940b426656960fe77dc5e2
Jon Sanders <jonsanders101@aol.com>
2017-09-30 15:34:21 +0100

    Refactored tests, removed redundancy

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,12 +1,11 @@
 class Airport
 
-attr_reader :planes
+  attr_reader :planes
 
+  def initialize
+    @planes = []
+  end  
   def land(plane)
     @planes << plane
-  end
-
-  def initialize(planes = [])
-    @planes = planes
   end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,13 +1,9 @@
 require 'airport'
 
 describe Airport do
-  it "stores landed planes" do
-    expect(subject.planes).to eq([])
-  end
   it "lands planes" do
-    expect(subject).to respond_to(:land).with(1).arguments
-    plane = double(:plane)
-    subject.land(plane)
-    expect(subject.planes[0]).to eq(plane)
-  end
+      plane = double
+      subject.land(plane)
+      expect(subject.planes[0]).to eq(plane)
+    end
 end
