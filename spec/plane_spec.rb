9f06a2557c9707e2b12bbbad8393c2094f0406d9
oliverpople <oliverpople@gmail.com>
2017-09-30 19:32:56 +0100

    Plane class now confirms whether there is stormy_weather

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -2,15 +2,18 @@ require 'plane'
 
 describe Plane do
 
-it 'expects the plane to arrive at airport' do
-  expect(subject.arrive_at_airport).to eq true
-end
+  it 'expects the plane to arrive at airport' do
+    expect(subject.arrive_at_airport).to eq true
+  end
 
- #it { is_expected.to respond_to :depart_from_airport}
- it 'expects the plane to depart from airport' do
+  it 'expects the plane to depart from airport' do
    expect(subject.depart_from_airport).to eq true
- end
+  end
 
-it { is_expected.to respond_to :stormy_weather?}
+  it 'expects plane to confirm whether there is stormy weather' do
+    expect(subject.stormy_weather?).to eq true
+  end
+
+  it { is_expected.to respond_to :stormy_weather?}
 
 end
