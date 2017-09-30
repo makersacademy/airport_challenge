db3590dfd9ddc17997fb776d8406a46d547365e3
hugosykes <hugo@denmeadfarm.co.uk>
2017-09-30 14:35:14 +0100

    Added land method to the plane class

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -1,3 +1,12 @@
+require 'plane'
 describe Plane do
-  
+
+  describe "#landing" do
+
+    it "should respond to the land method" do
+      expect(subject).to respond_to :land
+    end
+
+  end
+
 end
