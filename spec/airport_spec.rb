28fc3420d311526e8e6d5be71057840590209e89
lucasasalmins <lucasasalmins@gmail.com>
2017-09-30 12:38:32 +0100

    added test for returning plane when landing

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -4,6 +4,11 @@ describe Airport do
 
   it { is_expected.to respond_to(:land).with(1).argument }
 
+  it 'lands a plane' do
+    plane = Plane.new
+    expect(subject.land(plane)).to eq plane
+  end
+
   it { is_expected.to respond_to :take_off }
 
 end
