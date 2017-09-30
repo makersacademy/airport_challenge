8b3b15c997c73aac6fd748572c6f782c9df39c36
Ainsley <ab.chang@me.com>
2017-09-30 10:52:04 +0100

    further refactoring of tests

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -4,6 +4,7 @@ require 'airport'
 describe Plane do
 
   subject(:plane) {Plane.new}
+  subject(:airport) {Airport.new}
 
   describe '#land_at' do
 
@@ -19,7 +20,6 @@ describe Plane do
       expect(plane).to respond_to(:take_off_from).with(1).arguments
     end
     it 'plane has left the airport' do
-      airport = Airport.new
       plane.land_at(airport)
       plane.take_off_from(airport)
       expect(plane).to_not be_at(airport)
