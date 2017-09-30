35c2438e23b620654b36ea745c4be0faa26443c4
hugosykes <hugo@denmeadfarm.co.uk>
2017-09-30 16:10:46 +0100

    Added functionality to the aiport class. A method full? which just returns true at the moment. Two other methods, accept_planes and plane_take_off, which will do as their names suggest when necessary.

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,3 +1,14 @@
 class Airport
-  
+
+  def full?
+    true
+  end
+
+  def accept_plane
+    raise("Airport full -> can't accept planes!") if full?
+  end
+
+  def plane_take_off
+  end
+
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,4 +1,29 @@
 require 'airport'
 describe Airport do
 
+  describe "#dealing with full airport" do
+
+    it "should respond to full? method" do
+      expect(subject).to respond_to :full?
+    end
+
+  end
+
+  describe "#take off and landing of planes" do
+
+    it "should respond to accept_plane method" do
+      expect(subject).to respond_to :accept_plane
+    end
+
+    it "should respond to plane_take_off method" do
+      expect(subject).to respond_to :plane_take_off
+    end
+
+    it "if airport is full, not allow planes to land" do
+      # set full? to return true
+      expect { subject.accept_plane }.to raise_error("Airport full -> can't accept planes!")
+    end
+
+  end
+
 end
