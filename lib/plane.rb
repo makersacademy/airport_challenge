7fef12e9041bc851bbc6cc7e0828e7dcd4308763
Aram Simonian <aram.contacts@gmail.com>
2017-09-30 16:41:33 +0100

    plane can now takeoff

--- lib/plane.rb
+++ lib/plane.rb
@@ -2,4 +2,8 @@ class Plane
   def land(airport)
     true
   end
+
+  def takeoff
+    true
+  end
 end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -8,9 +8,15 @@ describe Plane do
   subject(:plane) { Plane.new }
   describe "#land" do
     it 'lands a plane at an airport' do
-      expect(plane.land(FakeAirport.new)).not_to be_nil
+      expect(plane.land(FakeAirport.new)).to be true
     end
   end
 
+  describe "#takeoff" do
+    it 'tells a plane to takeoff from an airport' do
+      expect(plane.takeoff).to be true
+    end
+
+  end
 
 end
