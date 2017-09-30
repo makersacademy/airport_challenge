ad2cfc518d45fd754188035ec6d047881086c3c0
Chris <cdunham1989@gmail.com>
2017-09-30 14:40:18 +0100

    raises error if landing when full or takeoff when empty

--- lib/airport.rb
+++ lib/airport.rb
@@ -9,12 +9,14 @@ class Airport
   end
 
   def land(plane)
+    fail 'Airport full!' if @planes.count == @capacity
     @planes << plane
     plane.land
     plane
   end
 
   def takeoff(plane)
+    fail 'Airport empty!' if @planes.empty?
     planes.delete(plane)
     plane.takeoff
     plane
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -16,6 +16,12 @@ describe Airport do
         airport.land(plane)
         expect(airport.planes).to include plane
       end
+
+      it 'raises error if landing when airport is full' do
+        plane = Plane.new
+        airport.land(plane)
+        expect { airport.land(plane) }.to raise_error 'Airport full!'
+      end
     end
   end
 
@@ -23,14 +29,21 @@ describe Airport do
     describe '#taking off' do
       it 'lets a plane takeoff from the airport' do
         plane = Plane.new
+        airport.land(plane)
         airport.takeoff(plane)
         expect(plane.landed).to be false
       end
 
       it 'removes plane after takeoff' do
         plane = Plane.new
+        airport.land(plane)
         airport.takeoff(plane)
         expect(airport.planes).not_to include plane
+      end
+
+      it 'raises error if taking off when empty' do
+        plane = Plane.new
+        expect { airport.takeoff(plane) }.to raise_error 'Airport empty!'
       end
     end
   end
