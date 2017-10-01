9a42660880b044673dbb767eb4b74b0028ac4fdb
Ainsley <ab.chang@me.com>
2017-10-01 16:48:17 +0100

    removed redundant methods and tests for plane class

--- lib/plane.rb
+++ lib/plane.rb
@@ -1,32 +1,13 @@
 class Plane
 
-  attr_reader :plane
+  attr_reader :landed
 
-  def initialize
-    @landed
-    @plane = self
-  end
-
-  def land_at(airport)
-    raise 'plane has already landed' if @landed
-    raise 'cannot land at full airport' if airport.full?
-    raise 'cannot land in stormy weather' unless airport.clear?
-    airport.planes << @plane
+  def land
     @landed = true
-    @plane
   end
 
-  def take_off_from(airport)
-    raise 'plane is already in the air' unless @landed
-    raise 'plane is not at this airport' unless at?(airport)
-    raise 'cannot take off in stormy weather' unless airport.clear?
-    airport.planes.delete(@plane)
+  def take_off
     @landed = false
-    @plane
-  end
-
-  def at?(airport)
-    airport.planes.include?(@plane)
   end
 
 end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -1,72 +1,23 @@
 require 'plane'
 
-class Airport
-  
-  attr_accessor :planes
-  
-  def initialize
-    @planes = []
-  end
-  
-end
-
 describe Plane do
 
   subject(:plane) {Plane.new}
-  subject(:airport) {Airport.new}
 
-  before do 
-    # stub airport class methods
-    allow(airport).to receive(:full?) {false}
-    allow(airport).to receive(:clear?) {true}
-  end
+  describe '#land' do
 
-  describe '#land_at' do
-
-    it 'lands at the airport' do
-      expect(plane.land_at(airport)).to eq plane
-    end
-    it 'raises error if weather is stormy' do
-      allow(airport).to receive(:clear?) {false}
-      expect {plane.land_at(airport)}.to raise_error 'cannot land in stormy weather'
-    end
-    it 'raises error if trying to land at a full airport' do
-      allow(airport).to receive(:full?) {true}
-      expect {plane.land_at(airport)}.to raise_error 'cannot land at full airport'
-    end 
-    it 'raises error if already landed' do
-      plane.land_at(airport)
-      expect {plane.land_at(airport)}.to raise_error 'plane has already landed'
+    it 'lands at an airport' do
+      plane.land
+      expect(plane.landed).to be_truthy
     end
 
   end
 
-  describe '#take_off_from' do
+  describe '#take_off' do
 
-    # ensure a plane has landed before taking off in each test
-    before(:each) do
-      plane.land_at(airport)
-    end
-
-    it 'confirms a plane has left the airport' do
-      expect(plane.take_off_from(airport)).to_not be_at(airport)
-      #expect(plane).to_not eq plane
-    end
-    it 'allows a plane to land again once taken off' do
-      plane.take_off_from(airport)
-      plane.land_at(airport)
-      expect(airport.planes[0]).to eq plane
-    end
-    it 'raises error if weather is stormy' do
-      allow(airport).to receive(:clear?) {false}
-      expect {plane.take_off_from(airport)}.to raise_error 'cannot take off in stormy weather'
-    end
-    it 'raises error if already in the air and attempting to take off' do
-      plane.take_off_from(airport)
-      expect {plane.take_off_from(airport)}.to raise_error 'plane is already in the air'
-    end
-    it 'raises error if in a different airport' do
-      expect {plane.take_off_from(Airport.new)}.to raise_error 'plane is not at this airport'
+    it 'takes off from an airport' do
+      plane.take_off
+      expect(plane.landed).to be_falsy
     end
 
   end
