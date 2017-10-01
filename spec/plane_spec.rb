c7aa5c9637140c25965720be0a770b51db9bc8f3
Ainsley <ab.chang@me.com>
2017-10-01 14:37:24 +0100

    refactoring plane_spec to remove repetition

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -8,20 +8,22 @@ describe Plane do
 
   describe '#land_at' do
 
+    before do
+      allow(airport).to receive(:clear?) {true}
+    end
+
     it 'lands at the airport' do
       expect(plane).to respond_to(:land_at).with(1).arguments
     end
     it 'raises error if weather is stormy' do
-      allow(airport.weather).to receive(:rand).with(0..9) {9}
+      allow(airport).to receive(:clear?) {false}
       expect {plane.land_at(airport)}.to raise_error 'cannot land in stormy weather'
     end
     it 'raises error if trying to land at a full airport' do
-      allow(airport.weather).to receive(:rand).with(0..9) {0}
       airport.capacity.times { Plane.new.land_at(airport) }
       expect {plane.land_at(airport)}.to raise_error 'cannot land at full airport'
     end 
     it 'raises error if already landed' do
-      allow(airport.weather).to receive(:rand).with(0..9) {0}
       plane.land_at(airport)
       expect {plane.land_at(airport)}.to raise_error 'plane has already landed'
     end
@@ -30,31 +32,30 @@ describe Plane do
 
   describe '#take_off_from' do
 
+    before do
+      allow(airport).to receive(:clear?) {true}
+    end
+
     it 'confirms a plane has left the airport' do
-      allow(airport.weather).to receive(:rand).with(0..9) {0}
       plane.land_at(airport)
       plane.take_off_from(airport)
       expect(airport.planes[0]).to_not eq plane
     end
     it 'allows a plane to land again once taken off' do
-      allow(airport.weather).to receive(:rand).with(0..9) {0}
       plane.land_at(airport)
       plane.take_off_from(airport)
       plane.land_at(airport)
       expect(airport.planes[0]).to eq plane
     end
     it 'raises error if weather is stormy' do
-      allow(airport.weather).to receive(:rand).with(0..9) {0}
       plane.land_at(airport)
-      allow(airport.weather).to receive(:rand).with(0..9) {9}
+      allow(airport).to receive(:clear?) {false}
       expect {plane.take_off_from(airport)}.to raise_error 'cannot take off in stormy weather'
     end
     it 'raises error if already in the air and attempting to take off' do
-      allow(airport.weather).to receive(:rand).with(0..9) {0}
       expect {plane.take_off_from(airport)}.to raise_error 'plane is already in the air'
     end
     it 'raises error if in a different airport' do
-      allow(airport.weather).to receive(:rand).with(0..9) {0}
       plane.land_at(airport)
       expect {plane.take_off_from(Airport.new)}.to raise_error 'plane is not at this airport'
     end
