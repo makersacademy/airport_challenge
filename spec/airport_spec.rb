50cd145b7d2c47fb51b1931bf18257657d914eac
Jon Sanders <jonsanders101@aol.com>
2017-10-01 13:15:45 +0100

    Nested #land and #take_off tests

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -4,16 +4,37 @@ describe Airport do
   let :plane {double :plane, flying: true, ground: nil, fly: nil}
   subject(:airport) {Airport.new}
 
-  it "stores landed planes" do
-    airport.land(plane)
-    expect(airport.planes).to include(plane)
+  it "has an adjustable plane capacity" do
+    airport = Airport.new(0)
+    expect(airport.capacity).to eq(0)
   end
 
-  context "plane landed then departed" do
+  describe "#land tests" do
+    it "stores landed planes" do
+      airport.land(plane)
+      expect(airport.planes).to include(plane)
+    end
+
+    it "won't allow landing after capacity reached" do
+      airport.capacity.times {airport.land(plane)}
+      expect {airport.land(plane)}.to raise_error("Airport full")
+    end
+
+    it "only lands a flying plane" do
+      allow(plane).to receive(:flying).and_return(false)
+      expect {airport.land(plane)}.to raise_error("Plane is grounded")
+    end
+
+    it "changes status of plane to not flying when landing" do
+      expect(plane).to receive(:ground)
+      airport.land(plane)
+    end
+  end
+
+  describe "#take_off tests" do
     it "releases planes after take-off" do
       allow(airport).to receive(:stormy?) {false}
       airport.land(plane)
-      allow(plane).to receive(:flying) {false}
       airport.take_off(plane)
       expect(airport.planes).not_to include(plane)
     end
@@ -24,42 +45,22 @@ describe Airport do
       airport.take_off(plane)
       expect(airport.at_airport?(plane)).to eq(false)
     end
-  end
 
-  it "prevents take-off in stormy weather" do
-    allow(airport).to receive(:stormy?) {true}
-    airport.land(plane)
-    expect {airport.take_off(plane)}.to raise_error("Cannot take-off during a storm")
-  end
+    it "prevents take-off in stormy weather" do
+      allow(airport).to receive(:stormy?) {true}
+      airport.land(plane)
+      expect {airport.take_off(plane)}.to raise_error("Cannot take-off during a storm")
+    end
 
-  it "won't allow landing after capacity reached" do
-    airport.capacity.times {airport.land(plane)}
-    expect {airport.land(plane)}.to raise_error("Airport full")
-  end
+    it "only releases planes it contains" do
+      expect {airport.take_off(plane)}.to raise_error("Plane not in airport")
+    end
 
-  it "has an adjustable plane capacity" do
-    airport = Airport.new(0)
-    expect(airport.capacity).to eq(0)
-  end
-
-  it "only releases planes it contains" do
-    expect {airport.take_off(plane)}.to raise_error("Plane not in airport")
-  end
-
-  it "only lands a flying plane" do
-    allow(plane).to receive(:flying).and_return(false)
-    expect {airport.land(plane)}.to raise_error("Plane is grounded")
-  end
-
-  it "changes status of plane to not flying when landing" do
-    expect(plane).to receive(:ground)
-    airport.land(plane)
-  end
-
-  it "changes status of plane to flying when taking-off" do
-    allow(airport).to receive(:stormy?) {false}
-    airport.land(plane)
-    expect(plane).to receive(:fly)
-    airport.take_off(plane)
+    it "changes status of plane to flying when taking-off" do
+      allow(airport).to receive(:stormy?) {false}
+      airport.land(plane)
+      expect(plane).to receive(:fly)
+      airport.take_off(plane)
+    end
   end
 end
