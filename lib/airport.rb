da2a6e89c400841ca2ce42eecbaaa09329566a13
Canace Wong <canacewongtoday@hotmail.com>
2017-10-01 06:08:02 -0700

    Raised error when capacity full

--- lib/airport.rb
+++ lib/airport.rb
@@ -2,17 +2,21 @@ require_relative '../lib/plane'
 
 class Airport
 
-  def plane_takeoff
-    plane = Plane.new
-    plane
-  end
-
   def land_plane(plane)
-   plane
+   @plane_count = 20
+   if @plane_count == 20
+     fail 'Airport capacity is full. No plane could land.'
+   end 
+   @plane = plane
   end
 
-  def confirm_takeoff(plane)
-    "#{plane.to_s} has now taken off"
+  def plane_takeoff
+    @plane = Plane.new
+    @plane
+  end
+
+ def confirm_takeoff
+    "#{@plane.to_s} has now taken off"
   end
 
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -3,21 +3,21 @@ require 'plane'
 
 describe Airport do
   subject(:airport) {Airport.new}
+  plane = Plane.new
 
   it { is_expected.to respond_to :plane_takeoff }
 
   it { is_expected.to respond_to(:land_plane).with(1).argument }
 
-  it 'lands the plane at the Airport' do
-    plane = Plane.new
-    expect(airport.land_plane(plane)).to eq plane
-  end
-
-  it { is_expected.to respond_to(:confirm_takeoff).with(1).argument }
+  it { is_expected.to respond_to(:confirm_takeoff) }
 
   it 'confirms when plane has taken off' do
-    plane = Plane.new
-    expect(airport.confirm_takeoff(plane)).to eq "#{plane.to_s} has now taken off"
+    plane = airport.plane_takeoff
+    expect(airport.confirm_takeoff).to eq "#{plane.to_s} has now taken off"
+  end
+
+  it "raises error as plane tries to land when capacity is full" do
+    expect { airport.land_plane(plane) }.to raise_error 'Airport capacity is full. No plane could land.'
   end
 
 
