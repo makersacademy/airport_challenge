5029d0572d1028ff02df994efae1862c5ea84660
David Halewood <davidnhalewood@hotmail.com>
2017-10-01 19:04:45 +0100

    adding parked_at method and first test

--- lib/plane.rb
+++ lib/plane.rb
@@ -9,4 +9,7 @@ class Plane
     @landed
   end
 
+  def parked_at
+  end
+
 end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -2,6 +2,12 @@ require 'plane'
 
 describe Plane do
 
+  let(:airport) do
+    airport = double :airport
+    allow(airport).to receive(:land_plane)
+    airport
+  end
+
   describe '#initialize' do
     it 'should be flying' do
       expect(subject.landed).to eq false
@@ -9,15 +15,16 @@ describe Plane do
   end
 
   describe '#landed?' do
-    class FakeAirport
-      def land_plane(plane)
-        plane.landed = true
-      end
-    end
-    it "will show if plane has landed at airport" do
-      jfk = FakeAirport.new
-      jfk.land_plane(subject)
+    it 'will show if plane has landed at airport' do
+      airport.land_plane(subject)
       expect(subject.landed?).to eq subject.landed
     end
   end
+
+  describe '#parked_at' do
+    it 'will show what airport plane is parked at' do
+    expect(subject).to respond_to (:parked_at)
+    end
+  end
+
 end
