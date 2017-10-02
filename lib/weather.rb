e723510a8996632313e2c6ef7dc7adeb7f1adde0
Alexis Carlier <alexis.carlier0@gmail.com>
2017-10-02 06:57:01 +0100

    Added weather class and test

--- lib/weather.rb
+++ lib/weather.rb
@@ -1,5 +1,5 @@
 class Weather
   def stormy?
-    
+
   end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -41,4 +41,18 @@ describe Airport do
       expect(subject.plane).to eq plane
     end
   end
+
+describe Weather do
+
+  let(:weather) { Weather.new }
+
+  it { is_expected.to respond_to(:stormy?) }
+
+  describe 'stormy' do
+    it 'returns true in stormy weather' do
+      allow(weather).to receive(:stormy?).and_return(true)
+      expect(weather.stormy?).to eq true
+    end
+  end
+end
 end
