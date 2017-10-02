8d753ca39039dcede372f205cc868999a6f80093
lucasasalmins <lucasasalmins@gmail.com>
2017-10-02 06:51:15 +0100

    removed already_landed? method as had bug

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,3 +1,5 @@
+require_relative 'weather.rb'
+
 class Airport
 
   DEFAULT_CAPACITY = 20
@@ -8,18 +10,22 @@ class Airport
     @capacity = capacity
   end
 
+  def weather
+    @weather = Weather.new.current
+  end
+
   def land(plane)
-    @plane = plane
+    # @plane = plane
     fail 'Airport is full!' if full?
-    fail 'That plane is already landed here!' if already_landed?
-    @plane.status = "Landed"
-    @planes << @plane
-    @plane
+    fail 'That plane is already landed here!' if @planes.include? plane
+    plane.status = "Landed"
+    @planes << plane
+    plane
   end
 
   def take_off
     fail 'There are no planes!' if empty?
-    # fail "Sorry, it is too stormy to take off" if weather == "Stormy"
+    fail "Sorry, it is too stormy to take off" if @weather == "Stormy"
     @planes.pop.status = "Taken off"
   end
 
@@ -34,7 +40,7 @@ class Airport
     @planes.empty?
   end
 
-  def already_landed?
-    @planes.include? @plane
-  end
+  # def already_landed?
+  #   @planes.include? @plane
+  # end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -44,11 +44,13 @@ describe Airport do
     end
   end
 
-  describe 'transit limited dictated by weather' do
-    # it 'prevents planes taking off in stormy weather' do
-    #   airport = Airport.new
-    #   airport.land(FakePlane.new)
-    #   expect { airport.take_off }.to raise_error 'Sorry, it is too stormy to take off'
-    # end
-  end
+  # describe 'transit limitations dictated by weather' do
+  #   it 'prevents planes taking off in stormy weather' do
+  #     weather = double(@stormy = "Stormy")
+  #     # subject.weather
+  #     p weather.stormy
+  #     subject.land(FakePlane.new)
+  #     expect { subject.take_off }.to raise_error 'Sorry, it is too stormy to take off'
+  #   end
+  # end
 end
