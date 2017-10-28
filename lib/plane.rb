5705877760739ecf2c39c499635fa10d08c3ddeb
Robert Clayton <robbie.c.clayton@gmail.com>
2017-10-29 00:53:37 +0100

    created a land method for plane & tests for planes to land

--- lib/plane.rb
+++ lib/plane.rb
@@ -1,3 +1,5 @@
 class Plane
+  def land(airport)
 
+  end
 end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -7,4 +7,13 @@ describe Plane do
       expect(test_plane).to be_instance_of(Plane)
     end
   end
+  describe 'land' do
+    it 'should send allow the plane to land' do
+      expect(test_plane).to respond_to(:land)
+    end
+
+    it 'should send allow the plane to land at an Airport' do
+      expect(test_plane).to respond_to(:land).with(1).argument
+    end
+  end
 end
