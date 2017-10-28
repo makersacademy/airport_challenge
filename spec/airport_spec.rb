21a3a0a804747a56fe9a0a690a8357169e850b4d
antonio <antoniobelmardacosta@gmail.com>
2017-10-28 14:09:07 +0100

    allows landing at an airport

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,7 +1,15 @@
 require "airport"
 
 describe Airport do
+
+  let(:airport) { Airport.new }
+
   it "allows an airport to exist" do
     expect(Airport.new).to be_an_instance_of(Airport)
   end
+
+  it "allows landing at an airport" do
+    expect(airport).to respond_to(:land)
+  end
+
 end
