64ea289fb84b6ff786b4235350c2ce3518b7905f
Lewis <lewisyoul@gmail.com>
2017-10-28 19:07:34 +0100

    add test for return of #stormy?

--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -2,7 +2,7 @@ require 'weather'
 
 describe Weather do
   subject { Weather.new }
-  let(:conditions_array) { ["stormy", "sunny", "cloudy", "rainy", "snowy"] }
+  let(:conditions_array) { [1, 0, 0, 0, 0] }
 
   it "should return an instance of Weather" do
     expect(subject).to be_an_instance_of(Weather)
@@ -19,6 +19,8 @@ describe Weather do
       expect(subject).to respond_to(:stormy?)
     end
 
-
+    it "should return an element of conditions_array" do
+      expect(conditions_array).to include(subject.stormy?)
+    end
   end
 end
