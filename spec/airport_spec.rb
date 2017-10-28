8947547447aecdee031156012a8eae78b275016f
cristhiandas <cristhiandf94@gmail.com>
2017-10-28 16:06:51 +0100

    Added some tests

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -9,8 +9,15 @@ describe Airport do
   subject { Airport.new }
 
   context "Checks Capacity" do
+    plane = FlyingPlane.new
     it "Raise an error if the Airport is full" do
-      expect{ 52.times{ subject.land(FlyingPlane.new)}}.to raise_error("Cannot land, airport at capacity")
+      expect{ 52.times{ subject.land(plane)}}.to raise_error("Cannot land, airport at capacity")
+    end
+    it "Lands the planes when above capacity" do
+      expect( 12.times{ subject.land(plane).landed_planes.count } ).to eq(12)
+    end
+    it "Lands all the planes up to the capacity" do
+      expect( 50.times{ subject.land(plane).landed_planes.count } ).to eq(50)
     end
   end
 
