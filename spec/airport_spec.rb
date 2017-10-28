20cf382f7f581b82436bbea11576b8621889ce30
Lewis <lewisyoul@gmail.com>
2017-10-28 23:00:51 +0100

    add test for #change_weather

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -5,6 +5,7 @@ describe Airport do
   subject { Airport.new }
   let(:plane) { Plane.new }
   let(:weather) { double :weather }
+  let(:airport_2) { double :airport_2 }
 
   it "should create a new airport" do
     expect(subject).to be_an_instance_of(Airport)
@@ -45,6 +46,9 @@ describe Airport do
     expect(subject).to respond_to(:change_weather)
   end
 
+  it "changes weather" do
+    expect(subject.change_weather).to eq(true).or eq(false)
+  end
 
   describe "Errors" do
     it "should raise error if plane not in airport" do
@@ -56,11 +60,12 @@ describe Airport do
       expect { subject.land(plane) }.to raise_error("plane already in airport")
     end
 
-    it "should raise error when #take_off is called when stormy" do
+    it "should raise error when stormy and #take_off is called " do
       allow(weather).to receive(:stormy?).and_return(1)
       airport = Airport.new(weather.stormy?)
       airport.land(plane)
       expect { airport.take_off(plane) }.to raise_error("the weather is stormy")
     end
+
   end
 end
