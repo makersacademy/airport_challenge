aea9b9255ef6085bd2683a7f702ef5629bcca6ea
Lewis <lewisyoul@gmail.com>
2017-10-28 19:29:39 +0100

    remove attr_reader

--- lib/weather.rb
+++ lib/weather.rb
@@ -1,12 +1,6 @@
 class Weather
-  attr_reader :conditions
-
-  # def initialize
-  #   @conditions = [1, 0, 0, 0, 0]
-  # end
 
   def self.stormy?
-    #@conditions
     [1, 0, 0, 0, 0].sample
   end
 
