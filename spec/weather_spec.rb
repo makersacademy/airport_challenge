7e3c007a0791dcc8a725fc61ef1fd115ef20d16f
Lewis <lewisyoul@gmail.com>
2017-10-28 18:25:26 +0100

    add test to respond to #stormy

--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -1,8 +1,13 @@
 require 'weather'
 
 describe Weather do
+  subject { Weather.new }
 
   it "should return an instance of Weather" do
-    expect(Weather.new).to be_an_instance_of(Weather)
+    expect(subject).to be_an_instance_of(Weather)
+  end
+
+  it "should respond to #stormy?" do
+    expect(subject).to respond_to(:stormy?)
   end
 end
