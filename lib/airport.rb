34eaf15ca5782c0bcc0848f417573f45843062cc
antonio <antoniobelmardacosta@gmail.com>
2017-10-28 22:21:16 +0100

    Refactors landing and taking-off methods to only check for existing planes and creates extra private methods to store or delete planes from @planes

--- lib/airport.rb
+++ lib/airport.rb
@@ -7,12 +7,22 @@ class Airport
 
   def land(plane)
     raise "This plane is already at this airport." if @planes.include?(plane)
-    @planes.push(plane)
+    add_plane(plane)
   end
 
   def take_off(plane)
     raise "This plane is not at this airport." unless @planes.include?(plane)
+    remove_plane(plane)
+  end
+
+private
+
+  def remove_plane(plane)
     @planes.delete(plane)
+  end
+
+  def add_plane(plane)
+    @planes.push(plane)
   end
 
 end
