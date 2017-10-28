709079b1012fcab0d2a849aa1b2775b15a58d09c
Marie Kerkstoel <mariekerkstoel@gmail.com>
2017-10-28 17:00:01 +0100

    Added take off method for planes

--- lib/plane.rb
+++ lib/plane.rb
@@ -2,4 +2,9 @@ class Plane
   def land(airport)
     "Plane has landed at #{airport}. "
   end
+
+  def take_off(airport)
+    "Plane has taken off and is no longer in #{airport}"
+  end
+
 end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -4,19 +4,19 @@ require 'airport'
 
 
 describe Plane do
+let(:airport) {double Airport.new}
 
   describe "#land" do
     it "should make planes land in the designated airport" do
-      airport = Airport.new
       expect(subject.land(airport)).to eq "Plane has landed at #{airport}. "
     end
   end
 
-  # describe "#take_off" do
-  #   it "should make plans take off and leave airport" do
-  #
-  #   end
-  # end
+  describe "#take_off" do
+    it "should make plans take off and leave airport" do
+      expect(subject.take_off(airport)).to eq "Plane has taken off and is no longer in #{airport}"
+    end
+  end
 
 
 end
