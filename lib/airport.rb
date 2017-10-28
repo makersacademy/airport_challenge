6b8ce79f89fef7591bd9fe82e69f33a6d30a0886
Lewis <lewisyoul@gmail.com>
2017-10-28 18:10:23 +0100

    create private methods #present? #remove_plane #add_plane

--- lib/airport.rb
+++ lib/airport.rb
@@ -7,17 +7,25 @@ class Airport
 
   def land(plane)
     fail "plane already in airport" if present?(plane)
-    @planes << plane
+    add_plane(plane)
   end
 
   def take_off(plane)
     fail "plane not in airport" unless present?(plane)
-    @planes.delete(plane)
+    remove_plane(plane)
   end
 
   private
 
   def present?(plane)
     @planes.include?(plane)
+  end
+
+  def remove_plane(plane)
+    @planes.delete(plane)
+  end
+
+  def add_plane(plane)
+    @planes << plane
   end
 end
