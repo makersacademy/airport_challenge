73450307766e9314306c253dfe6afdcf9b86bc92
Robert Clayton <robbie.c.clayton@gmail.com>
2017-10-29 00:43:10 +0100

    Tested that new airports and planes can be made

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,5 +1,10 @@
 require 'airport'
 
 describe Airport do
-
+  let(:test_airport) { Airport.new }
+  describe 'airport' do
+    it 'should create a new airport' do
+      expect(test_airport).to be_instance_of(Airport)
+    end
+  end
 end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -1,5 +1,10 @@
 require 'plane'
 
 describe Plane do
-
+  let(:test_plane) { Plane.new }
+  describe 'plane' do
+    it 'should create a new plane' do
+      expect(test_plane).to be_instance_of(Plane)
+    end
+  end
 end
