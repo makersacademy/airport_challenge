503d48a62ee6fa3149a270fb304ef2eead15873d
Lewis <lewisyoul@gmail.com>
2017-10-28 19:28:32 +0100

    refactor to call self.stormy?

--- lib/weather.rb
+++ lib/weather.rb
@@ -1,12 +1,13 @@
 class Weather
   attr_reader :conditions
 
-  def initialize
-    @conditions = [1, 0, 0, 0, 0]
-  end
+  # def initialize
+  #   @conditions = [1, 0, 0, 0, 0]
+  # end
 
-  def stormy?
-    @conditions.sample
+  def self.stormy?
+    #@conditions
+    [1, 0, 0, 0, 0].sample
   end
 
 end
