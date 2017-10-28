0a3a833a80882528dc3e12b04c900f2236f2723d
Lewis <lewisyoul@gmail.com>
2017-10-28 16:23:53 +0100

    add test for Airport#land

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,9 +1,13 @@
 require 'airport'
 
 describe Airport do
+  subject { Airport.new }
 
   it "should create a new airport" do
-    expect(Airport.new).to be_an_instance_of(Airport)
+    expect(subject).to be_an_instance_of(Airport)
   end
 
+  it "should respond to #land" do
+    expect(subject).to respond_to(:land)
+  end
 end
