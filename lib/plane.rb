3fb878867256c52365ba5bfc3db67a6920d56cfa
Eustaquio122 <tiagofsousa@gmail.com>
2017-10-28 18:44:51 +0100

    Plane status adapts correctly to landing and take off

--- lib/plane.rb
+++ lib/plane.rb
@@ -1,8 +1,16 @@
 class Plane
   attr_reader :status
 
-  def initialize
-    @status = "flying"
+  def initialize(status = "flying")
+    @status = status
+  end
+
+  def land(airport)
+    @status = "ground" if airport.request("landing", self)
+  end
+
+  def take_off(airport)
+    @status = "flying" if airport.request("take_off", self)
   end
 
 end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -3,11 +3,29 @@ require 'plane'
 describe Plane do
 
   subject(:plane) { Plane.new }
+  subject(:grounded_plane) { Plane.new("ground") }
+
+  let(:airport) { double('airport') }
 
   describe 'plane has adequate status information' do
     it 'initializes with status set to flying' do
       expect(plane.status).to eq "flying"
     end
+
+    before do
+      allow(airport).to receive(:request) { true }
+    end
+
+    it 'changes status to ground after landing' do
+      plane.land(airport)
+      expect(plane.status).to eq "ground"
+    end
+
+    it 'changes status to flying after take off' do
+      grounded_plane.take_off(airport)
+      expect(grounded_plane.status).to eq "flying"
+    end
+
   end
 
 end
