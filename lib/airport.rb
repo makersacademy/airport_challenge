ecf2857ed48d5b508f625ff746bf3a9283d8c17d
Oleg Grushetskyy <olegfkl@gmail.com>
2017-10-28 19:52:02 +0100

    Confirm plane has landed or taken off when passed to according method as an argument

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,12 +1,16 @@
 require_relative "plane"
 class Airport
   def initialize
+    @available = []
   end
 
   def land(plane)
+    @available << plane
+    true
   end
 
   def take_off(plane)
-
+      @available.pop
+      true
   end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,13 +1,19 @@
 require 'airport'
 
-
 describe Airport do
   subject(:airport) { described_class.new }
-
-  it { is_expected.to be_a(Airport) }
 
   it { is_expected.to respond_to(:land).with(1).argument }
 
   it { is_expected.to respond_to(:take_off).with(1).argument }
 
+  it 'Confirm the plane has landed' do
+    plane = double(:plane)
+    expect(subject.land(plane)).to eq true
+  end
+
+  it 'Confirm the plane has taken off' do
+    plane = double(:plane)
+    expect(subject.take_off(plane)).to eq true
+  end
 end
