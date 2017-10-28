1a84f75ce4350776440ab74e9554edb5b8d1f584
antonio <antoniobelmardacosta@gmail.com>
2017-10-28 22:15:21 +0100

    initializes planes with a false status for variable flying

--- lib/plane.rb
+++ lib/plane.rb
@@ -1,3 +1,11 @@
 class Plane
 
+  def initialize(flying=false)
+    @flying=flying
+  end
+
+  def flying?
+    @flying
+  end
+
 end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -10,6 +10,22 @@ describe Plane do
       expect(Plane.new).to be_an_instance_of(Plane)
     end
 
+    it "allows a plane to exist passing one argument, flying status" do
+      expect(Plane).to respond_to(:new).with(1).argument
+    end
+
+  end
+
+  describe "flying" do
+
+    it "expects a plane to respond to flying method" do
+      expect(plane).to respond_to(:flying?)
+    end
+
+    it "expects flying method to return false to a newly created plane" do
+      expect(plane.flying?). to eq false
+    end
+
   end
 
 end
