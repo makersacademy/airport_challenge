6851b76678bff28c234d7e84c32e091622f31f93
cristhiandas <cristhiandf94@gmail.com>
2017-10-28 14:02:35 +0100

    Created the Class plane, created a method to check if the plane is flying. Made an unit test

---
+++ lib/airplane.rb
@@ -0,0 +1,16 @@
+class Airplane
+  attr_accessor :landed
+
+#We initialize the plane, the first thing we need is to know if the
+#the plane is landed or not, so we will ask for that info, if the info is not given
+#we will assume that the plane is not in land
+  def initialize(landed = false)
+    @landed = landed
+  end
+
+#If the airplane is landed, then flying? will return false
+#If the airplane is not landed it means it is flying so it will return true
+  def flying?
+    !@landed
+  end
+end
