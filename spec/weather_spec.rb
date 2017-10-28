e57a30cab7f744e27215e0c670a9384173020bb8
Lewis <lewisyoul@gmail.com>
2017-10-28 18:45:34 +0100

    add test for conditions

--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -2,12 +2,23 @@ require 'weather'
 
 describe Weather do
   subject { Weather.new }
+  let(:conditions_array) { ["stormy", "sunny", "cloudy", "rainy", "snowy"] }
 
   it "should return an instance of Weather" do
     expect(subject).to be_an_instance_of(Weather)
   end
 
-  it "should respond to #stormy?" do
-    expect(subject).to respond_to(:stormy?)
+  describe "#conditions" do
+    it "should return the conditions array" do
+      expect(subject.conditions).to eq(conditions_array)
+    end
+  end
+
+  describe "#stormy?" do
+    it "should respond to #stormy?" do
+      expect(subject).to respond_to(:stormy?)
+    end
+
+
   end
 end
