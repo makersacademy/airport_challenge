6b828834b80faac861ce14633635cc267c4179e9
Tabish Raza <tabishraza@gmail.com>
2017-10-28 15:28:17 +0100

    test created for weather check - stormy

---
+++ spec/weather_spec.rb
@@ -0,0 +1,8 @@
+require 'Weather'
+
+describe Weather do
+  subject(:weather) {described_class.new}
+  it "should return that weather is stormy" do
+    expect(weather.stormy).to eq true
+  end
+end
