4b9c62ddf21ec35802e6cd6d7ce4f232ef084e46
thatdania@gmail.com <thatdania@gmail.com>
2017-10-29 18:24:56 +0000

    Have only two test. Checking land and argument no.

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,13 +2,13 @@ require './lib/airport.rb'
 
 describe Airport do
 it { expect(Airport.new).to respond_to(:land) }
-it { is_expected.to respond_to(:stand_by).with(1).argument }
+it { is_expected.to respond_to(:land).with(1).argument }
 end
 
-describe Airport do
- it 'returns landed plane' do
-  plane = Plane.new
-  subject.stand_by(plane)
-  expect(subject.plane).to eq plane
- end
-end
+# describe Airport do
+#  it 'returns landed plane' do
+#   plane = Plane.new
+#   subject.stand_by(plane)
+#   expect(subject.plane).to eq plane
+#  end
+# end
