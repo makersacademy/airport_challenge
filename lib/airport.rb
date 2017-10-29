297dcaf035f82d3d4b24bd5f10bee1f920a6f369
cristhiandas <cristhiandf94@gmail.com>
2017-10-29 21:00:13 +0000

    Re-ordered the code, used private to have a secure code

--- lib/airport.rb
+++ lib/airport.rb
@@ -8,22 +8,21 @@ class Airport
     @capacity = capacity
   end
 
-  def raise_stormy_errors(sunny_day)
-    raise "Cannot land or takeoff while stormy" if sunny_day == false
-  end
-
-  def landing_errors(sunny_day, flying)
-    raise_stormy_errors(sunny_day)
-    raise "Cannot land, airport at capacity" if full?
-    raise "Cannot land planes on the ground" unless flying
-  end
-
   def land(plane, sunny_day = true, flying = true)
     landing_errors(sunny_day, flying)
     @landed_planes << plane
     self
   end
 
+  def takeoff(sunny_day)
+    takeoff_errors(sunny_day)
+    tookoff = @landed_planes.pop
+    puts "The plane is now flying"
+    tookoff
+  end
+
+  private
+  
   def full?
     @capacity <= @landed_planes.count
   end
@@ -33,11 +32,14 @@ class Airport
     raise "Cannot takeoff if there are no planes" if @landed_planes.empty?
   end
 
-  def takeoff(sunny_day)
-    takeoff_errors(sunny_day)
-    tookoff = @landed_planes.pop
-    puts "The plane is now flying"
-    tookoff
+  def raise_stormy_errors(sunny_day)
+    raise "Cannot land or takeoff while stormy" if sunny_day == false
+  end
+
+  def landing_errors(sunny_day, flying)
+    raise_stormy_errors(sunny_day)
+    raise "Cannot land, airport at capacity" if full?
+    raise "Cannot land planes on the ground" unless flying
   end
 
 end
