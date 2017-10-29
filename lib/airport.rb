fa216e18b442d94d3f3ec4349442f95425534cb9
Tabish Raza <tabishraza@gmail.com>
2017-10-29 16:10:55 +0000

    added stub method to account for random weather function

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,9 +1,7 @@
 # require_relative 'weather'
-
 class Airport
 
   attr_reader :capacity, :terminal
-
   DEFAULT_CAPACITY = 2
 
   def initialize(capacity = DEFAULT_CAPACITY)
@@ -13,23 +11,23 @@ class Airport
 
   def land(plane)
     raise "Airport is full" if full?
-    weather ? "Got a storm front coming, cannot land here" : @terminal << plane
+    weather ? "Got a storm front coming, cannot land here" : terminal << plane
   end
 
   def take_off(plane)
-    weather ? "The skies are dark, best to stay on the ground" : @terminal.delete(plane)
+    weather ? "The skies are dark, best to stay on the ground" : terminal.delete(plane)
+  end
+
+  def weather
+    rand(1..10) > 5 ? true : false
   end
 
   private
 
   def full?
-    @terminal.length >= DEFAULT_CAPACITY
+    terminal.length >= DEFAULT_CAPACITY
   end
 
-  def weather
-    # if greater than 5, weather is stormy - else, it's clear
-    # rand(1..10)
-    (7 > 5) ? true : false
-  end
+
 
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,21 +1,44 @@
 require "airport"
-require "plane"
 
 describe Airport do
-  subject(:airport) { described_class.new }
 
-  it "if stormy, plane does NOT take off" do
-    plane = Plane.new
-    expect(airport.take_off(plane)).to eq "The skies are dark, best to stay on the ground"
+  subject(:airport) { described_class.new }
+  let(:plane) { double :plane }
+
+  it "confirm plane has landed" do
+    Airport.stub(:weather) do |w|
+      if w == true
+        expect(airport.land(plane)).to eq [plane]
+      end
+    end
   end
 
+  it "confirm plane has taken off" do
+    Airport.stub(:weather) do |w|
+      if w == true
+        airport.land(plane)
+        expect(airport.take_off(plane)).to eq [plane]
+      end
+    end
+  end
+
+  it "if stormy, plane does NOT take off" do
+    Airport.stub(:weather) do |w|
+      if w == true
+        expect(airport.take_off(plane)).to eq "The skies are dark, best to stay on the ground"
+      end
+    end
+  end
+  
   it "If stormy, plane does NOT land" do
-    plane = Plane.new
-    expect(airport.land(plane)).to eq ("Got a storm front coming, cannot land here")
+    Airport.stub(:weather) do |w|
+      if w == true
+        expect(airport.land(plane)).to eq "Got a storm front coming, cannot land here"
+      end
+    end
   end
 
   it "raises an error if terminal is full" do
-    plane = Plane.new
     Airport.stub(:weather) do |w|
       if w == true
         Airport::DEFAULT_CAPACITY.times { airport.land(plane) }
