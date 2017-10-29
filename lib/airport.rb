dc824b15025a653159889206dcbbad334e6e2edd
Lewis <lewisyoul@gmail.com>
2017-10-29 15:16:13 +0000

    remove comments

--- lib/airport.rb
+++ lib/airport.rb
@@ -3,32 +3,24 @@ class Airport
 
   DEFAULT_CAPACITY = 20
 
-  def initialize(capacity = DEFAULT_CAPACITY)#, weather = "sunny")
+  def initialize(capacity = DEFAULT_CAPACITY)
     @planes = []
-    @weather = Weather.new #weather
+    @weather = Weather.new
     @capacity = capacity
   end
 
   def land(plane)
     fail "plane already in airport" if present?(plane)
-    fail "the weather is stormy" if weather.weather == "stormy"
+    fail "the weather is stormy" if weather.stormy?
     fail "the airport is full" if at_capacity?
     @planes << plane
   end
 
   def take_off(plane)
     fail "plane not in airport" unless present?(plane)
-    fail "the weather is stormy" if weather.weather == "stormy"
+    fail "the weather is stormy" if weather.stormy?
     @planes.delete(plane)
   end
-
-  # def new_day(*weather)
-  #   if weather.empty?
-  #     @weather = Weather.new.weather
-  #   else
-  #     @weather = weather[0]
-  #   end
-  # end
 
   private
 
@@ -40,7 +32,4 @@ class Airport
     @planes.length >= @capacity
   end
 
-  # def stormy?
-  #   @weather == "stormy"
-  # end
 end
