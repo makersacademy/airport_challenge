f155c4b69e9a4521876d16c029caff12be8d755f
Andrew Davey <ajdavey8@gmail.com>
2017-10-29 10:57:54 +0000

    test passed for plane lands at airport and is in airport once landed

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,6 +2,11 @@ require 'Airport'
 
 describe Airport do
   subject(:airport) { described_class.new }
+  let(:plane) { double(:land) }
 
   it { is_expected.to respond_to(:land).with(1).argument }
+  it 'has the plane once landed' do
+    airport.land(plane)
+    expect(airport.hangar).to include plane
+  end
 end
