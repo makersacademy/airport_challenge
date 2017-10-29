6f5560e386978e96ece15c2e88d1f5a69004e6b9
thatdania@gmail.com <thatdania@gmail.com>
2017-10-29 18:24:23 +0000

    Delegated the input of planes to an array

--- lib/airport.rb
+++ lib/airport.rb
@@ -2,14 +2,14 @@ require './lib/plane.rb'
 
 class Airport
 
-attr_reader :plane
+attr_reader :hangar
 
-  def land
-  Plane.new
+  def initialize
+   @hangar = []
   end
 
-  def stand_by(plane)
-  @plane = plane
+  def land(plane)
+  @hangar << plane
   end
 
   def take_off
