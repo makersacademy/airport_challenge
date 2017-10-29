3df10a7f72d7b89affc1795893e411ed0422d92c
Lewis <lewisyoul@gmail.com>
2017-10-29 14:52:30 +0000

    weather class now determines weather at all times

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,23 +1,24 @@
 class Airport
-  attr_accessor :planes, :weather, :capacity
+  attr_reader :planes, :weather, :capacity
+
   DEFAULT_CAPACITY = 20
 
-  def initialize(capacity = DEFAULT_CAPACITY, weather = "sunny")
+  def initialize(capacity = DEFAULT_CAPACITY)#, weather = "sunny")
     @planes = []
-    @weather = weather
+    @weather = Weather.new #weather
     @capacity = capacity
   end
 
   def land(plane)
     fail "plane already in airport" if present?(plane)
-    fail "the weather is stormy" if stormy?
+    fail "the weather is stormy" if weather.weather == "stormy"
     fail "the airport is full" if at_capacity?
     @planes << plane
   end
 
   def take_off(plane)
     fail "plane not in airport" unless present?(plane)
-    fail "the weather is stormy" if stormy?
+    fail "the weather is stormy" if weather.weather == "stormy"
     @planes.delete(plane)
   end
 
