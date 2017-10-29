c1dc5be6f9522911026ad2ad8bc97a5bc1684864
thatdania@gmail.com <thatdania@gmail.com>
2017-10-29 15:53:02 +0000

    Returned standby plane

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,6 +1,27 @@
 class Airport
 
+attr_reader :plane
+
   def land
+     Plane.new
+  end
+
+  def stand_by(plane)
+  @plane = plane
   end
 
 end
+
+class Plane
+end
+
+
+
+
+  # def standby(plane)
+  #
+  # end
+  #
+  # def plane
+  #
+  # end
