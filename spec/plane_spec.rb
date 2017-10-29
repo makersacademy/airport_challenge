fa70b4f52b3e4ac473687f34a2aa002349d986f3
Andrew Davey <ajdavey8@gmail.com>
2017-10-29 11:02:42 +0000

    test passed for plane responding to take off

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -4,4 +4,5 @@ describe Plane do
   subject(:plane) { described_class.new }
 
   it { is_expected.to respond_to(:land).with(1).argument }
+  it { is_expected.to respond_to(:take_off).with(1).argument }
 end
