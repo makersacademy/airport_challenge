c06c739c738d05154a9cbea800067df4b452bc5d
Lewis <lewisyoul@gmail.com>
2017-10-29 10:33:01 +0000

    raise error for #take_off when stormy

--- lib/airport.rb
+++ lib/airport.rb
@@ -8,17 +8,22 @@ class Airport
 
   def land(plane)
     fail "plane already in airport" if present?(plane)
-    add_plane(plane)
+    fail "the weather is stormy" if @stormy
+    @planes << plane
   end
 
   def take_off(plane)
     fail "plane not in airport" unless present?(plane)
     fail "the weather is stormy" if @stormy
-    remove_plane(plane)
+    @planes.delete(plane)
   end
 
-  def change_weather
-    @stormy = Weather.new.stormy?
+  def change_weather(*stormy)
+    if stormy.empty?
+      @stormy = Weather.new.stormy?
+    else
+      @stormy = stormy[0]
+    end
   end
 
   private
@@ -27,11 +32,4 @@ class Airport
     @planes.include?(plane)
   end
 
-  def remove_plane(plane)
-    @planes.delete(plane)
-  end
-
-  def add_plane(plane)
-    @planes << plane
-  end
 end
