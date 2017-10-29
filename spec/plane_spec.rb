e3d3b729738cdabbbf6ca7cdcf7474fd742e226a
Andrew Davey <ajdavey8@gmail.com>
2017-10-29 18:04:50 +0000

    test passed for added stormy? method in weather class

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -1,8 +1,6 @@
 require 'plane'
 
 describe Plane do
-  subject(:plane) { described_class.new }
-
   it { is_expected.to respond_to(:land).with(1).argument }
   it { is_expected.to respond_to(:take_off).with(1).argument }
 end
