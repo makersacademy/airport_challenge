9ac48e002412c970f6b6f962bd509b66a9877827
gabrielabud <gabriela.budeanu@gmail.com>
2017-10-29 19:41:56 +0000

    default airport capacity

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,19 +1,32 @@
 require_relative './plane.rb'
 
 class Airport
-attr_reader :planes
-  def initialize
+attr_reader :capacity
+AIRPORT_CAPACITY=20
+  def initialize(capacity=AIRPORT_CAPACITY)
     @planes=[]
+    @capacity=capacity
   end
 
   def land(plane)
-    fail "airport is full" if @planes.count>=20
+    fail "airport is full" if full?
     @planes << plane
   end
 
   def take_off
-    fail 'no planes available' if @planes.empty?
+    fail 'no planes available' if empty?
     @planes.pop
+  end
+
+  private
+  attr_reader :planes
+
+  def full?
+    @planes.count>=capacity
+  end
+
+  def empty?
+    @planes.empty?
   end
 
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,33 +2,47 @@ require 'airport'
 
 describe Airport do
 
-  it { is_expected.to respond_to :land }
-
-  it { is_expected.to respond_to :take_off}
-
-  it { is_expected.to respond_to(:land).with(1).argument }
-
   describe '#land' do
 
-    it 'landing a plane' do
-      plane=Plane.new
-      expect(subject.land(plane)).to eq subject.planes
+    it { is_expected.to respond_to :land }
+    it { is_expected.to respond_to(:land).with(1).argument }
+    # it { is_expected.to respond_to(:planes) }
+    # it 'landing a plane' do
+    #   plane=Plane.new
+    #   expect(subject.land(plane)).to eq subject.planes
+    # end
+    # it 'landed planes' do
+    #   plane=Plane.new
+    #   subject.land(plane)
+    #   expect(subject.planes).to be_a Array
+    # end
+    let(:plane) {Plane.new}
+    it 'raises an error when airport is full' do
+      described_class::AIRPORT_CAPACITY.times do
+        subject.land(plane)
+      end
+      expect{ subject.land(plane) }.to raise_error(RuntimeError)
     end
 
-    it 'raises and error when airport is full' do
-      20.times { subject.land(Plane.new) }
-      expect { subject.land Plane.new }.to raise_error(RuntimeError)
-    end
 
   end
 
+
   describe '#take_off' do
 
+    it { is_expected.to respond_to :take_off}
     it 'raises an error when there are no planes available' do
       expect{ subject.take_off }.to raise_error(RuntimeError)
     end
 
   end
 
+  describe "airport capacity" do
+
+    it "has a default capacity" do
+      expect(subject.capacity).to eq Airport::AIRPORT_CAPACITY
+    end
+
+  end
 
 end
