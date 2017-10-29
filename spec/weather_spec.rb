5f9d65ed922998c6d92310946670bd38fe5fcb21
majdeddine <majda-jambouri@hotmail.fr>
2017-10-29 15:59:11 +0000

    update weather class

---
+++ spec/weather_spec.rb
@@ -0,0 +1,10 @@
+require 'weather'
+
+describe Weather do
+
+   it "should be rarely stormy " do
+     array =[]
+     10.times{array << Weather.stormy?}
+     expect(array.count(false)).to be > 5
+   end
+end
