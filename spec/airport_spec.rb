fce34563adc93629306d21b0d373e7d398684fe8
Lewis <lewisyoul@gmail.com>
2017-10-29 13:30:04 +0000

    refactor tests into method groups

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -4,56 +4,17 @@ require 'plane'
 describe Airport do
   subject { Airport.new }
   let(:plane) { Plane.new }
-  let(:weather) { double :weather }
-  let(:airport_2) { double :airport_2 }
+  let(:weather) { double( :weather => "stormy") }
 
-  it "should create a new airport" do
-    expect(subject).to be_an_instance_of(Airport)
-  end
+    it { is_expected.to be_an_instance_of(Airport) }
 
-  it "should respond to #land with 1 argument" do
-    expect(subject).to respond_to(:land).with(1).argument
-  end
+  describe "#land" do
 
-  it "should respond to #take_off with 1 argument" do
-    expect(subject).to respond_to(:take_off).with(1).argument
-  end
+    it{ is_expected.to respond_to(:land).with(1).argument }
 
-  it "should respond to #planes" do
-    expect(subject).to respond_to(:planes)
-  end
-
-  it "#planes should return an array" do
-    expect(subject.planes).to be_an_instance_of(Array)
-  end
-
-  it "should store a landed plane at the airport" do
-    subject.land(plane)
-    expect(subject.planes.first).to eq(plane)
-  end
-
-  it "#take_off should remove the plane from the airport" do
-    subject.land(plane)
-    subject.take_off(plane)
-    expect(subject.planes).to eq([])
-  end
-
-  it "should respond to #stormy" do
-    expect(subject).to respond_to(:weather)
-  end
-
-  it "responds to #change_weather" do
-    expect(subject).to respond_to(:change_weather)
-  end
-
-  it "changes weather" do
-    expect(subject.change_weather).to eq("stormy").or eq("sunny")
-  end
-
-  describe "Errors" do
-
-    it "should raise error if plane not in airport" do
-      expect { subject.take_off(plane) }.to raise_error("plane not in airport")
+    it "should store a landed plane at the airport" do
+      subject.land(plane)
+      expect(subject.planes.first).to eq(plane)
     end
 
     it "should raise error if plane already in airport" do
@@ -61,23 +22,65 @@ describe Airport do
       expect { subject.land(plane) }.to raise_error("plane already in airport")
     end
 
-    it "should raise error when stormy and #take_off is called " do
-      allow(weather).to receive(:weather) { "stormy" }
-      subject.land(plane)
-      subject.change_weather(weather.weather)
-      expect { subject.take_off(plane) }.to raise_error("the weather is stormy")
-    end
-
     it "should raise error if stormy & #land is called" do
-      allow(weather).to receive(:weather) { "stormy" }
-      airport = Airport.new(10, weather.weather)
+      airport = Airport.new(20, weather.weather)
       expect { airport.land(plane) }.to raise_error("the weather is stormy")
     end
 
     it "should raise error when full & #land called" do
-      10.times { subject.land(Plane.new) }
+      20.times { subject.land(Plane.new) }
       expect { subject.land(plane) }.to raise_error("the airport is full")
     end
+  end
 
+  describe "#take_off" do
+    it "should respond to #take_off with 1 argument" do
+      expect(subject).to respond_to(:take_off).with(1).argument
+    end
+
+    it "should raise error if plane not in airport" do
+      expect { subject.take_off(plane) }.to raise_error("plane not in airport")
+    end
+
+    it "should raise error when stormy and #take_off is called " do
+      subject.land(plane)
+      subject.new_day(weather.weather)
+      expect { subject.take_off(plane) }.to raise_error("the weather is stormy")
+    end
+  end
+
+  describe "#planes" do
+    it "#planes should return an array" do
+      expect(subject.planes).to be_an_instance_of(Array)
+    end
+
+    it "#take_off should remove the plane from the airport" do
+      subject.land(plane)
+      subject.take_off(plane)
+      expect(subject.planes).to eq([])
+    end
+  end
+
+  describe "#new_day" do
+    it "changes weather" do
+      expect(subject.new_day).to eq("stormy").or eq("sunny")
+    end
+  end
+
+  describe "#weather" do
+    it "responds to #weather" do
+      expect(subject).to respond_to(:weather)
+    end
+  end
+
+  describe "#capacity" do
+    it "expects default capacity to be 20" do
+      expect(Airport::DEFAULT_CAPACITY).to eq(20)
+    end
+
+    it "should have a capacity of 20" do
+      airport = Airport.new(50)
+      expect(airport.capacity).to eq(50)
+    end
   end
 end
