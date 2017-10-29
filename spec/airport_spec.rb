1d1bef98b059ab8cd2060a205e01e488b3f33d1a
thatdania@gmail.com <thatdania@gmail.com>
2017-10-29 15:53:29 +0000

    Added test to make sure standby plane is returned

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,5 +2,16 @@ require './lib/airport.rb'
 
 describe Airport do
 it { expect(Airport.new).to respond_to(:land) }
-
+it { is_expected.to respond_to(:stand_by).with(1).argument }
 end
+
+describe Airport do
+ it 'returns landed plane' do
+  plane = Plane.new
+  subject.stand_by(plane)
+  expect(subject.plane).to eq plane
+ end
+end
+
+#
+# it { is_expected.to respond_to(:plane) }
