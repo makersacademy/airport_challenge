3b72d044ce3e556e6ec2fdcd317666fdf0a26a8c
Lewis <lewisyoul@gmail.com>
2017-10-29 15:17:22 +0000

    remove commits

--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -2,15 +2,13 @@ require 'weather'
 
 describe Weather do
   subject { Weather.new }
-  let(:conditions_array) { ["stormy", "sunny", "sunny", "sunny", "sunny"] }
+  let(:conditions_array) { [true, false, false, false, false] }
 
   it { is_expected.to be_an_instance_of(Weather) }
 
   describe "#stormy?" do
-    it { is_expected.to respond_to(:weather) }
-
     it "should return an element of conditions_array" do
-      expect(conditions_array).to include(subject.weather)
+      expect(conditions_array).to include(subject.stormy?)
     end
   end
 end
