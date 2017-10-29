bd7950c59ba7c0f34099337de533e077c06a55f7
Marie Kerkstoel <mariekerkstoel@gmail.com>
2017-10-29 11:40:07 +0000

    added capacity that can be user specified with default value of 30

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,16 +1,37 @@
 class Airport
 
+attr_accessor :planes , :capacity
+
+
+  def initialize(capacity = 30)
+    @planes = []
+    @capacity = capacity
+  end
+
+
   def land(plane, weather = "sunny")
     @weather = weather
     if @weather == "stormy"
       "Plane not able to land due to storm"
-    else 
+    elsif @full == true
+      "Airport is full"
+    else
       "#{plane} landed"
     end
   end
 
-  def take_off(plane)
+  def take_off(plane, weather = "sunny")
+    @weather = weather
+    if @weather == "stormy"
+      "Plane can not take off due to stormy weather"
+    else
     "#{plane} has taken off"
+    end
   end
+
+  def full?(response)
+    @full = response
+  end
+
 
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -8,7 +8,13 @@ describe Airport do
 
   describe "#land" do
     it "should land planes into airport" do
+      subject.full?(false)
       expect(subject.land(plane, "sunny")).to eq "#{plane} landed"
+    end
+
+    it "should not land when airport is full" do
+      subject.full?(true)
+      expect(subject.land(plane)).to eq "Airport is full"
     end
 
     it "should not allow planes to land when weather is stormy" do
@@ -19,6 +25,25 @@ describe Airport do
   describe "#take_off" do
     it "should make planes take off" do
       expect(subject.take_off(plane)).to eq ("#{plane} has taken off")
+    end
+
+    it "should not be able to take off in stormy weather" do
+      expect(subject.take_off(plane, "stormy")).to eq ("Plane can not take off due to stormy weather")
+    end
+
+  end
+
+  describe "#capacity" do
+    it "should be able to hold 30 planes" do
+      expect(subject.capacity).to eq(30)
+    end
+
+    it 'should initialize with an empty array' do
+      expect(subject.planes).to be_a Array
+    end
+
+    it "should be able to be initialized with user specified capacity" do
+      expect(Airport.new(28).capacity).to eq(28)
     end
   end
 
