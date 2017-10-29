a3b589f81ec4decfb8bce82df1fbb0e8a4ed83a1
Oleg Grushetskyy <olegfkl@gmail.com>
2017-10-29 13:34:05 +0000

    Don't allow take of or land if weather is stormy

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,15 +1,20 @@
 require_relative "plane"
 require_relative "weather"
 class Airport
+
+  attr_reader :weather
+
   def initialize
     @available = []
   end
 
   def land(plane)
+    raise "It's stormy! You can't land right now" if stormy?
     @available << plane
   end
 
   def take_off(plane)
+      raise "It's stormy! We can't fly" if stormy?
       @available.pop
   end
 
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -11,18 +11,32 @@ describe Airport do
 
   it { is_expected.to respond_to(:stormy?) }
 
+  it 'Receive report if the weather is stormy' do
+    expect(subject.stormy?).to be(true).or be(false)
+  end
+
   it 'Confirm the plane has landed' do
     plane = double(:plane)
+    allow(subject).to receive(:stormy?).and_return false
     expect(subject.land(plane)).to eq [plane]
   end
 
   it 'Confirm the plane has taken off' do
     plane = double(:plane)
+    allow(subject).to receive(:stormy?).and_return false
     subject.land(plane)
     expect(subject.take_off(plane)).to eq plane
-end
+  end
 
-it 'Receive report if the weather is stormy' do
-    expect(subject.stormy?).to be(true).or be(false)
-end
+  it "Can't take off if weather is stormy" do
+    plane = double(:plane)
+    allow(subject).to receive(:stormy?).and_return true
+    expect { subject.take_off(plane) }.to raise_error(RuntimeError,"It's stormy! We can't fly")
+  end
+
+  it "Can't land if weather is stormy" do
+    plane = double(:plane)
+    allow(subject).to receive(:stormy?).and_return true
+    expect { subject.land(plane) }.to raise_error(RuntimeError,"It's stormy! You can't land right now")
+  end
 end
