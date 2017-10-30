62564e047f9a9a0d1bdf457b355eb735afea37f6
Joe Cowton <joecowton@gmail.com>
2017-10-30 10:23:13 +0000

    rubocopped

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,56 +1,56 @@
 require 'airport'
 
 describe Airport do
-  subject(:airport) {described_class.new}
-  let (:plane) { double (:plane) }
+  subject(:airport) { described_class.new }
+  let (:plane) { double :plane }
 
   before "Set weather to not stormy" do
-      allow(subject).to receive(:stormy?) { false }
-   end
+    allow(subject).to receive(:stormy?) { false }
+  end
 
   describe "#land" do
 
     it "Should land plane at airport" do
-      expect{ subject.land(plane) }.to change{ subject.planes.include?(plane) }
+      expect { subject.land(plane) }.to change { subject.planes.include?(plane) }
     end
 
     it "Should raise error if plane has already landed" do
       subject.land(plane)
-      expect{ subject.land(plane) }.to raise_error "Plane already landed"
+      expect { subject.land(plane) }.to raise_error "Plane already landed"
     end
 
     it "Should raise error if weather is bad" do
       allow(subject).to receive(:stormy?) { true }
-      expect{ subject.land(plane) }.to raise_error "Bad weather for landing"
+      expect { subject.land(plane) }.to raise_error "Bad weather for landing"
     end
 
     it "Should raise error if airport is full" do
-      expect{ 21.times { subject.land(double :plane) } }.to raise_error "Airport full"
+      expect { 21.times { subject.land(double :plane) } }.to raise_error "Airport full"
     end
 
-end
+  end
 
   describe "#plane_take_off" do
 
     it "Should raise error if weather is bad" do
       subject.land(plane)
       allow(subject).to receive(:stormy?) { true }
-      expect{ subject.take_off(plane) }.to raise_error "Bad weather for take off"
+      expect { subject.take_off(plane) }.to raise_error "Bad weather for take off"
     end
 
     it 'Should raise error if airport is empty' do
-      expect{ subject.take_off(plane) }.to raise_error "Airport is empty"
+      expect { subject.take_off(plane) }.to raise_error "Airport is empty"
     end
 
     it 'Should raise error if airport plane is not present' do
-      plane2 = double:plane
+      plane2 = double :plane
       subject.land(plane2)
-      expect{ subject.take_off(plane) }.to raise_error "Plane not present"
+      expect { subject.take_off(plane) }.to raise_error "Plane not present"
     end
 
     it 'Should allow plane to take off if weather is good' do
       subject.land(plane)
-      expect{ subject.take_off(plane) }.to change { subject.planes.include?(plane) }
+      expect { subject.take_off(plane) }.to change { subject.planes.include?(plane) }
     end
   end
 
