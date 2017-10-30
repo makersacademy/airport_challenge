0190d6f34940e0589951f150f3a22a169a4d502c
Allan Wazacz <cazwazacz@gmail.com>
2017-10-30 23:04:16 +0000

    Adds flying status and changing function Plane class

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -1,5 +1,21 @@
 require 'plane'
 
 describe Plane do
-  
+  describe "#status" do
+    it 'new plane has status of flying set to true' do
+      expect(Plane.new.flying?).to eq(true)
+    end
+  end
+
+  describe "#change_status" do
+    it 'changes flying from true to false' do
+      subject.change_status
+      expect(subject.flying?).to eq(false)
+    end
+    it 'changes flying from false to true' do
+      subject.change_status
+      subject.change_status
+      expect(subject.flying?).to eq(true)
+    end
+  end
 end
