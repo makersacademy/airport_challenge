ce4ea9ff13bdd730f4f21546c7d80befb5292317
cristhiandas <cristhiandf94@gmail.com>
2017-10-30 09:56:38 +0000

    Refactor the test file, fixed typo in weather class

--- lib/weather.rb
+++ lib/weather.rb
@@ -3,7 +3,7 @@ class Weather
   SUNNY_PORCENTAGE = 0.7
 
   def sunny?
-    rand < UNNY_PORCENTAGE
+    rand < SUNNY_PORCENTAGE
   end
 
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -23,28 +23,24 @@ describe Airport do
     end
   end
 
-  # subject { Airport.new }
-  let(:airport) { Airport.new }
+  let(:plane) {FlyingPlane.new}
+  let(:sunnyweather) {SunnyWeather.new}
+  let(:stormyweather) {StormyWeather.new}
+  let(:landed_plane) {LandedPlane.new}
 
   context "Checks Capacity" do
-    plane = FlyingPlane.new
-    weather = SunnyWeather.new
     it "Raise an error if the Airport is full" do
-      expect { 52.times { subject.land(plane, weather.sunny?) } }.to raise_error("Cannot land, airport at capacity")
+      expect { 52.times { subject.land(plane, sunnyweather.sunny?) } }.to raise_error("Cannot land, airport at capacity")
     end
     it "Lands the planes when above capacity" do
-      expect(12.times { subject.land(plane, weather.sunny?).landed_planes.count }).to eq(12)
+      expect(12.times { subject.land(plane, sunnyweather.sunny?).landed_planes.count }).to eq(12)
     end
     it "Lands all the planes up to the capacity" do
-      expect(50.times { subject.land(plane, weather.sunny?).landed_planes.count }).to eq(50)
+      expect(50.times { subject.land(plane, sunnyweather.sunny?).landed_planes.count }).to eq(50)
     end
   end
 
   context "When trying to land a plane" do
-    landed_plane = LandedPlane.new
-    plane = FlyingPlane.new
-    stormyweather = StormyWeather.new
-    sunnyweather = SunnyWeather.new
     it { expect(subject.landed_planes).to be_a(Array) }
     it 'Land a plane that is flying' do
       subject.land(plane, sunnyweather.sunny?)
@@ -62,19 +58,16 @@ describe Airport do
   end
 
   context "When trying to takeoff" do
-    plane = FlyingPlane.new
-    weather = SunnyWeather.new
-    stormyweather = StormyWeather.new
-    it { expect(subject.land(plane, weather.sunny?).takeoff(weather.sunny?)).to be_a(FlyingPlane) }
+    it { expect(subject.land(plane, sunnyweather.sunny?).takeoff(sunnyweather.sunny?)).to be_a(FlyingPlane) }
     it "confirms that the plane is gone" do
-      subject.land(plane, weather.sunny?).takeoff(weather.sunny?)
+      subject.land(plane, sunnyweather.sunny?).takeoff(sunnyweather.sunny?)
       expect(subject.landed_planes[-1]).to eq(nil)
     end
     it "Doesn't allow takeoff when the airport is empty" do
-      expect { subject.takeoff(weather.sunny?) }.to raise_error("Cannot takeoff if there are no planes")
+      expect { subject.takeoff(sunnyweather.sunny?) }.to raise_error("Cannot takeoff if there are no planes")
     end
     it "Doesn't allow planes to takeoff if stormy" do
-      expect { subject.land(plane, weather.sunny?).takeoff(stormyweather.sunny?) }.to raise_error("Cannot land or takeoff while stormy")
+      expect { subject.land(plane, sunnyweather.sunny?).takeoff(stormyweather.sunny?) }.to raise_error("Cannot land or takeoff while stormy")
     end
   end
 
