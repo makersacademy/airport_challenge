45dd345130a193703ebb2bb7e7befaa26e4f866a
antonio <antoniobelmardacosta@gmail.com>
2017-10-30 22:20:55 +0000

    Changes the way weather is calculated so that tests don't include set_instance_variable

--- lib/weather.rb
+++ lib/weather.rb
@@ -1,15 +1,9 @@
 class Weather
 
-  attr_reader :storm_chance
-
   STORM_DANGER = 0.9
 
-  def initialize
-    @storm_chance = rand
-  end
-
   def stormy?
-    @storm_chance > STORM_DANGER
+    rand > STORM_DANGER
   end
 
 end
--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -8,21 +8,17 @@ describe Weather do
       expect(subject).to be_an_instance_of(Weather)
     end
 
-    it "when initiated stores a weather variable with a float" do
-      expect(subject.storm_chance).to be_kind_of(Float)
-    end
-
   end
 
   describe "stormy?" do
 
     it "expects stormy to return true when storm_chance>0.9" do
-      subject.instance_variable_set(:@storm_chance, 1)
+      allow(subject).to receive(:rand).and_return(1)
       expect(subject.stormy?).to eq true
     end
 
     it "expects stormy to return false when storm_chance<=0.9" do
-      subject.instance_variable_set(:@storm_chance, 0.5)
+      allow(subject).to receive(:rand).and_return(0.5)
       expect(subject.stormy?).to eq false
     end
 
