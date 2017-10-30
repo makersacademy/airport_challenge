ef303c6f6ce273170bf9e755fe72d1fe403f342c
Tabish Raza <tabishraza@gmail.com>
2017-10-30 10:05:10 +0000

    replacement for stub method to more elegant solution

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -6,38 +6,29 @@ describe Airport do
   let(:plane) { double :plane }
 
   it "confirm plane has landed" do
-    Airport.stub(:weather) do |w|
-      expect(airport.land(plane)).to eq [plane] if w_is true
-    end
+    allow(airport).to receive(:weather) { false }
+    expect(airport.land(plane)).to eq [plane]
   end
 
   it "confirm plane has taken off" do
-    Airport.stub(:weather) do |w|
-      if w_is true
-        airport.land(plane)
-        expect(airport.take_off(plane)).to eq [plane]
-      end
-    end
+    allow(airport).to receive(:weather) { false }
+    airport.land(plane)
+    expect(airport.take_off(plane)).to eq plane
   end
 
   it "if stormy, plane does NOT take off" do
-    Airport.stub(:weather) do |w|
-      expect(airport.take_off(plane)).to eq "The skies are dark, best to stay on the ground" if w_is true
-    end
+    allow(airport).to receive(:weather) { true }
+    expect(airport.take_off(plane)).to eq "The skies are dark, best to stay on the ground"
   end
 
   it "If stormy, plane does NOT land" do
-    Airport.stub(:weather) do |w|
-      expect(airport.land(plane)).to eq "Got a storm front coming, cannot land here" if w_is true
-    end
+    allow(airport).to receive(:weather) { true }
+    expect(airport.land(plane)).to eq "Got a storm front coming, cannot land here"
   end
 
   it "raises an error if terminal is full" do
-    Airport.stub(:weather) do |w|
-      if w_is true
-        Airport::DEFAULT_CAPACITY.times { airport.land(plane) }
-        expect { airport.land(plane) }.to raise_error("Airport is full")
-      end
-    end
+    allow(airport).to receive(:weather) { false }
+    Airport::DEFAULT_CAPACITY.times { airport.land(plane) }
+    expect { airport.land(plane) }.to raise_error("Airport is full")
   end
 end
