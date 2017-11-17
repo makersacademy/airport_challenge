2aa35e408ccb4f3343fe60334b0b4ae68aab8f23
Yuliya Nedyalkova <yuliya.nedyalkova777@gmail.com>
2017-11-17 17:52:12 +0000

    Adjusted Plane mocks and created a To-Do for future reference.

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,4 +1,4 @@
-require 'plane'
+require_relative 'plane'
 
 # manages landing and leaving planes. Takes information from Weather class to
 # decide whether a plane should land or take off.
@@ -8,7 +8,8 @@ class Airport
     landed_plane = plane.land
     confirm(landed_plane)
   end
-
+  # TODO try to add an if clause to line 14 - 'if plane.landed?'
+  # TODO and figure out why such clause returns a nil.
   def confirm(plane)
     p "The plane #{plane.object_id} has landed."
   end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,14 +2,18 @@ require 'airport'
 
 describe Airport do
   let(:flying_plane) { double :plane }
+  let(:parked_plane) { double :plane }
 
   before(:each) do
     allow(flying_plane).to receive(:land)
+    allow(flying_plane).to receive(:landed?).and_return(:true)
+
   end
 
-  context 'landing of a plane' do
-    it 'can land a plane' do
+  context 'when landing of a plane' do
+    it 'lands a plane' do
       subject.land(flying_plane)
+      expect(flying_plane).to be_landed
     end
 
     it 'confirms the plane has landed' do
