199f74a304aaa0f2a019ca11840526e9a449f0cb
Yuliya Nedyalkova <yuliya.nedyalkova777@gmail.com>
2017-11-20 18:58:40 +0000

    Small refactoring for better readability

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -4,14 +4,14 @@ require 'weather'
 describe Airport do
   let(:flying_plane) { double :plane }
   let(:parked_plane) { double :plane }
-  let(:stormy_weather) { double :weather}
-  let(:sunny_weather) { double :weather}
+  let(:stormy_weather) { double :weather }
+  let(:sunny_weather) { double :weather }
 
   before(:each) do
-    allow(flying_plane).to receive(:land).and_return(flying_plane)
-    allow(flying_plane).to receive(:landed?).and_return(true)
-    allow(parked_plane).to receive(:take_off).and_return(parked_plane)
-    allow(parked_plane).to receive(:landed?).and_return(false)
+    allow(flying_plane).to receive(:land).and_return(parked_plane)
+    allow(parked_plane).to receive(:landed?).and_return(true)
+    allow(parked_plane).to receive(:take_off).and_return(flying_plane)
+    allow(flying_plane).to receive(:landed?).and_return(false)
     allow(stormy_weather).to receive(:stormy?).and_return(true)
     allow(sunny_weather).to receive(:stormy?).and_return(false)
   end
@@ -19,7 +19,7 @@ describe Airport do
   context 'when landing of a plane' do
     it 'lands a plane' do
       subject.land(flying_plane, sunny_weather)
-      expect(flying_plane).to be_landed
+      expect(parked_plane).to be_landed
     end
     it 'does not allow a plane to land if the weather is stormy' do
       message = 'Landing denied due to stormy weather'
