3202dd8919542b2f8c2291d1433d5821aff22280
Eleanor Wintram <eleanorwintram@gmail.com>
2017-11-25 09:00:50 +0000

    add default and settable capacity

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,20 +1,29 @@
 require_relative "plane"
 
 class Airport
-  attr_reader :planes
+  attr_reader :planes, :capacity
+  DEFAULT_CAPACITY = 10
 
-  def initialize
+  def initialize(capacity = DEFAULT_CAPACITY)
     @planes = []
+    @capacity = capacity
   end
 
   def land(plane)
     fail "Plane already in aiport" if @planes.include?(plane)
+    fail "Airport full" if full?
     @planes << plane
   end
 
   def take_off(plane)
     fail "Plane not in airport" if !@planes.include?(plane)
     @planes = @planes - [plane]
+  end
+
+private
+
+  def full?
+     @planes.length == @capacity
   end
 
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,6 +2,16 @@ require "airport"
 
 describe Airport do
 
+  describe "#initialize" do
+    it "should ensure that the default capacity is set to 10" do
+      expect(subject.capacity).to eq 10
+    end
+
+    it "should allow the user to set the capacity" do
+      expect(Airport.new(20).capacity).to eq 20
+    end
+  end
+
   describe "#land" do
     it {is_expected.to respond_to(:land).with(1).argument}
 
@@ -12,6 +22,11 @@ describe Airport do
     it "should raise an error if the plane is already in the airport" do
       subject.land(plane = Plane.new)
       expect {subject.land(plane)}.to raise_error("Plane already in aiport")
+    end
+
+    it "should raise an error if the aiport is full" do
+      Airport::DEFAULT_CAPACITY.times {subject.land(plane = Plane.new)}
+      expect {subject.land(Plane.new)}.to raise_error("Airport full")
     end
   end
 
