b6a66217db16a3a5e208b3a34f65c88c27a3d477
Rhys Kyte <rhyskyte@Rhyss-MacBook-Pro.local>
2017-11-25 19:07:09 +0000

    planes will not land if weather is stormy

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,13 +1,15 @@
 require_relative 'plane'
 
 class Airport
-  attr_accessor :planes
+  attr_accessor :planes, :stormy
 
   def initialize
     @planes = []
+    @stormy = false
   end
 
   def land(plane)
+    raise "Currently unsafe to land plane" if stormy == true
     plane.landing
     planes << plane
   end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,13 +1,18 @@
 require 'airport'
 
 describe Airport do
-  let(:plane) {double(:my_plane, landing: true, taking_off: true)}
+  let(:plane) { double(:my_plane, landing: true, taking_off: true) }
 
   describe "#land" do
 
     it "should land a plane" do
       subject.land(plane)
       expect(subject.planes.include?(plane)).to be true
+    end
+
+    it "should not land a plane if the weather is stormy" do
+      subject.stormy = true
+      expect { subject.land(plane) }.to raise_error "Currently unsafe to land plane"
     end
 
   end
