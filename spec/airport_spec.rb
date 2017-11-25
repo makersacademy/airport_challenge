5c2e5a5a667d9dae195893961369340cc15dcdd1
James Lizamore <jwlizamore@gmail.com>
2017-11-25 18:10:50 +0000

    organized and added some tests

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -4,17 +4,16 @@ describe Airport do
   let(:plane) { double(:plane, takeoff: nil) }
   describe '#depart' do
     it 'can depart a plan' do
-      subject.depart(plane)
-      expect(subject.find_plane(plane)).to be(false)
+      expect(subject).to respond_to(:depart).with(1).argument
     end
-    it 'raises error when plane not departed' do
-      subject.depart(plane)
-      subject.planes << plane
-      #expect
-    end
-    it 'can receive a plane' do
+    it 'confirms plane has departed' do
       subject.receive(plane)
-      expect(subject.planes.length).to eq(1)
+      subject.depart(plane)
+      expect(subject.find_plane(plane)).to be false
     end
+  end
+  it 'can receive a plane' do
+    subject.receive(plane)
+    expect(subject.planes.length).to eq(1)
   end
 end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -2,14 +2,22 @@ require 'plane'
 
 describe Plane do
   let(:airport) { double :airport }
-  it 'Can land' do
-    expect(subject).to respond_to(:land).with(1).argument
+  describe '#land' do
+    it 'Can land' do
+      expect(subject).to respond_to(:land).with(1).argument
+    end
+    it 'can not land when grounded' do
+      expect { subject.land(airport) }.to raise_error(RuntimeError, "Already grounded")
+    end
   end
-  it 'Can takeoff' do
-    subject.takeoff
-    expect(subject.grounded).to be false
-  end
-  it 'can not land when grounded' do
-    expect { subject.land(airport) }.to raise_error(RuntimeError, "Already grounded")
+  describe '#takeoff' do
+    it 'Can takeoff' do
+      subject.takeoff
+      expect(subject.grounded).to be false
+    end
+    it 'can not takeoff when flying' do
+      subject.takeoff
+      expect { subject.takeoff }.to raise_error(RuntimeError, "Already flying")
+    end
   end
 end
