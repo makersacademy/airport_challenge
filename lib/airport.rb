a38443acf6be4a5f2bd3cad6fe58c6160da6a031
Rhys Kyte <rhyskyte@Rhyss-MacBook-Pro.local>
2017-11-25 15:35:16 +0000

    airports can now tell planes to take off

--- lib/airport.rb
+++ lib/airport.rb
@@ -4,8 +4,13 @@ class Airport
   def initialize
     @planes = []
   end
-  
+
   def land(plane)
     planes << plane
   end
+
+  def takeoff(plane)
+    planes.delete(plane)
+  end
+  
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,10 +1,21 @@
 require 'airport'
+
 describe Airport do
+
   describe "#land" do
-    it "should accept a plane landing" do
-      plane = double(:my_plane)
+    it "should land a plane" do
+      plane = double(:my_landing_plane)
       subject.land(plane)
       expect(subject.planes.include?(plane)).to be true
+    end
+  end
+
+  describe "#takeoff" do
+    it "should make a plane take off" do
+      plane = double(:my_takeoff_plane)
+      subject.land(plane)
+      subject.takeoff(plane)
+      expect(subject.planes.include?(plane)).to be false
     end
   end
 end
