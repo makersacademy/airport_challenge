47fe034f58d73b98e724f549fa4496d83e7f53b6
Keran <kerbraich@gmail.com>
2017-11-26 11:11:19 +0000

    add airport capacity fail

--- lib/airport.rb
+++ lib/airport.rb
@@ -2,19 +2,22 @@ require_relative "plane"
 require_relative "weather"
 
 class Airport
+  CAPACITY = 10
   attr_reader :hangar
 
   def initialize
     @hangar = []
+    @capacity = CAPACITY
   end
 
   def land(plane)
+    fail "Airport at full capacity, plane unable to land" if full?
     @hangar << plane
     "Plane has landed"
   end
 
   def take_off
-    fail "No plane for take off" if hangar.empty?
+    fail "No plane for take off" if empty?
     @hangar.pop
     "Plane has taken off"
   end
@@ -23,5 +26,9 @@ class Airport
 
   def empty?
     hangar.length.zero? ? true : false
+  end
+
+  def full?
+    hangar.length >= @capacity ? true : false
   end
 end
