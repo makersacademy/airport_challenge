acbd99e06cc863c41f9ef468469c7a048812fdf3
Keran <kerbraich@gmail.com>
2017-11-26 18:21:07 +0000

    add test to raise error if plane already taken off

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -10,11 +10,15 @@ describe Airport do
       plane = Plane.new
       expect(subject.land(plane)).to eq "Plane has landed"
     end
-    it "raises an error if plane cannot land because airport at default capacity" do
+    it "raises an error if plane has already landed" do
       plane = Plane.new
-      Airport::DEFAULT_CAPACITY.times { subject.land(plane) }
+      subject.land(plane)
+      expect { subject.land(plane) }.to raise_error "Plane already landed"
+    end
+    it "raises an error if plane cannot land because airport at default capacity" do
+      Airport::DEFAULT_CAPACITY.times { subject.land(Plane.new) }
       message = "Airport at full capacity, plane unable to land"
-      expect { subject.land(plane) }.to raise_error message
+      expect { subject.land(Plane.new) }.to raise_error message
     end
     it "checks if airport capacity is default capacity" do
       expect(subject.capacity).to eq Airport::DEFAULT_CAPACITY
@@ -24,11 +28,10 @@ describe Airport do
       expect(subject.capacity). to eq 5
     end
     it "raises an error if airport full with non-default capacity" do
-      plane = Plane.new
       subject.capacity = 5
-      subject.capacity.times { subject.land(plane) }
+      subject.capacity.times { subject.land(Plane.new) }
       message = "Airport at full capacity, plane unable to land"
-      expect { subject.land(plane) }.to raise_error message
+      expect { subject.land(Plane.new) }.to raise_error message
     end
     it "doesn't allow plane to land due to stormy weather" do
       plane = Plane.new
@@ -42,10 +45,19 @@ describe Airport do
     it "allows the plane to take off" do
       plane = Plane.new
       subject.land(plane)
-      expect(subject.take_off).to eq "Plane has taken off"
+      expect(subject.take_off(plane)).to eq "Plane has taken off"
     end
-    it "raises an error if there is no plane for take off" do
-      expect { subject.take_off }.to raise_error "No plane for take off"
+    it "raises an error if plane has already taken off" do
+      plane = Plane.new
+      subject.land(plane)
+      subject.take_off(plane)
+      expect { subject.take_off(plane) }.to raise_error "Plane already taken off"
+    end
+    it "confirms plane taken off is no longer in hangar" do
+      plane = Plane.new
+      subject.land(plane)
+      subject.take_off(plane)
+      expect(subject.hangar).not_to include plane
     end
     it "confirms plane is no longer in airport" do
       expect(subject.hangar).to be_empty
@@ -55,7 +67,7 @@ describe Airport do
       subject.land(plane)
       allow(subject).to receive(:stormy?).and_return(true)
       message = "Stormy weather, plane unable to take off"
-      expect { subject.take_off }.to raise_error message
+      expect { subject.take_off(plane) }.to raise_error message
     end
   end
 
