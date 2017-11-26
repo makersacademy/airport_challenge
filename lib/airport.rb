3c3b6343dc8afe754b00b1f87f4d6869c59ed100
Keran <kerbraich@gmail.com>
2017-11-26 18:19:58 +0000

    add fail if plane has already taken off

--- lib/airport.rb
+++ lib/airport.rb
@@ -13,24 +13,21 @@ class Airport
   end
 
   def land(plane)
-    fail "Stormy weather, plane unable to land" if stormy?
+    fail "Plane already landed" if @hangar.include?(plane)
     fail "Airport at full capacity, plane unable to land" if full?
+    fail "Stormy weather, plane unable to land" if stormy?
     @hangar << plane
     "Plane has landed"
   end
 
-  def take_off
+  def take_off(plane)
+    fail "Plane already taken off" unless @hangar.include?(plane)
     fail "Stormy weather, plane unable to take off" if stormy?
-    fail "No plane for take off" if empty?
-    @hangar.pop
+    @hangar.delete(plane)
     "Plane has taken off"
   end
 
   protected
-
-  def empty?
-    hangar.length.zero? ? true : false
-  end
 
   def full?
     hangar.length >= @capacity ? true : false
