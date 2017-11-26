6e5ab778b6eefab574887dd5a50872ed8bfd0cee
Nikolaos Sakellarios <lunaticnick@hotmail.com>
2017-11-26 10:45:11 +0000

    Tests for docking and launching a plane as well as plane count in airport

--- lib/airport.rb
+++ lib/airport.rb
@@ -5,10 +5,15 @@ class Airport
   end
 
   def dock(plane)
+    @planes << plane
   end
 
   def launch(plane)
+    @planes.delete(plane)
   end
 
+  def count_of_planes
+    @planes.count
+  end
 
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,15 +1,44 @@
 require "airport"
+require "plane"
 
 describe Airport do
-
+  # Name the subject explicitly
+      subject(:airport) {described_class.new}
+      
   # Tests for Airport responding to methods
 
      it "plane can land" do
        expect(subject).to respond_to (:dock)
      end
 
+     it "plane can land" do
+
+       expect(subject).to respond_to (:dock)
+     end
+
      it "plane can take off" do
        expect(subject).to respond_to (:launch)
+     end
+
+     it "airport log how many planes are docked" do
+       expect(subject).to respond_to (:count_of_planes)
+     end
+
+  # Test for docking and launching a plane and checking
+  # how many planes are in the airport at any given time
+
+     it "a plane lands" do
+       # airport = Airport.new  --- Omitted as we have used subject(:airport) {described_class.new}
+       plane = Plane.new
+       airport.dock(plane)
+       expect(airport.count_of_planes).to eq 1
+     end
+
+     it "a plane takes off" do
+       # airport = Airport.new  --- Omitted as we have used subject(:airport) {described_class.new}
+       plane = Plane.new
+       airport.launch(plane)
+       expect(airport.count_of_planes).to eq 0
      end
 
 
