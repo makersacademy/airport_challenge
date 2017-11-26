7e84d8613215be016ceaa499a2839853d90c3ee8
George Whiting <george_whiting@hotmail.com>
2017-11-26 19:14:24 +0000

    planes must be on the runway before takeoff, and must be expected in order to land

--- lib/plane.rb
+++ lib/plane.rb
@@ -9,10 +9,14 @@ class Plane
   end
 
   def land(airport)
+    raise "The airport is not expecting that plane" if !airport.plane_expected?(self)
+    raise "Plane is already on the ground" if @status != "In the air"
     @status = airport
   end
 
   def takeoff
+    raise "Plane is already in the air" if @status == "In the air"
+    raise "The plane is not ready to take off" if !@status.on_runway?(self)
     @status = "In the air"
   end
 
