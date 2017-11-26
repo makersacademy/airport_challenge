af812b918915eaae63debed4eaeb3a84a50f1717
George Whiting <george_whiting@hotmail.com>
2017-11-26 19:12:44 +0000

    adds ability to expect incoming planes and send grounded planes to the runway for takeoff

--- lib/airport.rb
+++ lib/airport.rb
@@ -9,12 +9,31 @@ class Airport
   end
 
   def land(plane)
+    expect_plane(plane)
     @planes << plane
   end
 
-  def takeoff(plane)
+  def allow_takeoff(plane)
     raise "That plane does not exist" unless @planes.include?(plane)
     puts "Plane #{plane} has departed"
     @planes.delete(plane)
   end
+
+  def plane_expected?(plane)
+    plane == @expected_plane
+  end
+
+  def expect_plane(plane)
+    @expected_plane = plane
+  end
+
+  def on_runway?(plane)
+    plane == @on_runway
+  end
+
+  def send_to_runway(plane)
+    @on_runway = plane
+  end
+
+
 end
