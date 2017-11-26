d1ef00c02a161a73f7e65c4a1a0bf1d70446f4ea
umairb1 <umairbashir95@hotmail.com>
2017-11-26 22:03:49 +0000

    Test created: Planes cannot take off if weather is stormy

--- lib/airport.rb
+++ lib/airport.rb
@@ -2,10 +2,12 @@ require './lib/plane.rb'
 class Airport
   attr_reader :capacity
   attr_reader :planes_at_airport
+  attr_reader :weather
 
   def initialize(capacity = 3)
     @capacity = capacity
     @planes_at_airport = []
+    @weather = ['Sunny', 'Snow', 'Stormy'].sample
   end
 
   def land_plane(plane)
@@ -13,9 +15,15 @@ class Airport
     @planes_at_airport.last
   end
 
+  def stormy?
+    @weather == 'Stormy'
+  end
+
   def take_off(plane)
+    raise('Cannot take off weather is Stormy') if stormy?
     @planes_at_airport.delete(plane)
     @planes_at_airport.last
     @planes_at_airport
   end
+
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,7 +1,8 @@
 require "airport.rb"
 require "plane.rb"
 describe Airport do
-  plane = Plane.new
+  let(:airport) {Airport.new}
+  let(:plane) {Plane.new}
 
   describe '#land_plane(plane)' do
     it "should allow the user to set capacity for the airport" do
@@ -12,6 +13,12 @@ describe Airport do
   describe '#take_off(plane)' do
     it "should be able to tell plane to take off" do
       expect(subject.take_off(plane)).to eq subject.planes_at_airport
+    end
+
+    it "should be able to pervent take off if weather is stormy" do
+      allow(subject).to receive(:weather).and_return(@weather = 'Stormy')
+      allow(subject).to receive(:stormy?).and_return(true)
+      expect { subject.take_off(plane) }.to raise_error('Cannot take off weather is Stormy')
     end
   end
 end
