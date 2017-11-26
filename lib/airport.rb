769962aed5bb0dde9dfe873fa420f985ff7e4cd1
newtdogg <edwardfrancisnewton@live.com>
2017-11-26 12:00:34 +0000

    passes the first 2 tests, allowing plane to land and take off

--- lib/airport.rb
+++ lib/airport.rb
@@ -3,6 +3,10 @@ class Airport
     @planes_landed = []
   end
   def land plane
+    @planes_landed << plane
+  end
+  def take_off planes
+    @planes_landed.pop
   end
   def planes_landed
     @planes_landed
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -5,12 +5,16 @@ describe Airport do
     airport = Airport.new
     plane = double(:plane)
     airport.land(plane)
-    expect(airport.planes_landed).to eq 1
+    expect(airport.planes_landed.count).to eq 1
   end
 
-  # it "allows a plane to take off" do
-  #
-  # end
+  it "allows a plane to take off" do
+    airport = Airport.new
+    plane = double(:plane)
+    airport.land(plane)
+    airport.take_off(:plane)
+    expect(airport.planes_landed.count).to eq 0
+  end
   #
   # it "allows a plane to take off then removes it from the airport" do
   #
