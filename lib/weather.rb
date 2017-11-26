a03a09875ccb25076960820d96c5ea8305a29d21
Lan Pham <lanpham312@gmail.com>
2017-11-26 23:23:51 +0000

    Updating README with my steps

--- lib/weather.rb
+++ lib/weather.rb
@@ -3,6 +3,7 @@ class Weather
   attr_reader :stormy
 
   def stormy?
-    @stormy = [true, false, false].sample
+    # @stormy = [true, false, false].sample
+    rand(5) == 1 ? true : false
   end
 end
--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -3,7 +3,6 @@ require "weather"
 describe Weather do
   describe "#stormy" do
     it "returns true or false for a randomised weather condition" do
-      p subject.stormy?
       expect(subject.stormy?).to eq(true).or(eq(false))
     end
   end
