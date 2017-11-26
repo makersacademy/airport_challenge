79f488aa50542489940f3415cb68eaad2da7f925
Keran <kerbraich@gmail.com>
2017-11-26 17:28:43 +0000

    add fail for take off in stormy weather

--- lib/airport.rb
+++ lib/airport.rb
@@ -9,15 +9,18 @@ class Airport
   def initialize
     @hangar = []
     @capacity = DEFAULT_CAPACITY
+    @weather = Weather.new
   end
 
   def land(plane)
+    fail "Stormy weather, plane unable to land" if stormy?
     fail "Airport at full capacity, plane unable to land" if full?
     @hangar << plane
     "Plane has landed"
   end
 
   def take_off
+    fail "Stormy weather, plane unable to take off" if stormy?
     fail "No plane for take off" if empty?
     @hangar.pop
     "Plane has taken off"
@@ -31,5 +34,9 @@ class Airport
 
   def full?
     hangar.length >= @capacity ? true : false
+  end
+
+  def stormy?
+    @weather.current_weather == :stormy ? true : false
   end
 end
