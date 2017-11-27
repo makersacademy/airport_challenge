32645928a65e82861bda0c166eceeec73e82e349
Rhys Kyte <rhyskyte@Rhyss-MacBook-Pro.local>
2017-11-27 10:54:46 +0000

    spelling change

--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -3,7 +3,7 @@ require 'weather'
 describe Weather do
   subject(:weather) { described_class.new }
 
-  it "should store weather information" do
+  it "should give weather information" do
     expect(weather).to respond_to(:stormy?)
   end
 
