f22e651c1baa2e1eeb3c3dc93ae0c5dae3cee0e6
Rhys Kyte <rhyskyte@Rhyss-MacBook-Pro.local>
2017-11-27 11:10:02 +0000

    refactored plane specs

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -4,17 +4,13 @@ describe Plane do
   let(:airport) { double(:my_airport) }
 
   it "should know when it is flying" do
-    allow(airport).to receive(:takeoff) do |plane|
-      plane.taking_off
-    end
+    allow(airport).to receive(:takeoff) { |plane| plane.taking_off }
     airport.takeoff(subject)
     expect(subject.flying).to eq true
   end
 
   it "should know when it is landed" do
-    allow(airport).to receive(:land) do |plane|
-      plane.landing
-    end
+    allow(airport).to receive(:land) { |plane| plane.landing }
     airport.land(subject)
     expect(subject.flying).to eq false
   end
