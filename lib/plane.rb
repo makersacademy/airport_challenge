f5342ed9bc12f9e52faee42e3741c8bc5e981381
rcvink <rrvink@gmail.com>
2018-01-06 19:22:48 +0000

    Add plane takeoff

--- lib/plane.rb
+++ lib/plane.rb
@@ -1,7 +1,25 @@
 require_relative 'airport'
 
 class Plane
-  def land(airport)
-    airport.planes.push(self)
+  attr_reader :airport
+
+  def initialize
+    @airport = nil
+  end
+
+  def land(target_airport)
+    raise 'Plane is already landed' if is_landed?
+    target_airport.planes.push(self)
+    @airport = target_airport
+  end
+
+  def take_off
+    raise 'Plane is already airborne' unless is_landed?
+    @airport.planes.delete(self)
+    @airport = nil
+  end
+
+  def is_landed?
+    @airport.is_a?(Airport)
   end
 end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -5,9 +5,34 @@ describe Plane do
   it 'can create instances of Plane class' do
     expect(subject).to be_instance_of(Plane)
   end
+
   it 'can land at an airport' do
     airport = Airport.new
     subject.land(airport)
     expect(airport.planes[0]).to eq(subject)
+  end
+
+  it 'can take off from an airport' do
+    airport = Airport.new
+    subject.land(airport)
+    subject.take_off
+    expect(airport.planes).to be_empty
+  end
+
+  it 'knows whether it is airborne or landed' do
+    expect(subject.is_landed?).to be false
+    airport = Airport.new
+    subject.land(airport)
+    expect(subject.is_landed?).to be true
+  end
+
+  it 'fails when trying to land a landed plane' do
+    airport = Airport.new
+    subject.land(airport)
+    expect{subject.land(airport)}.to raise_error
+  end
+
+  it 'fails when trying to take off an airborne plane' do
+    expect{subject.take_off}.to raise_error
   end
 end
