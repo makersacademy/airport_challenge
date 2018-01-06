ba7d14dbc93e3b7f69d76403e08631b9adaf6c01
Tom Grand <thomas.a.grand@gmail.com>
2018-01-06 18:40:19 +0000

    add test for land method

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -2,8 +2,8 @@ require_relative "../lib/plane"
 
 describe Plane do 
 
-#  it "should respond to land instruction" do
- #   expect(subject).to respond_to?(:land)
-  #end
+  it "should respond to land instruction" do
+    expect(subject).to respond_to?(:land)
+  end
 
 end
