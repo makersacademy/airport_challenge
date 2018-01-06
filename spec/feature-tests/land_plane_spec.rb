078d2a0cab5fb7f65094e3ab84ef55bf14274f24
domvernon <3686359+domvernon@users.noreply.github.com>
2018-01-06 14:22:13 +0000

    updated feature to make sure planes can be viewed at airport

--- spec/feature-tests/land_plane_spec.rb
+++ spec/feature-tests/land_plane_spec.rb
@@ -2,9 +2,13 @@ require 'airport'
 require 'plane'
 
 describe 'Landing a plane' do
+  airport = Airport.new
+  plane = Plane.new
   it 'should be able to land a plane' do
-    airport = Airport.new
-    plane = Plane.new
     expect{ airport.land plane }.to_not raise_error
+  end
+  it 'should be able to display landed planes' do
+    airport.land plane
+    expect( airport.planes ).to eq [plane]
   end
 end
