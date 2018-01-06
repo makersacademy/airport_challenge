695a9a4eaa9c0d5dcb974d60cdb9784f1aa897c6
Leigh-ann <leighann.gant@gmail.com>
2018-01-06 12:56:42 +0000

    Tests now pass, plane cannot land if airport full

--- lib/airport.rb
+++ lib/airport.rb
@@ -7,12 +7,13 @@ class Airport
   end
 
   def land(plane)
-    fail 'Unable to land - weather is stormy' if @weather.stormy?
-    @planes << plane
+    fail 'Unable to land - weather is stormy' if weather.stormy?
+    fail 'Airport at full capacity' if !planes.empty?
+    planes << plane
   end
 
   def take_off(plane)
-    fail 'Unable to take off - weather is stormy' if @weather.stormy?
-    @planes.delete(plane)
+    fail 'Unable to take off - weather is stormy' if weather.stormy?
+    planes.delete(plane)
   end
 end
