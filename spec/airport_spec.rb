957897ca38b993b4339149b3edf8d6c46e866f1c
rcvink <rrvink@gmail.com>
2018-01-06 17:10:48 +0000

    Initial commit for barebones functionality

---
+++ spec/airport_spec.rb
@@ -0,0 +1,7 @@
+require 'airport'
+
+describe Airport do
+  it 'can create instances of Airport class' do
+    expect(subject).to be_instance_of(Airport)
+  end
+end
---
+++ spec/plane_spec.rb
@@ -0,0 +1,7 @@
+require 'plane'
+
+describe Plane do
+  it 'can create instances of Plane class' do
+    expect(subject).to be_instance_of(Plane)
+  end
+end
