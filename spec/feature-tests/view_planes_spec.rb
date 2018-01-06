81d2fbee9fccb9751295b15455dd39d1a996b143
domvernon <3686359+domvernon@users.noreply.github.com>
2018-01-06 15:19:03 +0000

    landing more than one plane should store them in an array

--- spec/feature-tests/view_planes_spec.rb
+++ spec/feature-tests/view_planes_spec.rb
@@ -4,8 +4,10 @@ require 'plane'
 describe 'Viewing the plane(s)' do
   airport = Airport.new
   plane = Plane.new
-  it 'should be able to display a landed plane' do
-    airport.land plane
-    expect( airport.planes ).to eq plane
+  it 'should be able to display landed planes' do
+    n_planes = rand(10)
+    expected_planes = Array.new(n_planes, plane)
+    n_planes.times { airport.land plane }
+    expect( airport.planes ).to eq expected_planes
   end
 end
