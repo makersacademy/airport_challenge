4de07086a6ffd7cb130b6d6ddb153702794c600e
Benjamin Pourian <b.pourian@hotmail.co.uk>
2018-01-06 22:06:37 +0000

    Created classes aiport and plance with tests

---
+++ spec/airport_spec.rb
@@ -0,0 +1,8 @@
+require 'Airport'
+
+
+describe Airport do
+  it "creates a new instance" do
+    expect(subject).to be_a Airport
+  end
+end
---
+++ spec/plane_spec.rb
@@ -0,0 +1,7 @@
+require 'Plane'
+
+describe Plane do
+  it "creates a new instance" do
+    expect(subject).to be_a Plane
+  end
+end
