218fd2db55b54183e96a2ef2f550acacdb03e416
GiadaSimonetti <21024993+GiadaSimonetti@users.noreply.github.com>
2018-01-06 21:36:34 +0000

    Plane can take off

--- lib/plane.rb
+++ lib/plane.rb
@@ -1,9 +1,16 @@
-# As an air traffic controller
-# So I can get passengers to a destination
-# I want to instruct a plane to land at an airport
 
 class Plane
+  # As an air traffic controller
+  # So I can get passengers to a destination
+  # I want to instruct a plane to land at an airport
   def land
-    p "We are landing"
+    "We are landing"
+  end
+
+  # As an air traffic controller
+  # So I can get passengers on the way to their destination
+  # I want to instruct a plane to take off from an airport and confirm that it is no longer in the airport
+  def take_off
+    "We are taking off"
   end
 end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -1,13 +1,28 @@
-# As an air traffic controller
-# So I can get passengers to a destination
-# I want to instruct a plane to land at an airport
 require "plane"
 
 describe Plane do
+  # As an air traffic controller
+  # So I can get passengers to a destination
+  # I want to instruct a plane to land at an airport
   it { is_expected.to respond_to(:land) }
 
-  it "can land" do
+  it "plane lands" do
     expect(subject.land).to eq "We are landing"
+  end
+
+  # As an air traffic controller
+  # So I can get passengers on the way to their destination
+  # I want to instruct a plane to take off from an airport and confirm that it is no longer in the airport
+
+  # it { is_expected.to respond_to(:take_off) }
+
+  describe "#take_off" do
+
+    it "plane takes off and it is no longer in the airport" do
+      airport = Airport.new
+      # let(:airport) { Airport.new }
+      expect(subject.take_off).to eq "We are taking off"
+    end
   end
 
 end
