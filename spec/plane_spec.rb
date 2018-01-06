d099c7094651097b1b143dc3264d8b4d2a825e9e
Tom Grand <thomas.a.grand@gmail.com>
2018-01-06 19:08:31 +0000

    add land method to plane

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -5,6 +5,7 @@ describe Plane do
   let(:airport)	{ double("airport") }
   
   describe "#land" do
+   
     it "should respond to land in an airport" do
       expect(subject).to respond_to(:land).with(1).argument
     end
