4549c78ca39ff6000ebb3d775d8f1f02e34d0ec7
Ed Perkins <ed@edperkins.co.uk>
2018-01-06 14:48:21 +0000

    test plane take off and confirm and pass

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,8 +1,12 @@
-require 'Plane'
+require 'plane'
 
 class Airport
 
   def land(plane)
+  end
+
+  def take_off(plane)
+    "#{plane} is no longer in the aiport"
   end
 
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,4 +1,4 @@
-require 'Airport'
+require 'airport'
 
 describe Airport do
 
@@ -6,4 +6,8 @@ describe Airport do
     it { is_expected.to respond_to(:land).with(1).argument }
   end
 
+  it "instructs a plane to take off and confirms that it is no longer in the airport" do
+    plane = Plane.new
+    expect(subject.take_off(plane)).to eq "#{plane} is no longer in the aiport"
+  end
 end
