38c41ae28121dac3d6f97336c34a5c3751cc7757
Alexy <alexandra.fina@yahoo.com>
2018-01-06 18:28:02 +0000

    Allows plane to take off from airport

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -8,4 +8,12 @@ describe Airport do
     airport.land(plane)
     expect(airport).to respond_to(:land).with(1).argument
   end
+
+  it 'instructs a plane to take off from an airport' do
+    airport = Airport.new
+    plane = Plane.new
+    airport.take_off(plane)
+    expect(airport).to respond_to(:take_off).with(1).argument
+  end
+
 end
