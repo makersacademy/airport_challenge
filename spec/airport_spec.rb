497d4ceba33bea985c8f43c6d50303964a9211e1
Leigh-ann <leighann.gant@gmail.com>
2018-01-06 09:42:15 +0000

    Tests refactored to use doubles

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,12 +1,14 @@
 require 'airport'
 
 describe Airport do
+  subject(:airport) {Airport.new}
+  let(:plane){double :plane}
+
   it {is_expected.to respond_to(:land).with(1).argument}
   describe '#land' do
     it 'stores plane when landed' do
-      plane = Plane.new
-      subject.land(plane)
-      expect(subject.plane).to eq plane
+      airport.land(plane)
+      expect(airport.plane).to eq plane
     end
   end
 end
