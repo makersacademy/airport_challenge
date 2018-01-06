7d90e5d0352cc88f1c67e2230f2c7f734c02fba4
Tom Grand <thomas.a.grand@gmail.com>
2018-01-06 19:07:12 +0000

    tidy up airport_spec.rb

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,9 +2,12 @@ require_relative "../lib/airport.rb"
 
 describe Airport do 
    
-   describe "#land" do
-   	 it "should respond to land in an airport" do
+  describe "#land" do
+   
+   	it "should respond to land in an airport" do
       expect(subject).to respond_to(:land).with(1).argument
     end
-   end
+    
+  end
+
 end
