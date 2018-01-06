6e7fe904aff262de79b988b73e354cb7c1f739aa
Ellie <elliewem@gmail.com>
2018-01-06 18:22:35 +0000

    Adds tests and functions for edge cases

--- lib/airport.rb
+++ lib/airport.rb
@@ -2,15 +2,19 @@ require_relative './plane.rb'
 
 class Airport
 
+  attr_reader :hanger
+
   def initialize
     @hanger = []
   end
 
   def land(plane)
+    raise("#{plane} is already in the hanger") if @hanger.include? plane
     @hanger << plane
   end
 
   def takeoff(plane)
+    raise("#{plane} is not in the hanger") unless @hanger.include? plane
     @hanger -= [plane]
     "#{plane} has left the hanger!"
   end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -16,10 +16,18 @@ describe Airport do
     expect(subject.land(plane)).to eq [plane]
   end
 
+  it 'Doesn\'t allow the same plane to be put in the hanger' do
+    subject.land(plane)
+    expect{subject.land(plane)}.to raise_error("#{plane} is already in the hanger")
+  end
+
   it 'Allows a plane to take off and confirms it has left the hanger' do
     subject.land(plane)
     expect(subject.takeoff(plane)).to eq "#{plane} has left the hanger!"
   end
 
+  it 'Doesn\'t allow a plane to take off if it\'s not in the hanger' do
+    expect{subject.takeoff(plane)}.to raise_error("#{plane} is not in the hanger")
+  end
 
 end
