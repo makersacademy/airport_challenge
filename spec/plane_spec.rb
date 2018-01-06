9ef47d355f041a38a66ae49abc83d78ce872dfe9
Tom Grand <thomas.a.grand@gmail.com>
2018-01-06 19:45:16 +0000

    Make sure plane gives itself to airport
    I realised that when a plane lands, it doesn't need to check
    if the airport has it. It just needs to make sure that it gives
    itself to whatever the landing thing is.

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -8,9 +8,9 @@ describe Plane do
    
     it { is_expected.to respond_to(:land) }
 
-    it "airport's hanger should contain landed plane" do
-      allow(airport).to receive(:land).with(subject).and_return(subject)
-      expect(subject.land(airport)).to eq(subject)
+    it "plane should give itself to hanger" do
+      allow(airport).to receive(:land).with(subject).and_return(true)
+      expect(subject.land(airport)).to be true
     end
     
   end
