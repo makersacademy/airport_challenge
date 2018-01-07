bdafd31575f9b37d7b3d30f829cbea7c87240ead
domvernon <3686359+domvernon@users.noreply.github.com>
2018-01-07 13:30:40 +0000

    airports instantiate with default capacity

--- lib/airport.rb
+++ lib/airport.rb
@@ -4,13 +4,17 @@ require_relative 'weather'
 class Airport
   attr_reader :planes, :weather
 
+  DEFAULT_CAPACTITY = 10
+
   def initialize
     @planes = []
     @weather = Weather.new
+    @capacity = DEFAULT_CAPACTITY
   end
 
   def land(plane)
     raise 'Could not complete landing due to weather' if weather.stormy?
+    raise 'Cannot land because airport is full' if full?
     plane.lands
     @planes << plane
   end
@@ -20,5 +24,11 @@ class Airport
     raise 'Could not complete takeoff due to weather' if weather.stormy?
     @planes[-1].takes_off
     @planes.pop
+  end
+
+  private
+
+  def full?
+    @planes.size == @capacity
   end
 end
--- spec/feature-tests/prevent_landing_when_full_spec.rb
+++ spec/feature-tests/prevent_landing_when_full_spec.rb
@@ -4,6 +4,7 @@ require 'plane'
 context 'Airport is full' do
   it 'should not allow planes to land' do
     airport = Airport.new
+    allow(airport.weather).to receive(:stormy?).and_return false
     plane = Plane.new
     Airport::DEFAULT_CAPACTITY.times { airport.land plane }
     expect { airport.land plane }.to raise_error 'Cannot land because airport is full'
