393856cb3a489e60abbc82f499662da2611712e9
Ellie <elliewem@gmail.com>
2018-01-07 09:33:41 +0000

    Adds full? method and tests for default and custom capacities

--- lib/airport.rb
+++ lib/airport.rb
@@ -3,14 +3,17 @@ require_relative './weather.rb'
 
 class Airport
 
-  attr_reader :hanger
+  DEFAULT_CAPACITY = 20
+  attr_reader :hanger, :capacity
 
-  def initialize
+  def initialize(capacity = DEFAULT_CAPACITY)
     @hanger = []
+    @capacity = capacity
   end
 
   def land(plane)
     raise("The plane can't land as there's a storm") if stormy?
+    raise("The plane can't land as the hanger is full") if full?
     raise("#{plane} is already in the hanger") if @hanger.include? plane
     @hanger << plane
   end
@@ -23,8 +26,12 @@ class Airport
   end
 
   private
-    def stormy?
-      Weather.new.stormy == 5
-    end
+  def stormy?
+    Weather.new.stormy == 5
+  end
+
+  def full?
+    @hanger.length >= @capacity
+  end
 
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -14,26 +14,48 @@ describe Airport do
   end
 
   it 'Allows a plane to be stored in the hanger' do
+    allow(subject).to receive(:stormy?) { false }
     expect(subject.land(plane)).to eq [plane]
   end
 
   it 'Doesn\'t allow the same plane to be put in the hanger' do
+    allow(subject).to receive(:stormy?) { false }
     subject.land(plane)
     expect { subject.land(plane) }.to raise_error("#{plane} is already in the hanger")
   end
 
   it 'Allows a plane to take off and confirms it has left the hanger' do
+    allow(subject).to receive(:stormy?) { false }
     subject.land(plane)
     expect(subject.takeoff(plane)).to eq "#{plane} has left the hanger!"
   end
 
   it 'Doesn\'t allow a plane to take off if it\'s not in the hanger' do
+    allow(subject).to receive(:stormy?) { false }
     expect { subject.takeoff(plane) }.to raise_error("#{plane} is not in the hanger")
   end
 
+  it 'Doesn\'t allow a plane to land if it is stormy' do
+    allow(subject).to receive(:stormy?) { true }
+    expect { subject.land(plane) }.to raise_error("The plane can't land as there's a storm")
+  end
+
   it 'Doesn\'t allow a plane to take off if it is stormy' do
-    allow(subject).to receive(:weather) { true }
-    expect(subject.land(plane)).to raise_error("The plane can't land as there's a storm")
+    allow(subject).to receive(:stormy?) { true }
+    expect { subject.takeoff(plane) }.to raise_error("The plane can't take off as there's a storm")
+  end
+
+  it 'Doesn\'t allow landing when the hanger is full' do
+    allow(subject).to receive(:stormy?) { false }
+    expect { 21.times { subject.land(Plane.new) } }.to raise_error("The plane can't land as the hanger is full")
+  end
+
+  it 'Sets a default capacity' do
+    expect(subject.capacity).to eq 20
+  end
+
+  it 'Allows a custom capacity to be set' do
+    expect(Airport.new(30).capacity).to eq 30
   end
 
 end
