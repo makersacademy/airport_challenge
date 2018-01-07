450ec43798c17b656b3b721015ba99c5c632cc1d
Tom Grand <thomas.a.grand@gmail.com>
2018-01-07 15:15:55 +0000

    refactor plane and plane tests

--- lib/plane.rb
+++ lib/plane.rb
@@ -1,19 +1,3 @@
 class Plane
-   
-  attr_reader :flying
 
-  def initialize
-    @flying = true
-  end
-   
-  def land
-    return raise("Already Grounded") unless @flying
-    @flying = false
-    true
-  end
-
-  def take_off
-    return raise("Already Flying") if @flying
-    @flying = true
-  end
 end
