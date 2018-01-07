a638c58e31129b0d569d2ac364784f713c624929
GiadaSimonetti <21024993+GiadaSimonetti@users.noreply.github.com>
2018-01-07 20:11:17 +0000

    prevent landing when the airport is full

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,5 +1,7 @@
 class Airport
 
+  DEFAULT_CAPACITY = 10
+
   attr_reader :hangar
 
   def initialize
@@ -8,7 +10,12 @@ class Airport
 
   def airstrip_land(plane)
     fail "No planes will land because the weather is stormy" if stormy?
+    fail "No planes will land because the hangar is full" if full?
     @hangar << plane
+  end
+
+  def full?
+    @hangar.count >= DEFAULT_CAPACITY
   end
 
   def airstrip_take_off(plane)
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -7,8 +7,8 @@ describe Airport do
 
     it "allows planes to land" do
       plane = Plane.new
-      subject.airstrip_land(plane)
       allow(subject).to receive(:stormy?).and_return false
+      subject.airstrip_land(plane)
       expect(subject.hangar).to include(plane)
     end
 
@@ -18,6 +18,13 @@ describe Airport do
       expect { subject.airstrip_land(plane) }.to raise_error "No planes will land because the weather is stormy"
     end
 
+    it "prevents landing when hangar is full" do
+      plane = Plane.new
+      allow(subject).to receive(:stormy?).and_return false
+      allow(subject).to receive(:full?).and_return true
+      expect { subject.airstrip_land(plane) }.to raise_error "No planes will land because the hangar is full"
+    end
+
   end
 
   describe "#airstrip_take_off" do
@@ -25,8 +32,8 @@ describe Airport do
     it "allows planes to take off" do
       plane = Plane.new
       subject.hangar << plane
-      subject.airstrip_take_off(plane)
       allow(subject).to receive(:stormy?).and_return false
+      subject.airstrip_take_off(plane)
       expect(subject.hangar).to_not include(plane)
     end
 
@@ -42,6 +49,13 @@ describe Airport do
     it "checks if the weather is stormy" do
       allow(subject).to receive(:weather).and_return("stormy")
       expect(subject.stormy?).to eq true
+    end
+  end
+
+  describe "#full?" do
+    it "checks if the hangar is full" do
+      Airport::DEFAULT_CAPACITY.times { subject.hangar << Plane.new }
+      expect(subject.full?).to eq true
     end
   end
 
