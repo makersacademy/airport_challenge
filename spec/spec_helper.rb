128d4ac93699d1ec35bd4a123124ca2714f9590a
domvernon <3686359+domvernon@users.noreply.github.com>
2018-01-07 20:15:44 +0000

    foramtting

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,8 @@
 require 'coveralls'
-Coveralls.wear!
-
 require 'simplecov'
 require 'simplecov-console'
+
+Coveralls.wear!
 
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
   SimpleCov::Formatter::Console,
