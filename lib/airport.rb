7329b3b76cd317aedbcb7851ce1ab7b71e90bedb
Tom Grand <thomas.a.grand@gmail.com>
2018-01-07 13:50:51 +0000

    add private method weather

--- lib/airport.rb
+++ lib/airport.rb
@@ -13,16 +13,21 @@ class Airport
   end
 
   def take_off(plane, weather)
-    return raise("No such plane in hanger") unless plane?(plane)
-    return raise("Weather too bad") if weather.forecast == "stormy"
+    plane?(plane)
+    weather?(weather)
     @hanger.delete(plane) if plane.take_off
   end
    
 
   private
   def plane?(plane)
-    @hanger.include?(plane) ? true : false
+    @hanger.include?(plane) ? true : raise("No such plane in hanger") 
   end
+
+  def weather?(weather)
+    return raise("Weather too bad") if weather.forecast == "stormy"
+  end
+  
 
  
 end
