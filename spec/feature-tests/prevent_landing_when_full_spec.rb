76ba6e5e23c32dc5fe23d99d4fb69f094a1e4c73
domvernon <3686359+domvernon@users.noreply.github.com>
2018-01-07 13:37:57 +0000

    should not land when full with a given capacity on instantiation

--- spec/feature-tests/prevent_landing_when_full_spec.rb
+++ spec/feature-tests/prevent_landing_when_full_spec.rb
@@ -2,11 +2,20 @@ require 'airport'
 require 'plane'
 
 context 'Airport is full' do
-  it 'should not allow planes to land' do
+  it 'should not allow planes to land (default capacity)' do
     airport = Airport.new
     allow(airport.weather).to receive(:stormy?).and_return false
     plane = Plane.new
     Airport::DEFAULT_CAPACTITY.times { airport.land plane }
+    expect { airport.land plane }.to raise_error 'Cannot land because airport is full'
+  end
+
+  it 'should not allow planes to land (given capacity)' do
+    custom_capacity = 5
+    airport = Airport.new(custom_capacity)
+    allow(airport.weather).to receive(:stormy?).and_return false
+    plane = Plane.new
+    Airport.capacity.times { airport.land plane }
     expect { airport.land plane }.to raise_error 'Cannot land because airport is full'
   end
 end
