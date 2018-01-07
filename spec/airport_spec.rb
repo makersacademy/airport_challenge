2259d23bc6a1749a3f2cf064eb0516e1ea64aade
ealitten <ea.litten@gmail.com>
2018-01-07 12:53:22 +0000

    Unit tests for plane landing

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -3,6 +3,8 @@ require 'airport'
 describe Airport do
 
   subject(:airport) { described_class.new }
+  let (:plane) {double :plane}
+  let (:weather) {double :weather}
   
   describe "#initialize" do
 
@@ -19,12 +21,11 @@ describe Airport do
 
   describe "#takeoff_plane" do
 
-    before do
-      weather = double(:weather, :stormy? => false)
-      airport.instance_variable_set(:@weather, weather)
-      plane = double(:plane)
-      airport.instance_variable_set(:@planes_in_hangar, [plane])
-    end
+    before {
+      airport.instance_variable_set(:@weather, weather) # replace random weather with controlled stub
+      airport.instance_variable_set(:@planes_in_hangar, [plane]) # add plane to hangar to take off
+      allow(weather).to receive(:stormy?).and_return(false) # ensure weather not stormy
+    }
 
     it "should instruct plane to take off" do
       expect(airport.takeoff(plane)).to eq plane
@@ -36,20 +37,33 @@ describe Airport do
     end
 
     it "should not allow plane to take off if stormy" do
-      weather = double(:weather, :stormy? => true)
-      airport.instance_variable_set(:@weather, weather)
+      allow(weather).to receive(:stormy?).and_return(true)
       expect{ airport.takeoff(plane) }.to raise_error "Planes cannot take off due to bad weather"
     end
 
   end
 
   describe "#land_plane" do
-    # it "should instruct plane to land" do
-    # end
-    # it "should not allow plane to land if stormy" do
-    # end
-    # it "should not allow plane to land if full" do
-    # end
+
+    before { 
+      airport.instance_variable_set(:@weather, weather) 
+      allow(weather).to receive(:stormy?).and_return(false)
+    }
+
+    it "should instruct plane to land" do
+      expect(airport.land(plane)).to eq plane
+    end
+
+    it "should not allow plane to land if stormy" do
+      allow(weather).to receive(:stormy?).and_return(true)
+      expect{ airport.land(plane) }.to raise_error "Planes cannot land due to bad weather"
+    end
+
+    it "should not allow plane to land if full" do
+      airport.instance_variable_set(:@planes_in_hangar, [plane])
+      expect{ airport.land(plane) }.to raise_error "Planes cannot land due to airport hangar full"
+    end
+
   end
 
 end
