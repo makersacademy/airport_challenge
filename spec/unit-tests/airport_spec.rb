c4e0dc7488d4833bed2971796441149d328fafbf
domvernon <3686359+domvernon@users.noreply.github.com>
2018-01-07 00:13:52 +0000

    removes redundant tests

--- spec/unit-tests/airport_spec.rb
+++ spec/unit-tests/airport_spec.rb
@@ -5,13 +5,6 @@ describe Airport do
   let(:plane) { double :plane }
 
   describe '#land' do
-    it 'has a land method' do
-      is_expected.to respond_to(:land).with(1).argument
-    end
-    it 'can land a plane' do
-      allow(plane).to receive(:lands)
-      expect { airport.land plane }.to_not raise_error
-    end
     it 'can land more than one plane' do
       allow(plane).to receive(:lands)
       expect { 2.times { airport.land plane } }.to_not raise_error
@@ -19,18 +12,7 @@ describe Airport do
   end
 
   describe '#planes' do
-    it 'has a planes method' do
-      is_expected.to respond_to(:planes)
-    end
-    it 'should return an object' do
-      is_expected.to be_truthy
-    end
     context 'when planes land' do
-      it 'should show a single landed plane' do
-        allow(plane).to receive(:lands)
-        airport.land plane
-        expect(airport.planes[0]).to eq plane
-      end
       it 'should show all the planes' do
         allow(plane).to receive(:lands)
         2.times { airport.land plane }
@@ -40,9 +22,6 @@ describe Airport do
   end
 
   describe '#take_off' do
-    it 'has a planes method' do
-      is_expected.to respond_to(:take_off)
-    end
     it 'allows a plane to take off' do
       allow(plane).to receive(:lands)
       allow(plane).to receive(:takes_off)
--- spec/unit-tests/plane_spec.rb
+++ spec/unit-tests/plane_spec.rb
@@ -4,23 +4,8 @@ describe Plane do
   subject(:plane) { described_class.new }
   let(:airport) { double :airport }
 
-  describe '#lands' do
-    it 'has a lands method' do
-      is_expected.to respond_to(:lands)
-    end
-  end
-
-  describe '#takes_off' do
-    it 'has a takes_off method' do
-      is_expected.to respond_to(:takes_off)
-    end
-  end
-
   describe '#status' do
-    it 'has a status method' do
-      is_expected.to respond_to(:status)
-    end
-    it 'is flying on instantiation' do
+    it 'is :flying on instantiation' do
       expect(plane.status).to eq :flying
     end
     context 'on landing' do
