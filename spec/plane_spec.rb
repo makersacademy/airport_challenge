2f0652b564d5c76f0496ef3f0d8949c18f614e53
Lewis Moore <lewis.moore2707@gmail.com>
2018-01-08 08:47:19 +0000

    Added some notes

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -1,1 +1,3 @@
 
+# Looking back, tests could be a bit more comprehensive. Test where a plane is, if its in an airport or not etc.
+#could also test if the airport array drops a plane during take_off 
