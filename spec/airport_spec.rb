929b3a54caef3f2027ff9e47d30472792481b905
rcvink <rrvink@gmail.com>
2018-01-08 10:49:54 +0000

    Refactor subjects

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -10,31 +10,31 @@ describe Airport do
 
   context 'when receiving planes' do
     it 'receives a plane successfully' do
-      subject.receive(plane)
-      expect(subject.has?(plane)).to be true
+      airport.receive(plane)
+      expect(airport.has?(plane)).to be true
     end
 
     it 'fails to receive a plane that is already present' do
-      subject.receive(plane)
-      expect { subject.receive(plane) }.to raise_error 'Cannot receive plane already present in airport'
+      airport.receive(plane)
+      expect { airport.receive(plane) }.to raise_error 'Cannot receive plane already present in airport'
     end
 
     it 'fails to receive a plane when airport is full' do
-      20.times { subject.receive(Plane.new) }
-      expect { subject.receive(plane) }.to raise_error 'Insufficient capacity to receive plane'
-      expect(subject.full?).to be true
+      20.times { airport.receive(Plane.new) }
+      expect { airport.receive(plane) }.to raise_error 'Insufficient capacity to receive plane'
+      expect(airport.full?).to be true
     end
   end
 
   context 'when dispatching planes' do
     it 'dispatches a plane successfully' do
-      subject.receive(plane)
-      subject.dispatch(plane)
-      expect(subject.has?(plane)).to be false
+      airport.receive(plane)
+      airport.dispatch(plane)
+      expect(airport.has?(plane)).to be false
     end
 
     it 'fails to dispatch a plane that is not present' do
-      expect { subject.dispatch(plane) }.to raise_error 'Cannot dispatch plane not present in airport'
+      expect { airport.dispatch(plane) }.to raise_error 'Cannot dispatch plane not present in airport'
     end
   end
 end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -5,30 +5,30 @@ describe Plane do
   let(:airport) { double(:airport) }
 
   it 'can create an instance of Plane class' do
-    expect(subject).to be_instance_of(Plane)
+    expect(plane).to be_instance_of(Plane)
   end
 
   context 'when landing' do
     it 'lands successfully' do
-      subject.land(airport)
-      expect(subject.grounded_at?(airport)).to be true
+      plane.land(airport)
+      expect(plane.grounded_at?(airport)).to be true
     end
 
     it 'fails to land when already grounded' do
-      subject.land(airport)
-      expect { subject.land(airport) }.to raise_error 'Plane is already grounded'
+      plane.land(airport)
+      expect { plane.land(airport) }.to raise_error 'Plane is already grounded'
     end
   end
 
   context 'when taking off' do
     it 'takes off successfully' do
-      subject.land(airport)
-      subject.take_off
-      expect(subject.landed?).to be false
+      plane.land(airport)
+      plane.take_off
+      expect(plane.landed?).to be false
     end
 
     it 'fails to take off when already airborne' do
-      expect { subject.take_off }.to raise_error 'Plane is already airborne'
+      expect { plane.take_off }.to raise_error 'Plane is already airborne'
     end
   end
 end
