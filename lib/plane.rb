c878560a438e3e0b8e6eddbe200ceda58682c8a2
GiadaSimonetti <21024993+GiadaSimonetti@users.noreply.github.com>
2018-01-08 11:02:42 +0000

    Refactor

--- lib/plane.rb
+++ lib/plane.rb
@@ -1,16 +1,10 @@
 
 class Plane
-  # As an air traffic controller
-  # So I can get passengers to a destination
-  # I want to instruct a plane to land at an airport
+
   def land
     "The plane landed"
   end
 
-  # As an air traffic controller
-  # So I can get passengers on the way to their destination
-  # I want to instruct a plane to take off from an airport
-  # and confirm that it is no longer in the airport
   def take_off
     "The plane took off"
   end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -3,23 +3,22 @@ require "plane"
 
 describe Airport do
 
+let(:plane) { double("plane") }
+
   describe "#airstrip_land" do
 
     it "allows planes to land" do
-      plane = Plane.new
       allow(subject).to receive(:stormy?).and_return false
       subject.airstrip_land(plane)
       expect(subject.hangar).to include(plane)
     end
 
     it "prevents landing when weather is stormy" do
-      plane = Plane.new
       allow(subject).to receive(:stormy?).and_return true
       expect { subject.airstrip_land(plane) }.to raise_error "No planes will land because the weather is stormy"
     end
 
     it "prevents landing when hangar is full" do
-      plane = Plane.new
       allow(subject).to receive(:stormy?).and_return false
       allow(subject).to receive(:full?).and_return true
       expect { subject.airstrip_land(plane) }.to raise_error "No planes will land because the hangar is full"
@@ -30,7 +29,6 @@ describe Airport do
   describe "#airstrip_take_off" do
 
     it "allows planes to take off" do
-      plane = Plane.new
       subject.hangar << plane
       allow(subject).to receive(:stormy?).and_return false
       subject.airstrip_take_off(plane)
@@ -38,7 +36,6 @@ describe Airport do
     end
 
     it "prevents taking off when weather is stormy" do
-      plane = Plane.new
       allow(subject).to receive(:stormy?).and_return true
       expect { subject.airstrip_take_off(plane) }.to raise_error "No planes will take off because of the stormy weather"
     end
