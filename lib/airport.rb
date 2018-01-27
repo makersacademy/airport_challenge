e610d59318bb75418ff4d9f650ce5019fb132d73
Heather Stock <33641095+heatherstock@users.noreply.github.com>
2018-01-27 21:42:34 +0000

    Created test and method for take_off, removing plane from array, pass

--- lib/airport.rb
+++ lib/airport.rb
@@ -12,4 +12,8 @@ class Airport
     @planes << plane
   end
 
+  def take_off(plane)
+    @planes.delete(plane)
+  end
+
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,13 +2,20 @@ require "Airport"
 
 describe Airport do
 
-  # subject(:airport) { described_class.new }
+  subject(:airport) { described_class.new }
   let(:plane) { Plane.new }
 
   # it { is_expected.to respond_to(:land).with(1).argument }
 
-  it "lands a plane" do
+  it "allows plane to land (add to array)" do
     expect(subject.land(plane)).to eq [plane]
+  end
+
+  # it { is_expected.to respond_to(:take_off) }
+
+  it "allows plane to take off(remove from array)" do
+    subject.land(plane)
+    expect(subject.take_off(plane)). to be_instance_of(Plane)
   end
 
 end
