32c9ad78ebec9c2bfdd30085d88174fb28355061
Stephen Tilley <stephentilley@gmail.com>
2018-01-27 23:01:06 +0000

    Airport land method lands a plane, test created and passed

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,9 +1,11 @@
 class Airport
 
   def land(plane)
+    @plane = plane
   end
 
   def take_off(plane)
+
   end
 
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,7 +2,19 @@ require "airport"
 
 describe Airport do
 
-  it { is_expected.to respond_to(:land).with(1).argument}
-  it { is_expected.to respond_to(:take_off).with(1).argument}
+  let(:plane) { double("A plane") }
+
+  it { is_expected.to respond_to(:land).with(1).argument }
+  it { is_expected.to respond_to(:take_off).with(1).argument }
+
+  context "#land" do
+    it "lands a plane" do
+      expect(subject.land(plane)).to eq(plane)
+    end
+  end
+
+  context "#take_off" do
+
+  end
 
 end
