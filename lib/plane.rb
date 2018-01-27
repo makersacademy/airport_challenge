6481605fddb1f15e18f25ab58f3b155a2ce6d16c
Laura Chan <laurachan205@gmail.com>
2018-01-27 13:02:48 +0000

    Add take off method to plane

--- lib/plane.rb
+++ lib/plane.rb
@@ -3,4 +3,8 @@ class Plane
   def land(airport)
   end
 
+  def take_off
+    print "Plane has left airport"
+  end
+
 end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -3,5 +3,19 @@ require 'plane'
 describe Plane do
 
   it {is_expected.to respond_to(:land).with(1).argument}
+  it {is_expected.to respond_to(:take_off)}
+
+  # describe '#land' do
+  #   it 'lands self at an airport' do
+  #     airport = double('airport')
+  #     expect{subject.land(airport)}
+  #   end
+  # end
+
+  describe '#take_off' do
+    it 'confirms self is no longer at the airport' do
+      expect {subject.take_off}.to output('Plane has left airport').to_stdout
+    end
+  end
 
 end
