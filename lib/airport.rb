3212d231fcdbdeec026f633d635ab1dafd907f49
kotpsot <zygmunt.justyna@gmail.com>
2018-01-27 13:58:54 +0000

    draft v. of land, take off and confirm take off methods in airport class

--- lib/airport.rb
+++ lib/airport.rb
@@ -8,4 +8,12 @@ def land(plane)
   @planes_collection << plane
 end
 
+def take_off(plane)
+  @planes_collection.delete(plane)
+end
+
+def confirm_take_off(plane)
+  "#{plane} took off"
+end
+
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -11,5 +11,20 @@ describe Airport do
     end
   end
 
+  describe "#take_off" do
+    it "take off the planes" do
+      plane = Plane.new
+      airport = Airport.new
+      expect(airport.take_off(plane)).to eq nil
+    end
+  end
+
+  describe "#confirm_take_off" do
+    it "confirms that the plane took off" do
+      plane = Plane.new
+      airport = Airport.new
+      expect(airport.confirm_take_off(plane)).to eq "#{plane} took off"
+    end
+  end
 
 end
