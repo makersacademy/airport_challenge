7ca712702978737d8171ad7441986a9ee6806cc7
kotpsot <zygmunt.justyna@gmail.com>
2018-01-27 16:24:40 +0000

    refactor tests

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -7,90 +7,80 @@ describe Airport do
 
   describe "#land" do
     it "lands the planes when sunny" do
-      airport = Airport.new
-      allow(airport).to receive(:tell_weather) { "sunny" }
-      expect(airport.land(plane)).to eq [plane]
+      allow(subject).to receive(:tell_weather) { "sunny" }
+      expect(subject.land(plane)).to eq [plane]
     end
   end
 
   describe "#land" do
-    airport = Airport.new
     it "prevent from landing when stormy" do
-      allow(airport).to receive(:tell_weather) { "stormy" }
-      expect(airport.land(plane)).to eq nil
+      allow(subject).to receive(:tell_weather) { "stormy" }
+      expect(subject.land(plane)).to eq nil
     end
   end
 
   describe "raising error when full" do
-    airport = Airport.new
     it 'raises error when full' do
-      subject.capacity.times { airport.land plane }
-      expect { airport.land plane }.to raise_error 'No free spots avaialble'
+      subject.capacity.times { subject.land plane }
+      expect { subject.land plane }.to raise_error 'No free spots avaialble'
     end
   end
 
   describe "#take_off" do
     it "take off the planes when sunny" do
-      airport = Airport.new
-      airport.land(plane)
-      allow(airport).to receive(:tell_weather) { "sunny" }
-      airport.take_off(plane)
-      expect(airport.planes_a).to eq []
+      subject.land(plane)
+      allow(subject).to receive(:tell_weather) { "sunny" }
+      subject.take_off(plane)
+      expect(subject.planes_a).to eq []
     end
   end
 
   describe "#take_off" do
     it "prevent from taking off the planes when stormy" do
-      airport = Airport.new
-      allow(airport).to receive(:tell_weather) { "sunny" }
-      airport.land(plane)
-      allow(airport).to receive(:tell_weather) { "stormy" }
-      airport.take_off(plane)
-      expect(airport.planes_a).to eq [plane]
+      allow(subject).to receive(:tell_weather) { "sunny" }
+      subject.land(plane)
+      allow(subject).to receive(:tell_weather) { "stormy" }
+      subject.take_off(plane)
+      expect(subject.planes_a).to eq [plane]
     end
   end
 
   describe "#confirm_take_off" do
     it "confirms that the plane took off" do
-      airport = Airport.new
-      airport.land(plane)
-      allow(airport).to receive(:tell_weather) { "sunny" }
-      airport.take_off(plane)
-      expect(airport.confirm_take_off(plane)).to eq "#{plane} took off"
+      subject.land(plane)
+      allow(subject).to receive(:tell_weather) { "sunny" }
+      subject.take_off(plane)
+      expect(subject.confirm_take_off(plane)).to eq "#{plane} took off"
     end
   end
 
   describe "#confirm_take_off" do
     it "says plane didnt take off" do
-      airport = Airport.new
-      allow(airport).to receive(:tell_weather) { "sunny" }
-      airport.land(plane)
-      allow(airport).to receive(:tell_weather) { "stormy" }
-      expect(airport.confirm_take_off(plane)).to eq "#{plane} didn't take off"
+      allow(subject).to receive(:tell_weather) { "sunny" }
+      subject.land(plane)
+      allow(subject).to receive(:tell_weather) { "stormy" }
+      expect(subject.confirm_take_off(plane)).to eq "#{plane} didn't take off"
     end
   end
 
   describe "#random" do
     it "returns a number from 0 to 6" do
-      airport = Airport.new
-      expect(airport).to receive(:random).and_return(1)
-      expect(airport.random).to eq 1
+      expect(subject).to receive(:random).and_return(1)
+      expect(subject.random).to eq 1
     end
   end
 
   describe "#tell_weather" do
     it "says it's stormy" do
-      airport = Airport.new
-      allow(airport).to receive(:random) { 1 }
-      expect(airport.tell_weather).to eq "stormy"
+      allow(subject).to receive(:random) { 1 }
+      expect(subject.tell_weather).to eq "stormy"
     end
   end
 
   describe "#tell_wheather" do
     it "says it's sunny" do
-      airport = Airport.new
-      allow(airport).to receive(:random) { 2 }
-      expect(airport.tell_weather).to eq "sunny"
+      allow(subject).to receive(:random) { 2 }
+      expect(subject.tell_weather).to eq "sunny"
     end
   end
 
