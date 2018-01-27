61f764ee60cebddbd2cb2524a5181619dc3b8ff9
marcusfgardiner <marcusfgardiner@gmail.com>
2018-01-27 12:29:44 +0000

    Plane can land then take off, currently only the last plane takes off rather than a specified plane

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -14,7 +14,7 @@ describe Airport do
   }
 
   it { is_expected.to respond_to(:land).with(1).argument}
-  it { is_expected.to respond_to(:take_off)}
+  it { is_expected.to respond_to(:take_off).with(1).argument}
 
   context "#land(plane)" do
 
