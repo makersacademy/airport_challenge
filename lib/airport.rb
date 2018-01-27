a4779828754436234a851a8eec4984e6ab76f80e
Magyar-Hunor Tamas <tamasmagyarhunor88@gmail.com>
2018-01-27 10:38:19 +0000

    airport actually lands a plane now

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,6 +1,8 @@
+require_relative 'plane'
+
 class Airport
   def land(plane)
-
+    plane
   end
 
   def take_off(plane)
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,11 +1,16 @@
 require 'airport'
 
 describe Airport do
-  it 'lands a plane' do
+  it 'responds to land(plane)' do
     expect(subject).to respond_to(:land).with(1).argument
   end
 
-  it 'takes off a plane' do
+  it 'responds to take_off(plane)' do
     expect(subject).to respond_to(:take_off).with(1).argument
+  end
+
+  it 'lands a plane' do
+    plane = Plane.new
+    expect(subject.land(plane)).to eq (plane)
   end
 end
