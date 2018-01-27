1d84d3819672152d706c1d2db8bec419cd65cb4d
carlostrapet <trapet_llamas@hotmail.com>
2018-01-27 20:38:51 +0000

    added tests to check if planes take off and land. All tests passing

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,9 +1,19 @@
 
 class Airport
 
+  attr_reader :planes
+
+  def initialize
+  @planes = [] # we could use @planes.delete("planename")
+  end
+
   def land(plane)
+    @planes << plane
   end
 
   def take_off(plane)
+    @planes.delete(plane)
+    return @planes
   end
+
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -5,4 +5,18 @@ describe Airport do
   it { is_expected.to respond_to(:land).with(1).argument }
   it { is_expected.to respond_to(:take_off).with(1).argument }
 
+  describe '#land' do 
+    it 'allows planes to land' do 
+      plane = Plane.new
+      expect(subject.land(plane)).to match_array([plane])
+    end
+  end
+
+  describe '#take_off' do 
+    it 'allows planes to take off' do 
+      plane = Plane.new
+      subject.land(plane)
+      expect(subject.take_off(plane)).to match_array([])
+    end
+  end
 end
