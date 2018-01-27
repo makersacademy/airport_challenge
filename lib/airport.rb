641d997cf3ddd2540d7583d6edab23b350ba978a
Magyar-Hunor Tamas <tamasmagyarhunor88@gmail.com>
2018-01-27 10:07:45 +0000

    airport test if responds to take_off(plane)

--- lib/airport.rb
+++ lib/airport.rb
@@ -2,4 +2,7 @@ class Airport
   def land(plane)
 
   end
+
+  def take_off(plane)
+  end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -4,4 +4,8 @@ describe Airport do
   it 'lands a plane' do
     expect(subject).to respond_to(:land).with(1).argument
   end
+
+  it 'takes off a plane' do
+    expect(subject).to respond_to(:take_off).with(1).argument
+  end
 end
