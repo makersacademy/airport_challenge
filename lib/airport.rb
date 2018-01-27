6f3d0af3ec2e7c789c200bbd53fc8b9bc50c05b0
carlostrapet <trapet_llamas@hotmail.com>
2018-01-27 21:18:05 +0000

    added customized capacity. all features added except weather related

--- lib/airport.rb
+++ lib/airport.rb
@@ -2,18 +2,33 @@
 class Airport
 
   attr_reader :planes
+  attr_accessor :capacity
 
-  def initialize
-  @planes = [] # we could use @planes.delete("planename")
+  DEFAULT_CAPACITY = 50
+
+  def initialize(capacity = DEFAULT_CAPACITY)
+    @planes = [] # we could use @planes.delete("planename")
+    @capacity = capacity
   end
 
   def land(plane)
+    fail 'hangar full' if full?
     @planes << plane
   end
 
   def take_off(plane)
+    fail 'hangar empty' if empty?
     @planes.delete(plane)
     return @planes
   end
 
+private
+
+  def full?
+    @planes.count >= @capacity
+  end
+
+  def empty?
+    @planes.empty?
+  end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -10,6 +10,10 @@ describe Airport do
       plane = Plane.new
       expect(subject.land(plane)).to match_array([plane])
     end
+    it 'raises an error when airport is full' do
+      subject.capacity.times { subject.land(Plane.new) }
+      expect { subject.land(Plane.new) }.to raise_error 'hangar full'
+    end
   end
 
   describe '#take_off' do 
@@ -17,6 +21,16 @@ describe Airport do
       plane = Plane.new
       subject.land(plane)
       expect(subject.take_off(plane)).to match_array([])
+    end
+    it 'raises an error when airport is empty' do 
+      expect { subject.take_off(Plane.new) }.to raise_error 'hangar empty'
+    end
+  end
+
+  describe '#initialize' do 
+    it 'allows to edit initialized capacity' do 
+      airport = Airport.new
+      expect(airport.capacity = 20).to eq 20
     end
   end
 end
