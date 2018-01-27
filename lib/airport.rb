2411bbb15b7c457451b5f86cc42b1346d826fff8
Josué Estévez Fernández <jestfer6@gmail.com>
2018-01-27 22:12:29 +0000

    program now defends against edge cases: landing twice, taking off from a different airport/while flying...

--- lib/airport.rb
+++ lib/airport.rb
@@ -10,11 +10,17 @@ class Airport
   end
 
   def land(plane)
-    raise "Hangar is at its full capacity" if hangar.count == capacity
-    hangar << plane
+    if hangar.include?(plane)
+      raise "Already in hangar"
+    elsif hangar.count == capacity
+      raise "Hangar is at its full capacity"
+    else
+      hangar << plane
+    end 
   end
 
   def take_off(plane)
+    raise "Plane not in hangar" if !hangar.include?(plane)
     hangar.delete(plane)
     "#{plane} has just taken off"
   end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,7 +1,7 @@
 require 'airport'
 
 describe Airport do
-  describe "#land" do
+  describe "#landing" do
 
     before do
       @plane = Plane.new
@@ -10,8 +10,12 @@ describe Airport do
 
     it { is_expected.to respond_to(:land).with(1).argument }
 
-    it 'should instruct a plane to land at the airport' do
+    it 'should instruct plane to land at the airport' do
       expect(subject.hangar).to include(@plane)
+    end
+
+    it 'should not allow plane to land again if in hangar' do
+      expect{ subject.land(@plane) }.to raise_error("Already in hangar")
     end
   end
 
@@ -28,29 +32,37 @@ describe Airport do
       expect(subject.take_off(@plane)).to eq "#{@plane} has just taken off"
     end
 
-    it 'should show that plane is no longer in the airport' do
+    it 'should output plane is no longer at the airport' do
       subject.take_off(@plane)
       expect(subject.hangar).not_to include(@plane)
+    end
+
+    it 'should raise error if plane takes off from wrong airport' do
+      another_airport = Airport.new
+      expect{ another_airport.take_off(@plane) }.to raise_error("Plane not in hangar")
     end
   end
 
   describe "#stormy weather: no takeoff or landing" do
 
+    before do
+      # Roll the die
+    end
 
-
+    it 'should prevent takeoff when weather is stormy'
 
 
   end
 
   describe "#hangar is full" do
-    it 'should raise an error when hangar capacity is exceeded' do
-      subject.capacity.times { subject.land Plane.new }
-      expect{ subject.land Plane.new }.to raise_error("Hangar is at its full capacity")
+    it 'should raise error when hangar capacity is exceeded' do
+      subject.capacity.times { subject.land(Plane.new) }
+      expect{ subject.land(Plane.new) }.to raise_error("Hangar is at its full capacity")
     end
   end
 
   describe "#modification of capacity" do
-    it 'should modify the default capacity as needed for other airports' do
+    it 'should modify default capacity as needed for other airports' do
       massive_airport = Airport.new(302)
       expect(massive_airport.capacity).to eq 302
     end
