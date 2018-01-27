1c463cd157599d20fd808ed288777575ef453ee4
Daniel Keen <keendaniel01@gmail.com>
2018-01-27 12:07:52 +0000

    Add take_off_plane method that removes plane from hangar

--- lib/airport.rb
+++ lib/airport.rb
@@ -2,7 +2,12 @@ class Airport
   def initialize
     @hangar = []
   end
+
   def land_plane(plane)
     @hangar << plane
+  end
+
+  def take_off_plane(plane)
+    @hangar.delete(plane)
   end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -11,4 +11,12 @@ describe Airport do
 
   end
 
+  describe '#take_off_plane' do
+
+    it 'should take off a plane and remove it from hangar' do
+      subject.land_plane(plane)
+      expect(subject.take_off_plane(plane)).to eq plane
+    end
+  end
+
 end
