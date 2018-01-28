2ac3d6a3bef222653e9b9d148bdfc25a0a15c82a
jose <jennymarin1989@hotmail.com>
2018-01-28 18:50:59 +0000

    Raising errors at the airport

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,3 +1,4 @@
+require_relative "weather"
 
 class Airport
 
@@ -9,14 +10,19 @@ class Airport
   def initialize(capacity = DEFAULT_CAPACITY)
     @hangar = []
     @capacity = capacity
+    @weather = Weather.new
   end
 
   def land(plane)
+    fail " Airplane is not allowed to land due to stormy weather" if storm?
     fail "Airplane is not allow to land, Airport is full" if full?
     @hangar << plane
+    "Plane has landed"
   end
 
   def take_off(plane)
+    #fail "Plane has already taken off" unless @hangar.include?(plane)
+    fail "Airplane is not allowed to take off due to stormy weather" if storm?
     @hangar.delete(plane)
     "Plane has taken off"
   end
@@ -25,6 +31,10 @@ class Airport
 
   def full?
     @hangar.length >= @capacity
+  end
+
+  def storm?
+    @weather.random_weather == "storm"
   end
 
   # def hangar
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,4 +1,5 @@
 require 'airport'
+require 'weather'
 
 describe Airport do
 
@@ -9,12 +10,18 @@ end
   describe "#land" do
     it { is_expected.to respond_to(:land).with(1).argument }
     it "allow planes to land" do
-      expect(subject.land(@plane)).to eq [@plane]
+      expect(subject.land(@plane)).to eq "Plane has landed"
     end
-    
+
     it "raises an error when airport is full" do
       Airport::DEFAULT_CAPACITY.times{ subject.land @plane}
       expect { subject.land @plane }.to raise_error "Airplane is not allow to land, Airport is full"
+    end
+
+    it "doesn't allow a plane to land if storm weather" do
+      subject.take_off(@plane)
+      allow(subject).to receive(:storm?).and_return(true)
+      expect {subject.land(@plane)}.to raise_error " Airplane is not allowed to land due to stormy weather"
     end
   end
 
@@ -23,6 +30,19 @@ end
     it "allow to planes to take off" do
       expect(subject.take_off(@plane)).to eq "Plane has taken off"
     end
+
+    it "doesn't allow plane to take off due to storm" do
+      subject.land(@plane)
+      allow(subject).to receive(:storm?).and_return(true)
+      expect {subject.take_off(@plane)}.to raise_error "Airplane is not allowed to take off due to stormy weather"
+    end
+
+   #  it "plane is not longer available, has already taken off" do
+   #   subject.land(@plane)
+   #   subject.take_off(@plane)
+   #   expect(subject.hangar).to_not include(@plane)
+   # end
+
   end
 
   describe "hangar" do
