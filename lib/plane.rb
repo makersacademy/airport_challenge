77845aeebc7dd2c990ab6fe147de7542f09ecb6c
Laura Chan <laurachan205@gmail.com>
2018-01-28 13:58:56 +0000

    Plane now only lands in fine weather

--- lib/plane.rb
+++ lib/plane.rb
@@ -5,20 +5,32 @@ class Plane
   attr_reader :current_airport
 
   def initialize
-    @current_airport = nil
+    @current_airport
   end
 
   def land(airport)
-    airport.planes << self
-    @current_airport = airport
+    if airport.weather == "stormy"
+      fail "It's stormy here. Cannot land at this airport"
+    else
+      landing_sequence(airport)
+    end
   end
 
   def take_off
-    take_off_sequence
-    take_off_message
+    if @current_airport == nil
+      fail "You are currently in flight"
+    else
+      take_off_sequence
+      take_off_message
+    end
   end
 
 private
+
+  def landing_sequence(airport)
+    airport.planes << self
+    @current_airport = airport
+  end
 
   def take_off_sequence
     @current_airport.planes.delete(self)
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -8,27 +8,34 @@ describe Plane do
   describe '#land' do
     it 'lands self at an airport' do
       airport = double('airport',:planes => [])
+      allow(airport).to receive(:weather)
       subject.land(airport)
       expect(airport.planes).to include(subject)
     end
 
-    it 'raises and error when airport.weather is stormy' do
-
+    it 'raises an error when airport.weather is stormy' do
+      airport = double('airport',:planes => [])
+      allow(airport).to receive(:weather){"stormy"}
+      expect{subject.land(airport)}.to raise_error "It's stormy here. Cannot land at this airport"
     end
   end
 
   describe '#take_off' do
-    let(:plane){Plane.new}
-    let(:airport){double('airport',:planes => [])}
 
     it 'takes off from its current airport' do
-      plane.land(airport)
+      current_airport = double('airport', :planes => [])
+      allow(current_airport).to receive(:weather)
+      plane = Plane.new
+      plane.land(current_airport)
       plane.take_off
-      expect(airport.planes).to eql([])
+      expect(current_airport.planes).to eql([])
     end
 
     it 'confirms self is no longer at the airport' do
-      plane.land(airport)
+      current_airport = double('airport', :planes => [])
+      allow(current_airport).to receive(:weather)
+      plane = Plane.new
+      plane.land(current_airport)
       expect{plane.take_off}.to output('Plane has left airport').to_stdout
     end
   end
