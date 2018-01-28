4a4fe372548f7ff4cf7fcfd498ff2306bf505cb9
Noel Vock <noel1uk@gmail.com>
2018-01-28 09:53:44 +0000

    Updated airport_spec.rb to test that planes may be landed

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,7 +1,17 @@
 require 'airport'
+require 'plane'
 
 describe Airport do
 
-  it { should respond_to(:land) } 
-
+  it { is_expected.to respond_to(:land) }
+  it { is_expected.to respond_to(:land).with(1).argument }
+  # context "a plane is ready to land at an empty airport" do
+  #   plane = Object.new
+  #   # subject.land(plane)
+  #   it "should store a plane in the airports planes variable" do
+  #     expect(subject.plane).to eq plane
+  it "lands a plane" do
+    plane = Plane.new
+    expect(subject.land(plane)).to eq plane
+  end
 end
