61cca9df97097d8cb8185049a8e2fc9350007847
Noel Vock <noel1uk@gmail.com>
2018-01-28 19:37:50 +0000

    Core functionality of airport system complete

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,12 +1,39 @@
 class Airport
-  attr_reader :plane, :weather
-  def land(plane)
 
-    @plane = plane
+  attr_reader :plane, :planes
+  attr_accessor :capacity
+
+  DEFAULT_CAPACITY = 10
+
+  def initialize(capacity = DEFAULT_CAPACITY)
+    @planes = []
+    @capacity = capacity
+  end
+
+  def land(plane)
+    if stormy_weather?
+      raise "It is too stormy to land"
+    end
+    if full?
+      raise "Airport is full"
+    end
+    @planes.push(plane)
   end
 
   def take_off
-    @plane = nil
+    if stormy_weather?
+      raise "It is too stormy to take off"
+    end
+    @planes.pop
+  end
+
+  private
+  def stormy_weather?
+    rand(2) == 1
+  end
+
+  def full?
+    planes.length >= capacity
   end
 
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -5,38 +5,88 @@ describe Airport do
 
   it { is_expected.to respond_to(:land) }
   it { is_expected.to respond_to(:land).with(1).argument }
-  # context "a plane is ready to land at an empty airport" do
-  #   plane = Object.new
-  #   # subject.land(plane)
-  #   it "should store a plane in the airports planes variable" do
-  #     expect(subject.plane).to eq plane
   it "lands a plane" do
     plane = Plane.new
-    expect(subject.land(plane)).to eq plane
-  end
-  it "stores a plane" do
-    plane = Plane.new
+    allow(subject).to receive(:stormy_weather?) { false }
     subject.land(plane)
-    expect(subject.plane).to eq plane
+    expect(plane).to eq subject.planes[0]
   end
+  # it "stores a plane" do
+  #   plane = Plane.new
+  #   allow(subject).to receive(:stormy_weather?) { false }
+  #   subject.land(plane)
+  #   expect(subject.plane).to eq plane
+  # end
   it { is_expected.to respond_to(:take_off) }
-  it "lets a plane take off" do
+  it "lets one plane land and take off" do
     plane = Plane.new
+    allow(subject).to receive(:stormy_weather?) { false }
     subject.land(plane)
     subject.take_off
-    expect(subject.plane).to eq nil
+    expect(subject.planes).to eq []
   end
-  it { is_expected.to respond_to(:weather) }
-
-  # it { is_expected.to respond_to(:stormy?) }
-
-  # context "stormy weather" do
-  #   let(:airport) { Airport.new }
-  #   let(:plane) { Plane.new }
-  #   it "should not let a plane land when weather is stormy" do
-  #     airport.stormy? = true
-  #     expect { airport.land(plane) }.to raise_error("The weather is too stormy to land")
-  #   end
-  # end
-
+  context "stormy weather" do
+    let(:airport) { Airport.new }
+    let(:plane) { Plane.new }
+    it "should not let a plane land when weather is stormy" do
+      allow(airport).to receive(:stormy_weather?) { true }
+      expect { airport.land(plane) }.to raise_error("It is too stormy to land")
+    end
+  end
+  context "plane is landed and weather is stormy" do
+    let(:airport) { Airport.new }
+    let(:plane) { Plane.new }
+    it "should not let a plane take off when weather is stormy" do
+      allow(airport).to receive(:stormy_weather?) { false }
+      airport.land(plane)
+      allow(airport).to receive(:stormy_weather?) { true }
+      expect { airport.take_off }.to raise_error("It is too stormy to take off")
+    end
+  end
+  context "Plane is landed weather is not stormy" do
+    let(:airport) { Airport.new }
+    let(:plane) { Plane.new }
+    it "should let a plane take off when weather is not stormy" do
+      allow(airport).to receive(:stormy_weather?) { false }
+      airport.land(plane)
+      allow(airport).to receive(:stormy_weather?) { false }
+      airport.take_off
+      expect(airport.plane).to eq nil
+    end
+  end
+  context "Airport is full." do
+    let(:airport) { Airport.new(1) }
+    let(:plane) { Plane.new }
+    let(:plane2) { Plane.new }
+    it "Should not land when airport is full" do
+      allow(airport).to receive(:stormy_weather?) { false }
+      airport.land(plane)
+      expect { airport.land(plane2) }.to raise_error("Airport is full")
+    end
+  end
+  it "has a default capacity" do
+    expect(subject.capacity).to eq 10
+  end
+  context "#initialize" do
+    let(:airport) { Airport.new(15) }
+    it "Default capacity can be changed when airport is made" do
+      expect(airport.capacity).to eq 15
+    end
+  end
+  context "After DEFAULT_CAPACITY has been set" do
+    let(:airport) { Airport.new }
+    it "lets capacity be altered post creation" do
+      airport.capacity = 5
+      expect(airport.capacity).to eq 5
+    end
+  end
+  context "landed planes added to the planes array" do
+    let(:plane) { Plane.new }
+    let(:plane2) { Plane.new }
+    it "lets multiple planes land" do
+      allow(subject).to receive(:stormy_weather?) { false }
+      subject.land(plane)
+      subject.land(plane2)
+    end
+  end
 end
