7a479756f0f7544fe26711caef11511cae113c11
Charlene Bastians <charlenebastians@hotmail.com>
2018-02-23 15:46:13 -0800

    genereate planes method

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,13 +1,23 @@
-require 'plane'
+
 
 class Airport
-  attr_reader :@airport_gates, :capacity
+  attr_reader :airport_gates, :capacity
 
   def initialize(capacity = 20)
     @airport_gates = []
     @capacity = capacity
   end
-
+  
+  def generate_planes
+    @capacity.times {@airport_gates.push(Plane.new)}
+    reg_no = 1
+    index = 0
+    while reg_no <= @capacity
+      @airport_gates[index] = "Plane#{reg_no}"
+      reg_no += 1
+      index += 1
+    end
+  end
 
   def land_plane
     @airport_gates.push
--- lib/plane.rb
+++ lib/plane.rb
@@ -1,8 +1,14 @@
-require 'airport'
 
-class plane
+
+class Plane
   def generate_planes
     @capacity.times {@airport_gates.push(Plane.new)}
     reg_no = 1
+    index = 0
     while reg_no <= @capacity
+      @airport_gates[index] = "Plane#{reg_no}"
+      reg_no += 1
+      index += 1
+    end
+  end 
 end
