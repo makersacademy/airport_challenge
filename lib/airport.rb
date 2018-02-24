f0f6858ad0c1aceb57aa1eb1222c0892a359a465
danielcampos <ddregalo@gmail.com>
2018-02-24 18:10:25 +0000

    Add land and takeoff methods and passing tests to Airport class

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,8 +1,18 @@
-require_relative './lib/plane'
+require_relative '../lib/plane'
 
 class Airport
+  attr_reader :landed_planes
 
-  def instruct_land
+  def initialize
+    @landed_planes = []
+  end
 
+  def land(plane)
+    @landed_planes << plane
+  end
+
+  def takeoff(plane)
+    @landed_planes.delete(plane)
+    @landed_planes
   end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,11 +2,39 @@ require_relative '../lib/airport'
 
 describe Airport do
 # testing the Airport class
-  describe '#instruct_land' do
+  describe '#land' do
     # testing for instruct_land method
-    # it should 'set @plane_to_land equal to true' do
-    #
-    # end
+    it 'should land plane into @landed_places' do # test landing 1 plane
+      london = Airport.new
+      plane = Plane.new
+      expect(london.land(plane)).to eq([plane])
+    end
+
+    it 'should land 2 planes into @landed_places' do # test landing 2 plane
+      london = Airport.new
+      plane1 = Plane.new
+      plane2 = Plane.new
+      london.land(plane1)
+      expect(london.land(plane2)).to eq([plane1, plane2])
+    end
   end
 
+  describe '#takeoff' do
+    # testing the takeoff method
+    it 'should takeoff a plane' do # test single plane takeoff
+      london = Airport.new
+      plane = Plane.new
+      london.land(plane)
+      expect(london.takeoff(plane)).to eq([])
+    end
+
+    it 'should return empty airport' do # test there is no planes in airport after takeoff
+
+      london = Airport.new
+      plane = Plane.new
+      london.land(plane)
+      london.takeoff(plane)
+      expect(london.landed_planes).to eq([])
+    end
+  end
 end
