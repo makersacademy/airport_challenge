b24bcf9f02cad9e1457f379e61d70cf08ddbb669
durranee <durranee@hotmail.com>
2018-02-24 12:39:13 +0000

    populated airport.rb and airport_spec.rb with tests and empty methods

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,3 +1,17 @@
 # Airport class definition
 class Airport
+  attr_accessor :capacity, :weather
+
+# initialize method to set default values if need to
+  def initialize; end
+
+# land method to allow planes to land
+  def land; end
+
+  def take_off; end
+
+  def prevent_landing; end
+
+  def prevent_take_off; end
+
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,4 +1,20 @@
 require 'airport'
 describe Airport do
 
+  # airport new (initialize) method check
+  it { should respond_to(:new) }
+
+  # airport land method check
+  it { should respond_to(:land) }
+  # it { should respond_to(:land).with.(1).arguments }
+
+  # airport take off method check
+  it { should respond_to(:take_off) }
+  # it { should respond_to(:take_off).with.(1).arguments }
+
+  # airport prevent landing method check
+  it { should respond_to(:prevent_landing) }
+
+  # airport prevent take off method check
+  it { should respond_to(:prevent_take_off) }
 end
