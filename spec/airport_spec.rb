3f02982a453c93fcde918f1934cb11c73b7676f1
Antonio <ciniglioantonio@gmail.com>
2018-02-24 19:37:28 +0000

    Add stormy test for take off and refactor further

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -7,7 +7,7 @@ describe Airport do
   let(:plane) { double :plane }
 
   describe '#instruct_landing' do
-    context "when weather not stormy" do
+    context 'when weather not stormy' do
       before do
         allow(airport).to receive(:stormy?).and_return(false)
       end
@@ -16,7 +16,7 @@ describe Airport do
         expect(airport).to respond_to(:instruct_landing).with(1).argument
       end
 
-    context 'when full ' do
+    context 'when full' do
       it 'does not allow planes to land' do
         20.times { airport.instruct_landing(plane) }
         expect { airport.instruct_landing(plane) }.to raise_error 'Cannot land. Airport is full'
@@ -24,7 +24,7 @@ describe Airport do
     end
   end
 
-    context "when weather stormy" do
+    context 'when weather stormy' do
       it 'does not allow plane to land' do
         allow(airport).to receive(:stormy?).and_return(true)
         expect { airport.instruct_landing(plane) }.to raise_error 'Cannot land due to stormy weather'
@@ -33,8 +33,17 @@ describe Airport do
   end
 
   describe '#instruct_take_off' do
-    it 'instructs a plane to take off' do
-      expect(airport).to respond_to(:instruct_take_off).with(1).argument
+    context 'when weather not stormy' do
+      it 'instructs a plane to take off' do
+        expect(airport).to respond_to(:instruct_take_off).with(1).argument
+      end
+    end
+
+    context 'when weather stormy' do
+      before do
+        allow(airport).to receive(:stormy?).and_return(true)
+        expect { airport.instruct_landing(plane) }.to raise_error 'Cannot land due to stormy weather'
+      end
     end
   end
 end
