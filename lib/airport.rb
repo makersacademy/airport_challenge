34e9e5678a03792b361114339cf942faa63456a0
durranee <durranee@hotmail.com>
2018-02-24 12:40:55 +0000

    added some comments for methods

--- lib/airport.rb
+++ lib/airport.rb
@@ -8,10 +8,13 @@ class Airport
 # land method to allow planes to land
   def land; end
 
+# take_off method to allow planes to take off
   def take_off; end
 
+# prevent_landing method to prevent landing
   def prevent_landing; end
 
+# prevent_take_off method to prevent planes from taking off
   def prevent_take_off; end
 
 end
