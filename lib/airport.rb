982f93242177262d11e2d9802039a247a96a58fa
Gia1987 <giacomo.n27@gmail.com>
2018-02-25 20:07:30 +0000

    Add default capacity and basic function to 'landing, take_off' methods, full tested

--- lib/airport.rb
+++ lib/airport.rb
@@ -2,16 +2,28 @@
 require_relative 'plane'
 class Airport
 
-  def landing(plane)
+  CAPACITY = 5
+  attr_reader :capacity
+  attr_reader :hangar
 
+  def initialize(capacity = CAPACITY)
+    @hangar = []
+    @capacity = capacity
   end
 
-  def take_off
+  def landing(plane)
+    @hangar << plane
+  end
 
+  def take_off(plane)
+     plane = @hangar.delete_at(-1)
+     plane
+    # puts " A plane left the Hangar!"
   end
 
   def full?
-
+    # @hangar.count >= @capacity
   end
+
 
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,5 +2,27 @@ require './lib/airport'
 
 describe Airport do
 
+  before(:each) do
+    @plane = Plane.new
+  end
+
   it { should respond_to(:landing, :full?, :take_off,) }
+
+
+  # it { expect(subject.full?).to eq false}
+  describe '#landing' do
+    it { should respond_to(:landing).with(1).argument }
+    # A plane is landing to the airport'
+    it "a plane is landing to the airport" do
+    expect(subject.landing(@plane).last).to eq @plane
+    end
+  end
+
+  describe '#take_off' do
+    it { should respond_to(:take_off).with(1).argument }
+    it " allow plane to leave the airport" do
+    subject.landing(@plane)
+    expect(subject.take_off(@plane)).to eq @plane
+   end
+  end
 end
