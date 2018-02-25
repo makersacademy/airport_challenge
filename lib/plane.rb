1cfd79db9f6e69e6697f5a04c9fa23651b423640
agnesdonat <agi.donat@gmail.com>
2018-02-25 23:18:41 +0000

    Sunday night commit

--- lib/plane.rb
+++ lib/plane.rb
@@ -1,6 +1,15 @@
 require_relative 'weather'
+require_relative 'airport'
 
 class Plane
+
+  def landing
+    airport.land(Plane.new)
+  end
+
+  def takeoff
+  end
+
   def left_airport?
     true
   end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -1,5 +1,12 @@
 require 'plane'
 
 describe Plane do
-  it { is_expected.to respond_to :left_airport? }
+  it { is_expected.to respond_to :left_airport?, :landing, :takeoff }
+
+  describe '#landing' do
+    it 'gives permission to land' do
+      airport = double(:airport)
+      expects(airport.land(subject)).to eq plane
+    end
+  end
 end
