5f9d94aa515b0dd20e853e4cfd61b5ffa9ec5090
Reenakoudi <kreena92@gmail.com>
2018-02-25 20:41:14 +0000

    Updated take_off method according to stormy weather

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,22 +1,28 @@
-require './lib/plane.rb'
+require_relative 'plane'
+require_relative 'weather'
 class Airport
-  attr_reader :hangar, :plane
+  attr_reader :hangar, :plane, :weather
 
   def initialize
     @hangar = []
+    @weather = Weather.new
   end
 
   def land(plane)
-    @plane = plane
-    @hangar << @plane
+    @hangar << plane
   end
 
-  def take_off
-    @hangar.pop
+  def take_off(plane)
+    fail 'Take off cancelled due to bad weather' if @weather.stormy? == true
+    @hangar.delete(plane)
     status
   end
 
   def status
-    "#{@hangar.count} planes at the airport"
+    if @hangar.count < 2
+      "#{@hangar.count} plane at the airport"
+    else
+      "#{@hangar.count} planes at the airport"
+    end
   end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,21 +1,25 @@
 require 'airport'
 require 'plane'
+require 'weather'
 
 describe Airport do
+  subject(:airport) { described_class.new }
+  subject(:plane) { described_class.new }
   describe '#land' do
     it 'should land a plane' do
-      airport = Airport.new
-      plane = Plane.new
       expect(airport.land(plane)).to eq [plane]
     end
   end
   describe '#take_off' do
-    it 'should take off the plane and confirm its gone' do
-      airport = Airport.new
-      plane = Plane.new
+    it 'should not allow take off when weather is stormy' do
+      allow(airport.weather).to receive(:stormy?).and_return true
+      expect { airport.take_off(plane) }.to raise_error 'Take off cancelled due to bad weather'
+    end
+    it 'should take off the plane when weather is sunny and confirm its gone' do
+      allow(airport.weather).to receive(:stormy?).and_return false
       airport.land(plane)
-      airport.take_off
-      expect(airport.status).to eq "0 planes at the airport"
+      airport.take_off(plane)
+      expect(airport.status).to eq "0 plane at the airport"
     end
   end
 end
