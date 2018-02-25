7d3f815b4687446f845d5713ec0ad51b62c19b7c
Vivian Allen <mrvivianallen@gmail.com>
2018-02-25 08:56:04 +0000

    weather refactored for review

--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -1,12 +1,21 @@
 require 'weather'
 
 describe Weather do
+
+  subject(:weather) { described_class.new }
+
+  before(:each) do
+    @weather_ary = []
+    100.times { @weather_ary.push(weather.report) }
+  end
+
   describe '#report' do
-    it 'reports both clear or stormy' do
-      weather_ary = []
-      100.times { weather_ary.push(subject.report) }
-      expect(weather_ary.include?(:clear)).to eq true
-      expect(weather_ary.include?(:stormy)).to eq true
+    it 'has clear weather' do
+      expect(@weather_ary.include?(:clear)).to eq true
+    end
+    it 'has stormy weather' do
+      expect(@weather_ary.include?(:stormy)).to eq true
     end
   end
+
 end
