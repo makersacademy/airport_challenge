4db44554da9a10a884afcb937f51fef4a978f2f4
agnesdonat <agi.donat@gmail.com>
2018-02-25 11:51:41 +0000

    Using instance variable with attr_reader

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,7 +1,21 @@
 require_relative 'plane'
 
 class Airport
+
   def take_off
     Plane.new
   end
+
+  def land(plane)
+    @plane = plane
+  end
+
+# this is an attribute reader, i.e. read the @plane attribute on an instance
+  # def plane
+  #   @plane
+  # end
+
+# shorthand to write the method above
+  attr_reader :plane
+
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -9,4 +9,20 @@ describe Airport do
     expect(plane).to be_left_airport
   end
 
+  it { is_expected.to respond_to(:land).with(1).argument }
+
+  it 'docks a landing plane' do
+    plane = Plane.new
+    expect(subject.land(plane)).to eq plane
+  end
+
+# turning #plane method into attribute reader the below is not needed
+  #  it { is_expected.to respond_to :plane }
+
+  # it 'returns the plane that landed' do
+  #   plane = Plane.new
+  #   subject.land(plane)
+  #   expect(subject.plane).to eq plane
+  # end
+
 end
