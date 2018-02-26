8210e07ca1d37ac059dfa8d0f3e1114f574f64b3
Hemesh-Unka <hemeshunka@gmail.com>
2018-02-26 06:34:34 +0000

    Removed comments from airport_spec.rb

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -3,23 +3,18 @@ require 'plane'
 require 'weather'
 
 describe Airport do
-  # set class up explicitly
-  airport(:airport) { described_class.new }
+  subject(:airport) { described_class.new }
 
-  # create some dummy weather conditions
   before(:each) do
     @fake_good_weather = double(:weather, :stormy? => false)
     @fake_stormy_weather = double(:weather, :stormy? => true)
   end
 
-  # create a fake plane to use across my tests
   let(:fake_plane) { double() }
 
   describe '#land' do
     it 'makes a plane land in airport' do
-      # tell Airport Class(airport) to land my dummy plane into a hangar
       airport.land(fake_plane, @fake_good_weather)
-      # expect hangar array to include the fake_plane
       expect(airport.hangar).to include(fake_plane)
     end
 
@@ -28,7 +23,6 @@ describe Airport do
     end
 
     it 'if hangar capacity is reached, and plane requests to land, throw an error' do
-      # force hangar to reach capacity by filling it with fake planes
       Airport::DEFAULT_CAPACITY.times { airport.land(fake_plane, @fake_good_weather) }
       expect { airport.land(fake_plane, @fake_good_weather) }.to raise_error(RuntimeError)
     end
@@ -36,11 +30,8 @@ describe Airport do
 
   describe '#take_off' do
     it 'makes a plane take off from airport' do
-      # tell Airport Class(airport) to land my dummy plane into a hangar
       airport.land(fake_plane, @fake_good_weather)
-      # tell Airport Class(airport) to take off a plane from the hangar
       airport.take_off(fake_plane, @fake_good_weather)
-      # expect hangar array to not include plane
       expect(airport.hangar).not_to include(fake_plane)
     end
 
@@ -55,7 +46,6 @@ describe Airport do
 
   describe '@capacity' do
     it 'if capacity is specified, it overwrites the default capacity' do
-      # see if a custom capacity can be set
       expect(Airport.new(100).capacity).to eq(100)
     end
   end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -1,6 +1,6 @@
 describe Plane do
   describe 'status' do
-    it 'plane status is flying if not on the ground' do
+    it 'plane status is flying if not in hangar' do
       expect(subject.status).to eq 'flying'
     end
   end
