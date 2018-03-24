912001b531d48dfa14ee0df11c984234343635af
Jordan Brooks <jordanbrooks@MacBook-Pro-9.local>
2018-03-24 16:45:08 +0000

    passing tests Airport.add_plane

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,8 +1,13 @@
 class Airport
-  attr_reader :max_capacity
+  attr_reader :max_capacity, :hangar
 
   def initialize(max_capacity = 100)
     @max_capacity = max_capacity
+    @hangar = Array.new 
+  end 
+  
+  def add_plane(airplane)
+    @hangar << airplane
   end 
   
   def weather
