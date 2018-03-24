7ed83ee3a6ab004d07c2029e8cd6eacd08dff6ea
Matthew <matt_burgoyne@live.co.uk>
2018-03-24 07:44:55 +0000

    first test written for a plane to respond to a land message passed

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -1,1 +1,10 @@
 require 'plane'
+
+describe Plane do
+  subject(:plane) { described_class.new }
+
+  describe '#land_plane'
+  it 'responds to land_plane' do
+    expect(plane).to respond_to(:land_plane)
+  end
+end
