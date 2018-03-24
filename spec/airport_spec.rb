4fb084207e6aa82d6b616d87ed49af54a7b33500
North <blarvinius@gmail.com>
2018-03-24 22:05:57 +0000

    add test for #land taking instance of Airport as arg

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,7 +2,9 @@ require 'airport'
 
 describe Airport do
 
+  describe '#stormy' do
 
+  end
 
 
 
@@ -11,7 +13,11 @@ end
 describe Airplane do
 
   describe '#land' do
-    it { is_expected.to respond_to(:land).with(1).argument }
+    it { is_expected.to respond_to(:land).with(1).argument } # that is QUITE what I want... I want to test that #land takes 1 argument.
+
+    it 'expects the argument to be an instance of Airport' do
+      expect { subject(:land).(instance_of(Airport)) }
+    end
 
   end
 
