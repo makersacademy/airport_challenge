95326ac9141f78f0e7e012659749c52dc532bd50
Sam Niechcial <samniechcial@gmail.com>
2018-03-25 10:52:01 +0100

    User story 1 completed - Plane can land @ Airport - RGR done, 0 Rubocop offenses

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,3 +1,17 @@
 class Airport
 
+  attr_accessor :ramp
+  attr_reader :capacity
+  attr_reader :name
+
+  def initialize(capacity = 1, name = "gatwick")
+    @capacity = capacity
+    @name = name
+    @ramp = []
+  end
+
+  def land(plane)
+    @ramp << plane
+  end
+
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -3,5 +3,12 @@ require 'plane'
 require 'weather'
 
 describe Airport do
+  subject(:gatwick) { described_class.new }
+  let(:ggajb) { double :plane }
 
-end 
+  it 'has the plane after landing' do
+    gatwick.land(ggajb)
+    expect(gatwick.ramp).to include ggajb
+  end
+
+end
