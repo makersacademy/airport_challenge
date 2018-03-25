1cbdc5918d8a113d6da30dfd7ffa0f72ffa76172
Daniel Tooke <daniel.tooke@gmail.com>
2018-03-25 21:37:24 +0100

    Made land method push planes to plane_array

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,6 +1,13 @@
 class Airport
 
+attr_reader :plane_array
+
+def initialize
+  @plane_array = []
+end
+
   def land(plane)
+  @plane_array << plane
   end
 
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -6,13 +6,24 @@ describe Airport do
     expect(Airport).to be_instance_of Class
   end
 
-  describe '#land'
-    it 'should have a land method' do
+  it 'should have an accessible collector to store plane objects' do
+    expect(subject).to respond_to(:plane_array)
+  end
+
+  describe '#land' do
+    it 'should exist as a method' do
       expect(subject).to respond_to(:land)
     end
 
-    it 'should take a plane in the land method argument' do
+    it 'should take one object (plane) as an argument' do
       expect(subject).to respond_to(:land).with(1).argument
     end
 
+    it 'should store the argument in plane_array' do
+      plane = double(plane)
+      subject.land(plane)
+      expect(subject.plane_array).to include(plane) 
+    end
+
+  end
 end
