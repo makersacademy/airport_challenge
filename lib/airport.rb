6cdd9105222bc9cdec7ebbb4ec2ec1abd4b2eab6
Gadiza Zerari <gadizaz@hotmail.com>
2018-03-25 11:18:43 +0100

    second user story

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,9 +1,16 @@
 require 'plane'
 
 class Airport
+  def initialize
+    @planes = []
+  end
 
   def land(plane)
+    @planes << plane
+  end
 
+  def take_off
+    @planes.pop
   end
 
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,5 +1,20 @@
 require 'airport'
+require 'plane'
 
 describe Airport do
   it { is_expected.to respond_to(:land).with(1).argument }
+  describe '#land' do
+    it "Docks plane when landed" do
+      airport = Airport.new
+      plane = Plane.new
+      expect(airport.land(plane)).to include(plane)
+    end
+  end
+  describe '#take_off' do
+    it 'Plane takes off from airport' do
+      airport = Airport.new
+      airport.land(Plane.new)
+      expect(airport.take_off).to be_an_instance_of Plane
+    end
+  end
 end
