0b79eea5c866df8f9f381636e17bb23e8263ff17
Jordan Brooks <jordanbrooks@MacBook-Pro-9.local>
2018-03-25 12:32:25 +0100

    failing tests for Weather.stormy?

--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -5,5 +5,22 @@ describe Weather do
     it 'initialises with a weather string' do 
       expect(subject.current).to be_a(String)
     end
+    
+    subject(:is_stormy) { described_class.new('stormy') }
+    it 'accepts an initial state' do 
+      5.times { expect(is_stormy.current).to eq('stormy') }
+    end 
   end
+
+  describe '.stormy?' do 
+    subject(:is_stormy) { described_class.new('stormy') }
+    it 'returns true when weather is stormy' do 
+      expect(is_stormy.stormy?).to eq(true)
+    end 
+    
+    subject(:is_grey) { described_class.new('grey') }
+    it 'returns false when weather is not stormy' do 
+      expect(is_grey.stormy?).to eq(false)
+    end 
+  end 
 end 
