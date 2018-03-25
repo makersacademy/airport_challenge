d4a84d84ca99b9050a1cca223ab8eaf9be0c82e9
Shih Han <swang05@mail.bbk.ac.uk>
2018-03-26 00:19:30 +0100

    04 new plane generate a name

--- lib/plane.rb
+++ lib/plane.rb
@@ -1,3 +1,13 @@
 class Plane
+  @@rl = "_ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  attr_accessor :plane
+  def self.new
+    @name = ""
+    @name << @@rl[rand(26).to_i]
+    @name << @@rl[rand(26).to_i]
+    @name << rand(100..999).to_s
+  end
 
 end
+
+# puts Plane.new
