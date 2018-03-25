c6e0d3db0af2d12046f9181f57c78cf1801bcb3c
Marwa Elali <marwaelali@gmail.com>
2018-03-26 00:44:41 +0200

    stuck trying to pass test when weather is stormy

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,9 +1,23 @@
 class Airport
+  attr_reader :plane
+
+  def initialize
+    @planes = []
+  end
+
   def land(plane)
     @plane = plane
+    fail "Cannot land due to stormy weather" if @state == 10
+    @planes.push(plane)
   end
 
   def takeoff
+    fail "Cannot takeoff due to stormy weather" if @state == 10
     @plane
+  end
+
+  def weather
+    @state = (1..10).rand
+    return "stormy" if @state.rand == 10
   end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,18 +2,12 @@ require "airport.rb"
 
 describe Airport do
   it "is expected to respond to the method :land" do
-    airport = Airport.new
     is_expected.to respond_to(:land)
   end
 
   it "should respond to the method :land with one argument" do
     is_expected.to respond_to(:land).with(1).argument
   end
-
-  # it "gets a plane in the airport when passed :land" do
-  #   airport = Airport.new
-  #   expect(airport.land(plane)).to be_an_instance_of(Plane)
-  # end
 
   it "should respond to the method :takeoff" do
     is_expected.to respond_to(:takeoff)
@@ -25,15 +19,21 @@ describe Airport do
     expect(airport.takeoff).to be_an_instance_of(Plane)
   end
 
-  it "returns true if plane is in the airport" do
+  it "returns false if plane is not in the airport" do
     airport = Airport.new
     airport.land(Plane.new)
     expect(airport.takeoff.in_airport?).to be(false)
   end
 
-  # it "should takeoff a plane from the airport" do
-  #   airport = Airport.new
-  #   expect(airport.takeoff)
-  # end
+  it "prevents takeoff when weather is stormy" do
+    double(:weather, "stormy")
+    airport = Airport.new
+    expect { airport.takeoff }.to raise_error("Cannot takeoff due to stormy weather")
+  end
 
+  it "prevents landing when weather is stormy" do
+    allow(Airport).to receive(:weather).with(10)
+    airport = Airport.new
+    expect { airport.land(Plane.new) }.to raise_error("Cannot takeoff due to stormy weather")
+  end
 end
