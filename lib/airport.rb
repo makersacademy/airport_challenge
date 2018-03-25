41cd553afff580e884014c19e4c979f4ce931517
Shih Han <swang05@mail.bbk.ac.uk>
2018-03-26 00:20:29 +0100

    05 not allowed to land and take off when stormy

--- lib/airport.rb
+++ lib/airport.rb
@@ -7,11 +7,20 @@ class Airport
   end
 
   def plane_land(plane)
+    raise 'Plane is not allowed to land when stormy' if now_stormy?
     @hanger.push(plane)
   end
 
-  def plane_take_off
-    @hanger.pop
+  def plane_take_off(plane)
+    raise 'Plane is not allowed to take off when stormy' if now_stormy?
+    @hanger.delete(plane)
+    return @hanger
+  end
+  private
+
+  def now_stormy?
+    return true if Weather.new.current_weather == "stormy"
+    false
   end
 
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,5 +1,6 @@
 require 'airport'
 require 'plane'
+require 'weather'
 
 describe Airport do
 
@@ -7,18 +8,35 @@ describe Airport do
 
     it 'plane land at an airport' do
       airport = Airport.new
+      allow(Kernel).to receive(:rand).with(0..5).and_return(0)
       plane = Plane.new
       expect(airport.plane_land(plane)).to include(plane)
     end
 
+    it 'raises an error that plane is not allowed to land when stormy' do
+      airport = Airport.new
+      plane = Plane.new
+      allow(Kernel).to receive(:rand).with(0..5).and_return(5)
+      expect { airport.plane_land(plane) }.to raise_error 'Plane is not allowed to land when stormy'
+    end
+
   end
 
-  describe 'plane_take_off' do
+  describe '#plane_take_off' do
 
     it 'plane take off from an airport' do
       airport = Airport.new
-      airport.plane_land(Plane.new)
-      expect(airport.plane_take_off).to be_an_instance_of(Plane)
+      plane = Plane.new
+      airport.plane_land(plane)
+      allow(Kernel).to receive(:rand).with(0..5).and_return(0)
+      expect(airport.plane_take_off(plane)).not_to include(plane)
+    end
+
+    it 'raises an error that plane is not allowed to take off when stormy' do
+      airport = Airport.new
+      plane = Plane.new
+      allow(Kernel).to receive(:rand).with(0..5).and_return(5)
+      expect { airport.plane_take_off(plane) }.to raise_error 'Plane is not allowed to take off when stormy'
     end
 
   end
