90b98a968d69cb52d17234748eb0dd3a39539c52
Jordan Brooks <jordanbrooks@MacBook-Pro-9.local>
2018-03-25 17:21:06 +0100

    passing test TrafficController.request_landing

--- lib/traffic_controller.rb
+++ lib/traffic_controller.rb
@@ -3,6 +3,10 @@ class TrafficController
      airport.allow_takeoff?
   end 
 
+  def request_landing(airport)
+    airport.allow_landing?
+  end 
+
   def depart_airport(airplane)
     airplane.take_off
   end 
