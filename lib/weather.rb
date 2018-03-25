6f747b0151dccf3658c987142f7d0cd081b536ce
Shih Han <swang05@mail.bbk.ac.uk>
2018-03-25 21:28:44 +0100

    03 weather method and spec changed

--- lib/weather.rb
+++ lib/weather.rb
@@ -5,7 +5,17 @@ class Weather
     @weather = ["sunny", "sunny", "sunny", "sunny", "sunny", "stormy"]
   end
 
+  # def current_weather
+  #   @weather[rand(0..5)]
+  # end
+
   def current_weather
-    @weather[rand(0..5)]
+    return @weather[Kernel.rand(0..5)]
+    # return true if @weather_for_now == "stormy"
+    # false
   end
+
 end
+
+# weather = Weather.new
+# puts weather.current_weather
--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -3,11 +3,14 @@ require 'weather'
 describe Weather do
 
   describe '#current_weather' do
+    it 'return current weather is stormy' do
+      allow(Kernel).to receive(:rand).with(0..5).and_return(5)
+      expect(subject.current_weather).to eq "stormy"
+    end
 
-    it 'return sunny or stormy by random' do
-      weather = Weather.new
-      weather_for_now = weather.current_weather
-      expect(weather.weather).to include(weather_for_now)
+    it 'return current weather is sunny' do
+      allow(Kernel).to receive(:rand).with(0..5).and_return(0)
+      expect(subject.current_weather).to eq "sunny"
     end
   end
 end
