56a0065710afbd5879dbdc82ca58b55700b0219b
Jordan Brooks <jordanbrooks@MacBook-Pro-9.local>
2018-03-25 19:16:00 +0100

    replace logic with guard clause

--- lib/traffic_controller.rb
+++ lib/traffic_controller.rb
@@ -1,10 +1,22 @@
 class TrafficController
   def request_takeoff(airport)
-     airport.allow_takeoff?
+    airport.allow_takeoff?
   end 
 
   def request_landing(airport)
     airport.allow_landing?
+  end 
+  
+  def make_landing(airplane, airport)
+    raise 'cannot land at this airport' unless request_landing(airport)
+    airport.add_plane(airplane)
+    airplane.land_airplane
+  end
+
+  def leave_airort(airplane, airport)
+    raise 'cannot take off from this airport' unless request_takeoff(airport)
+    airport.remove_plane(airplane)
+    airplane.take_off
   end 
 
   def depart_airport(airplane)
