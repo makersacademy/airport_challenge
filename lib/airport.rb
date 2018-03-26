ed30c9a251a7e6709b377409c35108de33f58d2d
Marwa Elali <marwaelali@gmail.com>
2018-03-26 09:02:15 +0200

    some more refactoring

--- lib/airport.rb
+++ lib/airport.rb
@@ -6,17 +6,19 @@ class Airport
   end
 
   def land(plane)
-    fail "Cannot land due to stormy weather" if @state == 10
+    fail "Cannot land due to stormy weather" if @state == "stormy"
     @plane = plane
     @planes.push(plane)
   end
 
   def takeoff
-    fail "Cannot takeoff due to stormy weather" if @state == 10
+    fail "Cannot takeoff due to stormy weather" if @state == "stormy"
     @plane
   end
+end
 
-  def weather
+class Weather
+  def state
     @state = (1..10).rand
     return "stormy" if @state.rand == 10
   end
