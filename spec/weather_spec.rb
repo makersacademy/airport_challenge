c554fc25ea69dae678d59c11ed4b60caf2d14640
William Powell <william.powell92@gmail.com>
2018-04-20 19:51:24 +0100

    Refactored wind speed spec file

--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -1,7 +1,11 @@
 require 'weather'
 
 describe Weather do
-  it { is_expected.to respond_to(:stormy?) }
+  describe '#stormy?' do
+    it { is_expected.to respond_to(:stormy?) }
+  end
 
-  it { is_expected.to respond_to(:wind_speed) }
+  describe '#wind_speed' do
+    it { is_expected.to respond_to(:wind_speed) }
+  end
 end
