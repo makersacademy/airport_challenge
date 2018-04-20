e7e315ffc046782064a1fce0f1dc1914fc3d0aca
William Powell <william.powell92@gmail.com>
2018-04-20 21:55:39 +0100

    Landed planes cant land

--- lib/plane.rb
+++ lib/plane.rb
@@ -8,10 +8,17 @@
    end
 
    def land
+     raise 'Plane already landed' if landed?
      @flying = false
    end
 
    def take_off
      @flying = true
+   end
+
+   private
+
+   def landed?
+     !@flying
    end
  end
