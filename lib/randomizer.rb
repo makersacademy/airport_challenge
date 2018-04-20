237f80aee560a6d4ff7b87260758057f5af0ed43
William Powell <william.powell92@gmail.com>
2018-04-20 20:18:05 +0100

    Randomizer returns random integers between two values

--- lib/randomizer.rb
+++ lib/randomizer.rb
@@ -1,5 +1,5 @@
 module Randomizer
   def self.rand_between(lower_boundary, upper_boundary)
-
+    lower_boundary + (rand * ((upper_boundary - lower_boundary) + 1)).floor
   end
 end
--- spec/randomizer_spec.rb
+++ spec/randomizer_spec.rb
@@ -4,9 +4,15 @@ describe Randomizer do
   describe '#rand_between' do
     it { is_expected.to respond_to(:rand_between).with(2).argument }
 
-    it 'is expected to return a random number between two values' do
+    it 'is expected to return a random number between 0 and a value' do
       50.times do
         expect(Randomizer.rand_between(0, 50)).to be_between(0, 50).inclusive
+      end
+    end
+
+    it 'is expected to return a random number between two values' do
+      50.times do
+        expect(Randomizer.rand_between(20, 50)).to be_between(20, 50).inclusive
       end
     end
   end
