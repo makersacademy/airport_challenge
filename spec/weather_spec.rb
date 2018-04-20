508b3c4807464b724096e5afd7c2e821ba121b98
William Powell <william.powell92@gmail.com>
2018-04-20 19:57:14 +0100

    Added failing test for weather wind speed

--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -7,5 +7,12 @@ describe Weather do
 
   describe '#wind_speed' do
     it { is_expected.to respond_to(:wind_speed) }
+
+    it 'gives an integer wind speed between 0 and 40' do
+      40.times do
+        weather = Weather.new
+        expect(weather.wind_speed).to be_between(0, 40).inclusive
+      end
+    end
   end
 end
