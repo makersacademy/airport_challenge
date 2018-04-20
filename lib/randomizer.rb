461e07cdafcf530d9190b884bf74efb5742dc09a
William Powell <william.powell92@gmail.com>
2018-04-20 20:13:06 +0100

    Added failing test for randomizer upper and lower bounds

---
+++ lib/randomizer.rb
@@ -0,0 +1,5 @@
+module Randomizer
+  def self.rand_between(lower_boundary, upper_boundary)
+
+  end
+end
--- spec/randomizer_spec.rb
+++ spec/randomizer_spec.rb
@@ -3,5 +3,11 @@ require 'randomizer'
 describe Randomizer do
   describe '#rand_between' do
     it { is_expected.to respond_to(:rand_between).with(2).argument }
+
+    it 'is expected to return a random number between two values' do
+      50.times do
+        expect(Randomizer.rand_between(0, 50)).to be_between(0, 50).inclusive
+      end
+    end
   end
 end
