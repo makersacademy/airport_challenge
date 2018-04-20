1e41f7ea7c2977301cfceb81d461799d207fc103
William Powell <william.powell92@gmail.com>
2018-04-20 21:43:01 +0100

    Added failing test for plane initializing as flying

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -3,5 +3,9 @@ require 'plane'
 describe Plane, :plane do
   describe '#flying?' do
     it { is_expected.to respond_to(:flying?) }
+
+    it 'is flying when initialized' do
+      expect(subject).to be_flying
+    end
   end
 end
