dcbc74f12b717d336802e5039c80f80e13a90e64
John Newman <newmanj903@gmail.com>
2018-04-21 11:18:32 +0100

    land method made to instruct plane to land at a airport

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -5,4 +5,11 @@ describe Plane do
     expect(Plane.new).to be_truthy
   end
 
+  describe '#land' do
+    it 'can instuct a plane to land in a aiport' do
+      is_expected.to respond_to(:land).with(1).argument
+    end
+
+  end
+
 end
