9124460866bed6b552a49e1e55754c52590424c7
Asia Antczak <aasiaantczak@gmail.com>
2018-04-21 18:15:55 +0100

    add status for flying plane

--- lib/plane.rb
+++ lib/plane.rb
@@ -1,8 +1,12 @@
 class Plane
 
-attr_reader :landed
+  attr_reader :landed
 
   def land
     @landed = true
+  end
+
+  def flying
+    @landed = false
   end
 end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -6,10 +6,14 @@ subject(:plane) { described_class.new }
 
   context "#land" do
     it 'returns status for landed planes' do
-      expect(subject).to be_truthy
+      expect(subject.land).to be_truthy
     end
   end
 
-
+  context "#flying" do
+    it 'returns status for flying plane' do
+      expect(subject.flying).to be_falsy
+    end
+  end
 
 end
