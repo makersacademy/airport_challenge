8f8c1625a17ba6c67e62ec45fb3a3e06199b5c3e
Mark Channer <markchanner@gmail.com>
2018-04-21 18:09:10 +0100

    Adds fail condition to take_off method for when whether is stormy

--- lib/airport.rb
+++ lib/airport.rb
@@ -11,9 +11,10 @@ class Airport
     @hangar.push(plane)
   end
 
-  def take_off(plane)
+  def take_off(plane, isStormy = false)
     fail 'No planes in airport' if @hangar.length <= 0
     fail 'Plane not in airport' if !@hangar.include?(plane)
+    fail 'Cannot take off in stormy weather' if isStormy
     plane_index = @hangar.index(plane)
     @hangar.delete_at(plane_index)
     plane.take_off
