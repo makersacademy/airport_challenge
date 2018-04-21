6a83d3ee8b1f3dbe95ca2f3aa48e103626b4946e
Salome Lambermont <salome.lambermont@gmail.com>
2018-04-21 10:44:38 +0100

    User story 2: allow a plane to take off and show that it is no longer in the aiport

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,7 +1,17 @@
 require_relative "plane.rb"
 
 class Airport
+  attr_reader :planes
+
+  def initialize
+    @planes = []
+  end
+
   def land(plane)
-    plane
+    @planes << plane
+  end
+
+  def take_off
+    @planes.pop
   end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -6,9 +6,26 @@ describe Airport do
     it 'should allow a plane to land' do
       expect(subject).to respond_to(:land)
     end
-    it 'should show that plane is in the airport' do
-      a_plane = Plane.new
-      expect(subject.land(a_plane)).to eq a_plane
+  end
+
+  describe "#take_off" do
+    it 'should allow a plane to take off' do
+      expect(subject).to respond_to(:take_off)
     end
   end
+
+  describe "#planes" do
+    it 'should show that the plane is in the airport' do
+      a_plane = Plane.new
+      subject.land(a_plane)
+      expect(subject.planes).to eq [a_plane]
+    end
+    it 'should show that the the plane is no longer in the airport' do
+      a_plane = Plane.new
+      subject.land(a_plane)
+      subject.take_off
+      expect(subject.planes).to eq []
+    end
+  end
+
 end
