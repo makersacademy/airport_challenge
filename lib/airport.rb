8b8d1cef0e51c3bc6d7d9b4f2320f14c95affdab
anderscodes <anders.j.persson@hotmail.com>
2018-04-21 15:00:13 +0100

    airport returns plane if one has landed

--- lib/airport.rb
+++ lib/airport.rb
@@ -2,16 +2,14 @@ require_relative 'plane.rb'
 
 class Airport
 
+attr_reader :plane
+
   def take_off
     Plane.new
   end
 
   def land(plane)
-    plane
+    @plane = plane
   end
-
-  def plane
-  end
-
 
 end
