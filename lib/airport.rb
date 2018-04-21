bddd5579f3eeb774767937d6a727614f2c02d169
benfurber <ben@furber-consultancy.com>
2018-04-21 13:11:32 +0100

    Tests/functionality for Airport.capacity and empty tests/class for weather

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,4 +1,13 @@
 require 'plane'
 
 class Airport
+  attr_reader :capacity
+
+  def initialize(capacity = 10)
+    @capacity = capacity
+  end
+
+  def change_capacity(capacity)
+    @capacity = capacity
+  end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,4 +1,19 @@
 require 'airport'
 
-describe Airport do
+describe 'Airport capacity' do
+  let(:subject) { Airport.new }
+
+  it 'Has a default capacity of 10' do
+    expect(subject.capacity).to equal 10
+  end
+
+  it 'The capacity can be changed' do
+    subject.change_capacity(15)
+    expect(subject.capacity).to equal 15
+  end
+
+  it 'Can be set by the user on initialisation' do
+    new_airport = Airport.new(30)
+    expect(new_airport.capacity).to equal 30
+  end
 end
