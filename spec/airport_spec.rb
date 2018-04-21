6c9168e309367df41e4890c5456487d07a285a2c
John Newman <newmanj903@gmail.com>
2018-04-21 11:07:15 +0100

    setup of files

---
+++ spec/airport_spec.rb
@@ -0,0 +1,8 @@
+require 'airport'
+
+describe Airport do
+  it 'can create a instance' do
+    expect(Airport.new).to be_truthy
+  end
+
+end
---
+++ spec/plane_spec.rb
@@ -0,0 +1,8 @@
+require 'plane'
+
+describe Plane do
+  it 'can create a instance' do
+    expect(Plane.new).to be_truthy
+  end
+
+end
