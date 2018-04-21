b5c0e6e0dbe8f228094652b9f67eeab11af0a644
George Sykes <georgesykes86@gmail.com>
2018-04-21 19:48:15 +0100

    Fixes plane tests after refactor

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -25,17 +25,17 @@ describe Plane do
       end
 
       it 'sets location to the controlling airport' do
-        subject.land(airport)
-        expect(subject.location?).to eq airport
+        plane.land(airport)
+        expect(plane.location?).to eq airport
       end
 
     end
 
     context 'when the plane is in the airport' do
-      before { subject.land(airport) }
+      before { plane.land(airport) }
 
       it 'raises an error' do
-        expect { subject.land(airport) }.to raise_error(PlaneError, "Plane already on the ground")
+        expect { plane.land(airport) }.to raise_error(PlaneError, "Plane already on the ground")
       end
     end
   end
@@ -44,44 +44,21 @@ describe Plane do
 
     context 'when the plane is in the air' do
       it "raises an error" do
-        subject.take_off
-        expect { subject.take_off }.to raise_error(PlaneError, "Plane already in-flight")
+        expect { plane.take_off }.to raise_error(PlaneError, "Plane already in-flight")
       end
     end
 
     context 'when the plane is not flying' do
-      before { subject.land }
+      before { plane.land(airport) }
 
       it "takes off" do
-        subject.take_off
-        expect(subject.flying?).to eq true
+        plane.take_off
+        expect(plane.flying?).to eq true
       end
 
-      it 'tells airport that it has taken off' do
-        expect(airport).to receive(:release_plane).with(subject)
-        subject.take_off
-      end
-    end
-
-
-  end
-
-
-
-  describe '#flying?' do
-    context 'when the plane has not taken off' do
-      it 'returns false' do
-        expect(subject.flying?).to eq false
-      end
-    end
-
-    context 'when a plane is flying' do
-      before do
-        subject.set_departure_airport(airport)
-        subject.take_off
-      end
-      it 'returns true' do
-        expect(subject.flying?).to eq true
+      it 'sets location to In-flight' do
+        plane.take_off
+        expect(plane.location?).to eq "In-flight"
       end
     end
   end
