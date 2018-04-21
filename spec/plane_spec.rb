812433f97cd6e12095990d34af072a7830c79dfd
Mark Channer <markchanner@gmail.com>
2018-04-21 13:52:02 +0100

    Adds two tests for plane's flying status

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -1,7 +1,14 @@
 require 'plane.rb'
 
 describe Plane do
-  it 'returns whether is if flying or landed' do
-    
+  context 'flying status' do
+    it 'has status of "landed" when on the ground' do
+      expect(subject.status).to eq 'landed'
+    end
+
+    it 'has status of "flying" after takeoff' do
+      subject.take_off
+      expect(subject.status).to eq 'flying'
+    end
   end
 end
