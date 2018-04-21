833b8e2c77dd0de31b28742e20be371e62dfb2b9
George Sykes <georgesykes86@gmail.com>
2018-04-21 14:52:43 +0100

    Adds template for first feature test

--- spec/feature_tests.rb
+++ spec/feature_tests.rb
@@ -1,9 +1,34 @@
 require_relative '../lib/airport'
 require_relative '../lib/plane'
 
-def set_up_instances
-  @airport1 = Airport.new
-  @airport2 = Airport.new
-  @plane1 = Plane.new
-  @plane2 = Plane.new
+class Tests
+
+  attr_reader :airport1, :airport2, :plane1, :plane2
+
+  def test_set_departure_airport
+    puts __method__.to_s
+    count = 0
+    print "   Test #{ count += 1 }: "
+    plane = Plane.new
+    airport = Airport.new
+    plane.set_departure_airport(airport)
+    if plane.departure_airport == airport && airport.planes.include?(plane)
+      puts "Success"
+    else
+      puts "Failure"
+    end
+    print "   Test #{ count += 1 }: "
+    begin
+      plane.set_departure_airport(airport)
+    rescue PlaneError
+      puts "Success"
+    else
+      puts "Failure"
+    end
+  end
+
+  def test_set_destination_airport
+
+  end
+
 end
