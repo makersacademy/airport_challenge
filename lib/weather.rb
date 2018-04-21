f1337fba7f966e12294bbeb7680d2175ca7960b2
Ellie Keen <elliekeen95@gmail.com>
2018-04-21 14:13:39 +0100

    Created Weather object & generate method

---
+++ lib/weather.rb
@@ -0,0 +1,5 @@
+class Weather
+  def generate
+    ['sunny', 'sunny', 'stormy'].sample
+  end
+end
---
+++ spec/weather_spec.rb
@@ -0,0 +1,23 @@
+require 'weather'
+
+describe Weather do
+
+  let(:weather) { Weather.new }
+
+  describe ' #generate ' do
+    it 'should generate a weather condition' do
+      expect(weather).to respond_to(:generate)
+    end
+
+    it 'should return sunny' do
+      expect_any_instance_of(Array).to receive(:sample).and_return('sunny')
+      expect(weather.generate).to eq('sunny')
+    end
+
+    it 'should return stormy' do
+      expect_any_instance_of(Array).to receive(:sample).and_return('stormy')
+      expect(weather.generate).to eq('stormy')
+    end
+  end
+
+end
