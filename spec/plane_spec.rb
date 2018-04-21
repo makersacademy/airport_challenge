e7933af34867646ae087c237e4f30b60d8acfd97
George Sykes <georgesykes86@gmail.com>
2018-04-21 09:12:19 +0100

    Adds test for flying?

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -2,6 +2,16 @@ require 'plane'
 
 describe Plane do
 
+  describe '#flying?' do
+    context " when the plane is in an airport " do
+        it 'returns false' do
+          expect(subject.flying?).to eq false
+        end
+    end
+
+
+  end
+
   describe '#land' do
 
     it 'lands the plane' do
