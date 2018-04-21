753e75d9cfe940dca5531aa080c15b561c463f80
Mark Channer <markchanner@gmail.com>
2018-04-21 17:11:41 +0100

    Adds confirmLeftAirport method

--- lib/airport.rb
+++ lib/airport.rb
@@ -13,12 +13,16 @@ class Airport
 
   def take_off(plane)
     fail 'No planes in airport' if @hangar.length <= 0
-    fail 'Plane not in airport' if !@hangar.include? plane
+    fail 'Plane not in airport' if !@hangar.include?(plane)
     plane_index = @hangar.index(plane)
     @hangar.delete_at(plane_index)
     plane.take_off
     puts "#{plane} has left the airport"
     return plane
+  end
+
+  def confirmLeftAirport(plane)
+    return !@hangar.include?(plane)
   end
 
 end
