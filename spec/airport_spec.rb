8cc8877e47cc94b90559e9f2627294b6e993d79c
Revilo1882 <oliverrharris@gmail.com>
2018-04-21 12:10:36 +0100

    layout changes

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -3,11 +3,12 @@ require 'plane'
 
 describe Airport do
 
+  let(:plane) { Plane.new }
+
   describe '#land' do
     it { is_expected.to respond_to(:land).with(1).argument }
 
     it 'lands a plane' do
-      plane = Plane.new
       expect(subject.land(plane)).to eq plane
     end
   end
