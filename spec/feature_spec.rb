db3cc16a3697bc3bf24fc464fb2bad5c73e362fe
Sam Worrall <samuel.worrall@hotmail.co.uk>
2018-04-21 16:51:13 +0100

    Added featur test for take off in stormy weather

--- spec/feature_spec.rb
+++ spec/feature_spec.rb
@@ -11,3 +11,11 @@ def take_off(plane)
   airport.take_off(plane)
   plane.status
 end
+
+def refuse_take_off
+  stormy
+  airport = Airport.new
+  plane = Plane.new
+  airport.land(plane)
+  airport.take_off(plane)
+end
