0da7f2059715eb884e3eeb3143e8c5617fdb88db
George Sykes <georgesykes86@gmail.com>
2018-04-21 10:59:10 +0100

    Adds ability ot store planes when they land

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -3,15 +3,11 @@ require 'airport'
 describe Airport do
   let(:flying_plane) { instance_double("Plane", land: self) }
 
-  it 'stores planes' do
-    expect(subject).to respond_to(:planes)
-  end
-
   describe '#land_plane' do
 
     it 'allows a plane to land' do
-      plane.land(subject)
-      expect(subject.planes).to include plane
+      subject.receive_plane(flying_plane)
+      expect(subject.planes).to include flying_plane
     end
   end
 
