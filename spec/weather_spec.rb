ada119d8f4aaaddadb4d63356da578aed39236cb
Habin <habin@hotmail.co.uk>
2018-04-22 12:27:33 +0100

    added expectation of it Weather class responding to wind speed method

--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -5,4 +5,9 @@ describe Weather do
   describe '#stormy?' do
     it { is_expected.to respond_to(:stormy?) }
   end
+
+  it { is_expected.to respond_to(:wind_speed) }
+  describe '#wind_speed' do
+    it { is_expected.to respond_to(:wind_speed) }
+  end
 end
