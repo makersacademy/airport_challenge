5f89f82da25ec1b13b0ee2fe2a8e466b0ea197ee
Habin <habin@hotmail.co.uk>
2018-04-22 12:26:04 +0100

    weather_spec, expected to respond to stormy? method

--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -1,5 +1,8 @@
+ require 'weather'
+
 describe Weather do
   it { is_expected.to respond_to(:stormy?) }
-
-  it { is_expected.to respond_to(:wind_speed) }
+  describe '#stormy?' do
+    it { is_expected.to respond_to(:stormy?) }
+  end
 end
