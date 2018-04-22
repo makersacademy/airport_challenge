a0091f8c1ae245eacfdf8309b97d3f34dc313d46
Ellie Keen <elliekeen95@gmail.com>
2018-04-22 14:24:53 +0100

    Refactored tests for readability

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -6,48 +6,49 @@ describe Airport do
   let(:weather) { Weather }
 
   it 'should have a hangar area for the planes' do
-    expect(airport.hangar.length).to eq 0
+    expect(airport.hangar).to eq([])
   end
 
-  it 'should have a read attribute' do
+  it 'should have a read attribute capacity' do
     expect(airport).to respond_to(:capacity)
   end
 
-  it 'should have a write attribute' do
+  it 'should have a write attribute capacity' do
     expect(airport).to respond_to(:capacity=)
   end
 
-  it 'should have a default capacity of 30 planes' do
+  it 'should have a default capacity' do
     expect(airport.capacity).to eq(Airport::DEFAULT_CAPACITY)
   end
 
   describe ' #land ' do
-    it 'should instruct a plane to land at an airport' do
+    it 'should allow a plane to land' do
       expect(airport).to respond_to(:land).with(1).argument
     end
 
-    it 'should park plane in hangar area' do
+    it 'should park planes in the hangar' do
       expect_any_instance_of(weather).to receive(:generate).and_return('sunny')
       airport.land(plane)
       expect(airport.hangar).to include(plane)
     end
 
-    it 'should prevent landing when weather is stormy' do
+    it 'raises an error when weather is stormy' do
       expect_any_instance_of(weather).to receive(:generate).and_return('stormy')
       expect { airport.land(plane) }.to raise_error(RuntimeError,
         "Unable to land due to weather conditions")
     end
 
-    it 'should raise an error when hangar has reached its capacity' do
+    it 'raises an error when hangar has reached full capacity' do
       expect_any_instance_of(weather).to receive(:generate).and_return('sunny')
       airport.instance_variable_set(:@hangar, [Plane.new, Plane.new, Plane.new])
-      expect { airport.land(plane) }.to raise_error(RuntimeError, "Unable to land. Hangar is full")
+      expect { airport.land(plane) }.to raise_error(RuntimeError,
+         "Unable to land. Hangar is full")
     end
 
   end
 
   describe ' #takeoff ' do
-    it 'should instruct a plane to take off from an airport' do
+    it 'should allow a plane to take off' do
       expect(airport).to respond_to(:takeoff).with(1).argument
     end
 
@@ -57,13 +58,13 @@ describe Airport do
       expect(airport.takeoff(plane)).to eq("The #{plane} is no longer in the Airport")
     end
 
-    it 'should prevent takeoff when weather is stormy' do
+    it 'raises an error when weather is stormy' do
       expect_any_instance_of(weather).to receive(:generate).and_return('stormy')
       expect { airport.takeoff(plane) }.to raise_error(RuntimeError,
         "Unable to take off due to weather conditions")
     end
 
-    it 'should remove plane from hangar area after take off' do
+    it 'should remove the correct plane from the hangar' do
       different_plane = Plane.new
       airport.instance_variable_set(:@hangar, [plane, different_plane])
       expect_any_instance_of(weather).to receive(:generate).and_return('sunny')
@@ -71,8 +72,7 @@ describe Airport do
       expect(airport.hangar).not_to include(plane)
     end
 
-    it 'should only release planes that are in the hangar' do
-      airport.instance_variable_set(:@hangar, [])
+    it 'raises an error when plane is not in the hangar' do
       expect_any_instance_of(weather).to receive(:generate).and_return('sunny')
       expect { airport.takeoff(plane) }.to raise_error(RuntimeError,
         "Plane not available in hangar for takeoff")
