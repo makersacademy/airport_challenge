d96df365b980ea5aeb61ab0f5b38d9cdbcc2727f
Charlene <charlenechetcuti@gmail.com>
2018-04-22 10:13:50 +0100

    wrote failing test for plane trying to land when it is already landing

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -7,12 +7,18 @@ describe Plane do
       subject.land
       expect(subject.landed).to eq true
     end
+
+    it 'raises an error when plane tries to lands if already landed' do
+      subject.land
+      expect { subject.land
+      }.to raise_error "Plane is already landed in an airport!"
+    end
   end
 
   describe '#take_off' do
     it 'takes off from airport when instructed by air traffic controller' do
       subject.take_off
-      expect(subject.landed).to eq false 
+      expect(subject.landed).to eq false
     end
   end
 
