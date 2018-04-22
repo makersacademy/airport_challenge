423fe8b26f0f07cebe46cd82484a9caac3dbf5ae
Muzzi Aldean <amiraldean@gmail.com>
2018-04-22 11:26:02 +0100

    Created methods for allowing plans to land at airports

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,2 +1,13 @@
 class Airport
-end 
+
+attr_accessor :planes
+
+def initialize
+  @planes = []
+end
+
+def land(plane)
+  @planes << plane
+end
+
+end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,4 +1,14 @@
 require 'airport'
 
-describe Airport do 
+describe Airport do
+let(:plane) {double :plane}
+  it "responds to landing method" do
+    expect(subject).to respond_to(:land).with(1).argument
+  end
+
+  it "allows a plane to land" do
+    subject.land(plane)
+    expect(subject.planes).to include(plane)
+  end
+
 end
