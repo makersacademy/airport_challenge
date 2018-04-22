a56e8b2bc5c43cd0f23e496634412df41653c641
Alastair Edmonds <Alastair.Edmonds@gmail.com>
2018-04-22 10:40:51 +0000

    Added feature test for take_off

--- spec/feature_test.rb
+++ spec/feature_test.rb
@@ -1,2 +1,11 @@
 require 'plane'
 require 'airport'
+
+def take_off
+  plane = Plane.new
+  airport = Airport.new
+  airport.lane(plane)
+  airport.take_off(plane)
+  airport.planes == []
+end
+    
