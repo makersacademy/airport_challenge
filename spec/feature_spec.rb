a0e1c17d09a339e51711ef178b086216f1c42c6d
Charlene <charlenechetcuti@gmail.com>
2018-04-22 16:01:25 +0100

    created feature spec file

---
+++ spec/feature_spec.rb
@@ -0,0 +1,15 @@
+# require './spec/spec_helper.rb'
+# require './lib/airport.rb'
+# require './lib/plane.rb'
+# require './lib/weather.rb'
+#
+# feature "feature" do
+#   scenario "Plane lands in airport" do
+#     plane = Plane.new
+#     airport = Airport.new
+#     weather = Weather.new(90)
+#     airport.plane_lands(plane, weather)
+#     p airport.planes
+#     p plane.landed
+#   end
+# end
