030f1a4490a74fe588559efa1751c7bf1ffc3507
Sam Worrall <samuel.worrall@hotmail.co.uk>
2018-04-22 13:10:10 +0100

    Refactor weather class

--- lib/weather.rb
+++ lib/weather.rb
@@ -1,11 +1,5 @@
 class Weather
   def stormy?
-    generate_weather
-  end
-
-  private
-
-  def generate_weather
     rand(5) == 1
   end
 end
--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -2,10 +2,8 @@ require 'weather'
 
 describe Weather do
   describe '#stormy?' do
-    before { allow(subject).to receive(:generate_weather).and_return(true) }
-
     it 'returns a boolean' do
-      expect(subject.stormy?).to eq true
+      expect(subject.stormy?).to eq(true).or eq(false)
     end
   end
 end
