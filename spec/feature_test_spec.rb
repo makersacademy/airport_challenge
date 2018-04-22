daf99c6c953910631bba63314a22ead1366530f6
Habin <habin@hotmail.co.uk>
2018-04-22 11:57:13 +0100

    feature test for when its stormy

--- spec/feature_test_spec.rb
+++ spec/feature_test_spec.rb
@@ -1,9 +1,10 @@
 require_relative '../lib/plane.rb'
 require_relative '../lib/airport.rb'
 
-plane = Plane.new
+def its_stormy
+  weather = Weather.new
+  plane = Plane.new
   airport = Airport.new
   airport.land(plane)
-  airport.takeoff(plane)
-  airport.planes == []
-end
+  airport.take_off(plane, weather)
++end
