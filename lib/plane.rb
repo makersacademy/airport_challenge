427f02d3dee90c6dcff7f0fe67be95f590597d8f
Igor Ryabchuk <riabtchuk@gmail.com>
2018-04-22 04:36:57 +0100

    refactor tests and slim down plane class

--- lib/plane.rb
+++ lib/plane.rb
@@ -1,15 +1,24 @@
 class Plane
 
-  attr_reader :status, :airport_id
+  attr_reader :airport_id
 
   def land(airport_id)
-    @status = 'at_airport'
+
+    raise 'Cannot land while at the airport' unless @airport_id.nil?
     @airport_id = airport_id
+
   end
 
   def takeoff(airport_id)
-    raise 'Instructions received from wrong airport' if airport_id != @airport_id
-    @status = 'in_flight'
+
+    if airport_id != @airport_id
+      raise @airport_id.nil? ? 'Cannot takeoff while in flight' :
+                'Instructions received from wrong airport'
+    end
+
     @airport_id = nil
+    "Left airport #{airport_id}"
+
   end
+
 end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -1,31 +1,22 @@
 require 'plane'
 
 describe Plane do
- let(:airport) {double :airport_double, object_id: 979797977}
- let(:other_airport) {double :other_airport_double, object_id: 123456}
 
- describe '#status method' do
-    it "should respond to #status" do
-      expect(subject).to respond_to(:status)
-    end
-  end
+  let(:airport) { double :airport_double, object_id: 979_797_977 }
+  let(:other_airport) { double :other_airport_double, object_id: 123_456 }
 
   describe '#land method' do
-    it 'should respond to #land' do
-      expect(subject).to respond_to(:land)
-    end
-
-    it 'should change #status to "at_airport" after landing' do
-      expect { subject.land(airport.object_id) }.to change { subject.status }
-      expect(subject.status).to eq("at_airport")
+    before(:each) do
+      subject.land(airport.object_id)
     end
 
     it "should record airport_id when it lands" do
-      subject.land(airport.object_id)
       expect(subject.airport_id).to eq airport.object_id
     end
 
-    it 'should raise an error when asked to land while #status is "at_airport"'
+    it 'should raise an error when asked to land while at the airport' do
+      expect { subject.land(airport.object_id) }.to raise_error 'Cannot land while at the airport'
+    end
   end
 
   describe '#takeoff method' do
@@ -34,31 +25,22 @@ describe Plane do
       subject.land(airport.object_id)
     end
 
-    it 'should respond to #takeoff' do
-      expect(subject).to respond_to(:takeoff)
-    end
-
-    it 'should change #status to "in_flight" after takeoff' do
-      expect { subject.takeoff(airport.object_id) }.to change { subject.status }
-    end
-
-    it 'should set #status to "in_flight"' do
+    it 'should clear airport_id on takeoff' do
       subject.takeoff(airport.object_id)
-      expect(subject.status).to eq("in_flight")
+      expect(subject.airport_id).to eq nil
+    end
+
+    it 'should report that it left the airport' do
+      expect(subject.takeoff(airport.object_id)).to eq "Left airport #{airport.object_id}"
     end
 
     it 'should only accept #takeoff method from the same airport it is in' do
-      expect {subject.takeoff(other_airport.object_id)}.to raise_error 'Instructions received from wrong airport'
+      expect { subject.takeoff(other_airport.object_id) }.to raise_error 'Instructions received from wrong airport'
     end
 
-    it 'should return error when asked to takeoff while #status is "in_flight"' do
+    it 'should return error when asked to takeoff while in flight' do
       subject.takeoff(airport.object_id)
-      expect {subject.takeoff(airport.object_id)}
-    end
-
-    it 'should return #airport_id == nil while #status is "in_flight"' do
-      subject.takeoff(airport.object_id)
-      expect(subject.airport_id).to eq nil
+      expect { subject.takeoff(airport.object_id) }.to raise_error 'Cannot takeoff while in flight'
     end
   end
 end
