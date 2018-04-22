0c83230dc93079ffdd632cc02bbb2fda070c2920
Habin <habin@hotmail.co.uk>
2018-04-22 11:51:46 +0100

    working test for plane taking off, with no planes remaining once it has

--- lib/plane.rb
+++ lib/plane.rb
@@ -1,7 +1,7 @@
 class Plane
 
   def land(airport)
-
+    
   end
 
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -10,6 +10,11 @@ describe Airport do
       subject.land(plane)
       expect(subject.planes).to eq [plane]
     end
+
+    it 'stores the planes when they land' do
+      is_expected.to respond_to(:planes)
+      expect(subject.planes).to eq []
+    end
   end
 
   describe '#planes' do
@@ -19,12 +24,19 @@ describe Airport do
           expect(subject.planes).to eq []
     end
   end
+
   describe '#takeoff' do
     it { is_expected.to respond_to(:takeoff).with(1).argument }
 
     it 'is expected for planes to take off' do
+      subject.land(plane)
+      expect(subject.takeoff(plane)).to eq plane
+    end
+
+    it 'is expected to take away a plane after takeoff' do
+      subject.land(plane)
       expect(subject.takeoff(plane)).to eq plane
     end
   end
-  end
+end
 end
