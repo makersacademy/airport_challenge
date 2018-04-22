a801d019b32e8137db13e2209c9c94951baf6cfe
Salome Lambermont <salome.lambermont@gmail.com>
2018-04-22 16:54:28 +0100

    Refactor tests

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -5,7 +5,10 @@ require 'weather'
 describe Airport do
 
   let(:stormy_weather) { double 'Weather', weather_value: rand(8..10) }
+  let(:stormy_weather_value) { stormy_weather.weather_value }
   let(:sunny_weather) { double 'Weather', weather_value: rand(1..7) }
+  let(:sunny_weather_value) { sunny_weather.weather_value }
+  let(:a_plane) { Plane.new }
 
   describe "#capacity" do
     it 'should allow system designer to set a capacity' do
@@ -16,34 +19,29 @@ describe Airport do
 
   describe "#land" do
     it 'should raise error message when the weather is stormy' do
-      a_plane = Plane.new
-      expect { subject.land(a_plane, stormy_weather.weather_value) }.to raise_error("It's too dangerous to land!")
+      expect { subject.land(a_plane, stormy_weather_value) }.to raise_error("It's too dangerous to land!")
     end
     it 'should raise error message when the airport is full' do
-      a_plane = Plane.new
-      Airport::DEFAULT_CAPACITY.times { subject.land(a_plane, sunny_weather.weather_value) }
-      expect { subject.land(a_plane, sunny_weather.weather_value) }.to raise_error("The airport is full!")
+      Airport::DEFAULT_CAPACITY.times { subject.land(a_plane, sunny_weather_value) }
+      expect { subject.land(a_plane, sunny_weather_value) }.to raise_error("The airport is full!")
     end
   end
 
   describe "#take_off" do
     it 'should raise an error message when weather is stormy' do
-      a_plane = Plane.new
-      subject.land(a_plane, sunny_weather.weather_value)
-      expect { subject.take_off(stormy_weather.weather_value) }.to raise_error("It's too dangerous to take off!")
+      subject.land(a_plane, sunny_weather_value)
+      expect { subject.take_off(stormy_weather_value) }.to raise_error("It's too dangerous to take off!")
     end
   end
 
   describe "#planes" do
     it 'should show that the plane is in the airport' do
-      a_plane = Plane.new
-      subject.land(a_plane, sunny_weather.weather_value)
+      subject.land(a_plane, sunny_weather_value)
       expect(subject.planes).to eq [a_plane]
     end
     it 'should show that the the plane is no longer in the airport' do
-      a_plane = Plane.new
-      subject.land(a_plane, sunny_weather.weather_value)
-      subject.take_off(sunny_weather.weather_value)
+      subject.land(a_plane, sunny_weather_value)
+      subject.take_off(sunny_weather_value)
       expect(subject.planes).to eq []
     end
   end
