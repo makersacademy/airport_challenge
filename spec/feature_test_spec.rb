54a913fe8f13c58a3a2ab97b7001d4df8ac3fe5e
Habin <habin@hotmail.co.uk>
2018-04-22 11:55:36 +0100

    feature test for a plane taking off and expectations in weather_spec of expected methods

--- spec/feature_test_spec.rb
+++ spec/feature_test_spec.rb
@@ -1,6 +1,10 @@
 require_relative '../lib/plane.rb'
 require_relative '../lib/airport.rb'
 
-def land_plane
-  plane = Plane.new
+plane = Plane.new
+  airport = Airport.new
+  airport.land(plane)
+  airport.takeoff(plane)
+  airport.take_off(plane)
+  airport.planes == []
 end
