a10b024a31ac6eb6ae2ad82f722d0843989b9921
Habin <habin@hotmail.co.uk>
2018-04-22 15:59:06 +0100

    tests for take off - plane is no longer flying and will raise error if it is

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -18,4 +18,16 @@ describe Plane, :plane do
       expect { subject.land }.to raise_error 'Plane has already landed'
     end
   end
+
+  describe '#takeoff' do
+    it 'flying is true after take off' do
+      subject.land
+      subject.takeoff
+      expect(subject).to be_flying
+    end
+
+    it 'will raise error if that plane is already flying' do
+      expect { subject.take_off }.to raise_error 'Plane is already in the air'
+    end
+  end
 end
