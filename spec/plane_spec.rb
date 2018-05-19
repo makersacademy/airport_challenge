79623ba075dfd67369fa706c94ca1a514ff42f2a
Benjamin Tomkins <bentomkins@hotmail.com>
2018-05-19 20:16:53 +0100

    updated review.md

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -13,4 +13,12 @@ describe Plane do
 	# I want to instruct a plane to take off from an airport and confirm that it is no longer in the airport
 	it { is_expected.to respond_to :take_off }
 
+	# Your code should defend against edge cases such as
+	# inconsistent states of the system ensuring that planes
+	# can only take off from airports they are in; planes
+	# that are already flying cannot takes off and/or be in
+	# an airport; planes that are landed cannot land again
+	# and must be in an airport, etc.
+	
+
 end
