60883bcacf7a192dea47bf131984e2110d1b2af4
anguspettifer <angus.pettifer@gmail.com>
2018-05-19 12:01:15 +0100

    first 2 user stories

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,1 +1,18 @@
- 
+require_relative 'plane'
+
+ class Airport
+   attr_reader :planes
+
+   def initialize
+     @planes = []
+   end
+
+   def let_plane_land(plane)
+     @planes << plane
+   end
+
+   def take_off
+     @planes.pop
+   end
+
+ end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,3 +1,30 @@
- require 'airport'
+require 'airport'
 
- describe "The plane lands at the airport"
+describe Airport do
+
+  describe "#let_plane_land" do
+    it "checks plane has landed in airport" do
+      plane = Plane.new
+      subject.let_plane_land(plane)
+      expect(subject.planes.last).to eq plane
+    end
+  end
+
+  describe "#take_off" do
+
+    it "checks plane has taken off from airport" do
+      plane = Plane.new
+      subject.let_plane_land(plane)
+      expect(subject.take_off).to eq plane
+    end
+
+    it "checks plane is no-longer in airport" do
+      plane = Plane.new
+      subject.let_plane_land(plane)
+      subject.take_off
+      expect(subject.planes.include?(plane)).to eq false
+    end
+
+  end
+
+end
