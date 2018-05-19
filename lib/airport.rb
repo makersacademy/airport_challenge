d858af6c80edc5a442061c70f93ad528dcacd739
Nick <nicholasjohnrichardson@gmail.com>
2018-05-19 12:02:33 +0100

    take off removes plane from hangar, land adds plane to hangar

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,8 +1,20 @@
 require_relative 'plane'
 
-
 class Airport
-  def land(plane)
-    "#{plane} has landed"
+  attr_reader :hangar
+
+  def initialize
+    @hangar = []
   end
+
+  def land(plane)
+    hangar << plane
+    "Plane has landed"
+  end
+
+  def take_off(plane)
+    hangar.delete(plane)
+    "Plane has taken off"
+  end
+
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,12 +2,39 @@ require 'airport'
 
 describe Airport do
   it { is_expected.to respond_to(:land) }
+  it { is_expected.to respond_to(:take_off) }
 
   describe "#land" do
 
     it "returns a message saying plane has landed" do
       plane = Plane.new
-      expect(subject.land(plane)).to eq "#{plane} has landed"
+      expect(subject.land(plane)).to eq "Plane has landed"
+    end
+
+    it "adds the plane to the hangar" do
+      plane = Plane.new
+      subject.land(plane)
+      expect(subject.hangar).not_to be_empty
+    end
+
+  end
+
+# As an air traffic controller
+# So I can get passengers on the way to their destination
+# I want to instruct a plane to take off from an airport and confirm that it is no longer in the airport
+
+  describe "#take_off" do
+
+    it "returns a message saying plane has taken off" do
+      plane = Plane.new
+      expect(subject.take_off(plane)).to eq "Plane has taken off"
+    end
+
+    it "removes the plane from the hangar" do
+      plane = Plane.new
+      subject.land(plane)
+      subject.take_off(plane)
+      expect(subject.hangar).to be_empty
     end
 
   end
