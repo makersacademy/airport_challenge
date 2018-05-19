8590bd7f872e265202d707d6a23c67ef7ccc9082
anguspettifer <angus.pettifer@gmail.com>
2018-05-19 14:54:09 +0100

    3rd story complete

--- lib/airport.rb
+++ lib/airport.rb
@@ -2,9 +2,15 @@ require_relative 'plane'
 
  class Airport
    attr_reader :planes
+   attr_reader :weather
 
    def initialize
      @planes = []
+     if rand(10) > 8
+       @weather = :stormy
+     else
+       @weather = :sunny
+     end
    end
 
    def let_plane_land(plane)
@@ -12,6 +18,7 @@ require_relative 'plane'
    end
 
    def take_off
+     fail "cannot take off due to stormy weather" if weather == :stormy
      @planes.pop
    end
 
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -15,14 +15,34 @@ describe Airport do
     it "checks plane has taken off from airport" do
       plane = Plane.new
       subject.let_plane_land(plane)
+      # let airport have sunny weather
+      allow(subject).to receive(:weather).and_return(:sunny)
       expect(subject.take_off).to eq plane
     end
 
     it "checks plane is no-longer in airport" do
       plane = Plane.new
       subject.let_plane_land(plane)
+      # let airport have sunny weather
+      allow(subject).to receive(:weather).and_return(:sunny)
       subject.take_off
       expect(subject.planes.include?(plane)).to eq false
+    end
+
+    it "should raise error if weather is stormy" do
+      plane = Plane.new
+      subject.let_plane_land(plane)
+      # let airport have weather that is stormy
+      allow(subject).to receive(:weather).and_return(:stormy)
+      expect{subject.take_off}.to raise_error "cannot take off due to stormy weather"
+    end
+
+  end
+
+  describe "#weather" do
+
+    it "expects airport to respond to weather" do
+      expect(subject).to respond_to(:weather)
     end
 
   end
