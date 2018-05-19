a55842c32731fe05f8019da93ae1c1633fecca18
Benjamin Tomkins <bentomkins@hotmail.com>
2018-05-19 17:50:17 +0100

    added weather class

--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -1,1 +1,10 @@
 require 'weather'
+
+describe Weather do
+	
+	# As an air traffic controller 
+	# To ensure safety
+	# I want to prevent landing when weather is stormy
+	it { is_expected.to respond_to :stormy?}
+
+end
