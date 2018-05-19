8eff8dca436416b74b640ab01cd5863bb66f7fb7
luciagore <lucy.gore@hotmail.co.uk>
2018-05-19 09:07:41 +0100

    prevent take off when stormy weather

--- lib/airport.rb
+++ lib/airport.rb
@@ -2,22 +2,19 @@ require_relative 'plane'
 
 class Airport
 
-# OUTLOOKS = [:stormy, :fine, :fine, :fine]
-
 attr_reader :planes
 
   def initialize
     @planes = []
-    # @weather = weather
   end
-
   def land_plane(plane)
     @planes << plane
   end
-
   def take_off
+    fail "Stormy weather" if stormy?
     @planes.pop
   end
-
-# (weather: Weather.new)
+  def stormy?
+    false
+  end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,35 +1,34 @@
 require './lib/airport'
 
 describe Airport do
-
   describe '#land_plane' do
   it 'responds to land_plane' do
     expect(Airport.new).to respond_to(:land_plane)
   end
-
   it 'responds to land_plane with one argument' do
       expect(Airport.new).to respond_to(:land_plane).with(1).argument
     end
-
   it 'allows plane to land in airport' do
     plane = Plane.new
     subject.land_plane(plane)
     expect(subject.planes).to include(plane)
   end
 end
-
   describe '#take_off' do
       it 'responds to take_off' do
         expect(Airport.new).to respond_to(:take_off)
       end
-
       it 'allows plane to leave airport' do
         plane = Plane.new
         subject.land_plane(plane)
         subject.take_off
         expect(subject.planes).not_to include(plane)
       end
-
+      it 'prevents take_off when stormy' do
+        plane = Plane.new
+        subject.land_plane(plane)
+        subject.stormy?
+        expect {subject.take_off}.to raise_error "Stormy weather"
+      end
   end
-
 end
