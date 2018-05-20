70b3a216d0bd9c88beffc15f5e3b3c9ad7394f71
Daniel Ridgway <ridginald@gmail.com>
2018-05-20 11:00:02 +0100

    US5refactor

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,14 +2,28 @@ require 'airport'
 
 describe Airport do
   subject(:airport) { described_class.new(20) }
+  let(:plane) { double :plane }
 
-  it 'instructs a plane to land' do
-    expect(airport).to respond_to(:land).with(1).argument
+  describe '#land' do
+    it 'instructs a plane to land' do
+      expect(airport).to respond_to(:land).with(1).argument
+    end
+
+    context 'when airport is full' do
+      it 'raises an error' do
+        20.times do
+          airport.land(plane)
+        end
+        expect { airport.land(plane) }.to raise_error 'Cannot land: airport full'
+      end
+    end
   end
 
+  describe '#take_off' do
   it 'instructs a plane to take off' do
     expect(airport).to respond_to(:take_off).with(1).argument
   end
+end
   #
   # it 'prevents plane from taking off when stormy' do
   #   allow(airport).to receive(:stormy?).and_return true
@@ -20,13 +34,5 @@ describe Airport do
   #   allow(airport).to receive(:stormy?).and_return true
   #   expect { airport.land(plane) }.to raise_error 'Cannot land: stormy weather'
   # end
-
-  it 'does not allow landing when airport is full' do
-    plane = double :plane
-    20.times do
-      airport.land(plane)
-    end
-    expect { airport.land(plane) }.to raise_error 'Cannot land: airport full'
-  end
 
 end
