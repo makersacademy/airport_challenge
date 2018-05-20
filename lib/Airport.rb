2e974613b7e2f46f8250e96074dbebf611836f57
Khie Biggs <khiebigzy@gmail.com>
2018-05-20 16:52:21 +0100

    Add comment to the methods of existing files

--- lib/Airport.rb
+++ lib/Airport.rb
@@ -1,11 +1,14 @@
 class Airport
   attr_reader :planes
+
+  # initialize the airport
   def initialize
     @planes = []
   end
 
+  # add a plane to the plane array
   def add_plane(plane)
     @planes << plane
   end
-  
+
 end
--- lib/TrafficController.rb
+++ lib/TrafficController.rb
@@ -1,6 +1,9 @@
 class TrafficController
+
+  # instruct a plane to land at a given airport 
   def instruct_land(plane, airport)
     plane.land(airport)
     return [plane, airport]
   end
+
 end
