9c7d46b9baf1d5ddf72b88d24fdd5c860c6a43ff
Simone <simonecaplin@gmail.com>
2018-05-20 09:15:58 +0100

    Prevent take-off when the weather is stormy

--- lib/airport.rb
+++ lib/airport.rb
@@ -2,12 +2,13 @@ require_relative 'plane'
 require_relative 'weather'
 
 class Airport
-  attr_reader :planes
+  attr_reader :planes, :weather
   DEFAULT_CAPACITY = 20
 
-  def initialize(capacity = DEFAULT_CAPACITY)
+  def initialize(capacity = DEFAULT_CAPACITY, weather = Weather.new)
     @planes = []
     @capacity = capacity
+    @weather = weather
   end
 
   def land(plane)
@@ -19,9 +20,13 @@ class Airport
 
   def take_off(plane)
     fail 'Plane is not at airport' unless @planes.include?(plane)
-    # fail 'Cannot take off, weather is stormy' if stormy?
+    fail 'Cannot take off, weather is stormy' if stormy
     plane.make_take_off
     @planes.delete(plane)
+  end
+
+  def stormy
+    weather.stormy?
   end
 
   private
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -5,40 +5,52 @@ describe Airport do
   let(:plane) { Plane.new }
 
   describe '#initialize' do
-    it 'prevents landing when the airport is full' do
+    it 'defaults capacity' do
       described_class::DEFAULT_CAPACITY.times do
         subject.land(plane)
         plane.make_take_off
       end
-      expect { subject.land(plane) }.to raise_error 'Airport is full'
+      expect(subject.planes.size).to eq described_class::DEFAULT_CAPACITY
     end
   end
 
-  describe '#land' do
-    it 'lands a plane at an airport' do
-      subject.land(plane)
-      expect(subject.planes).to include(plane)
+  context 'when the weather is good' do
+    describe '#land' do
+      it 'lands a plane at an airport' do
+        subject.land(plane)
+        expect(subject.planes).to include(plane)
+      end
+
+      it 'prevents landing when the airport is full' do
+        described_class::DEFAULT_CAPACITY.times do
+          subject.land(plane)
+          plane.make_take_off
+        end
+        expect { subject.land(plane) }.to raise_error 'Airport is full'
+      end
+    end
+
+    describe '#take_off' do
+      it 'takes off from the airport and is no longer there' do
+        allow(subject).to receive(:stormy) { false }
+        subject.land(plane)
+        subject.take_off(plane)
+        expect(subject.planes).not_to include(plane)
+      end
+
+      it 'only takes off planes from an airport they are in' do
+        expect { subject.take_off(plane) }.to raise_error 'Plane is not at airport'
+      end
     end
   end
 
-  describe '#take_off' do
-    it 'takes off from the airport and is no longer there' do
-      allow(subject).to receive(:stormy?) { false }
-      subject.land(plane)
-      subject.take_off(plane)
-      expect(subject.planes).not_to include(plane)
-    end
-
-    it 'only takes off planes from an airport they are in' do
-      expect { subject.take_off(plane) }.to raise_error 'Plane is not at airport'
+  context 'when the weather is stormy' do
+    before { allow(subject).to receive(:stormy) { true } }
+    describe '#take_off' do
+      it 'prevents take off when weather is stormy' do
+        subject.land(plane)
+        expect { subject.take_off(plane) }.to raise_error 'Cannot take off, weather is stormy'
+      end
     end
   end
-
-  # context 'when the weather is stormy' do
-  #   before { allow(subject).to receive(:stormy?) { true } }
-  #   it 'prevents take off when weather is stormy' do
-  #     subject.land(plane)
-  #     expect { subject.take_off(plane) }.to raise_error 'Cannot take off, weather is stormy'
-  #   end
-  # end
 end
