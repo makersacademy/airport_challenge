80e84a4d8379ff0659713648330a4f1e46e3d19b
luciagore <lucy.gore@hotmail.co.uk>
2018-05-21 07:25:33 +0100

    updates to weather_spec.rb

--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -5,9 +5,11 @@ describe Weather do
 
   describe '#stormy?' do
     it 'is not stormy' do
+      allow(weather).to receive(:stormy?).and_return false
       expect(weather.stormy?).to be false
     end
     it 'is stormy' do
+      allow(weather).to receive(:stormy?).and_return true
       expect(weather.stormy?).to be true
     end
   end
