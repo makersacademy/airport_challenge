be9d26b09813a0591b57281e5622237b4aa243d9
jessica stenning <jessicastenning@hotmail.co.uk>
2018-05-28 15:48:42 +0100

    removes magic number

--- lib/weather.rb
+++ lib/weather.rb
@@ -1,11 +1,13 @@
 class Weather
 
+  STORMY_WEATHER_THRESHOLD = 2
+
   def initialize(weather=rand(10))
     @weather = weather
   end
 
   def stormy?
-    if @weather < 2
+    if @weather < STORMY_WEATHER_THRESHOLD
       @weather = true
     else
       @weather = false
