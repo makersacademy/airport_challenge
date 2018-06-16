568de4e1a5e8c00770cf96480dd687a42904c738
kees <cheuperman@gmail.com>
2018-06-16 12:47:38 +0100

    Create Plane class with an empty function named land

---
+++ lib/plane.rb
@@ -0,0 +1,5 @@
+# Instances know how to land and take of from airports
+class Plane
+  def land
+  end
+end
---
+++ spec/plane_spec.rb
@@ -0,0 +1,7 @@
+require 'plane'
+
+describe Plane do
+  it "Has a land function" do
+    expect(subject).to respond_to(:land)
+  end
+end
