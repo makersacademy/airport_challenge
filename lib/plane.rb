1d19d97ff657945ff6b81b5ea4c907e5f08d2119
kees <cheuperman@gmail.com>
2018-06-16 16:40:06 +0100

    Add method to check if plane is currently at an airport

--- lib/plane.rb
+++ lib/plane.rb
@@ -2,11 +2,20 @@ require_relative './airport.rb'
 
 # Instances know how to land and take of from airports
 class Plane
+
+  attr_accessor :at_airport
+
   def land(airport)
     puts "Plane landed safely at #{airport}."
+    @at_airport = true
   end
 
-  def take_off
+  def takeoff
     puts "Plane took off successfully."
+    @at_airport = false
+  end
+
+  def at_airport?
+    @at_airport
   end
 end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -1,17 +1,33 @@
 require 'plane'
 
 describe Plane do
-  it 'Has a land function' do
-    expect(subject).to respond_to(:land)
+  describe 'landing' do
+    it 'Has a land function' do
+      expect(subject).to respond_to(:land)
+    end
+    it 'Reports if the plane has landed succesfully' do
+      airport = Airport.new
+      expect { subject.land(airport) }.to output("Plane landed safely at #{airport}.\n").to_stdout
+    end
   end
-  it 'Reports if the plane has landed succesfully' do
-    airport = Airport.new
-    expect { subject.land(airport) }.to output("Plane landed safely at #{airport}.\n").to_stdout
+
+  describe 'takeoff' do
+    it 'Has a takeoff function' do
+      expect(subject).to respond_to(:takeoff)
+    end
+    it 'Reports if plane has taken off successfully' do
+      expect { subject.takeoff }.to output("Plane took off successfully.\n").to_stdout
+    end
   end
-  it 'Has a take-off function' do
-    expect(subject).to respond_to(:take_off)
-  end
-  it 'Reports if plane has taken off successfully' do
-    expect { subject.take_off }.to output("Plane took off successfully.\n").to_stdout
+
+  describe 'Check if at airport' do
+    it 'Checks if plane is at an airport after landing' do
+      subject.land(Airport.new)
+      expect(subject).to be_at_airport
+    end
+    it 'Checks if plane is not at an airport after takeoff' do
+      subject.takeoff
+      expect(subject).not_to be_at_airport
+    end
   end
 end
