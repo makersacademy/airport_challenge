de12b72f9dca9f2eba8b7c397807b1cea4ebbea3
kees <cheuperman@gmail.com>
2018-06-16 13:02:50 +0100

    Add airport class and allow plane to land at airport

--- lib/plane.rb
+++ lib/plane.rb
@@ -1,5 +1,8 @@
+require_relative './airport.rb'
+
 # Instances know how to land and take of from airports
 class Plane
-  def land
+  def land(airport)
+    puts "Plane landed safely at #{airport}."
   end
 end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -1,7 +1,11 @@
 require 'plane'
 
 describe Plane do
-  it "Has a land function" do
+  it 'Has a land function' do
     expect(subject).to respond_to(:land)
+  end
+  it 'Reports if the plane has landed succesfully' do
+    airport = Airport.new
+    expect { subject.land(airport) }.to output("Plane landed safely at #{airport}.\n").to_stdout
   end
 end
