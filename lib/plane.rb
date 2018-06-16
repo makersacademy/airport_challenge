ece86d44bf410f2576d390b2f602153341db2256
kees <cheuperman@gmail.com>
2018-06-16 16:58:12 +0100

    Add weather class, set to stormy by default

--- lib/plane.rb
+++ lib/plane.rb
@@ -1,18 +1,19 @@
 require_relative './airport.rb'
 
 # Instances know how to land and take of from airports
+# and can report if they are currently at an airport.
 class Plane
 
   attr_accessor :at_airport
 
   def land(airport)
     puts "Plane landed safely at #{airport}."
-    at_airport = true
+    @at_airport = true
   end
 
   def takeoff
     puts "Plane took off successfully."
-    at_airport = false
+    @at_airport = false
   end
 
   def at_airport?
