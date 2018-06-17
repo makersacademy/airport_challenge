7751a47c785d97f10d3ad0b6fbdde2d358ba294c
Benjamin <ben.smith.gordon@gmail.com>
2018-06-17 19:36:43 +0100

    More stuff added

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,12 +1,22 @@
 class Airport
 
+attr_reader :capacity
+attr_reader :planes
+
+DEFAULT_CAPACITY = 20
+
+def initialize(capacity = DEFAULT_CAPACITY)
+  @capacity = capacity
+  @planes = []
+end
+
 def land(plane)
+  @planes << plane
 end
 
 def take_off(plane)
-end
-
-def stormy(plane)
+  @planes.pop
+  # fail "Runway full" if @planes > @capacity
 end
 
 
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -7,15 +7,34 @@ describe Airport do
     it { should respond_to(:land).with(1).argument }
 
     it 'allows planes to land' do
-
+      subject.land(plane)
+      expect(subject.planes).to include plane
     end
   end
 
   describe "#take_off" do
 
     it { should respond_to(:take_off).with(1).argument }
-
     it "allows planes to take off" do
+      subject.land(plane)
+      subject.take_off(plane)
+      expect(subject.planes).to_not include plane
     end
+  #   it "raises an error when full" do
+  #     subject.capacity.times { subject.take_off(plane) }
+  #     expect { subject.take_off(plane) }.to raise_error "Runway full"
+  # end
+end
+
+  describe "#initialize" do
+
+    it "it has a default capacity" do
+      expect(subject.capacity).to eq Airport::DEFAULT_CAPACITY
+    end
+    # it 'has a variable capacity' do
+    #   airport = Airport.new(50)
+    #   50.times { airport.land Plane.new }
+    #   expect{ airport.land Plane.new }.to raise_error 'Runway full'
+    # end
   end
 end
