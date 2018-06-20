de50e42f07c101e0f5b47c5105617b2c52db659e
Alice Lieutier <alice@lieutier.me>
2018-06-20 18:19:25 +0100

    big airport tests

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -9,6 +9,16 @@ describe Airport do
       Airport::DEFAULT_CAPACITY.times{ subject.landing(Plane.new) }
       expect(subject.can_land?).to be false
     end
+    it 'returns true if airport is not full in big airport' do
+      big_airport = Airport.new(500)
+      499.times{ big_airport.landing(Plane.new) }
+      expect(big_airport.can_land?).to be true
+    end
+    it 'returns false if airport is full in big airport' do
+      big_airport = Airport.new(500)
+      500.times{ big_airport.landing(Plane.new) }
+      expect(big_airport.can_land?).to be false
+    end
   end
   describe '#can_takeoff?' do
     it 'can tell a plane if it can take off' do
--- spec/features/feature_spec.rb
+++ spec/features/feature_spec.rb
@@ -2,7 +2,11 @@ require 'airport'
 require 'plane'
 
 describe 'Land a plane in sunny weather' do
+
   airport = Airport.new
-  plane = Plane.new
-  plane.land(airport)
+  Airport::DEFAULT_CAPACITY.times{
+    plane = Plane.new
+    plane.land(airport)
+  }
+
 end
