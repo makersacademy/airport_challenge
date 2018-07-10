450f41a67dae9b1a2ba0a01867fa3246ba7cf665
Angela Wolff <warriorwomenblog@gmail.com>
2018-07-10 14:33:41 +0100

    Corrected initial commit

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,6 +1,6 @@
 class Airport
-	attr_accessor :planes, :weather, :capacity :5
-	def initialize
+	attr_accessor :planes, :weather, :capacity
+	def initialize(weather: "fine", capacity:5)
 		@planes = []
 		@weather = weather
 		@capacity = capacity
