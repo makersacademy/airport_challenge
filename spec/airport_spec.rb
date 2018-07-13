a1d4afd0f07cbc9ead140b851076fe886dc86fd0
Tim Blanchard <tim.blanchard@hotmail.co.uk>
2018-07-13 09:21:36 +0100

    cleaned up exisiting code

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,18 +1,13 @@
-#die 
+# die - do it expect
+
 require 'airport.rb'
 
 describe Airport do 
- it "airport.new creates a new airport" do
-  expect(Airport.new).to be_instance_of Airport
- end 
+  it "airport.new creates a new airport" do
+    expect(Airport.new).to be_instance_of Airport
+  end 
 end 
-# describe Dice do 
-#     let(:subject) { described_class.new }
-#      it "dice.new creates new dice object" do
-#         expect(Dice.new).to be_instance_of Dice 
-#       end
-#        it "returns a random number from 1-6" do 
-#         expect(subject.numbers).to receive(:sample).and_return(12345)
-#         expect(subject.random).to eq 12345
-#     end
-#   end 
+# user story 1 
+# As an air traffic controller 
+# So I can get passengers to a destination 
+# I want to instruct a plane to land at an airport
