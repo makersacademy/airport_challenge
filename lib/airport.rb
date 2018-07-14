6a0c6c0ed3f213243f2d9d665896f961e0e07f3b
Tim Blanchard <tim.blanchard@hotmail.co.uk>
2018-07-14 17:09:55 +0100

    added rspec for hanger, and created hanger in airport.rb

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,11 +1,17 @@
 require_relative 'plane'
 
 class Airport 
+   attr_reader :hanger
+  def initialize
+  @hanger = []
+  end 
+
+    def land(plane)
+      @plane = plane 
+    end
   
-  def land(plane)
-  @plane = plane 
-  end
   def takeoff(plane)
+  
   end 
 
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -6,17 +6,24 @@ describe Airport do
   it "airport.new creates a new airport" do
     expect(Airport.new).to be_instance_of Airport
   end 
+  it "contains a hanger for planes to be stored (a hanger)" do
+    expect(subject.hanger).to eq([]) 
+  end  
   it "has the ability to accept a plane to land" do 
     expect(subject).to respond_to(:land).with(1).argument 
   end 
   it "returns the plane (landed in the airport) when the plane has landed" do 
    plane = Plane.new 
-   expect(subject.land(plane)).to eq plane 
+   expect(subject.land(plane)).to eq(plane) 
   end 
   it "has the ability to instuct  a plane to take off" do 
     expect(subject).to respond_to(:takeoff).with(1).argument 
   end 
-
+  # it "shows that plane is no longer in airport, once it has taken off" do 
+  # plane = Plane.new 
+  # subject.land(plane)
+  # expect(subject).takeoff.to eq ""
+  # end 
 end 
 
 # user story 1 
