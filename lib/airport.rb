6c49671ed04b887d0397ca047ca1d30b21a41451
Tim Blanchard <tim.blanchard@hotmail.co.uk>
2018-07-14 15:35:46 +0100

    User story 2: Added takeoff method

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,7 +1,11 @@
 require_relative 'plane'
 
 class Airport 
+  
   def land(plane)
   @plane = plane 
   end
+  def takeoff(plane)
+  end 
+
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -13,9 +13,18 @@ describe Airport do
    plane = Plane.new 
    expect(subject.land(plane)).to eq plane 
   end 
+  it "has the ability to instuct  a plane to take off" do 
+    expect(subject).to respond_to(:takeoff).with(1).argument 
+  end 
+
 end 
 
 # user story 1 
 # As an air traffic controller 
 # So I can get passengers to a destination 
 # I want to instruct a plane to land at an airport
+
+# As an air traffic controller 
+# So I can get passengers on the way to their destination 
+# I want to instruct a plane to take off from an airport and confirm that it is no longer in the airport
+
