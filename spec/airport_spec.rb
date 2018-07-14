8f363ed726265878021d999b9618e0e4ffda324d
Tim Blanchard <tim.blanchard@hotmail.co.uk>
2018-07-14 08:32:41 +0100

    completed first user story for airport challenge

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -6,8 +6,12 @@ describe Airport do
   it "airport.new creates a new airport" do
     expect(Airport.new).to be_instance_of Airport
   end 
-  it "is able to land a plane" do 
+  it "has the ability to accept a plane to land" do 
     expect(subject).to respond_to(:land).with(1).argument 
+  end 
+  it "returns the plane (landed in the airport) when the plane has landed" do 
+   plane = Plane.new 
+   expect(subject.land(plane)).to eq plane 
   end 
 end 
 
