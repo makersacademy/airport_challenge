e55db5b19cce63b9cb2a053d762e3cc2cc3cc82d
Marcus <marcus.kerr94@gmail.com>
2018-07-15 21:24:59 +0100

    Feature test: instruct a plane to take off from the airport, confirm no longer in airport, wrote unit test (RED)

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,6 +1,13 @@
 require "Airport"
 describe Airport do
+
+    # Check plane resposds to land method
     it "responds to land method" do
         expect(subject).to respond_to(:land).with(1).argument
+    end
+
+    # test, instruct a plane to take off
+    it "responds to take_off method" do
+        expect(subject).to respond_to(:take_off).with(1).argument
     end
 end
