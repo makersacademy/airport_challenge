49c7e0af329327b6c5c11ac3ec312dd029b05b36
Marcus <marcus.kerr94@gmail.com>
2018-07-15 21:34:38 +0100

    Wrote unit test to confirm that plane is no longer in the airport

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -6,8 +6,13 @@ describe Airport do
         expect(subject).to respond_to(:land).with(1).argument
     end
 
-    # test, instruct a plane to take off
+    # Test, instruct a plane to take off
     it "responds to take_off method" do
         expect(subject).to respond_to(:take_off).with(1).argument
+    end
+
+    # Test confirm plane no longer in hanger
+    it "Confirms plane is no longer in the hangar " do
+        expect(subject.take_off).to eq("Plane not in hangar")
     end
 end
