6000c182678eff73a4cc871f4a852e15d2d81fdd
Rashika Patel <rashika_patel@hotmail.com>
2018-08-11 12:05:30 +0100

    add plane_land method and Plane class

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,3 +1,4 @@
 class Airport
-
+  def plane_land(plane)
+  end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,5 +1,7 @@
 require 'airport'
 
 describe Airport do
-
+  it 'tells plane to land' do
+    expect(subject.plane_land(Plane.new)).to eq(nil)
+  end
 end
