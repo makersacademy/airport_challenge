14641badd05d46ef79f2a25241290f4dd9eb3740
Rashika Patel <rashika_patel@hotmail.com>
2018-08-11 21:26:41 +0100

    edge case tests added

--- lib/airport.rb
+++ lib/airport.rb
@@ -12,12 +12,14 @@ class Airport
   def plane_land(plane)
     fail "Bad weather, plane cannot land" if weather_random <= 10
     fail "Airport full, plane cannot land" if full?
+    fail "Plane already at airport" if @planes.include?(plane)
     @planes << plane
     plane.land_airport
   end
 
   def plane_take_off(plane)
     fail "Bad weather, plane cannot take off" if weather_random <= 10
+    fail "Plane not at airport" unless @planes.include?(plane)
     @planes.delete(plane)
     plane.leave_airport
   end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,13 +1,8 @@
 require 'airport'
 
 describe Airport do
-  # it 'tells plane to land' do
-  #   expect(subject.plane_land(Plane.new)).to eq(nil)
-  # end
 
-  # it 'tells plane to take off' do
-  #   expect(subject.plane_take_off(Plane.new)).to eq(false)
-  # end
+  let(:plane) { Plane.new }
 
   it 'when stormy weather, plane cannot take off' do
     allow(subject).to receive(:weather_random) { 5 }
@@ -19,14 +14,44 @@ describe Airport do
     expect { subject.plane_land(Plane.new) }.to raise_error "Bad weather, plane cannot land"
   end
 
-  it 'when airport is full, plane cannot land' do
+  before do
     allow(subject).to receive(:weather_random) { 15 }
+  end
+
+  it 'when airport is full, plane cannot land' do
     allow(subject).to receive(:full?) { true }
-    expect { subject.plane_land(Plane.new) }.to raise_error "Airport full, plane cannot land"
+    error = "Airport full, plane cannot land"
+    expect { subject.plane_land(plane) }.to raise_error error
   end
 
   it 'allow airport capacity to be overwritten' do
     airport = Airport.new(10)
     expect(airport.capacity).to eq 10
   end
+
+  it 'when plane is at airport returns true' do
+    subject.plane_land(plane)
+    expect(plane.at_airport).to eq(true)
+  end
+
+  it 'when plane has left airport, returns false' do
+    subject.planes << plane
+    subject.plane_take_off(plane)
+    expect(plane.at_airport).to eq(false)
+  end
+
+  it 'when plane has landed, is part of planes' do
+    subject.plane_land(plane)
+    expect(subject.planes).to include(plane)
+  end
+
+  it 'plane cannot take off if plane not in airport' do
+    expect { subject.plane_take_off(plane) }.to raise_error "Plane not at airport"
+  end
+
+  it 'plane cannot land if already in airport' do
+    subject.planes << plane
+    expect { subject.plane_land(plane) }.to raise_error "Plane already at airport"
+  end
+
 end
