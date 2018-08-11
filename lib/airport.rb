65d56f9571b07d348f82e4899238e379b526bd30
Lucien Najev <luciennajev@hotmail.com>
2018-08-11 09:23:50 +0100

    land_plane method added tested that airport responds to it - test green

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,3 +1,10 @@
 class Airport
 
+
+  def land_plane
+
+  end
+
+  
+
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,4 +2,12 @@ require 'airport'
 
 describe Airport do
 
+  describe "#land_plane" do
+
+    it ".land_plane - Airport responds to method" do
+      expect(subject).to respond_to(:land_plane)
+    end
+
+  end
+
 end
