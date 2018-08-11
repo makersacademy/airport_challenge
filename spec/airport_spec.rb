061038cc77d0d4428154e66dad23723f00377863
Esam Al-Dabagh <esam.aldabagh@gmail.com>
2018-08-11 16:44:51 +0100

    fix issue with airport_spec.rb where random weather would affect if a test passed or failed

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -3,6 +3,8 @@ require 'plane'
 
 describe Airport do
 
+  let(:heathrow) { Airport.new }
+
   it { is_expected.to respond_to(:land).with(1).argument }
 
   it "should have somewhere to store instances of planes" do 
@@ -10,7 +12,6 @@ describe Airport do
   end
 
   it "should land a plane" do 
-    heathrow = Airport.new
     allow(heathrow).to receive(:weather) { "sunny" }
     heathrow.land("BA123")
     expect(heathrow.planes[0].flight_number).to eq("BA123")
@@ -19,7 +20,6 @@ describe Airport do
   it { is_expected.to respond_to(:takeoff).with(1).argument }
 
   it "should remove given plane from airport when #takeoff is called" do
-    heathrow = Airport.new
     allow(heathrow).to receive(:weather) { "sunny" }
     heathrow.land("BA123")
     heathrow.takeoff("BA123")
@@ -27,33 +27,28 @@ describe Airport do
   end
 
   it "should return a message to confirm takeoff" do
-    heathrow = Airport.new
     expect(heathrow.confirm).to eq("Plane is no longer in the airport.")
   end
 
   it "should prevent landing when airport is full" do 
-    heathrow = Airport.new
     allow(heathrow).to receive(:weather) { "sunny" }
     Airport::DEFAULT_CAPACITY.times { heathrow.land("BA123") }
 
     expect { heathrow.land("EZ456") }.to raise_error("Airport is full.")
   end
 
-  it "should be able to prevent takeoff during stormy weather" do
-    heathrow = Airport.new 
+  it "should be able to prevent takeoff during stormy weather" do 
     heathrow.land("BA123")
     allow(heathrow).to receive(:weather) { "stormy" }
     expect { heathrow.takeoff("BA123") }.to raise_error("Cannot land due to weather.")
   end
 
-  it "should be able to prevent landing during stormy weather" do
-    heathrow = Airport.new 
+  it "should be able to prevent landing during stormy weather" do 
     allow(heathrow).to receive(:weather) { "stormy" }
     expect { heathrow.land("BA123") }.to raise_error("Cannot land due to weather.")
   end
 
-  it "should be able to land during sunny weather" do
-    heathrow = Airport.new 
+  it "should be able to land during sunny weather" do 
     previous_number_planes = heathrow.planes.length
     allow(heathrow).to receive(:weather) { "sunny" }
     heathrow.land("BA123")
@@ -61,10 +56,9 @@ describe Airport do
   end
 
   it "should be able to takeoff during sunny weather" do 
-    heathrow = Airport.new
+    allow(heathrow).to receive(:weather) { "sunny" }
     heathrow.land("BA123")
     previous_number_planes = heathrow.planes.length
-    allow(heathrow).to receive(:weather) { "sunny" }
     heathrow.takeoff("BA123")
     expect(heathrow.planes.length).to eq(previous_number_planes - 1)
   end
