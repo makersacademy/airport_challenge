3c6b5a9263f4c079f4865654d392654d5d7b11af
Rebecca Sedgwick <36798938+rebeccasedgwick@users.noreply.github.com>
2018-08-11 15:43:33 +0100

    Added Plane class, confirmed plane takeoff

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,9 +1,12 @@
+require_relative "plane"
+
 class Airport
 
   def land(plane)
   end
 
   def takeoff(plane)
+    "#{plane} has taken off"
   end
 
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,4 +1,6 @@
 require "airport"
+require "plane"
+
 
 describe Airport do
 
@@ -8,5 +10,10 @@ describe Airport do
 
   it "allows a plane to take off" do
     expect(subject).to respond_to(:takeoff).with(1).argument
+  end
+
+  it "confirms plane has gone when plane takes off" do
+    plane = Plane.new
+    expect(subject.takeoff(plane)).to eq "#{plane} has taken off"
   end
 end
