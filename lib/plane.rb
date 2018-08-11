24df4c24c8b519ad72a676c93b3cf1b3d97ee3e6
Esam Al-Dabagh <esam.aldabagh@gmail.com>
2018-08-11 10:23:34 +0100

    add flight number instance variable to Plane class

--- lib/plane.rb
+++ lib/plane.rb
@@ -1,3 +1,9 @@
 class Plane
 
+  attr_reader :flight_number
+
+  def initialize(flight_number)
+    @flight_number = flight_number
+  end
+
 end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -2,4 +2,9 @@ require 'plane'
 
 describe Plane do 
 
+  it "should expect #flight_number to not be empty" do 
+    jumbo = Plane.new("BA723")
+    expect(jumbo.flight_number).not_to eq(nil)
+  end 
+
 end
