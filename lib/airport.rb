e4fe0239b5664664f5d19b438456df7e15fde310
Rashika Patel <rashika_patel@hotmail.com>
2018-08-11 13:16:55 +0100

    raise error if plane takes off in stormy weather

--- lib/airport.rb
+++ lib/airport.rb
@@ -2,9 +2,16 @@ require_relative 'plane'
 
 class Airport
   def plane_land(plane)
+    fail "Bad weather, plane cannot land" if weather_random <= 10
   end
 
   def plane_take_off(plane)
+    fail "Bad weather, plane cannot take off" if weather_random <= 10
     plane.leave_airport
+  end
+
+  def weather_random
+  	# if return value is less than or equal to 10, weather is stormy
+    rand(100)
   end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -8,4 +8,14 @@ describe Airport do
   it 'tells plane to take off' do
     expect(subject.plane_take_off(Plane.new)).to eq(false)
   end
+
+  it 'when stormy weather, plane cannot take off' do
+    allow(subject).to receive(:weather_random) { 5 }
+    expect { subject.plane_take_off(Plane.new) }.to raise_error "Bad weather, plane cannot take off"
+  end
+
+  it 'when stormy weather, plane cannot land' do
+    allow(subject).to receive(:weather_random) { 5 }
+    expect { subject.plane_land(Plane.new) }.to raise_error "Bad weather, plane cannot land"
+  end
 end
