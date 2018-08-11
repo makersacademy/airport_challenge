a73b495a900b1e1855b71a85882d75b5abfca847
Rebecca Sedgwick <36798938+rebeccasedgwick@users.noreply.github.com>
2018-08-11 17:39:00 +0100

    Refactored tests

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,6 +2,7 @@ require "airport"
 require "plane"
 
 describe Airport do
+  let(:plane) { Plane.new }
 
   describe "#land" do
     it "allows a plane to land" do
@@ -9,7 +10,6 @@ describe Airport do
     end
 
     it "stores the planes that have landed" do
-      plane = Plane.new
       subject.land(plane)
       expect(subject.hangar).to include(plane)
     end
@@ -21,12 +21,10 @@ describe Airport do
     end
 
     it "returns a message confirming plane has gone when plane takes off" do
-      plane = Plane.new
       expect(subject.takeoff(plane)).to eq "#{plane} has taken off"
     end
 
     it "removes a plane that has just taken off from the airport hangar" do
-      plane = Plane.new
       subject.land(plane)
       subject.takeoff(plane)
       expect(subject.hangar).not_to include(plane)
