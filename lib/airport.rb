ab078e0f00ee58901d16931f49b24a3d5f1e3baa
Alastair Edmonds <alastair.edmonds@gmail.com>
2018-08-11 14:13:24 +0100

    mock plane class + gTest for #land adding a plane to the hangar

--- lib/airport.rb
+++ lib/airport.rb
@@ -2,12 +2,15 @@ require_relative 'plane'
 # require_relative 'weather'
 
 class Airport
+
+    attr_reader :hangar
     
     def initialize 
         @hangar = []
     end
 
     def land(plane)
+        @hangar << plane
     end
 
     def take_off(plane)
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -6,14 +6,23 @@
 # SR1. I would like a default airport capacity that can be overridden as appropriate
 
 require 'airport'
+# require 'plane'
+# Do I need to require 'plane' in order to double instances of the Plane class?
 
 describe Airport do  
     subject(:airport) { described_class.new }
+    let(:mockPlane) { double :plane }
+    # let(:mockNote) { double :note, :title => 't1', :body => 'b1' }
+    # let(:bike) { double :bike}
+    # let(:broken_bike) { double :bike, working: false }
 
     describe '#land', :land do
         it { is_expected.to respond_to(:land).with(1).argument }
-
-    #     it 'stores plane in @hangar' do 
+   
+        it 'puts a plane in the hangar' do
+            subject.land(:mockPlane)
+            expect(subject.hangar).to include (:mockPlane)
+        end
     end
 
     describe '#take_off', :take_off do 
