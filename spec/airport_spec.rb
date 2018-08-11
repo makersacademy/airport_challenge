345cb2c65159f2654acd75d63f71806d889b0d27
Samir Soormally <samirsoormally@gmail.com>
2018-08-11 14:28:04 +0100

    refactored airport_spec.rb for userstory 1

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -3,19 +3,14 @@ require "airport"
 describe Airport do
   # let mocking functions can go there e.g. ...
   # let(:mockWeather)  { double :weather, good?: true}
-  
-  it "iniatializes" do
-    expect(Airport.new).not_to eql(nil)
-  end
+  describe "when first initialized"
+    it { is_expected.not_to eql(nil) }
 
   describe "#land"
-  it "responds to land method" do
-    expect(subject).to respond_to(:land)
-  end
+    it { is_expected.to respond_to :land }
 
   it "takes 1 argument" do
     expect(subject).to respond_to(:land).with(1).arguments
   end
-
 
 end
