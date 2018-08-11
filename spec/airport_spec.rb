c9a56feb77315597f52d5baa2a5a98eb25366114
James Sutherland <jrsutherland78@googlemail.com>
2018-08-11 12:48:20 +0100

    update airport_spec.rb test - specific planes taking off are removed from #planes_on_ground

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -4,18 +4,20 @@ require 'plane'
 describe Airport do
 
   new_airport = Airport.new
-  new_plane = Plane.new
-  new_airport.landing(new_plane)
 
   it 'responds to #landing' do
     expect(subject).to respond_to(:landing).with(1).argument
   end
 
   it 'stores the names of the planes landing in an array' do
+    new_plane = Plane.new
+    new_airport.landing(new_plane)
     expect(new_airport.planes_on_ground).to eq ([new_plane])
   end
 
-  it 'removes a plane from the array when 1 takes off' do
-    expect(new_airport.take_off(new_plane)).to eq (new_plane)
+  it 'removes a named plane from @planes_on_ground when it takes off' do
+    specific_plane = Plane.new
+    new_airport.landing(specific_plane)
+    expect(new_airport.take_off(specific_plane)).to eq (specific_plane)
   end
 end
