ca56e008598c390c08d3e79b3400550a569bd687
Harry <harryjp1995@gmail.com>
2018-08-11 16:16:40 +0100

    Adds flight_no and condition instance variables to plane

--- lib/plane.rb
+++ lib/plane.rb
@@ -1,2 +1,8 @@
 class Plane
+  attr_reader :flight_no, :condition
+
+  def initialize(flight_no=nil, condition=:working)
+    @flight_no = flight_no
+    @condition = condition
+  end
 end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -1,5 +1,12 @@
 require 'plane'
 
 describe Plane do
+  it 'initializes with flight_no set to nil by default' do
+    expect(subject.flight_no).to eq nil
+  end
 
+  it 'flight_no can be set as required' do
+    subject.instance_variable_set(:@flight_no, 123)
+    expect(subject.flight_no).to eq 123
+  end 
 end
