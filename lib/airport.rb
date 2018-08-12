f71b52a533c5c8d085364055fa83c682e411868a
jdm79 <james.malvern@gmail.com>
2018-08-12 15:51:27 +0100

    added fail error to land method

--- lib/airport.rb
+++ lib/airport.rb
@@ -3,20 +3,35 @@ require './lib/plane'
 class Airport
 
   attr_reader :planes
+  attr_reader :stormy
 
   def initialize
     @planes = []
   end
 
   def land(plane)
+    fail "Weather is stormy - no landing, please" if stormy
     @planes << plane
     planes[0]
   end
 
   def take_off
+    fail "Weather is stormy - no taking off, please" if stormy
     @planes.pop
-    p "airport now empty"
     planes[-1]
+  end
+
+  def weather
+    chance = rand(1..20)
+    if chance == 20
+      @stormy = true
+    else
+      @stormy = false
+    end
+  end
+
+  def report_stormy
+    @stormy = true
   end
 
 end
