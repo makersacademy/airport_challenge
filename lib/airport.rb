b2ff57f0610bde178076b1a62c7e4a3554e5a10d
jdm79 <james.malvern@gmail.com>
2018-08-12 14:10:58 +0100

    added planes array to populate with planes using the land method

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,7 +1,16 @@
 require './lib/plane'
 
 class Airport
-  def land(*)
-    true
+
+  attr_reader :planes
+
+  def initialize
+    @planes = []
   end
+
+  def land(plane)
+    @planes << plane
+    planes[0]
+  end
+
 end
