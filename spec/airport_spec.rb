f2002ad6b72cc110bf4dfea9f5b0012ffd01445f
Marcus <marcus.kerr94@gmail.com>
2018-08-12 21:41:52 +0100

    Feature: Prevent takeoff and landing when stormy, UT(RED)

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,21 +1,55 @@
 require "Airport"
 describe Airport do
+
+    # Doubles
+    # Plane class
+    # Instance of plane class
+  let(:plane) { double :plane_instance }
+
+    # Weather class
+    # Instance of weather class
+  let(:weather_class) { double :weather_class, new: weather }
+  let(:weather)       { double :weather_instance            }
   subject(:airport) { described_class.new }
 
   describe "#land" do
     it "Lands a plane" do
-      plane = Plane.new
       airport.land(plane)
       expect(airport.land(plane)).to eq "#{plane} has landed"
     end
+  
+    context "stormy weather" do
+      it "does not allow a plane to land" do
+      end
+    end 
   end
 
   describe "#takeoff" do
     it "Allows a plane to take off, confirm it is no longer in the hangar" do
-      plane = Plane.new
       airport.land(plane)
       expect(airport.take_off(plane)).to eq "#{plane} has taken off"
       expect(airport.hangar).not_to include(plane)
+    end 
+  
+    context "stormy weather" do
+      it "does not allow a plane to takeoff" do
+      end
+    end 
+  end
+
+  describe "#isStormy" do
+    context "stomry" do
+      it "return true" do
+        allow(weather).to receive(:generate) { "Stomry" }
+        expect(airport.isStormy?).to eq true
+      end
+    end
+
+    context "sunny" do
+      it "returns false" do
+        allow(weather).to receive(:generate) { "Sunny" }
+        expect(airport.isStormy?).to eq false
+      end  
     end 
   end
 end
