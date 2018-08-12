11705af31dce021481691cee0542094eabc5fb15
Freddie <fredjlawson@gmail.com>
2018-08-12 18:47:56 +0100

    final commit, including one more test for .Weather

--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -1,8 +1,13 @@
 describe Weather do
-  describe "#check" do
+  describe "#today" do
     it "returns the weather today" do
       subject.generate
       expect(subject.today).to eq("Stormy").or(eq("Sunny"))
+    end
+  end
+  describe "#generate" do
+    it "randomly generates weather" do
+      expect(subject.generate).to eq("Stormy").or(eq("Sunny"))
     end
   end
 end
