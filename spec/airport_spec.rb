bab74f8d7cbfd1bceb29c4fe15d3663425224c3f
leekarho <k-h_lee@hotmail.co.uk>
2018-08-12 10:43:35 +0100

    sorting unit tests

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,56 +2,64 @@ require 'airport'
 
 describe Airport do
 
-  it 'lands a plane' do
-    expect(subject).to respond_to(:land_plane).with(1).argument
+  describe '#takeoff' do
+
+    it 'plane takes off' do
+      expect(subject).to respond_to (:takeoff)
+    end
+
+    it 'plane takes off and confirms no longer in airport' do
+      allow(subject).to receive(:forecast) {false}
+      plane = Plane.new
+      subject.land_plane(plane)
+      expect(subject.takeoff(plane)).to eq plane
+    end
+
+    it 'raises error for takeoff if the weather is stormy' do
+      allow(subject).to receive(:forecast).and_return('stormy')
+      plane = Plane.new
+      expect {subject.takeoff(plane)}.to raise_error 'weather is stormy'
+    end
+
+    it 'raises error if takeoff plane is not at airport' do
+      allow(subject).to receive(:forecast) {false}
+      plane = Plane.new
+      expect {subject.takeoff(plane)}.to raise_error 'plane not at airport'
+    end
+
   end
 
-  it 'lands a plane and shows plane in airport' do
-    plane = Plane.new
-    expect(subject.land_plane(plane)).to eq plane
-  end
+  describe '#land_plane' do
 
-  it 'plane takes off' do
-    expect(subject).to respond_to (:takeoff)
-  end
+    it 'lands a plane' do
+      expect(subject).to respond_to(:land_plane).with(1).argument
+    end
 
-  it 'plane takes off and confirms no longer in airport' do
-    allow(subject).to receive(:forecast) {false}
-    plane = Plane.new
-    subject.land_plane(plane)
-    expect(subject.takeoff(plane)).to eq plane
-  end
+    it 'lands a plane and shows plane in airport' do
+      plane = Plane.new
+      expect(subject.land_plane(plane)).to eq plane
+    end
 
-  it 'raises error if the weather is stormy' do
-    allow(subject).to receive(:forecast).and_return('stormy')
-    plane = Plane.new
-    expect {subject.land_plane(plane)}.to raise_error 'weather is stormy'
-  end
+    it 'raises error if the weather is stormy' do
+      allow(subject).to receive(:forecast).and_return('stormy')
+      plane = Plane.new
+      expect {subject.land_plane(plane)}.to raise_error 'weather is stormy'
+    end
 
-  it 'raises error for takeoff if the weather is stormy' do
-    allow(subject).to receive(:forecast).and_return('stormy')
-    plane = Plane.new
-    expect {subject.takeoff(plane)}.to raise_error 'weather is stormy'
-  end
+    it 'raises error if plane is already at the airport' do
+      allow(subject).to receive(:forecast) {false}
+      plane = Plane.new
+      subject.land_plane(plane)
+      expect {subject.land_plane(plane)}.to raise_error 'plane already at airport'
+    end
 
-  it 'raises error if plane is already at the airport' do
-    allow(subject).to receive(:forecast) {false}
-    plane = Plane.new
-    subject.land_plane(plane)
-    expect {subject.land_plane(plane)}.to raise_error 'plane already at airport'
-  end
+    it 'plane cannot land if airport is full' do
+      allow(subject).to receive(:forecast) {false}
+      plane = Plane.new
+      20.times {|x| subject.land_plane(x += 1)}
+      expect {subject.land_plane(plane)}.to raise_error 'airport is full'
+    end
 
-  it 'raises error if takeoff plane is not at airport' do
-    allow(subject).to receive(:forecast) {false}
-    plane = Plane.new
-    expect {subject.takeoff(plane)}.to raise_error 'plane not at airport'
-  end
-
-  it 'plane cannot land if airport is full' do
-    allow(subject).to receive(:forecast) {false}
-    plane = Plane.new
-    20.times {|x| subject.land_plane(x += 1)}
-    expect {subject.land_plane(plane)}.to raise_error 'airport is full'
   end
 
   it 'has a default capacity' do
