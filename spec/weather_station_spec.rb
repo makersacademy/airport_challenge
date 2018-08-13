6936ebeacacd5527b59abfd9501e4df224ccf401
Alastair Edmonds <alastair.edmonds@gmail.com>
2018-08-13 20:56:49 +0100

    lint weather_station_spec

--- spec/weather_station_spec.rb
+++ spec/weather_station_spec.rb
@@ -1,12 +1,12 @@
 require 'weather_station'
 
 describe WeatherStation do 
-    subject(:weatherstation) { described_class.new }      # Or: subject(:weatherstation) { WeatherStation.new }
+  subject(:weatherstation) { described_class.new } # Or: subject(:weatherstation) { WeatherStation.new }
 
-    describe '#conditions_safe?' do 
-        it 'checks conditions safe to manouevre' do
-            expect(subject.conditions_safe?).to eq(true).or(eq(false))
-        end   
-    end
+  describe '#conditions_safe?' do
+    it 'checks conditions safe to manouevre' do
+      expect(subject.conditions_safe?).to eq(true).or(eq(false))
+    end   
+  end
 
 end
