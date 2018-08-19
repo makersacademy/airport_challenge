904db57c17f24eb26b2442d810e4d0ff6d7f2b99
Rashika Patel <rashika_patel@hotmail.com>
2018-08-19 03:16:02 +0100

    fix test for randomness

--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -1,7 +1,13 @@
 require 'weather'
 
 describe Weather do
-  it 'stormy? is true or false' do
-    expect(subject.stormy?).to be(true).or(be(false))
+  it 'stormy? is false' do
+    expect(subject).to receive(:rand) { 50 }
+    expect(subject.stormy?).to eq false
+  end
+
+    it 'stormy? is true' do
+      expect(subject).to receive(:rand) { 1 }
+      expect(subject.stormy?).to eq true
   end
 end
