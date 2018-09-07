b10e595bc9f8ecdc950e191a649e33b9d7c7d0e9
Vu Le <core.safecrlf>
2018-09-07 15:43:02 +0100

    corrected rspec test to only test if the land_plane method exists and not  calling Airport.land_plane as it expects a object

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -3,6 +3,6 @@ require_relative "../lib/airport"
 describe Airport do
     
     it 'expect airport to responds to calling "land_plane' do
-        expect(subject.land_plane).to respond_to(:land_plane)
+        expect(subject).to respond_to(:land_plane)
     end
 end
