718cad34ce3dda0c6341549ef4ac503fea955a77
Vu Le <core.safecrlf>
2018-09-07 15:33:52 +0100

    wrote test for calling land plane on airport - rspec fails

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,1 +1,8 @@
-require_relative '../lib/airport'
+require_relative "../lib/airport"
+
+describe Airport do
+    
+    it 'expect airport to responds to calling "land_plane' do
+        expect(subject.land_plane).to respond_to(:land_plane)
+    end
+end
