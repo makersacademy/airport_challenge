14fc7c769aefa5ca3b6e6521e7f2829686999eb1
Vu Le <core.safecrlf>
2018-09-07 16:09:08 +0100

    unit and feature test passes, first story completed

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,6 +2,10 @@ require_relative "../lib/airport"
 
 describe Airport do
     
+    # As an air traffic controller 
+    # So I can get passengers to a destination 
+    # I want to instruct a plane to land at an airport
+
     it 'expect airport to responds to calling "land_plane' do
         expect(subject).to respond_to(:land_plane)
     end
