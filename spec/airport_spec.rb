89ea56e0824fe0da434ecf37607f0d13a8c4043a
Vu Le <core.safecrlf>
2018-09-07 16:13:58 +0100

    added test to test thaat airport has a take_off method

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -14,4 +14,15 @@ describe Airport do
         plane = Plane.new
         expect(subject.land_plane(plane)).to eq(plane)
     end
+
+    # As an air traffic controller 
+    # So I can get passengers on the way to their destination 
+    # I want to instruct a plane to take off from an airport and 
+    # confirm that it is no longer in the airport
+
+    it 'expect airport to responds to calling "take_off' do
+        expect(subject).to respond_to(:take_off)
+    end
+
+
 end
