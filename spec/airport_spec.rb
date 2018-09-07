e3f9ef4f09e515f19da9f3454ec90f545162e2fe
Vu Le <core.safecrlf>
2018-09-07 15:51:04 +0100

    wrote test for airport to be able to land an plane object

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -5,4 +5,8 @@ describe Airport do
     it 'expect airport to responds to calling "land_plane' do
         expect(subject).to respond_to(:land_plane)
     end
+
+    it 'expect airport to be able to land a plane at the airport' do
+        expect(subject.land_plane(plane)).to eq(plane)
+    end
 end
