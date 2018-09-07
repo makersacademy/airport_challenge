584b3618ae0891541d6ceb3e0467855e05bce066
Vu Le <core.safecrlf>
2018-09-07 16:51:59 +0100

    added code to now store the plane in an empty array that is initialize when airport is created - some test pass but other fails

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,11 +1,17 @@
 class Airport
     
+    attr_accessor :plane_list
+    
+    def initialize
+        @plane_list = []
+    end
+
     def land_plane(plane)
-        plane
+        @plane_list.push(plane)
     end
 
     def take_off(plane)
-        plane
+        @plane_list.pop
     end
 end
 
