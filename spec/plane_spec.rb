5ce6cb6ffc40c5e349987911e837dcce26f10cdc
Jon Urry <jon@urry.me>
2018-09-08 13:11:01 +0100

    Change subject of Plane tests to plane

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -1,12 +1,13 @@
 require 'plane'
 
 RSpec.describe Plane do
+  subject(:plane) { Plane.new }
   let(:airport) { double :airport }
   context 'As an air traffic controller 
            So I can get passengers to a destination 
            I want to instruct a plane to land at an airport' do
     it 'should land a plane' do
-      expect(subject.land(airport)).to eq true
+      expect(plane.land(airport)).to eq true
     end
   end
 end
