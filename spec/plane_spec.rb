1dc012080eb53872bcdcb576ca630c003218fa24
Andres Faraone <andresfaraone@outlook.com>
2018-09-08 15:13:25 +0100

    Added tests for @name

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -1,5 +1,15 @@
 require 'plane'
 
 describe Plane do
-  
+  it { is_expected.to respond_to(:name) }  
+  describe "@name" do
+    it 'assigns default value to @name' do
+      expect(subject.name).to eq("Plane")
+    end
+
+    it 'assigns initialised parameter as @name' do
+      subject =  Plane.new("Boeing 74")  
+      expect(subject.name).to eq("Boeing 74")
+    end
+  end
 end
