c13a1fe458077347dc82059d32ae0eacdebde16a
Darcie <darciewalsh@hotmail.com>
2018-09-08 12:45:40 +0100

    Instruct plane to take-off

--- lib/airport.rb
+++ lib/airport.rb
@@ -2,4 +2,8 @@ class Airport
   def land_plane
     p "Plane has landed"
   end
+
+  def takeoff(plane)
+    p "Plane has taken off"
+  end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,10 +1,27 @@
 require 'airport'
+require 'plane'
 
 describe Airport do
-  # desribe '#land_plane' do - add in nested desribe statements for each method 
+  # desribe '#land_plane' do - add in nested desribe statements for each method
   it { is_expected.to respond_to :land_plane}
 
   it 'lands a plane' do
     expect(Airport.new.land_plane).to eq("Plane has landed")
   end
+
+  # describe '#grounded_planes' do
+  # it "stores planes in the hangar" do
+  # expect(Airport.new.grounded_planes).to be_a(Array)
+  # end
+
+  # desrcibe '#takeoff(plane)'
+  it { is_expected.to respond_to(:takeoff).with(1).argument }
+
+  it 'allows plane to takeoff' do
+    plane = Plane.new
+    expect(Airport.new.takeoff(plane)).to eq("Plane has taken off")
+
+  end
+
+
 end
