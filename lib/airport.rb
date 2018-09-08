583e039d62c075a28615c6691872f6479e64c882
Andres Faraone <andresfaraone@outlook.com>
2018-09-08 15:54:40 +0100

    #land confirmation message includes a list of planes in hangar

--- lib/airport.rb
+++ lib/airport.rb
@@ -7,5 +7,13 @@ class Airport
   def land(plane)
     @hangar.push(plane)
     puts "#{plane.name} has landed"
+    puts "Hangar now holds #{display_hangar}"
+  end
+
+  private
+  def display_hangar
+    return if @hangar.empty?
+    names = @hangar.map { |plane| plane.name }
+    names.join(", ")
   end
 end
