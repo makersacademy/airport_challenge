0a5d1fe84ba09fc648315456d486e17d94305a9f
Jonathan Andrews <jonnyandrews.jma@gmail.com>
2018-09-08 14:48:08 +0000

    modified Plane#land_at to call  Airport#put_plane_in_hanger

--- lib/plane.rb
+++ lib/plane.rb
@@ -2,24 +2,20 @@ require "airport"
 
 class Plane
 
-  def intialze
+  def initialize
     @location
   end
 
   def land_at(airport)
-    return "Landing is too dangerous" if bad_weather?
+    return "Landing is too dangerous" if airport.bad_weather?
+    return "Sorry, the hanger is full" if airport.hanger_full?
+    airport.put_plane_in_hanger(self)
     @location = airport
   end
 
   def take_off
-    return "Taking off is too dangerous" if bad_weather?
+    return "Taking off is too dangerous" if @location.bad_weather?
     @location = "In the air."
-  end
-
-  def bad_weather?
-    # true if rand(10).zero?
-    true
-    # false
   end
 
 end
