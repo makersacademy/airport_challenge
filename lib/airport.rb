ef3ff98ace27d075e4570d89079a22a351b8fac2
Jonathan Andrews <jonnyandrews.jma@gmail.com>
2018-09-08 14:44:59 +0000

    Set default hanger size to 20

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,9 +1,24 @@
+require 'plane'
+
 class Airport
-  def intialze
+
+  DEFAULT_HANGER_SIZE = 20
+
+  def initialize
     @hanger = []
   end
 
+  def put_plane_in_hanger(plane)
+    @hanger.push(plane)
+  end
+
   def hanger_full?
-    true
+    true if @hanger.count == DEFAULT_HANGER_SIZE
+  end
+
+  def bad_weather?
+    # true if rand(10).zero?
+    # true
+    false
   end
 end
