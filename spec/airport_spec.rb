6fa836d169e202c6adc019b77dd21485c3302f4f
Andres Faraone <andresfaraone@outlook.com>
2018-09-08 16:58:26 +0100

    Tests #takeoff for @hangar status and confirmation output

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -11,8 +11,7 @@ mockplane1 = MockPlane.new("Plane 1")
 mockplane2 = MockPlane.new("Plane 2")
 
 describe Airport do
-  describe 'Tests for initialized instance' do
-    it { is_expected.to respond_to(:land).with(1).argument }
+  describe '@hangar' do
     it { is_expected.to respond_to(:hangar) }
     it 'has @hangar as empty array' do
       expect(subject.hangar).to eq([])
@@ -20,21 +19,35 @@ describe Airport do
   end
   
   describe '#land' do
+    it { is_expected.to respond_to(:land).with(1).argument }
     it 'adds a plane object to @hangar and puts a confirmation message' do
-      expect { subject.land(mockplane1) }.to output("Plane 1 has landed\nHangar now holds Plane 1\n").to_stdout
+      expect { subject.land(mockplane1) }.to output("Plane 1 has landed.\nThe hangar contains Plane 1.\n").to_stdout
       expect(subject.hangar).to eq([mockplane1])
-      expect { subject.land(mockplane2) }.to output("Plane 2 has landed\nHangar now holds Plane 1, Plane 2\n").to_stdout
+      expect { subject.land(mockplane2) }.to output("Plane 2 has landed.\nThe hangar contains Plane 1, Plane 2.\n").to_stdout
       expect(subject.hangar).to eq([mockplane1, mockplane2])
     end
   end
 
-  describe '#display_hangar (private method)' do
-    xit 'displays @hangar elements in user-friendly format' do
-      expect(subject.send(:display_hangar)).to eq(nil)
+  describe '#takeoff' do
+    it { is_expected.to respond_to(:takeoff).with(1).argument }
+    it 'removes a plane from @hangar and puts a confirmation message' do
       subject.land(mockplane1)
-      expect(subject.send(:display_hangar)).to eq("Plane 1")
       subject.land(mockplane2)
-      expect(subject.send(:display_hangar)).to eq("Plane 1, Plane 2")
+      expect{ subject.takeoff(mockplane1) }.to output("Plane 1 has taken off.\nThe hangar contains Plane 2.\n").to_stdout
+      expect(subject.hangar).to eq([mockplane2])
+      expect{ subject.takeoff(mockplane2) }.to output("Plane 2 has taken off.\nThere are no planes in the hangar.\n").to_stdout
+      expect(subject.hangar).to eq([])
+    end
+  end
+
+  # Test for private methods
+  describe '#display_hangar' do
+    it 'displays @hangar elements in user-friendly format' do
+      expect(subject.send(:display_hangar)).to eq("There are no planes in the hangar.")
+      subject.land(mockplane1)
+      expect(subject.send(:display_hangar)).to eq("The hangar contains Plane 1.")
+      subject.land(mockplane2)
+      expect(subject.send(:display_hangar)).to eq("The hangar contains Plane 1, Plane 2.")
     end
   end
 end
