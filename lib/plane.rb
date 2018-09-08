e5ade02e998df8e561ea414ecea9c0ef43d33d8b
Jonathan Andrews <jonnyandrews.jma@gmail.com>
2018-09-08 20:12:29 +0000

    added Plane#grounded(airport)

--- lib/plane.rb
+++ lib/plane.rb
@@ -1,2 +1,19 @@
+require_relative 'airport'
+
 class Plane
+
+  attr_reader :location
+
+  def initialize
+    @location = "The Factory"
+  end
+
+  def airborn
+    @location = "Airborn"
+  end
+
+  def grounded(airport)
+    @location = airport
+  end
+
 end
