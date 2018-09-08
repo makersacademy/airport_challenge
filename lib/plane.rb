133a0ae5a29d70563dfa4da783addc6c47911277
Jon Urry <jon@urry.me>
2018-09-08 13:33:16 +0100

    Plane should #take_off from an airport

--- lib/plane.rb
+++ lib/plane.rb
@@ -2,9 +2,13 @@ class Plane
   def initialize(airport = nil)
     @airport = airport
   end
-  
+
   def land(airport)
     @airport = airport
     true
+  end
+
+  def take_off
+    nil
   end
 end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -3,11 +3,19 @@ require 'plane'
 RSpec.describe Plane do
   subject(:plane) { Plane.new }
   let(:airport) { double :airport }
-  context 'As an air traffic controller 
-           So I can get passengers to a destination 
-           I want to instruct a plane to land at an airport' do
+
+  context 'when the weather is fine' do
+    # As an air traffic controller 
+    # So I can get passengers to a destination 
+    # I want to instruct a plane to land at an airport
     it 'should land a plane' do
       expect(plane.land(airport)).to eq true
+    end
+    # As an air traffic controller 
+    # So I can get passengers on the way to their destination 
+    # I want to instruct a plane to take off from an airport and confirm that it is no longer in the airport
+    it 'should take off from an airport and return nil' do
+      expect(plane.take_off).to eq nil
     end
   end
 end
