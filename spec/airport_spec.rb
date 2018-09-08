83d9f423bcfcab5318f4fee48971ac2140ce1794
Jonathan Andrews <jonnyandrews.jma@gmail.com>
2018-09-08 19:26:07 +0000

    testing overiding Airport default @hanger capacity

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,22 +1,28 @@
 require 'airport'
 
-# describe Airport do
-# # weather needs to be good
-# # and the hanger can't be full
-#   it "has a working Airport#land(plane) method" do
-#     plane = Plane.new
-#     expect(subject.land(plane)).to eq([plane])
-#   end
-#
-#   it "has a working Airport#takeoff(plane) method" do
-#     plane = Plane.new
-#     expect(subject.takeoff(plane)).to eq(plane)
-#   end
-#
-# end
+describe Airport do
+# weather needs to be good
+# and the hanger can't be full
+  it "has a working Airport#land(plane) method" do
+    plane = Plane.new
+    expect(subject.land(plane)).to eq([plane])
+  end
+
+  it "has a working Airport#takeoff(plane) method" do
+    plane = Plane.new
+    expect(subject.takeoff(plane)).to eq(plane)
+  end
+
+  it "can set Airport @hanger capacity to 0" do
+    airport = Airport.new(0)
+    expect(airport.land(Plane.new)).to eq("The plane can't land because the hanger is full")
+  end
+
+end
 
 # describe Airport do
 # # weather needs to be bad
+# # and the hanger can't be full
 #   it "has a working Airport#bad_weather? method" do
 #     expect(subject.bad_weather?).to eq(true)
 #   end
@@ -30,14 +36,14 @@ require 'airport'
 #   end
 # end
 
-describe Airport do
-# weather need to be good
-# and hanger needs to full
-  it "has a working Airport.hanger_full? method" do
-    expect(subject.hanger_full?).to eq(true)
-  end
-
-  it "can prevent landing when the hanger is full" do
-    expect(subject.land(Plane.new)).to eq("The plane can't land because the hanger is full.")
-  end
-end
+# describe Airport do
+# # weather need to be good
+# # and hanger needs to full
+#   it "has a working Airport.hanger_full? method" do
+#     expect(subject.hanger_full?).to eq(true)
+#   end
+#
+#   it "can prevent landing when the hanger is full" do
+#     expect(subject.land(Plane.new)).to eq("The plane can't land because the hanger is full")
+#   end
+# end
