682c17b787cb0ee2ac203fa66378369f220c61dc
Jonathan Andrews <jonnyandrews.jma@gmail.com>
2018-09-08 14:55:14 +0000

    testing overiding the DEFAULT_HANGER_SIZE

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -7,4 +7,10 @@ describe Airport do
     end
     expect(Plane.new.land_at(subject)).to eq("Sorry, the hanger is full")
   end
+
+  it "'s DEFAULT_HANGER_SIZE can be overidden" do
+    airport = Airport.new(1)
+    airport.put_plane_in_hanger(Plane.new)
+    expect(Plane.new.land_at(airport)).to eq("Sorry, the hanger is full")
+  end
 end
