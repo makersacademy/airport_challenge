6cf006bfc65d41e5e707edcca420ef42678882a7
Jonathan Andrews <jonnyandrews.jma@gmail.com>
2018-09-08 18:14:46 +0000

    Added Airport#take_off(plane)

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -5,4 +5,9 @@ describe Airport do
     plane = Plane.new
     expect(subject.land(plane)).to eq([plane])
   end
+
+  it "has a working Airport#take_off(plane) method" do
+    plane = Plane.new
+    expect(subject.take_off(plane)).to eq(plane)
+  end
 end
