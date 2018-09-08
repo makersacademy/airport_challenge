bb75eb42180d1eb066298d23aef1b3492b72d378
Jonathan Andrews <jonnyandrews.jma@gmail.com>
2018-09-08 18:40:27 +0000

    testing if Airport#takeoff(plane) is cancelled in bad weather conditions

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -6,12 +6,16 @@ describe Airport do
     expect(subject.land(plane)).to eq([plane])
   end
 
-  it "has a working Airport#take_off(plane) method" do
+  xit "has a working Airport#takeoff(plane) method" do
     plane = Plane.new
-    expect(subject.take_off(plane)).to eq(plane)
+    expect(subject.takeoff(plane)).to eq(plane)
   end
 
   it "has a working Airport#bad_weather? method" do
     expect(subject.bad_weather?).to eq(true)
+  end
+
+  it "can prevent takeoff when there is bad weather" do
+    expect(subject.takeoff).to eq("The weather is to bad to fly")
   end
 end
