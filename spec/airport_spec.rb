818b3b19ed44d9566fc7579cdd7d42c2dda36d14
Antonio <antoniodaria@msn.com>
2018-09-08 21:43:28 +0100

    refactoring the airport_spec.rb file

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -4,19 +4,26 @@ require 'plane'
 
 describe Airport do
   subject(:airport) { described_class.new(10) }
-  it 'instructs a plane to land' do
-    expect(airport).to respond_to(:land).with(1).argument
-  end
+  let(:plane) {double :plane}
 
-  it 'instructs a plane to take off' do
-    expect(airport).to respond_to(:take_off).with(1).argument
-  end
-
-  it 'dos not allow landing when at its full capacity' do
-    plane = double :plane
-    10.times do
-      airport.land(plane)
+  describe '#land' do
+    it 'instructs a plane to land' do
+      expect(airport).to respond_to(:land).with(1).argument
     end
-    expect { airport.land(plane) }.to raise_error 'Airport is full, cannot land'
+
+    context 'when airport is full' do
+      it 'raises an error' do
+        10.times do
+          airport.land(plane)
+        end
+        expect { airport.land(plane) }.to raise_error 'Airport is full, cannot land'
+      end
+    end
+  end
+
+  describe '#take_off' do
+    it 'instructs a plane to take off' do
+      expect(airport).to respond_to(:take_off).with(1).argument
+    end
   end
 end
