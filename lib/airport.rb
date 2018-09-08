40f80e997ca121c0574e2feb372139ac79bc8766
Antonio <antoniodaria@msn.com>
2018-09-08 19:22:25 +0100

    Implementing user story 2 allowing plane to take off

--- lib/airport.rb
+++ lib/airport.rb
@@ -2,4 +2,6 @@ class Airport
   def land(plane)
   end
 
+  def take_off(plane)
+  end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,13 +2,14 @@ require 'airport'
 require 'plane'
 # require 'pry'
 
-# As an air traffic controller
-# So I can get passengers to a destination
-# I want to instruct a plane to land at an airport
 
 describe Airport do
   subject(:airport) {described_class.new}
   it 'instructs a plane to land' do
   expect(airport).to respond_to(:land).with(1).argument
+  end
+
+  it 'instructs a plane to take off' do
+    expect(airport).to respond_to(:take_off).with(1).argument
   end
 end
