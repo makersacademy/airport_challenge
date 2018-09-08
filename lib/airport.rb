de3020b7b7abaf27e00f3dfb1804f8ccbef42fba
NadiaAiraf <aidanfaria@Aidans-MBP.lan>
2018-09-08 16:14:39 +0100

    added storm prevention controls, ability to set default capacity and prevent landing when airports are full

--- lib/airport.rb
+++ lib/airport.rb
@@ -6,14 +6,31 @@ class Airport
   def initialize(capacity = 20)
     @planes = []
     @capacity = capacity
+    @stormy = false
   end
 
   def land_plane(plane)
+    fail 'This airplane is already in the airport' if plane_exists?(plane)
+    fail 'The airport is full' if full?
+    fail 'Weather is stormy: no planes are to land' if @stormy
     @planes << plane
   end
 
   def take_off
+    fail 'Weather is stormy: no planes are to take off' if @stormy
     @planes.pop
     'airplane has taken off and is no longer in the airport'
+  end
+
+  def weather_turns
+    @stormy = !@stormy
+  end
+
+  def full?
+    @planes.size >= @capacity
+  end
+
+  def plane_exists?(plane)
+    @planes.include? plane
   end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -20,4 +20,30 @@ describe Airport do
     4.times { subject.take_off }
     expect(subject.planes.length).to eq 2
   end
+
+  it 'raises an error preventing planes from taking off if stormy' do
+    subject.weather_turns
+    expect { subject.take_off }.to raise_error('Weather is stormy: no planes are to take off')
+  end
+
+  it 'raises an error preventing planes from landing if stormy' do
+    subject.weather_turns
+    expect { subject.land_plane(Airplane.new) }.to raise_error('Weather is stormy: no planes are to land')
+  end
+
+  it 'raises an error when the airport is full' do
+    expect { 21.times { subject.land_plane(Airplane.new) } }.to raise_error('The airport is full')
+  end
+
+  it 'expects the airplane to allow 50 planes to land if we set capacity to 50' do
+    gatwick = Airport.new(50)
+    50.times { gatwick.land_plane(Airplane.new) }
+    expect(gatwick.planes.size).to eq 50
+  end
+
+  it 'raises an error if you try to land a plane when it is already in the airport' do
+    boeing = Airplane.new
+    subject.land_plane(boeing)
+    expect { subject.land_plane(boeing) }.to raise_error('This airplane is already in the airport')
+  end
 end
