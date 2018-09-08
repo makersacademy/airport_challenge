a68563b67ec2c7fa9e57d90c761dfbfeecd2cbfc
Jonathan Andrews <jonnyandrews.jma@gmail.com>
2018-09-08 12:43:29 +0000

    added Bad weather guard statement to Plane#take_off

--- lib/plane.rb
+++ lib/plane.rb
@@ -11,7 +11,12 @@ class Plane
   end
 
   def take_off
+    fail "Take off too dangerous" if bad_weather?
     @location = "In the air."
+  end
+
+  def bad_weather?
+    true if rand(10).zero?
   end
 
 end
