a33658774bdd2b12e6cb0261751125729528a3a7
Antonio <antoniodaria@msn.com>
2018-09-08 20:30:57 +0100

    Implementation of raising an error when airport is full

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,5 +1,14 @@
+
 class Airport
+
+  def initialize(capacity)
+    @capacity = capacity
+    @planes = []
+  end
+
   def land(plane)
+    raise 'Airport is full, cannot land' if @planes.length >= @capacity
+    @planes << plane
   end
 
   def take_off(plane)
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,14 +2,21 @@ require 'airport'
 require 'plane'
 # require 'pry'
 
-
 describe Airport do
-  subject(:airport) {described_class.new}
+  subject(:airport) { described_class.new(10) }
   it 'instructs a plane to land' do
-  expect(airport).to respond_to(:land).with(1).argument
+    expect(airport).to respond_to(:land).with(1).argument
   end
 
   it 'instructs a plane to take off' do
     expect(airport).to respond_to(:take_off).with(1).argument
+  end
+
+  it 'dos not allow landing when at its full capacity' do
+    plane = double :plane
+    10.times do
+      airport.land(plane)
+    end
+    expect { airport.land(plane) }.to raise_error 'Airport is full, cannot land'
   end
 end
