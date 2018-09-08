2467df45df07686738bfcd8ad25015da7a66c760
Andres Faraone <andresfaraone@outlook.com>
2018-09-08 15:52:47 +0100

    #land confirmation message includes a list of planes in hangar

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -21,10 +21,20 @@ describe Airport do
   
   describe '#land' do
     it 'adds a plane object to @hangar and puts a confirmation message' do
-      expect { subject.land(mockplane1) }.to output("Plane 1 has landed\n").to_stdout
+      expect { subject.land(mockplane1) }.to output("Plane 1 has landed\nHangar now holds Plane 1\n").to_stdout
       expect(subject.hangar).to eq([mockplane1])
-      expect { subject.land(mockplane2) }.to output("Plane 2 has landed\n").to_stdout
+      expect { subject.land(mockplane2) }.to output("Plane 2 has landed\nHangar now holds Plane 1, Plane 2\n").to_stdout
       expect(subject.hangar).to eq([mockplane1, mockplane2])
+    end
+  end
+
+  describe '#display_hangar (private method)' do
+    xit 'displays @hangar elements in user-friendly format' do
+      expect(subject.send(:display_hangar)).to eq(nil)
+      subject.land(mockplane1)
+      expect(subject.send(:display_hangar)).to eq("Plane 1")
+      subject.land(mockplane2)
+      expect(subject.send(:display_hangar)).to eq("Plane 1, Plane 2")
     end
   end
 end
