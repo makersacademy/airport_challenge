17559d6609f21abde26b644e1e5af9849e66e12e
Jonathan Andrews <jonnyandrews.jma@gmail.com>
2018-09-09 14:57:18 +0000

    modified first all test to use method stubs and doubles

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -4,6 +4,7 @@ describe Airport do
   context "The weather is good and there is space in the hanger" do
     # weather needs to be good
     # and the hanger can't be full
+
     let(:plane) { double :plane }
 
     before(:each) do
@@ -26,33 +27,38 @@ describe Airport do
     end
 
     it "rejects edge cases: Airborn plane trying to takeoff" do
-      airport = Airport.new
-      airport.takeoff(plane)
-      expect(airport.takeoff(plane)).to eq("Cannot takeoff. Plane already airborn")
+      allow(plane).to receive(:location).and_return("Airborn")
+
+      expect(subject.takeoff(plane)).to eq("Cannot takeoff. Plane already airborn")
     end
 
     it "rejects edge cases: Grounded plane trying to land" do
-      plane = Plane.new
-      airport = Airport.new
-      expect(airport.land(plane)).to eq("Cannot land. Plane already on the ground")
+      allow(plane).to receive(:location).and_return(subject)
+
+      expect(subject.land(plane)).to eq("Cannot land. Plane already on the ground")
     end
 
     it "rejects edge cases: Planes cannot takeoff from airports that they aren't in" do
-      plane = Plane.new
-      airport1 = Airport.new
       airport2 = Airport.new
-      airport1.takeoff(plane)
-      airport2.land(plane)
-      expect(airport1.takeoff(plane)).to eq("Plane at wrong airport")
+      allow(plane).to receive(:location).and_return(subject)
+
+      expect(airport2.takeoff(plane)).to eq("Plane at wrong airport")
+    end
+
+    it "has a working Airport.hanger_full? method" do
+      expect(subject.hanger_full?).to eq(false)
     end
   end
 
   context "The weather is good but there is no space in the hanger" do
+    # weather need to be good
+    # and hanger needs to full
 
     let(:plane) { double :plane }
 
-    it "can set Airport @hanger capacity to 0" do
+    it "can prevent landing when the hanger is full" do
       airport = Airport.new(0)
+
       allow(airport).to receive(:bad_weather?).and_return(false)
       allow(airport).to receive(:hanger_full?).and_return(true)
 
@@ -63,38 +69,31 @@ describe Airport do
     end
   end
 
+
+
+# weather needs to be bad
+# and the hanger can't be full
+  context "The weather is bad but there's space in the hanger" do
+
+    let(:plane) { double :plane }
+
+    before(:each) do
+        allow(subject).to receive(:bad_weather?).and_return(true)
+        allow(subject).to receive(:hanger_full?).and_return(false)
+    end
+
+    it "has a working Airport#bad_weather? method" do
+      expect(subject.bad_weather?).to eq(true)
+    end
+
+    it "can prevent takeoff when there is bad weather" do
+      allow(plane).to receive(:location).and_return(subject)
+      expect(subject.takeoff(plane)).to eq("The weather is too bad to fly")
+    end
+
+    it "can prevent landing when there is bad weather" do
+      allow(plane).to receive(:location).and_return("Airborn")
+      expect(subject.land(plane)).to eq("The weather is too bad to land")
+    end
+  end
 end
-
-# describe Airport do
-# # weather needs to be bad
-# # and the hanger can't be full
-#   it "has a working Airport#bad_weather? method" do
-#     expect(subject.bad_weather?).to eq(true)
-#   end
-#
-#   it "can prevent takeoff when there is bad weather" do
-#     expect(subject.takeoff(Plane.new)).to eq("The weather is too bad to fly")
-#   end
-#
-#   it "can prevent landing when there is bad weather" do
-#     plane = Plane.new
-#     # takeoff under good weather
-#     subject.takeoff(plane)
-#     # try to land during bad weather
-#     expect(subject.land(plane)).to eq("The weather is too bad to land")
-#   end
-# end
-
-# describe Airport do
-# # weather need to be good
-# # and hanger needs to full
-#   it "has a working Airport.hanger_full? method" do
-#     expect(subject.hanger_full?).to eq(true)
-#   end
-#
-#   it "can prevent landing when the hanger is full" do
-#     plane = Plane.new
-#     Airport.new.takeoff(plane)
-#     expect(subject.land(plane)).to eq("The plane can't land because the hanger is full")
-#   end
-# end
