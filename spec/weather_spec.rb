92daeb76c90cfeb21fa3ac3a506d0345f5f56796
Andres Faraone <andresfaraone@outlook.com>
2018-09-09 14:49:39 +0100

    Fixed test

--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -3,13 +3,13 @@ require 'weather'
 describe Weather do
   it { is_expected.to respond_to(:stormy) }
 
-  it 'sets @stormy? to true if Weather object initialized with number >= 90' do
-    allow(subject).to receive(:number) { 92 }
+  it '#stormy returns true if @number >= 90' do
+    subject.instance_eval { @number = 92 }
     expect(subject.stormy).to eq(true)
   end
 
-  it 'sets @stormy? to false if Weather object initalized with number < 90' do
-    allow(subject).to receive(:number) { 32 }
+  it '#stormy returns false if @number < 90' do
+    subject.instance_eval { @number = 32 }
     expect(subject.stormy).to eq(false)
   end
 end
