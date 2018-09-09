b8287847f40e656873d4f538d234f1c2a8f81860
Jonathan Andrews <jonnyandrews.jma@gmail.com>
2018-09-09 15:38:06 +0000

    extracted methods from conditionals

--- lib/airport.rb
+++ lib/airport.rb
@@ -4,62 +4,71 @@ class Airport
 
   DEFAULT_HANGER_CAPACITY = 20
 
+  GUARDS = {
+    airborn: "Cannot takeoff. Plane already airborn",
+    on_ground: "Cannot land. Plane already on the ground",
+    on_site: "Plane at wrong airport",
+    weather_takeoff: "The weather is too bad to fly",
+    weather_landing: "The weather is too bad to land",
+    full_hanger: "The plane can't land because the hanger is full"
+  }
+
   def initialize(capacity = DEFAULT_HANGER_CAPACITY)
     @hanger = []
     @capacity = capacity
-    @airport_id = self
+    # @guard = nil
   end
 
   def land(plane)
-    return landing_guards(plane, self) if landing_guards(plane, self)
+    return @guard if landing_guards(plane)
     plane.grounded(self)
     @hanger << plane
   end
 
-  def takeoff(plane)
-    return takeoff_guards(plane, self) if takeoff_guards(plane, self)
-    @hanger.delete(plane)
-    plane.airborn
+  def landing_guards(plane)
+    return @guard = GUARDS[:on_ground] if plane_grounded?(plane)
+    return @guard = GUARDS[:weather_landing] if bad_weather?
+    return @guard = GUARDS[:full_hanger] if hanger_full?
   end
 
-  def bad_weather?
-    # rand(10).zero?
-    # true
-    false
+  def plane_grounded?(plane)
+    true unless plane.location == "Airborn"
   end
 
   def hanger_full?
     @hanger.count == @capacity
-    # @hanger.count.zero?
   end
 
-  def takeoff_guards(plane, terminal)
-    return "Cannot takeoff. Plane already airborn" if plane_airborn?(plane)
-    return "Plane at wrong airport" unless plane_at_airport?(plane, terminal)
-    return "The weather is too bad to fly" if bad_weather?
+  def bad_weather?
+    rand(10).zero?
+  end
+
+  def takeoff(plane)
+    return @guard if takeoff_guards(plane, self)
+    @hanger.delete(plane)
+    plane.airborn
+  end
+
+  def takeoff_guards(plane, airport)
+    return @guard = GUARDS[:airborn] if plane_airborn?(plane)
+    return @guard = GUARDS[:on_site] unless plane_at_airport?(plane, airport)
+    return @guard = GUARDS[:weather_takeoff] if bad_weather?
   end
 
   def plane_airborn?(plane)
     true if plane.location == "Airborn"
   end
 
-  def plane_at_airport?(plane, terminal)
-    if terminal != @airport_id || plane.location != "The Factory"
-      false
-    else
-      true
-    end
+  def plane_at_airport?(plane, airport)
+    true if plane_on_site?(plane, airport) || new_plane?(plane)
   end
 
-  def landing_guards(plane, plane_location)
-    on_ground = "Cannot land. Plane already on the ground"
-    return on_ground if plane_grounded?(plane, plane_location)
-    return "The weather is too bad to land" if bad_weather?
-    return "The plane can't land because the hanger is full" if hanger_full?
+  def plane_on_site?(plane, airport)
+    plane.location == airport
   end
 
-  def plane_grounded?(plane, plane_location)
-    true if plane.location == plane_location || plane.location == "The Factory"
+  def new_plane?(plane)
+    plane.location == "The Factory"
   end
 
 end
