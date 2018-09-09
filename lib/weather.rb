1b1b9a055b539a1936c03a920d056c47d991053b
Cameron Whitehead <camjw119@gmail.com>
2018-09-09 17:29:18 +0100

    Completed weather tests, weather class and linted both files.

--- lib/weather.rb
+++ lib/weather.rb
@@ -2,8 +2,8 @@ class Weather
 
   attr_reader :stormy_probability
 
-  def initialize(stormy_probability=0.5)
-    @stormy_probability =  stormy_probability
+  def initialize(stormy_probability = 0.5)
+    @stormy_probability = stormy_probability
   end
 
   def check_weather
--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -3,16 +3,15 @@ require 'weather'
 RSpec.describe Weather do
 
   it "can be initialized with the probability of stormy weather" do
-
+    expect(Weather).to respond_to(:new).with(1).argument
   end
 
   it "has a method for generating the weather" do
-
+    expect(subject).to respond_to(:check_weather).with(0).argument
   end
 
   it "always returns weather which is either stormy or clear" do
-
+    expect([:stormy, :clear].include?(subject.check_weather)).to eq true
   end
 
-  
 end
