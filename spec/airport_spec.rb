d56f812d3c5e671a9efdd45d3a87ba72fc207b22
Ruaridhi O'Scanlain <ruaridhioscanlain@MacBook-Pro-7.local>
2018-09-09 11:41:51 +0100

    responds to take_off method

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -6,13 +6,19 @@ describe Airport do
     it 'respones to land_plane' do
       expect(subject).to respond_to(:land_plane)
     end
-    
+
     it 'adds plane to airport' do
       plane = Plane.new
       subject.land_plane(plane)
       # expect (subject.land_plane(plane)).to eq([plane])
       expect(subject.planes).to include(plane)
     end
-
   end
+
+  describe '#take_off' do 
+    it 'respones to take_off' do
+      expect(subject).to respond_to(:take_off)
+    end
+  end
+
 end
