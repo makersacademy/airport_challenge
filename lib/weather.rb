c946432795b146c6a9744be36fa9a3f5856ddc21
Cameron Whitehead <camjw119@gmail.com>
2018-09-09 17:22:09 +0100

    Wrote first weather class, which is really just a random number generator

---
+++ lib/weather.rb
@@ -0,0 +1,13 @@
+class Weather
+
+  attr_reader :stormy_probability
+
+  def initialize(stormy_probability=0.5)
+    @stormy_probability =  stormy_probability
+  end
+
+  def check_weather
+    rand > stormy_probability ? :stormy : :clear
+  end
+
+end
