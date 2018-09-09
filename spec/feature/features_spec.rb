f194ad9cb061ebc436fc57bc830329ef0458c050
Marianne Rachid <marianne.rachid@gmail.com>
2018-09-09 16:05:07 +0100

    add feature tests for airport and plane capacity

--- spec/feature/features_spec.rb
+++ spec/feature/features_spec.rb
@@ -29,4 +29,50 @@ describe 'Full airport integration' do
       expect { airport.land(plane) }.to raise_error('No landing, weather is stormy!')
     end
   end
+
+  context 'Testing airport capacity' do
+    it 'lets several planes land and take off' do
+      planes = []
+      airport = Airport.new
+      5.times { planes << Plane.new }
+      allow_any_instance_of(Airport).to receive(:stormy?).and_return(false)
+      planes.each { |plane| airport.land(plane) }
+      planes.each { |plane| airport.take_off(plane) }
+    end
+
+    it 'exceeds airport capacity' do
+      planes = []
+      airport = Airport.new
+      20.times { planes << Plane.new }
+      allow_any_instance_of(Airport).to receive(:stormy?).and_return(false)
+      planes.each { |plane| airport.land(plane) }
+      expect { airport.land(Plane.new) }.to raise_error('Airport is full!')
+    end
+  end
+
+  context 'Testing plane capacity' do
+    it 'lets several passenger board and disembark' do
+      passengers = []
+      20.times { passengers << Passenger.new(true, false) }
+      airport = Airport.new
+      plane = Plane.new
+      allow_any_instance_of(Airport).to receive(:stormy?).and_return(false)
+      airport.land(plane)
+      passengers.each { |passenger| plane.board(passenger) }
+      airport.take_off(plane)
+      airport.land(plane)
+      passengers.each { |passenger| plane.disembark(passenger) }
+    end
+  end
+
+    it 'exceeds plane capacity' do
+      passengers = []
+      100.times { passengers << Passenger.new(true, false) }
+      plane = Plane.new
+      airport = Airport.new
+      allow_any_instance_of(Airport).to receive(:stormy?).and_return(false)
+      airport.land(plane)
+      passengers.each { |passenger| plane.board(passenger) }
+      expect { plane.board(Passenger.new) }.to raise_error('Plane is full!')
+    end
 end
