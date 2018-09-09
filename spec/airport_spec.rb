43109d02a5aeefd610351ba307aecd0477bc0895
Andres Faraone <andresfaraone@outlook.com>
2018-09-09 16:00:37 +0100

    #land raises error if @hangar size equal to @capacity

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -14,11 +14,30 @@ end
 
 describe Airport do
   describe '@hangar' do
-    it 'has @hangar as empty array on initialisation' do
+    it 'has @hangar as empty array on instantiation' do
       expect(subject.hangar).to eq([])
     end
   end
 
+  describe '@capacity' do
+    it 'Airport::DEFAULT_CAPACITY set to 4' do
+      expect(Airport::DEFAULT_CAPACITY).to eq(4)
+    end
+
+    it '@capacity set to Airport::DEFAULT_CAPACITY by default' do
+      expect(subject.capacity).to eq(Airport::DEFAULT_CAPACITY)
+    end
+
+    it 'value can be set on instantiation' do
+      # Set up
+      subject = Airport.new(6)
+
+      # Assert
+      expect(subject.capacity).to eq(6)
+    end
+  end
+  
+  # #land requires Weather Class as argument. MockNotStormy and MockStormy used in these examples
   describe '#land' do
     it ' raises error if #land used for plane already in hangar' do
       # Set up
@@ -29,30 +48,36 @@ describe Airport do
       expect { subject.land(mockplane1, MockNotStormy) }.to raise_error("Plane 1 has already landed!")
     end
 
-    context 'when #stormy is true' do
-      it 'raises storm error' do
-        # Set up
-        mockplane1 = double(name: "Plane 1")
-        
-        # Assert
-        expect { subject.land(mockplane1, MockStormy) }.to raise_error("It is too stormy to do that!")
-      end
+    it 'raises error if hangar full' do
+      # Set up
+      mockplane1 = double(name: "Plane 1")
+      subject.instance_eval { @hangar = [1, 2, 3, 4] } # Make @hangar size same as @capacity
+
+      # Assert
+      expect { subject.land(mockplane1, MockNotStormy) }.to raise_error("No room in hangar!")
     end
 
-    context 'when #stormy is false' do
-      it 'adds a plane object to @hangar and puts a confirmation message' do
-        # Set up
-        mockplane1, mockplane2 = double(name: "Plane 1"), double(name: "Plane 2")
-        
-        # Assert
-        expect { subject.land(mockplane1, MockNotStormy) }.to output("Plane 1 has landed.\nThe hangar contains Plane 1.\n").to_stdout
-        expect(subject.hangar).to eq([mockplane1])
-        expect { subject.land(mockplane2, MockNotStormy) }.to output("Plane 2 has landed.\nThe hangar contains Plane 1, Plane 2.\n").to_stdout
-        expect(subject.hangar).to eq([mockplane1, mockplane2])
-      end
+    it 'raises storm error when #stormy is true' do
+      # Set up
+      mockplane1 = double(name: "Plane 1")
+      
+      # Assert
+      expect { subject.land(mockplane1, MockStormy) }.to raise_error("It is too stormy to do that!")
+    end
+
+    it 'adds a plane object to @hangar and puts a confirmation message' do
+      # Set up
+      mockplane1, mockplane2 = double(name: "Plane 1"), double(name: "Plane 2")
+      
+      # Assert
+      expect { subject.land(mockplane1, MockNotStormy) }.to output("Plane 1 has landed.\nThe hangar contains Plane 1.\n").to_stdout
+      expect(subject.hangar).to eq([mockplane1])
+      expect { subject.land(mockplane2, MockNotStormy) }.to output("Plane 2 has landed.\nThe hangar contains Plane 1, Plane 2.\n").to_stdout
+      expect(subject.hangar).to eq([mockplane1, mockplane2])
     end
   end
 
+  # #takeoff requires Weather Class as argument. MockNotStormy and MockStormy used in these examples
   describe '#takeoff' do
     it 'raises error if #takeoff uses plane not present in @hangar' do
       # Set up
@@ -62,45 +87,29 @@ describe Airport do
       expect { subject.takeoff(mockplane1, MockNotStormy) }.to raise_error("Plane 1 not in the hangar!")  
     end
 
-    context 'when #stormy is true' do
-      it 'raises storm error' do
-        # Set up
-        mockplane1 = double(name: "Plane 1")
-      
-        # Assert
-        expect { subject.takeoff(mockplane1, MockStormy) }.to raise_error("It is too stormy to do that!")
-      end
+    it 'raises storm error when #stormy is true' do
+      # Set up
+      mockplane1 = double(name: "Plane 1")
+      subject.instance_eval { @hangar = [mockplane1] }
+        
+      # Assert
+      expect { subject.takeoff(mockplane1, MockStormy) }.to raise_error("It is too stormy to do that!")
     end
     
-    context 'when #stormy is false' do
-      it 'removes a plane from @hangar and puts a confirmation message' do
-        # Set up
-        mockplane1, mockplane2 = double(name: "Plane 1"), double(name: "Plane 2")
-        subject.instance_eval { @hangar = [mockplane1, mockplane2] }
-        
-        # Assert
-        expect { subject.takeoff(mockplane1, MockNotStormy) }.to output("Plane 1 has taken off.\nThe hangar contains Plane 2.\n").to_stdout
-        expect(subject.hangar).to eq([mockplane2])
-        expect { subject.takeoff(mockplane2) }.to output("Plane 2 has taken off.\nThere are no planes in the hangar.\n").to_stdout
-        expect(subject.hangar).to eq([])
-      end
+    it 'removes a plane from @hangar and puts a confirmation message' do
+      # Set up
+      mockplane1, mockplane2 = double(name: "Plane 1"), double(name: "Plane 2")
+      subject.instance_eval { @hangar = [mockplane1, mockplane2] }
+
+      # Assert
+      expect { subject.takeoff(mockplane1, MockNotStormy) }.to output("Plane 1 has taken off.\nThe hangar contains Plane 2.\n").to_stdout
+      expect(subject.hangar).to eq([mockplane2])
+      expect { subject.takeoff(mockplane2) }.to output("Plane 2 has taken off.\nThere are no planes in the hangar.\n").to_stdout
+      expect(subject.hangar).to eq([])
     end
   end
 
-  # Test for private methods
-=begin
-  describe '#weather_check' do
-    it { is_expected.to respond_to(:weather_check).with(1).argument }
-
-    it 'raises error message if stormy is true' do
-      expect { subject.weather_check(true) }.to raise_error("It is too stormy to do that!")
-    end
-
-    it 'passes if stormy is false' do
-      expect { subject.weather_check(false) }.not_to raise_error
-    end
-  end
-=end
+  # Tests for private methods
 
   describe '#display_hangar (private)' do
     it 'displays @hangar elements in user-friendly format' do
