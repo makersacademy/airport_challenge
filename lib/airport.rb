9f034242440c52d678a5800373c528d03fed9170
Andres Faraone <andresfaraone@outlook.com>
2018-09-09 16:01:03 +0100

    Passed check for capacity in #land

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,18 +1,23 @@
 class Airport
-  attr_reader :hangar
-  def initialize
+  DEFAULT_CAPACITY = 4
+
+  attr_reader :hangar, :capacity
+
+  def initialize(capacity = Airport::DEFAULT_CAPACITY)
+    @capacity = capacity
     @hangar = []
   end
 
   # Find a way to refactor stormy error in a way that can be tested in rspec
   def land(plane, weather = Weather)
     raise "#{plane.name} has already landed!" if @hangar.include?(plane)
+    raise "No room in hangar!" if @hangar.size == @capacity
     raise "It is too stormy to do that!" if weather.new.stormy
     @hangar.push(plane)
     puts confirmation("landed", plane), display_hangar
   end
 
-  def takeoff(plane , weather = Weather)
+  def takeoff(plane, weather = Weather)
     raise "#{plane.name} not in the hangar!" unless @hangar.include?(plane)
     raise "It is too stormy to do that!" if weather.new.stormy
     @hangar.delete(plane) 
@@ -23,13 +28,6 @@ class Airport
   def confirmation(action, plane)
     "#{plane.name} has #{action}." 
   end
-
-=begin
-  def weather_check
-    @weather = Weather.new.stormy
-    raise "It is too stormy to do that!" if @weather
-  end
-=end
 
   def display_hangar
     return "There are no planes in the hangar." if @hangar.empty?
