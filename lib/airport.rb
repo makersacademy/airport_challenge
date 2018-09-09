6efd5021e8a321b8c93f6feb3565a725db4728d8
Darcie <darciewalsh@hotmail.com>
2018-09-09 16:27:52 +0100

    Check if airport is empty

--- lib/airport.rb
+++ lib/airport.rb
@@ -5,6 +5,7 @@ class Airport
 
   attr_reader :grounded_planes, :weather
   # attr_accessor :grounded_planes, :weather
+  DEFAULT_CAPACITY = 20
 
   def initialize
     @grounded_planes = []
@@ -12,16 +13,18 @@ class Airport
   end
 
   def land(plane)
-    fail "Unable to land: bad weather" if is_stormy? == true
-    fail "Unable to land: airport full" if full? == true
+    fail "Unable to land: bad weather" if is_stormy?
+    fail "Unable to land: airport full" if full?
     @grounded_planes << plane
+    #p @grounded_planes
   end
 
   def takeoff(plane)
-    fail "Unable to takeoff: bad weather" if is_stormy? == true
+    fail "Unable to takeoff: bad weather" if is_stormy?
+    fail "Unable to takeoff: no planes in airport" if empty?
     @grounded_planes.shift
     p "Plane has taken off"
-    #p @grounded_planes should = []
+    #p @grounded_planes
   end
 
   def is_stormy?
@@ -29,6 +32,11 @@ class Airport
   end
 
   def full?
-    true if @grounded_planes.count >= 20
+    true if @grounded_planes.count >= DEFAULT_CAPACITY
   end
+
+  def empty?
+    true if @grounded_planes.empty?
+  end
+
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -11,63 +11,52 @@ describe Airport do
   describe '#land(plane)' do
     it 'allows a plane to land in normal weather' do
       expect(airport).to respond_to(:land).with(1).argument
-      allow(airport).to receive(:is_stormy?).and_return false #stubbing
+      #allow(airport).to receive(:is_stormy?) {false} #stubbing
     end
-  end
 
-  context 'responds to #full?' do
     it 'can check if airport is full' do
       expect(airport).to respond_to(:full?)
     end
-  end
 
-  context 'cannot land if #full?' do
     it 'it raises an error if #full?' do
-      allow(airport).to receive(:is_stormy?).and_return false #stubbing
-      20.times { airport.land(plane) }
+      allow(airport).to receive(:is_stormy?) {false} #stubbing
+      Airport::DEFAULT_CAPACITY.times { airport.land(plane) }
       expect { airport.land(plane) }.to raise_error "Unable to land: airport full"
     end
-  end
 
-  context 'cannot land if #stormy?'do
     it 'does not let the plane land in stormy weather' do
-      allow(airport).to receive(:is_stormy?).and_return true #stubbing
+      allow(airport).to receive(:is_stormy?) {true} #stubbing
       expect { airport.land(plane) }.to raise_error "Unable to land: bad weather"
     end
-  end
 
-  describe '#grounded_planes list' do
-    it 'stores landed planes in a list' do
-      expect(airport.grounded_planes).to be_a(Array)
-    end
-  end
+    # it 'stores landed planes in a list' do
+    #   expect(airport.grounded_planes).to be_a(Array)
+    # end
 
-  describe 'stores plane in #grounded_planes' do
     it 'registers that a landed plane has been stored' do
       airport.land(plane)
       expect(airport.grounded_planes).to eq([plane])
     end
   end
-
  #-----------------Takeoff-----------------#
-
   describe '#takeoff(plane)' do
     it 'allows the plane to takeoff in normal weather' do
       expect(airport).to respond_to(:takeoff).with(1).argument
-      allow(airport).to receive(:is_stormy?).and_return false #stubbing
+      allow(airport).to receive(:is_stormy?) {false} #stubbing
     end
-  end
 
-  describe 'plane is no longer in #grounded_planes' do
-    it 'confirms that the plane is no longer at the airport' do
-      airport.takeoff(plane)
-      expect(airport.grounded_planes).to eq([]), "Plane has taken off"
+    it 'only allows planes already in airport to takeoff' do
+      allow(airport).to receive(:is_stormy?) {false}
+      expect { airport.takeoff(plane) }.to raise_error "Unable to takeoff: no planes in airport"
     end
-  end
 
-  describe 'weather #is_stormy? so plane cannot takeoff' do
+    # it 'confirms that the plane is no longer at the airport' do
+    #   airport.takeoff(plane)
+    #   expect(airport.grounded_planes).to eq([]), "Plane has taken off"
+    # end
+
     it 'does not let the plane takeoff in stormy weather' do
-      allow(airport).to receive(:is_stormy?).and_return true #stubbing
+      allow(airport).to receive(:is_stormy?) {true} #stubbing
       expect { airport.takeoff(plane) }.to raise_error "Unable to takeoff: bad weather"
     end
   end
