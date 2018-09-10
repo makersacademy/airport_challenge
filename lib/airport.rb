9fca16e6953ee1a9a0e5f6c0712a0cabb6450cec
Ibra <ibra@MacBook-Pro-6.local>
2018-09-10 10:36:37 +0100

    refactoring.

--- lib/airport.rb
+++ lib/airport.rb
@@ -5,7 +5,7 @@ class Airport
     #@weather is writeable because the airport's weather can change
     attr_accessor :weather
     DEFAULT_CAPACITY = 100
-    def initialize(weather: :clear, capacity: DEFAULT_CAPACITY)
+    def initialize(capacity: DEFAULT_CAPACITY)
         @weather = Weather.condition
         @planes = []
         @capacity = capacity
@@ -13,25 +13,25 @@ class Airport
     def take_off(plane)
         raise "Error, plane #{plane.id} is not at this airport" if !airport_contains_plane?(plane)
         raise "Error, too stormy to take off" if stormy_weather?
-        @planes.delete(plane)
+        planes.delete(plane)
         "Plane #{plane.id} has taken off!"                    
     end
     def land_plane(plane)
         raise "Error, plane #{plane.id} is already at this Airport" if airport_contains_plane?(plane)
         raise "Error, too stormy to land plane #{plane.id}" if stormy_weather?
         raise "Error, Airport is full" if full_airport?
-        @planes << plane
+        planes << plane
         "Plane #{plane.id} has landed!"
     end
 
     private
         def airport_contains_plane?(plane)
-            @planes.include?(plane)
+            planes.include?(plane)
         end
         def stormy_weather?
-            @weather == :stormy
+            weather == :stormy
         end
         def full_airport?
-            @planes.count >= @capacity
+            planes.count >= @capacity
         end
 end
