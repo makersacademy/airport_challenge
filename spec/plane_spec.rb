f4f55de4b4e65bafc03ac6e970ad373615687e56
Oliver Baum <oliver@baums.org.uk>
2018-09-10 08:54:28 +0100

    Added before(:each) block in spec and used instance variables to apply my doubling throughout code

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -1,12 +1,15 @@
 require 'plane'
 
 describe Plane do
-  storm = double('weather')
-  no_storm = double('weather')
-  allow(storm).to receive(:is_stormy?) { true }
-  allow(no_storm).to receive(:is_stormy?) { false }
-  plane = Plane.new
-  airport = Airport.new(no_storm)
+  before(:each) do
+    @storm = double('weather')
+    @no_storm = double('weather')
+    allow(@storm).to receive(:is_stormy?) { true }
+    allow(@no_storm).to receive(:is_stormy?) { false }
+    @plane = Plane.new
+    @airport = Airport.new(@no_storm)
+    puts "Before:each made it here"
+  end
 
   it {should respond_to(:land)}
   context '#land' do
@@ -35,10 +38,10 @@ describe Plane do
       expect{plane.take_off(airport2)}.to raise_error(
         "The plane can only take off from the airport where it is grounded!")
     end
-    it "should raise an error if the plane tries to take off in a storm" do
-      airport = Airport.new(storm)
-      plane.land(airport)
-      expect{plane.take_off(airport)}.to raise_error("Can't take off in stormy conditions!")
+    it "should raise an error if the plane tries to take off in a storm", focus:true do
+      airport = Airport.new(@storm)
+      @plane.land(airport)
+      expect{@plane.take_off(airport)}.to raise_error("Can't take off in stormy conditions!")
     end
   end
 end
