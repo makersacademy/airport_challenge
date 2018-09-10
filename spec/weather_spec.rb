2df8db4bda17e154616e9dabfa2f3d1acc1d2159
Darcie <darciewalsh@hotmail.com>
2018-09-10 19:52:54 +0100

    Add test to check if stormy? rand  generator works

--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -1,5 +1,10 @@
 require 'weather'
 
 describe Weather do
-  it { is_expected.to respond_to :stormy? }
+  let(:weather) { Weather.new }
+
+  it 'is stormy if rand(1..10) is > 8' do
+    allow(weather).to receive(:rand).and_return(9)
+    expect(weather.stormy?).to be true
+  end
 end
