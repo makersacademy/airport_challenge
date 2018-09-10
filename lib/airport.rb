3eb701f041b52c1dea4b2ab8d71914403f3fce7c
AlexDresco <alexandra.d1510@gmail.com>
2018-09-10 11:04:22 +0100

    Corrected errors, still need to do two users stories

--- lib/airport.rb
+++ lib/airport.rb
@@ -2,21 +2,20 @@ class Airport
   attr_reader :storage
 
   def initialize
-    @weather = "sunny" #I set it up as always stormy as if condition failed my rspec.
     @storage = []
   end
 
+  def stormy?
+    true
+  end
+
   def land(plane)
-    plane = Plane.new
     @storage << plane
   end
 
   def take_off(plane)
-    if @weather == "stormy"
-      raise RuntimeError
-    else
-      @storage.delete_at(-1)
-    end
+    fail RuntimeError, 'Too stormy' if stormy? == true
+    @storage.delete(plane)
+    @storage
   end
-
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -7,8 +7,8 @@ describe Airport do
   it { is_expected.to respond_to(:take_off).with(1).argument }
 
   it "raises an error when stormy" do
-    @weather = "stormy"
-    expect { subject.take_off(Plane.new) }.to raise_error RuntimeError
+    allow(subject).to receive(:stormy?) { true }
+    expect { subject.take_off(Plane.new) }.to raise_error 'Too stormy'
   end
 
   it "creates a variable for storage" do
@@ -22,7 +22,7 @@ describe Airport do
 
   it "should remove the plane from storage when take off" do
     second_plane = Plane.new
-    storage = [second_plane]
+    allow(subject).to receive(:stormy?) { false }
     expect(subject.take_off(second_plane)).to eq []
   end
 end
