45dd460c99102bf79d2211cb25b7fa6d2379604b
AnastasiiaBlaha <anastasiia.blaha@gmail.com>
2018-09-10 02:19:41 +0100

    planes take off STORY 2

--- spec/features/stories_spec.rb
+++ spec/features/stories_spec.rb
@@ -8,4 +8,14 @@ it 'planes land and instucted to land at an airport' do
   plane = Plane.new
   expect { airport.land(plane) }.not_to raise_error
  end
+
+ # As an air traffic controller
+ # So I can get passengers on the way to their destination
+ # I want to instruct a plane to take off from an airport and confirm that it is no longer in the airport
+
+ it 'instructs a plane to take off and confirm that it is no longer in the airport' do
+   airport = Airport.new
+   plane = Plane.new
+   expect { airport.takeoff(plane) }.not_to raise_error
+ end
 end
