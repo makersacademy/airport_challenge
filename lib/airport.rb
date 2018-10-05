fc4d9f5d3cd2ddea71bc67402648c01fe798a6dc
Ahmed <ahmed.marzook@cgi.com>
2018-10-05 11:11:07 +0000

    Checks if plane has landed in airport

--- lib/airport.rb
+++ lib/airport.rb
@@ -2,6 +2,6 @@ require_relative 'plane.rb'
 
 class Airport
   def land(plane)
-    
+    plane
   end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -4,4 +4,8 @@ describe Airport do
   it 'responds to land' do
     expect(subject).to  respond_to(:land).with(1).argument
   end
+  it 'check if plane is at airport' do
+      plane = Plane.new
+      expect(subject.land(plane)).to eq(plane)
+  end
 end
