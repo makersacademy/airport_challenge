0705fbdc851179d13555e5ca8ab323e0eb1d4395
melissasedgwick <melissa.sedgwick@yahoo.co.uk>
2018-10-05 16:40:15 +0100

    Refactored code

--- lib/airport.rb
+++ lib/airport.rb
@@ -10,19 +10,17 @@ class Airport
   end
 
   def land(plane)
-    raise "Unable to land due to stormy weather" if $weather == "stormy"
-    raise "Unable to land; airport is full" if @planes_in_airport.length >= @capacity
-    raise "Unable to land; plane is already in airport" unless $planes_outside_airports.include?(plane.object_id)
-    @planes_in_airport << plane
-    $planes_outside_airports.delete(plane.object_id)
+    raise "Unable to land due to stormy weather" if bad_weather?
+    raise "Unable to land; airport is full" if full?
+    raise "Unable to land; plane is already in airport" unless plane_in_airport?(plane)
+    land_plane_status(plane)
     return "Plane has landed"
   end
 
   def takeoff(plane)
     raise "Unable to takeoff due to stormy weather" if $weather == "stormy"
     raise "Unable to takeoff; plane not in airport" unless @planes_in_airport.include?(plane)
-    @planes_in_airport.delete(plane)
-    $planes_outside_airports << plane.object_id
+    takeoff_plane_status(plane)
     return "Plane has taken off"
   end
 
@@ -33,6 +31,28 @@ class Airport
     else
       $weather = "stormy"
     end
+  end
+
+  def full?
+    @planes_in_airport.length >= @capacity
+  end
+
+  def bad_weather?
+    $weather == "stormy"
+  end
+
+  def plane_in_airport?(plane)
+    $planes_outside_airports.include?(plane.object_id)
+  end
+
+  def land_plane_status(plane)
+    @planes_in_airport << plane
+    $planes_outside_airports.delete(plane.object_id)
+  end
+
+  def takeoff_plane_status(plane)
+    @planes_in_airport.delete(plane)
+    $planes_outside_airports << plane.object_id
   end
 
 end
