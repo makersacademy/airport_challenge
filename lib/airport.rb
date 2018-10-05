ce36bcd637a477a220d08a9f886e58949dca6cfc
Ahmed <ahmed.marzook@cgi.com>
2018-10-05 13:51:19 +0000

    Check if plane is in airport

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,9 +1,21 @@
 require_relative 'plane.rb'
 
 class Airport
+    
+   def initialize
+    @planes = []
+    @parked = false
+   end
+    
   def land(plane)
-    plane
+    @parked = true
+    @planes << plane
   end
+  
   def takeoff
+     @parked = false
+     @planes.pop
   end
+  
+  attr_reader :parked
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,14 +1,23 @@
 require 'airport'
 
 describe Airport do
+    
   it 'responds to land' do
     expect(subject).to  respond_to(:land).with(1).argument
   end
+  
   it 'check if plane is at airport' do
       plane = Plane.new
-      expect(subject.land(plane)).to eq(plane)
+      expect(subject.land(plane)).to eq([plane])
   end
+  
   it 'responds to takeoff' do
       expect(subject).to respond_to(:takeoff)
   end
+  
+  it 'check is plane is at airport' do
+   subject.land(Plane.new)
+  expect(subject.parked).to eq true
+  end
+  
 end
