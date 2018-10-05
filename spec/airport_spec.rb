532f69a1dc88f10207d030bcf61edd14c2722566
Benjamin Kearns <rapiidz1@gmail.com>
2018-10-05 13:05:08 +0000

    refactored plane method and added test

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -4,4 +4,8 @@ describe Airport do
   it "checks land method" do 
     expect(subject).to respond_to(:land).with(1).argument
   end
+  it "lands a plane" do 
+    plane = Plane.new
+    expect(subject.land(plane)).to eq plane
+    end
 end
