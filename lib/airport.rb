139ce9e13ac8fe01d7f8596ca70edda36c4773e0
hurc87 <zoe1987@btinternet.com>
2018-10-05 15:25:25 +0100

    added fail message when airport is empty so new planes cannot take off

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,14 +1,15 @@
 require_relative "plane"
 
 class Airport
+  attr_reader :plane
+
   def release_plane
-    Plane.new
+    fail "No planes at the airport to take off" unless @plane
+    @plane
   end
 
   def land(plane)
     @plane = plane
   end
-
-  attr_reader :plane
 
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -3,10 +3,10 @@ require 'airport'
 describe Airport do
   it { is_expected.to respond_to :release_plane }
 
-  it "releases a plane that has landed" do
-    plane = subject.release_plane
-    expect(plane).to be_landed
-  end
+  #it "releases a plane that has landed" do
+  #  plane = subject.release_plane
+  #  expect(plane).to be_landed
+  #end
 
   it { is_expected.to respond_to(:land).with(1).argument }
 
@@ -21,5 +21,19 @@ describe Airport do
     plane = Plane.new
     subject.land(plane)
     expect(subject.plane).to eq plane
+  end
+
+  describe "#release_plane" do
+    it "releases a plane" do
+      plane = Plane.new
+      subject.land(plane)
+      expect(subject.release_plane).to eq plane
+    end
+  end
+
+  describe "#release_plane" do
+    it "raises an error message when there are no planes at the airport which can take off" do
+      expect { subject.release_plane }.to raise_error "No planes at the airport to take off"
+    end
   end
 end
