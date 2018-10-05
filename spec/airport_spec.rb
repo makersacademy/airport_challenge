d82fc4eef349fce7b4c75c14aaef322228b29ddf
jebax <joel.eliot.baxter@gmail.com>
2018-10-05 21:29:03 +0100

    wrote second test

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,7 +2,17 @@ require 'airport'
 
 describe Airport do
   let(:plane) { double(:plane) }
-  it 'should be able to land a plane' do
-    expect { subject.land(plane) }.not_to raise_error
+
+  describe "#land" do
+    it 'should be able to land a plane' do
+      expect { subject.land(plane) }.not_to raise_error
+    end
+  end
+
+  describe "#hangar_report" do
+    it 'should be able to display a landed plane' do
+      subject.land(plane)
+      expect(subject.hangar_report).to eq "Planes currently in the airport: #{[plane]}"
+    end
   end
 end
