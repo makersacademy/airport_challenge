4e11494c95601c131ac8ade48e6f1b1733b01589
Ahmed <ahmed.marzook@cgi.com>
2018-10-05 14:32:25 +0000

    Weather feature in progress

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,21 +1,33 @@
 require_relative 'plane.rb'
 
 class Airport
-    
-   def initialize
+  
+  def initialize(weather = 1)
     @planes = []
     @parked = false
-   end
-    
+    @weather = weather
+  end
+  
   def land(plane)
     @parked = true
     @planes << plane
   end
   
   def takeoff
-     @parked = false
-     @planes.pop
+    fail 'Weather is Stormy' if stormy?
+    @parked = false
+    @planes.pop
   end
   
   attr_reader :parked
+  
+  def weather
+     @weather = rand(1..10)
+  end
+  
+  private
+  
+  def stormy?
+      @weather > 5 ? false : true
+  end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,23 +1,35 @@
 require 'airport'
 
 describe Airport do
-    
+  
   it 'responds to land' do
     expect(subject).to  respond_to(:land).with(1).argument
   end
   
   it 'check if plane is at airport' do
-      plane = Plane.new
-      expect(subject.land(plane)).to eq([plane])
+    plane = Plane.new
+    expect(subject.land(plane)).to eq([plane])
   end
   
   it 'responds to takeoff' do
-      expect(subject).to respond_to(:takeoff)
+    expect(subject).to respond_to(:takeoff)
   end
   
-  it 'check is plane is at airport' do
-   subject.land(Plane.new)
-  expect(subject.parked).to eq true
+  it 'check is plane is in airport' do
+    subject.land(Plane.new)
+    expect(subject.parked).to eq true
   end
   
+  it 'check if plane has left airport' do
+    subject.land(Plane.new)
+    subject.takeoff
+    expect(subject.parked).to eq false
+  end
+  
+  describe '#takeoff' do
+    it 'raises an error when weather is stormy' do
+      subject.land(Plane.new)
+      expect { subject.takeoff }.to raise_error 'Weather is Stormy'
+    end
+  end
 end
