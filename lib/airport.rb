b06f9f30232b6e2a9f1a99d78b1d61526afa9c77
Irina Baldwin <irinabaldwin@Irinas-MacBook-Air.local>
2018-10-05 16:55:50 +0100

    Prevent landing when airport is full; airport has a default capacity

--- lib/airport.rb
+++ lib/airport.rb
@@ -2,8 +2,13 @@ require 'pry'
 
 class Airport
 
-  def initialize
+  attr_reader :capacity
+
+  DEFAULT_CAPACITY = 10
+
+  def initialize(capacity = DEFAULT_CAPACITY)
     @planes = []
+    @capacity = capacity
   end
 
   def planes
@@ -11,16 +16,20 @@ class Airport
   end
 
   def receive(plane)
-    raise "Airport is closed due to weather conditions" if Weather.stormy? == true
+    raise "Airport is closed" if Weather.stormy? == true || full?
     @planes << plane
   end
 
   def sendoff(plane)
     # binding.pry
-    raise "Airport is closed due to weather conditions" if Weather.stormy? == true
+    raise "Airport is closed" if Weather.stormy? == true
     # binding.pry
     @planes.delete(plane)
     # binding.pry
+  end
+
+  def full?
+    true if @planes.count >= @capacity
   end
 
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -9,14 +9,28 @@ describe Airport do
     allow(Weather).to receive(:stormy?) { false }
     plane.land(airport)
     allow(Weather).to receive(:stormy?) { true }
-    expect { plane.takeoff }.to raise_error "Airport is closed due to weather conditions"
+    expect { plane.takeoff }.to raise_error "Airport is closed"
   end
 
   it "prevents landing when weather is stormy" do
     plane = Plane.new
     airport = Airport.new
     allow(Weather).to receive(:stormy?) { true }
-    expect { plane.land(airport) }.to raise_error "Airport is closed due to weather conditions"
+    expect { plane.land(airport) }.to raise_error "Airport is closed"
+  end
+
+  it "prevents landing if airport is full" do
+    airport = Airport.new
+    allow(Weather).to receive(:stormy?) { false }
+    Airport::DEFAULT_CAPACITY.times { airport.receive(Plane.new) }
+    allow(Weather).to receive(:stormy?) { false }
+    plane = Plane.new
+    expect { plane.land(airport) }.to raise_error "Airport is closed"
+  end
+
+  it "has a default capacity" do
+    airport = Airport.new
+    expect(airport.capacity).to eq(Airport::DEFAULT_CAPACITY)
   end
 
 end
