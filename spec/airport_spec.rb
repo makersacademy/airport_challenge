94189cfaeb5900ce66e3503e59e39f8f45ff5c46
Aimée Craig <aimeecraig1@gmail.com>
2018-10-05 14:15:45 +0100

    code updated to better meet rubocop standards

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -17,11 +17,11 @@ describe Airport do
 
   it 'returns an error in take off when weather is stormy' do
     allow(subject).to receive(:weather) { "stormy" }
-    expect{subject.take_off(Plane.new)}.to raise_error ("Cannot take off, weather is stormy.")
+    expect{ subject.take_off(Plane.new) }.to raise_error "Cannot take off, weather is stormy."
   end
 
   it 'returns an error in land when weather is stormy' do
     allow(subject).to receive(:weather) { "stormy" }
-    expect{subject.land(Plane.new)}.to raise_error ("Cannot land, weather is stormy.")
+    expect{ subject.land(Plane.new) }.to raise_error "Cannot land, weather is stormy."
   end
 end
