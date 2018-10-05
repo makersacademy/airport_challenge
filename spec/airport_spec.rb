bbb2eacc428bb36fc2626dacece478d730107c03
Aimée Craig <aimeecraig1@gmail.com>
2018-10-05 14:10:32 +0100

    fifth test added

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -16,6 +16,12 @@ describe Airport do
   end
 
   it 'returns an error in take off when weather is stormy' do
+    allow(subject).to receive(:weather) { "stormy" }
     expect{subject.take_off(Plane.new)}.to raise_error ("Cannot take off, weather is stormy.")
+  end
+
+  it 'returns an error in land when weather is stormy' do
+    allow(subject).to receive(:weather) { "stormy" }
+    expect{subject.land(Plane.new)}.to raise_error ("Cannot land, weather is stormy.")
   end
 end
