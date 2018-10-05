fff87427cd59072aee57aafacc1ae68c8879cbc2
PreetiUdas <preetiudas@yahoo.com>
2018-10-05 15:10:54 +0100

    border cases done

--- lib/plane.rb
+++ lib/plane.rb
@@ -5,23 +5,25 @@ class Plane
   def initialize
     @currentairport = ""
     @destination=""
-
+    @state="landed"
   end
 
   def land(airport, weather)
+    raise "Already landed; Cant do it twice!" unless @state == "flying"
     raise "Bad weather; no landing" unless weather == "sunny"
     airport.addtraffic(self)
     @currentairport = airport
     @destination = nil
-    true
+    @state="landed"
   end
 
   def takeoff(airport, weather)
+    raise "Already flying; Cant takeoff again!" unless @state == "landed"
     raise "Bad weather; no takeoff" unless weather == "sunny"
     airport.reducetraffic(self)
     @destination = airport
     @currentairport = nil
-    true
+    @state="flying"
   end
 
 end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -6,9 +6,11 @@ describe Plane do
 
   it "lands at the specified airport" do
     allow(airport).to receive(:addtraffic).and_return(true)
+    allow(airport).to receive(:reducetraffic).and_return(true)
+    subject.takeoff(airport,"sunny")
     weather = ["sunny","stormy"].sample
     if weather == "sunny"
-      expect(subject.land(airport,weather)).to eq(true)
+      expect(subject.land(airport,weather)).to eq("landed")
     elsif weather == "stormy"
       expect{subject.land(airport,weather)}.to raise_error("Bad weather; no landing")
     end
@@ -18,19 +20,23 @@ describe Plane do
     allow(airport).to receive(:reducetraffic).and_return(true)
     weather = ["sunny","stormy"].sample
     if weather == "sunny"
-      expect(subject.takeoff(airport,weather)).to eq(true)
+      expect(subject.takeoff(airport,weather)).to eq("flying")
     elsif weather == "stormy"
       expect{subject.takeoff(airport,weather)}.to raise_error("Bad weather; no takeoff")
     end
   end
 
   it "cannot land if it is not flying" do
+    allow(airport).to receive(:addtraffic).and_return("landed")
+    allow(airport).to receive(:reducetraffic).and_return(true)
+    subject.takeoff(airport,"sunny")
     subject.land(airport,"sunny")
-    expect{subject.land(airport,"sunny")}.to raise_error "Already landed; Cant do it twice!"
+    expect{subject.land(airport,"sunny")}.to raise_error("Already landed; Cant do it twice!")
   end
 
   it "cannot take off if it is already flying" do
+    allow(airport).to receive(:reducetraffic).and_return("flying")
     subject.takeoff(airport,"sunny")
-    expect{subject.takeoff(airport,"sunny")}.to raise_error "Already flying; Cant takeoff again!"
+    expect{subject.takeoff(airport,"sunny")}.to raise_error("Already flying; Cant takeoff again!")
   end
 end
