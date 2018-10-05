af1b86245c724eb1ebce0cb048e3d214b561ccf8
Aimée Craig <aimeecraig1@gmail.com>
2018-10-05 13:54:58 +0100

    second test added

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -4,4 +4,8 @@ describe Airport do
   it 'lands a plane' do
     expect(subject.land(Plane.new)).to eq true
   end
+
+  it 'allows a plane to take off' do
+    expect(subject.take_off(Plane.new)).to eq true
+  end
 end
