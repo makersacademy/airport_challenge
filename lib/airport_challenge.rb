477f7c1ebb5f6afb697ecf99ac4a03fd2f5feb76
Aimée Craig <aimeecraig1@gmail.com>
2018-10-05 10:12:25 +0100

    unit test added to spec/airport_spec.rb and test passed in lib/airport_challenge.rb

--- lib/airport_challenge.rb
+++ lib/airport_challenge.rb
@@ -2,6 +2,14 @@ class Airport
   def land(*)
     true
   end
+
+  def take_off(*)
+    true
+  end
+
+  def check
+    return []
+  end
 end
 
 class Plane
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -5,4 +5,10 @@ describe Airport do
     plane = Plane.new
     expect(subject.land(plane)).to eq true
   end
+
+  it 'allows a plane to take off and then returns an empty array' do
+    plane = Plane.new
+    expect(subject.take_off(plane)).to eq true
+    expect(subject.check).to eq []
+  end
 end
