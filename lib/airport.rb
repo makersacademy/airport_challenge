27a5b14396b3b079173472c61f117910cf98a1e6
jebax <joel.eliot.baxter@gmail.com>
2018-10-05 21:48:30 +0100

    added guard case for instructing a plane to take off that does not exist

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,13 +1,26 @@
 class Airport
+  attr_reader :landed_planes
+
   def initialize
     @landed_planes = []
   end
 
   def land(plane)
     @landed_planes << plane
+    "#{plane} landed successfully."
+  end
+
+  def take_off(plane)
+    raise "Specified plane is not in the airport!" unless @landed_planes.include? plane
+    @landed_planes.delete(plane)
+    "#{plane} taken off successfully."
   end
 
   def hangar_report
     "Planes currently in the airport: #{@landed_planes}"
   end
+
+  private
+
+  attr_reader :landed_planes
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,13 +2,33 @@ require 'airport'
 
 describe Airport do
   let(:plane) { double(:plane) }
+  let(:plane_2) { double(:plane) }
 
   it 'should be able to land a plane' do
-    expect { subject.land(plane) }.not_to raise_error
+    expect(subject.land(plane)).to eq "#{plane} landed successfully."
   end
 
   it 'should be able to display a landed plane' do
     subject.land(plane)
     expect(subject.hangar_report).to eq "Planes currently in the airport: #{[plane]}"
+  end
+
+  describe "#take_off" do
+    it 'should be able to instruct a plane to take off' do
+      subject.land(plane)
+      expect(subject.take_off(plane)).to eq "#{plane} taken off successfully."
+    end
+
+    it 'should raise an error if the specified plane is not in the airport' do
+      subject.land(plane)
+      expect { subject.take_off(plane_2) }.to raise_error "Specified plane is not in the airport!"
+    end
+  end
+
+  it 'should be able to confirm that a taken off plane is no longer in the hangar' do
+    subject.land(plane)
+    subject.land(plane_2)
+    subject.take_off(plane)
+    expect(subject.hangar_report).to eq "Planes currently in the airport: #{[plane_2]}"
   end
 end
