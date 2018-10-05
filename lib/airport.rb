d287209a883993059ac3138348ebe18c595ace52
Benjamin Kearns <rapiidz1@gmail.com>
2018-10-05 15:19:24 +0000

    started adding weather

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,6 +1,24 @@
 require_relative 'plane'
 class Airport
+  def inialize(weather = 1)
+    @weather = weather 
+  end
+  
   def land(plane)
-        plane
+    plane
+  end
+  
+  def takeoff(plane)
+    weather 
+    fail 'Weather is too stormy to take off' unless @weather <= 4
+    return plane, here?
+  end
+  
+  def here?
+    false
+  end
+  
+  def weather 
+    @weather = rand(1..5)
   end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,11 +1,23 @@
 require 'airport'
 require 'plane'
 describe Airport do 
-  it "checks land method" do 
+  it "checks land method argument" do 
     expect(subject).to respond_to(:land).with(1).argument
   end
   it "lands a plane" do 
     plane = Plane.new
     expect(subject.land(plane)).to eq plane
-    end
+  end
+  it "checks takeoff method argument" do
+    expect(subject).to respond_to(:takeoff).with(1).argument
+  end
+  it "checks here? method" do 
+    expect(subject).to respond_to(:here?)
+  end
+#  it "don't let plane takeoff if weather stormy" do 
+#    airport = Airport.new
+#    plane = Plane.new  
+#    allow(airport).to_recieve().and_return(5)
+#    expect(airport.takeoff(plane)).to raise_error 'Weather is too stormy to take off'
+#  end
 end
