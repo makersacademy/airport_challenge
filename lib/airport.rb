040c13295745aa1737dfcaab6cb8637babd7a98e
jebax <joel.eliot.baxter@gmail.com>
2018-10-05 21:32:53 +0100

    allowed planes to land and be viewed after landing

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,6 +1,13 @@
 class Airport
+  def initialize
+    @landed_planes = []
+  end
 
   def land(plane)
+    @landed_planes << plane
   end
-  
+
+  def hangar_report
+    "Planes currently in the airport: #{@landed_planes}"
+  end
 end
