413c2b48d6809a5bc153efb4f71bb377ca1c163f
Aimée Craig <aimeecraig1@gmail.com>
2018-10-05 10:44:10 +0100

    lib/airport_challenge.rb and spec/airport_spec.rb amended to account for random weather

--- lib/airport_challenge.rb
+++ lib/airport_challenge.rb
@@ -1,10 +1,20 @@
 class Airport
-  def land(*)
-    true
+  attr_accessor :weather
+  attr_accessor :hanger
+
+  def initialize
+    @weather = ["sunny", "stormy"].sample
+    @hanger = []
+  end
+
+  def land(plane)
+    self.weather == "sunny"
+    @hanger << plane
   end
 
   def take_off(*)
-    true
+    self.weather == "sunny"
+    @hanger.delete(plane)
   end
 
   def check
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,9 +1,10 @@
 require 'airport_challenge'
 
 describe Airport do
-  it 'lands a plane' do
+  it 'lands a plane when the weather is sunny' do
     plane = Plane.new
-    expect(subject.land(plane)).to eq true
+    allow(subject).to receive(:initialize) { "sunny" }
+    expect(subject.hanger).to eq [plane]
   end
 
   it 'allows a plane to take off and then returns an empty array' do
@@ -14,7 +15,7 @@ describe Airport do
 
   it 'prevents a plane from taking off when the weather is stormy' do
     plane = Plane.new
-    weather = "stormy"
+    allow(subject).to receive(:initialize) { "stormy" }
     expect(subject.take_off(plane)).to eq false
   end
 end
