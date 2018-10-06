0f46aa1f7debd09caebb71cb9584c13383f9c012
muge <muge.hasilci@hotmail.com>
2018-10-06 19:49:56 +0100

    Plane is no longer at the airport && plane landed test added and passed

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,9 +1,16 @@
 
 
 class Airport
+  attr_reader :flights
+  def initialize
+    @flights = []
+  end
+
   def land(plane)
+    @flights << plane
   end
 
   def takeoff(plane)
+    @flights.delete(plane)
   end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,12 +1,23 @@
 
 require 'airport'
+require 'Plane'
 
 describe Airport do
   describe '#land' do
     it { is_expected.to respond_to(:land).with(1).argument }
+    airport = Airport.new
+    plane = Plane.new
+    it 'lands at an airport' do
+      expect(subject.land(plane)).to include(plane)
+    end
   end
 
   describe '#takeoff' do
     it { is_expected.to respond_to(:takeoff).with(1).argument }
+    airport = Airport.new
+    plane = Plane.new
+    it 'confirms that plane is no longer in the airport' do
+      expect(subject.flights).not_to include(plane)
+    end
   end
 end
