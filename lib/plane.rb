58f2f36a2dbf10f920f066d2151799647dc28e99
imtiyazzaman1 <imtiyaz.zaman@live.co.uk>
2018-10-06 19:22:29 +0100

    Added fly and land method to the plane so plane knows when its flying

--- lib/plane.rb
+++ lib/plane.rb
@@ -1,2 +1,23 @@
 class Plane
+  def initialize(flying = true)
+    @flying = flying
+  end
+
+  def flying?
+    @flying
+  end
+
+  def landed?
+    !@flying
+  end
+
+  def land
+    @flying = false
+  end
+
+  def fly
+    @flying = true
+  end
+
+
 end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -1,4 +1,14 @@
 require 'plane'
+
 describe Plane do
+  it "should know when it is flying" do
+    subject.fly
+    expect(subject.flying?).to eq true
+  end
+
+  it "should know when it is landed" do
+    subject.land
+    expect(subject.flying?).to eq false
+  end
 
 end
