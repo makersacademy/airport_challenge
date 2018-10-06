0a982362beb0af5b03da9575fac3c1a1c0cc2fda
chloeverity <chloeverity@hotmail.co.uk>
2018-10-07 00:11:22 +0100

    failing initial test, classes established

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,1 +1,5 @@
+require_relative 'plane'
+require_relative 'weather'
 
+class Airport
+end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,1 +1,13 @@
+require 'airport'
 
+describe Airport do
+
+  describe '#landing' do
+    it 'allows planes to land' do
+      airport = Airport.new
+      plane = Plane.new
+      weather = Weather.new
+      expect(airport.land(plane)).to eq plane
+    end
+  end
+end
