3899d66d1e640f45590575643aef0bd0ce52a0f8
RyanWolfen7 <wolfen7lada@gmail.com>
2018-10-06 23:51:21 +0100

    Fixed old rspec syntax

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -6,13 +6,13 @@ describe Airport do
   describe '#Landing procedures' do
     it 'adds plane to the at_airport array' do
       plane = Plane.new
-      subject.should_receive(:stormy?).and_return(false)
+      expect(subject).to receive(:stormy?).and_return(false)
       expect(subject.instruct_landing(plane)).to eq [plane]
     end
 
     it 'Should hold multiple planes' do
       p1, p2, p3, p4 = Plane.new
-      subject.should_receive(:stormy?).at_least(4).times.and_return(false)
+      expect(subject).to receive(:stormy?).at_least(4).times.and_return(false)
       subject.instruct_landing(p1)
       subject.instruct_landing(p2)
       subject.instruct_landing(p3)
@@ -21,7 +21,7 @@ describe Airport do
     end
 
     it 'prevents landing when stormy' do
-      subject.should_receive(:stormy?).and_return(true)
+      expect(subject).to receive(:stormy?).and_return(true)
       expect { subject.instruct_landing Plane.new }.to raise_error 'Can not land do to stormy weather'
     end
   end
@@ -29,32 +29,32 @@ describe Airport do
   describe '#Takeoff procedures' do
     it 'expects plane to takeoff and leave the airport' do
       plane = Plane.new
-      subject.should_receive(:stormy?).and_return(false)
+      expect(subject).to receive(:stormy?).and_return(false)
       expect(subject.instruct_takeoff(plane)).to eq plane
     end
 
     it 'expects airport to be empty' do
       plane = Plane.new
-      subject.should_receive(:stormy?).at_least(2).times.and_return(false)
+      expect(subject).to receive(:stormy?).at_least(2).times.and_return(false)
       subject.instruct_landing(plane)
       subject.instruct_takeoff(plane)
       expect(subject.empty?).to eq true
     end
 
     it 'expects plain can not take off due to weather' do
-      subject.should_receive(:stormy?).and_return(true)
+      expect(subject).to receive(:stormy?).and_return(true)
       expect { subject.instruct_takeoff Plane.new }.to raise_error 'Can not takeoff do to stormy weather'
     end
   end
 
   describe '#stormy? feature' do
     it 'expected to respond to true' do
-      subject.should_receive(:stormy?).and_return(true)
+      expect(subject).to receive(:stormy?).and_return(true)
       expect(subject.stormy?).to eq true
     end
 
     it 'expected to respond to false' do
-      subject.should_receive(:stormy?).and_return(false)
+      expect(subject).to receive(:stormy?).and_return(false)
       expect(subject.stormy?).to eq false
     end
 
