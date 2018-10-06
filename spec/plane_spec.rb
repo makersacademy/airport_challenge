01dea1f080e6eea84a2c50906cca2208643de780
Irina Baldwin <2007inb@gmail.com>
2018-10-06 20:33:47 +0100

    Dry out plane_spec

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -8,31 +8,24 @@ describe Plane do
     @airport = Airport.new
     @plane = Plane.new
     @good_weather = allow(Weather).to receive(:stormy?) { false }
+    @plane.land(@airport)
   end
 
   it 'can land at an airport' do
-    @good_weather
-    @plane.land(@airport)
     expect(@airport.planes[0]).to eq(@plane)
   end
 
   it 'can take off from an airport' do
-    @good_weather
-    @plane.land(@airport)
     @good_weather
     @plane.takeoff
     expect(@airport.planes).to be_empty
   end
 
   it 'cannot land if it\'s already landed' do
-    @good_weather
-    @plane.land(@airport)
     expect { @plane.land(@airport) }.to raise_error "This plane has already landed"
   end
 
   it 'cannot take off if it\'s already in the air' do
-    @good_weather
-    @plane.land(@airport)
     @good_weather
     @plane.takeoff
     expect { @plane.takeoff }.to raise_error "This plane is already in the air"
