361e512d8b43c2699cf09d157271baad322cf987
jebax <joel.eliot.baxter@gmail.com>
2018-10-06 16:46:21 +0100

    refactored take off and land checks into private methods

--- lib/plane.rb
+++ lib/plane.rb
@@ -6,17 +6,27 @@ class Plane
   end
 
   def land(airport)
-    raise "Airport is full!" if airport.landed_planes.count >= airport.capacity
-    raise "Plane already in the airport." if airport.landed_planes.include? self
-    raise "Stormy weather, cannot land." if airport.weather == "stormy"
+    landing_checks(airport)
     airport.landed_planes << self
     @location = "airport"
   end
 
   def take_off(airport)
-    raise "Plane not in the airport." unless airport.landed_planes.include? self
-    raise "Stormy weather, cannot take off." if airport.weather == "stormy"
+    take_off_checks(airport)
     airport.landed_planes.delete(self)
     @location = "sky"
+  end
+
+  private
+
+  def landing_checks(airport)
+    raise "Airport is full!" if airport.landed_planes.count >= airport.capacity
+    raise "Plane already in the airport." if airport.landed_planes.include? self
+    raise "Stormy weather, cannot land." if airport.weather == "stormy"
+  end
+
+  def take_off_checks(airport)
+    raise "Plane not in the airport." unless airport.landed_planes.include? self
+    raise "Stormy weather, cannot take off." if airport.weather == "stormy"
   end
 end
