b4c127c9839ba719dafd0ee79cbc67afe17376c1
hurc87 <zoe1987@btinternet.com>
2018-10-06 08:01:42 +0100

    set default and changable capacity

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,16 +1,34 @@
 require_relative "plane"
 
 class Airport
-  attr_reader :plane
+  DEFAULT_CAPACITY = 20
+  attr_reader :capacity
+
+  def initialize(capacity=DEFAULT_CAPACITY)
+    @capacity = capacity
+    @planes = []
+  end
 
   def release_plane
-    fail "No planes at the airport to take off" unless @plane
-    @plane
+    fail "No planes at the airport to take off" if empty?
+    planes.pop
   end
 
   def land(plane)
-    fail "Airport is full" if @plane
-    @plane = plane
+    fail "Airport is full" if full?
+    planes << plane
+  end
+
+  private
+
+  attr_reader :planes
+
+  def full?
+    planes.count >= capacity
+  end
+
+  def empty?
+    planes.empty?
   end
 
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -5,18 +5,16 @@ describe Airport do
 
   it { is_expected.to respond_to(:land).with(1).argument }
 
-  it { is_expected.to respond_to(:plane) }
+#  it "lands the plane" do
+#    plane = Plane.new
+#    expect(subject.land(plane)).to eq plane
+#  end
 
-  it "lands the plane" do
-    plane = Plane.new
-    expect(subject.land(plane)).to eq plane
-  end
-
-  it "returns the landed planes" do
-    plane = Plane.new
-    subject.land(plane)
-    expect(subject.plane).to eq plane
-  end
+#  it "returns the landed planes" do
+#    plane = Plane.new
+#    subject.land(plane)
+#    expect(subject.plane).to eq plane
+#  end
 
   describe "#release_plane" do
     it "releases a plane" do
@@ -34,8 +32,23 @@ describe Airport do
 
   describe '#land' do
     it 'raises an error when full' do
-      subject.land(Plane.new)
+      subject.capacity.times { subject.land Plane.new}
       expect { subject.land Plane.new }.to raise_error "Airport is full"
+    end
+  end
+
+  it 'has a default capacity' do
+    expect(subject.capacity).to eq Airport::DEFAULT_CAPACITY
+  end
+
+  describe "initialization" do
+    subject { Airport.new}
+    let(:plane) { Plane.new }
+    it 'defaults capacity' do
+      described_class::DEFAULT_CAPACITY.times do
+        subject.land(plane)
+    end
+    expect{ subject.land(plane) }.to raise_error "Airport is full"
     end
   end
 
