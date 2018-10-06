538f1c896089a41d677217f96fe0c2ef9c5b527f
Tolvic <terence.h.mace@gmail.com>
2018-10-06 21:34:06 +0100

    tests written to test taking off in stormy weather

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -3,53 +3,6 @@ require 'airport'
 describe Airport do
   let(:plane) {double :plane}
 
-  describe 'Landing a plane' do
-    it 'in sunny weather' do
-      allow(subject).to receive(:check_weather) { 'sunny' }
-      expect(subject.land(plane)).to eq plane
-    end
-
-    it 'in stormy weather' do
-      allow(subject).to receive(:check_weather) { 'stormy' }
-      expect{subject.land(plane)}.to raise_error('Cannot land in stormy weather')
-    end
-  end
-
-
-
-  it 'A plane can take off' do
-    allow(subject).to receive(:check_weather) { 'sunny' }
-    subject.land(plane)
-    expect(subject.take_off(plane)).to eq plane
-  end
-
-  describe 'Check if plane is in the hanger:' do
-    it 'before it has landed' do
-      expect(subject.in_hanger?(plane)).to eq false
-    end
-
-    it 'after it has landed' do
-      allow(subject).to receive(:check_weather) { 'sunny' }
-      subject.land(plane)
-      expect(subject.in_hanger?(plane)).to eq true
-    end
-
-    it 'after it has taken off' do
-      allow(subject).to receive(:check_weather) { 'sunny' }
-      subject.land(plane)
-      subject.take_off(plane)
-      expect(subject.in_hanger?(plane)).to eq false
-    end
-
-    it 'after attempting to land in stormy weather' do
-      allow(subject).to receive(:check_weather) { 'stormy' }
-      subject.land(plane)
-      rescue
-      expect(subject.in_hanger?(plane)).to eq false
-    end
-
-  end
-
   describe 'Can check the weather' do
     it 'Weather is either sunny or stormy' do
       expect(subject.check_weather).to eq('stormy').or eq('sunny')
@@ -63,6 +16,69 @@ describe Airport do
       expect(subject.check_weather).to eq "sunny"
       expect(subject.check_weather).to eq "stormy"
     end
+  end
+
+  describe 'Landing a plane' do
+    it 'In sunny weather' do
+      allow(subject).to receive(:check_weather) { 'sunny' }
+      expect(subject.land(plane)).to eq plane
+    end
+
+    it 'In stormy weather' do
+      allow(subject).to receive(:check_weather) { 'stormy' }
+      expect{subject.land(plane)}.to raise_error('Cannot land in stormy weather')
+    end
+  end
+
+  describe 'Plane taking off' do
+    it 'In sunny weather' do
+      allow(subject).to receive(:check_weather) { 'sunny' }
+      subject.land(plane)
+      expect(subject.take_off(plane)).to eq plane
+    end
+
+    it 'In stormy weather' do
+      allow(subject).to receive(:check_weather) { 'sunny' }
+      subject.land(plane)
+      allow(subject).to receive(:check_weather) { 'stormy' }
+      expect{subject.take_off(plane)}.to raise_error('Cannot take off in stormy weather')
+    end
+  end
+
+  describe 'Check if plane is in the hanger:' do
+    it 'Before it has landed' do
+      expect(subject.in_hanger?(plane)).to eq false
+    end
+
+    it 'After it has landed' do
+      allow(subject).to receive(:check_weather) { 'sunny' }
+      subject.land(plane)
+      expect(subject.in_hanger?(plane)).to eq true
+    end
+
+    it 'After it has taken off' do
+      allow(subject).to receive(:check_weather) { 'sunny' }
+      subject.land(plane)
+      subject.take_off(plane)
+      expect(subject.in_hanger?(plane)).to eq false
+    end
+
+    it 'After attempting to land in stormy weather' do
+      allow(subject).to receive(:check_weather) { 'stormy' }
+      subject.land(plane)
+      rescue
+      expect(subject.in_hanger?(plane)).to eq false
+    end
+
+    it 'After attempting to take off in stormy weather' do
+      allow(subject).to receive(:check_weather) { 'sunny' }
+      subject.land(plane)
+      allow(subject).to receive(:check_weather) { 'stormy' }
+      subject.take_off(plane)
+      rescue
+      expect(subject.in_hanger?(plane)).to eq false
+    end
+
   end
 
 end
