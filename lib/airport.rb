fdab83f36fa190fb960df1ddbf643bcab6df0cbc
Muna <mmhussein125@gmail.com>
2018-10-06 19:28:26 +0100

    passing second test

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,6 +1,11 @@
+#require 'plane.rb'
+
 class Airport
 
   def land(plane)
   end
 
+  def takeoff(plane)
+    "Plane has taken off"
+  end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,4 +1,5 @@
 require 'airport.rb'
+require 'plane.rb'
 
 describe Airport do
 
@@ -7,6 +8,11 @@ describe Airport do
       expect { subject.land(plane).to_not raise_error }
     end
   end
-
-  
+  describe "#takeoff" do
+    it 'instructs a plane to take off and confirms plane is no longer there' do
+      airport = Airport.new
+      plane = Plane.new
+      expect(subject.takeoff(plane)).to eq "Plane has taken off"
+    end
+  end
 end
