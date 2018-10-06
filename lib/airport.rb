3c392b1e31a2f3fb2b9b2357d88f3435582b27fa
bmoishe <moishebarnes@gmail.com>
2018-10-07 00:40:36 +0100

    User storys 1-4 passing

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,24 +1,32 @@
 class Airport
 
-attr_accessor :hanger
+  attr_accessor :hanger
 
   def initialize
     @hanger = []
   end
 
   def land(plane)
-
-  @hanger << plane
+    @hanger << plane
+    raise "You cannot land. It is too stormy" unless weather != "stormy"
   end
 
   def total_planes(plane)
-  @hanger.include?(plane)
-
+    @hanger.include?(plane)
   end
 
+  def take_off(plane)
+    raise "You cannot takeoff. It is too stormy" unless weather != "stormy"
+    @hanger.delete(plane)
+  end
+
+  def weather
+     x = rand(2)
+    if x > 1 then return "stormy"
+    end
+  end
 end
 
 class Plane
-
 
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,17 +1,39 @@
 require 'airport.rb'
 
+# User Story 1
+describe Plane do
+
+end
+
 describe Airport do
-#   is there a plane?
 
   it 'I want to instruct a plane to land at an airport' do
     plane = Plane.new
     subject.land(plane)
     expect(subject.total_planes(plane)).to eq true
   end
-end
 
+# User Story 2
+  it 'Take-off and confirm that the plane is no longer in the airport' do
+    plane = Plane.new
+    subject.take_off(plane)
+    expect(subject.total_planes(plane)).to eq false
+  end
 
+# User Story 3
+it 'I want to prevent takeoff when weather is stormy' do
+   airport = Airport.new
+   plane = Plane.new
+   allow(airport).to receive(:weather) { "stormy" }
+   expect { airport.take_off(plane) }.to raise_error "You cannot takeoff. It is too stormy"
+ end
 
-describe Plane do
+# User Story 4
+it 'I want to prevent takeoff when weather is stormy' do
+   airport = Airport.new
+   plane = Plane.new
+   allow(airport).to receive(:weather) { "stormy" }
+   expect { airport.land(plane) }.to raise_error "You cannot land. It is too stormy"
+ end
 
 end
