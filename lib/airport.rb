257b88e1753490f37b01b45e7c0e79e98df7c6fb
Tolvic <terence.h.mace@gmail.com>
2018-10-06 17:15:06 +0100

    tests that airport can check weather now pass

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,4 +1,13 @@
 class Airport
+  attr_reader :weather
+  def initialize
+    @weather = Weather.new
+  end
+
+  def check_weather
+    @weather.check
+  end
+
   def land(plane)
     @hanger = plane
   end
