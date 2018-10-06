70b15f18f42ed572fc65c9df34f88a3691062ed8
chloeverity <chloeverity@hotmail.co.uk>
2018-10-07 00:25:43 +0100

    adding default capacity and tests/adding takeoff and tests

--- lib/airport.rb
+++ lib/airport.rb
@@ -2,14 +2,20 @@ require_relative 'plane'
 require_relative 'weather'
 
 class Airport
+  DEFAULT_CAPACITY = 100
 attr_reader :planes
+attr_reader :capacity
 
-  def initialize
+  def initialize(capacity = DEFAULT_CAPACITY)
     @planes = []
+    @capacity = capacity
   end
-  
-  def land(plane)
 
+  def land(plane)
     @planes << plane
+  end
+
+  def takeoff_plane
+    @planes.pop
   end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -9,4 +9,25 @@ describe Airport do
       expect(subject.land(plane)).to eq [plane]
     end
   end
+
+  describe '#takeoff' do
+    it 'allows the plane to take off' do
+      plane = Plane.new
+      weather = Weather.new
+      subject.land(plane)
+      expect(subject.takeoff_plane).to eq plane
+    end
+  end
+
+  describe '#initialize' do
+    it 'should initialize with given capacity' do
+      airport = Airport.new(200)
+      expect(airport.capacity).to eq(200)
+    end
+
+    it 'should initialize with default capacity given no capacity' do
+      airport = Airport.new
+      expect(airport.capacity).to eq Airport::DEFAULT_CAPACITY
+    end
+  end
 end
