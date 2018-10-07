1b08065ffc3a8f1cd4e49c60aeae9af70fae6685
Edyta Bednarz <edy.b.bednarz@gmail.com>
2018-10-07 22:13:23 +0100

    Add random weather algorhythm

--- lib/weather.rb
+++ lib/weather.rb
@@ -1,2 +1,9 @@
 class Weather
+  def stormy?
+    # generates a random number from 1 to 10
+    # if the number is bigger than 8, sets the weather to stormy
+    # this way, it's much more likely
+    # to be sunny (8 out of 10, or roughly 80% of the time)
+    rand(1..10) > 8
+  end
 end
