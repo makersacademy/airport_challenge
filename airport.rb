3e7ff107d3a00e7927972a6f914ce9dad8b7ddfd
Tom <tombjh@hotmail.co.uk>
2018-10-07 23:17:56 +0100

    started of airport tests and airport class

--- airport.rb
+++ airport.rb
@@ -1,2 +1,17 @@
 class Airport
+  attr_reader :planes
+  attr_accessor :capacity
+  def initialize(capacity: DEFAULT_CAPACITY, weather: weather.new)
+    @capacity = capacity
+    @weather = weather
+    @planes = Array.new
+  end
+  def land_plane(plane)
+
+  end
+  def takeoff_plane(plane)
+
+  end
+  private
+  DEFAULT_CAPACITY = 4
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,12 +1,20 @@
 require 'rspec'
 require_relative '../airport'
+require_relative '../plane'
+require_relative '../weather'
 
 describe Airport do
   subject(:airport){described_class.new}
-  describe 'new' do
+  describe '#new' do
     it 'should create a new airport object' do
       expect(described_class).to eq(Airport)
     end
+  end
+  describe '#land_plane' do
+
+  end
+  describe '#takeoff_plane' do
+
   end
 
 end
