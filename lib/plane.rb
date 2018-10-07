492db3278364c8e1adf674474d49bc06e56b1f98
chloeverity <chloeverity@hotmail.co.uk>
2018-10-07 03:08:59 +0100

    adding grounded/flying? tests

--- lib/plane.rb
+++ lib/plane.rb
@@ -3,6 +3,24 @@ require_relative 'airport'
 class Plane
 
   attr_accessor :broken
+  attr_accessor :status
+
+  def initialize(status = false)
+    @status = status
+  end
+
+  def update_status
+    @status = (@status == "Flying" ? "Landed" : "Flying")
+    @status
+  end
+
+  def landed?
+      @status == "Flying" ? false : true
+    end
+
+    def flying?
+      @status == "Flying" ? true : false
+    end
 
   def working?
     !@broken
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -1,5 +1,25 @@
 require 'plane'
 
 describe Plane do
+
+  before :each do
+    @airport = Airport.new
+    @plane = Plane.new
+    allow(Weather).to receive(:stormy?) { false }
+    allow(Plane).to receive(:status) { "Flying" }
+  end
+
   it { is_expected.to respond_to :working? }
+  it { is_expected.to respond_to :status }
+
+  it 'should return true when asked if grounded plane is grounded' do
+    @plane.status == "Landed"
+    expect(@plane.landed?).to be true
+  end
+
+  it 'should return false when asked if a grounded plane is in the air' do
+    @plane.status == "Landed"
+    expect(@plane.flying?).to eq false
+  end
+
 end
