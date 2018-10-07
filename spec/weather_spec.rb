3fb7c4df149c5d956fa9cc91eaead24ab31ddda3
Tom <tombjh@hotmail.co.uk>
2018-10-07 23:01:18 +0100

    completed weather tests

--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -3,10 +3,15 @@ require_relative '../weather'
 
 describe Weather do
   subject(:weather){described_class.new}
-  describe 'new' do
+  describe '#new' do
     it 'should create a new weather object' do
       expect(described_class).to eq(Weather)
     end
+  end
+  describe '#stormy?' do
+    it 'should return a boolean if it is stormy or not' do
+        expect(weather.stormy?).to be(true).or be(false)
+      end
   end
 
 end
--- weather.rb
+++ weather.rb
@@ -1,2 +1,11 @@
 class Weather
+  def stormy?
+    random_outlook == :stormy
+  end
+  private
+  OUTLOOKS = [:stormy, :clear, :clear, :clear]
+
+  def random_outlook
+    OUTLOOKS.sample
+  end
 end
