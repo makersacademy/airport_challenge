3ee61b4f5804c9b2c8637f639eeededb403799d4
Tom <tombjh@hotmail.co.uk>
2018-10-07 22:30:49 +0100

    completed plane class tests and plane

--- plane.rb
+++ plane.rb
@@ -1,2 +1,22 @@
 class Plane
+  attr_reader :landed
+  def initialize(landed = false)
+    @landed = landed
+  end
+
+  def land
+    unless @landed
+      @landed = true
+      return true
+    end
+    false
+  end
+
+  def takeoff
+    if @landed
+      @landed = false
+      return true
+    end
+    false
+  end
 end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -2,11 +2,43 @@ require 'rspec'
 require_relative '../plane'
 
 describe Plane do
-  subject(:plane){described_class.new}
-  describe 'new' do
+  before(:each) do
+    @plane = Plane.new
+    @plane2 = Plane.new(true)
+  end
+
+  describe '#new' do
     it 'should create a new plane object' do
       expect(described_class).to eq(Plane)
     end
+
+    it 'should set the landed state to false by default' do
+      expect(@plane.landed).to eq(false)
+    end
+  end
+  describe '#land' do
+    describe 'with landed equal to false' do
+      it 'should return true' do
+        expect(@plane.land).to eq(true)
+      end
+    end
+    describe 'with landed equal to true' do
+      it 'should return false' do
+        expect(@plane2.land).to eq(false)
+      end
+    end
   end
 
+  describe '#takeoff' do
+    describe 'with landed equal to true' do
+      it 'should return true' do
+        expect(@plane2.takeoff).to eq(true)
+      end
+    end
+    describe 'with landed equal to false' do
+      it 'should return false' do
+        expect(@plane.takeoff).to eq(false)
+      end
+    end
+  end
 end
