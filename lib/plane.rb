5fb922925b76fa3e128d861a460226426012791b
Muna <mmhussein125@gmail.com>
2018-10-07 17:12:10 +0100

    added methods to Plane class to check status of a plane

--- lib/plane.rb
+++ lib/plane.rb
@@ -1,3 +1,21 @@
 class Plane
 
+  attr_accessor :flying
+
+  def initialize
+    @flying = true
+  end
+
+  def flying?
+    @flying
+  end
+
+  def land
+    @flying = false
+  end
+
+  def takeoff
+    @flying = true
+  end
+
 end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -1,4 +1,19 @@
 require 'plane.rb'
 
 describe Plane do
+  it { is_expected.to respond_to :flying?}
+
+      describe "#land" do
+      it "can land a plane" do
+        subject.land
+        expect(subject.flying?).to be false
+      end
+    end
+
+    describe "#takeoff" do
+    it "can takeoff a plane" do
+      subject.takeoff
+      expect(subject.flying?).to be true
+    end
+  end
 end
