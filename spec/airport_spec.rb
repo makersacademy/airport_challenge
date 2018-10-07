d2fbab125d8e7cdb8e0a3fb7869ac0210435928a
Alfie Freeman <runticle@gmail.com>
2018-10-07 20:59:50 +0100

    finaly rubocop sorting and updated README.md

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,8 +2,8 @@ require "airport"
 
 describe Airport do
 
-  let (:plane) { double(:plane, land: true) }
-  let (:airport) { Airport.new }
+  let(:plane) { double(:plane, land: true) }
+  let(:airport) { Airport.new }
 
   it "can be full of planes" do
     10.times { airport.hanger << plane }
