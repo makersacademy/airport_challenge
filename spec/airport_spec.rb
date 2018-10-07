98b308a437dffe1083b9b153252effc619a414bb
Edyta Bednarz <edy.b.bednarz@gmail.com>
2018-10-07 20:01:58 +0100

    Added more test to guarantee we can land multiple planes

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -6,7 +6,7 @@ describe Airport do
     expect(subject).to respond_to(:land).with(1).argument
   end
 
-  it 'can land planes' do
+  it 'can land a plane' do
     plane = Plane.new
 
     subject.land(plane)
@@ -14,12 +14,23 @@ describe Airport do
     expect(subject.planes.first).to eq plane
   end
 
+  it 'can land multiple planes' do
+    plane1 = Plane.new
+    plane2 = Plane.new
+
+    subject.land(plane1)
+    subject.land(plane2)
+
+    expect(subject.planes[0]).to eq plane1
+    expect(subject.planes[1]).to eq plane2
+  end
+
   it 'can take off planes' do
     plane = Plane.new
     subject.land(plane)
 
     subject.take_off(plane)
-    
+
     expect(subject.planes).to be_empty
   end
 end
