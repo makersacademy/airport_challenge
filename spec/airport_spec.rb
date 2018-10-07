5d9b03ae5432505823c1a04543e6a79d0827f147
Alfie Freeman <runticle@gmail.com>
2018-10-07 20:50:37 +0100

    added test to check airport can be full

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,8 +1,14 @@
 require "airport"
 
 describe Airport do
-  
+
   let (:plane) { double(:plane, land: true) }
   let (:airport) { Airport.new }
+
+  it "can be full of planes" do
+    10.times { airport.hanger << plane }
+    expect(airport).to be_full
+
+  end
 
 end
