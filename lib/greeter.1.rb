42728fbf888f144a0eaabe280d69e3610df35036
EC2 Default User <ec2-user@ip-172-31-24-114.us-east-2.compute.internal>
2018-10-08 14:25:21 +0000

    DEPARTING AND WEATHER

--- lib/greeter.1.rb
+++ lib/greeter.1.rb
@@ -1,8 +1,8 @@
 def initialize
-  @airport = []   # max size of 25 planes has been set
+  @airport = []   # airport array created to track number of planes on the ground
 end
 
-def menu
+def menu      # providing the user with a series of options
   puts "Select an option:"
   puts "1. Arrive"
   puts "2. Depart"
@@ -14,9 +14,6 @@ def menu
   else
   end
 end
-
-
-
 
 def add_plane #adds plane to airport
   puts "Is there a plane inbound? (type 'yes' or 'no')"
@@ -44,15 +41,65 @@ def add_plane #adds plane to airport
     end
       puts "Is there another plane inbound?"
       input = gets.chomp
-    if input != "yes"
-      puts "Here are the flight numbers landing: "
-      puts @airport
-      break
-    end
+      if input != "yes"
+        puts "Here are the flight numbers landing: "
+        puts @airport
+        puts "Press 'm' to go back to the menu"
+        menureturn = gets.chomp
+        if menureturn == "m"
+          menu
+        end
+        break
+      end
   end
 end
 
-
+def take_off
+  puts "Is a plane ready take off?"
+  input = gets.chomp
+  while input == "yes"
+    puts"Enter plane number"
+    planenumber = gets.chomp
+    puts "*WARNING*"
+    puts "You must check weather before giving confirmation to depart!"
+    puts "Press 'w' to check for weather"
+    weather_check_option = gets.chomp
+      if weather_check_option == "w"
+         weather_status = rand(1..3)
+         puts "Conditions:"
+         puts "1 = Dry"
+         puts "2 = Rainy"
+         puts "3 = STORMY"
+         puts "The conditions for this plane are: #{weather_status}"
+      else puts "You cannot continue without checking the weather!"
+      end
+      if weather_status != 3
+        @airport.delete(planenumber)
+      else puts "***WARNING IT IS TOO DANGEROUS FOR THIS PLANE TO TAKE OFF***"
+      end
+      puts "Is there another plane departing?"
+      input = gets.chomp
+      if input != "yes"
+        puts "Here are the flight numbers remaining on the ground : "
+        puts @airport
+        puts "Press 'm' to go back to the menu"
+        menureturn = gets.chomp
+        if menureturn == "m"
+          menu
+        end
+        break
+      end
+  end
+end
+    
+    
+    
+    
+    
+    
+    
+    
+    
 
 
 
