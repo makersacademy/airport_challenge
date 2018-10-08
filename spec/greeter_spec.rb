21bdb59da811a2eabbee6af2c4ad28f52cbeac56
EC2 Default User <ec2-user@ip-172-31-24-114.us-east-2.compute.internal>
2018-10-08 16:32:47 +0000

    RSPEC

--- spec/greeter_spec.rb
+++ spec/greeter_spec.rb
@@ -1,9 +1,55 @@
-require 'greeter'
+require 'greeter.1'
 
-#describe Airport do
-#  describe @airport do
-    it 'adds plane to array' do
- #     expect(subject.add_plane(@airport)).to include(@planenumber)
-  #  end
-#end
+describe "User input" do
+ 
+ context "Arrive - ask for whether a plane is inbound " do
+    let(:user_input) { ["1\n"] }
+    let(:expected_output) {
+      ["--------------",
+      "Is there a plane inbound? (type 'yes' or 'no')"]}
+
+    it "`puts`es  description" do
+      set_user_input_and_check_expected_output
+
+    end
+  end
+  
+   
+ context "Depart - ask for whether a plane is outbound " do
+    let(:user_input) { ["2\n"] }
+    let(:expected_output) {
+      ["--------------",
+      "Is there a plane outbound? (type 'yes' or 'no')"]}
+
+    it "`puts`es  description" do
+      set_user_input_and_check_expected_output
+
+    end
+  end
+  
+   
+ context "Change capacity" do
+    let(:user_input) { ["3\n", "43\n"] }
+    let(:expected_output) {
+      ["The new capacity is 43 aircraft!",
+      "Type 'm' to return to the menu"]}
+
+    it "`puts`es  description" do
+      set_user_input_and_check_expected_output
+
+    end
+  end
+
+  context "Depart enter plane number" do
+    let(:user_input) { ["2\n", "yes\n"] }
+    let(:expected_output) { ["--------------", "Is a plane ready take off? (type 'yes' or 'no')", "--------------", "Total aircraft grounded: 0", "--------------","Enter plane number","--------------"]
+        
+    }
+
+    it "`Gives user option to enter flight number" do
+      set_user_input_and_check_expected_output
+    end
+  end
+  
+  
 end
