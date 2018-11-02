ded57adb752eedb1d2646d4477840751cbedd037
Aga <a.dufrat@gmail.com>
2018-11-02 10:27:44 +0000

    A plane can take off from the airport

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -4,10 +4,17 @@ describe Airport do
   let(:plane) {double ("plane")}
 
   context "#land" do
-    it "when landing a plane" do
+    it "is able to land a plane at the airport" do
       subject.land(plane)
       expect(subject.planes).to include(plane)
     end
   end
-  
+
+  context "#take_off" do
+    it "allows a plane to take off from the airport" do
+      subject.take_off(plane)
+      expect(subject.planes).to_not include(plane)
+    end
+  end
+
 end
