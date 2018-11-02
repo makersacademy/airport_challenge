2348ae12fbb000e8b538b0becde225ed0a9e50cb
alittlecross <alittlecross@live.co.uk>
2018-11-02 10:05:38 +0000

    feature test, unit test, passed for calling #land on Plane

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -4,4 +4,7 @@ describe Plane do
   it "expects a new plane to be created" do
     expect(Plane.new).to_not eq nil
   end
+  it "allows a plane to call #land" do
+    expect(subject).to respond_to(:land)
+  end
 end
