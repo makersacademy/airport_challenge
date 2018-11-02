8f584d6811108d2a1181dd161f26993ac5e55edd
alittlecross <alittlecross@live.co.uk>
2018-11-02 14:20:05 +0000

    feature test, unit test, passed for stormy preventing #takeoff

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,10 +1,22 @@
 require_relative 'plane'
 
 class Airport
+  DEAFULT_WEATHER = "fine"
+
+  attr_accessor :weather
+
+  def initialize(weather=DEAFULT_WEATHER)
+    @weather = weather
+  end
   def land
 
   end
   def takeoff
+    fail "The weather is stormy, we cannot take off." if stormy_weather?
     "We're now in the sky, not the airport."
+  end
+
+  def stormy_weather?
+    weather == "stormy"
   end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -10,4 +10,8 @@ describe Airport do
   it "expects #takeoff to return a message" do
     expect(subject.takeoff).to eq "We're now in the sky, not the airport."
   end
+  it "expects #takeoff not to run if weather is stormy" do
+    stansted = Airport.new("stormy")
+    expect { stansted.takeoff }.to raise_error "The weather is stormy, we cannot take off."
+  end
 end
