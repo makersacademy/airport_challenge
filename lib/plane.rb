a2132e464acaeab3e1fc31f356303e1190976843
Al <>
2018-11-02 16:26:57 +0000

    moved exception handling for Plane #land and #take_off to separate private methods

--- lib/plane.rb
+++ lib/plane.rb
@@ -1,16 +1,26 @@
 class Plane
   def land(airport)
-    raise 'This plane has already landed' unless @current_port.nil?
-    raise 'Stormy weather prevents the plane from landing' if airport.stormy?
-    raise 'Cannot land at this airport as it is full' if airport.full?
+    can_i_land_at?(airport)
     airport.accept(self)
     @current_port = airport
   end
 
   def take_off
-    raise 'The plane is not on the ground' if @current_port.nil?
-    raise 'Stormy weather prevents the plane from taking off' if @current_port.stormy?
+    can_i_take_off?
     @current_port.release(self)
     @current_port = nil
+  end
+
+  private
+
+  def can_i_land_at?(airport)
+    raise 'This plane is on the ground' unless @current_port.nil?
+    raise 'Stormy weather prevents the plane from landing' if airport.stormy?
+    raise 'Cannot land at this airport as it is full' if airport.full?
+  end
+
+  def can_i_take_off?
+    raise 'The plane is not on the ground' if @current_port.nil?
+    raise 'Stormy weather prevents the plane from taking off' if @current_port.stormy?
   end
 end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -4,15 +4,15 @@ require 'airport'
 describe Plane do
   it 'can land at an airport if not already landed' do
     plane = Plane.new
-    airport = instance_double('Airport', :stormy? => false, :accept => nil, :full? => false)
+    airport = instance_double('Airport', :stormy? => false, :full? => false, :accept => nil)
     plane.land(airport)
-    expect { plane.land(airport) }.to raise_error('This plane has already landed')
+    expect { plane.land(airport) }.to raise_error('This plane is on the ground')
   end
 
   it 'can take off if currently at an airport' do
     plane = Plane.new
     expect { plane.take_off }.to raise_error('The plane is not on the ground')
-    airport = instance_double('Airport', :stormy? => false, :accept => nil, :release => nil, :full? => false)
+    airport = instance_double('Airport', :stormy? => false, :full? => false, :accept => nil, :release => nil)
     plane.land(airport)
     expect { plane.take_off }.not_to raise_error
     expect { plane.take_off }.to raise_error('The plane is not on the ground')
@@ -20,20 +20,20 @@ describe Plane do
 
   describe 'stormy weather' do
     it 'will not take off if the weather is stormy' do
-      storm_port = instance_double('Airport', :stormy? => false, :accept => nil, :full? => false)
+      storm_port = instance_double('Airport', :stormy? => false, :full? => false, :accept => nil)
       subject.land(storm_port)
       allow(storm_port).to receive(:stormy?) {true}
       expect { subject.take_off }.to raise_error('Stormy weather prevents the plane from taking off')
     end
 
     it 'will not land if the weather is stormy' do
-      storm_port = instance_double('Airport', :stormy? => true, :accept => nil, :full? => false)
+      storm_port = instance_double('Airport', :stormy? => true, :full? => false, :accept => nil)
       expect { subject.land(storm_port) }.to raise_error('Stormy weather prevents the plane from landing')
     end
   end
 
   it 'will not land if the airport is full' do
-    airport = instance_double('Airport', :stormy? => false, :accept => nil, :full? => true)
+    airport = instance_double('Airport', :stormy? => false, :full? => true, :accept => nil)
     expect { subject.land(airport) }.to raise_error('Cannot land at this airport as it is full')
   end
 end
