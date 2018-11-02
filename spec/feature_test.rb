166315d938a75d87d9d5af325c3cf7cb10bb585c
SamJamCul <samuel.culley@digital.cabinet-office.gov.uk>
2018-11-02 10:37:12 +0000

    adds feature test for takeoff

--- spec/feature_test.rb
+++ spec/feature_test.rb
@@ -1,10 +1,14 @@
 require_relative './../lib/plane.rb'
 require_relative './../lib/airport.rb'
 p "Creates new Airport"
-airport1 = Airport.new
+p airport1 = Airport.new
 p "Creates new plane"
-plane1 = Plane.new
+p plane1 = Plane.new
 p "Lands plane at airport"
-plane1.land(airport1)
+p plane1.land(airport1)
 p "Shows plane in airport hangar"
+p airport1.hangar
+p "Makes the plane take off from the airport"
+p airport1.takeoff(plane1)
+p "Shows the plane has been removed from the hangar"
 p airport1.hangar
