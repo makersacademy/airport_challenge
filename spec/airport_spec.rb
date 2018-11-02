bb91bd1199b64207336f46fcec94f92c91722687
Will Wayman <will.jdw@gmail.com>
2018-11-02 11:30:28 +0000

    prevent takeoff when stormy with test amended

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -8,13 +8,15 @@ describe Airport do
   end
   describe '#takeoff' do
     context "when not stormy" do
+      let(:check_stormy) { false }
       it 'should remove a plane from the airport' do
         airbus = Plane.new
         expect(subject.takeoff(airbus)).to eq "#{airbus} has taken off"
       end
     end
     context "when stormy" do
-      it 'should prevent take off if stormy' do
+      let(:check_stormy) { true }
+      it 'should prevent take off' do
         expect(subject.takeoff(Plane.new)).to eq "it is too stormy to take off"
       end
     end
