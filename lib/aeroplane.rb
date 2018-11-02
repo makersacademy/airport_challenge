5198e6853af300266e22376f42bcac7428d6d89a
Kirill Zabrodin <kirillvzabrodin@gmail.com>
2018-11-02 10:23:54 +0000

    cd ..

--- lib/aeroplane.rb
+++ lib/aeroplane.rb
@@ -1,5 +1,10 @@
 require_relative 'airport'
 
 class Aeroplane
-  
+  DEFAULT_STATE = 'flying'
+  attr_reader :status
+
+  def initialize(state = DEFAULT_STATE)
+    @status = state
+  end
 end
--- spec/aeroplane_spec.rb
+++ spec/aeroplane_spec.rb
@@ -1,4 +1,10 @@
 require 'aeroplane'
 
 describe Aeroplane do
+  
+  it 'can tell it is in the air' do
+    expect(subject.status).to eq 'flying'
+  end
+
+
 end
