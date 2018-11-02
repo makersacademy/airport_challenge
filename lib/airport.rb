2dfaa88431aaacaec998847697484e4722aa7570
Aga <a.dufrat@gmail.com>
2018-11-02 13:34:44 +0000

    prevent take off if weather is stormy

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,8 +1,9 @@
 class Airport
-  attr_reader :planes
+  attr_accessor :planes, :stormy_weather
 
   def initialize
     @planes = []
+    @stormy_weather = [true, false].sample
   end
 
   def land(plane)
@@ -10,6 +11,11 @@ class Airport
   end
 
   def take_off(plane)
+    fail "Weather is stormy" if @stormy_weather == true
     @planes.pop
+  end
+
+  def stormy?
+    @stormy_weather
   end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,7 +1,10 @@
 require 'airport'
 
 describe Airport do
-  let(:plane) {double ("plane")}
+  let(:plane) { double("plane") }
+  # let(:airport) { double("stormy airport", :land => true, :take_off => true, :stormy? => false, :stormy_weather => false) }
+  # let(:stormy_airport) { double("stormy airport", :land => true, :take_off => false, :stormy? => true, :stormy_weather => true) }
+  # let(:sunny_airport) {double("sunny airport", :land => true, :take_off => true, :stormy => false)}
 
   context "#land" do
     it "is able to land a plane at the airport" do
@@ -12,9 +15,14 @@ describe Airport do
 
   context "#take_off" do
     it "allows a plane to take off from the airport" do
+      subject.stormy_weather = false
       subject.take_off(plane)
       expect(subject.planes).to_not include(plane)
     end
-  end
 
+    it "prevent take off if weather is stormy" do
+      subject.stormy_weather = true
+      expect { subject.take_off(plane) }.to raise_error "Weather is stormy"
+    end
+  end
 end
