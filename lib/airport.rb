a817711d0ec9b6abbabf53e93737184856ebbeae
alittlecross <alittlecross@live.co.uk>
2018-11-02 10:27:57 +0000

    folder restructuring

--- lib/airport.rb
+++
@@ -1,8 +0,0 @@
-class Plane
-  def land
-
-  end
-  def takeoff
-    "We're now in the sky, not the airport."
-  end
-end
---
+++ lib/plane.rb
@@ -0,0 +1,8 @@
+class Plane
+  def land
+
+  end
+  def takeoff
+    "We're now in the sky, not the airport."
+  end
+end
