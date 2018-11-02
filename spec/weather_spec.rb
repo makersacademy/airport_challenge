40f8e6387d7a3298341d5f78d9aae84bcb0268e5
Stuart Adair <stu@silverka.co.uk>
2018-11-02 16:20:10 +0000

    Additional weather tests ☀️🌩

--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -1,5 +1,17 @@
 require 'weather'
 
-describe 'weather' do
+describe Weather do
+
+  it "can be created" do
+    expect(subject).to be_a(Weather)
+  end
+
+  it "can report the weather" do
+    expect(subject).to respond_to(:weather_report)
+  end
+
+  it "is randomly set to 'fine' or 'stormy'" do
+    expect(1000.times.map { Weather.new.weather_report }).to include("fine", "stormy")
+  end
 
 end
