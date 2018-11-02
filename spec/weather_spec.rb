4031fd37dd4122b2aac7668ba45b988eab7422ff
SamJamCul <samuel.culley@digital.cabinet-office.gov.uk>
2018-11-02 15:51:20 +0000

    small changes to spacing and positioning

--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -1,9 +1,13 @@
 require 'weather'
 
 describe Weather do
+
   describe '#what_is_weather' do
+
     it 'always has either sunny or stormy in 1000 new weather patterns' do
       1000.times { expect(subject.what_is_weather).to match(/sunny|stormy/) }
     end
+    
   end
+
 end
