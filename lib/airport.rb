a25e1659af7331d13d5f5aa8951aa9312bb59759
Ajay <ajaymungur@hotmail.com>
2018-11-02 14:15:38 +0000

    commit 7 - Plane methods moved into Airport class, tests amended, re-run and passed

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,14 +1,19 @@
 class Airport
-end
 
-class Plane
+  def initialize
+    @landing_strip = []
+  end
 
-  def land
+  def land()
     "Safely landed"
   end
 
   def take_off
     "Plane is no longer at the airport"
   end
+
+end
+
+class Plane
 
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,36 +1,42 @@
 require 'airport'
 
 describe 'Plane' do
-
   let(:plane) {Plane.new}
 
   it 'can creates a plane' do
     expect(plane).to be_instance_of(Plane)
   end
-
-  it 'can respond to #lands' do
-    expect(plane).to respond_to(:land)
-  end
-
-  it 'can land and conmfirms the plane has landed' do
-    expect(plane.land).to eq "Safely landed"
-  end
-
-  it 'can respond to #take_off' do
-    expect(plane).to respond_to(:take_off)
-  end
-
-  it 'confirms plane not at aiport using #take_off' do
-    expect(plane.take_off).to eq "Plane is no longer at the airport"
-  end
-  # it 'Will not allow #take_off to run if weather is stormy' do
-  #   expect {plane.take_off}.to raise_error("Take off not possible too stormy")
-  # end
-
 end
 
 describe 'Airport' do
+  let(:airport) {Airport.new}
+
   it 'can create an airport' do
     expect(Airport.new).to be_instance_of(Airport)
   end
+
+  it 'can respond to #lands' do
+    expect(airport).to respond_to(:land)
+  end
+
+  it 'can land and conmfirms the plane has landed' do
+    expect(airport.land).to eq "Safely landed"
+  end
+
+  it 'can respond to #take_off' do
+    expect(airport).to respond_to(:take_off)
+  end
+
+  it 'confirms plane not at aiport using #take_off' do
+    expect(airport.take_off).to eq "Plane is no longer at the airport"
+  end
+
+  describe 'Plane' do
+    let(:plane) {Plane.new}
+
+    it 'can creates a plane' do
+      expect(plane).to be_instance_of(Plane)
+    end
+  end
+
 end
