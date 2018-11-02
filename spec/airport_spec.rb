93fc671bc7d35f732f1caf0d22e276d025f1901e
Ajay <ajaymungur@hotmail.com>
2018-11-02 10:08:22 +0000

    commit - Using TDD created land method which comminicates with a plane object

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,7 +1,15 @@
 require 'airport'
 
 describe 'Plane' do
+
+  let(:plane) {Plane.new}
+
   it 'Creates a new plane' do
-    expect(Plane.new).to be_instance_of(Plane)
+    expect(plane).to be_instance_of(Plane)
   end
+
+  it 'Lands a plane' do
+    expect(plane).to respond_to(:land)
+  end
+
 end
