9f7e42ffcbc6976baa922b1679aa400c63c3d163
Stuart Adair <stu@silverka.co.uk>
2018-11-02 14:43:54 +0000

    Revised tests to be cleaner

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -7,51 +7,44 @@ describe Airport do
   end
 
   it "has a default capacity of 10" do
-    plane = Plane.new
-    airport = Airport.new
-    allow(airport).to receive(:weather).and_return("fine")
+    allow(subject).to receive(:weather).and_return("fine")
 
-    10.times { plane.land(airport) }
+    10.times { Plane.new.land(subject) }
 
-    expect(plane.land(airport)).to eq false
+    expect(Plane.new.land(subject)).to eq false
   end
 
   it "can be created with a different capacity" do
-    plane = Plane.new
     airport = Airport.new(5)
     allow(airport).to receive(:weather).and_return("fine")
 
-    5.times { plane.land(airport) }
+    5.times { Plane.new.land(airport) }
 
-    expect(plane.land(airport)).to eq false
+    expect(Plane.new.land(airport)).to eq false
   end
 
   it "correctly responts to stormy_weather? when it's stormy" do
-    airport = Airport.new
-    allow(airport).to receive(:weather).and_return("stormy")
+    allow(subject).to receive(:weather).and_return("stormy")
 
-    expect(airport.stormy_weather?).to be true
+    expect(subject.stormy_weather?).to be true
   end
 
   it "correctly responts to stormy_weather? when it's fine" do
-    airport = Airport.new
-    allow(airport).to receive(:weather).and_return("fine")
+    allow(subject).to receive(:weather).and_return("fine")
 
-    expect(airport.stormy_weather?).to be false
+    expect(subject.stormy_weather?).to be false
   end
 
   it "correctly responts to fine_weather? when it's fine" do
-    airport = Airport.new
-    allow(airport).to receive(:weather).and_return("fine")
+    allow(subject).to receive(:weather).and_return("fine")
 
-    expect(airport.fine_weather?).to be true
+    expect(subject.fine_weather?).to be true
   end
 
   it "correctly responts to fine_weather? when it's stormy" do
-    airport = Airport.new
-    allow(airport).to receive(:weather).and_return("stormy")
+    allow(subject).to receive(:weather).and_return("stormy")
 
-    expect(airport.fine_weather?).to be false
+    expect(subject.fine_weather?).to be false
   end
 
 end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -3,109 +3,99 @@ require 'plane'
 describe Plane do
 
   it "can create a new plane" do
-    expect(Plane.new).to be_a(Plane)
+    expect(subject).to be_a(Plane)
   end
 
   it "can land at an airport" do
-    plane = Plane.new
     airport = Airport.new
     allow(airport).to receive(:weather).and_return("fine")
 
-    plane.land(airport)
+    subject.land(airport)
 
     expect(airport.landed_planes.first.is_landed?).to eq true
   end
 
   it "can take off from an airport" do
-    plane = Plane.new
     airport = Airport.new
     allow(airport).to receive(:weather).and_return("fine")
 
-    plane.land(airport)
-    plane.take_off(airport)
+    subject.land(airport)
+    subject.take_off(airport)
 
-    expect(plane.is_flying?).to eq true
+    expect(subject.is_flying?).to eq true
   end
 
   it "is no longer at an airport after take-off" do
-    plane = Plane.new
     airport = Airport.new
     allow(airport).to receive(:weather).and_return("fine")
 
-    plane.land(airport)
-    plane.take_off(airport)
+    subject.land(airport)
+    subject.take_off(airport)
 
     expect(airport.landed_planes.size).to eq 0
   end
 
   it "cannot take off if the weather is stormy" do
-    plane = Plane.new
     airport = Airport.new
     allow(airport).to receive(:weather).and_return("fine", "stormy")
 
-    plane.land(airport)
-    plane.take_off(airport)
+    subject.land(airport)
+    subject.take_off(airport)
 
-    expect(airport.landed_planes).to include plane
+    expect(airport.landed_planes).to include subject
   end
 
   it "cannot land if the weather is stormy" do
-    plane = Plane.new
     airport = Airport.new
     allow(airport).to receive(:weather).and_return("stormy")
 
-    plane.land(airport)
+    subject.land(airport)
 
     expect(airport.landed_planes.size).to eq 0
   end
 
   it "cannot land if the airport is full" do
-    plane = Plane.new
     airport = Airport.new
     allow(airport).to receive(:weather).and_return("fine")
 
-    10.times { plane.land(airport) }
+    10.times { Plane.new.land(airport) }
 
-    expect(plane.land(airport)).to eq false
+    expect(Plane.new.land(airport)).to eq false
   end
 
   it "can only take off from an airport it is in" do
-    plane = Plane.new
     airport = Airport.new
     wrong_airport = Airport.new
     allow(airport).to receive(:weather).and_return("fine")
 
-    plane.land(airport)
+    subject.land(airport)
 
-    expect(plane.take_off(wrong_airport)).to eq false
+    expect(subject.take_off(wrong_airport)).to eq false
   end
 
   it "cannot take off if already flying" do
-    plane = Plane.new
     airport = Airport.new
     allow(airport).to receive(:weather).and_return("fine")
 
-    expect(plane.take_off(airport)).to eq false
+    expect(subject.take_off(airport)).to eq false
   end
 
   it "cannot land if already landed" do
-    plane = Plane.new
     airport = Airport.new
     allow(airport).to receive(:weather).and_return("fine")
 
-    plane.land(airport)
+    subject.land(airport)
 
-    expect(plane.land(airport)).to eq false
+    expect(subject.land(airport)).to eq false
   end
 
   it "is in an airport when landed" do
-    plane = Plane.new
     airport = Airport.new
     allow(airport).to receive(:weather).and_return("fine")
 
-    plane.land(airport)
+    subject.land(airport)
 
-    expect(plane.location).to eq airport
+    expect(subject.location).to eq airport
   end
 
 end
