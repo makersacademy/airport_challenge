2ee8e01de1452971ec2f31ae4de1bf696d3b439a
SamJamCul <samuel.culley@digital.cabinet-office.gov.uk>
2018-11-02 14:24:46 +0000

    updates feature tests to reflect new method placement, and removes error checks in feature test as they're not really features

--- spec/feature_test.rb
+++ spec/feature_test.rb
@@ -5,39 +5,15 @@ p "Creates new Airport"
 p airport1 = Airport.new
 p "Creates new plane"
 p plane1 = Plane.new
-p "Changes the weather to stormy"
-p airport1.weather.what_is_weather = 'stormy'
-
-begin
-  p "Shows plane cannot land while stormy"
-  p plane1.land(airport1)
-rescue('Cannot land in stormy weather')
-  p "Cannot land while it's stormy"
-else
-  p "Plane somehow landed while stormy and everyone died"
-end
-
 p "Changes weather to sunny"
 p airport1.weather.what_is_weather = 'sunny'
 p "Lands plane at airport"
 p plane1.land(airport1)
 p "Shows plane in airport hangar"
 p airport1.hangar
-p "Changes weather to stormy"
-p airport1.weather.what_is_weather = 'stormy'
-
-begin
-  p "Shows plane cannot take off while weather is stormy"
-  p airport1.takeoff(plane1)
-rescue('Cannot take off in stormy weather')
-  p "Cannot take off while it's stormy"
-else
-  p "Plane somehow took off while stormy and everyone died"
-end
-
 p "Changes weather to sunny"
 p airport1.weather.what_is_weather = 'sunny'
 p "Makes the plane take off from the airport"
-p airport1.takeoff(plane1)
+p plane1.takeoff
 p "Shows the plane has been removed from the hangar"
 p airport1.hangar
