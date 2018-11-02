856e70e3aa6c72f692e5ae1e0b804cd6cfd8def9
Kirill Zabrodin <kirillvzabrodin@gmail.com>
2018-11-02 00:20:51 +0000

    working storm

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,5 +1,35 @@
-require 'airplane'
+require_relative 'aeroplane'
 
-describe Airport do
+class Airport
+  attr_reader :capacity
+  attr_accessor :aeroplanes, :storm
+  DEFAULT_CAPACITY = 20
+
+  def initialize(capacity = DEFAULT_CAPACITY)
+    @aeroplanes = []
+    @capacity = capacity
+    @storm = set_weather
+  end
+
+  def land(aeroplane)
+    raise "#{self.class.name} full" if full?
+    @aeroplanes << aeroplane
+  end
+
+  def takeoff(aeroplane)
+    raise "Too stormy" if @storm
+    @aeroplanes.delete(aeroplane)
+  end
+
+  private
+
+  def full?
+    @aeroplanes.count == @capacity
+  end
+
+  def set_weather
+    weather_quality = rand(10)
+    weather_quality < 1 ? @storm = true : @storm = false
+  end
 
 end
---
+++ spec/airport_spec.rb
@@ -0,0 +1,48 @@
+require 'airport'
+
+describe Airport do
+  let(:aeroplane) { double :aeroplane }
+
+  describe ' #land' do
+
+    it { is_expected.to respond_to :land }
+
+    it 'lands aeroplane at airport' do
+      expect(subject.land(aeroplane)).to eq [aeroplane]
+    end
+
+    it "won't allow takeoff when stormy" do
+      subject.storm = true
+      subject.aeroplanes = [aeroplane]
+      expect { subject.takeoff(aeroplane) }.to raise_error "Too stormy"
+    end
+  end
+
+  describe ' #takeoff' do
+
+    it { is_expected.to respond_to :takeoff }
+
+    it 'makes aeroplane leave airport' do
+      subject.land(aeroplane)
+      subject.takeoff(aeroplane)
+      expect(subject.aeroplanes).to eq []
+    end
+  end
+
+  describe ' #check_aeroplane' do
+    it 'checks if a plane is in the aeroport' do
+      subject.land(aeroplane)
+      expect(subject.aeroplanes).to eq [aeroplane]
+    end
+  end
+
+  describe ' #initialization' do
+    it 'defaults capacity' do
+      described_class::DEFAULT_CAPACITY.times do
+        subject.land(aeroplane)
+      end
+      expect { subject.land(aeroplane) }.to raise_error "#{described_class.name} full"
+    end
+  end
+
+end
