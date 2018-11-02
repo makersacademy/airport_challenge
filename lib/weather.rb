5666bf2225246045fd677c0e22a54a76f4b05ea2
Max Stevenson <maxstevenson@msn.com>
2018-11-02 13:36:02 +0000

    Developed rspec tests for a Weather class
    Three tests all around generate_weather() function
    
    Tests for random outputting of either stormy or sunny weather

--- lib/weather.rb
+++ lib/weather.rb
@@ -1,4 +1,7 @@
 class Weather
 	def generate_weather
+		# Righthand operand of modulus can be adjusted to set
+		# frequency chance of stormy weather.
+		(rand(1..10) % 3 == 0) ? "stormy" : "sunny"
 	end
 end
--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -5,7 +5,19 @@ describe Weather do
 
 	describe '#generate_weather' do
 		it 'generates a random state of weather' do
-			expect(weather).to respond_to(:generate_weather)
+			expect(weather.generate_weather).to eq("sunny") | eq("stormy")
+		end
+	end
+
+	describe '#generate_weather' do
+		it 'generates sunny weather' do
+			allow(weather).to receive(:generate_weather) {"sunny"}
+		end
+	end
+
+	describe '#generate_weather' do
+		it 'generates stormy weather' do
+			allow(weather).to receive(:generate_weather) {"stormy"}
 		end
 	end
 end
