9000b68001e02f06936d5ccfe07719aac7b67dd6
Kirill Zabrodin <kirillvzabrodin@gmail.com>
2018-11-03 09:38:33 +0000

    working tests and aeroplanes

--- lib/aeroplane.rb
+++ lib/aeroplane.rb
@@ -1,10 +1,17 @@
 require_relative 'airport'
 
 class Aeroplane
+
   DEFAULT_STATE = 'flying'
+
   attr_reader :status
 
   def initialize(state = DEFAULT_STATE)
     @status = state
   end
+
+  def land(where = 'airport')
+    @status = where
+  end
+
 end
--- spec/aeroplane_spec.rb
+++ spec/aeroplane_spec.rb
@@ -1,10 +1,14 @@
 require 'aeroplane'
 
 describe Aeroplane do
-  
+
   it 'can tell it is in the air' do
     expect(subject.status).to eq 'flying'
   end
 
+  it 'can tell it is at an airport' do
+    subject.land
+    expect(subject.status).to eq 'airport'
+  end
 
 end
