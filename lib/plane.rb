7859dbad7cfa118620dd8c016525c7b7c34ed466
Chris Gilbert <chrisgilbert4@icloud.com>
2018-11-04 10:11:23 +0000

    in_air on initialize added

--- lib/plane.rb
+++ lib/plane.rb
@@ -1,3 +1,7 @@
 class Plane
+  attr_reader :in_flight
+  def initialize
+    @in_flight = true
+  end
 
 end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -4,4 +4,10 @@ describe Plane do
 
   let(:plane) { Plane.new }
 
+  describe '#initialize' do
+    it 'tracks if a plane has grounded' do
+      expect(plane.in_flight).to eq true
+    end
+  end
+
 end
