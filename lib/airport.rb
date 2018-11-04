047565d33bb527dbf4ff2c787f8f8b92528787bf
James de Winton <james.dewinton@gmail.com>
2018-11-04 19:18:28 +0000

    second commit almost done

--- lib/airport.rb
+++ lib/airport.rb
@@ -8,7 +8,17 @@ class Airport
     "plane has taken off and no longer at airport"
 
   end
+  def storm
+    @storm = true
+    if @storm == true
+    "plane has been grounded due to storm"
+  end
+  end
+
+  def landing_from_air
+    "plane cannot land due to storm"
+
+  end
 
 
-
-end
+  end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -9,4 +9,14 @@ describe Airport do
   it 'instructs plane to take off' do
     expect(subject.take_off).to eq "plane has taken off and no longer at airport"
   end
+
+  it "grounds plane when storm is present" do
+    expect(subject.storm).to eq "plane has been grounded due to storm"
+  end
+
+  it "prevents plane from landing when storm is present" do
+    expect(subject.landing_from_air).to eq "plane cannot land due to storm"
+  end
+
+
 end
