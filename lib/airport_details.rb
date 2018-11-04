516b341a18c54bb3e1e9e889f85cd6ac8042a156
Cristopher Palacios <criszelaya24@hotmail.com>
2018-11-04 15:17:10 +0000

    Cancelling land because of the weather

--- lib/airport_details.rb
+++ lib/airport_details.rb
@@ -1,17 +1,22 @@
 #Class Weather
 class Weather
+	def initialize
+		@@storm = false
+	end
 	def stormy?
-		true
+		@@storm = true
 	end
 end
 
 # Class to know the status of the aircraft
 class Status < Weather
 	def land
-		@@land = true
+		fail 'You can not land the aircraft because of the storm' if @@storm == true
+		@land = true
 	end
 
 	def take_off
-		@@take_of = true
+		# fail 'You can not take off the aircraft because of the storm' if @@status == true
+		@take_of = true
 	end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,6 +1,16 @@
 #https://github.com/criszelaya24/airport_challenge
 require 'airport_details'
 
+describe Weather do
+	# Funtions to know how is the weather to prevent to land and take off the aircraft
+	context 'Is necessary to know if is stormy' do
+		it 'We need to receive a true value from this test' do
+			weather = Weather.new
+			expect(weather.stormy?).to eq true
+		end
+	end
+end
+
 describe Status do
 	# First test, we need to know if the plane has permission to land
 	context 'Status to know if is possible to land at the airport' do
@@ -20,19 +30,9 @@ describe Status do
 	# We need to raise and error and change the status of the aircraft because of the weather
 	context 'Change the status of the aircraft because of the weather' do
 		it 'Calcel the take off status of the aircraft' do
-			safety = Safety.new
+			safety = Status.new
 			safety.stormy?
-			expect { safety.take_off }.to raise_error('You can not take off the aircraft because of the storm')
-		end
-	end
-end
-
-describe Weather do
-	# Funtions to know how is the weather to prevent to land and take off the aircraft
-	context 'Is necessary to know if is stormy' do
-		it 'We need to receive a true value from this test' do
-			weather = Weather.new
-			expect(weather.stormy?).to eq true
+			expect{safety.land}.to raise_error('You can not land the aircraft because of the storm')
 		end
 	end
 end
