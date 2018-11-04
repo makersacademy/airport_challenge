d1cb3771f23c5f1a462c8277e010327bb0f74235
Tomas Doherty <tomasdoh1@gmail.com>
2018-11-04 20:34:47 +0000

    Updated land() method & test, created planes instance variable

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,8 +2,14 @@ require 'airport'
 
 describe Airport do
 
-  it 'responds to .land(plane)' do
+  it 'should respond to #land(plane)' do
     expect(subject).to respond_to(:land).with(1).argument
   end
-  
+
+  it 'allows planes to land' do
+    plane = Plane.new
+    subject.land(plane)
+    expect(subject.planes).to include plane
+  end
+
 end
