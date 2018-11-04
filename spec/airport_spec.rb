3588d4b7b26ac168e964d32273b5a48da9b77a4e
topickering <topickering@gmail.com>
2018-11-04 22:00:55 +0000

    refine following rubocop

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,6 +2,8 @@ require 'airport'
 
 describe Airport do
 
+  let(:plane) { double :plane }
+
   it 'sets a default capacity' do
     expect(subject.capacity).to eq Airport::DEFAULT_CAPACITY
   end
@@ -13,43 +15,46 @@ describe Airport do
 
   describe '#land' do
     it 'stores landed planes at the airport' do
-      allow(subject).to receive(:current_weather) { 'fine' }
+      allow(plane).to receive(:land)
+      receive(:current_weather).and_return 'fine'
       n = rand(1..5)
-      n.times { subject.land(Plane.new) }
+      n.times { subject.land plane }
       expect(subject.planes.length).to eq n
     end
     it 'changes the status of the landed plane' do
-      allow(subject).to receive(:current_weather) { 'fine' }
-      plane = Plane.new
-      subject.land(plane)
+      receive(:current_weather).and_return 'fine'
+      allow(plane).to receive(:land)
+      allow(plane).to receive(:status).and_return(subject)
+      subject.land plane
       expect(plane.status).to eq subject
     end
     it 'will not allow planes to land when stormy' do
-      allow(subject).to receive(:current_weather) { 'stormy' }
-      plane = Plane.new
-      expect { subject.land(plane) }.to raise_error 'Too stormy to try landing'
+      receive(:current_weather).and_return 'stormy'
+      allow(plane).to receive(:land)
+      expect { subject.land plane }.to raise_error 'Too stormy to try landing'
     end
     it 'will not allow planes to land when full' do
-      allow(subject).to receive(:current_weather) { 'fine' }
-      Airport::DEFAULT_CAPACITY.times { subject.land(Plane.new) }
-      expect { subject.land(Plane.new) }.to raise_error 'This airport is full'
+      receive(:current_weather).and_return 'fine'
+      allow(plane).to receive(:land).and_return(plane)
+      Airport::DEFAULT_CAPACITY.times { subject.land plane }
+      expect { subject.land plane }.to raise_error 'This airport is full'
     end
   end
 
   describe '#take_off' do
     it 'will only allow a plane to take off if at that airport' do
-      allow(subject).to receive(:current_weather) { 'fine' }
+      receive(:current_weather).and_return 'fine'
+      allow(plane).to receive(:land).and_return(plane)
       airport_1 = Airport.new
       airport_2 = Airport.new
-      plane = Plane.new
       airport_1.land(plane)
       expect { airport_2.take_off(plane) }.to raise_error 'Plane not at this airport'
     end
     it 'will not allow planes to take off when stormy' do
-      allow(subject).to receive(:current_weather) { 'fine' }
-      plane = Plane.new
-      subject.land(plane)
-      expect { subject.take_off(plane) }.to raise_error 'Too stormy to try take off'
+      receive(:current_weather).and_return 'stormy'
+      allow(plane).to receive(:land).and_return(plane)
+      subject.land plane
+      expect { subject.take_off plane }.to raise_error 'Too stormy to try take off'
     end
   end
 
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -2,22 +2,23 @@ require 'plane'
 
 describe Plane do
 
+  let(:airport) { double :airport }
+
   it { is_expected.to respond_to(:land).with(1).argument }
 
   describe '#land' do
     it 'is only possible to land a flying plane' do
-      subject.land('airport')
-      expect { subject.land('airport') }.to raise_error 'This plane has already landed'
+      subject.land(airport)
+      expect { subject.land(airport) }.to raise_error 'This plane has already landed'
     end
     it 'is possible to land a plane at a specific airport' do
-      airport = Airport.new
       expect(subject.land(airport).status).to eq airport
     end
   end
 
   describe '#take_off' do
     it 'is possible to order a plane to take off' do
-      expect(subject.land('here').take_off.status).to eq 'flying'
+      expect(subject.land(airport).take_off.status).to eq 'flying'
     end
     it 'is not possible for an already flying plane to take off' do
       expect { subject.take_off }.to raise_error 'This plane is already in the air'
