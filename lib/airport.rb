352d42206205dc635bb12cd7eb40b8205bd685fb
Lorna Harwood <lornapeepwreckers@gmail.com>
2018-11-04 10:56:05 +0000

    test for checking a plane takes off and isn't in airport anymore

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,12 +1,21 @@
 class Airport
 
-  # def initalize
-  #   @grounded = true
-  # end
+  attr_reader :plane_in_airports
 
-  def land(plane)
-    @plane = plane
-    # @grounded = true
+  def initialize
+    @plane_in_airport = true
+  end
+
+  def instruct_land
+    @plane_in_airport = true
+  end
+
+  def instruct_take_off
+    @plane_in_airport = false
+  end
+
+  def plane_is_in_airport
+    @plane_in_airport
   end
 
 
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -3,13 +3,16 @@ require 'airport'
 describe Airport do
 
   it "should instruct a plane to land at an airport" do
-    expect(subject).to respond_to(:land).with(1).argument
+    expect(subject.instruct_land).to eq_to true
+  end
+
+  it "instruct a plane to take off and confirm it is no longer in the airport" do
+    status = subject.plane_is_in_airport
+    expect(status).to eq_to true
+    subject.instruct_take_off
+    status = subject.plane_is_in_airport
+    expect(status).to eq_to false
   end
 
 
-
-  # it "instruct a plane to take off and confirm it is no longer in the airport" do
-  #   plane = subject.take_off
-  #   expect(plane)
-  # end
 end
