13d747b59f9a0b9fe81de3d4bd8436f879a1905a
Chris Gilbert <chrisgilbert4@icloud.com>
2018-11-04 14:32:26 +0000

    added tests to check that ground and unground return self

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -11,17 +11,26 @@ describe Plane do
   end
 
   describe '#ground_plane' do
-    it 'user can check if is in flight' do
+    it 'user can check if a plane is in flight' do
       plane.ground_plane
       expect(plane.in_flight).to eq false
     end
 
-    it 'user can check if plane is not in flight' do
-      plane.unground_plane do
-        plane.ground_plane
-        plane.unground_plane
-        expect(plane.in_flight).to eq true
-      end
+    it 'returns self after switching in flight to false' do
+      plane_1 = Plane.new
+      expect(plane_1.ground_plane).to eq plane_1
+    end
+
+    it 'user can check if a plane is not in flight' do
+      plane.ground_plane
+      plane.unground_plane
+      expect(plane.in_flight).to eq true
+    end
+
+    it 'returns self after switching in flight to true' do
+      plane_1 = Plane.new
+      plane_1.ground_plane
+      expect(plane_1.unground_plane).to eq plane_1
     end
   end
 
