25464ab35dc09dc6254ed795ba9d552579578051
will mcbrien <williamjamesmcbrien@gmail.com>
2018-11-05 10:56:37 +0000

    Better feature script

--- feature_test.rb
+++ feature_test.rb
@@ -3,13 +3,10 @@ Dir["./lib/*.rb"].each {|file| require file }
 air_traffic_controller = AirTrafficController.new
 
 aeroplane1 = Aeroplane.new
-
 aeroplane2 = Aeroplane.new
-
 aeroplane3 = Aeroplane.new
 
 airport1 = Airport.new
-
 airport2 = Airport.new
 
 weather = Weather.new
@@ -26,5 +23,14 @@ puts "airport1 has: #{airport1.aeroplanes}"
 puts "airport2 has: #{airport2.aeroplanes}"
 
 
+puts air_traffic_controller.ask_plane_to_take_off(aeroplane1, airport1, weather)
+puts air_traffic_controller.ask_plane_to_take_off(aeroplane2, airport2, weather)
+
+puts "Weather is stormy?: #{weather.stormy?}"
+puts "aeroplane1 is flying: #{aeroplane1.flying}"
+puts "aeroplane2 is flying: #{aeroplane2.flying}"
+puts "aeroplane3 has landed: #{aeroplane3.landed}"
+puts "airport1 has: #{airport1.aeroplanes}"
+puts "airport2 has: #{airport2.aeroplanes}"
 
 
