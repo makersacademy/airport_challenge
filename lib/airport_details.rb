d0f50af005c9734bccabc67fbf06f69cad3a9799
Cristopher Palacios <criszelaya24@hotmail.com>
2018-11-05 13:28:00 +0000

    Passing the test for max capacity

--- lib/airport_details.rb
+++ lib/airport_details.rb
@@ -15,13 +15,13 @@ class Airport < Weather
 	end
 
 	DEFAULT_CAPACITY = 20
-	
+
 	def full?
-		@full = true
+		@hangar.count >= DEFAULT_CAPACITY
 	end
 
 	def land
-		# fail 'You can not land the aircraft because the airport is full' if @full == true
+		fail 'You can not land the aircraft because the airport is full' if full?
 		fail 'You can not land the aircraft because of the storm' if @storm == true
 		@hangar << +1
 		@hangar.count
