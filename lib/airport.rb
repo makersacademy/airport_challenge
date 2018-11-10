678e5e4daa7f3252457e807f9233a375b707e84a
Ajay <ajaymungur@hotmail.com>
2018-11-10 21:23:08 +0000

    commit 16 - Used TDD to prevent planes taking off if they are flying, also refactored tests

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,33 +1,46 @@
 WEATHER = ["Sunny", "Stormy"]
+DEFAULT_CAPACITY = 20
 
 class Airport
-  attr_reader :landing_strip, :departure
-  attr_accessor :weather, :max_capacity
+  attr_reader :departure
+  attr_accessor :weather, :max_capacity, :landing_strip
 
-  def initialize(max_capacity = 20)
-    @landing_strip = []
+  def initialize(landing_strip = [Plane.new], max_capacity = DEFAULT_CAPACITY)
+    @landing_strip = landing_strip
     @max_capacity = max_capacity
   end
 
   def land(plane)
     fail 'Sorry, no room' if @landing_strip.length == @max_capacity
     fail 'Sorry, too stormy' if WEATHER.sample == "Stormy"
+    # plane.status = "landed"
     @landing_strip << plane
     "Safely landed"
   end
 
-  def take_off
+  def take_off(plane = landing_strip.last)
     fail 'Sorry, too stormy' if WEATHER.sample == "Stormy"
+    fail 'Already in the air' if plane.flying?
     @departure = @landing_strip.pop
+    departure.flying = true
     "flight number #{departure} is no longer at the airport"
+  end
+
+  def flying?
+    @flying
   end
 
 end
 
 class Plane
+  attr_accessor :flying
 
   def initialize
-    @plane
+    @flying = false
+  end
+
+  def flying?
+    @flying
   end
 
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -10,59 +10,68 @@ describe 'Airport' do
     expect(Airport.new).to be_instance_of(Airport)
   end
 
-  it 'can respond to #lands' do
-    expect(airport).to respond_to(:land)
+  describe '#land' do
+    context 'when the weather is sunny' do
+      before '' do
+        sunny
+      end
+      it 'can land and conmfirms the plane has landed' do
+        expect(airport.land(plane)).to eq "Safely landed"
+      end
+
+      it 'will prvent #land from executing if landing strip is full (max can be 20)' do
+        expect { 21.times { airport.land(plane) } }.to raise_error("Sorry, no room")
+      end
+    end
+
+    context 'when the weather is stormy' do
+      it 'will throw an error if #land is called when whether is stormy' do
+        stormy
+        expect { airport.land(plane) }.to raise_error("Sorry, too stormy")
+      end
+    end
   end
 
-  it 'can land and conmfirms the plane has landed' do
-    sunny
-    expect(airport.land(plane)).to eq "Safely landed"
-  end
+  describe '#take_off' do
+    context 'when the weather is sunny' do
+      before '#take_off, set weather to be sunny' do
+        sunny
+      end
+      it 'confirms plane not at aiport using #take_off' do
+        2.times { airport.land(plane) }
+        expect(airport.take_off).to eq "flight number #{plane} is no longer at the airport"
+      end
 
-  it 'can respond to #take_off' do
-    expect(airport).to respond_to(:take_off)
-  end
+      it 'confirms plane is not at the airport after #take_off' do
+        airport.land(plane)
+        airport.take_off
+        expect(airport.landing_strip).not_to include(plane)
+      end
 
-  it 'confirms plane not at aiport using #take_off' do
-    sunny
-    2.times { airport.land(plane) }
-    expect(airport.take_off).to eq "flight number #{plane} is no longer at the airport"
-  end
+      it "will thrown an error if a plane takes tires to #take_off while flying" do
+        allow(plane).to receive(:flying?).and_return(true)
+        expect { airport.take_off(plane) }.to raise_error("Already in the air")
+      end
+    end
 
-  it 'confirms plane is not at the airport after #take_off' do
-    sunny
-    airport.land(plane)
-    airport.take_off
-    expect(airport.landing_strip).not_to include(plane)
-  end
-
-  it 'will throw an error if #land is called when whether is stormy' do
-    stormy
-    expect { airport.land(plane) }.to raise_error("Sorry, too stormy")
-  end
-
-  it 'will prvent #land from executing if landing strip is full (max can be 20)' do
-    sunny
-    expect { 21.times { airport.land(plane) } }.to raise_error("Sorry, no room")
+    context 'when the weather is stormy' do
+      it 'will throw an error if #take_off is called when whether is stormy' do
+        stormy
+        expect { airport.take_off }.to raise_error("Sorry, too stormy")
+      end
+    end
   end
 
   it "Will allow you to increase the max capacity of the aiport (default is 20)" do
     sunny
-    airport.max_capacity = 21
+    airport.max_capacity = 22
     expect { 21.times { airport.land(plane) } }.not_to raise_error
   end
+end
 
-  it 'will throw an error if #take_off is called when whether is stormy' do
-    stormy
-    expect { airport.take_off }.to raise_error("Sorry, too stormy")
+describe 'Plane' do
+  let(:plane) { Plane.new }
+  it 'can create a plane' do
+    expect(plane).to be_instance_of(Plane)
   end
-
-  describe 'Plane' do
-    let(:plane) { Plane.new }
-
-    it 'can create a plane' do
-      expect(plane).to be_instance_of(Plane)
-    end
-  end
-
 end
