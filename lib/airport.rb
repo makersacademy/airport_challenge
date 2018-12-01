f4658156a3fb59bcab8fd511bd77a23ff9daf941
Connor Kam-Cheong <connorkc42@gmail.com>
2018-12-01 22:28:04 +0000

    Added default capacity and ability to change capacity of airports. Planes cannot take off if not in the relevant airport

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,20 +1,26 @@
 require_relative 'plane'
 
 class Airport
-  attr_reader :hangar
+  DEFAULT_CAPACITY = 20
+  attr_reader :hangar, :capacity, :weather
 
-  def initialize
+  def initialize(capacity = DEFAULT_CAPACITY)
     @hangar = []
+    @capacity = capacity
+    @weather = "sunny"
   end
 
   def land(plane)
-    raise 'Plane already landed' if plane.landed == true
+    raise 'Plane already landed' if plane.landed
+    raise 'Airport at capacity' if full?
+    # raise 'Plane cannot land due to stormy weather' if weather == "stormy"
     plane.landed = true
     hangar << plane
   end
 
   def take_off(plane)
-    raise 'Plane already flying' if plane.landed == false
+    raise 'Plane already flying' unless plane.landed
+    raise 'Plane not in current airport' unless in_airport?(plane)
     plane.landed = false
     hangar.delete(plane)
   end
@@ -23,8 +29,10 @@ class Airport
     hangar.include?(plane)
   end
 
-end
+  private
 
-# def hangar
-#   @hangar
-# end
+  def full?
+    hangar.count >= capacity
+  end
+
+end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -7,6 +7,10 @@ describe Airport do
   it { is_expected.to respond_to(:in_airport?).with(1).argument }
   it { is_expected.to respond_to(:take_off).with(1).argument }
 
+  it 'has a default capacity' do
+    expect(subject.capacity).to eq Airport::DEFAULT_CAPACITY
+  end
+
   describe '#land' do
     it 'lands a plane' do
       expect(subject.land(plane)).to eq [plane]
@@ -15,6 +19,11 @@ describe Airport do
     it 'does not let the same plane land twice' do
       subject.land(plane)
       expect { subject.land(plane) }.to raise_error 'Plane already landed'
+    end
+
+    it 'does not land a plane if airport is at capacity' do
+      Airport::DEFAULT_CAPACITY.times { subject.land(Plane.new) }
+      expect { subject.land(plane) }.to raise_error 'Airport at capacity'
     end
   end
 
@@ -25,14 +34,22 @@ describe Airport do
       expect(subject.hangar).to eq []
     end
 
-    it 'does not let the same plane take off twice' do
+    it 'does not let the same plane take_off twice' do
       subject.land(plane)
       subject.take_off(plane)
       expect { subject.take_off(plane) }.to raise_error 'Plane already flying'
     end
+
+    it 'does not take_off if the plane is not in current airport' do
+      airport1 = Airport.new
+      airport2 = Airport.new
+      airport1.land(plane)
+
+      expect { airport2.take_off(plane) }.to raise_error 'Plane not in current airport'
+    end
   end
 
-  describe '#in_airport' do
+  describe '#in_airport?' do
     it 'checks if a plane is not in an airport' do
       expect(subject.in_airport?(plane)).to eq false
     end
@@ -42,5 +59,11 @@ describe Airport do
       expect(subject.in_airport?(plane)).to eq true
     end
   end
+
+  # describe '#stormy' do
+  #   it 'does not let planes land if the weather is stormy' do
+  #     expect { subject.land(plane) }.to raise_error 'Plane cannot land due to stormy weather'
+  #   end
+  # end
 
 end
