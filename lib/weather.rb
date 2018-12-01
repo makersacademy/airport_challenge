2c4ae30b02c1331fe261c022a216c2d14fe5eb4c
CKKH <chriskkhassan@gmail.com>
2018-12-01 19:44:07 +0000

    Returns sunny if .sunny_or_stormy(1) passed on Weather

--- lib/weather.rb
+++ lib/weather.rb
@@ -1,4 +1,7 @@
 class Weather
-  def sunny_or_stormy
+  def sunny_or_stormy(number)
+    if number == 1
+      'sunny'
+    end 
   end
 end
--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -2,8 +2,12 @@ describe Weather do
   subject(:weather) { Weather.new }
 
   describe '#sunny_or_stormy' do
-    it 'responds to .sunny_or_stormy' do
-      expect(weather).to respond_to(:sunny_or_stormy)
+    it 'responds to .sunny_or_stormy  ' do
+      expect(weather).to respond_to(:sunny_or_stormy).with(1).argument
+    end
+
+    it 'returns sunny when 1 passed as argument' do
+      expect(weather.sunny_or_stormy(1)).to eq 'sunny'
     end
   end
 end
