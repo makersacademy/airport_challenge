e4fa497dca9adf5edefb898b7c0f7600312db0cc
Tom Russell <TR115251@hotmail.co.uk>
2018-12-01 20:06:05 +0000

    more refactoring

--- spec/Aeroplane_spec.rb
+++ spec/Aeroplane_spec.rb
@@ -4,6 +4,7 @@ require "Aeroplane.rb"
 # As an air traffic controller
 # So I can get passengers to a destination
 # I want to instruct a plane to land at an airport
+
 describe "Plane" do
 subject(:plane) { Plane.new }
 
