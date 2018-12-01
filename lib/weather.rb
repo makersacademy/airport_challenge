25b4ec5c24f286498c27190ae5e0a8c0252c108a
celine kaslin <celine.kaslin@gmail.com>
2018-12-01 22:31:01 +0000

    is_sunny method added

--- lib/weather.rb
+++ lib/weather.rb
@@ -1,11 +1,18 @@
 class Weather
 
-SUNNY = 1
-STORMY = 2
+  SUNNY = 1
+  STORMY = 2
 
   def forecast
     rand(1..2)
   end
 
+  def is_sunny?(weather)
+    if weather == SUNNY
+      true
+    else
+      false
+    end
+  end
 
 end
