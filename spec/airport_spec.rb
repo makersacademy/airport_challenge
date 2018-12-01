49911b466c29653a481c5287fad6681118e71f37
CKKH <chriskkhassan@gmail.com>
2018-12-01 13:47:48 +0000

    DRY'd spec file using subject {}'

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,10 +1,11 @@
 require 'airport'
 
 describe Airport do
+  subject(:airport) { Airport.new }
 
   describe '#land' do
     it 'responds to .land(plane)' do
-      expect(subject).to respond_to(:land).with(1).argument
+      expect(airport).to respond_to(:land).with(1).argument
     end
 
     it 'only allows .land argument to be kind of plane' do
@@ -15,7 +16,7 @@ describe Airport do
 
   describe '#release' do
     it 'responds to .release(plane)' do
-      expect(subject).to respond_to(:release).with(1).argument
+      expect(airport).to respond_to(:release).with(1).argument
     end
 
     it 'only allows planes to .release_plane' do
