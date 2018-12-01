de30e59d65ef31d7fb4b88ebb3dc968e6a4cb344
James Hotblack <james.c.hotblack@gmail.com>
2018-12-01 15:51:53 +0000

    Described Plane class

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -5,7 +5,12 @@ describe Airport do
   describe ".land" do
     it "lands a plane at the airport" do
       expect(subject).to respond_to :land
+      
     end
   end
+
+end
+
+describe Plane do
 
 end
