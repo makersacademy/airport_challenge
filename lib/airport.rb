f414bebe88b7a474882d2e66a6a4628453124613
celine kaslin <celine.kaslin@gmail.com>
2018-12-01 21:42:46 +0000

    airport class created + initializer + landing possibility method

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,6 +1,19 @@
 class Airport
 
-  def capacity
+  def initialize (name, capacity, landed_planes)
+    @name = name
+    @capacity = capacity
+    @landed_planes = landed_planes
+  end
+
+  attr_accessor :landed_planes
+
+  def landing_possibility
+    if @landed_planes >= @capacity
+      "you cannot land"
+    else
+      "you can land"
+    end
   end
 
 end
