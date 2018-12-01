9d3e3903d0d6581459c820f60fe86d7574977d20
CKKH <chriskkhassan@gmail.com>
2018-12-01 17:23:01 +0000

    DRY'd airport_spec file, removed responsiveness tests

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -5,11 +5,8 @@ describe Airport do
 
 
   describe '#land' do
-    it 'responds to .land(plane)' do
-      expect(airport).to respond_to(:land).with(1).argument
-    end
 
-    it 'expects .land to return plane' do
+    it 'expects .land(plane) to return plane' do
       plane = Plane.new
       expect(airport.land(plane)).to eq plane
     end
@@ -19,18 +16,17 @@ describe Airport do
       airport.land(plane)
       expect(airport.plane).to eq plane
     end
+
   end
 
-  describe '#release' do
-    it 'responds to .release_plane' do
-      expect(airport).to respond_to(:release_plane)
-    end
-
-    it 'expects .plane to return released plane ID' do
+  describe '#release_plane' do
+     
+    it 'expects .release_plane to return released plane ID' do
       plane = Plane.new
       airport.land(plane)
       expect {airport.release_plane}.to output("#{plane} departed").to_stdout
     end
+
   end
 
 end
