857b39fb45cfcfd285ca50731874ae9832bd4ff8
celine kaslin <celine.kaslin@gmail.com>
2018-12-01 21:43:55 +0000

    test for airport landing possibility method

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,11 +2,20 @@ require "airport"
 
 describe Airport do
 
-  it "should respond to the method capacity" do
-    expect(subject). to respond_to :capacity
+  it "should respond to the method landing_possibility" do
+    airport = Airport.new("Marignane", 100, 80)
+    expect(airport).to respond_to :landing_possibility
   end
 
+  it "should return 'you cannot land' if the airport is the capacity is equal to the number of landed planes" do
+    airport = Airport.new("Marignane", 100, 100)
+    expect(airport.landing_possibility).to eq "you cannot land"
 
+  end
 
+  it "should return 'you can land' if the airport is the capacity is greater than the number of landed planes" do
+    airport = Airport.new("Marignane", 100, 80)
+    expect(airport.landing_possibility).to eq "you can land"
+  end
 
 end
