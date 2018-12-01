89af351c560698f9719188185429ba27251ca16d
James Hotblack <james.c.hotblack@gmail.com>
2018-12-01 15:55:57 +0000

    Added working .land method

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -5,7 +5,8 @@ describe Airport do
   describe ".land" do
     it "lands a plane at the airport" do
       expect(subject).to respond_to :land
-      
+      plane = Plane.new
+      expect (subject.land(plane)).equal? plane
     end
   end
 
