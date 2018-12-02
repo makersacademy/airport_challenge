f876eaace6c0ab8519130263ab97b095094215c5
Joao Abbott-Gribben <joao.abbott.gribben@gmail.com>
2018-12-02 13:26:12 +0000

    airport can receive an instance of Plane class and responds to land method

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,4 +1,16 @@
 require 'airport'
+require 'plane'
 
 describe Airport do 
+  let(:airport) { described_class.new }    
+  let(:plane) { Plane.new }
+
+  it 'responds to #land' do
+    expect(airport).to respond_to(:land).with(1).argument
+  end
+
+  it 'receives a plane' do
+    expect(airport.land(plane)).to eql plane
+  end
+  
 end
