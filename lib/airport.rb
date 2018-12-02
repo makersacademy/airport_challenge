1ea2ee9e208537147e7e5cc6ae2cdf3bf9e9c271
Alina Goaga <goaga.alina@gmail.com>
2018-12-02 21:13:51 +0000

    Added checks around weather conditions and airplane state

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,21 +1,39 @@
 require_relative 'airplane'
 require_relative 'weather'
 
+require 'pry'
+
 class Airport
 
-  def initialize(weather = Weather.new)
+  DEFAULT_CAPACITY = 3
+
+  def initialize(capacity = DEFAULT_CAPACITY, weather = Weather.new)
+    @capacity = capacity
     @airplanes = []
     @weather = weather
   end
 
   def land(airplane)
-    @airplanes << airplane unless @weather.stormy?
+    fail 'The plane has already landed' if in_the_airport?(airplane)
+    fail 'Airport full' if full?
+    fail 'The weather is stormy' if @weather.stormy?
+    @airplanes << airplane
   end
 
-  # for the take off we need to ensure the plane has previously landed
-  # take_off(airplane) => look for plane in the array
   def take_off(airplane)
-    @airplanes.select { |plane| plane == airplane }.first unless @weather.stormy?
+    fail 'The plane hasnt landed yet' unless in_the_airport?(airplane) 
+    fail 'The weather is stormy' if @weather.stormy?
+    @airplanes.select { |plane| plane == airplane }.first
+  end
+
+  def in_the_airport?(airplane)
+    @airplanes.include?(airplane)
+  end
+
+  private
+
+  def full?
+    @airplanes.length >= @capacity
   end
 
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,31 +1,75 @@
 require 'airport'
+require 'pry'
 
 RSpec.describe Airport do
   before(:each) do
     @weather = Weather.new
-    @airport = Airport.new(@weather)
+    @airport = Airport.new(Airport::DEFAULT_CAPACITY, @weather)
+    allow(@weather).to receive(:stormy?) { false }
+  end
+
+  it 'ignores the default capacity if a new one is given' do
+    @airport = Airport.new(2, @weather)
+    2.times { @airport.land(Airplane.new) }
+    expect { @airport.land(Airplane.new) }.to raise_error 'Airport full'
   end
 
   describe '#land' do
+
+    it 'raises an error if the plane tries to land two times' do
+      airplane = Airplane.new
+      @airport.land(airplane)
+      expect { @airport.land(airplane) }.to raise_error 'The plane has already landed'
+    end
+
+    it 'raises an error if the plane tries to land and the airport is full' do
+      Airport::DEFAULT_CAPACITY.times { @airport.land(Airplane.new) }
+      expect { @airport.land(Airplane.new) }.to raise_error 'Airport full'
+    end
+
     it 'lets the plane land if the weather is good' do
       airplane = Airplane.new
-      allow(@weather).to receive(:stormy?) { false }
       expect(@airport.land(airplane)).to include(airplane)
     end
-    it 'prevents the plane from landing if the airport is full' do
-      airplane = Airplane.new
-      allow(@weather).to receive(:stormy?) { false }
-      expect(@airport.land(airplane)).to include(airplane)
+
+    it 'raises an error if the weather is stormy and the plane tries to land' do
+      allow(@weather).to receive(:stormy?) { true }
+      expect { @airport.land(Airplane.new) }.to raise_error 'The weather is stormy'
     end
+
   end
 
   describe '#take_off' do
+
+    it 'raises an error if the plane tries to take off but it hasnt landed yet' do
+      airplane1 = Airplane.new
+      airplane2 = Airplane.new
+      @airport.land(airplane1)
+      expect { @airport.take_off(airplane2) }.to raise_error 'The plane hasnt landed yet'
+    end
+
+    it 'raises an error if the plane tries to take off but there are no planes in the airport' do
+      expect { @airport.take_off(Airplane.new) }.to raise_error 'The plane hasnt landed yet'
+    end
+
+    it 'raises an error if the weather is stormy and the plane tries to take_off' do
+      airplane = Airplane.new
+      @airport.land(airplane)
+      allow(@weather).to receive(:stormy?) { true }
+      expect { @airport.take_off(airplane) }.to raise_error 'The weather is stormy'
+    end
+
     it 'lets the plane take off if the weather is good' do
       airplane = Airplane.new
-      allow(@weather).to receive(:stormy?) { false }
       @airport.land(airplane)
       expect(@airport.take_off(airplane)).to eq airplane
     end
+
+  end
+
+  it 'checks if the airplane is in the airport' do
+    airplane = Airplane.new
+    expect(@airport.in_the_airport?(airplane)).to eq FALSE
   end
 
 end
