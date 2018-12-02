fabd7ab24b73f39658c6fd8a0f7f35e7f88e64cb
Alina Goaga <goaga.alina@gmail.com>
2018-12-02 11:26:56 +0000

    Tested that the airplane can only land in good weather

--- lib/airport.rb
+++ lib/airport.rb
@@ -3,15 +3,15 @@ require_relative 'airplane'
 class Airport
 
   def land(airplane)
-    @airplane = airplane
+    @airplane = airplane unless stormy?
   end
 
   def take_off
-    @airplane unless stormy? == 1
+    @airplane unless stormy?
   end
 
   def stormy?
-    rand(0..1)
+    rand(0..1) == 1 ? true : false
   end
   # 0 is sunny , 1 is stormy
 
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -5,19 +5,25 @@ RSpec.describe Airport do
     @airport = Airport.new
   end
 
-  it 'returns the plane which has landed' do
-    airplane = Airplane.new
-    expect(@airport.land(airplane)).to eq airplane
+  it 'checks if the weather is stormy' do
+    expect(@airport.stormy?).to be(true).or be(false)
+  end
+
+  describe '#land' do
+    it 'lets the plane land if the weather is good' do
+      airplane = Airplane.new
+      allow(@airport).to receive(:stormy?) { false }
+      expect(@airport.land(airplane)).to eq airplane
+    end
   end
 
   describe '#take_off' do
     it 'lets the plane take off if the weather is good' do
       airplane = Airplane.new
-      allow(@airport).to receive(:stormy?) { 0 }
+      allow(@airport).to receive(:stormy?) { false }
       @airport.land(airplane)
       expect(@airport.take_off).to eq airplane
     end
-
   end
 
 end
