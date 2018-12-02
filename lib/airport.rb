37464ac6bded5b6f99def2f4279efc616b3b5e88
beatricecourage <bea_courage@hotmail.co.uk>
2018-12-02 19:20:33 +0000

    Weather

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,13 +1,39 @@
 require_relative 'plane'
 
+class Weather
+
+  def stormy?
+    if weather == :stormy
+      true
+    else
+      false
+    end
+  end
+
+end
+
 class Airport
-attr_reader :plane
+attr_reader :plane, :capacity, :weather
+
+  def initialize(capacity: 1, weather: Weather.new)
+     @planes = []
+     @weather = weather
+     @capacity = capacity
+  end
 
   def land(plane)
+  fail 'Bad weather' if weather.stormy?
+
+  @planes << plane
   end
 
 
-  def takeoff
+  def takeoff(plane)
+  fail 'Bad weather' if weather.stormy?
+  @planes.pop
   end
-  
+
+  # private
+  # attr_reader :weather
+
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -7,6 +7,18 @@ describe Airport do
     expect(airport).to respond_to(:land).with(1).argument
   end
 
-it { is_expected.to respond_to(:takeoff) }
+it { is_expected.to respond_to(:takeoff).with(1).argument }
+
+  it 'prevents takeoff when weather is stormy' do
+    allow(weather).to receive(:stormy?).and_return(true)
+    airport = Airport.new
+    expect{airport.takeoff}.to raise_error 'Cannot take off due to bad weather'
+  end
+
+  it 'prevents landing when weather is stormy' do
+    allow(weather).to receive(:stormy?).and_return(true)
+    airport = Airport.new
+    expect{airport.land}.to raise_error 'Cannot land due to bad weather'
+  end
 
 end
