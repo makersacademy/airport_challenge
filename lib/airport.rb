b252ffc4d86f9794c4a646a9edf3a7d3286eeefe
Jacques <jacques.laffy@gmail.com>
2018-12-02 20:00:11 +0000

    Functioning Airport

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,18 +1,37 @@
 class Airport
   attr_reader :planes, :weather
-  
-  def initialize (weather: Weather.new)
+  DEFAULT_CAPACITY = 150
+
+  def initialize (capacity = DEFAULT_CAPACITY)
     @planes = []
-    @weather = weather.stormy?
+    @weather = Weather.new.stormy?
+    @capacity = capacity
   end
 
   def land(plane)
+    fail "It is too stormy to land" if stormy?
+    fail "Airport is full" if full?
     @planes << plane
   end
 
   def take_off
-    fail "It is too stormy for take off" if weather == true
+    fail "It is too stormy for take off" if stormy?
+    fail "There are no planes" if empty?
     @planes.pop
+  end
+
+
+  private
+  def stormy?
+    weather == true
+  end
+
+  def full?
+    @planes.length >= @capacity
+  end
+
+  def empty?
+    @planes.length == 0
   end
 
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -5,39 +5,48 @@ describe Airport do
   let (:airport) {Airport.new}
   let (:plane) {Plane.new}
 
-  context '#land' do
+  context '#responds' do
 
     it 'responds to land' do
       expect(airport).to respond_to(:land)
     end
 
-    it 'adds the plane to the airport' do
-      airport.land(plane)
-      expect(airport.planes).to eql([plane])
-    end
-
-  end
-
-  context '#take_off' do
-
     it 'responds to take off' do
       expect(airport).to respond_to(:take_off)
     end
 
-    it 'removes the plane from the airport' do
-      allow(airport).to receive(:weather) {false}
+  end
+
+  context '#non-stormy weather' do
+    before { allow(airport).to receive(:weather) {false} }
+
+    it 'take off removes the plane from the airport' do
       airport.land(plane)
       airport.take_off
       expect(airport.planes).to eql([])
     end
 
+    it 'landing adds the plane to the airport' do
+      airport.land(plane)
+      expect(airport.planes).to eql([plane])
+    end
+
+    it 'cannot land if the airport is full' do
+      Airport::DEFAULT_CAPACITY.times {airport.land(plane)}
+      expect{airport.land(plane)}.to raise_error 'Airport is full'
+    end
+
   end
 
-  context '#take off in storm' do
+  context '#stormy weather' do
+    before { allow(airport).to receive(:weather) {true} }
 
     it 'cannot take off if weather is stormy' do
-      allow(airport).to receive(:weather) {true}
       expect{ airport.take_off }.to raise_error 'It is too stormy for take off'
+    end
+
+    it 'cannot land if weather is stormy' do
+      expect{ airport.land(plane) }.to raise_error 'It is too stormy to land'
     end
 
   end
