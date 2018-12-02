55146f2dcb11a6d4701c0253e942911ed97dad2b
Jethro Whaley <jethro.whaley@gmail.com>
2018-12-02 12:41:43 +0000

    airport.land stores a plane in the hangar

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,7 +1,17 @@
 class Airport
 
-  def land(plane)
+  attr_reader :plane, :hangar
 
+  def initialize
+    @hangar = []
+  end
+
+  def land(plane)
+    @hangar << plane
+  end
+
+  def take_off(plane)
+    @plane = plane
   end
 
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,13 +2,22 @@ require 'airport'
 
 describe '#airport' do
   let(:airport) { Airport.new }
+  let(:plane) { Plane.new }
 
   describe "#land" do
-    it "interacts with #land method and take 1 argument" do
+    it "accepts with #land method and takes 1 argument" do
       expect(airport).to respond_to(:land).with(1).argument
     end
-    it "#" do
 
+    it "stores the landed plane in the hangar array" do
+      airport.land(plane)
+      expect(airport.hangar).to include(plane)
+    end
+  end
+
+  describe "#take_off" do
+    it "interacts with #land method and take 1 argument" do
+      expect(airport).to respond_to(:take_off).with(1).argument
     end
   end
 end
