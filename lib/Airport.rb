a0593be19b72b2f7f80dd14c96ca3e29ca580951
Tom Russell <TR115251@hotmail.co.uk>
2018-12-02 23:21:00 +0000

    all user stories satisfied

--- lib/Airport.rb
+++ lib/Airport.rb
@@ -2,26 +2,31 @@ require_relative "Aeroplane.rb"
 
 class Airport
 
-  attr_accessor :planes, :default_capacity
+  attr_accessor :planes, :capacity
+  DEFAULT_CAPACITY = 25
 
-  def initialize(weather = rand(100))
+  def initialize(weather = rand(100), airport_capacity = DEFAULT_CAPACITY)
     @planes = []
     @weather = (weather > 75 ? "stormy" : "clear")
-    @default_capacity = 5
+    @capacity = airport_capacity
   end
 
   def takeoff(plane)
-    fail "#{plane} Cannot take off" if weather == false
+    fail "#{plane} Cannot take off" if weather? || full?
     @planes.delete(plane)
   end
 
   def land(plane)
-    fail "#{plane} Cannot land" if weather == false
+    fail "#{plane} Cannot land" if weather? || full?
     @planes << plane
   end
 
-  def weather
-    @weather == "stormy" || @planes.count > default_capacity ? false : true
+  def weather?
+    @weather == "stormy"
+  end
+
+  def full?
+    planes.count >= @capacity
   end
 
 end
--- spec/Airport_spec.rb
+++ spec/Airport_spec.rb
@@ -7,27 +7,19 @@ describe Airport do
 
   context '#land' do
 
-    it 'responds to land' do
-      expect(airport).to respond_to(:land)
-    end
-
-    it 'lands the plane in the airport' do
-      airport.land(plane)
-      expect(airport.planes).to include(plane)
-    end
-
-  end
-
-  context '#takeoff' do
-
-    it 'responds to take off' do
+    it 'responds to takeoff' do
       expect(airport).to respond_to(:takeoff)
     end
 
-    it 'confrims the plane is no longer in the airport' do
+    it 'Plane no longer in the airport after takeoff' do
       airport.land(plane)
       airport.takeoff(plane)
       expect(airport.planes).not_to include(plane)
+    end
+
+    it 'cannot land if the airport is full' do
+      subject.capacity.times { airport.land(plane) }
+      expect { airport.land(plane) }.to raise_error "#{plane} Cannot land"
     end
 
   end
@@ -36,6 +28,9 @@ describe Airport do
     let(:airport) { Airport.new(90) }
     it 'cannot take off if weather is stormy' do
       expect { airport.takeoff(plane) }.to raise_error("#{plane} Cannot take off")
+    end
+    it 'cannot land if weather is stormy' do
+      expect { airport.land(plane) }.to raise_error("#{plane} Cannot land")
     end
 
   end
