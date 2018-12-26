bc4d43b534230a720b17246bf46aff1d8ca0942d
SBriscoe2 <shabris@tiscali.co.uk>
2018-12-26 15:07:45 +0000

    Fixed indentation issues

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -5,13 +5,9 @@ describe Plane do
 
   describe '#take_off' do
     it { is_expected.to respond_to :take_off }
-  end
 
-    # context '#flying_plane' do
-      it 'raises an error if plane already flying' do
-        # airport.land(plane)
-        # flying_plane = airport.take_off(plane)
-        expect { plane.take_off }.to raise_error "Cannot take off: Plane already in air"
-      end
-    # end
+    it 'raises an error if plane already flying' do
+      expect { plane.take_off }.to raise_error "Cannot take off: Plane already in air"
+    end
+  end
 end
