4da178a6e41c24f521b18b5b7e98cd9631328492
Asad Khan <asad_khan47@hotmail.co.uk>
2019-01-02 19:35:18 +0000

    Overhaul of airport challenge using a feature test driven approach which is mimiced at the unit test level

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,42 +1,4 @@
-require_relative "./plane.rb"
-
 class Airport
-
-  attr_reader :hangar
-  attr_reader :stormy
-
-  DEFAULT_CAPACITY = 5
-
-  def initialize(capacity = DEFAULT_CAPACITY)
-    @hangar = []
-    @stormy = weather
-    @capacity = capacity
-  end
-
   def land(plane)
-    fail "Storm warning: Unable to land" if @stormy
-    fail "There are already max planes in hangar" if full?
-    @hangar << plane
   end
-
-  def takeoff(plane)
-    fail "Storm warning: Unable to takeoff" if @stormy
-    fail "There are already max planes in hangar" if empty?
-    @hangar.pop
-  end
-
-  private
-
-  def empty?
-    @hangar.empty?
-  end
-
-  def full?
-    @hangar.count >= @capacity
-  end
-
-  def weather
-    rand(1..4) < 2 ? true : false
-  end
-
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,58 +1,9 @@
-require "airport"
-require "weather"
+require 'airport'
 
 describe Airport do
+  subject(:airport) { described_class.new }
 
-  let(:airport) {Airport.new}
-
-  describe "#Checks initializations" do
-
-    it "Creates hangar" do
-      expect(airport.hangar).to eq []
-    end
-
+  it 'instructs a plane to land' do
+    expect(airport).to respond_to(:land).with(1).argument
   end
-
-  describe "#Planes can land" do
-
-    it { is_expected.to respond_to(:land) }
-
-    it "Can land a plane into the airport" do
-      plane = Plane.new
-      expect(subject.land(plane)).to eq [plane]
-    end
-
-    it "Prevents planes from landing due to weather" do
-      subject.stormy = true
-      expect{ subject.land(plane) }.to raise_error "Storm warning: Unable to land"
-    end
-
-  end
-
-  describe "#Planes can take off" do
-
-    it { is_expected.to respond_to(:takeoff) }
-
-    it "Allows planes to take off" do
-      plane = Plane.new
-      subject.land(plane)
-      expect(subject.takeoff(plane)).to eq plane
-    end
-
-    it "Prevents planes from takeing off due to weather" do
-      subject.stormy = true
-      expect{ subject.takeoff(plane) }.to raise_error "Storm warning: Unable to takeofff"
-    end
-
-  end
-
-  describe "#Fail for airport above capacity" do
-
-    it "Will fail when capacity exceeded" do
-      Airport::DEFAULT_CAPACITY.times{ subject.land Plane.new }
-      expect{ subject.land Plane.new }.to raise_error "There are already max planes in hangar"
-    end
-
-  end
-
 end
