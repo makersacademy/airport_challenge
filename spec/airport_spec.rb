c5ebc310627b05c7ce9d902989483470285e7287
Nikhil Vijayan <nkhilv@gmail.com>
2019-01-10 20:11:17 +0000

    Added airport and airport spec file

---
+++ spec/airport_spec.rb
@@ -0,0 +1,11 @@
+require 'airport'
+
+RSpec.describe Airport do
+
+  # As an air traffic controller 
+  # So I can get passengers to a destination 
+  # I want to instruct a plane to land at an airport
+
+  
+
+end
