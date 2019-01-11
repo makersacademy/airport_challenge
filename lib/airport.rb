32bc93c7721c71e949e23b8393bb6666606d1336
isabelcooper <isabel@triptease.com>
2019-01-11 10:57:11 +0000

    weather object returning random weather from array

--- lib/airport.rb
+++ lib/airport.rb
@@ -6,15 +6,20 @@ class Airport
     @planes_in_airport = []
   end
 
-  def instruct_to_land(plane)
-    true
-  end
+  # def instruct_to_land(plane)
+  #   true
+  # end
 
   def land_plane(plane)
     @planes_in_airport << (plane)
   end
 
-  def instruct_to_take_off(plane)
-    true
+  # def instruct_to_take_off(plane)
+  #   true
+  # end
+
+  def take_off(plane)
+    @planes_in_airport.pop
+    "#{plane} has now left airport"
   end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -8,23 +8,30 @@ describe Airport do
     @plane = Plane.new
   end
 
-  it 'should instruct a plane to land at an aiport' do
-    expect(@airport.instruct_to_land(@plane)).to eq true
-  end
-
-  it 'should instruct a plane to take off at an airport' do
-    expect(@airport.instruct_to_take_off(@plane)).to eq true
-  end
+  # it 'should instruct a plane to land at an aiport' do
+  #   expect(@airport.instruct_to_land(@plane)).to eq true
+  # end
+  #
+  # it 'should instruct a plane to take off at an airport' do
+  #   expect(@airport.instruct_to_take_off(@plane)).to eq true
+  # end
 
   it 'should add a plane to the array when it lands' do
     @airport.land_plane(@plane)
     expect(@airport.planes_in_airport).to eq([@plane])
   end
 
-  # it 'should contain an array of the planes in the airport' do
-  #   planes << @plane
-  #   expect(@airport.planes_in_airport).to eq([@plane])
-  # end
+  it 'should contain an array of the planes in the airport' do
+    @airport.land_plane(@plane)
+    expect(@airport.planes_in_airport).to eq([@plane])
+  end
+
+  it 'should remove a plane from array when instructed to take off and confirm this has been done' do
+    @airport.land_plane(@plane)
+    @airport.take_off(@plane)
+    expect( @airport.take_off(@plane)).to eq "#{@plane} has now left airport"
+  end
+
 
 
 end
