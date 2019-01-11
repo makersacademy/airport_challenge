bba49f04556b1d5bc1275e789b5ccc343b9daa4e
habakkukyong <habakkukyong@gmail.com>
2019-01-11 11:19:41 +0000

    Refactor giving land method to plane instead passing test

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,7 +1,5 @@
 require "airport"
 
 describe Airport do
-  it "Has a method to allow user to land plane" do
-    expect(subject).to respond_to(:land).with(1).argument
-  end
+  
 end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -1,5 +1,7 @@
 require "plane"
 
 describe Plane do
-  
+  it "Has a method to allow user to land plane at airport" do
+    expect(subject).to respond_to(:land).with(1).argument
+  end
 end
