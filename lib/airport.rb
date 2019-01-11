99c2762609a9a312a6a877c7fb2f06e8989d32b0
isabelcooper <isabel@triptease.com>
2019-01-11 10:36:15 +0000

    tracking planes in airport and adding to array when new planes land

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,5 +1,20 @@
 class Airport
-  def land(plane)
+
+  attr_reader :planes_in_airport
+
+  def initialize
+    @planes_in_airport = []
+  end
+
+  def instruct_to_land(plane)
+    true
+  end
+
+  def land_plane(plane)
+    @planes_in_airport << (plane)
+  end
+
+  def instruct_to_take_off(plane)
     true
   end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,9 +2,29 @@ require 'airport'
 require 'plane'
 
 describe Airport do
-  it 'should instruct a plane to land at an aiport' do
-    airport = Airport.new
-    plane = Plane.new
-    expect(airport.land(plane)).to eq true
+
+  before(:each) do
+    @airport = Airport.new
+    @plane = Plane.new
   end
+
+  it 'should instruct a plane to land at an aiport' do
+    expect(@airport.instruct_to_land(@plane)).to eq true
+  end
+
+  it 'should instruct a plane to take off at an airport' do
+    expect(@airport.instruct_to_take_off(@plane)).to eq true
+  end
+
+  it 'should add a plane to the array when it lands' do
+    @airport.land_plane(@plane)
+    expect(@airport.planes_in_airport).to eq([@plane])
+  end
+
+  # it 'should contain an array of the planes in the airport' do
+  #   planes << @plane
+  #   expect(@airport.planes_in_airport).to eq([@plane])
+  # end
+
+
 end
