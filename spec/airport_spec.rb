b1a74e52b38035bc1695b75302284ca8f5a1e9ea
Nikhil Vijayan <nkhilv@gmail.com>
2019-01-11 21:59:09 +0000

    Modified spec file formatting

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -20,9 +20,6 @@ RSpec.describe Airport do
   # I want to instruct a plane to take off from an airport and confirm that it is no longer in the airport
   it { is_expected.to respond_to(:take_off) }
   it { is_expected.to respond_to(:take_off).with(1).argument }
-  # it "can instruct a plane to take off" do
-  #   expect(subject.take_off(Plane.new)).to eq(true)
-  # end
 
   it "can instruct a plane to take off and confirm that the plane has left the airport" do
     plane = Plane.new
@@ -34,6 +31,7 @@ RSpec.describe Airport do
   # As an air traffic controller 
   # To ensure safety 
   # I want to prevent takeoff when weather is stormy 
+  
   # it "checks if weather is stormy" do
   #   expect(subject.stormy?).to eq(false)
   # end
