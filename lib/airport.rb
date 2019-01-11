e3b3d358654dbc8c30f13526a58fe9a84c0c97b5
Katie Cornish <katielcornish@gmail.com>
2019-01-11 14:14:08 +0000

    can now override the default airport capacity when a new airport is initialized

--- lib/airport.rb
+++ lib/airport.rb
@@ -3,26 +3,32 @@ require_relative 'weather'
 
 class Airport
 
-  attr_reader :planes
+  attr_reader :planes, :capacity
 
-  def initialize
+  def initialize(capacity = 10)
     @planes = []
+    @capacity = capacity
   end
 
-  def land_plane(plane)
+  def land_plane(plane, weather = Weather.new)
+    airport_full?
+    check_weather(weather)
     @planes << plane
     self
   end
 
   def take_off_plane(plane, weather = Weather.new)
-    fail "can't take off due to storms" if check_weather(weather) == "stormy"
-
+    check_weather(weather)
     @planes.delete(plane)
     self
   end
 
+  def airport_full?
+    fail "can't land as airport is full" if @planes.length == @capacity
+  end
+
   def check_weather(weather)
-    weather.condition
+    fail "can't do that due to storms" if weather.condition == "stormy"
   end
 
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -4,23 +4,34 @@ describe Airport do
 
   let(:plane) { double :plane }
   let(:stormy_weather) { double(:condition => "stormy") }
+  let(:clear_weather) { double(:condition => "clear") }
 
   it 'can add a plane to an array of planes in the airport by calling the "land_plane" method' do
-    expect(subject.land_plane(plane).planes).to eq [plane]
+    expect(subject.land_plane(plane, clear_weather).planes).to eq [plane]
   end
 
   it 'removes a plane from the airport when the "take_off_plane" method is called' do
-    airport = subject.land_plane(plane)
-    expect(airport.take_off_plane(plane).planes.include?(plane)).to eq false
+    airport = subject.land_plane(plane, clear_weather)
+    expect(airport.take_off_plane(plane, clear_weather).planes.include?(plane)).to eq false
   end
 
   it "won't allow a plane to take off if the weather is stormy" do
     airport = subject.land_plane(plane)
-    expect { airport.take_off_plane(plane, stormy_weather) }.to raise_error "can't take off due to storms"
+    expect { airport.take_off_plane(plane, stormy_weather) }.to raise_error "can't do that due to storms"
   end
 
-  it "returns the weather condition when 'check_weather' is called" do
-    expect(subject.check_weather(stormy_weather)).to eq "stormy"
+  it "won't allow a plane to land if the weather is stormy" do
+    expect { subject.land_plane(plane, stormy_weather) }.to raise_error "can't do that due to storms"
+  end
+
+  it "won't allow a plane to land if the airport is full" do
+    expect { subject.capacity.times { subject.land_plane(plane, clear_weather) } }.to_not raise_error
+    expect { subject.land_plane(plane, clear_weather) }.to raise_error "can't land as airport is full"
+  end
+
+  it "allows a user to amend the default airport capacity when initializing a new airport" do
+    airport = Airport.new(20)
+    expect(airport.capacity).to eq 20
   end
 
 end
