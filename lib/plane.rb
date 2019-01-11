5a7048f4deb381bea6aa20636d1175415cf13bba
Alex Hutchings <alexhutchings4@gmail.com>
2019-01-11 10:47:15 +0000

    Airport class is now able to store Planes in an array, so the land method now works as expected

--- lib/plane.rb
+++ lib/plane.rb
@@ -2,5 +2,6 @@ require_relative 'airport'
 
 class Plane
   def land(airport)
+    airport.planes << self
   end
 end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -2,8 +2,18 @@ require 'plane'
 
 describe Plane do
 
+  before(:each) do
+    @airport = Airport.new
+    @plane = Plane.new
+  end
+
   describe '#land' do
     it { is_expected.to respond_to(:land).with(1).argument }
+
+    it 'should exist in an airport after landing' do
+      @plane.land(@airport)
+      expect(@airport.planes).to include(@plane)
+    end
   end
 
 end
