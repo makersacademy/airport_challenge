2d8da0ed4f859aec81dbd16db42bc6114ea90580
your name <saule.guzyte@gmail.com>
2019-01-11 11:50:31 +0000

    Message to confirm that the plane has left added

--- lib/airport.rb
+++ lib/airport.rb
@@ -2,10 +2,11 @@ require_relative 'plane'
 
 class Airport
 
-  attr_reader :planes_at_airport
+  attr_reader :planes_at_airport, :plane_that_left
 
   def initialize
     @planes_at_airport = []
+    @plane_that_left = 0
   end
 
   def land(plane)
@@ -13,7 +14,12 @@ class Airport
   end
 
   def take_off(plane)
-    @planes_at_airport.delete(plane)
+    @plane_that_left = @planes_at_airport.delete(plane)
+
+    if @planes_at_airport.include?(@plane_that_left) == false
+      return "Plane #{@plane_that_left} has left"
+    end
   end
+
 
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -16,12 +16,30 @@ describe Airport do
     describe '#take_off' do
       it {is_expected.to respond_to :take_off}
 
-      it 'should allow plane to take off and check that it is gone' do
+      it 'should allow plane to take off' do
         plane=Plane.new
         subject.land(plane)
         subject.take_off(plane)
         expect(subject.planes_at_airport).not_to include(plane)
       end
+
+      it 'when the plane leaves it should confirm it has left the airport' do
+        plane=Plane.new
+        subject.land(plane)
+        expect(subject.take_off(plane)).to eq("Plane #{plane} has left")
+      end
     end
 
-end
+  end
+
+    # describe '#confirm_plane_left' do
+    #   it {is_expected.to respond_to :confirm_plane_left}
+    #
+    #   it 'should confirm that the plane has left the airport' do
+    #   plane=Plane.new
+    #   subject.land(plane)
+    #   subject.take_off(plane)
+    #   subject.confirm_plane_left(plane)
+    #   expect(subject.planes_at_airport).not_to include(plane)
+    #   end
+    # end
