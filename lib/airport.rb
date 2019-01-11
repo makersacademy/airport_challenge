593384908824336466e452c62ce35d3e2f1988f7
Szilvi Horvath <szil.horvath@gmail.com>
2019-01-11 15:49:42 +0000

    airport has a default capacity that can be overriden

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,11 +1,21 @@
 require_relative 'plane'
 
 class Airport
-  attr_reader :plane
+  attr_reader :plane, :planes, :capacity
+  DEFAULT_CAPACITY = 50
+
+  def initialize(capacity=DEFAULT_CAPACITY)
+    @planes = []
+    @capacity = capacity
+  end
+
+  def full?
+    planes.count >= @capacity
+  end
 
   def land(plane)
-    fail 'Airport is full' if @plane
-    @plane = plane
+    fail 'Airport is full' if full?
+    @planes << plane
   end
 
   def take_off(plane)
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -5,21 +5,28 @@ describe Airport do
   it { is_expected.to respond_to(:plane) }
   it { is_expected.to respond_to(:take_off).with(1).argument}
 
+  it 'has a default capacity' do
+    expect(subject.capacity).to eq Airport::DEFAULT_CAPACITY
+  end
+
   describe '#land' do
     it 'lands plane at airport' do
       plane = Plane.new
-      expect(subject.land(plane)).to eq plane
+      subject.land(plane)
+      expect(subject.planes[0]).to eq plane
     end
 
     it 'raises an error when airport is full' do
-      subject.land(Plane.new)
+      Airport::DEFAULT_CAPACITY.times do
+        subject.land Plane.new
+      end
       expect { subject.land Plane.new }.to raise_error 'Airport is full'
     end
 
     it 'returns planes that landed' do
       plane = Plane.new
       subject.land(plane)
-      expect(subject.plane).to eq plane
+      expect(subject.planes[0]).to eq plane
     end
   end
 
