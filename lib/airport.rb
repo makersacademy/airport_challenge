f53611282da9b3c107fd6cef4fe47c4e9470e9d1
Luca Fruzza <lfruzza@hotmail.co.uk>
2019-01-12 15:44:30 +0000

    confirms taken off airplane not in airport RGR.

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,7 +1,13 @@
 class Airport
+  attr_reader :planes
+
+  def initialize
+    @planes = []
+  end
+
   def land(plane)
   end
-  
+
   def takeoff(plane)
   end
 end
--- spec/air_traffic_controller_spec.rb
+++ spec/air_traffic_controller_spec.rb
@@ -1,9 +1,13 @@
 require 'airport'
 
 describe Airport do
+  it { is_expected.to respond_to(:planes) }
   it "can instruct a PLANE to LAND" do
     expect(subject).to respond_to(:land).with(1).arguments
     expect { subject.land(Plane.new) }.to_not raise_error
+  end
+  it "stores LANDed planes" do
+    
   end
   it "can instruct a PLANE to TAKEOFF" do
     expect(subject).to respond_to(:takeoff).with(1).arguments
@@ -11,7 +15,8 @@ describe Airport do
   end
   it "can confirm taken off plane no longer exists in airport" do
     pointer = Plane.new
-    subject.takeoff(myplane)
+    subject.land(pointer)
+    subject.takeoff(pointer)
     expect(subject.planes.include?(pointer)).to eq(false)
   end
 end
