dc57ba3b002a2ae151d7502ee2c56836c79b7e15
Ibrahim Butt <ibrahimbutt@me.com>
2019-01-12 09:13:53 +0000

    Add landing functionality

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,7 +1,13 @@
 require 'airport'
+require 'plane'
 
 RSpec.describe Airport do
   describe '#land' do
     it { should respond_to :land }
+
+    it 'should land a plane' do
+      plane = Plane.new
+      expect(subject.land(plane)).to eq plane
+    end
   end
 end
