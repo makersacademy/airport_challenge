3ac0723d48680faf50fcd6d13a86c68f6c104fb0
Vaith Schmitz <vaithschmitz@vaiths-mbp.home>
2019-01-12 17:36:52 +0000

    implemented capacity

--- lib/airport.rb
+++ lib/airport.rb
@@ -2,7 +2,11 @@ class Plane
 end
 
 class Airport
-	def initialize()
+	attr_reader :weather
+  DEFAULT_CAPACITY = 100
+
+	def initialize(capacity = DEFAULT_CAPACITY)
+		@capacity = capacity
 	  @hangar = []
 	end
 
@@ -11,8 +15,18 @@ class Airport
   end
 
   def takeoff(plane)
+  	fail "This plane is not in this airport" if !@hangar.include?(plane)
   	# using delete instead of plane to get the actual plane called for instead of the last plane landed
   	@hangar.delete(plane)
+  end
+  
+  # ~10% chance of rain
+  def stormy?
+    case rand(10)
+    	when 0..8 then false
+    else 
+      true
+    end
   end
 
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -17,9 +17,19 @@ describe Airport do
     it "plane is in hangar when landed" do 
     	expect(@empty_hangar.instance_variable_get(:@hangar)).to eq [@plane]
     end
+    # put full airport capacity of planes in hangar
+    before(:all) do 
+  		@full_hangar = Airport.new
+  		# THIS IS BROKEN. FIGURE OUT HOW TO PUSH TO ARR IN RSPEC!!!!
+  		# @full_hangar.instance_variable_get(:@capacity).times{|x| :@hangar << Plane.new}
+  	end
+  	it "reject plane if airport is at capacity" do 
+    	# expect(@full_hangar.instance_variable_get(:@hangar)).to eq [@plane]
+    	p @full_hangar
+    end
   end
  
- context "check takeoff" do
+  context "check takeoff" do
  	  # creating test hangar with plane in it, then call takeoff to see what happens
   	before(:all) do 
   		@test_hangar = Airport.new
@@ -32,4 +42,17 @@ describe Airport do
     end
   end
 
+  context "default capacity" do
+  	before(:all) do
+  		@full_airport = Airport.new(30)
+  		@default_airport = Airport.new
+  	end
+  	it "check that capacity can be set to maintainer input" do
+  	  expect(@full_airport.instance_variable_get(:@capacity)).to eq 30
+  	end
+  	it "check that capacity has default value when no input given" do
+  	  expect(@default_airport.instance_variable_get(:@capacity)).to eq Airport::DEFAULT_CAPACITY
+    end
+  end
 end
+
