905234890fbd9d5915b49e655326a66a8a44fdf3
Erin Stevenson <estevenson_94@live.com>
2019-01-12 12:03:17 +0000

    Land method can land a plane

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,7 +1,15 @@
+require_relative 'plane'
 
 class Airport
  
+attr_reader :plane
+
+  def initialize
+    @plane
+  end
+  
   def land(plane)
+    @plane = plane
   end
 
 
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -6,4 +6,10 @@ describe Airport do
       expect(Airport.new).to respond_to(:land).with(1).argument
     end
 
+    it 'land a plane' do
+      plane = Plane.new
+      airport = Airport.new
+      expect(airport.land(plane)).to eq plane
+    end
+
 end
