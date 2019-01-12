14cbd208b9eee88b201e78c64d8d19c46486853f
Colin Hulme <17675453+ch359@users.noreply.github.com>
2019-01-12 19:20:21 +0000

    Remove legacy comments in Plane

--- spec/units/plane_spec.rb
+++ spec/units/plane_spec.rb
@@ -2,14 +2,6 @@ require 'plane'
 require 'rspec'
 
 describe Plane do
-  # context 'plane can respond to ATC instructions' do
-  #   it 'should respond to landing requests' do
-  #     expect(subject).to respond_to(:land).with(1)
-  #   end
-  #   it 'should respond to takeoff requests' do
-  #     expect(subject).to respond_to(:takeoff)
-  #   end
-  # end
 
   before(:each) do
     @plane = Plane.new
