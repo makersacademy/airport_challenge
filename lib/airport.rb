45a7419a8583a06f6f47cf889d67c87cbab03d0a
jesslns <jesslns@yahoo.com.hk>
2019-01-12 12:43:32 +0000

    Include weather method and a stormy class to control take off and landing

--- lib/airport.rb
+++ lib/airport.rb
@@ -2,20 +2,31 @@ require_relative 'plane'
 
 class Airport
 
-attr_reader :plane
+attr_reader :planes, :weather
 
 def initialize
   @planes = []
+  @weather
+end
 
-def land (plane)
+def land(plane)
+  fail "Landing is forbidden!" if @weather
   @planes << plane
   return plane
 end
 
-def take_off (plane)
+def take_off(plane)
+  fail "Take off is forbidden!" if @weather
+  @weather
   @planes - [plane]
-  return @planes.include?(plane) 
-  end
+  return @planes.include?(plane)
 end
 
+def weather
+  @weather = Stormy.new
+end
+
+end
+
+class Stormy
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,18 +2,34 @@ require 'airport'
 
 describe Airport do
 
-it { is_expected.to respond_to :land }
-it { is_expected.to respond_to :take_off }
+it { is_expected.to respond_to(:land).with(1).argument }
+it { is_expected.to respond_to(:take_off).with(1).argument }
+it { is_expected.to respond_to :weather }
 
+describe '#land' do
 it "lands at an airprot" do
   plane = Plane.new
   expect(subject.land(plane)).to eq plane
 end
 
+it "cannot land if stormy" do
+  plane = Plane.new
+  stormy = subject.weather
+  expect{ subject.land(plane) }.to raise_error "Landing is forbidden!"
+end
+end
+
+describe '#take_off' do
 it "plane no loner in the airport" do
   plane = Plane.new
   expect(subject.take_off(plane)).to eq false
 end
+
+it "cannot take off when stormy" do
+  plane = Plane.new
+  stormy = subject.weather
+  expect{ subject.take_off(plane) }.to raise_error "Take off is forbidden!"
+end
 end
 
-# test plane is an array to build an array
+end
