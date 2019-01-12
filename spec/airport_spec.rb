d2811f2c88901ab2a079bad7a8b4b767ee300737
KrzysztofBalejko <chrisbalejko86@gmail.com>
2019-01-12 12:11:17 +0000

    Test: Airport allows plane to land

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,7 +2,11 @@ require 'airport'
 
 RSpec.describe Airport do
 
-
+  it 'Airport allows plane to land' do
+    gatwick = Airport.new
+    plane = Plane.new
+    expect(gatwick.land(plane))
+  end
 
 
 
