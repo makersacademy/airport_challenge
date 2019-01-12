919d62a35ffc81043115410a93fe7abf6dceb13b
Sherif Shendidy <shendidy@Sherifs-MacBook-Pro.local>
2019-01-12 01:17:24 +0000

    Planes can't take off or land in a stormy weather

--- lib/air_traffic_control.rb
+++ lib/air_traffic_control.rb
@@ -1,18 +1,21 @@
 require 'plane'
 
 class AirTrafficControl
-  attr_accessor :planes
+  attr_accessor :planes, :stormy
   @planes = Array.new
 
-  def initialize(planes = [])
+  def initialize(planes = [], stormy = false)
     @planes = planes
+    @stormy = stormy
   end
 
-  def land(plane)
-    @planes << (plane)
+  def land(plane, stormy = @stormy)
+    @planes << (plane) unless stormy
+    raise "Stormy weather, can't land now!" if stormy
   end
 
-  def take_off(plane)
-    @planes.delete(plane)
+  def take_off(plane, stormy = @stormy)
+    @planes.delete(plane) unless stormy
+    raise "Stormy weather, can't take off now!" if stormy
   end
 end
--- spec/air_traffic_control_spec.rb
+++ spec/air_traffic_control_spec.rb
@@ -11,12 +11,19 @@ describe AirTrafficControl do
   end
 
   context "Checking if land/take_off adds/removes a plane from airport" do
+    plane0 = Plane.new("plane0")
+    airport = AirTrafficControl.new
+    airport.land(plane0.plane_sign)
     it "Check if plane in airport or not" do
-      plane0 = Plane.new("plane0")
-      airport = AirTrafficControl.new
-      airport.land(plane0.plane_sign)
       airport.take_off(plane0.plane_sign)
       expect(airport.planes.include?(plane0.plane_sign)).to eq false
+    end
+    it "Check if plane will take off in a stormy weather" do
+      expect { airport.take_off(plane0.plane_sign, true) }.to raise_error("Stormy weather, can't take off now!")
+    end
+    it "Check if plane will land in a stormy weather" do
+      plane1 = Plane.new("plane1")
+      expect { airport.land(plane1.plane_sign, true) }.to raise_error("Stormy weather, can't land now!")
     end
   end
 end
