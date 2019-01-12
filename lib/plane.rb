8e07b28ac799613493e907b206e1f285bac9d01c
Debora <debora.piu92@gmail.com>
2019-01-12 23:45:58 +0000

    Plane initialised with landed status and methods to change it.

--- lib/plane.rb
+++ lib/plane.rb
@@ -1,3 +1,17 @@
 class Plane
 
+  attr_reader :landed
+
+  def initialize(landed = true)
+    @landed = landed
+  end
+
+  def lands
+    @landed = true
+  end
+
+  def takes_off
+    @landed = false
+  end
+
 end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -2,4 +2,19 @@ require 'plane'
 
 RSpec.describe Plane do
 
+  it 'should respond to landed with true or false' do
+    expect([true, false]).to include plane_test.landed
+  end
+
+  it 'should return true for landed plane' do
+    subject.takes_off
+    subject.lands
+    expect(subject.landed).to be_truthy
+  end
+
+  it 'should return false for landed plane' do
+    subject.takes_off
+    expect(subject.landed).to be_falsey
+  end
+
 end
