362b89b89781813bb55993495906302cdb03dde9
William Dunk <williamdunk@gmail.com>
2019-01-12 15:45:41 +0000

    refactored tests, tried to test random weather

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,6 +1,11 @@
 require 'airport'
 
 describe Airport do
+
+  before :each do
+    allow(subject).to receive(:stormy?) { false }
+  end
+
   # As an air traffic controller
   # So I can get passengers to a destination
   # I want to instruct a plane to land at an airport
@@ -12,17 +17,14 @@ describe Airport do
   # So I can get passengers on the way to their destination
   # I want to instruct a plane to take off from an airport and confirm that it is no longer in the airport
   it "can instruct plane to take off" do
-    allow(subject).to receive(:stormy?) { false }
     expect(subject).to respond_to(:take_off)
   end
 
   it "can store plane" do
-    allow(subject).to receive(:stormy?) { false }
     expect(subject.land(Plane.new)).to eq subject.planes
   end
 
   it "can confirm plane has left" do
-    allow(subject).to receive(:stormy?) { false }
     subject.land(Plane.new)
     expect(subject.take_off).to eq []
   end
@@ -35,7 +37,6 @@ describe Airport do
   end
 
   it "prevents take off when stormy" do
-    allow(subject).to receive(:stormy?) { false }
     subject.land(Plane.new)
     allow(subject).to receive(:stormy?) { true }
     expect { subject.take_off } .to raise_error "stormy can't take off"
@@ -50,10 +51,8 @@ describe Airport do
   end
 
   #test implementation of random weather
-  # it "#stormy is false most of the time" do
-  #   10.times {expect(subject.stormy?).not_to eql(subject.stormy?)}
-  # end
-
-
+  it "#stormy is false most of the time" do
+    10.times {expect(subject.stormy?).to eql(subject.stormy?)}
+  end
 
 end
