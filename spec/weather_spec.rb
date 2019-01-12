54c3e6789f80a0a04dcc8899450d7aff301595cb
KrzysztofBalejko <chrisbalejko86@gmail.com>
2019-01-12 15:45:09 +0000

    Test: forecasting weather

--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -2,5 +2,8 @@ require 'weather'
 
 RSpec.describe Weather do
 
-
+  it 'Stores forcasted weather in variable' do
+    weather = Weather.new
+    expect(weather.forecast).to be_a_kind_of(String)
+  end
 end
