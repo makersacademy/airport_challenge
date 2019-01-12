2a76011c9b1e2e4e3c483593ca4c5910d9689750
Luca Fruzza <lfruzza@hotmail.co.uk>
2019-01-12 14:51:49 +0000

    taken off plane no longer exists in airport feature test implemented

--- spec/air_traffic_controller_spec.rb
+++ spec/air_traffic_controller_spec.rb
@@ -7,5 +7,11 @@ describe Airport do
   end
   it "can instruct a PLANE to TAKEOFF" do
     expect(subject).to respond_to(:takeoff).with(1).arguments
+    expect { subject.takeoff(Plane.new) }.to_not raise_error
+  end
+  it "can confirm taken off plane no longer exists in airport" do
+    pointer = Plane.new
+    subject.takeoff(myplane)
+    expect(subject.planes.include?(pointer)).to eq(false)
   end
 end
