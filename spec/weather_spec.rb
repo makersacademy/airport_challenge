7bada98b5d94ceac86dadad2202edb9ec7aaedab
Ibrahim Butt <ibrahimbutt@me.com>
2019-01-12 10:51:04 +0000

    Fix respond test for Weather.current

--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -1,7 +1,7 @@
 require 'weather'
 
 RSpec.describe Weather do
-  it { should respond_to :forecast }
+  it { should respond_to :current }
 
   it 'should randomly return weather as stormy or safe' do
     expect(Weather.current).to eq 'stormy' or eq 'safe'
