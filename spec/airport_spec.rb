5e897d80e0758f5601d474d5e058695ff2fdc596
Aiden Gleave <aidengleave@gmail.com>
2019-01-12 22:08:45 +0000

    Commit 4: Passed - Plane lands at airport.

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,6 +1,11 @@
 require 'airport'
+require 'plane'
 RSpec.describe Airport do
   describe '#land' do
     it { should respond_to :land }
   end
+  it 'lands a plane' do
+    plane = Plane.new
+    expect(subject.land(plane)).to eq plane
+  end 
 end
