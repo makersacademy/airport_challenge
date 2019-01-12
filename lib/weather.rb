f80fdab6381ed58f965e3be43c180d9d1e52b495
Debora <debora.piu92@gmail.com>
2019-01-12 15:25:12 +0000

    random weather case added into class

--- lib/weather.rb
+++ lib/weather.rb
@@ -1,2 +1,12 @@
 class Weather
+
+  def stormy
+  end
+
+  def random_weather
+    case rand(100)
+      when 1..80 then return 'sunny'
+      when 81..100 then return 'stormy'
+    end
+  end
 end
--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -2,4 +2,21 @@ require 'weather'
 
 RSpec.describe Weather do
 
+  it "should respond to stormy weather" do
+    expect(subject).to respond_to(:stormy)
+  end
+
+  it "should respond to random weather" do
+    expect(subject).to respond_to(:random_weather)
+  end
+
+  it "should generate a random weather stormy or sunny" do
+    allow(subject).to receive(:rand).and_return(3)
+    expect(subject.random_weather).to eq ('sunny')
+  end
+
+  it "should generate a random weather stormy or sunny" do
+    allow(subject).to receive(:rand).and_return(81)
+    expect(subject.random_weather).to eq ('stormy')
+  end
 end
