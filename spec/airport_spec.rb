d8ac4802f66722ebdf0e99a2f9ec49dc23dc45a3
KrzysztofBalejko <chrisbalejko86@gmail.com>
2019-01-12 13:32:34 +0000

    Test: changes to both tests

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -5,13 +5,13 @@ RSpec.describe Airport do
   it 'Airport allows plane to land' do
     gatwick = Airport.new
     plane = Plane.new
-    expect(gatwick.land(plane))
+    expect(gatwick.land(plane)).to eq(plane)
   end
 
   it 'Airport allows plane to take off' do
     gatwick = Airport.new
     plane = Plane.new
-    expect(gatwick.take_off(plane))
+    expect(gatwick.take_off).to eq('Plane no longer in the airport')
   end
 
 
