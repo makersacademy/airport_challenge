1dec2117868706586cf2e158a4e4d2c8208267be
Simon Green <simon.green@thielsen.co.uk>
2019-01-12 13:53:30 +0000

    added takeoff tests

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,7 +1,13 @@
+require './lib/plane'
+
 class Airport
+  attr_reader :current_plane
+
   def land(plane_to_land)
+    @current_plane = plane_to_land
   end
 
-  def takeoff(plane_to_fly)
+  def take_off
+    @current_plane
   end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -3,6 +3,14 @@ require 'airport'
 RSpec.describe Airport do
   context 'when a default airport is initialised' do
     it { is_expected.to respond_to(:land).with(1).argument }
-    it { is_expected.to respond_to(:takeoff).with(1).argument }
+    it { is_expected.to respond_to(:take_off) }
   end
+  context 'when an airport has a plane landed'
+  before(:each) do
+    @airport = described_class.new
+    @plane = Plane.new
+    @airport.land(@plane)
+  end
+  it { expect(@airport.current_plane).to be_a(Plane) }
+  it { expect(@airport.take_off).to be @plane }
 end
