36c9fd48f6df6ede5b4c1699b8d9f70b1bacfdaf
Jedd Hopkins <jedd_hoppo@hotmail.com>
2019-01-13 22:39:54 +0000

    added context to part of the plane_landing method

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -6,11 +6,13 @@ describe Airport do
   describe '#plane_landing' do
     it { is_expected.to respond_to(:plane_landing).with(1).arguments }
 
-    it 'does not allow planes to land when the airport is full' do
-      # here we are making sure we can add the maximum number of planes and it will not break
-      expect { Airport::DEFAULT_CAPACITY.times { subject.plane_landing(:plane) } }.to_not raise_error
-      # here we are adding the final plane hoping that it will break
-      expect { subject.plane_landing(:plane) }.to raise_error 'Cannot land plane: Airport is full'
+    context 'when airport full' do # context can also be used to umbrella tests, and helps rspec read better.
+      it 'raises an error' do
+        # here we are making sure we can add the maximum number of planes and it will not break
+        expect { Airport::DEFAULT_CAPACITY.times { subject.plane_landing(:plane) } }.to_not raise_error
+        # here we are adding the final plane hoping that it will break
+        expect { subject.plane_landing(:plane) }.to raise_error 'Cannot land plane: Airport is full'
+      end
     end
   end
 
