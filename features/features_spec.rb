ddd4972f4af1d720e34c06e9751e033f47fe2ba7
Ibrahim Butt <ibrahimbutt@me.com>
2019-01-13 19:46:38 +0000

    Add US1 as feature test

--- features/features_spec.rb
+++ features/features_spec.rb
@@ -1,2 +1,10 @@
+require 'airport'
+require 'plane'
+
 RSpec.describe 'Feature Tests' do
+  it 'instruct a plane to land' do
+    airport = Airport.new
+    plane = Plane.new
+    expect { airport.land(plane) }.to_not raise_error
+  end
 end
