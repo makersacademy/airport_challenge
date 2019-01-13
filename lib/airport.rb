e90b267e61fc0da1f94a13682e5a35f6276eb354
Jedd Hopkins <jedd_hoppo@hotmail.com>
2019-01-13 21:15:59 +0000

    default capacity, initialize method, attr_reader added

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,25 +1,31 @@
 require_relative 'plane'
 
 class Airport
-  attr_reader :parked_planes
+  attr_reader :grounded_planes # the same as creating a 'grounded_planes' method that returns grounded_planes.
+  DEFAULT_CAPACITY = 50 # create this so when a initial argument for airport is not passed we still create one
 
-  def initialize
-    @parked_planes = []
+  def initialize(n = DEFAULT_CAPACITY) # when a new airport is created it has an empty
+    @capacity = n                      # array with a capacity of DEFAULT_CAPACITY planes
+    @grounded_planes = []
   end
 
   def plane_landing(plane)
-    @parked_planes << plane
+    raise 'Cannot land plane: Airport is full' if airport_full? # raise = raise error
+    @grounded_planes << plane
   end
 
   def plane_takeoff(plane)
-    # @parked_planes.delete(plane)
+
   end
 
-  # def full?
-  #   @parked_planes >= 20
-  # end
-  #
-  # def empty?
-  #   @parked_planes.empty?
-  # end
+  def airport_full? # create this method to take this function
+    @grounded_planes.length >= @capacity
+  end
+
+  def airport_empty?
+
+  end
+
+
+
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,24 +2,15 @@ require 'airport'
 
 describe Airport do
 
-  # it 'should respond to the plane_landing method' do
-  #   expect(subject).to respond_to(:plane_landing)
-  # end
-
   it { is_expected.to respond_to(:plane_landing).with(1).arguments }
-
-  it 'should allow a plane to land at the airport' do
-    expect(subject.plane_landing("easyjet")).to eq ["easyjet"]
-  end
-
-  # it 'should respond to the plane_takeoff method' do
-  #   expect(subject).to respond_to(:plane_takeoff)
-  # end
 
   it { is_expected.to respond_to(:plane_takeoff).with(1).arguments }
 
-  # it "should raise an error when plane_takeoff is called, airport is empty," do
-  #   expect{subject.plane_takeoff(plane)}.to raise_error("No planes available")if (subject.parked_planes.empty?)
-  # end
+  it 'does not allow planes to land when the airport is full' do
+    Airport::DEFAULT_CAPACITY.times do
+      subject.plane_landing(:plane)
+    end
+    expect { subject.plane_landing(:plane) }.to raise_error 'Cannot land plane: Airport is full'
+  end
 
 end
