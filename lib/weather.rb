3c286e3e0e68d42ff9f49aa11d1633b657991c87
Aiden Gleave <aidengleave@gmail.com>
2019-01-13 18:12:18 +0000

    Commit 10: Passed - Random weather function built

--- lib/weather.rb
+++ lib/weather.rb
@@ -1,4 +1,11 @@
 class Weather
-  def safe_skies?
+  attr_reader :weather_types
+
+  def initialize
+    @weather_types = ['sunny', 'stormy']
+  end
+
+  def now
+    @weather_types.sample
   end
 end
--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -1,11 +1,12 @@
 require 'weather'
 
 describe Weather do
-  it { should respond_to :stormy }
+  it { should respond_to :now }
 end
 
-describe '#stormy' do
+describe '#now' do
   it 'tells the plane whether it\'s safe to take off' do
-    allow(subject).to receive(:stormy) 
+    allow(subject).to receive(:now) { 'sunny' }
+    expect(subject.now).to eq('sunny')
   end
 end
