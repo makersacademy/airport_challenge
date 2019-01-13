fe375939cc74f9e91f2d62bf4f671c0d4efc1a34
david-mears <kieukmail@gmail.com>
2019-01-13 17:58:36 +0000

    Implemented #land to accept landing planes to @planes and raise error if plane is already present.

--- lib/airport.rb
+++ lib/airport.rb
@@ -2,10 +2,18 @@ require_relative 'weather'
 
 class Airport
 
+attr_accessor :planes, :message
+
   include WEATHER
 
+  def initialize
+    @planes = []
+    @message = "Warning: this plane is already in the airport."
+  end
+
   def land(arrival)
-    arrival
+    fail message if planes.include? arrival
+    planes.push(arrival)
   end
 
 end
--- spec/features/airport_features_spec.rb
+++ spec/features/airport_features_spec.rb
@@ -2,9 +2,38 @@ require 'airport'
 
 describe Airport do
 
-   it 'can be instructed to accept a plane landing' do
-     plane = double('plane')
-     expect(subject.land(plane)).to eq plane
-   end
+  describe '#land' do
+    let(:plane) { double('plane') }
+
+    it 'can be instructed to accept a plane landing' do
+      expect(subject.land(plane)).to eq [plane]
+    end
+
+    it 'contains any landed planes' do
+      subject.land(plane)
+      expect(subject.planes.count).to eq 1
+    end
+
+    context 'if the plane has already been landed' do
+
+      let(:airport) { Airport.new }
+
+      before do
+        airport.instance_variable_set(:@planes, [plane])
+      end
+
+      it 'throws error if plane is already landed' do
+        expect { airport.land(plane) }.to raise_error ("Warning: this plane is already in the airport.")
+      end
+
+      it 'does not add plane if it is already landed' do
+        expect { airport.land(plane) }.to raise_error ("Warning: this plane is already in the airport.")
+        # I have to expect the error message in order not to fail out of the test Example and proceed to the next line.
+        expect(airport.planes.count).to eq 1
+      end
+
+    end
+
+  end
 
 end
