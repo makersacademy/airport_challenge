b3cfd1d73934f4725bd2297ab748f7027041c1da
Ibrahim Butt <ibrahimbutt@me.com>
2019-01-13 19:52:47 +0000

    Add #take_off to Airport

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,4 +1,7 @@
 class Airport
   def land(plane)
   end
+
+  def take_off
+  end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -4,4 +4,8 @@ RSpec.describe Airport do
   it 'instructs a plane to land' do
     is_expected.to respond_to(:land).with(1).argument
   end
+
+  it 'instructs a plane to take off' do
+    is_expected.to respond_to(:take_off)
+  end
 end
