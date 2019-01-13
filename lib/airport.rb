369e3764e90c81f691bc1e149bf2196cf4679f08
Ibrahim Butt <ibrahimbutt@me.com>
2019-01-13 21:35:54 +0000

    Extract weather is stormy check to stormy?

--- lib/airport.rb
+++ lib/airport.rb
@@ -2,15 +2,21 @@ class Airport
   attr_reader :plane
 
   def land(plane)
-    raise 'Cannot Land: Turbulent Weather' if Weather.current == "stormy"
+    raise 'Cannot Land: Turbulent Weather' if stormy?
     raise 'Cannot Land: Airport Full' if @plane
 
     @plane = plane
   end
 
   def take_off(_plane)
-    raise 'Cannot Take Off: Turbulent Weather' if Weather.current == "stormy"
+    raise 'Cannot Take Off: Turbulent Weather' if stormy?
 
     @plane = nil
+  end
+
+  private
+
+  def stormy?
+    Weather.current == "stormy"
   end
 end
