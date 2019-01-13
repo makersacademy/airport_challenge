73f8856601d24ade62ed67759f64f0e15b1b285b
david-mears <kieukmail@gmail.com>
2019-01-13 18:34:40 +0000

    Implemented #take_off to return the plane which takes off and raise an error if the plane is not there

--- lib/airport.rb
+++ lib/airport.rb
@@ -2,19 +2,26 @@ require_relative 'weather'
 
 class Airport
 
-  attr_accessor :planes, :message
+  attr_accessor :planes, :message1, :message2
 
   include WEATHER
 
   def initialize
     @planes = []
-    @message = "Warning: this plane is already in the airport."
+    @message1 = "Warning: this plane is already in the airport."
+    @message2 = "That plane is not here"
   end
 
   def land(arrival)
-    fail message if planes.include? arrival
-    
+    fail message1 if planes.include? arrival
+
     planes.push(arrival)
+  end
+
+  def take_off(departure)
+    fail message2 unless planes.include? departure
+
+    planes.delete(departure)
   end
 
 end
--- spec/features/airport_features_spec.rb
+++ spec/features/airport_features_spec.rb
@@ -15,7 +15,6 @@ describe Airport do
     end
 
     context 'if the plane has already been landed' do
-
       let(:airport) { Airport.new }
 
       before do
@@ -31,7 +30,33 @@ describe Airport do
         # I have to expect the error message in order not to fail out of the test Example and proceed to the next line.
         expect(airport.planes.count).to eq 1
       end
+    end
+  end
 
+  describe '#takeoff' do
+    let(:plane) { double('plane') }
+    let(:airport) { Airport.new }
+
+    before do
+      airport.instance_variable_set(:@planes, [plane])
+    end
+
+    it 'can instruct a plane to take off' do
+      airport.take_off(plane)
+      expect(airport.planes.count).to eq 0
+    end
+
+    let(:cheeseburger) { double('cheeseburger') }
+
+    it 'returns the object which takes off' do
+      airport.instance_variable_set(:@planes, [cheeseburger])
+      expect(airport.take_off(cheeseburger)).to eq cheeseburger
+      p airport
+    end
+
+    it 'raises an error if the plane is not there' do
+      airport.instance_variable_set(:@planes, [])
+      expect { airport.take_off(plane) }.to raise_error 'That plane is not here'
     end
 
   end
