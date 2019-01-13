2494391c4ce1bd25c99495b9837b47cb037378f0
Diptis Halder <diptis.halder@gmail.com>
2019-01-13 21:37:24 +0000

    Passed 6th test : Airport class responds to confirm_gone(with_1_argument), modified airport_spec.rb and airport.rb

--- lib/airport.rb
+++ lib/airport.rb
@@ -6,7 +6,7 @@ class Airport
   def instruct_takeoff(arg1)
   end
 
-  def confirm_gone
+  def confirm_gone(arg1)
   end
 
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -6,4 +6,5 @@ describe Airport do
   it { is_expected.to respond_to :instruct_takeoff }
   it { expect(subject).to respond_to(:instruct_takeoff).with(1).argument }
   it { is_expected.to respond_to :confirm_gone }
+  it { expect(subject).to respond_to(:confirm_gone).with(1).argument }
 end
