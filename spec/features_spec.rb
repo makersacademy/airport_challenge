45300e2ba1d7d880ee4b3339723cc24112ddbf7e
Ibrahim Butt <ibrahimbutt@me.com>
2019-01-13 19:55:32 +0000

    Move features_spec.rb back to ./spec
    It's annoying running them seperately.

--- spec/features_spec.rb
+++ spec/features_spec.rb
@@ -7,4 +7,13 @@ RSpec.describe 'Feature Tests' do
     plane = Plane.new
     expect { airport.land(plane) }.to_not raise_error
   end
+
+  it 'instruct a plane to take off and check it left' do
+    airport = Airport.new
+    plane = Plane.new
+    airport.land(plane)
+    airport.take_off(plane)
+
+    expect(airport.plane).to_not eq plane
+  end
 end
