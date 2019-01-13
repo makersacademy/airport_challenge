b3e56ab5a9945d840a660c641ae8e58ea92c4ed2
Dan Groze <dangroze@Dans-MBP.home>
2019-01-13 13:01:59 +0000

    No take_off or landing when stormy

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,13 +1,21 @@
+require_relative 'weather'
 class Airport
-def initialize
-  @planes = []
-end
+  def initialize(weather: Weather.new)
+    @weather = weather
+    @planes = []
+  end
 
-def land(plane)
-  @planes << plane
-end
+  def land(plane)
+    raise 'Stormy weather!' if stormy?
+    @planes << plane
+  end
 
-def take_off
-  @planes.pop
-end
+  def take_off
+    raise 'Stormy weather!' if stormy?
+    @planes.pop
+  end
+
+  def stormy?
+    @weather.stormy?
+  end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -3,12 +3,18 @@ require 'airport'
 RSpec.describe Airport do
   it 'instructs a plane to land' do
     plane = double('it is a plane')
-    expect(subject.land(plane)).to eq [plane]
+    weather = double('weather')
+    airport = Airport.new(weather: weather)
+    allow(weather).to receive(:stormy?).and_return false
+    expect(airport.land(plane)).to eq [plane]
   end
   it 'instructs a plane to take off' do
     plane = double('it is a plane')
-    subject.land(plane)
-    subject.take_off
+    weather = double('weather')
+    airport = Airport.new(weather: weather)
+    allow(weather).to receive(:stormy?).and_return false
+    airport.land(plane)
     expect(subject.take_off).to eq nil
   end
+
 end
