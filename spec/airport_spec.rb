8cd33d6d4f669398e7dc783b6a2e9f773638b071
James <james.rudrum@hyperisland.se>
2019-01-13 23:33:52 +0000

    written test to prevent landing when full

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -5,8 +5,11 @@ RSpec.describe Airport do
   it { is_expected.to respond_to(:land).with(1).argument }
 
   it "instructs plane to land at airport" do
+    airport = Airport.new
+    allow(airport).to receive(:stormy).and_return(false)
     plane = Plane.new
-    expect(subject.land(plane)).to be_a_kind_of(Array)
+    airport.land(plane)
+    expect(airport.num_of_planes.include?(plane)).to eq(true)
   end
 
   it { is_expected.to respond_to(:take_off) }
@@ -34,6 +37,15 @@ RSpec.describe Airport do
     plane = Plane.new
     allow(airport).to receive(:stormy).and_return(true)
     expect { airport.land(plane) }.to raise_error "Too stormy to land"
+  end
+
+  it "prevents landing when airport is full" do
+    airport = Airport.new
+    allow(airport).to receive(:stormy).and_return(false)
+    plane = Plane.new
+    plane1 = Plane.new
+    airport.land(plane)
+    expect { airport.land(plane1) }.to raise_error("Airport at capacity, no landings")
   end
 
 end
