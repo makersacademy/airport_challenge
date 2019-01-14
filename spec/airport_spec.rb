b7eb13fef6a6cf64bd68acc709ac7262cbb42cdc
Diptis Halder <diptis.halder@gmail.com>
2019-01-14 10:15:12 +0000

    Refactored airport_spec.rb using a double for Plane class and replaced heathrow with subjecte

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,31 +1,27 @@
 require 'airport'
 
 describe Airport do
+  let(:airbus) { double :plane}
   it { is_expected.to respond_to :instruct_land }
   it { expect(subject).to respond_to(:instruct_land).with(1).argument }
   it { is_expected.to respond_to :instruct_takeoff }
   it { expect(subject).to respond_to(:instruct_takeoff).with(1).argument }
-  it { is_expected.to respond_to :confirm_gone? }
+  it { is_expected.to respond_to(:confirm_gone?) }
   it { expect(subject).to respond_to(:confirm_gone?).with(1).argument }
 
   it 'instructs a plane to land' do
-    heathrow = Airport.new
-    airbus = Plane.new
-    expect(heathrow.instruct_land(airbus)).to eq(airbus) 
+    # airbus = P:plane}
+    expect(subject.instruct_land(airbus)).to eq(airbus) 
   end
 
   it 'instructs a plane to take-off' do
-    heathrow = Airport.new
-    airbus = Plane.new
-    expect(heathrow.instruct_takeoff(airbus)).to eq(airbus) 
+    expect(subject.instruct_takeoff(airbus)).to eq(airbus) 
   end
 
   it 'checks the plane has really gone away after take-off' do
-    heathrow = Airport.new
-    airbus = Plane.new
-    heathrow.instruct_land(airbus)
-    heathrow.instruct_takeoff(airbus)
-    expect(heathrow.confirm_gone?(airbus)).to eq(true)
+    subject.instruct_land(airbus)
+    subject.instruct_takeoff(airbus)
+    expect(subject.confirm_gone?(airbus)).to eq(true)
   end
 
 end
