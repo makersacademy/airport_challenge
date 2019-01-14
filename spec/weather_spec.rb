f87b2889d7e0964702d321bb8d06e4c180286494
Kwaku Ankobia <kwaku.ankobia@gmail.com>
2019-01-14 01:14:23 +0000

    test for random weather generator passed

--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -6,7 +6,8 @@ end
 
 describe Weather do
   it "generates random number for weather conditions" do
-    expect(subject).to receive(:rand).and_return(0)
+    expect(subject).to receive(:rand).and_return(2)
+
     expect(subject.stormy?).to eq false
   end
 end
