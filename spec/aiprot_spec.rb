335c502d45f65948261517dd603a944133095279
Kwaku Ankobia <kwaku.ankobia@gmail.com>
2019-01-15 06:32:49 +0000

    As an air traffic controller So planes can land safely at my airport I would like to instruct a plane to land§

--- spec/aiprot_spec.rb
+++ spec/aiprot_spec.rb
@@ -1,7 +1,8 @@
 require "airport"
 
 describe Airport do
-  it 'instructs plane to land' do
-    expect(subject).to respond_to(:land).with(1).argument
+  subject(:airport) { described_class.new }
+  it 'comment for rspec test' do
+    expect(airport).to respond_to(:land).with(1).argument
   end 
 end
