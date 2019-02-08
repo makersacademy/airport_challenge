58551bf07d04e256c3b70cc9294a6fdec51aa381
Sarah Byrne <sarah.byrne@johnlewis.co.uk>
2019-02-08 23:08:20 +0000

    add take off and confirm whether plane is in airport

--- lib/airport.rb
+++ lib/airport.rb
@@ -8,4 +8,12 @@ class Airport
   def land(plane)
     @planes.push(plane)
   end
+
+  def take_off(plane)
+    @planes.delete(plane)
+  end
+
+  def has_plane?(plane)
+    @planes.include?(plane)
+  end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -7,4 +7,18 @@ describe Airport do
     airport.land(plane)
     expect(airport.planes).to include(plane)
   end
+
+  it "can instruct a plane to take off" do
+    airport = Airport.new
+    plane = Plane.new
+    airport.land(plane)
+    airport.take_off(plane)
+    expect(airport.planes).not_to include(plane)
+  end
+
+  it "can check whether a plane is at the airport" do
+    airport = Airport.new
+    plane = Plane.new
+    expect(airport.has_plane?(plane)).to be(false)
+  end
 end
