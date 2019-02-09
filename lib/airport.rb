48645cb8598fb5707346108cdf9aa157251d9cfa
Lima Vadakara <lima.vadakara@gmail.com>
2019-02-09 18:48:39 +0000

    Raise error when trying to land more planes than default capacity of 1 of the airport

--- lib/airport.rb
+++ lib/airport.rb
@@ -2,8 +2,13 @@ require_relative 'plane.rb'
 class Airport
   attr_reader :planes
 
+  def initialize
+    @planes = []
+  end
+
   def land_plane(plane)
-    @planes = [plane]
+    raise "The airport is full" if @planes.length == 1
+    @planes << plane
   end
 
   def take_off(plane)
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -15,4 +15,13 @@ describe Airport do
     expect(airport.take_off(plane)).to be plane
     expect(airport.planes).to be_empty
   end
+
+  it 'should have a default capacity of 1 and raise error if asked to land more planes' do
+    airport = Airport.new
+    plane = Plane.new
+    airport.land_plane(plane)
+    another_plane = Plane.new
+    expect {airport.land_plane(another_plane)}.to raise_error "The airport is full"
+
+  end
 end
