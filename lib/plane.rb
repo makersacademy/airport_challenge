3404e7c4a95cc200b2d11b1b75cfe1473f399987
Sarah Byrne <sarah.byrne@johnlewis.co.uk>
2019-02-09 11:20:50 +0000

    planes cannot take off if already flying

--- lib/plane.rb
+++ lib/plane.rb
@@ -1,8 +1,27 @@
 class Plane
   attr_reader :airport
 
+  def initialize
+    @airport = nil
+    @flying = true
+  end
+
   def land(airport)
     @airport = airport
+    @flying = false
   end
-  
+
+  def take_off(airport)
+    raise "already flying" if flying?
+    raise "plane not at airport" if @airport != airport
+
+    @airport = nil
+    @flying = true
+  end
+
+private
+
+  def flying?
+    @flying
+  end
 end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -14,4 +14,23 @@ describe Plane do
     expect(plane.airport).to eq(airport)
   end
 
+  it "can take off from an airport it is in" do
+    plane = Plane.new
+    airport = Airport.new
+    plane.land(airport)
+    plane.take_off(airport)
+    expect(plane.airport).to eq(nil)
+  end
+
+  it "cannot take off from an airport it is not in" do
+    plane = Plane.new
+    airport = Airport.new
+    plane.land(airport)
+    expect { plane.take_off(Airport.new) }.to raise_error("plane not at airport")
+  end
+
+  it "cannot take off if already flying" do
+    plane = Plane.new
+    expect { plane.take_off(Airport.new) }.to raise_error("already flying")
+  end
 end
