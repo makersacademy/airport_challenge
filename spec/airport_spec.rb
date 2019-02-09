e86e75267cc00d3ee406a0d4c1f87707a8282249
Andrew <24258342+acodeguy@users.noreply.github.com>
2019-02-09 19:09:53 +0000

    create global @plane, remove several instances created separately

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,15 +2,19 @@ require "./lib/airport"
 
 describe Airport do
 
+  before(:each) do
+    @plane = Plane.new
+  end
+
   it "is created with a default plane capacity that can be overriden upon creation" do
     airport = Airport.new(10)
     expect(airport.capacity).to eq 10
   end
 
   it "returns true if a given plane is hanagred" do
-    plane = Plane.new
-    subject.land(plane)
-    expect(subject.hangared?(plane)).to be true
+    # plane = Plane.new
+    subject.land(@plane)
+    expect(subject.hangared?(@plane)).to be true
   end
 
   it "responds to #stormy" do
@@ -45,8 +49,7 @@ describe Airport do
     end
 
     it "lands a plane and stores it in its hangar" do
-      plane = Plane.new
-      expect(subject.land(plane)).to eq [plane]
+      expect(subject.land(@plane)).to eq [@plane]
     end
 
     it "prevents a plane from landing if its hangar is full" do
@@ -56,9 +59,8 @@ describe Airport do
     end
 
     it "does not allow a plane to be landed twice" do
-      plane = Plane.new
-      subject.land(plane)
-      expect { subject.land(plane) }.to raise_error "Plane alredy landed here"
+      subject.land(@plane)
+      expect { subject.land(@plane) }.to raise_error "Plane alredy landed here"
     end
 
   end
@@ -70,24 +72,21 @@ describe Airport do
     end
 
     it "allows a plane to take-off from its hangar and confirm it is no longer in the airport" do
-      plane = Plane.new
-      subject.land(plane)
+      subject.land(@plane)
       allow(subject).to receive(:stormy?).and_return(false) # force stormy weather to be false
-      subject.takeoff(plane)
-      expect(subject.hangar).not_to include plane
+      subject.takeoff(@plane)
+      expect(subject.hangar).not_to include @plane
     end
 
     it "it prevents a plane from taking-off when the weather is stormy" do
-      plane = Plane.new
-      subject.land(plane)
+      subject.land(@plane)
       allow(subject).to receive(:stormy?).and_return(true) # force stormy weather to be true
-      expect { subject.takeoff(plane) }.to raise_error "Cannot take-off; weather is stormy"
+      expect { subject.takeoff(@plane) }.to raise_error "Cannot take-off; weather is stormy"
     end
 
     it "allows take-off only for planes in its hangar" do
-      plane = Plane.new
       allow(subject).to receive(:stormy?).and_return(false) # force stormy weather to be false
-      expect { subject.takeoff(plane) }.to raise_error "Plane is not hanagared at this airport"
+      expect { subject.takeoff(@plane) }.to raise_error "Plane is not hanagared at this airport"
     end
 
   end
