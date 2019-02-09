c53f6fcf603fbadd62c2f8bfd72ff2ef70f98fee
Student <student@Admins-MacBook-Pro-3.local>
2019-02-09 16:44:25 +0000

    story two test 3

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,16 +1,33 @@
 class Airport
+attr_reader :weather
+attr_reader :planes_in_airport
 
+ def initialize(weather)
+   @weather = weather
+   @planes_in_airport = []
+ end
 
 def land(plane)
-  true
+  @planes_in_airport.push(plane)
 end
 
 def take_off(plane)
-  true
+  index = @planes_in_airport.index{|item| item == plane}
+  take_off_plane = @planes_in_airport.delete_at(index)
+  #return plane = take_off_plane
 end
 
 def report(plane)
-  true
+  return !@planes_in_airport.include?(plane)
+end
+
+def weather
+  if @weather == "stormy"
+  raise "The plane cannot take off due to stormy weather condidtions."
+  else
+    airport = Airport.new
+    expect(airport.take_off(plane)).to eq true
+  end
 end
 
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -4,18 +4,29 @@ describe Airport do
 
 it 'instructs a plane to land' do
   plane = Plane.new
-  expect(subject.land(plane)).to eq true
+  airport = Airport.new("clear")
+  airport.land(plane)
+  expect(airport.planes_in_airport[0]).to eq plane
 end
 
 it 'instructs plane to take off' do
   plane = Plane.new
-  expect(subject.take_off(plane)).to eq true
+  airport = Airport.new("clear")
+  airport.land(plane)
+  airport.take_off(plane)
+  expect(airport.planes_in_airport.empty?).to eq true
 end
 
 it 'reports that a plane has taken off' do
   plane = Plane.new
-  subject.land(plane)
-  expect(subject.report(plane)).to eq true
+  airport = Airport.new("clear")
+  airport.land(plane)
+  airport.take_off(plane)
+  expect(airport.report(plane)).to eq true
 end
 
+it 'prevents takeoff in stormy weather' do
+plane = Plane.new
+
+end
 end
