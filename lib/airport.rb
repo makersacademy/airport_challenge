f1183818335e516b6019e1937ed083de12373223
Oliver <ofcharrison@gmail.com>
2019-02-09 15:03:03 +0000

    user story 2. takeoff

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,5 +1,19 @@
 class Airport
+  attr_reader :at_airport
+  def initialize
+    @at_airport = []
+  end
+
   def landing(plane)
+    @at_airport.push(plane)
+    puts @at_airport
     plane
+  end
+
+  def takeoff
+    if @at_airport.length>0
+       @at_airport.shift[-1]
+       @at_airport.length
+    end
   end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -3,6 +3,21 @@ require 'airport'
 describe Airport do
   it 'responds to a plane' do
     airport = Airport.new
-    expect(airport.landing("plane")).to eq("plane")
+    expect(airport.landing("plane1")).to eq("plane1")
+  end
+
+  it 'takes off from airport' do
+    puts "-----"
+    airport = Airport.new
+    airport.landing("plane1")
+    x = airport.at_airport.length
+    puts x
+    if x>0
+    expect(airport.takeoff).to eq(x-1)
+    else
+      fail "No planes at airport"
+    end
+
+
   end
 end
