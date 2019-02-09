e18a3e37b52d6f5e1bf8bb72ffe2af505c93c646
Lima Vadakara <lima.vadakara@gmail.com>
2019-02-09 18:02:50 +0000

    Instruct a plane to take off and confirm it is not in the airport anymore

--- lib/airport.rb
+++ lib/airport.rb
@@ -3,7 +3,11 @@ class Airport
   attr_reader :planes
 
   def land_plane(plane)
-    @planes = plane
+    @planes = [plane]
+  end
+
+  def take_off(plane)
+    @planes.pop
   end
 
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -5,6 +5,14 @@ describe Airport do
     airport = Airport.new
     plane = Plane.new
     airport.land_plane(plane)
-    expect(airport.planes).to equal plane
+    expect(airport.planes).to include(plane)
+  end
+
+  it 'instructs a plane to take off and confirm that it is no longer in the airport' do
+    airport = Airport.new
+    plane = Plane.new
+    airport.land_plane(plane)
+    expect(airport.take_off(plane)).to be plane
+    expect(airport.planes).to be_empty
   end
 end
