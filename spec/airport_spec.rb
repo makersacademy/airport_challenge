ac8f702a0ca523c65320129d0b74aab3b93bfe2b
Andrew <24258342+acodeguy@users.noreply.github.com>
2019-02-09 11:15:02 +0000

    create context for landing a plane

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,8 +2,10 @@ require "./lib/airport"
 
 describe Airport do
 
-  it "responds to #land_plane" do
-    expect(subject).to respond_to :land_plane
+  context "landing a plane" do
+    it "responds to #land_plane" do
+      expect(subject).to respond_to :land_plane
+    end
   end
 
 end
