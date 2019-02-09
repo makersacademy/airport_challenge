7fd66f2c715946d63cf24ad077422d121e1cecf2
Lima Vadakara <lima.vadakara@gmail.com>
2019-02-09 19:27:29 +0000

    airport should allow as many planes as defined in the override capacity if passed in to land

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,14 +1,15 @@
 require_relative 'plane.rb'
 class Airport
   attr_reader :planes
-  def initialize
+  DEFAULT_CAPACITY = 20
+  def initialize(capacity = DEFAULT_CAPACITY)
     @planes = []
-    @capacity = 1
+    @capacity = capacity
   end
 
   def land_plane(plane)
     raise "The airport is full" if @planes.length == @capacity
-    
+
     @planes << plane
   end
 
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -16,12 +16,22 @@ describe Airport do
     expect(airport.planes).to be_empty
   end
 
-  it 'should have a default capacity of 1 and raise error if asked to land more planes' do
+  it 'should have a default capacity and raise error if asked to land more planes' do
     airport = Airport.new
+    Airport::DEFAULT_CAPACITY.times do
+      airport.land_plane(Plane.new)
+    end
+    expect { airport.land_plane(Plane.new) }.to raise_error "The airport is full"
+
+  end
+
+  it 'should be created with an override capacity if passed in and allow as many planes as its defined capacity to land ' do
+    airport = Airport.new(2)
     plane = Plane.new
     airport.land_plane(plane)
     another_plane = Plane.new
-    expect { airport.land_plane(another_plane) }.to raise_error "The airport is full"
-
+    airport.land_plane(another_plane)
+    expect(airport.planes).to include(plane)
+    expect(airport.planes).to include(another_plane)
   end
 end
