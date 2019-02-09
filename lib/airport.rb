1489fc1b41ab56160197c01526837bc50052491b
Andrew <24258342+acodeguy@users.noreply.github.com>
2019-02-09 12:13:36 +0000

    pass test for plane take-off, confirm it is no longer in aiport

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,8 +1,14 @@
 class Airport
 
+  attr_reader :hangar
+
   def land_plane(plane)
     @hangar = []
     @hangar.push(plane)
+  end
+
+  def takeoff_plane
+    @hangar.pop
   end
 
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -15,4 +15,19 @@ describe Airport do
 
   end
 
+  context "getting a plane to take-off" do
+
+    it "responds to #takeoff_plane" do
+      expect(subject).to respond_to :takeoff_plane
+    end
+
+    it "allows a plane to take-off from its hangar and confirm it is no longer in the airport" do
+      plane = Plane.new
+      subject.land_plane(plane)
+      plane = subject.takeoff_plane
+      expect(subject.hangar).not_to include plane
+    end
+
+  end
+
 end
