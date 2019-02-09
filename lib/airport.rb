318d8670f36eec5cfdb91bf2e3d7da9454f7ea97
“Brenda” <odonnell.brenda@gmail.com>
2019-02-09 09:18:31 +0000

    First unit test: it creates Airport with capacity and weather

---
+++ lib/airport.rb
@@ -0,0 +1,8 @@
+class Airport
+
+  def initialize(capacity, weather)
+    # capacity = capacity
+    # weather = weather
+  end
+
+end
---
+++ spec/airport_spec.rb
@@ -0,0 +1,9 @@
+require 'airport'
+
+describe Airport do
+
+  it 'is created with a capacity and weather' do
+    Airport.new(20, "sunny")
+  end
+
+end
