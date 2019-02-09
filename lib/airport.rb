00f49769ca6a09210847874d4d5f11e64223e198
Sarah Byrne <sarah.byrne@johnlewis.co.uk>
2019-02-09 10:24:18 +0000

    correct rubocop offenses

--- lib/airport.rb
+++ lib/airport.rb
@@ -10,18 +10,15 @@ class Airport
 
   def land(plane)
     raise "airport full" if full?
-    if check_weather != BAD_WEATHER
-      @planes.push(plane)
-    end
+
+    @planes.push(plane) if check_weather != BAD_WEATHER
   end
 
   def take_off(plane)
-    if check_weather == BAD_WEATHER
-      return "bad weather: plane has not left"
-    else
-      @planes.delete(plane)
-      return "plane has left"
-    end
+    return "bad weather: plane has not left" if check_weather == BAD_WEATHER
+
+    @planes.delete(plane)
+    return "plane has left"
   end
 
   def check_weather
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -4,46 +4,45 @@ describe Airport do
   it "can instruct a plane to land in good weather" do
     airport = Airport.new
     plane = Plane.new
-    allow(airport).to receive(:check_weather) {2}
+    allow(airport).to receive(:check_weather) { 2 }
     airport.land(plane)
     expect(airport.planes).to include(plane)
   end
 
-    it "prevents landing if weather is stormy" do
-      airport = Airport.new
-      plane = Plane.new
+  it "prevents landing if weather is stormy" do
+    airport = Airport.new
+    plane = Plane.new
 
-      allow(airport).to receive(:check_weather) {1}
-      airport.land(plane)
-      expect(airport.planes).not_to include(plane)
-    end
+    allow(airport).to receive(:check_weather) { 1 }
+    airport.land(plane)
+    expect(airport.planes).not_to include(plane)
+  end
 
-    it "prevents landing if airport is full" do
-      airport = Airport.new
-      airport.capacity.times {airport.land(Plane.new)}
-      plane = Plane.new
-      expect{airport.land(plane)}.to raise_error("airport full")
-      expect(airport.planes).not_to include(plane)
-    end
+  it "prevents landing if airport is full" do
+    airport = Airport.new
+    airport.capacity.times { airport.land(Plane.new) }
+    plane = Plane.new
+    expect { airport.land(plane) }.to raise_error("airport full")
+    expect(airport.planes).not_to include(plane)
+  end
 
   it "can instruct a plane to take off in good weather" do
     airport = Airport.new
     plane = Plane.new
     airport.land(plane)
-    allow(airport).to receive(:check_weather) {2}
+    allow(airport).to receive(:check_weather) { 2 }
     expect(airport.take_off(plane)).to eq("plane has left")
     expect(airport.planes).not_to include(plane)
   end
 
-    it "prevents take off if weather is stormy" do
-      airport = Airport.new
-      plane = Plane.new
-      airport.land(plane)
+  it "prevents take off if weather is stormy" do
+    airport = Airport.new
+    plane = Plane.new
+    airport.land(plane)
 
-      allow(airport).to receive(:check_weather) {1}
-      expect(airport.take_off(plane)).to eq("bad weather: plane has not left")
-      expect(airport.planes).to include(plane)
-    end
-
+    allow(airport).to receive(:check_weather) { 1 }
+    expect(airport.take_off(plane)).to eq("bad weather: plane has not left")
+    expect(airport.planes).to include(plane)
+  end
 
 end
