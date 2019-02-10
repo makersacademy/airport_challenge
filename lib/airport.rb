89b36dcb33343fe1cec5f9267e4df0e6f0dc1d6f
James Ault <james.ault@johnlewis.co.uk>
2019-02-10 14:02:52 +0000

    First passing tests for a plane can land user story

---
+++ lib/airport.rb
@@ -0,0 +1,19 @@
+require_relative 'plane.rb'
+
+class Airport
+  attr_reader :planes
+  def initialize
+    @planes = []
+  end
+
+  def land_plane(plane)
+    @planes << plane
+  end
+
+  def takeoff(plane)
+    planes_for_takeoff = @planes.select { |num| num = plane }
+    plane_for_takeoff = planes_for_takeoff.pop
+    @planes.delete(plane_for_takeoff)
+    return plane_for_takeoff
+  end
+end
---
+++ spec/airport_spec.rb
@@ -0,0 +1,19 @@
+require './lib/airport.rb'
+
+describe Airport do
+
+  it 'can land a plane' do
+    plane = Plane.new
+    airport = Airport.new
+    airport.land_plane(plane)
+    expect(airport.planes).to include(plane)
+  end
+
+  it 'can allow a plane to takeoff' do
+    plane = Plane.new
+    airport = Airport.new
+    airport.land_plane(plane)
+    airport.takeoff(plane)
+    expect(airport.planes).to_not include(plane)
+  end
+end
