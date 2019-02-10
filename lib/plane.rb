82434a0baaea9a4cb4b356ee0b6b4d08d58efc38
Simon <01secn@gmail.com>
2019-02-10 09:45:23 +0000

    Plane has features to take off and land, and will not take off when weather is stormy (first 3 user stories)

--- lib/plane.rb
+++ lib/plane.rb
@@ -1,5 +1,13 @@
 class Plane
+  def initialize(weather)
+    @weather = weather
+  end
+
   def land(airport)
     airport.land_plane(self)
+  end
+
+  def take_off
+    @weather.get == 'sunny'
   end
 end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -2,11 +2,26 @@ require './lib/plane.rb'
 
 describe Plane do
   it 'should instruct plane to land at the specified airport' do
-    plane = Plane.new
+    weather_double = double(:weather, get: "sunny")
+    plane = Plane.new(weather_double)
     airport_double = double(:airport, land_plane: "")
 
     plane.land(airport_double)
 
     expect(airport_double).to have_received(:land_plane).with(plane)
+  end
+
+  it 'should instruct plane to take off and get back confirmation that plane is airborne' do
+    weather_double = double(:weather, get: "sunny")
+    plane = Plane.new(weather_double)
+
+    expect(plane.take_off).to eq(true)
+  end
+
+  it 'should prevent takeoff when weather is stormy' do
+    weather_double = double(:weather, get: "stormy")
+    plane = Plane.new(weather_double)
+
+    expect(plane.take_off).to eq(false)
   end
 end
