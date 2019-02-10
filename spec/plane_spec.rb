7877c26397ba50cb563ff78e95b5cd5e63775508
Simon <01secn@gmail.com>
2019-02-10 10:15:04 +0000

    Refactored tests - separated plane tests in sunny and stormy conditions into two different nested describe statements, so can do different set up for each

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -3,31 +3,37 @@ require './lib/plane.rb'
 describe Plane do
   before(:each) do
     @airport_double = double(:airport, land_plane: "")
-    @weather_double = double(:weather, get: "sunny")
-    @plane = Plane.new(@weather_double)
   end
 
-  it 'should instruct plane to land at the specified airport' do
-    @plane.land(@airport_double)
+  describe 'plane in sunny conditions' do
+    before(:each) do
+      @weather_double = double(:weather, get: "sunny")
+      @plane = Plane.new(@weather_double)
+    end
 
-    expect(@airport_double).to have_received(:land_plane).with(@plane)
+    it 'should instruct plane to land at the specified airport' do
+      @plane.land(@airport_double)
+  
+      expect(@airport_double).to have_received(:land_plane).with(@plane)
+    end
+
+    it 'should instruct plane to take off and get back confirmation that plane is airborne' do
+      expect(@plane.take_off).to eq(true)
+    end
   end
 
-  it 'should instruct plane to take off and get back confirmation that plane is airborne' do
-    expect(@plane.take_off).to eq(true)
-  end
+  describe 'plane in stormy conditions' do
+    before(:each) do
+      @weather_double = double(:weather, get: "stormy")
+      @plane = Plane.new(@weather_double)
+    end
 
-  it 'should prevent takeoff when weather is stormy' do
-    weather_double = double(:weather, get: "stormy")
-    plane = Plane.new(weather_double)
+    it 'should prevent takeoff when weather is stormy' do
+      expect(@plane.take_off).to eq(false)
+    end
 
-    expect(plane.take_off).to eq(false)
-  end
-
-  it 'should prevent landing when weather is stormy' do
-    weather_double = double(:weather, get: "stormy")
-    plane = Plane.new(weather_double)
-
-    expect { plane.land(@airport_double) }.to raise_error('Cannot land due to stormy weather')
+    it 'should prevent landing when weather is stormy' do
+      expect { @plane.land(@airport_double) }.to raise_error('Cannot land due to stormy weather')
+    end
   end
 end
