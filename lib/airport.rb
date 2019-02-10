da7e8c159e50a888c8d0e60b30fbea4b295c245c
Amina Jusic <amina_jusic@yahoo.com>
2019-02-10 13:40:31 +0000

    user story_2, instruct plane to take off

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,7 +1,21 @@
 class Airport
 
-  def instruct_to_land(plane)
+  def initialize
     @planes = []
+  end
+
+
+  def instruct_to_land(plane)
+    @flying = false
     @planes << plane
   end
+  
+
+  def instruct_to_takeoff(plane)
+    @flying = true
+    @planes.pop
+    puts "#{plane} has taken off from airport"
+  end
+
+
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -7,5 +7,8 @@ describe Airport do
     expect(subject.instruct_to_land(plane)).to eq [plane]
   end
 
+  it 'instructs a plane to take off and confirm that it is no longer in the airport' do
+    expect { subject.instruct_to_takeoff(Plane.new) }.equal? true
+  end
 
 end
