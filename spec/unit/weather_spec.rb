eb73b26c28d629ddaa735974df5138c845d09fde
Simon <01secn@gmail.com>
2019-02-10 14:14:31 +0000

    Added new test for weather class to check it returns sunny 95% of the time, and refactored tests to have shared set up

--- spec/unit/weather_spec.rb
+++ spec/unit/weather_spec.rb
@@ -1,10 +1,21 @@
 require './lib/weather.rb'
 
 describe Weather do
-  it 'should return stormy weather if random number generator returns 20' do
-    weather = Weather.new
-    allow(weather).to receive(:rand).and_return(20)
+  before(:each) do
+    @weather = Weather.new
+  end
 
-    expect(weather.stormy?).to eq(true)
+  it 'should return stormy 5% of time (IE if rand returns a 20)' do
+    allow(@weather).to receive(:rand).with(1..20).and_return(20)
+
+    expect(@weather.stormy?).to eq(true)
+  end
+
+  it 'should return sunny 95% of the time (IE if rand returns a number between 1 and 19)' do
+    (1..19).each do |number|
+      allow(@weather).to receive(:rand).with(1..20).and_return(number)
+
+      expect(@weather.stormy?).to eq(false)
+    end
   end
 end
