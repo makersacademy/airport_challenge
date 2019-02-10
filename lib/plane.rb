14a08a0acdfee3f812f88ddca532f6e52f7573a3
Simon <01secn@gmail.com>
2019-02-10 10:58:36 +0000

    Plane will not try to land if is already on the ground, but will throw exception

--- lib/plane.rb
+++ lib/plane.rb
@@ -1,9 +1,11 @@
 class Plane
-  def initialize(weather)
+  def initialize(weather, is_airborne = false)
     @weather = weather
+    @is_airborne = is_airborne
   end
 
   def land(airport)
+    raise 'Plane is already on the ground' unless @is_airborne
     raise 'Cannot land due to stormy weather' if @weather.is_stormy
 
     airport.land_plane(self)
--- spec/unit/plane_spec.rb
+++ spec/unit/plane_spec.rb
@@ -3,18 +3,25 @@ require './lib/plane.rb'
 describe Plane do
   before(:each) do
     @airport_double = double(:airport, land_plane: "")
+    @is_airborne = true
   end
 
   describe 'plane in sunny conditions' do
     before(:each) do
       @weather_double = double(:weather, is_stormy: false)
-      @plane = Plane.new(@weather_double)
+      @plane = Plane.new(@weather_double, @is_airborne)
     end
 
     it 'should instruct plane to land at the specified airport' do
       @plane.land(@airport_double)
   
       expect(@airport_double).to have_received(:land_plane).with(@plane)
+    end
+
+    it 'should raise exception if attempting to land plane whilst it is on the ground' do
+      @plane = Plane.new(@weather_double)
+
+      expect { @plane.land(@airport_double) }.to raise_error('Plane is already on the ground')
     end
 
     it 'should instruct plane to take off and get back confirmation that plane is airborne' do
@@ -25,7 +32,7 @@ describe Plane do
   describe 'plane in stormy conditions' do
     before(:each) do
       @weather_double = double(:weather, is_stormy: true)
-      @plane = Plane.new(@weather_double)
+      @plane = Plane.new(@weather_double, @is_airborne)
     end
 
     it 'should prevent takeoff when weather is stormy' do
