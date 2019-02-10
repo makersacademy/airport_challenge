7e20945d87b4fcf3a8e809541e137e71f8676354
Student <student@Admins-MacBook-Pro-3.local>
2019-02-10 10:29:28 +0000

    edge cases

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,29 +1,55 @@
+#require './lib/plane.rb'
+
 class Airport
 attr_accessor :weather
 attr_reader :planes_in_airport
+attr_reader :capacity
 
- def initialize(weather)
-
-   @weather = weather
+ def initialize(capacity=20)
+   @capacity = capacity
    @planes_in_airport = []
+   weather_man
  end
 
+def weather_man
+  weather_num = rand(0..100)
+  if weather_num <=80
+      @weather = "sunny"
+  else
+      @weather = "stormy"
+  end
+  @weather
+end
+
 def land(plane)
-  if @weather == "clear"
+  weather_man
+  if @weather == "sunny" && @planes_in_airport.count < capacity && report(plane) == true
      @planes_in_airport.push(plane)
+     plane.allow
   end
 end
 
 def take_off(plane)
-  if @weather == "clear"
+  weather_man
+  if @weather == "sunny" && report(plane) == false
     index = @planes_in_airport.index{|item| item == plane}
     take_off_plane = @planes_in_airport.delete_at(index)
+    plane.allow
   end
 end
 
+#reports if plane is not in airport
 def report(plane)
   return !@planes_in_airport.include?(plane)
 end
 
+def communicate(plane)
+  request = plane.get_request
+  if request == "take off"
+    take_off(plane)
+  elsif request == "land"
+    land(plane)
+  end
+end
 #
 end
--- lib/plane.rb
+++ lib/plane.rb
@@ -1,4 +1,36 @@
 class Plane
+attr_reader :state
+attr_reader :requested
+
+def initialize(state="landed")
+  @state = state
+  @requested = "none"
+end
+
+def set_request(requested)
+
+  if @state == "landed" && requested == "take off"
+    @requested = requested
+  elsif @state = "flying" &&  requested == "land"
+    @requested = requested
+  else
+    @requested = "none"
+  end
+end
+
+def get_request
+  @requested
+end
+
+def allow
+  if @requested == "land"
+    @state="landed"
+  elsif @requested == "take off"
+    @state="flying"
+  else
+  end
+end
+
 
 
 end
