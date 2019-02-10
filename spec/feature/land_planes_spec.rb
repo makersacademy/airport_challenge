c8e6feb951d30f12853d0b807ee5dcffd01ed517
Simon <01secn@gmail.com>
2019-02-10 12:32:57 +0000

    Expanded feature test to include planes taking off as well as landing

--- spec/feature/land_planes_spec.rb
+++ spec/feature/land_planes_spec.rb
@@ -2,14 +2,33 @@ require './lib/plane.rb'
 require './lib/airport.rb'
 
 describe 'land plane' do
-  it 'should land plane' do
-    weather_double = double(:weather, is_stormy: false)
-    is_airborne = true
-    plane = Plane.new(weather_double, is_airborne)
+  before(:each) do
+    @weather_double = double(:weather, is_stormy: false)
+    @is_airborne = true
+    @landed_planes = []
+    @airport = Airport.new
+  end
 
-    airport = Airport.new
-    plane.land(airport)
+  def land_plane(plane)
+    plane.land(@airport) 
+    @landed_planes << plane
+  end
 
-    expect(airport.planes).to eq([plane])
+  it 'should land and take off planes' do
+    2.times { 
+      plane = Plane.new(@weather_double, @is_airborne)
+      land_plane(plane) 
+      
+      expect(plane.is_airborne).to eq(false)
+    }
+
+    expect(@airport.planes).to eq(@landed_planes)
+
+    @landed_planes.each do |plane|
+      plane.take_off(@airport)
+      expect(plane.is_airborne).to eq(true)
+    end
+
+    expect(@airport.planes).to eq([])
   end
 end
