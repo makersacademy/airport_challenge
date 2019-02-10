f15068122d7f22649ee1c62b5f2cff36ccc4c340
Amina Jusic <amina_jusic@yahoo.com>
2019-02-10 22:43:15 +0000

    Pass the test for plane land and take off

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -5,10 +5,12 @@ describe Airport do
   it ' instructs a plane to land' do
     plane = Plane.new
     expect(subject.instruct_to_land(plane)).to eq [plane]
+    expect(subject.instruct_to_land(plane).to raise_error('Plane already landed'))
   end
 
   it 'instructs a plane to take off and confirm that it is no longer in the airport' do
     expect { subject.instruct_to_takeoff(Plane.new) }.equal? true
+    expect(subject.instruct_to_takeoff(Plane.new).to raise_error('Plane already took off'))
   end
 
 end
