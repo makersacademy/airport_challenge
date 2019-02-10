2d8a05bc220d831cc812d42c33d96f31bc514493
Simon <01secn@gmail.com>
2019-02-10 17:43:27 +0000

    Refactored airport tests, grouping them into describes

--- spec/unit/airport_spec.rb
+++ spec/unit/airport_spec.rb
@@ -1,55 +1,64 @@
 require './lib/airport.rb'
 
 describe Airport do
-  describe 'Given weather conditions are sunny' do
+  describe 'Given weather conditions are sunny, ' do
     before(:each) do
       @weather_double = double(:weather, stormy?: false)
       @airport = Airport.new(weather: @weather_double)
       @plane_double = double(:plane, land: "", take_off: "")
     end
 
-    it 'should instruct planes to land' do
-      @airport.land(@plane_double)
-  
-      expect(@plane_double).to have_received(:land)
+    describe 'When the airport has capacity and is instructed to land plane, ' do
+      before(:each) do
+        @airport.land(@plane_double)
+      end
+
+      it 'should instruct that plane to land' do
+        expect(@plane_double).to have_received(:land)
+      end
+
+      it 'should add plane to airport once landed' do
+        expect(@airport.planes).to eq([@plane_double])
+      end
     end
 
-    it 'should add plane to airport once landed' do
-      @airport.land(@plane_double)
-  
-      expect(@airport.planes).to eq([@plane_double])
+    describe 'When the airport is full up to the default capacity, ' do
+      it 'should prevent planes from landing' do
+        Airport::DEFAULT_CAPACITY.times { @airport.land(@plane_double) }
+    
+        expect { @airport.land(@plane_double) }.to raise_error('Plane cannot land as airport is full')
+      end
     end
 
-    it 'should prevent planes from landing when airport is full' do
-      Airport::DEFAULT_CAPACITY.times { @airport.land(@plane_double) }
-  
-      expect { @airport.land(@plane_double) }.to raise_error('Plane cannot land as airport is full')
+    describe 'When the airport is full up to the user-set capacity ' do
+      it 'should prevent planes from landing' do
+        capacity = 15
+        airport = Airport.new(capacity: capacity, weather: @weather_double)
+        capacity.times { airport.land(@plane_double) }
+    
+        expect { airport.land(@plane_double) }.to raise_error('Plane cannot land as airport is full')
+      end
     end
 
-    it 'should optionally set airport capacity when airport created' do
-      capacity = 15
-      airport = Airport.new(capacity: capacity, weather: @weather_double)
-      capacity.times { airport.land(@plane_double) }
-  
-      expect { airport.land(@plane_double) }.to raise_error('Plane cannot land as airport is full')
+    describe 'When airport is instructed to take off plane that is in airport, ' do
+      before(:each) do
+        @airport.land(@plane_double)
+        @airport.take_off(@plane_double)
+      end
+
+      it 'should instruct plane to take off' do
+        expect(@plane_double).to have_received(:take_off)
+      end
+
+      it 'should remove plane after take off' do
+        expect(@airport.planes).to eq([])
+      end
     end
 
-    it 'should instruct planes to take off' do
-      @airport.land(@plane_double)
-      @airport.take_off(@plane_double)
-  
-      expect(@plane_double).to have_received(:take_off)
-    end
-
-    it 'should remove planes when they take off' do
-      @airport.land(@plane_double)
-      @airport.take_off(@plane_double)
-  
-      expect(@airport.planes).to eq([])
-    end
-
-    it 'should raise exception if attempts to take off plane that is not in the airport' do
-      expect { @airport.take_off(@plane_double) }.to raise_error('Plane is not in this airport')
+    describe 'When airport is instructed to take off plane that is NOT in airpot' do
+      it 'should raise exception' do
+        expect { @airport.take_off(@plane_double) }.to raise_error('Plane is not in this airport')
+      end
     end
   end
 
