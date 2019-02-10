b92cf0afb80a882abb174a02401e87c1c55c510b
Simon <01secn@gmail.com>
2019-02-10 13:17:03 +0000

    Refactored plane tests with additional nested describes to enable shared test set up for all tests where plane is airborne, and a shared set up for those where plane is on the ground

--- spec/unit/plane_spec.rb
+++ spec/unit/plane_spec.rb
@@ -3,63 +3,67 @@ require './lib/plane.rb'
 describe Plane do
   before(:each) do
     @airport_double = double(:airport, land_plane: "", remove_plane: "")
-    @is_airborne = true
   end
 
-  describe 'plane in sunny conditions' do
+  describe 'Given plane is in sunny conditions' do
     before(:each) do
       @weather_double = double(:weather, is_stormy: false)
-      @plane = Plane.new(@weather_double, @is_airborne)
     end
 
-    it 'should instruct plane to land at the specified airport' do
-      @plane.land(@airport_double)
+    describe 'and plane is airborne, ' do
+      before(:each) do
+        is_airborne = true
+        @plane = Plane.new(@weather_double, is_airborne)
+      end
+
+      it 'should land at the specified airport' do
+        @plane.land(@airport_double)
+    
+        expect(@airport_double).to have_received(:land_plane).with(@plane)
+      end
+
+      it 'should raise exception if attempting to take off (as already airborne)' do
+        expect { @plane.take_off(@airport_double) }.to raise_error('Plane is already airborne')
+      end
+    end
+
+    describe ' and plane is on the ground, ' do
+      before(:each) do
+        is_airborne = false
+        @plane = Plane.new(@weather_double, is_airborne)
+      end
+
+      it 'should, when instructed to take off, instruct airport to remove it from the airport' do
+        @plane.take_off(@airport_double)
   
-      expect(@airport_double).to have_received(:land_plane).with(@plane)
-    end
+        expect(@airport_double).to have_received(:remove_plane).with(@plane)
+      end
 
-    it 'should raise exception if attempting to land plane whilst it is on the ground' do
-      plane = Plane.new(@weather_double)
+      it 'should, on take off, return confirmation that plane is airborne' do
+        expect(@plane.take_off(@airport_double)).to eq(true)
+      end
 
-      expect { plane.land(@airport_double) }.to raise_error('Plane is already on the ground')
-    end
-
-    it 'should, when instructed to take off, instruct airport to remove it from the airport' do
-      is_airborne = false
-      plane = Plane.new(@weather_double, is_airborne)
-
-      plane.take_off(@airport_double)
-
-      expect(@airport_double).to have_received(:remove_plane).with(plane)
-    end
-
-    it 'should, on take off, return confirmation that plane is airborne' do
-      is_airborne = false
-      plane = Plane.new(@weather_double, is_airborne)
-
-      expect(plane.take_off(@airport_double)).to eq(true)
-    end
-
-    it 'should raise exception if attempting to take off whilst it is airborne' do
-      expect { @plane.take_off(@airport_double) }.to raise_error('Plane is already airborne')
+      it 'should raise exception if attempting to land plane (as it is on the ground)' do
+        expect { @plane.land(@airport_double) }.to raise_error('Plane is already on the ground')
+      end
     end
   end
 
-  describe 'plane in stormy conditions' do
+  describe 'Given plane is in stormy conditions, ' do
     before(:each) do
       @weather_double = double(:weather, is_stormy: true)
       @plane = Plane.new(@weather_double, @is_airborne)
     end
 
     it 'should prevent takeoff when weather is stormy' do
-      is_airborne = false
-      plane = Plane.new(@weather_double, is_airborne)
-
-      expect { plane.take_off(@airport_double) }.to raise_error('Cannot take off due to stormy weather')
+      expect { @plane.take_off(@airport_double) }.to raise_error('Cannot take off due to stormy weather')
     end
 
     it 'should prevent landing when weather is stormy' do
-      expect { @plane.land(@airport_double) }.to raise_error('Cannot land due to stormy weather')
+      is_airborne = true
+      plane = Plane.new(@weather_double, is_airborne)
+
+      expect { plane.land(@airport_double) }.to raise_error('Cannot land due to stormy weather')
     end
   end
 end
