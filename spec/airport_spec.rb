37a78934ad7f949475f8605d3744b7b66e566890
Soroush Khosravi <sorush.kh68@gmail.com>
2019-02-10 11:13:42 +0000

    The randomness of the weather conditions in an airport was tested.

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -4,4 +4,10 @@ describe do
   it "Making an airport in the airport class." do
     airport = Airport.new()
   end
+
+  it "tests the randomness of the weather conditions output." do
+    airport = Airport.new()
+    allow(airport).to receive(:rand){1}
+    expect(airport.weather_conditions).to eq "sunny"
+  end
 end
