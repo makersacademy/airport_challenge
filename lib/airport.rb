0171269655511da8d9ec4f0013a6e5bf1df3230b
Steve Kahn <steve@kahn.org.uk>
2019-02-10 10:46:46 +0000

    Created a way for the plane to take off and returrn the plane

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,4 +1,8 @@
 class Airport
   def land(plane)
+    @plane = plane
+  end
+  def take_off(plane)
+    @plane
   end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,8 +1,17 @@
 require 'airport'
-
+require 'plane'
 describe Airport do
-  it "Lets a plane land" do
-    airport = Airport.new
-    expect(airport).to respond_to(:land).with(1).arguments
+  before :each do
+    @airport = Airport.new
   end
+  it "Lets a plane land" do
+    expect(@airport).to respond_to(:land).with(1).arguments
+  end
+
+  it "Lets the same plane land and takeoff" do
+    plane = Plane.new
+    @airport.land(plane)
+    expect(@airport.take_off(plane)).to eq plane
+  end 
+
 end
