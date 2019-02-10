450af1aed2b3d8bd7c9a351b890f14284f698ee5
Jimjule <roadknightj@gmail.com>
2019-02-10 13:06:17 +0000

    Takeoff method pops a plane, Airport responds to #weather

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,5 +1,19 @@
+require "plane.rb"
+
 class Airport
+
+  attr_reader :planes
+
+  def initialize
+    @planes = []
+    @planes << Plane.new
+  end
+
   def takeoff
+    @planes.pop
+  end
+
+  def weather
 
   end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,8 +1,20 @@
 require 'airport.rb'
+require 'plane.rb'
 
 describe Airport do
   it "responds to #takeoff" do
     airport = Airport.new
     expect(airport).to respond_to(:takeoff)
+  end
+
+  it "after #takeoff, confirms plane is not there" do
+    airport = Airport.new
+    airport.takeoff
+    expect(airport.planes.count).to eq(0)
+  end
+
+  it "responds to #weather" do
+    airport = Airport.new
+    expect(airport).to respond_to(:weather)
   end
 end
