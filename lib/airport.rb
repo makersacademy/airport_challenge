d3048555aec352517bef212dd2f3cf680b823051
anu <anu_iv2002@hotmail.com>
2019-02-10 21:25:28 +0000

    adding capacity and weather

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,15 +1,38 @@
 require_relative "plane"
 class Airport
 
+  def initialize
+    @capacity = 5
+    @planes = []
+  end
+
   def land(plane)
+    full?
+    airport = Airport.new
     plane = Plane.new
-    fail ("plane landed") if plane.plane_flying? == false
+    airport.weather("stormy")
     plane.plane_landed
+    @planes << plane
   end
 
   def take_off(plane)
     plane = Plane.new
-    fail ("plane landed") unless plane.plane_flying?
-    plane.plane_flying?
+    airport = Airport.new
+    if plane.plane_flying?
+      plane.plane_takeoff
+    end
+  end
+
+  def weather(random)
+    random = rand(1..2)
+    if random == 1
+      return "stormy"
+    else
+      return "sunny"
+    end
+  end
+
+  private def full?
+    fail "airport is full" if @planes.count >= @capacity
   end
 end
