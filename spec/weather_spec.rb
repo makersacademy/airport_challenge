9397b1eb1c75e29d726b5be03bc86546e2776b21
David Thai <davidthai@dafuloth.me>
2019-02-11 02:08:19 +0000

    need to look into test doubles, stubbing. Instead of hardcoding rand value

--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -1,13 +1,19 @@
 require_relative "../lib/weather"
 
 describe Weather do
-  it { is_expected.to respond_to :stormy? }
 
+  it { is_expected.to respond_to :stormy? }
+  
   it '#stormy returns true if the random number is 0' do
-    
- 
-    # allow(Weather).to receive(:stormy?).and_return true 
-    # allow(stormy?).to receive(:rand)
-    # expect(subject(rand(3)).stormy?). to eq true
+    random_number = 0
+    today = Weather.new(random_number)
+    expect(today.stormy?).to eq true
   end
+
+  it '#stormy returns false if the random number is not 0' do
+    random_number = 3
+    today = Weather.new(random_number)
+    expect(today.stormy?).to eq false
+  end
+
 end
