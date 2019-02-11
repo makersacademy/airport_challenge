d02d139b9cbc6d348975cb0f2c0f35d26fbd7474
Clare Pinder <Clare.pinder@live.co.uk>
2019-02-11 09:12:40 +0000

    new clone to try and push to git master

---
+++ lib/airport.rb
@@ -0,0 +1,36 @@
+class Airport
+  attr_accessor :planes_in_airport
+  attr_accessor :capacity
+
+  def initialize(capacity = DEFAULT_CAPACITY)
+    @planes_in_airport = []
+    @capacity = capacity
+  end
+
+  DEFAULT_CAPACITY = 20
+
+  def land(plane = Plane.new)
+    raise "Turn your plane around." if stormy? || full?
+
+    @planes_in_airport << plane
+  end
+
+  def take_off(plane)
+    raise "Plane cannot take off." if stormy? || empty?
+
+    @planes_in_airport.delete(plane)
+  end
+
+  def stormy?
+    [true, false, false, false, false].sample
+  end
+
+  def full?
+    @planes_in_airport.count >= @capacity
+  end
+
+  def empty?
+    @planes_in_airport.count.zero?
+  end
+
+end
