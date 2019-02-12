dea7c0474109a0e97454494d51ea302f25a22091
muge <muge.hasilci@hotmail.com>
2019-02-12 18:31:07 +0000

    user story 2 s an air traffic controller

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,8 +1,13 @@
 require 'airport'
 describe Airport do
   subject(:airport) { described_class.new}
+
   it 'instructs a plane to land' do
     expect(airport).to respond_to(:land).with(1).argument
+  end
+
+  it 'instruct a plane to take off' do
+    expect(airport).to respond_to(:take_off).with(1).argument
   end
 
 end
