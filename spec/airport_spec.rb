88d77b818a6ea7a35dbc37251730b342b8248bb4
muge <muge.hasilci@hotmail.com>
2019-02-12 14:37:55 +0000

    landed planes go into the hanger

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,8 +2,9 @@ require "airport"
 require "plane"
 
 describe Airport do
-  it 'should take a plane and return it' do
-    plane = Plane.new
-    expect(subject.land_plane(plane)).to eq plane
+  let(:plane) {:plane}
+  it 'should take a plane and add it to the hangar' do
+    expect(subject.land_plane(plane)).to eq [plane]
   end
+
 end
