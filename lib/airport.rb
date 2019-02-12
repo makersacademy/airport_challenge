023f6ff9b7fd6161d77d6c73b08bc2c9ff5866e1
muge <muge.hasilci@hotmail.com>
2019-02-12 14:22:04 +0000

    airport and plane objects are created, land_plane method takes a plane and returns it on airport

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,30 +1,6 @@
-
 class Airport
-     
-  def initialize(capacity)
-    @capacity = capacity
-    @flights = []
+
+  def land_plane(plane)
+    plane
   end
-
-  def land(plane)
-    raise 'No landing permitted due to bad weather' if stormy?
-    raise 'Airport is full' if full?
-    @flights.push(plane)
-
-  end
-
-  def takeoff(plane)
-    raise 'No takeoff permitted due to bad weather' if stormy?
-
-  end
-end
-
-private
-
-def stormy?
-   rand(1..6) > 4
- end
-
-def full?
-  @flights.count >= @capacity
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,51 +1,9 @@
 require "airport"
+require "plane"
 
 describe Airport do
-  subject(:airport) { described_class.new(10) }
-
-  describe "#land" do
-     it 'lands at an airport' do
-       allow(subject).to receive(:stormy?).and_return false
-       expect(subject).to respond_to(:land).with(1).argument
-     end
+  it 'should take a plane and return it' do
+    plane = Plane.new
+    expect(subject.land_plane(plane)).to eq plane
   end
-  describe '#takeoff' do
-    context 'when not stormy' do
-      it 'instructs a plane to take off from an airport' do
-        airport = Airport.new(10)
-        plane = Plane.new
-
-        expect(subject).to respond_to(:takeoff).with(1).argument
-      end
-    end
-    context 'when stormy' do
-      before do
-        allow(subject).to receive(:stormy?).and_return false
-      end
-      it 'prevents take off when weather is stormy' do
-        airport = Airport.new(10)
-        plane = Plane.new
-        expect { subject.takeoff(plane) }.to raise_error  'No takeoff permitted due to bad weather condition'
-      end
-    end
-  end
-
-context 'when stormy' do
-it 'prevents landing when weather is stormy' do
-  airport = Airport.new(10)
-  plane = Plane.new
-  allow(subject).to receive(:stormy?).and_return true
-  expect { subject.land(plane) }.to raise_error "No landing permitted due to bad weather"
-end
-end
-
-
-context 'when full' do
-  it 'prevents landing when the airport is full' do
-    allow(subject).to receive(:stormy?).and_return false
-    plane = double :plane
-    10.times { subject.land(plane) }
-    expect { subject.land(plane) }.to raise_error 'Airport is full'
-  end
-end
 end
