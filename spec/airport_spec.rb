e76d71a47af6ebfcc654a225439022228034caa8
muge <muge.hasilci@hotmail.com>
2019-02-13 17:59:13 +0000

    refactored

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -5,32 +5,35 @@ describe Airport do
   let(:plane) { double :plane}
 
   describe '#land' do
-    it 'instructs a plane to land' do
-      allow(airport).to receive(:stormy?).and_return false
-      expect(airport).to respond_to(:land).with(1).argument
-    end
-
-    context 'when full' do
-      it 'raises an error,does not allow landing' do
+    context 'when not stormy' do
+      it 'instructs a plane to land' do
         allow(airport).to receive(:stormy?).and_return false
-        20.times do
-          airport.land(plane)
+        expect(airport).to respond_to(:land).with(1).argument
+      end
+
+      context 'when full' do
+        it 'raises an error,does not allow landing' do
+          allow(airport).to receive(:stormy?).and_return false
+          20.times do
+            airport.land(plane)
+          end
+          expect { airport.land(plane) }.to raise_error 'Cannot land plane: airport full'
         end
-        expect { airport.land(plane) }.to raise_error 'Cannot land plane: airport full'
       end
     end
+
+  context 'when weather is stormy' do
+    it 'raises an error if asked to land a plane when stormy' do
+      allow(airport).to receive(:stormy?).and_return true
+      expect { airport.land(plane) }.to raise_error 'Cannot land plane: weather is stormy'
+    end
   end
+end
+
 
   describe '#take_off' do
     it 'instruct a plane to take off' do
       expect(airport).to respond_to(:take_off).with(1).argument
-    end
-  end
-
-  context 'when stormy' do
-    it 'raises an error if asked to land a plane when stormy' do
-      allow(airport).to receive(:stormy?).and_return true
-      expect { airport.land(plane) }.to raise_error 'Cannot land plane: weather is stormy'
     end
   end
 end
--- spec/features/user_stories_spec.rb
+++ spec/features/user_stories_spec.rb
@@ -1,10 +1,11 @@
 describe 'user stories' do
+  let(:airport) { Airport.new(20) }
+  let(:plane) { Plane.new }
+
   # As an air traffic controller
   # So I can get passengers to a destination
   # I want to instruct a plane to land at an airport
   it 'so planes land at airports, instruct a plane to land' do
-    airport = Airport.new(20)
-    plane = Plane.new
     allow(airport).to receive(:stormy?).and_return false
     expect { airport.land(plane) }.not_to raise_error
   end
@@ -13,8 +14,6 @@ describe 'user stories' do
   # So I can get passengers on the way to their destination
   # I want to instruct a plane to take off from an airport and confirm that it is no longer in the airport
   it 'so planes to take off from an airport, instruct a plane to take off' do
-    airport = Airport.new(20)
-    plane = Plane.new
     expect { airport.take_off(plane) }.not_to raise_error
   end
 
@@ -22,8 +21,6 @@ describe 'user stories' do
   # To ensure safety
   # I want to prevent landing when the airport is full
   it ' so to prevent landing when the airport is full' do
-    airport = Airport.new(20)
-    plane = Plane.new
     allow(airport).to receive(:stormy?).and_return false
     20.times do
       airport.land(plane)
@@ -36,11 +33,7 @@ describe 'user stories' do
   # I want to prevent landing or takeoff when weather is stormy
 
   it ' does not allow planes to land when stormy' do
-    airport = Airport.new(20)
-    plane = Plane.new
     allow(airport).to receive(:stormy?).and_return true
     expect { airport.land(plane) }.to raise_error "Cannot land plane: weather is stormy"
-
   end
-
 end
