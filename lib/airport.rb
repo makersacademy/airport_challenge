3ca461265a9baa7de26d1a9ef983e31f4f712601
muge <muge.hasilci@hotmail.com>
2019-02-13 14:08:20 +0000

    prevent landing when airport is full

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,5 +1,12 @@
 class Airport
+  def initialize(capacity)
+    @capacity = capacity
+    @number_of_planes_landed = 0
+  end
+
   def land(plane)
+    raise 'Cannot land plane: airport full' if @number_of_planes_landed >= @capacity
+    @number_of_planes_landed += 1
   end
 
   def take_off(plane)
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,6 +1,6 @@
 require 'airport'
 describe Airport do
-  subject(:airport) { described_class.new}
+  subject(:airport) { described_class.new(20) }
 
   it 'instructs a plane to land' do
     expect(airport).to respond_to(:land).with(1).argument
@@ -8,6 +8,13 @@ describe Airport do
 
   it 'instruct a plane to take off' do
     expect(airport).to respond_to(:take_off).with(1).argument
+  end
+
+  it 'does not allow landing at capacity' do
+    20.times do
+      airport.land(:plane)
+    end
+    expect { airport.land(:plane) }.to raise_error 'Cannot land plane: airport full'
   end
 
 end
