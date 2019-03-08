1566d8267403d3fa97ace77a1722f6237ff81383
James <jamesdcpalmer1@gmail.com>
2019-03-08 12:11:42 +0000

    Test for respond to takeoff method written

--- spec/air_traffic_control_spec.rb
+++ spec/air_traffic_control_spec.rb
@@ -19,4 +19,12 @@ describe Control do
 
   end
 
+# As an air traffic controller
+# So I can get passengers on the way to their destination
+# I want to instruct a plane to take off from an airport
+# and confirm that it is no longer in the airport
+
+  it {is_expected.to respond_to(:takeoff)}
+
+
 end
