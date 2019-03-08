cfff908ed71191c563133068dd9685d70d245c78
Sam Ixer <samantha.ixer@johnlewis.co.uk>
2019-03-08 21:33:31 +0000

    Refactored tests to be grouped by method. Also refactored after rubocop

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,24 +1,30 @@
-require 'Airport'
-require 'Plane'
+require 'airport'
+require 'plane'
 
 describe Airport do
-  it { is_expected.to respond_to(:land).with(1).argument }
+  describe '#land' do
+    it { is_expected.to respond_to(:land).with(1).argument }
 
-  it 'returns a success message when we land a plane' do
-    expect(subject.land(Plane.new)).to eq "Plane landed"
+    it 'returns a success message when we land a plane' do
+      expect(subject.land(Plane.new)).to eq "Plane landed"
+    end
   end
 
-  it { is_expected.to respond_to :planes}
+  describe '#planes' do
+    it { is_expected.to respond_to :planes }
 
-  it 'returns no planes when airport is empty' do
-    expect(subject.planes).to eq nil
+    it 'returns no planes when airport is empty' do
+      expect(subject.planes).to eq nil
+    end
+
+    it 'returns a plane when I check what has landed' do
+      my_plane = Plane.new
+      subject.land(my_plane)
+      expect(subject.planes).to eq my_plane
+    end
   end
 
-  it 'returns a plane when I check what has landed' do
-    my_plane = Plane.new
-    subject.land(my_plane)
-    expect(subject.planes).to eq my_plane
+  describe '#take_off' do
+    it { is_expected.to respond_to(:take_off) }
   end
-
-  it { is_expected.to respond_to(:take_off) }
 end
