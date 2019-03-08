f2cb2e6a3e1066f66b0fd90a9bab6ba279c2f6d2
Sam Ixer <samantha.ixer@johnlewis.co.uk>
2019-03-08 21:27:55 +0000

    created tests and passed them for when we check what planes are in the airport

--- lib/Airport.rb
+++ lib/Airport.rb
@@ -1,5 +1,11 @@
 class Airport
+
   def land(plane)
+    @planes = plane
     return "Plane landed"
+  end
+
+  def planes
+    return @planes
   end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -7,4 +7,16 @@ describe Airport do
   it 'returns a success message when we land a plane' do
     expect(subject.land(Plane.new)).to eq "Plane landed"
   end
+
+  it { is_expected.to respond_to :planes}
+
+  it 'returns no planes when airport is empty' do
+    expect(subject.planes).to eq nil
+  end
+
+  it 'returns a plane when I check what has landed' do
+    my_plane = Plane.new
+    subject.land(my_plane)
+    expect(subject.planes).to eq my_plane
+  end
 end
