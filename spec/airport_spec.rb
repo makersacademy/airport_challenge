657655c70682429a1fedeee31b22a19c5069ce25
Student <student@rodrigues-mbp.home>
2019-03-09 15:34:21 +0000

    write failing test for .land return value (RED)

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -11,5 +11,8 @@ describe Airport do
         it 'prints a confirmation message when a plane lands' do
             expect { @airport.land @plane }.to output("Plane landed!\n").to_stdout
         end
+        it 'returns the airport object when a plane lands' do
+            expect( @airport.land @plane ).to eq @airport
+        end
     end
 end
