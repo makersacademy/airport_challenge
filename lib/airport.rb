ded86fa6d8e31c9d41ee1c99db15840f737b5000
Wardah <wlt_93@hotmail.com>
2019-03-09 21:22:15 +0000

    Passes test by giving nil when instructed to take off on an empty runway

--- lib/airport.rb
+++ lib/airport.rb
@@ -8,12 +8,10 @@ class Airport
   end
 
   def instructions(instruction,plane)
-    # plane = Plane.new
      @plane = plane
     if instruction == 'land' 
       @runway << @plane
     elsif instruction == 'takeoff'
-      #l = @runway.length
       if @runway != []
         @runway.take(@runway.length - 1)
       end
