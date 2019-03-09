cc83cb8c7c852499804a8c8042d4617d8aa7003a
Wardah <wlt_93@hotmail.com>
2019-03-09 19:02:04 +0000

    Passes test for landing more than one plane on airport

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,8 +1,16 @@
 require 'airport'
 
 describe Airport do
-  it "Checks if plane lands on an airport" do
+  it "Checks if a plane lands on an airport" do
     plane = Plane.new
     expect(subject.land(plane)).to eq [plane]
   end
+
+  it "Checks if more than one plane can land on an airport" do
+    plane1 = Plane.new
+    subject.land(plane1)
+    plane2 = Plane.new
+    expect(subject.land(plane2)).to eq [plane1, plane2]
+  end
+  
 end
