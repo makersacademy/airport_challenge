111f08cca0c021090aa9dde0541afc3be93b1f86
Student <student@rodrigues-mbp.home>
2019-03-09 15:41:19 +0000

    test passes (GREEN)

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,6 +1,14 @@
 class Airport
+    attr_reader :planes
+
+    def initialize
+        @planes = []
+    end
+
     def land (plane)
+        @planes << plane
         puts 'Plane landed!'
         self
     end
+
 end
