8d3d9ba0d870ecd3f11e7f82c4b0caa817217a68
Student <student@rodrigues-mbp.home>
2019-03-09 15:31:11 +0000

    rewrite test descriptions (REFACTOR)

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,12 +2,14 @@ require 'airport'
 
 describe Airport do 
 
-    before(:each) do
+    before :each do
         @airport = Airport.new
         @plane = Plane.new
     end
 
-    it 'prints a confirmation message when a plane lands' do
-        expect { @airport.land @plane }.to output("Plane landed!\n").to_stdout
+    describe '.land' do
+        it 'prints a confirmation message when a plane lands' do
+            expect { @airport.land @plane }.to output("Plane landed!\n").to_stdout
+        end
     end
 end
