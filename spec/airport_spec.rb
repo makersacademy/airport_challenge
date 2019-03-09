7426fc7793fa58128881682d1586267a1504df46
Student <student@rodrigues-mbp.home>
2019-03-09 15:26:29 +0000

    we no longer need the 'responds to .land' test (REFACTOR)

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -7,10 +7,6 @@ describe Airport do
         @plane = Plane.new
     end
 
-    it 'responds to .land' do
-        expect(subject).to respond_to :land
-    end
-
     it 'prints a confirmation message when a plane lands' do
         expect { @airport.land @plane }.to output("Plane landed!\n").to_stdout
     end
