1d0d70b0f54ca0d676490bd959f70986e56d8722
Sam Ixer <samantha.ixer@johnlewis.co.uk>
2019-03-09 20:09:35 +0000

    Changed tests to add stubs. Added prevention of landing when Stormy

--- lib/Airport.rb
+++ lib/Airport.rb
@@ -3,6 +3,9 @@ class Airport
   attr_reader :airport_planes
 
   def land(plane)
+    weather = check_weather
+    raise "The weather is stormy - no landing allowed" if weather == "Stormy"
+
     @airport_planes = plane
     return "Plane landed"
   end
@@ -26,7 +29,6 @@ class Airport
   def check_weather
     random = Random.rand(1..50)
     return "Sunny" if random != 25
-
     return "Stormy"
   end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -6,8 +6,15 @@ describe Airport do
     it { is_expected.to respond_to(:land).with(1).argument }
 
     it 'returns a success message when we land a plane' do
+      allow(subject).to receive(:check_weather) { "Sunny" }
       expect(subject.land(Plane.new)).to eq "Plane landed"
     end
+
+    it 'should stop a plane from landing when the weather is Stormy' do
+      allow(subject).to receive(:check_weather) { "Stormy" }
+      expect { subject.land(Plane.new) }.to raise_error "The weather is stormy - no landing allowed"
+    end
+
   end
 
   describe '#planes' do
@@ -18,47 +25,54 @@ describe Airport do
     end
 
     it 'returns a plane when I check what has landed' do
+      allow(subject).to receive(:check_weather) { "Sunny" }
       my_plane = Plane.new
       subject.land(my_plane)
       expect(subject.planes).to eq my_plane
     end
+
   end
 
   describe '#take_off' do
     it { is_expected.to respond_to(:take_off).with(1).argument }
 
     it 'returns a confirmation that the plane has taken off' do
+      allow(subject).to receive(:check_weather) { "Sunny" }
       my_plane = Plane.new
       subject.land(my_plane)
       expect(subject.take_off(my_plane)).to eq "Successful take off"
     end
 
     it 'raises an error if I try to make a non-existent plane take off' do
+      allow(subject).to receive(:check_weather) { "Sunny" }
       my_plane = Plane.new
       subject.land(my_plane)
       expect { subject.take_off(Plane.new) }.to raise_error "That plane isn't at the airport"
     end
 
     it 'should confirm that the plane that has just taken off is no longer at the airport' do
+      allow(subject).to receive(:check_weather) { "Sunny" }
       my_plane = Plane.new
       subject.land(my_plane)
       subject.take_off(my_plane)
       expect(subject.airport_planes).not_to eq my_plane
     end
+
+    it 'should stop a plane from taking off when the weather is Stormy' do
+      allow(subject).to receive(:check_weather) { "Sunny" }
+      my_plane = Plane.new
+      subject.land(my_plane)
+      allow(subject).to receive(:check_weather) { "Stormy" }
+      expect { subject.take_off(my_plane) }.to raise_error "The weather is stormy - no take off allowed"
+    end
+
   end
 
   describe '#check_weather' do
     it { is_expected.to respond_to(:check_weather) }
 
     it 'should return either Sunny or Stormy' do
-      expect(subject.check_weather).to satisfy { |result| result = "Sunny" || result = "Stormy" }
-    end
-
-    it 'should stop a plane from taking off when the weather is Stormy' do
-      allow(subject).to receive(:check_weather) { "Stormy" }
-      my_plane = Plane.new
-      subject.land(my_plane)
-      expect { subject.take_off(my_plane) }.to raise_error "The weather is stormy - no take off allowed"
+      expect(subject.check_weather).to eq("Sunny").or eq("Stormy")
     end
 
   end
