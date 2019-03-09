a5cf698eb8b8a97643743ed9e4c374d4090bd1a4
Student <student@rodrigues-mbp.home>
2019-03-09 15:23:25 +0000

    write test for landing confirmation message (RED)

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,7 +1,17 @@
 require 'airport'
 
 describe Airport do 
+
+    before(:each) do
+        @airport = Airport.new
+        @plane = Plane.new
+    end
+
     it 'responds to .land' do
         expect(subject).to respond_to :land
+    end
+
+    it 'prints a confirmation message when a plane lands' do
+        expect { @airport.land @plane }.to output('Plane landed!').to_stdout
     end
 end
