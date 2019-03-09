f9d2d1f46bf9ffdd7307b328330dbf47a0b52b1d
Amy Jordan <amy_jordan@live.co.uk>
2019-03-09 12:55:10 +0000

    Refactored airport_spec tests

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -3,53 +3,55 @@ require 'weather'
 
 describe Airport do
 
-  describe '#land' do
+  context 'weather is clear' do
+    before(:each) do
+      @plane = Plane.new
+      @plane2 = Plane.new
+    end
 
-    it { is_expected.to respond_to(:land).with(1).argument }
-
-    it 'logs one plane has landed if not stormy' do
-      plane = Plane.new
+    it 'allows plane to land and logs landing' do
       expect(subject).to receive(:stormy?).and_return(false)
-      subject.land(plane)
-      expect(subject.hangar).to include plane
+      subject.land(@plane)
+      expect(subject.hangar).to include @plane
     end
 
-    it 'logs a second plane has landed if not stormy' do
-      plane = Plane.new
-      plane2 = Plane.new
+    it 'allows planes to land and logs second landing' do
       2.times { expect(subject).to receive(:stormy?).and_return(false) }
-      subject.land(plane)
-      subject.land(plane2)
-      expect(subject.hangar).to include plane2
-
+      subject.land(@plane)
+      subject.land(@plane2)
+      expect(subject.hangar).to include @plane2
     end
 
-    it 'does not land if weather is stormy' do
-      plane = Plane.new
-      expect(subject).to receive(:stormy?).and_return(true)
-      expect { subject.land(plane) }.to raise_error('Cannot land due to stormy weather')
+    it 'allows plane to take off and logs the departure' do
+      subject.land(@plane)
+      subject.take_off
+      expect(subject.hangar).not_to include @plane
     end
   end
 
-  describe '#take_off' do
+  context 'weather is stormy' do
+    before(:each) do
+      @plane = Plane.new
+    end
 
-    it { is_expected.to respond_to :take_off }
-
-    it 'confirms a plane is no longer in the airport' do
-      plane = Plane.new
-      subject.land(plane)
-      expect(subject).to receive(:stormy?).and_return(false)
-      subject.take_off
-      expect(subject.hangar).not_to include plane
+    it 'does not allow planes to land' do
+      expect(subject).to receive(:stormy?).and_return(true)
+      expect { subject.land(@plane) }.to raise_error('Cannot land due to stormy weather')
     end
 
     it 'does not take off if weather is stormy' do
-      plane = Plane.new
-      subject.land(plane)
+      subject.land(@plane)
       expect(subject).to receive(:stormy?).and_return(true)
       expect { subject.take_off }.to raise_error('Cannot depart due to stormy weather')
     end
+  end
 
+  describe '#land' do
+    it { is_expected.to respond_to(:land).with(1).argument }
+  end
+
+  describe '#take_off' do
+    it { is_expected.to respond_to :take_off }
   end
 
   describe '#stormy?' do
