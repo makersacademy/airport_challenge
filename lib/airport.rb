87b3a03581511902ddd5711a944f3045e0320af2
Riya Dattani <riya_dattani@hotmail.com>
2019-03-09 16:08:34 +0000

    Plane is added to hangar

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,7 +1,17 @@
 require 'plane'
 
 class Airport
+  attr_accessor :hangar
+
+  def initialize
+    @hangar = []
+  end
+
   def land(plane)
-    return "#{plane} has landed"
+    hangar << plane
+    return "Plane has landed"
+  end
+
+  def take_off
   end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,14 +1,27 @@
 require 'airport'
 
 describe Airport do
-  it { is_expected. to respond_to(:land) }
+  it { is_expected.to respond_to(:land) }
 
   describe "#land" do
+
     it "notifies that the plane has landed" do
       plane = Plane.new
-      expect(subject.land(plane)).to eq "#{plane} has landed"
+      expect(subject.land(plane)).to eq "Plane has landed"
     end
+
+    it "adds the plane to the hangar" do
+      plane = Plane.new
+      subject.land(plane)
+      expect(subject.hangar).not_to be_empty
+    end
+
   end
+
+  it { is_expected.to respond_to(:take_off) }
+
+  # describe "#take_off" do
+  # end
 
 
 end
