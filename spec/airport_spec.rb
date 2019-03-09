dbbd3c2967fa7304b0d3ac295a8d61ddc2f49cb0
Amy Jordan <amy_jordan@live.co.uk>
2019-03-09 09:35:36 +0000

    Updated #land method to allow one plane to land

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,4 +1,20 @@
 require 'airport'
 
 describe Airport do
+
+  describe '#land' do
+
+    it { is_expected.to respond_to(:land).with(1).argument }
+
+    it 'logs one plane has landed' do
+      plane = Plane.new
+      expect(subject.land(plane)).to eq plane
+    end
+
+  end
+
+  describe '#take_off' do
+    it { is_expected.to respond_to :take_off }
+  end
+
 end
