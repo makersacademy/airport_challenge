e0372817304a3d1466a7beb4814612c2e78b33dd
James <jamesdcpalmer1@gmail.com>
2019-03-10 13:57:17 +0000

    rubocop

--- spec/air_traffic_control_spec.rb
+++ spec/air_traffic_control_spec.rb
@@ -82,7 +82,6 @@ describe Control do
     end
   end
 
-
  # As the system designer
  # So that the software can be used for many different airports
  # I would like a default airport capacity that can be overridden as appropriate
