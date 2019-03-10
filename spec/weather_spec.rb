415556d0ba25c83f7636f4d059f7652a2f2ab248
Lauren <laurenqurashi@me.com>
2019-03-10 20:54:19 +0000

    Refactoring to pass Rubocop tests, and edited README

--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -1,12 +1,12 @@
 require 'weather.rb' # links the main code to the test file.
 
 describe Weather do
-  before(:each) do # will run these functions before doing each test... pretty jazzy.
+  before(:each) do
     @weather = Weather.new
   end
 
   it 'responds to forecast' do
-    expect(subject).to respond_to :forecast # simply a test that checks it recognises the method.
+    expect(subject).to respond_to :forecast
   end
 
   it 'returns either stormy or clear when forecast is called' do
@@ -14,14 +14,3 @@ describe Weather do
   end
 
 end
-
-# it 'raises a departure error if the weather is stormy' do
-#   @airport.land_plane(@plane)
-#   @airport.weather
-#   expect { @airport.fly_plane }.to raise_error("Stop, no departures or arrivals due to stormy weather!")
-# end
-#
-# it 'raises an arrival error if the weather is stormy' do
-#   @airport.weather
-#   expect { @airport.land_plane(@plane) }.to raise_error("Stop, no departures or arrivals due to stormy weather!")
-# end
