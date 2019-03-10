08e66e6cd0b6a7877160ddbdd5162f9a9347be99
kimdiep <kim.diep.techno@gmail.com>
2019-03-10 13:12:38 +0000

    Added onto weather_status random generation by testing it outputs stormy or sunny at random only

--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -2,6 +2,9 @@ require 'weather'
 
 describe Weather do
 
+  subject(:weather) { Weather.new }
+  let(:model_weather) { double :weather }
+
   it 'creates new weather object' do
     expect(Weather.new.is_a? Weather).to eq true
   end
@@ -11,8 +14,14 @@ describe Weather do
   it { is_expected.to respond_to(:weather_status) }
 
   it 'expects weather_status to return either "stormy" or "sunny" at random' do
-    weather = Weather.new
-    expect(["stormy","sunny"]).to include(weather.weather_status)
+    allow(model_weather).to receive(:weather_status)
+    weather.weather_status
+    expect(["stormy","sunny"]).to include (weather.weather_status)
   end
+
+  # it 'expects stormy? to return `true` if `weather_status` is equal to `stormy`' do
+  #   allow(weather).to receive(:weather_status).and_return("stormy")
+  #   expect(weather.stormy?).to eq true
+  # end
   
 end
