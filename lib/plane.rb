04b4d78c446a9fe4864d3c8df975262df26c9c82
Andrew McLelland <andrew.mclelland23@gmail.com>
2019-03-10 13:18:34 +0000

    Ran rubocop and fixed any linting errors

--- lib/plane.rb
+++ lib/plane.rb
@@ -6,12 +6,16 @@ class Plane
   end
 
   def land(airport)
-    raise StandardError.new("Can't land, weather is stormy") unless airport.weather == "sunny"
+    weather_err_msg = "Can't land, weather is stormy"
+    raise StandardError.new(weather_err_msg) unless airport.weather == "sunny"
+
     @location = airport
   end
 
   def take_off
-    raise StandardError.new("Can't take off, weather is stormy") unless @location.weather == "sunny"
+    weather_err_msg = "Can't take off, weather is stormy"
+    raise StandardError.new(weather_err_msg) unless @location.weather == "sunny"
+
     @location = "flying"
   end
 
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -26,12 +26,12 @@ describe Plane do
   it "Throws error when take_off method is called and @weather attribute for aiport is stormy" do
     airport = instance_double("Airport", :weather => "stormy")
     plane = Plane.new(airport)
-    expect{plane.take_off}.to raise_error(StandardError, "Can't take off, weather is stormy")
+    expect { plane.take_off }.to raise_error(StandardError, "Can't take off, weather is stormy")
   end
   it "Does not update @location attribute when take_off method is called and @weather attribute for aiport is stormy" do
     airport = instance_double("Airport", :weather => "stormy")
     plane = Plane.new(airport)
-    expect{plane.take_off}.to raise_error(StandardError, "Can't take off, weather is stormy")
+    expect { plane.take_off }.to raise_error(StandardError, "Can't take off, weather is stormy")
     expect(plane.location).to eq airport
   end
   it "Throws error when land method is called and @weather attribute for aiport passed in as argument is stormy" do
@@ -39,14 +39,14 @@ describe Plane do
     airport2 = instance_double("Airport", :weather => "stormy")
     plane = Plane.new(airport1)
     plane.take_off
-    expect{plane.land(airport2)}.to raise_error(StandardError, "Can't land, weather is stormy")
+    expect { plane.land(airport2) }.to raise_error(StandardError, "Can't land, weather is stormy")
   end
   it "Does not update @location attribute when land method is called and @weather attribute for aiport passed in as argument is stormy" do
     airport1 = instance_double("Airport", :weather => "sunny")
     airport2 = instance_double("Airport", :weather => "stormy")
     plane = Plane.new(airport1)
     plane.take_off
-    expect{plane.land(airport2)}.to raise_error(StandardError, "Can't land, weather is stormy")
+    expect { plane.land(airport2) }.to raise_error(StandardError, "Can't land, weather is stormy")
     expect(plane.location).to eq "flying"
   end
 end
