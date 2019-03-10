596e71a4d9d3fc5ff5572908c6983a38e0505ea1
Riya Dattani <riya_dattani@hotmail.com>
2019-03-10 00:12:43 +0000

    refactored: plane will not land or take off if it is stormy

--- lib/airport.rb
+++ lib/airport.rb
@@ -2,22 +2,27 @@ require 'plane'
 require 'weather'
 
 class Airport
-  attr_accessor :hangar
+  attr_accessor :hangar, :weather
 
   def initialize
     @hangar = []
+    @weather = Weather.new
   end
 
   def land(plane)
-    hangar << plane
-    return "Plane has landed"
+    if weather.stormy? == true
+      return "There is a storm, cannot land"
+    else hangar << plane
+      return "Plane has landed"
+    end
+
   end
 
   def take_off(plane)
 
     if hangar.include?(plane) == false
       return "Error: Plane is not in hangar"
-    elsif Weather.new.stormy? == true
+    elsif weather.stormy? == true
       return "There is a storm, can't take off"
     else hangar.delete(plane)
       return "Plane has taken off"
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -5,15 +5,20 @@ describe Airport do
   it { is_expected.to respond_to(:take_off) }
 
   describe "#land" do
-    it "notifies that the plane has landed" do
+  # scenario 1: the plane can only land if there is no storm :
+    it "cannot land the plane if it is stormy" do
       plane = Plane.new
-      expect(subject.land(plane)).to eq "Plane has landed"
+      unless Weather.new.stormy? == false
+        expect(subject.land(plane)).to eq "There is a storm, cannot land"
+      end
     end
-
-    it "adds the plane to the hangar" do
+  # scenario 2: the plan can land and can be added to hangar if there is no storm
+    it "adds the plane to the hangar if it is not stormy" do
       plane = Plane.new
-      subject.land(plane)
-      expect(subject.hangar).not_to be_empty
+      unless Weather.new.stormy? == true
+        expect(subject.land(plane)).to eq "Plane has landed"
+        expect(subject.hangar).not_to be_empty
+      end
     end
   end
 
@@ -24,15 +29,8 @@ describe Airport do
       subject.take_off(plane)
       expect(subject.take_off(plane)).to eq "Error: Plane is not in hangar"
     end
-  # scenario 2: the plane will only take_off if it is not stormy:
-    it "plane cannot take off if there is a storm" do
-      plane = Plane.new
-      subject.land(plane)
-      unless Weather.new.stormy? == false
-        expect(subject.take_off(plane)).to eq "There is a storm, can't take off"
-      end
-    end
-  # scenario 3: the plane will take off if it is not stormy:
+
+  #scenario 2: the plane will take off if it is not stormy:
     it "if there is no storm, the plane can take off" do
       plane = Plane.new
       subject.land(plane)
@@ -41,5 +39,5 @@ describe Airport do
       end
     end
   end
-  
+
 end
