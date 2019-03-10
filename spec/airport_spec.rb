3605c995dbfad6875996bc3b82c62d0bd4d5829e
masha-b <mashabudryte@gmail.com>
2019-03-10 14:23:43 +0000

    the planes can land and take off in sunny weather

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,10 +1,18 @@
 require 'airport'
+require 'weather'
+require 'plane'
 describe Airport do
-  it 'lands a plane' do
-    expect(subject).to respond_to(:land).with(1).argument
+  describe '#land' do
+    it { is_expected.to respond_to(:land).with(1).argument }
+    it 'the plane can be landed when the weather is sunny' do
+      expect { subject.land(double(:plane)) }.not_to raise_error
+    end
   end
 
-  it 'takes off a plane' do
-    expect(subject).to respond_to(:take_off).with(1).argument
+  describe '#take_off' do
+    it { is_expected.to respond_to(:take_off).with(1).argument }
+    it 'the plane can take off when the weather is sunny' do
+      expect { subject.take_off(double(:plane)) }.not_to raise_error
+    end
   end
 end
