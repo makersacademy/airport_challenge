9716a869d1c82ce115c3761594dd3081ebec19f3
alexanderchen6 <alexbaggio315@hotmail.com>
2019-03-10 15:27:17 +0000

    finish basic requirements

---
+++ lib/airport.rb
@@ -0,0 +1,56 @@
+class Airport
+attr_reader :planes
+attr_accessor :stormy
+attr_accessor :capacity
+attr_accessor :default_capacity
+
+	def initialize( default_capacity = 20, capacity= default_capacity, stormy = false)
+		@planes = []
+		@stormy = stormy
+		@default_capacity = default_capacity
+		@capacity = capacity
+		# if capacity != nil 
+		# @capacity = capacity  
+		# 	else
+		# @capacity = default_capacity
+		# end 
+	end 
+
+
+	def land(plane)
+		# p @planes.count	
+		raise 'cannot land' if stormy?
+		raise 'Airport full' if full? 
+		@planes << plane 
+
+	end 
+
+	def check 
+		@planes
+	end 
+
+	def takeoff
+		# p @planes
+		raise 'cannot takeoff' if stormy
+		@planes.pop
+		puts 'Plane has taken off'
+	
+	end 
+
+	def stormy?
+		@stormy
+	 #returns true or false
+	end 
+
+	def full?
+		# p @planes
+		# p @planes.count
+		if @planes.count >= @capacity
+			return true
+		end 
+
+	end 
+
+
+
+end 
---
+++ spec/airport_spec.rb
@@ -0,0 +1,77 @@
+require './lib/airport'
+require './lib/plane'
+
+describe Airport do
+	 # let(:default_capacity) { 20 }
+	describe '#landing' do 
+		
+
+		it 'lands a plane' do 
+			plane = Plane.new
+			expect(subject.land(plane)).to eq [plane]
+		end 
+		#do I replace this test?
+
+		# it 'initialize with no plane in airport' do 
+		# expect()
+		# end 
+		it 'check if any plane in airport and returns all planes' do 
+			plane = Plane.new
+			subject.land(plane)
+			expect(subject.check).to eq [plane]
+		end 
+		#maybe move this somewhere
+
+		it 'prevents landing when weather is stormy' do
+			plane = Plane.new
+			subject.stormy = true 
+			expect{subject.land(plane)}.to raise_error 'cannot land'
+		end 
+
+		it 'prevents landing and raises error when airport is full, if not it lands plane' do 
+			# p Airport::DEFAULT_CAPACITY
+			subject.default_capacity.times do 
+				subject.land(Plane.new)
+			end 
+			# p subject.planes
+			expect{subject.land(Plane.new)}.to raise_error 'Airport full'
+
+		end 
+
+	end 
+
+	describe '#takeoff' do
+		it 'takes off a plane and message or confirm plane is no longer in airport' do
+			plane = Plane.new
+			subject.land(plane)
+			expect{subject.takeoff}.to output("Plane has taken off\n").to_stdout
+			expect(subject.planes).to eq []
+		#checks 
+		end 
+
+		it 'prevents takeoff when stormy' do 
+			plane = Plane.new
+			subject.land(plane)
+			subject.stormy = true
+			expect{subject.takeoff}.to raise_error 'cannot takeoff'
+		end 
+	end 
+
+	describe '#stormy?' do 
+		it 'returns weather condition, if stormy or not' do 
+			expect(subject.stormy?).to eq false
+		end 
+	end 
+
+	describe 'default capacity' do 
+		it 'it can change default capacity' do 
+			# p Airport::DEFAULT_CAPACITY
+			# Airport::DEFAULT_CAPACITY = 30 
+
+			# airport = Airport.new()			
+			# 29.times do subject.land(Plane.new)
+			# end 
+			# expect(subject.planes.count).to eq 29
+		end 
+	end 
+end 
