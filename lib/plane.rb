87a436ad08a39cee6191f4764641983a39913428
Paul Martin <communty.hivemind@gmail.com>
2019-03-11 08:23:30 +0000

    implement .flying?, .land and .take_off methods on Plane

--- lib/plane.rb
+++ lib/plane.rb
@@ -1,5 +1,21 @@
 class Plane
+  def initialize
+    @flying = true
+  end
+  
   def flying?
-    true
+    @flying
+  end
+
+  def land
+    raise "That plane is not flying." unless @flying
+
+    @flying = false
+  end
+
+  def take_off
+    raise "That plane is flying." if @flying
+
+    @flying = true
   end
 end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -1,11 +1,53 @@
 require 'plane'
 
 describe Plane do
+  before :each do
+    @plane = Plane.new
+  end
+
   describe '#flying' do
     context "when it's initialized" do
       it "is true" do
-        @plane = Plane.new
         expect(@plane.flying?).to eq true
+      end
+    end
+  end
+
+  describe '#land' do
+    context "plane is flying" do
+      it "sets flying status to false" do
+        @plane.land
+        expect(@plane.flying?).to eq false
+      end
+    end
+
+    context "plane is not flying" do
+      before :each do
+        @plane.land
+      end
+
+      it "raises an error" do
+        expect { @plane.land }.to raise_error("That plane is not flying.")
+      end
+      # also test that @plane.flying == false here?
+    end
+  end
+
+  describe '#take_off' do
+    context "plane is not flying" do
+      before :each do
+        @plane.land
+      end
+      
+      it "sets flying status to true" do
+        @plane.take_off
+        expect(@plane.flying?).to eq true
+      end
+    end
+
+    context "plane is flying" do
+      it "raises an error" do
+        expect { @plane.take_off }.to raise_error("That plane is flying.")
       end
     end
   end
