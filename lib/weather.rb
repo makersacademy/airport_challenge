ff658591c9230838fd0cd3003e74b2dc18669bb1
Amy Jordan <amy_jordan@live.co.uk>
2019-03-12 10:30:43 +0000

    Updated Weather tests to test for randomness of method

--- lib/weather.rb
+++ lib/weather.rb
@@ -1,9 +1,7 @@
 class Weather
 
   def forecast
-    # prefer an if/else statement, but I couldn't get 100% coverage on the tests
-    # unless 'Clear' and 'Stormy' were on one line
-    rand(1..100) > 10 ? "Clear" : "Stormy"
+    Kernel.rand(1..100) > 10 ? "Clear" : "Stormy"
   end
 
 end
--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -5,11 +5,18 @@ describe Weather do
   describe '#forecast' do
     it { is_expected.to respond_to :forecast }
 
-# not sure how to check the random generator to make sure that
-# it is randomly generating either 'Clear' or 'Stormy' unless I create
-# a random method but that doesn't feel like it's part of the Weather class
     it "should randomly return 'Clear' or 'Stormy'" do
       expect(subject.forecast).to eq('Clear').or eq('Stormy')
+    end
+
+    it "returns 'Stormy' when randomness is less than 10" do
+      allow(Kernel).to receive(:rand).and_return 7
+      expect(subject.forecast).to eq('Stormy')
+    end
+
+    it "returns 'Clear' when randomness is more than 10" do
+      allow(Kernel).to receive(:rand).and_return 67
+      expect(subject.forecast).to eq('Clear')
     end
   end
 end
