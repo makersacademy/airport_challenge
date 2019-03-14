bc43cd088660e0b00fb25c740ee36c3b90b806dc
Amy Jordan <amy_jordan@live.co.uk>
2019-03-14 09:19:27 +0000

    Added land/take off methods for Plane and raise error if already in airport

--- lib/plane.rb
+++ lib/plane.rb
@@ -1,2 +1,24 @@
 class Plane
+
+  attr_reader :airport
+
+  def initialize
+    @flying = true
+  end
+
+  def land(airport)
+    raise 'Plane already landed at another airport' unless @airport.nil?
+    @flying = false
+    @airport = airport
+  end
+
+  def take_off
+    @flying = true
+    @airport = nil
+  end
+
+  def flying?
+    @flying
+  end
+
 end
