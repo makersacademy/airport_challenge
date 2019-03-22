ec2ec6295e4c66965a7a04e8165b3cc74d691db5
Josh Glasson <Josh.Glasson@tesco.com>
2019-03-22 14:16:51 +0000

    refactored some tests

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -5,15 +5,14 @@ describe Airport do
 
   describe '#landing planes' do
     it 'accepts planes to land at airport' do
-      plane = Plane.new
-      allow(plane).to receive(:flying).and_return(true)
-      plane.land
+      plane = double(:plane)
+      allow(plane).to receive(:landing?).and_return(true)
       subject.land_on_runway(plane)
-      expect(subject.planes.map { |airplane| airplane.landing? }).to eq [true]
+      expect(subject.planes).to include plane
     end
 
     it 'only accepts planes that have been told to land' do
-      plane = Plane.new
+      plane = double(:plane)
       allow(plane).to receive(:landing?).and_return(false)
       expect { subject.land_on_runway(plane) }.to raise_error "Plane not told to land"
     end
@@ -22,15 +21,15 @@ describe Airport do
 
   describe '#taking off planes' do
     it 'checks plane is gone after take off' do
-      plane = Plane.new
-      plane.take_off
+      plane = double(:plane)
+      allow(plane).to receive(:take_off).and_return(true)
       subject.planes << plane
       subject.take_off_from_runway(plane)
-      expect(subject.planes).not_to include [plane]
+      expect(subject.planes).not_to include plane
     end
 
     it 'only lets planes take off if told to' do
-      plane = Plane.new
+      plane = double(:plane)
       allow(plane).to receive(:take_off).and_return(false)
       expect { subject.take_off_from_runway(plane) }.to raise_error "Plane not told to take off"
     end
@@ -39,7 +38,7 @@ describe Airport do
   describe '#airport capacity' do
     it 'has a capacity' do
       ap = Airport.new(30)
-      plane = Plane.new
+      plane = double(:plane)
       allow(plane).to receive(:landing?).and_return(true)
       ap.capacity.times { ap.land_on_runway(plane) }
       expect { ap.land_on_runway(plane) }.to raise_error "Airport at capacity"
