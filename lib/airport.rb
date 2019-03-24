bfb08720fd71a6811b8c87c1b9764cf251efa8f9
BallSolHard <soleymanshahir@yahoo.com>
2019-03-24 21:55:34 +0000

    introduced single responisibility methods for empty and full airport

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,23 +1,41 @@
 require_relative 'plane'
 
 class Airport
+
+  def initialize
+    @planes = []
+  end
+
   attr_reader :plane
 
   def landing_plane
   end
 
   def airport_capacity(plane)
-    fail 'Airport is full' if @plane
-    @plane = plane
-  end 
+    fail 'Airport is full' if empty?
+    @planes.pop
+  end
 
   def land(plane)
     fail 'Stormy weather, do not land!' unless @plane
+    fail 'Airport is full' if full?
     @plane = plane
+    @planes << plane
   end
 
   def take_off(plane)
     fail 'Stormy weather, do not take off!' unless @plane
     @plane
   end
+
+private
+
+def full?
+  bikes.count >= 20
+end
+
+def empty?
+  @planes.empty?
+end
+
 end
