13332e7dd58d95b10f989eacc0d5de656a36c979
Joao Abbott-Gribben <joao.abbott.gribben@gmail.com>
2019-03-27 14:35:48 +0000

    refactor: Airport unit test for land refactored to use context block

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -5,21 +5,33 @@ describe Airport do
   let(:plane) { double(:plane) }
   let(:weather) { double :weather }
 
+describe '#land' do
+  context 'in good weather' do
+    before do
+      allow(weather).to receive(:stormy?).and_return false
+    end
 
-  it 'receives a plane with #land' do
-    allow(weather).to receive(:stormy?).and_return false
-    airport.land(plane)
-    expect(airport.hangar).to eql([plane])
+    it 'receives a plane with #land' do
+      airport.land(plane)
+      expect(airport.hangar).to eql([plane])
+    end
+
   end
+
+  context 'in bad weather' do
+    before do
+      allow(weather).to receive(:stormy?).and_return true
+    end
+
+     it 'raises an error' do
+       expect { airport.land(plane) }.to raise_error 'The weather is too stormy, you cannot land'
+     end
+  end
+end
 
   it 'only allows #take_off in sunny weather' do
     allow(weather).to receive(:stormy?).and_return true
     expect { airport.take_off(plane) }.to raise_error("The weather is too stormy, you cannot take off")
-  end
-
-  it 'only allows #land in sunny weather' do
-    allow(weather).to receive(:stormy?).and_return true
-    expect { airport.land(plane) }.to raise_error 'The weather is too stormy, you cannot land'
   end
 
   it 'only allows 5 planes to land' do
