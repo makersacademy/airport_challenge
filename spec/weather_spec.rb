129871e9d8457b655e5fd030cad4cc28392895a6
Joao Abbott-Gribben <joao.abbott.gribben@gmail.com>
2019-03-27 14:00:31 +0000

    test: Weather #stormy method can not be stormy

--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -7,5 +7,10 @@ describe Weather do
     allow(Kernel).to receive(:rand).and_return 10
     expect(weather.stormy?).to be true
   end
+
+  it 'can be not stormy' do
+    allow(Kernel).to receive(:rand).and_return 1
+    expect(weather.stormy?).to be false
+  end
   
 end
