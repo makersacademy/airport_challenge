84bc388bab0d12169c016c252fcc47eda9756422
Alasdair Wem <alasdair123@hotmail.co.uk>
2019-04-06 18:20:00 +0100

    test land method can land instance of plane

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,8 +2,13 @@ require 'airport'
 require 'plane'
 
 describe Airport do
-  
-  it 'should respond to land_plane' do
-    expect(subject).to respond_to :land_plane
+
+  it 'should respond to #land' do
+    expect(subject).to respond_to :land
+  end
+
+  it 'should land a plane' do
+    plane = Plane.new
+    expect(subject.land(plane)).to eq plane
   end
 end
