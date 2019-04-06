5cf3b814f811e3d5a921fddadd576d063607c283
Ayelisha Amoah <ayelisha.amoah@gmail.com>
2019-04-06 14:03:27 +0100

    Added require plane file and updated land method to return a plane

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,4 +2,11 @@ require 'airport'
 
 describe Airport do
   it { is_expected.to respond_to(:land).with(1).argument }
+
+  describe '#land' do
+    it 'should allow a user to land a plane' do
+      plane = Plane.new
+      expect(subject.land(plane)).to eq plane
+    end
+  end
 end
