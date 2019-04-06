4ba0ed54ce691e07b5711cf8154fe4f09cabd2f7
Ayelisha Amoah <ayelisha.amoah@gmail.com>
2019-04-06 14:07:23 +0100

    Removed obsolete respond to test

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,8 +1,6 @@
 require 'airport'
 
 describe Airport do
-  it { is_expected.to respond_to(:land).with(1).argument }
-
   describe '#land' do
     it 'should allow a user to land a plane' do
       plane = Plane.new
