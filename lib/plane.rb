49c2d91634cbecf68372b65eb6c5e73b2ed55689
Student <student@Makerss-MacBook-Pro.local>
2019-04-06 18:34:56 +0100

    created plane class

---
+++ lib/plane.rb
@@ -0,0 +1,7 @@
+class Plane
+
+  def land
+    "landing"
+  end
+
+end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -1,1 +1,9 @@
 require 'plane'
+
+describe Plane do
+  describe '#land' do
+    it 'tells the plane to land' do
+      expect(subject.land).to eq "landing"
+    end
+  end
+end
