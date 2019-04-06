bfa135cbfb0f7a83dc1d6102f8658cf824dcec42
sejinkay <sejinkay@gmail.com>
2019-04-06 16:03:41 +0100

    added possible error messages

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -6,14 +6,30 @@ describe Airport do
       subject.landing(Plane.new)
       expect(subject.landing(Plane.new)).to eq @plane
     end
+
+    # it "raises an error when the weather is stormy" do
+    #   weather = "stormy"
+    #   expect{subject.landing(Plane.new)}.to raise_error 'Not a good weather to land'
+    # end
+    #
+    # it "raises an error when the airport is full" do
+    #   capacity.full?
+    #   expect{subject.landing(Plane.new)}.to raise_error 'No space for landing'
+    # end
+
   end
 
-  # describe '#take_off' do
-  #   plane = Plane.new
-  #   subject.take_off(plane)
-  #   it 'takes off the plane' do
-  #     expect(subject.take_off(plane)).to eq Plane.new
-  #   end
-  # end
+  describe '#take_off' do
+    plane = Plane.new
+    subject.take_off(plane)
+    it 'takes off the plane' do
+      expect(subject.take_off(plane)).to eq Plane.new
+    end
+
+    # it "raises an error when the weather is stormy" do
+    #   weather = "stormy"
+    #   expect{subject.take_off(plane)}.to raise_error 'Not a good weather to fly'
+    # end
+  end
 
 end
