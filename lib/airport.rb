df8ca6c2fce16f70da2e649a4e1e690686b505c3
Oliver Cripps <ollycripps@gmail.com>
2019-04-06 16:54:43 +0100

    add full? method to understand airport capacity

--- lib/airport.rb
+++ lib/airport.rb
@@ -6,11 +6,17 @@ class Airport
   end
 
   def land_plane(plane)
-    fail "Airport is full" if @hanger.count >= @capacity
+    fail "Airport is full" if full?
     @hanger << plane
   end
 
   def take_off(plane)
+  end
+
+  private
+
+  def full?
+    @hanger.count >= @capacity
   end
 
 end
