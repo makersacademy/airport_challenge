25ce199c72c1b72ed9d2d7bd1db0ed7ea79c3e72
Oliver Cripps <ollycripps@gmail.com>
2019-04-06 16:17:05 +0100

    add feature/unit test for take_off method

--- lib/airport.rb
+++ lib/airport.rb
@@ -3,4 +3,7 @@ class Airport
   def land_plane(plane)
   end
 
+  def take_off(plane)
+  end
+
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -4,8 +4,12 @@ describe Airport do
 
   subject(:airport) { described_class.new }
 
-  it 'lands a plane at an airport' do
+  it 'tells a plane to land at an airport' do
     expect(airport).to respond_to(:land_plane).with(1).argument
+  end
+
+  it 'tells a plane to take off from an airport' do
+    expect(airport).to respond_to(:take_off).with(1).argument
   end
 
 end
