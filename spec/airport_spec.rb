0e4d56e3cf6ce28aa431036926489f5914778747
Oliver Cripps <ollycripps@gmail.com>
2019-04-06 17:01:11 +0100

    refactor test structure

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -3,21 +3,27 @@ require 'airport'
 describe Airport do
 
   subject(:airport) { described_class.new(50) }
+  let(:plane) { double :plane }
 
-  it 'tells a plane to land at an airport' do
-    expect(airport).to respond_to(:land_plane).with(1).argument
-  end
-
-  it 'tells a plane to take off from an airport' do
-    expect(airport).to respond_to(:take_off).with(1).argument
-  end
-
-  it 'will not let planes land when full' do
-    plane = double :plane
-    50.times do
-      airport.land_plane(plane)
+  describe '#land' do
+    it 'tells a plane to land at an airport' do
+      expect(airport).to respond_to(:land_plane).with(1).argument
     end
-    expect { airport.land_plane(plane) }.to raise_error "Airport is full"
+
+    context 'when full' do
+      it 'raises an error' do
+        50.times do
+          airport.land_plane(plane)
+        end
+        expect { airport.land_plane(plane) }.to raise_error "Airport is full"
+      end
+    end
+  end
+
+  describe '#take_off' do
+    it 'tells a plane to take off from an airport' do
+      expect(airport).to respond_to(:take_off).with(1).argument
+    end
   end
 
 end
