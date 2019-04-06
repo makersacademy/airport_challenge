f369234f89ea431458a370e806b430865150352f
Hannah Frost <h.7frost@gmail.com>
2019-04-06 18:42:04 +0100

    plane saved in airport, rspec test passes

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -6,6 +6,11 @@ describe Airport do
     it 'lands a plane' do
       expect(subject).to respond_to(:land_plane).with(1).argument
     end
-  end
 
+    it 'stores a plane at the airport' do
+      plane = Plane.new
+      subject.land_plane(plane)
+      expect(subject.planes).to eq [plane]
+    end
+  end
 end
