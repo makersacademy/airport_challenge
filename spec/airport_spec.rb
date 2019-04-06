7ba2b7d1bcb80824b87f4f85a64ab762d3a1cda6
Ayelisha Amoah <ayelisha.amoah@gmail.com>
2019-04-06 14:20:06 +0100

    Added take_off method

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,10 +1,20 @@
 require 'airport'
 
 describe Airport do
+
   describe '#land' do
     it 'should allow a user to land a plane' do
       plane = Plane.new
       expect(subject.land(plane)).to eq plane
     end
   end
+
+  describe '#take_off' do
+    it 'should allow to instruct a plane to take off' do
+      plane = Plane.new
+      subject.land(plane)
+      expect(subject.take_off).to eq plane
+    end
+  end
+
 end
