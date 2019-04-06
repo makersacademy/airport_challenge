e10a91c60bfab486515b7faeebac6d969bb6795b
sarpong <abasimisarpong@rocketmail.com>
2019-04-06 17:36:09 +0100

    included private method to enforce single resposibility principle

--- lib/airport.rb
+++ lib/airport.rb
@@ -2,16 +2,30 @@ require_relative 'plane'
 
 class AirPort
 
+  attr_reader :landed_plane
+
   def initialize
-    @track_landed_plane = []
+    @landed_plane = []
   end
 
   def land(plane)
-    @track_landed_plane.push(plane)
+    @landed_plane.push(plane)
   end
 
   def take_off(plane)
-    plane
+    fail 'There are currently no planes' if @landed_plane.empty?
+    
+    delete_plane(plane)
+    confirm?(plane)
   end
 
+  private
+  def confirm?(plane)
+    true unless @landed_plane.include?(plane)
+  end
+
+  def delete_plane(plane)
+    plane_id = @landed_plane.index(plane)
+    @landed_plane.delete_at(plane_id)
+  end
 end
