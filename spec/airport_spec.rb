83547ccc66f6bdf4ae19f62f78f6dc4b35b47b17
Alasdair Wem <alasdair123@hotmail.co.uk>
2019-04-06 17:50:38 +0100

    make airport respond to land_plane test pass

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,8 +1,9 @@
 require 'airport'
 require 'plane'
 
-describe 'airport' do
- it 'should respond to land_plane' do
-   expect(subject).to respond_to :land_plane
- end
-end 
+describe Airport do
+  
+  it 'should respond to land_plane' do
+    expect(subject).to respond_to :land_plane
+  end
+end
