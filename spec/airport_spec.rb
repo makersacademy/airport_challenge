c5f63cdce4dc5ce84cd4217aabe90588fb5b81c8
Callum Marshall <callum_marshall@hotmail.co.uk>
2019-04-06 16:30:00 +0100

    pass recieve and release

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -3,8 +3,21 @@ require 'airport'
 describe Airport do
   it { is_expected.to respond_to(:recieve_plane) }
   it { is_expected.to respond_to(:release_plane) }
-  # describe '#land' do
-  #
-  # end
+
+  describe '#recieve_plane' do
+    it 'recieves plane' do
+      plane = Plane.new
+      expect(subject.recieve_plane(plane)).to eq [plane]
+    end
+
+  end
+
+  describe '#release_plane' do
+    it 'releases plane' do
+      plane = Plane.new
+      subject.recieve_plane(plane)
+      expect(subject.release_plane).to eq plane
+    end
+  end
 
 end
