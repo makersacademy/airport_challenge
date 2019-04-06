98ba1e21756ee39dcd5f20a269ac68d8ca095a47
Alasdair Wem <alasdair123@hotmail.co.uk>
2019-04-06 18:26:34 +0100

    add test for take_off method and pass it

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -7,8 +7,22 @@ describe Airport do
     expect(subject).to respond_to :land
   end
 
-  it 'should land a plane' do
-    plane = Plane.new
-    expect(subject.land(plane)).to eq plane
+  describe '#land' do
+    it 'should land a plane' do
+      plane = Plane.new
+      expect(subject.land(plane)).to eq plane
+    end
   end
+
+  describe '#take_off' do
+    it 'should allow a plane to take_off' do
+      plane = Plane.new
+      expect(subject.take_off(plane)). to eq plane
+    end
+  end
+
+
+
+
+
 end
