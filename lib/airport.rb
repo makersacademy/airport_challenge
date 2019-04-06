c1923722138ccff6943759654b6268180acc86c6
Callum Marshall <callum_marshall@hotmail.co.uk>
2019-04-06 16:59:33 +0100

    add capacity and full? method

--- lib/airport.rb
+++ lib/airport.rb
@@ -7,10 +7,50 @@ class Airport
   end
 
   def recieve_plane(plane)
+    fail "airport is full" if full?
+
     @planes << plane
   end
 
   def release_plane
+    fail "airport is empty" if empty?
+
     @planes.pop
   end
 end
+
+private
+
+CAPACITY = 20
+
+def full?
+  @planes.count >= CAPACITY
+end
+
+def empty?
+  @planes.count < 1
+end
+
+# def initialize
+#   @bikes = []
+# end
+#
+# def release_bike
+#   fail 'No bikes available' if empty?
+#   @bikes.pop
+# end
+#
+# def dock(bike)
+#   fail "docking station is full" if full?
+#   @bikes << bike
+# end
+#
+# private
+#
+# def full?
+#   @bikes.count >= 20
+# end
+#
+# def empty?
+#   @bikes.empty?
+# end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,13 +1,18 @@
 require 'airport'
 
 describe Airport do
-  it { is_expected.to respond_to(:recieve_plane) }
+  it { is_expected.to respond_to(:recieve_plane).with(1).argument }
   it { is_expected.to respond_to(:release_plane) }
 
   describe '#recieve_plane' do
     it 'recieves plane' do
       plane = Plane.new
       expect(subject.recieve_plane(plane)).to eq [plane]
+    end
+
+    it "raises error when airport is full" do
+      20.times { subject.recieve_plane Plane.new }
+      expect { subject.recieve_plane(Plane.new) }.to raise_error("airport is full")
     end
 
   end
@@ -18,6 +23,11 @@ describe Airport do
       subject.recieve_plane(plane)
       expect(subject.release_plane).to eq plane
     end
+
+    it "raises error when airport is empty" do
+      expect { subject.release_plane }.to raise_error("airport is empty")
+    end
+
   end
 
 end
