8a0bf1d268ebd50807eecbd01c8bc0e8d1f9f6aa
Calum Darroch <calum_darroch@hotmail.com>
2019-04-06 21:25:50 +0100

    Airport responds to land_plane method

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -4,7 +4,7 @@ describe Airport do
 
   describe "#land_plane" do
     it "allows a plane to land" do
-      it {is_expected.to respond_to(:land_plane)}
+      expect(subject).to respond_to :land_plane
     end
   end
 end
