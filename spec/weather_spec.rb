a2423e88c110e5f640253a35e4236ef637ec8ad2
shektor <44544977+shektor@users.noreply.github.com>
2019-04-07 22:08:23 +0100

    Tests moved to plane_spec

--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -1,17 +1,4 @@
 require 'weather'
 
 describe Weather do
-  describe 'stormy?' do
-    it 'is true when random number generator returns > 95' do
-      allow(subject).to receive(:random_integer) { 96 }
-      expect(subject).to be_stormy
-    end
-    it 'is false when random number generator returns <= 95' do
-      allow(subject).to receive(:random_integer) { 95 }
-      expect(subject).not_to be_stormy
-    end
-  end
-  describe '#random_integer' do
-    it { expect(subject.random_integer).to be_integer }
-  end
 end
