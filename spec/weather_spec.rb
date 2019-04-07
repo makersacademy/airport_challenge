78e8e74cf7511b980c1125b0a13cf401e28b8d84
shektor <44544977+shektor@users.noreply.github.com>
2019-04-07 20:39:24 +0100

    Create tests for stormy? and random_integer

--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -1,5 +1,17 @@
 require 'weather'
 
 describe Weather do
-
+  describe 'stormy?' do
+    it 'should be true when random number generator returns > 95' do
+      allow(subject).to receive(:random_integer) { 96 }
+      expect(subject).to be_stormy
+    end
+    it 'should be false when random number generator returns <= 95' do
+      allow(subject).to receive(:random_integer) { 95 }
+      expect(subject).not_to be_stormy
+    end
+  end
+  describe '#random_integer' do
+    it { expect(subject.random_integer).to be_integer }
+  end
 end
