9e5ce9fca1059a2b28cb24872da6c2470cbabf3b
shektor <44544977+shektor@users.noreply.github.com>
2019-04-07 19:37:31 +0100

    Change location attr to airport and update tests and methods

--- lib/plane.rb
+++ lib/plane.rb
@@ -1,22 +1,22 @@
 require_relative 'airport'
 
 class Plane
-  attr_reader :location
+  attr_reader :airport
 
   def initialize
-    @location = "In flight"
+    # @airport = nil
   end
 
   def land(airport)
-    raise("Plane not in flight") unless @location == "In flight"
+    raise("Plane not in flight") unless @airport.nil?
     raise("Airport does not exist") unless airport.instance_of? Airport
 
-    @location = airport
+    @airport = airport
   end
 
   def take_off
-    raise("Plane not at an airport") if @location == "In flight"
+    raise("Plane not at an airport") if @airport.nil?
 
-    @location = nil
+    @airport = nil
   end
 end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -27,20 +27,20 @@ describe Plane do
       expect { subject.take_off }.to raise_error("Plane not at an airport")
     end
   end
-  describe '#location' do
-    it 'should return current plane location' do
-      expect(subject.location).to eq "In flight"
-    end
-    it 'should return the airport it last landed at' do
-      airport = Airport.new
-      subject.land(airport)
-      expect(subject.location).to eq airport
-    end
-    it 'should no longer be in the airport after landing and taking off' do
-      airport = Airport.new
-      subject.land(airport)
-      subject.take_off
-      expect(subject.location).to eq nil
-    end
-  end
+  # describe '#in_airport?' do
+  #   it 'should initialize as false' do
+  #     expect(subject.in_airport?).to eq false
+  #   end
+  #   it 'should return the airport it last landed at' do
+  #     airport = Airport.new
+  #     subject.land(airport)
+  #     expect(subject.airport).to eq airport
+  #   end
+  #   it 'should no longer be in the airport after landing and taking off' do
+  #     airport = Airport.new
+  #     subject.land(airport)
+  #     subject.take_off
+  #     expect(subject.airport).to eq nil
+  #   end
+  # end
 end
