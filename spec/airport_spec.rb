79152817a4fbc8bbc0c82bc3e9bd1802c33d29f3
Nandini Patel <nandini_patel96@hotmail.co.uk>
2019-04-07 12:19:53 +0100

    added test doubles

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,16 +2,16 @@ require 'airport'
 
 describe Airport do
 
+ plane = double('plane')
+
   describe '#land' do
     it 'lands plane at the airport' do
-      plane = 'plane'
       expect(subject.land(plane)).to eq 'plane'
     end
   end
 
   describe '#takeoff' do
     it 'plane can take off from airport' do
-      plane = 'plane'
       expect(subject.takeoff(plane)). to eq 'plane'
     end
   end
