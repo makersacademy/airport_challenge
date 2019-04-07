f56377a3bf65c4cb12da28754ea08e5f6e5551a6
sarpong <abasimisarpong@rocketmail.com>
2019-04-07 13:17:15 +0100

    no test failing

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -5,28 +5,36 @@ RSpec.describe AirPort do
   RSpec.configure do |config|
     config.example_status_persistence_file_path = './spec/fail_test.txt'
   end 
-
+  subject(:airport){described_class.new(10)}
   let(:plane) { Plane.new }
 
   describe '#land_plane' do
     it 'should be able to land plane' do
-      expect(subject.land(plane)).to eq([plane])
+      allow(airport).to receive(:bad_weather?).and_return(false)
+      expect(airport.land(plane)).to eq([plane])
+    end
+    it ' should raise and error when there is a stormy weather' do
+      allow(airport).to receive(:bad_weather?).and_return(true)
+      expect { airport.land(plane) }.to raise_error 'Sorry: can not land plane due to stormy weather' 
+    end
+
+    it 'should raise an error when airport is full' do
+      allow(airport).to receive(:bad_weather?).and_return(false)
+      10.times {airport.land(plane)}
+      expect { airport.land(plane) }. to raise_error 'Sorry: can not land plane, airport is full.' 
     end
   end
 
   describe '#take_off' do
-    it 'raises an error if there is no plane' do
-      expect { subject.take_off(plane) }.to raise_error 'There are currently no planes'
-    end
     it 'raises and error if the weather is stormy' do
-      allow(subject).to receive(:good_weather?).and_return true
-      expect { subject.take_off(plane) }.to raise_error 'The weather is stormy'
+      allow(airport).to receive(:bad_weather?).and_return true
+      expect { airport.take_off(plane) }.to raise_error 'The weather is stormy'
     end
   end
 
-  describe '#landed_plane' do
+  describe '#plane' do
     it 'should return an array' do
-      expect(subject.landed_plane).to eq(Array.new)
+      expect(airport.plane).to eq(Array.new)
     end
   end
 end
