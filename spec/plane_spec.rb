252f42aeb69228fe907a2e827b6719b6dda6198f
shektor <44544977+shektor@users.noreply.github.com>
2019-04-07 13:04:41 +0100

    Add test to land at an Airport

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -3,5 +3,9 @@ require 'plane'
 describe Plane do
   describe '#land' do
     it { expect(subject).to respond_to(:land).with(1).argument }
+    it 'should land at an airport' do
+      airport = Airport.new
+      expect(subject.land(airport)).to eq "The plane has landed at #{airport}"
+    end
   end
 end
