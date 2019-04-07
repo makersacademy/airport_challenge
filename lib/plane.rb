4151c993cdb442cafb6a83b7abd3af7467b8046a
shektor <44544977+shektor@users.noreply.github.com>
2019-04-07 15:29:20 +0100

    Add take_off and update land to store airport

--- lib/plane.rb
+++ lib/plane.rb
@@ -2,6 +2,11 @@ require_relative 'airport'
 
 class Plane
   def land(airport)
-    "The plane has landed at #{airport}"
+    @location = airport
+    "The plane has landed at #{@location}"
+  end
+
+  def take_off
+    "The plane has taken off from #{@location}"
   end
 end
