13c1d7348cb0123f76207234fcc7fe0ded0247c5
domw30 <domw30@gmail.com>
2019-04-07 13:25:43 +0100

    refactor feature test

--- spec/feature/feature_test_spec.rb
+++ spec/feature/feature_test_spec.rb
@@ -53,9 +53,4 @@ describe 'user stories' do
       expect { airport.land(plane) }.to raise_error 'Plane unable to land, weather is stormy'
     end
   end
-
-  # As the system designer
-  # So that the software can be used for many different airports
-  # I would like a default airport capacity
-  # that can be overridden as appropriate
 end
