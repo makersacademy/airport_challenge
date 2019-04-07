24c1ea02edeaffb27cd0d7f08aa36a4df98af0dd
yadlra <yadi.sb1@gmail.com>
2019-04-07 17:48:39 +0100

    more changes

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,10 +1,40 @@
+require_relative "weather"
+
 class Airport
-  attr_reader :planes
-  def initialize
+  Full_Capacity = 50
+
+  attr_reader = capacity
+
+  def initialize(capacity = Full_Capacity)
+    @capacity = capacity
+    @weather = weather
     @planes = []
   end
-  
+
   def land_plane(plane)
+    fail "Stormy weather. Unable to fly!" if @weather.stormy?
+    fail "Full Airport. Keep flying!" if full?
+    fail "Plane is at the airport!" if occupied_space?(plane)
+    plane.landed?
     @planes << plane
   end
+
+  def take_off(plane)
+    fail "Stormy weather. Unable to fly!" if @weather.stormy?
+    fail "There are no planes!" unless occupied_space?(plane)
+    plane.taking_off
+    @planes.delete(plane)
+  end
+
+  private
+  #Other useful methods to link everything
+
+    def occupied_space?(plane)
+      @planes.include?(plane)
+    end
+
+    def full?
+      @planes.size >= @capacity
+    end
+
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,16 +1,60 @@
 require 'airport'
+
 describe Airport do
-  describe '#land_plane' do
+let(:weather_go) {double(:weather, stormy?: false)}
+let(:weather_awful) {double(:weather, stormy?: true)}
+let(:plane) {double(:plane, landed?: (), taking_off: ())}
+
+context "if there is no storm" do
+  subject do
+    appointed_class.new(appointed_class::Full_Capacity, weather_go)
+  end
+
+end
+   describe '#land_plane' do
     it "instructs a plane to land at an airport" do
       expect(subject).to receive(:land_plane)
       subject.land_plane
     end
 
     it "returns a status of landed" do
-      plane = double(:plane)
-      allow(plane).to receive(:land)
-      subject.land_plane plane
-      expect(subject.planes).to include plane 
+      subject.land_plane (@plane)
+      expect(subject.planes).to include(@plane)
+    end
+
+    it "avoids landing as plane already landed" do
+      subject.land_plane(plane)
+      expect(subject.land_plane(plane)).to raise_error('Plane is at the airport!'[occupied_space?])
+    end
+
+     it "gives error if airport is full" do
+       appointed_class::Full_Capacity.times do
+       plane_here = double(:plane, landed?: (), taking_off: ())
+       subject.land_plane(plane_here)
+       expect(subject.land_plane(plane_here).to raise_error('Full Airport. Keep flying!')[full?]
+     end 
+   end
+end
+
+  describe '#take_off' do
+    it "gives error if plane is not at the airport" do
+      expect(subject.take_off(@plane)).to raise_error("There are no planes!")
+    end
+
+    it "gives instruction to take off" do
+      expect(subject).to receive(:take_off)
+      subject.take_off(@plane)
+
+    it  "makes sure the plane has taken off" do
+      subject.land_plane(@plane)
+      subject.take_off(@plane)
+      expect(subject.planes).to_not include(@plane)
+    end
+  end
+
+  describe 'Full_Capacity' do
+    it "this is the default capacity" do
+      expect(subject.capacity).to eq Airport::Full_Capacity
     end
   end
 end
