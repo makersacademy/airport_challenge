365af093ab600efa20240c6e4297cc84799d1577
Carly <carly.jenkinson65@googlemail.com>
2019-04-07 16:01:09 +0100

    Added take off functionality to airport.

--- lib/airport.rb
+++ lib/airport.rb
@@ -4,4 +4,7 @@ class Airport
   def land_plane(plane)
     plane
   end
+
+  def plane_take_off
+  end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -4,4 +4,9 @@ describe Airport do
     plane = Plane.new
     subject.land_plane(plane)
   end
+  
+  # planes can take off from an airport
+  describe "#plane_take_off" do
+    it { is_expected.to respond_to :plane_take_off }
+  end
 end
