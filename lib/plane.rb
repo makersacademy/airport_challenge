22ffb48b6e5bc864b46b90054efa7d009e8d4cdc
shektor <44544977+shektor@users.noreply.github.com>
2019-04-07 22:14:59 +0100

    Add stormy? raise checks to land and take off

--- lib/plane.rb
+++ lib/plane.rb
@@ -1,12 +1,16 @@
 require_relative 'airport'
+require_relative 'weather'
 
 class Plane
+  include Weather
+
   def in_airport?
     @airport.nil? ? false : true
   end
 
   def land(airport)
     raise("Plane not in flight") if in_airport?
+    raise("Weather is stormy, landing not allowed") if stormy?
     raise("Airport does not exist") unless airport.instance_of? Airport
 
     @airport = airport
@@ -14,6 +18,7 @@ class Plane
 
   def take_off
     raise("Plane not at an airport") unless in_airport?
+    raise("Weather is stormy, take off not allowed") if stormy?
 
     @airport = nil
   end
