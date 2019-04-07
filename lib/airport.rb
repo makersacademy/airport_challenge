d54f3d49e3cab6a98ad858f12f8ac35ef7071a1d
Calum Darroch <calum_darroch@hotmail.com>
2019-04-07 13:31:20 +0100

    take_off method for Airport added and working, attribute reader added

--- lib/airport.rb
+++ lib/airport.rb
@@ -2,8 +2,14 @@ require_relative 'plane'
 
 class Airport
 
+  attr_reader :plane
+
   def land(plane)
     @plane = plane
+  end
+
+  def take_off
+    return @plane
   end
 
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -5,8 +5,22 @@ describe Airport do
 
   describe "#land" do
     it "allows a plane to land" do
+      plane = Plane.new
+      expect(subject.land(plane)).to eq plane
+    end
+  end
+
+  it "returns landed planes" do
     plane = Plane.new
-    expect(subject.land(plane)).to eq plane
+    subject.land(plane)
+    expect(subject.plane).to eq plane
+  end
+
+  describe "#take_off" do
+    it "allows a plane to take-off" do
+      plane = Plane.new
+      subject.land(plane)
+      expect(subject.take_off).to eq plane
     end
   end
 
