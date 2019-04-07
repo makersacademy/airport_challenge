0611660c0cf3a0d52ebaa54bafd367550ba16e4d
shektor <44544977+shektor@users.noreply.github.com>
2019-04-07 17:18:40 +0100

    Add guard clause to take_off

--- lib/plane.rb
+++ lib/plane.rb
@@ -9,11 +9,14 @@ class Plane
 
   def land(airport)
     raise("Plane not in flight") unless @location == "In flight"
+
     @location = airport
     "The plane has landed at #{@location}"
   end
 
   def take_off
+    raise("Plane not at an airport") if @location == "In flight"
+    
     take_off_airport = @location
     @location = "In flight"
     "The plane has taken off from #{take_off_airport}"
