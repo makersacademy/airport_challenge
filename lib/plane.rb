9f49b3c241f2159d4975dc876d402668524ffaf8
shektor <44544977+shektor@users.noreply.github.com>
2019-04-07 15:40:19 +0100

    Add initialize and an attribute reader for location

--- lib/plane.rb
+++ lib/plane.rb
@@ -1,6 +1,12 @@
 require_relative 'airport'
 
 class Plane
+  attr_reader :location
+  
+  def initialize
+    @location = "In flight"
+  end
+
   def land(airport)
     @location = airport
     "The plane has landed at #{@location}"
