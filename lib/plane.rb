51abaf251d4845b52dc3ddc24865c17be580c33d
shektor <44544977+shektor@users.noreply.github.com>
2019-04-08 01:58:27 +0100

    Add Airport method calls to land and take_off

--- lib/plane.rb
+++ lib/plane.rb
@@ -11,14 +11,14 @@ class Plane
     raise("Plane not in flight") if in_airport?
     raise("Airport does not exist") unless airport.instance_of? Airport
     raise("Weather is stormy, landing not allowed") if airport.stormy?
-
+    airport.add_plane(self)
     @airport = airport
   end
 
   def take_off
     raise("Plane not at an airport") unless in_airport?
     raise("Weather is stormy, take off not allowed") if @airport.stormy?
-
+    @airport.remove_plane(self)
     @airport = nil
   end
 end
