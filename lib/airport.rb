c7c5436fd60c4ef8a5edd3ec9662d1bbb581b5ae
MaxRobertsDear <maxhroberts@gmail.com>
2019-04-08 08:56:45 +0100

    first user story satisfied (edit)

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,48 +1,16 @@
 require_relative 'plane'
 
-class Airport
-  attr_reader :identifier, :capacity, :planes_parked
-
-  # DEF_IDENTIFIER = "DEF"
-  DEF_CAPACITY = 50
-  STORM_PERCENTAGE = 0.80
-
-  def initialize(identifier = "DEF", capacity = DEF_CAPACITY)
-    raise error "Airport identifier must be a string" unless identifier.is_a?(String)
-    raise error "Airport identifier must be three capital letters" unless identifier.length == 3 && identifier.upcase == identifier
-    @identifier = identifier.to_s.to_sym
-    raise error "Airport capacity must be an integer" unless capacity.is_a?(Integer)
-    raise error "Airport capacity must be positive integer" unless capacity > 0
-    @capacity = capacity
-    @planes_parked = []
-    @weather = randomise_weather
+class Airport 
+  def land(plane)
+    @plane = plane 
   end
 
-  def park(plane)
-    raise AirportError, "Is not safe to land" unless safe?
-    raise AirportError, "Airport is full" if full?
-    plane.land_at(identifier)
-    @planes_parked << plane
+  def plane
+    @plane 
   end
 
-  def departure(plane)
-    raise AirportError, "Is not safe to departure" unless safe?
-    plane.take_off
-    @planes_parked.delete(plane)
-  end
-
-  private
-
-  def full?
-    planes_parked.count >= capacity ? true : false
-  end
-
-  def safe?
-    @weather == :safe ? true : false
-  end
-
-  def randomise_weather
-    rand < STORM_PERCENTAGE ? :safe : :storm
+  def take_off
+    @plane
   end
 
 end
--- lib/plane.rb
+++ lib/plane.rb
@@ -1,19 +1,5 @@
 class Plane
-  attr_reader :location
-
-  AIR = :air
-
-  def initialize
-    @location = :default_location
-  end
-
-  def land_at(airport_identifier)
-    fail "Plane needs to take_off before it arrives somewhere" if @location != AIR
-    @location = airport_identifier
-  end
-
-  def take_off
-    fail "Plane already in the air" if @location == AIR
-    @location = AIR
+  def landed?
+    @plane 
   end
 end
