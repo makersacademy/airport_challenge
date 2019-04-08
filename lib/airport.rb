597f7739a2d71b5e0f62b5eddffcdebdba3133d4
shektor <44544977+shektor@users.noreply.github.com>
2019-04-08 01:25:30 +0100

    Add capacity methods that add, remove and checks space

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,11 +1,30 @@
 require_relative 'weather'
 
 class Airport
-  def initialize
+  attr_reader :apron
+  attr_reader :capacity
+
+  def initialize(capacity = 5)
     @weather = Weather.new
+    @capacity = capacity
+    @apron = []
   end
 
   def stormy?
     @weather.current == "stormy"
+  end
+
+  def full?
+    @apron.count >= @capacity
+  end
+
+  def add_plane(plane)
+    raise("Airport is at capacity") if full?
+
+    @apron << plane
+  end
+
+  def remove_plane(plane)
+    @apron.delete(plane)
   end
 end
