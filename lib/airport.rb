44f57f6110b7b760701f4e3c85940efa98971a8a
lisamcc <lisamccormack85@gmail.com>
2019-05-04 10:15:39 +0100

    second user story

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,9 +1,19 @@
+require_relative 'plane'
+
 class Airport
-  def land
+
+  attr_reader :planes
+
+  def initialize
+    @planes = []
+  end
+
+  def land(plane)
     "Cleared to land"
   end
 
   def take_off
     "Cleared for take off"
+    planes.pop
   end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -3,19 +3,29 @@ require 'airport'
 describe Airport do
 
   it 'will respond to land' do
-    expect(Airport.new).to respond_to(:land)
+    expect(Airport.new(100)).to respond_to(:land)
   end
 
-  it 'will give planes permission to land' do
-    expect(subject.land).to eq("Cleared to land")
+  it 'create airport with a capacity' do
+    airport = Airport.new(100)
+    expect(airport.capacity).to eq(100)
+  end
+
+  describe '#land' do
+    it 'will give planes permission to land' do
+      airport = Airport.new(100)
+      plane = Plane.new
+      expect(airport.land(plane)).to eq("Cleared to land")
+    end
   end
 
   it 'will respond to take off' do
-    expect(Airport.new).to respond_to(:take_off)
+    expect(Airport.new(100)).to respond_to(:take_off)
   end
 
   it 'will instruct a plane to take off' do
-    expect(subject.take_off).to eq("Cleared for take off")
+    airport = Airport.new(100)
+    expect(airport.take_off).to eq("Cleared for take off")
   end
 
 end
