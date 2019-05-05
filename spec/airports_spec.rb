7ee6b883f689f333b3548d0b9e9f53a79dbeb514
Laurence <ltaylorapp@gmail.com>
2019-05-05 13:24:24 +0100

    Refactored spec files to include context

--- spec/airports_spec.rb
+++ spec/airports_spec.rb
@@ -13,7 +13,7 @@ describe Airport do
     expect(subject.planes.include?(plane)).to eq(true)
   end
 
-  it 'should not contain planes which have taken off' do
+  it 'should not contain planes which have just taken off' do
     not_stormy
     plane.land_at(subject)
     plane.take_off_from(subject)
--- spec/planes_spec.rb
+++ spec/planes_spec.rb
@@ -11,41 +11,49 @@ describe Plane do
     allow(airport.weather).to receive(:stormy?) { true }
   end
 
-  describe '#land_at' do
-    it 'should not be able to land if airport at capacity' do
-      not_stormy(airport)
-      airport.capacity.times { Plane.new.land_at(airport) }
-      message = 'Airport at capacity'
-      expect { Plane.new.land_at(airport) }.to raise_error message
+  context 'when it is not stormy' do
+    describe '#land_at' do
+      it 'should not be able to land if airport is at capacity' do
+        not_stormy(airport)
+        airport.capacity.times { Plane.new.land_at(airport) }
+        message = 'Airport at capacity'
+        expect { Plane.new.land_at(airport) }.to raise_error message
+      end
+
+      it 'should not be able to land if already landed' do
+        not_stormy(airport)
+        subject.land_at(airport)
+        message = 'This plane is already landed'
+        expect { subject.land_at(airport) }.to raise_error message
+      end
     end
 
-    it 'should not be able to land if already landed' do
-      not_stormy(airport)
-      subject.land_at(airport)
-      message = 'This plane is already landed'
-      expect { subject.land_at(airport) }.to raise_error message
-    end
-
-    it 'should not be able to land in stormy weather' do
-      stormy(airport)
-      message = 'Too stormy to land'
-      expect { subject.land_at(airport) }.to raise_error message
+    describe '#take_off_from' do
+      it 'should only take off from an airport it is in' do
+        not_stormy(airport)
+        message = 'The plane was not landed here'
+        expect { subject.take_off_from(airport) }.to raise_error message
+      end
     end
   end
 
-  describe '#take_off_from' do
-    it 'should only take off from an airport it is in' do
-      not_stormy(airport)
-      message = 'The plane was not landed here'
-      expect { subject.take_off_from(airport) }.to raise_error message
+  context 'when it is stormy' do
+    describe '#land_at' do
+      it 'should not be able to land in stormy weather' do
+        stormy(airport)
+        message = 'Too stormy to land'
+        expect { subject.land_at(airport) }.to raise_error message
+      end
     end
 
-    it 'should not be able to take off in stormy weather' do
-      not_stormy(airport)
-      subject.land_at(airport)
-      stormy(airport)
-      message = 'Too stormy to take off'
-      expect { subject.take_off_from(airport) }.to raise_error message
+    describe '#take_off_from' do
+      it 'should not be able to take off in stormy weather' do
+        not_stormy(airport)
+        subject.land_at(airport)
+        stormy(airport)
+        message = 'Too stormy to take off'
+        expect { subject.take_off_from(airport) }.to raise_error message
+      end
     end
   end
 end
