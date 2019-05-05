b5fbda06ac7241f4ef1861f3c07b6bee6813004c
Michael Hughes <mhughes36@live.co.uk>
2019-05-05 19:00:31 +0100

    traffic control instructs plane to land

--- lib/traffic_controller.rb
+++ lib/traffic_controller.rb
@@ -1,3 +1,8 @@
 class TrafficController
-
+  def plane(instruction)
+    case instruction
+    when 'land'
+      "Roger, plane approaching runway for landing."
+    end
+  end
 end
--- spec/traffic_controller_spec.rb
+++ spec/traffic_controller_spec.rb
@@ -1,5 +1,7 @@
 require 'traffic_controller'
 
 describe TrafficController do
-
+  it '#plane instructed to land' do
+    expect(subject.plane('land')).to eq("Roger, plane approaching runway for landing.")
+  end
 end
