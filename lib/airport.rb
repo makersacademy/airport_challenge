532125011abc65343c65778259b604852fb79574
Oliver Brownlow <olliebrownlow@gmail.com>
2019-05-06 11:23:50 +0100

    refactors airport.rb

--- lib/airport.rb
+++ lib/airport.rb
@@ -17,10 +17,10 @@ class Airport
   end
 
   def land_guards(plane)
-    raise "Airport full" if planes.length >= capacity
-    raise "Too stormy to land right now" if weather == "stormy"
-    raise "Plane already at the airport!" if planes.include?(plane)
-    raise "Plane already at another airport" if plane.airborne == false
+    raise "Airport full" if full?
+    raise "Too stormy to land right now" if stormy?
+    raise "Plane already at the airport!" if plane_at_airport?(plane)
+    raise "Plane already at another airport" if !airborne?(plane)
   end
 
   def take_off(plane)
@@ -32,8 +32,25 @@ class Airport
 
   def take_off_guards(plane)
     message = "Specified plane at another airport"
-    raise message if plane.airborne == false && !planes.include?(plane)
-    raise "Plane already airborne" if plane.airborne == true
-    raise "Too stormy to take off right now" if weather == "stormy"
+    raise message if !airborne?(plane) && !plane_at_airport?(plane)
+    raise "Plane already airborne" if airborne?(plane)
+    raise "Too stormy to take off right now" if stormy?
   end
+
+  def full?
+    planes.length >= capacity
+  end
+
+  def stormy?
+    weather == "stormy"
+  end
+
+  def plane_at_airport?(plane)
+    planes.include?(plane)
+  end
+
+  def airborne?(plane)
+    plane.airborne == true
+  end
+
 end
