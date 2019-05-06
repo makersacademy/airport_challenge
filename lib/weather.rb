273646fb6929301847a26e1fd46bcdbe3b525e16
Max Van Aken <mtvanaken@gmail.com>
2019-05-06 17:14:56 +0100

    Weather class will randomly return sunny or stormy

---
+++ lib/weather.rb
@@ -0,0 +1,9 @@
+class Weather
+  attr_reader :weather_arr, :rand_weather
+  def initialize
+    @weather_arr = ["Sunny", "Sunny", "Sunny", "Sunny", "Stormy"]
+  end
+  def rand_weather
+    @weather_arr.sample
+  end
+end
