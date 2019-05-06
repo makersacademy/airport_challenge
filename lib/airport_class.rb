37b4a6a08de467128a8fe4ee06b7a1bafc58461c
Vijay Kurian <kurianvijay7@gmail.com>
2019-05-06 20:33:43 +0100

    added airport is full method

--- lib/airport_class.rb
+++ lib/airport_class.rb
@@ -18,13 +18,24 @@ class Airport
      return "#{plane} has takenoff"
   end
 
+  def stormy?
+    weather = ["stormy", "sunny"]
 
 
 
+  # def full(plane)
+  #   if @plane.length < 1
+  #      @plane << plane
+  #   else
+	#     raise "airport is full"
+  #   end
+  # end
 
-
-
-
-
+  def full
+    plane = Plane.new
+    @plane << plane
+    fail "airport is full" if @plane.length == 1
+    @plane
+  end
 
 end
--- spec/airport_class_spec.rb
+++ spec/airport_class_spec.rb
@@ -20,6 +20,29 @@ describe Airport do
 
   end
 
+  describe "weather conditions and if/when planes can takeoff and land" do
+
+      it "prevents takeoff if weather is stormy" do
+        plane = Plane.new
+        airport = Airport.new
+        airport.takeoff(plane).stormy?
+        expect(airport.takeoff(plane).stormy?).to respond_to("takeoff not allowed
+          due to stormy conditions")
+      end
+  end
+
+   # describe "Airport capacity" do
+   #
+   #    it "prevents #land when airport is at capacity" do
+   #      plane = Plane.new
+   #      airport = Airport.new
+   #      #subject.land(plane)
+   #      airport.full
+   #      expect{ airport.full }.to raise_error("airport is full")
+   #    end
+   # end
+
+
 end
 
 
