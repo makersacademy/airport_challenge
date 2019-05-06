7ae38b4c225492f62c35a14c932d8aa090f41fdb
Ben Reynolds-Carr <bjr.kitz@gmail.com>
2019-05-06 14:58:27 +0100

    airport now lands and takes off planes and confirms if there is a plane in the airport

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,9 +1,15 @@
 class Airport
+  attr_reader :plane
+
   def land(plane)
     @plane = plane
   end
 
   def take_off
+    @plane = nil
+  end
+
+  def contain?
     @plane
   end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -7,8 +7,11 @@ plane = Plane.new
 describe Airport do
   it "lands a plane" do
     expect(airport.land(plane)).to eq(plane)
+    expect(airport.contain?).to eq(plane)
   end
   it "takes off a plane" do
-    expect(airport.take_off).to eq(plane)
+    expect(airport).to respond_to(:take_off)
+    airport.take_off
+    expect(airport.contain?).to eq(nil)
   end
 end
