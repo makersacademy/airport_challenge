39e4b9e566b29e2465777c15e68891b65aee8358
Student <magnusarbuthnott@gmail.com>
2019-05-06 09:40:43 +0100

    Function test on airport class #land to return a plane passed

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -5,6 +5,11 @@ describe Airport do
     expect(Airport.new.is_a? Airport).to eq true
   end
 
-  it {is_expected.to respond_to :land}
-  
+  it { is_expected.to respond_to :land }
+
+  it 'instructs an instance of plane to land and returns a plane' do
+    plane = Plane.new
+    expect(subject.land(plane)).to eq plane
+  end
+
 end
