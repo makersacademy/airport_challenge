a9e2bae5b231e8d19b5bb9540f8a01217b0762d9
Max Van Aken <mtvanaken@gmail.com>
2019-05-06 21:23:12 +0100

    Divide spec into categories depending on landing or takeoff

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,22 +1,26 @@
 require "airport"
 
 describe Airport do
-  subject(:airport) { described_class.new(5) }
+  subject(:airport) { described_class.new(50) }
+  let(:plane) { Plane.new }
 
-  it "instructs plane to land" do
-    expect(airport).to respond_to(:land).with(1).argument
-  end
-
-  it "instructs plane to take off" do
-    expect(airport).to respond_to(:take_off).with(1).argument
-  end
-
-  it "does not allow landing when airport is full" do
-    plane = double :plane
-    5.times do
-      airport.land(plane)
+  describe "#landing" do
+    it "instructs plane to land" do
+      expect(airport).to respond_to(:land).with(1).argument
     end
-    expect { airport.land(plane) }.to raise_error 'Airport Full - Cannot Land'
+
+    it "does not allow landing when airport is full" do
+      50.times do
+        airport.land(plane)
+      end
+      expect { airport.land(plane) }.to raise_error 'Airport Full - Cannot Land'
+    end
   end
 
+  describe "#takeoff" do
+    it "instructs plane to take off" do
+      expect(airport).to respond_to(:take_off).with(1).argument
+    end
+    
+  end
 end
