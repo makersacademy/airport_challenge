ad3ecde8d5d7c1c60715364a11b025e1347c7333
Oliver Brownlow <olliebrownlow@gmail.com>
2019-05-06 08:27:01 +0100

    adds no take off in stormy weather and more edge casses

--- lib/airport.rb
+++ lib/airport.rb
@@ -8,14 +8,15 @@ class Airport
   end
 
   def land(plane)
-    raise "Plane already at the airport!" if planes.include?(plane)
     raise "Too stormy to land right now" if weather == "stormy"
-    
+    raise "Plane already at the airport!" if planes.include?(plane)
+
     planes << plane
   end
 
   def take_off(plane)
     raise "Too stormy to take off right now" if weather == "stormy"
+    raise "Specified plane not at this airport" if !planes.include?(plane)
 
     planes.delete(plane)
     planes
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -20,12 +20,14 @@ describe Airport do
     it 'lands a plane' do
       plane = Plane.new
       airport = Airport.new
+      allow(airport).to receive(:weather).and_return "sunny"
       expect(airport.land(plane)).to include(plane)
     end
 
     it 'raises error when we try to land the same plane twice' do
       plane = Plane.new
       airport = Airport.new
+      allow(airport).to receive(:weather).and_return "sunny"
       airport.land(plane)
       message = "Plane already at the airport!"
       expect { airport.land(plane) }.to raise_error(message)
@@ -51,15 +53,6 @@ describe Airport do
       expect(airport.take_off(plane).length).to eq(n - 1)
     end
 
-    it 'raises error if it is stormy' do
-      plane = Plane.new
-      airport = Airport.new
-      allow(airport).to receive(:weather).and_return "stormy"
-      airport.land(plane)
-      message = "Too stormy to take off right now"
-      expect { airport.take_off(plane) }.to raise_error(message)
-    end
-
     it 'takes off a specified plane' do
       plane1 = Plane.new
       plane2 = Plane.new
@@ -68,6 +61,39 @@ describe Airport do
       airport.land(plane1)
       airport.land(plane2)
       expect(airport.take_off(plane1)).not_to include(plane1)
+    end
+
+    it 'raises error if it is stormy' do
+      plane = Plane.new
+      airport = Airport.new
+      allow(airport).to receive(:weather).and_return "sunny"
+      airport.land(plane)
+      allow(airport).to receive(:weather).and_return "stormy"
+      message = "Too stormy to take off right now"
+      expect { airport.take_off(plane) }.to raise_error(message)
+    end
+
+    it 'raises an error if plane at a different airport' do
+      plane1 = Plane.new
+      plane2 = Plane.new
+      airport1 = Airport.new
+      allow(airport1).to receive(:weather).and_return "sunny"
+      airport2 = Airport.new
+      allow(airport2).to receive(:weather).and_return "sunny"
+      airport1.land(plane1)
+      airport2.land(plane2)
+      message = "Specified plane not at this airport"
+      expect { airport1.take_off(plane2) }.to raise_error(message)
+    end
+
+    it 'raises error when we try to take off the same plane twice' do
+      plane = Plane.new
+      airport = Airport.new
+      allow(airport).to receive(:weather).and_return "sunny"
+      airport.land(plane)
+      airport.take_off(plane)
+      message = "Specified plane not at this airport"
+      expect { airport.take_off(plane) }.to raise_error(message)
     end
   end
 
