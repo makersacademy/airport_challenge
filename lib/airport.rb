fe7e6cb6e30c47ded8c8a10ddde94571347b72d2
Ben Reynolds-Carr <bjr.kitz@gmail.com>
2019-05-06 14:37:55 +0100

    plane now takes off from airport

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,5 +1,9 @@
 class Airport
- def land(plane)
-   plane
- end
+  def land(plane)
+    @plane = plane
+  end
+
+  def take_off
+    @plane
+  end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -8,4 +8,7 @@ describe Airport do
   it "lands a plane" do
     expect(airport.land(plane)).to eq(plane)
   end
+  it "takes off a plane" do
+    expect(airport.take_off).to eq(plane)
+  end
 end
