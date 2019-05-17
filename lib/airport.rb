e150927632cd30abe3f85100fa53fb3ff5581bf3
Anna <annas.wieclaw@gmail.com>
2019-05-17 12:25:08 +0100

    Airpoty only registers planes that landed.

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,10 +1,11 @@
 class Airport
   attr_reader :base
+
   def initialize
     @base = []
   end
 
   def add(plane)
-    @base.push(plane)
+    plane.landed ? @base.push(plane) : raise { RuntimeError.new("This plane has not landed") }
   end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -6,8 +6,18 @@ describe Airport do
   let(:fake_plane) {
     double(Plane.new)
   }
+  let(:plane) {
+    Plane.new
+  }
   it "knows that a plane has landed" do
-    airport.add(fake_plane)
+    allow(fake_plane).to receive(:landed) { true }
+
+    expect { airport.add(fake_plane) }.not_to raise_error
     expect(airport.base).to include(fake_plane)
+  end
+
+  it "only registers a plan if it has landed" do
+    allow(fake_plane).to receive(:landed) { false }
+    expect { airport.add(fake_plane) }.to raise_error(RuntimeError)
   end
 end
