b0a1c5ae504a3713f7f20bd629d35176ad8f1b81
Megan Goode <megan.e.goode@hotmail.com>
2019-05-17 10:20:08 +0100

    Test: Airport has a default capacity of 5 planes. Code: added.

--- docs/airport.rb
+++ docs/airport.rb
@@ -1,3 +1,7 @@
 class Airport
+  attr_reader :capacity
 
+  def initialize(capacity=5)
+    @capacity = capacity
+  end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -6,4 +6,8 @@ let(:airport) { Airport.new }
   it 'creates a new instance of airport' do
     expect(airport).to be_an_instance_of(Airport)
   end
+
+  it 'Airport has a default capacity of 5 planes' do
+    expect(airport.capacity).to eq(5)
+  end
 end
