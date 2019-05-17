a48082fd0188feac56b0afd04d06e4b4d2a972b7
Lauren O'Mara <lauren.omara22@yahoo.co.uk>
2019-05-17 12:44:51 +0100

    improved by using weather class and passing weather as an additional argument

--- lib/airport.rb
+++ lib/airport.rb
@@ -5,15 +5,15 @@ class Airport
     @landed_planes = []
   end
 
-  def take_off(plane)
-    raise "too stormy to take off" if plane.weather_number == 1
+  def take_off(plane, weather)
+    raise "too stormy to take off" if weather.weather_number == 1
     plane.flying = true
     @landed_planes = @landed_planes - [plane]
     @landed_planes
   end
 
-  def land_plane(plane)
-    raise "too stormy to land" if plane.weather_number == 1
+  def land_plane(plane, weather)
+    raise "too stormy to land" if weather.weather_number == 1
     plane.flying = false
     @landed_planes.push(plane)
   end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -8,34 +8,34 @@ describe Airport do
   it 'can land a plane using the land_plane method' do
     allow(plane).to receive(:flying=)
     allow(plane).to receive(:flying)
-    allow(plane).to receive(:weather_number).and_return(2)
+    allow(weather).to receive(:weather_number).and_return(2)
     
-    expect(airport.land_plane(plane)).to eq([plane])
+    expect(airport.land_plane(plane,weather)).to eq([plane])
   end
 
   it 'tells a plane to take off and returns empty landed_planes' do 
     allow(plane).to receive(:flying=)
     allow(plane).to receive(:flying)
-    allow(plane).to receive(:weather_number).and_return(2)
+    allow(weather).to receive(:weather_number).and_return(2)
 
-    airport.land_plane(plane)
+    airport.land_plane(plane,weather)
 
-    expect(airport.take_off(plane)).to eq([])
+    expect(airport.take_off(plane,weather)).to eq([])
   end 
 
   it 'can prevent takeoff in stormy weather' do
     allow(plane).to receive(:flying=)
     allow(plane).to receive(:flying)
-    allow(plane).to receive(:weather_number).and_return(1)
+    allow(weather).to receive(:weather_number).and_return(1)
 
-    expect{airport.take_off(plane)}.to raise_error("too stormy to take off")
+    expect{airport.take_off(plane, weather)}.to raise_error("too stormy to take off")
   end 
 
-  it 'can prevent landing in stormy weather' do
+  it 'can prevent takeoff in stormy weather' do
     allow(plane).to receive(:flying=)
     allow(plane).to receive(:flying)
-    allow(plane).to receive(:weather_number).and_return(1)
+    allow(weather).to receive(:weather_number).and_return(1)
 
-    expect{airport.land_plane(plane)}.to raise_error("too stormy to land")
+    expect{airport.land_plane(plane, weather)}.to raise_error("too stormy to land")
   end 
 end 
