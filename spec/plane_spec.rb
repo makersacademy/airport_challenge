ca9ba48208020dd675496edcd2e9cbb6452a57a6
Megan Goode <megan.e.goode@hotmail.com>
2019-05-17 12:27:51 +0100

    Refactored plane tests, removed irrelevant tests.

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -3,10 +3,6 @@ require_relative '../docs/plane'
 describe Plane do
 let(:plane) { Plane.new }
 
-  it 'responds to .report_in_flight' do
-    expect(plane).to respond_to(:report_in_flight)
-  end
-
   it '.report_in_flight as true' do
     plane.report_in_flight
     expect(plane.flying).to eq(true)
