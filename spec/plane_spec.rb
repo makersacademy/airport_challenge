740ca7103d5d32a9135ae03600f5b3d0840e3af6
Megan Goode <megan.e.goode@hotmail.com>
2019-05-17 10:47:11 +0100

    Renamed inital respond test to: responds to .flying?. Added test: .flying?

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -1,8 +1,13 @@
 require_relative '../docs/plane'
 
 describe Plane do
-  it '.flying?' do
+  it 'responds to .flying?' do
     plane = Plane.new
     expect(plane).to respond_to(:flying?)
+  end
+
+  it '.flying?' do
+    plane = Plane.new
+    expect(plane.flying?).to eq(true)
   end
 end
