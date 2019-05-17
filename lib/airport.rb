c5ea6577f61050fd16302f4880adda853fb5b824
Geoff Smith <smith260194@gmail.com>
2019-05-17 15:12:31 +0100

    Allowing airport to land a plane

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,2 +1,7 @@
 class Airport
+  attr_reader :landed
+  def land(flying_object)
+    @landed = flying_object
+    flying_object
+  end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,17 +1,35 @@
 require 'airport'
 
 describe Airport do
-  before do
-    # Do nothing
-  end
+  let(:my_airport) { Airport.new }
 
-  after do
-    # Do nothing
-  end
-
-  context 'when condition' do
+  context 'before creating an airport' do
     it 'allows Airport objects to be created' do
-      expect { Airport.new } .to_not raise_error
+      expect(Airport.new).to be_kind_of(Airport)
+    end
+  end
+
+  describe '#land' do
+    it 'can respond to land' do
+      expect(subject).to respond_to(:land)
+    end
+
+    it 'can respond to land with an argument' do
+      expect(subject).to respond_to(:land).with(1).argument
+    end
+
+    it 'returns the object it receives' do
+      flying_double = double
+      expect(my_airport.land(flying_double)).to eq(flying_double)
+    end
+
+    it 'stores the flying object' do
+      flying_double = double
+
+      my_airport.land(flying_double)
+
+      expect(my_airport.landed).to eq(flying_double)
+
     end
   end
 end
