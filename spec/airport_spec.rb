8832e52ec50b03819f76977aa91460a9b9bac104
edwardreeve <edreeve+git@gmail.com>
2019-05-17 10:01:20 +0100

    Airport responds to landing a plane

---
+++ spec/airport_spec.rb
@@ -0,0 +1,7 @@
+require 'airport'
+
+describe Airport do
+
+  it { should respond_to(:land).with(1).argument }
+
+end
---
+++ spec/irb.rb
@@ -0,0 +1,6 @@
+
+require './lib/airport'
+require './lib/plane'
+
+ap = Airport.new
+pl = Plane.new
