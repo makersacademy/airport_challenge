412c9018f4657ee1e0d9dc37aa919ef5d3b73875
Anthony Hull <anthony.hull@hotmail.co.uk>
2019-05-17 14:31:47 +0100

    Airport.weather? added Stubbed out in test

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,7 +1,12 @@
+require 'weather'
 class Airport
     attr_reader :planes
     def initialize
         @planes = []
+        @weather = Weather.new
+    end
+    def weather?
+        @weather.query
     end
     def land(plane)
         @planes.push(plane)
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,10 +1,16 @@
 require 'airport'
 describe Airport do
     let(:an_airport) { Airport.new }
-    context 'is empty' do
-        it ".land('plane')" do
+    context '#planes == []' do
+        it "#land('plane')" do
             an_airport.land('plane')
             expect(an_airport.planes.length).to eq(1)
         end
-    end        
+    end
+    context '::weather == double of weather set to sunny'
+        it '#weather?' do
+            weather_dbl = instance_double('Weather', :query => :sunny)
+            an_airport.instance_variable_set(:@weather, weather_dbl)
+            expect(an_airport.weather?).to eq(:sunny)
+        end
 end
