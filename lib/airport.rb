3722d4728a9899647320a08e03478287160230ab
Ollie Porter <ol.porter@me.com>
2019-05-17 14:54:24 +0100

    Set landing condition to max 4 planes & refactored to ternary

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,19 +1,18 @@
 class Airport
 
-  attr_reader :show_planes
+  attr_reader :show_planes, :weather
   # I want to see all planes
   # I want to add a plane to the airport (land a plane)
 
   def initialize
     @show_planes = []
   end
-
   def land(plane)
-    @show_planes.push(plane)
+    @show_planes.length < 4 ? @show_planes.push(plane) : "There's no more space on the runway"
   end
-
   def plane_takeoff
-    @show_planes.pop()
+      show_planes.pop()
   end
-
+  def weather
+  end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,33 +1,49 @@
 require 'airport'
 
 describe Airport do
+
+  let(:airport) {Airport.new}
+  let(:plane) {Plane.new}
+
   it 'expect airport to be a kind of Airport' do
-    airport = Airport.new
     expect(airport).to be_a_kind_of(Airport)
   end
   it 'expect airport to respond to the land method' do
-    airport = Airport.new
     expect(airport).to respond_to(:land).with(1).argument
   end
   it 'expect airport to respond to show_planes' do
-    airport = Airport.new
     expect(airport).to respond_to(:show_planes)
   end
   it 'expects a plane to be able to land at the airport' do
-    airport = Airport.new
-    plane = Plane.new
     output = airport.land(plane)
     expect(airport.show_planes.length).to eq(1)
   end
   it 'expects airport to respond to plane_takeoff' do
-    airport = Airport.new
     expect(airport).to respond_to(:plane_takeoff)
   end
   it 'expects a plane to be able to take off from the airport' do
-    airport = Airport.new
-    plane = Plane.new
     airport.land(plane)
     airport.plane_takeoff
     expect(airport.show_planes.length).to eq(0)
   end
+  it 'expects airport to respond to weather' do
+    expect(airport).to respond_to(:weather)
+  end
+  it 'expects a plane not to be able to land at the airport when the airport is full' do
+    airport.land(plane)
+    airport.land(plane)
+    airport.land(plane)
+    airport.land(plane)
+    expect(airport.land(plane)).to eq("There's no more space on the runway")
+  end
+  # it 'expects weather to return stormy' do
+  #   airport = Airport.new
+  #   output = airport.weather
+  #   expect(output).to eq("STORMY")
+  # end
+  # it 'expects weather to return sunny' do
+  #   airport = Airport.new
+  #   output = airport.weather
+  #   expect(output).to eq("SUNNY")
+  # end
 end
