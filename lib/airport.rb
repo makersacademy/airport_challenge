cc7a374bdf497152db71e72bb9accd2b22253827
edwardreeve <edreeve+git@gmail.com>
2019-05-17 22:20:59 +0100

    initial refactoring

--- lib/airport.rb
+++ lib/airport.rb
@@ -11,14 +11,20 @@ class Airport
 
   def land(plane, weather = Weather.new)
     raise 'Airport currently closed due to stormy weather' if weather.stormy?
-    raise 'Airport is full' if @hangar.count >= @capacity
+    raise 'Airport is full' if full?
 
     @hangar << plane
   end
 
   def take_off(plane, weather = Weather.new)
     raise 'Planes grounded: stormy weather!' if weather.stormy?
-    
+
     @hangar.delete(plane)
+  end
+
+  private
+
+  def full?
+    @hangar.count >= @capacity
   end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -6,24 +6,21 @@ describe Airport do
   let(:stormy) { instance_double("Weather", :stormy? => true) }
   let(:clear) { instance_double("Weather", :stormy? => false) }
   
-  it { should respond_to(:land).with(2).arguments }
-
   it 'should allow user to set capacity of new instance' do
-    large_airport = Airport.new(50)
-    expect(large_airport.capacity).to equal(50)
+    # large_airport = Airport.new(50)
+    expect(Airport.new(50).capacity).to equal(50)
   end
 
-  it 'should return landed planes when queried' do
+  it 'should store landed planes in hangar' do
     airport.land(plane, clear)
     expect(airport.hangar).to eq([plane])
-  end
-  it { should respond_to(:take_off).with(2).arguments }
-  
+  end  
   
   context 'with nice weather' do
     it 'should let the requested plane take off' do
-      airport.land(plane, clear)
-      expect(airport.take_off(plane, clear)).to eq(plane)
+      unique_plane = plane
+      airport.land(unique_plane, clear)
+      expect(airport.take_off(unique_plane, clear)).to eq(unique_plane)
     end
     it 'should no longer store the plane after take off' do
       airport.land(plane, clear)
@@ -44,8 +41,9 @@ describe Airport do
 
   context 'when it\'s full' do
     it 'should not let planes land' do
-      airport.land(plane, clear)
-      expect { airport.land(Plane.new) }.to raise_error('Airport is full')
+      test_port = Airport.new
+      Airport::DEFAULT_CAPACITY.times { test_port.land(Plane.new, clear) }
+      expect { test_port.land(Plane.new, clear) }.to raise_error('Airport is full')
     end
   end
 end
