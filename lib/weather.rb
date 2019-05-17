b632c2bca473ce033b10945d169814399c8c489a
Anthony Hull <anthony.hull@hotmail.co.uk>
2019-05-17 14:16:02 +0100

    refactored weather to use constant in test

--- lib/weather.rb
+++ lib/weather.rb
@@ -1,8 +1,11 @@
 class Weather
     Weather_weighted = {
-        sunny: 1.0,
-        stormy: 0.0
+        sunny: 0.9,
+        stormy: 0.1
     }
+    def types
+        Weather_weighted.keys
+    end
     def query
         Weather_weighted.max_by { |weather, weight| rand ** (1.0/ weight)}.first
     end
--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -1,7 +1,9 @@
 require 'weather'
 describe Weather do
     let(:weather) { Weather.new }
+    let(:weather_types) { weather = Weather.new; weather.types.each }
     it '.query' do
-        expect(weather.query).to eq(:sunny).or(eq(:stormy))
+        # expect(weather.query).to eq(:sunny).or(eq(:stormy))
+        expect(weather_types).to include(weather.query)
     end
 end
