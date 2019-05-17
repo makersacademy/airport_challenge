ec8715f6f13441f374251b60a9fcc715e54c2def
Megan Goode <megan.e.goode@hotmail.com>
2019-05-17 13:42:19 +0100

    weather responds to .generate

--- docs/weather.rb
+++ docs/weather.rb
@@ -1,3 +1,5 @@
 class Weather
-
+  def generate
+    
+  end
 end
--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -1,5 +1,8 @@
 require_relative '../docs/weather.rb'
 
 describe Weather do
-
+  it 'responds to .generate' do
+    weather = Weather.new
+    expect(weather).to respond_to(:generate)
+  end
 end
