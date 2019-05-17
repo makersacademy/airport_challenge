d976f4d5910a542ba5f6334b763bb80a09634235
JAYDA KADIR <jydkdr@gmail.com>
2019-05-17 14:30:03 +0100

    Add weather report feature in weather file

--- lib/weather.rb
+++ lib/weather.rb
@@ -1,7 +1,27 @@
 class Weather
-  
+# attr_reader :weather_today
+# attr_reader :report
+#
+#   def initialize
+#     @weather_today
+#   end
+
+#   def weather_generator
+#     @outlook = rand(10)
+#   end
+# #
   def report
+    outlook = rand(10)
+    outlook == 1? "stormy" : "sunny"
+
   end
+
+
+
+# private
+#   def stormy?
+#     @outlook == 1
+#   end
 
 
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -22,13 +22,14 @@ let(:aero) { Plane.new }
   end
 
   it 'can report the weather' do
-    expect(Weather.new).to respond_to(:report)
+    expect(Weather.new).to respond_to(:weather_generator)
   end
+  #
   # it 'can report weather as stormy' do
   #   weather = Weather.new
-  #   allow(weather).to receive(:report).and_return(1)
+  #   allow(weather).to receive(:weather_generator).and_return(1)
   #   expect(weather.report).to eq("stormy")
-  #   # allow(weather).to receive(:report) { 1 }
+    # allow(weather).to receive(:report) { 1 }
   # end
 
 
