d34f9f268d6f00e3904080176280233f989d3c02
Megan Goode <megan.e.goode@hotmail.com>
2019-05-17 11:02:35 +0100

    Refactored tests to remove irrelevant ones.

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -4,20 +4,12 @@ require_relative '../docs/plane'
 describe Airport do
 let(:airport) { Airport.new }
 
-  it 'creates a new instance of airport' do
-    expect(airport).to be_an_instance_of(Airport)
-  end
-
   it 'Airport has a default capacity of 5 planes' do
     expect(airport.capacity).to eq(5)
   end
 
   it 'Airport has empty parking station' do
     expect(airport.parking_station).to eql([])
-  end
-
-  it 'responds to .land' do
-    expect(airport).to respond_to(:land)
   end
 
   it '.land adds plane to airport parking_station' do
