26f7f004dff09e6043d4f290b3da3f89ea05d5fa
Megan Goode <megan.e.goode@hotmail.com>
2019-05-17 14:27:25 +0100

    Refactored weather tests to call weather as a let

--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -1,23 +1,21 @@
 require_relative '../docs/weather.rb'
 
 describe Weather do
+  let(:weather) { Weather.new }
+
   it '.initialize sets up array of weather outcomes' do
-    weather = Weather.new
     expect(weather.weather_generator).to eq(["sunny", "sunny", "sunny", "sunny", "stormy"])
   end
 
   it 'responds to .rand_num' do
-    weather = Weather.new
     expect(weather).to respond_to(:rand_num)
   end
 
   it '.rand_num outputs a number between 0 and 4' do
-    weather = Weather.new
     expect(weather.rand_num).to be_between(0, 4).inclusive
   end
 
   it 'responds to get_weather' do
-    weather = Weather.new
     expect(weather).to respond_to(:get_weather)
   end
 end
