ed5f480f9ce839a72f52beb23eadfaf9276a08ee
Ollie Porter <ol.porter@me.com>
2019-05-17 15:51:33 +0100

    Set default capacity using a constant which can change when Airport is given a capacity at instantiation

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,14 +1,16 @@
 class Airport
 
-  attr_reader :show_planes, :weather
+  attr_reader :show_planes, :weather, :capacity
   # I want to see all planes
   # I want to add a plane to the airport (land a plane)
+DEFAULT_CAPACITY = 4
 
-  def initialize
+  def initialize(capacity=DEFAULT_CAPACITY)
     @show_planes = []
+    @capacity = capacity
   end
   def land(plane)
-    @show_planes.length < 4 ? @show_planes.push(plane) : "There's no more space on the runway"
+    @show_planes.length < capacity ? @show_planes.push(plane) : "There's no more space on the runway"
   end
   def plane_takeoff
       show_planes.pop()
