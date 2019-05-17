0f09bc256632495f217b19370df6546432f076fe
Megan Goode <megan.e.goode@hotmail.com>
2019-05-17 12:18:35 +0100

    responds to .report_in_flight

--- docs/plane.rb
+++ docs/plane.rb
@@ -1,4 +1,8 @@
 class Plane
+  def report_in_flight
+
+  end
+  
   def flying?
     @in_flight = true
   end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -3,6 +3,10 @@ require_relative '../docs/plane'
 describe Plane do
 let(:plane) { Plane.new }
 
+  it 'responds to .report_in_flight' do
+    expect(plane).to respond_to(:report_in_flight)
+  end
+
   it '.flying?' do
     expect(plane.flying?).to eq(true)
   end
