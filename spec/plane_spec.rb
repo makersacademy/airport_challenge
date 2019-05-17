9acb2a80cb60dc1836c09d170d30b2a8c037657c
Megan Goode <megan.e.goode@hotmail.com>
2019-05-17 10:48:28 +0100

    Refactored Plane.new test code for Act. Removed respond to test as irrelivent now.

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -1,13 +1,9 @@
 require_relative '../docs/plane'
 
 describe Plane do
-  it 'responds to .flying?' do
-    plane = Plane.new
-    expect(plane).to respond_to(:flying?)
-  end
+let(:plane) { Plane.new }
 
   it '.flying?' do
-    plane = Plane.new
     expect(plane.flying?).to eq(true)
   end
 end
