659ba574f688f0c79734d2062be8e2f5189654e3
Anthony Hull <anthony.hull@hotmail.co.uk>
2019-05-17 14:09:09 +0100

    fixed weather test

--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -2,6 +2,6 @@ require 'weather'
 describe Weather do
     let(:weather) { Weather.new }
     it '.query' do
-        expect(weather.query).to eq(:sunny)
+        expect(weather.query).to eq(:sunny).or(eq(:stormy))
     end
 end
