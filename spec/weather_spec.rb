8d6fac210d8d894c4da77b1994a4cde94d206560
Anthony Hull <anthony.hull@hotmail.co.uk>
2019-05-17 15:02:04 +0100

    clean up weather tests

--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -3,7 +3,6 @@ describe Weather do
     let(:weather) { Weather.new }
     let(:weather_types) { weather = Weather.new; weather.types.each }
     it '.query' do
-        # expect(weather.query).to eq(:sunny).or(eq(:stormy))
         expect(weather_types).to include(weather.query)
     end
 end
