a8e981e1735ab64916d3606b21fbae03a12ecf7d
Geoff Smith <smith260194@gmail.com>
2019-05-18 08:53:20 +0100

    Formatting

--- lib/plane.rb
+++ lib/plane.rb
@@ -1,11 +1,14 @@
 class Plane
   attr_accessor :flying
+
   def initialize(flying = true)
     @flying = flying
   end
+
   def land
     self.flying = false
   end
+
   def flying?
     flying
   end
