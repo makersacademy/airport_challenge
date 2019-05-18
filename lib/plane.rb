2c87e256390e692dcb79d93491965d7c8a07c280
Peter Cole <11048765+peterjcole@users.noreply.github.com>
2019-05-18 20:11:21 +0100

    refactor and fix tests

--- lib/plane.rb
+++ lib/plane.rb
@@ -2,36 +2,45 @@ class Plane
   BAD_WEATHER_MESSAGE = "I can't do that, the weather is stormy!"
   NOT_AT_AIRPORT_MESSAGE = "I'm not at that airport!"
   ALREADY_AT_AIRPORT_MESSAGE = "I'm already at an airport!"
+  ALREADY_IN_AIR_MESSAGE = "I'm already in the air!!"
 
   def initialize(state = :flying)
     @state = state
   end
 
   def land(airport)
-    if @state == :flying
-      case airport.cleared_for_landing?(self)
-      when :weather
-        return BAD_WEATHER_MESSAGE
-      when true
-        airport.receive(self)
-        @state = :landed 
-        return "Plane landed at #{airport.name} (#{airport.code})"
-      end
-    else
-      return ALREADY_AT_AIRPORT_MESSAGE
+    return ALREADY_AT_AIRPORT_MESSAGE if @state == :landed
+    case airport.cleared_for_landing?(self)
+    when :weather
+      return BAD_WEATHER_MESSAGE
+    when true
+      landing_procedure(airport)
     end
   end
 
   def take_off(airport)
+    return ALREADY_IN_AIR_MESSAGE if @state == :flying
     case airport.cleared_for_takeoff?(self)
     when :weather
       return BAD_WEATHER_MESSAGE
     when :not_in_airport
       return NOT_AT_AIRPORT_MESSAGE
     when true 
-      airport.release(self)
-      @state = :flying
-      return "Plane took off from #{airport.name} (#{airport.code})"
+      takeoff_procedure(airport)
     end
+  end
+
+  private
+
+  def landing_procedure(airport)
+    airport.receive(self)
+    @state = :landed 
+    return "Plane landed at #{airport.name} (#{airport.code})"
+  end
+
+  def takeoff_procedure(airport)
+    airport.release(self)
+    @state = :flying
+    return "Plane took off from #{airport.name} (#{airport.code})"
   end
 end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -8,6 +8,8 @@ describe 'plane' do
   let(:bad_weather_message) { Plane::BAD_WEATHER_MESSAGE }
   let(:not_at_airport_message) { Plane::NOT_AT_AIRPORT_MESSAGE }
   let(:already_at_airport_message) { Plane::ALREADY_AT_AIRPORT_MESSAGE }
+  let(:already_in_air_message) { Plane::ALREADY_IN_AIR_MESSAGE }
+
 
   context 'when landing at an airport' do
     it 'returns a friendly string' do
@@ -53,21 +55,26 @@ describe 'plane' do
   context "when taking off from an airport" do
     it "returns a friendly string" do
       allow(lsx_airport).to receive(:cleared_for_takeoff?).with(plane).and_return(true)
+      allow(lsx_airport).to receive(:cleared_for_landing?).with(plane).and_return(true)
       plane.land(lsx_airport)
       expect(plane.take_off(lsx_airport)).to eq("Plane took off from #{lsx_airport.name} (#{lsx_airport.code})")
     end
     it "checks whether it's cleared for take off" do
+      allow(lsx_airport).to receive(:cleared_for_landing?).with(plane).and_return(true)
       plane.land(lsx_airport)
       expect(lsx_airport).to receive(:cleared_for_takeoff?)
       plane.take_off(lsx_airport)
     end
     it "calls release on airport" do
       allow(lsx_airport).to receive(:cleared_for_takeoff?).with(plane).and_return(true)
+      allow(lsx_airport).to receive(:cleared_for_landing?).with(plane).and_return(true)
       plane.land(lsx_airport)
       expect(lsx_airport).to receive(:release).with(plane)
       plane.take_off(lsx_airport)
     end
     it "calls cleared_for_takeoff? on airport with self" do
+      allow(lsx_airport).to receive(:cleared_for_landing?).with(plane).and_return(true)
+      plane.land(lsx_airport)
       expect(lsx_airport).to receive(:cleared_for_takeoff?).with(plane)
       plane.take_off(lsx_airport)
     end
@@ -76,6 +83,7 @@ describe 'plane' do
   context "when asked to take off from an airport but the weather is bad" do
     it "does not call release on airport" do
       allow(lsx_airport).to receive(:cleared_for_takeoff?).with(plane).and_return(:weather)
+      allow(lsx_airport).to receive(:cleared_for_landing?).with(plane).and_return(true)
       plane.land(lsx_airport)
       expect(lsx_airport).not_to receive(:release)
       plane.take_off(lsx_airport)
@@ -83,6 +91,7 @@ describe 'plane' do
     
     it 'returns a friendly string' do
       allow(lsx_airport).to receive(:cleared_for_takeoff?).with(plane).and_return(:weather)
+      allow(lsx_airport).to receive(:cleared_for_landing?).with(plane).and_return(true)
       plane.land(lsx_airport)
       expect(plane.take_off(lsx_airport)).to eq(bad_weather_message)
     end
@@ -92,6 +101,7 @@ describe 'plane' do
     it "does not call release on airport" do
       allow(lsx_airport).to receive(:cleared_for_takeoff?).with(plane).and_return(true)
       allow(dhs_airport).to receive(:cleared_for_takeoff?).with(plane).and_return(:not_in_airport)
+      allow(lsx_airport).to receive(:cleared_for_landing?).with(plane).and_return(true)
       plane.land(lsx_airport)
       expect(dhs_airport).not_to receive(:release)
       plane.take_off(dhs_airport)
@@ -100,13 +110,15 @@ describe 'plane' do
     it "returns a friendly string" do
       allow(lsx_airport).to receive(:cleared_for_takeoff?).with(plane).and_return(true)
       allow(dhs_airport).to receive(:cleared_for_takeoff?).with(plane).and_return(:not_in_airport)
-      plane.take_off(lsx_airport)
+      allow(lsx_airport).to receive(:cleared_for_landing?).with(plane).and_return(true)
+      plane.land(lsx_airport)
       expect(plane.take_off(dhs_airport)).to eq(not_at_airport_message)
     end
 
     it "can later be asked to take off from the correct airport" do
       allow(lsx_airport).to receive(:cleared_for_takeoff?).with(plane).and_return(true)
       allow(dhs_airport).to receive(:cleared_for_takeoff?).with(plane).and_return(:not_in_airport)
+      allow(lsx_airport).to receive(:cleared_for_landing?).with(plane).and_return(true)
       plane.land(lsx_airport)
       plane.take_off(dhs_airport)
       expect(lsx_airport).to receive(:release).with(plane)
@@ -116,6 +128,7 @@ describe 'plane' do
 
   context "when asked to take off if already in the air" do
     it "won't take off again until landing" do
+      allow(lsx_airport).to receive(:cleared_for_landing?).with(plane).and_return(true)
       plane.land(lsx_airport)
       allow(lsx_airport).to receive(:cleared_for_takeoff?).with(plane).and_return(true)
       plane.take_off(lsx_airport)
@@ -126,12 +139,13 @@ describe 'plane' do
     end
 
     it "returns a friendly string" do
+      allow(lsx_airport).to receive(:cleared_for_landing?).with(plane).and_return(true)
       plane.land(lsx_airport)
       allow(lsx_airport).to receive(:cleared_for_takeoff?).with(plane).and_return(true)
       plane.take_off(lsx_airport)
       expect(lsx_airport).not_to receive(:release)
       allow(lsx_airport).to receive(:cleared_for_takeoff?).with(plane).and_return(:not_in_airport)
-      expect(plane.take_off(lsx_airport)).to eq(not_at_airport_message)
+      expect(plane.take_off(lsx_airport)).to eq(already_in_air_message)
     end
 
   end
