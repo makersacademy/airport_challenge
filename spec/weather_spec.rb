0d0ed3ac0aa6888eba01216f9f077bb0b07af4d5
Alexis Josephides <ajosephides@gmail.com>
2019-06-01 13:48:23 +0100

    refactored weather test to remove a duplicate

--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -1,9 +1,7 @@
 require 'weather'
 
 describe Weather do
-
-  it { is_expected.to respond_to(:generate_weather) }
-
+  
   it 'generate weather that is either sunny or stormy' do
     expect(subject.generate_weather).to eq('sunny').or eq('stormy')
   end
