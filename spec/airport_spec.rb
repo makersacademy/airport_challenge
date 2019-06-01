4be7155e0b3b6b41ed22ac57e21c3140c316ed90
Danielandro <mtme13@hotmail.com>
2019-06-01 17:19:57 +0100

    Test land accepts one argument

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -4,4 +4,10 @@ describe Airport do
   it 'exists as a class' do
     expect(described_class).to eq(Airport)
   end
+
+  it { is_expected.to respond_to(:land) }
+
+  it 'accepts one argument to land' do         
+    expect { subject.land(1) }.not_to raise_error
+  end
 end
