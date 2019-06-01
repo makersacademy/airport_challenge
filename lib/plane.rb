50972e31d8c0d43d9828a01cde9e20e0e8cca9b2
Rachel Griffiths <racheljgriffiths@googlemail.com>
2019-06-01 16:11:54 +0100

    class setup

--- lib/plane.rb
+++ lib/plane.rb
@@ -1,7 +1,3 @@
 class Plane
 
-  def plane_land
-    true
-  end
-
 end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -1,8 +1,5 @@
 require 'plane'
 
 describe Plane do
-  it 'plane lands in airport' do
-    plane = Plane.new
-    expect(plane.plane_land).to eq true
-  end
+  
 end
