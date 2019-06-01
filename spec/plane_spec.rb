e05ddbc6875d2f747b3acd8e92c7c1e02ad9f45f
Alexis Josephides <ajosephides@gmail.com>
2019-06-01 16:37:38 +0100

    added test to allow plane to land if airport is safe

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -15,6 +15,14 @@ describe Plane do
       subject.take_off(airport)
       expect(subject).not_to be_at_airport
     end
+
+    it 'when it is safe the plane can land' do
+      airport = object_double(Airport.new)
+      allow(airport).to receive(:safe?).and_return(true)
+      subject.land(airport)
+      expect(subject).to be_at_airport
+    end
+
   end
 
   context "airport is not safe" do
