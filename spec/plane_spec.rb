280ea63e2d6e20890342da48b2ca78d7c81b3627
Alexis Josephides <ajosephides@gmail.com>
2019-06-01 16:31:13 +0100

    added test to verify when airport is not safe plane remains at airport. Also refactored and deleted obsolete / duplicate test

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -4,24 +4,25 @@ require 'airport'
 describe Plane do
   it { is_expected.to respond_to(:land) }
 
-  # it 'should confirm that it is no longer at airport once take off' do
-  #   subject.take_off
-  #   expect(subject).not_to be_at_airport 
-  # end
-
   it 'should be initialised with a state of being at the airport' do
     expect(subject.instance_variable_get(:@at_airport)).to be true
   end
 
   context "airport is safe" do
-    #airport = object_double(Airport.new)
-    #before { allow(airport).to receive(:safe?).and_return(true) }
-    
     it 'when it is safe the plane can take off' do
       airport = object_double(Airport.new)
       allow(airport).to receive(:safe?).and_return(true)
       subject.take_off(airport)
       expect(subject).not_to be_at_airport
+    end
+  end
+
+  context "airport is not safe" do
+    it 'when it is not safe the plane cannot take off' do
+      airport = object_double(Airport.new)
+      allow(airport).to receive(:safe?).and_return(false)
+      subject.take_off(airport)
+      expect(subject).to be_at_airport
     end
   end
 
