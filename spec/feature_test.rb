c21315049eba4ff30688abeb1277b1f7467fa8ed
Alexis Josephides <ajosephides@gmail.com>
2019-06-01 14:23:43 +0100

    added airport and weather structure to feature test

--- spec/feature_test.rb
+++ spec/feature_test.rb
@@ -1,8 +1,13 @@
 require_relative '../lib/plane.rb'
 require_relative '../lib/weather.rb'
+require_relative '../lib/airport.rb'
 
+airport = Airport.new
 plane = Plane.new
+weather = Weather.new
 puts "Plane at airport? #{plane.at_airport?}"
+puts "Weather at airport: #{weather.generate_weather}"
+puts "Airport condition: #{airport.safe?}"
 puts "Plane asked to take-off"
 plane.take_off
 puts "Plane at airport? #{plane.at_airport?}"
