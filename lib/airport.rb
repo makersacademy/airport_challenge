9fbb6678ed8afaed6ef96ac8b8900cc86dd6df0e
LucyMHall <lucymhall1995@gmail.com>
2019-06-01 17:18:06 +0100

    Plane cannot take off from an airport if it is already in the sky

--- lib/airport.rb
+++ lib/airport.rb
@@ -12,11 +12,13 @@ class Airport
   def land(plane, weather)
     raise "Weather preventing landing" if weather.stormy?
     raise "Airport full" if full?
+    raise "Plane already landed" if @contents.include?(plane)
     @contents << plane
   end
 
   def take_off(plane, weather)
     raise "Weather preventing takeoff" if weather.stormy?
+    raise "Plane already in the sky" if !@contents.include?(plane)
     @contents.delete(plane)
     "Take off complete"
   end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -8,6 +8,7 @@ before(:each) do
   @airport = Airport.new
   @plane = Plane.new
   @weather = Weather.new
+  allow(@weather).to receive(:stormy?) {false}
 end
 
 #User Story 1
@@ -16,7 +17,6 @@ end
   end
 
   it "adds a plane to contents when it has landed a plane" do
-    allow(@weather).to receive(:stormy?) {false}
     @airport.land(@plane,@weather)
     expect(@airport.contents).to include(@plane)
   end
@@ -27,22 +27,17 @@ end
   end
 
   it "no longer contains a plane once it has taken off" do
-    allow(@weather).to receive(:stormy?) {false}
     @airport.land(@plane,@weather)
-    allow(@weather).to receive(:stormy?) {false}
     @airport.take_off(@plane, @weather)
     expect(@airport.contents).not_to include(@plane)
   end
 
   it "confirms take off has occurred" do
-    allow(@weather).to receive(:stormy?) {false}
     @airport.land(@plane,@weather)
-    allow(@weather).to receive(:stormy?) {false}
     expect(@airport.take_off(@plane, @weather)).to eq("Take off complete")
   end
 #User Story 3
   it "does not allow take off if weather is stormy" do
-    allow(@weather).to receive(:stormy?) {false}
     @airport.land(@plane,@weather)
     allow(@weather).to receive(:stormy?) {true}
     expect{@airport.take_off(@plane,@weather)}.to raise_error("Weather preventing takeoff")
@@ -54,21 +49,37 @@ end
   end
 
 #User Story 5
-  it "has a default airport capacity which can be overridden" do
+  it "has a default airport capacity" do
     expect(@airport.capacity).to eq(5)
+  end
+
+  it "has a capacity which can be overridden" do
+    @airport.capacity = 3
+    expect(@airport.capacity).to eq(3)
   end
 
   it "knows when it is full" do
     allow(@airport).to receive(:capacity) {1}
-    allow(@weather).to receive(:stormy?) {false}
     @airport.land(@plane, @weather)
-    allow(@weather).to receive(:stormy?) {false}
-    expect{@airport.land(@plane, @weather)}.to raise_error("Airport full")
+    plane2 = Plane.new
+    expect{@airport.land(plane2, @weather)}.to raise_error("Airport full")
   end
 
-  it "does not all planes to land when airport is full" do
-    allow(@weather).to receive(:stormy?) {false}
+  it "does not allow planes to land when airport is full" do
     allow(@airport).to receive(:full?) {true}
     expect{@airport.land(@plane,@weather)}.to raise_error("Airport full")
   end
+
+  it "can't land a plane that is already inside it" do
+    planeB = Plane.new
+    @airport.land(planeB, @weather)
+    expect{@airport.land(planeB, @weather)}.to raise_error("Plane already landed")
+  end
+
+  it "can't take off a plane which is already in the air" do
+    @airport.land(@plane, @weather)
+    @airport.take_off(@plane, @weather)
+    expect{@airport.take_off(@plane, @weather)}.to raise_error("Plane already in the sky")
+  end
+
 end
