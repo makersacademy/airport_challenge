78b56453c2ddce0539fc4366003057682c7238b8
Rhys Collier <rhys.collier@hotmail.co.uk>
2019-06-01 13:33:23 +0100

    Prevents take off in stormy weather

--- lib/airport.rb
+++ lib/airport.rb
@@ -3,15 +3,19 @@ require './lib/plane.rb'
 class Airport
   def initialize
     @planes = []
+    # it's stormy 1 day a week
+    @weather = rand(7) == 1 ? "stormy" : "sunny"
   end
 
-  attr_reader :planes
+  attr_reader :planes, :weather
 
   def land(plane)
     @planes << plane
   end
 
   def take_off(plane)
+    raise "Stormy weather prevents take off" if weather == "stormy"
+    
     @planes.delete(plane)
   end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,20 +2,33 @@ require 'airport'
 require 'plane'
 
 describe Airport do
+  before(:each) do
+    @plane = Plane.new
+  end
+
   describe '.land' do
     it 'lands a plane' do
-      plane = Plane.new
-      subject.land(plane)
-      expect(subject.planes).to include(plane)
+      subject.land(@plane)
+      expect(subject.planes).to include(@plane)
     end
   end
+
   describe '.take_off' do
     it { is_expected.to respond_to(:take_off).with(1).argument }
     it 'confirms plane is no longer at airport' do
-      plane = Plane.new
-      subject.land(plane)
-      subject.take_off(plane)
+      subject.land(@plane)
+      subject.take_off(@plane)
       expect(subject.planes).to eq([])
+    end
+  end
+
+  context 'is stormy' do
+    before(:each) do
+      allow(subject).to receive(:weather) { "stormy" }
+    end
+
+    it 'prevents take off' do
+      expect { subject.take_off(@plane) }.to raise_error("Stormy weather prevents take off")
     end
   end
 end
