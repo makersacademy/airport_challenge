4fe98a374b6e3b26c544cd5625488a10922df025
Alexis Josephides <ajosephides@gmail.com>
2019-06-01 12:49:20 +0100

    added test to check if plane at airport once taken off

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -6,5 +6,10 @@ describe Plane do
 
   it {is_expected.to respond_to(:take_off)}
 
+  it 'should confirm that it is no longer at airport once take off' do
+    subject.take_off
+    expect(subject).not_to be_at_airport 
+  end
+
 
 end
