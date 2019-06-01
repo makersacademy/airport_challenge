1f0070184fa25709bbfcfb5ecbab2e0df46e8a61
Rhys Collier <rhys.collier@hotmail.co.uk>
2019-06-01 13:41:03 +0100

    refactors take off test

--- lib/airport.rb
+++ lib/airport.rb
@@ -4,7 +4,7 @@ class Airport
   def initialize
     @planes = []
     # it's stormy 1 day a week
-    @weather = rand(7) == 1 ? "stormy" : "sunny"
+    @weather = rand(7) == 0 ? "stormy" : "sunny"
   end
 
   attr_reader :planes, :weather
@@ -15,7 +15,7 @@ class Airport
 
   def take_off(plane)
     raise "Stormy weather prevents take off" if weather == "stormy"
-    
+
     @planes.delete(plane)
   end
 end
