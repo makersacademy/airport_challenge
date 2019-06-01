f06bfe37d567794f5b9e7d4fe5ae867e34e9740c
Alexis Josephides <ajosephides@gmail.com>
2019-06-01 13:47:48 +0100

    it generates weather that is either stormy or sunny

--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -4,6 +4,8 @@ describe Weather do
 
   it { is_expected.to respond_to(:generate_weather) }
 
-
+  it 'generate weather that is either sunny or stormy' do
+    expect(subject.generate_weather).to eq('sunny').or eq('stormy')
+  end
 
 end
