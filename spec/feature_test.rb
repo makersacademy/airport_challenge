9af30ee7ea6472db1a99a8597627011bf0bba146
Alexis Josephides <ajosephides@gmail.com>
2019-06-01 16:48:03 +0100

    added additional feature tests

--- spec/feature_test.rb
+++ spec/feature_test.rb
@@ -2,11 +2,34 @@ require_relative '../lib/plane.rb'
 require_relative '../lib/weather.rb'
 require_relative '../lib/airport.rb'
 
-airport = Airport.new
-plane = Plane.new
-puts "Plane at airport? #{plane.at_airport?}"
-airport.set_safety
-puts "Airport safe: #{airport.safe?}"
-puts "Plane asked to take-off"
-plane.take_off(airport)
-puts "Plane at airport? #{plane.at_airport?}"
+def test_take_off
+  airport = Airport.new
+  plane = Plane.new
+  puts "Plane at airport? #{plane.at_airport?}"
+  airport.set_safety
+  puts "Airport safe: #{airport.safe?}"
+  puts "Plane asked to take-off"
+  plane.take_off(airport)
+  puts "Plane at airport? #{plane.at_airport?}"
+end
+
+def test_landing
+  airport = Airport.new
+  plane = Plane.new
+  puts "Plane at airport? #{plane.at_airport?}"
+  airport.set_safety
+  puts "Airport safe: #{airport.safe?}"
+  puts "Plane asked to take-off"
+  plane.take_off(airport)
+  puts "Plane at airport? #{plane.at_airport?}"
+  airport.set_safety
+  puts "Airport safe: #{airport.safe?}"
+  puts "Plane asked to land"
+  plane.land(airport)
+  puts "Plane at airport? #{plane.at_airport?}"
+end
+
+puts "---------Test 1---------"
+test_take_off
+puts "---------Test 2---------"
+test_landing
