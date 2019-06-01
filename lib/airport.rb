c5fbb2f3adac20d5bebf249eebfa4ff75dd3869d
Ben Scott <bengscott2@gmail.com>
2019-06-01 09:50:01 +0100

    Added weather method with test case

--- lib/airport.rb
+++ lib/airport.rb
@@ -4,7 +4,7 @@ class Airport
   attr_reader :hanger
 
   def initialize
-  @hanger = []
+    @hanger = []
   end
 
   def land(plane)
@@ -12,7 +12,17 @@ class Airport
   end
 
   def takeoff(plane)
+    raise "Can't take off because it's stormy" if weather == 'stormy'
     @hanger.delete(plane)
+  end
+
+  def weather
+    weather = rand(10)
+    if weather <= 2
+      'stormy'
+    else
+      'sunny'
+    end
   end
 
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,7 +1,7 @@
 require 'airport.rb'
 
 describe Airport do
-  it 'lands plane when airport.land is called' do
+  it 'plane can land and get stored in hanger' do
     plane = Plane.new
     subject.land(plane)
     expect(subject.hanger).to include(plane)
@@ -10,9 +10,21 @@ describe Airport do
   it 'take off plane from airport and confirm plane is not in hanger' do
     airport = Airport.new
     plane = Plane.new
+    allow(airport).to receive(:weather) { "sunny" }
     airport.land(plane)
     airport.takeoff(plane)
     expect(airport.hanger).not_to include(plane)
+  end
+
+  context 'when weather is stormy' do
+
+    it 'raises error if trying to take off' do
+      airport = Airport.new
+      plane = Plane.new
+      allow(airport).to receive(:weather) { "stormy" }
+      expect { airport.takeoff(plane) }.to raise_error("Can't take off because it's stormy")
+    end
+
   end
 
 end
