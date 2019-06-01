dfe81197ac267794a431302e76d26bf0dbc06f95
Rhys Collier <rhys.collier@hotmail.co.uk>
2019-06-01 11:46:28 +0100

    adds take off method

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -8,4 +8,11 @@ describe Airport do
       expect(subject.land(plane)).to eq(plane)
     end
   end
+  describe '.take_off' do
+    it { is_expected.to respond_to(:take_off).with(1).argument }
+    it 'confirms plane is not at airport' do
+      plane = Plane.new
+      expect(subject.take_off(plane)).to eq(false)
+    end
+  end
 end
