99c93b8701f8834f35355a67cc7b3333e0950c26
Student <student@makerss-mbp-5.lan>
2019-06-01 19:26:51 +0100

    #take_off confirmation message with airplane id

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,9 +1,14 @@
 require 'airport'
 
 RSpec.describe Airport do 
-
+  
   it ' instruct an airplane to land at the airport ' do
-      expect(subject.land).to be_instance_of(Airplane)
-    end
+    expect(subject.land).to be_instance_of(Airplane)
+  end
+
+  it ' instruct a plane to take off ' do 
+    airplane = Airplane.new
+    expect(subject.take_off(airplane)).to eq "The airplane #{airplane} has taken off"
+  end
 
 end
