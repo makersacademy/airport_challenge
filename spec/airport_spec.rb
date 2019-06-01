c2a7cc00c819868899245899bfef351c18ee3533
Alexis Josephides <ajosephides@gmail.com>
2019-06-01 14:18:24 +0100

    added test to confirm that if weather is sunny airport is safe

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -4,4 +4,10 @@ describe Airport do
 
   it { is_expected.to respond_to(:safe?) }
 
+  it 'should be safe to land / takeoff if weather is sunny' do
+    weather = object_double(Weather.new)
+    allow(weather).to receive(:generate_weather) { 'sunny' }
+    expect(subject).to be_safe
+  end
+
 end
