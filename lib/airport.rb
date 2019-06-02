c56e507b7087e3a25d68a040f8342621252cbb73
Remy Manso <remzilla@Remys-MacBook-Pro.local>
2019-06-02 13:11:54 +0100

    Airport now initialized with state, added methods to land and take off a plane, checking for good flight conditions also. Added 'power of god' random weather generator

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,14 +1,49 @@
+require_relative "plane"
 class Airport
-    attr_accessor :spaces
-    def initialize
-        @spaces = 3
+    attr_accessor :capacity, :weather, :airport_name, :hanger
+
+    def initialize(name)
+        @airport_name = name
+        @capacity = 3
+        @hanger = []
+        @weather
+        weather_generator()
     end 
 
-    def taxi
-        if @spaces == 0
+    def taxi(plane)
+        if @capacity == 0
             raise "No space for new aircraft"
+        elsif @weather == "STORMY"
+            raise "Conditions are unfavourable"
         else
-            @spaces -= 1
+            @capacity -= 1
+            return @hanger << plane.land
+        end
+    end
+
+    def depart(plane)
+        if @hanger.include?(plane) == false
+            raise "There are no planes to takeoff"
+        elsif @weather == "STORMY"
+            raise "Conditions are unfavourable"
+        else
+            # Removes specific plane from hanger
+            @capacity += 1
+            @hanger.delete_at(hanger.index(plane.takeoff))
+            return @hanger
+        end
+    end
+
+    private
+    def weather_generator
+        god_mode = rand(3)
+        case god_mode
+            when 0
+                @weather = "SUNNY"
+            when 1
+                @weather = "CLOUDY"
+            when 2
+                @weather = "STORMY"
         end
     end
 end
