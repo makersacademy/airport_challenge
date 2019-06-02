4a44c72dacea6ec9088407c019530f040bf5652a
Alexis Josephides <ajosephides@gmail.com>
2019-06-02 09:40:28 +0100

    test coverage for completness on plane.

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -7,32 +7,14 @@ describe Plane do
     expect(subject.instance_variable_get(:@at_airport)).to be false
   end
 
-  # it 'when it is clear for landing the plane can land' do
-  #   airport = object_double(Airport.new)
-  #   allow(airport).to receive(:clear_for_landing?).and_return(true)
-  #   subject.land(airport)
-  #   expect(subject).to be_at_airport
-  # end
-
-  # it 'when it is not clear for landing the plane cannot land' do
-  #   airport = object_double(Airport.new)
-  #   allow(airport).to receive(:clear_for_landing?).and_return(false)
-  #   subject.land(airport)
-  #   expect(subject).not_to be_at_airport
-  # end
-
-  # it 'when it is clear for take off the plane will leave airport' do
-  #   airport = object_double(Airport.new)
-  #   allow(airport).to receive(:clear_for_take_off?).and_return(true)
-  #   subject.take_off(airport)
-  #   expect(subject).not_to be_at_airport
-  # end
-
-  # it 'when it is not clear for take off the plane will not leave airport' do
-  #   airport = object_double(Airport.new)
-  #   allow(airport).to receive(:clear_for_take_off?).and_return(false)
-  #   subject.take_off(airport)
-  #   expect(subject).to be_at_airport
-  # end
-
+  it 'should set the instance variable at airport' do
+    subject.in_hanger
+    expect(subject.at_airport).to be true
+  end
+ 
+  it 'should unset the instance variable at airport' do
+    subject.not_in_hanger
+    expect(subject.at_airport).to be false
+  end
+ 
 end
