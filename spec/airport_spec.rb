7c0a3b69538515dff6b53d3a7e6a875c105ec892
Rachel Griffiths <racheljgriffiths@googlemail.com>
2019-06-02 23:23:03 +0100

    initicalized capacity unit test updated

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -19,10 +19,13 @@ describe Airport do
   end
 
   describe 'intialization' do
-    it 'has a variable capacity' do
-      airport = Airport.new(20)
-      20.times { airport.land(Plane.new) }
-      expect { airport.land(Plane.new) }.to raise_error 'Airport full'
+    subject { Airport.new }
+    let(:plane) { Plane.new }
+    it 'defaults capacity' do
+      described_class::DEFAULT_CAPACITY.times do
+        subject.land(plane)
+      end
+      expect { subject.land(Plane.new) }.to raise_error 'Airport full'
     end
   end
 
