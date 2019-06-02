127bd1b9825ed84d10c10459055ab918f2aad292
Celia <celiafrom12@gmail.com>
2019-06-02 17:04:54 +0100

    create add method that saved plane into an array

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,7 +1,17 @@
+require_relative 'plane'
+
 class Airport
+  attr_reader :planetoadd
+  def initialize
+    @planetoadd = []
+  end
 
   def land
     Plane.new
+  end
+
+  def add(plane)
+    @planetoadd << plane
   end
 
   def take_off(plane)
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,12 +1,21 @@
 require 'airport'
 
 describe Airport do
+
   it 'plane takes off' do
     plane = Plane.new
     expect(plane).to respond_to(:take_off)
   end
+
   it 'plane lands' do
     plane = Plane.new
     expect(plane.land).to be_a Plane
-  end 
+  end
+
+  it 'adds a plane to the airport' do
+    plane = Plane.new
+    airport = Airport.new
+    airport.add(plane)
+    expect(airport.planetoadd).to include(plane)
+  end
 end
