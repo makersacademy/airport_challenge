68cc90eaeee286f805fee9a38b43d548d4323ac8
Student <student@makerss-mbp-6.home>
2019-06-03 00:15:39 +0100

    Airport can now store planes and responds to take off

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,4 +1,17 @@
+require './lib/plane'
 class Airport
+  attr_accessor :hangar
+
+  def initialize
+    @hangar = []
+  end
+
   def land(plane)
+    @hangar << plane
+  end
+
+  def take_off(plane)
+    @hangar.pop(plane)
+    "Airborne!"
   end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,8 +1,19 @@
 require 'airport'
+require 'plane'
 
 describe Airport do
   subject(:airport) { described_class.new }
   it 'tells a plane to land' do
     expect(airport).to respond_to(:land).with(1).argument
+  end
+  it "adds a new plane to hangar" do
+    airport = Airport.new
+    plane = Plane.new
+    airport.land(plane)
+    expect(airport.hangar).to include(plane)
+  end
+
+  it "tells a plane to take off" do
+    expect(airport).to respond_to(:take_off).with(1).argument
   end
 end
