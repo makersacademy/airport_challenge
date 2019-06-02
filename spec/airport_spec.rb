8c8b1b0808b221e08bdd7de7171a97f4958822a8
Celia <celiafrom12@gmail.com>
2019-06-02 14:31:02 +0100

    airport spec and fiel created

---
+++ spec/airport_spec.rb
@@ -0,0 +1,10 @@
+# As an air traffic controller
+# So I can get passengers on the way to their destination
+# I want to instruct a plane to take off from an airport
+# and confirm that it is no longer in the airport
+require 'airport'
+describe 'Airport' do
+  it 'plane takes off' do
+
+  end
+end
