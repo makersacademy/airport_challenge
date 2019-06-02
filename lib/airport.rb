ae8c9a430edba773ca8634a52fdca98b4a4bdf5b
Danielandro <mtme13@hotmail.com>
2019-06-02 13:20:10 +0100

    add plane state to airport

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,11 +1,12 @@
 class Airport
   def land(plane)
-    plane
+    @plane = plane
   end
 
   def take_off(plane)
   end
 
   def plane
+    @plane
   end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -12,6 +12,14 @@ describe Airport do
     expect(subject.land(plane)).to eq(plane)
   end
 
+  it 'can see plane is at airport' do
+    plane = Plane.new
+    airport = subject
+    airport.land(plane)
+
+    expect(airport.plane).to eq(plane)
+  end
+
   it { is_expected.to respond_to(:take_off).with(1).argument }
 
   it { is_expected.to respond_to(:plane) }
