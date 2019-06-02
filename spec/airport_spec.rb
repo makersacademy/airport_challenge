c50dc0ecd1587c836a18e08c6232e078c9491228
Student <kaczanowskamonika@gmail.com>
2019-06-02 15:56:58 +0100

    added Plane class

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -3,6 +3,7 @@
 # want to instruct a plane to land at an airport
 
 require 'airport'
+require 'plane'
 
 describe AirportController do
     it { is_expected.to respond_to(:land).with(1).argument }
@@ -12,4 +13,4 @@ describe AirportController do
 #  I want to instruct a plane to take off from an airport and confirm that it is no longer in the airport
   it { is_expected.to respond_to(:take_off).with(1).argument}
 
-end 
+end
