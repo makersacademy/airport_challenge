da3d5dc9bb7e5364f18a44730fdee40d4e4abe9f
Danielandro <mtme13@hotmail.com>
2019-06-02 15:04:44 +0100

    Restart process from defining classes

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,12 +1,3 @@
 class Airport
-  def land(plane)
-    @plane = plane
-  end
-
-  def take_off(plane)
-  end
-
-  def plane
-    @plane
-  end
+    
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,26 +1,9 @@
 require 'airport'
 
 describe Airport do
+  # let(:airport) { airport = Airport.new }
   it 'exists as a class' do
     expect(described_class).to eq(Airport)
   end
-
-  it { is_expected.to respond_to(:land).with(1).argument }
-
-  it 'lands a plane' do
-    plane = Plane.new
-    expect(subject.land(plane)).to eq(plane)
-  end
-
-  it 'can see plane is at airport' do
-    plane = Plane.new
-    airport = subject
-    airport.land(plane)
-
-    expect(airport.plane).to eq(plane)
-  end
-
-  it { is_expected.to respond_to(:take_off).with(1).argument }
-
-  it { is_expected.to respond_to(:plane) }
+  
 end
