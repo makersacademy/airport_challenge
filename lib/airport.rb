035d7cb8954e6cda7e9b5a88280c95bd87a2c780
Lucian Mot <lucian.i.mot@gmail.com>
2019-06-03 07:31:49 +0100

    outlined some of the objs and actions

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,4 +1,30 @@
+require_relative 'plane'
 
 class Airport
+
+  def initialize
+  @hanger = []
+  end
+
+  def land(plane)
+  @hanger << Plane.new
+  end
+
+  def takeoff(plane)
+  @hanger.slice!(0)
+  end
+
+end
+
+class Weather
+
+  def initialize
+  @weather = [sunny, sunny, sunny, sunny, sunny,
+              sunny, sunny, sunny, stormy, stormy]
+  end
+
+  def stormy
+  return @weather.sample
+  end
 
 end
