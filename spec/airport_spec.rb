ccc0382e11797a22ad55c69c74147d76710b752a
Student <student@MacBook-Pro-5.local>
2019-06-03 08:34:21 +0100

    Land/Takeoff method added + storage of planes

---
+++ spec/airport_spec.rb
@@ -0,0 +1,29 @@
+require 'airport'
+require 'plane'
+
+  describe Airport do
+
+	  it 'lands a plane' do
+      airport = Airport.new
+      expect(subject).to respond_to(:land).with(1).argument
+    end
+
+    it 'tells plane to take off' do
+      airport = Airport.new
+      expect(subject).to respond_to(:take_off).with(1).argument
+    end
+
+    it 'stores a plane' do
+      airport = Aiport.new
+      plane = Plane.new
+      aiport.land(plane)
+      airport.take_off(plane)
+      expect(airport.store_planes).not_to include(plane)
+    end
+    
+   describe '#land_plane' do
+	    it 'lands a plane' do
+	      planes = Planes.new
+	      expect(subject.land_plane).to eq [planes]
+	    end
+	  end
