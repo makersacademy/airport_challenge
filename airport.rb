da33e868eeac31faa62b825e5d117172662cf677
Aleks <ajmpawlik@gmail.com>
2019-06-03 08:55:56 +0100

    Add files via upload

---
+++ airport.rb
@@ -0,0 +1,20 @@
+class Airport
+  def get_hangar
+    @hangar = []
+  end
+
+  def allowed_to_land
+    # if :hangar.length > 3
+    #   false
+    # else
+    #   true
+    # end
+  end
+
+  def land_plane
+  end
+
+  def how_many_landed
+  end
+
+end
