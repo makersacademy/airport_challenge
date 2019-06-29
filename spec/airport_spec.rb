ef9638555a69300430f7cc2fb8dd9d4e5802acef
Will Howes <williameldenhowes@gmail.com>
2019-06-29 11:10:39 +0100

    Refactored airport_spec file

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -10,21 +10,24 @@ describe Airport do
   end
 
   describe '#land_plane' do
-    airport = Airport.new
-    plane = Plane.new
-    airport.land_plane(plane)
+
     it "Adds the landed plane to a list of planes" do
+      airport = Airport.new
+      plane = Plane.new
+      airport.land_plane(plane)
       expect(airport.planes).to include(plane)
     end
   end
 
   describe '#plane_take_off' do
-    airport_2 = Airport.new
-    plane_2 = Plane.new
-    airport_2.land_plane(plane_2)
-    airport_2.plane_take_off(plane_2)
+
     it "will remove plane from airport's stock of planes" do
+      airport_2 = Airport.new
+      plane_2 = Plane.new
+      airport_2.land_plane(plane_2)
+      airport_2.plane_take_off(plane_2)
       expect(airport_2.planes).to eq []
     end
   end
+
 end
