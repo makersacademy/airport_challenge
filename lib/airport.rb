752206d866b821b844915091d4beb96055049daf
TBrockster <tobybrock93@gmail.com>
2019-06-29 09:29:48 +0100

    added common-sense user story tests, and passed them, added plane takeoff method

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,5 +1,17 @@
 class Airport
+  def initialize
+    @planes = []
+  end
+
   def land_plane(plane)
+    @planes << plane
     plane
+  end
+
+  def takeoff_plane
+    fail 'No planes available for takeoff' if @planes.empty?
+
+    @planes.pop
+    'Plane has taken off'
   end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -3,8 +3,27 @@ require 'plane'
 
 describe Airport do
   it { is_expected.to respond_to(:land_plane).with(1).argument }
-  it 'stores planes' do
-    plane = Plane.new
-    expect(subject.land_plane(plane)).to eq plane
+  it { is_expected.to respond_to(:takeoff_plane) }
+  describe '#land_plane' do
+    it 'stores planes' do
+      plane = Plane.new
+      expect(subject.land_plane(plane)).to eq plane
+    end
+  end
+
+  describe '#takeoff_plane' do
+    it 'releases planes' do
+      plane = Plane.new
+      subject.land_plane(plane)
+      expect(subject.takeoff_plane).to eq 'Plane has taken off'
+    end
+    it 'does not release planes when no planes are stored' do
+      expect { subject.takeoff_plane }.to raise_error 'No planes available for takeoff'
+      plane = Plane.new
+      subject.land_plane(plane)
+      subject.takeoff_plane
+      expect { subject.takeoff_plane }.to raise_error 'No planes available for takeoff'
+
+    end  
   end
 end
