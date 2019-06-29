413a635dba12c38161400b0a5080bee240d066ad
TBrockster <tobybrock93@gmail.com>
2019-06-29 11:18:52 +0100

    planes will not land or takeoff during stormy weather

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,5 +1,6 @@
 class Airport
   attr_reader :local_weather
+  attr_writer :local_weather
   def initialize
     @planes = []
     @airport_weather_object = Weather.new
@@ -7,14 +8,22 @@ class Airport
   end
 
   def land_plane(plane)
+    fail 'The Stormy weather prevents landing' if @local_weather == 'Stormy'
+    
     @planes << plane
     plane
   end
 
   def takeoff_plane
     fail 'No planes available for takeoff' if @planes.empty?
+    fail 'The Stormy weather prevents takeoff' if @local_weather == 'Stormy'
 
     @planes.pop
     'Plane has taken off'
+  end
+
+  def regenerate_weather
+    puts "Regenerating weather - it is currently #{@local_weather}!"
+    @local_weather = @airport_weather_object.generate_weather
   end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -5,30 +5,52 @@ require 'weather'
 describe Airport do
   it { is_expected.to respond_to(:land_plane).with(1).argument }
   it { is_expected.to respond_to(:takeoff_plane) }
+  it { is_expected.to respond_to(:regenerate_weather) }
   
   it 'generates weather on instantiation' do
     expect(subject.local_weather).to eq('Sunny').or eq('Stormy')
   end
 
   describe '#land_plane' do
+    plane = Plane.new
     it 'stores planes' do
-      plane = Plane.new
+      subject.local_weather = 'Sunny'
       expect(subject.land_plane(plane)).to eq plane
+    end
+    it 'raises error if weather is stormy' do
+      subject.local_weather = 'Stormy'
+      expect { subject.land_plane(plane) }.to raise_error 'The Stormy weather prevents landing'
     end
   end
 
   describe '#takeoff_plane' do
+    plane = Plane.new
     it 'releases planes' do
-      plane = Plane.new
+      subject.local_weather = 'Sunny'
       subject.land_plane(plane)
       expect(subject.takeoff_plane).to eq 'Plane has taken off'
     end
     it 'does not release planes when no planes are stored' do
       expect { subject.takeoff_plane }.to raise_error 'No planes available for takeoff'
-      plane = Plane.new
+      subject.local_weather = 'Sunny'
       subject.land_plane(plane)
       subject.takeoff_plane
       expect { subject.takeoff_plane }.to raise_error 'No planes available for takeoff'
-    end  
+    end
+    it 'raises error if weather is stormy' do
+      subject.local_weather = 'Sunny'
+      subject.land_plane(plane)
+      subject.local_weather = 'Stormy'
+      expect { subject.takeoff_plane }.to raise_error 'The Stormy weather prevents takeoff'
+    end
+  end
+
+  describe '#regenerate_weather' do
+    it 'changes the local_weather object' do
+      x = subject.local_weather
+      subject.regenerate_weather
+      y = subject.local_weather
+      expect(x.object_id).not_to be(y.object_id)
+    end
   end
 end
