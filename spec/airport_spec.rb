4262afff1c7bd59d3612db98c1abb710afcdd89a
Kaja <catcalledkaja@gmail.com>
2019-06-29 14:49:23 +0100

    s an air traffic controller So I can get passengers to a destination I want to instruct a plane to land at an airport

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,7 +2,10 @@ require 'airport'
 require 'plane'
 
 describe Airport do
-  it { is_expected.to respond_to :land }
-
-
+  subject(:airport) { described_class.new }
+  describe '#land' do
+    it 'allows plane to land' do
+    expect(airport).to respond_to(:land).with(1).argument
+    end
+  end
 end
