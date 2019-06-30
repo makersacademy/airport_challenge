bed94dc760ce4c70078064f1cd7656a3e1eeffa9
Student <student@makerss-mbp.lan>
2019-06-30 22:41:27 +0100

    classes created

--- lib/Airport.rb
+++ lib/Airport.rb
@@ -1,15 +1,35 @@
 class Airport
+attr_reader :stormy
 
   def initialize
-    @hangar = []
+@hangar = []
+@stormy = stormy
   end
 
-  def land(plane)
+def land(plane)
   @hangar.push(plane)
+
+end
+
+def take_off
+ raise "hangar is empty" if @hangar.empty?
+ @hangar.pop
+ return "plane has left the airport"
+
+end
+
+def check_weather
+  todays_weather = rand
+  report_storm if todays_weather == "stormy"
+
+end
+
+def report_storm
+  @stormy = true
   end
 
-  def take_off
-  @hangar.pop
-  return "plane has left the airport"
-  end
+def rand
+random_weather = ["sunny", "sunny", "sunny", "sunny", "stormy"].sample
+end
+
 end
--- spec/Airport_spec.rb
+++ spec/Airport_spec.rb
@@ -1,23 +1,32 @@
 require 'Airport'
 require 'plane'
 
+
 describe Airport do
 
   describe '#land' do
   it 'responds to method "land"' do
     expect(subject).to respond_to(:land).with(1).argument
   end
+
   it 'can store a plane in an array' do
     plane = Plane.new
     expect(subject.land(plane)).to eq [plane]
-  end
 
+
+end
 end
 
 describe '#take_off' do
+
   it 'responds to method "take-off"' do
     expect(subject).to respond_to(:take_off)
   end
+
+  it 'raises error if hangar is empty' do
+    expect {subject.take_off}.to raise_error "hangar is empty"
+  end 
+
   it 'enables plane to leave airport' do
     plane = Plane.new
     subject.land(plane)
@@ -25,4 +34,18 @@ describe '#take_off' do
 end
 end
 
+describe '#check_weather' do
+  it 'can check the weather' do
+  #allow(subject).to receive(:check_weather) {"sunny"}
+  expect(subject.check_weather).to eq(nil).or eq(true)
+  end
+ end
+
+describe '#report_storm' do
+  it 'reports a storm if the weather is stormy' do
+    allow(subject).to receive(:rand) {"stormy"}
+    expect(subject.check_weather).to eq true
+
+  end
+end
 end
