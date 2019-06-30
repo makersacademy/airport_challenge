e2ff40d2ad302b1dcb420c1292fd60b3c5c2ede6
Kareem <kareem335@yahoo.co.uk>
2019-06-30 15:55:16 +0100

    default capacity added spec and lib up to date project complete

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,27 +1,39 @@
 require_relative 'plane'
 
 class Airport
+  DEFAULT_CAPACITY = 25
+
   attr_reader :planes
+  attr_accessor :capacity
+
+  def initialize(capacity = DEFAULT_CAPACITY)
+    @capacity = capacity
+    @planes = []
+  end
 
   def stormy?
     weather = ["Sunny", "Stormy"].sample
     weather == "Stormy"
   end
 
-  def initialize
-    @planes = []
-  end
-
-  def land(plane)
+  def land(weather, plane)
+    fail 'Airport full!' if full?
+    fail 'Weather is stormy!' if weather.stormy?
     planes << plane
   end
 
-  def takeoff(weather, planers)
+  def takeoff(weather)
     fail 'Weather is stormy!' if weather.stormy?
     planes.pop
   end
 
-  def confirm(plane)
+  def confirm
     planes.count
+  end
+
+  private
+
+  def full?
+    planes.count >= capacity
   end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,28 +2,58 @@ require 'airport'
 
 describe Airport do
   it 'harbours planes in airport' do
+    weather = double(:weather, stormy?: false)
     plane = Plane.new
-    expect(subject.land(plane)).to eq [plane]
+    expect(subject.land(weather, plane)).to eq [plane]
   end
 
   it 'lets planes take off from airport' do
     weather = double(:weather, stormy?: false)
     plane = Plane.new
-    subject.land(plane)
-    expect(subject.takeoff(weather, plane)).to eq plane
+    subject.land(weather, plane)
+    expect(subject.takeoff(weather)).to eq plane
   end
 
   it 'is a plane down after plane has taken off' do
     weather = double(:weather, stormy?: false)
     plane = Plane.new
-    subject.land(plane)
-    subject.takeoff(weather, plane)
-    expect(subject.confirm(plane)).to eq 0
+    subject.land(weather, plane)
+    subject.takeoff(weather)
+    expect(subject.confirm).to eq 0
   end
 
   it 'takeoff prevented when weather is stormy' do
     weather = double(:weather, stormy?: true)
+    expect { subject.takeoff(weather) }.to raise_error 'Weather is stormy!'
+  end
+
+  it 'landing prevented when weather is stormy' do
+    weather = double(:weather, stormy?: true)
     plane = Plane.new
-    expect{ subject.takeoff(weather, plane) }.to raise_error 'Weather is stormy!'
+    expect { subject.land(weather, plane) }.to raise_error 'Weather is stormy!'
+  end
+
+  it 'sets default capacity to 25' do
+    expect(subject.capacity).to eq Airport::DEFAULT_CAPACITY
+  end
+
+  it 'defaults capacity' do
+    described_class::DEFAULT_CAPACITY.times do
+      weather = double(:weather, stormy?: false)
+      plane = Plane.new
+      subject.land(weather, plane)
+    end
+    weather = double(:weather, stormy?: false)
+    plane = Plane.new
+    expect { subject.land(weather, plane) }.to raise_error 'Airport full!'
+  end
+
+  it 'does not exceed default capacity' do
+    Airport::DEFAULT_CAPACITY.times do
+      weather = double(:weather, stormy?: false)
+      subject.land(weather, Plane.new)
+    end
+    weather = double(:weather, stormy?: false)
+    expect { subject.land(weather, Plane.new) }.to raise_error 'Airport full!'
   end
 end
