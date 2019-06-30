96418fbbd860edbe193c55a8452bae8a6bc84dd2
Student <student@makerss-mbp.lan>
2019-06-30 23:27:56 +0100

    basic user stories done

--- lib/Airport.rb
+++ lib/Airport.rb
@@ -1,18 +1,26 @@
 class Airport
 attr_reader :stormy
+attr_reader :capacity
+DEFAULT_CAPACITY = 10
 
-  def initialize
+  def initialize(capacity = DEFAULT_CAPACITY)
 @hangar = []
 @stormy = stormy
+@capacity = capacity
+
   end
 
 def land(plane)
+  raise "hangar is full" if full?
+  raise "too stormy to land" if @stormy
   @hangar.push(plane)
+
 
 end
 
 def take_off
  raise "hangar is empty" if @hangar.empty?
+ raise "too stormy for take-off" if @stormy
  @hangar.pop
  return "plane has left the airport"
 
@@ -28,8 +36,15 @@ def report_storm
   @stormy = true
   end
 
+private
+
 def rand
 random_weather = ["sunny", "sunny", "sunny", "sunny", "stormy"].sample
+end
+
+def full?
+  @hangar.count >= @capacity ? true : false
+
 end
 
 end
--- spec/Airport_spec.rb
+++ spec/Airport_spec.rb
@@ -4,6 +4,15 @@ require 'plane'
 
 describe Airport do
 
+  it 'when 8 is passed as an initialized argument, hangar capacity will be 8' do
+      expect(Airport.new(8).capacity).to eq 8
+    end
+
+    it 'sets capacity to default, 10, if no capacity is given as an argument' do
+      expect(subject.capacity).to eq Airport::DEFAULT_CAPACITY
+    end
+
+
   describe '#land' do
   it 'responds to method "land"' do
     expect(subject).to respond_to(:land).with(1).argument
@@ -12,9 +21,21 @@ describe Airport do
   it 'can store a plane in an array' do
     plane = Plane.new
     expect(subject.land(plane)).to eq [plane]
-
-
 end
+
+  it 'can prevent a plane from landing if its too stormy' do
+    plane = Plane.new
+    allow(subject).to receive(:rand) {"stormy"}
+    subject.check_weather
+    expect {subject.land(plane)}.to raise_error "too stormy to land"
+  end
+
+  it "prevents a plane from landing if hangar is full" do
+  Airport::DEFAULT_CAPACITY.times {subject.land(Plane.new)}
+  expect {subject.land(Plane.new)}.to raise_error "hangar is full"
+  end
+
+
 end
 
 describe '#take_off' do
@@ -25,7 +46,15 @@ describe '#take_off' do
 
   it 'raises error if hangar is empty' do
     expect {subject.take_off}.to raise_error "hangar is empty"
-  end 
+  end
+
+  it 'prevents take-off if stormy' do
+    plane = Plane.new
+    subject.land(plane)
+    allow(subject).to receive(:rand) {"stormy"}
+    subject.check_weather
+    expect {subject.take_off}.to raise_error "too stormy for take-off"
+  end
 
   it 'enables plane to leave airport' do
     plane = Plane.new
