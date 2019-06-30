08d3cc3a519a475b58563ff06f25f98a5ca4daf1
Richie <richieganney@gmail.com>
2019-06-30 22:23:28 +0100

    Weather functionality now included. Tests also include method stubs.

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -4,11 +4,13 @@ describe Airport do
 
   describe '#instruct_land' do
     it 'instructs a plane to land at an airport' do
+        allow(subject.weather).to receive(:sample) { "sunny" }
         plane = Plane.new
         expect(subject.instruct_land(plane)).to eq subject.planes
     end
 
-    it '#instructs a plane to land when there is already one there' do
+    it 'instructs a plane to land when there is already one there' do
+      allow(subject.weather).to receive(:sample) { "sunny" }
       plane = Plane.new
       subject.instruct_land(plane)
       plane2 = Plane.new
@@ -16,27 +18,43 @@ describe Airport do
     end
 
     it 'raises an error if a grounded plane tries to land' do
+        allow(subject.weather).to receive(:sample) { "sunny" }
         plane = Plane.new
         subject.instruct_land(plane)
         expect { subject.instruct_land(plane) }.to raise_error("Error: this plane has already landed")
     end
 
+    it 'raises an error when a plane tries to land in stormy weather' do
+        allow(subject.weather).to receive(:sample) { "stormy" }
+        expect { subject.instruct_land Plane.new }.to raise_error("Permission to land denied due to stormy weather")
+      end
+
   end
 
   describe '#instruct_take_off' do
     it 'instructs a plane to take off from an airport' do
-      plane = Plane.new
-      subject.instruct_land(plane)
+      allow(subject.weather).to receive(:sample) { "sunny" }
+      subject.instruct_land Plane.new
       expect(subject.instruct_take_off).to eq subject.planes
     end
 
     it 'raises an error when there are no planes in the airport' do
+      allow(subject.weather).to receive(:sample) { "sunny" }
       expect {subject.instruct_take_off }.to raise_error("Error: no planes in airport")
     end
+    
+    it 'raises an error when a plane tries to take-off in stormy weather' do
+      allow(subject.weather).to receive(:sample) { "sunny" }
+      subject.instruct_land Plane.new
+      allow(subject.weather).to receive(:sample) { "stormy" }
+      expect { subject.instruct_take_off }.to raise_error("Permission to take off denied due to stormy weather")
+    end
+
   end
 
   describe '#max_capacity' do
     it 'raises an error when a plane tries to land in a full airport' do
+      allow(subject.weather).to receive(:sample) { "sunny" }
       subject.max_capacity.times { subject.instruct_land Plane.new }
       expect { subject.instruct_land Plane.new }.to raise_error("Error: Airport is full. find somewhere else to land")
     end
