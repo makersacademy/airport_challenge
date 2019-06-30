c7e9546588b898a5ed2660bb0e13fdca828e3a8d
Richie <richieganney@gmail.com>
2019-06-30 22:37:09 +0100

    rubocop clean-up

--- lib/airport.rb
+++ lib/airport.rb
@@ -3,48 +3,48 @@ require_relative './plane.rb'
 
 class Airport
 
-attr_reader :planes
-attr_reader :max_capacity
-attr_reader :weather
+  attr_reader :planes
+  attr_reader :max_capacity
+  attr_reader :weather
 
-def initialize(max_capacity = 20)
-  @max_capacity = max_capacity
-  @planes = []
-  @weather = ["stormy", "sunny", "sunny", "sunny", "sunny", "sunny"]
-end
+  def initialize(max_capacity = 20)
+    @max_capacity = max_capacity
+    @planes = []
+    @weather = ["stormy", "sunny", "sunny", "sunny", "sunny", "sunny"]
+  end
 
- def instruct_land(plane)
-   if stormy?
-     raise("Permission to land denied due to stormy weather")
-   elsif airport_full?
-     raise("Error: Airport is full. find somewhere else to land")
-   else
-     @planes.include?(plane) ? raise("Error: this plane has already landed") : planes << plane
-   end
- end
+  def instruct_land(plane)
+    if stormy?
+      raise("Permission to land denied due to stormy weather")
+    elsif airport_full?
+      raise("Error: Airport is full. find somewhere else to land")
+    else
+      @planes.include?(plane) ? raise("Error: this plane has already landed") : planes << plane
+    end
+  end
 
- def instruct_take_off
+  def instruct_take_off
     if stormy?
       raise("Permission to take off denied due to stormy weather")
     else 
       planes.empty? ? raise("Error: no planes in airport") : authorize
     end
- end
+  end
 
  private
 
- def authorize
-    puts "#{planes.pop} has successfully taken off" if !planes.empty?
+  def authorize
+    puts "#{planes.pop} has successfully taken off" unless planes.empty?
     @planes
- end
+  end
 
- def airport_full?
-   @planes.length >= @max_capacity
- end
+  def airport_full?
+    @planes.length >= @max_capacity
+  end
 
- def stormy?
-   weather.sample == "stormy"
- end
+  def stormy?
+    weather.sample == "stormy"
+  end
 
 end
 
