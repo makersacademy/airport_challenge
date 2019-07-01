cb77e75e7b6dc58deefceab50acbb2e88b2bda98
jamie palmer <james.palmer@cantab.net>
2019-07-01 02:36:16 +0100

    challenge completed

---
+++ lib/weather.rb
@@ -0,0 +1,10 @@
+module Weather
+  def stormy?
+    return true if chance == 0
+    return false
+  end
+  def chance
+    rand(5)
+  end
+
+end
---
+++ spec/weather_spec.rb
@@ -0,0 +1,2 @@
+#I test the methods of this module inside the specs of
+#the classes that use the random method
