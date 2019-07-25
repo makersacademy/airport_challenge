2378c2a13f8255d80597840c614a1fdac200d65d
Jodi <jodiallysonfoster@gmail.com>
2019-07-25 22:23:39 +0100

    When airport is empty, no plane is released

--- lib/Airport.rb
+++ lib/Airport.rb
@@ -1,11 +1,35 @@
+require_relative 'Plane.rb'
+
 class Airport
 
+  def takeoff
+    fail "No planes available" unless @plane
+    @plane
+    
+
+  end
+
   def land(plane)
-   "Plane landed"
+    @plane = plane
   end
 
-  def takeoff(plane)
-    "Plane has takenoff"
-  end
-
+  # def plane_check
+  # end
 end
+
+
+
+# class Airport
+#
+#   def land(plane)
+#    "Plane landed"
+#   end
+#
+#   def takeoff(plane)
+#     Plane.new
+#     "Plane has takenoff, no longer in the airport"
+#   end
+#
+#
+#
+# end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,15 +1,53 @@
-require "Airport.rb"
-require "Plane.rb"
+require "Airport"
+require "Plane"
 
 describe Airport do
 
-  it "allows a plane to land" do
-
-    expect(subject.land Plane).to eq("Plane landed")
-  end
-
-
-  it "allows a plane to take off, and confirms its gone" do
-  expect(subject.takeoff Plane).to eq("Plane has takenoff")
-  end
+  it "allows planes to land" do
+  plane = Plane.new
+  expect(subject.land(plane)).to eq plane
 end
+
+
+describe "#takeoff" do
+it "allows a plane to take off" do
+  plane = Plane.new
+  subject.land(plane)
+  expect(subject.takeoff).to eq plane 
+
+end
+
+  it "raises an error when there are no airplanes to takeoff" do
+  expect { subject.takeoff }.to raise_error "No planes available"
+end
+
+end
+end
+
+
+
+
+
+# it { is_expected.to respond_to(:plane_check)}
+
+
+# require "Airport.rb"
+# require "Plane.rb"
+#
+# describe Airport do
+#
+#   it "allows a plane to land" do
+#
+#     expect(subject.land Plane).to eq("Plane landed")
+#   end
+#
+#
+#   it "allows a plane to take off, and confirms its gone" do
+#   expect(subject.takeoff Plane).to eq("Plane has takenoff, no longer in the airport")
+#   end
+#
+#   it "prevents landing when the airport is full" do
+#
+#
+#       expect{subject.takeoff Bike.new}.to raise_error "Airport full, can't land"
+# end
