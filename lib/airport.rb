64dadec90fd75d0de2afa01ca8f38ad437d139d7
Michael Redgrave <michael@breathehr.com>
2019-07-27 14:43:00 +0100

    adding a plane being accepted at an airport, and added notes to files

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,7 +1,11 @@
 class Airport
 
 	def land_plane
-	
+	# for landing a plane
+	end
+
+	def accept_plane
+	# for airport accepting a plane
 	end
 
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,7 +1,14 @@
 require 'Airport'
 
 describe Airport do
+	# test being able to land plane at airport
 	it 'reponds to land_plane' do
 		expect(subject).to respond_to :land_plane
 	end
+
+	# test plane being accepted at airport
+	it 'reponds to accept_plane' do
+		expect(subject).to respond_to :accept_plane
+	end
+
 end
