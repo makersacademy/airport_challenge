352cf5d2632db5077f666d8752fd4d67cd6b2a3a
kierepkae <kierepkaedyta@gmail.com>
2019-07-27 15:15:54 +0100

    add private method full? - refactoring

--- lib/airport.rb
+++ lib/airport.rb
@@ -9,12 +9,17 @@ class Airport
   attr_reader :planes, :capacity
 
   def land(plane)
-    fail "Full capacity reached! You can not land here." if planes.count >= capacity
+    fail "Full capacity reached! You can not land here." if full?
     @planes << plane
   end
 
   def take_off(plane)
     @planes.delete(plane)
+  end
+
+  private
+  def full?
+    planes.count >= capacity
   end
 
 end
