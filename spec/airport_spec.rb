0a0b5300f72b31a723db77dcc85a78a1b98ddaf8
kierepkae <kierepkaedyta@gmail.com>
2019-07-27 16:21:49 +0100

    add failing test for takin off in stormy weahter

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,8 +1,8 @@
 require 'airport'
 require 'plane'
 
-
 describe Airport do
+
   subject(:airport) {described_class.new(10)}
   let(:plane) {double(:plane)}
 
@@ -12,6 +12,8 @@ describe Airport do
       allow(airport).to receive(:stormy?).and_return false
       expect(airport).to respond_to(:land).with(1).argument
     end
+
+
     context 'when airport is full'do
       it 'stop plane from landing when airport is full' do
         allow(airport).to receive(:stormy?).and_return false
@@ -19,23 +21,29 @@ describe Airport do
         expect { airport.land plane}.to raise_error "Full capacity reached! You can not land here."
       end
     end
+
     context 'when weather is stromy' do
       it "doesn't allow plane to land in stormy weather" do
         allow(airport).to receive(:stormy?).and_return true
         expect {airport.land(plane)}.to raise_error "Stormy weather! You can not land!!"
       end
     end
-
   end
 
 
 
   describe '#take_off' do
     it {is_expected.to respond_to(:take_off).with(1).argument}
-
     it "doesn't store plane after it took off" do
       airport.take_off(plane)
       expect(subject.planes).not_to include(plane)
+    end
+  end
+
+  context 'when weahter is stormy' do
+    it "doesn't allow plane to take_off when weahter is stomy" do
+      allow(airport).to receive(:stormy?). and_return true
+      expect{ airport.take_off(plane) }.to raise_error "Stormy weather! You can not take off!!"
     end
   end
 end
