b1704f49e28f36fdbb42cfedc21787db1a398b3f
kierepkae <kierepkaedyta@gmail.com>
2019-07-27 13:28:37 +0100

    update test for land method and add failing test to check ig land method returns landed plane

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -3,5 +3,14 @@ require 'plane'
 
 
 describe Airport do
-  it {is_expected.to respond_to :land}
+  describe '#land' do
+    it 'respond to land method with one argument' do
+      expect(subject).to respond_to(:land).with(1).argument
+    end
+
+    it 'return plane that landed' do
+      plane = Plane.new
+      expect(subject.land(plane)).to eq plane
+    end
+  end
 end
