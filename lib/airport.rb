78171b464f5eaa6ba7ebca7989f8f91476cec7dd
kierepkae <kierepkaedyta@gmail.com>
2019-07-27 14:13:33 +0100

    update airport state to store planes in array, update take_off method to delete plane that took off from array, test pass

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,9 +1,18 @@
 require_relative 'plane'
 
 class Airport
+  def initialize
+    @planes = []
+  end
+
+  attr_reader :planes
 
   def land(plane)
-    plane
+    @planes << plane
+  end
+
+  def take_off(plane)
+    @planes.delete(plane)
   end
 
 end
