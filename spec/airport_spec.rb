92be02b5150f6a7c2f309cc631e842d79a1d05dd
kierepkae <kierepkaedyta@gmail.com>
2019-07-27 14:12:46 +0100

    add test to check if planes are taking off and are not in the airport after that

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -4,17 +4,21 @@ require 'plane'
 
 describe Airport do
   describe '#land' do
+
     it 'respond to land method with one argument' do
       expect(subject).to respond_to(:land).with(1).argument
     end
 
-    it 'return plane that landed' do
-      plane = Plane.new
-      expect(subject.land(plane)).to eq plane
-    end
   end
 
   describe '#take_off' do
     it {is_expected.to respond_to :take_off}
+
+    it "doesn't containt plane that already took off" do
+      airplane = Plane.new
+      airport = Airport.new
+      airport.take_off(airplane)
+      expect(subject.planes).not_to include(airplane)
+    end
   end
 end
