ff2aa7aea5579ffd23025fcef3d68b208d3ed04f
Guillaume <liard.guillaume@gmail.com>
2019-07-27 16:09:16 +0100

    Added: test for random generation of Weather#stormy?

--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -4,4 +4,11 @@ describe Weather do
 
   it { is_expected.to respond_to :stormy? }
 
+  describe "#stormy?" do
+    it "should return true roughly 20% of the time by default" do
+      weather_tests = []
+      1000.times { weather_tests << subject.stormy? }
+      expect(weather_tests.count(true)).to be_within(50).of(200)
+    end
+  end
 end
