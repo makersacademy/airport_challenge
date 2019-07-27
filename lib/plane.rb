14a52c655f9f6b4d30fe693394feb4a87da88482
Guillaume <liard.guillaume@gmail.com>
2019-07-27 21:18:09 +0100

    Passed test: Plane#take_off doesn't allow take-offs from wrong airport

--- lib/plane.rb
+++ lib/plane.rb
@@ -8,11 +8,12 @@ class Plane
   end
 
   def land(airport)
-    @flying_status = false
     @current_airport = airport
+    @flying_status = false
   end
 
   def take_off(airport)
+    raise "Attempting take-off from wrong airport. Aborting procedure." if airport != @current_airport
     @flying_status = true
     @current_airport = nil
   end
