3da1360547cb69b2e8bcd6427f88679bf539fde7
kierepkae <kierepkaedyta@gmail.com>
2019-07-27 15:57:02 +0100

    add failing test to check if it raise an error when plane try to land in stomy weahter

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -11,14 +11,22 @@ describe Airport do
     it "let's plane to land" do
       expect(airport).to respond_to(:land).with(1).argument
     end
-
-    it 'stop plane from landing when airport is full' do
-      plane = double (:plane)
-      10.times {airport.land plane}
-      expect { airport.land plane}.to raise_error "Full capacity reached! You can not land here."
+    context 'when airport is full'do
+      it 'stop plane from landing when airport is full' do
+        10.times {airport.land plane}
+        expect { airport.land plane}.to raise_error "Full capacity reached! You can not land here."
+      end
+    end
+    context 'when weather is stromy' do
+      it "doesn't allow plane to land in stormy weather" do
+        allow(airport).to receive(:stormy?).and_return true
+        expect {airport.land(plane)}.to raise_error "Stormy weather! You can not land!!"
+      end
     end
 
   end
+
+
 
   describe '#take_off' do
     it {is_expected.to respond_to(:take_off).with(1).argument}
