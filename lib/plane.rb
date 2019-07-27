194441a51bf8e1911966e7fa1e3482a55bd4d89b
Guillaume <liard.guillaume@gmail.com>
2019-07-27 20:48:44 +0100

    Passed tests

--- lib/plane.rb
+++ lib/plane.rb
@@ -1,14 +1,22 @@
 class Plane
 
-  attr_reader :current_airport, :status
+  attr_reader :current_airport, :flying_status
+
+  def initialize(airport)
+    @current_airport = airport
+    @flying_status = false
+  end
 
   def land(airport)
   end
 
   def take_off(airport)
+    @flying_status = true
+    @current_airport = nil
   end
 
   def flying?
+    @flying_status
   end
 
 end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -1,9 +1,15 @@
 require 'plane'
 
 describe Plane do
+  subject { Plane.new(double("airport")) }
+
   it { is_expected.to respond_to(:flying?, :land, :take_off, :current_airport) }
   it "requires an airport at creation" do
     expect(subject.current_airport).not_to eq nil
+  end
+
+  it "is landed at creation" do
+    expect(subject).to_not be_flying
   end
 
   describe "#take off" do
