a107784155435b51393f795907a102b77f11acb4
Student <student@Makerss-MBP-6.broadband>
2019-07-27 18:53:20 +0100

    created a plane and landed it, then stored that plane

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,5 +1,16 @@
+require_relative 'plane'
 class AirportController
-    def land_plane
-        
+
+    #the land methods creates instances of planes
+    def land(plane)
+    # this is an instance variable to store planes
+      @plane = plane
     end
+
+    #the plane thats landed is added to this method called plane
+    #which will return the plane thats landed
+    def plane
+        @plane
+    end
+
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,7 +1,12 @@
 require 'airport'
 
 describe AirportController do
-    it 'responds to land_plane' do
-        expect(subject).to respond_to :land_plane
+    it 'lands a plane' do
+        plane = Plane.new
+        #subject here is airport
+        subject.land(plane)
+        expect(subject.land(plane)).to eq plane
       end
+      #plane method on docking_station
+      it { is_expected.to respond_to(:plane) }
 end
