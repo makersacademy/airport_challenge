c46a6f106a5b1423afc5a29c226ac70bbf91a2ec
kierepkae <kierepkaedyta@gmail.com>
2019-07-27 15:05:58 +0100

    created a test for argument for capacity

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -3,17 +3,18 @@ require 'plane'
 
 
 describe Airport do
+  subject(:airport) {described_class.new(10)}
 
   describe '#land' do
 
     it 'respond to land method with one argument' do
-      expect(subject).to respond_to(:land).with(1).argument
+      expect(airport).to respond_to(:land).with(1).argument
     end
 
     it 'prevent landing when airport full capacity is reached' do
-      airport = Airport.new(10)
-      10.times {airport.land Plane.new}
-      expect { airport.land Plane.new}.to raise_error "Full capacity reached!"
+      plane = Plane.new
+      10.times {airport.land plane}
+      expect { airport.land plane}.to raise_error "Full capacity reached! You can not land here."
     end
 
   end
@@ -22,10 +23,9 @@ describe Airport do
     it {is_expected.to respond_to(:take_off).with(1).argument}
 
     it "doesn't containt plane that already took off" do
-      airplane = Plane.new
-      airport = Airport.new
-      airport.take_off(airplane)
-      expect(subject.planes).not_to include(airplane)
+      plane = Plane.new
+      airport.take_off(plane)
+      expect(subject.planes).not_to include(plane)
     end
   end
 end
