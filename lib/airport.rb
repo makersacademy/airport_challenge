5e1cd412789bd3b019a60224b060fb9a3475c40a
kierepkae <kierepkaedyta@gmail.com>
2019-07-27 15:06:32 +0100

    implement capacity as a argument in initialize method in Airport class ,test pass

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,13 +1,15 @@
 require_relative 'plane'
 
 class Airport
-  def initialize
+  def initialize(capacity)
+    @capacity = capacity
     @planes = []
   end
 
-  attr_reader :planes
+  attr_reader :planes, :capacity
 
   def land(plane)
+    fail "Full capacity reached! You can not land here." if planes.count >= capacity
     @planes << plane
   end
 
