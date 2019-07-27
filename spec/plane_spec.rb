f32a3f57a1cc98ff974630b7cb38cb7b3bca8631
Guillaume <liard.guillaume@gmail.com>
2019-07-27 20:54:38 +0100

    Added 2 tests for Plane#land

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -3,7 +3,10 @@ require 'plane'
 describe Plane do
   subject { Plane.new(double("airport")) }
 
+  let(:fakeport) { double("Airport") }
+
   it { is_expected.to respond_to(:flying?, :land, :take_off, :current_airport) }
+
   it "requires an airport at creation" do
     expect(subject.current_airport).not_to eq nil
   end
@@ -15,13 +18,29 @@ describe Plane do
   describe "#take off" do
     
     it "sets plane flying" do
-      subject.take_off(double("airport"))
+      subject.take_off(fakeport)
       expect(subject).to be_flying
     end
 
     it "removes any current airport data" do
-      subject.take_off(double("airport"))
+      subject.take_off(fakeport)
       expect(subject.current_airport).to eq nil
+    end
+
+  end
+
+  describe '#land' do
+
+    it "stops plane from flying" do
+      subject.take_off(fakeport)
+      subject.land(fakeport)
+      expect(subject).not_to be_flying
+    end
+
+    it "records the aiport it lands at" do
+      subject.take_off(fakeport)
+      subject.land(fakeport)
+      expect(subject.current_airport).to eq fakeport
     end
 
   end
