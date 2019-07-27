2dc19104f4da49dee9ff2c4f78b91f4446513732
kierepkae <kierepkaedyta@gmail.com>
2019-07-27 16:50:43 +0100

    refactoring

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -8,43 +8,48 @@ describe Airport do
 
   describe '#land' do
 
-    it "let's plane to land" do
-      allow(airport).to receive(:stormy?).and_return false
-      expect(airport).to respond_to(:land).with(1).argument
-    end
-
-
-    context 'when airport is full'do
-      it 'stop plane from landing when airport is full' do
+    context 'weather not stormy' do
+      before do
         allow(airport).to receive(:stormy?).and_return false
+      end
+      it "plane land" do
+        expect(airport).to respond_to(:land).with(1).argument
+      end
+      context 'when airport is full'do
+      it 'stop plane from landing' do
         10.times {airport.land plane}
         expect { airport.land plane}.to raise_error "Full capacity reached! You can not land here."
       end
     end
+    end
 
-    context 'when weather is stromy' do
-      it "doesn't allow plane to land in stormy weather" do
+    context 'when weather is stormy' do
+      before do
         allow(airport).to receive(:stormy?).and_return true
+      end
+      it "doesn't allow plane to land" do
         expect {airport.land(plane)}.to raise_error "Stormy weather! You can not land!!"
       end
     end
   end
 
 
-
   describe '#take_off' do
+    context 'weahter is not stormy' do
     it {is_expected.to respond_to(:take_off).with(1).argument}
-    it "doesn't store plane after it took off" do
+    it "remove stored plane after take off" do
       allow(airport).to receive(:stormy?). and_return false
       airport.take_off(plane)
       expect(subject.planes).not_to include(plane)
     end
-  end
-
-  context 'when weahter is stormy' do
-    it "doesn't allow plane to take_off when weahter is stomy" do
-      allow(airport).to receive(:stormy?). and_return true
-      expect{ airport.take_off(plane) }.to raise_error "Stormy weather! You can not take off!!"
     end
+
+    context 'when weahter is stormy' do
+      it "doesn't allow plane to take_off" do
+        allow(airport).to receive(:stormy?). and_return true
+        expect{ airport.take_off(plane) }.to raise_error "Stormy weather! You can not take off!!"
+      end
+    end
+
   end
 end
