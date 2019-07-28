7d481114bf0a519e77ac753217153ae126b30120
EvelinaClayton <clayton.evelina@gmail.com>
2019-07-28 13:01:20 +0100

    refactored code to combine 'land' and 'takeoff' methods into one 'departed'

--- lib/plane.rb
+++ lib/plane.rb
@@ -1,16 +1,15 @@
 class Plane
-    attr_accessor :can_takeoff, :can_land
+    attr_accessor :departed
 
     def initialize
         #by default it is created on the ground (land)
-        @can_takeoff = true 
-        @can_land = false  
+        @departed = false 
     end
     
     def land
-        if @can_land
-            @can_land = false
-            @can_takeoff = true
+        # if @can_land
+        if @departed
+            @departed = false
             true
         else
             false
@@ -18,9 +17,8 @@ class Plane
     end
 
     def takeoff
-        if @can_takeoff
-            @can_land = true
-            @can_takeoff = false
+        if @departed == false
+            @departed = true
             true
         else
             false
