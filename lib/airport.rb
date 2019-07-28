d879a4bc71c2891c2f4f83673e2c1145ae1215a4
laurenlau <harris_lau@hotmail.co.uk>
2019-07-28 16:17:13 +0100

    Airport now has a default capacity that can be overridden

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,26 +1,29 @@
 class Airport
-  def initialize
+  DEFAULT_CAPACITY = 10
+  attr_reader :airport
+  attr_accessor :capacity
+
+  def initialize(capacity = DEFAULT_CAPACITY)
     @airport = []
+    @capacity = capacity
   end
 
   def full?
     # Declares capacity of airport
-    @airport.count >= 10
+    airport.count >= capacity
   end
-  
+
   def empty?
     # Checks if airport is empty
-    @airport.count.zero?
+    airport.count.zero?
   end
 
   def land(plane)
-    # New plane object
-    Plane.new = plane 
     # If the airport is full, raise an error
     fail 'Airport is full' if full?
 
     # If the airport isn't full add plane
-    @airport << plane
+    airport << plane
   end
 
   def take_off
@@ -32,5 +35,4 @@ class Airport
     # If airport is not allow plane to take off
     @airport.pop
   end
-
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -6,12 +6,39 @@ describe Airport do
   it { is_expected.to respond_to(:land).with(1).argument }
   it { is_expected.to respond_to(:take_off) }
 
+  # Capacity can be set upon initialization
+  describe 'initialization' do
+    it 'has a variable capacity' do
+      airport = Airport.new(50)
+      50.times { airport.land(Plane.new) }
+      expect { airport.land(Plane.new) }.to raise_error 'Airport is full'
+    end
+  end
+  # Capcity has a default 
+  describe 'initialization' do
+    it 'has a default capacity' do
+      described_class::DEFAULT_CAPACITY.times do
+        subject.land(Plane.new)
+      end
+      expect { subject.land(Plane.new) }.to raise_error 'Airport is full'
+    end
+  end
+
+  describe '#airport_capacity' do
+    # Declares user input as the airport capacity
+    it 'has a capacity that can be overridden as necessary' do
+      # Airport has a capacity of 10
+    10.times { subject.land(Plane.new) }
+		expect { subject.land(Plane.new) }.to raise_error 'Airport is full'
+		end
+  end
+
   describe '#land' do
     # Raises an error when airport is full
     it 'raises an error when airport is full' do
       # Airport has a capacity of 10
-      10.times { subject.land(Plane.new) }
-		  expect { subject.land(Plane.new) }.to raise_error 'Airport is full'
+    10.times { subject.land(Plane.new) }
+		expect { subject.land(Plane.new) }.to raise_error 'Airport is full'
 		end
   end
 
@@ -21,10 +48,4 @@ describe Airport do
       expect { subject.take_off }.to raise_error 'Airport is empty'
     end
   end
-
-  # Within the plane.rb
-  describe Plane do
-    it { is_expected.to respond_to(:a_plane?) }
-  end
-
 end
