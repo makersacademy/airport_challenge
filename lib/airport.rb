274ea142e8f4955932e14f0ffbf92225256cf62d
laurenlau <harris_lau@hotmail.co.uk>
2019-07-28 02:33:34 +0100

    Airport now has a capacity and planes can land/take off

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,7 +1,36 @@
 class Airport
-  def land
+  def initialize
+    @airport = []
+  end
+
+  def full?
+    # Declares capacity of airport
+    @airport.count >= 10
+  end
+  
+  def empty?
+    # Checks if airport is empty
+    @airport.count.zero?
+  end
+
+  def land(plane)
+    # New plane object
+    Plane.new = plane 
+    # If the airport is full, raise an error
+    fail 'Airport is full' if full?
+
+    # If the airport isn't full add plane
+    @airport << plane
   end
 
   def take_off
+    # If the airport is empty, raise an error
+    raise 'Airport is empty' if empty?
+
+    # Confirms plane is no longer in airport
+    puts "Plane #{@airport.last} has taken off"
+    # If airport is not allow plane to take off
+    @airport.pop
   end
+
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,7 +1,30 @@
 require 'airport'
+require 'plane'
 # Within the Airport class
 describe Airport do
 # Specifies the details of the Airport interface
-  it { is_expected.to respond_to(:land) }
+  it { is_expected.to respond_to(:land).with(1).argument }
   it { is_expected.to respond_to(:take_off) }
+
+  describe '#land' do
+    # Raises an error when airport is full
+    it 'raises an error when airport is full' do
+      # Airport has a capacity of 10
+      10.times { subject.land(Plane.new) }
+		  expect { subject.land(Plane.new) }.to raise_error 'Airport is full'
+		end
+  end
+
+  describe '#take_off' do
+    # Raises an error when airport is empty 
+    it 'raises an error when airport is full' do
+      expect { subject.take_off }.to raise_error 'Airport is empty'
+    end
+  end
+
+  # Within the plane.rb
+  describe Plane do
+    it { is_expected.to respond_to(:a_plane?) }
+  end
+
 end
