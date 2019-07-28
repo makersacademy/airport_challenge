27e2d1e582800544c9c490b468a46cbc965b515c
laurenlau <harris_lau@hotmail.co.uk>
2019-07-28 16:18:46 +0100

    Delete unnecessary code

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -24,15 +24,6 @@ describe Airport do
     end
   end
 
-  describe '#airport_capacity' do
-    # Declares user input as the airport capacity
-    it 'has a capacity that can be overridden as necessary' do
-      # Airport has a capacity of 10
-    10.times { subject.land(Plane.new) }
-		expect { subject.land(Plane.new) }.to raise_error 'Airport is full'
-		end
-  end
-
   describe '#land' do
     # Raises an error when airport is full
     it 'raises an error when airport is full' do
