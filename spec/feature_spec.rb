859c9105f3293bde796e70b256ba13bfb32bf14b
Michael Redgrave <michael@breathehr.com>
2019-07-28 19:05:34 +0100

    tidy up of files

---
+++ spec/feature_spec.rb
@@ -0,0 +1,12 @@
+
+I want to instruct a plane to land at an airport
+
+I want to instruct a plane to take off from an airport and confirm that it is no longer in the airport
+
+I want to prevent takeoff when weather is stormy
+
+I want to prevent landing when weather is stormy
+
+I want to prevent landing when the airport is full
+
+I would like a default airport capacity that can be overridden as appropriate
