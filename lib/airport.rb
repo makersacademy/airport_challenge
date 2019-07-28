0e608886f5245b2dbd9617ba9b7441c74145eba5
RussellWind <russell.wind@gmail.com>
2019-07-28 10:14:51 +0100

    refactored for SRP

--- lib/airport.rb
+++ lib/airport.rb
@@ -12,36 +12,52 @@ class Airport
   end
 
   def land(plane)
-    if @weather == "stormy"
-      raise "cannot land while weather is stormy"
-    elsif full?
-      raise "cannot land while airport is full"
-    elsif plane.location != "in transit"
-      raise "cannot land a plane which is already grounded"
-    else
-      plane.location = @location
-      @planes << plane
-    end
+    landing_checks(plane)
+    touchdown(plane)
   end
 
   def take_off
-    if @weather == "stormy"
-      raise "cannot take off while weather is stormy"
-    elsif @planes[0].location == "in transit"
-      raise "cannot take off while plane is in transit"
-    else
-      plane = @planes[0]
-      @planes.shift
-      plane.location = "in transit"
-      plane
-    end
+    flight_checks
+    charter_flight
   end
 
   def weather(report)
     @weather = report
   end
 
+  private
   def full?
     @planes.count >= @capacity
   end
+
+  def flight_checks
+    if @weather == "stormy"
+      raise "cannot take off while weather is stormy"
+    elsif @planes[0].location == "in transit"
+      raise "cannot take off while plane is in transit"
+    end
+  end
+
+  def charter_flight
+    plane = @planes[0]
+    @planes.shift
+    plane.location = "in transit"
+    plane
+  end
+
+  def landing_checks(plane)
+    if @weather == "stormy"
+      raise "cannot land while weather is stormy"
+    elsif full?
+      raise "cannot land while airport is full"
+    elsif plane.location != "in transit"
+      raise "cannot land a plane which is already grounded"
+    end
+  end
+
+  def touchdown(plane)
+    plane.location = @location
+    @planes << plane
+  end
+
 end
