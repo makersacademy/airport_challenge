2085a2f1a71ef4b1fca6ce326640353fd917d8b1
Chuan Cheng <c_chuan23@hotmail.com>
2019-07-29 09:26:48 +0100

    Use double for plane

--- lib/README.md
+++ lib/README.md
@@ -1,3 +1,3 @@
 The Airport class contain an array.
-Each plane is represented by a "Flight number" string. i.e. "abc123"
+Each plane is an object of class Plane
 Weather condition is represented by a variable "weather". When "weather" is 1 meaning stormy weather, "weather" being 0 means good weather.
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,62 +1,70 @@
 require 'airport'
+require 'plane'
 
 describe Airport do
 
   describe "#land" do
+    let(:random_plane) {double("plane")}
     it "Add one plane to plane" do
       weather = 0
-      random_flight_number = 'abc123'
-      subject.land(random_flight_number, weather)
-      expect(subject.include?(random_flight_number)).to eq true
+      subject.land(random_plane, weather)
+      expect(subject.include?(random_plane)).to eq true
     end
 
     it "prevent from landing in stormy weather" do
       weather = 1
-      expect{subject.land("abc123", weather)}.to output.to_stdout
+      expect{subject.land(random_plane, weather)}.to output.to_stdout
     end
 
     it "prevent from landing if airport is full" do
       weather = 0
-      flight_list = ["abc123", "sdf123", "wer123"]
+      plane1 = double
+      plane2 = double
+      plane3 = double
+      plane4 = double
+      flight_list = [plane1, plane2, plane3]
       flight_list.each{ |flight| subject.land(flight, weather) }
-      expect{subject.land("abb123", weather)}.to output.to_stdout
+      expect{subject.land(plane4, weather)}.to output.to_stdout
     end
 
     it "Raise error when land a plane already in plane list" do
       weather = 0
-      subject.land("abc123", weather)
-      expect{subject.land("abc123", weather)}.to raise_error
+      plane1 = double
+      subject.land(plane1, weather)
+      expect{subject.land(plane1, weather)}.to raise_error
     end
   end
 
   describe "#include?" do
+    let(:random_plane){double}
     it "return true if plane list include the flight number" do
       weather = 0
-      subject.land("abc123", weather)
-      expect(subject.include?("abc123")).to eq(true)
+      subject.land(random_plane, weather)
+      expect(subject.include?(random_plane)).to eq(true)
     end
   end
 
   describe "#takeoff" do
+    let(:random_plane){double}
     it "remove plane from the plane list" do
       weather = 0
-      subject.land("abc123", weather)
-      expect(subject.include?("abc123")).to eq(true)
-      subject.takeoff("abc123", 0)
-      expect(subject.include?("abc123")).to eq(false)
+      subject.land(random_plane, weather)
+      expect(subject.include?(random_plane)).to eq(true)
+      subject.takeoff(random_plane, 0)
+      expect(subject.include?(random_plane)).to eq(false)
     end
 
     it "prevent from taking off in stormy weather" do
       # Stormy weather = 1, good weather = 0
       weather = 0
-      subject.land("abc123", weather)
+      subject.land(random_plane, weather)
       weather = 1
-      expect{subject.takeoff("abc123", weather)}.to output.to_stdout
+      expect{subject.takeoff(random_plane, weather)}.to output.to_stdout
     end
 
     it "prevent from taking off if flight not in airport" do
       weather = 0
-      expect{subject.takeoff("abc123", weather)}.to output.to_stdout
+      expect{subject.takeoff(random_plane, weather)}.to output.to_stdout
     end
   end
 end
