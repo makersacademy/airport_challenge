78ffd080c92332ffcda4406b3b652124bae656ea
EloisePrasad <eloise.prasad@outlook.com>
2019-08-09 17:24:57 +0100

    added array to hold and delete and know total number of planes

--- docs/airport.rb
+++ docs/airport.rb
@@ -1,8 +1,21 @@
+
 class Airport
+
+  attr_reader :num_of_planes
+
+
+
+  def initialize
+    @num_of_planes = []
+  end
 
   def land
   end
 
   def take_off
-  end 
+  end
+
+  def total_planes
+    @num_of_planes.count
+  end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -7,7 +7,15 @@ describe Airport do
 
   it { is_expected.to respond_to :land }
 
-  it { is_expected.to respond_to :take_off}
+  it { is_expected.to respond_to :take_off }
 
+  it { is_expected.to respond_to :total_planes }
+
+  describe '#num_of_planes' do
+    it 'can ' do
+      expect(subject.total_planes).to eq(0)
+    end
+
+  end
 
 end
