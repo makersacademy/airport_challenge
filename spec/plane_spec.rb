c723b40d74dec44541af15f3846f482fa4c3f876
etelish <etelisha@googlemail.com>
2019-08-24 00:31:21 +0100

    test for plane landing at airport

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -4,7 +4,12 @@ describe Plane do
 
 it 'responds to #land_at_airport' do
 expect(subject).to respond_to :land_at_airport
- end
+end
+
+it 'allows plane to #land_at_airport' do
+expect(subject.land_at_airport).to eq plane
+end
+
 
  it 'responds to #take_off' do
    expect(subject).to respond_to :take_off
