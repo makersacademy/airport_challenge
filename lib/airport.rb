ed160376cfee4c19a0dda0ee88dda8f6e2a95e12
James Holton <james_holton@yahoo.co.uk>
2019-08-24 16:05:57 +0100

    Added array to store planes in the aitport

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,6 +1,20 @@
 class Airport
+  @planes_in_airport
+
+  def initialize
+    @planes_in_airport = []
+  end
+
   def land_plane(plane)
     plane
+  end
+
+  def takeoff_plane(plane)
+    true
+  end
+
+  def plane_in_airport?(plane)
+    true
   end
 end
 
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -5,4 +5,12 @@ describe Airport do
     plane1 = Plane.new
     expect(subject.land_plane(plane1)).to eq (plane1)
   end
+  it "Test allows a plane to take off and confirm its not in the airport" do
+    plane1 = Plane.new
+    expect(subject.takeoff_plane(plane1)).to eq (true)
+  end
+  it "Test arrived plane is at the airport" do
+    plane1 = Plane.new
+    expect(subject.plane_in_airport?(plane1)).to eq(true)
+  end
 end
