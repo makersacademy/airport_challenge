9cf5fef1d9fbe13f0546e6394a80b4f36db35a85
James Holton <james_holton@yahoo.co.uk>
2019-08-24 16:24:08 +0100

    Refactored and added a few comments

--- lib/airport.rb
+++ lib/airport.rb
@@ -5,19 +5,20 @@ class Airport
     @planes_in_airport = []
   end
 
+  # land planes at airport
   def land_plane(plane)
     @planes_in_airport << plane
   end
 
+  # Allow planes to take off
   def takeoff_plane(plane)
     @planes_in_airport.delete(plane)
     plane_in_airport?(plane)
   end
 
+  #check if plane is currently in the airport
   def plane_in_airport?(search_plane)
-    @planes_in_airport.each do |plane|
-      return true if search_plane == plane
-    end
+    @planes_in_airport.each {|plane| return true if search_plane == plane}
     false
   end
 end
