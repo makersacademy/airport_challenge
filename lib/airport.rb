42ba4702cd60cfc563c4645fd2e39655161df0fd
James Holton <james_holton@yahoo.co.uk>
2019-08-24 15:36:48 +0100

    added first test to land plane

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,4 +1,7 @@
 class Airport
+  def land_plane(plane)
+    plane
+  end
 end
 
 class Plane
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,5 +1,8 @@
 require "airport.rb"
 
 describe Airport do
-
+  it "Test allow airplane to land at airport" do
+    plane1 = Plane.new
+    expect(subject.land_plane(plane1)).to eq (plane1)
+  end
 end
