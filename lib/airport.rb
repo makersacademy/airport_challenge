17b52b271713274e4cb4c7b579faf63e90c16d5c
Ben <ben@benzeng.co.uk>
2019-08-24 13:47:27 +0100

    initialised @all_planes array

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,12 +1,23 @@
 class Airport
+  #DEFAULT_CAPACITY = 20
+
+  def initialize
+    @all_planes = []
+  end
+
   def land_plane(plane)
     plane
+  end
+
+  def takeoff_plane
+
   end
 end
 
 class Weather
   def stormy?
     return true if rand < 0.1
+
     false
   end
 end
