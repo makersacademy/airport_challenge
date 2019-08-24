8d8d1b15867854a2a65d27de22cd91cf4683a486
Ben <ben@benzeng.co.uk>
2019-08-24 13:23:19 +0100

    added #stormy? and tested for true and false outcomes

--- lib/airport.rb
+++ lib/airport.rb
@@ -5,5 +5,7 @@ end
 
 class Weather
   def stormy?
+    return true if rand < 0.1
+    false
   end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -6,7 +6,17 @@ describe Airport do
 end
 
 describe Weather do
-  it "responded to stormy?" do
-    expect(subject).to respond_to(:stormy?)
+  describe '#random?' do
+    it "Weather class responded to stormy?" do
+      expect(subject).to respond_to(:stormy?)
+    end
+    it "Returns false if #rand is 0.8 (0.8 < 0.1)" do
+      expect(subject).to receive(:rand).and_return(0.8)
+      expect(subject.stormy?).to eq false
+    end
+    it "Returns true if #rand is 0.05 (0.8 < 0.1)" do
+      expect(subject).to receive(:rand).and_return(0.05)
+      expect(subject.stormy?).to eq true
+    end
   end
 end
