481d14fd39518aa1ad9060ad1dde983496628845
Ben <ben@benzeng.co.uk>
2019-08-24 13:03:05 +0100

    added weather class, and stormy? method, along with a test to check Weather responds to stormy? method

--- lib/airport.rb
+++ lib/airport.rb
@@ -2,3 +2,8 @@ class Airport
   def test_method
   end
 end
+
+class Weather
+  def stormy?
+  end
+end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -4,3 +4,9 @@ describe Airport do
     expect(subject).to respond_to(:test_method)
   end
 end
+
+describe Weather do
+  it "responded to stormy?" do
+    expect(subject).to respond_to(:stormy?)
+  end
+end
