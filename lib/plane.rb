4549d7b7cc99c49c8563e8c97c8fd4d3aa060c19
James Holton <james_holton@yahoo.co.uk>
2019-08-25 14:34:08 +0100

    A few methods added to the Plane class, belt and braces checks

--- lib/plane.rb
+++ lib/plane.rb
@@ -1,9 +1,22 @@
 class Plane
+  FLYING = "Flying"
+  LANDED = "Landed"
+
   def initialize
     @status = ""
     @airport
   end
+
   def get_status
     @status
   end
+
+  def land
+    status = LANDED
+  end
+
+  def take_off
+    status = FLYING
+  end
+
 end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -4,4 +4,10 @@ describe Plane do
     subject.instance_variable_set(:@status, "Flying")
     expect(subject.get_status).to eq ("Flying")
   end
+  it "planes' status set to Landed when plane lands" do
+    expect(subject.land).to eq ("Landed")
+  end
+  it "planes' status set to Flying when plane takes off" do
+    expect(subject.take_off).to eq ("Flying")
+  end
 end
