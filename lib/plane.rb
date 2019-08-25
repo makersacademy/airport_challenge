ee8eea81be8b3fe5fc678e531d158445dec48fb0
Mike <mjwalshe@gmail.com>
2019-08-25 21:23:57 +0100

    Add take_off_from and confirmation message

--- lib/plane.rb
+++ lib/plane.rb
@@ -1,9 +1,8 @@
 class Plane
-
   def land_at(airport)
   end
 
   def take_off_from(airport)
+    "Confirming departure from #{airport}"
   end
-
 end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -2,4 +2,11 @@ require 'plane'
 
 describe Plane do
   it { is_expected.to respond_to(:land_at).with(1).argument }
+  it { is_expected.to respond_to(:take_off_from).with(1).argument }
+
+  it "confirms departure" do
+    plane_1 = Plane.new
+    Gatwick = Airport.new
+    expect(plane_1.take_off_from(Gatwick)).to eq("Confirming departure from #{Gatwick}")
+  end 
 end 
