62a0ceb3a52fc9b28b3de9a32b53e3deecdcb62f
Ben <ben@benzeng.co.uk>
2019-08-25 13:08:28 +0100

    utilised attr_reader and replaced instance variables

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,39 +1,45 @@
 class Airport
-
-  # lands plane after checking #stormy? / #full? / #at_airport?
-  def land_plane(plane)
-    fail "It is too stormy to land" if @weather.stormy?
-    fail "Airport is full" if full?
-    fail "Plane is already at airport!" if at_airport?(plane)
-    @all_planes << plane # appends Plane object to @all_planes
-    at_airport?(plane) # confirms plane is at airport
-  end
-
-  # takes off plane after checking #stormy? !#at_airport?
-  def takeoff_plane(plane)
-    fail "It is too stormy to take off" if @weather.stormy?
-    fail "Plane is not at airport!" if !at_airport?(plane)
-    @all_planes.delete(plane) # returns and removed last element of @all_planes
-    !at_airport?(plane) # not at airport?
-  end
-
-  # return true if plane is in airport (a.k.a @all_planes array)
-  def at_airport?(plane)
-    @all_planes.include?(plane)
-  end
-
-  # return true if airport is full
-  def full?
-    @all_planes.size >= DEFAULT_CAPACITY
-  end
-
-  private
   DEFAULT_CAPACITY = 20
+
+  attr_reader :capacity
+  attr_reader :all_planes
+  attr_reader :weather
 
   # initialise Airport instance
   def initialize
     @all_planes = []
     @weather = Weather.new
+    @capacity = DEFAULT_CAPACITY
   end
+
+  # lands plane after checking #stormy? / #full? / #at_airport?
+  def land_plane(plane)
+    fail "It is too stormy to land" if weather.stormy?
+    fail "Airport is full" if full?
+    fail "Plane is already at airport!" if at_airport?(plane)
+    all_planes << plane # appends Plane object to all_planes
+    at_airport?(plane) # confirms plane is at airport
+  end
+
+  # takes off plane after checking #stormy? !#at_airport?
+  def takeoff_plane(plane)
+    fail "It is too stormy to take off" if weather.stormy?
+    fail "Plane is not at airport!" if !at_airport?(plane)
+    all_planes.delete(plane) # returns and removed last element of @all_planes
+    !at_airport?(plane) # not at airport?
+  end
+
+  # return true if plane is in airport (a.k.a @all_planes array)
+  def at_airport?(plane)
+    all_planes.include?(plane)
+  end
+
+  # return true if airport is full
+  def full?
+    all_planes.size >= capacity
+  end
+
+  private
+
 
 end
