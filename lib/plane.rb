82df5a8fb885a87a312bbc1baa719b5b4542ae14
Mark Dobbins <mjdobbins12@gmail.com>
2019-08-25 20:24:02 +0000

    second test passed

--- lib/plane.rb
+++ lib/plane.rb
@@ -1,14 +1,26 @@
+class Weather
+  attr_reader :current_weather
+  
+  def check
+    rand
+  end
+end
+
 class Plane
 end
 
 class Airport
-	attr_reader :hangar
-	def initialize
-		@hangar = []
-	end
+  attr_reader :hangar
 	
-	def land(plane)
-		@hangar << plane
-	end
+  def initialize
+    @hangar = []
+  end
+	
+  def land(plane)
+    @hangar << plane
+  end
+	
+  def take_off(plane)
+    @hangar.delete(plane)
+  end
 end
-
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -2,10 +2,18 @@ require 'plane'
 
 describe "air traffic control" do
 	
-	it "can land a plane" do
-	airport = Airport.new
-	plane = Plane.new
-	airport.land(plane)
-	expect(airport.hangar).to include(plane)
-	end
+  it "can land a plane" do
+    airport = Airport.new
+    plane = Plane.new
+    airport.land(plane)
+    expect(airport.hangar).to include(plane)
+  end
+	
+  it "can instruct a plane to take off" do
+    airport = Airport.new
+    plane = Plane.new
+    airport.land(plane)
+    airport.take_off(plane)
+    expect(airport.hangar).to_not include(plane)
+  end
 end
