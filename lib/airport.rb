020b6d5504eba270d0516b57daf019d2ca085c79
EdZeno <eduard.kulcsickij.gut@gmail.com>
2019-08-26 15:28:26 +0100

    wrote a test to enable land method to store a plane

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,7 +1,6 @@
 class Airport
 
   def land(plane)
-
   end
 
   def take_off(plane)
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,11 +2,17 @@ require 'airport'
 
 describe Airport do
 
-  it 'land takes an argument(plane)' do
+  it 'respondes to land and takes an argument(plane)' do
     expect(subject).to respond_to(:land).with(1).argument
   end
 
-  it 'respondes to take_off' do
+  it '#land stores an instance variable plane' do
+    plane = Plane.new
+    expect(subject.land(plane)).to eq plane
+  end
+
+
+  it 'respondes to take_off and takes an argument' do
     expect(subject).to respond_to(:take_off).with(1).argument
   end
 
