f00043fbe207c66b8c3e64a4db9a85e77a21c8cc
Dan Gyi <daniel.gyi@gmail.com>
2019-08-26 01:27:38 +0100

    Completes with Rubocop passing

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,48 +1,46 @@
 require 'airport'
 require 'plane'
 
-
 describe Airport do
-    let(:plane) { Plane.new }
+  let(:plane) { Plane.new }
     
+  it 'instructs a plane to land at the airport' do
+    expect(subject).to respond_to(:land).with(1).argument
+  end
 
-    it 'instructs a plane to land at the airport' do
-        expect(subject).to respond_to(:land).with(1).argument
-    end
+  it 'instructs a plane to take off from the airport' do
+    expect(subject).to respond_to(:take_off).with(1).argument
+  end
 
-    it 'instructs a plane to take off from the airport' do
-        expect(subject).to respond_to(:take_off).with(1).argument
-    end
+  it 'checks whether a plane that has taken off is no longer at the airport' do
+    allow(subject).to receive(:weather) { "sunny" }
+    subject.land(plane)
+    subject.take_off(plane)
+    expect(subject.planes).not_to include(plane)
+  end
 
-    it 'checks whether a plane that has taken off is no longer at the airport' do
-        allow(subject).to receive(:weather) { "sunny" }
-        subject.land(plane)
-        subject.take_off(plane)
-        expect(subject.planes).not_to include(plane)
-    end
+  it 'checks the weather' do
+    expect(subject).to respond_to(:weather)
+  end
 
-    it 'checks the weather' do
-        expect(subject).to respond_to(:weather)
-    end
+  it 'allows default capacity of an airport to be overridden if required' do
+    random_capacity = Random.rand(1000)
+    subject = Airport.new(random_capacity)
+    expect(subject.capacity).to eq(random_capacity)
+  end
 
-    it 'allows default capacity of an airport to be overridden if required' do
-        random_capacity = Random.rand(1000)
-        subject = Airport.new(random_capacity)
-        expect(subject.capacity).to eq(random_capacity)
-    end
+  it 'raises exception if land is attempted when airport is full' do
+    allow(subject).to receive(:weather) { "sunny" }
+    subject.capacity.times { subject.land(plane) }
+    expect { subject.land(plane) }.to raise_error('Cannot land, airport full')
+  end
 
-    it 'raises exception if land is attempted when airport is full' do
-        allow(subject).to receive(:weather) { "sunny" }
-        subject.capacity.times { subject.land(plane) }
-        expect{subject.land(plane)}.to raise_error('Cannot land, airport full')
-    end
+  it 'raises exception if take_off is attempted in stormy weather' do
+    expect { subject.take_off(plane) }.to raise_error('Cannot take off, weather stormy') if subject.weather == 'stormy'
+  end
 
-    it 'raises exception if take_off is attempted in stormy weather' do
-        expect{subject.take_off(plane)}.to raise_error('Cannot take off, weather stormy') if subject.weather == 'stormy'
-    end
-
-    it 'raises exception if land is attempted in stormy weather' do
-        expect{subject.land(plane)}.to raise_error('Cannot land, weather stormy') if subject.weather == 'stormy'
-    end
+  it 'raises exception if land is attempted in stormy weather' do
+    expect { subject.land(plane) }.to raise_error('Cannot land, weather stormy') if subject.weather == 'stormy'
+  end
 
 end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -2,22 +2,22 @@ require 'airport'
 require 'plane'
 
 describe Plane do
-    let(:airport) { Airport.new }
+  let(:airport) { Airport.new }
   
-    it 'checks whether a particular plane has departed from airport' do
+  it 'checks whether a particular plane has departed from airport' do
     allow(airport).to receive(:weather) { "sunny" }
     airport.land(subject)
     expect(airport.take_off(subject)).to eq(subject)
   end
 
-    it 'checks whether a plane instance responds to #departed?' do
-        expect(subject).to respond_to(:departed?)
-    end
+  it 'checks whether a plane instance responds to #departed?' do
+    expect(subject).to respond_to(:departed?)
+  end
 
-    it 'checks whether a plane has departed' do
-        allow(airport).to receive(:weather) { "sunny" }
-        airport.land(subject)
-        airport.take_off(subject)
-        expect(subject.departed?).to eq true
-    end
+  it 'checks whether a plane has departed' do
+    allow(airport).to receive(:weather) { "sunny" }
+    airport.land(subject)
+    airport.take_off(subject)
+    expect(subject.departed?).to eq true
+  end
 end
