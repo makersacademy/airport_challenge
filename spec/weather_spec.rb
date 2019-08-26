954e57612c5338ecedec141b2729b58964525d46
EdZeno <eduard.kulcsickij.gut@gmail.com>
2019-08-26 17:11:16 +0100

    wrote test to check that #weather_forcast returns stormy

--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -3,7 +3,12 @@ require 'weather'
 describe Weather do
 
   it 'respondes to the method #weather_forcast' do
-  expect(subject).to respond_to :weather_forcast
+    expect(subject).to respond_to :weather_forcast
+  end
+
+  it 'returns stormy if the random_number = 2' do
+    allow(subject).to receive(:weather_forcast) { 2 }
+    expect(subject.weather_forcast).to eq 'stormy'
   end
 
 end
