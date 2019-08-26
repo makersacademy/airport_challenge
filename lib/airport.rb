4a5f8a6d2c6c1699716c88025f317ca74e895c18
Sayem995 <sayem.bokhari.1@gmail.com>
2019-08-26 20:44:02 +0100

    completed user story 3 and getting run time error on user story 4

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,8 +1,15 @@
 class Airport
 
   def land(plane)
+    raise 'cannot let plane land due to stormy weather' if stormy?
+  end
+
+  def stormy?
+    rand(1..6) > 4
   end
 
   def takeoff(plane)
+    raise 'cannot let plane takeoff due to stormy weather' if stormy?
   end
+
 end
--- spec/features/airport_spec.rb
+++ spec/features/airport_spec.rb
@@ -10,9 +10,23 @@ it { is_expected.to respond_to(:land).with(1).argument }
 
 it { is_expected.to respond_to(:takeoff).with(1).argument }
 
-  it 'for planes to take off, instruct planes to takeoff' do
+  it 'for planes to takeoff, instruct planes to takeoff' do
     airport = Airport.new
     plane = Plane.new
     expect { airport.takeoff(plane) }.not_to raise_error
-end
+  end
+
+  it 'not letting plane takeoff' do
+    airport = Airport.new
+    plane = Plane.new
+    allow(airport).to receive(:stormy?).and_return true
+    expect { airport.takeoff(plane) }.to raise_error 'cannot let plane takeoff due to stormy weather'
+  end
+
+  it 'not letting plane land' do
+    airport = Airport.new
+    plane = Plane.new
+    allow(airport).to receive(:stormy?).and_return false
+    expect { airport.land(plane) }.to raise_error 'cannot let plane land due to stormy weather'
+  end
 end
