3d42ce706e3b7498490e7b15ec118a9de86f5c0c
EdZeno <eduard.kulcsickij.gut@gmail.com>
2019-08-26 19:46:09 +0100

    updated test and method weather_forcast

--- lib/weather.rb
+++ lib/weather.rb
@@ -1,11 +1,12 @@
 class Weather
 
   def random_weather
-
-  end 
+    rand(2)
+  end
 
   def weather_forcast(random_weather)
-
+    return 'stormy' if random_weather == 1
+    return 'sunny' if random_weather == 0
   end
 
 end
--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -6,9 +6,8 @@ describe Weather do
     expect(subject).to respond_to(:weather_forcast).with(1).argument
   end
 
-  it 'returns stormy if the random_number = 2' do
-    allow(subject).to receive(:weather_forcast) { 1 }
-    expect(subject.weather_forcast).to eq 'stormy'
+  it 'returns stormy if the random_number = 1' do
+    expect(subject.weather_forcast(1)).to eq 'stormy'
   end
 
   it 'respondes to method random_weather' do
