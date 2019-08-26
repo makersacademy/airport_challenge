b6a7225ef7b9cb79b8e2d90f30ad5ccc602d0c38
Mike <mjwalshe@gmail.com>
2019-08-26 19:55:26 +0100

    Update respond_to tests

--- lib/airport.rb
+++ lib/airport.rb
@@ -2,15 +2,21 @@ require_relative 'plane'
 
 class Airport
 
+  def initialize
+    @planes = []
+  end 
+
   def weather 
     rand(1..8) > 1 ? @weather = 'sunny' : @weather = 'stormy'
   end 
 
   def take_off(plane)
     fail 'Cannot take off due to stormy weather' unless weather == 'sunny'
+    plane.take_off
   end
 
   def land(plane)
     fail 'Cannot land due to stormy weather' unless weather == 'sunny'
+    @planes << plane
   end
 end 
--- lib/plane.rb
+++ lib/plane.rb
@@ -1,4 +1,9 @@
 class Plane
+  
+  def land
+    # if flying == true 
+    # flying == false 
+  end
 
   def take_off_from(airport)
     "Confirming departure from #{airport}" #unless flying == true 
