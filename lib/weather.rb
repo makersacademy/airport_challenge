7fcf8b3714a317455b5ce87b1b9a41ce07e3893e
EdZeno <eduard.kulcsickij.gut@gmail.com>
2019-08-26 18:19:17 +0100

    changed weather_forcast test and method to accept an argument

--- lib/weather.rb
+++ lib/weather.rb
@@ -1,6 +1,7 @@
 class Weather
 
-  def weather_forcast
+  def weather_forcast(random_weather)
+
   end
-  
+
 end
--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -3,11 +3,11 @@ require 'weather'
 describe Weather do
 
   it 'respondes to the method #weather_forcast' do
-    expect(subject).to respond_to :weather_forcast
+    expect(subject).to respond_to(:weather_forcast).with(1).argument
   end
 
   it 'returns stormy if the random_number = 2' do
-    allow(subject).to receive(:weather_forcast) { 2 }
+    allow(subject).to receive(:weather_forcast) { 1 }
     expect(subject.weather_forcast).to eq 'stormy'
   end
 
