f639b2c7d488a5911dbcb341e94351f9396cace5
EdZeno <eduard.kulcsickij.gut@gmail.com>
2019-08-26 12:09:43 +0100

    wrote test for #take_off method

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -4,6 +4,10 @@ describe Airport do
 
   it 'respondes to #land' do
     expect(subject).to respond_to :land
+  end
+
+  it 'respondes to take_off' do
+  expect(subject).to respond_to :take_off
   end 
 
 end
