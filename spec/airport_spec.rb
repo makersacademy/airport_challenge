0220e1fcb76a8795f41a25ec7ce25522fa4e617a
Elsie Ashworth <44971680+eashworth@users.noreply.github.com>
2019-08-26 01:48:23 +0100

    mid-story 2 update land_a_plane spec

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -5,3 +5,10 @@ describe Airport do
 
   it { is_expected.to respond_to :take_off_plane }
 end
+
+describe "#land_a_plane" do
+  it "lands a plane" do
+    plane = Plane.new
+    expect(Airport.new.land_a_plane(plane)).to eq plane
+  end
+end
