0f14aef3c8358ab984325bfa9479e0be83a0408a
Dan Gyi <daniel.gyi@gmail.com>
2019-08-26 01:11:51 +0100

    Completes working airport, tests successfully with RSpec, needs to use Rubocop

--- lib/plane.rb
+++ lib/plane.rb
@@ -1,8 +1,14 @@
 class Plane
-    def initialize(name= 'Standard Airways Flight 3095')
+
+    def initialize(name = 'Standard Airways Flight 3095')
         @name = name
     end
 
+    def report_taken_off
+        @departed = true
+    end
+
     def departed?
+        @departed
     end    
 end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -3,13 +3,21 @@ require 'plane'
 
 describe Plane do
     let(:airport) { Airport.new }
-
+  
     it 'checks whether a particular plane has departed from airport' do
-        airport.land(subject)
-        expect(airport.take_off(subject)).to eq(subject)
+    allow(airport).to receive(:weather) { "sunny" }
+    airport.land(subject)
+    expect(airport.take_off(subject)).to eq(subject)
+  end
+
+    it 'checks whether a plane instance responds to #departed?' do
+        expect(subject).to respond_to(:departed?)
     end
 
-    it 'checks whether a plane instance has departed' do
-        expect(subject).to respond_to(:departed?)
+    it 'checks whether a plane has departed' do
+        allow(airport).to receive(:weather) { "sunny" }
+        airport.land(subject)
+        airport.take_off(subject)
+        expect(subject.departed?).to eq true
     end
 end
