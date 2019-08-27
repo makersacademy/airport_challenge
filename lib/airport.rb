81d9906bb0afa322d660912c8b3bf0594890052d
etelish <etelisha@googlemail.com>
2019-08-27 02:48:21 +0100

    raise exception for take off in stormy weather

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,15 +1,19 @@
 class Airport
   attr_reader :plane
 
+
   def land_at_airport(plane)
     @plane = plane
+
   end
 
 
   def take_off(plane)
-
+    raise "Take off not permitted weather is stormy" if weather == stormy
+    
+    @plane
 
   end
 
-  
+
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,22 +2,34 @@ require './lib/airport'
 
 describe Airport do
 
-it { is_expected.to respond_to(:land_at_airport).with(1).argument }
-it "lands at airport" do
+  it { is_expected.to respond_to(:take_off).with(1).argument }
+  it { is_expected.to respond_to(:land_at_airport).with(1).argument }
+  it { is_expected.to respond_to(:plane) }
+
+it "#lands_at_airport" do
   plane = Plane.new
   expect(subject.land_at_airport(plane)).to eq plane
 end
 
 
-it { is_expected.to respond_to(:plane) }
 it "shows landed planes" do
   plane = Plane.new
   subject.land_at_airport(plane)
   expect(subject.plane).to eq plane
-end 
+end
 
-it { is_expected.to respond_to(:take_off).with(1).argument }
+  it  'allows planes to #take_off' do
+    plane = Plane.new
+    subject.land_at_airport(plane)
+    expect(subject.take_off(plane)).to eq plane
+  end
 
+  it "raises exception when weather is stormy" do
+    plane = Plane.new
+     subject.land_at_airport(plane)
+    expect  { subject.take_off(plane) }.to raise_error("Take off not permitted weather is stormy")
+
+end
 # it "allows plane to take off" do
 # plane = subject.land_at_airport(plane)
 # # subject.land_at_airport(plane)
@@ -25,10 +37,11 @@ it { is_expected.to respond_to(:take_off).with(1).argument }
 #   expect(subject.take_off(plane)).not_to include(plane)
 # end
 
-it "confirms plane is not_in_airport" do
-  plane = subject.take_off(plane)
-  expect(plane).to eq :not_in_airport?
- end
+# it "confirms plane is not_in_airport" do
+#   plane = Plane.new
+#   plane = subject.take_off(plane)
+#   expect(plane).to eq :not_in_airport?
+#  end
 
 
 
