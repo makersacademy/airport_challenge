a8b7a9db5a5084f97befbdbeae5e9bfe88722e39
Student <student@Makerss-MBP.lan>
2019-09-07 07:18:47 +0100

    feat: plane can be marked as flying

--- lib/plane.rb
+++ lib/plane.rb
@@ -1,2 +1,12 @@
 class Plane
+  attr_reader :flying
+  def initialize
+    @flying = false
+  end
+
+  def mark_flying
+    @flying = true
+  end
+
+
 end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -1,4 +1,8 @@
-require 'plane.rb'
+require 'plane'
 
 describe Plane do
+  it 'is marked as flying after takeoff' do
+    subject.mark_flying
+    expect(subject.flying).to eq true
+  end
 end
