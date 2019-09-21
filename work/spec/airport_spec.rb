6ebb8fab00d0d3a37ab56a049ccfee5f6bff2fe9
DanielleInkster <danielle.inkster@gmail.com>
2019-09-21 22:34:36 +0100

    Creation of Airport and Plane

---
+++ work/spec/airport_spec.rb
@@ -0,0 +1,7 @@
+require 'airport'
+
+describe Airport do
+  it 'exists!' do
+    expect(subject.class).to eq Airport
+  end
+end
---
+++ work/spec/plane_spec.rb
@@ -0,0 +1,7 @@
+require 'plane'
+
+describe Plane do
+  it 'exists' do
+    expect(subject.class).to eq Plane
+  end
+end
