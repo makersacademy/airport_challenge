758aab28a92db1879de7258b95da5750d38d3a98
Nathan Hynes <nathanmail@hotmail.co.uk>
2019-09-21 15:52:04 +0100

    ensured planes are removed from hangar

--- lib/airport.rb
+++ lib/airport.rb
@@ -12,9 +12,18 @@ class Airport
     'Safe to land'
   end
 
+  def permission_to_takeoff(plane)
+    clear_for_takeoff(plane)
+    "You are clear for take off"
+  end
+
   private
 
   def taxi_to_hangar(plane)
     hangar << plane
+  end
+
+  def clear_for_takeoff(plane)
+    hangar.delete(plane)
   end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -22,4 +22,18 @@ describe Airport do
       expect(airport.permission_to_land(plane)).to eq "Safe to land"
     end
   end
+
+  describe '#permission_to_takeoff' do
+    it { is_expected.to respond_to(:permission_to_takeoff).with(1).argument }
+
+    it 'tells plane it is safe to take off' do
+      expect(airport.permission_to_takeoff(plane)).to eq "You are clear for take off"
+    end
+
+    it 'removes planes from hangar' do
+      airport.permission_to_land(plane)
+      airport.permission_to_takeoff(plane)
+      expect(airport.hangar).to be_empty
+    end
+  end
 end
