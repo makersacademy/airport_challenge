a6d46c8a08c2b7cb8febfaf84fe6bbf6e94c6c93
Olly Holovchenko <oleh.holo@gmail.com>
2019-09-21 23:55:28 +0100

    Add tests for a plane

--- lib/plane.rb
+++ lib/plane.rb
@@ -1,19 +1,23 @@
 class Plane
-  attr_reader :in_flight
 
   def initialize
     @in_flight = true
   end
 
   def land
-    in_flight = false
+    @in_flight = false
   end
 
+  def take_off
+    @in_flight = true
+  end
+  
+
   def landed?
-    !in_flight
+    !@in_flight
   end
 
   def in_flight?
-    in_flight
+    @in_flight
   end
 end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -1,14 +1,23 @@
 require 'plane'
 
 describe Plane do
+  subject(:plane) { described_class.new }
 
-# Plane can take off
-# Plane can take off if it's on land
-# Plane can land
-# Plane can land if it's not on land
-# Land and take of number of planes
-# Only take off from the airport it's in
-# Plane in the air can't take off and/or be in the airport
-# Plane when landed can't land again and must be in the airport
+describe '#land' do
+  it 'when it lands it is landed and it is no longer in flight' do
+    plane.land
+    expect(plane).to be_landed
+    expect(plane).not_to be_in_flight
+  end
+end
+
+describe '#take_off' do
+  it 'when it takes off it is not landed and it is in flight' do
+    plane.land
+    plane.take_off
+    expect(plane).not_to be_landed
+    expect(plane).to be_in_flight
+  end
+end
 
 end
