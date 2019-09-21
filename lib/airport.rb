a9c45678b0ebc6092077cbc8e04b6ed2dfabdba6
John Dennehy <john.dennehy@gmail.com>
2019-09-21 14:53:26 +0100

    Adds guard clause against capacity constant

--- lib/airport.rb
+++ lib/airport.rb
@@ -2,20 +2,29 @@ require_relative "plane"
 require_relative "weather"
 
 class Airport
+  CAPACITY = 20
+
   def initialize
     @planes = []
   end
+  
   def land(plane)
+    raise "At capacity" if full
+
     @planes << plane
     return "roger wilko"
   end
 
   def take_off(plane)
-    if @planes.include?(plane)
-      return "In the air"
-      @planes.delete(plane)
-    else
-      raise "No such plane" 
-    end
+    raise "No such plane" unless @planes.include?(plane)
+    
+    @planes.delete(plane)
+    return "In the air"
+  end
+
+  private
+
+  def full
+    @planes.length >= CAPACITY
   end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -7,6 +7,12 @@ describe Airport do
       plane_1 = Plane.new
       expect(gatwick.land(plane_1)).to eq "roger wilko"
     end
+    it "prevents landing if airport full" do
+      gatwick = Airport.new
+      20.times { gatwick.land(Plane.new) }
+      my_plane = Plane.new
+      expect { gatwick.land(my_plane) }.to raise_error "At capacity"
+    end
   end
 
   describe "#take_off" do
@@ -22,7 +28,7 @@ describe Airport do
       heathrow = Airport.new
       plane_1 = Plane.new
       gatwick.land(plane_1)
-      expect{heathrow.take_off(plane_1)}.to raise_error
+      expect { heathrow.take_off(plane_1) }.to raise_error "No such plane"
     end
   end
 end
