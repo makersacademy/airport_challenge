508fb519237f4c76cda57df63ee882ac51f995fe
Matt Stockman <mjjstockman@gmail.com>
2019-09-21 19:02:37 +0100

    complete user story 2 - 4 EXCEPT comfirming plane is no longer in airport

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,4 +1,18 @@
 class Airport
   def land(plane)
+    raise 'Plane cannot land because it is stormy' if :stormy?
+  end
+
+  def take_off(plane)
+    raise 'Plane cannot take off because it is stormy' if :stormy?
+  end
+
+  def stormy?
+    random_generator = rand(1..10)
+    if random_generator > 7
+      true
+    else
+      false
+    end
   end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -4,4 +4,30 @@ describe Airport do
   it 'asks a plane to land' do
     expect(subject).to respond_to(:land).with(1).argument
   end
+
+  it 'asks a plane to take off' do
+    expect(subject).to respond_to(:take_off).with(1).argument
+  end
+
+  it 'will not take off a plane if it is stormy' do
+    plane = Plane.new
+    allow(subject).to receive(:stormy?) { true }
+    expect { subject.take_off(plane) }.to raise_error('Plane cannot take off because it is stormy')
+  end
+
+  it 'will not land a plane if it is stormy' do
+    plane = Plane.new
+    allow(subject).to receive(:stormy?) { true }
+    expect { subject.land(plane) }.to raise_error('Plane cannot land because it is stormy')
+  end
+
+  # it '#stormy? returns a number between 1 and 10' do
+  #   expect(subject.stormy?).to be_between(1, 10)
+  # end
+
+  # it '#stormy? returns true if response is above 7' do
+  #   allow(subject).to receive(:stormy?).and_return(8)
+  #   # expect(subject.stormy?).to eq true
+  #
+  # end
 end
