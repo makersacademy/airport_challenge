c07c40a8e9b8e71469b5f04998adb447d7217af5
John Dennehy <john.dennehy@gmail.com>
2019-09-21 15:43:45 +0100

    Adds random weather to Weather class

--- lib/weather.rb
+++ lib/weather.rb
@@ -1,5 +1,28 @@
 class Weather
+  
+  def initialize
+    update
+  end
+
   def stormy?
-    true
+    @storm
+  end
+
+  def sunny?
+    @sunny
+  end
+   
+  def update
+    weather_id = rand(1..10)
+
+    case 
+    when weather_id < 8
+      @storm = false
+      @sunny = true
+    when weather_id >= 8 && weather_id <= 10
+      @storm = true
+      @sunny = false
+    end
+
   end
 end
