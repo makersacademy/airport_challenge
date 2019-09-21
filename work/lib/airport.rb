eae133e22b2746ffebf068348a742d772c414b54
DanielleInkster <danielle.inkster@gmail.com>
2019-09-21 23:22:47 +0100

    Landing planes

--- work/lib/airport.rb
+++ work/lib/airport.rb
@@ -2,7 +2,15 @@ require_relative 'plane'
 
 class Airport
 
+  attr_reader :plane
+
+  def initialize
+    @planes =[]
+  end
+
   def land(plane)
+    @planes << plane
+    puts @planes
   end
 
   def takeoff
