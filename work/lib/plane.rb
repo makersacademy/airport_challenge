52f65771edfe5de2b6b1c7d0cdd0d2e9eeee7ae5
DanielleInkster <danielle.inkster@gmail.com>
2019-09-21 22:44:37 +0100

    Plane responds to takeoff

--- work/lib/plane.rb
+++ work/lib/plane.rb
@@ -2,5 +2,7 @@ class Plane
 
   def land
   end
-  
+
+  def takeoff
+  end
 end
--- work/spec/plane_spec.rb
+++ work/spec/plane_spec.rb
@@ -6,4 +6,6 @@ describe Plane do
   end
 
   it { is_expected.to respond_to(:land) }
+
+  it { is_expected.to respond_to(:takeoff) }
 end
