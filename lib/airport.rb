e28ce116b7e3d5a5844f0d6b86f217bdc417acd9
Jay Issuree <Jay_Issuree@hotmail.co.uk>
2019-09-22 14:33:24 +0100

    Airport responds to land method and returns landed plane

--- lib/airport.rb
+++ lib/airport.rb
@@ -2,4 +2,8 @@ require_relative 'plane'
 
 class Airport
 
+  def land(plane)
+    @plane = plane
+  end
+
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,4 +2,13 @@ require 'airport'
 
 describe Airport do
 
+  it 'responds to land' do
+    expect(subject).to respond_to(:land).with(1).argument
+  end
+
+  it 'returns the landed plane' do
+    plane = Plane.new
+    expect(subject.land(plane)).to eq plane
+  end
+
 end
