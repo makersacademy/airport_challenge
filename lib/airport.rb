a577a928a1eac2f4a7772e9ba45c3927cab91354
jhmc22 <53091006+jmhc22@users.noreply.github.com>
2019-09-22 13:48:46 +0100

    Adds guard to prevent landing or take off during storms

--- lib/airport.rb
+++ lib/airport.rb
@@ -8,11 +8,20 @@ class Airport
   end
 
   def land_plane(plane)
+    fail "Cannot land in stormy weather" if stormy?
     @hanger << plane
   end
 
   def takeoff_plane(plane)
+    fail "Cannot take off in stormy weather" if stormy?
     @hanger.delete(plane)
+    plane
+  end
+
+  private
+
+  def stormy?
+    rand(1..10) >= 9
   end
 
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -5,19 +5,34 @@ describe Airport do
   subject(:airport) { described_class.new}
 
   describe '#land_plane' do
-    it 'A plane will land at the airport and enter the hanger' do
+    it 'When a plane lands it will enter the airport hanger' do
       plane = double(:plane)
+      allow(airport).to receive(:stormy?).and_return(false)
       airport.land_plane(plane)
       expect(airport.hanger).to include plane
+    end
+
+    it 'A plane will not land if the weather is stormy' do
+      plane = double(:plane)
+      allow(airport).to receive(:stormy?).and_return(true)
+      expect { airport.land_plane(plane) }.to raise_error('Cannot land in stormy weather')
     end
   end
 
   describe '#takeoff_plane' do
-      it 'A plane will take off and be removed from the airport hanger' do
+      it 'When a plane takes off it will be removed from the airport hanger' do
         plane = double(:plane)
+        allow(airport).to receive(:stormy?).and_return(false)
         airport.hanger << plane
         airport.takeoff_plane(plane)
         expect(airport.hanger).to_not include plane
+      end
+
+      it 'A plane will not take off if the weather is stormy' do
+        plane = double(:plane)
+        airport.hanger << plane
+        allow(airport).to receive(:stormy?).and_return(true)
+        expect { airport.takeoff_plane(plane) }.to raise_error('Cannot take off in stormy weather')
       end
     end
 
