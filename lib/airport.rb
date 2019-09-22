7769512f1f974174b0373e0aec8b9ff0ffd045f7
Matt Stockman <mjjstockman@gmail.com>
2019-09-22 16:41:48 +0100

    fix #stormy? bug, #take_off(plane) removes plane from @landed_planes

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,20 +1,26 @@
 class Airport
   attr_accessor :capacity
+  attr_accessor :landed_planes
 
   def initialize
     @capacity = 20
+    @landed_planes = []
   end
 
   def land(plane)
-    raise 'Plane cannot land because it is stormy' if :stormy?
+    raise 'Plane cannot land because it is stormy' if stormy?
+
+    @landed_planes << plane
   end
 
   def take_off(plane)
-    raise 'Plane cannot take off because it is stormy' if :stormy?
+    raise 'Plane cannot take off because it is stormy' if stormy?
+
+    @landed_planes.delete(plane)
   end
 
   def stormy?
     random_generator = rand(1..10)
-    random_generator > 7
+    p random_generator > 7
   end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,8 +1,25 @@
 require 'airport'
+require 'plane'
 
 describe Airport do
+
   it 'asks a plane to land' do
     expect(subject).to respond_to(:land).with(1).argument
+  end
+
+  it '#take_off(plane) removes plane from @landed_planes' do
+    plane = Plane.new
+    allow(subject).to receive(:stormy?).and_return(false)
+    subject.land(plane)
+    subject.take_off(plane)
+    expect(subject.landed_planes).not_to include(plane)
+  end
+
+  it 'stores landed planes' do
+    plane = Plane.new
+    allow(subject).to receive(:stormy?).and_return(false)
+    subject.land(plane)
+    expect(subject.landed_planes).to include(plane)
   end
 
   it 'asks a plane to take off' do
@@ -11,14 +28,20 @@ describe Airport do
 
   it 'will not take off a plane if it is stormy' do
     plane = Plane.new
-    allow(subject).to receive(:stormy?) { true }
+    allow(subject).to receive(:stormy?).and_return(true)
     expect { subject.take_off(plane) }.to raise_error('Plane cannot take off because it is stormy')
   end
 
   it 'will not land a plane if it is stormy' do
     plane = Plane.new
-    allow(subject).to receive(:stormy?) { true }
+    allow(subject).to receive(:stormy?).and_return(true)
     expect { subject.land(plane) }.to raise_error('Plane cannot land because it is stormy')
+  end
+
+  it 'will land a plane if it is not stormy' do
+    plane = Plane.new
+    allow(subject).to receive(:stormy?).and_return(false)
+    expect { subject.land(plane) }.not_to raise_error
   end
 
   it 'has a default capacity of 20' do
