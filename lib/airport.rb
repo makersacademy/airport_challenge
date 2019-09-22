1668153f8160afba9f43869af4fefb92a1b7175e
jhmc22 <53091006+jmhc22@users.noreply.github.com>
2019-09-22 15:35:35 +0100

    Adds a settable capacity with a default for airport objects

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,19 +1,24 @@
 require_relative 'plane'
 
 class Airport
-  attr_accessor :hanger
-
-  def initialize
+  attr_accessor :hanger, :capacity
+  DEAFUALTCAPACITY = 50
+  def initialize(capacity = DEAFUALTCAPACITY)
     @hanger = []
+    @capacity = capacity
   end
 
   def land_plane(plane)
+    fail "Cannot land, airport full" if full?
     fail "Cannot land in stormy weather" if stormy?
+
     @hanger << plane
   end
 
   def takeoff_plane(plane)
+    fail "No planes available for take off" if @hanger.empty?
     fail "Cannot take off in stormy weather" if stormy?
+
     @hanger.delete(plane)
     plane
   end
@@ -24,4 +29,7 @@ class Airport
     rand(1..10) >= 9
   end
 
+  def full?
+    @hanger.size >= @capacity
+  end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,7 +2,18 @@ require 'airport'
 
 describe Airport do
 
-  subject(:airport) { described_class.new}
+  subject(:airport) { described_class.new }
+
+  describe '#initialize' do
+    it 'Capacity can be set on initialization' do
+      test_capacity = 5
+      plane = double(:plane)
+      airport = Airport.new(test_capacity)
+      allow(airport).to receive(:stormy?).and_return(false)
+      test_capacity.times { airport.land_plane(plane) }
+      expect { airport.land_plane(plane) }.to raise_error('Cannot land, airport full')
+    end
+  end
 
   describe '#land_plane' do
     it 'When a plane lands it will enter the airport hanger' do
@@ -17,23 +28,28 @@ describe Airport do
       allow(airport).to receive(:stormy?).and_return(true)
       expect { airport.land_plane(plane) }.to raise_error('Cannot land in stormy weather')
     end
+
+    it 'A plane will land at an airport that is at full capacity' do
+      plane = double(:plane)
+      allow(airport).to receive(:full?).and_return(true)
+      expect { airport.land_plane(plane) }.to raise_error('Cannot land, airport full')
+    end
   end
 
   describe '#takeoff_plane' do
-      it 'When a plane takes off it will be removed from the airport hanger' do
-        plane = double(:plane)
-        allow(airport).to receive(:stormy?).and_return(false)
-        airport.hanger << plane
-        airport.takeoff_plane(plane)
-        expect(airport.hanger).to_not include plane
-      end
-
-      it 'A plane will not take off if the weather is stormy' do
-        plane = double(:plane)
-        airport.hanger << plane
-        allow(airport).to receive(:stormy?).and_return(true)
-        expect { airport.takeoff_plane(plane) }.to raise_error('Cannot take off in stormy weather')
-      end
+    it 'When a plane takes off it will be removed from the airport hanger' do
+      plane = double(:plane)
+      allow(airport).to receive(:stormy?).and_return(false)
+      airport.hanger << plane
+      airport.takeoff_plane(plane)
+      expect(airport.hanger).to_not include plane
     end
 
+    it 'A plane will not take off if the weather is stormy' do
+      plane = double(:plane)
+      airport.hanger << plane
+      allow(airport).to receive(:stormy?).and_return(true)
+      expect { airport.takeoff_plane(plane) }.to raise_error('Cannot take off in stormy weather')
+    end
+  end
 end
