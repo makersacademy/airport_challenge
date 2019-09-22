cde9eeb127964c4d96937f3dafb39ecfbe3a96f6
Jessica Marais <jessica_marais@hotmail.com>
2019-09-22 09:15:45 +0100

    Raise error for landing plane when stormy

--- lib/airport.rb
+++ lib/airport.rb
@@ -3,11 +3,12 @@ require_relative 'plane'
 class Airport
 
   def land_plane(plane)
+    fail 'Landing prevented due to stormy weather' if @weather == 'stormy'
     @plane = plane
   end
 
   def take_off
-    fail 'Take off prevented due to stormy weather' if @weather == "stormy"
+    fail 'Take off prevented due to stormy weather' if @weather == 'stormy'
     @plane
     "The plane has taken off"
   end
