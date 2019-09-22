67eeb70eed26c990e7510c6c1ec6d7d7cddbf55a
Melvin Lau <lau.melvin@icloud.com>
2019-09-22 18:27:35 +0100

    Isolated airport tests

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,12 +1,12 @@
-require "controller" # controller requires all the required files in one place
+require "airport"
 
-# As the system designer
-# So that the software can be used for many different airports
-# I would like a default airport capacity that can be overridden as appropriate
 describe Airport do
 
   let(:airport) { described_class.new }
-  let(:plane) { Plane.new }
+
+  # As the system designer
+  # So that the software can be used for many different airports
+  # I would like a default airport capacity that can be overridden as appropriate
 
   it "has a default capacity" do
     expect(airport.capacity).to eq described_class::DEFAULT_CAPACITY
@@ -17,8 +17,13 @@ describe Airport do
     expect(airport_two.capacity).to eq 100
   end
 
+  # Test airport behaviour for various plane landing and take off cases
+
   context "when a plane lands successfully in sunny weather" do
+
     it "has the plane" do
+      plane = instance_double("Plane")
+      allow(plane).to receive(:land).with(airport) { airport.add_plane(plane) }
       airport.sunny
       plane.land(airport)
       expect(airport.planes).to include plane
@@ -26,14 +31,21 @@ describe Airport do
   end
 
   context "when a plane landing is prevented due to stormy weather" do
+
     it "does not have the plane" do
+      plane = instance_double("Plane")
+      allow(plane).to receive(:land).with(airport) { }
       airport.stormy
+      plane.land(airport)
       expect(airport.planes).not_to include plane
     end
   end
 
   context "when a plane landing is prevented due to airport at full capacity" do
+
     it "does not have the plane" do
+      plane = instance_double("Plane")
+      allow(plane).to receive(:land).with(airport) { }
       airport.sunny
       airport.capacity.times { Plane.new.land(airport) }
       plane.land(airport)
@@ -42,7 +54,12 @@ describe Airport do
   end
 
   context "when a plane takes off successfully in sunny weather" do
+
     it "no longer has the plane" do
+      plane = instance_double("Plane")
+      allow(plane).to receive(:land).with(airport) { airport.add_plane(plane) }
+      allow(plane).to receive(:take_off).with(airport) { airport.remove_plane(plane) }
+
       airport.sunny # Create good conditions
       plane.land(airport) # Land the plane so it can take off
       plane.take_off(airport) # Now the plane takes off
@@ -51,7 +68,12 @@ describe Airport do
   end
 
   context "when a plane is prevented from take off due to stormy weather" do
+
     it "still has the plane" do
+      plane = instance_double("Plane")
+      allow(plane).to receive(:land).with(airport) { airport.add_plane(plane) }
+      allow(plane).to receive(:take_off).with(airport) { }
+
       airport.sunny # Create good conditions
       plane.land(airport) # Land the plane so it can take off
       airport.stormy # Now it is stormy
