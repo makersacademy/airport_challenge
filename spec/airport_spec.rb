cdfff89ab206e10995533ffbca9a6911ee5c1dcf
Melvin Lau <lau.melvin@icloud.com>
2019-09-22 17:53:29 +0100

    Moved airport expectations to airport.rb

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -5,14 +5,59 @@ require "controller" # controller requires all the required files in one place
 # I would like a default airport capacity that can be overridden as appropriate
 describe Airport do
 
+  let(:airport) { described_class.new }
+  let(:plane) { Plane.new }
+
   it "has a default capacity" do
-    airport = described_class.new # no argument provided
     expect(airport.capacity).to eq described_class::DEFAULT_CAPACITY
   end
 
   it "allows an overriding capacity to be specified upon instantiation" do
-    airport = described_class.new(100)
-    expect(airport.capacity).to eq 100
+    airport_two = described_class.new(100)
+    expect(airport_two.capacity).to eq 100
+  end
+
+  context "when a plane lands successfully in sunny weather" do
+    it "has the plane" do
+      airport.sunny
+      plane.land(airport)
+      expect(airport.planes).to include plane
+    end
+  end
+
+  context "when a plane landing is prevented due to stormy weather" do
+    it "does not have the plane" do
+      airport.stormy
+      expect(airport.planes).not_to include plane
+    end
+  end
+
+  context "when a plane landing is prevented due to airport at full capacity" do
+    it "does not have the plane" do
+      airport.sunny
+      airport.capacity.times { Plane.new.land(airport) }
+      plane.land(airport)
+      expect(airport.planes).not_to include plane
+    end
+  end
+
+  context "when a plane takes off successfully in sunny weather" do
+    it "no longer has the plane" do
+      airport.sunny # Create good conditions
+      plane.land(airport) # Land the plane so it can take off
+      plane.take_off(airport) # Now the plane takes off
+      expect(airport.planes).not_to include plane
+    end
+  end
+
+  context "when a plane is prevented from take off due to stormy weather" do
+    it "still has the plane" do
+      airport.sunny # Create good conditions
+      plane.land(airport) # Land the plane so it can take off
+      airport.stormy # Now it is stormy
+      plane.take_off(airport) # Try taking off (should be unsuccessful)
+      expect(airport.planes).to include plane
+    end
   end
 
 end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -4,6 +4,7 @@ describe Plane do
 
   let(:plane) { described_class.new }
   let(:airport) { Airport.new }
+  # airport = instance_double("Airport")
 
   # As an air traffic controller
   # So I can get passengers to a destination
@@ -15,12 +16,6 @@ describe Plane do
       before(:each) do
         airport.sunny
         plane.land(airport)
-      end
-
-      # might be more relevant in airport_spec => Do this later
-      # otherwise how to isolate the airport object?
-      it "causes an airport to confirm the plane is there" do
-        expect(airport.planes).to include plane
       end
 
       it "confirms that the plane is not airborne" do
@@ -50,10 +45,6 @@ describe Plane do
         expect(plane).to be_airborne
       end
 
-      it "causes the airport to confirm the plane is not there" do
-        expect(airport.planes).not_to include plane
-      end
-
     end
 
     # As an air traffic controller
@@ -72,10 +63,6 @@ describe Plane do
 
       it "keeps the plane airborne" do
         expect(plane).to be_airborne
-      end
-
-      it "causes the airport to confirm the plane is not there" do
-        expect(airport.planes).not_to include plane
       end
 
     end
@@ -103,7 +90,7 @@ describe Plane do
       end
 
       it "confirms the plane is no longer in the airport" do
-        expect(airport.planes).not_to include plane
+        expect(plane).to be_airborne
       end
 
       it "cannot 'take off' again if already airborne" do
@@ -130,10 +117,6 @@ describe Plane do
 
       it "raises an error" do
         expect { plane.take_off(airport) }.to raise_error "Cannot take off due to stormy weather"
-      end
-
-      it "causes airport to ground the plane" do
-        expect(airport.planes).to include plane
       end
 
       it "confirms plane is not airborne" do
