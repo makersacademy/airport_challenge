a50b908bc8df977dfd502ac7974f694a8b1923ec
Brad Phillips <bjrp@protonmail.com>
2019-09-22 23:21:45 +0100

    adds tests to plane_spec file

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -7,4 +7,27 @@ describe Plane do
   it 'creates a new plane' do
     expect(plane).to eq plane  
   end
+
+  describe '#grounded' do
+    it 'describes the state of a plane' do
+      expect(plane.grounded?).to eq false
+    end
+  end
+
+  describe '#land' do
+    it 'changes the grounded state of a plane from false to true' do
+      plane = Plane.new
+      plane.land
+      expect(plane.grounded?).to eq true
+    end
+  end
+
+  describe '#fly' do
+    it 'changes the grounded state of a plane from true to false' do
+      plane = Plane.new
+      plane.land
+      plane.fly
+      expect(plane.grounded?).to eq false
+    end
+  end
 end
