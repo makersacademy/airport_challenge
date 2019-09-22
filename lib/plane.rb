c746825ec0fefbe83f03a167e24ea075f186df78
zraso <zaira.rasool@hotmail.co.uk>
2019-09-22 11:13:40 +0100

    Adding first test passes for plane feature and take off, land methods

---
+++ lib/plane.rb
@@ -0,0 +1,18 @@
+class Plane
+
+  def land
+
+  end
+
+  def take_off
+
+  end
+
+#maybe this needs to be in airport class i.e. airport says if
+#it still has that particular plane object?
+
+  def plane_in_airport?
+    true
+  end
+
+end
