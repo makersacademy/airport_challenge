8921dcc4b2915bbf85ffa492b4f7877636146f87
Student <student@Makerss-MBP.home>
2019-09-23 01:48:13 +0100

    function - able to check if plane is in the air?

--- lib/plane.rb
+++ lib/plane.rb
@@ -1,3 +1,16 @@
 class Plane
-   
+
+  def initialize
+    @flying = false
+    # Not in flight by default.
+  end
+
+  def flying?
+    @flying
+  end
+
+  def in_air
+    @flying = true
+  end
+
 end
