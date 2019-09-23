b68db1bcbe627d4a390c34872b7f76b8095839d9
AlecDWalker <walkeralec@hotmail.com>
2019-09-23 13:55:54 +0100

    corrected weather

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,32 +1,31 @@
 require_relative 'plane'
 class Airport
+  attr_accessor :runway , :capacity , :stormy
   DEFAULT_CAPACITY = 30
-  WEATHER_RANDOMISER = rand(10)
-  attr_accessor :runway , :capacity , :weather
 
   def initialize(capacity = DEFAULT_CAPACITY)
     @runway = []
     @capacity = capacity
-    if WEATHER_RANDOMISER >= 9
-      @weather = 'stormy'
-    else
-      @weather = 'sunny'
-    end
+    stormy?
   end
 
   def land(plane)
-    fail 'Cannot land in stormy weather' if @weather == 'stormy'
+    fail 'Cannot land in stormy weather' if stormy?
     fail 'Cannot land when airport is full' if full?
     @runway << plane
   end
 
   def take_off(plane)
-    fail 'Cannot take off in stormy weather' if @weather == 'stormy'
+    fail 'Cannot take off in stormy weather' if stormy?
     @runway.delete(plane)
   end
 
-  def report_stormy
-    @weather = "stormy"
+  def stormy?
+    if rand(10) >= 1
+      @stormy = true
+    else
+      @stormy = false
+    end
   end
 
   def full?
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,58 +1,46 @@
 require 'airport'
 describe Airport do
+  let(:airport) {Airport.new}
+  let(:plane) {double :plane}
   it { is_expected.to respond_to :land }
   it { is_expected.to respond_to :take_off }
 
+before {allow(airport).to receive(:stormy?).and_return(false)}
+
 it 'should be able to land planes' do
-  plane = Plane.new
-  airport = double("airport")
-  allow(airport).to receive(:weather) { 'sunny' }
-  expect(subject.land(plane)).to eq [plane]
+  expect(airport.land(plane)).to eq [plane]
 end
 
 it 'should be able to let planes take off' do
-  plane = Plane.new
-  airport = double("airport")
-  allow(airport).to receive(:weather) { 'sunny' }
-  subject.land(plane)
-  expect(subject.take_off(plane)).to eq plane
+  airport.land(plane)
+  expect(airport.take_off(plane)).to eq plane
 end
 
 it 'should be able to remove planes from the runway after takeoff' do
-  plane = Plane.new
-  airport = double("airport")
-  allow(airport).to receive(:weather) { 'sunny' }
-  subject.land(plane)
-  subject.take_off(plane)
-  expect(subject.empty?).to eq true
+  airport.land(plane)
+  airport.take_off(plane)
+  expect(airport.empty?).to eq true
 end
 
 it 'should prevent planes from landing when stormy' do
-  plane = Plane.new
-  subject.report_stormy
-  expect{subject.land(plane)}.to raise_error 'Cannot land in stormy weather'
+  allow(airport).to receive(:stormy?) { true }
+  expect{airport.land(plane)}.to raise_error 'Cannot land in stormy weather'
 end
 
 it 'should prevent planes from taking off when stormy' do
-  plane = Plane.new
-  subject.land(plane)
-  subject.report_stormy
-  expect{subject.take_off(plane)}.to raise_error 'Cannot take off in stormy weather'
+  airport.land(plane)
+allow(airport).to receive(:stormy?) { true }
+  expect{airport.take_off(plane)}.to raise_error 'Cannot take off in stormy weather'
 end
 
 it 'should prevent landing when the airport is full' do
-  plane = Plane.new
-  airport = double("airport")
-  allow(airport).to receive(:weather) { 'sunny' }
-  subject.capacity.times {subject.land(plane)}
-  expect { subject.land(plane) }.to raise_error 'Cannot land when airport is full'
+  airport.capacity.times {airport.land(plane)}
+  expect { airport.land(plane) }.to raise_error 'Cannot land when airport is full'
 end
 
 it 'should allow users to set a new airport capacity' do
-  plane = Plane.new
-  airport = Airport.new(capacity = 10)
-  airport = double("airport")
-  allow(airport).to receive(:weather) { 'sunny' }
+  subject = Airport.new(capacity = 10)
+  allow(subject).to receive(:stormy?) { false }
   subject.capacity.times {subject.land(plane)}
   expect { subject.land(plane) }.to raise_error 'Cannot land when airport is full'
 end
