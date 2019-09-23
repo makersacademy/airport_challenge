d4ab3ffd81beb5de03bce01769bec0a1ed96a76f
Duncan Skinner <duncanskinner94@gmail.com>
2019-09-23 10:24:19 +0100

    allow user to set new capacity

--- lib/airport.rb
+++ lib/airport.rb
@@ -5,20 +5,29 @@ require_relative 'weather.rb'
 class Airport
 
   include Weather
+  DEFAULT_CAPACITY = 5
 
-  def initialize
-
+  def initialize(capacity = DEFAULT_CAPACITY)
     @airport = []
+    @capacity = capacity
   end
 
   def land(plane)
-    raise StandardError, "Plane is unable to land due to weather conditions" if stormy?
+    raise "Plane is unable to land due to weather conditions" if stormy?
+    raise "Airport is full" if full?
     @airport << plane
   end
 
   def take_off(plane)
-    raise StandardError, "Plane is unable to take off due to weather conditions" if stormy?
+    raise "Plane is unable to take off due to weather conditions" if stormy?
+
     @airport.pop ? "Plane is no longer at the airport" : nil
+  end
+
+  private
+
+  def full?
+    @airport.length >= @capacity
   end
 
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -5,45 +5,48 @@ require 'weather'
 describe Airport do
   let(:plane) { double :plane }
   let(:airport) { Airport.new }
+
   it { is_expected.to respond_to(:land).with(1).argument }
 
   describe '#land' do
     it 'lands a plane at the airport' do
-
       allow(airport).to receive(:stormy?).and_return(false)
-
       expect(airport.land(plane)).to eq [plane]
     end
 
     it 'prevents a plane from landing in the event of bad weather' do
-      airport = Airport.new
-      plane = Plane.new
-
       allow(airport).to receive(:stormy?).and_return(true)
-
-      expect { airport.land(plane) }.to raise_error(StandardError, "Plane is unable to land due to weather conditions")
+      expect { airport.land(plane) }.to raise_error("Plane is unable to land due to weather conditions")
     end
   end
 
   describe '#take_off' do
-
     before { allow(airport).to receive(:stormy?).and_return(false) }
 
     it 'confirms that plane has departed from airport' do
-      airport = Airport.new
       airport.land(plane)
-
       expect(airport.take_off(plane)).to eq "Plane is no longer at the airport"
     end
 
     it 'prevents take off in the event of bad weather' do
-      airport = Airport.new
-      plane = Plane.new
       airport.land(plane)
-
       allow(airport).to receive(:stormy?).and_return(true)
+      expect { airport.take_off(plane) }.to raise_error("Plane is unable to take off due to weather conditions")
+    end
+  end
 
-      expect { airport.take_off(plane) }.to raise_error(StandardError, "Plane is unable to take off due to weather conditions")
+  describe '#full?' do
+    it 'prevents landing if the airport is full' do
+      allow(airport).to receive(:stormy?).and_return(false)
+      Airport::DEFAULT_CAPACITY.times { airport.land(plane) }
+      expect { airport.land(plane) }.to raise_error("Airport is full")
+    end
+
+    it 'allows user to change airport capacity' do
+      allow(airport).to receive(:stormy?).and_return(false)
+      airport = Airport.new(3)
+      3.times { airport.land(plane) }
+      expect { airport.land(plane) }.to raise_error("Airport is full")
     end
   end
 end
