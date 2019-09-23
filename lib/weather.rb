5f55ed2fda22282e45ac507efe8aa2a05c158314
Student <student@Makerss-MBP.home>
2019-09-23 03:47:17 +0100

    added random weather

--- lib/weather.rb
+++ lib/weather.rb
@@ -1,3 +1,12 @@
 class Weather
 
+  def weather_condition
+  random_number = rand(10)
+    if random_number < 2
+      return "stormy"
+    else
+      return "sunny"
+    end
+  end
+
 end
--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -1,5 +1,5 @@
 require 'weather'
 
 describe Weather do
-
+  it { is_expected.to respond_to :weather_condition }
 end
