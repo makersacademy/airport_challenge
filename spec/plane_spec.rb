4a7dc68e141fc7982a87cd5d07240cadd22167cd
Student <student@Makerss-MBP.home>
2019-09-23 01:36:38 +0100

    set tests for plane to report being in flight

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -1,4 +1,17 @@
 require 'plane'
 describe Plane do
+  it { is_expected.to respond_to :flying? }
 
+    it 'Confirm plane is no longer in the airport' do
+      subject.in_air
+      expect(subject).to be_flying
+    end
+
+    describe '#in_air' do
+      it 'reports plane as in the air' do
+        plane = Plane.new
+        plane.in_air
+        expect(plane.flying?).to eq true
+      end
+    end
 end
