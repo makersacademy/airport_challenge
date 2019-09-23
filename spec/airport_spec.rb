855a6eb4593c4b5c3fc53748badfeed0331bcfa9
Student <student@Makerss-MBP.home>
2019-09-23 07:09:03 +0100

    tests for airport with capacity features

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -4,6 +4,20 @@ require 'weather'
 describe Airport do
   it { is_expected.to respond_to(:land_plane).with(1).argument }
 
+  describe 'initialization' do
+      subject { Airport.new }
+      let(:plane) { Plane.new }
+
+      it 'creates an airport with a default capacity' do
+        expect(subject.capacity).to eq described_class::DEFAULT_CAPACITY
+      end
+
+      it 'creates a full station with a specified max capacity' do
+        airport = Airport.new(12)
+        12.times { airport.land_plane(plane) }
+        expect { airport.land_plane(plane) }.to raise_error(RuntimeError, 'Airport is full')
+      end
+    end
 
   describe '#land_plane' do
     it 'raises error if plane is already on ground and tries to land again' do
