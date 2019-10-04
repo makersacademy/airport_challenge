0c0b99f020109f73c115cd7e210e000236eaf6e3
Student <Student@Admins-MacBook-Air.local>
2019-10-04 14:59:29 +0100

    Completed scenarios 1,2,3

---
+++ lib/airport.rb
@@ -0,0 +1,31 @@
+class Airport
+
+attr_reader :capasity
+
+  def initialize
+    @airport_planes = []
+    @capasity = 20
+  end
+
+  def land(plane)
+    fail 'Airport is full' if airport_full
+    @airport_planes << plane
+  end
+
+  def list_of_planes
+    return @airport_planes
+  end
+
+  def take_off(plane)
+    @airport_planes.pop
+  end
+end
+
+  def airport_full
+    @airport_planes.count >= capasity
+  end
+
+
+class Plane
+
+end
---
+++ spec/airport_spec.rb
@@ -0,0 +1,28 @@
+require 'airport'
+
+describe Airport do
+  it { is_expected.to respond_to :land }
+  it { is_expected.to respond_to :list_of_planes }
+
+
+  it 'lands a plane' do
+    plane = Plane.new
+    airport = Airport.new
+    airport.land(plane)
+    expect(airport.list_of_planes).to include(plane)
+  end
+
+  it 'takes off' do
+    plane = Plane.new
+    airport = Airport.new
+    airport.take_off(plane)
+    expect(airport.list_of_planes).not_to include(plane)
+  end
+
+  it 'raises an error if airport is full' do
+    plane = Plane.new
+    airport = Airport.new
+    airport.capasity.times { airport.land Plane.new }
+    expect { airport.land Plane.new }.to raise_error 'Airport is full'
+  end
+end
