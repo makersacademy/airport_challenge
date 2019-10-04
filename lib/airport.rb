6151b6e7acb77e01f99feafab58d147dbcdecfeb
Adam1824 <forrestadam07@gmail.com>
2019-10-04 10:10:49 +0100

    main three stories finished

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,12 +1,32 @@
 class Airport
-  attr_accessor :hanger
+  DEFAULT_CAPACITY = 20
+  attr_accessor :hanger, :capacity
 
-  def initialize
+  def initialize(capacity = DEFAULT_CAPACITY)
     @hanger = []
+    @capacity = capacity
   end
 
   def land_plane(plane)
+    raise "Hanger is full." if full?
     hanger << plane
+  end
+
+  def take_off_plane(plane)
+    i = 0
+    hanger.each do |current_plane|
+      if current_plane == plane
+        break
+      else
+        i += 1
+      end
+    end
+    hanger.delete_at(i)
+    puts "#{plane} has left the hanger..."
+  end
+
+  def full?
+    hanger.count >= capacity
   end
 end
 
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -9,4 +9,23 @@ describe Airport do
     # Assert
     expect(subject.hanger).to include(plane)
   end
+
+  it 'can get a plane to take off' do
+    # Arrange
+    plane = Plane.new
+    # Act
+    subject.land_plane(plane)
+    subject.take_off_plane(plane)
+    # Assert
+    expect(subject.hanger).to_not include(plane)
+  end
+
+  it 'can prevent planes from landing when hanger is full' do
+    # Arrange
+    plane = Plane.new
+    # Act
+    subject.capacity.times { subject.land_plane Plane.new }
+    # Assert
+    expect { raise subject.land_plane(plane) }.to raise_error 'Hanger is full.'
+  end
 end
