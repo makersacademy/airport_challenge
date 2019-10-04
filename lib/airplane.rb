e3a44e6e6d90d6e74512a786480bd356e3732a8f
TobiasBenjaminYoungCGI <toby.young@cgi.com>
2019-10-04 15:56:25 +0100

    all basic and 1 stretch

--- lib/airplane.rb
+++ lib/airplane.rb
@@ -3,5 +3,7 @@ class Airplane
   end
 
   def stormy?
+    # Attempted with Rand(1) to rotate weather, didn't achieve the desired goal and broke all my tests
+    # For future will do a Rand(10) and have 7/10 numbers deliver sunny in 
   end
 end
