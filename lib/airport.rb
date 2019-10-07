0d267b1c821c473b8e9f513f2bf85beba2a13ed3
adam robinson <adam.james.robinson@hotmail.co.uk>
2019-10-07 10:22:04 +0100

    corrected peer stuff

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,43 +1,45 @@
 require_relative "plane"
 
 class Airport
-attr_accessor :airport
-attr_accessor :capacity
+  attr_accessor :airport
+  attr_accessor :capacity
 
- def initialize(capacity)
-   @airport = []
-   @capacity = capacity
- end
+  def initialize(capacity)
+    @airport = []
+    @capacity = capacity
+  end
 
- def land(planes)
-   stormy?
-   fail "Airport is full" if full?
-   fail "conditions are stormy" if stormy? == "stormy"
-   if stormy? == "sunny"
-     @airport << planes
-   end
- end
+  def land(planes)
+    stormy?
+    fail "Airport is full" if full?
+    fail "conditions are stormy" if stormy? == "stormy"
 
- def take_off
-   fail "there are no more planes in the airport" if empty?
-   @airport.pop
- end
+    if stormy? == "sunny"
+      @airport << planes
+    end
+  end
+
+  def take_off
+    fail "there are no more planes in the airport" if empty?
+    
+    @airport.pop
+  end
 
 
- def empty?
-   @airport.empty?
- end
+  def empty?
+    @airport.empty?
+  end
 
- def full?
-   @airport.count >= capacity
- end
+  def full?
+    @airport.count >= capacity
+  end
 
- def stormy?
-   ran_num = rand(1..2)
-   if ran_num == 1
-     return "stormy"
-   elsif ran_num == 2
-     return "sunny"
-   end
- end
+  def stormy?
+    ran_num = rand(1..2)
+    if ran_num == 1
+      return "stormy"
+    elsif ran_num == 2
+      return "sunny"
+    end
+  end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,15 +2,15 @@ require "airport"
 #require "plane"
 
 describe Airport do #.new(1) do
- it "is expected to respond to airport" do
- airport = Airport.new(1)
- expect(airport).to respond_to(:airport)
- end
+  it "is expected to respond to airport" do
+    airport = Airport.new(1)
+    expect(airport).to respond_to(:airport)
+  end
 end
 
 describe Airport do #.new(1) do
 
-    it "should be able to land a plane" do
+  it "should be able to land a plane" do
     airport = Airport.new(1)
     plane = Plane.new #makes a new plane
     #airport.land(plane) #laning the neewly made plane which stores it into an array
@@ -20,29 +20,29 @@ describe Airport do #.new(1) do
 
   end
 
-    xit "can take off" do
+  it "can take off" do
     airport = Airport.new(1)
     plane = Plane.new
     airport.land(plane)
-    expect(airport.take_off).to eq (plane)
-   end
-    describe "take_off" do
-      #this test is not making a new instance of a plane and is
-      #not landing anything so there should be nothing at the airport
-      airport = Airport.new(1)
-      it 'shows when there are no more planes in the airport' do
+    expect(airport.take_off).to eq(plane)
+  end
+  describe "take_off" do
+    #this test is not making a new instance of a plane and is
+    #not landing anything so there should be nothing at the airport
+    airport = Airport.new(1)
+    it 'shows when there are no more planes in the airport' do
       expect{airport.take_off}.to raise_error('there are no more planes in the airport')
     end
   end
   describe Airport do
     describe 'initialize' do
-      xit 'has a variable capacity and shows full if that capacity is exceded' do
-      airport = Airport.new(50)
-      50.times { airport.land Plane.new  }
-      allow(airport).to receive(:stormy?).and_return("sunny")
-      expect{ airport.land Plane.new }.to raise_error 'Airport is full'
+      it 'has a variable capacity and shows full if that capacity is exceded' do
+        airport = Airport.new(50)
+        50.times { airport.land Plane.new  }
+        allow(airport).to receive(:stormy?).and_return("sunny")
+        expect{ airport.land Plane.new }.to raise_error 'Airport is full'
+      end
     end
-  end
     describe "land" do
       it "wont happen if its stormy" do
         airport = Airport.new(1)
