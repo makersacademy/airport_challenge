d7a06019b775d0419d8f9bccb56e993be8bed461
DMartinBGL <david.martin@bglgroup.co.uk>
2019-10-18 11:55:00 +0100

    created land mether test

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -4,4 +4,10 @@ describe Airport do
   it 'creates and instance of Airport' do
     expect(subject).to be_an_instance_of Airport
   end
+
+  it 'allows a plane to land at airport' do
+    plane = Plane.new
+    subject.land(plane)
+    expect(subect.land(plane)).to inlude plane
+  end
 end
