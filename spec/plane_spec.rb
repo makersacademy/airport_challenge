8b0cebf15dd34202b5ce12162217690997c855b7
Stuart Priestley <Stuart.Priestley@bglgroup.co.uk>
2019-10-18 13:08:06 +0100

    :white_check_mark: Set up failing tests for plane class to check flying status of plane

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -1,9 +1,31 @@
 require "plane"
+require "airport"
 
 describe Plane do
   let(:plane) { Plane.new }
+  let(:airport) { Airport.new }
 
   it "successfully instantiates the plane class" do
     expect(plane).to be_instance_of(Plane)
+  end
+
+  it { is_expected.to respond_to :flying? }
+
+  it { is_expected.to respond_to :landed! }
+
+  it { is_expected.to respond_to :take_off! }
+
+  it "has a flying status of false when created" do
+    expect(plane.flying?).to eq(false)
+  end
+
+  it "has a flying status of true when in the air" do
+    plane.take_off!
+    expect(plane.flying?).to eq(true)
+  end
+
+  it "has a flying status of false when landed" do
+    plane.landed!
+    expect(plane.flying?).to eq(false)
   end
 end
