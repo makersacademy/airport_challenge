57d21fbdb680949e0fb543884cd282c63d07a5e7
Stuart Priestley <Stuart.Priestley@bglgroup.co.uk>
2019-10-18 09:32:21 +0100

    :construction: Set up failing test for airport class instatiation

---
+++ spec/airport_spec.rb
@@ -0,0 +1,9 @@
+require "airport"
+
+describe Plane do
+  let(:airport) { Airport.new }
+
+  it "successfully instantiates the airport class" do
+    expect(airport).to be_instance_of(Airport)
+  end
+end
