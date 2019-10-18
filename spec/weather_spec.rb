fd63cfb3ed42e3dff887bf38a7e34584f31cd62c
Student <student@Admins-MacBook-Air.local>
2019-10-18 14:14:43 +0100

    testing weather array contains weather

--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -6,4 +6,8 @@ describe Weather do
   it "is a instance of weather" do
     expect(subject).to be_instance_of(Weather)
   end
+
+  it "has an array of weather types" do
+    expect(subject.weather_generator).to include("sun")
+  end
 end
