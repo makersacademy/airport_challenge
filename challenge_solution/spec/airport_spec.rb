d8d6b820ea90e4ef7b76206ceb71075e6b2522ef
Joe Ball <joe.ball@bglgroup.co.uk>
2019-10-18 10:03:47 +0100

    first commit, first test written and readme file updated

---
+++ challenge_solution/spec/airport_spec.rb
@@ -0,0 +1,36 @@
+# As an air traffic controller
+# So I can get passengers to a destination
+# I want to instruct a plane to land at an airport
+
+# As an air traffic controller
+# So I can get passengers on the way to their destination
+# I want to instruct a plane to take off from an airport and confirm that it is no longer in the airport
+
+# As an air traffic controller
+# To ensure safety
+# I want to prevent takeoff when weather is stormy
+
+# As an air traffic controller
+# To ensure safety
+# I want to prevent landing when weather is stormy
+
+# As an air traffic controller
+# To ensure safety
+# I want to prevent landing when the airport is full
+
+# As the system designer
+# So that the software can be used for many different airports
+# I would like a default airport capacity that can be overridden as appropriate
+
+require "./lib/plane"
+require "./lib/airport"
+
+
+describe Airport do
+    it "can land a plane at the airport" do
+        plane = Plane.new
+        airport = Airport.new
+        airport.land(plane)
+       expect(airport.landed.to eq [plane]
+    end
+end
