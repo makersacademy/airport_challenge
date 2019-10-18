426396e81f6f6edd57001f23a1ed68e094ea058a
Stuart Priestley <Stuart.Priestley@bglgroup.co.uk>
2019-10-18 13:08:51 +0100

    :white_check_mark: Pass tests for checking flying status of plane

--- lib/plane.rb
+++ lib/plane.rb
@@ -1,2 +1,17 @@
 class Plane
+  def initialize
+    landed!
+  end
+
+  def flying?
+    @flying
+  end
+
+  def landed!
+    @flying = false
+  end
+
+  def take_off!
+    @flying = true
+  end
 end
