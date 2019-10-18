d4e4c5e088104a4d4de52e251df558075db7a7aa
Shahrukh Khan <Shahrukh.Khan.2597@gmail.com>
2019-10-18 11:09:51 +0100

    added test to pick weather at random

--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -10,4 +10,12 @@ describe Weather do
     it 'creates an array of weather' do
         expect(subject.weather_arr).to include('Sunny')
     end
+
+    describe '#set_weather' do
+        it 'returns one item in the array at random' do
+            expect(subject.set_weather).to include(
+                'Sunny', 'Stormy', 'Raining', 'Cloudy'
+            )
+        end
+    end
 end
