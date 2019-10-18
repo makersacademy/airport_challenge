834425b4f997aa1c756a2d1637662e39e7487e15
jtsalva <tim@jtsalva.dev>
2019-10-18 11:17:14 +0100

    check weather before giving plane permission to take off

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,8 +1,9 @@
 class Airport
-  attr_accessor :planes
+  attr_accessor :planes, :weather
 
   def initialize
     @planes = []
+    @weather = 'clear'
   end
 
   def dock(plane)
@@ -10,6 +11,7 @@ class Airport
   end
 
   def get_permission_to_take_off(plane)
+    return nil if @weather == 'stormy'
     @planes.index(plane)
   end
 end
