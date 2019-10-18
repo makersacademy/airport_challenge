eb7540dd6ec11f5a680f0d5fbd232062f7e91105
Shahrukh Khan <Shahrukh.Khan.2597@gmail.com>
2019-10-18 10:15:40 +0100

    Added failing test for landing method

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -9,4 +9,12 @@ describe Airport do
     it 'should have an empty array of planes' do
         expect(subject.planes_arr).to be_empty
     end
+
+    describe '#land_at_airport' do
+        it 'adds a plane to the planes array' do
+            plane = Plane.new
+            subject.land_at_airport(plane)
+            expect(subject.planes_arr).to include(plane)
+        end
+    end
 end
