622ddc2a4ebfe29ddc28a125453bd3817dbe8a0e
jade-genevieve <26174144+jade-genevieve@users.noreply.github.com>
2019-10-18 11:34:12 +0100

    Test plane lands at location failing

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -4,4 +4,11 @@ describe Plane do
   describe "#land" do
     it { is_expected.to respond_to :land }
   end
+
+  it "lands at the specified destination" do
+    airliner1 = Plane.new
+    airliner1.land("Tokyo")
+    expect(airliner1.landed).to be(true)
+    expect(airliner1.location).to eq("Tokyo")
+  end
 end
