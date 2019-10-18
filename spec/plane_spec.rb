4305c0ac7b3f033eec3bdd5d5103a5f39a7c92a4
Rhys <mrevinz@gmail.com>
2019-10-18 09:52:54 +0100

    created and passed 4th test

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -5,14 +5,20 @@ describe Plane do
     subject { Plane.new }
     it "should instruct the plane to land at the airport" do
         airport = Airport.new
-        subject.Land(airport)
+        subject.Land(airport, "Sunny")
         expect(airport.planes).to include(subject)
     end
 
     it "should instruct the plane to take off" do
         airport = Airport.new
-        subject.Land(airport)
-        subject.TakeOff(airport)
+        subject.Land(airport, "Sunny")
+        subject.TakeOff(airport, "Sunny")
+        expect(subject.isFlying).to eq(true)
+    end
+
+    it "should not allow the plane to land if weather is stormy" do
+        airport = Airport.new
+        subject.Land(airport, "Stormy")
         expect(subject.isFlying).to eq(true)
     end
 end
--- src/Plane.rb
+++ src/Plane.rb
@@ -3,20 +3,24 @@ require_relative 'Airport'
 class Plane
     attr_reader :isFlying
     def initialize
-        @isFlying = false
-    end
-
-    def Land(airport)
-        airport.planes << self
-        @isFlying = false
-    end
-
-    def TakeOff(airport)
-        airport.planes.each_with_index do |plane, index|
-            if plane == self
-                airport.planes.delete_at(index)
-            end
-        end
         @isFlying = true
+    end
+
+    def Land(airport, weather)
+        if weather != "Stormy"
+            airport.planes << self
+            @isFlying = false
+        end
+    end
+
+    def TakeOff(airport, weather)
+        if weather != "Stormy"
+            airport.planes.each_with_index do |plane, index|
+                if plane == self
+                    airport.planes.delete_at(index)
+                end
+            end
+            @isFlying = true
+        end
     end
 end
