2cfdded3a9e7b59475bcb63203378294c152fb7e
Stuart Priestley <Stuart.Priestley@bglgroup.co.uk>
2019-10-18 10:15:21 +0100

    :white_check_mark: Pass test to stop plane landing if hangar is already full and add default capacity and private method for full

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,16 +1,26 @@
 class Airport
+  DEFAULT_CAPACITY = 20
   attr_accessor :hangar
+  attr_reader :capacity
 
-  def initialize
+  def initialize(capacity = DEFAULT_CAPACITY)
     @hangar = []
+    @capacity = capacity
   end
 
   def instruct_landing(plane)
+    raise "Hangar capacity at maximum, please continue to circle" if full?
     hangar << plane
     plane.land
   end
 
   def instruct_take_off(plane)
     plane.take_off
+  end
+
+  private
+
+  def full?
+    hangar.length >= @capacity
   end
 end
