fc7fdd16a229c4a843180d909919f39a80b3a5ab
Rob Baile <robbaile12@outlook.com>
2019-10-18 09:24:22 +0100

    created test for landing planes

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -4,4 +4,12 @@ describe Airport do
     it 'should be an airport' do
         expect(subject).to be_an_instance_of(Airport)
     end
+
+    it 'should instruct planes to land' do 
+        plane = double(:plane)
+        
+        subject.land_plane(plane)
+
+        expect(subject.planes.length).to be 1
+    end
 end
