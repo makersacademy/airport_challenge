bc390e97b709ab388dfc8806e8b4da460c7126f4
Student <student@Admins-MacBook-Air.local>
2019-10-18 10:26:14 +0100

    added plane capacity test

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,2 +1,7 @@
 class Airport
+  attr_reader :passengers
+
+  def initialize
+    @passengers = []
+  end
 end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -2,7 +2,11 @@ require "plane"
 
 describe Plane do
   let(:subject) { Plane.new }
+
   it "is an instance of Plane" do
     expect(subject).to be_a(Plane)
+  end
+  it "has a limited number of seats" do
+    expect(subject.capacity).to eq(50)
   end
 end
