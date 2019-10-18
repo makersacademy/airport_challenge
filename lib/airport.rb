ac261200be085ffc70ecc2504b30439ff100b555
Shahrukh Khan <Shahrukh.Khan.2597@gmail.com>
2019-10-18 10:26:28 +0100

    created a land at method that adds a plane to an array of planes

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,7 +1,11 @@
 class Airport
     attr_reader :planes_arr
-    
+
     def initialize
         @planes_arr = []
+    end
+
+    def land_at_airport(plane)
+        planes_arr << plane
     end
 end
