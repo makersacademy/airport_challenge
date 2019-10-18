da9c33fd0e23385cba63c1bc8906c424dcc8ecbd
Rob Baile <robbaile12@outlook.com>
2019-10-18 14:07:00 +0100

    test for creating randon flight number

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -4,4 +4,9 @@ describe Plane do
     it 'should be a type of plane' do 
         expect(subject).to be_an_instance_of(Plane)    
     end
+
+    it 'should have a random flight number created' do 
+        expect(subject.flight_number.length).to eq 8
+        expect(subject.flight_number.match(/^[[:alpha:][:blank:]]+$/)).to be true
+    end
 end
