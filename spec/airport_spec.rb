50fbdb67fa34354932e8b3168be6a5a16b370298
jtsalva <tim@jtsalva.dev>
2019-10-18 10:51:06 +0100

    add failing tests for airport

---
+++ spec/airport_spec.rb
@@ -0,0 +1,15 @@
+require 'airport'
+
+describe Airport do
+  it 'is an instance of airport' do
+    expect(subject).to be_an_instance_of Airport
+  end
+  it 'tracks planes that have landed' do
+    expect(subject.planes).to be_an_instance_of Array
+  end
+  it 'docks planes that have landed' do
+    plane = double :plane
+    subject.dock plane
+    expect(subject.planes.include? plane).to eq true
+  end
+end
