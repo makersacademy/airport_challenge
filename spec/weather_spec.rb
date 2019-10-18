75daa52f0049adb4ff4770d59834cb64b27fdcc9
Rob Baile <robbaile12@outlook.com>
2019-10-18 13:33:04 +0100

    added a stormy? parameter test

--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -4,4 +4,7 @@ describe Weather do
     it 'should be an instance of weather' do
         expect(subject).to be_an_instance_of(Weather)
     end
+
+    it 'should have a stormy value' do 
+        expect(subject.stormy?).to be false
 end
