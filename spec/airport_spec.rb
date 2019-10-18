3f5999672834e32c4e6033d660a27f730773dd32
jtsalva <tim@jtsalva.dev>
2019-10-18 11:02:11 +0100

    add failing test for #get_permission_to_take_off

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -9,4 +9,13 @@ describe Airport do
     subject.dock plane
     expect(subject.planes.include? plane).to eq true
   end
+  it 'gives permission for planes to takeoff' do
+    # if permission is granted #get_permission_to_take_off
+    # should return index of plane in Airport.planes
+    plane = double :plane
+    subject.planes = [plane]
+    position = subject.get_permission_to_take_off plane
+    expect(position).to be_an_instance_of Integer
+    expect(subject.position).to be >= 0
+  end
 end
