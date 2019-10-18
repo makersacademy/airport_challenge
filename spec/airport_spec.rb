e1ab47355bd216cc88835b6859dddbaffce9e222
krisn64209 <krisnorris@gmail.com>
2019-10-18 10:23:10 +0100

    added test to check Planes can be instructed to take off

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,19 +2,25 @@ require './lib/airport.rb'
 require './lib/plane.rb'
 
 describe Airport do
-# This test checks that new Airport
-# class instances can be made
   it 'can create a new instance of the Airport class' do
     airport = Airport.new
     expect(airport).to be_a(Airport)
   end
- # This test checks that instances of the
- # Plane class can be stored within an
- # instance of the Airport class 
+ # Checks that instances of the Plane class can be stored
+ # within an instance of the Airport class 
   it 'can instuct planes to land at an airport' do
   airport = Airport.new
   plane = Plane.new
   airport.land_plane(plane)
   expect(airport.planes.length).to eq 1
+  end
+  # Checks that instances of the Plane class can be
+  # removed from instances of the Airport class
+  it 'can instruct planes to take off' do
+  airport = Airport.new
+  plane = Plane.new
+  airport.land_plane(plane)
+  airport.take_off
+  expect(airport.planes.length).to eq 0 
   end
 end
