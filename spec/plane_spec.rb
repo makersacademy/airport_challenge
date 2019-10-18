ddd530e078cb71c4688184eac41b67fa39cc27b6
jade-genevieve <26174144+jade-genevieve@users.noreply.github.com>
2019-10-18 13:45:26 +0100

    Test plane leaves the airport failing

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -3,17 +3,26 @@ require "plane"
 describe Plane do
   describe "#land" do
     it { is_expected.to respond_to :land }
+
+    it "lands at the specified destination" do
+      airliner1 = Plane.new
+      airliner1.land("Tokyo")
+      expect(airliner1.landed).to be(true)
+      expect(airliner1.location).to eq("Tokyo")
+    end
+
+    it "reports plane as landed" do
+      airliner1 = Plane.new
+      expect { airliner1.land("Munich") }.to output("Landed at Munich airport at #{Time.new}\n").to_stdout
+    end
   end
 
-  it "lands at the specified destination" do
-    airliner1 = Plane.new
-    airliner1.land("Tokyo")
-    expect(airliner1.landed).to be(true)
-    expect(airliner1.location).to eq("Tokyo")
-  end
-
-  it "reports plane as landed" do
-    airliner1 = Plane.new
-    expect { airliner1.land("Munich") }.to output("Landed at Munich airport at #{Time.new}\n").to_stdout
+  describe "takeoff" do
+    it "leaves the current airport" do
+      airliner1 = Plane.new
+      airliner1.takeoff
+      expect(airliner1.flying).to be(true)
+      expect(airliner1.location).to be("in transit")
+    end
   end
 end
