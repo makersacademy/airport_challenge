e83367a4a2e049f99a19f340c02d6865a0665d42
Stuart Priestley <Stuart.Priestley@bglgroup.co.uk>
2019-10-18 09:42:51 +0100

    :construction: Set up failing test for landing a plane at the airport

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,8 +2,14 @@ require "airport"
 
 describe Airport do
   let(:airport) { Airport.new }
+  let(:plane) { double :plane }
 
   it "successfully instantiates the airport class" do
     expect(airport).to be_instance_of(Airport)
+  end
+
+  it "a plane can land at the airport" do
+    expect(plane).to receive(:land)
+    airport.land(plane)
   end
 end
