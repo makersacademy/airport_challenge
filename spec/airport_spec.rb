87da2e11603c1542a7917246f214a254e3391e1b
Marc Blenkinsop <marcblenky@hotmail.com>
2019-10-18 10:34:05 +0100

    wrote first test for landing a plane, currently failing

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,2 +1,10 @@
 require "./lib/airport"
 require "./lib/plane"
+
+describe Airport do
+  it "can receive a landing plane" do
+    @airport = Airport.new
+    @airport.land_plane(@plane.land)
+    expect(@airport.hangar.length).to eq 1
+  end
+end
