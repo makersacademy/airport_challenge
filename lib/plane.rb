588fcdd5d54ac09f07887ea832b390a142112844
jtsalva <tim@jtsalva.dev>
2019-10-18 10:41:29 +0100

    implement passing #take_off method

--- lib/plane.rb
+++ lib/plane.rb
@@ -1,4 +1,6 @@
 class Plane
+  attr_accessor :landed_at
+
   def initialize
     @landed_at = nil
   end
@@ -7,5 +9,11 @@ class Plane
     airport.planes << self
 
     @landed_at = airport
+  end
+
+  def take_off
+    position = @landed_at.planes.index(self)
+    @landed_at.planes.delete_at(position)
+    @landed_at = nil
   end
 end
