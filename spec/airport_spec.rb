ef867a2d2e6a9650a60a1d3efef6bcf4d93f75f8
Stuart Priestley <Stuart.Priestley@bglgroup.co.uk>
2019-10-18 11:08:24 +0100

    :construction: Set up failing tests for weather validation checks

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,4 +1,5 @@
 require "airport"
+require "weather"
 
 describe Airport do
   let(:airport) { Airport.new }
@@ -9,22 +10,26 @@ describe Airport do
   end
 
   it "a plane can land at the airport" do
+    allow(airport).to receive(:good_weather?).and_return(true)
     expect(plane).to receive(:land)
     airport.instruct_landing(plane)
   end
 
   it "a plane can take off from the airport" do
+    allow(airport).to receive(:good_weather?).and_return(true)
     expect(plane).to receive(:take_off)
     airport.instruct_take_off(plane)
   end
 
   it "confirms the airport is empty when there are no planes landed" do
+    allow(airport).to receive(:good_weather?).and_return(true)
     expect(plane).to receive(:take_off)
     airport.instruct_take_off(plane)
     expect(airport.hangar).to be_empty
   end
 
   it "if the hangar is full a plane cannot land and receives a message" do
+    allow(airport).to receive(:good_weather?).and_return(true)
     allow(plane).to receive(:land)
     expect { 21.times { airport.instruct_landing(plane) } }.to raise_error(RuntimeError, "Hangar capacity at maximum, please continue to circle")
   end
@@ -33,5 +38,15 @@ describe Airport do
     test_capacity = rand(1..100)
     test_airport = Airport.new(test_capacity)
     expect(test_airport.capacity).to eq (test_capacity)
+  end
+
+  it "if the weather is stormy, a plane cannot take-off" do
+    allow(airport).to receive(:good_weather?).and_return(false)
+    expect { airport.instruct_take_off(plane) }.to raise_error("Storms persist, take-off is delayed")
+  end
+
+  it "if the weather is stormy, a plane cannot land" do
+    allow(airport).to receive(:good_weather?).and_return(false)
+    expect { airport.instruct_landing(plane) }.to raise_error("Storms persist, please continue to circle")
   end
 end
--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -3,7 +3,13 @@ require "weather"
 describe Weather do
   let(:weather) { Weather.new }
 
-  it "successfully instantiates the airport class" do
+  it "successfully instantiates the weather class" do
     expect(weather).to be_instance_of(Weather)
   end
+
+  it { is_expected.to respond_to :forecast }
+
+  it { is_expected.to respond_to :magic_8_ball }
+
+  it { is_expected.to respond_to :good_weather? }
 end
