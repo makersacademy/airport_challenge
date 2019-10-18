3b21fc0dcbc80867ed2f279d896dfd881754ad15
krisn64209 <krisnorris@gmail.com>
2019-10-18 09:41:24 +0100

    test to check Airport class instances can be created, passed

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,6 +2,8 @@ require './lib/airport.rb'
 require './lib/plane.rb'
 
 describe Airport do
+# This first test checks that new Airport
+# class instances can be made
   it 'can create a new instance of the Airport class' do
     airport = Airport.new
     expect(airport).to be_a(Airport)
