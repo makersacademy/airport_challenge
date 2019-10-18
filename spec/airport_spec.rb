0bc5d4130ee07ab2b78b883c5c0450e886a6afa1
Marc Blenkinsop <marcblenky@hotmail.com>
2019-10-18 11:22:54 +0100

    wrote failing test to not allow planes to land in stormy weather

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,19 +2,23 @@ require "./lib/airport"
 require "./lib/plane"
 
 describe Airport do
-  it "can receive a landing plane" do
+  before :each do
     @airport = Airport.new
     @plane = Plane.new
+  end
+
+  it "can receive a landing plane" do
     @airport.land_plane(@plane)
-    p @airport.runway
     expect(@airport.runway.length).to eq 1
   end
 
   it "can allow planes to take off" do
-    @airport = Airport.new
-    @plane = Plane.new
     @airport.land_plane(@plane)
     @airport.allow_take_off
     expect(@airport.runway).to be_empty
+  end
+
+  it "does not allow landing in stormy weather" do
+    expect { @airport.land_plane(@plane, stormy) }.to raise_error("Planes cannot land in stormy weather")
   end
 end
