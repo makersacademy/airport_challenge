8b349197672111568c05b01070a733e2197201fe
bradley <bradley.plater1@gmail.com>
2019-10-18 10:01:02 +0100

    Default value is now created when user does not supply a value

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,7 +1,9 @@
 class Airport
   attr_reader :plane_capacity, :max_capacity
 
-  def initialize(max_capacity)
+  DEFAULT_CAPACITY = 10
+
+  def initialize(max_capacity = DEFAULT_CAPACITY)
     @max_capacity = max_capacity
     @plane_capacity = []
   end
