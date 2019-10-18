5be3c830b19c8b694e57790c9f2153d01f722046
krisn64209 <krisnorris@gmail.com>
2019-10-18 09:48:34 +0100

    added test to check planes can be landed at an airport

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,10 +2,17 @@ require './lib/airport.rb'
 require './lib/plane.rb'
 
 describe Airport do
-# This first test checks that new Airport
+# This test checks that new Airport
 # class instances can be made
   it 'can create a new instance of the Airport class' do
     airport = Airport.new
     expect(airport).to be_a(Airport)
+  end
+ # This test checks that instances of the
+ # Plane class can be stored within an
+ # instance of the Airport class 
+  it 'can instuct planes to land at an airport' do
+  airport = Airport.new
+  expect(airport.planes.length).to eq 1
   end
 end
