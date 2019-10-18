5e6ef3c6ef922ee0418aeac7821c88f3f583e9e1
krisn64209 <krisnorris@gmail.com>
2019-10-18 12:59:43 +0100

    passed test to check weather is stormy or sunny

--- lib/airport.rb
+++ lib/airport.rb
@@ -8,14 +8,23 @@ class Airport
     @planes = []
   end
 
+  def check_weather
+    weather = Kernel.rand(1..40)
+    if weather == 1
+      return "Stormy"
+    else
+      return "Sunny"
+    end
+  end
+
   def land_plane(plane)
     @planes << plane
   end
 
   def take_off
-  # removes Planes from the Airport's @planes array,
-  # and confirms that the number of Planes in the
-  # @planes array has decreased by 1
+    # removes Planes from the Airport's @planes array,
+    # and confirms that the number of Planes in the
+    # @planes array has decreased by 1
     landed_planes = @planes.length
     @planes.pop
     if landed_planes.-(1) == @planes.length
