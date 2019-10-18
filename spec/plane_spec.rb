34692e41cd0163f52a3cb4ba74adf287ddf53fe7
DMartinBGL <david.martin@bglgroup.co.uk>
2019-10-18 11:52:25 +0100

    created plane.rb

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -1,5 +1,7 @@
-desribe Plane do
+require 'plane'
+
+describe Plane do
   it 'creates and instance of plane' do
-    expect(subject).to be_an_instance_of plane
+    expect(subject).to be_an_instance_of Plane
   end
 end
