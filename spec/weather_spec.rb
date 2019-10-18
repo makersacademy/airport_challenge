2d45e35de2dcad8e38e137c804749300026cb542
bradley <bradley.plater1@gmail.com>
2019-10-18 10:27:05 +0100

    Created test to check random weather can generate both outcomes

--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -6,4 +6,12 @@ describe Weather do
   it 'Can create an instance of Weather' do
     expect(subject).to be_instance_of Weather
   end
+
+  it 'Can return sunny when randomly choosing weather' do
+    expect (subject.stub(:random_weather) {1..8}).to eq "Sunny"
+  end
+
+  it 'Can return stormy when randomly choosing weather' do
+    expect (subject.stub(:random_weather) {9..10}).to eq "Stormy"
+  end
 end
