be258a0aa364bbbe4fe11e65ac9a149a4f0912ae
Student <student@makerss-mbp.home>
2019-10-20 16:27:40 +0100

    user story 2 complete

--- lib/airport.rb
+++ lib/airport.rb
@@ -3,6 +3,10 @@ class Airport
   def land(plane)
   end
 
+  def take_off(plane)
+    return "plane has taken off and is no longer in the airport"
+  end
+
 end
 
 
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -7,4 +7,8 @@ describe Airport do
     expect(airport).to respond_to(:land).with(1).argument
   end
 
+  it 'instructs a plane to take off' do
+    expect(airport).to respond_to(:take_off).with(1).argument
+  end
+
 end
