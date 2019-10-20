520a520c879a96acca15379a7eedeb5efa2df58b
Emma Van Oss <e.vanoss@gmail.com>
2019-10-20 11:52:38 +0100

    plane can take off when not stormy, raises error when stormy

--- lib/plane.rb
+++ lib/plane.rb
@@ -1,7 +1,9 @@
 class Plane
   def land(airport)
+    raise "Too stormy to land" if airport.stormy?
   end
 
   def take_off(airport)
+    airport.plane_leaves
   end
 end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -2,15 +2,25 @@ require 'plane'
 require 'airport'
 
 describe Plane do
-  it "can land" do
-    plane = Plane.new
-    airport = Airport.new
-    expect { plane.land(airport) }.not_to raise_error
-  end
-
-  it "can take off" do
+  it 'can take off' do
     plane = Plane.new
     airport = Airport.new
     expect { plane.take_off(airport) }.not_to raise_error
+  end
+
+  context 'when not stormy' do
+    it 'can land' do
+      plane = Plane.new
+      airport = double('Airport', :stormy? => false)
+      expect { plane.land(airport) }.not_to raise_error
+    end
+  end
+
+  context 'when stormy' do
+    it 'raises an error when land is called' do
+      plane = Plane.new
+      airport = double('Airport', :stormy? => true)
+      expect { plane.land(airport) }.to raise_error "Too stormy to land"
+    end
   end
 end
