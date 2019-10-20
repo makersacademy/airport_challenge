90f9c0f1c76a7ebea518b23175f671f2311e3806
Alistair Phipps <alistair.phipps@johnlewis.co.uk>
2019-10-20 20:57:15 +0100

    take off and landing edge case coverage

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,7 +1,7 @@
 require "weather"
 
 class Airport
-  attr_reader :planes
+  attr_reader :planes, :name
 
   def initialize(name = "Heathrow")
     @weather = Weather.new
@@ -16,8 +16,20 @@ class Airport
   def land(plane)
     raise "Stormy, can't land planes" unless open_for_landing?
 
-    plane.land(self)
+    raise "Can't land, already at the airport" if plane.airport == @name
+
+    plane.land(@name)
     @planes << plane
+  end
+
+  def takeoff(plane)
+    raise "Stormy, can't take off planes" unless open_for_landing?
+
+    raise "Plane not at airport" unless planes.include?(plane)
+
+    plane.takeoff
+    planes.reject! { |x| x == plane }
+
   end
 
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -4,6 +4,7 @@ describe Airport do
 
   subject(:an_airport) { Airport.new }
   subject(:a_plane) { Plane.new }
+
   context "When Sunny airport is open" do
     it "open_for_landing? returns true" do
       # mock rand to set weather class to return "Sunny"
@@ -24,12 +25,48 @@ describe Airport do
     it "land a plane when sunny" do
       allow(an_airport).to receive(:open_for_landing?).and_return(true)
       expect(an_airport.land(a_plane)).to eq [a_plane]
-      expect(an_airport.planes[0].airport).to eq an_airport
+      expect(an_airport.planes[0].airport).to eq an_airport.name
+    end
+
+    it "raise an error if landing a plane already at the airport" do
+      allow(an_airport).to receive(:open_for_landing?).and_return(true)
+      allow(a_plane).to receive(:airport).and_return("Heathrow")
+      expect { an_airport.land(a_plane) }.to raise_error(RuntimeError, "Can't land, already at the airport")
     end
 
     it "raise an error if landing a plane while stormy" do
       allow(an_airport).to receive(:open_for_landing?).and_return(false)
       expect { an_airport.land(a_plane) }.to raise_error(RuntimeError, "Stormy, can't land planes")
+    end
+  end
+
+  describe "takeoff" do
+
+    it "take off a plane when sunny" do
+      allow(an_airport).to receive(:open_for_landing?).and_return(true)
+      an_airport.land(a_plane)
+      an_airport.takeoff(a_plane)
+      expect(an_airport.planes).to be_empty
+    end
+
+    it "take off a specific plane when sunny" do
+      allow(an_airport).to receive(:open_for_landing?).and_return(true)
+      an_airport.land(a_plane)
+      3.times { an_airport.land(Plane.new) }
+      an_airport.takeoff(a_plane)
+      expect(an_airport.planes).not_to include a_plane
+      expect(a_plane.airport).to eq ""
+      p an_airport.planes
+    end
+
+    it "raise an error if taking off a plane while stormy" do
+      allow(an_airport).to receive(:open_for_landing?).and_return(false)
+      expect { an_airport.takeoff(a_plane) }.to raise_error(RuntimeError, "Stormy, can't take off planes")
+    end
+
+    it "raise an error if taking off a plane not at the airport" do
+      allow(an_airport).to receive(:open_for_landing?).and_return(true)
+      expect { an_airport.takeoff(a_plane) }.to raise_error(RuntimeError, "Plane not at airport")
     end
   end
 end
