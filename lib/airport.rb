6c4a85ed23f802c09b8c50ca51db553120b10dd5
KOlofinmoyin <kehindeolofinmoyin@hotmail.com>
2019-10-20 01:19:44 +0100

    User Story 3 As an air traffic controller To ensure safety I want to prevent takeoff when weather is stormy

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,8 +1,15 @@
 class Airport
+
   def land(plane)
   end
 
-  def take_off(*)
+  def take_off(_plane)
+    fail "Cannot takeoff: weather is stormy" if stormy?
+
     "taken off"
+  end
+
+  def stormy?
+    true
   end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -3,16 +3,29 @@ require 'plane'
 
 describe Airport do
   subject(:airport) { described_class.new }
+  let(:plane) { Plane.new }
 
-  it 'instructs a plane to land' do
-    expect(airport).to respond_to(:land).with(1).argument
+  describe "#land" do
+    it 'instructs a plane to land' do
+      expect(airport).to respond_to(:land).with(1).argument
+    end
   end
 
-  it "instructs a plane to take off" do
-    expect(airport).to respond_to(:take_off).with(1).argument
-  end
+  describe "#take_off" do
+    it "instructs a plane to take off" do
+      expect(airport).to respond_to(:take_off).with(1).argument
+    end
 
-  it "confirms that plane is no longer in the airport" do
-    expect(airport.take_off).to eq "taken off"
+    it "confirms that plane is no longer in the airport" do
+      allow(airport).to receive(:stormy?).and_return false
+      expect(airport.take_off(plane)).to eq "taken off"
+    end
+
+    context "when stormy" do
+      it "prevents takeoff when weather is stormy" do
+        allow(airport).to receive(:stormy?).and_return true
+        expect { airport.take_off(plane) }.to raise_error "Cannot takeoff: weather is stormy"
+      end
+    end
   end
 end
