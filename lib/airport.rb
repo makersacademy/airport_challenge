e0cf988bed9eaef066403704b42d0f4040f44cda
Student <student@makerss-mbp.home>
2019-10-20 22:03:46 +0100

    user story 5 complete

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,19 +1,27 @@
 class Airport
-  attr_reader :planes_on_groud
+  attr_reader :planes
 
+  def initialize(capacity)
+    @capacity = capacity
+    @planes = []
+  end
 
   def land(plane)
+    raise "cannot land the plane: airport at capacity!" if full?
     raise "cannot land the plane: weather is stormy!" if stormy?
-    @planes_on_groud = plane
+    @planes << plane
   end
 
   def take_off(plane)
     raise "cannot take off: weather is stormy!" if stormy?
-    @planes_on_groud
+    @planes
   end
 
   def stormy?
     rand(1..6) > 4
   end
 
+  def full?
+    @planes.length >= @capacity
+  end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,29 +2,32 @@ require 'airport.rb'
 require 'plane.rb'
 
 describe Airport do
-  subject(:airport) { described_class.new }
+  subject(:airport) { described_class.new(20) }
+  let(:plane) { double :plane }
 
-  it 'instructs a plane to land' do
-    expect(airport).to respond_to(:land).with(1).argument
+  describe '#land' do
+    it 'instructs a plane to land' do
+      expect(airport).to respond_to(:land).with(1).argument
+    end
+    it 'lands a plane' do
+      allow(airport).to receive(:stormy?).and_return false
+      plane = Plane.new
+      expect(airport.land(plane)).to eq [plane]
+    end
   end
 
-  it 'lands a plane' do
-    allow(airport).to receive(:stormy?).and_return false
-    plane = Plane.new
-    airport.land(plane)
-    expect(airport.land(plane)).to eq plane
+  describe '#take_off' do
+    it 'instructs a plane to take off' do
+      expect(airport).to respond_to(:take_off).with(1).argument
+    end
+    it 'plane takes off' do
+      allow(airport).to receive(:stormy?).and_return false
+      plane = Plane.new
+      airport.land(plane)
+      expect(airport.take_off(plane)).to eq [plane]
+    end
   end
 
-  it 'instructs a plane to take off' do
-    expect(airport).to respond_to(:take_off).with(1).argument
-  end
-
-  it 'plane takes off' do
-    allow(airport).to receive(:stormy?).and_return false
-    plane = Plane.new
-    airport.land(plane)
-    expect(airport.take_off(plane)).to eq plane
-  end
 
   it 'raises an error when asked to land a plane when stormy' do
     allow(airport).to receive(:stormy?).and_return true
@@ -38,4 +41,12 @@ describe Airport do
     expect { airport.take_off(plane) }.to raise_error "cannot take off: weather is stormy!"
   end
 
+  it 'does not allow landing when at capacity' do
+    allow(airport).to receive(:stormy?).and_return false
+    plane = Plane.new
+    20.times do
+      airport.land(plane)
+    end
+    expect { airport.land(plane) }.to raise_error "cannot land the plane: airport at capacity!"
+  end
 end
