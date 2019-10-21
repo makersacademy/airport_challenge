43130a8a36612fc4a5803bc8e661d223dc8400c2
jade-genevieve <26174144+jade-genevieve@users.noreply.github.com>
2019-10-21 09:32:29 +0100

    Test airport has changeable capacity failing

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,4 +2,13 @@ require "airport"
 
 describe Airport do
   it { is_expected.to respond_to :planes }
+
+  it "has a default capacity that can be changed" do
+    expect(subject.capacity).to eq Airport::DEFAULT_CAPACITY
+    airport = Airport.new 100
+    expect(airport.capacity).to eq(100)
+  end
+
+  it "stores an array of planes landed at the airport" do
+  end
 end
