c27a20c55612d2e058aeef40c5ee8bac4ebe04bc
Simone <simonewlkr@hotmail.com>
2019-11-04 12:39:37 +0000

    Class created in prodcution code and synced to spec file. Passes rspec and rubocop.

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,6 +1,8 @@
+require "./lib/airport.rb"
+
 describe Plane do
   it 'makes an instance of the plane class' do
-  plane = Plane.new
-  expect(plane).to be_instance_of(Plane)
+    plane = Plane.new
+    expect(plane).to be_instance_of(Plane)
   end
 end
