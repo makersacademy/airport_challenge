520a6777cfde701d55d25ce179060cbfc28502e7
Simone <simonewlkr@hotmail.com>
2019-11-04 13:49:36 +0000

    Gone back to unit test checking if plane class exists. Fails as no plane class created i product code.

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,12 +1,6 @@
-require "./lib/airport.rb"
-describe Airport do
-  it 'makes an instance of the airport class' do
-    airport = Airport.new
-    expect(airport).to be_instance_of(Airport)
-  end
-
-  it 'make plane land' do
-    airport = Airport.new
-    expect(airport).to respond_to(:land_plane)
+describe Plane do
+  it 'makes an instance of the plane class' do
+    plane = Plane.new
+    expect(plane).to be_instance_of(Plane)
   end
 end
