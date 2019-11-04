7f11d9db9149f90ff0ba10496997cb0e85ded759
Rahul Shah <rahulshah7@outlook.com>
2019-11-04 22:39:06 +0000

    Add test for raising error if plane is already landed

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -5,6 +5,15 @@ describe Airport do
 
   describe '#land' do
     it { is_expected.to respond_to(:land).with(1).argument }
+
+    context 'if plane is already landed' do
+      let(:landed_plane) { double("landed_plane", :landed => true) }
+      it 'raises an error' do
+        expect { subject.land(landed_plane) }.to raise_error(RuntimeError, "Plane is already landed.")
+      end
+    end
+
+    
   end
 
 end
