19d80ea598de985dae032dce12e45a5ebc88e73a
Rebecca Varley <rebecca.varley@gmail.com>
2019-11-04 11:11:47 +0000

    failing test to respond to the method of land

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -1,8 +1,15 @@
 require './lib/plane.rb'
 
 describe Plane do
+
   it 'expects plane to be an instance of the plane class' do
   plane = Plane.new
   expect(plane).to be_instance_of(Plane)
 end
+
+  it 'responds to the method land' do
+    plane = Plane.new
+    plane.land
+    expect(plane).to respond_to(:land)
+  end
 end
