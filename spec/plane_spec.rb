14ad2c3ee6989a526260b0d85562feb50f450d44
Anh Nguyen <anhnguyenis@gmail.com>
2019-11-04 14:02:53 +0000

    add test for plane to respond to take_off

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -6,8 +6,14 @@ describe Plane do
     expect(plane).to be_kind_of(Plane)
   end
 
- it 'responds to land_at_airport' do
-   plane = Plane.new
-   expect(plane).to respond_to :land_at_airport
- end
+  it 'responds to land_at_airport' do
+    plane = Plane.new
+    expect(plane).to respond_to :land_at_airport
+  end
+
+  it 'responds to take_off' do
+    plane = Plane.new
+    expect(plane).to respond_to :take_off
+  end
+
 end
