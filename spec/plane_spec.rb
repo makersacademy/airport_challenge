9bd000ba56c8fcd460dc3fde6a91a94c90a3138d
Sonia Quintiao <soniaquintiao@gmail.com>
2019-11-04 12:54:35 +0000

    Add unit test for 'land' method, refactored

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -1,7 +1,18 @@
 require 'plane'
 describe Plane do
+
   it {is_expected.to be_kind_of(Plane)}
+
   # it 'can create an instance of plane' do
   #   plane = Plane.new
   #   expect(plane).to be_kind_of(Plane)
-  end
+  # end
+
+  it {is_expected.to respond_to(land)}
+
+  # it 'responds to land' do
+  #   plane = Plane.new
+  #   expect(plane.land).to respond_to(land)
+  # end
+
+end
