1f3ee9e4561768431e0120f0fbadf548e5c25ce6
pavidshetty <pavitrashetty23@gmail.com>
2019-11-04 16:34:06 +0000

    plane count

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,7 +1,15 @@
 require 'airport'
 class Airport
+  default_capacity = 10
+
   def land(plane)
 end
+
  def takeoff(plane)
+end
+
+
+def plane_count
+  @planes.count
 end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -14,4 +14,12 @@ end
 #instruct a plane to take off from an airport
 describe Airport do
   it {is_expected.to respond_to(:takeoff).with(1). argument}
-end 
+end
+
+#landing cant be done when the airport is full. We need to have a count for this.
+#does not accept planes and raise and error .
+describe Airport do
+  it "doesnot accept planes and raise an error when full" do
+    expect(airport.land(plane)).to raise_error('Airport is full,the plane cannot land.')
+  end
+end
