feca4c77eb79bb159d88287091be32539f91082d
Rahul Shah <rahulshah7@outlook.com>
2019-11-04 16:06:30 +0000

    Add land plane description to spec

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,5 +1,8 @@
 require 'airport'
 
 describe Airport do
-  it { is_expected.to respond_to(:land).with(1).argument }
+  describe 'instruct a plane to land at an airport' do
+    it { is_expected.to respond_to(:land).with(1).argument }
+  end
+
 end
