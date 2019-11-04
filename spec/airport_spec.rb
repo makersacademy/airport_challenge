0d3278d37a0749fad1e87fd7131e85f301858183
Sam King <samuelking94@gmail.com>
2019-11-04 12:40:26 +0000

    Failing test written for takeoff method

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -4,4 +4,6 @@ describe Airport do
   it { is_expected.to be_instance_of(Airport) }
 
   it { is_expected.to respond_to(:land).with(1).argument }
+
+  it { is_expected.to respond_to(:takeoff) }
 end
