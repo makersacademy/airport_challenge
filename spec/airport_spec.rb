8806d2eda15f71b4bd131b8dc8a6788bfd906a73
Simone <simonewlkr@hotmail.com>
2019-11-04 13:08:46 +0000

    Started again. Unit test to see if airport class exists. Fails rspec test.

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,8 +1,6 @@
-require "./lib/airport.rb"
-
-describe Plane do
-  it 'makes an instance of the plane class' do
-    plane = Plane.new
-    expect(plane).to be_instance_of(Plane)
+describe Airport do
+  it 'makes an instance of the airport class' do
+    airport = Airport.new
+    expect(airport).to be_instance_of(Airport)
   end
 end
