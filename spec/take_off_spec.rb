fe6b6f3b8ef306e311e78b6e43e173d47416cea0
Selina Erhabor <selinaerhabor@gmail.com>
2019-11-04 12:02:30 +0000

    Commit for failing test of plane take off

--- spec/take_off_spec.rb
+++ spec/take_off_spec.rb
@@ -1,6 +1,6 @@
 require './lib/plane.rb'
 describe Plane do
-  it 'instructs plane to take off from an airport' do
+  it 'instructs a plane to take off from an airport' do
     plane = Plane.new
     expect(plane).to respond_to :plane_take_off
   end
