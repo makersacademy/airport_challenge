c68eecee6757bb3036793f97dd018b938cafb1cb
Jo Nuckley <jnuckley@yahoo.co.uk>
2019-11-04 22:08:55 +0000

    failed to respond to airplanes

--- docs/airport.rb
+++ docs/airport.rb
@@ -1,2 +1,21 @@
 class Airport
+default_capacity = 10
+
+# attr_reader :airplanes
+
+  def initialize
+    @airplanes = []
+  end
+
+  # def capacity
+  # end
+  #
+  # def land_plane
+  # end
+  #
+  # def takeoff_plane
+  # end
+
+
+
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,7 +1,26 @@
 require './docs/airport'
 
 describe Airport do
+
 it 'creates an instance of airport class' do
   airport = Airport.new
+  expect(airport).to be_instance_of(Airport)
+end
+it 'is expected to respond to :airplanes' do
+  airport = Airport.new
+  expect(airport).to respond_to(airplanes)
 end
 end
+
+# it 'has a specific capacity of planes' do
+#   airport = Airport.new
+# end
+
+#respond to method land_plane
+
+#allow a plane to land
+
+#respond to method takeoff_plane
+
+
+#respond to the array of airplanes
