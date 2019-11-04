8134b6f9ae8603265614fd47c50a0b0074b1cfc6
Sam King <samuelking94@gmail.com>
2019-11-04 15:58:56 +0000

    Failing test written to confirm that takeoff method returns instance of Plane class

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,4 +1,5 @@
 require './lib/airport.rb'
+require './lib/plane.rb'
 
 describe Airport do
   it { is_expected.to be_instance_of(Airport) }
@@ -6,4 +7,8 @@ describe Airport do
   it { is_expected.to respond_to(:land).with(1).argument }
 
   it { is_expected.to respond_to(:takeoff) }
+
+  it "expects that takeoff method returns plane object" do
+    expect(subject.takeoff).to be_instance_of(Plane)
+  end
 end
