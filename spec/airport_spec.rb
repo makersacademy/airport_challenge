83011bbb0b73d2d5cd382eb0068fe20684071f04
Sam King <samuelking94@gmail.com>
2019-11-04 16:07:34 +0000

    Failing test written to check for error when 2 planes attempt to land at airport

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -11,4 +11,8 @@ describe Airport do
   it "expects that takeoff method returns plane object" do
     expect(subject.takeoff).to be_instance_of(Plane)
   end
+
+  it "expects that error raised when landing attempted at full airport" do
+    expect{ 2.times { subject.land(Plane.new) } }.to raise_error(RuntimeError)
+  end
 end
