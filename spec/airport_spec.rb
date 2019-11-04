72bf1e331feb7a6062c072fcfe08d4ea1ee230e0
Marianne OConnor <marianneoco@gmail.com>
2019-11-04 17:19:58 +0000

    Failing test - test that Plane class can be used to create a new plane instance

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,7 +1,7 @@
 require './lib/airport'
 
 describe Airport do
-  it "makes an instance of the airport class" do
+  it "makes an instance of the Airport class" do
     airport = Airport.new
     expect(airport).to be_instance_of(Airport)
   end
---
+++ spec/plane_spec.rb
@@ -0,0 +1,8 @@
+require './lib/plane'
+
+describe Plane do
+  it "makes an instance of the Plane class" do
+  plane = Plane.new
+  expect(plane).to be_instance_of(Plane)
+  end
+end
