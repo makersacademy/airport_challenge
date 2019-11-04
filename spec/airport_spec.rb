887baed275c5c83fa19573b89da4ab763486d843
Rahul Shah <rahulshah7@outlook.com>
2019-11-04 22:51:31 +0000

    Do not raise error if plane not landed

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -8,11 +8,17 @@ describe Airport do
 
     context 'if plane is already landed' do
       let(:landed_plane) { double("landed_plane", :landed => true) }
-      it 'raises an error' do
+      it 'raises RuntimeError: "Plane is already landed."' do
         expect { subject.land(landed_plane) }.to raise_error(RuntimeError, "Plane is already landed.")
       end
     end
 
+    context 'if plane is in flight' do
+      let(:inflight_plane) { double("inflight_plane", :landed => false) }
+      it 'does not raise RuntimeError: "Plane is already landed."' do
+        expect { subject.land(inflight_plane) }.not_to raise_error(RuntimeError, "Plane is already landed.")
+      end
+    end
     
   end
 
