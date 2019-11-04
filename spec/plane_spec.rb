143048d3219267675291415127607864faee65fb
Sonia Quintiao <soniaquintiao@gmail.com>
2019-11-04 12:43:16 +0000

    Refactor first unit test.

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -1,7 +1,7 @@
 require 'plane'
 describe Plane do
-  it 'can create an instance of plane' do
-    plane = Plane.new
-    expect(plane).to be_kind_of(Plane)
+  it {is_expected.to be_kind_of(Plane)}
+  # it 'can create an instance of plane' do
+  #   plane = Plane.new
+  #   expect(plane).to be_kind_of(Plane)
   end
-end
