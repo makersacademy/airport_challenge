7a0a052c23b26b328bb94aa254d4a72f83876493
Rahul Shah <rahulshah7@outlook.com>
2019-11-04 22:02:14 +0000

    Add airport initialization test

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,6 +1,8 @@
 require 'airport'
 
 describe Airport do
+  it { is_expected.to be_an_instance_of(Airport) }
+
   describe 'instruct a plane to land at an airport' do
     it { is_expected.to respond_to(:land).with(1).argument }
   end
