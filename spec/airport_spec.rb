7c51208158a5f2c4db6402c117eb1a3d02d46a78
djlonboy <djlonboy@gmail.com>
2019-11-05 10:43:12 +0000

    Created failing test for landing a plane

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -4,4 +4,11 @@ describe Airport do
 
   it { is_expected.to be_instance_of(Airport) }
 
+  it 'can instruct a plane to land at the airport' do
+    plane = Plane.new
+    airport = Airport.new
+    airport.land_plane(plane)
+    expect(airport.planes).to eq(plane)
+  end
+
 end
