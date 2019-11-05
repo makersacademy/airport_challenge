b82ebfdac11e777cee9213c9a77e82863830ea8e
Sam King <samuelking94@gmail.com>
2019-11-05 09:48:02 +0000

    Updated plane and plane spec to discard unused instance attribute

--- lib/plane.rb
+++ lib/plane.rb
@@ -1,7 +1,3 @@
 class Plane
-  attr_accessor :at_airport
 
-  def initialize
-    @at_airport = false
-  end
 end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -2,6 +2,4 @@ require './lib/plane.rb'
 
 describe Plane do
   it { is_expected.to be_instance_of(Plane) }
-
-  it { is_expected.to respond_to(:at_airport) }
 end
