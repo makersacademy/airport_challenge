098171023fa38108e783018ab7c23e4221f5014f
djlonboy <djlonboy@gmail.com>
2019-11-06 14:07:49 +0000

    Passed test for take off and refactored code so planes variable is now an array.

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,10 +1,18 @@
 require './lib/plane.rb'
 
 class Airport
-  attr_reader :planes
 
-  def land_plane(plane)
-    @planes = plane
+  attr_accessor :planes
+
+  def initialize
+    @planes = []
   end
 
+  def land_plane(plane)
+    @planes.push(plane)
+  end
+
+  def take_off_plane(plane)
+    @planes.delete(plane)
+  end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,17 +1,20 @@
 require 'airport'
+require './lib/plane.rb'
 
 describe Airport do
 
   it { is_expected.to be_instance_of(Airport) }
 
-  it 'can instruct a plane to land there' do
+  it 'can instruct a plane to land' do
     testplane = Plane.new
     subject.land_plane(testplane)
-    expect(subject.planes).to eq(testplane)
+    expect(subject.planes).to include(testplane)
   end
 
-  it 'can instruct a plane to take off from there' do
+  it 'can instruct a plane to take off' do
     testplane = Plane.new
+    subject.planes
+    subject.planes << testplane
     subject.take_off_plane(testplane)
     expect(subject.planes).not_to include(testplane)
   end
