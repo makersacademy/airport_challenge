a0fb875afcb9b82893e920c0914d6679f90f8641
djlonboy <djlonboy@gmail.com>
2019-11-06 10:23:13 +0000

    Created failing test for take off plane

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -4,10 +4,16 @@ describe Airport do
 
   it { is_expected.to be_instance_of(Airport) }
 
-  it 'can instruct a plane to land at the airport' do
-    plane = Plane.new
-    subject.land_plane(plane)
-    expect(subject.planes).to eq(plane)
+  it 'can instruct a plane to land there' do
+    testplane = Plane.new
+    subject.land_plane(testplane)
+    expect(subject.planes).to eq(testplane)
+  end
+
+  it 'can instruct a plane to take off from there' do
+    testplane = Plane.new
+    subject.take_off_plane(testplane)
+    expect(subject.planes).not_to include(testplane)
   end
 
 end
