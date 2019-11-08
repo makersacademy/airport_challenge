8d82ef4e0cbb3b8949302e46983fcb4b2bc59283
djlonboy <djlonboy@gmail.com>
2019-11-08 15:43:52 +0000

    refactored tests to use plane double

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -3,19 +3,21 @@ require './lib/plane.rb'
 
 describe Airport do
 
+  let(:testplane) { double :Plane }
+
   it { is_expected.to be_instance_of(Airport) }
 
   it 'can instruct a plane to land' do
-    testplane = Plane.new
     subject.land_plane(testplane)
     expect(subject.planes).to include(testplane)
   end
 
   it 'can instruct a plane to take off' do
-    testplane = Plane.new
     subject.planes << testplane
     subject.take_off_plane(testplane)
     expect(subject.planes).not_to include(testplane)
   end
+
+
 
 end
