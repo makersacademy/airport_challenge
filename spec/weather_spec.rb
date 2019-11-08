d2445847490937da9c479e636a119f2ad66cff9f
djlonboy <djlonboy@gmail.com>
2019-11-08 17:18:04 +0000

    refactored weather test to deal with errors, then wrote weather code to match.

--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -7,12 +7,11 @@ describe Weather do
   it 'returns stormy weather approximately one day in every 10' do
     storeweather = []
     10000.times do
-      storeweather << subject.weather
+      storeweather << subject.getweather
     end
-    sunny = storeweather.count(sunny)
-    stormy = storeweather.count(stormy)
-    p percentage = stormy / sunny
-    expect(percentage).to be_within(0.1).of(0.1)
-
-    end
+    sunny = storeweather.count("sunny")
+    stormy = storeweather.count("stormy")
+    percentage = stormy.to_f / sunny.to_f
+    expect(percentage).to be_within(0.05).of(0.1)
+  end
 end
