e2b65bc622d0666d9191c00421f7ec5fe0f59e56
djlonboy <djlonboy@gmail.com>
2019-11-09 11:55:05 +0000

    Added test to check new planes are landed by default:

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -2,6 +2,12 @@ require 'plane'
 
 describe Plane do
 
+  let(:testairport) { double :Airport }
+
   it { is_expected.to be_instance_of(Plane) }
+
+  it 'is initialized as landed' do
+    expect(subject.status).to eq("landed")
+  end
 
 end
