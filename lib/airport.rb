3bec19f596d0f70cd353c6812b215f71a74675f5
ajbacon <47368135+ajbacon@users.noreply.github.com>
2019-11-16 16:34:52 +0000

    added plane taking off functionality

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,7 +1,19 @@
+require_relative "plane.rb"
 class Airport
+
+  def initialize
+    @plane = nil
+  end
 
   def land_plane(plane)
     @plane = plane
   end
-  
+
+  def take_off
+    raise RuntimeError, "No planes at the airport" unless @plane
+
+    @plane = nil
+    "Plane has taken-off"
+  end
+
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -10,11 +10,12 @@ require 'airport'
 
 describe Airport do
 
-
+  # it "should have a default capacity of 10 planes" do
+  #   expect(Airport).to eq(10)
+  # end
+  airport = Airport.new
   describe "#land_plane" do
-    airport = Airport.new
-
-    it "should land a plane" do 
+    it "should have a land_plane method" do 
       expect(airport).to respond_to(:land_plane).with(1).argument
     end
 
@@ -24,5 +25,24 @@ describe Airport do
     end
   end
 
+  describe "#take_off" do
+    it "should allow planes to take off" do
+      plane = double(:plane)
+      airport.land_plane(plane)
+      expect(airport.take_off).to eq("Plane has taken-off")
+    end
 
+    it "should raise an error if instructing a plane to take off when there are no planes to take off" do 
+      expect { airport.take_off }.to raise_error RuntimeError
+    end
+  end
+
+    # it "should allow the user to overide the default capacity" do
+    # end
+
+    # it "should be able to land 5 planes if user determined capacity is 5" do
+    # end
+
+    # it "should raise an error if trying to land a plane when the airport is at capacity" do
+    # end
 end
