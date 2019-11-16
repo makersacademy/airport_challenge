fce0c3215a88c4375ed09414c9c42e53540286dc
Hyan18 <Hyan18@github.com>
2019-11-16 15:41:03 +0000

    plane does not land when airport is full

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,6 +1,19 @@
 class Airport
+  attr_reader :plane
+
+  CAPACITY = 1
+
+  def initialize
+    @counter = 0
+  end
+
   def land(plane)
-    "Plane has been landed"
+    if @counter >= CAPACITY
+      "That airport is full, cannot land"
+    else
+      @counter += 1
+      "Plane has been landed"
+    end
   end
 
   def take_off(plane)
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -4,19 +4,31 @@ require 'plane'
 describe Airport do
 
   describe "#land" do
+
     it "should land the plane at the airport" do
       airport = Airport.new
       plane = Plane.new
       expect(airport.land(plane)).to eq "Plane has been landed"
     end
+
+    it "should not land when the airport is full" do
+      airport = Airport.new
+      plane = Plane.new
+      airport.land(plane)
+      plane_2 = Plane.new
+      expect(airport.land(plane_2)).to eq "That airport is full, cannot land"
+    end
+    
   end
 
   describe "#take_off" do
+
     it "should have the plane take off from the airport" do
       airport = Airport.new
       plane = Plane.new
       expect(airport.take_off(plane)).to eq "Plane has taken off"
     end
+
   end
-    
+
 end
