89ea38a1c6557f01ac7b1805e7ce235c091f3f05
Raluca Ciucu <raluca.ciucu@gmail.com>
2019-11-16 18:13:38 +0000

    Refactor after CI and Rubocop

--- lib/plane.rb
+++ lib/plane.rb
@@ -1,19 +1,21 @@
 class Plane
-    def initialize
-        @in_flight = false
-    end
+  def initialize
+    @in_flight = false
+  end
 
-    def land
-        fail "Plane already landed" if !@in_flight
-        @in_flight = false
-    end
+  def land
+    fail "Plane already landed" unless @in_flight
 
-    def take_off
-        fail "Plane already departed" if @in_flight
-        @in_flight = true 
-    end
+    @in_flight = false
+  end
 
-    def in_flight?
-        @in_flight
-    end
+  def take_off
+    fail "Plane already departed" if @in_flight
+
+    @in_flight = true 
+  end
+
+  def in_flight?
+    @in_flight
+  end
 end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -1,37 +1,37 @@
 require "plane"
 
 describe Plane do
-    
-    it "should be able to land" do
-        expect(subject).to respond_to(:land)
-    end
+  
+  it "should be able to land" do
+    expect(subject).to respond_to(:land)
+  end
 
-    it "should be able to take off" do
-        expect(subject).to respond_to(:take_off)
-    end
+  it "should be able to take off" do
+    expect(subject).to respond_to(:take_off)
+  end
 
-    it "should not be in flight after landing" do
-        plane = Plane.new
-        plane.take_off
-        plane.land
-        expect(plane.in_flight?).to eq false
-    end
+  it "should not be in flight after landing" do
+    plane = Plane.new
+    plane.take_off
+    plane.land
+    expect(plane.in_flight?).to eq false
+  end
 
-    it "should be in flight after take off" do
-        plane = Plane.new
-        plane.take_off
-        expect(plane.in_flight?).to eq true
-    end
+  it "should be in flight after take off" do
+    plane = Plane.new
+    plane.take_off
+    expect(plane.in_flight?).to eq true
+  end
 
-    it "should not be able to land more than once" do
-        plane = Plane.new
-        expect { plane.land }.to raise_error "Plane already landed"
-    end
+  it "should not be able to land more than once" do
+    plane = Plane.new
+    expect { plane.land }.to raise_error "Plane already landed"
+  end
 
-    it "should not be able to take off more than once" do
-        plane = Plane.new
-        plane.take_off
-        expect { plane.take_off }.to raise_error "Plane already departed"
-    end
+  it "should not be able to take off more than once" do
+    plane = Plane.new
+    plane.take_off
+    expect { plane.take_off }.to raise_error "Plane already departed"
+  end
 
 end
