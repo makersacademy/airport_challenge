3d1093019977e3ee88ac05b76162a1bb9f60f7f3
Student <student@Makerss-MacBook-Air.local>
2019-11-16 14:33:51 +0000

    first test passing - airport is an instance of Airport

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,16 +2,13 @@ require './lib/airport'
 
 describe Airport do
 
-  #check that airport is an instance of the Airport class
-  describe airport do
-    # it "should be an instance of Airport" do
-    #   airport = Airport.new
-    #   expect(airport).to be_instance_of(Airport)
-    # end
-
-    it {is_expected.to be_instance_of(Airport)}
+  # check that airport is an instance of the Airport class
+  it "expect airport be an instance of Airport" do
+    airport = Airport.new
+    expect(airport).to be_instance_of(Airport)
   end
 
-  #next
+
+  # next
 
 end
