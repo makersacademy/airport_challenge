53bc4b517333de5b64a61e3a25e1008253e30550
Alastair Gilles <agilles91@gmail.com>
2019-11-16 13:07:38 +0100

    Added further tests for Airport#count

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,6 +2,7 @@ require 'airport'
 
 describe Airport do
   let(:plane) { double(:plane, flying?: false) }
+  let(:plane2) { double(:plane, flying?: false) }
   subject { Airport.new "Paris" }
 
   it "should have a name" do
@@ -22,6 +23,19 @@ describe Airport do
       subject.accept plane
       allow(plane).to receive(:flying?).and_return(true)
       expect(subject.count).to eq 0
+    end
+
+    it "should be 2 if two planes have landed" do
+      subject.accept plane
+      subject.accept plane2
+      expect(subject.count).to eq 2
+    end
+
+    it "should be 1 if two planes have landed and one takes off" do
+      subject.accept plane
+      subject.accept plane2
+      allow(plane).to receive(:flying?).and_return(true)
+      expect(subject.count).to eq 1
     end
   end
 end
