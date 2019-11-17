5aa395b05b71fa19f2834b6d9181deec040407bf
shafali03 <shaf03sm@gmail.com>
2019-11-17 20:33:53 +0000

    Check capacity of airport

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,8 +1,17 @@
 class Airport
-    def land(plane)
-        
+
+    def initialize(capacity)
+        @capacity = capacity 
+        @planes = []
     end
+    
+    
+    def land(plane)
+      raise 'Cannot land: Airport full' if @planes.length >= @capacity
+      @planes << plane
+    end
+    
     def take_off(plane)
-        
+            
     end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,13 +1,21 @@
 require 'airport'
 
 describe Airport do
-    subject(:airport) { described_class.new}
+    subject(:airport) { described_class.new(20)}
 
     it 'instruct a plane to land' do
-        expect(airport).to respond_to(:land).with(1).argument
-      end 
+    expect(airport).to respond_to(:land).with(1).argument
+  end 
 
-      it 'instruct a plane to take off' do
-        expect(airport).to respond_to(:take_off).with(1).argument
-      end
+   it 'instruct a plane to take off' do
+    expect(airport).to respond_to(:take_off).with(1).argument
+  end
+  
+  it 'does not allow landing when at capacity' do
+    plane = double :plane
+    20.times do 
+      airport.land(plane)
+    end
+     expect { airport.land(plane) }.to raise_error 'Cannot land: Airport full'
+  end
 end
