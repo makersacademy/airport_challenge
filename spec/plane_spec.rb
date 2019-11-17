7fda667a585c964b96e79bf7ddeab7f6489ffac5
Hyan18 <Hyan18@github.com>
2019-11-17 15:33:10 +0000

    refactor

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -6,7 +6,7 @@ describe Plane do
   it "should not be in an airport if flying" do
     plane = Plane.new
     # plane is in flight upon creation
-    expect(plane.airport).to eq nil
+    expect(plane.airport).not_to be_an_instance_of(Airport)
   end
 
 end
