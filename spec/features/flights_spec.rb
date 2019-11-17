06e921a3b4ff150c698931294bcc2bc6f0fa4dd6
Sam Folorunsho <s.folorunsho@hotmail.co.uk>
2019-11-17 22:16:11 +0000

    split feature test into take off and landing

--- spec/features/flights_spec.rb
+++ spec/features/flights_spec.rb
@@ -9,28 +9,38 @@ RSpec.describe 'Flights' do
   let(:clear_day) { double(:weather, weather_report: :clear) }
   let(:stormy_day) { double(:weather, weather_report: :stormy) }
   
-  it 'should be a successful journey' do
-    # given a plane is signed off to travel,
-    gatwick_airport.weather = clear_day.weather_report
-    schiphol_airport.weather = clear_day.weather_report
-    gatwick_airport.harbour_plane(plane_a)
-    gatwick_airport.harbour_plane(plane_b)
+  context 'a successful journey' do
+    it 'should include a successful take off' do
+      # given a plane is signed off to travel,
+      gatwick_airport.weather = clear_day.weather_report
+      gatwick_airport.harbour_plane(plane_a)
+      gatwick_airport.harbour_plane(plane_b)
 
-    # when a plane takes off,
-    gatwick_airport.commission_flight plane_a
+      # when a commissioned flight is ready to depart,
+      gatwick_airport.commission_flight plane_a
 
-    expect(plane_a.airborne).to be true
+      # then it should take off successfully.
+      expect(plane_a.airborne).to be true
+      expect(the_sky.planes_in_transit).to include plane_a
+      expect(gatwick_airport.hangar).not_to include plane_a
+    end
 
-    # then it should travel through the sky,
-    expect(the_sky.planes_in_transit).to include(plane_a)
-    expect(gatwick_airport.hangar).not_to include(plane_a)
+    it 'should include a successful landing' do
+      # given a plane is cleared for descent,
+      gatwick_airport.weather = clear_day.weather_report
+      gatwick_airport.harbour_plane(plane_a)
+      gatwick_airport.harbour_plane(plane_b)
+      gatwick_airport.commission_flight plane_a
+      schiphol_airport.weather = clear_day.weather_report
 
-    # and safely land at its destination.
-    schiphol_airport.harbour_plane(plane_a)
+      # when it approaches its destination
+      schiphol_airport.harbour_plane(plane_a)
 
-    expect(plane_a.airborne).to be false
-    expect(schiphol_airport.hangar).to include(plane_a)
-    expect(the_sky.planes_in_transit).not_to include(plane_a)
-    expect { schiphol_airport.harbour_plane plane_b }.to raise_error Errors::CURRENTLY_GROUNDED
+      # it should land safely on the runway
+      expect(plane_a.airborne).to be false
+      expect(schiphol_airport.hangar).to include plane_a
+      expect(the_sky.planes_in_transit).not_to include plane_a
+      expect { schiphol_airport.harbour_plane plane_b }.to raise_error Errors::CURRENTLY_GROUNDED
+    end
   end
 end
