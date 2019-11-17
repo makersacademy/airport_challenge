b5d40aea1e2feed9e3e687cc894bf1099a2599e6
Student <student@Makerss-MacBook-Air.local>
2019-11-17 18:27:08 +0000

    Added test for a plane to take off at the airport. Past the unit test by implementing takeOff method.

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,5 +1,8 @@
 class Airport
   def land
-    "Plane has landed safely!"
+    'Plane has landed safely!'
+  end
+  def takeOff
+    'Plane has taken off safely!'
   end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -3,8 +3,13 @@ require 'airport'
 describe Airport do
   it 'should create an aiport object'
   describe '#land' do
-    it 'land the plane at the airport' do
-      expect(subject.land).to eq("Plane has landed safely!")
+    it 'should land the plane at the airport' do
+      expect(subject.land).to eq('Plane has landed safely!')
+    end
+  end
+  describe '#takeOff' do
+    it 'should let user know that the plane has taken off' do
+      expect(subject.takeOff).to eq('Plane has taken off safely!')
     end
   end
 end
