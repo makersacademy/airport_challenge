2476042b1d605dd3ce719a961798bb5eca98b942
Andrea Diotallevi <andrea.diotallevi@outlook.com>
2019-11-17 12:58:53 +0000

    Refactor tests to include 'context' and 'let'

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -10,8 +10,9 @@ describe Airport do
   describe "#initialize" do
 
     it "should be able to override the instance variable capacity" do
-      airport = Airport.new(50, double(:weather, stormy?: false))
-      50.times do
+      weather = double(:weather, stormy?: false)
+      airport = Airport.new(10, weather)
+      10.times do
         plane = double(:plane, landed?: false, land: true)
         airport.land(plane)
       end
@@ -24,9 +25,10 @@ describe Airport do
   describe "#land" do
 
     context "when the weather is stormy" do
+      let(:weather) { double(:weather, stormy?: true) }
+      let(:airport) { Airport.new(1, weather) }
 
       it "should raise an error" do
-        airport = Airport.new(1, double(:weather, stormy?: true))
         plane = double(:plane)
         expect { airport.land(plane) }.to raise_error("The weather is stormy")
       end
@@ -34,30 +36,23 @@ describe Airport do
     end
 
     context "when the weather is not stormy" do
-    
-      it "should raise an error when the airport is full" do
-        airport = Airport.new(1, double(:weather, stormy?: false))
-        plane = double(:plane)
-        airport.capacity.times do
-          plane = double(:plane, landed?: false, land: true)
-          airport.land(plane)
-        end
-        plane = double(:plane, landed?: false)
-        expect { airport.land(plane) }.to raise_error("The airport is full")
-      end
+      let(:weather) { double(:weather, stormy?: false) }
+      let(:airport) { Airport.new(1, weather) }
 
       it "should be able to land one plane" do
-        airport = Airport.new(1, double(:weather, stormy?: false))
         plane = double(:plane, landed?: false, land: true)
         expect(airport.land(plane)).to eq plane
       end
 
+      it "should raise an error if the airport is full" do
+        plane = double(:plane, landed?: false, land: true)
+        airport.land(plane)
+        expect { airport.land(plane) }.to raise_error("The airport is full")
+      end
+
       it "should raise an error if the plane has already landed" do
-        airport = Airport.new(2, double(:weather, stormy?: false))
-        plane1 = double(:plane, landed?: false, land: true)
-        airport.land(plane1)
-        plane2 = double(:plane, landed?: true)
-        expect { airport.land(plane2) }.to raise_error("Plane already landed")
+        plane = double(:plane, landed?: true)
+        expect { airport.land(plane) }.to raise_error("Plane already landed")
       end
 
     end
@@ -67,9 +62,10 @@ describe Airport do
   describe "#take_off" do
 
     context "when the weather is stormy" do
+      let(:weather) { double(:weather, stormy?: true) }
+      let(:airport) { Airport.new(1, weather) }
 
       it "should raise an error" do
-        airport = Airport.new(1, double(:weather, stormy?: true))
         plane = double(:plane)
         expect { airport.take_off(plane) }.to raise_error("The weather is stormy")
       end
@@ -77,22 +73,21 @@ describe Airport do
     end
 
     context "when the weather is not stormy" do
-
-      it "should raise an error when the airport is empty" do
-        airport = Airport.new(1, double(:weather, stormy?: false))
-        plane = double(:plane)
-        expect { airport.take_off(plane) }.to raise_error("The airport is empty")
-      end
+      let(:weather) { double(:weather, stormy?: false) }
+      let(:airport) { Airport.new(1, weather) }
 
       it "should be able to take off a plane" do
-        airport = Airport.new(1, double(:weather, stormy?: false))
         plane = double(:plane, landed?: false, land: true, take_off: false)
         airport.land(plane)
         expect(airport.take_off(plane)).to eq plane
       end
 
+      it "should raise an error if the airport is empty" do
+        plane = double(:plane)
+        expect { airport.take_off(plane) }.to raise_error("The airport is empty")
+      end
+
       it "should raise an error if the plane has not landed in this airport" do
-        airport = Airport.new(1, double(:weather, stormy?: false))
         plane1 = double(:plane, landed?: false, land: true)
         airport.land(plane1)
         plane2 = double(:plane, landed?: true)
