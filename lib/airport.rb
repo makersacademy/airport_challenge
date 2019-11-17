0e268fa54f0866dccf44c706b3ef009653ba9420
Daniel <demartinezgatell@gmail.com>
2019-11-17 20:00:16 +0000

    air traffic controller stories complete

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,18 +1,25 @@
 class Airport
 
   attr_reader :planes
+  attr_reader :weather
 
   def initialize
     @planes = []
+    @weather = Weather.new
   end
 
   def land(plane)
+    todays_landing_weather = @weather.generate
+    fail "There's a storm brewing. You can't land." if todays_landing_weather == "Stormy weather!"
     fail "The airport is full." if @planes.size >= 20
     print "Plane landed!"
     @planes << plane
   end
 
   def take_off
+    # weather = Weather.new
+    todays_weather = @weather.generate
+    fail "There's a storm brewing and you can't fly off." if todays_weather == "Stormy weather!"
     print "Plane has taken off!"
     @planes.pop
   end
@@ -20,7 +27,22 @@ class Airport
 end
 
 class Plane
-
   plane = Plane.new
+end
+
+class Weather
+
+  def generate
+    num = rand(5)
+    if num == 1
+      return "Sunny"
+    elsif num == 2
+      return "Light drizzle"
+    elsif num == 3
+      return "Apocalypitcally mild"
+    else num == 4
+      return "Stormy weather!"
+    end
+  end
 
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,19 +2,36 @@ require './lib/airport'
   describe Airport do
       airport = Airport.new
 
-  it "Does plane land at the airport" do
+   it "Does plane land at the airport" do
+     allow(airport.weather).to receive(:generate) {"Sunny"}
     expect{airport.land(Plane.new)}. to output("Plane landed!").to_stdout
-  end
+   end
 
-  it "Does plane take off from aiport" do
-    plane = Plane.new
-   expect{airport.take_off}. to output("Plane has taken off!").to_stdout
-  end
+   it "Does plane take off from airport" do
+     airport = Airport.new
+     allow(airport.weather).to receive(:generate) {"Sunny"}
+     plane = Plane.new
+     expect{airport.take_off}. to output("Plane has taken off!").to_stdout
+   end
 
-  it "Airport full of planes. No room to land!" do
+   it "Airport full of planes. No room to land!" do
+     airport = Airport.new
+     allow(airport.weather).to receive(:generate) {"Sunny"}
+     20.times {airport.land(Plane.new)}
+     expect {airport.land(Plane.new)}.to raise_error("The airport is full.")
+   end
+
+  it "Prevent take off when weather is stormy." do
     airport = Airport.new
-    20.times {airport.land(Plane.new)}
-    expect {airport.land(Plane.new)}.to raise_error("The airport is full.")
+    allow(airport.weather).to receive(:generate) {"Stormy weather!"}
+    expect {airport.take_off}.to raise_error("There's a storm brewing and you can't fly off.")
   end
+
+ it "Prevent landing when weather is stormy" do
+   airport = Airport.new
+   plane = Plane.new
+   allow(airport.weather).to receive(:generate) {"Stormy weather!"}
+   expect {airport.land(plane)}.to raise_error("There's a storm brewing. You can't land.")
+ end
 
 end
