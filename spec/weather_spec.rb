a542568bfce6d59dafe2134e7b0427b4d4f73d45
Jade Beresford <jade_fletcher@hotmail.co.uk>
2019-11-17 21:33:10 +0000

    Test coverage improved

--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -2,6 +2,10 @@ require 'weather'
 
 describe Weather do
   describe "#stormy?" do
+    it "chooses a weather condition at random and returns a boolean" do
+      expect(subject.stormy?).to be(true).or be(false)
+    end
+
     it "returns true if the weather is stormy" do
       allow(subject).to receive(:current_forecast) { :stormy }
       expect(subject.stormy?).to eq true
