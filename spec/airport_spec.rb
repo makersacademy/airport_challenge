2a63129c0d8bf8a67e8e8611f8062846b36c2598
Student <student@Makerss-MacBook-Air.local>
2019-11-17 23:45:15 +0000

    made changes to airport.rb class. Removed stormy? method and replaced with stormy instance variable. Set weather when airport object is created. Made unit tests to check whether it is stormy before take off and landing.

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,30 +1,27 @@
 require 'airport'
 
 describe Airport do
-  it 'should create an aiport object'
   let (:plane) { double :plane }
   describe '#land' do
-    it 'should land the plane at the airport' do
+    it 'should land the plane at the airport if not stormy' do
       expect(subject.land(plane)).to eq('Plane has landed safely!')
     end
     it 'should raise an error message when the airport is full' do
       5.times { subject.land plane }
       expect{ subject.land plane }.to raise_error("WARNING airport is full, no space to land")
     end
+    it 'should raise an error message when its stormy' do
+      subject.stormy = true
+      expect{ subject.land plane }.to raise_error("WARNING stormy weather at airport abort landing")
+    end
   end
   describe '#take_off' do
-    it 'should let user know that the plane ha taken off' do
+    it 'should let user know that the plane has taken off if not stormy' do
       expect(subject.take_off(plane)).to eq('Plane has taken off safely!')
     end
-  end
-  describe '#stormy?' do
-    let (:weather) {double :weather}
-    it 'should create weather object and return false when not stormy' do
-      expect(subject.stormy?).to eq(false)
+    it 'should not let the plane take off if it is stormy' do
+      subject.stormy = true
+      expect{ subject.take_off plane }.to raise_error('WARNING too stormy to take off, abort take off')
     end
-    # let (:weather) {double Weather(true).new}
-    # it 'should create weather object and return true when stormy' do
-    #   expect(subject.stormy?).to eq(true)
-    # end
   end
 end
