62125eb8e0430f8bca2c14d005c2e96e37685f36
Raluca Ciucu <raluca.ciucu@gmail.com>
2019-11-17 21:17:11 +0000

    Add weather method and tests

--- lib/airport.rb
+++ lib/airport.rb
@@ -10,8 +10,11 @@ class Airport
       fail "Airport is full"  
     end 
 
+    fail "Weather is stormy, landing not possible" if weather == :stormy
+
     plane.land
     @ramp << plane
+
   end
 
   def take_off_plane(plane)
@@ -19,8 +22,12 @@ class Airport
       fail "Plane never landed here in the first place"
     end
 
+    
+    fail "Weather is stormy, take off not possible" if weather == :stormy
+    
     plane.take_off
     @ramp.delete(plane)
+
   end
 
   def parked_planes_count
@@ -28,7 +35,11 @@ class Airport
   end
 
   def weather
-    @weather
+    if rand(10) > 3
+      :sunny
+    else
+      :stormy
+    end
   end
 
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -6,12 +6,14 @@ describe Airport do
   it "should allow planes to land" do
     plane = Plane.new(true)
     airport = Airport.new(1)
+    allow(airport).to receive(:weather) { :sunny }
     airport.land_plane(plane)
   end
 
   it "should allow planes to take off" do
     plane = Plane.new(true)
     airport = Airport.new(1)
+    allow(airport).to receive(:weather) { :sunny }
     airport.land_plane(plane)
     airport.take_off_plane(plane)
     expect(plane.in_flight?).to eq true
@@ -20,6 +22,7 @@ describe Airport do
   it "should allow 1 plane to land" do
     plane = Plane.new(true)
     airport = Airport.new(1)
+    allow(airport).to receive(:weather) { :sunny }
     airport.land_plane(plane)
     expect(airport.parked_planes_count).to eq 1
   end
@@ -28,6 +31,7 @@ describe Airport do
     plane = Plane.new(true)
     plane_2 = Plane.new(true)
     airport = Airport.new(2)
+    allow(airport).to receive(:weather) { :sunny }
     airport.land_plane(plane)
     airport.land_plane(plane_2)
     expect(airport.parked_planes_count).to eq 2
@@ -35,6 +39,7 @@ describe Airport do
 
   it "should not allow a plane to land when airport is full" do
     airport = Airport.new(5)
+    allow(airport).to receive(:weather) { :sunny }
     5.times { airport.land_plane(Plane.new(true)) }
     expect { airport.land_plane(Plane.new(true)) }.to raise_error("Airport is full")
   end
@@ -42,6 +47,7 @@ describe Airport do
   it "should allow a landed plane to take off" do
     plane = Plane.new(true)
     airport = Airport.new(1)
+    allow(airport).to receive(:weather) { :sunny }
     airport.land_plane(plane)
     airport.take_off_plane(plane)
   end
@@ -55,5 +61,33 @@ describe Airport do
   it "should keep track of the local weather" do
     airport = Airport.new(1)
     expect(airport).to respond_to(:weather)
+  end
+
+  it "should be sunny most of the time" do
+    airport = Airport.new(1)
+    allow(airport).to receive(:rand) { 5 }
+    expect(airport.weather).to eq(:sunny)
+  end
+
+  it "should be stormy sometimes" do
+    airport = Airport.new(1)
+    allow(airport).to receive(:rand) { 1 }
+    expect(airport.weather).to eq(:stormy)
+  end
+
+  it "should prevent landing if the weather is stormy" do
+    plane = Plane.new(true)
+    airport = Airport.new(1)
+    allow(airport).to receive(:weather) { :stormy }
+    expect { airport.land_plane(plane) }.to raise_error("Weather is stormy, landing not possible")
+  end
+
+  it "should prevent take off if weather is stormy" do
+    plane = Plane.new(true)
+    airport = Airport.new(1)
+    allow(airport).to receive(:weather) { :sunny }
+    airport.land_plane(plane)
+    allow(airport).to receive(:weather) { :stormy }
+    expect { airport.take_off_plane(plane) }.to raise_error("Weather is stormy, take off not possible")
   end
 end
