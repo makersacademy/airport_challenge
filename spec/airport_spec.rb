fbec53d26b8bb56217af28d7bd65e9cfedf44614
Jonathan Palma <jonathanpalma89@gmail.com>
2019-11-19 12:59:38 +0000

    More readable Unit Test

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -14,15 +14,15 @@ describe Airport do
 
   describe '#plane'
     
-    context 'when Plane lands' do
+    context 'When Plane lands' do
       before do # checks if weather is sunny before doing any actions
         allow(airport).to receive(:stormy?).and_return(false)
       end
-      it 'Should land.' do
+      it 'should land.' do
         expect(airport).to respond_to(:to_land).with(1).argument # responds to 'to_land' method with '1' argument, in this case 'plane'.
       end 
-      context 'Abort when' do
-        it 'Airport is full' do # raise an error if airport is full (Default capacity - see airport.rb)
+      context 'abort when' do
+        it 'is full' do # raise an error if airport is full (Default capacity - see airport.rb)
           airport.capacity.times { airport.to_land(plane) }
           expect { airport.to_land(plane) }.to raise_error "Airport is full: Take off some planes."
         end
@@ -40,11 +40,11 @@ describe Airport do
       it 'should take off' do
         expect(airport).to respond_to(:take_off) # responds to 'take_off' method. 
       end
-      context 'Abort when' do
-        it 'empty airport' do # raise an error to take off if there is 0 planes in the airport
+      context 'abort when' do
+        it 'is empty' do # raise an error to take off if there is 0 planes in the airport
           expect { airport.take_off }.to raise_error "Airport is empty: Insert a plane." 
         end
-        it 'storm' do
+        it 'stormy' do
           allow(airport).to receive(:stormy?).and_return(true)
           expect { airport.take_off }.to raise_error "Storm hapenning: Please try again later."
         end
