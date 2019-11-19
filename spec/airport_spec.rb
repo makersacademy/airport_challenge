577a13e0c46af28f16f522de792ef2fdf3a540d2
Jonathan Palma <jonathanpalma89@gmail.com>
2019-11-19 12:56:40 +0000

    More readable Unit Test

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -12,42 +12,44 @@ describe Airport do
     end
   end
 
-  describe '#to_land' do
-    before do # checks if weather is sunny before doing any actions
-      allow(airport).to receive(:stormy?).and_return(false)
-    end
-    it 'Instructs a plane to land.' do
-      expect(airport).to respond_to(:to_land).with(1).argument # responds to 'to_land' method with '1' argument, in this case 'plane'.
-    end 
-    context 'Abort when' do
-      it 'full airport' do # raise an error if airport is full (Default capacity - see airport.rb)
-        airport.capacity.times { airport.to_land(plane) }
-        expect { airport.to_land(plane) }.to raise_error "Airport is full: Take off some planes."
+  describe '#plane'
+    
+    context 'when Plane lands' do
+      before do # checks if weather is sunny before doing any actions
+        allow(airport).to receive(:stormy?).and_return(false)
       end
-      it 'storm' do
-        allow(airport).to receive(:stormy?).and_return(true)
-        expect { airport.to_land(plane) }.to raise_error "Storm hapenning: Please try again later."
-      end
-    end
-  end
-  
-  describe '#take_off' do
-    before do # checks if weather is sunny before doing any actions
-      allow(airport).to receive(:stormy?).and_return(false)
-    end
-    it 'Instructs a plane to take off' do
-      expect(airport).to respond_to(:take_off) # responds to 'take_off' method. 
-    end
-    context 'Abort when' do
-      it 'empty airport' do # raise an error to take off if there is 0 planes in the airport
-        expect { airport.take_off }.to raise_error "Airport is empty: Insert a plane." 
-      end
-      it 'storm' do
-        allow(airport).to receive(:stormy?).and_return(true)
-        expect { airport.take_off }.to raise_error "Storm hapenning: Please try again later."
+      it 'Should land.' do
+        expect(airport).to respond_to(:to_land).with(1).argument # responds to 'to_land' method with '1' argument, in this case 'plane'.
+      end 
+      context 'Abort when' do
+        it 'Airport is full' do # raise an error if airport is full (Default capacity - see airport.rb)
+          airport.capacity.times { airport.to_land(plane) }
+          expect { airport.to_land(plane) }.to raise_error "Airport is full: Take off some planes."
+        end
+        it 'stormy' do
+          allow(airport).to receive(:stormy?).and_return(true)
+          expect { airport.to_land(plane) }.to raise_error "Storm hapenning: Please try again later."
+        end
       end
     end
-  end
+    
+    context 'When Plane takes off' do
+      before do # checks if weather is sunny before doing any actions
+        allow(airport).to receive(:stormy?).and_return(false)
+      end
+      it 'should take off' do
+        expect(airport).to respond_to(:take_off) # responds to 'take_off' method. 
+      end
+      context 'Abort when' do
+        it 'empty airport' do # raise an error to take off if there is 0 planes in the airport
+          expect { airport.take_off }.to raise_error "Airport is empty: Insert a plane." 
+        end
+        it 'storm' do
+          allow(airport).to receive(:stormy?).and_return(true)
+          expect { airport.take_off }.to raise_error "Storm hapenning: Please try again later."
+        end
+      end
+    end
     
   describe '#storm' do
     it 'may happen...' do
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -1,8 +1,5 @@
 require 'plane'
 
 describe Plane do
-
-  context 'does exist?' do
     it { is_expected.to be_an_instance_of Plane }
-  end 
 end
