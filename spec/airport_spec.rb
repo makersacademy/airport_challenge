c4385e16e1474c80455f0a2947b6c83d946b028d
Callum McDoiugall <cal-mcd@outlook.com>
2019-12-13 16:02:02 +0000

    comitting before pushing all changes to pull

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -7,22 +7,21 @@ describe Plane do
 
   it 'tells a plane to land' do
     my_plane = Plane.new
-    expect(my_plane).to respond_to(:land)
+      expect(my_plane).to respond_to(:land)
   end
 
   it 'tells a plane to take off' do
     my_plane = Plane.new
-    expect(my_plane).to respond_to(:depart)
+      expect(my_plane).to respond_to(:depart)
   end
 
   it 'confirms the plane is no longer at the airport' do
     my_plane = Plane.new
     string = "Plane has left the airport"
-    string.should eql("Plane has left the airport")
+      string.should eql("Plane has left the airport")
   end
 
   it 'raises an error when airport is full' do
-    my_plane = Plane.new
     expect { subject.land }.to raise_error 'Do not land, the airport is full!'
   end
 end
