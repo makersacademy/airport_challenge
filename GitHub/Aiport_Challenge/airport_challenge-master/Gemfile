f3ed5d92410052102d6a7619ae9744511b7a177b
Samantha Dalzell Matthew <samidm13@gmail.com>
2019-12-13 11:00:05 +0000

    error for no constant airport, creating an airport for the plane to land

---
+++ GitHub/Aiport_Challenge/airport_challenge-master/Gemfile
@@ -0,0 +1,10 @@
+source 'https://rubygems.org'
+
+gem 'rake'
+gem 'rubocop', '0.71.0'
+
+group :test do
+  gem 'rspec'
+  gem 'simplecov', require: false
+  gem 'simplecov-console', require: false
+end
---
+++ GitHub/Aiport_Challenge/airport_challenge-master/spec/plane_spec.rb
@@ -0,0 +1,4 @@
+# User Stories:
+# *I want to instruct a plane to land at an airport
+# *I want to instruct a plane to take off from an airport and confirm that it is no longer in the airport
+# *I want to prevent landing when the airport is full 
