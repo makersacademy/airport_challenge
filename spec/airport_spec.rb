ddff05d91c62eaabe47edc3bfba1f3959e1c7651
christymasterson <christymasterson@gmail.com>
2019-12-13 12:06:16 +0000

    Created test to check land_plane method works

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -9,4 +9,9 @@ describe Airport do
 
   it { is_expected.to respond_to :land_plane }
 
+  it 'lands a plane' do
+    my_plane = Plane.new
+    expect(subject.land_plane(my_plane)).to eq my_plane
+  end
+
 end
