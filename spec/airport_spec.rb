6f0f770888b95737c21a254f4d5b270aa468cabf
sarita-sumbria <saritasumbria@gmail.com>
2019-12-13 12:28:12 +0000

    created a unit test to check that land method respond to the instance of Airport class

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -5,4 +5,9 @@ describe Airport do
     airport_name = Airport.new
     expect(airport_name).to be_instance_of Airport
   end
+
+  it 'will make Airport instance to respond to the land method' do
+    airport_name = Airport.new
+    expect(airport_name). to respond_to(:land)
+  end
 end
