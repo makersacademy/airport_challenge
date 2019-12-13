a371070ae4747f479ee5768a2c9594244cf3c747
Aaron Redden <aaron.redden01@vodafone.com>
2019-12-13 16:02:50 +0000

    started to refactor to accomdate plane count

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,6 +1,11 @@
 require 'airport'
+
 describe Airport do
-    it { is_expected.to respond_to :land }
+    it 'expects plane to land' do
+      plane = Plane.new
+      subject.land(plane)
+      expect(subject.plane).to eq plane
+    end
   end
   describe Airport do
     it { is_expected.to respond_to :take_off }
@@ -8,7 +13,6 @@ describe Airport do
   describe Airport do
     it { is_expected.to respond_to :airport_full }
   end
-
 
 # describe Airport do
 #   it 'expects landing planes' do
