8448610a1f9db5682e3155f574779c83aa7cf1d3
Aaron Redden <aaron.redden01@vodafone.com>
2019-12-13 13:02:22 +0000

    failed test to expect landing plane.

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,3 +1,9 @@
-describe Airport do
+require 'airport'
 
+describe Airport do
+  it 'expects landing planes' do
+    plane = Plane.new
+    subject.land(plane)
+    expect(subject.plane).to eq plane
+  end
 end
