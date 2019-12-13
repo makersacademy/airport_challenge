2f8dd59c39f1c570d61381d35926c8beb6d8da55
nboyse <natasha.boyse@trade.gov.uk>
2019-12-13 12:19:28 +0000

    Update airport_spec.rb

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -9,4 +9,11 @@ describe Airport do
     allow(airplane).to receive(:land)
     expect(subject.land(airplane)).to include(airplane)
   end
+
+  #TDD step 2
+  it 'instructs plane to take off and confirms not at airport' do
+    allow(airplane).to receive(:land).and_return(true)
+    subject.take_off(airplane)
+    expect(subject.airplanes).not_to include(airplane)
+  end
 end
