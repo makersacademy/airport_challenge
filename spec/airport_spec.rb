9429af9d9c7f0473e3c6eb9a7f567e970d3a964d
Laura Kytonen <laura.kytonen@gmail.com>
2019-12-13 11:02:29 +0000

    RED: added a test to test that it can create an instance of the Airport class(should have done this first so I've put it as the first one.

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,6 +2,11 @@ require 'airport'
 
 describe Airport do
 
+  it "can create an instance of the class Airport" do
+    airport = Airport.new
+    expect(airport).to be_a Airport
+  end
+  
   it "has a method to instruct a plane to land at the airport" do
     expect(Airport.new).to respond_to(:land)
   end
