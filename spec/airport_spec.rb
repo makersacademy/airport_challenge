c63bfc0beb03a74582bc1ad08f876571840b7f63
Julia Jones <j.jones.13@ucl.ac.uk>
2019-12-13 11:20:51 +0000

    I still get a no NoMethodError (undefined method land_plane for #<Airport:0x00007f92e5136bc0>) in Rspec test. Now to write the ruby file to amend this

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,5 +1,13 @@
 require 'airport'
 
 describe Airport do
+  it 'creating a new instance of airport' do
+  expect subject.to be_a Airport
+  end
+end
 
+describe Airport do
+  it 'landing the plane at the airport' do
+  expect subject.to respond_to :land_plane
+  end
 end
