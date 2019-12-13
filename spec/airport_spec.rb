a48d0f843cb9fe0189e6459620e516ad3d41de66
Aaron Diable <aaron.diable@gmail.com>
2019-12-13 11:57:43 +0000

    Added test to confirm if a plane took off

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -3,8 +3,15 @@ require './lib/airport'
 describe Airport do
   context 'Airport allows plane to land' do
     it 'returns true if plane lands' do
-      plane = Plane.new
+      # removed due to useless assignment
+      # plane = Plane.new
       expect(subject.land_plane).to be true
+    end
+
+    context 'Airport allows plane to take off' do
+      it 'returns true after takeoff' do
+        expect(subject.take_off).to be true
+      end
     end
   end
 end
