f9262270c74349b2058c6ae0fe3ed38ffe7ec845
Aaron Redden <aaron.redden01@vodafone.com>
2019-12-13 13:50:09 +0000

    failed test for plane landing.

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -3,7 +3,9 @@ require 'airport'
 describe Airport do
     it { is_expected.to respond_to :land }
   end
-
+  describe Airport do
+    it { is_expected.to respond_to :take_off }
+  end
 # describe Airport do
 #   it 'expects landing planes' do
 #     plane = Plane.new
