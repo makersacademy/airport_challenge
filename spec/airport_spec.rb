006d0ac78996896f04913f2645fa241ce397c3d6
Aaron Redden <aaron.redden01@vodafone.com>
2019-12-13 15:25:32 +0000

    airport full failed test.

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,11 +1,15 @@
 require 'airport'
-
 describe Airport do
     it { is_expected.to respond_to :land }
   end
   describe Airport do
     it { is_expected.to respond_to :take_off }
   end
+  describe Airport do
+    it { is_expected.to respond_to :airport_full }
+  end
+
+
 # describe Airport do
 #   it 'expects landing planes' do
 #     plane = Plane.new
