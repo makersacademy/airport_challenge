6d3331890e1fb09041eed028b438138e68241c5a
Callum McDoiugall <cal-mcd@outlook.com>
2019-12-13 11:09:31 +0000

    adding failing test for calling 'land' method

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,6 +1,12 @@
 require 'airport'
 
 describe Airport do
-  it 'creates an airport constance'
+  it 'creates an airport constant' do
     my_airport = Airport.new
+  end
+
+  it 'tells a plane to land' do
+    my_airport = Airport.new
+    expect(Airport.new).to respond_to(:land)
+  end
 end
