59968e5b1a5530ae2404ff7bd5586f8748df8145
Laura Kytonen <laura.kytonen@gmail.com>
2019-12-13 15:52:11 +0000

    trying to get the last test to green but not really working

--- lib/airport.rb
+++ lib/airport.rb
@@ -2,13 +2,13 @@ class Airport
 
   attr_reader :landed_planes
 
-  def initialize
+  def initialize(capacity = 20)
     @landed_planes = []
-    @capacity = 20
+    @max_capacity = capacity
   end
 
   def land(plane)
-    if @landed_planes.count == @capacity
+    if full? == true
       raise("Landing not possible, the airport is at full capacity.")
     else
       @landed_planes.push(plane)
@@ -16,6 +16,25 @@ class Airport
   end
 
   def take_off(plane)
-    @landed_planes.delete(plane)
+    if check_weather == "stormy"
+      raise("Take off not possible due to stormy weather.")
+    else
+      @landed_planes.delete(plane)
+    end
+  end
+
+  def check_weather
+    weather_options = ["sunny", "stormy"]
+    return weather_options.sample
+  end
+
+private
+
+  def full?
+    if @landed_planes.count >= @max_capacity
+      return true
+    else
+      return false
+    end
   end
 end
