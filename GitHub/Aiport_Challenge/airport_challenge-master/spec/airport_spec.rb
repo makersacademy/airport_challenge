4f2b1d79769c668bd167e252cf42bc2f4ead3d27
Samantha Dalzell Matthew <samidm13@gmail.com>
2019-12-13 11:05:37 +0000

    creating instance of airport

--- GitHub/Aiport_Challenge/airport_challenge-master/spec/airport_spec.rb
+++ GitHub/Aiport_Challenge/airport_challenge-master/spec/airport_spec.rb
@@ -3,10 +3,10 @@
 # *I want to instruct a plane to take off from an airport and confirm that it is no longer in the airport
 # *I want to prevent landing when the airport is full
 
-require Airport
+require 'airport'
 
 describe Airport.new do
-  to 'creates an instance of Airport' do
+  it 'creates an instance of Airport' do
     expect(airport).to be_kind_of(Airport)
   end
 end
