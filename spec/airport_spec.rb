c296d8387dff5a1922209eaa5152c35ba50c8875
Anne-Louise Darko <ms.darko@live.co.uk>
2019-12-13 10:57:41 +0000

    first test updated

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,10 +1,12 @@
 require 'airport.rb'
 
 describe Airport do
+ it 'expects a class which creates instances' do
+   expect(Airport.new).to be_a(Airport)
+ end
+
   it 'can respond to message land_plane' do
     expect(Airport.new).to respond_to(:land_plane)
   end
 
 end
-
-#instance of class
