3138e2b8bd4716ffeab2ecf46e06f951d8950831
Iryna Jurkiv <iryna.jurkiv@gmail.com>
2019-12-13 12:49:16 +0000

    User Story 2 I am trying to take reading airport.plane after a plane has taken off from the airport

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -14,7 +14,13 @@ describe Airport do
       expect(plane).to be_an_instance_of(Plane)
     end
 
-    it 'is expected to respodn to take off with 1 arguement' do
+    it 'is expected to respond to take off with 1 arguement' do
       expect(subject).to respond_to(:takeoff).with(1).argument
+    end
+
+    it 'is expected to respond to take off and read plane' do
+      plane = Plane.new
+      subject.takeoff(plane)
+      expect(subject.plane).to eq plane
     end
 end
