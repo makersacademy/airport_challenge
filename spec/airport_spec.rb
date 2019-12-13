2f8153e06f4c3be902a3ec9213d7c6368ee119bb
Iryna Jurkiv <iryna.jurkiv@gmail.com>
2019-12-13 12:00:00 +0000

    User Story 1 Redone I am redoing User Story 1 and written a test for airport.land to release a new plane and get an error that currently nil is produced when airport.land

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,2 +1,17 @@
-Describe Plane do
+require 'Airport'
+
+describe Airport do
+    it 'responds to land' do
+      expect(subject).to respond_to(:land)
+    end
+
+    it 'responds to take off' do
+      expect(subject).to respond_to(:takeoff)
+    end
+
+    it 'when a plane lands a new instance of plane is created at airport' do
+      airport =Airport.new
+      plane = Plane.new
+      expect(airport.land).to eq plane
+    end
 end
