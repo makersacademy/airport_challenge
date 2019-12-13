5afacb9247adf73559755b61a2827b69f6381789
Laura Kytonen <laura.kytonen@gmail.com>
2019-12-13 12:42:11 +0000

    refactored to reflect third user story.

--- lib/airport.rb
+++ lib/airport.rb
@@ -4,10 +4,15 @@ class Airport
 
   def initialize
     @landed_planes = []
+    @capacity = 20
   end
 
   def land(plane)
-    @landed_planes.push(plane)
+    if @landed_planes.count == @capacity
+      raise("Landing not possible, the airport is at full capacity.")
+    else
+      @landed_planes.push(plane)
+    end
   end
 
   def take_off(plane)
