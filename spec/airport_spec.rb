468da120edcaef4349bce27d91c5f4db31615fcb
nboyse <natasha.boyse@trade.gov.uk>
2019-12-13 12:55:00 +0000

    Update airport_spec.rb

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -5,20 +5,14 @@ describe Airport do
   airplane = Airplane.new
   weather = 'normal'
 
-  # TDD Step 1
-  it 'instructs plane to land at airport' do
-    allow(airplane).to receive(:land)
-    expect(subject.land(airplane, weather)).to include(airplane)
-  end
-
-  # TDD step 2
-  it 'instructs plane to take off and confirms not at airport' do
-    allow(airplane).to receive(:land).and_return(true)
-    subject.take_off(airplane, weather)
-    expect(subject.airplanes).not_to include(airplane)
-  end
-
   describe 'land' do
+
+    # TDD Step 1
+    it 'instructs plane to land at airport' do
+      allow(airplane).to receive(:land)
+      expect(subject.land(airplane, weather)).to include(airplane)
+    end
+
     # TDD step 3 + 4 (But this is on ruby file)
     it 'raises an error when full' do
       subject.capacity.times { subject.land Airplane.new }
@@ -31,10 +25,18 @@ describe Airport do
     end
   end
 
-  # TDD step 5
   describe 'takeoff' do
+
+    # TDD step 5
     it 'prevents takeoff of plane when weather is stormy' do
       expect { subject.take_off Airplane.new, 'stormy' }.to raise_error 'Cannot takeoff plane due to stormy weather'
+    end
+
+    # TDD step 2
+    it 'instructs plane to take off and confirms not at airport' do
+      allow(airplane).to receive(:land).and_return(true)
+      subject.take_off(airplane, weather)
+      expect(subject.airplanes).not_to include(airplane)
     end
   end
 
