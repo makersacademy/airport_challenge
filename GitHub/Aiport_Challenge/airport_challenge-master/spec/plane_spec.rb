498a4eb03bd49d3344b5a9624bbf2345609f219e
Samantha Dalzell Matthew <samidm13@gmail.com>
2019-12-13 11:23:10 +0000

    trying to get plane to land, test is failing because have not defined

--- GitHub/Aiport_Challenge/airport_challenge-master/spec/plane_spec.rb
+++ GitHub/Aiport_Challenge/airport_challenge-master/spec/plane_spec.rb
@@ -6,8 +6,16 @@
 require "plane"
 
 describe Plane do
-  plane = Plane.new
+
+
   it "creates an instance of plane" do
+    plane = Plane.new
     expect(plane).to be_kind_of(Plane)
   end
+
+  it "should land at airport" do
+    plane = Plane.new
+    expect(plane).to respond_to(:land_plane)
+  end
+
 end
