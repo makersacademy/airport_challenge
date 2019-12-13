702e3f1795bad5d68d7bc141a4b0df82519acaf2
Samantha Dalzell Matthew <samidm13@gmail.com>
2019-12-13 11:11:42 +0000

    error when making plane constant

--- GitHub/Aiport_Challenge/airport_challenge-master/spec/plane_spec.rb
+++ GitHub/Aiport_Challenge/airport_challenge-master/spec/plane_spec.rb
@@ -1,4 +1,13 @@
 # User Stories:
 # *I want to instruct a plane to land at an airport
 # *I want to instruct a plane to take off from an airport and confirm that it is no longer in the airport
-# *I want to prevent landing when the airport is full 
+# *I want to prevent landing when the airport is full
+
+require "plane"
+
+describe Plane do
+  plane = Plane.new
+  it "creates an instance of plane" do
+    expect(plane).to be_kind_of(Plane)
+  end
+end
