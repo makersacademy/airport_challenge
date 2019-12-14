dd35df5e845dde4836262f351aca5f53ed3e49f5
Umberto Valente <uvalente89@gmail.com>
2019-12-14 13:20:03 +0000

    Created land and takeoff methods

--- lib/airport.rb
+++ lib/airport.rb
@@ -2,9 +2,26 @@ require_relative 'plane'
 
 class Airport
   attr_accessor :capacity
-  DEFAULT_CAPACITY = 1
+  attr_reader :apron
+  DEFAULT_CAPACITY = 2
 
   def initialize(capacity = DEFAULT_CAPACITY)
     @capacity = capacity
+    @apron = []
+  end
+
+  def land(plane)
+    raise 'The airport is full!' if full?
+    @apron << plane
+  end
+
+  def takeoff(plane)
+    @apron.delete(plane)
+  end
+
+  private
+
+  def full?
+    @apron.size >= DEFAULT_CAPACITY
   end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,6 +1,12 @@
 require 'airport'
 
 describe Airport do
+  let(:plane) { double(:plane) }
+  let(:plane_second) { double(:plane) }
+
+  it { is_expected.to respond_to(:land).with(1).argument }
+
+  it { is_expected.to respond_to(:takeoff).with(1).argument }
 
   describe '#initialize' do
     it 'Creating an Airport should set the default capacity' do
@@ -13,4 +19,30 @@ describe Airport do
     end
   end
 
+  describe '#land' do
+    it 'A landed plane should be visible' do
+      subject.land(plane)
+      expect(subject.apron).to include plane
+    end
+
+    it 'Should raise an error if the apron is full' do
+      Airport::DEFAULT_CAPACITY.times { subject.land(plane) }
+      expect { subject.land(plane) }.to raise_error('The airport is full!')
+    end
+  end
+
+  describe '#takeoff' do
+    it 'After takeoff the plane should not be in the apron' do
+      subject.land(plane)
+      subject.takeoff(plane)
+      expect(subject.apron).to be_empty
+    end
+
+    it 'After takeoff the other planes in the apron should still exist' do
+      subject.land(plane)
+      subject.land(plane_second)
+      subject.takeoff(plane)
+      expect(subject.apron).to be_any
+    end
+  end
 end
