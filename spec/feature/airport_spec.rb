10770b3acb9d3ae38837a470f1c0440d208beaeb
Vish Mayer <vish.mayer@gmail.com>
2019-12-14 16:16:20 +0000

    instructs planes to land

--- spec/feature/airport_spec.rb
+++ spec/feature/airport_spec.rb
@@ -4,4 +4,7 @@ describe Airport do
   it "instructs a plane to land" do
   expect(subject).to respond_to(:land).with(1).argument
   end
+  it "instructs a plane to take_off" do
+  expect(subject).to respond_to(:take_off).with(1).argument
+  end
 end
