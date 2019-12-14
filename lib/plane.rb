f085d54d94c9edb1e2b890fd8adec5389eacbee5
Umberto Valente <uvalente89@gmail.com>
2019-12-14 12:23:47 +0000

    Created Plane class

---
+++ lib/plane.rb
@@ -0,0 +1,7 @@
+class Plane
+  attr_reader :model
+
+  def initialize(model)
+    @model = model
+  end
+end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -1,1 +1,8 @@
 require 'plane'
+
+describe Plane do
+  it 'Creating a new plane should save the model' do
+    boeing = Plane.new('Boeing')
+    expect(boeing.model).to eq 'Boeing'
+  end
+end
