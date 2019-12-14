d5d8f883ee99c99ebb35add5acd74b08765dc32d
Lucian Boatright <lucian.boatright@gmail.com>
2019-12-14 17:29:22 +0000

    Test for take_off method

--- Airport_challange/spec/airport_spec.rb
+++ Airport_challange/spec/airport_spec.rb
@@ -1,13 +1,17 @@
 require "Airport.rb"
 
 describe Airport do
-  # it "test if plane can land" do
-  #   plane = Plane.new
-  #   subject.lane(plane)
+
   it 'lands plane' do
     plane = Plane.new
     expect(subject.land_plane(plane)).to eq plane
   end
+
+  it 'plane takes off' do
+    plane = Plane.new
+    expect(subject.take_off(plane)).to eq plane
+  end
+
 end
 
 
