c88a7806f8d399627e8be90439b68d8d1c78be8b
David Papamichael <davidpapamichael@gmail.com>
2019-12-14 19:22:03 +0000

    Airport test and class added

---
+++ spec/airport_spec.rb
@@ -0,0 +1,9 @@
+require "airport"
+
+describe Airport do
+    it "expects Airport to make working airports" do
+      airport = Airport.new
+      expect(airport).to eq airport
+    git end
+  end
+  
