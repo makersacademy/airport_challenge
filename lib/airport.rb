166e4a9adde8c1733301d171b12dedd37a4a2a48
Vish Mayer <vish.mayer@gmail.com>
2019-12-14 21:33:04 +0000

    more refactoring

--- lib/airport.rb
+++ lib/airport.rb
@@ -6,10 +6,17 @@ class Airport
   end
 
   def land(plane)
-    raise "airport full - cannot land plane" if @planes_landed.length >= @capacity
+    raise "airport full - cannot land plane" if full?
     @planes_landed << plane
   end
 
   def take_off(plane)
   end
+
+  private
+
+  def full?
+    @planes_landed.length >= @capacity
+  end
+
 end
--- spec/feature/airport_spec.rb
+++ spec/feature/airport_spec.rb
@@ -2,17 +2,25 @@ require 'airport'
 
 describe Airport do
   subject(:airport) { described_class.new(20) }
+  let(:plane) { double :plane }
 
-  it "instructs a plane to land" do
-    expect(airport).to respond_to(:land).with(1).argument
+  describe "#land" do
+    it "instructs a plane to land" do
+      expect(airport).to respond_to(:land).with(1).argument
   end
-  it "instructs a plane to take_off" do
-    expect(airport).to respond_to(:take_off).with(1).argument
+
+   context "when full" do
+   it "raises an error" do
+     20.times do
+       airport.land(plane)
+     end
+     expect { airport.land(plane) }.to raise_error "airport full - cannot land plane"
+   end
   end
-  it "prevents landing when at capacity" do
-    20.times do
-      airport.land(:plane)
-    end
-    expect { airport.land(:plane) }.to raise_error "airport full - cannot land plane"
-  end
-end
+   end
+   describe "#take_off" do
+   it "instructs a plane to take_off" do
+   expect(airport).to respond_to(:take_off).with(1).argument
+   end
+ end
+end 
