250098f335e08a839ead49d0972975c4e9cec7bd
Vish Mayer <vish.mayer@gmail.com>
2019-12-14 20:54:59 +0000

    Refactored

--- lib/airport.rb
+++ lib/airport.rb
@@ -2,12 +2,12 @@ class Airport
 
   def initialize(capacity)
     @capacity = capacity
-    @planes_landed = 0
+    @planes_landed = []
   end
 
   def land(plane)
-    raise "airport full - cannot land plane" if @planes_landed >= @capacity
-    @planes_landed += 1  
+    raise "airport full - cannot land plane" if @planes_landed.length >= @capacity
+    @planes_landed << plane
   end
 
   def take_off(plane)
