06befa5eff6af86b24c481a3ef62feb213fd8a3d
David Papamichael <davidpapamichael@gmail.com>
2019-12-14 19:29:17 +0000

    Plane test and class added

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -4,6 +4,5 @@ describe Airport do
     it "expects Airport to make working airports" do
       airport = Airport.new
       expect(airport).to eq airport
-    git end
-  end
-  
+    end
+end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -1,1 +1,8 @@
 require "plane"
+
+describe Plane do
+    it "expects Plane to make working planes" do
+      plane = Plane.new
+      expect(plane).to eq plane
+    end
+end
