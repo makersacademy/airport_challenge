3a3b51844a92aea02c01b54460001d1383840823
cpcwood <cpcwood@outlook.com>
2019-12-15 18:18:59 +0000

    Plane cleared to land rspec and passed

--- lib/plane.rb
+++ lib/plane.rb
@@ -1,3 +1,17 @@
 class Plane
+  attr_reader :in_flight
+  AVAILABLE_AIRPORTS = [:LHR, :LTN, :LGW]
+
+  def initialize(airport = nil)
+    @in_flight = true
+    if airport != nil
+      cleared_to_land(airport)
+    end
+  end
+
+  def cleared_to_land(airport)
+    airport.land_plane(self)
+    @in_flight = false
+  end
 
 end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -1,7 +1,23 @@
 require 'plane'
 
 describe Plane do
-  it 'lands after being cleared to land' do
-
+  it 'default initialization is in flight' do
+    default_plane = Plane.new()
+    expect(default_plane.in_flight).to eq true
   end
+
+  it 'can be initialized in an airport' do
+    heathrow = double("heathrow", :land_plane => nil)
+    heathrow_plane = Plane.new(heathrow)
+    expect(heathrow_plane.in_flight).to eq false
+    heathrow = double("heathrow", :planes => [heathrow_plane])
+    expect(heathrow.planes).to eq [heathrow_plane]
+  end
+
+  it 'lands after being cleared to land' do
+    heathrow = double("heathrow", :land_plane => true)
+    subject.cleared_to_land(heathrow)
+    expect(subject.in_flight).to eq false
+  end
+
 end
