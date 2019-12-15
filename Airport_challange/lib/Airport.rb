9df89f105efc94e6c5c2cbce3a2ce3e0a93793d4
Lucian Boatright <lucian.boatright@gmail.com>
2019-12-15 17:51:17 +0000

    Test.8- Add empty check to test, return statment if not plane

--- Airport_challange/lib/Airport.rb
+++ Airport_challange/lib/Airport.rb
@@ -6,17 +6,23 @@ class Airport
 
   DEFAULT_CAPACITY = 20
 
-  def initialize(capacity=DEFAULT_CAPACITY)
-    # @capcaity = capacity
+  def initialize(capacity=(DEFAULT_CAPACITY))
+    @capcaity = capacity
     @hanger = []
+    # @weather = weather
   end
 
+  # def weather 
+  #   weather = rand[1..9]
+  # end
+
   def land_plane(plane)
-    fail "Cannot land, Airport full" if full?
+    fail "Cannot land, Hanger full" if full?
     @hanger << plane
   end
 
   def take_off(plane)
+    fail "Cannot take off, No plane available" if empty?
     @hanger.delete(plane)
   end
 
@@ -36,62 +42,66 @@ class Airport
     @hanger.count >= DEFAULT_CAPACITY
   end
 
+
 end
 
 
 
-
-
-
-
-
-# require_relative "plane"
+# require "plane.rb"
 
 # class Airport
 
 #   attr_reader :hanger
 
-#   def initiailze
+#   DEFAULT_CAPACITY = 20
+
+#   def initialize(capacity=(DEFAULT_CAPACITY))
+#     @capcaity = capacity
 #     @hanger = []
+#     @weather = weather
 #   end
 
-#   def hanger_check
+#   def weather 
+#     weather_check = rand(1..9)
+#     if weather_check <= 6
+#       @weather = true
+#     else
+#       @weather = false
+#     end
+#   end
+
+#   def land_plane(plane)
+#     fail "Cannot land, Airport full" if full?
+#     if @weather == true
+#       @hanger << plane
+#     else
+#       puts 'Weather is to bad'
+#     end
+#   end
+
+#   def take_off(plane)
+#     fail "Cannot take off, No Airport" if full? == false
+#     if @weather == true 
+#       @hanger.delete(plane)
+#     else 
+#       puts "Weather is to bad"
+#     end
+#   end
+
+#   def hanger_check 
 #     @hanger
 #   end
 
-#   def land_plane(plane)
-#     @hanger << plane
-#   end
-
-#   def controler(plane)
-#     if !@plane.include? plane
-#       'The plane has taken off'
-#     else
-#       raise "The Weather was to bad"
+#   def air_traffic_controller(plane)
+#     if !@hanger.include? plane
+#       "The plane has taken off"
+#     else 
+#       raise "The Plane is grounded"
 #     end
 #   end
-# end
-  # def confirm_take_off(plane)
-  #   if !@hanger.include? plane
-  #     'Plane has left the airport'
-  #   else
-  #     raise 'Plane has not taken off.'
-  #   end
-  # end
 
-
-
-
-# class Airport
-
-#   attr_reader :plane
-
-#   def land_plane(plane)
-#     plane
-#   end
-
-#   def take_off(plane)
-#     plane
+#   def full?
+#     @hanger.count >= DEFAULT_CAPACITY
 #   end
 
 # end
@@ -100,32 +110,92 @@ end
 
 
 
-  # def controler_confirm
-  #   if !@hanger.include? plane
-  #     "Plane has taken off"
-  #   else
-  #     raise "Plane was not allowed to take off"
-  #   end
-  # end
 
 
-# class Airport
-#   # attr_reader :plane
 
-#   def initiailze(hanger,plane)
-#     @hanger = []
-#   end
+# # require_relative "plane"
 
-#   def land_plane(plane)
-#     @hanger << plane
-#   end
+# # class Airport
 
-#   def take_off(plane)
-#     plane
-#   end
+# #   attr_reader :hanger
+
+# #   def initiailze
+# #     @hanger = []
+# #   end
+
+# #   def hanger_check
+# #     @hanger
+# #   end
+
+# #   def land_plane(plane)
+# #     @hanger << plane
+# #   end
+
+# #   def controler(plane)
+# #     if !@plane.include? plane
+# #       'The plane has taken off'
+# #     else
+# #       raise "The Weather was to bad"
+# #     end
+# #   end
+# # end
+#   # def confirm_take_off(plane)
+#   #   if !@hanger.include? plane
+#   #     'Plane has left the airport'
+#   #   else
+#   #     raise 'Plane has not taken off.'
+#   #   end
+#   # end
 
 
-# end
+
+
+# # class Airport
+
+# #   attr_reader :plane
+
+# #   def land_plane(plane)
+# #     plane
+# #   end
+
+# #   def take_off(plane)
+# #     plane
+# #   end
+
+# # end
+
+
+
+
+
+#   # def controler_confirm
+#   #   if !@hanger.include? plane
+#   #     "Plane has taken off"
+#   #   else
+#   #     raise "Plane was not allowed to take off"
+#   #   end
+#   # end
+
+
+# # class Airport
+# #   # attr_reader :plane
+
+# #   def initiailze(hanger,plane)
+# #     @hanger = []
+# #   end
+
+# #   def land_plane(plane)
+# #     @hanger << plane
+# #   end
+
+# #   def take_off(plane)
+# #     plane
+# #   end
+
+
+# # end
+
+
 
 
 
--- Airport_challange/spec/airport_spec.rb
+++ Airport_challange/spec/airport_spec.rb
@@ -6,13 +6,13 @@ require 'Airport.rb'
 describe Airport do
   it { is_expected.to respond_to :land_plane }
 
-  it 'test that planes is not empty' do
+  it 'test that hanger is not empty' do
     plane_1 = Plane.new
     subject.land_plane(plane_1)
     expect(subject.hanger_check.include? plane_1).to eq true
   end
 
-  it 'Test that plane is empty when take_off' do
+  it 'Test that hanger is empty when take_off' do
     plane_2 = Plane.new
     subject.take_off(plane_2)
     expect(subject.hanger_check.include? plane_2).to eq false 
@@ -24,22 +24,56 @@ describe Airport do
     expect(subject.air_traffic_controller(plane_3)).to eq "The plane has taken off"
   end
 
-  describe '#land_plane' do
-    it "raised error statement when @hanger full" do
-      Airport::DEFAULT_CAPACITY.times {subject.land_plane(Plane.new)}
-      expect {subject.land_plane(Plane.new)}.to raise_error "Cannot land, Airport full"
-    end
-  end
-  context "Weather check, if weather is < 7 weather is good" do
-    describe "#weather" do
-      it "return true is weather is < 7" do
-        weather_check = Plane.new(weather: 5)
-        expect(weather_check.weather?).to eq (ture)
+  describe 'Initialization' do
+    subject{Airport.new}
+    let(:plane) {Plane.new}
+    it 'Default_capacity' do
+      Airport::DEFAULT_CAPACITY.times do
+        subject.land_plane(plane)
       end
+      expect{subject.land_plane(plane)}.to raise_error "Cannot land, Hanger full"
     end
   end
 
+  describe '#land_plane' do
+    it 'raised error and print statement when no planes' do
+      subject.capacity.times {subject.land_plane(Plane.new)}
+      expects {subject.land_plane(Plane.new)}.to raise_error "Cannot take off, No plane available"
+    end
+  end
+
+  # describe '#take_off' do
+  #   it 'raises message when no planes available' do
+  #     expect {subject.take_off(Plane.new)}.to raise_error "Cannot take off, No plane available"
+  #   end
+  # end
+
+  # describe '#land_plane' do
+  #   it 'Raises error message when No room in hanger' do
+  #     subject.capacity.times {subject.land_plane(Plane.new)}
+  #     expect{subject.land_plane(Plane.new)}.to raise_error "Cannot land, Hanger full"
+  #   end
+  # end
 end
+
+  # context "Weather check, if weather is < 7 weather is good" do
+  #   describe "#weather" do
+  #     it "return true is weather is < 7" do
+  #       weather_check = Plane.new(weather: 5)
+  #       expect(weather_check.weather?).to eq (ture)
+  #     end
+  #   end
+  # end
+
+
+
+
+  # describe '#land_plane' do
+  #   it "raised error statement when @hanger full" do
+  #     Airport::DEFAULT_CAPACITY.times {subject.land_plane(Plane.new)}
+  #     expect {subject.land_plane(Plane.new)}.to raise_error "Cannot land, Airport full"
+  #   end
+  # end
 
   # describe 'Initialization' do
   #   subject{Airport.new}
