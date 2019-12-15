e8a7bb10cb897a90041f2e00d5cda8593e158a0d
Meghan <meghan.e.mccue@gmail.com>
2019-12-15 20:44:19 +0000

    Update weather_spec tests

--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -1,8 +1,18 @@
 describe Weather do
   it { is_expected.to respond_to :stormy? }
 
-  it 'returns false when weather is not stormy' do
-    allow(Weather).to receive(:stormy?) { false }
-    expect(subject.stormy?).to eq false
+  describe '#stormy?' do
+
+    it 'returns false when weather is not stormy' do
+      allow(Weather).to receive(:stormy?) { false }
+      expect(subject.stormy?).to eq false
+    end
+
+    it 'returns true when weather is stormy' do
+      allow(Weather).to receive(:stormy?) { true }
+      expect(subject.stormy?).to eq true
+    end
+
   end
+
 end
