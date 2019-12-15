7dadde41419b7d9ce87a0b2a6a792c3cb8d8c9bd
cpcwood <cpcwood@outlook.com>
2019-12-15 21:37:18 +0000

    Fixed typo

--- lib/plane.rb
+++ lib/plane.rb
@@ -3,7 +3,7 @@ class Plane
 
   def initialize(airport = '')
     @in_flight = true
-    cleared_to_land(airport) if airport == ''
+    cleared_to_land(airport) if airport != ''
   end
 
   def cleared_to_land(airport)
@@ -15,7 +15,7 @@ class Plane
 
   def cleared_for_take_off(airport)
     raise('Plane already in flight!') if @in_flight == true
-    
+
     airport.plane_departed(self)
     @in_flight = true
   end
