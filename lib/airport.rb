49850c6b83b9dad4da895e3a85ce7ec38bbb8f68
David Papamichael <davidpapamichael@gmail.com>
2019-12-15 16:30:49 +0000

    Add guard condition to land method, planes_landed array and capacity with initialize method

--- lib/airport.rb
+++ lib/airport.rb
@@ -2,10 +2,19 @@ require_relative "plane"
 
 class Airport
 
+  def initialize(capacity)
+    @capacity = capacity
+    @planes_landed = []
+  end
+
   def land(plane)
+    fail "Error, airport full!" if @planes_landed.count >= @capacity
+
+    @planes_landed << plane
   end
 
   def take_off(plane)
+
   end
 
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,17 +1,27 @@
 require "airport"
 
 describe Airport do
+
   it "expects Airport to make working airports" do
-    airport = Airport.new
+    airport = Airport.new(10)
     expect(airport).to eq airport
   end
 
   it "airport to land a plane" do
-    expect(subject).to respond_to(:land).with(1).argument
+    expect(Airport.new(10)).to respond_to(:land).with(1).argument
   end
 
   it "airport to take_off a plane" do
-    expect(subject).to respond_to(:take_off).with(1).argument
+    expect(Airport.new(10)).to respond_to(:take_off).with(1).argument
+  end
+
+  it "airport not allow landing when is full" do
+    airport = Airport.new(10)
+    plane = Plane.new
+    10.times do
+      airport.land(plane)
+    end
+    expect { airport.land(plane) }.to raise_error "Error, airport full!"
   end
 
 end
