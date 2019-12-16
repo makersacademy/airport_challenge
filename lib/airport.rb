0a60a175e64c36dca7f3c20a3a3f9d93573ea5a6
Student <student@Makerss-MacBook-Air.local>
2019-12-16 07:45:06 +0000

    user story 3

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,16 +1,18 @@
 class Airport
+  def initialize(capacity)
+    @capacity = capacity
+    @array = 0 
+  end 
+  def land(plane)
+  raise 'Capacity full cannot land another plane ' if @array >= @capacity 
+  @array = @array + 1
+  end 
 
-def land(plane)
+   def take_off(plane)
+   end 
 end 
 
-def take_off(plane)
-
-end 
 
 
 
 
-
-
-
-end 
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,9 +1,19 @@
 require 'airport'
 describe Airport do 
+    subject(:airport) { described_class.new(20) }
     
-it { is_expected.to respond_to(:land).with(1).argument }
-
-it { is_expected.to respond_to(:take_off) }
+it 'tells a plane to land' do 
+    expect(airport).to respond_to(:land).with(1).arguments
+end 
+it 'tells a plane to take off' do
+expect(airport).to respond_to(:take_off).with(1).arguments
 
 end 
 
+it 'does not allow planes to land when at capacity' do 
+20.times do 
+    airport.land(:plane)
+end 
+expect { airport.land(:plane) }.to raise_error 'Capacity full cannot land another plane '
+end
+end 
