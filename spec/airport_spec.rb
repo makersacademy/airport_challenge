c80866e4a61640b4d666fd17f0cc159707a073ad
Student <student@Makerss-MacBook-Air.local>
2019-12-16 08:50:29 +0000

    ready to start story 4

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,7 +1,7 @@
 require 'airport'
 describe Airport do 
     subject(:airport) { described_class.new(20) }
-    
+    let(:plane) {double :plane}
 it 'tells a plane to land' do 
     expect(airport).to respond_to(:land).with(1).arguments
 end 
@@ -9,7 +9,7 @@ it 'tells a plane to take off' do
 expect(airport).to respond_to(:take_off).with(1).arguments
 
 end 
-
+ 
 it 'does not allow planes to land when at capacity' do 
 20.times do 
     airport.land(:plane)
