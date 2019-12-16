6f8cb3eff0c5a36392f314219f060d34fc92c95d
lajoiedevivre89 <emilyspencer89@hotmail.co.uk>
2019-12-16 21:46:43 +0000

    Deleted reflection. Aim to redo challenge

--- reflection.rb
+++
@@ -1,9 +0,0 @@
-# Reflection
-
-# I have addressed the six user stories and two edge cases
-# I have used mocking to account for stormy and sunny weather
-# SRP has been adhered to by extracting code from the land and 
-# take_off methods into private methods.
-# Test coverage for the airport class is good.
-# Test coverage for the weather class currently is very poor. 
-# This is because I haven't tested the random number generator yet.
