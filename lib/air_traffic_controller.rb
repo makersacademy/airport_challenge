103eef72adfdcbd0f6bdfb0d60fc05c71d815fa5
cpcwood <cpcwood@outlook.com>
2019-12-17 11:34:40 +0000

    AirTrafficController takes hash as arugment

--- lib/air_traffic_controller.rb
+++ lib/air_traffic_controller.rb
@@ -10,7 +10,9 @@ class AirTrafficController
     @weather = Weather.new
   end
 
-  def tell_plane_to_land(airport, plane)
+  def tell_plane_to_land(args)
+    airport = args[:airport]
+    plane = args[:plane]
     raise("Bad weather at #{airport.iata_code}, cannot land plane!") if weather.good_weather?(airport) == false
 
     raise("#{airport.iata_code} at capacity, cannot land plane!") if airport.airport_at_capacity? == true
@@ -19,7 +21,9 @@ class AirTrafficController
     log_action(airport, plane, 'landing at')
   end
 
-  def tell_plane_to_depart(airport, plane)
+  def tell_plane_to_depart(args)
+    airport = args[:airport]
+    plane = args[:plane]
     raise("Bad weather at #{airport.iata_code}, plane cannot take off!") if weather.good_weather?(airport) == false
 
     raise("Plane not at airport!") if airport.plane_departure_ready?(plane) == false
@@ -27,6 +31,8 @@ class AirTrafficController
     plane.cleared_for_take_off(airport)
     log_action(airport, plane, 'take off from')
   end
+
+  private
 
   def log_action(airport, plane, action)
     File.open("./logs/log.txt", 'a') { |line| line.write "#{Time.now} - User: #{object_id} - Action: Flight #{plane.flight_id} cleared for #{action} #{airport.iata_code}\n" }
--- spec/air_traffic_controller_spec.rb
+++ spec/air_traffic_controller_spec.rb
@@ -5,21 +5,21 @@ describe AirTrafficController do
     a380 = double('a380')
     heathrow = double('heathrow', :iata_code => :LHR)
     srand(2)
-    expect { subject.tell_plane_to_land(heathrow, a380) }.to raise_error("Bad weather at LHR, cannot land plane!")
+    expect { subject.tell_plane_to_land({:airport => heathrow, :plane => a380}) }.to raise_error("Bad weather at LHR, cannot land plane!")
   end
 
   it 'Planes do not land if airport is at capacity' do
     a380 = double('a380')
     heathrow = double('heathrow', :iata_code => :LHR, :airport_at_capacity? => true)
     srand(4)
-    expect { subject.tell_plane_to_land(heathrow, a380) }.to raise_error("LHR at capacity, cannot land plane!")
+    expect { subject.tell_plane_to_land({:airport => heathrow, :plane => a380}) }.to raise_error("LHR at capacity, cannot land plane!")
   end
 
   it 'Planes land if no bad weather and airport has capacity' do
     a380 = double('a380', :cleared_to_land => nil, :in_flight => false, :flight_id => 1)
     heathrow = double('heathrow', :iata_code => :LHR, :airport_at_capacity? => false, :planes => [a380])
     srand(4)
-    subject.tell_plane_to_land(heathrow, a380)
+    subject.tell_plane_to_land({:airport => heathrow, :plane => a380})
     expect(heathrow.planes).to eq [a380]
     expect(a380.in_flight).to eq false
   end
@@ -28,21 +28,21 @@ describe AirTrafficController do
     a380 = double('a380')
     heathrow = double('heathrow', :iata_code => :LHR)
     srand(2)
-    expect { subject.tell_plane_to_depart(heathrow, a380) }.to raise_error("Bad weather at LHR, plane cannot take off!")
+    expect { subject.tell_plane_to_depart({:airport => heathrow, :plane => a380}) }.to raise_error("Bad weather at LHR, plane cannot take off!")
   end
 
   it 'Planes do not depart if they are not in the airport' do
     a380 = double('a380')
     heathrow = double('Airport', :iata_code => :LHR, :plane_departure_ready? => false)
     srand(4)
-    expect { subject.tell_plane_to_depart(heathrow, a380) }.to raise_error("Plane not at airport!")
+    expect { subject.tell_plane_to_depart({:airport => heathrow, :plane => a380}) }.to raise_error("Plane not at airport!")
   end
 
   it 'Planes depart if weather is good and they are ready to depart' do
     a380 = double('a380', :cleared_for_take_off => nil, :in_flight => true, :flight_id => 1)
     heathrow = double('Airport', :iata_code => :LHR, :plane_departure_ready? => true, :planes => [])
     srand(4)
-    subject.tell_plane_to_depart(heathrow, a380)
+    subject.tell_plane_to_depart({:airport => heathrow, :plane => a380})
     a380 = double('a380', :in_flight => true)
     expect(heathrow.planes).to eq []
     expect(a380.in_flight).to eq true
@@ -51,17 +51,14 @@ describe AirTrafficController do
   it 'Logs take offs and landings' do
     srand(4)
     a380 = double('a380', :in_flight => true, :cleared_to_land => nil, :flight_id => 1)
-
     heathrow = double('Airport', :iata_code => :LHR, :airport_at_capacity? => false)
-    subject.tell_plane_to_land(heathrow, a380)
+    subject.tell_plane_to_land({:airport => heathrow, :plane => a380})
+    
     a380 = double('a380', :cleared_for_take_off => nil, :in_flight => false, :flight_id => 1)
-
     heathrow = double('Airport', :iata_code => :LHR, :plane_departure_ready? => true, :planes => [a380])
-    subject.tell_plane_to_depart(heathrow, a380)
-    p a380.flight_id
+    subject.tell_plane_to_depart({:airport => heathrow, :plane => a380})
 
     log = File.read("./logs/log.txt").split("\n")[-2, 2]
-
     expect(log).to eq ["#{Time.now} - User: #{subject.object_id} - Action: Flight #{a380.flight_id} cleared for landing at #{heathrow.iata_code}", "#{Time.now} - User: #{subject.object_id} - Action: Flight #{a380.flight_id} cleared for take off from #{heathrow.iata_code}"]
   end
 end
