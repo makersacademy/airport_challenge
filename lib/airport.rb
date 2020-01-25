f0a77e5f53b41b1ecb94143d323d6fd52830df4d
Joe Ellingworth <joeellingworth@gmail.com>
2020-01-25 14:42:12 +0000

    added test and passing test to confirm plane leaves airport after .takeoff

--- lib/airport.rb
+++ lib/airport.rb
@@ -5,12 +5,17 @@ class Airport
     def initialize
         @plane_storage = []
     end 
-    
+
     def land(plane)
         @plane_storage << plane
     end 
 
     def plane_count 
         @plane_storage.count
+    end 
+
+    def take_off
+        @plane_storage.pop
+        @plane_storage
     end 
 end 
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -6,4 +6,12 @@ describe Airport do
     airport = Airport.new
         expect(airport.land(plane)).to eq [plane]
     end 
+
+    it 'removes plane from storage when taking off' do 
+        plane = Plane.new
+        airport = Airport.new
+        subject.land(plane)
+        expect(subject.take_off).to eq []
+    end 
+
 end 
