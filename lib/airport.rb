36a06dc8d8cef8d4fa15db41f1cfa853f2603ef0
Josh Blumberg <jlblumberg@gmail.com>
2020-01-25 10:30:10 +0000

    third user story satisfied

--- lib/airport.rb
+++ lib/airport.rb
@@ -4,7 +4,11 @@ class Airport
   @parked_planes = 0
 
   def tell_to_land(plane)
+    if @parked_planes == 1
+      raise "Can't land. Airport is full."
+    else
     @parked_planes = 1
+    end
   end
 
   def tell_to_takeoff(plane)
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,25 +1,35 @@
 require 'airport'
 describe Airport do
+
   it 'Can instruct a plane to land' do
     expect(subject).to respond_to(:tell_to_land)
   end
+  
+    describe '#tell_to_land' do
+      it 'accepts a single plane' do
+        plane = Planes.new
+        expect { subject.tell_to_land(plane) }.not_to raise_error RuntimeError, "Can't land. Airport is full."
+      end
+      it 'raises an error if airport is full' do
+        plane = Planes.new
+        subject.tell_to_land(plane)
+        expect { subject.tell_to_land(plane) }.to raise_error
+      end
+    end
+
   it 'Can instruct a plane to take off' do
     expect(subject).to respond_to(:tell_to_takeoff)
   end
-  describe '#tell_to_land' do
-    it 'accepts a single plane' do
-      plane = Planes.new
-      expect { subject.tell_to_land(plane) }.not_to raise_error
+
+    describe '#tell_to_takeoff' do
+      it 'accepts a single plane' do
+        plane = Planes.new
+        expect { subject.tell_to_takeoff(plane) }.not_to raise_error
+      end
+      it 'confirms that the plane is no longer at the airport' do
+        plane = Planes.new
+        expect(subject.tell_to_takeoff(plane)).to eq 0
+      end
     end
-  end
-  describe '#tell_to_takeoff' do
-    it 'accepts a single plane' do
-      plane = Planes.new
-      expect { subject.tell_to_takeoff(plane) }.not_to raise_error
-    end
-    it 'confirms that the plane is no longer at the airport' do
-      plane = Planes.new
-      expect(subject.tell_to_takeoff(plane)).to eq 0
-    end
-  end
+
 end
