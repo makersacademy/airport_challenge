a7d7151f76514c7f1d99942a586b6c0c3a3990af
Josh Blumberg <jlblumberg@gmail.com>
2020-01-25 12:08:21 +0000

    Add initial capacity which can be written, and pass fourth user story

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,17 +1,21 @@
 require_relative 'plane'
 class Airport
 
-  @parked_planes = 0
+  def initialize(capacity=1)
+    @capacity = capacity
+    @parked_planes = []
+  end
 
   def tell_to_land(plane)
-    if @parked_planes == 1
-      raise "Can't land. Airport is full."
+    if @parked_planes.size == @capacity
+      raise "Can't land, because airport is full. Capacity is #{@capacity}, and there are #{@parked_planes.size} parked planes."
     else
-    @parked_planes = 1
+    @parked_planes << plane
     end
   end
 
   def tell_to_takeoff(plane)
-    @parked_planes = 0
+    @parked_planes.pop
+    @parked_planes.count
   end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -8,12 +8,12 @@ describe Airport do
     describe '#tell_to_land' do
       it 'accepts a single plane' do
         plane = Planes.new
-        expect { subject.tell_to_land(plane) }.not_to raise_error RuntimeError, "Can't land. Airport is full."
+        expect { subject.tell_to_land(plane) }.not_to raise_error
       end
       it 'raises an error if airport is full' do
         plane = Planes.new
         subject.tell_to_land(plane)
-        expect { subject.tell_to_land(plane) }.to raise_error
+        expect { subject.tell_to_land(plane) }.to raise_error RuntimeError
       end
     end
 
@@ -28,8 +28,14 @@ describe Airport do
       end
       it 'confirms that the plane is no longer at the airport' do
         plane = Planes.new
-        expect(subject.tell_to_takeoff(plane)).to eq 0
+        expect(subject.tell_to_takeoff(plane)).to eq(0)
       end
     end
+
+  it 'Has a default capacity of 1 which can be overwritten' do
+    plane = Planes.new
+    expect(Airport.new.instance_variable_get(:@capacity)).to eq(1)
+    expect(Airport.new(3).instance_variable_get(:@capacity)).to eq(3)
+  end
 
 end
