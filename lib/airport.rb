af95ebf65d09562f0afc1490f6343942e46fbfd6
Josh Blumberg <jlblumberg@gmail.com>
2020-01-25 10:07:47 +0000

    first two user stories satisfied

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,11 +1,13 @@
+require_relative 'plane'
 class Airport
 
-  parked_planes = []
+  @parked_planes = 0
 
   def tell_to_land(plane)
+    @parked_planes = 1
   end
 
   def tell_to_takeoff(plane)
-    parked_planes -= plane
+    @parked_planes = 0
   end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -8,12 +8,18 @@ describe Airport do
   end
   describe '#tell_to_land' do
     it 'accepts a single plane' do
-      expect { subject.tell_to_land("plane") }.not_to raise_error
+      plane = Planes.new
+      expect { subject.tell_to_land(plane) }.not_to raise_error
     end
   end
   describe '#tell_to_takeoff' do
     it 'accepts a single plane' do
-      expect { subject.tell_to_takeoff("plane") }.not_to raise_error
+      plane = Planes.new
+      expect { subject.tell_to_takeoff(plane) }.not_to raise_error
+    end
+    it 'confirms that the plane is no longer at the airport' do
+      plane = Planes.new
+      expect(subject.tell_to_takeoff(plane)).to eq 0
     end
   end
 end
