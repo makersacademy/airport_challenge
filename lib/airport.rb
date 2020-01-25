2329c8f4c26cbed6d4f4d108ced4f2b9745a6b6c
hansa-lallu <hansalallu@gmail.com>
2020-01-25 13:19:35 +0000

    added method land_plane in class Airport w test

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,3 +1,5 @@
 class Airport
-    
+    def land_plane
+        
+    end 
 end 
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -5,4 +5,11 @@ describe Airport do
     it 'checks if airport exists' do
         expect(subject).to be_a Airport
     end
+
+# my third test - checks if the class Airport responds to the method land_plane
+    it 'responds to land_plane' do
+        expect(subject).to respond_to :land_plane
+    end 
+# one line syntax is it {is_expected.to respond_to :land_plane}
+
 end 
