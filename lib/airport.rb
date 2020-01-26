d7b09729f8342a84a2b8bd38b1554e614ae27790
DavidStewartLDN <davidstewart.work@gmail.com>
2020-01-26 12:55:01 +0000

    Step 1 - 3.Create land method with 1 argument

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,3 +1,5 @@
 class Airport
-
+  def land(plane)
+  
+  end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,4 +1,11 @@
 require "airport.rb"
 RSpec.describe Airport do
-
+  describe "land method" do
+    context "exists" do
+      it { is_expected.to respond_to(:land)}
+    end
+    context "can take one argument" do
+      it { is_expected.to respond_to(:land).with(1).argument }
+    end
+  end
 end
