9d9521cabfa0d30276a9a81cdf0a0c14de24c5f2
Student <student@makerss-air.home>
2020-01-26 14:28:50 +0000

    plane change status to fly or landed

--- lib/plane.rb
+++ lib/plane.rb
@@ -1,5 +1,12 @@
 class Plane
-    def in_airport?
-        return true
+    attr_reader :status
+    def initialize
+        @status = "stock"
+    end
+    def land(airport)
+        @status = airport
+    end
+    def fly
+        @status = "flying"
     end
 end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -1,5 +1,20 @@
 require "plane"
+require "airport"
 
 describe Plane do 
-  it { is_expected.to respond_to :in_airport? }
+  it "check if the plane respond to status"
+  expect(subject).to respond_to :status
+  end
+
+  it "check if the plane respond to land"
+  expect(subject).to respond_to :land
+  end
+
+  it "check if the plane respond to land"
+  expect(subject).to respond_to :fly
+  end
+
+  it "check if the plane change to fly"
+  expect(subject.fly).to respond_to "flying"
+  end
 end
