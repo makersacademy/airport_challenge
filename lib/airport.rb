1b720d9297513f991a78d4762b69ac7a3a45e6de
Chris Eadie <chris.eadie@outlook.com>
2020-01-26 18:59:28 +0000

    Raises error if landing when airport full

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,11 +1,27 @@
 require_relative 'plane'
 
 class Airport
+  DEFAULT_CAPACITY = 1
+  attr_reader :planes, :capacity
+  
+  def initialize(capacity = DEFAULT_CAPACITY) 
+    @planes = []
+    @capacity = capacity
+  end
+  
   def land(plane)
-    true
+    fail "Airport is full" if full?
+
+    planes.push(plane)
   end
 
   def takeoff(plane)
-    "Plane has left"
+    "#{plane} has left"
   end
+
+  private
+  def full?
+    @planes.length >= capacity
+  end
+
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,14 +2,21 @@ require 'airport'
 
 describe Airport do
   plane = Plane.new
+  
   context '#land(Plane)' do
     it 'runs' do
-      expect(subject.land(Plane)).to be
+      expect(subject.land(plane)).to be
+    end
+    it 'raises error when airport is full' do
+      airport = Airport.new
+      airport.land(plane)
+      expect { airport.land(plane) }.to raise_error(RuntimeError, "Airport is full")
     end
   end
+
   context '#takeoff(Plane)' do
     it 'advises plane has left' do
-      expect(subject.takeoff(Plane)).to eq("Plane has left")
+      expect(subject.takeoff(plane)).to eq("#{plane} has left")
     end
   end
 end
