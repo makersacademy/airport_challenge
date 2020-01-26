72d710c48bae6856d96fe0ab87ba497b9cddba97
Peter <pc.dean@icloud.com>
2020-01-26 18:12:52 +0000

    planes can only take off from the airport they are at

--- lib/plane.rb
+++ lib/plane.rb
@@ -2,9 +2,21 @@ require 'airport.rb'
 
 class Plane
 
+  attr_reader :location
+
+  def initialize(location = 'sky')
+    @location = location
+  end
+
   def land(airport)
     raise('invalid airport') unless airport.is_a? Airport
+
     'landing'
   end
+
+   def take_off(airport)
+     raise('invalid take off location') unless self.location == airport
+     'taken off'
+   end
 
 end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -15,9 +15,26 @@ describe Plane do
     expect(subject.land(airport)).to eq('landing')
   end
 
-  it 'planes should only land at airports' do
+  it 'Planes should only land at airports' do
     airport = 'not an airport'
-    expect{subject.land(airport)}.to raise_error('invalid airport')
+    expect { subject.land(airport) }.to raise_error('invalid airport')
+  end
+
+  it 'Planes should respond to take_off' do
+    expect(subject).to respond_to(:take_off)
+  end
+
+  it 'Planes should take off from airports' do
+    airport = Airport.new
+    plane = Plane.new(airport)
+    expect(plane.take_off(airport)).to eq('taken off')
+  end
+
+  it 'Planes should only take off from the airport they are at' do
+    airport_1 = Airport.new
+    airport_2 = Airport.new
+    plane = Plane.new(airport_1)
+    expect { plane.take_off(airport_2) }.to raise_error('invalid take off location')
   end
 
 end
