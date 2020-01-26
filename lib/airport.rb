88fac88b70289359d5e3c06faf85a754bab11e77
BarriF13 <info@bdesignstudio.co.uk>
2020-01-26 12:40:05 +0000

    Trying to understand instance var

--- lib/airport.rb
+++ lib/airport.rb
@@ -2,10 +2,16 @@
 require_relative 'plane'
 
 class Airport
+  
+  def plane
+    @plane
+  end
+
   def land_plane
     Plane.new
   end
-  # def take_off_plane
-  #
-  # end
+
+  def take_off_plane(plane)
+    @plane = plane
+  end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,16 +1,24 @@
 require 'airport'
 
-
 describe Airport do
 # this is for the landing_plane method in airport.rb
 
   it { is_expected.to respond_to :land_plane }
-  it { is_expected.tp respond_to :take_off_plane }
+  it { is_expected.to respond_to :take_off_plane }
 
   # it {is_expected.to respond_to(:land).with(1).argument }
 
   it "land a plane" do
     plane = subject.land_plane
     expect(plane).to be_landed
+  end
+  it "take off a Plane" do
+    plane = subject.take_off_plane
+    expect(plane).to be_take_off
+  end
+  it "plane took off confirmed" do
+    plane = Plane.new
+    subject.take_off_plane(plane)
+    expect(subject.plane).to eq plane
   end
 end
