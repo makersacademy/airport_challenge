2e985d7dcbcc131624c881ba2cadbe125b9d15b0
Peter <pc.dean@icloud.com>
2020-01-26 16:45:14 +0000

    initial tests on planes class and created airport files

---
+++ lib/plane.rb
@@ -0,0 +1,7 @@
+class Plane
+
+  def self.land
+
+  end
+
+end
---
+++ spec/plane_spec.rb
@@ -0,0 +1,12 @@
+require 'plane.rb'
+
+describe Plane do
+
+  it 'There should be planes' do
+  end
+
+  it 'Plane should respond to land' do
+    expect(subject).to respond_to(&:land)
+  end
+
+  end
