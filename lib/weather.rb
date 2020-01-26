d3558ded652f6345d88b274a4ecec45a5523734f
DavidStewartLDN <davidstewart.work@gmail.com>
2020-01-26 21:13:58 +0000

    Step 5 - 3. Defined stormy? method to return a value of true or false

--- lib/weather.rb
+++ lib/weather.rb
@@ -1,5 +1,7 @@
 class Weather
   def stormy?
-  
+    x = rand(11)
+    return true if x == 10
+    false
   end
 end
--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -4,5 +4,10 @@ RSpec.describe Weather do
     context "exists" do
       it { is_expected.to respond_to(:stormy?)}
     end
+    context "returns boolean" do
+      it "true or false" do
+        expect(subject.stormy?).to be_(true).or be (false)
+      end
+    end
   end
 end
