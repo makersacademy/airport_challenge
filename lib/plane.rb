5c6d39c72b64b88416c8d43261e1ca55f329c3bd
CodeKrakken <newholm1@hotmail.com>
2020-01-27 14:56:52 +0000

    Cleaning up commit history

--- lib/plane.rb
+++ lib/plane.rb
@@ -7,32 +7,12 @@ class Plane
     @flying = flying
   end
   
-# As an air traffic controller 
-# So I can get passengers to a destination 
-# I want to instruct a plane to land at an airport
-
-# As an air traffic controller 
-# To ensure safety 
-# I want to prevent landing when weather is stormy
-
-# As an air traffic controller 
-# To ensure safety 
-# I want to prevent landing when the airport is full 
-
   def land(airport)
     return "Already landed" if @flying == false
     airport.hangar << self unless airport.stormy == true || 
     airport.hangar.count >= airport.capacity
     @flying = false
   end
-  
-# As an air traffic controller 
-# So I can get passengers on the way to their destination 
-# I want to instruct a plane to take off from an airport and confirm that it is no longer in the airport
-
-# As an air traffic controller 
-# To ensure safety 
-# I want to prevent takeoff when weather is stormy 
 
   def take_off(airport)
     return "Already in flight" if @flying == true
@@ -47,8 +27,3 @@ class Plane
   end
 
 end
-
-
-# As the system designer
-# So that the software can be used for many different airports
-# I would like a default airport capacity that can be overridden as appropriate
