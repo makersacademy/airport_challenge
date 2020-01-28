6eae025ad382efed3f5acdae56559d8fba595b31
Peter <pc.dean@icloud.com>
2020-01-28 12:15:28 +0000

    refactored

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,20 +1,23 @@
 require 'airport.rb'
 require 'plane.rb'
 
+
+
 describe Airport do
 
+  before(:each) do
+    @airport = Airport.new
+    allow(@airport).to receive(:weather) { 'sunny' }
+    @plane = Plane.new
+    @plane.land(@airport)
+  end
+
   it 'Landing a plane should result in num_of_plane increase by 1' do
-    allow(subject).to receive(:weather) { 'sunny' }
-    plane = Plane.new
-    plane.land(subject)
-    expect(subject.num_of_planes).to eq(1)
+    expect(@airport.num_of_planes).to eq(1)
   end
 
   it 'Take off should result in num_of_plane reduce by one' do
-    allow(subject).to receive(:weather) { 'sunny' }
-    plane = Plane.new
-    plane.land(subject)
-    plane.take_off(subject)
+    @plane.take_off(@airport)
     expect(subject.num_of_planes).to eq(0)
   end
 
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -3,10 +3,14 @@ require 'airport.rb'
 
 describe Plane do
 
+  before (:each) do
+    @airport = Airport.new
+    allow(@airport).to receive(:weather) { 'sunny' }
+    @plane = Plane.new
+  end
+
   it 'Planes should land at airports' do
-    airport = Airport.new
-    allow(airport).to receive(:weather) { 'sunny' }
-    expect(subject.land(airport)).to eq('landing')
+    expect(@plane.land(@airport)).to eq('landing')
   end
 
   it 'Planes should only land at airports' do
@@ -15,19 +19,15 @@ describe Plane do
   end
 
   it 'Planes should take off from airports and return new location (sky)' do
-    airport = Airport.new
-    allow(airport).to receive(:weather) { 'sunny' }
-    plane = Plane.new(airport)
-    expect(plane.take_off(airport)).to eq('sky')
+    plane = Plane.new(@airport)
+    expect(plane.take_off(@airport)).to eq('sky')
   end
 
   it 'Planes should only take off from the airport they are at' do
     airport_1 = Airport.new
-    airport_2 = Airport.new
-    allow(airport_1).to receive(:weather) { 'sunny' }
-    allow(airport_2).to receive(:weather) { 'sunny' }
-    plane = Plane.new(airport_1)
-    expect { plane.take_off(airport_2) }.to raise_error('invalid take off location')
+    # allow(airport_1).to receive(:weather) { 'sunny' }
+    @plane.land(@airport)
+    expect { @plane.take_off(airport_1) }.to raise_error('invalid take off location')
   end
 
   it 'Planes can not take off if they are already in the sky' do
@@ -37,27 +37,23 @@ describe Plane do
   it 'Planes can not land if the airport is full' do
     airport = Airport.new(0)
     allow(airport).to receive(:weather) { 'sunny' }
-    expect { subject.land(airport) }.to raise_error('airport is full')
+    expect { @plane.land(airport) }.to raise_error('airport is full')
   end
 
   it 'Planes can not land if not in sky' do
-    airport = Airport.new
-    plane = Plane.new(airport)
-    allow(airport).to receive(:weather) { 'sunny' }
-    expect { plane.land(airport) }.to raise_error('plane already on land')
+    @plane.land(@airport)
+    expect { @plane.land(@airport) }.to raise_error('plane already on land')
   end
 
   it 'Plane.take_off should raise_error if weather is stormy' do
-    airport = Airport.new
-    allow(airport).to receive(:weather) { 'stormy' }
-    plane = Plane.new(airport)
-    expect { plane.take_off(airport) }.to raise_error('stormy weather')
+    @plane.land(@airport)
+    allow(@airport).to receive(:weather) { 'stormy' }
+    expect { @plane.take_off(@airport) }.to raise_error('stormy weather')
   end
 
   it 'Plane.landing should raise_error if weather is stormy' do
-    airport = Airport.new
-    allow(airport).to receive(:weather) { 'stormy' }
-    expect { subject.land(airport) }.to raise_error('stormy weather')
+    allow(@airport).to receive(:weather) { 'stormy' }
+    expect { @plane.land(@airport) }.to raise_error('stormy weather')
   end
 
 end
