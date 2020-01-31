7d801abe0e0021a07864f8603b393923f3ab461b
Rhodine <rorleans_lindsay@yahoo.co.uk>
2020-01-31 11:41:35 +0000

    another failing test because stub has not been called.

--- JavaScriptAirport/spec/airportSpec.js
+++ JavaScriptAirport/spec/airportSpec.js
@@ -1,11 +1,17 @@
 'use strict'
 describe('Airport', function(){
   var airport;
+  var plane;
   beforeEach(function(){
     airport = new Airport();
+    plane = jasmine.createSpyObj('plane', ['clearForLanding']);
   });
-  it('can land at airport', function() {
+  it('allows plane to land at airport', function() {
     expect(airport.land).not.toBeUndefined()
+  });
+  it('planes can land at airport', function(){
+    airport.land(plane);
+    expect(plane.clearForLanding).toHaveBeenCalledWith(airport);
   });
   it('has no planes by default', function(){
     expect(airport.planes()).toEqual([]);
