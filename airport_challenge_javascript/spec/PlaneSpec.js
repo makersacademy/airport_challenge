6632a047b3e92f7c97b802a4e364d55f4fbd87df
Carmen-Alina <carmen.panu@gmail.com>
2020-01-31 13:37:51 +0000

    User Story 2 - unit test for Plane object

--- airport_challenge_javascript/spec/PlaneSpec.js
+++ airport_challenge_javascript/spec/PlaneSpec.js
@@ -13,4 +13,9 @@ describe('Plane', function() {
     plane.land(airport);
     expect(airport.clearForLanding).toHaveBeenCalledWith(plane);
   });
+  it('can takeoff from an airport', function() {
+    plane.land(airport);
+    plane.takeoff();
+    expect(airport.clearForTakeOff).toHaveBeenCalled();
+  });
 });
