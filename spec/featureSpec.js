4fd9d23042e9b37262c2c3d19b0c529ede767f91
Samantha Dalzell Matthew <samidm13@gmail.com>
2020-01-31 10:57:46 +0000

    first test for instructing plane to land written

---
+++ spec/featureSpec.js
@@ -0,0 +1,17 @@
+'use strict'
+
+describe('Feature Test', function() {
+  var plane
+  var airport
+
+  beforeEach(function() {
+    plane = new Plane;
+    airport = new Airport;
+
+  });
+
+  it('instructs planes to land at airport', function() {
+    plane.land(airport);
+    expect(airport.plane()).toContain(plane)
+  });
+});
