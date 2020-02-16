c9ad4372d333a26ac05ddabb4760a793221743a5
Alex <aclifford336@gmail.com>
2020-02-16 11:35:44 +0000

    refactored test

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -10,7 +10,7 @@ describe Plane do
   end
 
   describe '#in_flight' do
-    it 'should show airborne flase when landed' do
+    it 'should show airborne true when in flight' do
       subject.in_flight
       expect(subject.airborne).to eq(true)
     end
