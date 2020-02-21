525d41a5b72965df6d62749390ea70bc32efb011
Jo Redfern <mrsjoredfern@yahoo.co.uk>
2020-02-21 12:07:16 +0000

    airport full and empty guard clauses added

--- lib/airport.rb
+++ lib/airport.rb
@@ -3,9 +3,15 @@ class Airport
     @plane = 0
   end
   def land(plane)
+    if full?
+      raise "This airport is full, you can't land here!"
+    end
     @plane += 1
   end
   def take_off(plane)
+    if empty?
+      raise "There are no planes to take off!"
+    end
     @plane -=1
   end
 
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -15,4 +15,18 @@ describe Airport do
     #act/assert
     expect(airport.take_off(plane)).to eq(0)
   end
+  it "doesn't allow plane to land if airport is full" do
+    #arrange = we have an airport, we have landed #capacity planes
+    airport = Airport.new
+    plane = Plane.new
+    20.times {airport.land(plane)}
+    #act/assert
+    expect{airport.land(plane)}.to raise_error("This airport is full, you can't land here!")
+  end
+  it "doesn't allow plane to take off if plane doesn't exist" do
+    #arrange = we have an airport, we have a plane, we haven't landed it
+    airport = Airport.new
+    plane = Plane.new
+    expect{airport.take_off(plane)}.to raise_error("There are no planes to take off!")
+  end
 end
