83d559823dcc117ac73842ace2638c83e00e8912
Sophia Bell <Kittaru87@gmail.com>
2020-02-21 13:36:36 +0000

    new directory for rb files

---
+++ lib/airport.rb
@@ -0,0 +1,25 @@
+require 'plane'
+
+class Airport
+  
+  attr_reader :hanger, :capacity
+  
+  CAPACITY = 20
+
+  def initialize(capacity=CAPACITY)
+    @hanger = []
+    @capacity = capacity
+  end
+  
+  def land(plane)
+    @hanger << plane
+  end
+
+  def takeoff
+    @hanger.pop && (puts "A plane has left the airport\n")
+  end
+
+
+
+  
+end
