7915ac88a7d3a7b0258e2cb3d9ec2ad0ad44d0e9
Jo Redfern <mrsjoredfern@yahoo.co.uk>
2020-02-21 15:32:17 +0000

    tidied up based on rubocop

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,27 +1,49 @@
+=begin
+I needed quite a bit of help from Tatsiana setting up the environment,
+managed to install new version of ruby ok but needed to update bundler too
+which I didn't pick up from the error message
+got there in the end though :-
+=end
 class Airport
   DEFAULT_CAPACITY = 5
   attr_reader :plane
   attr_accessor :capacity
+
   def initialize(capacity = DEFAULT_CAPACITY)
     @plane = 0
     @capacity = capacity
   end
+
   def land(plane)
+    if stormy? == true
+      raise "Weather is stormy, you cannot land here"
+    end
     if full?
       raise "This airport is full, you can't land here!"
     end
+
     @plane += 1
   end
+
   def take_off(plane)
+    if stormy? == true
+      raise "Weather is stormy, you cannot take off"
+    end
     if empty?
       raise "There are no planes to take off!"
     end
-    @plane -=1
+
+    @plane -= 1
   end
+
+  # Please note for "stormy?" method below I haven't worked out how to use doubles successfully yet,
+  # but I tested all code in repl.it and it works there, and I tested the below with
+  # just "return true" and "return false", and the correct tests seemed to pass/fail each time
   def stormy?
-    if rand(3) == 0
+    if rand(3).zero?
       return true
     end
+
     return false
   end
 
@@ -30,15 +52,17 @@ private
     if @plane == capacity
       return true
     end
-      return false
-  end
-  def empty?
-    if @plane == 0
-      return true
-    end
+
     return false
   end
 
+  def empty?
+    if @plane.zero?
+      return true
+    end
+    
+    return false
+  end
 end
 
 class Plane
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,57 +1,48 @@
 require "airport"
 describe Airport do
-  #I have assumed that an airport is created with no planes
   it "allows plane to land" do
-    #there is an airport, there is a plane
     airport = Airport.new
     plane = Plane.new
-    #act/assert
     expect(airport.land(plane)).to eq(1)
   end
   it "allows plane to take off" do
-    #arrange = there is an airport, there is a plane, a plane has landed at the airport
     airport = Airport.new
     plane = Plane.new
     airport.stormy? == false
     airport.land(plane)
-    #act/assert
     expect(airport.take_off(plane)).to eq(0)
   end
   it "doesn't allow plane to land if airport is full" do
-    #arrange = there is an airport, #capacity planes have landed
     airport = Airport.new
     plane = Plane.new
     airport.stormy? == false
-    airport.capacity.times {airport.land(plane)}
-    #act/assert
-    expect{airport.land(plane)}.to raise_error("This airport is full, you can't land here!")
+    airport.capacity.times { airport.land(plane) }
+    expect { airport.land(plane) }.to raise_error("This airport is full, you can't land here!")
   end
   it "doesn't allow plane to take off if plane doesn't exist" do
-    #arrange = there is an airport, there is a plane, no planes have landed at the airport
     airport = Airport.new
     plane = Plane.new
     airport.stormy? == false
-    expect{airport.take_off(plane)}.to raise_error("There are no planes to take off!")
+    expect { airport.take_off(plane) }.to raise_error("There are no planes to take off!")
   end
   it "allows airport to be created with over-writeable default capacity" do
-    #arrange
     airport = Airport.new(7)
-    #act/assert
     expect(airport.capacity).to eq(7)
   end
   it "allows for a weather condition to be created" do
-    #arrange
     airport = Airport.new
-    plane = Plane.new
-    #act/assert
     expect(airport.stormy?).to be(true).or be(false)
   end
   it "doesn't allow planes to land if weather is stormy" do
-    #arrange
     airport = Airport.new
     plane = Plane.new
     airport.stormy? == true
-    #act / assert
-    expect{airport.land(plane)}.to raise_error("Weather is stormy, you cannot land here")
+    expect { airport.land(plane) }.to raise_error("Weather is stormy, you cannot land here")
+  end
+  it "doesn't allow planes to take off if weather is stormy" do
+    airport = Airport.new
+    plane = Plane.new
+    airport.stormy? == true
+    expect { airport.take_off(plane) }.to raise_error("Weather is stormy, you cannot take off")
   end
 end
