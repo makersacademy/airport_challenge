3beb24cf8a95e6600b6e4b7b54e64237f8624daf
Student <student@Makerss-MacBook-Air-2.local>
2020-02-21 16:02:48 +0000

    plane lands

---
+++ spec/feature/user_stories_spec.rb
@@ -0,0 +1,7 @@
+describe 'User Stories' do
+  it 'so planes land at airports, instruct a plane to land' do
+    airport = Airport.new
+    plane = Plane.new
+    expect { airport.land(plane) }.not_to raise_error
+  end
+end
