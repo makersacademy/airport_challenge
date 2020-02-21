98e77b3d240f0bc2ed9d874ede4e4b850eab31f8
FunmiBADev <funmiba@hotmail.com>
2020-02-21 15:08:56 +0000

    FAIL: NoMethodError for airport.plane

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -5,6 +5,10 @@ describe Airport do
 		it { is_expected.to respond_to(:plane_land).with(1).argument } 
 	end
 
+	context "records that plane has landed" do 
+		it { is_expected.to respond_to :plane }
+	end
+
 	context "allows plane to take off" do
 		it { is_expected.to respond_to :plane_take_off }
 	end
