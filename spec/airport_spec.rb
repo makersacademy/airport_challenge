f59ef93a970dfec291244459a647eec2cc7526b3
LXie-Saintex <admin@saintex.co.uk>
2020-02-21 11:36:40 +0000

    added test on plane doubles

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -5,13 +5,14 @@ describe Airport do
 	it { is_expected.to respond_to(:capacity) }
 	it 'instructs plane to take off' do 
 		airport = Airport.new
-		plane = double("Plane", :lands => true, :takes_off => true)
+		plane = double("Plane", :landed => true)
+		plane.stub(:takes_off).and_return(true)
 		airport.fly_plane(plane)
 		expect(airport.planes).not_to include(plane)
 	end
 	it "instructs plane to land" do
 		airport = Airport.new
-		plane = double("Plane", :lands => true)
+		plane = double("Plane", :landed => true)
 		airport.land_plane(plane)
 		expect(airport.planes).to include(plane)
 	end
@@ -20,3 +21,12 @@ describe Airport do
 	it "prevents plane to land" do 
 	end
 end
+
+describe 'plane_doubles' do 
+	it "expects the message 'takes_off' and return true" do
+		plane = double("Plane", :landed => true)
+		plane.stub(:takes_off).and_return(true)
+		plane.takes_off.should eq(true)
+	end
+end
+
