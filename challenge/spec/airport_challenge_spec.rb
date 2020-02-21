2dc181883bacc9753259941e4d8a05274b097e57
Hodan <hodanahmed0000@gmail.com>
2020-02-21 11:18:22 +0000

    Created passing test & method for landing a plane

--- challenge/spec/airport_challenge_spec.rb
+++ challenge/spec/airport_challenge_spec.rb
@@ -1,13 +1,13 @@
 require 'airport_challenge'
 
 describe 'Airport' do
-
 it 'can create an instance of class Airport' do
   jfk_airport = Airport.new
   expect(jfk_airport).to be_a Airport
 end
-end
-
-describe '#land plane' do
-it {is_expected.to respond_to(:land)}
+  it 'can land a plane' do
+    jfk_airport = Airport.new
+    jfk_airport.land
+  expect(jfk_airport).to respond_to(:land)
+  end
 end
