3ec2a3b4e3dc6cf255091b07e71df2b666d5f018
LXie-Saintex <admin@saintex.co.uk>
2020-02-21 10:50:07 +0000

    added test files

---
+++ spec/airport_spec.rb
@@ -0,0 +1,12 @@
+require 'airport'
+
+describe Airport do 
+	it 'instructs plane to take off' do 
+	end
+	it "instructs plane to land" do
+	end
+	it "prevents plane to take off" do
+	end
+	it "prevents plane to land" do 
+	end
+end
---
+++ spec/plane_spec.rb
@@ -0,0 +1,8 @@
+require "plane"
+
+describe Plane do 
+	it "takes off" do 
+	end
+	it "lands" do
+	end
+end
