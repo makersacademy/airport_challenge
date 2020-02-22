30f27afd5dc9c417023c2a7cf56cd58451143b4a
Sophia Bell <Kittaru87@gmail.com>
2020-02-22 16:13:33 +0000

    adding stormy weather exceptions to land and takeoff methods

--- lib/airport.rb
+++ lib/airport.rb
@@ -13,19 +13,24 @@ class Airport
   
   def land(plane)
     raise "airport full" if full?
+    raise "It's too stormy to land" if stormy?
     @hanger << plane
   end
 
   def takeoff
+    raise "It's too stormy to take off" if stormy?
     @hanger.pop && (puts "A plane has left the airport\n")
   end
 
 
   private
-  
+
   def full?
-    @hanger.length >= @capacity ? true : false
+    @hanger.length >= @capacity
   end
 
+  def stormy?
+    rand(0..100) > 90
+  end
   
 end
