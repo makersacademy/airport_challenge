81f2705c51cb7afbd30f2d7761314e22bf0c67a0
plkujaw <jawor.jakub@gmail.com>
2020-02-22 14:44:54 +0000

    fixed test for plane.take_off

--- lib/plane.rb
+++ lib/plane.rb
@@ -3,8 +3,13 @@ class Plane
   attr_reader :location
   attr_reader :status
 
-  def initialize(location = "in air", status = "flying")
+  def initialize(location = "airport", status = "landed")
     @location = location
     @status = status
+  end
+
+  def take_off(airport)
+    @location = "in the air"
+    @status = "flying"
   end
 end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -1,11 +1,20 @@
 require "plane"
 
 describe Plane do
+
   describe "#take_off" do
     it "Changes plane's status to 'flying'" do
       plane = Plane.new
+      airport = Airport.new
+      subject.take_off(airport)
       expect(subject.status).to eq("flying")
+    end
 
+    it "Changes plane's location to 'in the air'" do
+      plane = Plane.new
+      airport = Airport.new
+      subject.take_off(airport)
+      expect(subject.location).to eq("in the air")
     end
   end
 
