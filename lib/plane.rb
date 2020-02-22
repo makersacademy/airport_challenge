f3688acbcbc3c4f26be69c662dde3c56686a590c
plkujaw <jawor.jakub@gmail.com>
2020-02-22 14:35:53 +0000

    added and passed a test to change plane's status to flying after take off from an airport

--- lib/plane.rb
+++ lib/plane.rb
@@ -1,3 +1,10 @@
 class Plane
 
+  attr_reader :location
+  attr_reader :status
+
+  def initialize(location = "in air", status = "flying")
+    @location = location
+    @status = status
+  end
 end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -1,1 +1,12 @@
 require "plane"
+
+describe Plane do
+  describe "#take_off" do
+    it "Changes plane's status to 'flying'" do
+      plane = Plane.new
+      expect(subject.status).to eq("flying")
+
+    end
+  end
+
+end
