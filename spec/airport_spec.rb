29f62fedcdfe05c3dad5d4639a2388397e684a1d
naeglinghaff <kate.o.haffenden@gmail.com>
2020-02-22 11:51:04 +0000

    Refactored unit tests

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -7,18 +7,6 @@ describe Airport do
     @airport = Airport.new
   end
 
-  describe '#land' do
-    it 'lets a plane land' do
-      expect {@plane.land}.not_to raise_error
-    end
-  end
-
-  describe '#take_off' do
-    it 'lets a plane take off from the airport' do
-      expect {@plane.take_off}.not_to raise_error
-    end
-  end
-
   describe '#empty' do
     it 'lets the controller check that a plane has left the airport' do
       @plane.take_off
---
+++ spec/plane_spec.rb
@@ -0,0 +1,12 @@
+require "airport"
+
+describe Plane do
+
+  describe '#land' do
+    it { is_expected.to respond_to(:land) }
+  end
+
+  describe '#take_off' do
+    it { is_expected.to respond_to(:take_off) }
+  end
+end
