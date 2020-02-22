35e943ce21bd8c7ca5f48dd44b3dbfcf44a35c69
Student <student@makerss-air.home>
2020-02-22 12:21:40 +0000

    added airport capacity and Plane class

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,10 +1,24 @@
+require_relative 'plane'
+
 class Airport
 
-  def land(plane)
+  attr_reader :planes, :capacity
+  DEFAULT_CAPACITY = 10
+
+  def initialize(capacity=DEFAULT_CAPACITY)
+    @capacity = capacity
+    @planes = Array.new
   end
 
-  def take_off(plane)
-    "#{plane} is no longer in the airport"
+  def land(plane)
+    # not passing SRP atm
+    raise "Airport has reached capacity: Cannot land plane" if @planes.size == @capacity
+    @planes.push(plane)
+  end
+
+  def take_off
+    "#{@planes[-1]} is no longer in the airport"
+    @planes.pop
   end
 
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -6,8 +6,20 @@ describe Airport do
 
   end
 
+  it "has a default capacity" do
+    expect(subject.capacity).to eq Airport::DEFAULT_CAPACITY
+  end
+  it "capacity can be overridden if appropriate" do
+    airport = Airport.new 15
+    expect(airport.capacity).to eq 15
+  end
+
   describe '#land' do
     it { is_expected.to respond_to(:land).with(1).argument }
+    it "prevents landing when the airport if full" do
+      subject.capacity.times { subject.land Plane.new }
+      expect { subject.land Plane.new }.to raise_error "Airport has reached capacity: Cannot land plane"
+    end
   end
 
   describe '#take_off' do
