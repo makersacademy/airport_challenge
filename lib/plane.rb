704855f2138c8a3266954563d4cce8b2317d233a
Sophia Bell <Kittaru87@gmail.com>
2020-02-22 17:32:45 +0000

    adding location identifiers

--- lib/plane.rb
+++ lib/plane.rb
@@ -1,7 +1,24 @@
+require_relative 'airport'
 
 class Plane
+    
+attr_reader :location
 
+def initialize
+  @location = :sky
 
+end
 
+def flying?
+  
+end
+
+def landed
+  @location = :airport
+end
+
+def flying
+  @location = :sky
+end
 
 end
