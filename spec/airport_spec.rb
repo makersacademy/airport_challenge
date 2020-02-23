a3a8618b9f579fb37e9f5d2173c92b8b91d0ff2a
Jara Santamaria <jarasmar@gmail.com>
2020-02-23 14:25:16 +0000

    redefine test namesto be more descriptive

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -10,36 +10,36 @@ describe Airport do
     expect(subject).to respond_to(:take_off)
   end
 
-  it "confirms that a plane has take_off" do
+  it "confirms that a plane has #take_off" do
     allow(subject).to receive(:storm?) { false } # overrides storm true
     subject.land(Plane.new)
     expect { subject.take_off }.to output("Plane has taken off").to_stdout
   end
 
-  it "raises error if #land and airport is over default capacity of 100 planes" do
-    allow(subject).to receive(:storm?) { false }
+  it "raises error if #land and airport is full" do
+    allow(subject).to receive(:storm?) { false } # overrides storm true
 
     Airport::DEFAULT_CAPACITY.times { subject.land(Plane.new) }
     expect { subject.land(Plane.new) }.to raise_error("Airport is full")
   end
 
-  it "raises an error if #take_off and there is no planes" do
+  it "raises an error if #take_off and airport is empty" do
     allow(subject).to receive(:storm?) { false }
     expect { subject.take_off }.to raise_error("Airport is empty")
   end
 
-  it "creates a storm randomly" do
+  it "defines weather to randomly be #storm?" do
     expect(subject).to receive(:rand).and_return(7)
     expect(subject.storm?).to eq true
   end
 
-  it "raises an error if #take_off and #storm" do
+  it "raises an error if #take_off and #storm?" do
     allow(subject).to receive(:empty?) { false }
     allow(subject).to receive(:storm?) { true }
     expect { subject.take_off }.to raise_error("There is a storm")
   end
 
-  it "raises an error if #land and #storm" do
+  it "raises an error if #land and #storm?" do
     allow(subject).to receive(:full?) { false }
     allow(subject).to receive(:storm?) { true }
     expect { subject.land(Plane.new) }.to raise_error("There is a storm")
