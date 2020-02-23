8dcc898f044d85bc13ccd453b527c88e0215fb04
Ben Galley <bengalley21@gmail.com>
2020-02-23 20:12:03 +0000

    added a weather class and method stormy?

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,11 +1,15 @@
 require_relative 'plane'
 
+# DEFAULT_CAPACITY = 10
+
 class Airport
-  def initialize
+  def initialize(capacity = 10)
     @planes = []
+    @capacity = capacity
   end
 
   def land_plane(plane)
+    raise "There is no space!" if full?
     @planes << plane
   end
 
@@ -14,6 +18,22 @@ class Airport
   end
 
   def take_off(plane)
-    @planes.delete(plane)  
+  raise "There are no planes!" if empty?
+    @planes.delete(plane)
   end
+
+  private
+
+  def full?
+    if @planes.count >= 10
+      return true
+    end
+  end
+
+  def empty?
+    if @planes.count == 0
+      return true
+    end
+  end
+
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,10 +1,14 @@
 require 'airport'
 require 'plane'
+require 'weather'
 
 describe Airport do
 
+
+
   let(:airport) { described_class.new }
   let(:plane) { Plane.new }
+  let(:weather) { Weather.new }
 
   context '#land_plane'
 
@@ -15,17 +19,29 @@ describe Airport do
       expect{airport.land_plane(plane)}.to change{airport.plane_count}.by 1
     end
 
+    it "raises an error if airport is full" do
+      10.times {airport.land_plane(plane)}
+      expect{airport.land_plane(plane)}.to raise_error("There is no space!")
+    end
+
+    it "raises an error if weather is stormy" do
+      expect{airport.land_plane(plane)}.to raise_error("Too stormy to land!") if weather.stormy?
+    end
+
+
 
 
   context '#take_off'
 
     it "allows planes to take off" do
-      plane.take_off
-      expect{airport.take_off(plane)}.not_to raise_error
+      airport.land_plane(plane)
+      expect{airport.take_off(plane)}.to change{airport.plane_count}.by -1
     end
 
-    # it "gives a message confirming the plane has taken off"
-    #   airport = Airport.new
-    #   plane = Plane.new
-    #   expect(airport.take_off).to eq ("the plane took off")
+    it "raises and error if there are no planes" do
+      expect{airport.take_off(plane)}.to raise_error "There are no planes!"
+    end
+
+    
+
 end
