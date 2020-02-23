7a918f7a44e2a30ab8ea76adbd0d71a133704b92
Peter Stevens <Peterstevens510@gmail.com>
2020-02-23 13:01:44 +0000

    added changes

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,24 +2,25 @@ require 'airport.rb'
 require 'plane.rb'
 
 describe Airport do 
-  subject(:airport) { described_class.new }
-  #subject(:plane) { described_class.new }
+  let(:airport) { described_class.new } # Creates a new instance of the airport class which can be used throughout the entire block
+  let(:plane) { Plane.new }
 
-  #it { is_expected.to respond_to(:land).with(1).argument } 
-  it { expect(subject).to respond_to(:land).with(1).argument } 
+  it "creates attributes capacity and hangar" do
+    expect(subject).to have_attributes(hangar: [], capacity: 10)
+  end
+
+  it "pushes planes into the hangar array" do 
+    expect { subject.land(plane) }.to change { subject.hangar.size }.by(1)
+  end
+
   
 
-  it 'instructs a plane to take off from the airport"' do
+  it { expect(subject).to respond_to(:land).with(1).argument } 
+  
+  it "instructs a plane to take off from the airport" do
     expect(subject).to respond_to(:take_off)
   end
 
-
-   
-
-  
-  
-    #it 'instructs a plane to take off from the airport' do 
-    #expect { subject.take_off(plane).to respond_to :take_off }
 
 end
 
