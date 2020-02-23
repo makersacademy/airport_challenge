55dbe830858bbaea433ba9cdcaade3876c3174d3
Peter Stevens <Peterstevens510@gmail.com>
2020-02-23 23:13:03 +0000

    updated files

--- Lib/airport.rb
+++ Lib/airport.rb
@@ -2,17 +2,21 @@ require_relative "./plane.rb"
 
 class Airport
   attr_reader(:capacity, :hangar)
-  
-  def initialize
-    @capacity = 10
+  DEFAULT_CAPACITY = 10
+  def initialize(capacity = DEFAULT_CAPACITY)
+    @capacity = capacity
     @hangar = []
   end 
 
   def land(plane)
+    raise "Plane cannot land: airport full" if @hangar.count >= @capacity
     @hangar.push(plane)
   end
 
   def take_off(plane)
-    "No longer in the airport"
+    
+    @hangar.delete(plane)
   end
 end
+
+
