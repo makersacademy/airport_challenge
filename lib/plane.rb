3d65fb4fca7a6d97602c2f3f08ff02667bc312a7
lilawalker <lilawlker@gmail.com>
2020-02-23 21:21:40 +0000

    Add method to plane class to land plane

--- lib/plane.rb
+++ lib/plane.rb
@@ -1,2 +1,15 @@
+require_relative 'airport'
+
 class Plane
+
+  attr_reader :location
+
+  def land
+    @location = :landed
+  end
+
+  def landed
+    @location == :landed
+  end
+
 end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -1,4 +1,13 @@
 require 'plane.rb'
 
 describe Plane do 
+
+  it { is_expected.to respond_to(:land) } 
+
+  describe '#land' do
+    it 'lands a plane' do
+      expect(subject.land).to eq(:landed)
+    end
+  end
+
 end
