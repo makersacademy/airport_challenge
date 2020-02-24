295abd2f904b8ebd9259e1001be0baa8eca414f4
George Swarbrick <glswarbrick@hotmail.co.uk>
2020-02-24 12:48:59 +0000

    Completed tests for plane, weather and airport. Need to re write airport.rb to fit tests

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -5,7 +5,26 @@ describe Plane do
 
   describe "#land" do
     it "lands a plane" do
-        expect(subject.land).to eq(:landed)
+      expect(subject.land).to eq(:landed)
     end
-end
+
+    it "raises an error if the plane has already landed" do
+      subject.land
+      expect { subject.land }.to raise_error "Planes already here boss"
+    end
+  end
+
+  it { is_expected.to respond_to(:take_off) }
+
+  describe "#take_off" do
+    it "allows the plane to take off" do
+      expect(subject.take_off). to eq(:flying)
+    end
+
+    it "raises and error if plane is already in the air" do
+      subject.land
+      subject.take_off
+      expect { subject.take_off }.to raise_error "Planes already in the sky boss"
+    end
+  end
 end
