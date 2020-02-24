e94b0d881b46ed27b1c272b72556f7d0875a1c91
dariathompson <daria.kambalina@gmail.com>
2020-02-24 10:52:47 +0000

    added comment for weather

--- lib/weather.rb
+++ lib/weather.rb
@@ -1,6 +1,7 @@
 require_relative 'airport'
 require_relative 'plane'
 class Weather
+  # get random number up to 5 and if it is 5 weather stormy? returns true
   def stormy?
     rand(6) == 5
   end
