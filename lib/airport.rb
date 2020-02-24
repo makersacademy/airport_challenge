f6d7fa62878c3a8da06be251b52796365804c9a5
Olivia Thetgyi <othetgyi@gmail.com>
2020-02-24 09:05:29 +0000

    Airport challenge submission

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,9 +1,22 @@
 require_relative 'plane'
 
 class Airport
-
-  def land(plane)
-
+  def initialize
+    @total_planes = 0
   end
 
+  def land(plane)
+    @total_planes += 1
+    @location = "airport"
+  end
+
+  def number_of_planes
+    @total_planes
+  end
+
+  def takeoff(plane)
+    @plane
+    @total_planes -= 1
+    @location = "sky"
+  end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,8 +1,42 @@
 require 'airport'
 
 describe Airport do
-  it 'allows plans to land' do
-    expect(Airport.new).to respond_to(:land).with(1).argument
+
+  describe '#land' do
+    it 'instructs plane to land' do
+      expect(Airport.new).to respond_to(:land).with(1).argument
+    end
+
+    it 'adds one to the total number of planes when planes land' do
+      airport = Airport.new
+      plane = Plane.new
+      expect{airport.land(plane)}.to change{airport.number_of_planes}.by(1)
+    end
+
+    it 'sets the planes position to airport' do
+      airport = Airport.new
+      plane = Plane.new
+      allow(airport).to receive(:land).and_return("airport")
+      expect(airport.land).to eq("airport")
+    end
   end
 
+  describe '#takeoff' do
+    it "instructs plane to takeoff" do
+      expect(Airport.new).to respond_to(:takeoff).with(1).argument
+    end
+
+    it "subtracts by one the total number of planes when a plane takes off" do
+      airport = Airport.new
+      plane = Plane.new
+      expect{airport.takeoff(plane)}.to change{airport.number_of_planes}.by(-1)
+    end
+
+    it 'sets the planes position to sky' do
+      airport = Airport.new
+      plane = Plane.new
+      allow(airport).to receive(:takeoff).and_return("sky")
+      expect(airport.takeoff).to eq("sky")
+    end
+end
 end
