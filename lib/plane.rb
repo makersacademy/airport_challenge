de5b17e0d437b68b53af68c88779e371411d0997
ian-1 <ian.walton@portmoor.com>
2020-03-21 23:16:30 +0000

    add #land to Plane

--- lib/plane.rb
+++ lib/plane.rb
@@ -1,4 +1,5 @@
-
 class Plane
+  def land
 
+  end
 end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -1,5 +1,7 @@
 require './lib/plane'
 
 describe Plane do
-
+  it "check responds to #land" do
+    expect(Plane.new).to respond_to(:land)
+  end
 end
