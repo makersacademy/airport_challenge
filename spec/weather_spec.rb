d1e6e07e22c7e9e0b056e4fdef59d35c32189c54
Phil Vigus <philipvigus@yahoo.co.uk>
2020-03-21 12:32:49 +0000

    Change current_weather to a class method
    Doing this means we don't need to instantiate an instance of
    Weather to get the current weather.

--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -2,10 +2,10 @@ require 'weather'
 
 describe Weather do
   it 'allows you to ask what the weather is like at the moment' do
-    expect(subject).to respond_to(:current_weather)  
+    expect(Weather).to respond_to(:current_weather)  
   end
 
   it 'should tell you it is either stormy or clear' do
-    expect(subject.current_weather).to eq(:stormy).or eq(:clear)
+    expect(Weather.current_weather).to eq(:stormy).or eq(:clear)
   end
 end
