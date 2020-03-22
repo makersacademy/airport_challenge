e0dc4e8e7d895f1d27d311424fa43b19c319794b
Gina Frankel <g.frankel22@gmail.com>
2020-03-22 17:30:08 +0000

    tested to see if airport.plane_landing' would return an instance of Plane class, then changed 'airport.rb' to pass test

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,6 +1,9 @@
-require 'plane'
+require './lib/plane'
 
 class Airport
   def plane_landing
+    Plane.new
+  end
+  def take_off
   end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,12 +2,16 @@ require 'airport'
 
 describe Airport do
   subject(:airport) {described_class.new}
-  let(:plane) {Plane.new}
+  #let(:plane) {Plane.new}
 
   describe "#plane_landing" do
-    it "plane lands at airport" do
+    it "airport responds to plane_landing" do
       expect(airport).to respond_to :plane_landing
     end
+    it "plane lands at airport" do
+       expect(airport.plane_landing).to be_an_instance_of(Plane)
+    end
   end
+
 
 end
