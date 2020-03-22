7d829d89a9194ca448348fcabade41ab47ed2e3d
ian-1 <ian.walton@portmoor.com>
2020-03-22 13:55:13 +0000

    seperate method tests

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,11 +1,15 @@
 require './lib/airport'
 
 describe Airport do
-  it "check responds to #land(plane)" do
-    expect(Airport.new).to respond_to(:land).with(1).argument
+  describe "#land" do
+    it "check responds to #land(plane)" do
+      expect(Airport.new).to respond_to(:land).with(1).argument
+    end
   end
 
-  it "check responds to #take_off(plane)" do
-    expect(Airport.new).to respond_to(:take_off).with(1).argument
+  describe "#take_off" do
+    it "check responds to #take_off(plane)" do
+      expect(Airport.new).to respond_to(:take_off).with(1).argument
+    end
   end
 end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -1,11 +1,15 @@
 require './lib/plane'
 
 describe Plane do
-  it "check responds to #land(airport)" do
-    expect(Plane.new).to respond_to(:land).with(1).argument
+  describe "#land" do
+    it "check responds to #land(airport)" do
+      expect(Plane.new).to respond_to(:land).with(1).argument
+    end
   end
 
-  it "check responds to #take_off(airport)" do
-    expect(Plane.new).to respond_to(:take_off).with(1).argument
+  describe "#take_off" do
+    it "check responds to #take_off(airport)" do
+      expect(Plane.new).to respond_to(:take_off).with(1).argument
+    end
   end
 end
