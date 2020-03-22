c07bc3eefec363f3d52ad9b82e875ca9cbc3d8d5
Steven-Klavins <stevenklavins94@gmail.com>
2020-03-22 16:39:31 +0000

    added take off/ present constant

--- lib/plane.rb
+++ lib/plane.rb
@@ -1,5 +1,12 @@
 class Plane 
+    attr_reader :present 
+    def initialize (present = true)
+        @present = present
+    end
     def land_plane
 
+    end
+    def take_off
+        @present = false
     end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -3,4 +3,14 @@ describe Plane do
 it 'instructs a plane to land' do
     expect(subject).to respond_to :land_plane
 end
+    it 'instructs the plain to take off' do
+        expect(subject).to respond_to :take_off
+    end
+    
+    it 'lets you know when the plane is no longer present' do
+        plane = Plane.new
+        plane.take_off
+        expect(plane::present).to eq false
+    end
+
 end
