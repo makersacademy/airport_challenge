b749ecf548d8c2c507ae135685ab66f5d15b4c1f
Phil Vigus <philipvigus@yahoo.co.uk>
2020-03-23 06:10:40 +0000

    Implement directing planes method for all and no planes
    The direct method in the AirTrafficController class was extended
    so that it handles cases where all planes are successfully
    directed and none are.

--- lib/air_traffic_controller.rb
+++ lib/air_traffic_controller.rb
@@ -1,5 +1,20 @@
+require_relative './plane'
+
 class AirTrafficController
-  def direct_planes(planes)
-    
+  def direct_planes(directions)
+    directions_processed = 0
+      directions.each_with_index do |direction, index|
+        begin
+          if direction[1]
+            direction[0].land(direction[1])    
+          else
+            direction[0].take_off
+          end
+          directions_processed += 1
+        rescue
+          return 'No planes were successfully directed'
+        end
+      end
+    'All planes successfully directed'
   end
 end
--- spec/air_traffic_controller_spec.rb
+++ spec/air_traffic_controller_spec.rb
@@ -1,7 +1,35 @@
 require 'air_traffic_controller'
 
 describe AirTrafficController do
+  let(:set_weather_to_clear) { allow(Weather).to receive(:now).and_return(Weather::CLEAR) }
+  let(:set_weather_to_stormy) { allow(Weather).to receive(:now).and_return(Weather::STORMY) }
+
   it 'can tell multiple planes to take off and land' do
     expect(subject).to respond_to(:direct_planes).with(1).argument
+  end
+
+  it 'will report when all planes were successfully directed' do
+    set_weather_to_clear
+    airport = Airport.new
+    plane_1 = Plane.new
+    plane_1.land(airport)
+    plane_2 = Plane.new
+    plane_2.land(airport)
+    plane_4 = Plane.new
+    directions = [[plane_1, nil], [plane_4, airport], [plane_2, nil]]
+    expect(subject.direct_planes(directions)).to eq('All planes successfully directed')
+  end
+
+  it 'will report when no planes were successfully directed, giving you the reason why' do
+    set_weather_to_clear
+    airport = Airport.new
+    plane_1 = Plane.new
+    plane_1.land(airport)
+    plane_2 = Plane.new
+    plane_2.land(airport)
+    plane_4 = Plane.new
+    set_weather_to_stormy
+    directions = [[plane_1, nil], [plane_4, airport], [plane_2, nil]]
+    expect(subject.direct_planes(directions)).to eq('No planes were successfully directed')
   end
 end
