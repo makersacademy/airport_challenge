4d000e642c7f47d64e195c2ec8bb4855ed18a538
@willhead <60652390+will-head@users.noreply.github.com>
2020-04-18 16:13:43 +0100

    Add failing test to see if weather.stormy? returns a boolean

--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -1,7 +1,14 @@
 require 'weather'
 
 describe Weather do
+
   it 'responds to stormy?' do
     expect(subject).to respond_to(:stormy?)
   end
+
+  it 'returns true or false' do
+    weather = Weather.new
+    expect(weather.stormy?).to eq(true).or eq(false)
+  end
+
 end
