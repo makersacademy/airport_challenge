1fb492257c554309015dd4a5f6dbedd9ff311b40
@willhead <60652390+will-head@users.noreply.github.com>
2020-04-18 17:50:41 +0100

    Add failing test to ./spec/airport_spec.rb for airport.full? returns boolean

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -6,4 +6,9 @@ describe Airport do
     expect(subject).to respond_to(:full?)
   end
 
+  it 'returns true or false' do
+    airport = Airport.new
+    expect(airport.full?).to eq(true).or eq(false)
+  end
+
 end
