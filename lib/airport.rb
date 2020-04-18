b086cc19713d885d9bd0effa85019c5d4d61666c
mbrad26 <mbrad26@gmail.com>
2020-04-18 14:46:36 +0100

    added tests and implemented #land and #take_off methods

---
+++ lib/airport.rb
@@ -0,0 +1,9 @@
+class Airport
+
+  def land(plane)
+  end
+
+  def take_off
+  end
+
+end
---
+++ spec/airport_spec.rb
@@ -0,0 +1,27 @@
+require 'airport'
+
+describe Airport do
+  # As an air traffic controller
+  # So I can get passengers to a destination
+  # I want to instruct a plane to land at an airport
+
+  describe '#land' do
+    it 'instructs a plane to land' do
+      expect(subject).to respond_to(:land).with(1).argument
+    end
+  end
+# As an air traffic controller
+# So I can get passengers on the way to their destination
+# I want to instruct a plane to take off from an airport
+# and confirm that it is no longer in the airport
+  describe '#take_off' do
+    it 'instructs a plane to take off' do
+      expect(subject).to respond_to(:take_off)
+    end
+
+    it 'confirms that a plane is no longer at the airport after take_off' do
+
+    end
+  end
+
+end
