a91d17512869fc77c08da6886ea416b8994e1ac6
Declan <declankolakowski@gmail.com>
2020-04-18 10:23:59 +0000

    Initialized Airport planes as an array

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,4 +2,10 @@ require 'airport'
 
 describe Airport do
   it { is_expected.to respond_to(:planes) }
+  
+  describe "#planes" do
+    it "returns an Array" do
+      expect(subject.planes).to be_a_kind_of(Array)
+    end
+  end
 end
