06f9abb4e5cf7c42b9732d3ac3eb097b28c62872
Joanne Young <joanneyoung_@hotmail.com>
2020-04-18 17:39:44 +0100

    weather randomiser, and weather override

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,19 +1,29 @@
 require_relative 'plane'
 
 class Airport
-  def land(plane)
-  end
+  # def land(plane)
+  # end
 
   def take_off(plane)
+    @plane = nil
   end
 
   def sunny?
-    true
+    arr = [true, true, false]
+    @weather = arr.sample
   end
 
-  def park(plane)
+  def land(plane)
     @plane = plane
   end
 
-  
+  def full?
+    false
+  end
+
+ def weather_override
+  print "is it sunny? Type 'true' for sunny, 'false' for stormy"
+  @weather = gets.chomp!
+ end
+
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -3,9 +3,9 @@ require 'airport'
 describe Airport do
   
   #test 1, it should respond to a method 'land'
-  it "it should respond to the method with 1 arguement" do
-    expect(subject).to respond_to(:land).with(1).argument
-  end
+  # it "it should respond to the method with 1 arguement" do
+  #   expect(subject).to respond_to(:land).with(1).argument
+  # end
 
   #test 2, it should respond to method 'take_off'
   it "should respond to the method with 1 arguement" do
@@ -17,18 +17,32 @@ describe Airport do
     expect(subject.sunny?).to be true
   end
 
-  #test 4 it should respond to the method 'park'
-  it "should park the plane" do
-    expect(subject).to respond_to(:park).with(1).argument
+  #test 4 it should respond to the method 'land'
+  it "should land the plane" do
+    expect(subject).to respond_to(:land).with(1).argument
   end
 
-  #test 5 park method returns a plane
-  it "should park plane and return the plane object" do
+  #test 5 land method returns a plane that's passed into it
+  it "should land plane and return the plane object" do
     plane = Plane.new
-    subject.park(plane)
-    expect(subject.park(plane)).to eq plane
+    subject.land(plane)
+    expect(subject.land(plane)).to eq plane
   end
 
-  #it should respond to the method plane, for returning parked planes
+  #test 6 airport should not be full
+  it "should not be full" do
+    expect(subject.full?).to eq false
+  end
+
+  #test 7 airport should release plane on takeoff
+  it "should forget variable after takeoff" do
+    plane = Plane.new
+    expect(subject.take_off(plane)).to be_nil
+  end
+
+  # test 8 weather override
+  it "should have a weather overide" do
+    expect(subject).to respond_to(:weather_override)
+  end
 
 end
