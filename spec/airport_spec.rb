cc79a7609d23ee20067f1fde773fce7941e75488
GarethWoodman <gareth.woodman92@gmail.com>
2020-04-18 12:57:16 +0100

    Added test to allow default capacity at airport to be overriden

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -7,4 +7,10 @@ describe Airport do
     Airport::DEFAULT_CAPACITY.times { Plane.new.land_at_airport(subject) }
     expect{Plane.new.land_at_airport(subject)}.to raise_error("Airport is full")
   end
+
+  it 'allows to override default capacity' do
+    subject.capacity = Airport::DEFAULT_CAPACITY + 1
+    expect(subject.capacity > Airport::DEFAULT_CAPACITY).to eq(true)
+  end
+
 end
