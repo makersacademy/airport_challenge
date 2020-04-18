666660172e180e96427616eebcb89f48e0e12a46
Ed <lookatemail@gmail.com>
2020-04-18 16:49:03 +0100

    added ability to prevent  planes from landing if full?

--- lib/airport.rb
+++ lib/airport.rb
@@ -2,14 +2,26 @@ require_relative 'plane'
 
 class Airport
 
-  def land(plain)
-    @plain = plain
+  DEFAULT_CAPACITY = 2
+
+  def initialize(capacity = DEFAULT_CAPACITY)
+    @parked_planes = []
+    @capacity = capacity
+  end
+
+  def land(plane)
+    fail "Airport is full" if full?
+    @parked_planes << plane
+    # @parked_planes << plane unless @parked_planes.include? plane
   end
 
   def take_off
-    fail "Elvis has left the bulding" unless @plain
-    @plain
+    fail "Elvis has left the bulding" if @parked_planes
+    @parked_planes.shift
   end
 
+  def full?
+    @parked_planes.size >= @capacity 
+  end
 
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -9,9 +9,18 @@ describe Airport do
   end
 
   # I want to instruct a plane to take off from an airport and confirm that it is no longer in the airport
-  it 'should instruct the plane to #take_off if #land' do
+  it 'should instruct the plane to #take_off and receive a respond' do
+    subject.land "Boeing 777"
     message = "Elvis has left the bulding"
     expect { subject.take_off }.to raise_error message
+  end
+  
+  # I want to prevent landing when the airport is full
+  it 'should prevent #land if @plane' do
+    plane = Plane.new
+    message = "Airport is full"
+    Airport::DEFAULT_CAPACITY.times { subject.land(plane) }
+    expect { subject.land(plane) }.to raise_error message
   end
 
 end
