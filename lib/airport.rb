4db15fa1525d1100e77a6b4b5e4cf3e026c81514
GarethWoodman <gareth.woodman92@gmail.com>
2020-04-18 16:02:55 +0100

    Refactored airport file and test

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,13 +1,15 @@
 class Airport
   attr_accessor :planes, :capacity
+  attr_reader :weather
   DEFAULT_CAPACITY = 10
 
-  def initialize(capacity = DEFAULT_CAPACITY)
+  def initialize(weather, capacity = DEFAULT_CAPACITY)
     @planes = []
     @capacity = capacity
+    @weather = weather
   end
 
-  def is_full?
+  def full?
     @planes.count == @capacity ? airport_full_message : false
   end
 
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,16 +1,19 @@
 require './lib/plane'
 require './lib/airport'
+require './lib/weather'
 
 describe Airport do
+  weather_no_storm = Weather.new(0)
+  default_capacity = Airport::DEFAULT_CAPACITY
+  airport = Airport.new(weather_no_storm)
 
-  it 'prevents landing' do
-    Airport::DEFAULT_CAPACITY.times { Plane.new.land_at_airport(subject) }
-    expect{Plane.new.land_at_airport(subject)}.to raise_error("Airport is full")
+  it 'prevents landing if at full capacity' do
+    default_capacity.times { Plane.new.land_at_airport(airport) }
+    expect { Plane.new.land_at_airport(airport) }.to raise_error("Airport is full")
   end
 
   it 'allows to override default capacity' do
-    subject.capacity = Airport::DEFAULT_CAPACITY + 1
-    expect(subject.capacity > Airport::DEFAULT_CAPACITY).to eq(true)
+    airport.capacity = default_capacity + 1
+    expect(airport.capacity > default_capacity).to eq(true)
   end
-
 end
