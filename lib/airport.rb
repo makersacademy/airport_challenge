d957be5163a680f59e33070cdcb562dc38f3ac3e
Ed <lookatemail@gmail.com>
2020-04-18 13:29:33 +0100

    Unit test to enable plane to take off and get confirmation message

--- lib/airport.rb
+++ lib/airport.rb
@@ -3,7 +3,12 @@ require_relative 'plane'
 class Airport
 
   def land(plain)
+    @plain = plain
+  end
 
+  def take_off
+    fail "Elvis has left the bulding" unless @plain
+    @plain
   end
 
 
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,10 +2,16 @@ require 'airport'
 require 'plane'
 
 describe Airport do
-  
+
   # I want to instruct a plane to land at an airport
   it 'shout be able to instruct the plane to #land ' do
     is_expected.to respond_to(:land).with(1).argument
+  end
+
+  # I want to instruct a plane to take off from an airport and confirm that it is no longer in the airport
+  it 'should instruct the plane to #take_off if #land' do
+    message = "Elvis has left the bulding"
+    expect { subject.take_off }.to raise_error message
   end
 
 end
