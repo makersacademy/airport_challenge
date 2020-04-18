ffb3d2a94b4aa6a81c9899c795217399c23c67ad
Declan <declankolakowski@gmail.com>
2020-04-18 14:25:47 +0000

    Added check_weather method to WeatherReporter class

--- lib/weather_reporter.rb
+++ lib/weather_reporter.rb
@@ -1,2 +1,4 @@
 class WeatherReporter
+  def check_weather
+  end
 end
--- spec/weather_reporter_spec.rb
+++ spec/weather_reporter_spec.rb
@@ -1,4 +1,5 @@
 require 'weather_reporter'
 
 describe WeatherReporter do
+  it { is_expected.to respond_to(:check_weather) }
 end
