4578afcdb4555181c16bc9c40d8d0532a58cc0e2
dpwdec <declankolakowski@gmail.com>
2020-04-19 11:19:01 +0100

    removed redundant respond_to tests from weather report spec

--- spec/weather_reporter_spec.rb
+++ spec/weather_reporter_spec.rb
@@ -1,18 +1,17 @@
 require 'weather_reporter'
 
 describe WeatherReporter do
-  it { is_expected.to respond_to(:check_weather) }
-  
+
   describe "#check_weather" do
     it "returns a either 'Sunny' or 'Stormy'" do
       expect(subject.check_weather).to eq("Sunny").or eq("Stormy")
     end
-    
+
     it "returns 'Sunny' when it is Sunny" do
       srand(14)
       expect(subject.check_weather).to eq("Sunny")
     end
-    
+
     it "returns 'Stormy' when it is stormy" do
       srand(15)
       expect(subject.check_weather).to eq("Stormy")
