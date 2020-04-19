74c185e5289a0486a6aaaadf61dd887082c3ad44
Ellis Trickett <ellis.trickett@gmail.com>
2020-04-19 08:56:12 +0100

    Raise error when at capacity

--- lib/airport.rb
+++ lib/airport.rb
@@ -2,7 +2,14 @@ require_relative 'plane'
 
 class Airport
 
+  def initialize(capacity = 20)
+    @capacity = capacity
+    @planes_landed = []
+  end
+
   def land(plane)
+    raise 'Airport has reached capacity therefore you cannot land' if @planes_landed.length >= @capacity
+    @planes_landed << plane
   end
 
   def take_off(plane)
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,16 +1,26 @@
 require 'airport'
 
 RSpec.describe Airport do
+  subject(:airport) { described_class.new(20) }
 
+  it 'does not allow planes to land when capacity is reached' do
+    20.times do 
+      subject.land(Plane.new)
+    end
+      expect{subject.land(Plane.new)}.to raise_error 'Airport has reached capacity therefore you cannot land'
+    end
+
+  describe '#land' do
   it 'instructs plane to land' do
     expect(subject).to respond_to(:land).with(1).argument
   end 
+end
 
-  it 'instructs plane to take off' do
-    expect(subject).to respond_to(:take_off).with(1).argument
-  end 
 
   describe '#take_off' do 
+    it 'instructs plane to take off' do
+      expect(subject).to respond_to(:take_off).with(1).argument
+    end 
     it 'confirms plane is no longer at the airport' do 
       expect(subject.take_off(Plane.new)).to eq 'Plane has left the airport'
     end
