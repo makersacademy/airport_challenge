85a833ca0667867dc1c1fb9ec2c7cae95a362ac2
GarethWoodman <gareth.woodman92@gmail.com>
2020-04-19 11:17:55 +0100

    Added method to clear weather in airport_spec

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -3,16 +3,21 @@ require './lib/airport'
 require './lib/weather'
 
 describe Airport do
-  weather_no_storm = Weather.new(0)
   default_capacity = Airport::DEFAULT_CAPACITY
-  airport = Airport.new(weather_no_storm)
+  airport = Airport.new
+
+  def clear_weather_at(airport)
+    allow(airport.weather).to receive(:chance) { 100 }
+  end
 
   it 'prevents landing if at full capacity' do
+    clear_weather_at(airport)
     default_capacity.times { Plane.new.land_at_airport(airport) }
     expect { Plane.new.land_at_airport(airport) }.to raise_error("Airport is full")
   end
 
   it 'allows to override default capacity' do
+    clear_weather_at(airport)
     airport.capacity = default_capacity + 1
     expect(airport.capacity > default_capacity).to eq(true)
   end
