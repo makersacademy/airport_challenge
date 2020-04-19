763d47309bdcb8b8e1381b3ef1035d85a29a5973
tanilserbes <serbestanil@gmail.com>
2020-04-19 20:57:11 +0100

    weather added

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,16 +1,40 @@
 # understading airport object
 require_relative 'plane'
 class Airport
+  attr_accessor :capacity, :weather
   attr_reader :plane
 
+  DEFAULT_CAPACITY = 20
+
+  def initialize(capacity = DEFAULT_CAPACITY)
+    @capacity = capacity
+  end
+
   def takeoff
+    raise 'air is stormy' if stormy?
+
     @plane
   end
 
   def land(plane)
     raise 'Airport is full now' if @plane != nil
+    raise 'air is stormy' if stormy?
 
     @plane = plane
   end
 
+private
+  def randomweather
+    weather = ['sunny', 'sunny', 'sunny', 'sunny',
+               'sunny', 'sunny', 'sunny', 'sunny',
+               'sunny', 'sunny', 'sunny', 'sunny',
+               'sunny', 'sunny', 'sunny', 'stormy']
+
+    @weather = weather[rand(weather.length - 1)]
+  end
+
+  def stormy?
+    weather
+    @weather == 'stormy'
+  end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,14 +1,20 @@
 # understading airport object
 require 'airport'
+
 describe Airport do
   subject(:airport) { described_class.new }
   let(:plane) { Plane.new }
 
-  it 'initialize' do
-    expect(airport).to respond_to :plane
+  describe ' #initialize' do
+    it { is_expected.to respond_to :plane }
+    it { is_expected.to respond_to :capacity }
+    it 'set the default capacity:10' do
+      airport = Airport.new(10)
+      expect(airport.capacity).to eq 10
+    end
   end
 
-  describe '#land' do
+  describe ' #land' do
     it { is_expected.to respond_to(:land).with(1).argument }
 
     it 'is instruct a plane to land' do
@@ -21,14 +27,26 @@ describe Airport do
       plane1 = Plane.new
       expect { airport.land(plane1) } .to raise_error('Airport is full now')
     end
+
+    it 'raise an error when air is stormy' do
+      allow(airport).to receive(:rand).and_return 15
+      expect { airport.land(plane) } .to raise_error('air is stormy')
+    end
   end
 
-  describe '#takeoff' do
+  describe ' #takeoff' do
     it { is_expected.to respond_to(:takeoff) }
 
     it 'confirm that plane which is taken off flyies' do
       airport.land(plane)
       expect(airport.takeoff).to be_flying
     end
+
+    it 'raise an error when air is stormy' do
+      allow(airport).to receive(:rand).and_return 15
+      expect { airport.takeoff } .to raise_error('air is stormy')
+    end
+
   end
+
 end
