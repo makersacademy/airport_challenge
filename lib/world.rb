605d1ec23abae2af2ece01397a3fec40f35da45a
@willhead <60652390+will-head@users.noreply.github.com>
2020-04-19 10:02:07 +0100

    Tests: World reponds to World.add(object) and returns true if Air, Airport or Plane or false for anything else

--- lib/world.rb
+++ lib/world.rb
@@ -1,6 +1,10 @@
 class World
 
   def add(object)
+    return true if object.is_a?(Air)
+    return true if object.is_a?(Airport)
+    return true if object.is_a?(Plane)
+    
     false
   end
 
--- spec/world_spec.rb
+++ spec/world_spec.rb
@@ -1,5 +1,7 @@
 require 'world'
 require 'air'
+require 'airport'
+require 'plane'
 
 describe World do
 
@@ -10,6 +12,19 @@ describe World do
   it 'returns true or false' do
     air = Air.new
     expect(subject.add(air)).to eq(true).or eq(false)
+  end
+  
+  it 'returns false if object is not Air, Airport or Planes' do
+    expect(subject.add(1)).to eq(false)
+    
+    air = Air.new
+    expect(subject.add(air)).to eq(true)
+    
+    airport = Airport.new
+    expect(subject.add(airport)).to eq(true)
+    
+    plane = Plane.new
+    expect(subject.add(plane)).to eq(true)
   end
   
 end
