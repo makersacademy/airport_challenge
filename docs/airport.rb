377f67936f15669af94daea6dccd034f0ccdfa58
joannacollier <collj035@gmail.com>
2020-04-19 20:53:05 +0100

    Landing error when airport full

--- docs/airport.rb
+++ docs/airport.rb
@@ -1,19 +1,22 @@
-
-
 class Airport 
+  CAPACITY = 1
+  attr_accessor :planes
 
-    attr_accessor :planes
+  def initialize
+    @planes = []
+  end
 
-    def initialize
-        @planes = []
-    end
+  def instruct_plane_to_land(plane)
+    @planes << plane unless full?
+  end 
 
-    def instruct_plane_to_land(plane)
-        @planes << plane
-    end 
+  def instruct_plane_to_takeoff(plane)
+    @planes.delete(plane)
+  end 
 
-    def instruct_plane_to_takeoff(plane)
-        @planes.delete(plane)
-    end 
+  private
+
+  def full?
+    fail "Unable to land as Airport is full." if @planes.length >= CAPACITY
+  end 
 end 
-
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,9 +1,9 @@
 require './docs/airport'
 
-
 describe Airport do 
   let(:airport) { Airport.new }
   let(:plane) { "plane1" }
+  let(:plane2) { "plane2" }
 
   it "Instruct plane to land" do
     expect(airport).to respond_to(:instruct_plane_to_land)
@@ -11,7 +11,7 @@ describe Airport do
 
   it "Adds a plane to an array in airport when landing method ran" do
     airport.instruct_plane_to_land(plane)
-    expect(airport.planes).to eq ([plane])
+    expect(airport.planes).to eq([plane])
   end 
 
   it "Instructs plane to take off" do 
@@ -21,8 +21,11 @@ describe Airport do
   it "Removed plane from array when taking off method ran" do
     airport.instruct_plane_to_land(plane)
     airport.instruct_plane_to_takeoff(plane)
-    expect(airport.planes).to eq ([])
+    expect(airport.planes).to eq([])
   end 
-
   
+  it "Cannot land plane in full airport" do
+    airport.instruct_plane_to_land(plane)
+    expect{airport.instruct_plane_to_land(plane2)}.to raise_error("Unable to land as Airport is full.")
+  end 
 end 
