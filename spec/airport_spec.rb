db62944344aace8e945b017f3519944fb2be7858
pauladarias <pauladarias@gmail.com>
2020-04-19 15:35:26 +0100

    Take off method added with 1 argument

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -6,4 +6,8 @@ describe Airport do
     it 'instructs a plane to land' do
         expect(subject).to respond_to(:land).with(1).argument
     end 
+
+    it 'instructs a plane to take off' do 
+        expect(subject).to respond_to(:take_off).with(1).argument 
+    end 
 end 
--- spec/features/user_stories_spec.rb
+++ spec/features/user_stories_spec.rb
@@ -5,5 +5,11 @@ describe 'UserStory' do
         plane = Plane.new
         expect { airport.land(plane) }.not_to raise_error
         end 
+
+    it 'planes take off from airport, instruct a plane to take off' do 
+        airport = Airport.new
+        plane = Plane.new
+        expect { airport.take_off(plane)}.not_to raise_error
+    end 
 end 
 
