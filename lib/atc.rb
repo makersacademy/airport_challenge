350b8523a3e7c426a61f0191ba2dce97c990ed58
@willhead <60652390+will-head@users.noreply.github.com>
2020-04-19 15:04:40 +0100

    Test: Change Atc.where_is to accecpt (plane, world); passes

--- lib/atc.rb
+++ lib/atc.rb
@@ -1,7 +1,7 @@
 class Atc
 
-  def where_is(plane)
-    Airport.new
+  def where_is(plane, world)
+    world.where_is(plane)
   end
   
 end
--- spec/atc_spec.rb
+++ spec/atc_spec.rb
@@ -4,15 +4,16 @@ require 'air'
 require 'airport'
 require 'weather'
 require 'plane'
+require 'world'
 
 describe Atc do
 
-  it 'responds to where_is(Plane)' do
-    expect(subject).to respond_to(:where_is).with(1).argument
+  it 'responds to where_is(Plane, World)' do
+    expect(subject).to respond_to(:where_is).with(2).argument
   end
   
   it 'where_is(Plane) returns Air, Airport or false' do
-    plane = Plane.new
-    expect(subject.where_is(plane)).to be_an_instance_of(Air).or be_an_instance_of(Airport).or eq(false)
+#    plane = Plane.new
+    expect(subject.where_is(Plane.new, World.new)).to be_an_instance_of(Air).or be_an_instance_of(Airport).or eq(false)
   end
 end
