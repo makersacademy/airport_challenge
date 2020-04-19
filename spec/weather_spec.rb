893670a2330c250984586d3ac0c43ccfa855a466
GarethWoodman <gareth.woodman92@gmail.com>
2020-04-19 11:13:33 +0100

    Added method to set storm in weather_spec

--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -3,16 +3,21 @@ require './lib/airport'
 require './lib/weather'
 
 describe Weather do
-  weather_with_storm = Weather.new(100)
-  airport = Airport.new(weather_with_storm)
+  airport = Airport.new
   plane = Plane.new
   airport.planes << plane
 
+  def set_storm_at(airport)
+    allow(airport.weather).to receive(:chance) { 0 }
+  end
+
   it 'prevents takeoff if stormy' do
+    set_storm_at(airport)
     expect { plane.leave_airport(airport) }.to raise_error("Weather is stormy")
   end
 
   it 'prevents landing if stormy' do
+    set_storm_at(airport)
     expect { plane.land_at_airport(airport) }.to raise_error("Weather is stormy")
   end
 end
