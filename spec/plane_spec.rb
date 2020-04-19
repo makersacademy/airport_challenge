f2ccf2f268be400e31e9d9ad984e16085054d2a6
GarethWoodman <gareth.woodman92@gmail.com>
2020-04-19 11:27:02 +0100

    Added method to clear weather in plane_spec

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -3,16 +3,21 @@ require './lib/airport'
 require './lib/weather'
 
 describe Plane do
-  weather_no_storm = Weather.new(0)
-  airport = Airport.new(weather_no_storm)
+  airport = Airport.new
   plane = Plane.new
 
+  def clear_weather_at(airport)
+    allow(airport.weather).to receive(:chance) { 100 }
+  end
+
   it 'lands at airport' do
+    clear_weather_at(airport)
     plane.land_at_airport(airport)
     expect(airport.planes[0]).to eq(plane)
   end
 
   it 'leaves airport' do
+    clear_weather_at(airport)
     plane.leave_airport(airport)
     expect(airport.planes.empty?).to eq(true)
   end
