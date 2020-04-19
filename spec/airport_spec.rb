65e0b07b3a25ec8e2f2f954fdf9b86e02178c21d
@willhead <60652390+will-head@users.noreply.github.com>
2020-04-19 09:21:20 +0100

    Tests: add Airport tests to match Air tests and pass

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -15,6 +15,10 @@ describe Airport do
   it 'responds to weather' do
     expect(subject).to respond_to(:weather)
   end
+  
+  it 'responds to planes' do
+    expect(subject).to respond_to(:planes)
+  end
 
   it 'responds to add(Plane)' do
     expect(subject).to respond_to(:add).with(1).argument
@@ -27,6 +31,12 @@ describe Airport do
   it 'check for @capacity instance variable' do
     airport = Airport.new
     expect(airport.instance_variable_get(:@capacity))
+  end
+  
+  it 'check for @planes instance variable' do
+    # TODO: This doesn't seem to actually check for the instance variable
+    airport = Airport.new
+    expect(airport.instance_variable_get(:@planes))
   end
 
   it 'check @capacity instance variable is positive integer' do
@@ -68,6 +78,27 @@ describe Airport do
   it '.weather returns :sunny or :stormy' do
     airport = Airport.new
     expect(airport.weather).to eq(:sunny).or eq(:stormy)
+  end
+
+  it '.add(Plane) returns true or false' do
+    airport = Airport.new
+    plane = Plane.new
+    expect(airport.add(plane)).to eq(true).or eq(false)
+  end
+  
+  it '.del(Plane) returns Plane or false' do
+    airport = Airport.new
+    plane = Plane.new
+    airport.add(plane)
+    expect(airport.del(plane)).to be_an_instance_of(Plane).or eq(false)
+  end
+
+  it '.planes returns array' do
+    # TODO: check it's an array of planes
+    airport = Airport.new
+    plane = Plane.new
+    airport.add(plane)
+    expect(airport.planes).to be_an_instance_of(Array)
   end
 
 end
