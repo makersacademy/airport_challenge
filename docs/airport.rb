feaf0083665e7178310d8c4f343add040eeebdc0
joannacollier <collj035@gmail.com>
2020-04-19 19:24:05 +0100

    Airport Instance repsonds to instruct_plane_to_land method

--- docs/airport.rb
+++ docs/airport.rb
@@ -1,3 +1,10 @@
 class Airport 
 
+    def initalize 
+        @planes = []
+    end 
+
+    def instruct_plane_to_land(plane)
+    
+    end 
 end 
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,4 +2,9 @@ require './docs/airport'
 
 describe Airport do 
   let(:airport) { Airport.new }
+  let(:plane) { "plane1" }
+
+  it "Instruct plane to land" do
+    expect(airport).to respond_to(:instruct_plane_to_land)
+  end
 end 
