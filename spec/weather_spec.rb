5534b3f88657d8e8ef84bcce16a7eadf1f4a2e9f
GarethWoodman <gareth.woodman92@gmail.com>
2020-04-19 11:27:26 +0100

    Refactored weather_spec

--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -7,17 +7,17 @@ describe Weather do
   plane = Plane.new
   airport.planes << plane
 
-  def set_storm_at(airport)
+  def storm_at(airport)
     allow(airport.weather).to receive(:chance) { 0 }
   end
 
   it 'prevents takeoff if stormy' do
-    set_storm_at(airport)
+    storm_at(airport)
     expect { plane.leave_airport(airport) }.to raise_error("Weather is stormy")
   end
 
   it 'prevents landing if stormy' do
-    set_storm_at(airport)
+    storm_at(airport)
     expect { plane.land_at_airport(airport) }.to raise_error("Weather is stormy")
   end
 end
