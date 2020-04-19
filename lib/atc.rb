354f5e9ad9079e8ed1c82f8a41e25a5a2095b899
LTurns <lizzieturney@gmail.com>
2020-04-19 16:12:20 +0100

    air traffic control spec and class complete

--- lib/atc.rb
+++ lib/atc.rb
@@ -1,28 +1,49 @@
-require_relative 'plane'
+#require_relative 'plane'
 
 module ATC
+  class Dummy_Class
+  def confirm_weather_descent
+    print "The weather today is currently "
+    puts result = ["sunny", "sunny", "stormy", "sunny"].sample
+    if result == "sunny"
+      land_plane
+    else
+      cancel_landing
+    end
+  end
 
-  def confirm_weather
-    ["sunny", "sunny", "stormy", "sunny"].sample
+  def confirm_weather_ascent
+    print "The weather today is currently "
+    puts result = ["sunny", "sunny", "stormy", "sunny"].sample
+    if result == "sunny"
+      take_off_plane
+    else
+      cancel_take_off
+    end
   end
 
   def land_plane
-    "land plane at airport"
+   "Permission to land plane has been given. Your final task is to confirm that the plane has landed and the passengers have safetly reached their destination. To do this, please type 'plane.confirm_landing'."
+  end
+
+  def confirm_landing
+    "The eagle has landed! Another plane is due to take-off. To complete your next task, create a new plane (plane = Plane.new('name') and type plane.prepare_take_off. Alternatively, try to land another plane by doing the same but typing plane.spotted."
   end
 
   def take_off_plane
-    "prepare plane for take-off"
+    "Plane has been prepared for take-off. Your final job is to confirm that the plane is in the sky and the passengers are on route to their destination. Please type 'plane.confirm_take_off'."
   end
 
   def confirm_take_off
-    "plane is no longer at the airport"
+    "The eagle is flying! Please make more planes if you would like to keep going. To initiate the plane, activate either 'plane.spotted' and 'plane.prepare_take_off.'"
   end
 
   def cancel_take_off
-    "plane cannot take off"
+    "plane cannot take off. Plane wil need to wait until data improves. Ascend another plane by typing (plane = Plane.new('name') and plane.prepare_take_off. Alternatively, try to land another plane by typing plane.spotted."
   end
 
   def cancel_landing
-    "plane cannot land"
+    "Landing has been cancelled. Plane will need to hover until data improves or land at another airport. Another plane is due to take-off. To complete your next task, create a new plane (plane = Plane.new('name') and type plane.prepare_take_off. Alternatively, try to land another plane by typing plane.spotted."
   end
+end
 end
--- spec/atc_spec.rb
+++ spec/atc_spec.rb
@@ -1,34 +1,56 @@
 require 'atc'
 
-RSpec.describe ATC do
-  describe '#land_plane' do
-    it "shows 'land plane at airport'" do
-      subject = described_class.new
-      expect(subject.land_plane).to eq("land plane at airport")
+module ATC
+  RSpec.describe ATC do
+    describe '#confirm_weather_descent' do
+      context "when stormy" do
+        it "logs a message 'stormy'" do
+          subject = Dummy_Class.new
+          allow(subject).to receive(:confirm_weather_descent) { "stormy" }
+        end
+      end
+      context "when sunny" do
+        it "logs a message 'sunny'" do
+          subject = Dummy_Class.new
+          allow(subject).to receive(:confirm_weather_descent) { "sunny" }
+        end
+      end
     end
-  end
-
-  describe '#take_off_plane' do
-    it "shows 'prepare plane for take-off'" do
-      expect(subject.take_off_plane).to eq("prepare plane for take-off")
+    describe '#land_plane' do
+      it "shows permission to land plane" do
+        subject = Dummy_Class.new
+        expect(subject).to respond_to(:land_plane)
+      end
     end
-  end
-
-  describe '#confirm_take_off' do
-    it "shows 'plane is no longer at the airport'" do
-      expect(subject.confirm_take_off).to eq("plane is no longer at the airport")
+    describe '#confirm_landing' do
+      it "confirms plane has landed" do
+        subject = Dummy_Class.new
+        expect(subject).to respond_to(:confirm_landing)
+      end
     end
-  end
-
-  describe '#cancel_take_off' do
-    it "shows 'plane cannot take off'" do
-      expect(subject.cancel_take_off).to eq("plane cannot take off")
+    describe '#take_off_plane' do
+      it "prepares plane for take-off" do
+        subject = Dummy_Class.new
+        expect(subject).to respond_to(:take_off_plane)
+      end
     end
-  end
-
-  describe '#cancel_landing' do
-    it "shows 'plane cannot land'" do
-      expect(subject.cancel_landing).to eq("plane cannot land")
+    describe '#confirm_take_off' do
+      it "confirms plane is in air" do
+        subject = Dummy_Class.new
+        expect(subject).to respond_to(:confirm_take_off)
+      end
+    end
+    describe '#cancel_take_off' do
+      it "confirms take-off is cancelled" do
+        subject = Dummy_Class.new
+        expect(subject).to respond_to(:cancel_take_off)
+      end
+    end
+    describe '#cancel_landing' do
+      it "confirms landing has been cancelled" do
+        subject = Dummy_Class.new
+        expect(subject).to respond_to(:cancel_landing)
+      end
     end
   end
 end
