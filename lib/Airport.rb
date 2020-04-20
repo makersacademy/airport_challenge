4738723fa8425a873c2a1214b0906845785b86b0
Student <student@Makerss-MacBook-Air-2.local>
2020-04-20 06:03:53 +0100

    poor code I'm sure, tried my best to understand principles at least

--- lib/Airport.rb
+++ lib/Airport.rb
@@ -1,4 +1,15 @@
+
+
 class Airport
+  attr_accessor :capacity, :weather
+
   def land(plane)
+    @weather == stormy
   end
+  def take_off(plane)
+    @weather == stormy
+  end
+  def land_full(plane)
+    @capacity == ()
+end
 end
---
+++ spec/airport_spec.rb
@@ -0,0 +1,18 @@
+require 'airport'
+
+describe Airport do
+  subject(:airport) { described_class.new }
+
+  it 'instructs a plane to land' do
+    expect(airport).to respond_to(:land).with(1).argument unless @weather
+end
+
+  it 'instructs a plane to take off unless stormy' do
+    expect(airport).to respond_to(:take_off).with(1).argument unless @weather
+
+
+end
+  it 'prevents landing when airport is full' do
+    expect(airport).to respond_to(:land_full) if @capacity
+end
+end
