ece19d022c2fe9d3c2df833ee7ca36000940fc8a
joannacollier <collj035@gmail.com>
2020-04-20 07:50:04 +0100

    Plane responding to land/takeoff/flying method + land method assigns airport to plane location

--- docs/plane.rb
+++ docs/plane.rb
@@ -1,6 +1,19 @@
 class Plane
 
-    def initialize
-    end 
+  attr_accessor :location
 
+  def initialize(location = nil)
+    @location = location
+  end 
+
+  def land(airport)
+    @location = airport
+  end
+
+  def takeoff(airport)
+  end 
+
+  def flying
+  end 
+  
 end 
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -4,4 +4,24 @@ require './docs/plane'
 describe Plane do 
   let(:airport) { Airport.new }
   let(:plane) { Plane.new }
+
+
+  it "Plane responds to landing" do 
+    expect(plane).to respond_to(:land)
+  end 
+
+  it "Plane responds to taking off method" do 
+    expect(plane).to respond_to(:takeoff)
+  end 
+
+  it "Plane responds to flying method" do 
+    expect(plane).to respond_to(:flying)
+  end 
+
+  it "Assigns airport to plane when landing" do
+    plane.land(aiport)
+    expect(plane.location).to eq (airport)
+  end
+
+
 end 
