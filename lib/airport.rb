8d92241288a97d272892f858c4101e7efb9c58c4
pauladarias <pauladarias@gmail.com>
2020-04-25 11:31:13 +0100

    first story: instructs a plane to land

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,16 +1,6 @@
 class Airport
-
-    def initialize(capacity)
-        @capacity = capacity 
-        @planes = 0
-    end 
-
     def land(plane)
-        raise 'Cannot land plane: airport full' if @planes.length >= @capacity
-        @planes  +=1
     end 
 
-    def take_off(plane)
-    end 
 end 
 
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,20 +1,9 @@
 require 'airport'
 
 describe Airport do 
-    #this line is to first test to see if responds:
-    #it { is_expected.to respond_to (:land).wit(1).argument }
-    it 'instructs a plane to land' do
-        expect(subject).to respond_to(:land).with(1).argument
-    end 
+    subject(:airport) {described_class.new}
 
-    it 'instructs a plane to take off' do 
-        expect(subject).to respond_to(:take_off).with(1).argument 
-    end 
-
-    it 'does not allow landind when full' do 
-        10.times do 
-            airport.land(:plane)
-        end 
-        expect { airport.land(:plane)}.to raise_error 'Cannot land plane: airport full'
+    it "instructs a plane to land" do 
+        expect(airport).to respond_to(:land).with(1).argument
     end 
 end 
