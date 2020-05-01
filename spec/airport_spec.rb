5f07bf598ced1f26508bd18982a50a841c413d90
Avin <avin.hurry@digital.education.gov.uk>
2020-05-01 19:23:49 +0100

    Planes can take off and land from airports, tests implemented and passes (user stories 1 & 2 complete

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -6,28 +6,33 @@ describe Airport do
 # So I can get passengers to a destination
 # I want to instruct a plane to land at an airport
 
-  it "instructs a plane to land at an airport" do
-  #arrange
-  heathrow_airport = Airport.new
-  plane = Plane.new
-  #act
-  plane.land(plane)
-  # assert
-  expect(plane).to respond_to(:land)
-  end
+  it "instructs planes to land at an airport" do
+    #arrange
+    plane = Plane.new
+    airport = Airport.new
 
-#As an air traffic controller
-# So I can get passengers on the way to their destination
-# I want to instruct a plane to take off from an airport and confirm that it is no longer in the airport
+    # act
+    plane.land(airport)
+    airport.inventory
 
-  it "instructs a plane to take off from an airport" do
-  #arrange
-  airport = Airport.new
-  plane = Plane.new
-  #act
-  plane.take_off(plane)
-  #assert
-  expect(plane).to respond_to(:take_off)
-  end
+    #assert
+    expect(airport.inventory.length).to be(1)
+    end
 
+    #As an air traffic controller
+    # So I can get passengers on the way to their destination
+    # I want to instruct a plane to take off from an airport and confirm that it is no longer in the airport
+
+    it "instructs planes to take off from an airport and confirms this" do
+      #arrange
+      plane = Plane.new
+      airport = Airport.new
+
+      #act
+      plane.take_off(airport)
+      airport.inventory
+
+      #assert
+      expect(airport.inventory.length).to be(0)
+    end
 end
