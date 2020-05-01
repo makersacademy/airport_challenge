eea1d84a72a3dc727fc85a40848c44721a91c509
Catriona <catrionamairi@blueyonder.co.uk>
2020-05-01 10:31:16 +0100

    added test for stormy? and refactored check test

--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -2,8 +2,12 @@ require 'weather'
 
 describe Weather do
   it 'checks what the weather is doing' do
-    weather = Weather.new
-    allow(weather).to receive(:rand).and_return(1)
-    expect(weather.check).to eq(1)
+    allow(subject).to receive(:rand) { 1 }
+    expect(subject.check).to eq(1)
+  end
+
+  it 'sees if the weather is stormy' do
+    allow(subject).to receive(:check) { 1 }
+    expect(subject).not_to be_stormy
   end
 end
