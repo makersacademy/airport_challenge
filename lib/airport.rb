03fc136f18bdf35f4048d404ffa732cb50259ab8
Jonathan Filar <jonathanfilar@hotmail.com>
2020-05-01 15:14:35 +0100

    After three User Stories

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,11 +1,14 @@
 require_relative 'plane'
 
 class Airport
+  DEFAULT_CAPACITY = 5
   def initialize
     @plane = []
+    @capacity = DEFAULT_CAPACITY
   end
 
   def land(plane)
+    fail 'The airport is full, you cannot land' if @plane.count >= DEFAULT_CAPACITY
     @plane << plane
   end
 
