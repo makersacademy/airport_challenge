1a2326d3ca8aa1616ea1ee2b6147afa5daff4599
sabarinath kunnambath <sabarinathk7@gmail.com>
2020-05-01 14:29:37 +0100

    initialize_Methods /classes

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,4 +1,10 @@
 
 class Airport
+    attr_reader :planes, :capacity
+    DEFAULT_CAPACITY = 300
 
+def initialize (capacity =DEFAULT)
+  @planes = []
+  @capacity = capacity
 end
+end 
---
+++ spec/airport_spec.rb
@@ -0,0 +1,12 @@
+require "airport"
+require "weather"
+
+
+describe Airport do
+
+describe "#landing_plane" do
+  it "Allows plane to land" do
+    plane = Plane.new
+
+  end
+end
