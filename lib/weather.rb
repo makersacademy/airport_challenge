19d3211cbd0f9bf47291ecd0418fa726933f023a
Kabir Qazi <kqazi2@ford.com>
2020-05-03 15:28:03 +0100

    Tests for plane takeoff and landing during stormy weather

--- lib/weather.rb
+++ lib/weather.rb
@@ -2,4 +2,9 @@ require_relative 'airport'
 require_relative 'plane'
 
 class Weather
+
+  def weather_condition
+    ["Sunny", "Cloudy", "Rainy", "Windy", "Snowy", "Stormy"].sample
+  end
+
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -9,14 +9,14 @@ describe Airport do
     expect(airport.land(plane)).to eq("Plane landed at airport")
   end
 
-  it "Plane can take off from an airport" do
+  it "Plane can takeoff from an airport" do
     airport = Airport.new
     plane = Plane.new
     airport.land(plane)
     expect(airport.take_off).to be_instance_of Plane
   end
 
-  it "After take off plane is no longer at airport" do
+  it "After takeoff plane is no longer at airport" do
     airport = Airport.new
     plane = Plane.new
     airport.land(plane)
@@ -36,7 +36,22 @@ describe Airport do
     plane = Plane.new
     airport.capacity.times{airport.land(plane)}
     expect{airport.land(plane)}.to raise_error("Airport at full capacity!")
+  end
 
+  let(:weather) { double :weather }
+  it "Plane cannot takeoff when weather is stormy" do
+    airport = Airport.new
+    plane = Plane.new
+    allow(weather).to receive(:weather_condition).and_return("Stormy")
+    puts weather
+    expect{airport.take_off(plane)}.to raise_error("Plane cannot takeoff due to stormy weather!")
+  end
+
+  it "Plane cannot land when weather is stormy" do
+    airport = Airport.new
+    plane = Plane.new
+    allow(weather).to receive(:weather_condition).and_return("Stormy")
+    expect{airport.land(plane)}.to raise_error("Plane cannot land due to stormy weather!")
   end
 
 end
