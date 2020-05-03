ebf6b0649cbc276daaa4257f4f365d3c0eeb258b
Kabir Qazi <kqazi2@ford.com>
2020-05-03 13:40:31 +0100

    Added full capacity tests and added method to make tests pass

--- lib/airport.rb
+++ lib/airport.rb
@@ -9,15 +9,38 @@ class Airport
   def initialize(capacity=DEFAULT_CAPACITY)
     @capacity = capacity
     @landed_planes = []
+    check_capacity()
+  end
+  
+  def take_off
+    check_capacity()
+    if @avaliable
+      @landed_planes.pop
+    end
   end
 
   def land(plane)
-    @landed_planes << plane
-    "#{plane} landed at airport"
+    check_capacity()
+    if @avaliable && @full
+      fail ("Airport at full capacity!")
+    elsif @full == false
+      @landed_planes << plane
+      return "Plane landed at airport"
+    end
   end
 
-  def take_off
-    @landed_planes.pop
-    Plane.new
+  attr_reader :landed_planes
+
+  def check_capacity
+    if @landed_planes.empty?
+      @avaliable = false
+      @full = false
+    elsif @landed_planes.count == @capacity
+      @avaliable = true
+      @full = true
+    else
+      @full = false
+      @avaliable = true
+    end
   end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -5,24 +5,38 @@ require 'weather.rb'
 describe Airport do
   it "Plane can land at an airport" do
     airport = Airport.new
-    expect(airport.land("plane0")).to eq("plane0 landed at airport")
+    plane = Plane.new
+    expect(airport.land(plane)).to eq("Plane landed at airport")
   end
 
   it "Plane can take off from an airport" do
     airport = Airport.new
-    airport.land("plane0")
+    plane = Plane.new
+    airport.land(plane)
     expect(airport.take_off).to be_instance_of Plane
   end
 
-  # it "After take off plane is no longer at airport" do
-  #   airport = Airport.new
-  #   airport.land("plane0")
-  #   airport_after_take_off = airport.take_off
-  #   p airport_after_take_off
-  #   expect(airport_after_take_off).not_to include("plane0")
-  # end
+  it "After take off plane is no longer at airport" do
+    airport = Airport.new
+    plane = Plane.new
+    airport.land(plane)
+    airport.take_off
+    expect(landed_planes).to be_empty
+  end
 
   it "Plane cannot land when airport is full" do
+    airport = Airport.new
+    plane = Plane.new
+    Airport::DEFAULT_CAPACITY.times{airport.land(plane)}
+    expect{airport.land(plane)}.to raise_error("Airport at full capacity!")
+  end
+
+  it "Plane cannot land if airport if full and capacity is changed" do
+    airport = Airport.new(30)
+    plane = Plane.new
+    airport.capacity.times{airport.land(plane)}
+    expect{airport.land(plane)}.to raise_error("Airport at full capacity!")
+
   end
 
 end
