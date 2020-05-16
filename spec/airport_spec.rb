969994f11d57fe855f93d5095a656b3e74e17d14
Nick Butler <nwmbutler@gmail.com>
2020-05-16 15:59:59 +0100

    Green for airport.land

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,5 +1,11 @@
 require 'airport'
+require 'plane'
 
 describe Airport do
+
+  it "allows a plane to land at the airport" do
+    ap = Airport.new
+    expect(ap).to respond_to(:land)
+  end
 
 end
