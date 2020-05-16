17f4c0270f2c7281246109235f41af7e2288b462
J Perrin <perrinjack96@gmail.com>
2020-05-16 14:45:51 +0100

    refactoring airport.rb with attr:readers

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,6 +1,9 @@
-require './plane.rb'
+require './lib/plane.rb'
 class Airport
   DEFAULT_CAPACITY = 20
+
+  attr_reader :hangar, :capacity
+  
   def initialize(capacity = DEFAULT_CAPACITY)
     @capacity = capacity
     @hangar = []
@@ -9,33 +12,41 @@ class Airport
   def takeoff(plane)
     raise 'No departures due to adverse weather' if bad_weather?
 
-    plane.lift_off
-    @hangar.pop    
+    out_hangar_and_takeoff(plane)
   end
 
   def land(plane)
-    raise 'Airport is full' if full?
-    raise 'No arrivals due to adverse weather' if bad_weather?
+    landing_possible_checks
+    land_and_store(plane)
 
-    plane.touch_down
-    @hangar << plane
   end
-
-  
 
   private
 
   def full?
-    @hangar.count >= @capacity
+    hangar.count >= capacity
   end
 
   def bad_weather?
     rand(1..5) > 4
   end
 
+  def landing_possible_checks
+    raise 'Airport is full' if full?
+    raise 'No arrivals due to adverse weather' if bad_weather?
+  end
+
+  def land_and_store(plane)
+    plane.touch_down
+    hangar << plane
+  end
+
+  def out_hangar_and_takeoff(plane)
+    plane.lift_off
+    hangar.delete(plane)  
+  end
 end
 
-#airport = Airport.new(100)
-#plane = Plane.new
-#airport.land(plane)
-
+# airport = Airport.new(100)
+# plane = Plane.new
+# airport.land(plane)
