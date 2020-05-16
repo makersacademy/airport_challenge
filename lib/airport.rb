d39e82dfe1f51e50a7473f46b0951e989377176c
Student <student@makerss-air-2.mynet>
2020-05-16 14:50:24 +0100

    added land method to airport class

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,3 +1,9 @@
 class Airport
+  def initialize
+    @planes_in = []
+  end
 
+  def land(plane)
+    @planes_in << plane
+  end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,7 +1,18 @@
 require 'airport'
+require 'plane'
 
 describe Airport do
   it 'creates an instance of the class Airport' do
-    expect(Airport.new).to be_instance_of Airport
+    expect(subject).to be_instance_of Airport
+  end
+  it 'creates an array for storing planes' do
+    expect(subject.instance_variable_get :@planes_in)
+  end
+  it 'allows a plane to land' do
+    expect(subject).to respond_to(:land).with(1).argument
+  end
+  it 'stores a plane when landed' do
+    plane = Plane.new
+    expect(subject.land(plane)).to match_array([plane])
   end
 end
