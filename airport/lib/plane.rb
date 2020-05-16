6e17daa0bf28fd6ab956ed06dfa7cb301f3b4418
minsi yang <yangminsi@gmail.com>
2020-05-16 16:54:07 +0100

    Modified land method with returning an airport instance

--- airport/lib/plane.rb
+++ airport/lib/plane.rb
@@ -3,7 +3,10 @@ require_relative 'airport'
 class Plane
 
   def land(airport)
-
+    @airport = airport
   end
 
+  def take_off(airport)
+    "Plane is no longer in the airport"
+  end
 end
--- airport/spec/plane_spec.rb
+++ airport/spec/plane_spec.rb
@@ -3,7 +3,14 @@ require 'plane'
 describe Plane do
   it { is_expected.to respond_to(:land).with(1).argument }
 
-  # it "take off from airport" do
-  #   expect(subject.take_off).to eq ""
-  # end
+  it "land a plane in an airport" do
+    airport = Airport.new
+    expect(subject.land(airport)).to eq airport 
+  end
+
+  it { is_expected.to respond_to(:take_off).with(1).argument }
+
+  it "take off from airport" do
+    expect(subject.take_off(1)).to eq "Plane is no longer in the airport"
+  end
 end
