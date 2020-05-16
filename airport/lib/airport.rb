ce5a22c0da3e59f334f6c7c75e125e8196ef0c0a
minsi yang <yangminsi@gmail.com>
2020-05-16 22:43:03 +0100

    Added details for take off method to take one argument, passed

--- airport/lib/airport.rb
+++ airport/lib/airport.rb
@@ -6,7 +6,7 @@ class Airport
     @plane = plane
   end
 
-  def take_off
+  def take_off(plane)
 
   end
 
--- airport/spec/airport_spec.rb
+++ airport/spec/airport_spec.rb
@@ -12,5 +12,7 @@ describe Airport do
 
   it { is_expected.to respond_to :take_off }
 
+  it { is_expected.to respond_to(:take_off).with(1).argument }
+
 
 end
