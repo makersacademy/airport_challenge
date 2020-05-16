8269104638f2593949790fc812b3847214621160
Rachael <rachaelewins@hotmail.com>
2020-05-16 10:09:39 +0100

    refactor, via a before(:each)

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -2,40 +2,42 @@ require_relative '../lib/plane.rb'
 
 describe Plane do ## update test when airport class is created
 
-  let(:plane) { plane = Plane.new }
-  let(:planeA) { planeA = Plane.new }
-  let(:airportA) { airportA = Airport.new }
-  let(:airportB) { airportB = Airport.new }
+  before(:each) do
+    @airport_a = Airport.new
+    @airport_b = Airport.new
+    @plane_a = Plane.new
+    @plane_b = Plane.new
+  end
 
   describe '#land_at(airport)' do
     it "should be at the airport it landed at,and tell the operator that it landed" do
-      expect { plane.land_at(airportA) }.to output("landed at #{airportA}\n").to_stdout
+      expect { @plane_a.land_at(@airport_a) }.to output("landed at #{@airport_a}\n").to_stdout
     end
     it "should only be able to land if it is flying, and tell the operator about the problem" do
-      plane.land_at(airportA)
-      expect { plane.land_at(airportB) }.to output("currently located at #{airportA} so can not land\n").to_stdout
+      @plane_a.land_at(@airport_a)
+      expect { @plane_a.land_at(@airport_b) }.to output("currently located at #{@airport_a} so can not land\n").to_stdout
     end
     it "should not land at full airports, and tell the operator about the situation" do
-      airportA.capacity = 1
-      plane.land_at(airportA)
-      expect { planeA.land_at(airportA) }.to output("#{airportA} is full\n").to_stdout
+      @airport_a.capacity = 1
+      @plane_a.land_at(@airport_a)
+      expect { @plane_b.land_at(@airport_a) }.to output("#{@airport_a} is full\n").to_stdout
     end
   end
 
   describe '#takeoff_from(airport)' do
     it "should leave airport and begin flying, and tell the operator it departed" do
-      plane.land_at(airportA)
-      expect { plane.takeoff_from(airportA) }.to output("departed #{airportA}\n").to_stdout
+      @plane_a.land_at(@airport_a)
+      expect { @plane_a.takeoff_from(@airport_a) }.to output("departed #{@airport_a}\n").to_stdout
     end
     it "shouldn't takeoff from somewhere it is not, and tell the operator about the problem" do
-      plane.land_at(airportA)
-      expect { plane.takeoff_from(airportB) }.to output("currently located at #{airportA} so can not takeoff\n").to_stdout
+      @plane_a.land_at(@airport_a)
+      expect { @plane_a.takeoff_from(@airport_b) }.to output("currently located at #{@airport_a} so can not takeoff\n").to_stdout
     end
-    it "should be removed from the airport's list of planes" do
-      airportA.capacity = 1
-      plane.land_at(airportA)
-      plane.takeoff_from(airportA)
-      expect { planeA.land_at(airportA) }.to output("landed at #{airportA}\n").to_stdout
+    it "should be removed from the airport's list of plane_as" do
+      @airport_a.capacity = 1
+      @plane_a.land_at(@airport_a)
+      @plane_a.takeoff_from(@airport_a)
+      expect { @plane_b.land_at(@airport_a) }.to output("landed at #{@airport_a}\n").to_stdout
     end
   end
 end
