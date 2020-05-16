ceea14efb4c1aa8cfdc5c3c9432a0633771f7a7f
Joshua Brook <joshuabrookuk@gmail.com>
2020-05-16 15:41:37 +0100

    instruct a plane to take off from an airport and confirm empty ecit

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,5 +1,11 @@
 class Airport
+
   def land(plane)
-    plane
+    @plane = plane
   end
+
+  def take_off(plane)
+    "#{@plane} has taken off"
+  end
+
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -6,4 +6,8 @@ describe Airport do
   it 'should instruct a plane to land at an airport' do
     expect(subject).to respond_to(:land).with(1).argument
   end
+
+  it 'should instruct a plane to take off from an airport and confirm empty' do
+expect(subject).to respond_to(:take_off).with(1).argument
+  end
 end
