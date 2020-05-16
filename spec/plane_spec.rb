2d3d288b1f6a327358915f7980a832f595e3094d
Olatunji Oduola <ola.oduola@gmail.com>
2020-05-16 16:35:10 +0100

    Added test. After plan take_off. airport to respond to plane_in_airport?

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -4,7 +4,12 @@ describe Plane do
   it 'respnds to #land' do
     expect(subject).to respond_to(:land)
   end
+
   it 'respnds to #take_off' do
     expect(subject).to respond_to(:take_off)
+  end
+  it 'takes off from airport' do
+    airport = subject.take_off
+    expect(airport).to be_plane_in_airport
   end
 end
