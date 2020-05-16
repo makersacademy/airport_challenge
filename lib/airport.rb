5f9e70419675b0545eac5f35b7398edf9a3b9c34
J Perrin <perrinjack96@gmail.com>
2020-05-16 14:01:29 +0100

    first 6 user stories complete

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,14 +1,41 @@
-require './lib/plane'
+require './plane.rb'
 class Airport
-
-  def takeoff
+  DEFAULT_CAPACITY = 20
+  def initialize(capacity = DEFAULT_CAPACITY)
+    @capacity = capacity
+    @hangar = []
+  end
   
+  def takeoff(plane)
+    raise 'No departures due to adverse weather' if bad_weather?
+
+    plane.lift_off
+    @hangar.pop    
   end
 
   def land(plane)
-    @plane = plane
-   
+    raise 'Airport is full' if full?
+    raise 'No arrivals due to adverse weather' if bad_weather?
 
+    plane.touch_down
+    @hangar << plane
+  end
+
+  
+
+  private
+
+  def full?
+    @hangar.count >= @capacity
+  end
+
+  def bad_weather?
+    rand(1..5) > 4
   end
 
 end
+
+#airport = Airport.new(100)
+#plane = Plane.new
+#airport.land(plane)
+
--- lib/plane.rb
+++ lib/plane.rb
@@ -1,3 +1,18 @@
 class Plane
+  
+  def initialize
+    @airborne = true
+  end
 
+  def touch_down
+    @airborne = false
+  end
+
+  def lift_off
+    @airborne = true
+  end
+
+  def flying?
+    @airborne
+  end
 end
