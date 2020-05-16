50f4a0e5259803a973d618d3439ba133cda74c23
Tristan Langford <tristanlangford@hotmail.com>
2020-05-16 13:34:04 +0100

    take off from aiport. one plane only

--- lib/airport.rb
+++ lib/airport.rb
@@ -2,14 +2,14 @@ require_relative 'plane'
 
 class Airport
 
-  attr_reader :planes 
+  attr_reader :planes
 
   def landing(plane)
     @planes = plane
   end
 
-  def take_off
-    @planes
+  def depart(plane)
+    @planes = nil 
   end
 
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -3,26 +3,23 @@ require 'airport'
 describe Airport do
   subject { Airport.new }
 
-  context 'landing' do
+  let (:plane) { Plane.new }
 
-    let (:plane) { Plane.new }
-
-    before do
-      plane.land(subject)
-    end
-
-    it 'airport responds to landing method and takes an argument' do
-      expect(subject).to respond_to(:landing).with(1).argument
-    end
-
-    it 'adds the plane to the airport class' do
-      expect(subject.instance_variable_get(:@planes)).to eq(plane)
-    end
-
+  before do
+    plane.land(subject)
   end
 
-  context 'take_off' do
+  it 'airport responds to landing method and takes an argument' do
+    expect(subject).to respond_to(:landing).with(1).argument
+  end
 
+  it 'adds the plane to the airport class' do
+    expect(subject.instance_variable_get(:@planes)).to eq(plane)
+  end
+
+  it 'removes plane from airport when departs' do
+    subject.depart(plane)
+    expect(subject.instance_variable_get(:@planes)).to eq(nil)
   end
 
 end
