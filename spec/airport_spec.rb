3f1648990dc27a1dc8792b2b1e4891c241a9c9ca
Rachael <rachaelewins@hotmail.com>
2020-05-16 08:01:31 +0100

    refactor on airport_spec.rb: removing redundant test

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,21 +1,13 @@
 require_relative '../lib/airport.rb'
 
 describe Airport do
-
-  describe '#initialize' do
-    it "should be able to take one or no parameters" do
-      expect(Airport).to respond_to(:new).with(1).argument
-      expect(Airport).to respond_to(:new).with(0).arguments
-    end
-  end
-
   let(:airport) { Airport.new }
 
   describe '#capacity' do
     it "should have default capacity" do
       expect(airport.capacity).to eq(Airport::DEFAULT_CAPACITY)
     end
-    it "should have capacity set when created" do
+    it "should have correct capacity when created specified capacity" do
       [4, 6, 7, 8, 343].each do |num|
         expect(Airport.new(num).capacity).to eq(num)
       end
