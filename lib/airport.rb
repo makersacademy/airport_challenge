760b80265d318b439ddf9c490040ed914953215e
Sophie <sophiebrown438@gmail.com>
2020-05-16 12:56:37 +0100

    Plane at airport

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,18 +1,32 @@
+require 'weather'
+
 class Airport
+    DEFAULT_CAPACITY = 35
     attr_reader :capacity
-    attr_accessor :plane
+    attr_accessor :airport
+
+  def initialize(capacity = DEFAULT_CAPACITY)
+     @airport = []
+     @capacity = capacity
+  end
  
   def takeoff 
-    fail "Too stormy to take-off" if stormy?
-    # puts "The flight has taken off"
+    # fail "Too stormy to take-off" if stormy?
+    puts "The flight has taken off"
+    @airport.pop
     true
   end
 
-  def land
-    fail "Too stormy to land" if stormy?
-  end
+  def land(plane)
+     fail "Airport at capacity" if full?
+    @airport.push(plane) 
+end
   
-  def stormy?
-    true
+#   def stormy?
+#     random_boolean = [true, false].sample
+#   end
+
+  def full?
+    airport.count >= @capacity
   end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -4,26 +4,23 @@ describe Airport do
   let(:plane) { Airport.new } 
 
   describe "#airport" do
-    it { is_expected.to respond_to :plane }
+    it { is_expected.to respond_to :airport }
 
     it "should have a plane take off and confirm it has gone" do
     expect(plane.takeoff).to eq true
     end
 
-    # it "raise an error if airport is full" do
-    # # airport.capacity.times { airport.land(plane) }
-    # expect { airport.land }.to raise_error "Airport at capacity"
-    # end
+    it "should raise an error if airport is full" do
+    plane.capacity.times { plane.land(plane) }
+    expect { plane.land(plane) }.to raise_error "Airport at capacity"
+    end
   end
-  describe "#stormy" do
-  it { is_expected.to respond_to :stormy? }
 
-  it "should prevent landing when stormy" do 
-    expect { plane.land }.to raise_error "Too stormy to land"
- end
+#   it "should prevent landing when stormy" do 
+#     expect { plane.land.stormy? }.to raise_error "Too stormy to land"
+#  end
 
- it "should prevent take-off when stormy" do 
-    expect { subject.takeoff }.to raise_error "Too stormy to take-off"
- end
-  end
+#  it "should prevent take-off when stormy" do 
+#     expect { subject.takeoff }.to raise_error "Too stormy to take-off"
+#  end
 end
