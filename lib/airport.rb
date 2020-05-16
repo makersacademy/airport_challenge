a8b9c4d32c3cb4036271c6fcbdbdbd1047295085
Nick Butler <nwmbutler@gmail.com>
2020-05-16 16:33:40 +0100

    G is_full doesn't allow landings

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,5 +1,20 @@
 class Airport
+@planes = []
+@capacity
+
   def land
   end
+
+  def is_full?
+    fail "Airport FULL" if full?
+  end
+
+
+  def full?
+    true
+  end
+
+def empty?
+end
 
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,9 +2,14 @@ require 'airport'
 require 'plane'
 
 describe Airport do
+ap = Airport.new
 
   it "allows a plane to land at the airport" do
-    ap = Airport.new
     expect(ap).to respond_to(:land)
   end
+
+  it "prevents landing when airport is full" do
+    expect{ap.is_full?}.to raise_error('Airport FULL')
+  end
+
 end
