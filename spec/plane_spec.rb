1201b7ad8a60e5ffd46256dddb62971c89276b48
Olatunji Oduola <ola.oduola@gmail.com>
2020-05-16 15:40:08 +0100

    Created unit test for take_off method

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -1,5 +1,10 @@
 require 'plane'
 
 describe Plane do
-  
+  it 'respnds to #land' do
+    expect(subject).to respond_to(:land)
+  end
+  it 'respnds to #take_off' do
+    expect(subject).to respond_to(:take_off)
+  end
 end
