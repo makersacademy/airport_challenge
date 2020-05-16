4600bcdf1922a44d26dbda573b16c13a64d9a597
Olatunji Oduola <ola.oduola@gmail.com>
2020-05-17 00:18:35 +0100

    Updated spec and program for at_airport?

--- lib/plane.rb
+++ lib/plane.rb
@@ -3,6 +3,5 @@ require_relative 'airport'
 class Plane
 
   def at_airport?
-    false
   end
 end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -4,12 +4,4 @@ describe Plane do
   it 'responds to #at_airport?' do
     expect(subject).to respond_to(:at_airport?)
   end
-
-  describe '#at_airport' do
-    it 'returns false when plane takes off' do
-      airport = Airport.new
-      subject = airport.take_off
-      expect(subject.at_airport?).to eq(false)
-    end
-  end
 end
