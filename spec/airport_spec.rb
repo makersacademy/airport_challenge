dca25238c122e701f66faf8ee8a6855d08d41bac
J Perrin <perrinjack96@gmail.com>
2020-05-16 14:03:22 +0100

    README updates to include user stories

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,4 +1,4 @@
-require 'airport.rb'
+require 'airport'
 
 describe Airport do
 
@@ -8,12 +8,44 @@ describe Airport do
     expect(airport).to respond_to :takeoff
   end
 
+  it 'plane takes off and is no longer at the airport' do
+    easyjet = Plane.new
+    allow(subject).to receive(:bad_weather?).and_return(false)
+    subject.land(easyjet)
+    subject.takeoff(easyjet)
+    expect(easyjet.flying?).to eq true
+  end
+
+  it 'prevents #takeoff when weather is stormy' do
+    allow(airport).to receive(:bad_weather?).and_return(true)
+    expect { airport.takeoff(Plane.new) }.to raise_error 'No departures due to adverse weather'
+  end
+
   it 'responds to #land' do
     expect(airport).to respond_to :land
   end
 
   it '#land returns an instance of plane' do
-    expect(airport.land(Plane.new)).to be_an_instance_of(Plane)
+    allow(airport).to receive(:bad_weather?).and_return(false)
+    expect(airport.land(Plane.new)).to all(be_a(Plane))
+  end
+
+  it "Prevents landing when Airport is full" do
+    allow(airport).to receive(:bad_weather?).and_return(false)
+    Airport::DEFAULT_CAPACITY.times { airport.land(Plane.new) }
+    expect { airport.land(Plane.new) }.to raise_error 'Airport is full'
+  end
+
+  it "Prevents landing when Airport is full with capacity not equal to default" do
+    big_airport = Airport.new(100)
+    allow(big_airport).to receive(:bad_weather?).and_return(false)
+    100.times { big_airport.land(Plane.new) }
+    expect { big_airport.land(Plane.new) }.to raise_error 'Airport is full'
+  end
+
+  it "Prevents #landing when weather is stormy" do
+    allow(airport).to receive(:bad_weather?).and_return(true)
+    expect { airport.land(Plane.new) }.to raise_error 'No arrivals due to adverse weather'
   end
 
 end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -1,1 +1,22 @@
+require 'plane'
 
+describe Plane do
+  it 'responds to touch down' do
+    expect(subject).to respond_to :touch_down
+  end
+
+  it 'responds to lift off' do
+    expect(subject).to respond_to :lift_off
+  end
+
+  it 'updates flying? state when landing' do
+    subject.touch_down
+    expect(subject.flying?).to eq false
+  end
+
+  it 'updates flying? state when taking off' do
+    subject.lift_off
+    expect(subject.flying?).to eq true
+  end
+
+end
