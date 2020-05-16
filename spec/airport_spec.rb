9fbaa1387b8c2fd205fd268d1387995ed824652a
Olatunji Oduola <ola.oduola@gmail.com>
2020-05-17 00:16:14 +0100

    Updated test for #land(plane) and #take_off(plane)

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,22 +1,18 @@
 require 'airport'
 
 describe Airport do
-  it 'responds to #land plane' do 
-    expect(subject).to respond_to(:land).with(1).argument
+
+  it '#lands a plane' do 
+    plane1 = Plane.new
+    expect(subject.land(plane1)).to eq(plane1)
   end
 
-  it 'responds to #take_off plane' do 
-    expect(subject).to respond_to(:take_off)
+  it ' #take_off a plane' do 
+    plane = Plane.new
+    expect(subject.take_off(plane)).to eq(plane)
   end
 
   it 'responds to #full?' do 
     expect(subject).to respond_to(:full?)
-  end
-  
-  describe '#take_off' do
-    it 'releases a plane' do
-      plane = subject.take_off 
-      expect(plane).to be_instance_of(Plane)
-    end
   end
 end
