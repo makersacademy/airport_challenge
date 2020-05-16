f2783f6d3f140d082fa58f58de4ba565765ba32c
Olatunji Oduola <ola.oduola@gmail.com>
2020-05-17 00:35:29 +0100

    Created test for #at_airport to return false if plane has taken off

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -4,4 +4,10 @@ describe Plane do
   it 'responds to #at_airport?' do
     expect(subject).to respond_to(:at_airport?)
   end
+
+  it 'returns false once taken off' do
+    airport = Airport.new
+    airport.take_off(subject)
+    expect(subject.at_airport?).to eq(false)
+  end
 end
