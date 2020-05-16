d66da0cee5d545eb99c518c9ff2178de17aad8ee
Rachael <rachaelewins@hotmail.com>
2020-05-16 07:18:37 +0100

    refactor on plane.rb and plane_spec.rb

--- lib/plane.rb
+++ lib/plane.rb
@@ -1,7 +1,5 @@
 class Plane
 
-  attr_reader :location
-
   def initialize
     @location = "air"
   end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -5,40 +5,21 @@ describe Plane do ## update test when airport class is created
   let(:plane) { Plane.new }
 
   describe '#land_at(airport)' do
-    it "should be at the airport it landed at," do
-      plane.land_at("airport")
-      expect(plane.location).to eq("airport")
-    end
-    it "and tell the operator that it landed" do
+    it "should be at the airport it landed at,and tell the operator that it landed" do
       expect { plane.land_at("airport") }.to output("landed at airport\n").to_stdout
     end
-    it "should only be able to land if it is flying, " do
-      plane.land_at("airportA")
-      plane.land_at("airportB")
-      expect(plane.location).to eq("airportA")
-    end
-    it "and tell the operator about the problem" do
+    it "should only be able to land if it is flying, and tell the operator about the problem" do
       plane.land_at("airportA")
       expect { plane.land_at("airportB") }.to output("currently located at airportA so can not land\n").to_stdout
     end
   end
 
   describe '#takeoff_from(airport)' do
-    it "should leave airport and begin flying," do
-      plane.land_at("airport")
-      plane.takeoff_from("airport")
-      expect(plane.location).to eq("air")
-    end
-    it "and tell the operator it departed" do
+    it "should leave airport and begin flying, and tell the operator it departed" do
       plane.land_at("airport")
       expect { plane.takeoff_from("airport") }.to output("departed airport\n").to_stdout
     end
-    it "shouldn't takeoff from somewhere it is not," do
-      plane.land_at("airportA")
-      plane.takeoff_from("airportB")
-      expect(plane.location).to eq("airportA")
-    end
-    it "and tell the operator about the problem" do
+    it "shouldn't takeoff from somewhere it is not, and tell the operator about the problem" do
       plane.land_at("airportA")
       expect { plane.takeoff_from("airportB") }.to output("currently located at airportA so can not takeoff\n").to_stdout
     end
