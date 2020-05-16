a39f1de371ef742851d3dcc1f045ba2e5f22b51a
Olatunji Oduola <ola.oduola@gmail.com>
2020-05-16 20:55:30 +0100

    Updated test so it responds to take_off method

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -4,4 +4,8 @@ describe Airport do
   it 'responds to #land plane' do 
     expect(subject).to respond_to(:land).with(1).argument
   end
+
+  it 'responds to #take_off plane' do 
+    expect(subject).to respond_to(:take_off).with(1).argument
+  end
 end
