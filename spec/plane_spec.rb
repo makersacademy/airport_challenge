57ab05d334a377fef00aced070d6b423d2849e4e
Rachael <rachaelewins@hotmail.com>
2020-05-16 05:48:17 +0100

    removed a test that is covered by later test in plane_spec.rb

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -3,9 +3,6 @@ require_relative '../lib/plane.rb'
 describe Plane do
   let(:plane) {Plane.new}
   describe '#land_at(airport)' do
-    it "should respond to" do
-      expect(plane).to respond_to(:land_at).with(1).argument
-    end
     it "should be at the airport it landed at" do ## update test when airport class is created
       plane.land_at("airport")
       expect(plane.location).to eq("airport")
