a0de4aa6bd8bff64e7c3c7fe222d5f5003ea81d2
Nikita <latinisnotdead07@gmail.com>
2020-05-16 16:26:48 +0100

    refactors airport_spec to remove respond_to(:method) examples

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -4,12 +4,10 @@ describe Airport do
   subject(:airport) { Airport.new }
 
   it 'has a default capacity' do
-    expect(airport).to respond_to(:capacity)
     expect(airport.capacity).to eq(100)
   end
 
   it 'is able to have planes' do
-    expect(airport).to respond_to(:planes)
     expect(airport.planes).to be_an_instance_of(Array)
   end
 
