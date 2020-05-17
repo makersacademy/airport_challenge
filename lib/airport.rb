c2ab40aad96a053d726a0424d1d2eab53552d816
nadine <nadine.loepfe@gmail.com>
2020-05-17 14:49:42 +0100

    added initialize method

--- lib/airport.rb
+++ lib/airport.rb
@@ -2,6 +2,9 @@ require_relative 'plane'
 
 class Airport
 
+  def initialize(capacity)
+  end
+
   def land(plane)
   end
 
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,12 +2,12 @@ require 'airport'
 require 'plane'
 
 describe Airport do
-  subject(:airport) { described_class.new }
+  subject(:airport) { described_class.new(20) }
   it 'instructs a plane to land' do
     expect(airport).to respond_to(:land).with(1).argument
   end
 
   it 'instructs a plane to take off' do
   expect(airport).to respond_to(:take_off).with(1).argument
-end
+  end
 end
