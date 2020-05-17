ca461771b18343e20ba10ad5dc19206108a6c92b
Katie McDonagh <mcdonagh.katie@outlook.com>
2020-05-17 13:37:30 +0100

    unit test RED introduce land method to accept an argument

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,7 +1,5 @@
 require 'airport'
 
 describe Airport do
-  it "allows a plane to land at the selected airport" do
-    expect(subject).to respond_to :land 
-  end
+  it { is_expected.to respond_to(:land).with(1).argument }
 end
