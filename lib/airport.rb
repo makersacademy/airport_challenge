02acf736fbd35d70c6dc37e8339ba00fcb152ab8
victorvallet <victorvallet8@gmail.com>
2020-05-17 17:47:09 +0200

    Initialized Airport, passed test for take_off

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,3 +1,15 @@
-class Airport
+require_relative 'plane'
 
+class Airport
+  def initialize
+    @planes = []
+  end
+
+  def land(plane)
+    @planes.push(plane)
+  end
+
+  def take_off(plane)
+    @planes - [plane]
+  end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,8 +1,22 @@
 require './lib/airport'
+require './lib/plane'
 
 describe Airport do
-    subject(:airport) {Airport.new}
+    let(:airport) {Airport.new}
+    let(:plane) {Plane.new}
     it 'exists' do
         expect(airport).to be_instance_of(Airport)
+    end
+    it 'responds to #land' do
+    expect(airport).to respond_to(:land)
+    end
+    it 'is waiting at the airport after landing' do
+        expect(airport.land(plane)).to include(plane)
+    end
+    it 'responds to #take_off' do
+        expect(airport).to respond_to(:take_off)
+    end
+    it 'leaves the airport and finally take off, see you soon!' do
+        expect(airport.take_off(plane)).not_to include(plane)
     end
 end
