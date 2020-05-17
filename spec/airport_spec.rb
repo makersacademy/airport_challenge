4637f0b962a54900003b1437e32570d2569d8452
Katie McDonagh <mcdonagh.katie@outlook.com>
2020-05-17 14:12:59 +0100

    add to unit test to expect .land to return plane object into airport object

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,5 +1,13 @@
 require 'airport'
 
+# describe Airport do
+#   # land
+#   it { is_expected.to respond_to(:land).with(1).argument }
+# end
+
 describe Airport do
-  it { is_expected.to respond_to(:land).with(1).argument }
+  it 'lands the plane' do
+    plane = Plane.new
+    expect(subject.land(plane)).to eq plane
+  end
 end
