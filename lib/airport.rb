b50bbc683306bcdc66c1bb7204c957c6795410ca
nadine <nadine.loepfe@gmail.com>
2020-05-18 00:29:50 +0100

    User Story 9 done

--- lib/airport.rb
+++ lib/airport.rb
@@ -14,28 +14,39 @@ class Airport
     raise 'Cannot land plane: airport full' if full?
     raise 'Cannot land plane: weather is stormy' if stormy?
     plane.land(self)
-    @planes << plane
+    add_plane(plane)
   end
 
   def take_off(plane)
     raise 'Cannot take off plane: weather is stormy' if stormy?
     raise 'Cannot take off plane: plane not at this airport' unless at_airport?(plane)
     plane.take_off
-    @planes.pop
+    remove_plane(plane)
     plane
   end
 
   private
 
+attr_reader :capacity, :weather_service
+
   def full?
-    @planes.length >= @capacity
+    planes.length >= capacity
   end
 
   def stormy?
-    @weather_service.stormy?
+    weather_service.stormy?
   end
 
   def at_airport?(plane)
-    @planes.include?(plane)
+    planes.include?(plane)
   end
+
+  def add_plane(plane)
+    planes << plane
+  end
+
+  def remove_plane(plane)
+    planes.pop
+  end
+
 end
--- lib/plane.rb
+++ lib/plane.rb
@@ -4,17 +4,26 @@ class Plane
   end
 
   def take_off
-    raise 'Plane cannot take off: plane already flying' if @flying
+    raise 'Plane cannot take off: plane already flying' if flying
   end
 
   def land(airport)
-    raise 'Plane cannot land: plane already landed' unless @flying
+    raise 'Plane cannot land: plane already landed' if landed
     @flying = false
     @airport = airport
   end
 
   def airport
-    raise 'Plane cannot be at an airport: plane already flying' if @flying
+    raise 'Plane cannot be at an airport: plane already flying' if flying
     @airport
   end
+
+  private
+
+  attr_reader :flying
+
+  def landed
+    !flying
+  end
+
 end
