f1c9e6589ddfb6677fbfc6dcbdc0e20f1d48f8d1
Olatunji Oduola <ola.oduola@gmail.com>
2020-05-17 15:51:33 +0100

    Wrote unit test to specify Plane object is returned after take off

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,16 +1,26 @@
 require 'airport'
 
 describe Airport do
-  it 'responds to #land' do 
+  it 'responds to #land with 1 argument' do 
     expect(subject).to respond_to(:land).with(1).argument
   end
 
-  it 'returns plane after #land' do
-    plane = Plane.new
-    expect(subject.land(plane)).to eq(plane)
+  describe '#land' do
+    it 'returns plane after landing' do
+      plane = Plane.new
+      expect(subject.land(plane)).to eq(plane)
+    end
   end
 
-  it 'responds to #take_off' do
+  it 'responds to #take_off with 1 argument' do
     expect(subject).to respond_to(:take_off).with(1).argument
   end
+
+  describe '#take_off' do
+    it 'returns plane after take_off' do
+      plane = Plane.new
+      expect(subject.take_off(plane)).to eq(plane)
+    end
+  end
+
 end
