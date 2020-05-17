4f8ce614031381a6e7cd81dbbfdbbb421ef43af3
minsi yang <yangminsi@gmail.com>
2020-05-17 21:11:18 +0100

    Added test to set airport default capacity method to DEFAULT_CAPACITY constant of Airport class. Added attr_reader for capacity and planes method

--- airport/lib/airport.rb
+++ airport/lib/airport.rb
@@ -2,15 +2,18 @@ require_relative "plane"
 
 class Airport
   DEFAULT_CAPACITY = 30
-  # attr_reader :planes
+
+  attr_reader :capacity
 
   def initialize
     @planes = []
+    @capacity = DEFAULT_CAPACITY
+
   end
 
   def land(plane)
     raise "Airport is full" if full?
-    @planes << plane
+    planes << plane
 
   end
 
@@ -20,8 +23,10 @@ class Airport
 
   private
 
+  attr_reader :planes
+
   def full?
-    @planes.count >= DEFAULT_CAPACITY
+    planes.count >= capacity
   end
 
 end
--- airport/spec/airport_spec.rb
+++ airport/spec/airport_spec.rb
@@ -2,33 +2,23 @@ require 'airport'
 
 describe Airport do
 
+  # test for a default capacity for the initialize method
+  it 'has a default capacity' do
+    expect(subject.capacity).to eq Airport::DEFAULT_CAPACITY
+  end
+
   # test for land method for Airport
   it { is_expected.to respond_to :land }
   it { is_expected.to respond_to(:land).with(1).argument}
-  # describe "#land" do
-  #   it "land a plane in airport" do
-  #     plane = Plane.new
-  #     expect(subject.land(plane)).to eq plane
-  #   end
-  # end
 
   describe "#land" do
     it "raise an error when full" do
-      Airport::DEFAULT_CAPACITY.times do
+      subject.capacity.times do
         subject.land(Plane.new)
       end
       expect{ subject.land(Plane.new) }.to raise_error "Airport is full"
     end
   end
-
- # # test for showing landed plane (instance) method for Airport
- #  it { is_expected.to respond_to :plane}
- #
- #  it "#planes" do
- #    plane = Plane.new
- #    subject.land(plane)
- #    expect(subject.plane).to eq plane
- #  end
 
  # test for take_off method for Airport
   it { is_expected.to respond_to :take_off }
