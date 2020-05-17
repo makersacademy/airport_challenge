bf4d31dd69cb95ec623c7fee6c5dd42ec6160834
Katie McDonagh <mcdonagh.katie@outlook.com>
2020-05-17 15:26:09 +0100

    add RED unit test for .takeoff method

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,8 +2,14 @@ require 'airport'
 
 describe Airport do
   it { is_expected.to respond_to(:land).with(1).argument }
+
   it 'lands the plane' do
     plane = @plane
     expect(subject.land(plane)).to eq plane
   end
-end
+
+  it 'allows planes to take off' do
+    expect(subject).to respond_to :takeoff
+  end
+  end
+  
