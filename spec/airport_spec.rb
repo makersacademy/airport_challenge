ff4514f540f2059246ca84d9e9cb915e437e0e01
Olatunji Oduola <ola.oduola@gmail.com>
2020-05-17 14:28:06 +0100

    Wrote test to specify the return of a Plane object after landing

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,6 +2,11 @@ require 'airport'
 
 describe Airport do
   it 'responds to #land' do 
-    expect(subject).to respond_to(:land)
+    expect(subject).to respond_to(:land).with(1).argument
+  end
+
+  it 'returns plane after #land' do
+    plane = Plane.new
+    expect(subject.land(plane)).to eq(plane)
   end
 end
