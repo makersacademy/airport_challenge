adf018fe3f7f79a540cdfe4287e5e6f0b836ce70
Joshua Brook <63238425+joshuaabrookuk@users.noreply.github.com>
2020-05-18 10:37:18 +0100

    Updated airport spec

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,14 +1,13 @@
 require 'airport'
 
-
 describe Airport do
 
   it 'should instruct a plane to land at an airport' do
-expect(subject).to respond_to(:land).with(1).argument
+    expect(subject).to respond_to(:land).with(1).argument
   end
-
+  
   it 'should instruct a plane to take off from an airport and confirm empty' do
-expect(subject).to respond_to(:take_off).with(1).argument
+    expect(subject).to respond_to(:take_off).with(1).argument
   end
 
    describe '#land' do
@@ -18,12 +17,8 @@ expect(subject).to respond_to(:take_off).with(1).argument
     end
   end
 
-  # it 'airport capacity that can be overridden' do
-  # expect(subject).to respond_to(:capacity).with(1).argument
-  # end
-
   it 'airport capacity that can be overridden' do
-  expect(subject).to respond_to(:capacity).with(1).argument
+    expect(subject).to respond_to(:capacity).with(1).argument
   end
 
 end
