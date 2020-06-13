9576b7f99faaec81bc1d321b9ee0754dd6c13424
Alex Klink <alex_klink123@hotmail.co.uk>
2020-06-13 19:55:20 +0000

    land(plane) method created and tested for airport instance. User Story 1 complete

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -3,4 +3,10 @@ describe Airport do
   it 'should create an instance of an airport' do
     expect(Airport.new).to be_an_instance_of(Airport)
   end
+  
+  describe '#land' do
+    it 'should land a plane' do
+      expect(subject).to respond_to(:land).with(1).argument 
+    end
+  end
 end
