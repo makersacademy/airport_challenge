dbf73f41ee25e4386b90a72505cef8aad70d71a9
Alex Klink <alex_klink123@hotmail.co.uk>
2020-06-13 20:00:15 +0000

    User story 1 complete: refactoring done w let statements

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,12 +1,15 @@
 require 'airport'
+
 describe Airport do
+  let(:airport) { Airport.new }
+  
   it 'should create an instance of an airport' do
-    expect(Airport.new).to be_an_instance_of(Airport)
+    expect(airport).to be_an_instance_of(Airport)
   end
   
   describe '#land' do
     it 'should land a plane' do
-      expect(subject).to respond_to(:land).with(1).argument 
+      expect(airport).to respond_to(:land).with(1).argument 
     end
   end
 end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -1,6 +1,7 @@
 require 'plane'
 describe Plane do
+  let(:plane) { Plane.new }
   it 'creates new instance of Plane class' do
-    expect(Plane.new).to be_an_instance_of(Plane)
+    expect(plane).to be_an_instance_of(Plane)
   end
 end
