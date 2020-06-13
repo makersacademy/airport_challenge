1198939ab125105e365bfa7e28140a2b3bb0a12e
sam landman <sam landman>
2020-06-13 13:35:52 +0100

    Takeoff and Landed functionality included"

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,4 +1,22 @@
-
+require_relative('plane')
 
 class Airport
+    attr_accessor :planes, :capacity
+
+    def initialize(capacity = 1)
+        @planes = []
+        @capacity = capacity
+    end
+
+    def land(plane)
+        full ? "fail" : @planes << plane && "The plane has landed"
+    end
+    def takeoff(plane)
+        @planes = []
+        true
+    end
+    
+    def full
+    @planes.count >= @capacity ? true : false
+    end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,7 +1,34 @@
 require('airport')
 
 describe 'airport' do
+
     it 'creates a new class of airport' do
         expect(Airport.new).to be_an_instance_of(Airport)
     end
+
+    it 'allows a plane to land at an airport' do
+        airport = Airport.new
+        plane = Plane.new
+        expect(airport.land(plane)).to eq("The plane has landed")
+    end
+
+    it 'allows a plane to take off' do
+        airport = Airport.new
+        plane = Plane.new
+        expect(airport.takeoff(plane)).to eq(true)
+    end
+
+    it 'expects there to be an empty array for planes as a default' do
+        airport = Airport.new
+        expect(airport.planes).to eq([])
+    end
+
+    it 'expects a landing to be prevented if airport is full' do
+        airport = Airport.new
+        plane1 = Plane.new
+        plane2 = Plane.new
+        airport.land(plane1)
+        expect(airport.land(plane2)).to eq("fail")
+    end
+
 end
