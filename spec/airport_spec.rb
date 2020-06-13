22adcbb0b607b569110a0344fc347c3ac38c60dd
Catriona <catrionamairi@blueyonder.co.uk>
2020-06-13 17:40:06 +0100

    Improved tests including deleting some redundant ones

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,61 +1,52 @@
 require 'airport'
 
 describe Airport do
-  it 'lands a plane' do
-    expect(subject).to respond_to(:land)
+  before(:each) do
+    @airport = Airport.new
+    @plane = Plane.new
   end
 
-  it 'takes off a plane' do
-    expect(subject).to respond_to(:take_off)
+  it 'has a default capacity' do
+    expect(@airport.capacity).to eq Airport::DEFAULT_CAP
+  end
+
+  describe '#land' do
+    it 'lands a plane' do
+      allow(@airport.weather).to receive(:stormy?) { false }
+      @airport.land(@plane)
+      expect(@airport.planes).to eq [@plane]
+    end
+
+    it 'raises error if airport is full' do
+      allow(@airport.weather).to receive(:stormy?) { false }
+      Airport::DEFAULT_CAP.times { @airport.land(@plane) }
+      expect { @airport.land(@plane) }.to raise_error("Airport is full")
+    end
   end
 
   describe '#take_off' do
     it 'takes off a plane' do
-      allow(subject.weather).to receive(:stormy?) { false }
-      plane = Plane.new
-      subject.land(plane)
-      expect(subject.take_off).to eq plane
+      allow(@airport.weather).to receive(:stormy?) { false }
+      @airport.land(@plane)
+      @airport.take_off
+      expect(@airport.planes).to eq []
+    end
+
+    it 'raises error if no planes in airport' do
+      allow(@airport.weather).to receive(:stormy?) { false }
+      expect { @airport.take_off }.to raise_error("No planes in airport")
     end
   end
 
-  it "attr_reader" do
-    expect(subject).to respond_to(:planes)
-  end
-
-  it "attr_reader" do
-    expect(subject).to respond_to(:weather)
-  end
-
-  describe 'initialization' do
-    subject { Airport.new }
-    let(:plane) { Plane.new }
-    it 'defaults capacity' do
-      allow(subject.weather).to receive(:stormy?) { false }
-      described_class::DEFAULT_CAP.times do
-        subject.land(plane)
-      end
-      expect { subject.land(plane) }.to raise_error("Airport is full")
+  describe 'Raises error if stormy weather' do
+    it 'does not land plane' do
+      allow(@airport.weather).to receive(:stormy?) { true }
+      expect { @airport.land(@plane) }.to raise_error("Weather is stormy, don't land!")
     end
-  end
 
-  describe 'no planes in airport' do
-    it 'raises error' do
-      allow(subject.weather).to receive(:stormy?) { false }
-      expect { subject.take_off }.to raise_error("No planes in airport")
-    end
-  end
-
-  describe 'Not to land in stormy weather' do
-    it 'raises error' do
-      allow(subject.weather).to receive(:stormy?) { true }
-      expect { subject.land Plane.new }.to raise_error("Weather is stormy, don't land!")
-    end
-  end
-
-  describe 'Not to take off in stormy weather' do
-    it 'raises error' do
-      allow(subject.weather).to receive(:stormy?) { true }
-      expect { subject.take_off }.to raise_error("Weather is stormy, don't take off!")
+    it 'does not let plane take off' do
+      allow(@airport.weather).to receive(:stormy?) { true }
+      expect { @airport.take_off }.to raise_error("Weather is stormy, don't take off!")
     end
   end
 end
--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -1,13 +1,17 @@
 require 'weather'
 
 describe Weather do
+  before(:each) do
+    @weather = Weather.new
+  end
+
   it 'checks what the weather is doing' do
-    allow(subject).to receive(:rand) { 1 }
-    expect(subject.check).to eq(1)
+    allow(@weather).to receive(:rand) { 1 }
+    expect(@weather.check).to eq(1)
   end
 
   it 'sees if the weather is stormy' do
-    allow(subject).to receive(:check) { 1 }
-    expect(subject).not_to be_stormy
+    allow(@weather).to receive(:check) { 1 }
+    expect(@weather).not_to be_stormy
   end
 end
