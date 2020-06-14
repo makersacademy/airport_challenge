4c99848dbd1484c866d1b030c2e786bf2cf04194
Alex Klink <alex_klink123@hotmail.co.uk>
2020-06-14 10:41:56 +0000

    User Story 2: airport.plane will show a landed instance of a plane

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -11,9 +11,11 @@ describe Airport do
     it 'should land a plane' do
       expect(airport).to respond_to(:land).with(1).argument 
     end
-    it 'should return the plane that is landed' do
+    it 'should return the plane that was just landed' do
       plane = Plane.new
-      expect(airport.land(plane)).to eq(plane)
+      # expect(airport.land(plane)).to eq(plane) : old test
+      airport.land(plane)
+      expect(airport.plane). to eq plane
     end
   end
   
