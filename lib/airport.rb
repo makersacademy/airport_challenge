1f31e0eb255a7fb360aa4e25a15c4a2b506853e8
Alex Klink <alex_klink123@hotmail.co.uk>
2020-06-14 10:35:25 +0000

    User story 2: When a plane is landed, it is now returned in the method

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,7 +1,11 @@
 class Airport
   def land(plane)
+    plane
   end
   
   def take_off(plane)
+  end
+  
+  def plane
   end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -11,11 +11,21 @@ describe Airport do
     it 'should land a plane' do
       expect(airport).to respond_to(:land).with(1).argument 
     end
+    it 'should return the plane that is landed' do
+      plane = Plane.new
+      expect(airport.land(plane)).to eq(plane)
+    end
   end
   
   describe '#take_off' do
     it 'should allow a plane to take off' do
       expect(airport).to respond_to(:take_off).with(1).argument
+    end
+  end
+  
+  describe '#plane' do
+    it 'should show you a plane that is in the airport' do
+      expect(airport).to respond_to :plane
     end
   end
 end
