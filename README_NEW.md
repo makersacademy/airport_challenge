bcab670b919e9190a54cf3e44286122383c12a98
India Rex <face940@gmail.com>
2020-06-14 13:32:48 +0100

    plane responds to land

--- README_NEW.md
+++ README_NEW.md
@@ -1,1 +1,6 @@
 Airport Shmairport
+
+steps:
+create airport class
+create plane class
+be able to land a plane at an airport
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -1,5 +1,9 @@
 require 'plane'
 
 describe Plane do
-
+  describe '#land' do
+    it 'responds to land' do
+      expect(subject).to respond_to :land
+    end
+  end
 end
