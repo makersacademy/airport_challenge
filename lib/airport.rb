bd839687a93f16dba8f9069c62d371128dfdfa30
Student <student@Makerss-Air.broadband>
2020-07-11 17:37:01 +0100

    Airports can be landed at by planes, those planes can then takeoff again and they will no longer be in the airport

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,4 +1,13 @@
 require_relative 'plane'
 class Airport
-
+  attr_reader :planes
+  def initialize
+    @planes = []
+  end
+  def land(plane)
+    @planes << plane
+  end
+  def takeoff(plane)
+    @planes.delete(plane)
+  end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,4 +1,13 @@
 require 'airport'
 describe Airport do
-
+  it 'can land a plane' do
+    plane = Plane.new
+    expect(subject.land(plane)).to include(plane)
+  end
+  it 'can allow a plane to takeoff' do
+    plane = Plane.new
+    subject.land(plane)
+    subject.takeoff(plane)
+    expect(subject.planes).not_to include(plane)
+  end
 end
