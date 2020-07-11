813a139655f5468d9c61f2f6dae22bf2a379d21b
Ryan Viegas <rjkviegas@gmail.com>
2020-07-11 13:33:13 +0100

    added respond to capacity test and default capacity test for Airplane class

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,11 +1,18 @@
 require_relative 'plane'
 
 class Airport
+    attr_reader :capacity
+
+    DEFAULT_CAPACITY = 100
+
+    def initialize(capacity=DEFAULT_CAPACITY)
+        @capacity = capacity
+    end
 
     def full?
     end
 
     def stormy?
     end
-    
+
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -8,4 +8,13 @@ describe Airport do
     describe '#stormy?' do
         it {is_expected.to respond_to :stormy?}
     end
+
+    describe '#capacity' do
+        it {is_expected.to respond_to :capacity}
+    end
+
+    it '#default capacity of airport objects' do
+        expect(Airport.new.capacity).to eq Airport::DEFAULT_CAPACITY
+    end
+
 end
