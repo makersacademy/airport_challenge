63bf0e42c9ec849862e01efb171688fc550dc698
Ryan Viegas <rjkviegas@gmail.com>
2020-07-11 13:54:55 +0100

    added weather method test and that weather results in a element from WEATHER_CONDITIONS tes

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,12 +1,14 @@
 require_relative 'plane'
 
 class Airport
-    attr_reader :capacity
+    attr_reader :capacity, :weather
 
     DEFAULT_CAPACITY = 100
+    WEATHER_CONDITIONS = ["Sunny", "Stormy"]
 
     def initialize(capacity=DEFAULT_CAPACITY)
         @capacity = capacity
+        @weather = WEATHER_CONDITIONS.sample
     end
 
     def full?
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -11,10 +11,20 @@ describe Airport do
 
     describe '#capacity' do
         it {is_expected.to respond_to :capacity}
+        it 'default capacity assigned when none provided' do
+            expect(Airport.new.capacity).to eq Airport::DEFAULT_CAPACITY
+        end
     end
 
-    it '#default capacity of airport objects' do
-        expect(Airport.new.capacity).to eq Airport::DEFAULT_CAPACITY
-    end
+    describe '#weather' do
+        it {is_expected.to respond_to :weather}
+        it 'weather condition is a String' do
+            expect(Airport.new.weather).to be_a(String)
+        end
 
+        it 'weather to be from WEATHER_CONDITIONS' do
+            airport1 = Airport.new
+            expect(Airport::WEATHER_CONDITIONS).to include(airport1.weather)
+        end
+    end
 end
