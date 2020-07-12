209944dbf3a4ea8fd2ee536ac993cf055dfc1e00
EC2 Default User <ec2-user@ip-172-31-55-228.ec2.internal>
2020-07-12 18:11:36 +0000

    default and variable capacity added

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,11 +1,17 @@
 require "./lib/plane.rb"
 
 class TrafficControl
-  def initialize
+  attr_reader :runway
+  attr_accessor :capacity
+  DEFAULT_CAPACITY = 20
+  def initialize(capacity = DEFAULT_CAPACITY)
+    @capacity = capacity
     @runway = []
   end
   
   def land(plane)
+    fail "Runway at maximum capacity" if full?
+    
     @runway << plane
   end
   
@@ -15,5 +21,9 @@ class TrafficControl
   
   def show_runway
     @runway
+  end
+  
+  def full?
+    @runway.count >= @capacity
   end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -15,3 +15,16 @@ describe "TrafficControl" do
     expect(atc.show_runway).to eq []
   end
 end
+
+describe initialize do
+  it "does not allow planes to land when airport is full" do
+    atc = TrafficControl.new(50)
+    50.times { atc.land(Plane.new) }
+    expect { atc.land(Plane.new) }.to raise_error "Runway at maximum capacity"
+  end
+  
+  it "has a default capacity" do
+    atc = TrafficControl.new(5)
+    expect(atc.capacity).to eq 5
+  end
+end
