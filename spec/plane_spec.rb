542459d149421b1378d0480947a093816a53f984
Daniel M <danielim@hotmail.co.uk>
2020-07-12 16:42:11 +0100

    Completed Airport challenge

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -3,7 +3,23 @@ require_relative '../lib/plane'
 
 describe Plane do
   it { is_expected.to be_an(Plane) }
+  it { is_expected.to(respond_to :flying?) }
+  it { is_expected.to(respond_to :land) }
+  it { is_expected.to(respond_to :takeoff) }
   it 'Should either be flying or not' do
     expect(subject.flying).to eq(true).or(false)
+    expect(subject.flying?).to eq(true).or(false)
+  end
+  it 'Can only #land when in flight' do
+    expect{ Plane.new(false).land }.to raise_error("You are not in flight")
+  end
+  it 'Sets flying to false after landing' do
+  expect(Plane.new(true).land).to eq(false)
+  end
+  it 'Can only #takeoff when not in flight' do
+    expect{Plane.new(true).takeoff}.to raise_error("You are not grounded")
+  end
+  it 'Sets flying to true after takeoff' do
+    expect(Plane.new(false).takeoff).to eq(true)
   end
 end
--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -1,1 +1,9 @@
+require 'rspec'
+require_relative './spec_helper'
+require_relative '../lib/airport'
 
+describe Weather do
+  it { is_expected.to(respond_to :stormy?) }
+  it { is_expected.to(respond_to :weather_types) }
+  it { expect(Weather::WEATHER_TYPES).to be_an Array }
+end
