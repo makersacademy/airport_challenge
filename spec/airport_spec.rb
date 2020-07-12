dc2f3b53e3b2078ece7c3947acc4ae10631d1f57
Ryan Viegas <rjkviegas@gmail.com>
2020-07-12 12:38:04 +0100

    refactored airport_spec.rb tests

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -18,8 +18,7 @@ describe Airport do
             expect(Airport.new.checks_for(Plane.new)).to eq false
         end
         it 'tests that true is returned if checked for plane is at the airport' do
-            weather1 = double("weather1")
-            allow(weather1).to receive(:stormy?).and_return(false)
+            weather1 = double("weather1", stormy?: false)
             airport1 = Airport.new(1, weather1)
             plane1 = Plane.new.taken_off
             airport1.land(plane1)
@@ -30,32 +29,28 @@ describe Airport do
     describe '#land(plane=Plane.new)' do
 
         it 'tests that a plane is stored in airport upon landing' do
-            weather1 = double("weather1")
-            allow(weather1).to receive(:stormy?).and_return(false)
+            weather1 = double("weather1", stormy?: false)
             airport1 = Airport.new(1, weather1)
             plane1 = Plane.new.taken_off
             expect(airport1.land(plane1)).to eq airport1.planes
         end
 
         it 'tests an error is raised if a non flying plane tries to land at an airport' do
-            weather1 = double("weather1")
-            allow(weather1).to receive(:stormy?).and_return(false)
+            weather1 = double("weather1", stormy?: false)
             airport1 = Airport.new(1, weather1)
             plane1 = Plane.new.landed
             expect { airport1.land(plane1) }.to raise_error('Plane is not flying')
         end
 
         it 'tests an error is raised if a plane tries to land if the airport is at capacity' do
-            weather1 = double("weather1")
-            allow(weather1).to receive(:stormy?).and_return(false)
+            weather1 = double("weather1", stormy?: false)
             airport1 = Airport.new(0, weather1)
             plane1 = Plane.new.taken_off
             expect { airport1.land(plane1) }.to raise_error('Airport is at capacity')
         end
 
         it 'tests that an error is raised if a plane tries to land if weather is stormy?' do
-            weather1 = double("weather1")
-            allow(weather1).to receive(:stormy?).and_return(true)
+            weather1 = double("weather1", stormy?: true)
             airport1 = Airport.new(1, weather1)
             plane1 = Plane.new.taken_off
             expect { airport1.land(plane1) }.to raise_error('Weather conditions do not permit landing')
@@ -65,8 +60,7 @@ describe Airport do
     describe '#take_off(plane)' do
 
         it 'tests that a plane leaves airport upon take_off' do
-            weather1 = double("weather1")
-            allow(weather1).to receive(:stormy?).and_return(false)
+            weather1 = double("weather1", stormy?: false)
             airport1 = Airport.new(1, weather1)
             plane1 = Plane.new.taken_off
             airport1.land(plane1)
@@ -75,24 +69,21 @@ describe Airport do
         end
 
         it 'tests an error is raised if the take_off plane is not at the airport' do
-            weather1 = double("weather1")
-            allow(weather1).to receive(:stormy?).and_return(false)
+            weather1 = double("weather1", stormy?: false)
             airport1 = Airport.new(1, weather1)
             plane1 = Plane.new.landed
             expect { airport1.take_off(plane1) }.to raise_error('Plane is not at airport')
         end
 
         it 'tests an error is raised if take_off attempted during stormy weather' do
-            weather1 = double("weather1")
-            allow(weather1).to receive(:stormy?).and_return(true)
+            weather1 = double("weather1", stormy?: true)
             airport1 = Airport.new(1, weather1)
             plane1 = Plane.new
             expect { airport1.take_off(plane1) }.to raise_error('Weather conditions do not permit take off')
         end
 
         it 'tests an error is raised if plane tries to take_off while it is already flying' do
-            weather1 = double("weather1")
-            allow(weather1).to receive(:stormy?).and_return(false)
+            weather1 = double("weather1", stormy?: false)
             airport1 = Airport.new(1, weather1)
             plane1 = Plane.new.taken_off
             expect { airport1.take_off(plane1) }.to raise_error('Plane is flying')
