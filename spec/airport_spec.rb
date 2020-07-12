9244a117533c08074435ba91fc9a16ad1d2c0610
Hilda Amponsah <hilda.amp@hotmail.com>
2020-07-12 20:45:52 +0100

    airport take off

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,7 +1,14 @@
 require "airport"
 
 describe Airport do
-    it "instructs plane to land" do
-        expect(subject).to respond_to(:land) 
+    subject(:airport) { described_class.new }
+
+    it "#ask plane to land" do
+      expect(airport).to respond_to(:land) 
    end
+
+   it "#ask plane to take off" do
+      expect(airport).to respond_to(:take_off)
+   end
+
 end
