f26b67993a29a3a2ce30bfbcd2e3b6e96ee7c2c7
Ryan Viegas <rjkviegas@gmail.com>
2020-07-12 11:46:20 +0100

    updated Weather test description

--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -1,10 +1,11 @@
 require_relative '../lib/weather.rb'
 
 describe Weather do
-    it '#initialize' do
-        weather = Weather.new
-        allow(weather).to receive(:rand).and_return(2)
-        expect(weather.stormy?).to eq false
+    describe '#initialize' do
+        it 'tests that when the random number is not equal to 1 the weather is not stormy' do
+            weather = Weather.new
+            allow(weather).to receive(:rand).and_return(2)
+            expect(weather.stormy?).to eq false
+        end
     end
-    
 end
