96fe05cd79cd595e432c04ee7c1a40454bdedc2b
Matteo Micozzi <micozzimatteo@yahoo.it>
2020-07-13 03:37:41 +0100

    Last implementation

--- lib/AirPort.rb
+++ lib/AirPort.rb
@@ -9,15 +9,20 @@ class AirPort
     @capacity = 200
   end
 
-  def land
+  def land(plane)
+    fail 'Impossible to land weather stormy!' if @weather == 'stormy'
     fail 'AirPort reached max capacity!' if @hangar.length >= @capacity
-    @hangar << Plane.new
-    'A new plane arrived!'
+    @hangar << plane
   end
 
   def take_off
+    fail 'Impossible to take off weather stormy!' if @weather == 'stormy'
     @hangar.delete_at(0)
     'Plane left the Ariport!'
   end
 
+  def weather
+    @weather = ['sunny', 'stormy'].sample
+    @weather
+  end
 end
--- spec/AirPort_spec.rb
+++ spec/AirPort_spec.rb
@@ -9,21 +9,44 @@ describe AirPort do
     expect(subject.capacity).to eq(400)
   end
 
-describe '#land a plane'
+describe '#land a plane' do
+  it 'raise Error if weather stormy' do
+    sky = subject.weather
+    while sky == 'sunny'
+      sky = subject.weather
+    end
+    expect { subject.land(Plane.new) }.to raise_error('Impossible to land weather stormy!')
+  end
+
   it 'raise Error if AirPort hangar full' do
-    subject.capacity.times { subject.land }
-    expect { subject.land }.to raise_error('AirPort reached max capacity!')
+    subject.capacity.times { subject.land Plane.new }
+    expect { subject.land Plane.new }.to raise_error('AirPort reached max capacity!')
   end
 
-  it 'take_off a plane' do
-    subject.land
-    expect(subject.take_off).to eq('Plane left the Ariport!')
+  it 'check if plane landing is already in the hangar' do
+    subject.land(Plane.new)
+    subject.land(Plane.new)
+    expect(subject.hangar[0] == subject.hangar[1]).to eq(false)
+  end
+end
+
+  it 'check random weather' do
+    allow(subject).to receive(:weather) { 'stormy' }
   end
 
-  it 'Confirm plane left the hangar' do
-    subject.land
-    plane = subject.hangar[0]
-    subject.take_off
-    expect(subject.hangar.include?(plane)).to eq(false)
+  describe '#take_off a plane' do
+    it 'raise Error if weather stormy' do
+      sky = subject.weather
+      while sky == 'sunny'
+        sky = subject.weather
+      end
+      expect { subject.take_off }.to raise_error('Impossible to take off weather stormy!')
+    end
+
+    it 'Confirm plane left the hangar' do
+      plane = subject.hangar[0] = Plane.new
+      subject.take_off
+      expect(subject.hangar.include?(plane)).to eq(false)
+    end
   end
 end
