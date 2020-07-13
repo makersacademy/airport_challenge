0a709a323554fab09b70a14b672e57920e8893e7
Alexis Simonian <alexis792.as@gmail.com>
2020-07-13 10:10:34 +0200

    solved weather pb

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,17 +2,17 @@ require 'airport'
 
 describe Plane do
   it 'can land if sunny' do
-    weather = double(:weather => 'sunny')
+    subject.stub(:weather).and_return('sunny')
     expect(subject.land).to eq(true)
   end
 
   it 'can take off if sunny' do
-    weather = double(:weather => 'sunny')
+    subject.stub(:weather).and_return('sunny')
     expect(subject.take_off).to eq(true)
   end
 
   it 'cannot land nor take off if stormy' do
-    weather = double(:weather => 'stormy')
+    subject.stub(:weather).and_return('stormy')
     expect(subject.land).to eq(false)
     expect(subject.take_off).to eq(false)
   end
@@ -21,11 +21,13 @@ end
 describe Airport do
   it 'stores plane when landed' do
     plane = Plane.new
+    plane.stub(:weather).and_return('sunny')
     expect(subject.landing(plane)).to eq([plane])
   end
 
   it 'removes plane when took off' do
     plane = Plane.new
+    plane.stub(:weather).and_return('sunny')
     plane.land
     expect(subject.taking_off(plane)).not_to include(plane)
   end
@@ -33,6 +35,7 @@ describe Airport do
   it 'does not accept plane if full' do
     described_class::DEFAULT_CAPACITY.times do
       plane = Plane.new
+      plane.stub(:weather).and_return('sunny')
       subject.landing(plane)
     end
     expect{subject.landing(Plane.new)}.to raise_error 'Plane cannot land'
@@ -46,6 +49,7 @@ describe Airport do
     airport = Airport.new(10)
     10.times {
       plane = Plane.new
+      plane.stub(:weather).and_return('sunny')
       airport.landing(plane)
     }
     expect{airport.landing(Plane.new)}.to raise_error 'Plane cannot land'
