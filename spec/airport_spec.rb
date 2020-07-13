6f31b44cce3856a5b336cfb4d64592177c44fd26
Student <student@Makerss-Air.broadband>
2020-07-13 11:04:21 +0100

    Added use of doubles for planes in airport spec instead of creating new objects

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,27 +1,29 @@
 require 'airport'
 describe Airport do
   it 'can land a plane' do
-    plane = Plane.new
+    plane = double(:plane)
     expect(subject.land(plane)).to include(plane)
   end
   it 'can allow a plane to takeoff' do
-    plane = Plane.new
+    plane = double(:plane)
     subject.land(plane)
     expect(subject.takeoff(plane)).to eq(plane)
   end
   it 'removes a plane from the airport once it has taken off' do
-    plane = Plane.new
+    plane = double(:plane)
     subject.land(plane)
     subject.takeoff(plane)
     expect(subject.planes).not_to include(plane)
   end
   it 'has a default capacity of planes' do
-    Airport::DEFAULT_CAPACITY.times { subject.land(Plane.new) }
-    expect { subject.land(Plane.new) }.to raise_error "Airport is at maximum capacity"
+    plane = double(:plane)
+    Airport::DEFAULT_CAPACITY.times { subject.land(plane) }
+    expect { subject.land(plane) }.to raise_error "Airport is at maximum capacity"
   end
   it 'has a capacity that can be set when initialized' do
     airport = Airport.new(30)
-    30.times { airport.land(Plane.new) }
-    expect { airport.land(Plane.new) }.to raise_error "Airport is at maximum capacity"
+    plane = double(:plane)
+    30.times { airport.land(plane) }
+    expect { airport.land(plane) }.to raise_error "Airport is at maximum capacity"
   end
 end
