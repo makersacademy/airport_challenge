a0cbaa07158b68b5e0dc409115f8974669e87d6d
Tamara Jeffrey <just-tam@live.com>
2020-08-07 12:41:46 +0100

    Passing test for plane responding to land_plane method

---
+++ lib/airport.rb
@@ -0,0 +1,6 @@
+class Airport
+
+  def land_plane
+  end
+
+end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,6 +1,7 @@
 #As an air traffic controller
 #So I can get passengers to a destination
 #I want to instruct a plane to land at an airport
+require 'airport'
 
 describe Airport do
   it "Expects plane to land at an airport" do
