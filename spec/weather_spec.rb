22dae1d6482d175784a2228ddaa603a2a55d17bd
JRG <47917431+JR-G@users.noreply.github.com>
2020-08-08 21:27:50 +0100

    Added a test to check that weather condition returns a number between 1 and 10

--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -11,4 +11,8 @@ describe Weather do
   it 'returns a random number' do
     allow(subject).to receive(:condition) { 5 }
   end
+
+  it 'returns a number between 1 and 10' do
+    expect(subject.condition).to all(be_between(1, 10))
+  end
 end
