90d375a8c944a9c76a0d95036260d9fa9a11e857
Iliyan-Y <maximizebg@gmail.com>
2020-08-08 14:49:24 +0300

    Test for plain status added

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -4,4 +4,10 @@ describe Plane do
   it "Create instance of a plain" do 
     expect(subject).to be_instance_of(Plane)
   end
+
+  describe "#status" do 
+    it "check the status of the plain and return true if it's flying" do 
+      expect(subject.status).to eq true
+    end 
+  end 
 end
