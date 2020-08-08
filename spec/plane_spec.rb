7df5ebbd859f78eca97b22842538ea1ce2cee11d
JRG <47917431+JR-G@users.noreply.github.com>
2020-08-08 14:11:59 +0100

    Passed first test

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -2,7 +2,7 @@ require 'plane'
 
 describe Plane do
   it 'can create instance of Plane' do
-  plane = Plane.new
-  expect(plane).to be_kind_of(Plane)
+    plane = Plane.new
+    expect(plane).to be_kind_of(Plane)
   end
 end
