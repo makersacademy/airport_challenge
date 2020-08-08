dd959bfbd409ad1b18c3b73df4914ad89f05792f
JRG <47917431+JR-G@users.noreply.github.com>
2020-08-08 18:30:07 +0100

    Added failing tests to set the plane as landed

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -7,11 +7,20 @@ describe Plane do
   end
 
   it { is_expected.to respond_to :flying? }
+  it { is_expected.to respond_to :set_as_flying }
+  it { is_expected.to respond_to :set_as_landed }
 
   describe '#set_as_flying' do
     it 'sets a plane as flying' do
       subject.set_as_flying
       expect(subject).to be_flying
+    end
+  end
+
+  describe '#set_as_landed' do
+    it 'sets a plane as landed' do
+      subject.set_as_landed
+      expect(subject).not_to be_flying
     end
   end
 end
