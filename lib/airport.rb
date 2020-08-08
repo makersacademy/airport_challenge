ce4f439ef28a51e557088f017f7304c526d1dfe9
JRG <47917431+JR-G@users.noreply.github.com>
2020-08-08 20:29:24 +0100

    Refactored methods

--- lib/airport.rb
+++ lib/airport.rb
@@ -8,18 +8,32 @@ class Airport
   end
 
   def land(plane)
-    fail "Airport is full" if @planes.count >= 10
+    fail "Airport is full" if full?
 
-    fail "This plane has already landed" if @planes.include? plane
+    fail "This plane has already landed" if duplicate? plane
 
     plane.set_as_landed
     @planes << plane
   end
 
   def takeoff
-    fail "No planes are at the airport" if @planes.empty?
+    fail "No planes are at the airport" if empty?
 
     @planes[-1].set_as_flying
     @planes.pop
+  end
+
+  private
+  
+  def full?
+    @planes.count >= 10
+  end
+
+  def duplicate?(value_to_check)
+    @planes.include? value_to_check
+  end
+
+  def empty?
+    @planes.empty?
   end
 end
