c4a3bc14cf4e8e26f3930df40d9c010146f798af
joshSteadman <josh.steadman15@gmail.com>
2020-08-08 11:08:22 +0100

    third failed test dose not respon to land

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -3,5 +3,5 @@ describe Airport do
   it 'creates an instance of Airport' do
     expect(subject).to be_instance_of Airport
   end
-
+  it { is_expected.to respond_to :land }
 end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -1,6 +1,6 @@
-
+require 'plane'
 describe Plane do
   it 'creates an instance of Plane' do
-    expect(subject).to be_instances_of Plane
+    expect(subject).to be_instance_of Plane
   end
 end
