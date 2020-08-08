fcc2c9f5cd2a69a2b5542068fe9814f7fb94ca82
JRG <47917431+JR-G@users.noreply.github.com>
2020-08-08 16:14:31 +0100

    Added a failing test to check that the planes array changes after a plane takes off

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -5,22 +5,39 @@ describe Airport do
     airport = Airport.new
     expect(airport).to be_kind_of(Airport)
   end
-    
-  # I want to instruct a plane to land at an airport
-  it { is_expected.to respond_to(:land).with(1).argument }
 
-  it 'returns a plane that has landed' do
-    plane = Plane.new
-    expect(subject.land(plane)).to eq plane
+  describe '#land' do
+    it { is_expected.to respond_to(:land).with(1).argument }
+
+    it 'returns a plane that has landed' do
+      plane = Plane.new
+      expect(subject.land(plane)).to eq plane
+    end
   end
 
-  # I want to instruct a plane to take off from an airport and confirm that it is no longer in the airport
-  it { is_expected.to respond_to :takeoff }
+  describe '#takeoff' do
+    # I want to instruct a plane to take off from an airport and confirm that it is no longer in the airport
+    it { is_expected.to respond_to :takeoff }
 
-  it 'has a plane take off' do
-    plane = Plane.new
-    subject.land(plane)
-    expect(subject.takeoff).to eq plane
+    it 'has a plane take off' do
+      plane = Plane.new
+      subject.land(plane)
+      expect(subject.takeoff).to eq plane
+    end
+
+    it 'decreases in count after takeoff' do
+      difference = 1
+
+      plane = Plane.new
+      subject.land(plane)
+      initial_count = subject.planes.count
+
+      subject.takeoff
+      final_count = subject.planes.count
+
+      expect(final_count - initial_count).to eq(difference)
+    end
+
   end
 
   # I want to prevent landing when the airport is full 
