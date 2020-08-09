8f0b4ffd77f960cce5fe7501fa89b3518ca33af7
Tamara Jeffrey <just-tam@live.com>
2020-08-09 11:01:32 +0100

    Failing test for plane responding to land method

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,9 +1,20 @@
 # As an air traffic controller
 # So I can get passengers to a destination
 # I want to instruct a plane to land at an airport
+
+# As an air traffic controller
+# So I can get passengers on the way to their destination
+# I want to instruct a plane to take off from an airport and confirm that it is no longer in the airport
+
 require 'airport'
 describe Airport do
   it "Expects Airport to respond to landing a plane" do
     expect(subject).to respond_to(:land)
+  end
+
+  it "Expects Plane to respond to land" do
+    airport = Airport.new
+    plane = Plane.new
+    expect(plane).to respond_to(:land)
   end
 end
