e7d5589f07831b60ead5bd57324abb457e5f27cd
Yasmin Underdown <yunderdown1@gmail.com>
2020-08-09 10:18:06 +0100

    land a plane test passed

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -12,7 +12,7 @@ describe Airport do
 
     it "lands a plane" do
       plane = Plane.new
-      expect (subject.land(plane)).to eq(plane)
+      expect(subject.land(plane)).to eq(plane)
     end
 
   end
