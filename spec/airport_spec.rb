040cd3e58da092862eb4f55d2cb6a4cb8128310c
JRG <47917431+JR-G@users.noreply.github.com>
2020-08-09 19:43:47 +0100

    Refactored the tests to include context and before do

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -4,6 +4,9 @@ describe Airport do
 
   let(:plane) { Plane.new }
 
+  it { is_expected.to respond_to(:land).with(1).argument }
+  it { is_expected.to respond_to :takeoff }
+
   it 'can create an instance of Airport' do
     airport = Airport.new
     expect(airport).to be_kind_of Airport
@@ -40,75 +43,84 @@ describe Airport do
   end
 
   describe '#land' do
-    it { is_expected.to respond_to(:land).with(1).argument }
+    context 'when weather is sunny' do
+      before do
+        allow(subject).to receive(:stormy?) { false }
+      end
 
-    it 'has a plane land' do
-      allow(subject).to receive(:stormy?) { false }
-      expect(subject.land plane).to eq [plane]
+      it 'has a plane land' do
+        expect(subject.land plane).to eq [plane]
+      end
+  
+      it 'raises an error when the airport is full' do
+        Airport::DEFAULT_CAPACITY.times { subject.land Plane.new }
+        expect { subject.land plane }.to raise_error "Airport is full"
+      end
+  
+      it 'sets a plane as landed' do
+        expect { subject.land plane }.to change { plane.flying }.to false
+      end
+  
+      it 'raises an error if the plane has already landed' do
+        duplicated_plane = Plane.new
+        subject.land duplicated_plane
+        expect { subject.land duplicated_plane }.to raise_error "This plane has already landed"
+      end
     end
 
-    it 'raises an error when the airport is full' do
-      allow(subject).to receive(:stormy?) { false }
-      Airport::DEFAULT_CAPACITY.times { subject.land Plane.new }
-      expect { subject.land plane }.to raise_error "Airport is full"
-    end
-
-    it 'sets a plane as landed' do
-      allow(subject).to receive(:stormy?) { false }
-      expect { subject.land plane }.to change { plane.flying }.to false
-    end
-
-    it 'raises an error if the plane has already landed' do
-      allow(subject).to receive(:stormy?) { false }
-      duplicated_plane = Plane.new
-      subject.land duplicated_plane
-      expect { subject.land duplicated_plane }.to raise_error "This plane has already landed"
-    end
-
-    it 'raises an error when the weather is stormy' do
-      allow(subject).to receive(:stormy?) { true }
-      expect { subject.land plane }.to raise_error "Unable to land due to stormy weather"
+    context 'when the weather is stormy' do
+      before do
+        allow(subject).to receive(:stormy?) { true }
+      end
+      it 'raises an error' do
+        expect { subject.land plane }.to raise_error "Unable to land due to stormy weather"
+      end
     end
   end
 
   describe '#takeoff' do
-    it { is_expected.to respond_to :takeoff }
-
-    it 'has a plane take off' do
-      allow(subject).to receive(:stormy?) { false }
-      subject.land plane
-      expect(subject.takeoff).to eq plane
-    end
-
-    it 'decreases in count after take off' do
-      allow(subject).to receive(:stormy?) { false }
-      difference = 1
-
-      subject.land plane
-      initial_count = subject.planes.count
-
-      subject.takeoff
-      final_count = subject.planes.count
-
-      expect(initial_count - final_count).to eq difference
-    end
-
-    it 'changes the plane to flying' do
-      allow(subject).to receive(:stormy?) { false }
-      subject.land plane
-      expect { subject.takeoff }.to change { plane.flying }.to true
-    end
-
     it 'raises an error when there are no planes' do
       expect { subject.takeoff }.to raise_error "No planes are at the airport"
     end
 
-    it 'raises an error when the weather is stormy' do
-      airport = Airport.new
-      allow(airport).to receive(:stormy?) { false }
-      airport.land plane
-      allow(airport).to receive(:stormy?) { true }
-      expect { airport.takeoff }.to raise_error "Unable to take off due to stormy weather"
+    context 'when the weather is sunny' do
+      before do
+        allow(subject).to receive(:stormy?) { false }
+      end
+
+      it 'has a plane take off' do
+        subject.land plane
+        expect(subject.takeoff).to eq plane
+      end
+  
+      it 'decreases in count after take off' do
+        difference = 1
+  
+        subject.land plane
+        initial_count = subject.planes.count
+  
+        subject.takeoff
+        final_count = subject.planes.count
+  
+        expect(initial_count - final_count).to eq difference
+      end
+  
+      it 'changes the plane to flying' do
+        subject.land plane
+        expect { subject.takeoff }.to change { plane.flying }.to true
+      end
+    end
+
+    context 'when the weather is stormy' do
+      before do
+        allow(subject).to receive(:stormy?) { false }
+      end
+
+      it 'raises an error' do
+        subject.land plane
+        allow(subject).to receive(:stormy?) { true }
+        expect { subject.takeoff }.to raise_error "Unable to take off due to stormy weather"
+      end
     end
   end
 end
