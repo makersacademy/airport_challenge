862d65370adb6f911274db5a4c0a78b58a3ce12e
Yasmin Underdown <yunderdown1@gmail.com>
2020-08-09 13:05:44 +0100

    false at_airport if plane taken off test failed

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -7,6 +7,15 @@ describe Plane do
     expect(plane).to be_an_instance_of(Plane)
   end
 
-  it { is_expected.to respond_to :at_airport? }
+  describe "#at_airport?" do
+    it { is_expected.to respond_to :at_airport? }
+
+    it "is false when plane has taken off" do
+      airport = Airport.new
+      airport.land(subject)
+      airport.take_off
+      expect(subject.at_airport?).to eq false
+    end
+  end
 
 end
