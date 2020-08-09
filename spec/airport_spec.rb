cc59887928ff4d20491f6423e87a1eafe3161b8b
Djura22 <michaeliangroves@gmail.com>
2020-08-09 10:45:47 +0100

    land method defined, Plane class added and next test written

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -4,5 +4,12 @@ describe Airport do
 	
 	it { is_expected.to respond_to('land') }
 
+	describe '#land' do
+		plane = Plane.new
+		subject.land(plane)
+		expect(subject.land). to eq plane
+	end
+
+
 
 end
