ce56ea36f672ad80f75d9a4ff93a0dc696d4d51e
JRG <47917431+JR-G@users.noreply.github.com>
2020-08-09 13:07:12 +0100

    Added failing tests for the condition class to return a string value of sunny

--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -7,10 +7,21 @@ describe Weather do
   end
 
   it { is_expected.to respond_to :generator }
+  it { is_expected.to respond_to :condition }
 
   describe '#initialize' do
     it 'returns a number between 1 and 10' do
       expect(subject.generator).to be_between(1, 10)
+    end
+  end
+
+  describe '#condition' do
+    it 'returns a string' do
+      expect(subject.condition).to be_a String
+    end
+
+    it 'returns a weather type' do
+      allow(subject.condition).to receive(3) { "sunny" }
     end
   end
 end
