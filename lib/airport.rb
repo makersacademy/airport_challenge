63019c510cb8b18dcee82137bcbd930c8845e77d
JRG <47917431+JR-G@users.noreply.github.com>
2020-08-09 16:37:53 +0100

    Small refactor

--- lib/airport.rb
+++ lib/airport.rb
@@ -2,13 +2,14 @@ require_relative 'plane'
 require_relative 'weather'
 
 class Airport
-  attr_reader :planes, :capacity
+  attr_reader :planes, :capacity, :weather_conditions
 
   DEFAULT_CAPACITY = 10
 
   def initialize(capacity = DEFAULT_CAPACITY)
     @planes = []
     @capacity = capacity
+    @weather_conditions = Weather.new
   end
 
   def land(plane)
