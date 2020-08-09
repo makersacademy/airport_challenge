fd31495092687a0a380793cf727b59c01e09ab2c
Djura22 <michaeliangroves@gmail.com>
2020-08-09 11:47:02 +0100

    refactored land test, added array for planes, written new test for take off of planes from Airport

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -5,16 +5,27 @@ describe Airport do
 	it { is_expected.to respond_to(:land).with(1).argument }
 
 
-
-
 	describe 'land' do
+
 		it 'land a plane' do
 		  plane = Plane.new
-		  airport = Airport.new
-		  airport.land(plane)
-		  expect(subject.land(plane)).to eq plane
+		  expect(subject.land(plane)).to eq [plane]
 		end
+
+
 	end
+
+	describe 'take_off' do
+
+		it 'plane has left the airport' do
+			plane = Plane.new
+			expect(subject.take_off).to eq plane
+		end
+
+
+	end
+
+
  
 
 
