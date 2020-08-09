d82a189fb83ecd6bb6ba08997f20b427d9d81f97
Yasmin Underdown <yunderdown1@gmail.com>
2020-08-09 22:11:42 +0100

    stormy randomness tested

--- lib/weather.rb
+++ lib/weather.rb
@@ -1,11 +1,15 @@
 class Weather
 
   def stormy?
+    forecast == :stormy
   end
 
 
   private
+  OUTLOOKS = [:stormy,:fine,:clear,:sunny,:cloudy]
 
-
+  def forecast
+    OUTLOOKS.sample
+  end
 
 end
--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -6,14 +6,20 @@ describe Weather do
     expect(weather).to be_an_instance_of(Weather)
   end
 
-  it { is_expected.to respond_to :stormy? }
+  describe "#stormy" do
+    it { is_expected.to respond_to :stormy? }
 
-  it { is_expected.to respond_to :forecast }
+    it "return true if forecast is stormy" do
+      weather = Weather.new
+      allow(weather).to receive(:forecast) { :stormy }
+      expect(weather.stormy?).to be true
+    end
 
-  it "stormy to be true" do
-    weather = Weather.new
-    allow(weather).to receive(:forecast) { :stormy }
-    expect(weather.stormy?).to be true
+    it "return false if forecast is sunny" do
+      weather = Weather.new
+      allow(weather).to receive(:forecast) { :sunny }
+      expect(weather.stormy?).to be false
+    end
   end
 
 end
