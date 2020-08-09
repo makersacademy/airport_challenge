814efc974bdedfc38cb1edde2a900df313972e56
Djura22 <michaeliangroves@gmail.com>
2020-08-09 14:27:44 +0100

    weather and spec file created, two tests for land and take_off methods interactions with the weather created, not passed.

---
+++ lib/weather.rb
@@ -0,0 +1,11 @@
+
+
+def weather
+	
+
+
+	sunny = (1..7)
+  stormy = (8-10)
+
+
+end
---
+++ spec/weather_spec.rb
@@ -0,0 +1,9 @@
+require 'weather'
+require 'airport'
+
+describe weather do 
+	it { is_expected.to respond_to(:land).with(1).argument}
+
+	it { is_expected.to respond_to(:take_off).with(1).argument}
+
+end
