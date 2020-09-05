5fc1d3a6480e4a869e3c6bf67b8e5a0f388bad5e
Johnny Yip <jyippy2012@gmail.com>
2020-09-05 13:34:55 +0100

    Raise error in taking off

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,9 +1,18 @@
 class Airport
 
+    attr_accessor :airplanes
+
+    def initialize
+    @airplanes = []
+    end
+
+
     def land(plane)
+        raise "The airport is full"
     end
 
     def take_off(plane)
+        raise "There are no planes" if @airplanes.empty?
     end
 
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -4,5 +4,19 @@ describe Airport do
     it { is_expected.to respond_to(:land).with(1).arguments }
 
     it { is_expected.to respond_to(:take_off).with(1).arguments }
+
+
+    it "raise an error when plane is trying to take off when no plane at airport" do
+        airport = Airport.new
+        plane = Plane.new
+        expect {airport.take_off(plane).to raise_error "There are no planes"}
+
+    end
+
+    it "raise an error when airport is full and trying to land a plane " do
+        airport = Airport.new
+        plane = Plane.new
+        expect {airport.land(plane).to raise_error "The airport is full"}
+    end
 end
 
