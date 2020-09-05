fa3b3608134b048b8f0cc4670211200a50cf1f44
Richard <rich_pryce@yahoo.co.uk>
2020-09-05 09:19:06 +0100

    added test for Weather :crap_weather

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -1,9 +1,12 @@
 require 'plane'
 
 describe Planes do
-        it "responds to method flyinf" do
-        plane = Planes.new
-            expect(plane).to respond_to :flying
+plane = Planes.new
+        it "responds to method flying" do
+           expect(plane).to respond_to :flying
+        end
+        it "returns flying method true by default" do
+            expect(plane.flying).to eq true
         end
     
 end
