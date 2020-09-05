4b1c7b75dda648b0018697c339cc57848b9fbfd0
Victoria Jusko <vikjusko@gmail.com>
2020-09-05 11:35:58 +0100

    Added error plane that already flying cannot take off

--- lib/plane.rb
+++ lib/plane.rb
@@ -2,10 +2,18 @@
 # a plane should take off
 
 class Plane 
-	
+	attr_reader :flying, :airport_planes
+
+	def initialize(flying = true)
+		@flying = flying
+		@airport_planes = nil 
+	end 
+
 	def land
 	end 
 
 	def take_off
+		raise "Plane is already flying" unless @flying == false
 	end 
+
 end 
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -2,4 +2,9 @@ describe Plane do
 	it { is_expected.to respond_to :land }
 
 	it { is_expected.to respond_to :take_off }
+
+	it "raises error if the plane is already flying and is trying to take off " do
+		expect { subject.take_off }.to raise_error "Plane is already flying"
+	end 
+		
 end 
