45a1d80da28ac8f9bba0a4ebf7c103eb8fe91b7a
Xavier Defontaine <xdefontaine@gmail.com>
2020-09-06 13:34:09 +0100

    Tweaking error message if airport is full

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,6 +1,7 @@
 class Airport
     
   def land(plane)
+    fail "Cannot land anymore plane, airport is full." if plane
   end
 
   def take_off(plane)
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -7,8 +7,13 @@ describe Airport do
     context "planes landing"
     it { is_expected.to respond_to(:land).with(1).argument }
     
+    it "raises an error if landing a plane when airport is full" do
+    subject.land(Plane.new)
+    expect { subject.land(Plane.new) }.to raise_error("Cannot land anymore plane, airport is full.")
+    end
+
     context "planes taking off"
-      it { is_expected.to respond_to :take_off }
+      it { is_expected.to respond_to(:land).with(1).argument }
     
       it "returns the plane taking off" do
       expect(subject.take_off(plane)).to eq(plane) 
