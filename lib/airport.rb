a2089cb86a3b3d04bf5563a460e7dc113e553d43
Louis Kirkham <louiskirkham@gmail.com>
2020-09-06 10:40:24 +0100

    unable to land if exceeds variable capacity

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,10 +1,23 @@
 require_relative 'plane'
 
 class Airport
-  attr_reader :plane
+  DEFAULT_CAPACITY = 5
+  attr_reader :planes
+  attr_accessor :capacity
+
+  def initialize(capacity=DEFAULT_CAPACITY)
+    @capacity = capacity
+    @planes = []
+  end
+
+
   
   def land(plane)
-    @plane = plane
+    if @planes.count >= @capacity
+      fail 'Landing is not currently possible'
+    else
+      @planes << plane
+    end
   end
 
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -5,7 +5,13 @@ describe Airport do
   describe '#land' do
     it 'lands a plane' do
       plane = Plane.new
-      expect(subject.land(plane)).to eq plane
+      expect(subject.land(plane)).to eq [plane]
+    end
+
+    it 'raises an error when airport is full' do
+      plane = Plane.new
+      Airport::DEFAULT_CAPACITY.times { subject.land(plane) }
+      expect{ subject.land(plane) }.to raise_error "Landing is not currently possible"
     end
 
   end
