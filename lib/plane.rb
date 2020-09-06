1db921ed66b4bd282d0be1864b31d2e806155e47
Veronica Lee <veronicavlee@icloud.com>
2020-09-06 11:37:09 +0100

    Add initial test and class for Plane

---
+++ lib/plane.rb
@@ -0,0 +1,6 @@
+class Plane
+
+  def land(airport)
+    true
+  end
+end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -1,8 +1,16 @@
-# require 'plane'
-#
-# describe Plane do
-#   describe '#land' do
-#
-#   end
-#
-# end
+require 'plane'
+
+class AirportDouble
+end
+
+describe Plane do
+
+  let(:airport) { AirportDouble.new }
+
+  describe '#land(airport)' do
+    it 'returns true if the plane is in the air' do
+      expect(subject.land(airport)).to eq true
+    end
+  end
+
+end
