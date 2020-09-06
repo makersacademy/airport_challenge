eca6f32f66115513b07e8de03fada12fde3de29b
Connor Worthington <worthington.connor@gmail.com>
2020-09-06 11:35:56 +0100

    add test and method for adding airport capacity

--- lib/airport_class.rb
+++ lib/airport_class.rb
@@ -1,8 +1,12 @@
 class Airport
 
   attr_reader :hangar
+  attr_accessor :capacity
 
-  def initialize
+  DEFAULT_CAPACITY = 5
+
+  def initialize(capacity= DEFAULT_CAPACITY)
     @hangar = []
+    @capacity = capacity
   end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,8 +2,12 @@ require 'airport_class'
 
 describe Airport do
 
-  it 'initialises airport with Aiport with array to store planes' do
+  it 'initialises Aiport with array to store planes' do
     expect(subject.hangar).to be_an_instance_of(Array)
+  end
+
+  it 'initialises Airport with capacity that can be set as argument of initialise method' do
+    expect(subject.capacity).to eq 5
   end
 
 end
