3aad06ac90b0234e1a9aebd504539ccc9718a43a
Louis Kirkham <louiskirkham@gmail.com>
2020-09-06 09:38:33 +0100

    planes can land at an airport

---
+++ spec/airport_spec.rb
@@ -0,0 +1,16 @@
+require 'airport'
+
+describe Airport do
+  
+  describe '#land' do
+    it 'lands a plane' do
+      plane = Plane.new
+      expect(subject.land(plane)).to eq plane
+    end
+
+  end
+  
+end
+
+
+
