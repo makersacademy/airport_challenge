a86f98c82d120d7dcea482492d4114b1b95e231f
cristina <c.fujiname@gmail.com>
2020-09-06 16:29:16 +0100

    refactoring code to a more readable code, using predicate method and private

--- lib/airport.rb
+++ lib/airport.rb
@@ -7,15 +7,22 @@ class Airport
   end
 
   def land(plane)
-    raise 'Airport full, no more planes allowed' if @planes.length >= @capacity
-    # asking to raise the message above if the length of the array with planes in it 
-    # satisfies the comparison
-
+    raise 'Airport full, no more planes allowed' if full? 
     @planes << plane # inserting the plane that landed to the array of planes
 
   end
 
   def take_off(plane)
+  end
+
+  ptivate # no need to call this method outside of the class, so calling 'private' section
+
+  def full? # creating a 'full?' method with question mark to return either true or false 
+    # this is a predicate method 
+    @planes.length >= @capacity
+    # asking to raise the message above if the length of the array with planes in it 
+    # satisfies the comparison
+
   end
 
 end
