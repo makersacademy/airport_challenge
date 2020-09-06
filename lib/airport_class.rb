33833e7c8a39959fe19b858b60a59ad2ce512b99
Connor Worthington <worthington.connor@gmail.com>
2020-09-06 11:27:47 +0100

    add test for initialising Airport class

--- lib/airport_class.rb
+++ lib/airport_class.rb
@@ -1,34 +1,8 @@
 class Airport
 
+  attr_reader :hangar
+
   def initialize
-    #Change initialize to accept capacity as argument to allow custom capacity
-    @capacity = 3
-    @current_planes = []
+    @hangar = []
   end
-
-  def land(plane)
-    if @capacity <= @current_planes.length
-      fail "Airport full"
-    else
-      @plane_landed = plane
-      @current_planes << @plane_landed
-    end
-  end
-
-  def take_off
-  end
-
-  def has_departed
-    true
-  end
-
-end
-
-module Weather
-
-  def weather_today
-    weather_conditions = [:sunny, :stormy]
-    weather.sample
-  end
-  
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,22 +1,9 @@
 require 'airport_class'
-require "plane_class"
 
 describe Airport do
 
-  describe 'plane respond to #land' do
-    it { expect(subject).to respond_to(:land)}
+  it 'initialises airport with Aiport with array to store planes' do
+    expect(subject.hangar).to be_an_instance_of(Array)
   end
-
-  describe 'plane respond to #take_off' do
-    it { expect(subject).to respond_to(:take_off)}
-  end
-
-  describe 'plane has left airport' do
-    it { expect(subject.has_departed).to eq true}
-  end
-
-  describe 'prevent landing if airport is full' do
-    it { expect {raise (subject.land(plane))}.to raise_error}
-  end 
 
 end
