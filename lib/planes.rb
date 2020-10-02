8d80440cadb59bdbad4a18bd8a7f846f55a42b81
Ben <ben.lynch1931@gmail.com>
2020-10-02 13:22:52 +0100

    PLANES: Red/Green Test - 'confirm_status rsponds to call'
    Planes
      #take_off
        responds when called
      #land
        responds when called
      #confirm_status
        responds when called
    
    Have you considered running rubocop? It will help you improve your code!
    Try it now! Just run: rubocop
    
    Finished in 0.00208 seconds (files took 0.15062 seconds to load)
    3 examples, 0 failures
    
    COVERAGE: 100.00% -- 18/18 lines in 4 files

--- lib/planes.rb
+++ lib/planes.rb
@@ -1,10 +1,11 @@
 class Planes
 
   def take_off
-
   end
 
   def land
+  end
 
+  def confirm_status
   end
 end
--- spec/planes_spec.rb
+++ spec/planes_spec.rb
@@ -2,17 +2,24 @@ require 'planes'
 
 describe Planes do
 
-  describe "#take_off" do
+  describe '#take_off' do
     it 'responds when called' do
       plane = Planes.new
       expect(plane).to respond_to :take_off
     end
   end
 
-  describe "#land" do
+  describe '#land' do
     it 'responds when called' do
       plane = Planes.new
       expect(plane).to respond_to :land
+    end
+  end
+
+  describe '#confirm_status' do
+    it 'responds when called' do
+      plane = Planes.new
+      expect(plane).to respond_to :confirm_status
     end
   end
 end
