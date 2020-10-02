b205b3a7a2768196fffd75a94f294bce2880b258
David Millen <70208036+dm-devtech@users.noreply.github.com>
2020-10-02 18:47:34 +0100

    Added failing test which expects weather_outcome to equal 'sunny'

--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -10,4 +10,10 @@ describe Weather do
 
     it { is_expected.to respond_to :weather_outcome }
 
+    it 'random number generator' do
+      weatheroutcome = Weather.new
+      weatheroutcome.stub(:random_number) { 1 }
+      expect(weatheroutcome.weather_outcome).to eq("Sunny")
+    end
+
 end
