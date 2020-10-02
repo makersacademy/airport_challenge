72480fb474df46290dda680264d36ffe8112e4c6
David Millen <70208036+dm-devtech@users.noreply.github.com>
2020-10-02 19:36:13 +0100

    Removed superfluous rand stub test.  Added test to check test outputs the correct weather

--- lib/Weather.rb
+++ lib/Weather.rb
@@ -1,10 +1,18 @@
 class Weather
+  def initialize
+    @number = 0
+  end
 
   def random_number
-    rand(2)
+    rand(2000000)
   end
 
   def weather_outcome
+    if random_number == 100
+      "Sunny"
+    else
+      "Rain"
+    end
   end
 
 end
--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -2,17 +2,11 @@ require 'Weather.rb'
 describe Weather do
   it { is_expected.to respond_to :random_number }
 
-  it 'random number generator' do
-    weatheroutcome = Weather.new
-    weatheroutcome.stub(:random_number) { 345678 }
-    expect(weatheroutcome.random_number).to eq(345678)
-  end
-
     it { is_expected.to respond_to :weather_outcome }
 
     it 'random number generator' do
       weatheroutcome = Weather.new
-      weatheroutcome.stub(:random_number) { 1 }
+      weatheroutcome.stub(:random_number) { 100 }
       expect(weatheroutcome.weather_outcome).to eq("Sunny")
     end
 
