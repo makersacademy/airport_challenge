1ee99e4c9081178700f14f30c21180b314055e95
Kiril <kiril@olgas-air.mynet>
2020-10-02 11:25:43 +0100

    Added tests for default capacity and not being able to land if full

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -21,4 +21,14 @@ describe Airport do
     airport.takeoff(plane)
     expect(airport.spaces).not_to include(plane)
   end
+
+  it "has a default capacity" do
+    expect(Airport::DEFAULT_CAPACITY).to exist
+  end
+
+  it "cannot land planes if full" do
+    Airport::DEFAULT_CAPACITY.times { airport.land(plane) }
+    expect{airport.land(plane)}.to raise_error("Airport full, landing denied.")
+  end
+  
 end
