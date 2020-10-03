e507de5d63056baca71ad57b9ebd36e923a3491e
Ben <ben.lynch1931@gmail.com>
2020-10-03 12:37:03 +0100

    AIRPORTS: Red/Green test - sets default capacity to 20 planes
    Airports
      sets default plane capacity to 20 planes
    
    Planes
      #take_off
        responds when called
      #land
        responds when called
      #confirm_status
        responds when called
    
    Weather
      initializes with random number
      sets weather to clear when rand between 11-89
      sets weather to stormy when rand <=10 or >=90
    
    Have you considered running rubocop? It will help you improve your code!
    Try it now! Just run: rubocop
    
    Finished in 0.00823 seconds (files took 0.13184 seconds to load)
    7 examples, 0 failures
    
    COVERAGE: 100.00% -- 46/46 lines in 6 files

--- lib/airports.rb
+++ lib/airports.rb
@@ -1,3 +1,8 @@
 class Airports
+  DEFAULT_CAPACITY = 20
+  attr_reader :capacity
 
+  def initialize(capacity = DEFAULT_CAPACITY)
+    @capacity = capacity
+  end
 end
--- spec/airports_spec.rb
+++ spec/airports_spec.rb
@@ -1,5 +1,10 @@
 require 'airports'
 
 describe Airports do
-  
+
+  it 'sets default plane capacity to 20 planes' do
+    # This is only a test program, normally it would be more!
+
+    expect(subject.capacity).to eq 20
+  end
 end
