6d770054bd5320aaac1940294f15b2cbf7358f37
Emily Sesto <emilyalicesesto@gmail.com>
2020-10-03 18:03:03 +0100

    RED: Airport full test updated to match expected capacity and is now failing.

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -17,7 +17,7 @@ describe Airport do
     it { is_expected.to respond_to(:land).with(1).argument }
 
     it 'should raise error if Airport is full' do
-      subject.land(Plane.new)
+      20.times { subject.land(Plane.new) }
       expect { subject.land(Plane.new) }.to raise_error "This airport is full."
     end
   end
