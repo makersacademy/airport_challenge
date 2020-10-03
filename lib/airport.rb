9384724db86b0413a605731720a33b1b945af246
Charlie Galbraith <galbraith.charlie@gmail.com>
2020-10-03 16:10:22 +0100

    Add guard condition to check if a plane is in the hangar before requesting take off

--- lib/airport.rb
+++ lib/airport.rb
@@ -20,6 +20,7 @@ class Airport
 
   def request_take_off(plane)
     empty?
+    check_roster(plane)
     check_weather
     plane.take_off(self)
   end
@@ -36,6 +37,10 @@ class Airport
 
   def check_weather
     return raise "DANGER! STORMY WEATHER!" if Weather.new.status == "STORMY"
+  end
+
+  def check_roster(plane)
+    return raise "Plane not in hangar" if !@hangar.include?(plane)
   end
 
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -23,7 +23,7 @@ describe Airport do
   it "throws an error if a plane tries to take off when the hangar is empty" do
     airport = Airport.new
     allow_any_instance_of(Weather).to receive(:randomizer).and_return(5)
-    expect{airport.request_take_off(Plane.new)}.to raise_error("Hangar is empty")
+    expect{airport.request_take_off(Plane.new)}.to raise_error
   end
   it "throws an error if a plane tries to land when it's stormy" do
     airport = Airport.new
@@ -34,6 +34,7 @@ describe Airport do
   it "throws an error if a plane tries to take off when it's stormy" do
     airport = Airport.new
     plane = Plane.new
+    allow_any_instance_of(Weather).to receive(:randomizer).and_return(5)
     airport.request_landing(plane)
     allow_any_instance_of(Weather).to receive(:randomizer).and_return(10)
     expect{ airport.request_take_off(plane) }.to raise_error("DANGER! STORMY WEATHER!")
@@ -44,7 +45,7 @@ describe Airport do
     plane2 = Plane.new
     allow_any_instance_of(Weather).to receive(:randomizer).and_return(8)
     airport.request_landing(plane1)
-    expect{ airport.request_take_off(plane2) }.to raise_error("Plane already airborne")
+    expect{ airport.request_take_off(plane2) }.to raise_error
   end
   it "raises an error if a plane on the ground is asked to land" do
     airport = Airport.new
@@ -56,6 +57,16 @@ describe Airport do
   it "allows users to override default hangar capacity" do
     airport = Airport.new(1)
     expect(airport.capacity).to eq(1)
+  end
+  it "raises an error if planes are asked to take off from an airport other than the one they landed at" do
+    airport1 = Airport.new
+    airport2 = Airport.new
+    plane1 = Plane.new
+    plane2 = Plane.new
+    allow_any_instance_of(Weather).to receive(:randomizer).and_return(8)
+    airport1.request_landing(plane1)
+    airport2.request_landing(plane2)
+    expect{airport2.request_take_off(plane1)}.to raise_error("Plane not in hangar")
   end
 
 end
