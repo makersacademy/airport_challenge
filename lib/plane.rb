cfac0ee957d0ddcc295549ef13abe5187dafb95e
KC Thomas <thisiskcthomas@gmail.com>
2020-10-03 14:37:47 +0100

    Planes can take off from airports they are in

--- lib/plane.rb
+++ lib/plane.rb
@@ -10,4 +10,12 @@ class Plane
         @destination = Airport.new
         @passengers = rand(101)
     end
+
+    def take_off(airport)
+        if airport == @location
+            @status = 'flying'
+            @location = 'in air'
+            true
+        end
+    end
 end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -22,4 +22,12 @@ describe Plane do
         expect(@plane.passengers).to be_between(1, 100)
     end
 
+    it "takes off from the airport it's in" do 
+        airport = @plane.location
+        expect(@plane.take_off(airport)).to be true
+    end
+    # planes can only take off from airports they are in; 
+    # planes that are already flying cannot take off and/or be in an airport; 
+    # planes that are landed cannot land again and must be in an airport, etc.
+
 end
