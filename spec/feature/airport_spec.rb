d0e1075ae3c9c8e42953f23bc68840980ce7fc6b
Joshua <joshuamacleod@live.com>
2020-10-03 21:42:54 +0100

    Made our airport with a land method

--- spec/feature/airport_spec.rb
+++ spec/feature/airport_spec.rb
@@ -1,4 +1,5 @@
 require 'airport'
 
 describe Airport do
+  it { is_expect.to respond_to :land }
 end
--- spec/feature/user_stories_spec.rb
+++ spec/feature/user_stories_spec.rb
@@ -5,6 +5,6 @@ describe 'user stories' do
   it 'planes should land at airports, instruct a place to land' do
     airport = Airport.new
     plane = Plane.new
-    expect { airport.land(plane) }.not_to raise_error
+    expect { airport.land(plane) }.not_to raise_error 
   end
 end
