31d259cc8657f88f028699ad8b3a489e874e02e8
Kiril <kiril@olgas-air.mynet>
2020-10-03 13:03:21 +0100

    tests for weather class and pass

--- lib/weather.rb
+++ lib/weather.rb
@@ -1,3 +1,15 @@
 class Weather
+  attr_reader :options
+  
+  def initialize
+    @options = [:stormy, :sunny, :sunny, :sunny, :sunny, :sunny]
+  end
 
-end 
+  def generate
+    options.sample
+  end
+
+  def stormy?
+    generate == :stormy
+  end
+end
--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -5,8 +5,12 @@ describe Weather do
 
   it { is_expected.to be_an_instance_of Weather }
 
-  it "can be stormy or sunny" do
-    expect(weather.options).to include(:stormy)
-    expect(weather.options).to include(:sunny)
+  it "can pick a random value from the options" do
+    expect(weather.options).to include(weather.generate)
+  end
+
+  it "understands that it is stormy" do
+    allow(weather).to receive(:generate).and_return(:stormy)
+    expect(weather.stormy?).to be true
   end
 end
