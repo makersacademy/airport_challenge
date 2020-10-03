8ae92ac7d0fc85529d77864093b0718cd9d6dafd
Ben <ben.lynch1931@gmail.com>
2020-10-03 12:23:57 +0100

    WEATHER: Red/Green Test - weather is clear when rand between 11-89
    Planes
      #take_off
        responds when called
      #land
        responds when called
      #confirm_status
        responds when called
    
    Weather
      initializes with random number
      sets weather to clear when rand between 11-89
    
    Have you considered running rubocop? It will help you improve your code!
    Try it now! Just run: rubocop
    
    Finished in 0.00742 seconds (files took 0.13172 seconds to load)
    5 examples, 0 failures
    
    COVERAGE: 100.00% -- 33/33 lines in 5 files

--- lib/weather.rb
+++ lib/weather.rb
@@ -3,6 +3,11 @@ class Weather
 
   def initialize
     @weather_no = rand(100)
+    weather_calculator
+  end
+
+  def weather_calculator
+    @weather_no.between?(11,89) ? @weather = "clear" : @weather = "stormy"
   end
 
 end
--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -6,4 +6,9 @@ describe Weather do
     allow_any_instance_of(Weather).to receive(:rand).and_return(50)
     expect(subject.weather_no).to eq 50
   end
+
+  it 'sets weather to clear when rand between 11-89' do
+    allow_any_instance_of(Weather).to receive(:rand).and_return(rand(11..89))
+    expect(subject.weather).to eq 'clear'
+  end
 end
