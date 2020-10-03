bebf159c00ed8894b73da7c73f1fdffcd5792bdf
Dan Holmes <dh@danielholmes.com>
2020-10-03 10:36:56 +0100

    refactor airport spec to use subject

--- spec/Airport_spec.rb
+++ spec/Airport_spec.rb
@@ -4,40 +4,39 @@ describe Airport do
   # Arrange
   before(:each) do
     @plane = Plane.new
-    @airport = Airport.new
   end
   it "can land a plane" do
     # Act
-    @airport.land(@plane)
+    subject.land(@plane)
 
     # Assert
-    expect(@airport.planes).to include @plane
+    expect(subject.planes).to include @plane
   end
 
   it "can let a plane takeoff" do
     # Arrange
-    allow(@airport.sky).to receive(:rand) { 5 }
-    @airport.sky.change_weather
-    @airport.land(@plane)
+    allow(subject.sky).to receive(:rand) { 5 }
+    subject.sky.change_weather
+    subject.land(@plane)
     # Act
-    @airport.takeoff(@plane)
+    subject.takeoff(@plane)
     # Assert
-    expect(@airport.planes).to_not include @plane
+    expect(subject.planes).to_not include @plane
   end
 
   it "won't land a plane if at capacity" do
     # Act
-    Airport::CAPACITY.times { @airport.land(Plane.new) }
+    Airport::CAPACITY.times { subject.land(Plane.new) }
     # Assert
-    expect { @airport.land(Plane.new) }.to raise_error "Airport is full."
+    expect { subject.land(Plane.new) }.to raise_error "Airport is full."
   end
 
   it "won't allow takeoff if weather is stormy" do
     # Arrange
-    allow(@airport.sky).to receive(:rand) { 1 }
+    allow(subject.sky).to receive(:rand) { 1 }
     # Act
-    @airport.sky.change_weather
+    subject.sky.change_weather
     # Assert
-    expect { @airport.takeoff(@plane) }.to raise_error "The sky is too stormy to fly."
+    expect { subject.takeoff(@plane) }.to raise_error "The sky is too stormy to fly."
   end
 end
