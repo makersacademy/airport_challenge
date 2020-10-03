68abc911346bbf44b1802e0538ffc6f73f705d0c
Kiril <kiril@olgas-air.mynet>
2020-10-03 14:13:41 +0100

    removed weather class from everywhere, refactored the tests in airport_spec

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -10,67 +10,65 @@ describe Airport do
     expect(airport.spaces).to be_empty
   end
 
-  context "stormy weather" do
-    let(:weather) { double :weather, stormy?: true }
+  describe "Landing tests" do
+    context "when sunny" do
+      before do
+        allow(airport).to receive(:stormy?).and_return false
+      end
+      
+      it { is_expected.to respond_to("land").with(1).argument }
+      
+      it "cannot land planes if full" do
+        airport.capacity.times { 
+          airport.land(plane)
+          plane.flying
+        }
+        expect { airport.land(plane) }.to raise_error("Airport full, landing denied.")
+      end
 
-    it "prevents landing in stormy weather" do
-      expect { airport.land(plane) }.to raise_error("Too stormy for landing.")
-    end
+      it "cannot land a plane that is grounded" do
+        allow(plane).to receive(:in_air?).and_return(false)
+        expect { 
+          plane.grounded
+          airport.land(plane) 
+        }.to raise_error("Plane already landed")
+      end
 
-    it "prevents takeoff in stormy weather" do
-      allow(plane).to receive(:in_air?).and_return(false)
-      airport.spaces << plane
-      expect { airport.takeoff(plane) }.to raise_error("Too stormy for takeoff.")
+      it "capacity can be changed" do
+        new_airport = Airport.new(200)
+        200.times { 
+          new_airport.land(plane)
+          plane.flying
+        }
+        expect { new_airport.land(plane) }. to raise_error("Airport full, landing denied.")
+      end
     end
   end
 
-  context "landing a flying plane" do
-    before do
-      airport.land(plane)
+  describe "Takeoff tests" do
+    context "when sunny" do
+      before do
+        allow(airport).to receive(:stormy?).and_return false
+        airport.land(plane)
+      end
+
+      it { is_expected.to respond_to("takeoff").with(1).argument }
+      
+      it "can instruct a plane to take off and confirm plane is gone" do
+        allow(plane).to receive(:in_air?).and_return(false)
+        airport.takeoff(plane)
+        expect(airport.spaces).not_to include(plane)
+      end
+
+      it "cannot takeoff a plane that is flying" do
+        plane.flying
+        expect { airport.takeoff(plane) }.to raise_error("Plane already flying")
+      end
+
+      it "cannot takeoff a plane if airport empty" do
+        airport.spaces.clear
+        expect { airport.takeoff(plane) }.to raise_error("There are no planes to take off")
+      end
     end
-    
-    it "can land a plane" do
-      expect(airport.spaces).to include(plane)
-    end
-
-    it "can instruct a plane to take off and confirm plane is gone" do
-      allow(plane).to receive(:in_air?).and_return(false)
-      airport.takeoff(plane)
-      expect(airport.spaces).not_to include(plane)
-    end
-
-    it "cannot takeoff a plane that is flying" do
-      plane.flying
-      expect { airport.takeoff(plane) }.to raise_error("Plane already flying")
-    end
-  end
-
-  it "cannot land planes if full" do
-    airport.capacity.times { 
-      airport.land(plane)
-      plane.flying
-    }
-    expect { airport.land(plane) }.to raise_error("Airport full, landing denied.")
-  end
-  
-  it "capacity can be changed" do
-    new_airport = Airport.new(200)
-    200.times { 
-      new_airport.land(plane)
-      plane.flying
-    }
-    expect { new_airport.land(plane) }. to raise_error("Airport full, landing denied.")
-  end
-
-  it "cannot takeoff a plane if airport empty" do
-    expect { airport.takeoff(plane) }.to raise_error("There are no planes to take off")
-  end
-
-  it "cannot land a plane that is grounded" do
-    allow(plane).to receive(:in_air?).and_return(false)
-    expect { 
-      plane.grounded
-      airport.land(plane) 
-    }.to raise_error("Plane already landed")
   end
 end
