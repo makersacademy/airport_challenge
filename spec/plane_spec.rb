17cd844a8c4ebdd8e136cb58ef7219c52a994f0c
Emily Sesto <emilyalicesesto@gmail.com>
2020-10-03 17:07:26 +0100

    RED: planes not returning true to in_air? after take-off.

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -3,5 +3,12 @@ require 'plane'
 describe Plane do
   describe '#in_air' do
     it { is_expected.to respond_to(:in_air?) }
+
+    it 'responds true after take-off' do
+      airport = Airport.new
+      airport.land(subject)
+      plane = airport.take_off(subject)
+      expect(plane.in_air?).to eq(true)
+    end
   end
 end
