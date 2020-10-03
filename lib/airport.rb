7cc70b4eb87d9e80b488ebe874c05422bde99b35
Cameron Logie <cameronlogie24@gmail.com>
2020-10-03 18:55:25 +0100

    tried to add airport array and integrate a landed method that increased the airport array length to 1

--- lib/airport.rb
+++ lib/airport.rb
@@ -2,10 +2,16 @@ require_relative 'plane'
 
 class Airport
 
+  def initialize
+    @grounded_planes = []
+  end
 
   def land(plane)
     @plane = plane
   end
 
+  def landed(plane)
+    @grounded_planes.push(plane)
+  end
 
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,6 +1,7 @@
 require 'airport'
 
 describe Airport do
+
   describe '#land(plane)' do
     it 'should take in a plane when a plane is instructed to land' do
       plane = Plane.new
@@ -8,6 +9,23 @@ describe Airport do
       expect(airport.land(plane)).to eq plane
     end
   end
-end
 
-  describe
+  describe '#landed_plane' do
+    it 'should log that the plane is at the airport' do
+      plane = Plane.new
+      airport = Airport.new.land(plane)
+      expect(airport.landed(plane).grounded_planes.length).to eq 1
+    end
+  end
+
+
+  describe '#takeoff(plane)' do
+    it 'should remove a plane from the airport' do
+    end
+  end
+
+  describe '#confirm_takeoff' do
+    it 'should send a message confirming that the plane has left the airport' do
+    end
+  end
+end
