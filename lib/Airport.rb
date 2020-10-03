d6f1bffdf11191183bf75c4d39c36d8284a70c11
David Millen <70208036+dm-devtech@users.noreply.github.com>
2020-10-03 12:25:49 +0100

    Adding test to check there is an array for planes in airport (airport_spec.rb) and updating Airport.rb so that test passes.  Also added test for default airport capacity which fails (Airport.rb not yet updated to pass this test).

--- lib/Airport.rb
+++ lib/Airport.rb
@@ -1,2 +1,10 @@
 class Airport
+attr_reader :name, :planes_in_airport
+
+def initialize(name)
+  @name = name
+  @planes_in_airport = []
+end
+
+
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,4 +1,18 @@
 require 'Airport.rb'
 describe Airport do
 
+    it 'airport can be named' do
+      new_airport = Airport.new("Hamburg")
+      expect(new_airport.name).to eq("Hamburg")
+    end
+
+    it 'plane array empty' do
+      new_airport = Airport.new("Hamburg")
+      expect(new_airport.planes_in_airport).to eq([])
+    end
+
+    it 'default airport capacity = 5' do
+      new_airport = Airport.new("Hamburg")
+      expect(new_airport.capacity).to eq(5)
+    end
 end
