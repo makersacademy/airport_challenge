5e1fde0ae27709bc639ac957f66beb2051412e0e
Krzysztof Jankowski <k-j@xl.wp.pl>
2020-10-03 16:31:50 +0100

    tests refactoring

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,7 +1,7 @@
 require_relative './plane.rb'
 
 class Airport
-  attr_reader :plane, :hangar
+  attr_reader :hangar
 
   def initialize
     @hangar = []
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,47 +2,47 @@ require 'airport'
 
 describe Airport do
 
-  it { is_expected.to respond_to(:allows_for_landing).with(1).argument }
-  it { is_expected.to respond_to(:allows_for_take_off).with(1).argument }
+  let(:airbus747) { Plane.new }
 
-  it 'checks for permission' do
-    airbus747 = Plane.new
-    subject.allows_for_landing(airbus747)
-    expect(airbus747).to be_asking_for_permission
+  context 'during landing' do
+    it { is_expected.to respond_to(:allows_for_landing).with(1).argument }
+
+    it 'checks for landing permission' do
+      subject.allows_for_landing(airbus747)
+      expect(airbus747).to be_asking_for_permission
+    end
+
+    it 'puts plane into a hangar after landing' do
+      subject.allows_for_landing(airbus747)
+      expect(subject.hangar).to include(airbus747)
+    end
   end
 
-  it { is_expected.to respond_to(:allows_for_take_off) }
+  context 'during take off' do
+    it { is_expected.to respond_to(:allows_for_take_off).with(1).argument }
 
-  it'checks for permission' do
-    airbus747 = Plane.new
-    subject.allows_for_landing(airbus747)
-    airbus747 = subject.allows_for_take_off(airbus747)
-    expect(airbus747).to be_asking_for_permission
+    it'checks for take off permission' do
+      subject.allows_for_take_off(airbus747)
+      expect(airbus747).to be_asking_for_permission
+    end
+
+    it 'updating hangar count after plane take off' do
+      subject.allows_for_landing(airbus747)
+      subject.allows_for_take_off(airbus747)
+      expect(subject.hangar.size). to eq 0
+    end
+
+    it 'allows for take off only planes from the hangar' do
+      tupolev102 = Plane.new
+      subject.allows_for_landing(airbus747)
+      subject.allows_for_landing(tupolev102)
+      subject.allows_for_take_off(airbus747)
+      expect(subject.hangar).not_to include(airbus747)
+    end
   end
 
+  context 'about an airport'
   it 'has a hangar' do
     expect(subject.hangar).to be_an_instance_of(Array)
-  end
-
-  it 'puts plane into a hangar after landing' do
-    airbus747 = Plane.new
-    subject.allows_for_landing(airbus747)
-    expect(subject.hangar).to include(airbus747)
-  end
-
-  it 'updating hangar count after plane take off' do
-    airbus747 = Plane.new
-    subject.allows_for_landing(airbus747)
-    subject.allows_for_take_off(airbus747)
-    expect(subject.hangar.size). to eq 0
-  end
-
-  it 'allows for take off only planes from the hangar' do
-    airbus747 = Plane.new
-    tupolev102 = Plane.new
-    subject.allows_for_landing(airbus747)
-    subject.allows_for_landing(tupolev102)
-    subject.allows_for_take_off(airbus747)
-    expect(subject.hangar).not_to include(airbus747)
   end
 end
