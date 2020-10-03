b780383f439c6c759b785babb8e73f24d5678009
Emily Sesto <emilyalicesesto@gmail.com>
2020-10-03 16:42:10 +0100

    RED: Airport class doesn't yet have a take_off method.

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -4,4 +4,8 @@ describe Airport do
   describe '#land' do
     it { is_expected.to respond_to(:land).with(1).argument }
   end
+
+  describe '#take_off' do
+    it { is_expected.to respond_to(:take_off).with(1).argument }
+  end
 end
