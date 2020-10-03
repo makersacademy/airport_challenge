db1566d532639e820a6ab9a50e792159e6c1e002
Ben <ben.lynch1931@gmail.com>
2020-10-03 13:17:09 +0100

    PLANES: Red/Green Test: `put`es status  confirmation to screen
    Airports
      sets default plane capacity to 20 planes
      #full?
        responds to full? method
        returns false when not full
        returns true when full
    
    Planes
      #take_off
        responds when called
        updates status when called
        prints <plane> has taken off
      #land
        responds when called
        updates status when called
        prints <plane> has landed
      #confirm_status
        responds when called
    
    Weather
      initializes with random number
      sets weather to clear when rand between 11-89
      sets weather to stormy when rand <=10 or >=90
    
    Have you considered running rubocop? It will help you improve your code!
    Try it now! Just run: rubocop
    
    Finished in 0.01504 seconds (files took 0.14198 seconds to load)
    14 examples, 0 failures
    
    COVERAGE: 100.00% -- 72/72 lines in 6 files

--- lib/planes.rb
+++ lib/planes.rb
@@ -3,10 +3,12 @@ class Planes
 
   def take_off
     @status = 'in air'
+    puts "#{self} has taken off"
   end
 
   def land
     @status = 'landed'
+    puts "#{self} has landed"
   end
 
   def confirm_status
--- spec/planes_spec.rb
+++ spec/planes_spec.rb
@@ -12,6 +12,10 @@ describe Planes do
       subject.take_off
       expect(subject.status).to eq 'in air'
     end
+
+    it 'prints <plane> has taken off' do
+      expect{ subject.take_off }.to output("#{subject} has taken off\n").to_stdout
+    end
   end
 
   describe '#land' do
@@ -23,6 +27,10 @@ describe Planes do
     it 'updates status when called' do
       subject.land
       expect(subject.status).to eq 'landed'
+    end
+
+    it 'prints <plane> has landed' do
+      expect{ subject.land }.to output("#{subject} has landed\n").to_stdout
     end
   end
 
