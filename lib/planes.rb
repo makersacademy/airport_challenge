97f87e3cb0403a6b40fe84c3147c427a012c7939
Ben <ben.lynch1931@gmail.com>
2020-10-03 13:30:16 +0100

    PLANES: Refactored using SRP

--- lib/planes.rb
+++ lib/planes.rb
@@ -3,22 +3,35 @@ class Planes
 
   def take_off
     if @status == 'in air'
-      puts "Error: #{self} is already in the air..."
+      error_status('take off')
     else
       @status = 'in air'
-      puts "#{self} has taken off"
+      confirm_status('take off')
     end
   end
 
   def land
     if @status == 'landed'
-      puts "Error: #{self} has already landed..."
+      error_status('land')
     else
       @status = 'landed'
-      puts "#{self} has landed"
+      confirm_status('land')
     end
   end
 
-  def confirm_status
+  def confirm_status(action)
+    if action == 'land'
+      puts "#{self} has landed"
+    elsif action == 'take off'
+      puts "#{self} has taken off"
+    end
+  end
+
+  def error_status(action)
+    if action == 'land'
+      puts "Error: #{self} has already landed..."
+    elsif action == 'take off'
+      puts "Error: #{self} is already in the air..."
+    end
   end
 end
