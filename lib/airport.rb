229ebf69f73eff0f89c2d745ee0e956c8d9b7cb9
Otis <otisvickersgraver@gmail.com>
2020-10-04 15:28:59 +0100

    land does not land a plane if airport capacity is full

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,10 +1,12 @@
 class Airport
+  AIRPORT_CAPACITY = 3
   def initialize(airport_name)
-    @hangar = []
+    @hangar = [ ]
     @airport_name = airport_name
   end
 
   def land(plane)
+    raise 'airport full' unless @hangar.length < 3
     @hangar << plane
   end
 
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,13 +2,35 @@ require 'airport'
 require 'plane'
 
 describe Airport do
-  it '#land method lands a plane at an airport' do
+  it '#land lands a plane at an airport' do
     airport = Airport.new("London Airport")
     plane = Plane.new
     expect(airport.land(plane)).to include plane
   end
 
-  it '#depart method departs a plane from an airport' do
+  it '#land does not land plane if airport capacity is full' do
+    airport = Airport.new("London Airport")
+    plane1 = Plane.new
+    plane2 = Plane.new
+    plane3 = Plane.new
+    plane4 = Plane.new
+    airport.land(plane1)
+    airport.land(plane2)
+    airport.land(plane3)
+    expect { airport.land(plane4) }.to raise_error(RuntimeError, 'airport full')
+  end
+
+  # describe "#release_bike" do
+  #   it 'does not release a bike if there are none in the docking station' do
+  #     docking_station = DockingStation.new
+  #     expect { docking_station.release_bike }.to raise_error(RuntimeError, 'There are no bikes available')
+  #   end
+  #
+  # end
+
+
+
+  it '#depart departs a plane from an airport' do
     airport = Airport.new("London Airport")
     plane1 = Plane.new
     plane2 = Plane.new
@@ -18,20 +40,14 @@ describe Airport do
     expect(airport.instance_variable_get(:@hangar).length).to eq 1
   end
 
-  it '#depart method confirms that a plane is no longer in the airport' do
+  it '#depart confirms that a plane is no longer in the airport' do
     airport = Airport.new("London Airport")
     plane = Plane.new
     airport.land(plane)
     expect(airport.depart(plane)). to eq "Plane #{plane} has left London Airport"
   end
+
 end
-
-
 # As an air traffic controller
-# So I can get passengers on the way to their destination
-# I want to instruct a plane to take off from an airport and confirm that it is no longer in the airport
-
-# depart
-#
-# arr.pop(plane)
-# #{plane} has left #{airport}
+# To ensure safety
+# I want to prevent landing when the airport is full
