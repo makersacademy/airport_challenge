f5e16a64de8d39bf9ddb85e27facaed5593f70cd
Maiara <maiara@Maiaras-MacBook-Air.local>
2020-10-04 17:30:25 +0100

    4rth user story, overrides default capacity

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -10,9 +10,14 @@ describe Airport do
     subject.take_off
     expect(subject.take_off).to eq "Plane has taken off"
   end
-  
+
   it " does not allow landing when full" do
     Airport::DEFAULT_CAPACITY.times { subject.land(Airport.new) }
     expect { subject.land(plane) }.to raise_error "Airport is full" 
   end
+  
+  it "overrides default capacity" do
+    airport = Airport.new(75)
+    expect(airport.capacity).to eq 75
+  end 
 end 
