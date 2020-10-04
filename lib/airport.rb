ae96f75a07bb32a1e80b76d6613884484e23e57c
Meryl Rastall <merylrastall@gmail.com>
2020-10-04 09:52:51 +0100

    feat: created new  unit tests and lib files to pass tests

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,11 +1,20 @@
 class Airport
 
   attr_reader :plane
+  attr_reader :capacity
+  attr_reader :planes
+
+  DEFAULT_CAPACITY = 20
+
+  def initialize(capacity = DEFAULT_CAPACITY)
+    @planes = []
+    @capacity = capacity
+  end
 
   def land(plane)
-    fail "The airport is full!" if @plane
-    
-    @plane = plane
+    fail "The airport is full!" if @planes.count == @capacity
+
+    @planes << plane
   end
 
   def take_off
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -9,26 +9,35 @@ describe Airport do
   
   it { is_expected.to respond_to :take_off }
 
-  it "knows that planes are in flight" do
-    plane = airport.take_off
-    expect(plane).to be_in_flight
-  end
-
   it { is_expected.to respond_to :plane }
 
   describe '#land(plane)' do
     it "lands a plane" do
+      airport = Airport.new
       plane = Plane.new
-      expect(airport.land(plane)).to eq plane
+      19.times do airport.land(plane) end
+      
+      expect(airport.land(plane)).to eq airport.planes
     end
 
     it "does not land planes if at full capacity" do
       plane = Plane.new
       airport = Airport.new
-      airport.land(plane)
+      20.times { airport.land(plane) }
 
       expect { airport.land(plane) }.to raise_error 'The airport is full!'
     end
   end
+    
+  it "can have a maximum capacity set buy user" do 
+    airport = Airport.new(30)
 
+    expect(airport.capacity).to eq 30
+  end
+
+  it "has a default capacity of 20" do
+    airport = Airport.new
+
+    expect(airport.capacity).to eq 20
+  end
 end
