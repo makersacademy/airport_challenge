80e08b2566ed9f162f09b4d70d4b0e1850494012
Rebekah Galliano <rebekahgalliano@gmail.com>
2020-10-04 22:21:28 +0100

    RED/GREEN: Weather is stormy when rand is >= 10
    Airport
      is expected to respond to #land with 1 argument
      is expected to respond to #take_off
      #land
        will raise an error if the runway is full
      #take_off
        allows planes to take off
        will raise an error if there are no planes to take off
    
    Plane
      is expected to respond to #landed?
      is expected to respond to #taken_off?
      print plane status
        prints: plane has taken off
    
    Weather
      sets weather_report to stormy when weather >= 10
    
    Have you considered running rubocop? It will help you improve your code!
    Try it now! Just run: rubocop
    
    Finished in 0.01554 seconds (files took 0.14895 seconds to load)
    9 examples, 0 failures
    
    COVERAGE:  98.46% -- 64/65 lines in 6 files
    
    +----------+--------------+-------+--------+---------+
    | coverage | file         | lines | missed | missing |
    +----------+--------------+-------+--------+---------+
    |  87.50%  | lib/plane.rb | 8     | 1      | 5       |
    +----------+--------------+-------+--------+---------+
    5 file(s) with 100% coverage not shown

--- lib/weather.rb
+++ lib/weather.rb
@@ -1,2 +1,11 @@
 class Weather
+  attr_reader :weather
+  @stromy = false
+  def initialize
+    @weather = rand(1..20)
+  end
+
+  def weather_report
+    @weather >= 10 ? @stormy = true : @stromy = false
+  end
 end
--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -1,4 +1,7 @@
 require "./lib/weather"
 describe Weather do
-
+  it "sets weather_report to stormy when weather >= 10" do
+    allow_any_instance_of(Weather).to receive(:rand).and_return(10)
+    expect(Weather.new.weather_report).to eq true
+  end
 end
