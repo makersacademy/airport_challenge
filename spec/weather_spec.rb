ea2d42f85bb7581891d65ab29a88397f9f12a3eb
Rebekah Galliano <rebekahgalliano@gmail.com>
2020-10-04 22:26:02 +0100

    RED/GREEN: Added test and code to check if weather is clear:
    Airport
      is expected to respond to #land with 1 argument
      is expected to respond to #take_off
      #land
        will raise an error if the runway is full
      #take_off
        allows planes to take off
        will raise an error if there are no planes to take off
    
    Plane
      is expected to respond to #landed?
      is expected to respond to #taken_off?
      print plane status
        prints: plane has taken off
    
    Weather
      sets weather_report to stormy when weather >= 10
      sets weather_report to clear when weather <= 9
    
    Have you considered running rubocop? It will help you improve your code!
    Try it now! Just run: rubocop
    
    Finished in 0.01282 seconds (files took 0.14946 seconds to load)
    10 examples, 0 failures
    
    COVERAGE:  98.53% -- 67/68 lines in 6 files
    
    +----------+--------------+-------+--------+---------+
    | coverage | file         | lines | missed | missing |
    +----------+--------------+-------+--------+---------+
    |  87.50%  | lib/plane.rb | 8     | 1      | 5       |
    +----------+--------------+-------+--------+---------+
    5 file(s) with 100% coverage not shown

--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -4,4 +4,9 @@ describe Weather do
     allow_any_instance_of(Weather).to receive(:rand).and_return(10)
     expect(Weather.new.weather_report).to eq true
   end
+
+  it "sets weather_report to clear when weather <= 9" do
+    allow_any_instance_of(Weather).to receive(:rand).and_return(9)
+    expect(Weather.new.weather_report).to eq false
+  end
 end
