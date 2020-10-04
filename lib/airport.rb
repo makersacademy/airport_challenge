0de2ca92aeb6c7eb2e77e476ff3754d8c99aa98f
Tim Castillo-Gill <timgillviola@gmail.com>
2020-10-04 12:51:41 +0100

    Added default capacity and at_capacity method

--- lib/airport.rb
+++ lib/airport.rb
@@ -3,13 +3,28 @@ require_relative 'plane'
 
 class Airport
 
-  def initialize
+  def initialize (capacity = 10)
     @parked = []
+    @capacity = capacity
   end
 
   def land(plane)
     @parked.push(plane)
   end
+
+  def take_off(plane)
+    @parked.pop
+  end
+
+  def at_capacity
+    if @parked.length >= @capacity
+      "Please wait we are at max capacity"
+    else
+      "Please land"
+    end
+  end
+
+
 
 end
 
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -5,8 +5,32 @@ describe Airport do
   it 'commands a plane to land' do
     heathrow = Airport.new
     ba = Plane.new
-    heathrow.land('ba')
+    heathrow.land(ba)
     expect(heathrow).to respond_to(:land).with(1).argument
   end
 
+  it 'commands a plane to take off' do
+    heathrow = Airport.new
+    ba = Plane.new
+    heathrow.take_off(ba)
+    expect(heathrow).to respond_to(:take_off).with(1).argument
+  end
+
+  it 'prevents plane landing when airport is at capacity' do
+    heathrow = Airport.new
+    ba = Plane.new
+    10.times do
+      heathrow.land(ba)
+    end
+
+    expect(heathrow.at_capacity).to eq("Please wait we are at max capacity")
+  end
+
+  # it 'prevents plane landing when weather is stormy' do
+  #   heathrow = Airport.new
+  #   ba = Plane.new
+  #   expect(ba).to receive(:weather).and_return(:stormy)
+  # end
+
+  
 end
