1d9f6f1edecc126f401b39c69f4ea82124398580
Emily Sesto <emilyalicesesto@gmail.com>
2020-10-04 10:49:04 +0100

    GREEN: Second basic test added to weather_spec file. Weather module acting as expected.

--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -8,5 +8,11 @@ describe Weather do
       allow(airport).to receive(:chance_of_storms).and_return(8)
       expect(airport.weather_report).to eq("stormy")
     end
+
+    it 'returns "sunny" when chance_of_storms is < 7' do
+      airport = Airport.new
+      allow(airport).to receive(:chance_of_storms).and_return(6)
+      expect(airport.weather_report).to eq("sunny")
+    end
   end
 end
