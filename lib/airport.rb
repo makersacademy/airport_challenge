9d61ed4d3d738931b7355ab8db4089c30cb5e902
Otis <otisvickersgraver@gmail.com>
2020-10-04 17:24:43 +0100

    test stubs to override random weather, plane departures prevented whilst weather is stormy

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,15 +1,22 @@
 class Airport
   AIRPORT_CAPACITY = 3
+
   def initialize(name, capacity = AIRPORT_CAPACITY)
     @hangar = []
     @name = name
     @capacity = capacity
   end
 
+  def randomize_weather
+    weather = ['sunny', 'sunny', 'sunny', 'sunny', 'stormy']
+    randomize = rand(0..4)
+    weather[randomize]
+  end
+
   def land(plane)
     raise "You can't land. #{@name}'s hangar is full." unless @hangar.length < @capacity
-
-    @hangar << plane
+    raise "You can't land yet. It's too stormy." if randomize_weather == 'stormy'
+    @hangar << plane if randomize_weather == 'sunny'
   end
 
   def depart(plane)
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -8,19 +8,24 @@ describe Airport do
 
   it 'has a variable capacity' do
     airport = Airport.new("London Airport", 30)
+    airport.stub(:randomize_weather).and_return('sunny')
     plane = Plane.new
+
     30.times { airport.land(plane) }
     expect { airport.land(plane) }.to raise_error(RuntimeError, "You can't land. London Airport's hangar is full.")
   end
 
   it '#land lands a plane at an airport' do
     airport = Airport.new("London Airport")
+    airport.stub(:randomize_weather).and_return('sunny')
     plane = Plane.new
     expect(airport.land(plane)).to include plane
   end
 
   it '#land does not land plane if airport capacity is full' do
+
     airport = Airport.new("London Airport")
+    airport.stub(:randomize_weather).and_return('sunny')
     plane1 = Plane.new
     plane2 = Plane.new
     plane3 = Plane.new
@@ -33,6 +38,7 @@ describe Airport do
 
   it '#depart departs a plane from an airport' do
     airport = Airport.new("London Airport")
+    airport.stub(:randomize_weather).and_return('sunny')
     plane1 = Plane.new
     plane2 = Plane.new
     airport.land(plane1)
@@ -43,12 +49,14 @@ describe Airport do
 
   it '#depart confirms that a plane is no longer in the airport' do
     airport = Airport.new("London Airport")
+    airport.stub(:randomize_weather).and_return('sunny')
     plane = Plane.new
     airport.land(plane)
-    expect(airport.depart(plane)). to eq "Plane #{plane} has left London Airport"
+    expect(airport.depart(plane)).to eq "Plane #{plane} has left London Airport"
   end
 
+  it '#randomize_weather returns either "sunny" or "stormy"' do
+    airport = Airport.new("London Airport")
+    expect(airport.randomize_weather).to eq 'sunny' or 'stormy'
+  end
 end
-# As an air traffic controller
-# To ensure safety
-# I want to prevent landing when the airport is full
