2ca1abb509e6a931d275e9c74d0a76e33a2e3df3
Maiara <maiara@Maiaras-MacBook-Air.local>
2020-10-04 18:58:19 +0100

    5th user story, Not allowed to take off when weather is stormy

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,24 +1,34 @@
 class Airport
 
-  attr_reader :capacity, :planes
+  attr_reader :capacity, :planes, :weather
   DEFAULT_CAPACITY = 50
   
   def initialize(capacity = DEFAULT_CAPACITY)
     @planes = []
     @capacity = capacity
+    @weather = weather
   end
   
   def land(plane)
     fail "Airport is full" if full?
+    
 
     @planes << plane
   end 
-
+ 
   def take_off
-    "Plane has taken off"
+    fail "cannot take off, weather is too stormy" if stormy?
   end 
 
   def full?
     @planes.length == @capacity
   end  
+ 
+  def stormy?
+    random == 8
+  end
+
+  def random
+    rand(1..8)
+  end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -5,19 +5,21 @@ describe Airport do
 
   it " allows planes to land" do
     expect(subject).to respond_to(:land).with(1).argument
-  end 
-  it "takes off from an airport" do
-    subject.take_off
-    expect(subject.take_off).to eq "Plane has taken off"
   end
 
   it " does not allow landing when full" do
     Airport::DEFAULT_CAPACITY.times { subject.land(Airport.new) }
     expect { subject.land(plane) }.to raise_error "Airport is full" 
   end
-  
+
   it "overrides default capacity" do
     airport = Airport.new(75)
     expect(airport.capacity).to eq 75
   end 
+
+  it "plane can not takeoff if weather is stormy" do
+    allow(subject).to receive(:random) { 8 }
+    expect { subject.take_off }.to raise_error "cannot take off, weather is too stormy"
+  end
+
 end 
