3da3710b6ec8fa50cdaf14218d78f62c5ff59d8d
Maiara <maiara@Maiaras-MacBook-Air.local>
2020-10-04 17:08:35 +0100

    3rd story, prevent landing when airport is full, updating

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -11,15 +11,6 @@ describe Airport do
     expect(subject.take_off).to eq "Plane has taken off"
   end
   
-#   describe 'initialization' do
-#     subject { Airport.new }
-#     let(:plane) { Plane.new }
-#     it 'defaults capacity' do
-#       described_class::DEFAULT_CAPACITY.times do
-#         subject.land(plane)
-#       end
-#     end
-#   end 
   it " does not allow landing when full" do
     Airport::DEFAULT_CAPACITY.times { subject.land(Airport.new) }
     expect { subject.land(plane) }.to raise_error "Airport is full" 
