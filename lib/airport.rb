7396e52b21d57edf4b09f72a090c3fe2aff0ebbc
Arabella Knowles <arabellaknowles@hotmail.co.uk>
2020-10-04 14:40:56 +0100

    Plane added to bunker when landed

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,6 +1,13 @@
 class Airport
+  attr_reader :bunker
+  def initialize
+    @bunker = []
+  end
   def land(plane)
+    @bunker << Plane.new
   end
   def takeoff(plane)
+  end
+  def capacity
   end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -4,6 +4,12 @@ describe Airport do
   subject(:airport) { described_class.new }
   it { is_expected.to respond_to(:land).with(1).argument }
   it { is_expected.to respond_to(:takeoff).with(1).argument }
+  it { is_expected.to respond_to :capacity }
+  it 'adds plane to bunker when landed' do
+    plane = Plane.new
+    airport.land(plane)
+    expect(airport.bunker.length).to eq(1)
+  end
 
 
 end
