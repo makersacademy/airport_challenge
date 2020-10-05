e1b273b2351a327e7ee06c81cf3f4621520dd14d
Charlie Galbraith <galbraith.charlie@gmail.com>
2020-10-05 12:18:27 +0100

    Refactor splane_spec to be more efficient

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -1,6 +1,8 @@
 require 'plane'
 
 describe Plane do
+  let(:airport) {Airport.new}
+
   it "responds to call to #land from Air Traffic Controller" do
     expect(subject).to respond_to(:land)
   end
@@ -8,16 +10,13 @@ describe Plane do
     expect(subject).to respond_to(:take_off)
   end
   it "Confirms take_off with a message" do
-    airport = Airport.new
-    plane = subject
     allow_any_instance_of(Weather).to receive(:randomizer).and_return(5)
-    airport.request_landing(plane)
-    expect{airport.request_take_off(plane)}.to output("Flying free!\n").to_stdout
+    airport.request_landing(subject)
+    expect{airport.request_take_off(subject)}.to output("Flying free!\n").to_stdout
   end
   it "confirms contains a variable that outputs flight status" do
     plane1 = Plane.new
     plane2 = Plane.new
-    airport = Airport.new
     airport.request_landing(plane1)
     expect(plane2.airborne).to eq(true)
     expect(plane1.airborne).to eq(false)
