5d2ddd7a5e6589f087b0e0679409757d00dd6617
Mace <maceplus@gmail.com>
2020-10-05 03:30:22 +0100

    tests set and passed

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -9,14 +9,18 @@ describe Plane do
   it 'confirm, has plane has taken off?' do
     airport = Airport.new
     airport.land(subject)
-    plane = airport.take_off(subject)
+    plane = airport.take_off
     expect(plane.up_in_air).to eq(true)
   end
 
-  it 'confirm, plane has not taken off?' do
-    airport = Airport.new
-    airport.land(subject)
-    expect(subject.up_in_air).to eq(false)
-  end
+  it 'confirm, plane has not taken off' do
+      expect(subject.up_in_air).to eq(false)
+    end
 
+    it 'responds false after landing, pre-take-off' do
+      airport = Airport.new
+      plane = airport.land(subject)
+      plane = plane.pop
+      expect(plane.up_in_air).to eq(false)
+    end
 end
