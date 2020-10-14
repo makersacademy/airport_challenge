c1865ce02ea18de02432b29dc198e4ed01a18aef
Duncan Morrison <duncanmorrison2001@yahoo.com>
2020-10-14 18:00:14 +0100

    implemented weather attribute and random generator

---
+++ lib/airport.rb
@@ -0,0 +1,12 @@
+class Airport
+  attr_reader :weather
+
+  def initialize
+    @weather = generate_weather
+  end
+
+  def generate_weather
+    rng = rand(1..5)
+    return rng == 5 ? "stormy" : "sunny"
+  end
+end
