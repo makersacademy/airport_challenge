1a1ff4e68ecd2943cf27511ee4eff87640023601
GDCStoney <g.stoney@btinternet.com>
2020-10-14 10:44:53 +0100

    Inital class creation test and pass code

--- spec/classes_spec.rb
+++ spec/classes_spec.rb
@@ -2,7 +2,17 @@
 # inteded to make sure that the "world" and "objects" are present
 # a bit like a board setup check for chess
 
+# checking file structure - update as needed during world creation
 require 'class_plane'
 require 'class_airport'
 require 'user_controller'
 require 'user_designer'
+
+# checking core class availability
+RSpec.describe Airport do
+
+end
+
+RSpec.describe Plane do
+
+end
