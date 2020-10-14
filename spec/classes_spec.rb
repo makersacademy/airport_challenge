3afb4ff2810918e5326b014765f3d9f70cd65950
GDCStoney <g.stoney@btinternet.com>
2020-10-14 10:33:03 +0100

    Tests for code 'world' setup and code to pass :- classes_spec

---
+++ spec/classes_spec.rb
@@ -0,0 +1,8 @@
+# this is the test file for checking Class object availability
+# inteded to make sure that the "world" and "objects" are present
+# a bit like a board setup check for chess
+
+require 'class_plane'
+require 'class_airport'
+require 'user_controller'
+require 'user_designer'
