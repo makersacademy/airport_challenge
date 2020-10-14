ce6eaadf8672d0eb11ba0ac401d8f381b9b945ba
GDCStoney <g.stoney@btinternet.com>
2020-10-14 11:12:00 +0100

    US01 - testing ability to instruct 'plane' to 'land'

---
+++ spec/controller_spec.rb
@@ -0,0 +1,18 @@
+# this is a set of tests to run as a "Functional Test"
+# testing the capabilities of the Air Traffic controller
+require 'user_controller'
+
+=begin
+User story 1 -
+As an Air Traffic Controller
+So I can get passengers to a destination
+I want to instruct a plane to land at an Airport
+=end
+
+# Test breakdown:
+# ability for the controller to instruct plane to land
+RSpec.describe Plane do
+  it "- Controller to instruct with 'land'" do
+    expect(subject).to respond_to(:land)
+  end
+end
