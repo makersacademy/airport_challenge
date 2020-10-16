d7c5e1bcfa1bc79df5a5cd23d50e89573bb1b705
Jack <jack3009@hotmail.co.uk>
2020-10-16 10:21:55 +0100

    A plane can now land at an airport

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,3 +1,17 @@
 class Airport
 
+  attr_reader :airport_array
+
+  def initialize
+    @airport_array = []
+  end
+
+  def land(plane)
+    @airport_array << plane
+  end
+
+end
+
+class Plane
+
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,4 +2,17 @@ require 'airport'
 
 RSpec.describe Airport do
 
+  describe 'land' do
+    it 'land a plane at an airport' do
+      airport = Airport.new
+      plane = Plane.new
+      airport.land(plane)
+      expect(airport.airport_array.length).to eq 1
+    end
+  end
+  
+end
+
+RSpec.describe Plane do
+
 end
