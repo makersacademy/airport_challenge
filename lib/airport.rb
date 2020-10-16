f8c2e312dbb93ecfc0269be6fc3a01b0e73deef6
Jack <jack3009@hotmail.co.uk>
2020-10-16 14:47:46 +0100

    randomised weather integrated into the landing method

--- lib/airport.rb
+++ lib/airport.rb
@@ -2,18 +2,17 @@ class Airport
 
   attr_reader :airport_array
 
-  def initialize(capacity = 5, weather = :sunny)
+  def initialize(capacity = 5)
     @airport_array = []
     @capacity = capacity
-    @weather = weather
   end
 
-  def land(plane)
-    return full ? (raise "The airport is full") : @airport_array << plane
+  def land(plane, forecast)
+    forecast.weather_check == true ? (return full ? (raise "The airport is full") : @airport_array << plane) : "It is too stormy to take off"
   end
 
-  def take_off(plane)
-    return good_weather ?
+  def take_off(plane, forecast)
+    return forecast.weather_check ?
     (@airport_array.delete_at(airport_array.find_index(plane)); "The plane has successfully left the airport") : (raise "It is too stormy to take off")
   end
 
@@ -23,12 +22,22 @@ class Airport
     @airport_array.length >= @capacity
   end
 
-  def good_weather
-    @weather == :sunny
-  end
-
 end
 
 class Plane
+
+end
+
+class Weather
+
+  attr_reader :weather
+
+  def initialize
+    @weather = (rand(1..10) == 10 ? "stormy" : "sunny")
+  end
+
+  def weather_check
+    @weather == "sunny"
+  end
 
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -3,40 +3,56 @@ require 'airport'
 RSpec.describe Airport do
 
   describe '#land' do
+    airport = Airport.new
+    plane = Plane.new
+    weather = Weather.new
     it 'land a plane at an airport' do
-      airport = Airport.new
-      plane = Plane.new
-      airport.land(plane)
+      allow(weather).to receive(:weather_check) { true }
+      airport.land(plane, weather)
       expect(airport.airport_array.length).to eq 1
     end
   end
 
-  describe '#take_off' do
-    it 'a plane takes off from an airport and confirms it is no longer at the airport' do
-      airport = Airport.new
-      plane = Plane.new
-      airport.land(plane)
-      expect(airport.take_off(plane)).to eq "The plane has successfully left the airport"
+  describe '#land' do
+    airport = Airport.new
+    plane = Plane.new
+    weather = Weather.new
+    it 'check the weather and if the weather is stormy, prevent landing' do
+      allow(weather).to receive(:weather_check) { false }
+      airport.land(plane, weather)
+      expect { airport.take_off(plane, weather) }.to raise_error("It is too stormy to take off")
     end
   end
 
   describe '#take_off' do
-    it 'if the weather is stormy, prevent take off' do
-      airport = Airport.new(5, :stormy)
-      plane = Plane.new
-      expect { airport.take_off(plane) }.to raise_error("It is too stormy to take off")
+    airport = Airport.new
+    plane = Plane.new
+    weather = Weather.new
+    it 'a plane takes off from an airport and confirms it is no longer at the airport' do
+      allow(weather).to receive(:weather_check) { true }
+      airport.land(plane, weather)
+      expect(airport.take_off(plane, weather)).to eq "The plane has successfully left the airport"
+    end
+  end
+
+  describe '#take_off' do
+    airport = Airport.new
+    plane = Plane.new
+    weather = Weather.new
+    it 'check the weather and if the weather is stormy, prevent take off' do
+      airport.land(plane, weather)
+      allow(weather).to receive(:weather_check) { false }
+      expect { airport.take_off(plane, weather) }.to raise_error("It is too stormy to take off")
     end
   end
 
   describe "#full?" do
+    airport = Airport.new(0)
+    plane = Plane.new
+    weather = Weather.new
     it 'if an airport is full, you will not be able to land a plane there' do
-      airport = Airport.new(0)
-      plane = Plane.new
-      expect { airport.land(plane) }.to raise_error("The airport is full")
+      allow(weather).to receive(:weather_check) { true }
+      expect { airport.land(plane, weather) }.to raise_error("The airport is full")
     end
   end
-end
-
-RSpec.describe Plane do
-
 end
