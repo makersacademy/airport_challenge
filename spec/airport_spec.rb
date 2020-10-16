4853633171f721e8f1ebdf48fe01f55c7eb1614a
swizel <swizel.sarah@gmail.com>
2020-10-16 16:52:43 +0100

    mocking (double) added to test

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,6 +1,7 @@
 require 'airport'
 
 describe Airport do
+  let(:plane) { Plane.new }
 
   describe '#initialize' do
     it 'sets a default capacity for the airport' do
@@ -10,53 +11,48 @@ describe Airport do
 
   describe '#landing' do
     it 'lands a plane into the airport' do
-      plane = Plane.new
       allow(subject.weather).to receive(:stormy).and_return false
-      expect(subject.land(plane)).to eq "#{plane}, has has landed"
+      plane = :plane
+      expect(subject.land(:plane)).to eq "#{plane}, has has landed"
     end
 
     it 'does not allow landing if hanger full' do
       allow(subject.weather).to receive(:stormy).and_return false
       subject.capacity.times { subject.land(Plane.new) }
-      expect { subject.land(Plane.new) }.to raise_error "Hanger full, connot land"
+      expect { subject.land(:plane) }.to raise_error "Hanger full, connot land"
     end
 
     it 'does not allow landing if stormy' do
-      plane = Plane.new
       allow(subject.weather).to receive(:stormy).and_return true
-      expect { subject.land(plane) }.to raise_error "Weather is stormy cannot land"
+      expect { subject.land(:plane) }.to raise_error "Weather is stormy cannot land"
     end
 
     it 'does not allow landing if plane already landed' do
-      plane = Plane.new
       allow(subject.weather).to receive(:stormy).and_return false
-      subject.land(plane)
-      expect { subject.land(plane) }.to raise_error "Plane already landed"
+      subject.land(:plane)
+      expect { subject.land(:plane) }.to raise_error "Plane already landed"
     end
 
   end
 
   describe '#takeoff' do
     it 'plane takesoff out the airport and confirms its out' do
-      plane = Plane.new
       allow(subject.weather).to receive(:stormy).and_return false
-      subject.land(plane)
-      subject.takeoff(plane)
+      subject.land(:plane)
+      subject.takeoff(:plane)
       expect(subject.planes).to eq []
     end
 
     it 'does not allow takeoff if stormy' do
-      plane = Plane.new
       allow(subject.weather).to receive(:stormy).and_return true
-      expect { subject.takeoff(plane) }.to raise_error "Weather is stormy cannot takeoff"
+      expect { subject.takeoff(:plane) }.to raise_error "Weather is stormy cannot takeoff"
     end
 
     it 'does not allow takeoff if plane already takenoff' do
-      plane = Plane.new
       allow(subject.weather).to receive(:stormy).and_return false
-      subject.land(plane)
-      subject.takeoff(plane)
-      expect { subject.takeoff(plane) }.to raise_error "Plane already taken off"
+      subject.land(:plane)
+      subject.takeoff(:plane)
+      expect { subject.takeoff(:plane) }.to raise_error "Plane already taken off"
     end
   end
 
