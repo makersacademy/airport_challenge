a590e6203f64ffee72a607c5a884cf149de76e6a
Adam Baker <adamjamesbaker@gmail.com>
2020-10-16 09:55:41 +0100

    Red - Tests for landing plane

---
+++ spec/airport_spec.rb
@@ -0,0 +1,17 @@
+require 'airport'
+require 'plane'
+
+describe Airport do
+
+  describe '#land_plane' do
+
+    it 'takes an plane and stores it to the storage' do
+
+      actual_value = nil
+      expected_value = nil
+
+      expect(actual_value).to eq expected_value
+    end
+  end
+
+end
