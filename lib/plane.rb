36772b455d9ab49ebfb2b33681ff9b37ea4f046a
Esther <esthercog97@gmail.com>
2020-10-16 13:15:38 +0100

    Plane conditional basics added

--- lib/plane.rb
+++ lib/plane.rb
@@ -3,11 +3,17 @@ require_relative 'airport'
 class Plane
 
   def land(airport)
+    fail "Airport is full." if airport.full?
+    fail "Weather is stormy. Cannot land." if airport.stormy?
     airport.landed_planes << self
+    airport
   end
 
   def take_off(airport)
+    fail "Weather is stormy. Cannot take off." if airport.stormy?
     airport.landed_planes.delete(self)
+    confirm = "#{self} has left #{airport}."
+    confirm if airport.landed_planes.include?(self) == false
   end
 
 end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -3,20 +3,33 @@ require 'plane'
 RSpec.describe Plane do
 
   it "lands at an airport" do
-    plane = Plane.new
+    Airport.any_instance.stub(:stormy?) {false}
+    Airport.any_instance.stub(:full?) {false}
     airport = Airport.new
+    plane = Plane.new
     plane.land(airport)
     expect(airport.landed_planes.include?(plane)).to eq true
   end
 
-  it "takes off from an airport" do
-    plane = Plane.new
+  it "takes off from an airport and confirms" do
+    Airport.any_instance.stub(:stormy?) {false}
+    Airport.any_instance.stub(:full?) {false}
     airport = Airport.new
+    plane = Plane.new
     plane.land(airport)
+    expect(plane.take_off(airport)).to be_a(String)
     plane.take_off(airport)
     expect(airport.landed_planes.include?(plane)).to eq false
   end
 
+  it "does not land if airport is full" do
+    Airport.any_instance.stub(:stormy?) {false}
+    airport = Airport.new(1)
+    plane1 = Plane.new
+    plane2 = Plane.new
+    plane1.land(airport)
+    expect{plane2.land(airport)}.to raise_error
+  end
 
 
 
