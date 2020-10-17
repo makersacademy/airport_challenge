dcd0da550a3672b6d04aff48a6a20b5aa4947e25
GDCStoney <g.stoney@btinternet.com>
2020-10-17 13:46:49 +0100

    Removes interface for min feature development

--- lib/software_airport.rb
+++ lib/software_airport.rb
@@ -2,14 +2,5 @@ require './lib/class_airport'
 require './lib/class_plane'
 
 def interface
-  action_request = ''
 
-  while action_request != "exit"
-    # minimal interface - replace with command checker later
-    # command checker to only allow 'valid' actions
-    puts "What would you like to do?"
-    puts "list, land-<plane>, exit"
-    action_request = gets.chomp
-
-  end
 end
