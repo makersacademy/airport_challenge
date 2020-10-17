efcd2758fa447996cd754c67ba23ba13f2473776
Ben Jones <benwjones76@outlook.com>
2020-10-17 12:52:16 +0100

    landing in good weather

--- lib/airport.rb
+++ lib/airport.rb
@@ -2,13 +2,17 @@ require_relative "./weather"
 require_relative "./plane"
 
 class Airport
-  def initialize(weather)
+  def initialize(weather = Weather.new)
     @local_weather = weather
   end
 
   def takeoff
     raise "Cant take off due to stormy weather" unless @local_weather.forcast == "sunny"
-    
+
     Plane.new
+  end
+
+  def land(plane)
+    plane
   end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,8 +2,7 @@ require "airport"
 
 describe Airport do
   describe "#takeoff" do
-    weather = Weather.new
-    subject { described_class.new(weather) }
+
     it "expects the method to exist" do
       expect(subject).to respond_to(:takeoff)
     end
@@ -23,6 +22,21 @@ describe Airport do
 
       it "will take off if weather is fine" do
         expect(subject.takeoff).to be_kind_of Plane
+      end
+    end
+  end
+  describe "#land" do
+    it "expects the method to exist" do
+      expect(subject).to respond_to(:land)
+    end
+
+    context "In good weather" do
+      let(:good_weather) { double("Good Weather", forcast: "sunny") }
+      subject { described_class.new(good_weather) }
+
+      it "can land at this airport" do
+        plane = Plane.new
+        expect(subject.land(plane)).to eq plane
       end
     end
   end
