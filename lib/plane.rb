c3785151c33929e1d44d202147717df845b35a79
Eunice Kontor <Eunice.Kontor@ft.com>
2020-10-18 00:49:42 +0100

    take-off code

--- lib/plane.rb
+++ lib/plane.rb
@@ -1,16 +1,28 @@
 class Plane   # the class - is an object = noun in the user story
-  def land   # insatnce method = verb in the user story
+  def land   # instance method = verb in the user story
     @location = "airport"    # intance variable (its like a bucket - take/put information)
   end
 
   def location
-    @location
+    @location = "airport"
+  end
+
+  def capacity
+    @capacity = 1
+  end
+
+  def no_landing
+    @no_landing = "full"
+  end
+
+  def take_off
+    @availability = 0
   end
 end
 
-
+# puts plane 
 # plane_1 = Plane.new 
 # plane_2 = Plane.new 
 
-# plane_1.land   # 
+# plane_1.land   # this will show location as airport because i have given it the string 
 # plane_1.location
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -3,7 +3,7 @@ require 'Plane'
 
 RSpec.describe Plane do  #subject stands for the class (ie plane)
 
-  describe "land_at_airport" do
+  describe "land_at_airport" do    #land_at_airport is my method test
     it {is_expected.to respond_to (:land)}
   end
 
@@ -18,11 +18,36 @@ RSpec.describe Plane do  #subject stands for the class (ie plane)
   #   it {is_expected.to eq ("airport")}
   # end
 
+  #long hand way of writing our test methods
   describe "confirm_location_of_plane" do 
-    plane_1 = Plane.new #making sure i have plane and location are at the right place to do a test against
-    plane_1.land   #same as above note 
-    it "checking that locaiton is now airport" do
-      expect(plane_1.location).to eq ("airport")
+    plane = Plane.new #making sure i have plane and location are at the right place to do a test against
+    plane.land   #same as above note 
+    it "checking that location is now airport" do
+      expect(plane.location).to eq ("airport")
+    end
+  end
+
+  describe "confirm_airport_capacity" do
+    plane = Plane.new
+    plane.capacity
+    it "check that airport is at capacity" do
+      expect(plane.capacity).to eq (1)
+    end
+  end
+
+  describe "no_landing_if_airport_at_capacity" do
+    plane = Plane.new
+    plane.capacity
+    it "should not allow landing if airport at capacity" do
+      expect(plane.no_landing).to eq ("full")
+    end
+  end
+
+  describe "take_off" do
+    plane = Plane.new
+    plane.location
+    it "should tell you the plane not at airport" do
+      expect(plane.take_off).to eq (0)
     end
   end
 
