80743730525fb10a8f3817aead2b93d2049d1d19
Eunice Kontor <Eunice.Kontor@ft.com>
2020-10-18 18:46:36 +0100

    notes taken off the code to leave it clean for reading

--- lib/plane.rb
+++ lib/plane.rb
@@ -1,6 +1,6 @@
-class Plane   # the class - is an object = noun in the user story
-  def land   # instance method = verb in the user story
-    @location = "airport"    # intance variable (its like a bucket - take/put information)
+class Plane   
+  def land   
+    @location = "airport"    
   end
 
   def location
@@ -31,10 +31,3 @@ class Plane   # the class - is an object = noun in the user story
     @capacity = 0
   end
 end
-
-# puts plane 
-# plane_1 = Plane.new 
-# plane_2 = Plane.new 
-
-# plane_1.land   # this will show location as airport because i have given it the string 
-# plane_1.location
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,9 +1,9 @@
 require 'Airport'
 require 'Plane'
 
-RSpec.describe Plane do  #subject stands for the class (ie plane)
+RSpec.describe Plane do 
 
-  describe "land_at_airport" do    #land_at_airport is my method test
+  describe "land_at_airport" do    
     it {is_expected.to respond_to (:land)}
   end
 
@@ -11,17 +11,9 @@ RSpec.describe Plane do  #subject stands for the class (ie plane)
     it {is_expected.to respond_to (:location)}
   end
 
-  # describe "confirm_location_of_plane" do  #this is short hand 
-  #   plane = Plane.new
-  #   plane.land
-  #   subject {plane.location}
-  #   it {is_expected.to eq ("airport")}
-  # end
-
-  #long hand way of writing our test methods
   describe "confirm_location_of_plane" do 
-    plane = Plane.new #making sure i have plane and location are at the right place to do a test against
-    plane.land   #same as above note 
+    plane = Plane.new 
+    plane.land   
     it "checking that location is now airport" do
       expect(plane.location).to eq ("airport") 
     end
