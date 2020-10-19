49ee79a869a219321ff86048fa319a8fc59b8f6d
Bhupesh <bhupesh100@gmail.com>
2020-10-19 10:33:51 +0100

    airport-challenge

---
+++ lib/airport.rb
@@ -0,0 +1,9 @@
+class Airport
+
+  attr_reader :capacity
+
+  def initialize
+    @capacity = 1
+  end
+
+end
---
+++ spec/airport_spec.rb
@@ -0,0 +1,14 @@
+require 'airport'
+
+RSpec.describe Airport do
+
+airport = Airport.new
+
+  describe Airport do
+    it 'should assign a default capacity' do
+      expect(airport.capacity).to eq 1
+
+    end
+  end
+
+end
