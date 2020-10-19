43fa44003f3dfdc1c2f64d4b25937058ae188ed6
Fabiha <fabiha.hassan96@gmail.com>
2020-10-19 13:11:49 +0100

    more doubles and stubbing

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,73 +2,63 @@ require "airport"
 
 describe Airport do
 
+  subject { Airport.new(name) }
   let(:plane) { double :plane }
+  let(:name) { "lhr" }
+
+  before { allow(subject).to receive(:stormy?).and_return(false) }
 
   describe "#takeoff" do
 
     it "raises an error if the plane is already in air" do
-      lhr = Airport.new("lhr")
       allow(plane).to receive(:location) { "in air" }
-      allow(lhr).to receive(:stormy?).and_return(false)
-      expect { lhr.takeoff(plane) }.to raise_error("Plane is already in air")
+      expect { subject.takeoff(plane) }.to raise_error("Plane is already in air")
     end
 
     it "raises an error if the plane is not in the airport" do
-      lhr = Airport.new("lhr")
       jfk = Airport.new("jfk")
       allow(plane).to receive(:location) { jfk }
-      allow(lhr).to receive(:stormy?).and_return(false)
-      expect { lhr.takeoff(plane) }.to raise_error("Plane is not in this airport")
+      expect { subject.takeoff(plane) }.to raise_error("Plane is not in this airport")
     end
 
     it "raises an error if the weather is stormy" do
-      lhr = Airport.new("lhr")
-      allow(plane).to receive(:location) { lhr }
-      allow(lhr).to receive(:stormy?).and_return(true)
-      expect { lhr.takeoff(plane) }.to raise_error("Weather is stormy! Do not takeoff")
+      allow(plane).to receive(:location) { subject }
+      allow(subject).to receive(:stormy?).and_return(true)
+      expect { subject.takeoff(plane) }.to raise_error("Weather is stormy! Do not takeoff")
     end
 
     it "deletes the plane from the planes list" do
-      lhr = Airport.new("lhr")
-      allow(plane).to receive(:location) { lhr }
-      allow(lhr).to receive(:stormy?).and_return(false)
+      allow(plane).to receive(:location) { subject }
       allow(plane).to receive(:change_location).with("in air")
-      lhr.takeoff(plane)
-      expect(lhr.planes.include?(plane)).to eq false
+      subject.takeoff(plane)
+      expect(subject.planes.include?(plane)).to eq false
     end
 
   end
 
   describe "#land" do
     it "raises an error if the plane is already in an airport" do
-      lhr = Airport.new("lhr")
-      allow(plane).to receive(:location) { lhr }
-      allow(lhr).to receive(:stormy?).and_return(false)
-      expect { lhr.land(plane) }.to raise_error("Plane is already in lhr")
+      allow(plane).to receive(:location) { subject }
+      expect { subject.land(plane) }.to raise_error("Plane is already in lhr")
     end
 
     it "raises an error if there is no space in the Airport" do
-      lhr = Airport.new("lhr")
       allow(plane).to receive(:location) { "in air" }
-      allow(lhr).to receive(:stormy?).and_return(false)
-      allow(lhr).to receive(:full?).and_return(true)
-      expect { lhr.land(plane) }.to raise_error("No space in airport! Do not land")
+      allow(subject).to receive(:full?).and_return(true)
+      expect { subject.land(plane) }.to raise_error("No space in airport! Do not land")
     end
 
     it "appends the plane to the planes list" do
-      lhr = Airport.new("lhr")
       allow(plane).to receive(:location) { "in air" }
-      allow(lhr).to receive(:stormy?).and_return(false)
-      allow(plane).to receive(:change_location).with(lhr)
-      lhr.land(plane)
-      expect(lhr.planes.include?(plane)).to eq true
+      allow(plane).to receive(:change_location).with(subject)
+      subject.land(plane)
+      expect(subject.planes.include?(plane)).to eq true
     end
 
     it "raises an error if the weather is stormy" do
-      lhr = Airport.new("lhr")
       allow(plane).to receive(:location) { "in air" }
-      allow(lhr).to receive(:stormy?).and_return(true)
-      expect { lhr.land(plane) }.to raise_error("Weather is stormy! Do not land")
+      allow(subject).to receive(:stormy?).and_return(true)
+      expect { subject.land(plane) }.to raise_error("Weather is stormy! Do not land")
     end
 
   end
