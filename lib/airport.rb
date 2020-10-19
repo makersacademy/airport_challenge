09665100b5ddd54ed8196d9393deca06c4e8cea5
Jamie <jamiejoyce01@gmail.com>
2020-10-19 09:42:36 +0100

    airport

---
+++ lib/airport.rb
@@ -0,0 +1,31 @@
+require_relative 'plane.rb'
+class Airport
+
+  attr_reader :capacity
+
+  def initialize(capacity = 20)
+    @capacity = capacity
+    @hangar = []
+  end
+
+  def takeoff
+    raise "There are no planes currently in the hangar." if @hangar.empty?
+
+    raise "The weather is too stormy to take off." if stormy?
+
+    return @hangar.pop
+  end
+
+  def land(plane)
+    raise "There is not enough space to land another plane!" if @hangar.length >= @capacity
+
+    raise "The weather is too stormy to land." if stormy?
+
+    @hangar << plane
+    return @hangar.last
+  end
+
+  def stormy?
+    rand(1..9) <= 3
+  end
+end
---
+++ spec/airport_spec.rb
@@ -0,0 +1,56 @@
+require 'airport.rb'
+
+RSpec.describe Airport do
+
+  describe "Capacity var" do
+    it { is_expected.to respond_to(:capacity) }
+
+    it 'checks to see if capacity can be changed' do
+      truetest = rand(1..100)
+      airport = Airport.new(truetest)
+      allow(airport).to receive(:stormy?) { false }
+      truetest.times { airport.land(Plane.new) }
+      expect { airport.land(Plane.new) }.to raise_error 'There is not enough space to land another plane!'
+    end
+  end
+
+  describe "Land function" do
+    it 'checks to see if the plane has landed' do
+      plane = Plane.new
+      allow(subject).to receive(:stormy?) { false }
+      expect(subject.land(plane)).to eq plane
+    end
+
+    it 'checks to see if it is too stormy to land' do
+      plane = Plane.new
+      allow(subject).to receive(:stormy?) { true }
+      expect { subject.land(plane) }.to raise_error 'The weather is too stormy to land.'
+    end
+  end
+
+  describe "Takeoff function" do
+    it 'checks to see if the plane has taken off' do
+      allow(subject).to receive(:stormy?) { false }
+      subject.land(Plane.new)
+      plane = subject.takeoff
+      expect(plane).to be_working
+    end
+
+    it 'checks to see if it is too stormy to takeoff' do
+      plane = Plane.new
+      allow(subject).to receive(:stormy?) { false }
+      subject.land(plane)
+      allow(subject).to receive(:stormy?) { true }
+      expect { subject.takeoff }.to raise_error 'The weather is too stormy to take off.'
+    end
+  end
+
+  describe "stormy? function" do
+    it { is_expected.to respond_to(:stormy?) }
+      it 'return true or false' do
+        allow(subject).to receive(:rand).and_return(2)
+        expect(subject.stormy?).to be true
+      end
+
+  end
+end
