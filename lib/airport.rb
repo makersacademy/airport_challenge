60a9209e9e6dfbfc276b94ed7502100eef0c19be
olliekennedy <ollie.kennedy1@gmail.com>
2020-10-26 10:37:46 +0000

    Changed names of planes to be more descriptive

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,32 +1,32 @@
 class Airport
   attr_reader :capacity
-  attr_accessor :planes
+  attr_accessor :hangar
   DEFAULT_CAPACITY = 10
   def initialize(capacity = DEFAULT_CAPACITY)
     @capacity = capacity
-    @planes = []
+    @hangar = []
   end
 
   def land(plane)
     raise "Airport full, go away." if airport_full?
     raise "No landing in stormy weather." if Weather.new.bad?
 
-    @planes << plane
+    @hangar << plane
     "Landing!"
   end
 
   def take_off(plane)
-    raise "That plane is not here." unless @planes.include?(plane)
+    raise "That plane is not here." unless @hangar.include?(plane)
     raise "No flying in stormy weather." if Weather.new.bad?
 
-    @planes.delete(plane)
+    @hangar.delete(plane)
     "I, #{plane}, have taken off."
   end
 
   private
 
   def airport_full?
-    @planes.count >= @capacity
+    @hangar.count >= @capacity
   end
 
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -7,17 +7,17 @@ RSpec.describe Airport do
     context 'weather is good' do
       before(:each) do
         allow_any_instance_of(Weather).to receive(:bad?).and_return(false)
-        @plane = Plane.new
+        @wizz_air = Plane.new
       end
       it 'instructs a plane to land at the airport' do
-        expect(subject.land(@plane)).to eq "Landing!"
+        expect(subject.land(@wizz_air)).to eq "Landing!"
       end
       it 'denies landing if the airport is full' do
         subject.capacity.times {
           extra_plane = Plane.new
           subject.land(extra_plane)
         }
-        expect { subject.land(@plane) }.to raise_error("Airport full, go away.")
+        expect { subject.land(@wizz_air) }.to raise_error("Airport full, go away.")
       end
       it 'denies landing if the airport is full with different capacity' do
         big_airport = Airport.new(Airport::DEFAULT_CAPACITY * 5)
@@ -25,7 +25,7 @@ RSpec.describe Airport do
           extra_plane = Plane.new
           big_airport.land(extra_plane)
         }
-        expect { big_airport.land(@plane) }.to raise_error("Airport full, go away.")
+        expect { big_airport.land(@wizz_air) }.to raise_error("Airport full, go away.")
       end
       it 'allows landing if the airport has capacity with different capacity' do
         big_airport = Airport.new(Airport::DEFAULT_CAPACITY * 5)
@@ -33,17 +33,17 @@ RSpec.describe Airport do
           extra_plane = Plane.new
           big_airport.land(extra_plane)
         }
-        expect(big_airport.land(@plane)).to eq "Landing!"
+        expect(big_airport.land(@wizz_air)).to eq "Landing!"
       end
     end
 
-    context 'weather is good' do
+    context 'weather is bad' do
       before(:each) do
         allow_any_instance_of(Weather).to receive(:bad?).and_return(true)
-        @plane = Plane.new
+        @wizz_air = Plane.new
       end
       it 'denies landing if the weather is stormy' do
-        expect { subject.land(@plane) }.to raise_error("No landing in stormy weather.")
+        expect { subject.land(@wizz_air) }.to raise_error("No landing in stormy weather.")
       end
     end
 
@@ -53,37 +53,37 @@ RSpec.describe Airport do
     context 'weather is good' do
       before(:each) do
         allow_any_instance_of(Weather).to receive(:bad?).and_return false
-        @plane = Plane.new
-        subject.planes << @plane
+        @wizz_air = Plane.new
+        subject.hangar << @wizz_air
       end
 
       it 'instructs a plane to take_off from the airport and confirm it is no longer there' do
-        expect(subject.take_off(@plane)).to eq "I, #{@plane}, have taken off."
-        expect(subject.planes.include?(@plane)).to eq false
+        expect(subject.take_off(@wizz_air)).to eq "I, #{@wizz_air}, have taken off."
+        expect(subject.hangar.include?(@wizz_air)).to eq false
       end
       it 'denies take_off if the plane is not at the airport' do
-        plane2 = Plane.new
-        expect { subject.take_off(plane2) }.to raise_error("That plane is not here.")
+        virgin_atlantic = Plane.new
+        expect { subject.take_off(virgin_atlantic) }.to raise_error("That plane is not here.")
       end
       it 'prevents a plane taking off from a different airport' do
         airport2 = Airport.new
-        expect { airport2.take_off(@plane) }.to raise_error("That plane is not here.")
+        expect { airport2.take_off(@wizz_air) }.to raise_error("That plane is not here.")
       end
       it 'prevents a plane that is already flying from taking off' do
-        subject.take_off(@plane)
-        expect { subject.take_off(@plane) }.to raise_error("That plane is not here.")
+        subject.take_off(@wizz_air)
+        expect { subject.take_off(@wizz_air) }.to raise_error("That plane is not here.")
       end
     end
 
     context 'weather is stormy' do
       before(:each) do
         allow_any_instance_of(Weather).to receive(:bad?).and_return true
-        @plane = Plane.new
-        subject.planes << @plane
+        @wizz_air = Plane.new
+        subject.hangar << @wizz_air
       end
 
       it 'prevents take_off when weather is stormy' do
-        expect { subject.take_off(@plane) }.to raise_error("No flying in stormy weather.")
+        expect { subject.take_off(@wizz_air) }.to raise_error("No flying in stormy weather.")
       end
     end
 
