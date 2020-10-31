23f3bf267d3b4a233372fb9edffd79890cbdee07
Malachi Spencer <m.spencer@live.co.uk>
2020-10-31 09:39:34 +0000

    Refactored Plane respond to land test, added with 1 arg

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -3,7 +3,7 @@ require 'plane'
 describe Plane do
   context '#land' do
     it 'can be called on a Plane' do
-      expect(subject).to respond_to(:land)
+      expect(subject).to respond_to(:land).with(1).argument
     end
   end
 end
