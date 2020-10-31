0ea606476947ef261d33214e29dca59be2e0bf77
Malachi Spencer <m.spencer@live.co.uk>
2020-10-31 10:32:34 +0000

    Green: Added and passed test -  a plane has landed attr false by default

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -5,5 +5,9 @@ describe Plane do
     it 'can be called on a Plane' do
       expect(subject).to respond_to(:landed)
     end
+
+    it 'is false - i.e. flying - by default' do
+      expect(subject.landed).to be_instance_of(FalseClass)
+    end
   end
 end
