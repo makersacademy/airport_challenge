fd74111ec8a51202d13cc34df2a3d7e171e44125
Malachi Spencer <m.spencer@live.co.uk>
2020-10-31 14:38:56 +0000

    Added test that weather returns sunny or stormy at random and passed it

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -63,5 +63,17 @@ describe Airport do
     it 'returns a string' do
       expect(subject.weather).to be_instance_of(String)
     end
+
+    it 'returns sunny or stormy at random' do
+      outputs = []
+
+      until outputs.include?('sunny') && outputs.include?('stormy') do
+        outputs.push(subject.weather)
+      end
+
+      outputs.each do |output| 
+        expect(output).to eq('sunny').or eq('stormy')
+      end
+    end
   end
 end
