a156de8ec6094bf0b5b7a6fa64988369f56ad819
TamMelPer <mystic101@gmail.com>
2020-10-31 14:22:49 +0000

    2nd User Story: Instruct a plane to take off from the airport

--- lib/airport.rb
+++ lib/airport.rb
@@ -3,7 +3,11 @@ require_relative 'plane'
 class Airport
 
   def land(plane)
+    plane = Plane.new
+  end
+
+  def take_off
 
   end
-  
+
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -4,4 +4,8 @@ describe Airport do
   it 'responds to #land with one argument' do
     expect(subject).to respond_to(:land).with(1).argument
   end
+
+  it 'responds to #take_off with one argument' do
+    expect(subject).to respond_to :take_off
+  end
 end
