6b0ccad73ac2074a5a87351c06cda0ba4dc9e68d
K Annabel Bligh <kabligh@gmail.com>
2020-11-01 17:48:42 +0000

    Refactored with new full? method and capacity constant

--- lib/airport.rb
+++ lib/airport.rb
@@ -2,18 +2,25 @@ require_relative 'plane'
 
 class Airport
   attr_reader :hangar
+  DEFAULT_CAPACITY = 100
 
   def initialize
     @hangar = []
   end
 
   def land(plane)
-    raise "Airport is full" if @hangar.count >= 100
+    raise "Airport is full" if full?
     hangar << plane 
   end
 
   def take_off(plane)
     @hangar.pop
   end
+
+  private
+
+  def full?
+    @hangar.count >= DEFAULT_CAPACITY
+  end 
 
 end
