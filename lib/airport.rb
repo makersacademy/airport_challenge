69d9e5f53658ca66c4771910f036612a54550698
Francesco <francescofacca@gmail.com>
2020-11-01 00:23:15 +0000

    Completed User Story 3

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,9 +1,21 @@
 class Airport
 
+  def initialize(capacity)
+    @capacity = capacity
+    @planes = []
+  end
+
   def land(plane)
+    raise "ATTENTION - It is not possibile to land because the airport is full!" if full?
+    @planes << plane
   end
 
   def take_off(plane)
   end
 
+  private
+
+  def full?
+    @planes.length >= @capacity
+  end
 end
--- spec/Airport_spec.rb
+++ spec/Airport_spec.rb
@@ -1,7 +1,8 @@
 require "airport"
 
 describe Airport do
-  subject(:airport) { described_class.new }
+  let(:plane) { double :plane }
+  subject(:airport) { described_class.new(100) }
 
   it "instructs a plane to land at an airport" do
     expect(airport).to respond_to(:land)
@@ -9,5 +10,12 @@ describe Airport do
 
   it "instructs a plane to take off from an airport" do
     expect(airport).to respond_to(:take_off)
+  end
+
+  it "is not possible to land when airport is full" do
+    100.times do
+      airport.land(plane)
+    end
+    expect { airport.land(plane) }.to raise_error "ATTENTION - It is not possibile to land because the airport is full!"
   end
 end
