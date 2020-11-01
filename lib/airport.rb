7966e6e2a51d1f0ce0429d29e2b5dde42243d02b
K Annabel Bligh <kabligh@gmail.com>
2020-11-01 10:18:33 +0000

    User story 3

--- lib/airport.rb
+++ lib/airport.rb
@@ -4,13 +4,15 @@ class Airport
   attr_reader :plane
 
   def land(plane)
-    @plane
+    raise "Airport is full" if @plane # assumes airport has one plane capacity
+    
+    @plane = plane
     # hangar = []
     # hangar << @plane
   end
 
   def take_off(plane)
-    @plane = plane
+    @plane = plane 
   end
 
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,18 +2,31 @@ require 'airport'
 
 describe Airport do
 
-    it "can land a plane" do
-      expect(subject).to respond_to(:land).with(1).argument
-    end
+  describe "#land" do 
 
-  it "instructs a plane to take off" do
-    expect(subject).to respond_to(:take_off).with(1).argument 
-  end 
+    it "prevents a landing if airport is full" do 
+      subject.land(Plane.new)
+      expect { subject.land Plane.new }.to raise_error "Airport is full"
+    end 
 
-  it "it confirms a plane that has taken off is not in the airport" do 
-    plane = Plane.new
-    subject.take_off(plane)
-    expect(subject.plane).to eq plane 
+    # it "can land a plane" do
+    #  expect(subject).to respond_to(:land).with(1).argument
+    # end
+
+  end
+
+  describe "#take_off" do 
+
+    it "instructs a plane to take off" do
+      expect(subject).to respond_to(:take_off).with(1).argument 
+    end 
+
+    it "confirms a plane that has taken off is not in the airport" do 
+      plane = Plane.new
+      subject.take_off(plane)
+      expect(subject.plane).to eq plane 
+    end 
+
   end 
 
 end
