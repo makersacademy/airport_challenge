87809ecdbf553e652fd57003b2bee05b08681077
Symion Edwards <symion.edwards@gmail.com>
2020-11-01 08:57:55 +0000

    Completed user story 4 by creating a default capacity that can be overridden during Airport instance creation

--- lib/airport.rb
+++ lib/airport.rb
@@ -2,15 +2,16 @@ require_relative 'plane'
 
 class Airport
   attr_reader :planes
+  DEFAULT_CAPACITY = 20
 
-  def initialize
+  def initialize(capacity = DEFAULT_CAPACITY)
     @planes = []
+    @capacity = capacity
   end
 
   def land(plane)
     raise "This plane has already landed" if @planes.include?(plane)
-    raise "Airport full! You cannot land here!" if @planes.count >= 20
-
+    raise "Airport full! You cannot land here!" if @planes.count >= @capacity
     @planes << plane
   end
 
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -13,15 +13,14 @@ describe Airport do
       subject.land(plane)
       expect(subject.planes).to include(plane)
     end
-    describe "When a plane is already in an airport"
-      it "Can not land" do
-        plane = Plane.new
-        subject.land(plane)
-        expect { subject.land(plane) }.to raise_error
-      end
+    it "When a plane is in an airport, it will not respond to #land" do
+      plane = Plane.new
+      subject.land(plane)
+      expect { subject.land(plane) }.to raise_error "This plane has already landed"
+    end
     describe "When airport is at capacity" do
       it "Does not allow planes to land" do
-        20.times { subject.land Plane.new } 
+        Airport::DEFAULT_CAPACITY.times { subject.land Plane.new } 
         expect { subject.land Plane.new }.to raise_error "Airport full! You cannot land here!"
       end
     end
