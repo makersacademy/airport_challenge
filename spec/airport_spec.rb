211f730f17a3a2f23773f2ee2baa5489d5f99974
Margarida Martelo <margmartelo@gmail.com>
2020-11-01 16:45:59 +0000

    Adding third unit test: throwing an error if trying to land at an airport at full capacity

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -7,20 +7,29 @@ require 'plane'
 
 describe Airport do
   it "instruct a plane to land at an airport" do
-    airport = Airport.new
+    airport = Airport.new(10)
     plane = Plane.new
     expect(airport.land(plane)).to eq 1
   end
-
 
 # As an air traffic controller
 # So I can get passengers on the way to their destination
 # I want to instruct a plane to take off from an airport and confirm that it is no longer in the airport
 
   it "take_off method to return updated number of airplanes" do
-    airport = Airport.new
+    airport = Airport.new(10)
     plane = Plane.new
     airport.land(plane)
     expect(airport.take_off(plane)).to eq 0
+  end
+
+# As an air traffic controller
+# To ensure safety
+# I want to prevent landing when the airport is full
+
+  it "it will throw an error if airport at full capacity" do
+    airport = Airport.new(10)
+    10.times { airport.land(Plane.new) }
+    expect { airport.land(Plane.new) }.to raise_error("Airport at full capacity")
   end
 end
