e580ca52c3fdb0d297af47715a14688bd2252161
TamMelPer <mystic101@gmail.com>
2020-11-01 08:32:21 +0000

    3rd User Story Prevent landing with an error message when the airport is full(10)

--- lib/airport.rb
+++ lib/airport.rb
@@ -9,11 +9,17 @@ attr_reader :plane
   end
 
   def land(plane)
+    fail 'Airport Full' if full?
     @plane << plane
   end
 
   def take_off(plane)
     @plane.pop
   end
+
+  private
+    def full?
+      @plane.count >= 10
+    end
 
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,9 +2,17 @@ require 'airport'
 
 describe Airport do
 
+  it "Allows the user to create an instance of Airport" do
+    expect(subject).to be_an_instance_of(Airport)
+  end
+
   describe "#land" do
     it 'responds to #land with one argument' do
       expect(subject).to respond_to(:land).with(1).argument
+    end
+    it 'raises an error when full' do
+      10.times { subject.land Plane.new }
+      expect { subject.land Plane.new }.to raise_error 'Airport Full'
     end
   end
 
