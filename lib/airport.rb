33f8db84ccb349dc2309e286db69d23611110103
Alex Withington-Smith <alex@awithsmith.com>
2020-11-01 18:50:51 +0000

    changed weather to be a module

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,16 +1,13 @@
 require_relative 'weather'
 
 class Airport
+  include Weather
+
   DEFAULT_CAPACITY = 50
   attr_reader :capacity, :planes
 
   def initialize(capacity = DEFAULT_CAPACITY, planes = [])
     @capacity = capacity
     @planes = planes
-    @weather = Weather.new
-  end
-
-  def current_weather
-    @weather.current_weather
   end
 end
--- lib/weather.rb
+++ lib/weather.rb
@@ -1,4 +1,4 @@
-class Weather
+module Weather
   def current_weather
     rand(1..5) == 1 ? 'stormy' : 'chill'
   end
