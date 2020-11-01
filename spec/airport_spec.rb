afca724d783d9e2782c2e2222b7c7975678cc4fe
Margarida Martelo <margmartelo@gmail.com>
2020-11-01 20:34:25 +0000

    Adding fifth unit test: testing weather conditions on take_off

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -4,10 +4,16 @@
 
 require 'airport'
 require 'plane'
+require 'weather'
 
 describe Airport do
+  let(:weather) {
+    double(:weather)
+  }
+
   it "instruct a plane to land at an airport" do
-    airport = Airport.new(10)
+    allow(weather).to receive(:weather_status).and_return "good"
+    airport = Airport.new(weather, 10)
     plane = Plane.new
     expect(airport.land(plane)).to eq 1
   end
@@ -17,7 +23,8 @@ describe Airport do
 # I want to instruct a plane to take off from an airport and confirm that it is no longer in the airport
 
   it "take_off method to return updated number of airplanes" do
-    airport = Airport.new(10)
+    allow(weather).to receive(:weather_status).and_return "good"
+    airport = Airport.new(weather, 10)
     plane = Plane.new
     airport.land(plane)
     expect(airport.take_off(plane)).to eq 0
@@ -28,7 +35,8 @@ describe Airport do
 # I want to prevent landing when the airport is full
 
   it "it will throw an error if airport at full capacity" do
-    airport = Airport.new(10)
+    allow(weather).to receive(:weather_status).and_return "good"
+    airport = Airport.new(weather, 10)
     10.times { airport.land(Plane.new) }
     expect { airport.land(Plane.new) }.to raise_error("Airport at full capacity")
   end
@@ -39,12 +47,21 @@ describe Airport do
 # I would like a default airport capacity that can be overridden as appropriate
 
   it "it will throw an error if airport at default capacity" do
-    airport = Airport.new()
+    allow(weather).to receive(:weather_status).and_return "good"
+    airport = Airport.new(weather)
     Airport::DEFAULT_CAPACITY.times { airport.land(Plane.new) }
     expect { airport.land(Plane.new) }.to raise_error("Airport at full capacity")
   end
-end
+
 
 # As an air traffic controller
 # To ensure safety
 # I want to prevent takeoff when weather is stormy
+
+  it 'throw an error at takeoff when wheater is stormy' do
+    allow(weather).to receive(:weather_status).and_return "stormy"
+    airport = Airport.new(weather, 10)
+    plane = Plane.new
+    expect { airport.take_off(plane) }.to raise_error("The storm is preventing us to takeoff")
+  end
+end
