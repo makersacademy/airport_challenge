2dbc74d24a4c9221490c0df1e025ca2a3bf05709
savannaelbey <savanna_e@icloud.com>
2020-11-02 12:22:34 +0000

    refactoring

--- lib/Plane.rb
+++ lib/Plane.rb
@@ -4,13 +4,13 @@ class Plane
 
   def land(airport)
     fail "Airport full!" if airport.planes.count >= airport.capacity
-    fail "Stormy conditions preventing landing" if airport.stormy? == true
+    fail "Stormy conditions preventing landing" if airport.stormy?
 
     airport.planes.push(self)
   end
 
   def take_off(airport)
-    fail "It's stormy, cannot take off!" if airport.stormy? == true
+    fail "It's stormy, cannot take off!" if airport.stormy?
 
     airport.planes.delete(self)
     "Plane no longer in airport"
