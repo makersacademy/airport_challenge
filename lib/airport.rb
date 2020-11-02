95964df4ff457f884259bfc2ac88ee433df613b1
Samantha <samanthagottlieb31@gmail.com>
2020-11-02 01:57:46 +0000

    Add #full? tests and method, update #land method to raise error if airport full

--- lib/airport.rb
+++ lib/airport.rb
@@ -8,11 +8,16 @@ class Airport
   end
 
   def land(plane)
+    raise 'Unable to land due to full airport' if full?
     @planes << plane
   end
 
   def take_off(plane)
     @planes.delete(plane)
     @planes
+  end
+
+  def full?
+
   end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -4,7 +4,7 @@ describe Airport do
   plane = Plane.new
 
   it 'allows the user to create an Airport instance' do
-    expect(subject).to be_instance_of Airport
+    expect(subject).to be_instance_of(Airport)
   end
 
   describe '#land' do
@@ -25,6 +25,14 @@ describe Airport do
     it 'confirms that the plane is no longer in the airport' do
       subject.land(plane)
       expect(subject.take_off(plane)).to eq(subject.planes)
+    end
+  end
+
+  context '#full?' do
+    it 'prevents landing if airport full' do
+      allow(subject).to receive(:full?).and_return(true)
+      error_message = 'Unable to land due to full airport'
+      expect { subject.land(plane) }.to raise_error error_message
     end
   end
 end
