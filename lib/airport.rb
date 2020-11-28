8422fc57bcbafec196dd47f744273c19efa2682a
Will-Helliwell <68912961+Will-Helliwell@users.noreply.github.com>
2020-11-28 13:23:06 +0000

    abstracted full? method from land_plane

--- lib/airport.rb
+++ lib/airport.rb
@@ -9,13 +9,17 @@ class Airport
   end
 
   def land_plane(plane)
-    fail "Airport full!" if @planes.length == @capacity
+    fail "Airport full!" if self.full?
     @planes << plane
   end
 
   def take_off_plane(plane)
     fail "Plane not at airport!" if !@planes.include?(plane)
     @planes.delete(plane)
+  end
+
+  def full?
+    @planes.length == @capacity ? true : false
   end
 
 end
