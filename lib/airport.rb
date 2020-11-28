215b33838dc72739eb7e3b60db41240b0e182074
Kiki Dawson <kikiadawson@gmail.com>
2020-11-28 19:05:47 +0000

    added planes method

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,8 +1,12 @@
 class Airport
-  def land_plane
+  def land(plane)
+    plane
   end
-  
+
   def take_off
     "Plane has taken off"
   end
+
+  def planes
+  end 
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,20 +1,32 @@
 require 'airport'
+require 'plane'
 
 describe Airport do
 
-  it 'allows planes to land in an airport' do
-    airport = Airport.new
-    expect(airport).to respond_to(:land_plane)
-  end
+  it { is_expected.to respond_to(:land).with(1).argument }
+  it { is_expected.to respond_to(:take_off) }
 
-  it 'allows planes to take off from an airport' do
-    airport = Airport.new
-    expect(airport).to respond_to(:take_off)
+  it 'returns plane when plane lands in airport' do
+    plane = Plane.new
+    expect(subject.land(plane)).to eq plane
   end
 
   it 'returns message to say plane has taken off after take_off' do
-    airport = Airport.new
-    expect(airport.take_off).to eq "Plane has taken off"
+    plane = Plane.new
+    subject.land(plane)
+    expect(subject.take_off).to eq "Plane has taken off"
   end
 
+  it { is_expected.to respond_to(:planes) }
+
+  # it 'can hold planes' do
+  #   expect(subject)
+  # end
+
+  # it 'raises an error when plane tries to land in full airport' do
+  #   airport = Airport.new
+  #   plane = Plane.new
+  #   airport.land(plane)
+  #   expect(airport.land(plane)).to raise_error 'Airport full; plane cannot land'
+  # end
 end
