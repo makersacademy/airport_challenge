ee5b8cc499486557c66a513ddce4eee23e1c8865
Will-Helliwell <68912961+Will-Helliwell@users.noreply.github.com>
2020-11-28 10:39:27 +0000

    create land_plane function. Create instance variable @planes, which is updated by .land_plane

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,8 +1,24 @@
-require './spec/plane_spec.rb'
 require './lib/airport.rb'
 
 describe Airport do
   it "can create instances of itself" do
     expect(subject).to be_an(Airport)
   end
+  it "can show planes currently at the airport" do
+    p1 = Plane.new
+    subject.land_plane(p1)
+    expect(subject.planes).to eq(p1)
+  end
+
+  describe '#land_plane' do
+    it "can be called on an airport with 1 argument" do
+      expect(subject).to respond_to(:land_plane).with(1).argument
+    end
+    it "adds the landed plane to the airport" do
+      p1 = Plane.new
+      subject.land_plane(p1)
+      expect(subject.planes).to eq(p1)
+    end
+  end
+
 end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -1,8 +1,6 @@
 require 'rspec'
 require './lib/plane.rb'
 
-require './spec/airport_spec.rb'
-
   describe Plane do
     it "can create instances of itself" do
       expect(subject).to be_a(Plane)
