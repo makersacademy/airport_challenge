953802a0adb398a9e8e754f5e1e275b5efca2205
Kiki Dawson <kikiadawson@gmail.com>
2020-11-29 19:38:21 +0000

    default capacity added and can be overridden

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,12 +1,31 @@
 class Airport
+  attr_accessor :capacity
+  attr_reader :planes
+
+  DEFAULT_CAPACITY = 5
+
+  def initialize(capacity = DEFAULT_CAPACITY)
+    @capacity = capacity
+    @planes = []
+  end
+
   def land(plane)
-    plane
+    fail 'Airport full' if full
+
+    planes << plane
   end
 
-  def take_off
-    "Plane has taken off"
+  def take_off(plane)
+    # fail 'No planes available' if @planes.empty?
+    #
+    planes.delete(plane)
+    "#{plane} has taken off"
   end
 
-  def planes
-  end 
+  private
+
+  def full
+    planes.count >= capacity
+  end
+
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -4,29 +4,57 @@ require 'plane'
 describe Airport do
 
   it { is_expected.to respond_to(:land).with(1).argument }
-  it { is_expected.to respond_to(:take_off) }
+  it { is_expected.to respond_to(:take_off).with(1).argument }
 
-  it 'returns plane when plane lands in airport' do
-    plane = Plane.new
-    expect(subject.land(plane)).to eq plane
-  end
-
-  it 'returns message to say plane has taken off after take_off' do
+  it 'removes plane from airport after take off' do
     plane = Plane.new
     subject.land(plane)
-    expect(subject.take_off).to eq "Plane has taken off"
+    subject.land(Plane.new)
+    subject.take_off(plane)
+    expect(subject.planes).not_to include(plane)
+  end
+
+  it 'returns a message to say plane has taken-off' do
+    plane = Plane.new
+    subject.land(plane)
+    expect(subject.take_off(plane)).to include "#{plane} has taken off"
   end
 
   it { is_expected.to respond_to(:planes) }
 
-  # it 'can hold planes' do
-  #   expect(subject)
-  # end
+  it 'allows planes to land' do
+    plane = Plane.new
+    expect(subject.land(plane)).to eq [plane]
+  end
 
-  # it 'raises an error when plane tries to land in full airport' do
-  #   airport = Airport.new
-  #   plane = Plane.new
-  #   airport.land(plane)
-  #   expect(airport.land(plane)).to raise_error 'Airport full; plane cannot land'
+  it 'adds landed plane to planes array' do
+    plane = Plane.new
+    subject.land(plane)
+    expect(subject.planes).to include(plane)
+  end
+
+  it 'has default capacity' do
+    expect(subject.capacity).to eq Airport::DEFAULT_CAPACITY
+  end
+
+  describe '#land' do
+    it 'raises an error when the airport is full' do
+      subject.capacity.times { subject.land(Plane.new) }
+      expect { subject.land(Plane.new) }.to raise_error 'Airport full'
+    end
+  end
+
+  describe 'initialization' do
+    it 'has a variable capacity' do
+      airport = Airport.new(10)
+      10.times { airport.land(Plane.new) }
+      expect { airport.land(Plane.new) }.to raise_error 'Airport full'
+    end
+  end
+
+  # describe '#take_off' do
+  #   it 'raises an error when there is no plane to take off' do
+  #     expect { subject.take_off }.to raise_error 'No planes available'
+  #   end
   # end
 end
