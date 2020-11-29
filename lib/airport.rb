3bef591b3e78cbf999cbcecaa38f575e233e87b7
calavell <calavell@tcd.ie>
2020-11-29 17:44:24 +0000

    refactored land_plane and take_off methods

--- lib/airport.rb
+++ lib/airport.rb
@@ -13,6 +13,7 @@ class Airport
   end
 
   def land_plane(plane)
+    reset_airport_weather
     fail 'too stormy' if stormy?
 
     fail 'plane already on land' if plane.flying? == false
@@ -21,17 +22,16 @@ class Airport
 
     @hangar << plane
     plane.current_location = 'landed'
-    reset_airport_weather
   end
 
   def take_off(plane)
+    reset_airport_weather
     fail 'too stormy' if stormy?
 
     fail 'plane not located here' unless plane_present?(plane)
 
     @hangar.delete(plane)
     plane.current_location = 'flying'
-    reset_airport_weather
   end
 
   def plane_present?(plane)
