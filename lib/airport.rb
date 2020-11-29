9a00727fb83d86882d877bc53813a5fbda3c1684
calavell <calavell@tcd.ie>
2020-11-29 12:30:45 +0000

    First passing test: Airport responds to land_plane with 1 argument

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,34 +1,6 @@
-require './lib/plane.rb'
-
 class Airport
 
-  attr_reader :planes, :capacity
-  DEFAULT_CAPACITY = 10
-
-  def initialize(capacity = DEFAULT_CAPACITY)
-    @planes = []
-    @capacity = capacity
-  end
-
-  def receive_plane(plane)
-    fail 'Airport full!' if full? == true
-
-    @planes.push(plane)
-  end
-
-  def request_take_off(plane)
-    fail 'No planes left!' if empty? == true
-
-    @planes.delete(plane)
+  def land_plane(plane)
 
   end
-
-  def full?
-    @planes.length >= @capacity
-  end
-
-  def empty?
-    @planes.empty? == true
-  end
-
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,50 +1,8 @@
 require 'airport'
-require 'plane'
 
 describe Airport do
   let(:airport) { described_class.new }
-  it 'responds to receive_plane method' do
-    expect(subject).to respond_to(:receive_plane).with(1).argument
-  end
-
-  it 'responds to request_take_off method' do
-    expect(subject).to respond_to(:request_take_off).with(1).argument
-  end
-
-  it 'can accept 1 argument with initialize' do
-    expect(Airport).to respond_to(:new).with(1).argument
-  end
-
-  describe '#receive_plane' do
-    it 'allows airport to accept a plane and store it' do
-      plane = Plane.new
-      airport.receive_plane(plane)
-      expect(airport.planes).to include(plane)
-    end
-
-    it 'accepts instances of Plane class' do
-      plane = Plane.new
-      airport.receive_plane(plane)
-      expect(airport.planes[-1]).to be_instance_of Plane
-    end
-
-    it 'raises error when airport is full' do
-      plane = Plane.new
-      airport.capacity.times { airport.receive_plane(plane) }
-      expect { airport.receive_plane(plane) }.to raise_error 'Airport full!'
-    end
-  end
-
-  describe '#request_take_off' do
-    it 'removes a plane from the airport' do
-      plane = Plane.new
-      airport.receive_plane(plane)
-      expect(airport.request_take_off(plane)).to be_instance_of Plane
-    end
-
-    it 'raises error when no planes left in airport' do
-      plane = Plane.new
-      expect { airport.request_take_off(plane) }.to raise_error 'No planes left!'
-    end
+  it 'responds to land_plane' do
+    expect(subject).to respond_to(:land_plane).with(1).argument
   end
 end
