8e82a1c7f246990decf11c96f8c56302bff183b5
calavell <calavell@tcd.ie>
2020-11-29 12:53:34 +0000

    Third passing test: Plane responds to flying? method

--- lib/plane.rb
+++ lib/plane.rb
@@ -1,3 +1,13 @@
 class Plane
 
+  attr_reader :current_airport
+
+  def initialize
+    @current_airport = nil
+  end
+
+  def flying?
+    @current_airport == nil
+  end
+
 end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -1,5 +1,9 @@
 require 'plane'
 
 describe Plane do
-  
+  let(:plane) { described_class.new }
+  let(:airport) { Airport.new}
+  it 'responds to flying? method' do
+    expect(plane).to respond_to(:flying?)
+  end
 end
