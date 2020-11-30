79f1804fe4f25442d725fd7468fe5a0ba363654b
Natasha McIntyre <natashamc.cs@gmail.com>
2020-11-30 11:58:38 +0000

    amend airport.rb to include fail? method

--- lib/airport.rb
+++ lib/airport.rb
@@ -12,21 +12,13 @@ class Airport
   end
 
   def land(plane)
-    fail "Airport full" if full?
-
-    fail "Plane already landed" if present?(plane)
-
-    fail "Landing aborted due to stormy weather" if stormy?
-
+    landing_fail(plane)
     plane.land
     hangar << plane
   end
 
   def take_off(plane)
-    fail "Plane not at this airport" unless present?(plane)
-
-    fail "Take off aborted due to stormy weather" if stormy?
-
+    take_off_fail(plane)
     plane.depart
     hangar.delete(plane)
   end
@@ -41,6 +33,22 @@ class Airport
 
   def stormy?
     weather.stormy?
+  end
+
+  def landing_fail(plane)
+    fail "Airport full" if full?
+
+    fail "Plane already landed" if present?(plane)
+
+    fail "Landing aborted due to stormy weather" if stormy?
+
+  end
+
+  def take_off_fail(plane)
+    fail "Plane not at this airport" unless present?(plane)
+
+    fail "Take off aborted due to stormy weather" if stormy?
+
   end
 
 #  private
