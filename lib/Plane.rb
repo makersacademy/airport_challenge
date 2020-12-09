17340893a4c1a112844cd7704176c655ead51474
AtiyyahRawat <atiyyah.rawat@gmail.com>
2020-12-09 13:24:04 +0000

    Set up Plane IDs that increment by 1 each plane that's created

--- lib/Plane.rb
+++ lib/Plane.rb
@@ -1,2 +1,13 @@
 require 'TrafficController'
 require 'Airport'
+
+class Plane
+  @@next_id = 1
+  attr_reader :ID
+
+  def initialize
+    @ID = @@next_id
+    @@next_id += 1
+  end
+
+end
--- spec/Plane_spec.rb
+++ spec/Plane_spec.rb
@@ -1,7 +1,20 @@
 require 'Plane'
 
 describe Plane do
-    it do
-        
-    end
+
+  it "The first plane to be created has an ID of 1" do
+    plane = Plane.new
+    expect(plane.ID).to eq 1
+  end
+
+  it "Each plane has a unique ID" do
+    plane_ids = []
+    5.times { plane_ids << Plane.new.ID }
+    # Create 5 planes and push their ids onto the array
+    num_of_repeated_ids = plane_ids.count(plane_ids[0])
+    # Count how many of the first ID in the list are in the array
+    # Should be one
+    expect(num_of_repeated_ids).to eq 1 
+  end
+
 end
