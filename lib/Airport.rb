daef65d6c023c58de409d8069b780572cf43031f
AtiyyahRawat <atiyyah.rawat@gmail.com>
2020-12-09 14:20:44 +0000

    Add ability for airport to check if a plane is in the airport and to land a plane

--- lib/Airport.rb
+++ lib/Airport.rb
@@ -1,2 +1,21 @@
 require 'TrafficController'
 require 'Plane'
+
+class Airport
+
+  def initialize
+    @max_capacity = 5
+    @airplanes = []
+  end
+
+  def in_airport?(search_plane)
+    return @airplanes.any? {|airplane| airplane.ID == search_plane.ID }
+    # returns true if any of the airplanes in the array have
+    # the same ID as the search plane
+  end
+
+  def land_plane(plane)
+    @airplanes << plane
+  end
+
+end
--- spec/Airport_spec.rb
+++ spec/Airport_spec.rb
@@ -1,7 +1,23 @@
 require 'Airport'
+require 'Plane'
 
 describe Airport do
-    it do
-        
+
+    it "checks to see if a plane is in the airport" do
+      airport = Airport.new
+      plane = Plane.new
+      result = airport.in_airport?(plane)
+      # Create new plane and ask airport if it is in the airport
+      expect(result).to eq false
     end
+   
+    it "lets a plane be landed" do
+        airport = Airport.new
+        plane = Plane.new
+        airport.land_plane(plane)
+        was_successful = airport.in_airport?(plane)
+        expect(was_successful).to eq true
+    end
+
+   
 end
