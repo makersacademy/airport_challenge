8b2779ee567130472e257dff8e2e8eb1976f4b33
Bianca <biannca.iordache@gmail.com>
2020-12-14 10:36:45 +0000

    initial commit

---
+++ lib/airport.rb
@@ -0,0 +1,30 @@
+class Airport
+
+  def initialize
+    @hangar = []
+  end
+
+  def land(plane)
+    @hangar << plane
+  end
+
+  def hangar
+    @hangar
+  end
+
+  def take_off(plane)
+    @hangar.delete(plane)
+  end
+
+  def full?
+    fail 'Airport is full - plane cannot land' if @hangar.length >= 1
+  end
+
+  def prevent_landing?
+    fail 'Bad weather, plane cannot land' if true
+  end
+
+  def prevent_take_off?
+    fail 'Bad weather, plane cannot take off' if true
+  end
+end
