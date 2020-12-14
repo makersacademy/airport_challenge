5674c6d5da0f3d81017e05131eaa07b2c025f6ed
Mio Chung <miochung7@gmail.com>
2020-12-14 10:39:03 +0000

    amended weather_spec

--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -1,23 +1,17 @@
 require 'weather'
 
 describe Weather do
-  let(:current_weather) { Weather.new }
+  let(:weather) { Weather.new }
 
-  describe '#random' do
-    it 'generates a random number between 1 and 3' do
-      allow(current_weather).to receive(:rand).and_return(1)
-    end
-  end
-
-  describe '#condition' do
+  describe '#stormy_weather?' do
     it 'assigns weather as sunny' do
-      allow(current_weather).to receive(:condition).and_return('sunny')
-      expect(current_weather.condition).to eq 'sunny'
+      allow(weather).to receive(:stormy_weather?).and_return(false)
+      expect(weather.stormy_weather?).to eq false
     end
 
     it 'assigns weather as stormy' do
-      allow(current_weather).to receive(:condition).and_return('stormy')
-      expect(current_weather.condition).to eq 'stormy'
+      allow(weather).to receive(:stormy_weather?).and_return(true)
+      expect(weather.stormy_weather?).to eq true
     end
   end
 end
