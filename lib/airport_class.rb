ae40cf0b9f4d3db77cb6f8265e6ea1182eb45ab5
Elena <elenaevans10@gmail.com>
2020-12-16 10:50:24 +0000

    added functional tests and methods for user stories 2-3

--- lib/airport_class.rb
+++ lib/airport_class.rb
@@ -1,12 +1,18 @@
 require "plane_class.rb"
 class Airport 
   attr_accessor :planes
+  AIRPORT_CAPACITY = 5
   def initialize
     @planes = []
   end
 
   def land_plane(plane)
+    fail "The plane cannot land as the airport is full" if @planes.length >= AIRPORT_CAPACITY
     @planes.push(plane)
     puts @planes
+  end
+
+  def take_off(plane)
+    @planes.delete(plane)
   end
 end
--- spec/airport_class_spec.rb
+++ spec/airport_class_spec.rb
@@ -1,10 +1,46 @@
 require "airport_class.rb"
 
 describe Airport do
+  # As an air traffic controller 
+  # So I can get passengers to a destination 
+  # I want to instruct a plane to land at an airport
 
-  it "has a land_plane method that adds a plane to the hangar" do
+  it "can land planes and put them into the hangar" do
     plane = Plane.new
     subject.land_plane(plane)
     expect(subject.planes).to include plane
   end
+
+  # As an air traffic controller 
+  # So I can get passengers on the way to their destination 
+  # I want to instruct a plane to take off from an airport and confirm that it is no longer in the airport
+
+  it "can instruct a plane to take off from the airport" do
+    plane = Plane.new
+    subject.land_plane(plane)
+    subject.take_off(plane)
+    expect(subject.planes).not_to include plane
+  end
+
+  # As an air traffic controller 
+  # To ensure safety 
+  # I want to prevent landing when the airport is full 
+
+  it "does not allow a plane to land when the airport is full" do
+    plane = Plane.new
+    Airport::AIRPORT_CAPACITY.times {subject.land_plane(plane)}
+    expect{subject.land_plane(plane)}.to raise_error "The plane cannot land as the airport is full"
+  end
+
+  # As the system designer
+  # So that the software can be used for many different airports
+  # I would like a default airport capacity that can be overridden as appropriate
+
+  # As an air traffic controller 
+  # To ensure safety 
+  # I want to prevent takeoff when weather is stormy 
+
+  # As an air traffic controller 
+  # To ensure safety 
+  # I want to prevent landing when weather is stormy
 end
