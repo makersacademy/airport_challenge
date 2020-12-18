6ae44885e1935d821970f70f9e5806959f8e67c4
Bianca <biannca.iordache@gmail.com>
2020-12-18 14:56:08 +0000

    refactor to use let

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,10 +2,11 @@ require 'airport'
 
 describe Airport do
 
+  let(:airport) { Airport.new }
+
   describe '#land' do
     it 'takes a plane and puts it in the hangar' do
       # arrange -> gather all the objects we need
-      airport = Airport.new
       #act -> execute the method under test
       airport.land('BA123')
       # assert -> place some assertion regarding what the method should have done
@@ -13,7 +14,6 @@ describe Airport do
     end
 
     it 'raises an error when full' do
-      airport = Airport.new
       airport.land('BA123')
       expect{ airport.land('BA123') }.to raise_error 'Airport is full'
     end
@@ -22,7 +22,6 @@ describe Airport do
   describe '#take_off' do
     it 'takes a plane and removes it from the hangar' do
       # arrange
-      airport = Airport.new
       airport.land('BA123')
       #act
       airport.take_off('BA123')
