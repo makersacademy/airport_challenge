49c0f41a536a61905fd86d5d5fe30123b9a6c346
IliasMariosG <57366310+IliasMariosG@users.noreply.github.com>
2021-01-07 20:33:38 +0200

    User Story 3 - it prevents landing when the airport is full (1 plane capacity)

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,11 +1,19 @@
 #require './lib/plane.rb'
 
 class Airport 
-  attr_reader :plane
+  attr_reader :capacity, :planes
+
+  def initialize(capacity)
+    @capacity = capacity
+    @planes = 0
+  end
+
   def land(plane)
-    @plane = plane
+    raise 'Airport cannot accept more than one' if @planes >= @capacity
+    @planes += 1
   end
 
   def take_off(plane)
+    @planes -= 1
   end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,29 +1,46 @@
 require 'simplecov'
 require 'airport'
 
-describe Airport do
-  let(:airport) { Airport.new }
-  
-  it { is_expected.to respond_to(:land).with(1).argument }
+describe Airport do  
+  # The one-liner below is not necessary for the purposes of our methods to do something
+  # it { is_expected.to respond_to(:land).with(1).argument }
   
   describe '#land' do
     
     it 'lands a plane' do
+      airport = Airport.new(1)
       plane = Plane.new
 
       actual_result = airport.land(plane)
-      expected_result = plane
+      expected_result = 1
 
       expect(actual_result).to eq expected_result
     end
+    
+    it 'does not allow planes to land when airport is full (capacity == 1 plane)' do
+      airport = Airport.new(1)
+      plane = Plane.new
+      
+      airport.land(plane)
+      
+      expect { airport.land(plane) }.to raise_error 'Airport cannot accept more than one'
+    end
   end
   
-  it { is_expected.to respond_to(:take_off).with(1).argument }
+  # The one-liner below is not necessary for the purposes of our methods, since we want out methods do something
+  #it { is_expected.to respond_to(:take_off).with(1).argument }
   
   describe '#take_off' do
     
     it 'allows a plane to take off from the airport' do
-    
+      airport = Airport.new(1)
+      plane = Plane.new
+
+      landed_plane = airport.land(plane)
+      actual_result = airport.take_off(landed_plane)
+      expected_result = 0
+
+      expect(actual_result).to eq expected_result
     end
   end
 end
