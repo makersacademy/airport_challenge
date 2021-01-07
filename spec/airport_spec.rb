2e64790022ffc35004201a2ccea4806b49543e7c
IliasMariosG <57366310+IliasMariosG@users.noreply.github.com>
2021-01-07 14:30:31 +0200

    Refactor using one-liner syntax

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,18 +2,23 @@ require 'simplecov'
 require 'airport'
 
 describe Airport do
+  let(:airport) { Airport.new }
+  
+  it { is_expected.to respond_to(:land).with(1).argument }
+  
   describe '#land' do
+    
     it 'can land a plane' do
-      airport = Airport.new
       
-      expect(airport).to respond_to(:land).with(1).argument
     end
   end
+  
+  it { is_expected.to respond_to(:take_off).with(1).argument }
+  
   describe '#take_off' do
-    it 'allows a plane to take off from the airport' do
-      airport = Airport.new
     
-    expect(airport).to respond_to(:take_off).with(1).argument
+    it 'allows a plane to take off from the airport' do
+    
     end
   end
 end
