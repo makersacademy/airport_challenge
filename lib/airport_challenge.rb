4ddcfebd3339dbc027f64c8c17cf04a1e78e28fe
Anna Cavalla <anna.cavalla@gmail.com>
2021-01-08 18:02:55 +0000

    Passes takeoff test

--- lib/airport_challenge.rb
+++ lib/airport_challenge.rb
@@ -1,5 +1,15 @@
 class Airport
-  def land(plane)
-    @plane = plane
+  def initialize
+    @planes = []
   end
+
+  def land(plane)
+    @planes << plane
+  end
+
+  def takeoff(plane)
+    @planes.delete(plane)    
+  end
+
+  attr_reader :planes
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -4,17 +4,13 @@ require 'plane'
 describe Airport do
   it "receives a landing plane" do
     plane = Plane.new
-    expect(subject.land(plane)).to eq plane
+    expect(subject.land(plane)[0]).to eq plane
   end
 
   it "a plane takes off and is gone from airport" do
     plane = Plane.new
     subject.land(plane)
-    planes = subject.contents.length
     subject.takeoff(plane)
-    expect(planes - 1).to eq subject.contents.length
-    expect(subject.contents).not_to include plane
+    expect(subject.planes).not_to include plane
   end
 end
-
-#subject.dock(bike)
