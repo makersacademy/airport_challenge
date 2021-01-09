231c466a3f763fa7c04b4ca99a52abd9ea8b5df1
Oscar Bertrand <oscarb1989@yahoo.co.uk>
2021-01-09 12:54:55 +0000

    Raising expectation when airport if full

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,18 +1,18 @@
 class Airport
 
-attr_reader :plane
+    attr_reader :plane
 
-def initialize
-    @hangar = []
-end
+    def initialize
+        @hangar = []
+    end
 
-def land(plane)
-    fail 'Airport is full' if @plane
-    @plane = plane
-end
+    def land(plane)
+        fail 'Airport is full' if @plane
+        @hangar << plane
+    end
 
-def take_off(plane)
-    @hangar.pop(plane)
-end
+    def take_off(plane)
+        @hangar.pop(plane)
+    end
 
 end
