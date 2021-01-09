b3167c25eca127de8da76dbf4411dbdd4b44428f
megan gosnell <megan_gosnell@aol.co.uk>
2021-01-09 11:10:41 +0000

    user story 2 complete and refractoring

--- lib/airport_challenge.rb
+++
@@ -1,5 +0,0 @@
-class Airport
-    def land_plane
-        puts "Land at an airport"
-    end 
-end 
--- spec/airport_challenge_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'airport_challenge'
-describe Airport do 
-
-    it 'responds to plane landing' do 
-        airport = Airport.new
-    expect(airport).to respond_to(:land_plane)
-    end 
-end 
