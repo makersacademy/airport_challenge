dabe26c212ccd868f0cddc95128995eaf550fe0b
Reeshul <reeshul@hotmail.com>
2021-01-09 18:13:27 +0000

    completed third user story

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,14 +2,19 @@ require 'airport'
 
 describe Airport do
 
-  subject(:airport) { Airport.new }
+  subject(:airport) { Airport.new(20) }
 
-  it 'instruct a plane to land at an airport' do
+  it 'instruct a plane to land' do
     expect(airport).to respond_to(:land).with(1).argument
   end
 
-  it 'instruct a plane to take off from an airport' do
+  it 'instruct a plane to take off' do
     expect(airport).to respond_to(:take_off).with(1).argument
   end
 
+  it 'does not allow landing when full' do
+    plane = double :plane
+    20.times { airport.land(plane) }
+    expect { airport.land(plane) }.to raise_error "Cannot land plane - airport full"
+  end
 end
--- spec/features/user_stories_spec.rb
+++ spec/features/user_stories_spec.rb
@@ -3,7 +3,7 @@ describe 'User Stories' do
   # I want to instruct a plane to land at an airport
 
   it 'instruct a plane to land at an airport' do
-    airport = Airport.new
+    airport = Airport.new(20)
     plane = Plane.new
     expect {airport.land(plane)}.not_to raise_error
   end
@@ -11,9 +11,18 @@ describe 'User Stories' do
   # I want to instruct a plane to take off from an airport
 
   it 'instruct a plane to take off from an airport' do
-    airport = Airport.new
+    airport = Airport.new(20)
     plane = Plane.new
     expect {airport.take_off(plane)}.not_to raise_error
+  end
+
+  # I want to prevent landing when the airport is full 
+
+  it 'prevents planes landing when airport is full' do
+    airport = Airport.new(20)
+    plane = Plane.new
+    20.times {airport.land(plane)}
+    expect { airport.land(plane) }.to raise_error "Cannot land plane - airport full"
   end
 
 end
