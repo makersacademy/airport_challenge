808654e01d242ce8d2164b0a2ef0dc2992ecedc1
Hugh Cavanagh <hacavanagh@gmail.com>
2021-01-09 15:53:33 +0000

    Added test for contains? in Airport and passed test

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,9 +1,13 @@
 class Airport
-
+  def contains?(plane)
+  end
 end
 
 class Plane
   def land(airport)
     self
+  end
+
+  def take_off
   end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,18 +1,25 @@
 require 'airport'
 
 describe Airport do
-
+  it { is_expected.to respond_to(:contains?).with(1).argument}
 end
 
 describe Plane do
   it { is_expected.to respond_to(:land).with(1).argument }
+  it { is_expected.to respond_to(:take_off) }
+
+  let(:airport) { Airport.new }
 
   describe '#land' do
-    let(:airport) { Airport.new }
     context 'when an airport is given as argument' do
       it 'returns the plane that just landed' do
         expect(subject.land(airport)).to eq subject
-      end 
+      end
     end
   end
+
+  describe '#take_off' do
+
+  end
+
 end
