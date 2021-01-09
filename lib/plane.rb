4c8b94e09f6d507b84a2f7420b8bc3bfc2d36c08
Karsten Finlay <karstenfinlay@hotmail.com>
2021-01-09 12:11:13 +0000

    Plane class created

---
+++ lib/plane.rb
@@ -0,0 +1,7 @@
+class Plane
+
+  def ready_to_fly
+
+  end
+
+end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,9 +1,15 @@
 require "airport"
+require "plane"
 
 describe Airport do
 
-  it "responds to land" do
-    expect(subject).to respond_to :land
+  it "checks if a plane instance was created" do
+    plane = Plane.new
+    expect(plane).to respond_to(:ready_to_fly)
+  end
+
+  it "lands plane at airport" do
+    expect(subject).to respond_to(:land).with(1).argument
   end
   
 end
