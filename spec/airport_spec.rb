5f6af136407ff69aabc117e1e639f2215abbcc75
Sean Edwards <seantrevoredwards@gmail.com>
2021-01-09 08:01:58 +0000

    first user story 'As an air traffic controller, So I can get passengers to a destination, I want to instruct a plane to land at an airport' completed

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -5,4 +5,10 @@ describe Airport do
 
   it { is_expected.to respond_to(:land).with(1).argument }
 
+  describe '#land' do
+    it 'should let a plane land at the airport' do
+      plane = Plane.new
+      expect(subject.land(plane)).to eq plane
+    end
+  end
 end
