103657f1cb9c14201c59e8c72b4f2d63d9a42a9f
Sean Edwards <seantrevoredwards@gmail.com>
2021-01-09 18:04:32 +0000

    refactored rspec test to include context for full

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,7 +2,7 @@ require 'airport'
 
 describe Airport do
   subject(:airport) { described_class.new }
-  #Let(:plane) { double :plane }
+  # Let(:plane) { Plane.new }
   plane = Plane.new
 
   it { is_expected.to respond_to(:land).with(1).argument }
@@ -17,7 +17,7 @@ describe Airport do
       expect(airport.land(plane)).to eq [plane]
     end
     context 'when full' do
-      it 'it raises an error' do
+      it 'it raises an error "Airport is full"' do
         Airport::DEFAULT_CAPACITY.times { airport.land(plane) }
         expect { airport.land(plane)}.to raise_error "Airport is full"
       end
@@ -28,6 +28,12 @@ describe Airport do
     it 'allows a plane to take off and confirm plan has left' do
       airport.land(plane)
       expect(airport.take_off).to eq "#{plane} has taken off."
+    end
+  end
+  context 'when stormy' do 
+    it 'it raises an error "Cannot land, due to stormy weather"' do
+      allow(airport).to receive(:stormy?).and_return true
+      expect { airport.land(plane) }.to raise_error 'Cannot land, due to stormy weather'
     end
   end
 end
