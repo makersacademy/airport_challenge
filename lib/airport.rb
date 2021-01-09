37b8400dea7de8293d5ef9b24e15472be809bf1b
Oscar Bertrand <oscarb1989@yahoo.co.uk>
2021-01-09 11:30:21 +0000

    Pop planes from hangar when taking_off

--- lib/airport.rb
+++ lib/airport.rb
@@ -2,12 +2,16 @@ class Airport
 
 attr_reader :plane
 
+def initialize
+    @hangar = []
+end
+
 def land(plane)
     @plane = plane
 end
 
 def take_off(plane)
-    
+    @hangar.pop(plane)
 end
 
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -6,7 +6,7 @@ describe Airport do
         expect(Airport.new).to respond_to(:land).with(1).argument
     end
 
-    it 'reports plane in airport' do
+    it 'reports plane in hangar' do
         plane = Plane.new
         expect(subject.land(plane)).to eq plane
     end
@@ -14,4 +14,9 @@ describe Airport do
     it 'should repond to take_off' do
         expect(Airport.new).to respond_to(:take_off).with(1).argument
     end
+
+    # it 'confirms that the plane is not longer in the hangar' do
+    #     expect(subject.take_off(Plane.new)).to eq @hangar.pop(Plane.new)
+    # end
+
 end
