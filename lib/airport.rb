f006fbbf2f0adcb7597df423cc32dacca7ed8d8b
Sean Edwards <seantrevoredwards@gmail.com>
2021-01-09 09:00:05 +0000

    fourth user story completed 'As the system designer,   So that the software can be used for many different airports I would like a default airport capacity that can be, overridden as appropriate'

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,14 +1,25 @@
 require_relative 'plane'
 
 class Airport
+  DEFAULT_CAPACITY = 20
 
+  attr_reader :planes
+  attr_accessor :capacity
+
+  def initialize(capacity = DEFAULT_CAPACITY)
+    @planes = []
+    @capacity = capacity
+  end
   def land(plane)
-    fail "Airport is full" if @plane
-    @plane = plane
+    fail "Airport is full" if full?
+    @planes.push(plane)
   end
 
   def take_off
-    "#{@plane} has taken off."
+    "#{@planes.pop} has taken off."
   end
 
+  def full?
+    @planes.count >= capacity
+  end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -7,20 +7,21 @@ describe Airport do
   it { is_expected.to respond_to(:land).with(1).argument }
   it { is_expected.to respond_to(:take_off) }
 
-  describe '#land' do
+  it 'should have a default capacity' do
+    expect(airport.capacity).to eq Airport::DEFAULT_CAPACITY
+  end
 
+  describe '#land' do
     it 'should let a plane land at the airport' do
-      expect(airport.land(plane)).to eq plane
+      expect(airport.land(plane)).to eq [plane]
     end
     it 'should not let planes land if airport is full' do
-      airport.land(plane)
+      Airport::DEFAULT_CAPACITY.times { airport.land(plane) }
       expect { airport.land(plane)}.to raise_error "Airport is full"
     end
-
   end
 
   describe '#take_off' do
-
     it 'allows a plane to take off and confirm plan has left' do
       airport.land(plane)
       expect(airport.take_off).to eq "#{plane} has taken off."
