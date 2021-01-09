2fd17c6114492eefc5ad0e9f2b17f0268417cff9
Dan T <dp.tyler@hotmail.co.uk>
2021-01-09 13:23:33 +0000

    Step 2 complete - plane lands

--- lib/airport.rb
+++ lib/airport.rb
@@ -2,8 +2,25 @@ require_relative 'plane'
 
 class Airport
 
-		def land(plane)
-			@plane = plane
+		attr_reader :hanger
+
+		def initialize
+				@hanger = []
 		end
+
+		def land(plane)
+				plane = plane
+				hanger << plane
+				plane
+		end
+
+		def take_off(plane)
+				plane = plane 
+				hanger.reject! do 
+					|x| x == plane
+				end
+				puts "#{plane} has taken off"
+				self
+		end 
 
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -5,8 +5,17 @@ describe Airport do
 		it { is_expected.to respond_to(:land).with(1).argument }
 
 		it 'lands a plane' do
-			plane = Plane.new
-			expect(subject.land(plane)).to eq plane
+				plane = Plane.new
+				expect(subject.land(plane)).to eq plane
+		end
+
+		it { is_expected.to respond_to(:take_off).with(1).argument }
+
+		it 'takes off a plane' do 
+				plane = Plane.new 
+				plane = Plane.new
+				subject.land(plane)
+				expect(subject.take_off(plane)).to eq subject
 		end
 
 end 
