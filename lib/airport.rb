261af9d04eec5753ce83103e5f2df007e65e874c
Morne van Zyl <Morne@Mornes-MacBook-Air.local>
2021-01-09 16:49:29 +0000

    Added Plane class and array to store planes.

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,20 +1,34 @@
+require_relative 'plane'
+
 class Airport
-    attr_reader :full, :capacity
+    attr_reader :planes, :capacity
 
     DEFAULT_CAPACITY = 20
 
     def initialize(capacity=DEFAULT_CAPACITY)
-        @full = true
+        @planes = []
         @capacity = capacity
     end
-    def land
-        fail 'Airport is full' if @full
-        true
+    def land(plane)
+        fail 'Airport is full' if full?
+        fail 'Weather conditions too stormy for landing' if stormy?
+        @planes.push(plane)
     end
     def take_off
-        fail 'Weather conditions too stormy for landing' if stormy?
-        true
+        fail 'No planes available' if empty?
+        fail 'Weather conditions too stormy for take-off' if stormy?
+        @planes.pop
     end
+
+    private
+    def full?
+        return @planes.count >= @capacity ? true : false
+    end
+
+    def empty?
+        return @planes.empty? ? true : false
+    end
+
     def stormy?
         rand(10) > 7
     end
