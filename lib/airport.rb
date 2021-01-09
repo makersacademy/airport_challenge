b3e1655dcf088c404e6b3530ca8c5a8ae9469ff1
Katrina Harradine <katrinaharradine@gmail.com>
2021-01-09 14:22:40 +0000

    User story 3 - I want to prevent landing when the airport is full

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,8 +1,14 @@
 require_relative 'plane'
 
 class Airport
+  def initialize(capacity)
+    @capacity = capacity
+    @hangar = []
+  end
+  
   def land(plane)
-    raise "Airport is full" if plane
+    raise "Airport is full" if @hangar.length <= @capacity
+    @hangar << plane
   end
 
   def take_off(plane)
