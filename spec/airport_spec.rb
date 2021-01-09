808a68ea03e5dc1bb01963d85b9b7d176800fbfe
Miranda Wilson <mirandascwilson@gmail.com>
2021-01-09 16:27:35 +0000

    test for airport full

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -3,8 +3,15 @@ require "plane"
 
 describe Airport do 
 
+  it "full if add planes to capacity" do
+    Airport::DEFAULT_CAPACITY.times { Plane.new.land(subject) }
+    expect(subject).to be_full
+  end
+
   it "prevents landing when full" do
     Airport::DEFAULT_CAPACITY.times { Plane.new.land(subject) }
     expect { Plane.new.land(subject) }.to raise_error "Unable to land; airport at capacity"
   end
+
+
 end
