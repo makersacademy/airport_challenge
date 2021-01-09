646f250d791d73d35bf3ac3f210fca9e2096c60f
Glykeria Stravodimou <strglykeria@gmail.com>
2021-01-09 11:38:46 +0000

    Raising error when full

--- lib/airport.rb
+++ lib/airport.rb
@@ -5,11 +5,18 @@ class Airport
   end
 
   def land(plane)
+    fail "Airport is full" if full?
+
     @landed_planes << plane
   end
 
   def takeoff(plane)
     @landed_planes.delete(plane)
     "#{plane} has left the Airport"
+  end
+
+  private
+  def full?
+    !@landed_planes.empty?
   end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -8,10 +8,23 @@ describe Airport do
     expect(subject.land(plane)).to eq [plane]
   end
 
-  it {is_expected.to respond_to(:takeoff).with(1).argument}
+  it 'should raise error when airport full' do
+    plane = Plane.new
+    subject.land(plane)
+    expect { subject.land(plane) }.to raise_error "Airport is full"
+  end
+
+  it { is_expected.to respond_to(:takeoff).with(1).argument }
   it 'should instruct a plane to take off' do
     plane = Plane.new
     subject.land(plane)
     expect(subject.takeoff(plane)).to eq "#{plane} has left the Airport"
+  end
+
+
+  it 'should return true or false if full' do
+    expect(subject.send(:full?)).to eq false
+    subject.land(Plane.new)
+    expect(subject.send(:full?)).to eq true
   end
 end
