f4b6c218defb4ebb5d77d3a52723e7cf7dbcbd7c
Dan T <dp.tyler@hotmail.co.uk>
2021-01-09 11:24:05 +0000

    First commit - plane landing

---
+++ spec/airport_spec.rb
@@ -0,0 +1,12 @@
+require 'airport'
+
+describe Airport do
+
+		it { is_expected.to respond_to(:land).with(1).argument }
+
+		it 'lands a plane' do
+			plane = Plane.new
+			expect(subject.land(plane)).to eq plane
+		end
+
+end 
