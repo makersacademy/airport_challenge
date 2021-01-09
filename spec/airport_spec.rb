56c47e2caa7cd10daa4f646ebb550e04b4a949b0
Karsten Finlay <karstenfinlay@hotmail.com>
2021-01-09 16:00:53 +0000

    Grouped up tests

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -3,41 +3,32 @@ require "plane"
 
 describe Airport do
 
-  it "checks if a plane instance was created" do
-    plane = Plane.new
-    expect(plane.docked).to eq false 
-  end
-
-  it "lands plane at airport" do
-    expect(subject).to respond_to(:land).with(1).argument
-  end
-
-  it "checks it airport responds to take off" do
-    expect(subject).to respond_to(:take_off)
-  end
-
-  it "checks if there are planes that have landed" do
-    expect(subject).to respond_to(:plane)
-  end
-
-  it "sets a default capacity" do
-    expect(subject.capacity).to eq Airport::STANDARD_CAPACITY
-  end
-  
-  describe "Plane has taken off" do 
-    it "raises a fail if there is a plane after take off" do
-      expect { subject.take_off }.to raise_error "Plane has taken off"
+  describe "land" do
+    it "lands plane at airport" do
+      expect(subject).to respond_to(:land).with(1).argument
     end
-  end
 
-  describe "Airport is full" do
     it "raises a fail if the airport is full" do
       10.times { subject.land(Plane.new) }
       expect { subject.land(Plane.new) }.to raise_error "Airport is full"
     end
   end
 
+  describe "take off" do
+    it "checks if airport responds to take off" do
+      expect(subject).to respond_to(:take_off)
+    end
+    
+    it "raises a fail if there is a plane after take off" do
+      expect { subject.take_off }.to raise_error "Plane has taken off"
+    end
+  end
+
   describe "Initialization" do
+    it "sets a default capacity" do
+      expect(subject.capacity).to eq Airport::STANDARD_CAPACITY
+    end
+    
     it "sets variable capacity" do
       airport = Airport.new(40)
       40.times { airport.land(Plane.new) }
