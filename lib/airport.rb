74d8fb4336ce4f39975ce37045780713376c7674
Glykeria Stravodimou <strglykeria@gmail.com>
2021-01-09 10:02:15 +0000

    Plane takes off/confirmation

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,5 +1,15 @@
 class Airport
+
+  def initialize
+    @landed_planes = []
+  end
+
   def land(plane)
-    plane
+    @landed_planes << plane
+  end
+
+  def takeoff(plane)
+    @landed_planes.delete(plane)
+    "#{plane} has left the Airport"
   end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -3,9 +3,15 @@ require "airport"
 
 describe Airport do
   it { is_expected.to respond_to(:land).with(1).argument }
-  it 'should land at the said airport' do
+  it 'should instruct a plane to land' do
     plane = Plane.new
-    airport = Airport.new
-    expect(airport.land(plane)).to eq plane
+    expect(subject.land(plane)).to eq [plane]
+  end
+
+  it {is_expected.to respond_to(:takeoff).with(1).argument}
+  it 'should instruct a plane to take off' do
+    plane = Plane.new
+    subject.land(plane)
+    expect(subject.takeoff(plane)).to eq "#{plane} has left the Airport"
   end
 end
