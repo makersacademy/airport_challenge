80c27a31c11b7717adbd654f4f0f543cff51fa2f
Karsten Finlay <karstenfinlay@hotmail.com>
2021-01-10 11:11:55 +0000

    Stubbed plane instance

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,13 +1,11 @@
 require "airport"
-require "plane"
-require "weather"
 
 describe Airport do
   subject { Airport.new(@weather) }
 
   before(:each) do
-    @weather = double("weather")
-    allow(@weather).to receive(:forecast).and_return("sunny")
+    @weather = double("weather", :forecast => "sunny")
+    @plane = double("plane", :docked => false, :docked= => nil)
   end
 
   describe "land" do
@@ -16,32 +14,28 @@ describe Airport do
     end
 
     it "raises a fail if the airport is full" do
-      Airport::STANDARD_CAPACITY.times { subject.land(Plane.new) }
-      expect { subject.land(Plane.new) }.to raise_error "Airport is full"
+      Airport::STANDARD_CAPACITY.times { subject.land(@plane) }
+      expect { subject.land(@plane) }.to raise_error "Airport is full"
     end
 
     it "lands a plane" do
-      plane = Plane.new
-      subject.land(plane)
-      expect(subject.planes.include?(plane)).to eq true
+      subject.land(@plane)
+      expect(subject.planes.include?(@plane)).to eq true
     end
 
     it "sets plane to docked" do
-      plane = Plane.new
-      subject.land(plane)
-      expect(plane.docked).to eq true
+      expect(@plane).to receive(:docked=).with(true)
+      subject.land(@plane)
     end
 
     it "raises an error when trying to land a plane that has already landed" do
-      plane = Plane.new
-      subject.land(plane)
-      expect { subject.land(plane) }.to raise_error "Already landed"
+      allow(@plane).to receive(:docked).and_return(true)
+      expect { subject.land(@plane) }.to raise_error "Already landed"
     end
 
     it "raises an arror when trying to land and weather is stormy" do
-      plane = Plane.new
       allow(@weather).to receive(:forecast).and_return("stormy")
-      expect { subject.land(plane) }.to raise_error "Can't land while stormy"
+      expect { subject.land(@plane) }.to raise_error "Can't land while stormy"
     end
   end
 
@@ -55,17 +49,15 @@ describe Airport do
     end
 
     it "removes a plane" do
-      plane = Plane.new
-      subject.land(plane)
-      expect(subject.take_off).to eq plane
-      expect(subject.planes.include?(plane)).to eq false
+      subject.land(@plane)
+      expect(subject.take_off).to eq @plane
+      expect(subject.planes.include?(@plane)).to eq false
     end
 
     it "sets plane to be not docked" do
-      plane = Plane.new
-      subject.land(plane)
+      subject.land(@plane)
       subject.take_off
-      expect(plane.docked).to eq false
+      expect(@plane.docked).to eq false
     end
   end
 
@@ -76,13 +68,12 @@ describe Airport do
     
     it "sets variable capacity" do
       airport = Airport.new(@weather, 40)
-      40.times { airport.land(Plane.new) }
-      expect { airport.land(Plane.new) }.to raise_error "Airport is full"
+      40.times { airport.land(@plane) }
+      expect { airport.land(@plane) }.to raise_error "Airport is full"
     end
 
     it "raises an error if a plane tries to take off while stormy" do
-      plane = Plane.new
-      subject.land(plane)
+      subject.land(@plane)
       allow(@weather).to receive(:forecast).and_return("stormy")
       expect { subject.take_off }.to raise_error "Can't take off while stormy"
     end
