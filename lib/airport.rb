2d6a19100bcd6af9bfa01ac11c6f4736f2323925
bateso88 <bateso@hotmail.co.uk>
2021-01-10 20:05:04 +0000

    Refactored

--- lib/airport.rb
+++ lib/airport.rb
@@ -12,20 +12,12 @@ class Airport
   end
 
   def land(plane)
-    raise "Airport is full" if full?
-
-    raise "Plane has already landed" if @planes.include?(plane)
-
-    raise "It is too stormy to land" if stormy?
-
+    check_landing_possible(plane)
     @planes << plane
   end
 
   def takeoff
-    raise "There are no planes" if empty?
-
-    raise "It is too stormy to takeoff" if stormy?
-
+    check_takeoff_possible
     @planes.pop
   end
 
@@ -43,6 +35,24 @@ private
 
   def empty?
     @planes.empty?
+  end
+
+  def includes?(plane)
+    @planes.include?(plane)
+  end
+
+  def check_landing_possible(plane)
+    raise "Airport is full" if full?
+
+    raise "Plane has already landed" if includes?(plane)
+
+    raise "It is too stormy to land" if stormy?
+  end
+
+  def check_takeoff_possible
+    raise "There are no planes" if empty?
+
+    raise "It is too stormy to takeoff" if stormy?
   end
 
 end
