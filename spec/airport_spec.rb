b8f24364dd822499bfa43d520de7df2dc6ec55e2
jshields123 <jackshields@live.co.uk>
2021-01-10 14:01:52 +0000

    added in code to pass test allowing airport to respond to land taking in plane as an argument

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,10 +1,7 @@
 require 'airport'
 
 describe Airport do
-
-  describe 'land' do
-    it 'responds to land' do
-      expect { is_expected.to respond_to :land }
-    end
+  it 'allows plane to land' do
+    expect(subject).to respond_to(:land).with(1).argument
   end
 end
