c0b0c5cb4398cb20ff19ee2645e3f9eaeb94e4a6
Katy Day <day.katy@gmail.com>
2021-01-10 16:04:26 +0000

    cleaning up code

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,22 +1,25 @@
 require_relative "airplane"
 
 class Airport
-attr_accessor :capacity
-attr_reader :airplanes 
-DEFAULT_CAPACITY = 20
 
-  def initialize(capacity=DEFAULT_CAPACITY)
+  attr_accessor :capacity
+  attr_reader :airplanes 
+  DEFAULT_CAPACITY = 20
+
+  def initialize(capacity = DEFAULT_CAPACITY)
     @airplanes = Array.new
     @capacity = capacity
   end
 
   def land(airplane)
     fail "Airport is full" if full?
+
     @airplanes.push(airplane)
   end
 
   def take_off
     fail "No airplanes at airport" if empty?
+
     @airplanes.pop
   end
 
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -13,43 +13,46 @@ describe Airport do
   describe "initialization" do
     subject { Airport.new }
     let(:airplane) { Airplane.new }
+     
     it "defaults capacity" do
       described_class::DEFAULT_CAPACITY.times do
         subject.land(airplane)
+      end
+
+      expect { subject.land(airplane) }.to raise_error "Airport is full"
+
+    end
+  end
+
+  describe "#land" do
+    
+    it { is_expected.to respond_to :land }
+
+    it "instructs the plane to land at the airport" do
+      airplane = Airplane.new
+      expect(subject.land(airplane)).to eq [airplane]
+    end 
+
+    it "raises an error when airport is full" do
+      Airport::DEFAULT_CAPACITY.times { subject.land(Airplane.new) }
+      expect { subject.land Airplane.new }.to raise_error "Airport is full"
     end
 
-    expect { subject.land(airplane)}.to raise_error "Airport is full"
-  end
-end
-
-describe "#land" do
-  it { is_expected.to respond_to :land }
-
-it "instructs the plane to land at the airport" do
-  airplane = Airplane.new
-  expect(subject.land(airplane)).to eq [airplane]
-end 
-
-it "raises an error when airport is full" do
-  Airport::DEFAULT_CAPACITY.times { subject.land(Airplane.new)}
-  expect { subject.land Airplane.new}.to raise_error "Airport is full"
-end
-
-end
-
-describe "#take_off" do
-  it { is_expected.to respond_to :take_off }
-  
-  it "instructs the plane to take off from the airport" do
-    airplane = Airplane.new
-    subject.land(airplane)
-    expect(subject.take_off).to eq airplane
   end
 
-  it "should not instruct a plane to take off if a plane is not at the airport" do
-expect { subject.take_off }.to raise_error "No airplanes at airport"
-  end
+  describe "#take_off" do
 
-end
+    it { is_expected.to respond_to :take_off }
+    
+    it "instructs the plane to take off from the airport" do
+      airplane = Airplane.new
+      subject.land(airplane)
+      expect(subject.take_off).to eq airplane
+    end
+
+    it "should not instruct a plane to take off if a plane is not at the airport" do
+      expect { subject.take_off }.to raise_error "No airplanes at airport"
+    end
+  end
 
 end
