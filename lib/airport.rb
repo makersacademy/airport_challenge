d4a3aaf7def1ac9b7887876bd77e53484b62f9a1
Katy Day <day.katy@gmail.com>
2021-01-10 15:40:10 +0000

    first commit for airport challenge

---
+++ lib/airport.rb
@@ -0,0 +1,31 @@
+require_relative "airplane"
+
+class Airport
+attr_accessor :capacity
+attr_reader :airplanes 
+DEFAULT_CAPACITY = 20
+
+  def initialize(capacity=DEFAULT_CAPACITY)
+    @airplanes = Array.new
+    @capacity = capacity
+  end
+
+  def land(airplane)
+    fail "Airport is full" if full?
+    @airplanes.push(airplane)
+  end
+
+  def take_off
+    fail "No airplanes at airport" if empty?
+    @airplanes.pop
+  end
+
+  def full?
+    @airplanes.count >= DEFAULT_CAPACITY
+  end
+
+  def empty?
+    @airplanes.empty?
+  end
+
+end
---
+++ spec/airplane_spec.rb
@@ -0,0 +1,5 @@
+require "airplane"
+
+describe Airplane do
+
+end
