b198d61b8ddfbe90e0ae0820dc6b01242b7fb686
Anna Cavalla <anna.cavalla@gmail.com>
2021-01-10 22:33:16 +0000

    Refactored error messages into separate method

--- lib/airport_challenge.rb
+++ lib/airport_challenge.rb
@@ -10,26 +10,32 @@ class Airport
   end
 
   def land(plane)
-    raise "This airport is full." if full?
-
-    raise "Too stormy to land." if stormy?
-
+    landing_safety_check
     @planes << plane
   end
 
+  def landing_safety_check
+    raise "This airport is full." if full?
+
+    raise "Too stormy to land." if stormy?
+  end
+
   def takeoff(plane)
+    takeoff_safety_check(plane)
+    @fly << plane
+    @planes.delete(plane)
+  end
+
+  def takeoff_safety_check(plane)
     raise "Too stormy for takeoff." if stormy?
 
     raise "Plane in air." if flying?(plane)
 
     raise "Plane not in airport." if absent?(plane)
-
-    @fly << plane
-    @planes.delete(plane)
   end
 
-  attr_accessor :planes, :stormy
-  attr_reader :capacity, :fly
+  attr_accessor :planes
+  attr_reader :capacity, :fly, :stormy
 
   private
   def full?
