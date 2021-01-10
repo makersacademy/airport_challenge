09fe2d4dee158aa9f376767c965dc0b1ce7b64a1
jshields123 <jackshields@live.co.uk>
2021-01-10 14:11:53 +0000

    added in allows plane to take off method and test

--- lib/airport.rb
+++ lib/airport.rb
@@ -5,4 +5,7 @@ class Airport
   def land(plane)
   end
 
+  def take_off(plane)
+  end
+
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -4,4 +4,8 @@ describe Airport do
   it 'allows plane to land' do
     expect(subject).to respond_to(:land).with(1).argument
   end
+
+  it 'allows plane to take off' do
+    expect(subject). to respond_to(:take_off).with(1).argument
+  end
 end
