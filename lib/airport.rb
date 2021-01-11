c36f50f5ca397e6f3e1480f3dbd36554a0216247
bullhornfixie <dawson.jon@hotmail.com>
2021-01-11 21:29:56 +0000

    updated README

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,42 +1,12 @@
-class Airport 
-  
-  def initialize(name, capacity=100) 
-    @name = name 
-    @capacity = capacity 
-  end
-  
-  def landing(plane)
-    case 
-      when weather == 0 then print "Good visibility - land" 
-        track_inbound(plane)
-      when weather == 1 then print "Declining visibility - land with caution" 
-        track_inbound(plane)
-      when weather == 2 then print "Poor visibility - defer landing until further update"
-    end
-  end
-  
-  def take_off(plane)
-    if @tracker.include?(plane)
-      "Plane ready to take off"
-      track_outbound(plane)
-    else
-      "Plane is not in airport"
-    end
-  end
-  
-  def track_inbound(plane)
-    @tracker = []
-    @tracker << plane
-    return nil 
-  end
-  
-  def track_outbound(plane)
-    @tracker.delete(plane)
-    print "Plane is no longer in the airport"
-  end
-  
-  def weather 
-    0 # rand(3) 
-  end
-end
 
+class Airport 
+ 
+  def initialize 
+  
+  end
+  
+  def land(plane)
+    
+  end
+  
+end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,22 +1,84 @@
 require "airport"
+require "plane"
 
 describe Airport do 
-  subject { Airport.new(:name => "lax", :capacity => 100) }
+  subject(:airport) {described_class.new}          # subject is an Airport instance 
+  let(:plane) {double :plane}                      # let Plane instance be a stunt double of Plane instance
   
-  it "creates an airport with two attributes that can be changed" do 
-    expect(subject).to respond_to :landing
+  context "plane will land" do                     # describe a scenario
+    it {is_expected.to respond_to(:land).with(1).argument }
   end
-  
-  it "can instruct a plane to land at the airport if weather is good" do
-    plane = "Swiss Air"
-    expect{subject.landing(plane)}.to output("Good visibility - land").to_stdout
-  end
-  
-  it "can instruct a plane to take off from an airport and confirm no longer in the airport" do
-    value = subject.landing("Swiss Air")
-    plane = "Swiss Air"
-    expect{subject.take_off(plane)}.to output("Plane is no longer in the airport").to_stdout
-  end
-  
+
 end
 
+
+=begin 
+require 'airport'
+require 'plane'
+
+describe Airport do
+  subject(:airport) { described_class.new }
+  let(:plane) { double :plane }
+
+  context 'plane will land' do
+    it { is_expected.to respond_to(:land).with(1).argument }
+
+    it 'as instructed' do
+      expect(airport.land(:plane)).to include(:plane)
+    end
+  end
+
+  context 'plane will take off' do
+    it { is_expected.to respond_to(:take_off).with(1).argument }
+
+    it 'will confirm that the plane is no longer in the airport' do
+      airport.land(:plane)
+      expect(airport.take_off(:plane)).to eq "The plane: #{:plane} has taken off"
+    end
+
+    it 'will remove plane from airport' do
+      airport.land(:plane)
+      airport.take_off(:plane)
+      expect(airport.hangar).not_to include(:plane)
+    end
+  end
+
+  context 'is full' do
+    it 'will prevent landing and will have a default airport capacity that can be overridden' do
+      Airport::DEFAULT_CAPACITY.times { airport.land(:plane) }
+      expect { airport.land(:plane) }.to raise_error "Airport is full"
+    end
+  end
+
+  context 'is stormy' do
+    it { is_expected.to respond_to :report_storm }
+
+    it 'will report the weather' do
+      expect(airport.report_storm).to eq true
+    end
+
+    context 'storm has been reported' do
+      before do
+        airport.report_storm
+      end
+
+      it 'will prevent takeoff' do
+        expect { airport.take_off(:plane) }.to raise_error "Plane cannot take off due to storm"
+      end
+
+      it 'will prevent landing' do
+        expect { airport.land(:plane) }.to raise_error "Plane cannot land due to storm"
+      end
+    end
+  end
+  # Tests that can no longer be used as methods are now private
+
+  # it { is_expected.to respond_to :stormy? }
+
+  # it 'will output the weather as stormy' do
+  #   subject.report_storm
+  #   expect(subject).to be_stormy
+  # end
+end
+
+=end
