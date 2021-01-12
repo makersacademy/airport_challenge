8fcd53eecbaccc3870879e31d6900b1171bc136b
bateso88 <bateso@hotmail.co.uk>
2021-01-12 09:55:33 +0000

    replacing instance variables with class method

--- lib/airport.rb
+++ lib/airport.rb
@@ -14,12 +14,12 @@ class Airport
 
   def land(plane)
     check_landing_possible(plane)
-    @planes << plane
+    planes << plane
   end
 
   def takeoff
     check_takeoff_possible
-    @planes.pop
+    planes.pop
   end
 
   def stormy?
@@ -29,15 +29,15 @@ class Airport
 private
 
   def full?
-    @planes.length >= capacity
+    planes.length >= capacity
   end
 
   def empty?
-    @planes.empty?
+    planes.empty?
   end
 
   def includes?(plane)
-    @planes.include?(plane)
+    planes.include?(plane)
   end
 
   def check_landing_possible(plane)
--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -6,6 +6,6 @@ describe Weather do
     expect(weather.stormy?).to eq(true).or eq(false)
   end
   it "should check weather is a number between 1 and 10" do
-    expect(subject.weather).to(be >= 1).and(be <= 10)
+    expect(subject.weather).to be_between(1, 10)
   end
 end
