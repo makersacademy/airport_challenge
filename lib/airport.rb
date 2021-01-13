2697aaf4e99e0cadbc6725bad40a2324dd9be4fb
CodeRed30 <katrinaharradine@gmail.com>
2021-01-13 22:11:10 +0000

    Extracted weather class

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,4 +1,5 @@
 require_relative 'plane'
+require_relative 'weather'
 
 class Airport
   DEFAULT_CAPACITY = 10
@@ -6,32 +7,29 @@ class Airport
   def initialize(capacity = DEFAULT_CAPACITY)
     @capacity = capacity
     @hangar = []
+    @weather = Weather.new
   end
 
   def land(plane)
     raise "Airport is full" if full?
 
-    raise "Plane cannot land due to storm" if stormy?
+    raise "Plane cannot land due to storm" if @weather.stormy?
 
     @hangar << plane
   end
 
   def take_off(plane)
-    raise "Plane cannot take off due to storm" if stormy?
+    raise "Plane cannot take off due to storm" if @weather.stormy?
 
     hangar.delete(plane)
     "The plane: #{plane} has taken off"
   end
 
   def report_storm
-    @storm = true
+    @weather.storm = true
   end
 
   private
-
-  def stormy?
-    @storm
-  end
 
   def full?
     @hangar.length >= @capacity
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,15 +1,17 @@
 require 'airport'
 require 'plane'
+require 'weather'
 
 describe Airport do
   subject(:airport) { described_class.new }
   let(:plane) { double :plane }
+  let(:weather) { double :weather, storm: true }
 
   context 'plane will land' do
     it { is_expected.to respond_to(:land).with(1).argument }
 
     it 'as instructed' do
-      expect(airport.land(:plane)).to include(:plane)
+      expect(airport.land(plane)).to include(plane)
     end
   end
 
@@ -17,21 +19,21 @@ describe Airport do
     it { is_expected.to respond_to(:take_off).with(1).argument }
 
     it 'will confirm that the plane is no longer in the airport' do
-      airport.land(:plane)
-      expect(airport.take_off(:plane)).to eq "The plane: #{:plane} has taken off"
+      airport.land(plane)
+      expect(airport.take_off(plane)).to eq "The plane: #{plane} has taken off"
     end
 
     it 'will remove plane from airport' do
-      airport.land(:plane)
-      airport.take_off(:plane)
-      expect(airport.hangar).not_to include(:plane)
+      airport.land(plane)
+      airport.take_off(plane)
+      expect(airport.hangar).not_to include(plane)
     end
   end
 
   context 'is full' do
     it 'will prevent landing and will have a default airport capacity that can be overridden' do
-      Airport::DEFAULT_CAPACITY.times { airport.land(:plane) }
-      expect { airport.land(:plane) }.to raise_error "Airport is full"
+      Airport::DEFAULT_CAPACITY.times { airport.land(plane) }
+      expect { airport.land(plane) }.to raise_error "Airport is full"
     end
   end
 
@@ -39,7 +41,8 @@ describe Airport do
     it { is_expected.to respond_to :report_storm }
 
     it 'will report the weather' do
-      expect(airport.report_storm).to eq true
+      airport.report_storm
+      expect(weather.storm).to be true
     end
 
     context 'storm has been reported' do
@@ -48,20 +51,12 @@ describe Airport do
       end
 
       it 'will prevent takeoff' do
-        expect { airport.take_off(:plane) }.to raise_error "Plane cannot take off due to storm"
+        expect { airport.take_off(plane) }.to raise_error "Plane cannot take off due to storm"
       end
 
       it 'will prevent landing' do
-        expect { airport.land(:plane) }.to raise_error "Plane cannot land due to storm"
+        expect { airport.land(plane) }.to raise_error "Plane cannot land due to storm"
       end
     end
   end
-  # Tests that can no longer be used as methods are now private
-
-  # it { is_expected.to respond_to :stormy? }
-
-  # it 'will output the weather as stormy' do
-  #   subject.report_storm
-  #   expect(subject).to be_stormy
-  # end
 end
