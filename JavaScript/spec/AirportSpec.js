7ff46b436df17b84db087f4320bef243a1546dd2
xavierloos <xavier.loos04@gmail.com>
2021-01-13 12:29:42 +0000

    responds to capacity

--- JavaScript/spec/AirportSpec.js
+++ JavaScript/spec/AirportSpec.js
@@ -3,10 +3,12 @@ describe("Airport", () => {
         var airport = new Airport()
         it("responds to hangar", () => {
             expect(airport.hangar).toBeDefined();
-        }) 
+        })
         it("hangar is empty when starts", () => {
             expect(airport.hangar).toEqual([]);
         })
+        it("responds to capacity", () => {
+            expect(airport.capacity).toBeDefined();
+        })
     })
-    
 })
--- JavaScript/src/Airport.js
+++ JavaScript/src/Airport.js
@@ -1,6 +1,10 @@
 class Airport {
     constructor() {
         this.hangar = []
+        this.capacity = this.setCapacity
+    }
+    setCapacity() {
+        
     }
     
 }
