f400a9be34bb1e0ada6d60484d58d3075879f7ce
Vincenzo Florio <vincenzoflorio31@gmail.com>
2021-01-29 16:25:39 +0000

    Add checks for capacity, takeoff, plane count and landing

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,23 +1,40 @@
+require_relative 'plane'
+
 class Airport
-  attr_accessor :capacity, :parked
+  attr_accessor :capacity, :planes
   DEFAULT_CAPACITY = 10
   def initialize(capacity = DEFAULT_CAPACITY)
     @capacity = capacity
-    @parked = []
+    @planes = []
   end
 
-  def confirmed_landing(plane)
-    raise "Cannot land planes as the airport is already at maximum 
-    capacity." if @parked.length >= @capacity
+  def approved_for_landing(tail_number)
+    check_landing_conditions(tail_number)
+    @planes << tail_number
+  end
 
-    raise "Weather is still stormy, please wait for weather 
-    to improve before landing" if stormy? == true
+  def approved_for_takeoff(tail_number)
+    @planes.delete(tail_number)
+  end
 
-    @parked << plane
+  def plane_count
+    @planes.length
+  end
+
+  def full?
+    @planes.length >= @capacity
   end
 
   def stormy?
     false
   end
 
+  def check_landing_conditions(tail_number)
+  raise "Plane cannot land because airport is at full capacity" if full?
+
+  raise "Weather is still stormy, please wait for weather 
+  to improve before landing" if stormy?
+  end
 end
+
+
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -6,10 +6,37 @@ RSpec.describe Airport do
   let(:airport) { Airport.new }
   let(:plane) { Plane.new }
 
-  describe '#confirmed_landing' do
+  describe '#approved_for_landing' do
     it 'parks a plane once it has landed' do
-      airport.confirmed_landing("DLH430")
-      expect(airport.parked).to include("DLH430")
+      airport.approved_for_landing("DLH430")
+      expect(airport.planes).to include("DLH430")
+    end
+
+    it 'can tell air traffic control that capacity has reached maximum' do
+      10.times do
+        airport.approved_for_landing("DLH430")
+      end
+    expect {airport.approved_for_landing("DLH430")}.to raise_error(RuntimeError, "Plane cannot land because airport is at full capacity")
+    end
+  end
+
+  describe '#plane_count' do
+    it 'can check the amount of planes that are parked' do
+      airport.approved_for_landing("DLH430")
+      expect(airport.plane_count).to eq(1)
+    end
+  end
+
+  describe '#approved_for_takeoff' do
+    it 'takes a plane and removes it from the parking lot' do
+      airport.approved_for_landing("DLH430")
+      airport.approved_for_takeoff("DLH430")
+      expect(airport.planes).not_to eq("DLH430")
+    end
+
+    it 'returns flight number of plane that took off' do
+      airport.approved_for_landing("DLH430")
+      expect(airport.approved_for_takeoff("DLH430")).to eq("DLH430")
     end
   end
   
@@ -17,12 +44,6 @@ RSpec.describe Airport do
     it 'has an adaptable default capacity' do
       airport.capacity = 20
       expect(airport.capacity).to eq(20)
-    end
-  end
-
-  describe 'weather is good so it is clear to take off' do
-    before do
-      allow(airport).to receive(:stormy?).and_return(false)
     end
   end
 
