7fe5b53b07a0da310693134f93f5d7520397261f
Anastasia <anastasiapolinesi@gmail.com>
2021-01-29 12:31:18 +0000

    user story 2

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -9,4 +9,12 @@ describe Airport do
      end
   end
 
+  describe '#take_off' do
+    it 'checks .take_off instructs a plane to take off from an airport' do
+      airport = Airport.new
+      plane = Plane.new
+      expect { airport.take_off(plane) }.not_to raise_error
+    end
+  end
+
 end
