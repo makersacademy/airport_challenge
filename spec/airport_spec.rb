b705b310ff302c9926ac54721e3dc85a549e4b92
AlistairTait12 <alistair.tait1@gmail.com>
2021-01-29 10:50:02 +0000

    rewrote first test to reflect desired cofirmation message

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,8 +2,10 @@ require 'airport'
 
 describe Airport do
   describe '#land' do
-    it 'lets a plane land at the airport' do
-
+    it 'informs air traffic controller plane has landed' do
+      plane = Plane.new
+      airport = Airport.new
+      expect(airport.land(plane)).to eq "#{plane.id} successfully landed at #{airport.name}"
     end
   end
 end
