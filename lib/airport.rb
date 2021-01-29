a0a240d5c7ebe8a498264eb9ff414bcc9559aea8
Anastasia <anastasiapolinesi@gmail.com>
2021-01-29 14:06:24 +0000

    User Story 3

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,10 +1,22 @@
 
 class Airport
-  def land(plane)
+  def initialize
+    @queue = []
+    @capacity = 20
+  end 
 
+  def land(plane)
+    raise 'Cannot land, airport full' if @queue.length >= @capacity
+    @queue.push(plane) 
+    
   end
 
   def take_off(plane)
 
+  end
+
+  def hangar
+    @queue
+    
   end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,11 +2,22 @@ require 'airport'
 
 describe Airport do
   describe '#land' do
-     it 'checks .land instructs a plane to land at airport' do
+    it 'checks .land instructs a plane to land at airport' do
         airport = Airport.new 
         plane = Plane.new
-        expect { airport.land(plane) }.not_to raise_error
-     end
+        expect airport.land(plane) 
+        expect(airport.hangar).to include (plane)
+    end
+
+    it 'checks .land prevents landing when airport is full' do
+      airport = Airport.new
+      plane = Plane.new
+      20.times do
+        airport.land(plane)
+      end
+      expect { airport.land(plane) }.to raise_error "Cannot land, airport full"
+    end
+    
   end
 
   describe '#take_off' do
@@ -16,5 +27,15 @@ describe Airport do
       expect { airport.take_off(plane) }.not_to raise_error
     end
   end
-
+  
+  describe '#hangar' do
+    # it "checks .hangar prevents landing when the airport is full" do
+    #   airport = Airport.new
+    #   plane = Plane.new
+    #   20.times do
+    #        @queue.push(plane)
+    #    end
+    #    expect { queue.push(plane) }.to raise_error "Cannot land, airport full"
+    #  end
+    end
 end
