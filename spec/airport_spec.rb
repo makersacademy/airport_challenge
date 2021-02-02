8363dae01b9ddc1aabb6c4b5cf21521322bf55b1
HarryCarton <harrycarton@hotmail.com>
2021-02-02 13:23:11 +0000

    fixed tests

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -3,34 +3,49 @@ require 'airport'
 describe Airport do
   describe '#land_plane' do 
     it 'when .land_plane is run with string string is in airport hangar' do
+      weather = Weather.new
+      subject.instance_variable_set(:@weather, weather)
+      expect(weather).to receive(:storm?).and_return(false)
       subject.land_plane('plane_string')
       expect(subject.hangar).to include('plane_string')
     end
+    it 'checks update_weather is called in land_plane' do
+      plane = Plane.new
+      weather = Weather.new
+      subject.instance_variable_set(:@weather, weather)
+      expect(weather).to receive(:update_weather)
+      subject.land_plane(plane)
+    end
     it 'when .land_plane is run with plane the plane is in hangar' do
       plane = Plane.new 
+      weather = Weather.new
+      subject.instance_variable_set(:@weather, weather)
+      expect(weather).to receive(:storm?).and_return(false)
       subject.land_plane(plane)
       expect(subject.hangar).to include(plane)
     end
     it 'wont let a plane land if the airport is full' do
-      plane1 = Plane.new
-      plane2 = Plane.new
+      plane = Plane.new
       weather = Weather.new
-      airport = Airport.new(1)
-      airport.land_plane(plane1)
-      weather.instance_variable_set(:@stormy, false)
-      subject.instance_variable_set(:@weather, weather)
-      expect { airport.land_plane(plane2) }.to raise_error('Airport is full')
+      airport = Airport.new(0)
+      airport.instance_variable_set(:@weather, weather)
+      expect(weather).to receive(:storm?).and_return(false)
+      expect { airport.land_plane(plane) }.to raise_error('Airport is full')
     end
     it 'wont let a plane land if the weather is stormy' do 
       plane = Plane.new 
       weather = Weather.new
-      weather.instance_variable_set(:@stormy, true)
       subject.instance_variable_set(:@weather, weather)
+      expect(weather).to receive(:storm?).and_return(true)
       expect { subject.land_plane(plane) }.to raise_error{'It is too stormy'}
     end
     it 'wont let a plane land twice' do 
       plane = Plane.new
+      weather = Weather.new
+      subject.instance_variable_set(:@weather, weather)
+      expect(weather).to receive(:storm?).and_return(false)
       subject.land_plane(plane)
+      expect(weather).to receive(:storm?).and_return(false)
       expect {subject.land_plane(plane)}.to raise_error{'Plane is already in hangar'}
     end
   end
@@ -40,9 +55,10 @@ describe Airport do
     it 'when .take_off is called with plane as arg it leaves hangar' do
       plane = Plane.new 
       weather = Weather.new
-      subject.land_plane(plane)
-      weather.instance_variable_set(:@stormy, false)
       subject.instance_variable_set(:@weather, weather)
+      expect(weather).to receive(:storm?).and_return(false)
+      subject.land_plane(plane)
+      expect(weather).to receive(:storm?).and_return(false)
       subject.take_off(plane)
       expect(subject.hangar).to_not include(plane)
     end
@@ -50,16 +66,17 @@ describe Airport do
     it 'doesnt let a plane take off if the weather is stormy' do 
       plane = Plane.new 
       weather = Weather.new
-      weather.instance_variable_set(:@stormy, true)
       subject.instance_variable_set(:@weather, weather)
+      expect(weather).to receive(:storm?).and_return(true)
       expect { subject.take_off(plane) }.to raise_error{'It is too stormy'}
     end
     it 'lets a plane take off if the weather is not stormy' do 
       plane = Plane.new 
-      subject.land_plane(plane)
       weather = Weather.new
-      weather.instance_variable_set(:@stormy, false)
       subject.instance_variable_set(:@weather, weather)
+      expect(weather).to receive(:storm?).and_return(false)
+      subject.land_plane(plane)
+      expect(weather).to receive(:storm?).and_return(false)
       subject.take_off(plane)
       expect(subject.hangar).to_not include(plane)
     end
