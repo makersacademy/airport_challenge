9ead1f9c83cba0db16966cb1dcfafcf23660a715
Adam Jonas <adamjonas25@gmail.com>
2021-02-05 21:04:32 +0000

    first commit

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,8 @@
 require 'simplecov'
 require 'simplecov-console'
 
+require 'airport_challenge.rb'
+
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
   SimpleCov::Formatter::Console,
   # Want a nice code coverage website? Uncomment this next line!
