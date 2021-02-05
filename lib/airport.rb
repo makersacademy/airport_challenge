3f58d880a5be20048f2a65335307f43993a125ea
girl-interrupted <d.pavliuchyk@gmail.com>
2021-02-05 17:37:06 +0000

    added weather class, still need to work on my tests

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,25 +1,36 @@
+require_relative 'plane'
+require_relative 'weather'
 
 class Airport
 
-  attr_reader :all_planes
+  attr_accessor :all_planes, :capacity, :weather
 
   DEFAULT_CAPACITY = 35
-  def initialize(capacity = DEFAULT_CAPACITY)
+
+  def initialize(capacity = DEFAULT_CAPACITY, weather = Weather.new)
     @capacity = capacity
     @all_planes = []
+    @weather = weather
   end
 
-  def can_land_a_plane(flight)
+  def can_land(flight)
+    fail "cannot land at the moment due to stormy weather" if weather.stormy?
     @all_planes.push(flight)
   end
 
   def can_take_off(flight)
+    !weather.stormy?
     @all_planes.delete(flight)
   end
 
-  def full_airport(capacity)
-     if @capacity == @all_planes.size
-     puts 'airport is full now, cannot land any planes'
-     end
+  private
+
+  def airport_full?
+    capacity <= all_planes.length
+    fail "airport is full now, cannot land any planes" if airport_full?
+  end
+  
+  def stormy?
+    weather.stormy?
   end
 end
--- lib/weather.rb
+++ lib/weather.rb
@@ -1,3 +1,8 @@
+require_relative 'airport'
+
 class Weather
-  
+  def stormy?
+    @weather_today = rand(1..10)
+    return true if @weather_today > 8
+  end
 end
