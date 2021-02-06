5a24db4774bf01ae9ffd26e023ff56da9680a24e
olga <itsglimerry@gmail.com>
2021-02-06 12:34:51 +0000

    Added tests and implementation for methods .weather_check and .capacity_check

--- lib/safety.rb
+++ lib/safety.rb
@@ -4,13 +4,21 @@ require "weather"
 
 class SafetyCheck
 
-  DEAFAULT_CAPACITY = 5
 
-  def initialize
+
+  def initialize(capacity)
     @weather = Weather.new
-    @capacity = DEAFAULT_CAPACITY
+    @capacity = capacity
   end
 
-  
+  def weather_check(weather)
+    return 'Weather check: safe' if weather =='sunny'
+    raise 'Too stormy to land safely' if weather == 'stormy'
+  end
+
+  def capacity_check(current_capacity_level)
+    return 'Airport capacity level check: safe' if @capacity > current_capacity_level
+    raise 'Airport too full to land safely'
+  end
 
 end
--- spec/safety_spec.rb
+++ spec/safety_spec.rb
@@ -1,14 +1,32 @@
 require "safety"
 require "./air_traffic_controller"
-
+require 'weather'
 describe SafetyCheck do
 
-  describe '#landing_protocol' do
+default_capacity = 10
+
+    describe '#weather_check' do
+      it 'informs when weather is safe' do
+        safety = SafetyCheck.new(default_capacity)
+        expect(safety.weather_check('sunny')).to eq 'Weather check: safe'
+      end
+
+      it 'raises an error is its stormy' do
+        safety = SafetyCheck.new(default_capacity)
+        expect{safety.weather_check('stormy')}.to raise_error'Too stormy to land safely'
+      end
+    end
+
+   describe '#capacity_check' do
+     it 'informs when capacity is at safe level' do
+      safety = SafetyCheck.new(default_capacity)
+      expect(safety.capacity_check(2)).to eq'Airport capacity level check: safe'
+    end
+
+    it 'raises an error when capacity level is unsafe' do
+     safety = SafetyCheck.new(default_capacity)
+     expect{safety.capacity_check(11)}.to raise_error'Airport too full to land safely'
+   end
 
   end
-
-  describe '#take_off_protocol' do
-
-  end
-  
 end
