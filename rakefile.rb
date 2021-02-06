4b0e295f7b4bf26e60dde68555a1fe543f095ed7
Adam Jonas <adamjonas25@gmail.com>
2021-02-06 21:01:05 +0000

    fix typo

--- rakefile.rb
+++ rakefile.rb
@@ -3,7 +3,7 @@ require 'rspec/core/rake_task'
 
 RSpec::Core::RakeTask.new(:spec) do |task|
   task.pattern = Dir.glob('spec/**/*_spec.rb')
-  task.rspec_opts = '--format documenation'
+  task.rspec_opts = '--format documentation'
 end
 
 task default: :spec
