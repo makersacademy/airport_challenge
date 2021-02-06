4e1d41f2fb71e3cbbeea5f717cc3299a4ec5d7f8
Sandy Pounoussamy <pounoussamy.sandy@gmail.com>
2021-02-06 15:02:43 +0000

    Added Plane#take_off(airport) and success output

--- lib/plane.rb
+++ lib/plane.rb
@@ -2,11 +2,16 @@ require_relative 'airport'
 
 class Plane
 
-  def initialize
-  end
-  
   def land(airport)
     airport.hangar << self
   end
 
+  def take_off(airport)
+    airport.hangar.delete(airport)
+    successful_take_off(airport)
+  end
+
+  def successful_take_off(airport)
+    puts "#{self} has successfully taken-off from #{airport}"
+  end
 end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -7,11 +7,31 @@ let(:heathrow_airport) { Airport.new }
     it 'should take one argument' do
       expect(subject).to respond_to(:land).with(1)
     end
-    it 'should add the plane the airport specified' do
+    it 'should add the plane the airport hangar specified' do
       plane_1 = Plane.new
       plane_1.land(heathrow_airport)
       expect(heathrow_airport.hangar).to include(plane_1)
     end
   end
 
+  describe "#take_off" do
+    # let(:plane_1) { Plane.new }
+    # let(:plane_2) { instance_double(Plane) }
+    # let(:airport) { Airport.new }
+
+    context 'when the plane is in the airport hangar' do
+      it 'should remove the plane from the hangar' do
+        plane_1 = Plane.new
+        airport = Airport.new @hangar=[plane_1]
+        # airport.hangar << plane_1
+        plane_1.take_off(airport)
+        expect(airport.hangar).to be_empty
+      end
+      it 'should output a take-off success message' do
+        plane_1 = Plane.new
+        airport = Airport.new @hangar=[plane_1]
+        expect { plane_1.take_off(airport) }.to output.to_stdout
+      end
+    end
+  end
 end
