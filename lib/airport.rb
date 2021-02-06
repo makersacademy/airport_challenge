dd01efa966a1f799d6705af880fd69a7891c6e86
jai-jk <jay.jk@hotmail.com>
2021-02-06 23:35:33 +0000

    Rewrite tests and code for first user story

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,25 +1,7 @@
 require_relative 'plane'
 
 class Airport
-
+  def land(plane)
+    @plane = plane
+  end
 end
-
-
-# def land(plane)
-#   Airport.new
-# end
-#
-# def take_off(plane)
-#   plane = Plane.new
-#   @plane_at_aiport = false
-# end
-#
-# def plane_at_airport?(plane)
-#   if @plane_at_airport == true
-#     true
-#   else
-#     false
-#   end
-# end
-
-# def full?
--- lib/plane.rb
+++ lib/plane.rb
@@ -3,14 +3,3 @@ require_relative 'airport'
 class Plane
 
 end
-
-
-#
-# def land
-#   Airport.new
-# end
-#
-# def take_off
-#   Plane.new
-#
-# end
