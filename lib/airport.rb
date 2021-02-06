ae74ddaf065c2bf5a9210f5e739674fb2171c7eb
Christine Chee <christinechee88@hotmail.com>
2021-02-06 21:52:50 +0000

    Raise an error when trying to land a plane if the airport is full

--- lib/airport.rb
+++ lib/airport.rb
@@ -2,14 +2,24 @@ require_relative 'plane'
 
 class Airport
 
-attr_accessor :hangar
+  attr_accessor :hangar
+  DEFAULTCAPACITY = 10
 
-def initialize
-  @hangar = []
-end
+  def initialize
+    @hangar = []
+  end
 
-def land(plane)
-  hangar << plane
-end
+  def land(plane)
+    fail "Airport is full" if full?
 
+    hangar << plane
+  end
+
+  def take_off(_plane)
+    hangar.pop
+  end
+
+  def full?
+    hangar.length >= DEFAULTCAPACITY
+  end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,13 +2,28 @@ require 'airport'
 require 'plane'
 
 describe Airport do
-  it {is_expected.to be_a(Airport)}
+  it { is_expected.to be_a(Airport) }
 
   describe '#land' do
-    it 'lands a plane' do
+    it 'lands a plane at the airport' do
       airport = Airport.new
       plane = Plane.new
       expect(airport.land(plane)).to eq(airport.hangar)
+    end
+
+    it 'raises an error and prevents plane landing when the airport is full' do
+      airport = Airport.new
+      10.times { airport.land(Plane.new) }
+      expect { airport.land(Plane.new) }.to raise_error("Airport is full")
+    end
+  end
+
+  describe '#take_off' do
+    it 'instructs a plane to take off and is no longer at the airport' do
+      airport = Airport.new
+      plane = Plane.new
+      airport.take_off(plane)
+      expect(airport.hangar).to eq([])
     end
   end
 end
