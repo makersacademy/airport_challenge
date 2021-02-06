bc07803e1c9ab36f7bca46986c06f23987263183
Adam Jonas <adamjonas25@gmail.com>
2021-02-06 21:35:53 +0000

    adjust spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,14 +1,14 @@
 require 'coveralls'
 Coveralls.wear!
 
-require 'simplecov'
-require 'simplecov-console'
+# require 'simplecov'
+# require 'simplecov-console'
 
 require 'airport_challenge.rb'
 
-SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
-  SimpleCov::Formatter::Console,
-  # Want a nice code coverage website? Uncomment this next line!
-  # SimpleCov::Formatter::HTMLFormatter
-])
-SimpleCov.start
+# SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
+#   SimpleCov::Formatter::Console,
+#   # Want a nice code coverage website? Uncomment this next line!
+#   # SimpleCov::Formatter::HTMLFormatter
+# ])
+# SimpleCov.start
