7d1e1cdfb5229ab13070eb7952832e37421f2434
Joey <joey.s.hornsby@gmail.com>
2021-02-07 09:52:54 +0000

    Revert "added weather attribute to airport to read weather at last checking, weather_fetcher method to get weather update (weather generated at random) and weather? method to return true if sunny and false otherwise. rspec gaps as to whether random weather generator is working randomly, however this has been feature tested in irb."
    This reverts commit 0cadf3773337cc9fe5764534ce5cb04bffac57c3.

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,29 +1,9 @@
 class Airport
-  attr_reader :planes, :title, :weather
+  attr_reader :planes, :title
 
-  def initialize(title = "Unnamed Airport", planes = [], weather = "sunny")
+  def initialize(title = "Unnamed Airport", planes = [])
     @title = title
     @planes = planes
-    @weather = weather
-  end
-
-  def weather_fetcher
-    if rand(1..10).between?(1,8)
-      @weather = "sunny"
-      return @weather
-    else
-      @weather = "stormy"
-      return @weather
-    end
-  end
-
-  def weather?
-    weather_fetcher
-    if @weather == "sunny"
-      true
-    else
-      false
-    end
   end
 
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -5,26 +5,9 @@ describe Airport do
   let(:plane) { double(:plane, :status => "unassigned") }
 
   it { is_expected.to respond_to(:planes) }
-  it { is_expected.to respond_to(:weather?) }
-  it { is_expected.to respond_to(:weather) }
-  it { is_expected.to respond_to(:weather_fetcher) }
 
   it "should contain no planes when new" do
     expect(airport.planes).to eq []
   end
-
-  describe '#weather?' do
-    it 'should return true or false' do
-      expect(airport.weather?).to eq true || false
-    end
-
-  end
-
-  describe '#weather_fetcher' do
-    it 'should update weather attribute of Airport' do
-      expect(airport.weather_fetcher).to eq airport.weather
-    end
-  end
-
 
 end
