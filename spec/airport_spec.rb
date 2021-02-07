f23f1d38f09abc25fa1a28adcda5f7fbf2ae1068
AdamP2000 <Adam2000Phillips@gmail.com>
2021-02-07 20:55:39 +0000

    Unit tests for preventing landing when airport is full and a default airport capacity that can be overridden

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -5,4 +5,22 @@ describe Airport do
     plane = Plane.new
     expect(airport.land(plane).length).to eq 1
   end
+  describe '#land' do
+    it 'raises an error when full' do
+      Airport::DEFAULT_CAPACITY.times do
+        subject.land Plane.new
+      end
+      expect { subject.land Plane.new }.to raise_error 'Airport full'
+    end
+  end
+  it 'has a default capacity' do
+    expect(subject.capacity).to eq Airport::DEFAULT_CAPACITY
+  end
+  describe 'initialization' do
+    it 'has a variable capacity' do
+      airport = Airport.new(50)
+      50.times { airport.land Plane.new }
+      expect { airport.land Plane.new }.to raise_error 'Airport full'
+    end
+  end
 end
