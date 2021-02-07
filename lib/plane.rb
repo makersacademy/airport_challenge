2458179fe49b02c1c7e7e7a8df0a0e64cbeff730
Iain Aitken <iain_aitken@yahoo.co.uk>
2021-02-07 08:45:19 +0000

    refactoring plane.land method

--- lib/plane.rb
+++ lib/plane.rb
@@ -1,3 +1,11 @@
 class Plane
+  attr_reader :location
 
+  def initialize(location)
+    @location = location
+  end
+
+  def land(airport)
+    @location = airport
+  end
 end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -1,9 +1,17 @@
 require 'plane'
 
 describe Plane do
-  let(:plane) { Plane.new }
+  let(:plane) { Plane.new("airborne") }
 
   it 'plane class exists in program' do
     expect(plane).to be_a(Plane)
+  end
+
+  describe '#land' do
+
+    it 'land allows plane to land at airport' do
+      airport = double("airport")
+      expect { plane.land(airport) }.to change { plane.location }.from("airborne").to(airport)
+    end
   end
 end
