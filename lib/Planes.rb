8e991fb8c5b78db182699e37bd36d813cce49c80
Nicolas Bermejo <nicbermejo@gmail.com>
2021-02-07 10:35:49 +0000

    RSPEC errors with weather condition

--- lib/Planes.rb
+++ lib/Planes.rb
@@ -6,7 +6,7 @@ end
 class Airport
   attr_reader :plane, :capacity, :weather_today
   MAX_AIRPORT_CAPACITY = 20
-  WEATHER = ["clear", "stormy"]
+  WEATHER = [1, 2]
   def initialize(capacity = MAX_AIRPORT_CAPACITY, weather = WEATHER.sample)
     @weather_today = weather
     @capacity = capacity
@@ -14,18 +14,26 @@ class Airport
   end
 
   def land(plane)
-    if @plane.length >= @capacity
-      fail 'Airport is full'
-    else
-      @plane << plane
+    if @weather_today == 1
+      if @plane.length >= @capacity
+        fail 'Airport is full'
+      else
+        @plane << plane
+      end
+    elsif @weather_today == 2 
+      fail 'It is too risky to land'
     end
   end
   
   def takeoff
-    if @plane.length < 1
-      fail 'There are no planes ready for takeoff'
-    else
-      @plane.pop
+    if @weather_today == 1
+      if @plane.length < 1
+        fail 'There are no planes ready for takeoff'
+      else
+        @plane.pop
+      end
+    elsif @weather_today == 2
+      fail 'It is too risky to takeoff'
     end
   end
 
