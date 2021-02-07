8eb83e4fe8ba0f7232f20308c28811b044268796
Laura <keenlaura@hotmail.co.uk>
2021-02-07 13:43:59 +0000

    Added describe blocks to rspec to make layout of tests easier to understand

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,10 +1,15 @@
 require 'airport.rb'
 
 describe Airport do
-  it { is_expected.to respond_to :land }
-
-  it { is_expected.to respond_to :takeoff }
 
   it { is_expected.to respond_to :empty? }
 
+  describe 'Landing' do
+    it { is_expected.to respond_to :land }
+  end
+
+  describe 'Takeoff' do
+    it { is_expected.to respond_to :takeoff }
+  end
+    
 end
