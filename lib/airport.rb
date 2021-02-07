1f51d922350e815cfe7330e0baeaffdf472c028a
jai-jk <jay.jk@hotmail.com>
2021-02-07 00:31:34 +0000

    Add tests and functionality for second user story

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,7 +1,28 @@
 require_relative 'plane'
 
 class Airport
-  def land(plane)
-    @plane = plane
+
+  def initialize
+    @planes_at_airport = []
   end
+
+  def land(plane)
+    @planes_at_airport << plane
+    return plane
+  end
+
+  def take_off(plane)
+    if @planes_at_airport.include?(plane)
+      @planes_at_airport.delete_at(@planes_at_airport.index(plane))
+    end
+    plane
+  end
+
+  def plane_at_airport?(plane)
+    puts plane
+    return true if @planes_at_airport.include?(plane)
+
+    false
+  end
+
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,12 +1,43 @@
 require 'airport'
 
 describe Airport do
+
   it { should respond_to :land }
 
-  it 'is expected to take a plane as a parameter when trying to land planes' do
+  it 'is expected to take a plane as a parameter when :land is called, and return the same instance' do
     airport = Airport.new
     plane = Plane.new
     expect(airport.land(plane)).to eq plane
   end
 
+  it { should respond_to :take_off }
+
+  it 'is expected to take a plane as a parameter when :take_off is called, and return the same instance' do
+    airport = Airport.new
+    plane = Plane.new
+    expect(airport.take_off(plane)).to eq plane
+  end
+
+  it { should respond_to :plane_at_airport? }
+
+  it 'is expected to take a plane as a parameter when :plane_at_aiport? is called, and return the same instance' do
+    airport = Airport.new
+    plane = Plane.new
+    allow(airport).to receive(:plane_at_airport) { plane }
+  end
+
+  it 'is expected to return false if a plane has taken off from the airport' do
+    airport = Airport.new
+    plane = Plane.new
+    airport.land(plane)
+    airport.take_off(plane)
+    expect(airport.plane_at_airport?(plane)).to eq false
+  end
+
+  it 'is expected to return true if a plane is still at the airport' do
+    airport = Airport.new
+    plane = Plane.new
+    airport.land(plane)
+    expect(airport.plane_at_airport?(plane)).to eq true
+  end
 end
