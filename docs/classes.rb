fbc8b01ffa106c7d7371e83c7083b4c02e819d4c
kasey <kasey.purvor@gmail.com>
2021-02-07 16:14:51 +0000

    all user sotries added.

--- docs/classes.rb
+++ docs/classes.rb
@@ -1,20 +1,49 @@
 class Airport 
-  DEFAULT_CAPACITY = 20 
+  DEFAULT_CAPACITY = 20 # default airport capacity
+  
   def initialize(capacity = DEFAULT_CAPACITY)
     @capacity = capacity 
     @planes = [] 
+    @weather  
   end 
     
-  def land(plane) 
-    fail "Airport Full. Cannot Land" if @planes.size >= @capacity
+  def weather_generator(run_type) # run type allows for sunny tests / stormy tests. To bypass random weather for test where not needed. 
+    if run_type == "stormy_test" # if block to allow garanteed stormy weather for testing stormy takeoff error
+      @weather = "stormy" # in the 'test' condition the weather will alwasys be stormy
+    elsif run_type == "sunny_test" # default value for testing that is not dependant on weather. 
+      @weather = "sunny"
+    elsif run_type == "run" # value for when random weather is required. Default running condition 
+      weather_gen = rand(10)
+      (weather_gen >= 8) ? (@weather = "stormy") : (@weather = "sunny") # otherwise it will use the weather_gen vaiable to assign wetaher
+    end
+  end  
+  
+  def land(plane, run_type = "run") 
+    full?
+    @weather = weather_generator(run_type)
+    weather? 
     (@planes.size < @capacity) ? @planes << plane : nil
   end
 
-  def take_off(plane)
-    print "Successful take off:"
-    print "true, plane no longer in airport" 
+  def take_off(plane, run_type = "run")
+    empty?
+    weather_generator(run_type)
+    weather?
+    @planes.delete(plane)
     true 
   end 
+end 
+
+def full? # checks to see if airport is full. And thus no planes can land here. 
+  fail "Airport Full. Cannot Land" if @planes.size >= @capacity 
+end
+
+def empty? # checks to see if the airport is empty. And thus no planes can take off from it. 
+  fail "Airport Empty" if @planes.size == 0 
+end
+
+def weather? # checks to see if stormy weather. If so It return error"  
+  fail "Weather Stormy" if @weather == "stormy" 
 end 
 
 class Plane
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -11,20 +11,34 @@ end
 
 it "confirms the plane is no longer at the airpoirt, after takeoff method called" do
   plane = Plane.new
-  expect(subject.take_off(plane)).to eq(true)
+  subject.land(plane, "sunny_test") 
+  expect(subject.take_off(plane, "sunny_test")).to eq(true)
 end  
 
 it "prevents landing when the airport if full at default capacity" do 
   plane = Plane.new 
-  Airport::DEFAULT_CAPACITY.times{subject.land(plane)}
-  expect{subject.land(plane)}.to raise_error("Airport Full. Cannot Land") 
+  Airport::DEFAULT_CAPACITY.times{subject.land(plane, "sunny_test")}
+  expect{subject.land(plane, "sunny_test")}.to raise_error("Airport Full. Cannot Land") 
 end 
 
 it "prevents landing when full, at non default capacity" do 
   plane = Plane.new
   airport = Airport.new(50) 
-  50.times{ airport.land(plane) } 
-  expect{airport.land(plane)}.to raise_error("Airport Full. Cannot Land") 
+  50.times{ airport.land(plane, "sunny_test") } 
+  expect{airport.land(plane, "sunny_test")}.to raise_error("Airport Full. Cannot Land") 
+end 
+
+it "prevents take_off when the weather is stormzy" do 
+  airport = Airport.new(10)
+  plane = Plane.new 
+  airport.land(plane, "sunny_test")
+  expect{airport.take_off(plane, "stormy_test")}.to raise_error("Weather Stormy") 
+end 
+
+it "prevents landing when the weather is stormzy" do 
+  airport = Airport.new(10) 
+  plane = Plane.new 
+  expect{airport.land(plane, "stormy_test")}.to raise_error("Weather Stormy")
 end 
 
 end 
