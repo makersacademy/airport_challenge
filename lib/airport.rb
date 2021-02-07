2ae4eea3e2d40023ec851d13aa80ab2b567330b7
AdamP2000 <Adam2000Phillips@gmail.com>
2021-02-07 21:01:52 +0000

    Passing test to prevent landing when full and setting defauly capacity that can be changed

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,13 +1,24 @@
 require_relative 'plane'
 class Airport
-
-  def initialize
+  DEFAULT_CAPACITY = 33
+  attr_accessor :capacity
+  def initialize(capacity = DEFAULT_CAPACITY)
     @planes = []
+    @capacity = capacity
   end
 
   def land(plane)
+    fail 'Airport full' if full?
+    
     @planes << plane
     @planes
   end
 
+  private
+
+  attr_reader :planes
+
+  def full?
+    planes.count >= @capacity
+  end
 end
