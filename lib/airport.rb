13baa789e1c4e10143f18989b3f81d3d188a461b
Eds <steadyede@live.co.uk>
2021-02-08 00:46:24 +0000

    User stories complete, rubocopped

--- lib/airport.rb
+++ lib/airport.rb
@@ -8,11 +8,12 @@ class Airport
 
   def land(plane)
     full?
+    check_weather(set_weather, "land")
     @hangar.push(plane)
   end
 
   def take_off(plane)
-
+    check_weather(set_weather, "take_off")
     @hangar.delete(plane)
   end
 
@@ -20,10 +21,23 @@ class Airport
     @hangar
   end
 
+  def set_weather
+    gw = [1, 2, 3, 4, 5, 6, 7].sample
+    return "stormy" if gw > 5
+
+    "sunny"
+  end 
+
+  def check_weather(weather, mode)
+    return unless weather == "stormy"
+    fail 'Cant land, bad weather!' if mode == "land"
+    fail 'Cant take off, bad weather!' if mode == "take_off"
+  end
+
   def full?
-    if @hangar.count >= @capacity
-      raise 'Airport is full' 
-    end
+    return unless @hangar.count >= @capacity
+    
+    raise 'Airport is full' 
   end
 
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,7 +1,6 @@
 require "airport"
 require "plane"
 
-
 describe Airport do
 
   it { is_expected.to respond_to(:land) }
@@ -10,47 +9,50 @@ describe Airport do
     it 'sets a default airport capacity' do
       # default capacity = 5
       dxb = Airport.new
-      expect{ 100.times {dxb.land Plane.new} }.to raise_error 'Airport is full'
+      allow(dxb).to receive(:set_weather) { "sunny" }
+      expect { 100.times { dxb.land Plane.new } }.to raise_error 'Airport is full'
     end
     it 'can override the default airport capacity' do
       dxb = Airport.new(30)
+      allow(dxb).to receive(:set_weather) { "sunny" }
       30.times { dxb.land Plane.new }
-      expect{ dxb.land(Plane.new) }.to raise_error 'Airport is full'
+      expect { dxb.land(Plane.new) }.to raise_error 'Airport is full'
     end
   end
   
   describe "#land" do
     it 'allows airplanes to land' do
       dxb = Airport.new
+      allow(dxb).to receive(:set_weather) { "sunny" }
       pj = Plane.new
       dxb.land(pj)
       expect(dxb.hangar).to include(pj)
+    end
+    it 'prevents landing if weather is stormy' do
+      dxb = Airport.new
+      allow(dxb).to receive(:set_weather) { "stormy" }
+      expect { dxb.land(Plane.new) }.to raise_error 'Cant land, bad weather!'
     end
   end
 
   describe "#take_off" do
     it 'instructs airplane to take off and confirms its left the airport' do  
       dxb = Airport.new
+      allow(dxb).to receive(:set_weather) { "sunny" }
       pj = Plane.new
       dxb.land(pj)
       dxb.take_off(pj)
       expect(dxb.view_hangar).not_to include(pj)
     end
-  end
-
-
-end  
-=begin  
-  describe "#hangar" do
-    it 'allows airplanes to land' do
+    it 'prevents landing if weather is stormy' do
       dxb = Airport.new
+      allow(dxb).to receive(:set_weather) { "sunny" }
       pj = Plane.new
-      expect(dxb.hangar).to include(pj)
-    end 
+      dxb.land(pj)
+      allow(dxb).to receive(:set_weather) { "stormy" }
+      expect { dxb.take_off(Plane.new) }.to raise_error 'Cant take off, bad weather!'
+    end
+
   end
-=end
-=begin
-  describe "#initialize" do
-    it 'allows user to set initial station capacity' do
-      station =
-=end
+
+end
