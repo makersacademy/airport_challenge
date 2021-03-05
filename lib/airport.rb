fdb855ca3dd51ee87700e441fe4cd43da813bf66
David Greaves <dm_greaves@msn.com>
2021-03-05 21:45:21 +0000

    Successful first unit test for Airport class

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,2 +1,4 @@
 class Airport
+  def land(plane)
+  end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,9 +2,6 @@ require 'airport'
 
 describe Airport do
 
-  it 'instructs planes to land at an airport' do
-    it { is_expected. to respond_to(:land).with(1).argument
-    }
-  end
+    it { is_expected. to respond_to(:land).with(1).argument }
 
 end
