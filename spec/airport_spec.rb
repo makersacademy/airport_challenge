f571a930da5252fc54356a8932d07ab23ba51946
Lou Reade <39278690+loushark@users.noreply.github.com>
2021-03-06 12:47:34 +0000

    passed test for capacity full / added test for capacity at different airports

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,14 +1,15 @@
 require 'airport'
 
-heathrow = Airport.new
-bertie = Plane.new
-
 describe Airport do
+  let(:heathrow) { Airport.new }
+  let(:bertie) { Plane.new }
+
   it "allows a plane to land at an airport" do
     expect(heathrow.land(bertie)).to eq([bertie])
   end
 
   it "allows a plane to take off" do
+    heathrow.land(bertie)
     expect(heathrow.take_off(bertie)).to eq(bertie)
   end
 
@@ -30,6 +31,11 @@ describe Airport do
   it "prevents a plane from landing when the airport is at capacity" do
     heathrow.land(bertie)
     colin = Plane.new
-  expect { heathrow.land(colin) }.to raise_error("A plane can't land, there's no room!")
+    expect { heathrow.land(colin) }.to raise_error("A plane can't land, there's no room!")
+  end
+
+  it "allows airports to have different capacities" do
+    expect(heathrow.capacity).to eq(3)
+    expect(gatwick.capacity).to eq(5)
   end
 end
