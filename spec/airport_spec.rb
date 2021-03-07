cbf1419c1e1e0dcf8140f6e798e84008dc2cb36a
taran314 <tarandep-nandhra@hotmail.co.uk>
2021-03-07 21:01:06 +0000

    Added README

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -4,6 +4,8 @@ describe Airport do
   let(:plane) { Plane.new('concord', 'air') }
   let(:airport) { Airport.new('heathrow', [], 1) }
 
+  # test redundant?
+
   describe 'full?' do
     it 'returns true if hangar is at capacity' do
       airport.hangar << plane
