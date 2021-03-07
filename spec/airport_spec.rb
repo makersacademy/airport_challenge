0390c52658c8446ecf341567c05868d0db1bbe6c
StuBehan <33100049+StuBehan@users.noreply.github.com>
2021-03-07 17:40:37 +0000

    refactor tests to ensure that random was taken care off after some spurious test failures

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,14 +2,15 @@ require 'plane'
 require 'airport'
 
 describe Airport do
-  test_airport = Airport.new
-  test_plane = Plane.new("AB123", test_airport, test_airport)
+  test_airport = Airport.new("ABC")
 
-  it "has a code and returns it" do
-    expect(subject.code).to be_a(String)
+  describe "airport code" do
+    it "must be a string" do
+      expect(test_airport.code).to be_a(String)
+    end
   end
 
-  it "should have a maximum capactity" do
+  it "must have a default maximum capactity" do
     expect(Airport.new.capacity).to eq(Airport::DEFAULT_CAPACITY)
   end
 
@@ -22,30 +23,30 @@ describe Airport do
   end
   
   describe 'safe_to_land?' do
-    it "returns false when it is considered unsafe to land" do
+    it "must return false when it is considered unsafe to land" do
       allow(subject).to receive(:local_weather) { :stormy }
       expect(subject.safe_to_land?).to eq(false)
     end  
   end
 
   describe 'safe_to_takeoff?' do
-    it "returns false when it is considered unsafe to takeoff" do
+    it "must return false when it is considered unsafe to takeoff" do
       allow(subject).to receive(:local_weather) { :stormy }
       expect(subject.safe_to_takeoff?).to eq(false)
     end
   end
 
   describe 'local_weather' do
-    it "returns :clear when rand throws 1..5" do
+    it "must return :clear when rand throws 1..5" do
       allow(subject).to receive(:rand).and_return(1, 5)
       expect(subject.local_weather).to be(:clear)
     end
-    it "returns :stormy when the rand throws a 6" do
+    it "must return :stormy when rand throws a 6" do
       allow(subject).to receive(:rand).and_return(6)
       expect(subject.local_weather).to be(:stormy)
     end
     context "if the weather is stormy" do
-      it "it updates the safety methods" do
+      it "the outcomes of safe_to_land and safe_to_Takeoff? must be false" do
         allow(subject).to receive(:local_weather) { :stormy }
         expect(subject.safe_to_land?).to eq(false)
         expect(subject.safe_to_takeoff?).to eq(false)
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -5,24 +5,26 @@ describe Plane do
   test_airport = Airport.new("ABC")
   test_plane = Plane.new("AB123", test_airport, test_airport)
 
-  it "returns a callsign" do
-    expect(subject.callsign).to be_a(String)
-  end
+  context "upon creation" do
+    it "must return a callsign" do
+      expect(subject.callsign).to be_a(String)
+    end
 
-  it "must have an origin and destination that are of the class Airport" do
-    expect(subject.origin).to be_a(Airport)
-    expect(subject.destination).to be_a(Airport)
-  end
+    it "must have an origin and destination that are of the class Airport" do
+      expect(subject.origin).to be_a(Airport)
+      expect(subject.destination).to be_a(Airport)
+    end
 
-  it "must report its flying status as true or false" do
-    expect(subject.in_flight).to be(true).or be(false)
+    it "must report its flying status as true or false" do
+      expect(subject.in_flight).to be(true).or be(false)
+    end
   end
 
   context "in response to a request for location" do
-    it "responds with its origin code if in_flight == false" do
+    it "must respond with its origin code if landed at an airport" do
       expect(test_plane.location.length).to eq(3)
     end
-    it "responds with a string message when in flight" do
+    it "must respond with a string message when in flight" do
       allow(test_airport).to receive(:rand).and_return(1)
       test_plane.takeoff(test_airport)
       expect(test_plane.location).to be_a(String)
@@ -30,7 +32,7 @@ describe Plane do
     end
   end
     
-  context "when the airport weather is good for landing and takeoff a plane" do
+  context "when the airport weather is good for landing and takeoff" do
     it "is able to land at a given airport" do
       allow(test_airport).to receive(:rand).and_return(1)
       expect(test_airport.safe_to_land?).to eq(true)
@@ -41,14 +43,14 @@ describe Plane do
     end
   end
 
-  context "the plane's in_flight status must" do
-    it "be true when the plane has taken off" do
+  context "must report its in_flight status as" do
+    it "true when the plane has taken off" do
       subject.origin = test_airport
       allow(test_airport).to receive(:rand).and_return(1)
       subject.takeoff(test_airport)
       expect(subject.in_flight).to eq(true) 
     end
-    it "be false when the plane has landed" do
+    it "false when the plane has landed" do
       subject.origin = test_airport
       allow(test_airport).to receive(:local_weather) { :clear }
       subject.takeoff(test_airport)
@@ -58,24 +60,27 @@ describe Plane do
     end
   end
 
-  context "when a plane is in flight it" do 
-    it "returns and error if given order to take off" do
+  context "when in flight" do 
+    it "must return an error if given order to take off" do
       allow(test_airport).to receive(:rand).and_return(1)
-      subject.takeoff(test_airport)
-      expect { subject.takeoff(test_airport) }.to raise_error("This callsign is already in flight")
+      test_plane.takeoff(test_airport)
+      expect { test_plane.takeoff(test_airport) }.to raise_error("This callsign is already in flight")
     end
-    it "returns an error if ordered to land and the weather at destination is :stormy" do
-      allow(test_airport).to receive(:rand).and_return(1)
-      subject.takeoff(test_airport)
+    it "must return an error if ordered to land and the weather at destination is :stormy" do
       allow(test_airport).to receive(:rand).and_return(6)
-      expect { subject.land(test_airport) }.to raise_error("The weather prevents landing")
+      expect { test_plane.land(test_airport) }.to raise_error("The weather prevents landing")
     end
   end
 
-  context "when a plane has landed it" do
-    it "returns an error if ordered to land" do
+  context "when landed" do
+    it "must return an error if ordered to land" do
       allow(test_airport).to receive(:rand).and_return(1)
-      expect { subject.land(test_airport) }.to raise_error("This callsign has already landed")
+      test_plane.land(test_airport)
+      expect { test_plane.land(test_airport) }.to raise_error("This callsign has already landed")
+    end
+    it "must return an error if ordered to takeoff and the weather at origin is :stormy" do
+      allow(test_airport).to receive(:rand).and_return(6)
+      expect { test_plane.takeoff(test_airport) }.to raise_error("The weather prevents take off")
     end
   end
 end
