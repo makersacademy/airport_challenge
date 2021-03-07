2ae2292fe07d0f13c530c84d13cb7f8c51ec88bc
Jack Hooper <77017281+crotchetycrow@users.noreply.github.com>
2021-03-07 12:15:14 +0000

    Complete user story #3

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,7 +1,25 @@
+require 'planes'
+
 class Airport
-  def instruct_to_land(plane)
+  attr_reader :plane, :capacity
+  DEFAULT_CAPACITY = 100
+
+  def initialize(capacity = DEFAULT_CAPACITY)
+    @plane = []
+    @planes = Plane.new
+    @capacity = capacity
   end
 
-  def instruct_to_take_off(place, confirm)
+  def instruct_to_land(plane)
+    fail "CANNOT LAND, AIRPORT AT CAPACITY" if full?
+    @plane << plane
+  end
+
+  def instruct_to_take_off(plane, confirm_status)
+  end
+
+  private
+  def full?
+    @capacity == @plane.count
   end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,6 +1,17 @@
 require 'airport'
+require 'planes'
 
 describe Airport do
-  it { is_expected.to respond_to(:instruct_to_land).with(1).argument }
-  it { is_expected.to respond_to(:instruct_to_take_off).with(2).argument }
+  let(:plane) { Plane.new }
+  describe "instruct_to_land" do
+    it { is_expected.to respond_to(:instruct_to_land).with(1).argument }
+    it "prevents landing if airport is full" do
+      allow(plane).to receive(:instruct_to_land).and_return(false)
+      Airport::DEFAULT_CAPACITY.times { subject.instruct_to_land(plane) }
+      expect { subject.instruct_to_land Plane.new }.to raise_error "CANNOT LAND, AIRPORT AT CAPACITY"
+    end
+  end
+  describe "instruct_to_take_off" do
+    it { is_expected.to respond_to(:instruct_to_take_off).with(2).argument }
+  end
 end
