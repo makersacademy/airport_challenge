1a9d7a15481716d602f28b8bff042bc90c52e339
Jack Hooper <77017281+crotchetycrow@users.noreply.github.com>
2021-03-07 11:03:49 +0000

    Complete user story #2

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,4 +1,7 @@
 class Airport
   def instruct_to_land(plane)
   end
+
+  def instruct_to_take_off(place, confirm)
+  end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,4 +2,5 @@ require 'airport'
 
 describe Airport do
   it { is_expected.to respond_to(:instruct_to_land).with(1).argument }
+  it { is_expected.to respond_to(:instruct_to_take_off).with(2).argument }
 end
