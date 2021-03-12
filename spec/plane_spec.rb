7a36712ce799d75b0bcd5b305f8523d54da1e0a3
Alice Lui <aytlui@gmail.com>
2021-03-12 10:58:39 +0000

    user story 1

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -2,4 +2,10 @@ require 'spec_helper'
 require 'plane'
 
 describe Plane do
+  it 'responds to land' do
+    expect(subject).to respond_to :land
+  end
+
+  it { is_expected.to respond_to(:land).with(1).argument }
+
 end
