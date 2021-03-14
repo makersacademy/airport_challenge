4cd9299e5b2252603ef1f02dca131a97e1bda674
Robert <mccannb401@gmail.com>
2021-03-14 16:58:53 +0000

    As an air traffic controller So I can get passengers on the way to their destination I want to instruct a plane to take off from an airport and confirm that it is no longer in the airport

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,4 +1,12 @@
+require_relative 'plane'
+
 class Airport
+  attr_reader :plane
   def land_plane
   end
+
+  def plane_departed?(plane)
+    @plane = plane
+  end
+
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,4 +2,11 @@ require 'airport'
 
 describe Airport do
   it { is_expected.to respond_to :land_plane }
+
+  it { is_expected.to respond_to(:plane_departed?).with(1).argument }
+  it 'departs a plane' do
+    plane = subject.plane_departed?(Plane.new)
+    expect(plane).to eq plane
+  end
+
 end
