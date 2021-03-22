eadaa644272d837377c7d5445bba3d8457e599e8
RJ21BL <rigorous@live.co.uk>
2021-03-22 14:44:33 +0000

    Airport challenge

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,6 +1,7 @@
 class Airport
 
-  attr_accessor :planes, :capacity, :stormy
+  attr_accessor :capacity
+  attr_reader :planes
 
   DEFAULT_CAPACITY = 10
 
@@ -15,16 +16,12 @@ class Airport
     if capacity == @planes.length
       raise "airport capacity full"
     end
-
     @planes << plane
   end
 
   def plane_landed?(plane)
-    if @planes.include?(plane)
-      return true
-    else
-      return false
-    end
+    return true if @planes.include?(plane)
+    false
   end
 
   def take_off(plane)
@@ -32,9 +29,7 @@ class Airport
   end
 
   def prevent_landing(plane)
-    raise "planes cannot land when airport is full"
-    if @planes.count >= @capacity
-    end
+    raise "planes cannot land when airport is full" if @planes.count >= @capacity
   end
 end
   # def prevent_take_off(plane)
@@ -42,3 +37,9 @@ end
   #     raise "planes cannot take off when weather is stormy"
   #   end
   # end
+
+ #  use private method - can not call outside class
+ #
+ #  def stormy?
+ #   p rand(1..7) > 4
+ # end
