b8bce913a8a11ccf4040c7985b823bbcb7cccf86
jasonrowsell <jasonrowsell@hotmail.co.uk>
2021-04-02 20:24:19 +0100

    Added plane rspec and plane class

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,5 +1,14 @@
 require 'airport'
 
 describe Airport do
+
+  it { is_expected.to be_instance_of Airport }
+
   it { is_expected.to respond_to(:land).with(1).argument }
+
+  it 'returns a landed plane' do
+    plane = Plane.new
+    expect(subject.land(plane)).to eq(plane)
+  end
+
 end
