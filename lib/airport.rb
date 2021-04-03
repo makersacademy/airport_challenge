963c87eb85f3bb80e29ca21738b1b1f500b5086c
jasonrowsell <jasonrowsell@hotmail.co.uk>
2021-04-03 11:56:56 +0100

    Refactored RSpec

--- lib/airport.rb
+++ lib/airport.rb
@@ -2,7 +2,17 @@ require_relative 'plane'
 
 class Airport
 
+  attr_reader :planes
+
+  def initialize
+    @planes = []
+  end
+
   def land(plane)
+    @planes << plane
+  end
+
+  def takeoff(plane)
     plane
   end
 
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,13 +2,23 @@ require 'airport'
 
 describe Airport do
 
+  before do
+    @plane = Plane.new
+  end
+
   it { is_expected.to be_instance_of Airport }
 
   it { is_expected.to respond_to(:land).with(1).argument }
 
-  it 'returns a landed plane' do
-    plane = Plane.new
-    expect(subject.land(plane)).to eq(plane)
+  it 'lands a given plane' do
+    expect(subject.land(@plane)).to eq([@plane])
   end
 
+  it 'returns the landed planes' do
+    subject.land(@plane)
+    expect(subject.planes).to eq([@plane])
+  end
+
+  it { is_expected.to respond_to(:takeoff).with(1).argument }
+  
 end
