59b3882c3700ad25eca588805dcf4cb21d791842
Kerri McMahon <kerrimcm25@gmail.com>
2021-04-03 12:32:09 +0100

    Completed second User Story

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,4 +1,23 @@
+require_relative 'plane'
 class Airport 
+  attr_reader :planes
+  def initialize
+    @planes = []
+  end 
+
   def land(plane)
+    @planes << plane
+  end 
+
+  def takeoff(*)
+    @planes.pop
+  end 
+
+  def include?(plane)
+    if @planes.include?(plane)
+      true
+    else 
+      false
+    end 
   end 
 end 
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,4 +1,30 @@
 require 'airport'
 describe Airport do
   it { is_expected.to respond_to(:land).with(1).argument }
+
+  it { is_expected.to respond_to(:takeoff).with(1).argument }
+
+  it 'lands planes' do
+    plane = Plane.new
+    expect(subject.land(plane)).to eq [plane]
+  end 
+
+  it 'allows planes to take off' do
+    plane = Plane.new
+    subject.land(plane)
+    expect(subject.takeoff(plane)).to eq plane
+  end 
+
+  it 'confirms the plane is no longer in the airport' do
+    plane = Plane.new
+    subject.land(plane)
+    subject.takeoff(plane)
+    expect(subject.include?(plane)).to be false
+  end 
+
+  it 'confirms the planes are in the airport' do
+    plane = Plane.new
+    subject.land(plane)
+    expect(subject.include?(plane)).to be true
+  end 
 end 
