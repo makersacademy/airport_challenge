72eb97fafbe033caabde479b94b0afba5b796969
Tiago <tiagomanuelpereiracampos@hotmail.com>
2021-04-03 20:28:36 +0100

    takes of planes

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,16 +1,21 @@
 require_relative 'plane'
 class Airport
+    ::DEFAULT_CAPACITY = 2
     attr_reader :planes
-    def initialize
+    attr_reader :capacity
+    
+    def initialize(capacity = DEFAULT_CAPACITY)
         @planes = []
+        @capacity = capacity
     end
     def land(plane)
+        raise "full" if full?
         @planes << plane
     end
     def take_of(plane)
-         
         @planes.delete(plane)
-        
-        
+    end
+    def full?
+        @planes.length >= DEFAULT_CAPACITY
     end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -6,9 +6,14 @@ describe Airport do
         plane = Plane.new
     expect(subject.land(plane)).to eq([plane])
     end
+    it 'doesnt land planes when its full' do
+        ::DEFAULT_CAPACITY.times {subject.land Plane.new }
+   expect { subject.land Plane.new }.to raise_error('full')
+  end
     #take of
     it {is_expected.to respond_to (:take_of) }
-    it 'takes of a plane' do 
+    
+  it 'takes of a plane' do 
         @planes = []
         plane = Plane.new
         subject.land(plane)
@@ -16,16 +21,22 @@ describe Airport do
         subject.take_of(plane)
         @planes.delete(plane)
         expect(@planes).not_to include(plane)
-       
     end
-  #####it 'takes of a plane' do 
-     ####plane = Plane.new
-    ### subject.land(plane)
-   ## expect(subject.take_of(plane)).to ("plane has taken of")
-    #end
-    
 
+    
+        
+        
+        
+        
+        #plane = Plane.new
+        
+        #expect(subject.land(plane)).to raise("its full")
+    #end
+
+  
+    
+       
          
     #off from an airport and confirm 
-    #that it is no longer in the airport
+    #that it is no longer in the airport missing this on the take of part
 end
