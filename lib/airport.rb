f2b8137cb4f7c87d592c9be6e982d6c92e9a7b84
Tiffany Vallo <tiffanyvallo@tiffanys-mbp.home>
2021-04-03 21:36:27 +0100

    4th Refactored

--- lib/airport.rb
+++ lib/airport.rb
@@ -8,13 +8,19 @@ DEFAULT_CAPACITY = 20
   
   def land(plane)
     plane = Plane.new
-    fail "Sorry, Airport packed over capacity" if @runway.length >= DEFAULT_CAPACITY
+    fail "Sorry, Airport packed over capacity" if full?
     @runway << plane
   end
 
   def takeoff(plane)
     @runway.delete(plane)
     return "#{plane} is in the clouds"
+  end
+
+  private
+  
+  def full?
+    @runway.length >= DEFAULT_CAPACITY
   end
 
 end
