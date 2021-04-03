ca0e4c0006053f7c0d9d777a5e888a003528f7fa
dominicmeddick <dominicmeddick@gmail.com>
2021-04-03 17:28:51 +0100

    Halfway through no2

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,5 +1,23 @@
-class Airport
-  def land(plane)
+# require_relative 'plane'
 
+class Airport 
+  def land(plane)
+    
+  end
+
+  # def initialize
+  #   @planes = []
+  # end
+
+  # def empty?
+  #   @plane == []
+  # end
+
+  def takeoff(plane)
+    # if empty?
+    #   raise "the plane is no longer in the airport"
+    # else
+    #   return 
+    # end
   end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,8 +2,23 @@ require 'airport'
 
 describe Airport do
   
-  it 'responds to land' do
+  it 'airport responds to land' do
     expect(subject).to respond_to(:land).with(1).argument
+  end
+
+  it 'airport responds to takeoff' do
+    expect(subject).to respond_to(:takeoff).with(1).argument
+  end
+
+  it 'plane lands in airport' do
+    plane = Plane.new
+    expect(subject.land(plane)).to eq plane
+  end
+
+  it 'plane takesoff from airport' do
+    plane = Plane.new
+    subject.land(plane)
+    expect(subject.takeoff(plane)).to eq plane
   end
 
 end
