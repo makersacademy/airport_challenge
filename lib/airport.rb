85b5806cfc95ba71cb5ec3adbbc2a36f23bdcd70
Matt <mattdawson18@gmail.com>
2021-04-03 13:49:52 +0100

    improved functionality of weather object to allow more realistic usage, and refactored spec files to reduce codebase

--- lib/airport.rb
+++ lib/airport.rb
@@ -2,20 +2,19 @@ require_relative 'plane'
 require_relative 'weather'
 
 class Airport 
-  attr_reader :plane, :hangar, :capacity, :storm, :weather
+  attr_reader :plane, :hangar, :capacity, :weather
   DEFAULT_CAPACITY = 10
 
   def initialize(capacity = DEFAULT_CAPACITY, weather: Weather.new)
     @hangar = []
     @capacity = capacity
-    @storm = weather.stormy?
-    # @hangar << Plane.new
+    @weather = weather
   end
 
   def land(plane)
     fail 'Airport is full' if full?
 
-    fail 'Weather is too bad' if @storm
+    fail 'Weather is too bad' if @weather.stormy?
 
     @hangar.push(plane)
   end
@@ -23,13 +22,9 @@ class Airport
   def take_off
     fail 'Airport is empty' if empty?
 
-    fail 'Weather is too bad' if @storm
+    fail 'Weather is too bad' if @weather.stormy?
 
     @hangar.pop
-  end
-
-  def add_plane
-    @hangar << Plane.new
   end
 
   private 
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,6 +2,10 @@ require 'airport'
 require 'weather'
 
 describe Airport do
+  let(:plane) {Plane.new}
+  def storm_false
+    allow(subject.weather).to receive(:stormy?) { false }
+  end
 
   it 'has a default capacity' do
     expect(subject.capacity).to eq(10)
@@ -13,8 +17,8 @@ describe Airport do
   end
 
   it 'can have a storm value' do
-    Weather.any_instance.stub(:stormy?).and_return(false)
-    expect(subject.storm).to eq(false)
+    storm_false
+    expect(subject.weather.stormy?).to eq(false)
   end
 
   describe '#land' do
@@ -27,27 +31,25 @@ describe Airport do
     end
   
     it 'can land a plane' do
-      plane = Plane.new
       subject.land(plane)
-      Weather.any_instance.stub(:stormy?).and_return(false)
+      storm_false
       expect(subject.hangar).to include(plane)
     end
   
     it 'can respond to plane' do
-      plane = Plane.new
+      storm_false
       subject.land(plane)
       expect(subject.hangar).to include(plane)
     end
 
     it 'prevents planes from landing when airport is full' do
-      plane = Plane.new
+      storm_false
       subject.capacity.times { subject.land(plane) }
       expect { subject.land(plane) }.to raise_error 'Airport is full'
     end
 
     it 'prevents a plane from landing in a storm' do
-      plane = Plane.new
-      Weather.any_instance.stub(:stormy?).and_return(true)
+      allow(subject.weather).to receive(:stormy?) { true }
       expect { subject.land(plane) }.to raise_error 'Weather is too bad'
     end
   end
@@ -59,21 +61,20 @@ describe Airport do
     end
 
     it 'lets a plane take_off' do
-      plane = Plane.new
       subject.land(plane)
-      Weather.any_instance.stub(:stormy?).and_return(false)
+      storm_false
       subject.take_off
       expect(subject.hangar).to be_empty
     end
 
     it 'prevents planes from taking off when airport is empty' do
+      storm_false
       expect { subject.take_off }.to raise_error 'Airport is empty'
     end
 
     it 'prevents a plane from taking off in a storm' do
-      Weather.any_instance.stub(:stormy?).and_return(true)
-      subject.add_plane
-      p subject
+      subject.land(plane)
+      allow(subject.weather).to receive(:stormy?) { true }
       expect { subject.take_off }.to raise_error 'Weather is too bad'
     end
   end
