429429907050327decacdb8eb667621a55112472
Tiago <tiagomanuelpereiracampos@hotmail.com>
2021-04-04 10:34:25 +0100

    new rspec syntax

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,39 +1,42 @@
 require 'airport'
 describe Airport do
   subject(:airport) {described_class.new}
-    # land
+  let(:plane) {double :plane}
+    describe '#land' do
   it { is_expected.to respond_to(:land).with(1).argument }
 
   it 'lands a plane' do 
     plane = Plane.new
     expect(airport.land(plane)).to eq([plane])
   end
-
-  it 'doesnt land planes when its full' do
-    2.times { airport.land Plane.new }
-    expect { airport.land Plane.new }.to raise_error('Airport full:take of a plane to land this one') 
+context 'when full' do
+  it 'raises an error' do
+    2.times { airport.land(plane)}
+    expect { airport.land(plane)}.to raise_error('Airport full:take of a plane to land this one') 
   end
-  
-  # take of
-    
-  it { is_expected.to respond_to(:take_of).with(1).argument }
-    
-  it 'takes of a plane' do 
-    @planes = []
-    plane = Plane.new
-    airport.land(plane)
-    @planes << plane
-    airport.take_of(plane)
-    @planes.delete(plane)
-    expect(@planes).not_to include(plane)
   end
+end
+  describe '#take of' do
+    
+    it { is_expected.to respond_to(:take_of).with(1).argument }
+    
+    it 'takes-of a plane' do 
+      @planes = []
+      plane = Plane.new
+      airport.land(plane)
+      @planes << plane
+      airport.take_of(plane)
+      @planes.delete(plane)
+      expect(@planes).not_to include(plane)
+    end
 
-  it 'confirms that plane is no longer at the airport' do
-    @planes = []
-    plane = Plane.new
-    airport.land(plane)
-    @planes << plane
-    expect(airport.take_of(plane)).to eq(plane)
+    it 'confirms that plane is no longer at the airport' do
+      @planes = []
+      plane = Plane.new
+      airport.land(plane)
+      @planes << plane
+      expect(airport.take_of(plane)).to eq(plane)
+    end
   end
 end   
   
