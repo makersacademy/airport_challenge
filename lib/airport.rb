598b7b17ae8830fb7e3085905b6f689c9538fa2d
dominicmeddick <dominicmeddick@gmail.com>
2021-04-04 11:37:13 +0100

    User Story 2

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,23 +1,26 @@
-# require_relative 'plane'
+require_relative 'plane'
 
 class Airport 
-  def land(plane)
-    
+  attr_reader :planes
+
+  def initialize 
+    @planes = []
   end
 
-  # def initialize
-  #   @planes = []
-  # end
-
-  # def empty?
-  #   @plane == []
-  # end
+  def land(plane)
+    @planes << plane
+  end
 
   def takeoff(plane)
-    # if empty?
-    #   raise "the plane is no longer in the airport"
-    # else
-    #   return 
-    # end
+    @planes.pop
   end
+
+  def no_longer_in_airport(plane)
+    if @planes.include?(plane)
+      return "The plane is in the airport"
+    else 
+      return "The plane is no longer in the airport"
+    end
+  end
+
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -12,13 +12,20 @@ describe Airport do
 
   it 'plane lands in airport' do
     plane = Plane.new
-    expect(subject.land(plane)).to eq plane
+    expect(subject.land(plane)).to eq [plane]
   end
 
   it 'plane takesoff from airport' do
     plane = Plane.new
     subject.land(plane)
     expect(subject.takeoff(plane)).to eq plane
+  end
+
+  it 'confirms planes are no longer in the airport' do
+    plane = Plane.new
+    subject.land(plane)
+    subject.takeoff(plane)
+    expect(subject.no_longer_in_airport(plane)).to eq "The plane is no longer in the airport"
   end
 
 end
