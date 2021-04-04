3b39a415ef38655d11e84e54afdf36095c48c6d1
ian-1 <ian@portmoor.com>
2021-04-04 17:51:51 +0100

    confirm if plane is is airport

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,9 +1,17 @@
 class Airport
-  def land(plane)
+  def initialize
+    @planes = []
+  end
 
+  def land(plane)
+    @planes.push(plane)
   end
 
   def take_off(plane)
+    @planes.delete(plane)
+  end
 
+  def hangar?(plane)
+    @planes.include?(plane)
   end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -4,4 +4,59 @@ describe Airport do
   it { is_expected.to respond_to(:land).with(1).argument }
 
   it { is_expected.to respond_to(:take_off).with(1).argument }
+
+  describe '#hangar?' do
+    it { is_expected.to respond_to(:hangar?).with(1).argument }
+  
+    it 'plane never landed in airport' do
+      plane = Plane.new
+      expect(subject.hangar?(plane)).to be false
+    end
+
+    it 'plane landed in airport' do
+      plane = Plane.new
+      subject.land(plane)
+      expect(subject.hangar?(plane)).to be true
+    end
+
+    it 'plane landed then took off from airport' do
+      plane = Plane.new
+      subject.land(plane)
+      subject.take_off(plane)
+      expect(subject.hangar?(plane)).to be false
+    end
+
+    it 'different plane landed in airport' do
+      plane_one = Plane.new
+      plane_two = Plane.new
+      subject.land(plane_one)
+      expect(subject.hangar?(plane_two)).to be false
+    end
+
+    it 'both planes landed in airport' do
+      plane_one = Plane.new
+      plane_two = Plane.new
+      subject.land(plane_one)
+      subject.land(plane_two)
+      expect(subject.hangar?(plane_one)).to be true
+    end
+
+    it 'both planes landed in airport, other plane took off' do
+      plane_one = Plane.new
+      plane_two = Plane.new
+      subject.land(plane_one)
+      subject.land(plane_two)
+      subject.take_off(plane_two)
+      expect(subject.hangar?(plane_one)).to be true
+    end
+
+    it 'both planes landed in airport, this plane took off' do
+      plane_one = Plane.new
+      plane_two = Plane.new
+      subject.land(plane_one)
+      subject.land(plane_two)
+      subject.take_off(plane_two)
+      expect(subject.hangar?(plane_two)).to be false
+    end
+  end
 end
