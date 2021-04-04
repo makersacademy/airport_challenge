148433520b4a7cf501099a7920339ba511fcc7d3
Abdur Rehman <wabdulrehman@yahoo.com>
2021-04-04 14:33:19 +0100

    refactor

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -1,23 +1,22 @@
 require 'plane'
 describe Plane do
+  let(:airport) { Airport.new }
   it { is_expected.to be_instance_of(Plane) }
   describe "#land" do
     it 'lands the plane' do
       subject.take_off
-      expect(subject.land(Airport.new)).to be_truthy
+      expect(subject.land(airport)).to be_truthy
     end
     it 'raise error when airport is full' do
-      airport = Airport.new
       Plane.new.take_off.land(airport)
       expect { subject.take_off.land(airport) }.to raise_error('airport full')
     end
     it 'raise error when already on ground' do
-      expect { subject.land(Airport.new) }.to raise_error('already on ground')
+      expect { subject.land(airport) }.to raise_error('already on ground')
     end
   end
   describe "#take_off" do
     it 'tells the plane to take off' do
-      subject.take_off.land(Airport.new)
       subject.take_off
       expect(subject).to be_in_air
     end
