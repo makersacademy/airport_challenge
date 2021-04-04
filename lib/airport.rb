43ba12f9f8adc88e2d49f70c0ce6ab667d2f06f7
fwill22 <79596926+fwill22@users.noreply.github.com>
2021-04-04 23:55:41 +0100

    Airport default capacity set using initialize, land method updated to store planes in array

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,12 +1,20 @@
 require_relative 'plane'
 
 class Airport
-  def land(plane)
+  attr_accessor :hangar, :capacity
+  DEFAULT_CAPACITY = 20
 
+  def initialize(capacity = DEFAULT_CAPACITY)
+    @hangar = []
+    @capacity = capacity
+  end
+
+  def land(plane)
+    @hangar << plane
   end
 
   def take_off(plane)
-  
+    
   end
-  
+
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,11 +2,19 @@ require 'airport'
 require 'plane'
 
 describe 'Airport' do
-  subject(:airport) {Airport.new}
+  subject(:airport) { Airport.new }
+  let(:plane) { Plane.new }
+
+  it "has a default capacity which can be changed as required" do
+    airport.capacity = 20
+    expect(airport.capacity).to eq(20)
+  end
   
   describe 'conditions for #land(plane)' do
     it "allows airport to accept landing plane" do
       allow(airport).to receive(:land)
+      airport.hangar << plane
+      expect(airport.hangar).to include(plane)
     end
   end
   
