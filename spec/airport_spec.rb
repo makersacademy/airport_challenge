12ca02980af7c5309f7ba92f63078fa320bf2a59
Muhammad Nasr Ahmad <nasrahmadcheema@hotmail.com>
2021-04-04 02:16:45 +0100

    User story 1 - add plane, airport classes and land method

---
+++ spec/airport_spec.rb
@@ -0,0 +1,10 @@
+require 'airport.rb'
+describe Airport do
+
+  it { is_expected. to be_an_instance_of(Airport) }
+
+  describe '#land' do
+    it { is_expected.to respond_to(:land).with(1).argument }
+  end
+
+end
