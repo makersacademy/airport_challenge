0ae45e687ae61e63baf7bec807e5aa7aa2767983
Phil Smithies <phil@phils-macbook-pro-1.home>
2021-04-04 09:58:33 +0200

    basic functionality added with tests

---
+++ lib/airport.rb
@@ -0,0 +1,40 @@
+require_relative 'plane.rb'
+
+class Airport
+attr_reader :hangar, :capacity
+HANGAR_CAPACITY = 50
+
+  def initialize(capacity = HANGAR_CAPACITY)
+    @hangar = []
+    @capacity = capacity
+  end
+
+  def land(plane)
+    if full?
+    raise RuntimeError, "Cannot land plane, Airport is full"
+    elsif stormy?
+    raise RuntimeError, 'It is stormy cannot land'
+    else
+    @hangar << plane
+    end
+  end
+
+  def take_off
+    raise RuntimeError, 'It is stormy cannot takeoff' if stormy?
+    @hangar.pop
+  end
+
+  
+  def stormy?(weather_prediction = rand(1..5))
+    if weather_prediction == 5 
+      true
+    else 
+      false
+    end
+  end 
+
+  private
+  def full? 
+    @hangar.size >= @capacity ? true : false
+  end
+end
---
+++ spec/airport_spec.rb
@@ -0,0 +1,28 @@
+require "airport.rb"
+
+describe Airport do 
+  it "It responds to land plane" do 
+    expect(subject).to respond_to(:land)
+  end
+  it "Checks there is a hangar to land in" do 
+    expect(subject.hangar).to eq([])
+  end
+  it "Checks that a plane can take off" do
+    expect(subject).to respond_to(:take_off)
+  end
+  it "Will not land a plane if the airport is full" do
+    Airport::HANGAR_CAPACITY.times {subject.land Plane.new}
+    expect{subject.land(Plane.new)}.to raise_error(RuntimeError, 'Cannot land plane, Airport is full')
+  end
+  it "Allows the default capacity of the airport to be overwritten" do
+    expect(Airport.new(27).capacity).to eq(27)
+  end
+  it "Checks that the weather can be stormy" do
+    expect(subject.stormy?(5)).to eq(true)
+  end
+  it "Will not take a plane off if the weather is stormy" do 
+    airport = Airport.new 
+    airport.stub(:stormy?).and_return(true)
+    expect{subject.take_off}.to raise_error(RuntimeError, 'It is stormy cannot takeoff')
+  end
+end
