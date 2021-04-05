1cb6b562fbb3578f62f26ac883962f79aaa67a04
Fifolu Akinyemi <fkinyemi@gmail.com>
2021-04-05 19:41:29 +0100

    Raises an exception when airport is full

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,22 +1,21 @@
 class AirportFull < StandardError
-  attr_reader :action
   def initialize(message)
     super(message)
   end
 end
 
 class Airport
-  def initialize=(plane = 0, max_capacity = 3)
+  def initialize(plane, max_capacity = 3)
     @plane = plane
     @max_capacity = max_capacity
   end
-  
+
   def land
     if @plane == @max_capacity
       raise AirportFull.new("Airport at maximum capacity")
     else
-      puts "Plane has successfully landed"
       @plane += 1
+      puts "Plane has successfully landed"    
     end
   end
 
