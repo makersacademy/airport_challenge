a6394bfd3104139eedc47f9c777c9a7b9616830c
Ionut Cristian Mintar <ionutcristian_mintar@yahoo.com>
2021-04-06 07:03:32 +0100

    Added default capacity that can be overriden

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,13 +1,28 @@
 require_relative 'plane'
 
 class Airport
-
+  DEFAULT_CAPACITY = 6
+  
+  attr_accessor :capacity
+  
+  def initialize(capacity=DEFAULT_CAPACITY)
+    @planes = []
+    @capacity = DEFAULT_CAPACITY
+  end 
+  
   def land(plane)
-    plane
+    fail 'Airport is full' if full?
+    @planes << plane
   end
 
   def take_off
     Plane.new
+  end
+
+  private
+
+  def full?
+    @planes.count >= capacity
   end
 
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -7,6 +7,27 @@ describe Airport do
   
   it 'lands the plane' do
     plane = Plane.new
-    expect(subject.land(plane)).to eq plane
+    expect(subject.land(plane)).to eq [plane]
   end
+
+  it 'raises an error when airport is full' do
+    subject.capacity.times { subject.land Plane.new }
+    expect { subject.land Plane.new }.to raise_error 'Airport is full'
+  end
+
+  it 'has a default capacity' do
+    expect(subject.capacity).to eq Airport::DEFAULT_CAPACITY
+  end
+
+  describe 'initialization' do
+    subject { Airport.new }
+    let(:plane) { Plane.new }
+    it 'defaults capacity' do
+      described_class::DEFAULT_CAPACITY.times do
+        subject.land(plane)
+      end
+      expect{ subject.land(plane) }.to raise_error 'Airport is full'
+    end
+  end
+
 end
