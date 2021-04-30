96268fd446c0d238fbab938257caa2ced6206d4d
BecaLParker <becaparker@gmail.com>
2021-04-30 13:03:44 +0100

    Adds pseudocode for fuller unit tests of stormy weather sceanrios

--- spec/atc_spec.rb
+++ spec/atc_spec.rb
@@ -1,6 +1,17 @@
 require 'air_traffic_control.rb'
 describe ATC do
-  it { is_expected.to respond_to :check_weather }
+  describe 'checks the weather' do
+    it { is_expected.to respond_to :check_weather }
+    # test that checks if returns stormy when weather == stormy
+  end
 
-  it { is_expected.to respond_to :land_plane }
+  describe 'instructs planes to land' do
+    it { is_expected.to respond_to :land_plane }
+    # test that expects "Can't land - too stormy" whne weather  == stormy
+  end
+
+  describe 'instructs planes to takeoff' do
+    it { is_expected.to respond_to :takeoff_plane }
+    # test that expects "Can't takeoff - too stormy" when weather  == stormy
+  end
 end
