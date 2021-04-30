7c25476025c9a757ddfdc53c384b3679014b37b1
Jay Mullen <81382978+Jaikido007@users.noreply.github.com>
2021-04-30 12:47:06 +0100

    set up: Airport spec and Airport, Plane spec and plane.  2 examples, 0 failiures

---
+++ spec/features/user_stories_spec.rb
@@ -0,0 +1,10 @@
+describe 'user stories' do
+  # As an air traffic controller
+  # So I can get passengers to a destination
+  # I want to instruct a plane to land at an airport
+  it "so planes can land at airports, instructs a plane to land" do
+    airport = Airport.new
+    plane = Plane.new
+    expect { airport.land(plane) }.not_to raise_error
+  end
+end
