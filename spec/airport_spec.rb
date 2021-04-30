bff105fb037b231c7f19cdd72bb24c07d5fe90c7
Adam R <adamroyal12@gmail.com>
2021-04-30 17:36:20 +0100

    6th commit. User Story Stormy Weather  Unit Tests failing.

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -14,11 +14,18 @@ describe Airport do
     expect(subject.return_planes).to eq []
   end
 
+  it "prevents planes from taking off when weather is stormy" do
+    @weather = double('weather')                                            #this bit doesn't work.
+    allow('weather').to receive(:weather) { "stormy" }
+    expect { subject.take_off(Plane.new) }.to raise_exception "Weather is stormy"
+
+  end
+
   it "raises an error when full" do
     subject.capacity.times do
       subject.land_plane(Plane.new)
     end
-    expect { subject.land_plane(Plane.new) }.to raise_error "Landing Declined: Airport full"
+    expect { subject.land_plane(Plane.new) }.to raise_exception "Landing Declined: Airport full"
 
   end
 
