5ad8463d6d8f8c2a140bf14caee36b18884e2857
Adam R <adamroyal12@gmail.com>
2021-04-30 13:02:12 +0100

    Second Commit. Second User Story Satisfied

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,5 +1,5 @@
-require "plane.rb"
-
+require_relative "plane"
+ 
 class Airport
   attr_reader :planes
     
@@ -9,6 +9,11 @@ class Airport
 
   def land_plane(plane)
     @planes.push(plane)
+  end
+
+  def take_off(plane)
+    @planes.delete(plane)
+    @planes
   end
 
   def return_planes
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,10 +1,18 @@
 require "airport.rb"
 
 describe Airport do
-  it "allows air traffic controller to instruct planes to land" do
+  it "air traffic controller instructs planes to land" do
     airport = Airport.new
     plane = Plane.new
     airport.land_plane(plane)
     expect(airport.return_planes).to eq [plane]
+  end
+
+  it "air traffic controller instructs planes to take off" do
+    airport = Airport.new
+    plane = Plane.new
+    airport.land_plane(plane)
+    airport.take_off(plane)
+    expect(airport.return_planes).to eq []
   end
 end
