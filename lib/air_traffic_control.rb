a3b39de2f7be3e6f7e7f32632f39787d92d88639
BecaLParker <becaparker@gmail.com>
2021-04-30 12:25:13 +0100

    Adds land_plane with early error message for stormy weather

--- lib/air_traffic_control.rb
+++ lib/air_traffic_control.rb
@@ -1,7 +1,14 @@
 class ATC
   def check_weather
-    p weather = "stormy"
+    weather = "stormy"
   end
+
+  def land_plane
+   if weather == "stormy"
+    puts "Can't land - too stormy"
+   end
+  end
+
 end
-luton = ATC.new
-luton.check_weather
+# luton = ATC.new
+# luton.check_weather
--- spec/atc_spec.rb
+++ spec/atc_spec.rb
@@ -1,4 +1,6 @@
 require 'air_traffic_control.rb'
 describe ATC do
   it { is_expected.to respond_to :check_weather }
+
+  it { is_expected.to respond_to :land_plane }
 end
