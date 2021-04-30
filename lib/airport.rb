1650ca85ac712349b915b7cc6f41f3e46ac44d7c
Liam Kilkenny <81643363+lkilk@users.noreply.github.com>
2021-04-30 16:44:37 +0100

    Airport Challenge

---
+++ lib/airport.rb
@@ -0,0 +1,38 @@
+class Airport
+  DEFAULT_CAPACITY = 20
+  attr_reader :capacity
+  attr_reader :planes
+
+  def initialize(capacity = DEFAULT_CAPACITY)
+    @planes = []
+    @capacity = capacity
+    @weather = rand(1..10)
+  end
+
+  def land_plane(plane)
+    fail 'Airport full' if full?
+
+    fail "Stormy weather - landing not permitted" if stormy?
+
+    @planes << plane
+  end
+
+  def take_off(plane)
+    fail "Stormy weather - take off not permitted" if stormy?
+
+    @planes.delete(plane)
+  end
+
+  def full?
+    @planes.count >= capacity
+  end
+
+  def stormy?
+    if @weather <= 1
+      true
+    else
+      false
+    end
+  end
+
+end
---
+++ lib/plane.rb
@@ -0,0 +1,11 @@
+# planes can only take off from airports they are in;
+# planes that are already flying cannot take off and/or be in an airport;
+# planes that are landed cannot land again and must be in an airport, etc.
+
+class Plane
+
+  def landed?
+    
+
+  end
+end
