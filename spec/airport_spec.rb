602130925516349c42a789753353de54210db2e6
Jay Mullen <81382978+Jaikido007@users.noreply.github.com>
2021-04-30 12:58:51 +0100

    added plane can take_off, 4 examples, 0 failiures

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -6,4 +6,8 @@ describe Airport do
   it "Expects a plane to land" do
     expect(airport).to respond_to(:land).with(1).argument
   end
+
+  it "Instructs a plane to take_off" do
+    expect(airport).to respond_to(:take_off).with(1).argument
+  end
 end
