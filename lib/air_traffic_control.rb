d4f18602d770d2ee6decabff215ea3d9db4954ec
BecaLParker <becaparker@gmail.com>
2021-04-30 13:16:13 +0100

    Passes feature teting for stormy scenarios, but unit test RSpec needs writing properly

--- lib/air_traffic_control.rb
+++ lib/air_traffic_control.rb
@@ -1,14 +1,19 @@
 class ATC
   def check_weather
-    weather = "stormy"
+    @weather = "stormy"
   end
 
   def land_plane
-   if weather == "stormy"
+   if self.check_weather == "stormy"
     puts "Can't land - too stormy"
    end
   end
 
+  def takeoff_plane
+    if self.check_weather == "stormy"
+      puts "Can't takeoff - too stormy"
+    end
+  end
 end
 # luton = ATC.new
 # luton.check_weather
