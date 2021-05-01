20b2a2f9d402ce00022ec759cc38332e032cf6a8
Charlie Slater <charliemslater1701@gmail.com>
2021-05-01 10:43:55 +0100

    added take_off

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,5 +1,12 @@
 require './lib/plane.rb'
 class Airport
+  attr_reader :hangar
+
   def land(plane)
+    @hangar = plane
+  end
+
+  def take_off(plane)
+    
   end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,8 +2,24 @@ require './lib/airport.rb'
 
 describe Airport do
   describe '#land' do
-    it 'can land a plane' do
+    it 'responds to #land with 1 argument' do
       expect(subject).to respond_to(:land).with(1).argument
+    end
+    it 'stores a plane in the airport hangar' do
+      plane = Plane.new
+      subject.land(plane)
+      expect(subject.hangar).to eq(plane)
+    end
+  end
+  describe '#take_off' do
+    it 'responds to #take_off with 1 argument' do
+      expect(subject).to respond_to(:take_off).with(1).argument
+    end
+    it 'removes the plane from the airport hangar' do
+      plane = Plane.new
+      subject.land(plane)
+      subject.take_off(plane)
+      expect(subject.hangar).to eq(nil)
     end
   end
 end
