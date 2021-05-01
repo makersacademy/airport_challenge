58ca5c0a8464c7514b932366bd5b16cb3da0415f
charlie-casserly <charliecasserly215@gmail.com>
2021-05-01 13:44:15 +0100

    User Story 3 designed and implemented

--- lib/airport.rb
+++ lib/airport.rb
@@ -2,16 +2,22 @@ require_relative 'plane'
 
 class Airport
   attr_reader :planes
+  attr_reader :capacity
 
-  def initialize
+  def initialize(capacity = 20)
     @planes = []
+    @capacity = capacity
   end
 
   def land(plane)
-    planes << plane
+    self.full? ? (fail "Cannot land. The airport is full.") : planes << plane
   end
 
   def take_off(plane)
     return planes.delete(plane)
+  end
+
+  def full?
+    planes.length >= capacity
   end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,36 +1,56 @@
 require 'airport'
 
-describe Airport do 
-  it 'lands at an airport' do
-    airport = Airport.new
-    plane = Plane.new
-    expect(airport.land(plane)).to eq([plane])
+describe Airport do
+  describe '#land' do
+    it 'lands at an airport' do
+      airport = Airport.new
+      plane = Plane.new
+      expect(airport.land(plane)).to eq([plane])
+    end
+
+    it 'stores planes in an airport' do
+      airport = Airport.new
+      plane = Plane.new
+      airport.land(plane)
+      expect(airport.planes).to eq([plane])
+    end
   end
 
-  it 'stores planes in an airport' do
-    airport = Airport.new
-    plane = Plane.new
-    airport.land(plane)
-    expect(airport.planes).to eq([plane])
+  describe '#take_off' do
+    it 'takes off' do
+      airport = Airport.new
+      plane = Plane.new
+      airport.land(plane)
+
+      expect(airport.take_off(plane)).to eq(plane)
+    end
+
+    it 'removes itself from an airport after takin off' do
+      airport = Airport.new
+      plane = Plane.new
+      plane2 = Plane.new
+
+      airport.land(plane)
+      airport.land(plane2)
+      airport.take_off(plane)
+
+      expect(airport.planes).not_to include([plane])
+    end
   end
 
-  it 'takes off' do
-    airport = Airport.new
-    plane = Plane.new
-    airport.land(plane)
+  describe '#full?' do
+    it 'checks to see if an airport is full' do
+      airport = Airport.new
+      airport.capacity.times{airport.land(Plane.new)}
+      expect(airport.full?).to eq true
+    end
 
-    expect(airport.take_off(plane)).to eq(plane)
-  end
+    it 'raises an error when landing at full airport' do
+      airport = Airport.new(2)
+      airport.land(Plane.new)
+      airport.land(Plane.new)
 
-  it 'removes itself from an airport after takin off' do
-    airport = Airport.new
-    plane = Plane.new
-    plane2 = Plane.new
-
-    airport.land(plane)
-    airport.land(plane2)
-    airport.take_off(plane)
-
-    expect(airport.planes).not_to include([plane])
+      expect{airport.land(Plane.new)}.to raise_error('Cannot land. The airport is full.')
+    end
   end
 end
