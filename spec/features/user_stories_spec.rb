8c43fbec67f15db33e664b7854b36ab4f9fe954b
JP Ferreira <jpferreira@ma301.home>
2021-05-01 08:11:08 +0100

    adding user stories, plane and airport spec and rb files

---
+++ spec/features/user_stories_spec.rb
@@ -0,0 +1,10 @@
+describe 'User Stories' do
+    # As an air traffic controller 
+    # So I can get passengers to a destination 
+    # I want to instruct a plane to land at an airport
+    it 'lands a plane at the airport after instruction to land' do
+        airport = Airport.new
+        plane = Plane.new
+        expect { aiport.land(plane) }.not_to raise_error
+    end
+end
