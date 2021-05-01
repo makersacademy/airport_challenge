b7cf74ce8c4002326f2053bd3a97835959e1617f
JP Ferreira <jpferreira@ma301.home>
2021-05-01 09:20:32 +0100

    Planes can now take off and passed RSPEC

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -6,4 +6,8 @@ describe Airport do
     it 'instructs a plane to land at the airport safely' do
         expect(airport).to respond_to(:land).with(1).argument
     end
+    
+    it 'instructs a plane to take off from the airport safely' do
+        expect(airport).to respond_to(:take_off).with(1).argument
+    end    
 end
--- spec/features/user_stories_spec.rb
+++ spec/features/user_stories_spec.rb
@@ -7,4 +7,22 @@ describe 'User Stories' do
         plane = Plane.new
         expect { airport.land(plane) }.not_to raise_error
     end
+    # As an air traffic controller 
+    # So I can get passengers on the way to their destination 
+    # I want to instruct a plane to take off from an airport and confirm that it is no longer in the airport
+    it 'lets a plane take off so it is no longer at the airport' do
+        airport = Airport.new
+        plane = Plane.new
+        expect { airport.take_off(plane) }.not_to raise_error
+    end
 end
+
+# describe 'User Story 2' do
+#     # As an air traffic controller 
+#     # So I can get passengers on the way to their destination 
+#     # I want to instruct a plane to take off from an airport and confirm that it is no longer in the airport
+#     it 'lets a plane take off so it is no longer at the airport'
+#     airport = Airport.new
+#     plane = Plane.new
+#     expect { airport.take_off(plane) }.not_to raise_error
+# end
