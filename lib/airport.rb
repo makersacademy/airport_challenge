4e298fc991e282bd23537a66ed1f46aad48868c7
Robert Dupplaw <dupplawrobert@gmail.com>
2021-05-01 18:47:27 +0100

    refactored conditions for #land and #launch

--- lib/airport.rb
+++ lib/airport.rb
@@ -6,7 +6,7 @@ class Airport
   end
 
   def land(plane)
-    if @weather.now == 'sunny' && @planes.length < @capacity && !@planes.include?(plane) && !plane.landed?
+    if plane_can_land?(plane)
       @planes << plane
       true
     else
@@ -15,11 +15,37 @@ class Airport
   end
 
   def launch(plane)
-    if @weather.now == 'sunny' && @planes.include?(plane)
+    if plane_can_launch?(plane)
       @planes.delete(plane)
       true
     else
       false
     end
+  end
+
+  private
+
+  def plane_can_land?(plane)
+    weather_good? && space? && !plane_here?(plane) && !plane_landed?(plane)
+  end
+
+  def plane_can_launch?(plane)
+    weather_good? && plane_here?(plane)
+  end
+
+  def weather_good?
+    @weather.now == 'sunny'
+  end
+
+  def space?
+    @planes.length < @capacity
+  end
+
+  def plane_here?(plane)
+    @planes.include?(plane)
+  end
+
+  def plane_landed?(plane)
+    plane.landed?
   end
 end
