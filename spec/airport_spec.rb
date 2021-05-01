bae61a59d9a3282ed1e8b2ab718df722cd4a082c
Arif Ebrahim <>
2021-05-01 10:49:15 +0100

    updated user story 1

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,5 +1,9 @@
 require 'airport'
 
 RSpec.describe Airport do
-  
+  describe "#land" do
+    it "should tell a plane to land" do
+      expect(subject).to respond_to(:land).with(1).argument
+    end
+  end
 end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -1,10 +1,6 @@
 require 'plane'
 
 RSpec.describe Plane do
-  describe "#land_at" do
-    it "should tell a plane to land at an airport" do
-      expect(subject).to respond_to(:land_at).with(1).argument
-    end
-  end
+
 
 end
