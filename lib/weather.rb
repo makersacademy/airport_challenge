4f78568e28e1bccdc2c838e49b79b8040ab23e3c
BecaLParker <becaparker@gmail.com>
2021-05-02 10:45:58 +0100

    Refectors so status setting is extracted to a method, rather than replying on intialise

--- lib/weather.rb
+++ lib/weather.rb
@@ -3,11 +3,7 @@ class Weather
   WEATHER_POSSIBLES = ["sunny", "sunny", "stormy"]  
   # more sunnys  than stormys so that only occasionally the status is stormy
 
-  attr_reader :status
-
-  def initialize(status = WEATHER_POSSIBLES.sample)
-    @status = status
+  def status
+    WEATHER_POSSIBLES.sample
   end
-
-  
 end
