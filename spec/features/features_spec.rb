3f1bd11d23f2bf10324a99cff7c4f3646eb3fe17
Gunel Mehdiyeva-Chau <gunel.mehdiyev@gmail.com>
2021-05-02 07:10:47 +0100

    user story 2

--- spec/features/features_spec.rb
+++ spec/features/features_spec.rb
@@ -6,5 +6,14 @@ describe "features" do
     airport = Airport.new
     plane = Plane.new
     expect {airport.land(plane)}.not_to raise_error
-  end 
+  end
+
+# As an air traffic controller 
+# So I can get passengers on the way to their destination 
+# I want to instruct a plane to take off from an airport and confirm that it is no longer in the airport
+ it 'instructs a plane to take off so passengers get to a destination' do
+    airport = Airport.new
+    plane = Plane.new
+    expect {airport.take_off(plane)}.not_to raise_error
+ end 
 end
