09d4176b94f25f70e44502df6cd555a0cead7dcd
BecaLParker <becaparker@gmail.com>
2021-05-02 13:52:50 +0100

    Tests and implements for 'Airport knows it starts with 0 planes on the ground'

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,14 +1,15 @@
 class Airport
   DEFAULT_CAPACITY = 4
   attr_reader :capacity
+  attr_reader :planes_in_port
 
   def initialize(capacity = DEFAULT_CAPACITY)
     @capacity = capacity
+    @planes_in_port = []
   end
 
   def capacity_status
-    planes_in_port = []
-    available = @capacity - planes_in_port.size
+    available = @capacity - @planes_in_port.size
     available <= 0 ? "full" : "there are spaces to land in"
   end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,6 +1,6 @@
 require 'airport'
 
-describe 'Airport' do
+describe Airport do
   it 'instantiates with a default capacity value of 4' do
     default_airport = Airport.new
     expect(default_airport.capacity).to eq 4
@@ -10,4 +10,11 @@ describe 'Airport' do
     sixer_airport = Airport.new(6)
     expect(sixer_airport.capacity).to eq 6
   end
+
+  it 'it starts with 0 planes on the ground' do
+    expect(subject.planes_in_port.size).to eq 0
+  end
+
+  # test to see whether planes on ground increases by 1 when a plane lands
+  # test to see whether planes on ground decreases by 1 when a plane takesoff
 end
