341b49d84e16e09d6bcacab51e497d52b2fb3148
Maria Valero Gonzalez <mariavalerogonzalez@ma376.home>
2021-05-03 19:56:28 +0100

    6 test passing out of 8 needed

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,18 +1,100 @@
 require "./lib/airplane.rb"
 
 class Airport
-  #attr_reader airplane
+  attr_reader :airplanes, :capacity
+  DEFAULT_CAPACITY = 7
 
   def initialize
     @airplanes = []
-    #@capacity = DEFAULT_CAPACITY
+    @capacity = DEFAULT_CAPACITY
+  end
+
+  def random_weather
+    weather = ["stormy", "sunny", "cloudy", "rainy", "windy"]
+    weather[rand(weather.size)]
   end
   
   def land(airplane)
+    #random_weather
+    #p random_weather
+    #fail 'Too stormy to land' if stormy?
+    fail 'The airport is full' if full?
     @airplanes << airplane
   end
   
-  def take_off
+  def take_off(airplane)
+    random_weather
+    p random_weather
+    fail 'The airport is empty' if empty?
+    fail 'Too stormy to takeoff' if stormy?
     @airplanes.pop
   end
+
+  private
+  attr_reader :airplanes
+
+  def full?
+    @airplanes.count >= DEFAULT_CAPACITY
+  end
+
+  def empty?
+    @airplanes.empty?
+  end
+
+  def stormy?
+    random_weather == "stormy"
+  end
 end
+
+=begin
+
+class Airport
+
+  DEFAULT_CAPACITY = 5
+
+  attr_reader :capacity, :air, :planes
+
+  def initialize
+    @planes = []
+    @air = []
+    @capacity = DEFAULT_CAPACITY
+  end
+
+  def random_weather
+    weather = ["stormy", "sunny", "cloudy", "rainy", "windy"]
+    weather[rand(weather.size)]
+  end
+
+  def land(plane)
+    #WEATHER.sample(random: Random.new(1))
+    puts random_weather
+    fail 'Airport full' if full?
+    fail 'Too stormy to land' if stormy?
+
+    @planes << plane
+  end
+
+  def takeoff(plane)
+    puts random_weather
+    fail 'Airport is empty' if empty?
+    fail 'Too stormy to takeoff' if stormy?
+
+    @planes.pop
+    @air << plane
+  end
+
+  def full?
+    @planes.count >= DEFAULT_CAPACITY
+  end
+
+  def empty?
+    @planes.empty?
+  end
+
+  def stormy?
+    random_weather == "stormy"
+  end
+end
+
+
+=end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,7 +2,6 @@ require "./lib/airport.rb"
 require "./lib/airplane.rb"
 
 describe Airport do
-  
   describe '#land' do
     it 'responds to the instruction to land a plane' do
       expect(subject).to respond_to(:land).with(1).argument
@@ -13,7 +12,11 @@ describe Airport do
       #subject.land(airplane)
       expect(subject.land(airplane)).to eq [airplane]
     end
-
+    
+    it 'raises an error when full' do
+      subject.capacity.times { subject.land Airplane.new }
+      expect { subject.land Airplane.new }.to raise_error 'The airport is full'
+    end
   end
 
   describe '#take_off' do
@@ -21,10 +24,67 @@ describe Airport do
       expect(subject).to respond_to(:take_off)
     end
 
-    #it 'instructs the plane #to take_off' do
-      #airplane = Airplane.new
-      #airplane = subject.take_off
-      #expect(bike).to 
+    it 'instructs the plane #to take_off' do
+      airplane = Airplane.new
+      subject.land(airplane)
+      expect(subject.take_off(airplane)).to eq airplane
+    end
+
+    it 'raises an error when there are no planes in land' do
+      expect { subject.take_off Airplane.new }.to raise_error 'The airport is empty'
+    end
+
+    #it 'raises an error when the weather is stormy' do
+    #  expect { subject.take_off(airplane) }.to raise_error 'Too stormy to takeoff'
     #end
   end
+
+  #describe 'empty' do
+  #  it 'raises error if airport is empty' do
+  #    subject.capacity.times { subject.land Airplane.new }
+  #    expect { subject.land Airplane.new }.to raise_error 'Airport is full'
+  #  end
+  #end
+
+  #it 'has a default capacity' do
+  #  expect(subject.capacity).to eq Airport::DEFAULT_CAPACITY
+  #end 
+
 end
+
+=begin
+require './docs/airport.rb'
+
+describe Airport do
+
+  describe '#land_plane' do
+    it 'lands a plane' do
+      plane = Plane.new
+      expect(subject.land(plane)).to eq [plane]
+    end
+  end
+
+  describe '#take_off' do
+    it 'takes off' do
+      plane = Plane.new
+      expect(subject.takeoff(plane)).to eq [plane]
+    end
+  end
+
+  describe 'full' do
+    it 'raises error if airport is full' do
+      subject.capacity.times { subject.land Plane.new }
+      expect { subject.land Plane.new }.to raise_error 'Airport full'
+    end
+  end
+
+  describe 'empty' do
+    it 'raises error if airport is empty' do
+      subject.capacity.times { subject.land Plane.new }
+      expect { subject.land Plane.new }.to raise_error 'Airport full'
+    end
+  end
+end
+
+
+=end
