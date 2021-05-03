dea35bec8a12273482bdde2a7fb27231f8dba19d
Maria Valero Gonzalez <mariavalerogonzalez@ma376.home>
2021-05-03 12:05:04 +0100

    after three successful tests

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,13 +1,18 @@
 require "./lib/airplane.rb"
 
 class Airport
+  #attr_reader airplane
+
+  def initialize
+    @airplanes = []
+    #@capacity = DEFAULT_CAPACITY
+  end
   
-  def land
-    
+  def land(airplane)
+    @airplanes << airplane
   end
   
   def take_off
-    
+    @airplanes.pop
   end
-  
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -5,8 +5,26 @@ describe Airport do
   
   describe '#land' do
     it 'responds to the instruction to land a plane' do
-      expect(subject).to respond_to :land
+      expect(subject).to respond_to(:land).with(1).argument
     end
+
+    it 'instructs the plane to #land' do
+      airplane = Airplane.new
+      #subject.land(airplane)
+      expect(subject.land(airplane)).to eq [airplane]
+    end
+
   end
 
+  describe '#take_off' do
+    it 'responds to the instruction to take off' do
+      expect(subject).to respond_to(:take_off)
+    end
+
+    #it 'instructs the plane #to take_off' do
+      #airplane = Airplane.new
+      #airplane = subject.take_off
+      #expect(bike).to 
+    #end
+  end
 end
