152400d787c59bc7d38c4695471db52613ae6903
Felix <felixcpiercy@gmail.com>
2021-05-03 11:30:53 +0100

    Step 2 complete: Instruct plane to take-off

--- lib/airport.rb
+++ lib/airport.rb
@@ -2,8 +2,12 @@ require_relative 'Plane'
 
 class Airport 
 
-  def land(_plane)
+  def land(plane)
     "Aircraft has landed at airport" 
+  end
+
+  def take_off(plane)
+    "Aircraft has taken off and is no longer in the airport"
   end
 
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -10,7 +10,18 @@ describe Airport do
       airport.land(plane)
       # assert 
       expect(airport.land(plane)).to eq("Aircraft has landed at airport")
-    end 
+    end  
+  end 
+  describe '#take_off' do 
+    it 'instructs plane to take-off from airport' do
+      # arrange 
+      airport = Airport.new
+      plane = Plane.new
+      # act
+      airport.take_off(plane)
+      # assert
+      expect(airport.take_off(plane)).to eq("Aircraft has taken off and is no longer in the airport")
+    end
   end 
   
 end 
