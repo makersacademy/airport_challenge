b815fbdc04f4dcd6bcdc0925bdaf6d40c26cfb5e
BecaLParker <becaparker@gmail.com>
2021-05-07 11:58:33 +0100

    refactors spec

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -5,9 +5,9 @@ require 'air_traffic_control'
 describe Airport do
   let(:plane1){double(:plane)}
   let(:plane2){double(:plane)}
+  
   it 'instantiates with a default capacity value of 4' do
-    default_airport = Airport.new
-    expect(default_airport.capacity).to eq 4
+    expect(subject.capacity).to eq 4
   end
 
   it 'can be instantiated with an overriden capacity value' do
@@ -20,25 +20,19 @@ describe Airport do
   end
 
   it 'increases planes_in_port by 1 when a plane arrives' do
-    airport = Airport.new
-    airport.plane_arrives(plane1)
-    expect(airport.plane_count).to eq 1
+    subject.plane_arrives(plane1)
+    expect(subject.plane_count).to eq 1
   end
 
   it 'decreases planes_in_port by 1 when a plane departs' do
-    airport = Airport.new
-    airport.plane_arrives(plane1)
-    airport.plane_arrives(plane2)
-    airport.plane_departs
-    expect(airport.plane_count).to eq 1
+    subject.plane_arrives(plane1)
+    subject.plane_arrives(plane2)
+    subject.plane_departs
+    expect(subject.plane_count).to eq 1
   end
 
   it 'stores the specific plane instance from atc during landing process' do
-    sunny_weather = double(Weather.new)
-    allow(sunny_weather).to receive(:status) { "sunny" }
-
-    atc = ATC.new(sunny_weather, subject)
-    atc.land_plane(plane1)
+    subject.plane_arrives(plane1)
     expect(subject.planes_in_port).to include plane1
   end
 
