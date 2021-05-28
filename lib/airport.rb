4dabb539a65699a0630868073fc6267dd80e252f
Cynthia Fu <yinnycfu@gmail.com>
2021-05-28 23:16:07 +0100

    planes can take off + edge cases, refactoring incl private methods

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,8 +1,28 @@
 class Airport
   attr_reader :hangar
+  
+  def initialize
+    @hangar = []
+  end
 
   def land(plane)
-    @hangar = plane
+    @hangar << plane
+  end
+
+  def take_off(plane)
+    raise 'That plane is not here.' unless parked?(plane)
+    @hangar.delete_at(parking_spot(plane))
+    @hangar
+  end
+
+  private
+
+  def parked?(plane)
+    @hangar.index(plane) != nil
+  end
+
+  def parking_spot(plane)
+    @hangar.index(plane)
   end
 
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,18 +1,32 @@
 require 'airport'
 
 describe Airport do
+  # mock an instance of the Plane class each time it's needed
+  let(:plane) { instance_double(Plane) }
+
   it { is_expected.to respond_to(:land).with(1).argument }
 
   it 'can land a plane' do
-    # mock an instance of the Plane class before it is created
-    plane = instance_double(Plane)
-    expect(subject.land(plane)).to eq plane
+    expect(subject.land(plane)).to eq [plane]
   end
 
   it 'returns parked planes in the hangar' do
-    plane = instance_double(Plane)
     subject.land(plane)
-    expect(subject.hangar).to eq plane
+    expect(subject.hangar).to eq [plane]
   end
+
+  it { is_expected.to respond_to(:take_off).with(1).argument }
+
+  it 'can allow planes to take off' do
+    # put a plane in the hangar first
+    subject.land(plane)
+    # when the plane takes off, we want to see whats in the hangar? or we want to see error?
+    expect(subject.take_off(plane)).to eq []
+  end
+
+  it 'only allows planes that are at that airport to take off' do
+    expect { subject.take_off(plane) }.to raise_error 'That plane is not here.'
+  end
+
 
 end
