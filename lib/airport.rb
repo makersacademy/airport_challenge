c9ae33361bf246c8d5ba6b905f04a05d82950de7
Cynthia Fu <yinnycfu@gmail.com>
2021-05-29 00:07:59 +0100

    refactoring, linting

--- lib/airport.rb
+++ lib/airport.rb
@@ -7,11 +7,13 @@ class Airport
 
   def land(plane)
     raise 'That plane has already landed.' if parked?(plane)
+
     @hangar << plane
   end
 
   def take_off(plane)
     raise 'That plane is not here.' unless parked?(plane)
+    
     @hangar.delete_at(parking_spot(plane))
     @hangar
   end
