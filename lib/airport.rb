62ed4608c5df4569308fcbdfd7f5986a893e04ba
Sean Phillips <seanphillips@Seans-Air.broadband>
2021-05-29 17:01:00 +0100

    Completing final user story

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,2 +1,40 @@
+require_relative 'plane'
+
 class Airport
+
+  attr_reader :hanger
+  attr_accessor :capacity
+
+  def initialize
+    @hanger = []
+    @capacity = 1
+  end
+ 
+  def land_plane(plane)
+    if weather == 'Sunny' && full? != 'Full'
+      @hanger << plane
+    elsif full? == 'Full'
+      'The airport is full'
+    elsif weather == 'Stormy'
+      'The weather is bad'
+    end
+  end
+
+  def release_plane
+    weather == 'Sunny' ? @hanger.pop : "The weather is bad, you cant take off"
+  end
+
+  def empty?
+    @hanger.empty? ? 'There are no planes in the hanger' : 
+    "There are #{@hanger.count} planes in the Airport"
+  end 
+
+  def full?
+    @hanger.count >= @capacity ? 'Full' : 'Not full'
+  end
+
+  def weather
+    rand(10) <= 9 ? 'Sunny' : 'Stormy'
+  end
+
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,4 +1,69 @@
 require 'airport'
 
-describe 'airport' do
+describe Airport do
+
+  describe '#land_plane' do
+    it 'lands a plane' do
+      allow(subject).to receive(:weather) { 'Sunny' }
+      plane = Plane.new
+      expect(subject.land_plane(plane)).to eq [plane]
+    end
+    it 'Doesnt land a plane if the airport is full' do
+      allow(subject).to receive(:weather) { 'Sunny' }
+      plane = Plane.new
+      subject.land_plane(plane)
+      plane2 = Plane.new
+      expect(subject.land_plane(plane2)).to eq 'The airport is full'
+    end
+    it 'Doesnt land a plane if the weather is bad' do
+      allow(subject).to receive(:weather) { 'Stormy' }
+      plane = Plane.new
+      expect(subject.land_plane(plane)).to eq 'The weather is bad'
+    end
+  end
+
+  describe '#release_plane' do
+    it 'releases a plane' do
+      allow(subject).to receive(:weather) { 'Sunny' }
+      plane = Plane.new
+      subject.land_plane(plane)
+      expect(subject.release_plane).to eq plane
+    end
+    it 'doesnt release a plane in bad weather' do 
+      allow(subject).to receive(:weather) { 'Stormy' }
+      plane = Plane.new
+      subject.land_plane(plane)
+      expect(subject.release_plane).to eq "The weather is bad, you cant take off"
+    end
+  end
+
+  describe '#empty?' do
+    it 'confirms the airport is empty' do
+      plane = Plane.new
+      subject.land_plane(plane)
+      subject.release_plane
+      expect(subject.empty?).to eq 'There are no planes in the hanger'
+    end
+  end
+
+  describe '#full?' do
+    it 'lets us know the airport is full' do
+      plane = Plane.new
+      subject.land_plane(plane)
+      expect(subject.full?).to eq 'Full'
+    end
+  end
+
+  describe '#capacity' do
+    it 'Can be changed if necessary' do
+      subject.capacity = 10
+      expect(subject.capacity).to eq 10
+    end
+  end
+
+  describe '#weather' do
+    it 'returns a random weather event' do
+      expect(subject.weather).to eq 'Sunny' || 'Stormy'
+    end
+  end
 end
