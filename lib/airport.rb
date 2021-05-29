087bf7b22c93fc08470878da7f3a3aa86bfa4e44
Ashley Slaney <ashleyslaney96@gmail.com>
2021-05-29 16:12:21 +0100

    airport class finished

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,13 +1,46 @@
 class Airport
-  attr_reader :airport
+  attr_reader :airport, :message, :capacity
 
-  def initialize
+  DEFAULT_CAPACITY = 50
+
+  def initialize(capacity = DEFAULT_CAPACITY)
     @airport = []
-    p @airport
+    @capacity = capacity
   end
 
   def land
     @airport << 'plane'
-    p @airport
+    @message = 'Plane has landed'
+  end
+
+  def prevent_landing
+    if full?
+      @message = 'Abort landing, airport is full'
+    elsif weather == 'stormy'
+      @message = 'The weather is not suitable for landing'
+    else
+      land
+    end
+  end
+
+  def takeoff
+    @airport.pop
+    @message = 'Plane has taken off and is no longer at the airport'
+  end
+
+  def prevent_takeoff
+    if weather == 'stormy'
+      @message = 'The weather is not suitable for takeoff'
+    else
+      takeoff
+    end
+  end
+
+  def weather
+    rand(50) < 25 ? 'sunny' : 'stormy'
+  end
+
+  def full?
+    @airport.length == @capacity
   end
 end
