466277b123bd8ecc4517abcb16e41d59221f1794
mikey-roberts <mikey_roberts@outlook.com>
2021-05-30 22:27:39 +0100

    call to landed changed with flying inside land method tests tweaked and passing

--- lib/plane.rb
+++ lib/plane.rb
@@ -6,16 +6,17 @@ class Plane
   end
   
   def take_off
-    fail 'Plane is already in-flight' if flying
-    flying = true
+    fail 'Plane is already in-flight' if @flying
+    @flying = true
   end
 
   def land
+    fail 'Plane is already on the ground' if !@flying
     landed
   end
 
   def landed
-    !flying
+    @flying = false
   end
 
 end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -1,7 +1,7 @@
 require 'plane'
 
 describe Plane do
-  let(:plane) { subject }
+  let(:plane) { Plane.new }
   let(:flying) { subject }
 
   it 'Plane flying on initialization' do
@@ -9,21 +9,21 @@ describe Plane do
   end
 
   it { should respond_to(:take_off) }
-  it 'Error if trying to land when plane in-flight' do
+  it 'Error when trying to land when plane in-flight' do
     expect{subject.take_off}.to raise_error 'Plane is already in-flight'
   end 
     
   it 'Plane in-flight' do
-    subject.take_off
-    expect(subject.plane).to flying == true
+    subject.land
+    expect(subject.take_off).to eq true
   end  
   
   it { should respond_to(:land) }
-  it 'Error if trying to land when plane is on the ground' do
+  it 'Error when trying to land when plane is already on the ground' do
+    subject.land
     expect{subject.land}.to raise_error 'Plane is already on the ground'
   end  
   it 'Plane has landed' do
-    subject.land
     expect(subject.land).to eq !flying
   end  
 
