0d23cf49fc0f39b79956f722f790493a8288f967
Camilla <camilla.lima000@gmail.com>
2021-05-30 20:50:56 +0100

    Prevent plane landing when the airport is full.

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,8 +1,16 @@
 class Airport
 
+  def initialize(capacity)
+    @capacity = capacity
+    @planes_landed = 0
+  end
+
   def land(plane)
+    raise "Plane cannot land, airport full" if @planes_landed >= @capacity
+    @planes_landed += 1
   end
 
   def take_off(plane)
+    
   end
 end
--- spec/features/user_stories_spec.rb
+++ spec/features/user_stories_spec.rb
@@ -4,7 +4,7 @@ describe 'User Stories' do
   # I want to instruct a plane to land at an airport
   
   it "if plane can land instruct a plane to land at the airport" do
-    airport = Airport.new
+    airport = Airport.new(10)
     plane = Plane.new
     expect { airport.land(plane) }.not_to raise_error
   end
@@ -13,7 +13,7 @@ describe 'User Stories' do
 # So I can get passengers on the way to their destination 
 # I want to instruct a plane to take off from an airport and confirm that it is no longer in the airport
  it "planes should take of from the airport" do
-  airport = Airport.new
+  airport = Airport.new(10)
   plane = Plane.new
   expect { airport.take_off(plane) }.not_to raise_error
  end
@@ -21,6 +21,15 @@ describe 'User Stories' do
 # To ensure safety 
 # I want to prevent landing when the airport is full 
 
+it " does not let planes land when airport is full" do
+  airport = Airport.new(10)
+  plane = Plane.new
+  10.times do
+    airport.land(plane)
+  end
+    expect { airport.land(plane) }.to raise_error "Plane cannot land, airport full"
+  end
+end
 # As the system designer
 # So that the software can be used for many different airports
 # I would like a default airport capacity that can be overridden as appropriate
@@ -32,4 +41,3 @@ describe 'User Stories' do
 # As an air traffic controller 
 # To ensure safety 
 # I want to prevent landing when weather is stormy 
-end
