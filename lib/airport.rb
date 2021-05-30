7d24656cee66fbd270d275a1c51a61c4d4cf793f
Tarun/Theo Weisz <theo.weisz@gmail.com>
2021-05-30 12:25:55 +0100

    planes can take-off and land from airports

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,5 +1,24 @@
 require_relative 'plane'
 
 class Airport
-  
+  attr_reader = :planes
+
+  def initialize
+    @planes = []
+  end
+
+  def land(plane)
+    @planes << plane
+  end
+
+  def take_off(plane)
+    @planes.delete(plane)
+  end
+
+  def planes_in_airport
+    puts @planes
+  end
+
+
 end
+
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,5 +1,25 @@
 require 'airport'
 
 describe Airport do
+  it 'allows planes to land' do
+    plane = Plane.new
+    expect(subject.land(plane)).to eq [plane]
+  end
+
+  it 'allows planes to take off' do
+    plane = Plane.new
+    subject.land(plane)
+    expect(subject.take_off(plane)).to eq plane
+  end
+
+  it 'confirms plane is not in the airport after take off' do
+    plane = Plane.new
+    subject.land(plane)
+    subject.take_off(plane)
+    expect(subject.planes_in_airport).to eq nil
+  end
+  
+
+
   
 end
