ff59aae160e38870c6c4524c3895743cef819e4d
JooMin Choi <joominchoi23@gmail.com>
2021-05-31 15:42:32 +0100

    updated to return plane when landed

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,5 +1,13 @@
 require "airport"
 
 describe Airport do
+
+  it { is_expected.to be_instance_of Airport}
+
   it { is_expected.to respond_to(:land).with(1).argument }
+
+  it "returns a plane that has landed" do
+    plane = Plane.new
+    expect(subject.land(plane)).to eq(plane)
+  end
 end
