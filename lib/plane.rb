5895457d31b45a960059507a3adbcc8a609dee89
Chuka Okoye <chukanorth@hotmail.co.uk>
2021-05-31 18:42:55 +0100

    Got to the third user story, needs work

--- lib/plane.rb
+++ lib/plane.rb
@@ -1,9 +1,13 @@
+require_relative 'airport'
+
 class Plane
-  def land
-    "Plane landed!"
+  def land(plane)
+    puts @airport << plane
+    land_confirm
   end
 
   def take_off
     "Plane taken off! No longer at airport."
   end
+
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,19 +1,37 @@
 # As an air traffic controller 
-# So I can get passengers on the way to their destination 
-# I want to instruct a plane to take off from an airport and confirm that it is no longer in the airport
+# To ensure safety 
+# I want to prevent landing when the airport is full 
 
 require 'airport.rb'
 require 'plane.rb'
 
 describe Airport do
   it 'instruct plane to land' do
+    airport = Airport.new
     plane = Plane.new
-    expect(plane.land).to eq("Plane landed!")
+    expect(airport.land(plane)).to eq [plane]
+  end
+
+  it 'raises an error if airport is full' do
+    airport = Airport.new
+    expect{ airport.land }.to raise_error 'airport is full'
   end
 
   it 'plane take off and confirm' do
+    airport = Airport.new
+    expect(airport.take_off).to eq("Plane taken off! No longer at airport.")
+  end
+
+  it 'raises an error if no planes to take off' do
+    airport = Airport.new
+    expect{ airport.take_off }.to raise_error "No planes available"
+  end
+
+  it 'airport full' do
+    airport = Airport.new
     plane = Plane.new
-    expect(plane.take_off).to eq("Plane taken off! No longer at airport.")
+    airport.land(plane)
+    expect(airport.airport_full).to eq(true)
   end
 
 end
