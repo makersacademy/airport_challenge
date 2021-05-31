8f01f4adaf05a94f1360b2ffe6b6b9a48fde0eac
Merryn <merrynhr@gmail.com>
2021-05-31 10:40:01 +0100

    added let to airport_spec

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,23 +2,21 @@ require 'airport'
 require 'plane'
 
 describe 'airport' do
+    let(:plane) { Plane.new }
+    let(:airport) { Airport.new }
     #user story 1
     it 'can instruct plane to land safely' do
-        airport = Airport.new
         allow(airport).to receive(:stormy?).and_return(false)
         expect(airport).to respond_to(:land).with(1).arguments
     end
     #user story 2
     it 'can instruct a plane to take off safely' do
-        airport = Airport.new
         allow(airport).to receive(:stormy?).and_return(false)
         expect(airport).to respond_to(:take_off).with(1).arguments
     end
     #user story 3
     describe '#land' do
         it 'raises an error if airport full' do  
-          airport = Airport.new
-          plane = Plane.new
           allow(airport).to receive(:stormy?).and_return(false)
           Airport::DEFAULT_CAPACITY.times do
               current_plane = Plane.new
@@ -30,7 +28,6 @@ describe 'airport' do
      #user story 4
      it 'allows for changes to airport capacity' do
         bigger_airport = Airport.new(50)
-        plane = Plane.new
         allow(bigger_airport).to receive(:stormy?).and_return(false)
         50.times do 
             current_plane = Plane.new
@@ -41,17 +38,13 @@ describe 'airport' do
     end
     #user story 5 and 6
     it 'stops planes from landing when stormy' do
-    airport = Airport.new
-    plane = Plane.new
     allow(airport).to receive(:stormy?).and_return(true)
     expect {airport.land(plane)}.to raise_error('Stormy!')
     expect {airport.take_off(plane)}.to raise_error('Stormy!')
     end
     #Additional unit test 1
     it 'raises error if planes attempt to take off from incorrect airport' do
-        airport = Airport.new
         airport2 = Airport.new
-        plane = Plane.new
         allow(airport).to receive(:stormy?).and_return(false)
         allow(airport2).to receive(:stormy?).and_return(false)
         airport2.land(plane)
