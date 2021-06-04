4c904ad9824e716125dee32a2bb4568d21e620f9
Arran Oakes <arran.oakes@gmail.com>
2021-06-04 12:14:21 +0100

    take_off_plane returns confirmation

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,5 +1,12 @@
-# require './plane.rb'
+require_relative 'plane.rb'
 class Airport
   def land_plane
   end
+
+  def take_off_plane(plane)
+    "Plane has left the airport"
+  end
 end
+
+# plane = Plane.new
+# take_off(plane)
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -6,5 +6,13 @@ describe Airport do
   it 'can land a plane' do
     expect(subject).to respond_to(:land_plane)
   end
-  
+
+  it 'can tell a plane to take off' do
+    expect(subject).to respond_to(:take_off_plane)
+  end
+
+  it 'confirms the plane is no longer at the airport' do
+    plane = Plane.new
+    expect(subject.take_off_plane(plane)).to eq "Plane has left the airport"
+  end
 end
