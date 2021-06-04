047d8f45217ade5f53d2b7a0ee43dd18a6c55344
Juel07 <juel.braganza97@gmail.com>
2021-06-04 23:52:00 +0100

    refactored, replace with let()

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,46 +2,40 @@ require "airport"
 require "plane"
 
 describe Airport do
+  let(:plane) { Plane.new }
+
   it "can instruct plane to land" do
-    plane1 = Plane.new
     expect(subject).to respond_to(:land).with(1).argument
-    expect(subject.land(plane1)).to eq([plane1])
+    # expect(subject.land(plane)).to eq([plane])
   end
   #   it { is_expected.to respond_to :land }
   it "can instruct a plane to take off from airport" do
-    plane1 = Plane.new
-    subject.land(plane1)
+    subject.land(plane)
     expect(subject).to respond_to(:take_off).with(1).argument
   end
 
   it "can confirm that a plane has taken off and is not at airport" do
-    plane1 = Plane.new
-    subject.take_off(plane1)
-    expect(subject.planes).not_to include(plane1) # need to use attr_accessor for planes
-    expect(subject.confirm_takeoff(plane1)).to eq(false)
+    subject.take_off(plane)
+    expect(subject.planes).not_to include(plane) # need to use attr_accessor for planes
+    expect(subject.confirm_takeoff(plane)).to eq(false)
   end
 
   it "can prevent landing when airport is full" do
-    50.times { subject.land(Plane.new) }
-    expect { subject.land(Plane.new) }.to raise_error("Airport is full!")
+    50.times { subject.land(plane) }
+    expect { subject.land(plane) }.to raise_error("Airport is full, cannot land!") if subject.planes.size >= subject.capacity
   end
 
   it "can override default airport capacity" do
     random_capacity = Random.rand(500)
     subject = Airport.new(random_capacity)
     expect(subject.capacity).to eq(random_capacity)
-
-    heathrow = Airport.new(60)
-    60.times { heathrow.land(Plane.new) }
-    expect { heathrow.land(Plane.new) }.to raise_error("Airport is full!")
   end
 
   it "can prevent takeoff when weather is stormy" do
-    plane1 = Plane.new
-    expect { subject.take_off(plane1) }.to raise_error("Weather is stormy, cannot take off!") if subject.weather == "stormy"
+    expect { subject.take_off(plane) }.to raise_error("Weather is stormy, cannot take off!") if subject.weather == "stormy"
   end
+
   it "can prevent landind when weather is stormy" do
-    plane1 = Plane.new
-    expect { subject.land(plane1) }.to raise_error("Weather is stormy, cannot land!") if subject.weather == "stormy"
+    expect { subject.land(plane) }.to raise_error("Weather is stormy, cannot land!") if subject.weather == "stormy"
   end
 end
