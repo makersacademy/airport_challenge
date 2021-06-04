6b46d4f6340279ffebe548ff322cdf99fabba4e1
Juel07 <juel.braganza97@gmail.com>
2021-06-04 15:09:36 +0100

    add departed? method

--- lib/plane.rb
+++ lib/plane.rb
@@ -1,2 +1,7 @@
 class Plane
+  #   attr_accessor :status
+
+  def departed?
+    false
+  end
 end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -1,2 +1,8 @@
 require "airport"
 require "plane"
+
+describe Plane do
+  it "can confirm if departed" do
+    expect(subject.departed?).to eq(false)
+  end
+end
