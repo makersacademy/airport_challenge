07a05560c24e7e899abd3e549be1e3ac3f4a0b92
Leslie <leslie.liszcz@gmail.com>
2021-06-04 13:22:14 +0100

    coverage added for planes already landed

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,6 +1,16 @@
 class Airport
-  def land
-    true
+  def initialize
+    @planes = []
   end
-  
+
+  def land_plane(plane)
+    fail "Plane already in airport" unless in_airport?(plane)
+    
+    @planes << plane
+  end
+
+  def in_airport?(plane)
+    @planes.include?(plane)
+  end
+
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,6 +2,12 @@ require 'airport'
 
 describe Airport do
   it 'can land a plane at the airport' do
-    expect(subject.land).to eq true
+    expect(subject).to respond_to(:land_plane)
   end
+
+  it 'cannot land if plane is already in an airport' do
+    plane = "plane one"
+    expect { subject.land_plane(plane) }.to raise_error "Plane already in airport"
+  end
+  
 end
