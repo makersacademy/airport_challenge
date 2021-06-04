0f0a36e8168f89a2bf344a68117ace42e82e36ac
Alex <alexandrachorbe@MA324.local>
2021-06-04 21:17:39 +0100

    user story 3 - prevent landing when the airport is full

--- lib/traffic_controller.rb
+++ lib/traffic_controller.rb
@@ -1,9 +1,17 @@
    
 class Airport 
+  def initialize (capacity)
+    @capacity = capacity
+    @number = 0 
+    @planes = []
+  end 
 
   def land(plane)
+    raise "Cannot land: airoport full." if @planes.length >= @capacity
+     @planes << plane
   end
-  
+
+
   def take_off(plane)
   end
 end 
--- spec/traffic_controller_spec.rb
+++ spec/traffic_controller_spec.rb
@@ -1,11 +1,18 @@
 
 require "traffic_controller"
 describe Airport do
-  subject(:airport) { described_class.new }
+  subject(:airport) { described_class.new(50) }
   it "instruct a plane to land" do 
     expect(airport).to respond_to(:land).with(1).argument
   end
   it "instruct a plane to take off" do 
     expect(airport).to respond_to(:take_off).with(1).argument
+  end 
+  it "does not allow landing because of capacity" do
+     plane = double :plane
+    50.times do 
+      airport.land(plane)
+    end 
+    expect { airport.land(plane) }.to raise_error "Cannot land: airoport full."
   end
 end
