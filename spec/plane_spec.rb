24e50e2181d09f7de2ba817125d0590e2addabad
Juel07 <juel.braganza97@gmail.com>
2021-06-05 00:15:30 +0100

    test departed? method

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -4,5 +4,6 @@ require "plane"
 describe Plane do
   it "can confirm if departed" do
     expect(subject).to respond_to(:departed?).with(0).argument
+    expect(subject.departed?).to eq(true)
   end
 end
