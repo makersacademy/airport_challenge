6e706ccbb96d9e1b0d3f9fd8773a1ae0ba861138
anitaRead <areadman91@gmail.com>
2021-06-05 15:21:27 +0100

    User story 1 - land plane

---
+++ spec/airport_spec.rb
@@ -0,0 +1,10 @@
+require 'airport'
+
+describe Airport do
+    subject(:airport) { described_class = new }
+    it 'instructs a plane to land' do
+        airport = Airport.new
+        expect(airport).to respond_to(:land).with(1).argument
+    end
+
+end
---
+++ spec/features/user_stories_spec.rb
@@ -0,0 +1,13 @@
+require 'airport'
+
+describe Airport do
+    # As an air traffic controller 
+    # So I can get passengers to a destination 
+    # I want to instruct a plane to land at an airport
+    
+    it 'instructs a plane to land at an airport' do
+        airport = Airport.new
+        plane = Plane.new
+        expect{ airport.land(plane) }.not_to raise_error
+    end
+end
