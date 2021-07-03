8d202e9d0d29e1b6a684849e2d50220ca1a50bf0
Isobel <isobelpeppiatt@yahoo.co.uk>
2021-07-03 11:34:27 +0100

    User story 2

--- lib/plane.rb
+++ lib/plane.rb
@@ -2,4 +2,13 @@ class Plane
 
   def land
   end
+
+  def take_off
+    @left_airport = true
+  end
+
+  def left_airport?
+    @left_airport
+  end
+
 end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -4,4 +4,13 @@ describe Plane do
   it 'responds to land' do
     expect(subject).to respond_to(:land)
   end
+
+  it 'responds to take off' do
+    expect(subject).to respond_to(:take_off)
+  end
+  
+  it 'confirms the plane has left the airport if it has taken off' do
+    subject.take_off
+    expect(subject.left_airport?).to eq true
+  end
 end
