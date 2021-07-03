8c8b3b174d275c36f8b37b9e5cfcc8d4eb932b33
Matt Firmston <mattfirmston@gmail.com>
2021-07-03 17:29:36 +0100

    added feature to instruct plane to take off and confirm what airport it is in, or if it is in flight

--- lib/plane.rb
+++ lib/plane.rb
@@ -1,4 +1,16 @@
 class Plane
+  attr_reader :airport
+  DEFAULT_POSITION = 'In flight'
+  
+  def initialize
+    @airport = DEFAULT_POSITION
+  end
+
   def land(airport)
+    @airport = airport
+  end
+
+  def take_off
+    @airport = DEFAULT_POSITION
   end
 end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -1,7 +1,23 @@
 require 'plane'
 
-describe Plane do
+describe Plane do 
   describe '#land' do
     it { is_expected.to respond_to(:land).with(1).argument }
+  end
+
+  describe '#take_off' do
+    it { is_expected.to respond_to(:take_off) }
+  end
+
+  describe '#airport' do
+    it "should declare which airport it is at" do
+      airport = double(:airport)
+      subject.land(airport)
+      expect(subject.airport).to eq(airport)
+    end
+
+    it "should declare it is in flight if not in airport" do
+      expect(subject.airport).to eq('In flight')
+    end
   end
 end
