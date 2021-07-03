1f40fe4398091508f56a6dc7fd65bccd7553f6a2
Pav Rao <pav0107@gmail.com>
2021-07-03 17:49:13 +0100

    plane to take off and confirm no longer at airport

--- lib/plane.rb
+++ lib/plane.rb
@@ -1,5 +1,16 @@
 class Plane
+
+  attr_accessor :location
+
+  def initialize
+    @location = nil
+  end
+
   def land(airport)
-    airport
+    @location = airport
+  end
+
+  def take_off(airport)
+    @location = "enroute"
   end
 end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -2,4 +2,26 @@ require 'plane'
 
 describe Plane do
   it { is_expected.to respond_to(:land).with(1).argument }
+
+  it { is_expected.to respond_to(:take_off).with(1).argument }
+
+  it { is_expected.to respond_to(:location) }
+
+  describe 'location' do
+    it 'is expected to be in the airport when landed' do
+      plane = Plane.new
+      plane.land("airport1")
+      expect(plane.location).to eq("airport1")
+    end
+
+    it 'is expected to be in the sky after take-off' do
+      plane = Plane.new
+      plane.land("airport1")
+      plane.take_off("airport1")
+      expect(plane.location).not_to eq("airport1")
+    end
+
+  end
+  
+
 end
