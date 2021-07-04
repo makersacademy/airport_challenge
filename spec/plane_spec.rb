185964f38d6e273f38ab2506f0f12fbf4d5368c4
lushcodes <lushcodes@gmail.com>
2021-07-04 20:17:06 +0100

    Cleaned up spec files using let variables

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -2,25 +2,25 @@ require 'plane'
 require 'airport'
 
 describe Plane do
+  let(:airport) { Airport.new }
+  let(:plane_lands) { subject.land(airport) }
+
   it { is_expected.to respond_to(:land).with(1).argument }
   it { is_expected.to respond_to(:take_off).with(1).argument }
 
   it 'should land a plane at an airport' do
-    airport = Airport.new
-    subject.land(airport)
+    plane_lands
     expect(airport.hangar).to eq([subject])
   end
 
   it 'should let a plane take off from an airport' do
-    airport = Airport.new
-    subject.land(airport)
+    plane_lands
     subject.take_off(airport)
     expect(airport.hangar).to eq([subject] - [subject])
   end
   describe '#take_off' do
     it 'should confirm that the plane has left the airport' do
-      airport = Airport.new
-      subject.land(airport)
+      plane_lands
       subject.take_off(airport)
       expect("#{subject} has left the #{airport}")
     end
@@ -28,9 +28,8 @@ describe Plane do
   
   describe '#land' do
     it 'raises an error when plane tries to land in a full airport' do
-      airport = Airport.new
       airport.capacity.times { subject.land(airport) }
-      expect { subject.land(airport) }.to raise_error("Cannot land - Airport full")
+      expect { plane_lands }.to raise_error("Cannot land - Airport full")
     end
   end
 end
