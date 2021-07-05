741a51ffa638b9d37a3d88ede2b78caa1ca7e65a
Brett Karlson <brettjkarlson@gmail.com>
2021-07-05 06:38:08 +0100

    Files after fifth user story

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,29 +2,32 @@ require 'airport'
 
 RSpec.describe Airport do
 
+subject(:plane) { described_class.new }
+
   it { is_expected.to respond_to(:take_off) }
 
   it { is_expected.to respond_to(:land).with(1).argument }
 
-  it 'returns an airborne plane' do
-    plane = Plane.new
-    subject.take_off(plane)
-    expect(subject.take_off(plane)).to eq plane
+  context 'returns status of planes' do
+
+    it 'returns airborne plane' do
+      subject.take_off(plane)
+      expect(subject.take_off(plane)).to eq plane
+    end
+
+    it 'returns landed plane' do
+      subject.land(plane)
+      expect(subject.land(plane)).to eq plane
+    end
   end
 
-  it 'returns a landed plane' do
-    plane = Plane.new
-    subject.land(plane)
-    expect(subject.land(plane)).to eq plane
+  it "raises error if airport full" do
+    subject.capacity.times { subject.land plane }
+    expect { subject.land plane }.to raise_error "Airport is full"
   end
 
   it 'has a default capacity' do
     expect(subject.capacity).to eq Airport::DEFAULT_CAPACITY
   end
  
-  it "raises error if airport full" do
-    subject.capacity.times { subject.land Plane.new }
-    p @hangar
-    expect { subject.land Plane.new }.to raise_error "Airport is full"
-   end
 end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -7,18 +7,18 @@ RSpec.describe Plane do
   it { is_expected.to respond_to(:take_off).with(1).argument }
 
   it { is_expected.to be_airborne }
+  
+  context 'returning messages from plane' do
+    subject(:airport) { described_class.new }
 
-  it 'confirms it is no longer in the airport' do
-    airport = Airport.new
-    subject.take_off(airport)
-    expect(subject.take_off(airport)).to eq( "We have left the airport" )
+    it 'confirms it is no longer in the airport' do
+      subject.take_off(airport)
+      expect(subject.take_off(airport)).to eq('We have left the airport')
+    end
+
+    it 'confirms it has landed' do
+      subject.land(airport)
+      expect(subject.land(airport)).to eq('We have landed')
+    end
   end
-
-  it 'confirms it has landed' do
-    airport = Airport.new
-    subject.land(airport)
-    expect(subject.land(airport)).to eq( "We have landed" )
-  end
-
-
 end
