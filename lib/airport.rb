aef6ad92b7b146533a35f9ab51ccbef587ac5367
ltaylorbrown <lauren2.brown.lb@googlemail.com>
2021-07-23 11:12:20 +0100

    airport full

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,11 +1,15 @@
 class Airport 
 
   def land
-    
+    fail 'Airport full: Cannot land' if full?
   end
 
   def take_off
 
+  end
+
+  def full?
+   true
   end
   
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -10,5 +10,8 @@ describe Airport do
     expect(subject).to respond_to(:take_off)
   end
   
-  
+  it 'plane cannot land if airport full' do
+    expect {subject.land}.to raise_error 'Airport full: Cannot land'
+  end
+
 end
