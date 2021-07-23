29a490e147198f4adb5e63e3d79ef2157d8cf15e
danyaal <danyaalibnasif@gmail.com>
2021-07-23 17:10:55 +0100

    all user stories, no edge cases yet

--- airport.rb
+++ airport.rb
@@ -1,27 +1,44 @@
 class Airport
-  attr_accessor :landed_planes
+  attr_accessor :landed_planes, :default_capacity
 
-  def initialize(capacity)
+  def initialize(default_capacity: 25)
     @landed_planes = []
-    @capacity = capacity
+    @default_capacity = default_capacity
     @number_of_planes_landed = 0
   
   end
 
   def land(plane)
-    if @capacity > @number_of_planes_landed
+    if stormy?
+      p "plane cannot land: weather is stormy"
+    elsif
+      @default_capacity > @number_of_planes_landed
       @number_of_planes_landed += 1
       @landed_planes << plane
     else
-      p "cannot land plane: airport full. "
+      p "plane cannot land: airport full. "
     end
   end
 
   def take_off(plane)
-    @number_of_planes_landed -= 1
-    @landed_planes.delete(plane)
-    p "#{plane} has taken off and is no longer in the airport" 
+    if stormy?
+      p "plane cannot takeoff: weather is stormy"
+    else
+      @number_of_planes_landed -= 1
+      @landed_planes.delete(plane)
+      p "#{plane} has taken off and is no longer in the airport" 
+    end
   end
+
+  def change_capacity(new_capacity)
+    @default_capacity = new_capacity
+  end
+
+  def stormy?
+    rand(1..6) > 4 #if rand is greater than 4 it returns true
+
+  end
+
 
 
   
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -4,15 +4,17 @@ require_relative  "../plane.rb"
 
 describe Airport do
   it "instructs a plane to land at an airport" do
-    airport1 = Airport.new(7)
+    airport1 = Airport.new
     plane1 = Plane.new
     airport1.land(plane1)
+    allow(airport1).to receive(:stormy?).and_return false
     expect(airport1.landed_planes). to eq [plane1]
   end
 
   it "instructs a plane to take off and confirms it is no longer in the airport" do
-    airport1 = Airport.new(10)
+    airport1 = Airport.new
     plane1 = Plane.new
+    allow(airport1).to receive(:stormy?).and_return false
     
     airport1.land(plane1)
     airport1.take_off(plane1)
@@ -20,18 +22,52 @@ describe Airport do
    
     plane2 = Plane.new
     airport1.land(plane2)
+    allow(airport1).to receive(:stormy?).and_return false
     expect(airport1.take_off(plane2)).to eq "#{plane2} has taken off and is no longer in the airport" 
   end
 
   it "prevents landing when airport is full" do
-    airport = Airport.new(3)
+    airport = Airport.new()
     plane = Plane.new
+    allow(airport).to receive(:stormy?).and_return false
     
-    3.times do
+    25.times do
       airport.land(plane)
     end
-    expect(airport.land(plane)).to eq "cannot land plane: airport full. "
+    expect(airport.land(plane)).to eq "plane cannot land: airport full. "
   end
+
+  it "default airport capacity can be overridden" do
+    airport1 = Airport.new #default capacity is 25
+    airport2 = Airport.new #different airport, need to change capacity to 20
+    expect(airport2.change_capacity(20)).to eq 20
+    allow(airport2).to receive(:stormy?).and_return false
+
+    #land 21 planes at airport2 to see if it raises the error
+    plane = Plane.new
+    20.times do
+      airport2.land(plane)
+    end
+    expect(airport2.land(plane)).to eq "plane cannot land: airport full. "
+  end
+
+  it "prevents takeoff when stormy" do
+    airport = Airport.new
+    plane = Plane.new
+    allow(airport).to receive(:stormy?).and_return true  #stub: allows consistent tests
+    expect(airport.take_off(plane)).to eq "plane cannot takeoff: weather is stormy"
+  end
+
+  it "prevents landing when stormy" do
+    airport = Airport.new
+    plane = Plane.new
+    allow(airport).to receive(:stormy?).and_return true  #stub: allows consistent tests
+    expect(airport.land(plane)).to eq "plane cannot land: weather is stormy"
+  end
+
+
+
+
 
 
 end
