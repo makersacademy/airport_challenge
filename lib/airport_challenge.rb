084d65820f3ccaa54f6091c01201103726575c47
Roisin <roisinmaguire@MA359.local>
2021-07-23 17:31:03 +0100

    bad weather, bad weather code

--- lib/airport_challenge.rb
+++ lib/airport_challenge.rb
@@ -1,35 +1,36 @@
 class Airport
 
-  attr_reader :landing_strip, :capacity, :DEFAULT_CAPACITY
+  attr_reader :landing_strip, :capacity, :weather, :DEFAULT_CAPACITY
   DEFAULT_CAPACITY = 20
 
   def initialize(capacity = DEFAULT_CAPACITY)
     @landing_strip = []
     @capacity = capacity
+    @weather = self.weather_forecast
   end
 
   def land(flight)
     fail 'Sorry we are full' if full?
-    fail 'Weather warning. Cannot land' if self.weather_forecast == :stormy
+    fail 'Weather warning. Cannot land.' if @weather == :stormy
 
     @landing_strip << flight
   end
 
   def take_off(flight)
-    fail 'Weather warning. Cannot take-off.' if self.weather_forecast == :stormy
+    fail 'Weather warning. Cannot take-off.' if @weather == :stormy
 
     @landing_strip.delete(flight)
     @landing_strip
-  end
-
-  def weather_forecast
-    [:sunny, :sunny, :sunny, :stormy].shuffle.pop
   end
 
   private
 
   def full?
     @landing_strip.count >= @capacity
+  end
+
+  def weather_forecast
+    [:sunny, :sunny, :sunny, :stormy].shuffle.pop
   end
 
 end
--- spec/airport_challenge_spec.rb
+++ spec/airport_challenge_spec.rb
@@ -5,42 +5,48 @@ describe Airport do
   let(:plane) { double :plane }
   let(:plane2) { double :plane2 }
 
-  it { is_expected.to respond_to(:land).with(1).argument }
+  describe '#land' do
+    it { is_expected.to respond_to(:land).with(1).argument }
 
-  it 'allows planes to land in sunny weather' do
-    allow(plane).to receive(:weather_forecast).and_return(:sunny)
-    expect(subject.land(plane)).to eq [plane]
+    it 'allows planes to land in sunny weather' do
+      @weather == :sunny
+      expect(subject.land(plane)).to eq [plane]
+    end
+
+    it 'does not allow planes to land in stormy weather' do
+      @weather == :stormy
+      expect { subject.land(plane) }. to raise_error 'Weather warning. Cannot land.'
+    end
+
+    it 'prevents planes from landing when airport is full' do
+      @weather == :sunny
+      subject.capacity.times { subject.land(plane) }
+      expect { subject.land(plane) }.to raise_error 'Sorry we are full'
+    end
   end
 
-  it 'does not allow planes to land in stormy weather' do
+  describe '#take_off' do
+
+    it 'allows planes to take off from airport' do
+      subject.land(plane)
+      @weather == :sunny
+      expect(subject.take_off(plane)).to eq []
+    end
+
+    it 'prevents take off when weather is stormy' do
+      @weather == :sunny
+      subject.land(plane)
+      @weather == :stormy
+      expect { subject.take_off(plane) }. to raise_error 'Weather warning. Cannot take-off.'
+    end
+
+    it 'keeps remaining planes at airport when plane leaves' do
+      @weather == :sunny
+      subject.land(plane)
+      subject.land(plane2)
+      expect(subject.take_off(plane)).to eq [plane2]
+    end
 
   end
 
-  it 'allows planes to take off from airport' do
-    subject.land(plane)
-    allow(plane).to receive(:weather_forecast).and_return(:sunny)
-    expect(subject.take_off(plane)).to eq []
-  end
-
-  it 'prevents take off when weather is stormy' do
-    allow(plane).to receive(:weather_forecast).and_return(:sunny)
-    subject.land(plane)
-    allow(plane).to receive(:weather_forecast).and_return(:stormy)
-    plane.weather_forecast
-    expect { subject.take_off(plane) }. to raise_error 'Weather warning. Cannot take-off.'
-  end
-
-  it 'keeps remaining planes at airport when plane leaves' do
-    allow(plane).to receive(:weather_forecast).and_return(:sunny)
-    allow(plane2).to receive(:weather_forecast).and_return(:sunny)
-    subject.land(plane)
-    subject.land(plane2)
-
-    expect(subject.take_off(plane)).to eq [plane2]
-  end
-
-  it 'prevents planes from landing when airport is full' do
-    subject.capacity.times { subject.land(plane) }
-    expect { subject.land(plane) }.to raise_error 'Sorry we are full'
-  end
 end
