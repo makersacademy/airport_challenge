3e70e354d2836410d4cfd30f4cad27ac0d0c23ea
danyaal <danyaalibnasif@gmail.com>
2021-07-23 15:15:56 +0100

    user story 4: prevents landing if airport is full

--- airport.rb
+++ airport.rb
@@ -1,20 +1,26 @@
 class Airport
   attr_accessor :landed_planes
 
-  def initialize
-  @landed_planes = []
+  def initialize(capacity)
+    @landed_planes = []
+    @capacity = capacity
+    @number_of_planes_landed = 0
+  
   end
 
   def land(plane)
-  @landed_planes << plane
+    if @capacity > @number_of_planes_landed
+      @number_of_planes_landed += 1
+      @landed_planes << plane
+    else
+      p "cannot land plane: airport full. "
+    end
   end
 
   def take_off(plane)
-    
+    @number_of_planes_landed -= 1
     @landed_planes.delete(plane)
     p "#{plane} has taken off and is no longer in the airport" 
-   
-   
   end
 
 
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,19 +1,36 @@
 require_relative  "../airport.rb"
+require_relative  "../plane.rb"
 
 
 describe Airport do
   it "instructs a plane to land at an airport" do
-    airport1= Airport.new
-    airport1.land("plane1")
-    expect(airport1.landed_planes). to eq ["plane1"]
+    airport1 = Airport.new(7)
+    plane1 = Plane.new
+    airport1.land(plane1)
+    expect(airport1.landed_planes). to eq [plane1]
   end
 
   it "instructs a plane to take off and confirms it is no longer in the airport" do
-    airport1 = Airport.new
-    airport1.land("plane1")
-    airport1.take_off("plane1")
+    airport1 = Airport.new(10)
+    plane1 = Plane.new
+    
+    airport1.land(plane1)
+    airport1.take_off(plane1)
     expect(airport1.landed_planes).to eq []
-    expect(subject.take_off("plane2")).to eq "plane2 has taken off and is no longer in the airport" 
+   
+    plane2 = Plane.new
+    airport1.land(plane2)
+    expect(airport1.take_off(plane2)).to eq "#{plane2} has taken off and is no longer in the airport" 
+  end
+
+  it "prevents landing when airport is full" do
+    airport = Airport.new(3)
+    plane = Plane.new
+    
+    3.times do
+      airport.land(plane)
+    end
+    expect(airport.land(plane)).to eq "cannot land plane: airport full. "
   end
 
 
