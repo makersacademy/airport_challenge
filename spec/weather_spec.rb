042e53ac5467b07670d7f3d4b0f462cb6058601d
Michael Poole <michaelpoole10@gmail.com>
2021-07-24 02:26:58 +0100

    The weather class is written it just needs implimenting into the main script and testing, but thats a tomorrow job :D

---
+++ spec/weather_spec.rb
@@ -0,0 +1,10 @@
+require "weather"
+
+describe "Weather" do
+  describe "#weather" do
+      it "will produce a weather from the WEATHER hash" do
+      weather = Weather.new
+      expect(weather.weather).to(eq("sunny" || "stormy"))
+    end
+  end
+end
