55fb28e75ec9413495069bdcac79d441065eec56
Roisin <roisinmaguire@ma359.home>
2021-07-25 22:02:34 +0100

    weather cracked

--- lib/airport_challenge.rb
+++ lib/airport_challenge.rb
@@ -6,31 +6,30 @@ class Airport
   def initialize(capacity = DEFAULT_CAPACITY)
     @landing_strip = []
     @capacity = capacity
-    @weather = self.weather_forecast
   end
 
   def land(flight)
     fail 'Sorry we are full' if full?
-    fail 'Weather warning. Cannot land.' if @weather == :stormy
+    fail 'Weather warning. Cannot land.' if stormy?
 
     @landing_strip << flight
   end
 
   def take_off(flight)
-    fail 'Weather warning. Cannot take-off.' if @weather == :stormy
+    fail 'Weather warning. Cannot take-off.' if stormy?
 
     @landing_strip.delete(flight)
     @landing_strip
+  end
+
+  def stormy?
+    rand(9) > 7
   end
 
   private
 
   def full?
     @landing_strip.count >= @capacity
-  end
-
-  def weather_forecast
-    [:sunny, :sunny, :sunny, :stormy].shuffle.pop
   end
 
 end
--- spec/airport_challenge_spec.rb
+++ spec/airport_challenge_spec.rb
@@ -9,17 +9,18 @@ describe Airport do
     it { is_expected.to respond_to(:land).with(1).argument }
 
     it 'allows planes to land in sunny weather' do
-      @weather == :sunny
+      allow(subject).to receive(:stormy?).and_return(false)
       expect(subject.land(plane)).to eq [plane]
     end
 
     it 'does not allow planes to land in stormy weather' do
-      @weather == :stormy
+      # @weather == :stormy
+      allow(subject).to receive(:stormy?).and_return(true)
       expect { subject.land(plane) }. to raise_error 'Weather warning. Cannot land.'
     end
 
     it 'prevents planes from landing when airport is full' do
-      @weather == :sunny
+      allow(subject).to receive(:stormy?).and_return(false)
       subject.capacity.times { subject.land(plane) }
       expect { subject.land(plane) }.to raise_error 'Sorry we are full'
     end
@@ -28,22 +29,24 @@ describe Airport do
   describe '#take_off' do
 
     it 'allows planes to take off from airport' do
+      allow(subject).to receive(:stormy?).and_return(false)
       subject.land(plane)
-      @weather == :sunny
       expect(subject.take_off(plane)).to eq []
     end
 
     it 'prevents take off when weather is stormy' do
-      @weather == :sunny
+      allow(subject).to receive(:stormy?).and_return(false)
       subject.land(plane)
-      @weather == :stormy
+      allow(subject).to receive(:stormy?).and_return(true)
+      # @weather == :stormy
       expect { subject.take_off(plane) }. to raise_error 'Weather warning. Cannot take-off.'
     end
 
     it 'keeps remaining planes at airport when plane leaves' do
-      @weather == :sunny
+      allow(subject).to receive(:stormy?).and_return(false)
       subject.land(plane)
       subject.land(plane2)
+      allow(subject).to receive(:stormy?).and_return(false)
       expect(subject.take_off(plane)).to eq [plane2]
     end
 
