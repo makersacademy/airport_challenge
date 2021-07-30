2c65815ef2067892bae7678d72af64fef6d14591
sa-mcquanzie <sa.mcquanzie@gmail.com>
2021-07-30 13:17:50 +0100

    Adding planes array to Airport class

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,7 +1,8 @@
 class Airport
-  attr_accessor :capacity
+  attr_accessor :capacity, :planes
 
   def initialize capacity = 1
     @capacity = capacity >= 0 ? capacity : 1
+    @planes = []
   end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -9,4 +9,6 @@ describe Airport do
   it 'should have a non-negative capacity' do
     expect(@airport.capacity).to be > 0
   end
+
+  it { is_expected.to have_attributes(:planes => []) }
 end
