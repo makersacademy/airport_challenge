8e22765f11d89977b7386742705272c929d62c25
Hotu Nga Morehu Elkington <hotungamorehuelkington@ma388.home>
2021-07-31 15:45:50 +0100

    added guard against storms and max capacity value

--- lib/airport.rb
+++ lib/airport.rb
@@ -9,17 +9,26 @@ class Airport
   end
 
   def land_plane
+    raise "The weather is too stormy to land" if weather == 1
     raise "Airport hanger is full" if full?
-
+    
     @hanger.push(Plane.new)
   end
 
   def plane_take_off
+    raise "The weather is too stormy to take off" if weather == 1
+    
     @hanger.delete_at(-1)
   end
 
+  private
+  
   def full?
     hanger.count >= DEFAULT_CAPACITY
+  end
+
+  def weather
+    rand(1..100_00)
   end
 
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -3,7 +3,10 @@ require 'airport'
 describe Airport do
 
   let (:full_hanger) { DEFAULT_CAPACITY.times { subject.hanger << Plane.new } }
+  let (:half_full_hanger) { 10.times { subject.hanger << Plane.new } }
+
   
+
   it { is_expected.to respond_to(:land_plane) }
   it { is_expected.to respond_to(:plane_take_off) }
 
@@ -20,4 +23,16 @@ describe Airport do
     full_hanger
     expect { subject.land_plane }.to raise_error("Airport hanger is full")
   end
+
+  it "tests to see if default capacity is set" do
+    expect(Airport.new(3).capacity).to eq(3)
+  end
+
+  it "is expected to deny planes to land or take off in stormy weather" do
+    half_full_hanger
+    allow(subject).to receive(:weather) { 1 }
+    expect { subject.land_plane }.to raise_error("The weather is too stormy to land")
+    expect { subject.plane_take_off }.to raise_error("The weather is too stormy to take off")
+  end
+
 end
