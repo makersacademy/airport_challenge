208fc66fd0ea9dffbcb033215aec0dc7c00208ba
Paul Rainsford <paulrainsford69@gmail.co>
2021-08-01 00:57:33 +0100

    Prevent landing if full

--- lib/airport.rb
+++ lib/airport.rb
@@ -3,8 +3,11 @@
 class Airport 
   
   def land(plane)
+    raise "Airport full" if @plane
+    @plane = plane
   end
 
   def take_off(plane)
+    "#{@plane} has taken off"
   end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -4,4 +4,12 @@ describe Airport do
   subject(:airport) { described_class.new }
   it { is_expected.to respond_to(:land).with(1).argument }
   it { is_expected.to respond_to(:take_off).with(1).argument }
+
+
+ describe '#land' do
+   it "raises an error when full" do
+     subject.land (Plane.new)
+     expect { subject.land Plane.new }.to raise_error "Airport full"
+   end
+ end
 end
