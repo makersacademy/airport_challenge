a69e76c6dff9b1a514c33fcfe60ee178e0c948c1
nicola <nicolaccarroll@gmail.com>
2021-07-31 15:35:39 +0100

    raising error when airport full

--- lib/airport.rb
+++ lib/airport.rb
@@ -2,6 +2,7 @@ class Airport
 attr_reader :plane
 
   def land(plane)
+    raise "There is no space at this airport" if @plane
     @plane = plane
   end
 
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -16,6 +16,11 @@ let(:plane) { Plane.new }
       expect(subject.plane).to eq plane
     end
 
+    it 'returns an error when airport is full' do
+      subject.land(plane)
+      expect {subject.land(Plane.new)}.to raise_error("There is no space at this airport")
+    end
+
   end
 
   describe '#take_off' do
