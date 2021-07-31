636cc07846fa8df8803fbfa7967c9d1b1e7dbf28
nicola <nicolaccarroll@gmail.com>
2021-07-31 14:08:51 +0100

    planes can land

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -1,5 +1,16 @@
 require './lib/plane'
 
 describe Plane do
+
   it { is_expected.to be_instance_of(Plane) }
+
+  describe '#land_at' do
+    
+    it 'planes can land' do
+      expect(subject).to respond_to(:land_at)
+    end
+
+  end
+
+
 end
