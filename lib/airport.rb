a2e327d8c3fe173ff16ada1369eb1188de84d7f9
sa-mcquanzie <sa.mcquanzie@gmail.com>
2021-07-31 15:13:52 +0100

    Edited for bracket and quotation consistency across files

--- lib/airport.rb
+++ lib/airport.rb
@@ -9,31 +9,31 @@ class Airport
     @planes = planes
   end
 
-  def receive(plane)
-    clear_for_landing(plane)
+  def receive plane
+    clear_for_landing plane
     plane.land
     @planes << plane
   end
 
-  def release(plane)
-    clear_for_takeoff(plane)
+  def release plane
+    clear_for_takeoff plane
     plane.takeoff
     @planes.delete plane
   end
 
   private
 
-  def clear_for_landing(plane)
-    raise "Only planes are allowed to land" unless plane.class == Plane
-    raise "Plane is not in flight" unless plane.in_flight?
-    raise "Unsafe landing conditions" unless safe?
-    raise "Airport at capacity" if full?
+  def clear_for_landing plane
+    raise 'Only planes are allowed to land' unless plane.class == Plane
+    raise 'Plane is not in flight' unless plane.in_flight?
+    raise 'Unsafe landing conditions' unless safe?
+    raise 'Airport at capacity' if full?
   end
 
-  def clear_for_takeoff(plane)
-    raise "Plane is already in flight" if plane.in_flight?
-    raise "Plane not at airport" unless @planes.include? plane
-    raise "Unsafe takeoff conditions" unless safe?
+  def clear_for_takeoff plane
+    raise 'Plane is already in flight' if plane.in_flight?
+    raise 'Plane not at airport' unless @planes.include? plane
+    raise 'Unsafe takeoff conditions' unless safe?
   end
 
   def full?
