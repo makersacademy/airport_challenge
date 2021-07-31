eb821872692502b369c224cd45ddec9cb975f259
edpackard <edpackard81@gmail.com>
2021-07-31 19:19:36 +0100

    Added code and tests for user story 5 and some further refactoring

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,25 +1,29 @@
 require_relative 'plane'
+require_relative 'weather'
 
 class Airport
 
   DEFAULT_CAPACITY = 10
 
-  attr_reader :hangar, :capacity
+  attr_reader :hangar, :capacity, :weather
 
   def initialize(capacity = DEFAULT_CAPACITY)
     raise "Airport capacity must be a positive integer" if not_positive_integer?(capacity)
 
     @capacity = capacity
     @hangar = Array.new
+    @weather = Weather.new
   end
 
   def land(plane)
     raise "Landing not permitted: airport full!" if full?
-    
+
     hangar << plane
   end
 
   def take_off(plane)
+    raise "Take off not permitted when weather is stormy" if weather.stormy?
+
     hangar.delete(plane)
   end
 
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -13,40 +13,39 @@ describe Airport do
   end
 
   it "stores a plane in hangar after landing" do 
-    subject.land(Plane.new)
+    subject.land(double :plane)
     expect(subject.hangar.size).to eq 1
   end
 
   it "can confirm a plane is at the airport" do
-    plane = Plane.new
+    plane = double :plane
     subject.land(plane)
     expect(subject.contains?(plane)).to be true
   end
 
   it "can confirm a plane, after taking off, is no longer at the airport" do
-    plane = Plane.new
-    subject.land(plane)
+    allow(subject.weather).to receive(:stormy?) { false }
+    plane = double :plane
+    subject.hangar << plane
     subject.take_off(plane)
     expect(subject.contains?(plane)).to be false
   end
 
-  it "can accept 10 planes" do
-    airport = Airport.new
-    10.times { airport.land(Plane.new) }
-    expect(airport.hangar.size).to eq 10
+  it "can accept default capacity number of planes" do
+    subject.capacity.times { subject.land(double :plane) }
+    expect(subject.hangar.size).to eq subject.capacity
   end
 
   it "prevents landing when the airport is full" do
-    airport = Airport.new
-    airport.capacity.times { airport.land(Plane.new) }
-    expect { airport.land(Plane.new) }.to raise_error "Landing not permitted: airport full!"
+    subject.capacity.times { subject.land(double :plane) }
+    expect { subject.land(double :plane) }.to raise_error "Landing not permitted: airport full!"
   end
 
   it "allows landing if a space becomes available" do
-    airport = Airport.new
-    airport.capacity.times { airport.land(Plane.new) }
-    airport.take_off(airport.hangar.last)
-    expect { airport.land(Plane.new) }.not_to raise_error
+    allow(subject.weather).to receive(:stormy?) { false }
+    subject.capacity.times { subject.land(double :plane) }
+    subject.take_off(subject.hangar.last)
+    expect { subject.land(double :plane) }.not_to raise_error
   end
 
   it "can tell user the airport capacity and confirm default capacity" do
@@ -54,14 +53,18 @@ describe Airport do
   end
 
   it "allows user to set custom capacity when instantiating new airport object" do
-    big_airport = Airport.new(100)
-    expect(big_airport.capacity).to eq 100
+    expect(Airport.new(100).capacity).to eq 100
   end
 
   it "accepts only positive integers as capacity" do
-    [0, -3, "1", 4.5, true, []].each do |bad_capacity|
+    [0, -3, "1", 4.5, true, Array.new].each do |bad_capacity|
       expect { Airport.new(bad_capacity) }.to raise_error "Airport capacity must be a positive integer"
     end
   end
-    
+
+  it "prevents take off if weather is stormy" do
+    allow(subject.weather).to receive(:stormy?) { true }
+    expect { subject.take_off(double :plane) }.to raise_error "Take off not permitted when weather is stormy"
+  end
+
 end
