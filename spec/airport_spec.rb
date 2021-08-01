10ca3472abdec167d3b36fdef89bf6b134b81ecd
Yousuf Mohamed <yousuf_mohamed@hotmail.com>
2021-08-01 16:48:30 +0100

    Created tests for #land method

---
+++ spec/airport_spec.rb
@@ -0,0 +1,16 @@
+require "./lib/airport.rb"
+
+describe Airport do
+  let(:plane) { Plane.new }
+  
+  context "#land method" do
+    it "should instruct a plane to land at airport" do
+      subject.land(plane)
+      expect(subject.landed_planes.last).to eq(plane)
+    end
+
+    it "should only let planes land" do
+      expect { subject.land(helicopter) }.to raise_error "This is not a plane"
+    end
+  end
+end
