9bf3e6bb790f9f3127400ec1fcca2113da6fde7c
danroche10 <danielroche10@gmail.com>
2021-08-03 12:28:31 +0100

    weather tests now isolate weather class

--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -2,15 +2,8 @@ require 'weather'
 
 describe Weather do
 
-  it 'provides the weather from a few different options' do
-    my_airport = Airport.new
-    expect(my_airport.send(:weather).send(:weather_today)).to eq(:stormy).or(eq(:sunny))
+  it '#stormy should return a boolean' do
+    expect(subject.stormy?).to eq(true).or eq(false)
   end
 
-  it "returns false if the weather is sunny" do
-
-    my_airport = Airport.new
-    allow(my_airport.send(:weather)).to receive(:weather_today) { :sunny }
-    expect(my_airport.send(:weather).stormy?).to eq false
-  end
 end
