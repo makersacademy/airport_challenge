ca55fb0c452521c1a092c30ee780c0f3ab95f6bb
danroche10 <danielroche10@gmail.com>
2021-08-04 12:36:06 +0100

    test isolate relevant classes

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -26,7 +26,7 @@ describe Airport do
     context "good weather" do
 
       before(:each) do
-        allow(subject.send(:weather)).to receive(:stormy?) { false }
+        allow(subject).to receive(:stormy?).and_return(false)
       end
 
       it "plane that has landed can be found in the airport" do
@@ -41,7 +41,7 @@ describe Airport do
       end
 
       it "prevents landing when airport is full" do
-        5.times { subject.land(plane) }
+        5.times { subject.land(plane)  }
         message = "Airport is at full capacity"
         expect { subject.land(plane) }.to raise_error message
       end
@@ -50,8 +50,11 @@ describe Airport do
 
     context "bad weather" do
 
+      before(:each) do
+        allow(subject).to receive(:stormy?).and_return(true)
+      end
+
       it "prevent landing when weather is stormy" do
-        allow(subject.send(:weather)).to receive(:stormy?) { true }
         message = "Weather is too bad"
         expect { subject.land(plane) }.to raise_error message
       end
@@ -65,7 +68,7 @@ describe Airport do
     context "good weather" do
 
       before(:each) do
-        allow(subject.send(:weather)).to receive(:stormy?) { false }
+        allow(subject).to receive(:stormy?).and_return(false)
       end
 
       it "after plane takes off, it is no longer at the airport" do
@@ -83,7 +86,7 @@ describe Airport do
 
       it "planes can only take off from airport they are in" do
         another_airport = Airport.new
-        allow(another_airport.send(:weather)).to receive(:stormy?) { false }
+        allow(another_airport).to receive(:stormy?).and_return(false)
         subject.send(:planes) << plane
         message = "this plane isn't at the airport"
         expect { another_airport.take_off(plane) }.to raise_error message
@@ -94,7 +97,7 @@ describe Airport do
     context "good weather" do
     
       it "don't let plane take off if weather is stormy" do
-        allow(subject.send(:weather)).to receive(:stormy?) { true }
+        allow(subject).to receive(:stormy?).and_return(true)
         subject.send(:planes) << plane
         message = "Weather is too bad"
         expect { subject.take_off(plane) }.to raise_error message
@@ -103,18 +106,22 @@ describe Airport do
 
   end
 
-  # make sure feature test isolates airport class
-  
   describe "#feature test with two airports, three planes and only sunny weather", type: :feature do
 
     let(:plane) { double(:plane) }
+    let(:plane_2) { double(:plane_2) }
+    let(:plane_3) { double(:plane_3) }
 
-    before(:each) do
+    before do
+      allow(plane_2).to receive(:landed).and_return(false)
+      allow(plane_2).to receive(:land).and_return(@landed = true)
+      allow(plane_2).to receive(:take_off).and_return(@landed = false)
+      allow(plane_3).to receive(:landed).and_return(false)
+      allow(plane_3).to receive(:land).and_return(@landed = true)
+      allow(plane_3).to receive(:take_off).and_return(@landed = false)
       @another_airport = Airport.new
-      @plane_2 = Plane.new
-      @plane_3 = Plane.new
-      allow(subject.send(:weather)).to receive(:stormy?) { false }
-      allow(@another_airport.send(:weather)).to receive(:stormy?) { false } 
+      allow(subject).to receive(:stormy?).and_return(false)
+      allow(@another_airport).to receive(:stormy?).and_return(false)
     end
 
     it "planes travel between airports with one left at each" do
@@ -128,17 +135,17 @@ describe Airport do
 
     def the_planes_land
       subject.land(plane)
-      subject.land(@plane_2)
-      @another_airport.land(@plane_3)
+      subject.land(plane_2)
+      @another_airport.land(plane_3)
     end
 
     def one_travels_between_airports
-      subject.take_off(@plane_2)
-      @another_airport.land(@plane_2)
+      subject.take_off(plane_2)
+      @another_airport.land(plane_2)
     end
 
     def this_plane_takes_off_again
-      @another_airport.take_off(@plane_3)
+      @another_airport.take_off(plane_3)
     end
 
     def should_be_only_first_plane_at_my_airport
@@ -146,11 +153,11 @@ describe Airport do
     end
 
     def should_be_only_second_plane_at_another_airport
-      expect(@another_airport.send(:planes).last).to eq(@plane_2)
+      expect(@another_airport.send(:planes).last).to eq(plane_2)
     end
 
     def third_plane_should_not_be_landed
-      expect(@plane_3.landed).to eq(false)
+      expect(plane_3.landed).to eq(false)
     end
 
   end
