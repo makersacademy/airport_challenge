081c57a6ed632c062f9318e8e3a7f8ece159828a
Florence Bain <florence_bain@outlook.com>
2021-08-28 20:54:58 +0100

    raise error when airport is full

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,9 +1,26 @@
+require_relative 'plane'
+
 class Airport 
+
+attr_reader :max_limit
+
+	def initialize(max_limit = 5)
+		@max_limit = max_limit
+		@planes = []
+ end 
 
 	 def take_off
 	 end 
 
-	 def land_plane
+	 def land_plane(plane)
+		fail 'The airport is full' if full?
+		@planes << plane
 	 end 
+	 private
 
+	 attr_reader :planes
+	 
+		def full?
+		 @planes.count >= max_limit
+		end
 end 
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,7 +1,13 @@
 require './lib/airport.rb'
-describe Airport do
-    it { is_expected.to respond_to :land_plane }
 
+describe Airport do
     it { is_expected.to respond_to(:take_off) }
 
+    describe '#land_plane' do
+      it { is_expected.to respond_to :land_plane }
+      it 'raises an error if airport full' do
+         5.times { subject.land_plane Plane.new }
+         expect { subject.land_plane Plane.new }.to raise_error 'The airport is full'
+      end 
+    end
 end 
