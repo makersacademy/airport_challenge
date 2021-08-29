3a9dbc440feaada1c3a1d72d1f890d481ee29ed7
jake <jakebs540@gmail.com>
2021-08-29 23:16:27 +0100

    Weather creator test made, not working corretly

--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -1,5 +1,17 @@
 require 'weather'
 
 describe Weather do
+
   it { is_expected.to be_kind_of(described_class) }
+  
+    it 'Create ramdom weather generator' do
+      expect(subject).to respond_to(:ramdom_weather_generator)
+    end
+
+    it 'Generates ramdom weather, Sunny or Stormy' do
+      weather = Weather.new
+      allow (weather.ramdom_weather_generator).to receive(:rand).and_return(3)
+      expect(ramdom_weather_generator).to eq "Sunny"
+  end
+
 end
