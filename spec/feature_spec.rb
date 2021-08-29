7f51f406ee911447b4d16293cb3a8799cba3c906
jake <jakebs540@gmail.com>
2021-08-29 17:25:48 +0100

    feature test updated

--- spec/feature_spec.rb
+++ spec/feature_spec.rb
@@ -1,7 +1,7 @@
 # require './lib/plane'
 # require './lib/airport'
 
-# airport1 = Airport.new
-# plane1 = Plane.new
-# airport1.landing(plane1)
-# airport1.take_off(plane1)
+# airport = Airport.new
+# plane = Plane.new
+# airport.landing(plane)
+# airport.take_off(plane)
