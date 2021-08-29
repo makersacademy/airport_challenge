63fd6364c9509efe53ffb7da08b558b477c0bf8e
jake <jakebs540@gmail.com>
2021-08-29 10:07:20 +0100

    airport spec refactored, can land plane

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,7 +1,11 @@
 require 'airport'
 
 describe Airport do
-  
-  it { is_expected.to respond_to :land_plane }
 
+  context 'So I can get passengers to a destination' do 
+
+    it 'Checks if the airport can land planes' do
+      expect(subject).to respond_to(:land_plane)
+    end
+  end
 end
