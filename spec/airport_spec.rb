9c1ffd28e3aab6c8078cbb964cd0a12676a6c9f2
jake <jakebs540@gmail.com>
2021-08-29 12:08:15 +0100

    airport spec test pass for plane takeoff with msg

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,10 +2,22 @@ require 'airport'
 
 describe Airport do
 
+  
   context 'So I can get passengers to a destination' do 
 
     it 'Checks if the airport can land planes' do
-      expect(subject).to respond_to(:land_plane)
+      expect(subject).to respond_to(:landing)
     end
   end
+
+  context 'So I can get passengers on the way to their destination' do
+    
+    it 'Instructs a plane to take off and confirm that it is no longer in the airport' do
+      expect(subject).to respond_to(:take_off).with(1).argument
+      plane = Plane.new
+      subject.landing(plane)
+      expect(subject.take_off(plane)).to eq "Plane has taken off"
+    end
+  end
+
 end
