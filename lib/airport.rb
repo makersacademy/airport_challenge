33d0a007a4df433dbd3af560cf03257a2b429156
Adrienne Galloway <adriennegalloway@hotmail.com>
2021-08-29 19:38:37 +0100

    Refactor of User Story 3

--- lib/airport.rb
+++ lib/airport.rb
@@ -5,11 +5,15 @@ class Airport
   end 
 
   def land(plane)
-    raise 'Cannot land plane: airport full' if @planes.length >= @capacity
+    raise 'Cannot land plane: airport full' if full?
     @planes << plane
   end 
 
   def take_off(plane)
   end
-  
+
+  def full?
+    @planes.length >= @capacity
+  end
+
 end 
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -3,36 +3,26 @@ require './lib/plane'
 
 describe Airport do
   subject(:airport) { described_class.new(10) }
+  let(:plane) { double :plane }
 
-it 'Should land the plane at the airport' do
-  expect(airport).to respond_to(:land).with(1).argument
-end
+  describe '#land' do 
+    it 'Should land the plane at the airport' do
+    expect(airport).to respond_to(:land).with(1).argument
+    end
 
-it 'Instructs plane to take off from the airport' do 
-  expect(airport).to respond_to(:take_off).with(1).argument
-end
-
-it 'Does not allow planes to land when the airport is full' do
-  plane = double :plane
-  10.times do 
-    airport.land(plane)
+    context 'when airport is full' do
+    it 'raises an error' do
+      10.times do 
+        airport.land(plane)
+      end 
+      expect { airport.land(plane) }.to raise_error 'Cannot land plane: airport full'
+    end 
   end 
-  expect { airport.land(plane) }.to raise_error 'Cannot land plane: airport full'
-end 
+end
 
-# it 'Should return true when the airport is full' do 
-#   airport = Airport.new
-#   plane = Plane.new 
-#   expect(airport).to respond_to(:full?)
-#   expect(airport.full?).to eq(true)
-# end 
-
-# it 'Has a default capacity' do
-# end
-
-# it 'Prevents takeoff when weather is stormy' do 
-# end 
-
-# it 'Prevents landing when weather is stormy' do
-# end 
+  describe '#take_off' do 
+    it 'instructs plane to take off from the airport' do 
+      expect(airport).to respond_to(:take_off).with(1).argument
+    end
+  end
 end 
