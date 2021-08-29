501881feeedb9387e61c41dddb1b26b4fa2ece35
jake <jakebs540@gmail.com>
2021-08-29 21:11:33 +0100

    feature spec update, differant size airports

--- spec/feature_spec.rb
+++ spec/feature_spec.rb
@@ -1,7 +1,10 @@
-# require './lib/plane'
 # require './lib/airport'
 
 # airport = Airport.new
 # plane = Plane.new
 # airport.landing(plane)
 # airport.take_off(plane)
+
+
+# airport_small = Airport.new(10)
+# airport_big = Airport.new(30)
