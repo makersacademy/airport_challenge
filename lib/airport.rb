590d2dcf17177c7df80511f13e108c96d98e2c2e
Sophie Bonneval <maldorana@gmail.com>
2021-08-29 18:15:33 +0100

    Add random weather

--- lib/airport.rb
+++ lib/airport.rb
@@ -8,13 +8,21 @@ class Airport
   end
 
   def land(plane)
+    weather
     fail "Airport is full" if @planes.size >= @capacity
+    fail "Bad weather" if weather == "Stormy"
 
     @planes << plane
   end
 
   def take_off(plane)
+    weather
+    fail "Bad weather" if weather == "Stormy"
+
     @planes.delete(plane)
   end
 
+  def weather
+    ["Sunny", "Sunny", "Sunny", "Stormy"].sample
+  end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,31 +1,45 @@
 require 'airport'
-require 'plane'
 
 describe Airport do
+  subject(:airport) { described_class.new }
+  let(:plane) { Plane.new }
+
   describe '#land' do
     it 'should land a plane' do
-      plane = Plane.new
+      allow(subject).to receive(:weather) { "Sunny" }
       subject.land(plane)
       expect(subject.planes).to include(plane)
     end
 
     it 'should not land if airport is full' do
+      allow(subject).to receive(:weather) { "Sunny" }
       subject.capacity.times { subject.land(Plane.new) }
       expect { subject.land(Plane.new) }.to raise_error("Airport is full")
     end
 
     it 'should have a default capacity that can be overwritten' do
       airport = Airport.new(2)
+      allow(airport).to receive(:weather) { "Sunny" }
       airport.capacity.times { airport.land(Plane.new) }
       expect { airport.land(Plane.new) }.to raise_error("Airport is full")
+    end
+
+    it 'should prevent landing when weather is stormy' do
+      allow(subject).to receive(:weather) { "Stormy" }
+      expect { subject.land(plane) }.to raise_error("Bad weather")
     end
   end
 
   describe '#take_off' do
     it 'should make a plane take off' do
-      plane = Plane.new
+      allow(subject).to receive(:weather) { "Sunny" }
       subject.take_off(plane)
       expect(subject.planes).not_to include(plane)
+    end
+
+    it 'should prevent taking off when weather is stormy' do
+      allow(subject).to receive(:weather) { "Stormy" }
+      expect { subject.take_off(plane) }.to raise_error("Bad weather")
     end
   end
 
