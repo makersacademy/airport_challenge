1fe9285bc8641ad85d751086b9b9bc6d9b3da6ab
Ifrah Hussein <Ifrahhssn@outlook.com>
2021-08-30 09:17:57 +0100

    Created array to store planes

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,4 +1,10 @@
 class Airport
+  attr_reader :planes
+  
+  def initialize
+    @planes = []
+  end
+  
   def land_plane
   end
 end
--- spec/air.rb
+++ spec/air.rb
@@ -6,5 +6,8 @@ describe Airport do
       Airport.new
       expect(subject).to respond_to(:land_plane)
     end  
+    it 'checks whether aiport can store planes' do
+      expect(subject.planes).to be_instance_of(Array)
+    end
   end
 end 
