6b01c50b956f0f2b48457b391516b8387c881a90
Osneil Drakes <osneil-drakes@hotmail.com>
2021-08-30 23:55:58 +0100

    updating landing plane method and test

--- Challenge/lib/airport.rb
+++ Challenge/lib/airport.rb
@@ -1,7 +1,26 @@
 
 class Airport
 
+attr_reader :planes
+
+def initialize
+@planes = []
+end
+
+
 def land_plane(plane)
+
+  @planes << plane
+
+end
+
+end
+
+
+class Plane
+
+def initialize 
+
 end
 
 end
--- Challenge/spec/airport_spec.rb
+++ Challenge/spec/airport_spec.rb
@@ -4,6 +4,17 @@ require 'airport'
 describe Airport do 
 
   it {is_expected.to respond_to(:land_plane).with(1).argument}
+  let (:plane) {double :plane}
+
+  describe "#land plane" do 
+    it "should land a plane" do 
+    subject.land_plane(plane)
+    expect(subject.planes).to include plane
+    end
+  end 
+
+  
+
 
 
 end 
