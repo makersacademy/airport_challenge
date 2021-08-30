8f0b8e4761dcaaddaa97b7d0dbc4f15d0162b9e9
Florence Bain <florence_bain@outlook.com>
2021-08-30 10:23:50 +0100

    refactor initialization test

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,21 +2,24 @@ require './lib/airport.rb'
 
 describe Airport do
     it { is_expected.to respond_to(:take_off) }
- 
-    it 'should have a default capacity' do
-        expect(subject.default_capacity).to eq Airport::DEFAULT_CAPACITY
+
+ describe '#initialization' do
+  subject { Airport.new }
+  let(:plane) { Plane.new }
+  it 'defaults the capacity for airport' do
+    described_class::DEFAULT_CAPACITY.times do
+      subject.land_plane(plane)
     end 
+    expect { subject.land_plane(plane) }.to raise_error 'The airport is full'
+  end 
+end
 
-    describe '#land_plane' do
-      it { is_expected.to respond_to :land_plane }
-      it 'raises an error if airport full' do
-         5.times { subject.land_plane Plane.new }
-         expect { subject.land_plane Plane.new }.to raise_error 'The airport is full'
-      end 
-    end
+ describe '#land_plane' do
+    it { is_expected.to respond_to :land_plane }  
+    it 'raises an error if airport full' do
+        5.times { subject.land_plane Plane.new }
+        expect { subject.land_plane Plane.new }.to raise_error 'The airport is full'
+    end 
+ end
+ 
 end 
-
-
-# As the system designer
-# So that the software can be used for many different airports
-# I would like a default airport capacity that can be overridden as appropriate
