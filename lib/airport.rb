c4ea927af92993808ed2c38864ab18c94e61c3ca
LauraMaddaluno <lauralondon.4@gmail.com>
2021-08-30 18:50:19 +0100

    Add test and code to prevent landing when the airport is full

--- lib/airport.rb
+++ lib/airport.rb
@@ -12,4 +12,11 @@ class Airport
     @hangar.pop 
     return 'Plane no longer in the airport' 
   end
+  def prevent_landing(plane)
+    fail "Sorry, the airport is full" if full?
+    @hangar << plane
+  end
+  def full?
+    @hangar.size >= 1
+  end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -3,6 +3,7 @@ require_relative '../lib/airport'
 describe Airport do 
   it { is_expected.to respond_to(:land).with(1).argument}
   it { is_expected.to respond_to(:take_off).with(1).argument}
+  it { is_expected.to respond_to(:prevent_landing).with(1).argument}
   describe "aircraft landing" do 
     it 'plane can land at airport' do
       plane = Plane.new
@@ -15,4 +16,12 @@ describe Airport do
       expect(subject.take_off(plane)).to eq 'Plane no longer in the airport' 
     end
   end
+  describe "prevent landing" do 
+    it "plane can't land if hangar is full" do
+      plane = Plane.new
+      1.times {subject.land(plane)}
+      expect{subject.prevent_landing(plane)}.to raise_error "Sorry, the airport is full"
+    end
+  end
+
 end 
