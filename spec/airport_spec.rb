12667b295c40883d8c3ac84e2bd91acb963624bb
Leah Pitter <leahpitter95@gmail.com>
2021-08-30 01:47:58 +0000

    refactored files

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -10,8 +10,9 @@ describe 'Airport' do
       expect(plane).to respond_to(:land)
       subject.land(plane)
     end
-    #it 'raises an error if plane, already in airport, tries to land' do
-      #xpect { subject.land }.to raise_error 'Plane has already landed'
-    #end
+    
+    it 'raises an error if plane, already in airport, tries to land' do
+      expect { subject.land }.to raise_error 'Plane has already landed'
+    end
   end
 end
