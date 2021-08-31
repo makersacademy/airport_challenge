d0dfd3f3f65209f56ec1aae1881af3c98c1eb3e6
tj <tomisinjoseph3@gmail.com>
2021-08-31 01:13:16 +0100

    raising an error comments

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -5,20 +5,28 @@ describe Airport do
   #   expect(subject).to respond_to :land_plane
   # end
 
-  # it lands something
-  it 'lands something' do
-    plane = Plane.new
-    expect(subject.land(plane)).to eq plane
+  describe '#land' do
+    # it 'raises an error when airport is full' do
+    #   plane = Plane.new
+    #   expect { subject.land(plane) }.to raise_error 'Airport full'
+    # end
+
+    it 'lands a plane' do
+      plane = Plane.new
+      subject.land(plane)
+      expect(subject.land(plane)).to eq plane
+    end
   end
+  
   # it { is_expected.to respond_to(:land).with(1).argument }
 
   it { is_expected.to respond_to(:plane)}
   
-it 'returns a plane' do
-  plane = Plane.new
-  subject.land(plane)
-  expect(subject.plane).to eq plane
-end
+  it 'returns a plane' do
+    plane = Plane.new
+    subject.land(plane)
+    expect(subject.plane).to eq plane
+  end
 
   it 'responds to take_off_plane' do
     expect(subject).to respond_to :take_off_plane
