3325a46aaa094c3323ab0c7fb6b4b545e1f1aacf
Keldra <KeldraSJ@gmail.com>
2021-09-01 13:20:59 +0100

    Updated land method to prevent landing in stormy weather

--- lib/airport.rb
+++ lib/airport.rb
@@ -5,15 +5,22 @@ class Airport
   attr_reader :plane
 
   def initialize 
+    
     @plane = []
+
   end
 
   def land(plane)
-    fail "Airport is full" if @plane.size >= 20
+    raise "Airport is full" if @plane.size >= 20
+    raise "Plane cannot land due to stormy weather" if stormy?
     @plane << plane
   end
 
   def take_off
+  end
+
+  def stormy?
+    rand(1..3) > 2
   end
 
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,22 +1,42 @@
 require 'airport'
+
 describe Airport do
+  
   subject (:airport) { described_class.new }
 
   describe '#land' do
     it 'instructs planes to land at airport' do
       plane = Plane.new
-      subject.land(plane)
+      airport.land(plane)
       expect(plane).to eq plane
     end
 
-    it 'confirms that airport is full' do
-        20.times { airport.land(Plane.new) }
-        expect { airport.land(Plane.new) }.to raise_error("Airport is full")
-      end
+    it 'planes cannot land when airport is full' do
+      allow(airport).to receive(:stormy?).and_return false
+      20.times { airport.land(Plane.new) }
+      expect { airport.land(Plane.new) }.to raise_error("Airport is full")
     end
 
-  it 'instructs planes to take off from the airport' do
-    expect(subject).to respond_to :take_off
+    it 'prevents plane landing when weather is stormy' do
+      allow(airport).to receive(:stormy?).and_return true
+      expect { airport.land(Plane.new) }.to raise_error("Plane cannot land due to stormy weather")
+    end
+
   end
+
+  describe '#take_off' do 
+    it 'instructs planes to take off from the airport' do
+      expect(airport).to respond_to :take_off
+    end
+  end 
+  
+
+    # it 'confirms plane is no longer at the airport, after take off' do
+    
+    # describe '#take_off' do
+    #   plane = Plane.new
+    #   airport.land(plane)
+    #   expect(airport.take_off).to eq(plane)
+    # end
 
 end 
