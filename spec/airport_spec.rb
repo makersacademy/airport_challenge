b13f829b476c055e78f8ea27fb3d1a8af310eba0
Lewis Jones <lewis.jones1997@protonmail.com>
2021-09-23 17:38:56 +0100

    refactor(airport): refactor test to use BDD

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,8 +1,10 @@
-describe Airport do
-  let(:plane) { double(:plane) }
+require 'airport'
 
+describe Airport do
+  let(:plane) { double :plane }
+  
   describe "#land" do
     subject.land(plane)
-    expect(subject.planes).to include plane
+    expect(subject.show_planes).to include plane
   end
 end
