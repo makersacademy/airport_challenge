b35fb5ca2fc895d30e24595ab0a7d0567c5de7c2
Douglas <douglaskerins@gmail.com>
2021-09-24 12:07:48 +0100

    add code to pass return weather test

--- lib/weather.rb
+++ lib/weather.rb
@@ -1,3 +1,15 @@
 class Weather
-
+  def current
+    weather_number = random_number
+    if weather_number == 1
+      weather = "stormy"
+    else 
+      weather = "sunny"
+    end
+    return weather
+  end
+  
+  def random_number
+    rand(10)
+  end
 end
