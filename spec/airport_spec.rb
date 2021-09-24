cc72f5f0614a247594c17c00bf6a2b9dbbab8a96
Dan Marshall <daniel.o.marshall@gmail.com>
2021-09-24 11:22:24 +0100

    Airport responds to land command

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,3 +1,12 @@
+require './lib/airport.rb'
+
 describe Airport do
-  
+  it 'responds to a command to land' do
+    expect(subject).to respond_to :land
+  end
 end
+
+# 1. 
+# As an air traffic controller 
+# So I can get passengers to a destination 
+# I want to instruct a plane to land at an airport
