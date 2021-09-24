4a149cddde4f306ad4c3733f3babe49540ce691d
Douglas <douglaskerins@gmail.com>
2021-09-24 12:36:02 +0100

    add test to check that planes can land when weather is sunny (Passed with no further code added)

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -18,6 +18,13 @@ describe Airport do
       current_weather = weather.current
       expect{subject.land(plane, current_weather)}.to(raise_error("CANNOT LAND. THE WEATHER IS STORMY."))
     end
+    it "should show plane in airport array after airport tells it to land during sunny weather" do
+      plane = double("plane double")
+      weather = double("weather double", :current => "sunny")
+      current_weather = weather.current
+      subject.land(plane, current_weather)
+      expect(subject.planes.length).to(eq(1))
+    end
   end
 
   describe ".takeoff" do
