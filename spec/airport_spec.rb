32808151808d8e6469210a4762c1e1d0351045c3
Lewis Jones <lewis.jones1997@protonmail.com>
2021-09-24 10:07:45 +0100

    test(airport): add test for instructing a plane to take off

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,16 +1,27 @@
 require 'airport'
 
 describe Airport do
-  let(:plane) { double :plane, "flying=": false, flying: false}
+  let(:landing_plane) { double :plane, "flying=": false, flying: false}
+  let(:takeoff_plane) { double :plane, "flying=": true, flying: true}
+  
   describe "#land" do
     it "lets a plane land at an airport" do
-      subject.land(plane)
-      expect(subject.planes).to include plane  
+      subject.land(landing_plane)
+      expect(subject.planes).to include landing_plane
     end
 
     it "confirms that the plane is no longer flying" do
-      subject.land(plane)
-      expect(plane.flying).to eq false
+      subject.land(landing_plane)
+      expect(landing_plane.flying).to eq false
     end
   end
+
+  describe "#take_off" do
+    it "instructs a plane to take off" do
+      subject.land(takeoff_plane)
+      subject.take_off(takeoff_plane)
+      expect(subject.planes).to_not include takeoff_plane
+    end
+  end
+
 end
