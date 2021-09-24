bfed88a92d3a7eb0e82b82c41b4c0a1a3b7d2e53
Lewis Jones <lewis.jones1997@protonmail.com>
2021-09-24 10:41:16 +0100

    refactor(airport_spec): DRY code

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -4,40 +4,38 @@ describe Airport do
   let(:plane) { double :plane}
   
   describe "#land" do
-    it "lets a plane land at an airport" do
+    before(:each) do
       allow(plane).to receive(:flying=) { :false }
       subject.land(plane)
+    end
+
+    it "lets a plane land at an airport" do
       expect(subject.planes).to include plane
     end
 
     it "confirms that the plane is no longer flying" do
-      allow(plane).to receive(:flying=) { :false }
       allow(plane).to receive(:flying) { false }
-      subject.land(plane)
       expect(plane.flying).to eq false
     end
   end
 
   describe "#take_off" do
-    it "instructs a plane to take off" do
+
+    before(:each) do
       allow(plane).to receive(:flying=) { :false }
       subject.land(plane)
-      
+    end
+
+    it "instructs a plane to take off" do
       expect(subject.take_off(plane)).to eq plane
     end
 
     it "plane is no longer in airport" do
-      allow(plane).to receive(:flying=) { :false }
-      subject.land(plane)
-
       subject.take_off(plane)
       expect(subject.planes).to_not include plane
     end
 
     it "tells the plane that it is no longer landed" do
-      allow(plane).to receive(:flying=) { :false }
-      subject.land(plane)
-
       subject.take_off(plane)
       allow(plane).to receive(:flying=) { :true }
       allow(plane).to receive(:flying) { true }
