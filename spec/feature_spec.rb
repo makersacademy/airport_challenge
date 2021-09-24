2670f4260db279172a678e9a32a43bbae987af77
Lewis Jones <lewis.jones1997@protonmail.com>
2021-09-24 10:55:10 +0100

    test(feature_spec): add feature test for not letting another plane land when at capacity

--- spec/feature_spec.rb
+++ spec/feature_spec.rb
@@ -2,20 +2,36 @@ require 'airport'
 require 'plane'
 
 describe Airport do
-  describe "#land" do
-    before(:each) do
-      @plane = Plane.new
-      subject.land(@plane)
+  context "airport is not full" do
+    describe "#land" do
+      before(:each) do
+        @plane = Plane.new
+        subject.land(@plane)
+      end
+  
+      it "instructs a plane to land at the airport" do
+        expect(subject.planes).to include @plane
+      end
+  
+      it "tells the plane that it is no longer flying" do
+        expect(@plane.flying).to eq false
+      end
     end
 
-    it "instructs a plane to land at the airport" do
-      expect(subject.planes).to include @plane
-    end
-
-    it "tells the plane that it is no longer flying" do
-      expect(@plane.flying).to eq false
+    context "airport is full" do
+      it "doesn't let another plane land if airport is full" do
+        20.times do
+          subject.land(Plane.new)
+        end
+  
+        twenty_first_plane = Plane.new
+  
+        expect { subject.land(twenty_first_plane) }.to raise_error("Airport is full")
+        expect(subject.planes).to_not include twenty_first_plane
+      end
     end
   end
+  
 
   describe "#take_off" do
     before(:each) do
