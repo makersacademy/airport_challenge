d0a3999e7ce61ff9c5d6ed5c4cc490344be237ed
Tom Suckling <tomsuckling@Toms-MacBook-Air.local>
2021-09-24 17:27:01 +0100

    lands a plane in airport

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -5,15 +5,10 @@ describe Airport do
     expect(subject).to respond_to :land
   end  
 
-  it 'lands a plane' do
-    plane = Plane.new
-    expect(subject.land(plane)).to eq (plane)
-  end
-
-  it 'returns a plane ' do 
+  it "should land when given a plane" do
     plane = Plane.new
     subject.land(plane)
-    expect(subject.land(plane).is_a? Plane).to eq true
+    expect(subject.plane_array).to eq([plane])
   end
 
 end
