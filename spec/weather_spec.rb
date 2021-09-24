420cbebcca74b38d9b861b12809370093ab75f02
Douglas <douglaskerins@gmail.com>
2021-09-24 20:56:26 +0100

    add test for random number generator (pass with no added code

--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -6,4 +6,9 @@ describe Weather do
       expect(subject.current).to(eq("sunny" || "stormy"))
     end
   end
+  describe ".random_number" do
+    it "should return number between 0 and 10" do
+      expect(subject.random_number).to(be_within(5).of(5))
+    end
+  end
 end
