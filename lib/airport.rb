726be9ebaf48f3a70954d20ed62aa2bbe0d22725
Douglas <douglaskerins@gmail.com>
2021-09-24 11:26:14 +0100

    add code to pass full? method test

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,12 +1,17 @@
 class Airport
   attr_reader :planes
+  attr_reader :capacity
   def initialize
     @planes = []
+    @capacity = 1
   end
   def land(plane)
     @planes << plane
   end
-  def takeoff()
+  def takeoff
     @planes.shift
+  end
+  def full?
+    @planes.length == @capacity ? true : false
   end
 end
