0a1e460cb4d5ff5001000fdb2008f829849135a4
Douglas <douglaskerins@gmail.com>
2021-09-24 11:12:08 +0100

    add test for takeoff command

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -6,4 +6,10 @@ describe Airport do
     subject.land(plane)
     expect(subject.planes.length).to(eq(1))
   end
+  it "should show plane is no longer in airport array after airport tells it to takeoff" do
+    plane = double("plane double")
+    subject.planes << plane
+    subject.takeoff()
+    expect(subject.planes.length).to(eq(0))
+  end
 end
