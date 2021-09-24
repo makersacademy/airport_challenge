f5ddca2427b58c554825bb8e12fad7a0c28e5b6d
Anmol <bhatianmol566@yahoo.co.uk>
2021-09-24 12:29:36 +0100

    Initial commit. Forgot to commit until reached user story 5, sorry!

---
+++ lib/airport.rb
@@ -0,0 +1,40 @@
+# require_relative 'plane'
+class Airport
+  DEFAULT_CAPACITY = 10
+  attr_reader :planes, :capacity
+
+  def initialize(capacity = DEFAULT_CAPACITY)
+    raise ArgumentError.new("Please enter a positive integer") unless valid_capacity?(capacity)
+    @planes = []
+    @capacity = capacity
+  end
+  
+  def land(plane = nil)
+    plane_check(plane)
+    fail "Plane already in airport" if in_airport?(plane)
+
+    @planes << plane
+  end
+
+  def take_off(plane = nil)
+    plane_check(plane)
+    fail "Plane not in airport" unless in_airport?(plane)
+    
+    @planes.delete(plane)
+    return "Plane has taken off"
+  end
+
+  private
+
+  def in_airport?(plane)
+    @planes.include?(plane)
+  end
+
+  def plane_check(plane)
+    raise ArgumentError.new("Plane not given") unless plane.is_a?(Plane)
+  end
+
+  def valid_capacity?(capacity)
+    capacity.is_a?(Integer) && capacity > 0
+  end
+end
---
+++ spec/airport_spec.rb
@@ -0,0 +1,72 @@
+require 'airport'
+
+RSpec.describe Airport do
+  let(:plane) { instance_double(Plane, :is_a? => Plane) }
+  let(:plane_1) { instance_double(Plane, :is_a? => Plane) }
+
+  describe "#initialize" do
+    it "initializes with a default capacity" do
+      expect(subject.capacity).to eq Airport::DEFAULT_CAPACITY
+    end
+
+    it "sets a custom capacity given" do
+      bigger_airport = Airport.new(20)
+      expect(bigger_airport.capacity).to eq 20
+    end
+
+    it "raises error unless positive integer given" do
+      expect { Airport.new(-1) }.to raise_error(ArgumentError, "Please enter a positive integer")
+      expect { Airport.new("1") }.to raise_error(ArgumentError, "Please enter a positive integer")
+      expect { Airport.new(0) }.to raise_error(ArgumentError, "Please enter a positive integer")
+      expect { Airport.new(3.5) }.to raise_error(ArgumentError, "Please enter a positive integer")
+    end
+  end
+  describe "#land" do
+    it "raises error when anything other than plane given" do
+      expect { subject.land }.to raise_error(ArgumentError, "Plane not given")
+      expect { subject.land(Object.new)}.to raise_error(ArgumentError, "Plane not given")
+    end
+
+    it "lands a plane" do
+      subject.land(plane)
+      expect(subject.planes.include?(plane)).to be true
+    end
+
+    it "doesn't land a plane already in the airport" do
+      subject.land(plane)
+
+      expect {subject.land(plane)}.to raise_error "Plane already in airport"
+    end
+
+
+  end
+
+  describe "#take_off" do
+    it "raises error when anything other than plane given" do
+      expect { subject.take_off }.to raise_error(ArgumentError, "Plane not given")
+      expect { subject.take_off(Object.new) }.to raise_error(ArgumentError, "Plane not given")
+    end
+
+    it "takes off a plane" do
+      subject.land(plane)
+      subject.take_off(plane)
+      expect(subject.planes).to_not include plane
+    end
+
+    it "doesn't take off a plane when not in airport" do
+      expect { subject.take_off(plane) }.to raise_error "Plane not in airport"
+    end
+
+    it "confirms a plane has taken_off" do
+      subject.land(plane)
+      expect(subject.take_off(plane)).to eq "Plane has taken off"
+    end
+
+    it "only takes off the plane given" do
+      subject.land(plane)
+      subject.land(plane_1)
+      subject.take_off(plane)
+      expect(subject.planes).to include plane_1
+    end
+  end
+end
