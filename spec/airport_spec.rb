5416deff2d4e3e951bc74feb47bf8d4323432508
Douglas <douglaskerins@gmail.com>
2021-09-24 11:22:36 +0100

    add test for .full? method

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,15 +1,28 @@
 require "airport"
   
 describe Airport do
-  it "should show plane in airport array after airport tells it to land" do
-    plane = double("plane double")
-    subject.land(plane)
-    expect(subject.planes.length).to(eq(1))
+  describe ".land" do
+    it "should show plane in airport array after airport tells it to land" do
+      plane = double("plane double")
+      subject.land(plane)
+      expect(subject.planes.length).to(eq(1))
+    end
   end
-  it "should show plane is no longer in airport array after airport tells it to takeoff" do
-    plane = double("plane double")
-    subject.planes << plane
-    subject.takeoff()
-    expect(subject.planes.length).to(eq(0))
+
+  describe ".takeoff" do
+    it "should show plane is no longer in airport array after airport tells it to takeoff" do
+      plane = double("plane double")
+      subject.planes << plane
+      subject.takeoff()
+      expect(subject.planes.length).to(eq(0))
+    end
+  end
+
+  describe ".full?" do
+    it "should return true if method is called on a full airport" do
+      plane = double("plane double")
+      subject.planes << plane
+      expect(subject.full?).to(eq(true))
+    end
   end
 end
