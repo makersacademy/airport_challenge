f102842464ab070d004462002835b6a6fec2717a
MattW <mattnwarnock@yahoo.co.uk>
2021-09-24 22:10:28 +0100

    Pass takeoff tests

--- lib/airport.rb
+++ lib/airport.rb
@@ -5,7 +5,16 @@ require 'plane'
 class Airport
   attr_reader :hanger
 
+  def initialize
+    @hanger = []
+  end
+
   def land(plane)
-    @hanger = plane
+    hanger.push(plane)
+  end
+
+  def take_off
+    hanger.pop
+    'the plane is now airborn'
   end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -3,16 +3,35 @@
 require 'airport'
 
 RSpec.describe Airport do
-  it { is_expected.to respond_to(:land) }
+  let(:airport) { described_class.new }
+  let(:plane) { Plane.new }
+
+  it { is_expected.to respond_to(:land).with(1).argument }
+
+  it { is_expected.to respond_to(:take_off) }
 
   describe '#land' do
     it 'lands a plan at airport' do
-      plane = Plane.new
-      airport = described_class.new
-
       airport.land(plane)
 
-      expect(airport.hanger).to eq plane
+      expect(airport.hanger.first).to eq plane
+    end
+  end
+
+  describe '#take_off' do
+    it 'departs plane from airport' do
+      empty_hanger = []
+      airport.land(plane)
+
+      airport.take_off
+
+      expect(airport.hanger).to eq empty_hanger
+    end
+
+    it "returns the plan's status" do
+      airport.land(plane)
+
+      expect(airport.take_off).to include('the plane is now airborn')
     end
   end
 end
