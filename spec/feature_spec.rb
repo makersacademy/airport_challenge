cff06cd155e2fea63fa5b90215ec7ef6d13b9041
Lewis Jones <lewis.jones1997@protonmail.com>
2021-09-24 10:29:21 +0100

    test(feature): add first feature test for landing a plane

---
+++ spec/feature_spec.rb
@@ -0,0 +1,12 @@
+require 'airport'
+
+describe Airport do
+  describe "#land" do
+    it "instructs a plane to land at the airport" do
+      plane = Plane.new
+      subject.land(plane)
+
+      expect(subject.planes).to include plane
+    end
+  end
+end
