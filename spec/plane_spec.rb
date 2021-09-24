c323749b5ebc8d8d42f73908159524b68049612f
Tom <t.p.kellett@gmail.com>
2021-09-24 15:16:35 +0100

    Updates to airport class to generate weather

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -9,13 +9,17 @@ describe Plane do
     subject.land(airport)
     expect(subject.current_airport).to eq airport
   end
+
   it 'plane takes off from airport and confirms it is no longer in airport' do
     subject.take_off
     expect(subject.in_flight).to eq true
     expect(subject.current_airport).to eq nil
   end
+
   it "prevents landing when the airport is full" do
     full_airport = double("full_airport", :landed_planes => [1], :capacity => 1)
     expect { subject.land(full_airport) }.to raise_error
   end
+
+  
 end
