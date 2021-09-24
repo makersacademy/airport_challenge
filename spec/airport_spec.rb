70e99213219518dcbde78507bd0d0bbbb82000fb
twilliams1988 <tom@teow.co.uk>
2021-09-24 13:54:32 +0100

    Add take off planes from airport

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -7,5 +7,18 @@ describe Airport do
     it 'allows airport to store instance of a plane' do
       expect(subject.land(plane)[0]).to eq(plane)
     end
+    it 'allows airport to store multiple planes' do
+      subject.land(plane)
+      subject.land(plane)
+      expect(subject.planes).to_not be_empty
+    end
+  end
+
+  describe '#take_off' do
+    it 'removes plane instance from airport' do
+      subject.land(plane)
+      subject.take_off(plane)
+      expect(subject.planes).to be_empty
+    end
   end
 end
