630faf7cb3c1a579646a60a3308b0f4f8f9cfe19
Troy Snowden <troybradsnowden@gmail.com>
2021-09-24 11:44:45 +0100

    Build airport class using unit tests to maintain TDD

--- lib/airport.rb
+++ lib/airport.rb
@@ -4,6 +4,22 @@ class Airport
 
   def initialize(capacity = 20)
     @CAPACITY = capacity
+    @planes = []
+  end
+
+  def get_planes
+    @planes
+  end
+
+  def add_plane(plane)
+    @planes << plane
+    "Plane added to hangar!"
+  end
+
+  def remove_plane(plane)
+    plane = @planes.delete(plane)
+    "Plane removed from hangar!"  
+
   end
 
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -32,7 +32,7 @@ describe Airport do
     it 'should remove a plane from the planes array when a plane has taken off from the airport' do
       airport.add_plane(plane)
       expect(airport.get_planes.length).to eq(1)
-      airport.remove_plane(plane)
+      expect(airport.remove_plane(plane)).to eq("Plane removed from hangar!")
       expect(airport.get_planes).to eq([])
       expect(airport.get_planes.length).to eq(0)
     end
