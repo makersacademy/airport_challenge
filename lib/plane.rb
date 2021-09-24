5bf48c38ff6042b5ca764dac0030c42556c14ebe
michelle <michelle.watkins@engineering.digital.dwp.gov.uk>
2021-09-24 16:24:48 +0100

    refactoring lib files and spec files to include new methods and improve test coverage

--- lib/plane.rb
+++ lib/plane.rb
@@ -1,13 +1,10 @@
 class Plane
-  # def landing?
-  # #   # default_capacity = 300
-  # #   # airport_capacity = gets.chomp
-  # #   # if airport_capacity > default_capacity
-  # #   #   return "Airport is at capacity. You currently cannot land."
-  # #   if @weather == 'stormy'
-  # #     return "Landing delayed"
-  # #   else 
-  # #     return "Proceed to landing."
-  # #   end    
-  # # end
+  def landing?
+    if @weather == 'stormy'
+      return "Landing delayed"
+    else 
+      @weather == 'sunny'
+      return "Proceed to landing."
+    end    
+  end
 end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -1,17 +1,16 @@
-# require 'plane'
-# require 'airport'
+require 'plane'
+require 'airport'
 
-# describe Plane do
-#   it { is_expected.to respond_to :landing? }
-#   it { is_expected.to respond_to :take_off }
+describe Plane do
+  it { is_expected.to respond_to :landing? }
 
-#   it 'instructs the plane to delay landing if the weather is stormy' do
-#     plane = subject.take_off
-#     expect(plane).to eq "Landing delayed" if @weather == 'stormy'
-#   end
+  it 'instructs the plane to delay landing if the weather is stormy' do
+    plane = subject.landing?
+    expect(plane).to eq "Landing delayed" if @weather == 'stormy'
+  end
 
-#   it 'instructs the plane to land' do
-#     plane = subject.take_off
-#     expect(plane).to eq "Proceed to landing."
-#   end
-# end
+  it 'instructs the plane to land' do
+    plane = subject.landing?
+    expect(plane).to eq "Proceed to landing."
+  end
+end
