fc3cbd3561d3172e64182ff512337a09ad56b768
Patrick Chan <q_p18@hotmail.com>
2021-09-24 11:17:52 +0100

    Complete user story 2

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,5 +1,19 @@
 class Airport
+
+  def initialize
+    @planes = []
+  end
+
   def land
     "land plane"
   end
+
+  def take_off
+    "take off"
+  end
+
+  def not_at_airport?
+    true
+  end
+
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -6,4 +6,11 @@ describe Airport do
     expect(subject.land).to eq "land plane"
   end
 
+  it "instructs a plane to take off from the airport" do
+    expect(subject.take_off).to eq "take off"
+  end
+
+  it "confirms a plane is no longer at the airport" do
+    expect(subject.not_at_airport?).to eq true
+  end
 end
