2631a98307b07903e72c101e4d6433fb05bf9456
Douglas <douglaskerins@gmail.com>
2021-09-24 14:10:03 +0100

    add test for change_position method

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -4,4 +4,8 @@ describe Plane do
   it "position method should respond when called by plane instance" do
     expect(subject).to(respond_to(:position))
   end
+  it "change_position method changes plane's position from air to ground" do
+    subject.change_position
+    expect(subject.position).to(eq("ground"))
+  end
 end
