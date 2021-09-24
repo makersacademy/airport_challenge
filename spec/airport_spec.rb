c5745259fe92e743fc9672347fae0e8a8a59ae92
Troy Snowden <troybradsnowden@gmail.com>
2021-09-24 11:32:45 +0100

    Add unit tests for airport class

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,6 +1,9 @@
 require 'airport'
+require 'plane'
+
 describe Airport do
   subject(:airport) {described_class.new}
+  let(:plane) {double :plane}
   
   describe '#initialize' do
     it 'should set a default capacity if one is not provided' do
@@ -10,6 +13,28 @@ describe Airport do
     it 'should change the capacity if one is provided' do
       airport = described_class.new(30)
       expect(airport.CAPACITY).to eq(30)
+    end
+
+    it 'should initialize an empty array of planes in airport' do
+      expect(airport.get_planes).to eq([])
+    end
+  end
+
+  describe '#add_plane' do
+    it 'should add a plane to the planes array when plane has landed at airport' do
+      expect(airport.add_plane(plane)).to eq("Plane added to hangar!")
+      expect(airport.get_planes).to eq([plane])
+      expect(airport.get_planes.length).to eq(1)
+    end
+  end
+
+  describe '#remove_plane' do
+    it 'should remove a plane from the planes array when a plane has taken off from the airport' do
+      airport.add_plane(plane)
+      expect(airport.get_planes.length).to eq(1)
+      airport.remove_plane(plane)
+      expect(airport.get_planes).to eq([])
+      expect(airport.get_planes.length).to eq(0)
     end
   end
 
