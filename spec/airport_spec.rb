62e147502e10538dfb6724bdbc0fa99f51e439e7
Dan Marshall <daniel.o.marshall@gmail.com>
2021-09-24 13:08:59 +0100

    Refactored spec using let

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,8 +1,13 @@
 require './lib/airport.rb'
 
 describe Airport do
-  it { is_expected.to respond_to(:land).with(1).argument }
-  it { is_expected.to respond_to(:takeoff) }
+  let (:airport) { subject }
+  let (:plane) {Plane.new}
+
+  describe 'Airport should respond to messages' do
+    it { is_expected.to respond_to(:land).with(1).argument }
+    it { is_expected.to respond_to(:takeoff) }
+  end
   
   describe 'hangar' do
     it 'should create a hanger for a new Airport' do
@@ -12,28 +17,23 @@ describe Airport do
   
   describe '#land' do
     it 'should add a new plane to the hangar' do
-      airport = subject
-      plane = Plane.new
       airport.land(plane)
 
       expect(airport.hangar.last).to eq plane
     end
 
     it 'should not land a plane if the Airport is full' do
-      airport = subject
-      plane = Plane.new
       airport.land(plane)
-      
+
       expect { subject.land(Plane.new) }.to raise_error "Airport is full, cannot land plane"
     end
   end
 
   describe '#takeoff' do
     it 'should remove plane from hangar' do
-      airport = subject
-      plane = Plane.new
       airport.land(plane)
       airport.takeoff
+      
       expect(airport.hangar).not_to include plane
     end
   end  
