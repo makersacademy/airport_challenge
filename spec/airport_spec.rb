8ce4dfbb1a3c92489203a0d8f851d0e7f9fd9071
Tom <t.p.kellett@gmail.com>
2021-09-24 14:07:02 +0100

    adding test and feature for airport to have default capacity

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,5 +1,9 @@
 require 'airport'
 
 describe Airport do
-  
+  it "airport should have a default capacity that can be overridden as appropriate" do
+    expect(subject.capacity).to eq 10 
+    expect(Airport.new(20).capacity).to eq 20
+
+  end
 end
