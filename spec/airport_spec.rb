4c65ac3884a44125fbc964f42e35149dbbc5a55b
Jacob Cho <jacobcho@live.com>
2021-09-25 22:10:20 +0100

    set up skeleton of airport_spec

---
+++ spec/airport_spec.rb
@@ -0,0 +1,46 @@
+require 'airport'
+
+describe Airport do
+
+    context 'when weather is clear' do
+    
+        it 'should land a plane'
+
+        it 'should tell plane to take off'
+
+        it 'should confirm a recently airborne plane is no longer at airport'
+
+    end
+
+
+    context 'when weather is stormy' do
+
+        it 'should not land a plane'
+
+        it 'should not tell a plane to take off'
+
+    end
+
+
+
+    context 'when Airport is full' do
+
+        it 'should not land a plane'
+
+    end
+
+    context 'when Airport is empty' do
+
+        it 'should not tell a plane to take off'
+        
+    end
+
+    context 'when system designing' do
+
+        it 'should have a default capacity'
+
+        it 'should change the default capacity'
+        
+    end
+
+end
