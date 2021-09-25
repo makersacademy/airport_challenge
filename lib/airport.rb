a03b63d2ecfcee89e4568b3ae87b48ae91fc8509
Alex Boyling <alexboyling@gmail.com>
2021-09-25 14:28:37 +0100

    Added default airport capacity and override

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,15 +1,23 @@
 class Airport
-  attr_reader :planes
+  attr_reader :capacity, :planes
 
-  def initialize
+  def initialize(capacity = 10)
     @planes = []
+    @capacity = capacity
   end
 
   def land(plane)
+    raise 'Airport full' if full?
     @planes << plane
   end
 
   def takeoff(plane)
     @planes.pop
+  end
+
+  private
+
+  def full?
+    @planes.count >= @capacity
   end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -9,6 +9,10 @@ describe Airport do
     it 'allows a plane to land at an airport' do
       allow(plane).to receive(:land).and_return([plane])
     end
+    it 'prevents landing when the airport is full' do
+      subject.capacity.times { subject.land(plane) }
+      expect { subject.land(plane) }.to raise_error 'Airport full'
+    end
   end
 
   describe '#takeoff' do
@@ -20,11 +24,21 @@ describe Airport do
     it 'allows a plane to takeoff from an airport and confirms that it is no longer in the airport' do
       subject.land(plane)
       subject.takeoff(plane)
-      expect(subject.planes).to eq Array.new
+      expect(subject.planes).not_to include(plane)
     end
   end
 
   describe '#airport status' do
+    describe '#capacity' do
+      it { is_expected.to respond_to :capacity }
+      it 'has a default capacity of 10 when initialized with no argument given' do
+        expect(subject.capacity).to eq 10
+      end
+      it 'has a capacity that can be overidden with an argument' do
+        airport = Airport.new(20)
+        expect(airport.capacity).to eq 20
+      end
+    end
     it { is_expected.to respond_to :planes }
     it 'returns an empty array when there are no planes at the airport' do
       expect(subject.planes).to eq Array.new
