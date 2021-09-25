916faa020276bdb899a5b9f08a7bfba151a26550
Alex Boyling <alexboyling@gmail.com>
2021-09-25 13:56:45 +0100

    Added takeoff feature

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,7 +1,15 @@
 class Airport
+  attr_reader :planes
+
+  def initialize
+    @planes = []
+  end
+
   def land(plane)
+    @planes << plane
   end
 
   def takeoff(plane)
+    @planes.pop
   end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,13 +1,33 @@
 require 'airport'
 
 describe Airport do
+  let(:plane) { double :plane }
+
   describe '#landing' do
     it { is_expected.to respond_to :land }
     it { is_expected.to respond_to(:land).with(1).argument }
+    it 'allows a plane to land at an airport' do
+      allow(plane).to receive(:land).and_return([plane])
+    end
   end
 
   describe '#takeoff' do
     it { is_expected.to respond_to :takeoff }
     it { is_expected.to respond_to(:takeoff).with(1).argument }
+    it 'allows a plane to takeoff from an airport' do
+      allow(plane).to receive(:takeoff)
+    end
+    it 'allows a plane to takeoff from an airport and confirms that it is no longer in the airport' do
+      subject.land(plane)
+      subject.takeoff(plane)
+      expect(subject.planes).to eq Array.new
+    end
+  end
+
+  describe '#airport status' do
+    it { is_expected.to respond_to :planes }
+    it 'returns an empty array when there are no planes at the airport' do
+      expect(subject.planes).to eq Array.new
+    end
   end
 end
