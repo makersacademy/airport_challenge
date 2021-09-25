20e2fb89a5ce5afd830cccb517ef7553dac37494
Sergei Palmer <zigpalmer@gmail.com>
2021-09-25 17:44:44 +0100

    Created forecast method inside Weather class - next step is to create the stub so that rspec gets a consistent forecast to match against

--- lib/weather.rb
+++ lib/weather.rb
@@ -1,4 +1,13 @@
 class Weather
   def forecast
+    wf = rand(1..2)
+    case wf
+    when 1
+      "clear skies"
+    when 2
+      "stormy"
+    end
+    # 1 = good weather
+    # 2 = stormy weather 
   end
 end
