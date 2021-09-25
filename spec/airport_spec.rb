2a86ad11a2b627242fc163c833d68c5e19551709
Sergei Palmer <zigpalmer@gmail.com>
2021-09-25 13:02:23 +0100

    Fixed some formatting using rubocop

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,6 +2,6 @@ require 'airport'
 
 describe Airport do
   it 'responds to the method empty?' do
-  expect(subject).to respond_to(:empty?)
-end
+    expect(subject).to respond_to(:empty?)
+  end
 end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -2,11 +2,15 @@ require 'plane'
 
 describe Plane do
   it 'responds to the takeoff method' do
-  expect(subject).to respond_to(:takeoff)
-end
+    expect(subject).to respond_to(:takeoff)
+  end
 
   it 'responds to the land method' do
     expect(subject).to respond_to(:land)
   end
+end
 
+describe "#takeoff" do
+  it 'lands a plane at the airport' do
+  end
 end
