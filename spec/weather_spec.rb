c650bf48c54fd7502b95ac900be6e7032c86a12d
Sergei Palmer <zigpalmer@gmail.com>
2021-09-25 17:39:09 +0100

    Created unit test for forecast method to return stormy based on a stub I will create

--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -5,3 +5,9 @@ describe Weather do
     expect(subject).to respond_to(:forecast)
   end
 end
+
+describe "#forecast" do
+  it 'generates a weather forecast' do
+  expect(Weather.new.forecast).to eq("stormy")
+end
+end
