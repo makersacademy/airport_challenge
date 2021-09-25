2fe97cc9ed1fe90b924c24f7a5f58605eb048f79
ConorButler <cwdbutler@gmail.com>
2021-09-25 12:53:54 +0100

    Airport accounts for weather

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,18 +1,20 @@
 class Airport
   DEFAULT_CAPACITY = 50
   attr_reader :hangar, :capacity
-  
+
   def initialize(capacity = DEFAULT_CAPACITY)
     @hangar = []
     @capacity = capacity
   end
   
-  def take_off(plane)
+  def take_off(plane, weather=Weather.new)
+    raise "Dangerous weather" if weather.stormy
     @hangar.delete(plane)
   end
 
-  def land(plane)
-    raise "Airport full" unless @hangar.length < @capacity
+  def land(plane, weather=Weather.new)
+    raise "Dangerous weather" if weather.stormy
+    raise "Airport full" if @hangar.length == @capacity
     @hangar << plane
   end
 end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -6,12 +6,16 @@ describe Airport do
     let(:airport) { Airport.new }
     it "lands a plane and stores it" do
       plane = double("Plane")
-      airport.land(plane)
+      weather = double("Safe weather")
+      allow(weather).to receive(:stormy).and_return(false)
+      airport.land(plane, weather)
       expect(airport.hangar.length).to eq(1)
     end
     it "doesn't land when airport is full" do
-      50.times { airport.land(double("Plane")) }
-      expect { airport.land(double("Plane")) }.to raise_error("Airport full")
+      weather = double("Safe weather")
+      allow(weather).to receive(:stormy).and_return(false)
+      50.times { airport.land(double("Plane"), weather) }
+      expect { airport.land(double("Plane"), weather) }.to raise_error("Airport full")
     end
     it "defaults to a capacity of 50" do
       expect(airport.capacity).to eq(50)
@@ -22,20 +26,30 @@ describe Airport do
     let(:airport) { Airport.new }
     it "can tell planes to take off" do
       plane = double("Plane")
-      expect{airport.take_off(plane)}.not_to raise_error
+      weather = double("Safe weather")
+      allow(weather).to receive(:stormy).and_return(false)
+      expect{airport.take_off(plane, weather)}.not_to raise_error
     end
     it "removes planes from the hangar when they take off" do
       plane_1 = (double("Plane"))
       plane_2 = (double("Plane2"))
-      airport.take_off(plane_1)
+      weather = double("Safe weather")
+      allow(weather).to receive(:stormy).and_return(false)
+      airport.land(plane_1, weather)
+      airport.land(plane_2, weather)
+      airport.take_off(plane_1, weather)
       expect(airport.hangar.include?(plane_1)).to eq false
     end
   end
   
-  # context 'weather' do
-  #   let(:airport) { Airport.new }
-  #   it "knows the current weather" do
-  #     expect(airport).to respond_to(:weather)
-  #   end
-  # end
+  context 'weather' do
+    let(:airport) { Airport.new }
+    it "doesn't let planes take off or land if the weather is bad" do
+      plane = double("Plane")
+      weather = double("Stormy weather")
+      allow(weather).to receive(:stormy).and_return(true)
+      expect { airport.land(plane, weather) }.to raise_error("Dangerous weather")
+      expect { airport.take_off(plane, weather) }.to raise_error("Dangerous weather")
+    end
+  end
 end
