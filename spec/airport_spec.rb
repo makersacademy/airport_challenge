7db66138b8b6e083bb6335c3f07432e99101c48f
pinar <pnr_ozbakir@msn.com>
2021-09-25 14:43:13 +0100

    complete the first user story'

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,4 +1,10 @@
-# require 'airport'
+require 'airport'
+
+describe Airport do
+  it 'allows a plane to land' do
+    expect(subject.land).to respond_to(:land).with(1).arguments
+  end
+end
 
 # describe Airport do
 # #   it { is_expected.to respond_to :took_off }
