530e653ff99c01d32b28b337a3dd3a35dda271f9
nejla <nejlalowe@gmail.com>
2021-09-25 15:34:36 +0100

    Second User Story

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -3,4 +3,8 @@ describe Airport do
   it "allows a plane to land at airport" do
     expect(subject).to respond_to(:land).with(1).arguments
   end
+  it "allows a plane to takeoff from the airport" do
+    expect(subject).to respond_to(:takeoff).with(1).arguments
+    expect(Plane.new.departed).to eq true
+  end
 end
