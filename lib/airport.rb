e07eff3a2465a868f2696a8c4851264e0f2530ea
amin-newcastle <muhammad-karim@hotmail.com>
2021-09-25 03:47:49 +0100

    User story 6 - complete, 5 - still to do

--- lib/airport.rb
+++ lib/airport.rb
@@ -6,7 +6,7 @@ class Airport
         @max_cap = 3
     end
 
-    def check_ap_capacity()
+    def check_ap_capacity
         @max_cap
         current_cap = @my_airport.length
         if current_cap + 1 <= @max_cap
@@ -15,13 +15,17 @@ class Airport
     end
 
     def land_plane
-        # Should add plain to my_airport[]
-        if check_ap_capacity() == true
-			@plane = Plane.new
-	        @my_airport.push(@plane)
-	        puts "The plane has landed" + @my_airport.to_s
-	        return @my_airport
-		else
+    # Should add plain to my_airport[]
+        if check_ap_capacity == true
+            if check_weather == "Sunny"
+                @plane = Plane.new
+                @my_airport.push(@plane)
+                puts "The plane has landed" + @my_airport.to_s
+                return @my_airport            
+            else
+                puts "The weather is STORMY!"
+            end
+        else
 			puts "Your airport is full, wait until another plane has taken off"
 		end
     end
@@ -37,11 +41,27 @@ class Airport
 
     def take_off
         # Should remove the plane from my_airport[]
-        @my_airport.shift
-        puts "The plane has taken off and left the airport" + @my_airport.to_s
+        if check_ap_capacity == true
+            if check_weather == "Sunny"
+                @my_airport.shift
+                puts "The plane has taken off and left the airport"
+                return @my_airport 
+            else
+                puts "The weather is STORMY!"
+            end
+        else
+			puts "Your airport is full, wait until another plane has taken off"
+		end
+
+        
     end
 
     def update_max_cap(num)
         @max_cap = num
+    end
+
+    def check_weather
+        type = { 1 => "Sunny", 2 => "Stormy"}
+        return weather = type[rand(1..2)]
     end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -4,6 +4,8 @@ describe Airport do
     it { is_expected.to respond_to :land_plane }
 
     it "Should checks that land_plane adds a Plane object to my_airport[]" do
+        # stub
+        allow(subject).to receive(:rand).and_return(1)
         expect(subject.land_plane).to include(Plane)
     end
 
@@ -15,18 +17,25 @@ describe Airport do
     end
 
     it "Should check a Plane exists" do
+        # stub
+        allow(subject).to receive(:rand).and_return(1)
         subject.land_plane
         expect(subject.search_for_plane).to eq "This plane is in the Airport"
     end
 
-    it { is_expected.to respond_to :check_ap_capacity, :update_max_cap }
+    it { is_expected.to respond_to :check_ap_capacity}
 
     it "Should return true or false when calling check_ap_capacity" do
         expect(subject.check_ap_capacity).to eq(true || false)
     end
 
+    it { is_expected.to respond_to :update_max_cap }
+
     it "Should return max_cap" do
-        num = 3
+        num = 10
         expect(subject.update_max_cap(num)).to eq(num)
     end
+
+    it { is_expected.to respond_to :check_weather }
+
 end
