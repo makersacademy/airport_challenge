ed3531368d8e8c1f45368702abb503b2697ac133
parsa <parsamesg@gmail.com>
2021-09-26 20:52:01 +0100

    add unit test for hangar

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,6 +1,12 @@
 require "airport"
 
 describe Airport do
+  describe "#hangar" do
+    it "creates a hangar to store planes" do
+      expect(subject.hangar).to eq []
+    end
+  end
+
   describe "#land" do
     it { is_expected.to respond_to(:land) }
     it { is_expected.to respond_to(:land).with(1).argument }
