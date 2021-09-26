b553ff9a22573b61f77e3db2dd4ae24f5e0ee790
Rob <rob.eagleheart@gmail.com>
2021-09-26 13:35:11 +0000

    Tests + capacity and landing

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,5 +1,31 @@
+require_relative 'airplane'
+require_relative 'weather'
+
 class AirPort
+
+  DEFAULT_CAPACITY = 20
+
+  attr_accessor :capacity
+  attr_reader :port
+  
+  def initialize(capacity=DEFAULT_CAPACITY)
+    @port = []
+    @capacity = capacity
+  end
+  
+  def land(airplane)
+    fail 'Airport is full' if full?
+    @port << airplane
+  end
+
   def exist?
     true
   end
+  
+  private 
+  
+  def full?
+    @port.count >= @capacity
+  end
+  
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -5,4 +5,23 @@ describe AirPort do
   it "Check if airport exist" do
     expect(port).to exist
   end 
-end
+  
+ # it "Land plane" do
+ #   expect(port.land).to eq()
+ # end
+  describe "#capacity" do
+    it "Airport has a default capacity" do
+      expect(subject.capacity).to eq(AirPort::DEFAULT_CAPACITY)
+    end
+  end
+  
+  subject { AirPort.new }
+  let(:plane) { AirPlane.new }
+  it 'Maximum capacity test' do
+    described_class::DEFAULT_CAPACITY.times do
+      subject.land(plane)
+    end
+    expect{ subject.land(plane) }.to raise_error 'Airport is full'
+  end
+  
+end 
