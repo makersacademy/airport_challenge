69a226f40156d2fc39471d4fde85df6bd47f59db
Alex Nakov <alex.nakov3@gmail.com>
2021-09-26 14:07:28 +0100

    removed testing init from plane_spec. Will ask cohort whether its necessary and how to do it.

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -1,14 +1,6 @@
 require_relative "../lib/plane"
 
 RSpec.describe Plane do
-  describe ".initialize" do
-    it "create a new plane object at the airport which is not flying" do
-      plane = double("Plane", :flying => false)
-      plane = Plane.new
-      expect(plane).to eq plane
-    end
-  end
-
   describe ".change_status" do
     it "changes whether the plane is 'flying' or 'not'" do
       plane = Plane.new
