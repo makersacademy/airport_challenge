19006625c260e885f3fd6c867e54c0d4708b9aaa
ConorButler <cwdbutler@gmail.com>
2021-09-26 14:54:04 +0100

    Test accuracy improvements

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -10,7 +10,7 @@ describe Airport do
     # The same mock Plane object is reused because only the method and capacity is being tested,
     # we are are not concerned about identifying individual planes at this point
 
-    it "lands a plane, updates the airport, and stores it" do
+    it "lands a plane, updates its airport, and stores it" do
       expect(plane).to receive(:update_airport).with(1500)
       airport.land(plane, weather)
 
@@ -19,7 +19,7 @@ describe Airport do
 
     it "doesn't land when airport is full" do
       49.times do # 1 plane is already at airport
-        expect(plane).to receive(:update_airport).with(1500)
+        allow(plane).to receive(:update_airport).with(1500)
         airport.land(plane, weather)
       end
 
@@ -39,6 +39,7 @@ describe Airport do
       }.to raise_error("Plane already at this airport")
       expect { airport3000.land(plane_in_other_airport, weather)
       }.to raise_error("Plane already at another airport")
+      # don't need to tell doubles to receive messages because they don't get past the guard clause
     end
   end
 
@@ -51,10 +52,11 @@ describe Airport do
     # Weather double is reused because we want it to not be a factor in these tests
 
     it "removes planes from the airport when they take off" do
-      allow(plane1).to receive(:update_airport).and_return(0)
+      allow(plane1).to receive(:update_airport).with(2000)
       airport.land(plane1, weather)
       allow(plane1).to receive(:current_airport).and_return(2000)
 
+      allow(plane1).to receive(:update_airport).with(0)
       airport.take_off(plane1, weather)
       allow(plane1).to receive(:current_airport).and_return(0)
 
@@ -66,15 +68,14 @@ describe Airport do
       plane3 = double("Plane3", :current_airport => 0)
       plane4 = double("Plane4", :current_airport => 0)
       
-      allow(plane3).to receive(:update_airport).and_return(0)
-      allow(plane4).to receive(:update_airport).and_return(0)
-      # planes are asked for their current airport before landing
+      allow(plane3).to receive(:update_airport).with(2000)
+      allow(plane4).to receive(:update_airport).with(2000)
       airport.land(plane3, weather)
       airport.land(plane4, weather)
       allow(plane3).to receive(:current_airport).and_return(2000)
       allow(plane4).to receive(:current_airport).and_return(2000)
-      # current airport is updated as they land. airport 2000 now has 2 planes
-      
+
+      allow(plane3).to receive(:update_airport).with(0)
       airport.take_off(plane3, weather)
       allow(plane3).to receive(:current_airport).and_return(0)
       # plane3 is back in sky
@@ -82,6 +83,7 @@ describe Airport do
       expect(airport.confirm_departure(plane3)).to eq("Plane has departed")
       expect(airport.confirm_departure(plane4)).to eq("Plane has not departed")
       
+      allow(plane4).to receive(:update_airport).with(0)
       airport.take_off(plane4, weather)
       allow(plane4).to receive(:current_airport).and_return(0)
       # removing the other plane double from the airport for the next test
@@ -107,7 +109,7 @@ describe Airport do
     let(:good_weather) { double("Weather", :stormy => false) }
 
     it "doesn't let planes take off or land if the weather is stormy" do
-      allow(plane).to receive(:update_airport).and_return(0)
+      allow(plane).to receive(:update_airport).with(6000)
 
       expect { airport.land(plane, bad_weather) }.to raise_error("Dangerous weather")
 
@@ -116,6 +118,7 @@ describe Airport do
       # plane is now in airport
 
       expect { airport.take_off(plane, bad_weather) }.to raise_error("Dangerous weather")
+      # don't need to tell doubles to receive messages because they don't get past the guard clause
     end
   end
 end
