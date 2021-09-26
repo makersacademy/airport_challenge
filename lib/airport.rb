a0bbff31d83557db435120c2d98597508bc28d25
Alex Nakov <alex.nakov3@gmail.com>
2021-09-26 15:00:57 +0100

    airport can only land planes

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,34 +1,17 @@
 require_relative './plane'
 
 class Airport
-  @@weather_possibilities = ["sunny", "sunny", "stormy", "sunny"]
-
-  attr_accessor :weather
+  attr_reader :planes_at_airport, :plane_capacity
   def initialize(plane_capacity = 50)
-    @weather = nil
     @plane_capacity = plane_capacity
     @planes_at_airport = []
   end
 
   def land(plane)
-    @weather = @@weather_possibilities.sample
-    if @planes_at_airport.count < @plane_capacity && weather != "stormy"
-      plane.flying = !plane.flying
+    if @planes_at_airport.count == @plane_capacity
+      raise "Cannot land, airport is full"
+    else
       @planes_at_airport.push(plane)
-    elsif weather == "stormy"
-      raise "The weather is stormy. It's too dangerous to land"
-    else
-      raise "Airport is full"
-    end
-  end
-
-  def take_off(plane)
-    @weather = @@weather_possibilities.sample
-    if weather != "stormy" and !plane.flying
-      plane.flying = !plane.flying
-      @planes_at_airport.delete(plane)
-    else
-      raise "The weather is stormy. It's too dangerous to take off"
     end
   end
 end
