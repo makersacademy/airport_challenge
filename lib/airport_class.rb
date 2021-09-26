a22ed522a5570599e4e03db64fca2931b63a8dde
khalid <khalidmiah@hotmail.co.uk>
2021-09-26 12:22:54 +0100

    cannot land if full and change defaukt airport capacity

--- lib/airport_class.rb
+++ lib/airport_class.rb
@@ -1,14 +1,36 @@
 class Airport 
-    def initialize
-        @plane
-        @landed = []
-    end     
+    def initialize(capacity)
+      @capacity = capacity
+      @stored_plane  = []
+    
+    end  
+     
+
+    def add_capacity
+      @capacity += 1
+    end   
+
+
     def land(plane)
-        @plane = plane
-        @landed << @plane
+      if @stored_plane.length < @capacity
+        return @stored_plane.push(plane)
+      else 
+        return "Cannot land Airport is full"
+      end   
+        
+        
     end   
     def take_off(plane)
-        @plane = plane
-        "Plane has taken off"
+      if @stored_plane.include?(plane)
+        @stored_plane.delete(plane)
+        return "Plane has taken off"
+      end   
+      
     end  
+
+    def weather
+
+    end   
+
+      
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,25 +2,41 @@ require 'airport_class'
 require 'plane_class'
 
 RSpec.describe Airport do 
-  describe 'instruct a plane to land at an airport' do
-    it 'should land a plane at an airport' do
-      airport = Airport.new
-      plane = Plane.new
-      expect(airport.land(plane)).to eq [plane]
-    end   
-    it 'should instruct plane to take off' do
-      airport = Airport.new
-      plane = Plane.new
-      expect(airport.take_off(plane)).to eq "Plane has taken off"
-    end  
-    # it 'should confirm plane no longer in airport' do
-    #   airport = Airport.new
-    #   plane = Plane.new
-    #   airport.take_offplane)
-    #   expect(airport.land(plane)).to eq plane
-    # end  
+  it 'should land a plane at an airport' do
+    airport = Airport.new(2)
+    plane = Plane.new
+    expect(airport.land(plane)).to eq [plane]
+  end   
+  it 'should instruct plane to take off
+  and confirm if not in airport' do
+    airport = Airport.new(2)
+    plane = Plane.new
+    airport.land(plane)
+    expect(airport.take_off(plane)).to eq "Plane has taken off" 
+  end  
+  it 'prevent landing when the airport is full' do
+    airport = Airport.new(2)
+    plane = Plane.new
+    plane2 = Plane.new
+    plane3 = Plane.new
+    first_plane = airport.land(plane)
+    second_plane = airport.land(plane2)
+    third_plane = airport.land(plane3)
+    expect(third_plane).to eq "Cannot land Airport is full"
+  end 
+
+  it 'overide default airport capacity when 
+  appropriate' do
+    airport = Airport.new(2)
+  
+    expect(airport.add_capacity).to eq 3
+   end   
+
+
+
+   
 
       
-  end 
+  
 end      
     
