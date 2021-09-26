7a1f39fb813c1db2d3f642b4ff3ec8f96c57671c
khalid <khalidmiah@hotmail.co.uk>
2021-09-26 21:23:05 +0100

    rubocop applied

--- lib/airport_class.rb
+++ lib/airport_class.rb
@@ -1,50 +1,42 @@
 class Airport 
-    def initialize(capacity)
-      @capacity = capacity
-      @stored_plane  = []
-     
-    end  
-     
+  def initialize(capacity)
+    @capacity = capacity
+    @stored_plane = []
+  end       
 
-    def add_capacity
-      @capacity += 1
-    end   
+  def add_capacity
+    @capacity += 1
+  end   
 
+  def land(plane)
+    @plane = plane
+    weather
 
-    def land(plane)
-      @plane = plane
-      weather
-
-      if @weather == 1 
-        return "Weather is stormy: do not land"
-      elsif @weather == 2 || @weather == 3
-        if @stored_plane.length < @capacity
-          return @stored_plane.push(@plane)
-        else
-          return "Cannot land Airport is full" 
-        end   
-      end         
-      
-     
-    end     
-
+    if @weather == 1 
+      return "Weather is stormy: do not land"
+    elsif @weather == 2 || @weather == 3
+      if @stored_plane.length < @capacity
+        return @stored_plane.push(@plane)
+      else
+        return "Cannot land Airport is full" 
+      end   
+    end         
+    
+   
+  end     
         
-    def take_off(plane)
-      weather 
-      if @weather == 1 
-        return "Weather is stormy: do not take off"
-      elsif @weather == 2 || @weather == 3
-        @stored_plane.include?(plane)
-          @stored_plane.delete(plane)
-          return "Plane has taken off"
-        end   
-      
-    end  
+  def take_off(plane)
+    weather 
+    if @weather == 1 
+      return "Weather is stormy: do not take off"
+    elsif @weather == 2 || @weather == 3
+      @stored_plane.include?(plane)
+      @stored_plane.delete(plane)
+      return "Plane has taken off"
+    end   
+  end        
 
-      
-
-    def weather
-      @weather = rand(1..3)
-      
-    end     
+  def weather
+    @weather = rand(1..3)
+  end     
 end
