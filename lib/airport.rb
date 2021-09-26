c06fbad0f0b3c1886d7d4fd1caf872e52d9b6fcd
Jacob Cho <jacobcho@live.com>
2021-09-26 14:03:22 +0100

    tests for capacity, and refusing landing when airport is full

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,19 +1,31 @@
 require 'plane'
+require 'weather'
 
 class Airport
+    DEFAULT_CAPACITY = 100
 
-    attr_accessor :hangar
+    attr_accessor :hangar, :capacity
 
-    def initialize
+    def initialize(capacity = DEFAULT_CAPACITY)
         @hangar =[]
+        @capacity = capacity
     end
 
     def land(plane)
+        raise "Airport is full" if full
         @hangar << plane.class.to_s #adds Plane into hangar, instead of object id
     end
 
     def take_off
         @hangar.pop
     end
+
+    private
+
+    def full
+        @hangar.length >= capacity
+    end
+
+
 
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,11 +1,12 @@
 require 'airport'
 require 'plane'
+require 'weather'
 
 describe Airport do
 
     context 'when weather is clear' do
     
-        it 'should land a plane in hangar' do
+         it 'should land a plane in hangar' do
             airport = Airport.new
             plane = Plane.new
             expect(airport.land(plane)).to eq(["Plane"])
@@ -19,39 +20,52 @@ describe Airport do
             expect(airport.hangar.length).to eq(0)
         end
         
-        it 'should confirm a recently airborne plane is no longer at airport'
+        it 'should confirm a recently airborne plane is no longer at airport' do
+            airport = Airport.new
+            plane = Plane.new
+            airport.hangar.push("plane1", "plane2")
+            airport.take_off
+            expect(airport.hangar).to_not include('plane2')
+        end
 
     end
 
 
     context 'when weather is stormy' do
 
-        it 'should not land a plane'
+        it 'should not land a plane' 
 
-        it 'should not tell a plane to take off'
+        it 'should not tell a plane to take off' 
 
-    end
-
-
-
-    context 'when Airport is full' do
-
-        it 'should not land a plane'
-
-    end
-
-    context 'when Airport is empty' do
-
-        it 'should not tell a plane to take off'
-        
     end
 
     context 'when system designing' do
 
-        it 'should have a default capacity'
+        it 'should have a default capacity' do
+            expect(Airport.new.capacity).to eq Airport::DEFAULT_CAPACITY
+        end
 
-        it 'should change the default capacity'
+        it 'should override the default capacity' do
+            airport = Airport.new
+            airport.capacity = 250
+            expect(airport.capacity).to_not eq Airport::DEFAULT_CAPACITY
+        end
         
+    end
+
+
+    context 'when Airport is full' do
+
+        it 'should not land a plane' do
+            airport = Airport.new
+            plane = Plane.new
+            airport.capacity = Airport::DEFAULT_CAPACITY
+
+            Airport::DEFAULT_CAPACITY.times { airport.land(plane) }
+
+            expect { airport.land(plane) }.to raise_error 'Airport is full'
+        end
+
     end
 
 end
