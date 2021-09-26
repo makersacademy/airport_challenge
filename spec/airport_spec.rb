70b6e3cc1a7c5389bf668663dd7dd2c625664ffc
parsa <parsamesg@gmail.com>
2021-09-26 15:17:36 +0100

    refactor to one liner for land command

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,8 +2,6 @@ require "airport"
 
 describe Airport do
   describe "#land" do
-    it "responds to land command" do
-      expect(subject).to respond_to(:land)
-    end
+    it { is_expected.to respond_to(:land) }
   end
 end
