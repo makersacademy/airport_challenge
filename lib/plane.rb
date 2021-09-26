e6ab9ad43e1e86f2121eec3196c4b0b83e21bcc0
ConorButler <cwdbutler@gmail.com>
2021-09-26 19:12:14 +0100

    Planes can only be created without an airport

--- lib/plane.rb
+++ lib/plane.rb
@@ -1,8 +1,8 @@
 class Plane
   attr_reader :current_airport
-  def initialize(plane_id, current_airport = 0)
+  def initialize(plane_id)
     @plane_id = plane_id
-    @current_airport = current_airport
+    @current_airport = 0
   end
   
   def update_airport(airport_id)
