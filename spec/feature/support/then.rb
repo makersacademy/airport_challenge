9edf5c44a58200bcd890aed3ea157f896872ade6
Steve Jones <steve.jones763@gmail.com>
2021-09-26 16:36:52 +0100

    stop landing due to bad weather feature test added

--- spec/feature/support/then.rb
+++ spec/feature/support/then.rb
@@ -8,6 +8,18 @@ def then_the_plane_can_land
   expect(@plane.status).to eq airport_code
 end
 
+def then_the_plane_cannot_land
+  airport_code = @airport.code
+  airport_name = @airport.airport_name
+  plane_id = @plane.id
+  plane_name = @plane.name
+  expected = "Plane #{plane_id} (#{plane_name}) delayed landing due to bad weather at #{airport_name}"
+  
+  expect(@atc.clear_for_landing(airport_code, plane_id)).to eq expected
+  expect(@airport.plane_count).to eq 0
+  expect(@plane.status).to eq Aeroplane::FLYING
+end
+
 def then_the_plane_can_take_off
   airport_code = @airport.code
   plane_id = @plane.id
