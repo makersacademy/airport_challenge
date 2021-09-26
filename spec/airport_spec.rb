e0b47e0a581183ed6eb4fb992ddcbf77f70809c5
parsa <parsamesg@gmail.com>
2021-09-26 15:31:16 +0100

    add respond to land command with 1 arg unit test

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -3,5 +3,9 @@ require "airport"
 describe Airport do
   describe "#land" do
     it { is_expected.to respond_to(:land) }
+
+    it "responds to land command with 1 argument" do
+      expect(subject).to respond_to(:land).with(1).argument
+    end
   end
 end
