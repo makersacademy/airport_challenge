41f5ec5a38c3ca1fb11b9b1ac3154d6d4bcc7827
KNtanos <86077849+Kntanos@users.noreply.github.com>
2021-09-26 17:38:20 +0100

    Add ground location and fixed error for airport full

--- lib/plane.rb
+++ lib/plane.rb
@@ -1,19 +1,25 @@
 require_relative 'airport'
 
 class Plane
+attr_accessor :ground_location
 
-  def initialize
+  def initialize(ground_location = nil)
+    @ground_location = ground_location
   end
 
   def land(airport)
     fail "The weather is bad! You can't land!" if stormy
     fail "You can't land here. The airport is full!" if airport.full
+    fail "This plane is already on the ground!" if self.ground_location == :ground
+    self.ground_location = :ground
     airport.hangar << self
   end
 
   def take_off(airport)
     fail "The weather is bad! Stay on the ground!" if stormy
+    fail "This plane is not in this airport!" if left?(airport)
     airport.hangar.delete(self)
+    self.ground_location = :sky
   end
 
   def left?(airport)
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -7,52 +7,60 @@ describe Plane do
     expect(subject).to respond_to(:land)
   end
 
-    describe "#land" do
-      it "should accept one argument" do
-        expect(subject).to respond_to(:land).with(1).argument
-      end
-    
-      it "should allow a plane to land at an airport" do
-        expect(subject.land(airport)).to eq(airport.hangar << subject)
-      end
-
-      it "should raise an error if the airport is full" do
-        plane = Plane.new
-        airport = Airport.new
-        allow(plane).to receive(:stormy).and_return false
-        Airport::DEFAULT_CAPACITY.times{plane.land(airport)}
-        expect{plane.land(airport)}.to raise_error "You can't land here. The airport is full!"
-      end
-
-      it "should raise an error if the weather is stormy" do    
-        allow(subject).to receive(:stormy).and_return true
-        expect{subject.land(airport)}.to raise_error "The weather is bad! You can't land!"
-      end
-    end
-
   it "should respond to #take_off" do
     expect(subject).to respond_to(:take_off)
   end
-
-    describe "#take_off" do
-      it "should accept one argument" do
-        expect(subject).to respond_to(:take_off).with(1).argument
-      end    
-      
-      it "should raise an error if the weather is stormy" do    
-        allow(subject).to receive(:stormy).and_return true
-        expect{subject.take_off(airport)}.to raise_error "The weather is bad! Stay on the ground!"
-      end
-    end
 
   it "should respond to #left?" do
     expect(subject).to respond_to(:left?)
   end
 
+  describe "#land" do
+    it "should accept one argument" do
+      expect(subject).to respond_to(:land).with(1).argument
+    end
+  
+    it "should allow a plane to land at an airport" do
+      expect(subject.land(airport)).to eq(airport.hangar << subject)
+    end
+
+    it "should raise an error if the airport is full" do
+      plane = Plane.new
+      airport = Airport.new
+      allow(plane).to receive(:stormy).and_return false
+      allow(airport).to receive(:full).and_return true
+      expect{plane.land(airport)}.to raise_error "You can't land here. The airport is full!"
+    end
+
+    it "should raise an error if the weather is stormy" do    
+      allow(subject).to receive(:stormy).and_return true
+      expect{subject.land(airport)}.to raise_error "The weather is bad! You can't land!"
+    end
+  end
+
+  describe "#take_off" do
+    it "should accept one argument" do
+      expect(subject).to respond_to(:take_off).with(1).argument
+    end    
+    
+    it "should raise an error if the weather is stormy" do    
+      allow(subject).to receive(:stormy).and_return true
+      expect{subject.take_off(airport)}.to raise_error "The weather is bad! Stay on the ground!"
+    end
+
+    it "should raise an error if plane not in the airport" do
+      plane = Plane.new
+      airport = Airport.new
+      airport.hangar.delete(plane)
+      allow(plane).to receive(:stormy).and_return false
+      expect{plane.take_off(airport)}.to raise_error "This plane is not in this airport!"
+    end
+  end
+
   describe "#left?" do
     it "should accept one argument" do
       expect(subject).to respond_to(:left?).with(1).argument
-    end
+    end 
 
     it "should confirm that is no longer in the airport" do
       plane = Plane.new
