43a34e7225e08db9d24cc0a25dfc39f4b14ec962
Stewart-Alexander ANNANI <wxst.stewart@gmail.com>
2021-09-26 16:02:00 +0100

    added functionality to override capacity of an airport

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,21 +1,25 @@
+require_relative 'plane'
+
 class Airport
+  attr_reader :landed, :capacity  
   DEFAULT_CAPACITY = 10 
 
-  def initialize 
+  def initialize (capacity=DEFAULT_CAPACITY)
     @landed = []
+    @capacity = capacity
   end
 
   def land(plane)
     raise 'Not accepting more planes to land' if full?
-    @landed << plane
+    landed << plane
   end
 
   def take_off
-    @landed.pop
+    landed.each_with_index {|plane,index| landed.delete_at(index) ; return plane}
   end
 
   def full?
-    @landed.length >= DEFAULT_CAPACITY
+    @landed.length >= capacity
   end
     
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -8,15 +8,26 @@ describe Airport do
     plane = Plane.new
     expect(subject.land(plane)).to eq([plane])
   end 
-  
-  it 'should not accept more than 10 planes to land at a time' do 
-    10.times {subject.land(Plane.new)}
-    plane = Plane.new
-    expect{subject.land(plane)}.to raise_error 'Not accepting more planes to land'
-  end
 
   it 'expects instances of Airport class to respond to the take_off method' do 
     plane = Plane.new
     expect(Airport.new).to respond_to(:take_off)
   end 
+
+  it 'raises an error if landing space is full ' do
+    Airport::DEFAULT_CAPACITY.times {subject.land(Plane.new)}
+    plane = Plane.new
+    expect {subject.land(plane)}.to raise_error 'Not accepting more planes to land'
+  end
+
+  it "expects to be able to set passed capacity" do
+    number = 15
+    airport = Airport.new(number)
+    expect(airport.capacity).to eq number
+  end
+
+  it "expects default capacity to be ten " do
+    airport = Airport.new
+    expect(airport.capacity).to eq Airport::DEFAULT_CAPACITY
+  end
 end
