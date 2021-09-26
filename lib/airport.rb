d096af20cb0c2ef9ba1fc740336ed8afb53e6a3f
luke <luke44@gmail.com>
2021-09-26 17:27:39 +0100

    Readme, also some feature testing

--- lib/airport.rb
+++ lib/airport.rb
@@ -11,13 +11,11 @@ class Airport
 
   def take_off(plane)
     raise StandardError.new "That plane is not in the hangar" unless @hangar.include?(plane)
-    hangar.delete(plane) if landing_permitted
-    plane.plane_is_flying
+    (hangar.delete(plane) && plane.plane_is_flying) if landing_permitted
   end
 
   def land(plane)
-    hangar.append(plane) if landing_permitted && !full? && plane.landed == false
-    plane.plane_has_landed
+    (hangar.push(plane) && plane.plane_has_landed) if (landing_permitted && !full? && plane.landed == false)
   end
 
   def check_plane_in_hangar(plane)
