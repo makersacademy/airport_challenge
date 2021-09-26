e35c4d1ef10a908d0909e0d355f563dedb5f166a
Stewart-Alexander ANNANI <wxst.stewart@gmail.com>
2021-09-26 15:15:53 +0100

    added full? method to the Airport class

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,14 +1,21 @@
 class Airport
+  DEFAULT_CAPACITY = 10 
+
   def initialize 
     @landed = []
   end
 
   def land(plane)
+    raise 'Not accepting more planes to land' if full?
     @landed << plane
   end
 
   def take_off
     @landed.pop
+  end
+
+  def full?
+    @landed.length >= DEFAULT_CAPACITY
   end
     
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -5,13 +5,16 @@ describe Airport do
   it { is_expected.to respond_to(:land).with(1).argument }
 
   it 'expects a plane to land at an aiport' do
-    airport = Airport.new
     plane = Plane.new
-    expect(airport.land(plane)).to eq([plane])
+    expect(subject.land(plane)).to eq([plane])
   end 
-end
+  
+  it 'should not accept more than 10 planes to land at a time' do 
+    10.times {subject.land(Plane.new)}
+    plane = Plane.new
+    expect{subject.land(plane)}.to raise_error 'Not accepting more planes to land'
+  end
 
-describe "#take_off" do 
   it 'expects instances of Airport class to respond to the take_off method' do 
     plane = Plane.new
     expect(Airport.new).to respond_to(:take_off)
