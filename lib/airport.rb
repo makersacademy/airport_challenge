9b6f2889afd3a212d74db5d1d55318b037944b43
Beth Lambert-Matthews <blambertmatthews@gmail.com>
2021-09-26 08:16:01 +0100

    Refactored tests for Airport class

--- lib/airport.rb
+++ lib/airport.rb
@@ -2,23 +2,16 @@ class Airport
   def initialize(default_capacity)
     @capacity = default_capacity
     @planes_parked = ["Plane 1"]
-  end
-
-  def specific_plane_in_airport?
-    return true
-  end
-
-  def airport_full?
-    return true
+    @number_of_planes = @planes_parked.length
   end
 
   def check_capacity
     return @capacity
   end
 
-  def which_planes_in_airport
-    # return the contents of an array?
-    return @planes_parked
+  def change_capacity(new_capacity)
+    @capacity = new_capacity
+    return true
   end
 
   def how_many_planes_in_airport
@@ -26,14 +19,32 @@ class Airport
     return @number_of_planes
   end
 
+  def airport_full?
+    @number_of_planes = @planes_parked.length
+    if @number_of_planes >= @capacity
+      return true
+    else
+      return false
+    end
+  end
+
   def park_plane(plane)
     @planes_parked.push(plane)
     return true
   end
 
-  def change_capacity(new_capacity)
-    @capacity = new_capacity
-    return true
+  def which_planes_in_airport
+    # return the contents of an array
+    return @planes_parked
+  end
+
+  def specific_plane_in_airport?(plane)
+    if @planes_parked.include?(plane)
+      # if plane is a member of the array called @planes_parked
+      return true
+    else
+      return false
+    end
   end
   
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -4,102 +4,79 @@ require 'airport'
 RSpec.describe "Airport" do
   # tests looking at the Airport class
 
-  it "looks for a plane in the airport" do
-    # arrange
-    my_airport = Airport.new(20)
-  
-    # act
-    my_airport.specific_plane_in_airport?
-  
-    # assert
-    expect(my_airport.specific_plane_in_airport?).to eq true 
-  end
-
-  it "checks if the airport is full" do
-    # arrange
-    my_airport = Airport.new(20)
-  
-    # act
-    my_airport.airport_full?
-  
-    # assert
-    expect(my_airport.airport_full?).to eq true 
-  end
-
   it "checks an airport's maximum capacity" do
-    # arrange
+    # arrange, act, assert
     my_airport = Airport.new(20)
-  
-    # act
-  
-    # assert
     expect(my_airport.check_capacity).to eq 20
   end
 
-  it "finds out which planes are in the airport" do
-    # arrange
-    my_airport = Airport.new(20)
-
-    # act
-    my_airport.which_planes_in_airport
-
-    # assert
-    expect(my_airport.which_planes_in_airport).to eq ["Plane 1"]
-  end  
-
-  it "counts how many planes are in the airport" do
-    # arrange
-    my_airport = Airport.new(20)
-  
-    # act
-    my_airport.how_many_planes_in_airport
-
-    # assert
-    expect(my_airport.how_many_planes_in_airport).to eq 1
-  end
-
-  it "checks that a plane can land at the airport" do
-    # arrange
-    my_airport = Airport.new(20)
-  
-    # act
-    my_airport.park_plane("Plane 2")
-
-    # assert
-    expect(my_airport.park_plane("Plane 2")).to eq true
-  end
-
-  it "checks that a plane landing at the airport is added to @planes_parked" do
-    # arrange
-    my_airport = Airport.new(20)
-  
-    # act
-    my_airport.park_plane("Plane 2")
-
-    # assert
-    expect(my_airport.which_planes_in_airport).to eq ["Plane 1", "Plane 2"]
-  end
-
   it "can change an airport's maximum capacity" do
-    # arrange
     my_airport = Airport.new(20)
-  
-    # act
-    my_airport.change_capacity(40)
-  
-    # assert
     expect(my_airport.change_capacity(40)).to eq true
   end
 
-  it "changes an airport's maximum capacity to 40" do
-    # arrange
+  it "confirms an airport's maximum capacity has been changed to 40" do
     my_airport = Airport.new(20)
-  
-    # act
     my_airport.change_capacity(40)
-  
-    # assert
     expect(my_airport.check_capacity).to eq 40
+  end
+
+  it "counts how many planes are in the airport" do
+    my_airport = Airport.new(20)
+    my_airport.park_plane("Plane 2")
+    my_airport.park_plane("Plane 3")
+    expect(my_airport.how_many_planes_in_airport).to eq 3
+  end
+  
+  it "checks if the airport is full" do
+    my_airport = Airport.new(1)
+    expect(my_airport.airport_full?).to eq true 
+  end
+
+  it "checks if the airport is full (when capacity is 3 and there are 3 planes)" do
+    my_airport = Airport.new(3)
+    my_airport.park_plane("Plane 2")
+    my_airport.park_plane("Plane 3")
+    expect(my_airport.airport_full?).to eq true 
+  end
+
+  it "checks if the airport has space for more planes" do
+    my_airport = Airport.new(20)
+    expect(my_airport.airport_full?).to eq false
+  end
+
+  it "checks that a plane can land and park at the airport" do
+    my_airport = Airport.new(20)
+    expect(my_airport.park_plane("Plane 2")).to eq true
+  end
+
+  # NEED TO ADD TESTS HERE - CHECK FOR PLANE BEING ABLE TO LAND IN CORRECT WEATHER / CAPACITY
+      
+  it "checks that a plane landing at the airport is added to the list of planes parked" do
+    my_airport = Airport.new(20)
+    my_airport.park_plane("Plane 2")
+    expect(my_airport.which_planes_in_airport).to eq ["Plane 1", "Plane 2"]
+  end
+
+  it "finds out which planes are in the airport" do
+    my_airport = Airport.new(20)
+    expect(my_airport.which_planes_in_airport).to eq ["Plane 1"]
+  end  
+
+  it "looks for Plane 1 in the airport" do
+    my_airport = Airport.new(20)
+    expect(my_airport.specific_plane_in_airport?("Plane 1")).to eq true 
+  end
+
+  it "looks for and finds Plane 2 in the airport" do
+    my_airport = Airport.new(20)
+    my_airport.park_plane("Plane 2")
+    expect(my_airport.specific_plane_in_airport?("Plane 2")).to eq true 
+  end
+
+  it "looks for and does not find Plane 5 in the airport" do
+    my_airport = Airport.new(20)
+    expect(my_airport.specific_plane_in_airport?("Plane 5")).to eq false 
   end
 
 end
