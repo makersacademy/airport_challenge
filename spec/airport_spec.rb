e6db97b8183e4de1421c2c65b53b8ad8603e2311
parsa <parsamesg@gmail.com>
2021-09-26 17:02:00 +0100

    add respond to takeoff command unit test

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -5,4 +5,7 @@ describe Airport do
     it { is_expected.to respond_to(:land) }
     it { is_expected.to respond_to(:land).with(1).argument }
   end
+  describe "#takeoff" do
+    it { is_expected.to respond_to(:takeoff) }
+  end
 end
