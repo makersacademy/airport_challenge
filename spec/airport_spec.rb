3453c7b7aadd42288bd48837f1fa8c1a8b4e28d9
Arthur Fincham <arthurfincham@me.com>
2021-09-26 13:02:03 +0100

    refactor context

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,79 +1,70 @@
 require 'airport' 
 
 describe Airport do
-  let(:plane) {Plane.new}
+  let(:plane) { Plane.new }
   subject(:airport) { described_class.new }
 
   describe '#config' do
-    it 'can store planes on its tarmac' do
-      expect(subject.tarmac).to be_a_kind_of(Array)
-    end
-
     it 'can change capacity' do
-      airport = Airport.new('NYC', 10)
+      airport = Airport.new(10)
       expect(airport.capacity).to eq 10
     end
-
   end
 
   describe '#landing' do
-    it 'is permitted when sunny' do
-      allow(airport).to receive(:stormy?).and_return(false)
-      subject.land(plane)
-      expect(subject.tarmac).to include(plane.name)
+    before do
+      allow(plane).to receive(:land)
     end
 
-    it 'is denied if stormy' do
-      allow(airport).to receive(:stormy?).and_return(true)
-      expect { subject.land(plane) }.to raise_error('Treacherous conditions. Stay put.')
+    context 'weather is sunny' do
+      before do
+        allow(airport).to receive(:stormy?).and_return(false)
+      end
+
+      it 'is permitted when sunny' do
+        subject.land(plane)
+        expect(subject.tarmac).to include(plane)
+      end
+      
+      it 'is denied if capacity is full' do
+        allow(airport).to receive(:full?).and_return(true)
+        expect { subject.land(plane) }.to raise_error('Tarmac is full.')
+      end
     end
 
-    it 'is denied if capacity is full' do
-      allow(airport).to receive(:full?).and_return(true)
-      allow(airport).to receive(:stormy?).and_return(false)
-      expect { subject.land(plane) }.to raise_error('Tarmac is full.')
+    context 'weather is stormy' do
+      it 'is denied if stormy' do
+        allow(airport).to receive(:stormy?).and_return(true)
+        expect { subject.land(plane) }.to raise_error('Treacherous conditions. Stay put.')
+      end
     end
-
   end
 
   describe "#take_off" do
-      it 'is permitted if sunny' do
-        allow(airport).to receive(:stormy?).and_return(false)
-        subject.land(plane)
+
+    before do
+      allow(plane).to receive(:take_off)
+      allow(airport).to receive(:stormy?).and_return(false)
+      subject.land(plane)
+    end
+
+    context 'weather is sunny' do
+      it 'planes can take off' do
         subject.take_off(plane)
-        expect(subject.tarmac).to_not include(plane.name)
-        expect(plane.ground).to eq false
-        expect(plane.location).to eq 'Sky'
+        expect(subject.tarmac).to_not include(plane)
       end
 
       it 'can confirm that the plane has taken off' do
-        allow(airport).to receive(:stormy?).and_return(false)
-        subject.land(plane)
         subject.take_off(plane)
         expect(subject.confirm(plane)).to eq 'Flying high!'
       end
-
-      it 'prevents take off if stormy' do
-        allow(airport).to receive(:stormy?).and_return(true)
-        plane = Plane.new('EZY999')
-        plane.ground = false
-        plane.location = subject.name
-        subject.tarmac << plane
-        expect { subject.take_off(plane) }.to raise_error('Stormy. Cannot take off.')
-      end
-
-      it 'cannot take off a plane in another airport' do
-        allow(airport).to receive(:stormy?).and_return(false)
-        heathrow = Airport.new("heathrow", 1)
-        heathrow
-        ezy = Plane.new
-        ba = Plane.new
-        airport.land(ba)
-        heathrow.land(ezy)
-        expect { airport.take_off(ezy) }.to raise_error("Aircraft is not under your control.")
-      end
-
     end
 
+    context 'weather is stormy' do
+      it 'planes cannot take off' do
+        allow(airport).to receive(:stormy?).and_return(true)
+        expect { subject.take_off(plane) }.to raise_error('Stormy. Cannot take off.')
+      end
+    end
   end
-
+end
