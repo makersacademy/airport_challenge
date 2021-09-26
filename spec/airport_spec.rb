5c0b7e3139fc78070ed8919f1e3be88ba5034261
parsa <parsamesg@gmail.com>
2021-09-26 18:36:07 +0100

    add unit test for land method to return plane

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -4,6 +4,11 @@ describe Airport do
   describe "#land" do
     it { is_expected.to respond_to(:land) }
     it { is_expected.to respond_to(:land).with(1).argument }
+
+    it "lands a new plane and returns it" do
+      plane = Plane.new
+      expect(subject.land(plane)).to eq plane
+    end
   end
   describe "#takeoff" do
     it { is_expected.to respond_to(:takeoff) }
