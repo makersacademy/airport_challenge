b3efd4ecfdf23560f380585e7f79a909df972119
Philip Carr <philipcarr@Philips-MacBook-Air.local>
2021-09-27 03:26:05 +0100

    As far as I got

---
+++ spec/airport_spec.rb
@@ -0,0 +1,21 @@
+require 'airport'
+describe Airport do
+  subject(:airport) {described_class.new}
+
+  it 'instructs a plane to land' do
+    #test
+  end
+
+  it 'instructs a plane to take off' do
+    #test
+  end
+
+  it 'instructs a plane to take off and confirm not at airport' do
+    #test
+  end
+
+  it 'has a default capacity' do
+    #test
+  end
+
+end
