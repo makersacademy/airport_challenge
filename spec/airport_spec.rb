412781ccdfe14d8d7d270e87e51e92fed982d9da
Jacob Cho <jacobcho@live.com>
2021-09-27 12:06:39 +0100

    refactored spec using before(:each)

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -5,25 +5,24 @@ require 'weather'
 describe Airport do
 
     context 'when weather is clear' do
-        let(:plane) {Plane.new}
-        let(:weather) {Weather.new}
+        before(:each) do
+            @plane = Plane.new
+            allow(subject.weather).to receive(:bad?).and_return false
+        end
 
         it 'should land a plane in hangar' do
-            allow(subject.weather).to receive(:bad?).and_return false 
-            subject.land(plane)
+            subject.land(@plane)
             expect(subject.hangar.length).to eq(1)
         end
 
         it 'should tell plane to take off' do
             subject.hangar << "plane in hangar"
-            allow(subject.weather).to receive(:bad?).and_return false
             subject.take_off
             expect(subject.hangar.length).to eq(0)
         end
         
         it 'should confirm a recently airborne plane is no longer at airport' do
             subject.hangar.push("plane1", "plane2")
-            allow(subject.weather).to receive(:bad?).and_return false
             subject.take_off
             expect(subject.hangar).to_not include('plane2')
         end
@@ -32,16 +31,17 @@ describe Airport do
 
 
     context 'when weather is stormy' do
-        let(:plane) {Plane.new}
-        # let(:weather) {subject.weather}
-
-        it 'should tell plane not to land ' do
+   
+        before(:each) do
+            @plane = Plane.new
             allow(subject.weather).to receive(:bad?).and_return true 
-            expect{ subject.land(plane) }.to raise_error "Weather is bad, cannot land plane"
+        end
+        
+        it 'should tell plane not to land ' do
+            expect{ subject.land(@plane) }.to raise_error "Weather is bad, cannot land plane"
         end
 
         it 'should not tell a plane to take off' do
-            allow(subject.weather).to receive(:bad?).and_return true 
             expect{ subject.take_off }.to raise_error "Weather is bad, plane cannot take off"
         end
 
