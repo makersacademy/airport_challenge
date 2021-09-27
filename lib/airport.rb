24c1f9569d7faf9b60562d1e71d27aded2ce71ff
ConorButler <cwdbutler@gmail.com>
2021-09-27 13:52:07 +0100

    Only 1 weather object is created

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,3 +1,6 @@
+require 'weather'
+$weather = Weather.new
+
 class Airport
   DEFAULT_CAPACITY = 50
   attr_reader :planes, :capacity
@@ -12,19 +15,19 @@ class Airport
     @planes.length >= @capacity
   end
 
-  def land(plane, weather = Weather.new)
+  def land(plane)
     raise "Plane already at this airport" if confirm_departure(plane) == "Plane has not departed"
     raise "Plane already at another airport" unless plane.current_airport.zero?
-    raise "Dangerous weather" if weather.stormy
+    raise "Dangerous weather" if $weather.stormy
     raise "Airport full" if full?
     @planes << plane
     plane.update_airport(@airport_id)
   end
 
-  def take_off(plane, weather = Weather.new)
+  def take_off(plane)
     raise "Plane is already in the sky" if plane.current_airport.zero?
     raise "Plane is at another airport" if confirm_departure(plane) == "Plane has departed"
-    raise "Dangerous weather" if weather.stormy
+    raise "Dangerous weather" if $weather.stormy
     @planes.delete(plane)
     plane.update_airport(0)
   end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -4,26 +4,28 @@ describe Airport do
 
   context 'landing:' do
     airport = Airport.new(1500)
+    let(:good_weather) { $weather = double("Weather", :stormy => false) }
     let(:plane) { double("Plane", :current_airport => 0) }
-    let(:weather) { double("Weather", :stormy => false) }
-    
+
     # The same mock Plane object is reused because only the method and capacity is being tested,
     # we are are not concerned about identifying individual planes at this point
 
     it "lands a plane, updates its airport, and stores it" do
+      good_weather
       expect(plane).to receive(:update_airport).with(1500)
-      airport.land(plane, weather)
+      airport.land(plane)
 
       expect(airport.planes).to include plane
     end
 
     it "doesn't land when airport is full" do
+      good_weather
       49.times do # 1 plane is already at airport
         allow(plane).to receive(:update_airport).with(1500)
-        airport.land(plane, weather)
+        airport.land(plane)
       end
 
-      expect { airport.land(plane, weather) }.to raise_error("Airport full")
+      expect { airport.land(plane) }.to raise_error("Airport full")
     end
 
     it "defaults to a capacity of 50" do
@@ -31,13 +33,14 @@ describe Airport do
     end
 
     it "can only land a plane from the sky" do
+      good_weather
       airport3000 = Airport.new(3000)
       plane_in_this_airport = double("Plane in airport 3000", :current_airport => 3000)
       plane_in_other_airport = double("Plane in airport 3001", :current_airport => 3001)
 
-      expect { airport3000.land(plane_in_this_airport, weather)
+      expect { airport3000.land(plane_in_this_airport)
       }.to raise_error("Plane already at this airport")
-      expect { airport3000.land(plane_in_other_airport, weather)
+      expect { airport3000.land(plane_in_other_airport)
       }.to raise_error("Plane already at another airport")
       # don't need to tell doubles to receive messages because they don't get past the guard clause
     end
@@ -45,19 +48,20 @@ describe Airport do
 
   context 'taking off:' do
     airport = Airport.new(2000)
-    let(:weather) { double("Weather", :stormy => false) }
+    let(:good_weather) { $weather = double("Weather", :stormy => false) }
     let(:plane1) { double("Plane1", :current_airport => 0) }
     let(:plane2) { double("Plane2", :current_airport => 0) }
 
     # Weather double is reused because we want it to not be a factor in these tests
 
     it "removes planes from the airport when they take off" do
+      good_weather
       allow(plane1).to receive(:update_airport).with(2000)
-      airport.land(plane1, weather)
+      airport.land(plane1)
       allow(plane1).to receive(:current_airport).and_return(2000)
 
       allow(plane1).to receive(:update_airport).with(0)
-      airport.take_off(plane1, weather)
+      airport.take_off(plane1)
       allow(plane1).to receive(:current_airport).and_return(0)
 
       expect(airport.planes.include?(plane1)).to eq false
@@ -65,18 +69,19 @@ describe Airport do
     end
     
     it "confirms a plane has departed" do
+      good_weather
       plane3 = double("Plane3", :current_airport => 0)
       plane4 = double("Plane4", :current_airport => 0)
       
       allow(plane3).to receive(:update_airport).with(2000)
       allow(plane4).to receive(:update_airport).with(2000)
-      airport.land(plane3, weather)
-      airport.land(plane4, weather)
+      airport.land(plane3)
+      airport.land(plane4)
       allow(plane3).to receive(:current_airport).and_return(2000)
       allow(plane4).to receive(:current_airport).and_return(2000)
 
       allow(plane3).to receive(:update_airport).with(0)
-      airport.take_off(plane3, weather)
+      airport.take_off(plane3)
       allow(plane3).to receive(:current_airport).and_return(0)
       # plane3 is back in sky
 
@@ -84,18 +89,20 @@ describe Airport do
       expect(airport.confirm_departure(plane4)).to eq("Plane has not departed")
       
       allow(plane4).to receive(:update_airport).with(0)
-      airport.take_off(plane4, weather)
+      airport.take_off(plane4)
       allow(plane4).to receive(:current_airport).and_return(0)
       # removing the other plane double from the airport for the next test
     end
 
     it "only lets planes take off from airports they are in" do
+      good_weather
       plane_in_other_airport = double("Plane in airport 4000", :current_airport => 4000)
       expect { airport.take_off(plane_in_other_airport) 
       }.to raise_error("Plane is at another airport")
     end
 
     it "planes already flying can't take off" do
+      good_weather
       plane_in_sky = double("Plane in sky", :current_airport => 0)
       expect { airport.take_off(plane_in_sky)
       }.to raise_error("Plane is already in the sky")
@@ -103,22 +110,24 @@ describe Airport do
   end
   
   context 'weather:' do
-    let(:airport) { Airport.new(6000) }
+    airport = Airport.new(6000)
+    let(:bad_weather) { $weather = double("Weather", :stormy => true) }
     let(:plane) { double("Plane", :current_airport => 0) }
-    let(:bad_weather) { double("Weather", :stormy => true) }
-    let(:good_weather) { double("Weather", :stormy => false) }
 
     it "doesn't let planes take off or land if the weather is stormy" do
+      bad_weather
       allow(plane).to receive(:update_airport).with(6000)
+      
+      expect { airport.land(plane) }.to raise_error("Dangerous weather")
 
-      expect { airport.land(plane, bad_weather) }.to raise_error("Dangerous weather")
-
-      airport.land(plane, good_weather)
+      allow($weather).to receive(:stormy).and_return(false)
+      airport.land(plane)
       allow(plane).to receive(:current_airport).and_return(6000)
       # plane is now in airport
+      allow($weather).to receive(:stormy).and_return(true)
 
-      expect { airport.take_off(plane, bad_weather) }.to raise_error("Dangerous weather")
       # don't need to tell doubles to receive messages because they don't get past the guard clause
+      expect { airport.take_off(plane) }.to raise_error("Dangerous weather")
     end
   end
 end
