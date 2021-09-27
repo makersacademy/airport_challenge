8d5c33f8f93b494cb91965ece6b6c68d663e75d8
Troy Snowden <troybradsnowden@gmail.com>
2021-09-27 08:40:52 +0100

    Add readme and fix rubocop issues

--- spec/weather_gen_spec.rb
+++ spec/weather_gen_spec.rb
@@ -1,7 +1,7 @@
 require 'weather_gen'
 
 describe WeatherGenerator do
-  subject(:weather) {described_class.new}
+  subject(:weather) { described_class.new }
 
   it "it should have a boolean stormy property" do
     expect([TrueClass, FalseClass].include?(weather.forecast_stormy?.class)).to eq(true)
