abc66c92eb8844d300f57ecba9147be7325aec90
Jacob Cho <jacobcho@live.com>
2021-09-27 12:12:34 +0100

    refactored weather randomizer

--- lib/weather.rb
+++ lib/weather.rb
@@ -2,11 +2,8 @@ class Weather
 
     def bad?
         bad = rand(1..20)  
-        if bad == 1
-            true
-        else
-            false
-        end
+        return true if bad == 1
+        false
     end
 
 end
--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -1,26 +1,20 @@
 require 'weather'
 
 describe Weather do
+let(:weather) {Weather.new}
 
     it 'If weather is bad' do
-        weather = Weather.new
         allow(weather).to receive(:bad?).and_return true 
         expect(weather.bad?).to eq(true)
     end
 
     it 'If weather is good' do
-        weather = Weather.new
         allow(weather).to receive(:bad?).and_return false
         expect(weather.bad?).to eq(false)
     end
 
     it 'If weather.bad? is boolean' do
-        weather = Weather.new
         expect([true, false]).to include(weather.bad?)
     end
-
-
-
-
 
 end
