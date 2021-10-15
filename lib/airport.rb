464d3fb80d27a9d70dc2ba7f297090a00cc76a50
yasmin <yasmin.jones@digital.justice.gov.uk>
2021-10-15 16:43:22 +0100

    all 3 tests pass

--- lib/airport.rb
+++ lib/airport.rb
@@ -4,16 +4,17 @@ class Airport
 
     def initialize
         @plane = []
+        @capacity = 10 
 
     end 
 
-    def get_plane(plane)
+    # def get_plane(plane)
+    #     @plane << plane 
+    # end 
+
+    def land(plane)
+        fail 'Airport is full' if full_airport
         @plane << plane 
-    end 
-
-    def land
-        fail if 'Airport is full' if @plane 
-        @plane.pop
     end 
 
     def take_off(plane)
@@ -25,7 +26,7 @@ class Airport
     end 
 
     def full_airport
-        
+        @plane.count >= @capacity
     end 
 
 
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -5,20 +5,21 @@ require "airport"
 describe Airport do 
     it 'instructs plane to land at airport' do 
         plane = Plane.new 
-        subject.get_plane(plane)
-        expect(subject.land).to eq (plane)
+        # subject.get_plane(plane) 
+        expect(subject.land(plane)).to eq [plane]
     end 
 
     it 'instruct plane to take off from airport' do 
-        plane = Plane.new 
+        plane= Plane.new 
         subject.take_off(plane)
         expect(subject.plane_gone).to eq (true)
         
     end 
 
     it 'prevents landing when airport is full ' do 
-        plane = Plane.new
-        expect{ subject.land}.to raise_error 'Airport is full'
+        plane = Plane.new()
+        10.times{ subject.land(plane)}
+        expect{ subject.land(plane)}.to raise_error 'Airport is full'
 
     end 
 
