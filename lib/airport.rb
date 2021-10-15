bfc1fda16bb028197d0a6e44e2ab2e073a370934
Patrick Barsch <PatrickGB@gmx.de>
2021-10-15 13:39:40 +0200

    User story 2 - airport can instruct a plane to take off

--- lib/airport.rb
+++ lib/airport.rb
@@ -3,4 +3,7 @@ class Airport
   def instruct_to_land(plane) 
   end
 
+  def instruct_to_take_off(plane)
+  end
+
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -8,7 +8,16 @@ describe Airport do
       plane = double("plane")
       expect(subject).to respond_to(:instruct_to_land).with(1).argument
     end
+
+  end
   
+  describe '#instruct_to_take_off' do 
+  
+    it 'can instruct an airplane to take off' do 
+      plane = double("plane")
+      expect(subject).to respond_to(:instruct_to_take_off).with(1).argument
+    end
+
   end
 
 end
