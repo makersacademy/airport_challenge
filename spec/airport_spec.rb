96067eed8598cee50570eae9a11be2889df0c45f
Patrick Barsch <PatrickGB@gmx.de>
2021-10-15 14:00:36 +0200

    Refactored tests using let syntax for plane to make them DRY

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,10 +2,11 @@ require 'airport'
 
 describe Airport do
 
+  let (:plane) { double("plane") }
+
   describe '#instruct_to_land' do 
   
     it 'can instruct an airplane to land' do 
-      plane = double("plane")
       expect(subject).to respond_to(:instruct_to_land).with(1).argument
     end
 
@@ -14,7 +15,6 @@ describe Airport do
   describe '#instruct_to_take_off' do 
   
     it 'can instruct an airplane to take off' do 
-      plane = double("plane")
       expect(subject).to respond_to(:instruct_to_take_off).with(1).argument
     end
 
@@ -23,7 +23,6 @@ describe Airport do
   describe '#plane_at_airport?' do 
 
     it 'can confirm, that a plane left the airport' do 
-      plane = double("plane")
       subject.instruct_to_land(plane)
       subject.instruct_to_take_off(plane)
       expect(subject.plane_at_airport?(plane)).to eq false
