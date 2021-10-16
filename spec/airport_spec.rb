6796ba989f0a116317dfb5c2c5d5a52f0e52b1fb
antlerhead <antlerhead@hotmail.co.uk>
2021-10-16 19:00:58 +0100

    finished user story 2

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,11 +1,17 @@
 require './lib/airport'
 
-subject(:airport) { decribed_class.new }
     # plane = Plane.new
 
     describe Airport do
-        it 'lands plane' do
-          airport = Airport.new
-          expect { subject.to respond.to(:land).with(1).argument }
-        end
+      subject (:airport) { decribed_class.new }
+
+      it 'lands plane' do
+        airport = Airport.new
+        expect { (airport).to respond.to(:land).with(1).argument }
+      end
+
+      it 'allows planes to take off' do
+      expect { (airport).to respond.to(:take_off).with(1).argument }
+      end 
+
     end
