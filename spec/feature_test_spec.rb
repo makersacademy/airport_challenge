2fa009e49d87e0e9c99dcf52efcf131518f06d93
Patrick Barsch <PatrickGB@gmx.de>
2021-10-17 22:28:08 +0200

    Refactored Feature test for readability

--- spec/feature_test_spec.rb
+++ spec/feature_test_spec.rb
@@ -2,7 +2,7 @@ require 'airport'
 require 'plane'
 require 'weather'
 
-def manage_transition(from, to, airport, type)
+def organize_plane_to(from, to, airport, type)
   plane = from.last
   transitioned_plane = airport.__send__(type, plane, weather)
   return if transitioned_plane.nil?
@@ -12,24 +12,32 @@ end
 
 describe "let's create some air traffic, so I can visit Makers in person" do
 
+  cologne_bonn = Airport.new(30)
+  london_heathrow = Airport.new(30)
+  flying_planes = Array.new(100) { Plane.new }
+  cologne_planes = []
+  london_planes = []
+
+  land_cologne = [flying_planes, cologne_planes, cologne_bonn, :instruct_to_land]
+  land_london = [flying_planes, london_planes, london_heathrow, :instruct_to_land]
+  take_off_cologne = [cologne_planes, flying_planes, cologne_bonn, :instruct_to_take_off]
+  take_off_london = [london_planes, flying_planes, london_heathrow, :instruct_to_take_off]
+
   it 'allows us to let several planes land/take off' do  
-    cologne_bonn = Airport.new(30)
-    london_heathrow = Airport.new(30)
-    flying_planes = Array.new(100) { Plane.new }
-    cologne_planes = []
-    london_planes = []
+
     srand(1234)
     25.times do 
-      manage_transition(flying_planes, cologne_planes, cologne_bonn, :instruct_to_land)
-      manage_transition(flying_planes, london_planes, london_heathrow, :instruct_to_land)
+      organize_plane_to(*land_cologne)
+      organize_plane_to(*land_london)
     end
     100.times do
-      manage_transition(cologne_planes, flying_planes, cologne_bonn, :instruct_to_take_off)
-      manage_transition(london_planes, flying_planes, london_heathrow, :instruct_to_take_off)
-      manage_transition(flying_planes, cologne_planes, cologne_bonn, :instruct_to_land)
-      manage_transition(flying_planes, london_planes, london_heathrow, :instruct_to_land)
+      organize_plane_to(*take_off_cologne)
+      organize_plane_to(*take_off_london)
+      organize_plane_to(*land_cologne)
+      organize_plane_to(*land_london)
     end
-    distribution = [flying_planes.count, cologne_planes.count, london_planes.count]
-    expect(distribution).to eq([63, 15, 22])
+    all_planes = [flying_planes, cologne_planes, london_planes]
+    plane_distribution = all_planes.map! { |location| location.count }
+    expect(plane_distribution).to eq([63, 15, 22])
   end
 end
