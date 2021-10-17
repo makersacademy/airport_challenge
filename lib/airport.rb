72f47d3c3b7bc515a2b9de37d140c978927dc332
yasmin <yasmin.jones@digital.justice.gov.uk>
2021-10-17 21:03:16 +0100

    user story 5 is complete and works in irb and code in lib works but test is getting a runtime error - unsure if that was suppose to happen

--- lib/airport.rb
+++ lib/airport.rb
@@ -2,10 +2,13 @@ require_relative 'plane'
 
 class Airport 
 
-    def initialize
-        @plane = []
-        @capacity = 10 
+    DEFAULT_CAPACITY = 100
 
+    attr_reader :capacity 
+
+    def initialize(capacity=DEFAULT_CAPACITY)
+        @plane = []
+        @capacity = capacity
     end 
 
     # def get_plane(plane)
@@ -18,15 +21,27 @@ class Airport
     end 
 
     def take_off(plane)
-        @plane.pop 
+        fail 'Weather is stormy, plane cannot take off' if weather == 'stormy'
+        @plane.pop # removes the last element of the array 
     end 
 
     def plane_gone
         @plane.empty? 
     end 
 
+    def weather
+        todays_weather = ['sunny','rain','cloudy','stormy'].sample 
+
+    end 
+
+
+    private 
+
+    attr_reader :plane 
+    
+
     def full_airport
-        @plane.count >= @capacity
+        @plane.count >= capacity
     end 
 
 
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -5,23 +5,58 @@ require "airport"
 describe Airport do 
     it 'instructs plane to land at airport' do 
         plane = Plane.new 
+        aiport = Airport.new 
         # subject.get_plane(plane) 
-        expect(subject.land(plane)).to eq [plane]
+        expect(aiport.land(plane)).to eq [plane]
     end 
 
-    it 'instruct plane to take off from airport' do 
-        plane= Plane.new 
+    #WORKING CODE - before the weather user story was implimeneted 
+    # it 'instruct plane to take off from airport' do 
+    #     plane= Plane.new 
+    #     subject.take_off(plane)
+    #     expect(subject.plane_gone).to eq (true)
+    # end 
+
+    it 'plane cannot take off it weather is stormy' do 
+        plane = Plane.new 
         subject.take_off(plane)
-        expect(subject.plane_gone).to eq (true)
-        
-    end 
+        allow(subject.take_off(plane)).to receive(subject.weather) {'stormy'}
+        expect{ subject.take_off(plane) }.to raise_error 'Weather is stormy, plane cannot take off'
+    end
+    # RuntimeError: - Weather is stormy, plane cannot take off
 
-    it 'prevents landing when airport is full ' do 
-        plane = Plane.new()
-        10.times{ subject.land(plane)}
-        expect{ subject.land(plane)}.to raise_error 'Airport is full'
 
+    #WORKING CODE - when airport class does not have a default value
+    # it 'prevents landing when airport is full ' do 
+    #     plane = Plane.new()
+    #     100.times{ subject.land(plane)}
+    #     expect{ subject.land(plane)}.to raise_error 'Airport is full'
+
+    # end 
+    
+    it 'airport has a default capacity/prevents landing when airport is full' do 
+        # airport  = Airport.new  - I used subject instead of airport 
+        plane = Plane.new 
+        Airport::DEFAULT_CAPACITY.times { subject.land(plane) }
+        expect { subject.land(plane) }.to raise_error 'Airport is full'
     end 
+  
+
+
+
+
+
+    # it 'the default airport capacity can be overridden' do 
+
+    # end 
+
+
+
+    # it 'if weather is stormy, do not takeoff ' do 
+    #     plane = Plane.new
+    #     allow(subject.weather).to receive(:weather) {'stormy'}
+    #     expect{ subject.weather}.to raise_error 'weather is stormy, cannot takeoff'
+    # end 
 
     
 end 
