eab4244c3d92d240ec2c5d77f47d384d2cb43cdf
antlerhead <antlerhead@hotmail.co.uk>
2021-10-18 00:28:08 +0100

    completed user stories

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,11 +1,38 @@
 
 class Airport
 
+    Default_capacity = 15
+
+    def initialize(capacity = Default_capacity)
+      @capacity = capacity
+      @planes = []
+    end
+  
+#   def count_planes
+#     @planes.count
+#   end
+
   def land(plane)
+    fail 'no landing space! airport full' if full?
+    fail 'cannot land plane if weather stormy' if stormy?
+    @planes << plane
   end
 
   def take_off(plane)
+    fail 'cannot take off if weather stormy' if stormy?
   end
+
+  def full?
+    @planes.length >= @capacity
+  end
+
+  def stormy?
+    rand(1..100) > 80
+  end
+
+#   def empty?
+#     count_planes == 0
+#   end
 
 end
 
