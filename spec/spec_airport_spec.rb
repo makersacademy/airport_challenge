731e1dcbe2a175666d21f874109e62a155bc53f0
michael steinacher <steinacherm@hotmail.co.uk>
2021-10-18 11:31:01 +0100

    updated class instance tests in spec to be airport

--- spec/spec_airport_spec.rb
+++ spec/spec_airport_spec.rb
@@ -2,19 +2,19 @@ require 'airport'
 
 describe Airport do
   it 'can instruct a plane to land' do
-    controller = TrafficController.new
+    controller = Airport.new
 
     expect(controller.land).to eq("plane landed")
   end
 
   it 'can instruct a plane to take off who will confirm it has' do
-    controller = TrafficController.new
+    controller = Airport.new
 
     expect(controller.take_off).to eq("plane has taken off")
   end
 
   it 'ensures safety by preventing landings when the airport is full' do
-    controller = TrafficController.new
+    controller = Airport.new
 
     expect(controller.full_airport).to eq("do not land airport is full")
   end
