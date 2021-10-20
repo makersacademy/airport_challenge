7f8285e52aafb122aaaccbc115a8b20709f7e64c
Jenaro Garcia <jjegarcia@hotmail.com>
2021-10-20 09:25:59 +0100

    fixed as per review comments

--- lib/weather.rb
+++ lib/weather.rb
@@ -1,11 +1,9 @@
 WEATHER_STATES = { stormy: true, not_stormy: false }.freeze
-STORMY_ODDS = [WEATHER_STATES[:not_stormy], WEATHER_STATES[:not_stormy],
-               WEATHER_STATES[:not_stormy], WEATHER_STATES[:stormy]].freeze
+
 class Weather
-  private def random_weather
-    STORMY_ODDS.sample
-  end
   def stormy?
-    random_weather == WEATHER_STATES[:stormy]
+    return WEATHER_STATES[:not_stormy] if rand(100) < 90
+    WEATHER_STATES[:stormy]
   end
 end
+
