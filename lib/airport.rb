c12c78fc654e3765162aeffbe881787ed34989c9
Matt Zimmer <zimmermja@gmail.com>
2021-10-22 20:01:40 +0100

    automatic capacity setting added; 6 tests passed

--- lib/airport.rb
+++ lib/airport.rb
@@ -2,5 +2,31 @@ require_relative "plane"
 require_relative "weather"
 
 class Airport
+  DEFAULT_CAPACITY = 20
 
+  attr_reader :capacity
+
+  def initialize(arg_cap = nil)
+    @hanger = []
+    update_capacity(arg_cap)
+  end
+
+  def update_capacity(arg_cap = nil)
+    fail fail_message_cap if invalid_cap?(arg_cap)
+    @capacity = arg_cap ? arg_cap.to_i : DEFAULT_CAPACITY
+  end
+
+  private
+
+  def invalid_cap?(v)
+    return false if v.nil?
+    return true if cant_convert_to_int(v)
+    v.to_i.zero?
+  end
+
+  def cant_convert_to_int(v) !(v.respond_to? :to_i) end
+
+  def fail_message_cap 
+    'Capacity must be a numerical value (e.g. 10, 10.0, "10") > 0'
+  end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,1 +1,22 @@
 require "airport"
+
+describe Airport do
+  it 'sets capacity to arg value when initialised with a numerical arg' do
+    expect(Airport.new(10).capacity).to eq 10
+    expect(Airport.new("10").capacity).to eq 10
+    expect(Airport.new(10.75).capacity).to eq 10
+  end
+
+  it 'sets capacity to default when initialised without argument' do
+    expect(subject.capacity).to eq Airport::DEFAULT_CAPACITY
+  end
+
+  it 'returns error when initialised with non-numerical arg' do
+    expect { Airport.new("ten") }.to raise_error 'Capacity must be a numerical value (e.g. 10, 10.0, "10") > 0'
+    expect { Airport.new([10]) }.to raise_error 'Capacity must be a numerical value (e.g. 10, 10.0, "10") > 0'
+  end
+
+  it 'returns error when initialised with arg == 0' do
+    expect { Airport.new(0) }.to raise_error 'Capacity must be a numerical value (e.g. 10, 10.0, "10") > 0'
+  end
+end
