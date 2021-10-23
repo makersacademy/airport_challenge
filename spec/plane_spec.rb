272d2c26dfb193aa0c26ca15db3856578a499616
Kate Young <89363807+katemyoung@users.noreply.github.com>
2021-10-23 17:29:57 +0100

    Add land method in plane.rb

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -2,8 +2,8 @@ require 'plane.rb'
 
 describe Plane do
 
-#   it 'reponds to land' do
-#     expect(subject).to respond_to(:land)
-#   end
+it 'reponds to land' do
+  expect(subject).to respond_to(:land)
+end
 
 end
