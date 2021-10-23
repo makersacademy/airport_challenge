305b45c63e57b7455071734b65afc0710001e352
Matt Zimmer <zimmermja@gmail.com>
2021-10-23 21:58:09 +0100

    update_airport added to Plane class

--- lib/plane.rb
+++ lib/plane.rb
@@ -1,3 +1,24 @@
 class Plane
 
+  attr_reader :current_airport
+
+  def initialize
+    @current_airport = nil
+  end
+
+  def update_airport(airport = nil)
+    fail fail_message_sna if invalid_airport(airport)
+    @current_airport = airport
+  end
+
+  private
+
+  def invalid_airport(airport)
+    (airport.class != Airport) && (airport != nil)
+  end
+
+  def fail_message_sna
+    'Call error: must include an Airport as an argument'
+  end
+
 end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -1,1 +1,25 @@
 require "plane"
+
+describe Plane do
+  it 'itiates with no airport in @current_airport' do
+    expect(subject.current_airport).to eq nil
+  end
+
+  describe '#update_airport' do
+    it 'sets @current_airport to the passed airport' do
+      new_airport = Airport.new
+      subject.update_airport(new_airport)
+      expect(subject.current_airport).to eq new_airport
+    end
+
+    it 'sets @airport to nil if no args passed' do
+      subject.update_airport
+      expect(subject.current_airport).to eq nil
+    end
+
+    it 'returns an error if non-Airport argument is passed' do
+      expect { subject.update_airport("Airport") }.to raise_error 'Call error: must include an Airport as an argument'
+      expect { subject.update_airport(Airport.new) }.to_not raise_error
+    end
+  end
+end
