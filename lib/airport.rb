ced80cf84696d70a6ab0942472dafbc6372cc673
Mohammed Salah <mohammed_salah@icloud.com>
2021-10-23 17:05:44 +0100

    User Story 2

--- lib/airport.rb
+++ lib/airport.rb
@@ -4,4 +4,7 @@ class Airport
 
   end
 
+  def take_off
+    "Taken Off"
+  end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,8 +1,15 @@
 require './lib/airport'
+require './lib/plane'
 
 describe Airport do
+  subject(:airport) {described_class.new}
 
   it "instructs a plane to land" do
-    expect(subject).to respond_to(:land).with(1).argument
+    expect(airport).to  respond_to(:land).with(1).argument
   end
+
+  it "instructs a plane to take off" do
+    expect(airport.take_off).to eq "Taken Off"
+  end
+
 end
