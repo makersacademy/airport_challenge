369e657496ffb8e4e8a037f95360662c260e0f0a
Ginny <ginnynjon@gmail.com>
2021-10-24 16:17:58 +0100

    in take_off method, had plane array remove the last plane of the array out (to simulate a plane taking off)

--- lib/airport.rb
+++ lib/airport.rb
@@ -10,14 +10,14 @@ class Airport
 
   def land(plane)
     # instructs a plane to land at an airport.  A plane will be needed to be able to land
-
     @plane << plane
     @plane.pop
   end
 
 
   def take_off
-
+    @plane >> plane
+    p @plane.pop
   end
 end
 
