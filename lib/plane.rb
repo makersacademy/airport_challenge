a4a7c9214a102653b3cde00bfb6fe174baf6bd58
Will <w_birdseye@hotmail.co.uk>
2021-10-24 22:03:57 +0100

    Content of files corrected to more accurately reflect user story

--- lib/plane.rb
+++ lib/plane.rb
@@ -1,4 +1,2 @@
 class Plane
-  def land
-  end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,4 +1,7 @@
 require 'airport'
 
 describe Airport do
+  it "instructs a plane to land" do
+    expect(subject).to respond_to :land
+  end
 end
