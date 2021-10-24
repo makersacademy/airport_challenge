28f8cf027a126e8d53ce604f370511a82034fa26
Will <w_birdseye@hotmail.co.uk>
2021-10-24 22:09:29 +0100

    airport now has take_off method and response test

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -4,4 +4,8 @@ describe Airport do
   it "instructs a plane to land" do
     expect(subject).to respond_to :land
   end
+
+  it "instructs a plane to take_off" do
+    expect(subject).to respond_to :take_off
+  end
 end
