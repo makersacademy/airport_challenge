ca84c761109477e926cf9659f7e4c5bd8a9b13c4
ayseldilan <ayselbulut1@gmail.com>
2021-10-24 12:31:22 +0100

    changed the structure

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,18 +2,21 @@ require "airport"
 
 describe Airport do
   subject(:airport) { described_class.new(30) }
-  it "lands at an airport" do
+
+  describe "#land" do
+    it "lands at an airport" do
     expect(airport).to respond_to(:land).with(1).argument
-  end
-
-  it "take off from an airport and confirm that it is no longer in the airport" do
-    expect(airport).to respond_to(:take_off).with(1).argument
-  end
-
-  it "not allow landing when capacity is full" do
-    30.times do
-        airport.land(:plane)
     end
-    expect { airport.land(:plane).to raise_error "Cannot land, airport is full!!"}
+    it "not allow landing when capacity is full" do
+        30.times do
+            airport.land(:plane)
+        end
+        expect { airport.land(:plane).to raise_error "Cannot land, airport is full!!"}
+      end
   end
+  describe "#take_off" do
+    it "take off from an airport and confirm that it is no longer in the airport" do
+    expect(airport).to respond_to(:take_off).with(1).argument
+    end
+end
 end
