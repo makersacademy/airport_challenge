4c023bcca09ed333d1bc441e3c97e6ef438ccee2
olaleye <olaoduola01@gmail.com>
2021-10-24 21:23:25 +0100

    take off

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,6 +1,8 @@
 class Airport
   def land(plane)
     end
+  def take_off
+  end
 end
 
   
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -3,4 +3,7 @@ describe Airport do
   it 'instructing a plane to land' do
     expect(subject).to respond_to(:land).with(1).argument
   end
+  it "letting us know plain has left airport" do
+    expect(subject).to respond_to(:take_off)
+  end
 end
