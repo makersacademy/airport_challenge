7a63b3a2c6bdddd383c2a487b4ac9fd5bc384ef9
Tatiana Nantes <tatiananantes@gmail.com>
2021-10-24 18:27:59 +0100

    refactor tests to describe 'method'

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -3,22 +3,23 @@ require 'airport'
 
 describe Airport do
   subject(:airport) { described_class.new(20) }
+  let(:plane) { double :plane }
   
-  it 'lands a plane' do
-    expect(airport).to respond_to(:land).with(1).argument
+  describe '#land' do
+    it 'lands a plane' do
+      expect(airport).to respond_to(:land).with(1).argument
+    end
+    
+    it 'does not allow a plane to land if airport is at full capacity' do
+      20.times { airport.land(plane) }
+      expect { airport.land(plane) }.to raise_error 'Can not land - full capacity!'
+    end
   end
-   
-  it 'allows plane to take off' do
-    expect(airport).to respond_to(:take_off).with(1).argument
-  end
-
-  it 'does not allow a plane to land if airport is at full capacity' do
-    plane = double :plane
-    20.times { airport.land(plane) }
-    expect { airport.land(plane) }.to raise_error 'Can not land - full capacity!'
-  end
-
-
   
+  describe '#take-off' do
+    it 'allows plane to take off' do
+      expect(airport).to respond_to(:take_off).with(1).argument
+    end 
+  end
 
 end
