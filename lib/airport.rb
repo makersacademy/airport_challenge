ff5c28b3282c336aff18a03be0ffad31bb71cd82
bmargerison <bmargerison@outlook.com>
2021-10-24 12:00:25 +0100

    Refactoring errors into class variable array

--- lib/airport.rb
+++ lib/airport.rb
@@ -2,28 +2,41 @@ class Airport
 
   attr_reader :planes, :capacity
 
-  def initialize(capacity=3)
+  DEFAULT_CAPACITY = 3
+
+  @@errors = [
+    'It is too stormy',
+    'Plane already landed',
+    'The airport is full.',
+    'You are not at this airport'
+  ]
+
+  def initialize(capacity=DEFAULT_CAPACITY)
     @planes = []
     @capacity = capacity
   end
 
+  def self.errors
+    @@errors
+  end
+
+  def self.stormy?
+    rand < 0.10
+  end
+
   def landing(plane)
-    raise 'It is too stormy' if Airport.stormy?
-    raise 'Plane already landed' unless plane.location == 'airborne'
-    raise 'The airport is full.' unless @planes.length < @capacity
+    raise Airport.errors[0] if Airport.stormy?
+    raise Airport.errors[1] unless plane.location == 'airborne'
+    raise Airport.errors[2] unless @planes.length < @capacity
     @planes << plane
     plane.location = self
   end
 
   def take_off(plane)
-    raise 'You are not at this airport' unless @planes.include? plane
-    raise 'It is too stormy' if Airport.stormy?
+    raise Airport.errors[3] unless @planes.include? plane
+    raise Airport.errors[0] if Airport.stormy?
     plane.location = 'airborne'
     @planes.delete(plane)
-  end
-
-  def self.stormy?
-    rand < 0.10
   end
 
 end
