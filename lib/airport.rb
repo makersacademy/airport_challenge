391bcd5166b3d1de3e7917f2f6832a8eca332964
Ginny <ginnynjon@gmail.com>
2021-10-24 17:46:36 +0100

    went too far and messed up code, went back to basics, passing 1st test of landing a plane

--- lib/airport.rb
+++ lib/airport.rb
@@ -2,22 +2,22 @@ require_relative './plane'
 
 class Airport
 
-  attr_reader :plane
-
-  def initialize
-    @plane = []
-  end
-
   def land(plane)
     # instructs a plane to land at an airport.  A plane will be needed to be able to land
-    @plane << plane
-    @plane.pop
+    plane
+
   end
 
+  #def take_off
 
-  def take_off
-    @plane >> plane
-    p @plane.pop
+  #end
+end
+=begin
+  def take_off(plane)
+    puts "The plane is no longer at the airport"
+    @plane.pop
+
   end
 end
 
+=end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,18 +1,34 @@
 require_relative '../lib/airport'
 
 describe Airport do
+
   describe '#land' do
     it 'instructs a plane to land at an airport' do
       plane = Plane.new
-     expect(subject.land(plane)).to eq plane
+
+      expect(subject.land(plane)).to eq plane
       end
 
     it {is_expected.to respond_to :land}
   end
+end
+
+
+=begin
 
   describe '#take_off' do
     it 'instructs a plane to take off' do
-    is_expected.to respond_to :take_off
+      plane = Plane.new
+      expect(subject.take_off(plane)).to eq plane
+    end
+    it 'confirms it is no longer in the airport' do
+      expect (subject.take_off(plane)).should_receive(:plane), "The plane is no longer at the airport"
     end
   end
+  it { is_expected.to respond_to :take_off }
+
+
+
+
 end
+=end
