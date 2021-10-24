6be0d39a21d899889972a0d9a1b12d83fa308538
ayseldilan <ayselbulut1@gmail.com>
2021-10-24 12:20:59 +0100

    not allow landing when capacity is full

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,6 +1,12 @@
 class Airport
-  def land(plane)
+  def initialize(capacity)
+    @capacity = capacity
+    @number_of_planes = 0
+  end
 
+  def land(plane)
+    raise "Cannot land, airport is full!!" if @number_of_planes >= @capacity
+    @number_of_planes += 1
   end
 
   def take_off(plane)
