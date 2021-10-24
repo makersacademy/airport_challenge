f08cc6804bf14fca1393910eba0cf50c486012eb
olaleye <olaoduola01@gmail.com>
2021-10-25 00:11:10 +0100

    cannot land weather stormy

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,13 +1,15 @@
 class Airport
-  def initialize(capacity = 20)
+  def initialize(capacity)
     @capacity = capacity
     @planes = []
   end
 
   def land(plane)
+    raise "Cannot land plane: weather is stormy" if stormy
     raise 'Cannot land plane: airport full' if full
     @planes << plane
   end
+  
   def take_off(plane)
     raise "Cannot take off plane: weather is stormy" if stormy
   end
