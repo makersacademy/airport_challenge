f655edc084b582849fc145bdad13e489a97028f6
Simon Jacobs <simonjacobs@simons-air.home>
2021-10-24 16:30:51 +0100

    Second commit of airport challenge - code passes all tests

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,3 +1,5 @@
+require './lib/plane'
+
 class Airport
   DEFAULT_CAPACITY = 50
   attr_reader :plane_capacity
@@ -5,15 +7,18 @@ class Airport
   def initialize(plane_capacity = DEFAULT_CAPACITY)
     @planes = []
     @plane_capacity = plane_capacity
+    @weather = stormy?
   end
 
-  def land(landing_plane)
-    fail 'No capacity at airport' if self.full?
-    @planes << landing_plane
-    landing_plane
+  def land(arriving_plane)
+    fail 'Too stormy to land' if stormy?
+    fail 'No capacity at airport' if full?
+    @planes << arriving_plane
+    arriving_plane
   end
 
   def take_off
+    fail 'Too stormy to take off' if stormy?
     @planes.pop
   end
 
@@ -21,7 +26,7 @@ class Airport
     @planes.count == @plane_capacity
   end
 
-  def empty?
-    @planes.empty?
+  def stormy?
+    rand(1..5) > 3
   end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,26 +2,40 @@ require 'airport'
 require 'plane'
 
 describe Airport do
+  airport = Airport.new
+  plane = Plane.new
+  
+  before do
+    allow(airport).to receive(:stormy?).and_return(true)
+  end
+
+  it 'doesn\'t allow a plane to take off if weather is stormy' do
+    expect(airport).to receive(:stormy?).and_return(true)
+    expect { airport.take_off }.to raise_error 'Too stormy to take off'
+  end
+
+  it 'doesn\'t allow a plane to land if weather is stormy' do
+    expect(airport).to receive(:stormy?).and_return(true)
+    expect { airport.land(plane) }.to raise_error 'Too stormy to land'
+  end
+
+  before do
+    airport = Airport.new
+    plane = Plane.new
+    allow(subject).to receive(:stormy?).and_return(false)
+  end
 
   it 'allows a plane to land at the airport' do
-    plane = Plane.new
     expect(subject.land(plane)).to eq plane
   end
 
-=begin
-  As an air traffic controller 
-  So I can get passengers on the way to their destination 
-  I want to instruct a plane to take off from an airport and confirm that it is no longer in the airport
-=end
-
   it 'allows a plane to take off from the airport' do
-    plane = Plane.new
     subject.land(plane)
     expect(subject.take_off).to eq plane
   end
 
   it 'doesn\'t allow a plane to land if airport capacity reached' do
-    Airport::DEFAULT_CAPACITY.times {subject.land(Plane.new)}
+    Airport::DEFAULT_CAPACITY.times { subject.land(Plane.new) }
     expect { subject.land(Plane.new) }.to raise_error 'No capacity at airport'
   end
 
@@ -29,11 +43,4 @@ describe Airport do
     airport = Airport.new(1)
     expect(airport.plane_capacity).to eq 1
   end
-
-=begin
-  As an air traffic controller 
-  To ensure safety 
-  I want to prevent takeoff when weather is stormy 
-=end
-
 end
