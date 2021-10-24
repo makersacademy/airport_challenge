e35d901ecc4b2a6e2c52572ed14612af2f4ddd80
Ben Dowsett <bendowsett@gmail.com>
2021-10-24 19:32:06 +0100

    fourth user story

--- lib/airport.rb
+++ lib/airport.rb
@@ -7,10 +7,18 @@ class Airport
 
     end 
     def land(plane)
+        raise "cannot land its stormy" if stormy?
         raise "cannot land plane: airport full" if @airstrip.length >= @capacity
+      
         @airstrip.push(plane)
         
     end 
+    
     def take_off(plane)
+        raise "Cant take off its stormy" if stormy?
+    end 
+
+    def stormy?
+        rand(1..6) > 4
     end 
 end 
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,22 +1,32 @@
 require 'airport'
-#require 'plane'
+require 'plane'
 
 describe Airport do 
     airport = Airport.new(20)
     it "instructs a plane to land" do 
+        allow(airport).to receive(:stormy?).and_return false
         expect(airport).to respond_to(:land).with(1).argument
     end 
   
     it "instructs a plane to take off" do
         expect(airport).to respond_to(:take_off).with(1).argument
     end 
+    it "does not allow planes to take off when stormy" do 
+        allow(airport).to receive(:stormy?).and_return true
+        expect { airport.take_off(:plane) }.to raise_error "Cant take off its stormy"
+    end 
 
     it "does not allow landing when at capacity" do
         plane = Plane.new
+        allow(airport).to receive(:stormy?).and_return false
          20.times do
-            airport.land(plane)
+            airport.land(:plane)
          end 
-         expect { airport.land(plane) }.to raise_error "cannot land plane: airport full"
+         expect { airport.land(:plane) }.to raise_error "cannot land plane: airport full"
 
+    end 
+       it "raises an error when asked to land plane when stormy" do 
+        allow(airport).to receive(:stormy?).and_return true
+        expect {airport.land(:plane)}.to raise_error "cannot land its stormy"
 end 
 end 
