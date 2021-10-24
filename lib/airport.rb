42f70f720dfb44f81b531ec90349c0eb311ff363
olaleye <olaoduola01@gmail.com>
2021-10-24 22:49:35 +0100

    airport full

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,8 +1,20 @@
 class Airport
+  def initialize(maximum)
+    @maximum = maximum
+    @planes = []
+  end
+
   def land(plane)
-    end
-  def take_off
+    raise 'Cannot land plane: airport full' if full
+    @planes << plane
+  end
+  def take_off(plane)
   end
 end
 
+private
+def full
+  @planes.length >= @maximum
+end
+  
   
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,9 +1,24 @@
 require "airport"
+require "plane"
 describe Airport do
-  it 'instructing a plane to land' do
-    expect(subject).to respond_to(:land).with(1).argument
-  end
-  it "letting us know plain has left airport" do
-    expect(subject).to respond_to(:take_off)
+  subject(:airport) { Airport.new(20) }
+  let(:plane) { Plane.new }
+  describe '#land' do
+    it 'instructing a plane to land' do
+      expect(subject).to respond_to(:land).with(1).argument
+    end
+    context 'when full' do
+        it 'Does not let plane land' do
+          20.times do
+            subject.land(plane)
+          end
+          expect { subject.land(plane) }.to raise_error 'Cannot land plane: airport full'
+        end
+    end
+end
+  describe "#take-off" do
+    it "letting us know plain has left airport" do
+      expect(subject).to respond_to(:take_off).with(1).argument
+    end
   end
 end
