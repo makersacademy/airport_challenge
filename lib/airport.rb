868a474e5f3d4767b893ac4f9d80922c98c2f0a9
ayseldilan <ayselbulut1@gmail.com>
2021-10-25 08:26:26 +0100

    system designer default airport capacity overriden

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,14 +1,33 @@
+require "weather"
+
 class Airport
-  def initialize(capacity)
+  DEFAULT_CAPACITY = 30
+  def initialize(weather, capacity = DEFAULT_CAPACITY)
     @capacity = capacity
+    @weather = weather
     @number_of_planes = 0
   end
 
   def land(plane)
     raise "Cannot land, airport is full!!" if @number_of_planes >= @capacity
+    raise "Cannot land plane, the weather is stormy" if stormy?
     @number_of_planes += 1
   end
 
   def take_off(plane)
+    raise "Cannot take off, weather is stormy" if stormy?
+    raise "Cannot take off, plane not at the right airport" unless at_airport?(plane)
+  end
+
+  def full?
+    @number_of_planes += 1
+  end
+
+  def stormy?
+    @weather.stormy?
+  end
+
+  def at_airport?(plane)
+    @planes.include?(plane)
   end
 end
