c3942c7017eabbad002d51ce3410cec0b7a5e9d0
Lilly Daniell <76227019+lildann@users.noreply.github.com>
2021-11-16 15:36:05 +0000

    Update airport.rb

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,29 +1,27 @@
 class Airport 
-  DEFAULT_CAPACITY = 5
   attr_accessor :capacity
   attr_reader :plane, :airport
+  
+  DEFAULT_CAPACITY = 5
+  
   def initialize(capacity = DEFAULT_CAPACITY)
-    @airport = []
+    @hangar = []
     @capacity = capacity
   end
 
   def land(plane)
-    fail "Airport is full" if @airport.length >= @capacity
+    fail "Airport is full" if @hangar.length >= @capacity
     fail "Stormy weather: do not land" if weather? == "Stormy"
-
-    @plane = plane
-    @airport << @plane
+    @hangar << plane
   end
 
   def take_off(plane)
     fail "Stormy weather: do not fly" if weather? == "Stormy"
-    fail "Plane not at airport" unless @airport.include?(@plane)
-
-    @plane = plane
-    @airport.each do |grounded_plane| 
-      if grounded_plane == @plane && weather? == "Sunny"
-        @airport.delete(@plane) 
-        return "#{@plane} has departed"
+    fail "Plane not at airport" unless @hangar.include?(plane)
+    @hangar.each do |grounded_plane| 
+      if grounded_plane == plane && weather? == "Sunny"
+        @hangar.delete(plane) 
+        return "#{plane} has departed"
       end
     end
   end
