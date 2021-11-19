d1f2578d99774752a1cfb40719bfad797274b406
Sherwin Panganiban <sherwingpanganiban@gmail.com>
2021-11-19 21:31:06 +0000

    Write test to check for stormy weather

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -8,4 +8,8 @@ describe Airport do
     airport = Airport.new(3)
     expect(airport.capacity).to be(3)
   end
+
+  it 'can check for stormy weather' do
+    expect (subject.is_stormy?).to be_boolean
+  end
 end
