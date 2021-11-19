7707370250705e16a708175993d4cbc091bc1f08
Kimberley Morgan <kimberley.morgan135@gmail.com>
2021-11-19 20:15:23 +0000

    Refactoring the way we check if airport is full

--- lib/airport.rb
+++ lib/airport.rb
@@ -5,11 +5,15 @@ class Airport
   attr_reader :plane
 
   def land(plane)
-    fail "Airport full" if @plane
+    fail "Airport full" if full?
     @plane = plane
   end
 
   def takeoff
     @plane = nil
+  end
+
+  def full?
+    @plane
   end
 end
