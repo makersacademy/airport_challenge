3202775808e8fb003ccba8f3d0f36bbf5d15d7d3
Sherwin Panganiban <sherwingpanganiban@gmail.com>
2021-11-19 20:43:24 +0000

    Add default capacity for airport and full? method

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,6 +1,12 @@
 class Airport
   attr_reader :planes
+  DEFAULT_CAPACITY = 10
+
   def initialize
     @planes = []
+  end
+
+  def full?
+    return @planes.length >= DEFAULT_CAPACITY
   end
 end
--- lib/plane.rb
+++ lib/plane.rb
@@ -1,5 +1,6 @@
 class Plane
   def land(airport)
+    if airport.full? then fail "airport is full" end
     airport.planes.push(self)
   end
 
