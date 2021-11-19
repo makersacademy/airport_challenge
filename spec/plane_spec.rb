922e886a70dcf61213bda956c4b05cb974727090
Sherwin Panganiban <sherwingpanganiban@gmail.com>
2021-11-19 21:44:38 +0000

    Write test for preventing landing in stormy weather

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -4,22 +4,27 @@ require 'airport'
 describe Plane do
   subject(:plane) { Plane.new }
 
-  it 'plane lands at airport' do
+  it 'lands at airport' do
     airport = Airport.new
     subject.land(airport)
     expect(airport.planes.length).to be(1)
   end
 
-  it 'plane departs from airport' do
+  it 'departs from airport' do
     airport = Airport.new
     subject.land(airport)
     subject.depart(airport)
     expect(airport.planes.length).to be (0)
   end
 
-  it 'plane cannot land if airport is full' do
+  it 'cannot land if airport is full' do
     airport = Airport.new
     Airport::DEFAULT_CAPACITY.times { subject.land(airport) }
     expect{ subject.land(airport) }.to raise_error("airport is full")
+  end
+
+  it 'cannot land if weather is stormy' do
+    airport = Airport.new
+    expect { subject.depart(airport) }.to raise_error("weather is stormy")
   end
 end
