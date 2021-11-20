3b2590cad2f3bb4ce1af78dd421b7cd418aeb635
Francesco <francescocollotto@gmail.com>
2021-11-20 11:50:29 +0000

    add rspec test plane is no longer in the airport

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,10 +1,16 @@
 require_relative './plane'
 
 class Airport
+  attr_reader :landed
   def initialize
     @landed = []
   end
+  
   def land(plane)
     @landed << plane
+  end
+
+  def departure
+    @landed.shift
   end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -5,10 +5,25 @@ describe Airport do
   let(:gatwick) { described_class.new }
   let(:wings) { Plane.new }
   
-  it "instruct a plane to land" do
-    expect(gatwick).to respond_to(:land).with(1).argument
+  context "land" do
+    it "instruct a plane to land" do
+      expect(gatwick).to respond_to(:land).with(1).argument
+    end
+    it "land a plane in the airport" do
+      expect(gatwick.land(wings)).to eq [wings]
+    end
   end
-  it "land a plane in the airport" do
-    expect(gatwick.land(wings)).to eq [wings]
+
+  context "take off" do
+    it "instruct a plane to take off" do
+      expect(gatwick).to respond_to(:departure)
+    end
+    it "confirms that the plane is no longer in the airport" do
+      gatwick.land(wings)
+      departing_plane = gatwick.landed[0]
+      gatwick.departure
+      expect(gatwick.landed).not_to include(departing_plane)
+    end
   end
+
 end
