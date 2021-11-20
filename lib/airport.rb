8c7065be67514f076c6eb431d842be55af369b86
riky moroni <riky5.moroni@gmail.com>
2021-11-20 19:41:21 +0000

    use rubocop to inspect files and corect offenses detected

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,17 +1,16 @@
 class Airport
 
-    def initialize(capacity = 5)
-        @planes = []
-        @capacity = capacity
-    end
+  def initialize(capacity = 5)
+    @planes = []
+    @capacity = capacity
+  end
 
-    def land_plane
-        fail "Landing not possible, airport is full" if @planes.count >= @capacity
-        @planes << Plane.new
-    end
+  def land_plane
+    fail "Landing not possible, airport is full" if @planes.count >= @capacity
+    @planes << Plane.new
+  end
 
-    def plane_take_off
-        "The plane is no longer in the airport"
-    end
-
+  def plane_take_off
+    "The plane is no longer in the airport"
+  end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,30 +2,27 @@ require 'airport'
 require 'plane'
 
 describe Airport do
+  it { is_expected.to respond_to :land_plane }
+  it { is_expected.to respond_to :plane_take_off }
 
-    it { is_expected.to respond_to :land_plane }
-    it { is_expected.to respond_to :plane_take_off }
+  describe "#plane_take_off" do
+    it "should confirm the plane is no longer in the airport" do
+      subject = Airport.new
+      expect(subject.plane_take_off).to eq("The plane is no longer in the airport")
+    end
+  end
 
-    describe "#plane_take_off" do
-        it "should confirm the plane is no longer in the airport" do
-            subject = Airport.new
-            expect(subject.plane_take_off).to eq("The plane is no longer in the airport")
-        end
+  describe "#land_plane" do
+    it "shouldn't land if airport is full (max capacity 1) " do
+      subject = Airport.new(1)
+      subject.land_plane
+      expect { subject.land_plane }.to raise_error "Landing not possible, airport is full"
     end
 
-    describe "#land_plane" do
-        it "shouldn't land if airport is full (max capacity 1) " do
-            subject = Airport.new(1)
-            subject.land_plane
-            expect{subject.land_plane}.to raise_error "Landing not possible, airport is full"
-        end
-
-        it "should have a default capacity of 5 and raise error when capacity is full" do
-            subject = Airport.new
-            5.times { subject.land_plane }
-            expect{subject.land_plane}.to raise_error "Landing not possible, airport is full"
-        end
-
+    it "should have a default capacity of 5 and raise error when capacity is full" do
+      subject = Airport.new
+      5.times { subject.land_plane }
+      expect { subject.land_plane }.to raise_error "Landing not possible, airport is full"
     end
-
+  end
 end
