698bbdff60450e19a2cea67e3ec79c88484debb4
Giorgi Gutsaev <giorgi.gutsaevi@gmail.com>
2021-11-20 15:04:53 +0000

    corrected my gemfile to reflect my ruby version. 3.0.2 -> 3.0.0 and ran bundle install again

---
+++ spec/airport_spec.rb
@@ -0,0 +1,14 @@
+require 'airport'
+=begin
+User Story 1 
+As an air traffic controller 
+So I can get passengers to a destination 
+I want to instruct a plane to land at an airport
+=end
+
+# Let's create an Airport class
+describe Airport do
+	
+end
+
+
