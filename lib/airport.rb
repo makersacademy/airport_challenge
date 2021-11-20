26e2dbb715693fa35aa5561f216afe972856baa4
Sabrina <sabrinalord@msn.com>
2021-11-20 13:09:12 +0000

    airports now have default capacitys

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,4 +1,12 @@
 class Airport
+    attr_reader :capacity
+
+    DEFAULT_CAPACITY = 1
+
+    def initialize(capacity = DEFAULT_CAPACITY)
+        @capacity = capacity
+    end 
+
     def is_full?
     true
     end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,7 +2,7 @@ require 'airport'
 require 'plane'
 
 describe Airport do 
-
-
-
+  it 'has a default capacity' do
+    expect(subject.capacity).to eq Airport::DEFAULT_CAPACITY
+  end 
 end 
