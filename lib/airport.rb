d3ded3896f97289202bc18ac2c8cc37e40889b6c
Jessica Gordon <ketchesj@gmail.com>
2021-11-20 12:57:52 +0000

    User Story 3 passes RSpec

--- lib/airport.rb
+++ lib/airport.rb
@@ -4,15 +4,22 @@ class Airport
 
   attr_reader :name
 
+  DEFAULT_CAPACITY = 1
+
   def initialize(name)
     @name = name
+    @capacity = DEFAULT_CAPACITY
+    @planes = []
   end
 
   def land(plane)
-    "successful landing"
+    raise "airport full: cannot land plane here" if @planes.length >= @capacity 
+    @planes << plane
+    plane
   end
 
   def take_off(plane)
+    @planes.delete(plane)
     "sucessful take off of #{plane} from #{@name}"
   end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,17 +2,33 @@ require 'airport'
 require 'plane'
 
 describe Airport do
+
   it 'lands a plane at an airport' do
     plane = Plane.new
     airport = Airport.new("Heathrow")
-    expect(airport.land(plane)).to eq "successful landing"
+
+    expect(airport.land(plane)).to eq plane
   end
+
   it 'confirms plane is no longer in the airport when plane takes off' do
     plane = Plane.new
     airport = Airport.new("Heathrow")
+
     airport.land(plane)
+
     expect(airport.take_off(plane)).to eq "sucessful take off of #{plane} from #{airport.name}"
   end
+
+  it 'prevents landing a plane when the airport is full' do
+    plane1 = Plane.new
+    plane2 = Plane.new
+    airport = Airport.new("Heathrow")
+
+    airport.land(plane1)
+
+    expect { airport.land(plane2) }.to raise_error("airport full: cannot land plane here") 
+  end
+
 end
 
 # As an air traffic controller 
