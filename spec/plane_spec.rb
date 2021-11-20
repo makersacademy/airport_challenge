9d43b1c3905873ed52a2d2395a6953ce07d5eaf3
Tomas Garcia <tomasgarciadev@gmail.com>
2021-11-20 10:24:11 +0000

    plane class and land method created

---
+++ spec/plane_spec.rb
@@ -0,0 +1,9 @@
+require 'plane'
+
+describe Plane do
+  it "should be able to create an instance of plane" do
+    expect(Plane).to respond_to(:new)
+  end
+
+  it { is_expected.to respond_to :land } 
+end
