2a8059c94242acab8f3219c3132e4ab3784e0d09
Chris Clement <chrisclement@Chriss-Air.connect>
2021-11-21 17:23:42 +0000

    Add context and before into rspec tests

--- lib/airport.rb
+++ lib/airport.rb
@@ -4,23 +4,24 @@ class Airport
 
   attr_reader :planes
   attr_accessor :capacity
+  DEFAULT_CAPACITY = 25
 
   def initialize
     @planes = []
-    @capacity = 25
+    @capacity = DEFAULT_CAPACITY
   end
 
   def land(plane)
-    fail "Airport is full" if @planes.count >= @capacity
+    fail "Airport is full" if planes.count >= capacity
     fail "Stormy weather, can't land" if stormy?
     fail "This plane has already landed" if landed?(plane)
-    @planes << plane
+    planes << plane
   end
 
   def take_off
     fail "Stormy weather, can't take off" if stormy?
     fail "Airport is empty, no planes can take off" if empty?
-    @planes.pop
+    planes.pop
   end
 
   def stormy?
@@ -28,11 +29,11 @@ class Airport
   end
 
   def landed?(plane)
-    @planes.include? plane
+    planes.include? plane
   end
 
   def empty?
-    @planes.empty?
+    planes.empty?
   end
 
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,66 +2,98 @@ require 'airport'
 require 'plane'
 
 describe Airport do 
-  it { is_expected.to respond_to :land }
 
-  it { is_expected.to respond_to :take_off }
-
-  it "When I land a plane, I expect that plane to be at the airport if it is not stormy" do 
-    airport = Airport.new
-    boeing = Plane.new
-    allow(airport).to receive(:stormy?).and_return(false)
-    airport.land(boeing)
-    expect(airport.planes).to include boeing
+  before(:each) do
+    @airport = Airport.new
+    @plane = Plane.new
   end
 
-  it "After instructing a plane to take off, I expect that plane to not be in the airport if the weather is not stormy" do 
-    airport = Airport.new
-    boeing = Plane.new
-    allow(airport).to receive(:stormy?).and_return(false)
-    airport.land(boeing)
-    airport.take_off
-    expect(airport.planes).to be_empty
-  end
+  describe '#land' do
 
-  it "Prevents landing a plane when the airport is full" do 
-    airport = Airport.new
-    allow(airport).to receive(:stormy?).and_return(false)
-    airport.capacity.times { airport.land Plane.new }
-    expect { airport.land Plane.new }.to raise_error "Airport is full"
-  end
+    context "When not stormy" do
 
-  it "You can set the capacity of the airport" do 
-    airport = Airport.new
-    airport.capacity = 10
-    expect(airport.capacity).to eq 10
-  end
+      before(:each) do
+        allow(@airport).to receive(:stormy?).and_return(false)
+      end
 
-  it "If the weather is stormy, you should not be able to take off" do 
-    airport = Airport.new
-    allow(airport).to receive(:stormy?).and_return(false)
-    airport.capacity.times { airport.land Plane.new }
-    allow(airport).to receive(:stormy?).and_return(true)
-    expect { airport.take_off }.to raise_error "Stormy weather, can't take off"
-  end
+      it { is_expected.to respond_to :land }
 
-  it "If the weather is stormy, you should not be able to land" do 
-    airport = Airport.new
-    allow(airport).to receive(:stormy?).and_return(true)
-    expect { airport.land(Plane.new) }.to raise_error "Stormy weather, can't land"
-  end
+      it "When I land a plane, I expect that plane to be at the airport" do 
+        @airport.land(@plane)
+        expect(@airport.planes).to include @plane
+      end
 
-  it "Cannot land a plane that has already landed" do 
-    airport = Airport.new
-    plane = Plane.new
-    allow(airport).to receive(:stormy?).and_return(false)
-    airport.land(plane)
-    expect { airport.land(plane) }.to raise_error "This plane has already landed"
-  end
+      it "Prevents landing a plane when the airport is full" do 
+        @airport.capacity.times { @airport.land Plane.new }
+        expect { @airport.land @plane }.to raise_error "Airport is full"
+      end
 
-  it "Cannot take off from an empty airport" do 
-    airport = Airport.new
-    allow(airport).to receive(:stormy?).and_return(false)
-    expect { airport.take_off }.to raise_error "Airport is empty, no planes can take off"
+      it "Cannot land a plane that has already landed" do 
+        @airport.land(@plane)
+        expect { @airport.land(@plane) }.to raise_error "This plane has already landed"
+      end
+
+    end
+
+    context "when stormy" do 
+        
+      before(:each) do
+        allow(@airport).to receive(:stormy?).and_return(true)
+      end
+
+      it "Plane should not be able to land" do 
+        expect { @airport.land(@plane) }.to raise_error "Stormy weather, can't land"
+      end
+
+    end
+
+  end
+  
+  describe '#take_off' do
+
+    context "when not stormy" do
+
+      before(:each) do
+        allow(@airport).to receive(:stormy?).and_return(false)
+      end
+
+      it { is_expected.to respond_to :take_off }
+
+      it "After instructing a plane to take off, I expect that plane to not be in the airport if the weather is not stormy" do 
+        @airport.land(@plane)
+        @airport.take_off
+        expect(@airport.planes).to be_empty
+      end
+
+      it "Cannot take off from an empty airport" do 
+        expect { @airport.take_off }.to raise_error "Airport is empty, no planes can take off"
+      end
+    
+    end
+      
+    context "when stormy" do 
+
+      before(:each) do
+        allow(@airport).to receive(:stormy?).and_return(true)
+      end
+
+      it "If the weather is stormy, you should not be able to take off" do 
+        allow(@airport).to receive(:stormy?).and_return(false)
+        @airport.capacity.times { @airport.land Plane.new }
+        allow(@airport).to receive(:stormy?).and_return(true)
+        expect { @airport.take_off }.to raise_error "Stormy weather, can't take off"
+      end
+
+    end    
+
+  end
+  
+  describe '#capacity' do
+    it "You can set the capacity of the airport" do 
+      @airport.capacity = 10
+      expect(@airport.capacity).to eq 10
+    end
+    
   end
 
 end
