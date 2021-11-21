28aab241554acd55a74be38af34e88a6395ed21a
Bryn <bryn2009@live.co.uk>
2021-11-21 15:40:36 +0000

    user story 5 & 6

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,19 +2,38 @@ require 'airport.rb'
 
 describe Airport do
     subject(:airport) { described_class.new(15) }
-    it 'instructs plane to land' do
-        expect(airport).to respond_to(:land).with(1).argument
-    end
+    let(:plane) { double :plane }
 
-    it 'instructs plane to takeoff' do
-        expect(airport).to respond_to(:takeoff).with(1).argument
-    end
-
-    it 'cannot land if airport full' do
-        plane = double :plane
-        15.times do
-            airport.land(plane)
+    describe '#land' do
+        it 'instructs plane to land' do
+            allow(airport).to receive(:stormy?).and_return false
+            expect(airport).to respond_to(:land).with(1).argument
         end
-        expect { airport.land(plane) }.to raise_error 'cannot land plane as airport full'
+
+        it 'cannot land if airport full' do
+            allow(airport).to receive(:stormy?).and_return false
+            15.times do
+                airport.land(plane)
+            end
+            expect { airport.land(plane) }.to raise_error 'cannot land plane as airport full'
+        end
+
+        it 'raises an error if asked to land when stormy' do
+            allow(airport).to receive(:stormy?).and_return true
+            expect {  airport.land(plane) }.to raise_error 'cannot land plane: its stormy'
+        end
+    end
+
+    describe '#takeoff' do
+        it 'instructs plane to takeoff' do
+            allow(airport).to receive(:stormy?).and_return false
+            expect(airport).to respond_to(:takeoff).with(1).argument
+        end
+
+        it 'raises an error if asked to take off when stormy' do
+            allow(airport).to receive(:stormy?).and_return true
+            expect { airport.takeoff(plane) }.to raise_error 'cannot land plane: its stormy'
+        end
     end
 end
+
