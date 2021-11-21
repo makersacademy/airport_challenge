ed67d96d0a9367b6dd78471b68f38112131da178
Inimesh <ijmeshoulam@gmail.com>
2021-11-21 17:49:43 +0000

    added funcitonality: Airport instance method 'hanger_full?'

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,5 +1,10 @@
 class Airport
   def initialize(capacity = 20)
-    
+    @capacity = capacity
+    @hanger = []
+  end
+
+  def hanger_full?
+    @hanger.length == @capacity
   end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,5 +1,20 @@
 require 'airport'
+require 'plane'
 
 describe Airport do
-  
+  describe '#hanger_full?' do
+    context 'hanger is not full' do
+      it 'detects when hanger is not full' do
+        expect(subject.hanger_full?).to be false
+      end
+
+    end
+
+    context 'hanger is full' do
+      it 'detects when hanger is full' do
+        airport = Airport.new(0)
+        expect(airport.hanger_full?).to be true
+      end
+    end
+  end
 end
