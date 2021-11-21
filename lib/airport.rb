50900f6579cc93148f8fa8652c30d9c59564886b
Bryn <bryn2009@live.co.uk>
2021-11-21 13:11:43 +0000

    User story 3

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,6 +1,12 @@
 class Airport
-    def land(plane)
+    def initialize(capacity)
+        @capacity = capacity
+        @planes_landed = []
+    end
 
+    def land(plane)
+        raise 'cannot land plane as airport full' if @planes_landed.length >= @capacity
+        @planes_landed << plane
     end
 
     def takeoff(plane)
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,12 +1,20 @@
 require 'airport.rb'
 
 describe Airport do
-    subject(:airport) { described_class.new }
+    subject(:airport) { described_class.new(15) }
     it 'instructs plane to land' do
         expect(airport).to respond_to(:land).with(1).argument
     end
 
     it 'instructs plane to takeoff' do
         expect(airport).to respond_to(:takeoff).with(1).argument
+    end
+
+    it 'cannot land if airport full' do
+        plane = double :plane
+        15.times do
+            airport.land(plane)
+        end
+        expect { airport.land(plane) }.to raise_error 'cannot land plane as airport full'
     end
 end
