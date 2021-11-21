be46c62a25c42ecc79c4f2782cf253a0cf2ffba6
Giorgi Gutsaev <giorgi.gutsaevi@gmail.com>
2021-11-21 16:05:45 +0000

    refactoring and clean up

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,12 +1,5 @@
 require 'airport'
-=begin
-# User Story 1 
-As an air traffic controller 
-So I can get passengers to a destination 
-I want to instruct a plane to land at an airport
-=end
 
-# Let's test that we have class Airport
 describe Airport do
 	subject(:jfk) {Airport.new}
 	let(:plane) {Plane.new}
