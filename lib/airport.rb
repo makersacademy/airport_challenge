22329cc68a5c30899c92a0b6b04680b7a0ffed92
Christopher <dagswebb@gmail.com>
2021-11-21 19:54:38 +0000

    Added capacity and takeoff method

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,8 +1,35 @@
 require_relative 'plane'
 
 class Airport
-    
-    def land(plane)
+
+attr_reader :planes
+CAPACITY = 1
+
+  def initialize (capacity = CAPACITY)
+    @planes = []
+    @planes_taken_off = []
+    @capacity = capacity
+  end
+
+  def land(plane)
+    if @planes.count == 0
+        @planes.push(plane)
+    else fail "Airport full"
     end
+  end
+
+  def takeoff(plane)
+    @planes.pop
+    puts "#{plane} has now taken off! There are currently #{@planes.count} planes in the airport."
+    @planes_taken_off << plane
+  end
+
+  #def full?
+    #if @planes.count >= @capacity
+      #true
+    #else
+      #false
+    #end
+  #end
 
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -3,12 +3,37 @@ require 'plane'
 
 describe Airport do
 
-    it "should be able to land" do
-        plane = Plane.new
-        airport = Airport.new
-        expect(airport).to respond_to :land
-    end
+  it "should be able to land" do
+    plane = Plane.new
+    airport = Airport.new
+    expect(airport).to respond_to :land
+  end
 
-it { is_expected.to respond_to(:land).with(1).argument }
+  it { is_expected.to respond_to(:land).with(1).argument }
+
+  it "should be able to instruct a plane to land" do
+    airport = Airport.new
+    plane = Plane.new
+    airport.land(plane)
+    expect(airport.planes).to include plane
+  end
+
+  it "should be able to instruct a plane to take off" do
+    airport = Airport.new
+    plane = Plane.new
+    airport.takeoff(plane)
+    expect(airport).to respond_to :takeoff
+  end
+
+  #it "raises error if pilot attempts to land and airport is full" do
+    #@planes == 1
+   # expect { Airport.new.land Plane.new }.to raise_error
+  #end
+
+  it "raise an error when you try to land a plane in a full airport" do
+    airport = Airport.new
+    Airport::CAPACITY.times { airport.land Plane.new } 
+    expect { airport.land Plane.new }.to raise_error 'Airport full'
+  end
 
 end
