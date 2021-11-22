3e69ddfab4b8fa52f51d6c3aafd26b503951be10
kehinde alaka <kennyalaka93@gmail.com>
2021-11-22 11:38:26 +0000

    made changes to unit test inline with peer review of code

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -6,23 +6,19 @@ describe Airport do
     expect(subject).to be_an_instance_of(Airport)
   end
   
-  it "allows a plane to take off from the airport" do
-    expect(subject).to respond_to(:take_off).with(1).argument
-  end
+  it { is_expected.to respond_to(:take_off).with(1).argument }
 
   before do
     allow(airport).to receive(:stormy?).and_return false
   end
 
   it "raises an error when landing a plane and airport is full" do
-    5.times { subject.land(Plane.new) }
+    Airport::DEFAULT_CAPACITY.times { subject.land(Plane.new) }
     expect { subject.land(Plane.new) }.to raise_error "Airport is Full"
   end
   
   describe "#land" do
-    it "can respond to laning a plane" do
-      expect(subject).to respond_to(:land).with(1).argument
-    end
+    it { is_expected.to respond_to(:land).with(1).argument }
     
     it "can store the landed plane in the airport" do
       plane = Plane.new
@@ -39,8 +35,8 @@ describe Airport do
   describe "#initilize" do
     it "will initilize with a default capacity of 5" do
       allow(airport).to receive(:stormy?).and_return false
-      5.times { subject.land(Plane.new) }
-      expect(subject.capacity).to eq 5
+      Airport::DEFAULT_CAPACITY.times { subject.land(Plane.new) }
+      expect(subject.capacity).to eq Airport::DEFAULT_CAPACITY
     end 
 
     it "will initilize with the capacity specified" do
