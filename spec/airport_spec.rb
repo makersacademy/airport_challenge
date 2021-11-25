b7a534dccb5995b6b1285a1815d52cc65d4a1ba8
zidehen <zoeidehen@gmail.com>
2021-11-25 12:00:32 +0000

    Defined airport as a named subject to refactor tests

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,11 +2,12 @@
 require 'airport'
 
 describe Airport do
-  
+  subject(:airport) {described_class.new}
+
   describe '#land(plane)' do
     it 'lands a plane' do
       plane = Plane.new
-      expect(subject.land(plane)).to eq plane
+      expect(airport.land(plane)).to eq plane
     end
   end
 
