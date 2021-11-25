97edee93f022852ab8c4df43f95d5f46007d7344
zidehen <zoeidehen@gmail.com>
2021-11-25 12:50:26 +0000

    User story 3 updated-initialized capacity at 5 and added @planes array to store planes along with unit test

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,16 +1,20 @@
 require_relative 'plane'
 
 class Airport
+  def initialize(capacity)
+    @capacity = 5
+    @planes = []
+  end
 
   attr_reader :plane
 
   def land(plane)
-    fail 'Airport is full' if @plane
-    @plane = plane 
+    fail 'Airport is full' if @planes.length >= @capacity
+    @planes << plane 
   end 
 
-  def take_off
-    @plane
+  def take_off #(plane)
+    @plane = plane
     'Plane has taken off'
   end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,28 +1,44 @@
+# describe Airport do
+#   it 'can respond to plane' do
+#     airport = Airport.new
+#     plane = Plane.new
+#     expect(airport).to respond_to(:plane)
+#   end
 
+#   it 'should land a plane' do
+#     airport = Airport.new
+#     plane = Plane.new
+#     expect(airport).to respond_to(:plane(land)) # .with(1).argument
+#   end
+# end
 require 'airport'
 
 describe Airport do
-  subject(:airport) {described_class.new}
+  subject(:airport) {described_class.new(5)}
 
   describe '#land(plane)' do
-    it 'lands a plane' do
+    it { is_expected.to respond_to(:land).with(1).argument }
+    
+    it "can store the landed plane in the airport" do
       plane = Plane.new
+      plane = airport.land(plane)
       expect(airport.land(plane)).to eq plane
+    end
+
+    it 'raises an error when the airport is at full capacity' do
+      plane = Plane.new
+      5.times { subject.land(Plane.new) }
+      expect { subject.land(Plane.new) }.to raise_error 'Airport is full'
     end
   end
 
-  it { is_expected.to respond_to :take_off } 
+  it { is_expected.to respond_to(:take_off)} #.with(1).argument 
 
   it 'confirms plane is no longer at the airport' do
     plane = Plane.new
     expect(subject.take_off).to eq 'Plane has taken off'
   end  
   
-  it 'raises an error when the airport is full' do
-    plane = Plane.new
-    airport = Airport.new
-    subject.land(plane)
-    expect { subject.land(plane) }.to raise_error 'Airport is full'
-  end
+  
 
 end
