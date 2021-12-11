30d8edc55f40fd5fff20ad0200e7073fa38785dc
icvotria <icvotria@gmail.com>
2021-12-11 17:45:30 +0000

    added default capacity, made planes array, made Airport method land(plane) more meaningful

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,12 +1,22 @@
 class Airport
+  DEFAULT_CAPACITY = 10
+
+  attr_reader :capacity
+
+  def initialize
+    @capacity = DEFAULT_CAPACITY
+    @planes = []
+  end
+
   def confirm_takeoff
   end
 
   def full?
-    true
+    @planes.size >= capacity
   end
 
   def land(plane)
     raise 'No clearance to land: Airport is full' if full?
+    @planes << plane
   end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -7,8 +7,13 @@ describe Airport do
 
   it { is_expected.to respond_to(:confirm_takeoff) }
   it { is_expected.to respond_to(:full?) }
+  
+  it 'has a default capacity' do
+    expect(subject.capacity).to eq Airport::DEFAULT_CAPACITY
+  end
 
   it 'raises error if a plane tries to land at a full airport' do
+    10.times { airport.land(plane) }
     message = 'No clearance to land: Airport is full'
     expect { airport.land(plane) }.to raise_error message
   end
