4dd12b35026686ac2ccb031d81f80e00e12fec9c
mikitaro-27 <matt.diak@gmail.com>
2021-12-11 19:41:50 +0000

    add land_plane method that returns a plane

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,3 +1,8 @@
+require './lib/plane'
+
 class Airport
-  
+  # We wanna land a plane
+  def land_plane(plane)
+    plane
+  end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,5 +1,13 @@
 require 'airport'
 
 describe Airport do
-  
+  let(:airport) { Airport.new }
+  let(:plane) { Plane.new }
+
+  it { is_expected.to respond_to(:land_plane).with(1).argument }
+
+  it 'returns a plane, after landing' do
+    expect(airport.land_plane(plane)).to eq plane
+  end
+
 end
