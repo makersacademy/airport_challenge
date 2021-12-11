f288125318634ce421a1f520443d8ac6488a1ca1
Haydn Stokes <haydnstokes92@gmail.com>
2021-12-11 13:57:06 +0000

    Added feature that stops planed landing if airport is full

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,6 +1,12 @@
 class Airport
+  def initialize(capacity)
+  @capacity = capacity
+  @planes = []
+  end
 
   def land(plane)
+    raise 'Airport is full' if @planes.length >= @capacity
+    @planes << plane
   end
 
   def take_off(plane)
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,12 +1,20 @@
 require 'airport'
 
 describe Airport do
+  subject(:airport) { described_class.new(20) }
   # Test - Instruct plane to land at airport
   it 'instructs plane to land' do
-    expect(subject).to respond_to(:land)
+    expect(airport).to respond_to(:land)
   end
-  # Test - instruct a plane to takeoff
+  # Test - Instruct a plane to takeoff
   it 'instructs plane to takeoff' do
-    expect(subject).to respond_to(:take_off)
+    expect(airport).to respond_to(:take_off)
+  end
+  # Test - Stop planes from landing when airport is full
+  it 'does not allow plane to land if Airport is full' do
+    20.times do
+      airport.land(:plane)
+    end
+    expect { airport.land(:plane) }.to raise_error 'Airport is full'
   end
 end
