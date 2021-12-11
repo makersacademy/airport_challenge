cbe210b867e13f1ff0c232a4f1aba1b6e429c94d
mikitaro-27 <matt.diak@gmail.com>
2021-12-11 20:40:08 +0000

    add readbility to airport_spec.rb-; describe block for each method

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -14,21 +14,24 @@ describe Airport do
     expect(airport.capacity).to eq(2)
   end
 
-  it { is_expected.to respond_to(:land_plane).with(1).argument }
-  
-  it 'returns a plane, after landing' do
-    expect(airport.land_plane(plane)).to eq plane
-  end
+  describe '#land_plane(plane)' do
+    it { is_expected.to respond_to(:land_plane).with(1).argument }
+    
+    it 'returns a plane, after landing' do
+      expect(airport.land_plane(plane)).to eq plane
+    end
 
-  it 'is full; raises an error when landing' do
-    airport.land_plane(plane)
-    expect { airport.land_plane(plane) }.to raise_error("Permission not granted. The airport is full.")
+    it 'is full; raises an error when landing' do
+      airport.land_plane(plane)
+      expect { airport.land_plane(plane) }.to raise_error("Permission not granted. The airport is full.")
+    end
   end
-  
-  it { is_expected.to respond_to(:launch_plane).with(1).argument }
-  
-  it 'returns a plane, after take off' do
-    expect(airport.launch_plane(plane)).to eq plane
+    
+  describe '#launch_plane(plane)' do
+    it { is_expected.to respond_to(:launch_plane).with(1).argument }
+    
+    it 'returns a plane, after take off' do
+      expect(airport.launch_plane(plane)).to eq plane
+    end
   end
-
 end
