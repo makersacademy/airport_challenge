4097443e243d47d90646695f789360fee688e45b
Jason Humbert <jasonhumbert@outlook.com>
2021-12-12 22:40:54 +0000

    Add test to check Airport responds to plane and checks plane method returns a landed plane

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,11 +2,20 @@ require 'airport'
 
 describe Airport do
   it { is_expected.to respond_to(:land).with(1).argument }
+  it { is_expected.to respond_to :plane }
+
+  let (:plane) { Plane.new }
 
   describe '#land' do
     it 'instructs a plane to land' do
-      plane = Plane.new
       expect(subject.land(plane)).to eq plane
+    end
+  end
+
+  describe '#plane' do
+    it 'returns a landed plane' do
+      subject.land(plane)
+      expect(subject.plane).to be plane
     end
   end
 end
