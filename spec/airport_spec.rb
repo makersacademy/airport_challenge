cfeb9f21d911e5fb9ba72d72d4998a35c09362fa
Ben Coyne <bencoyne123@yahoo.co.uk>
2021-12-12 12:50:34 +0000

    General refactor in airport_spec (double and let)

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,12 +2,15 @@ require 'airport'
 require 'plane'
 
 describe Airport do
+  subject(:airport) {described_class.new}
+  let(:plane) { double :plane }
+  
   it { is_expected.to respond_to(:instruct_landing).with(1).argument }
   it { is_expected.to respond_to :instruct_take_off }
   it { is_expected.to respond_to :check_weather }
   
   it "has a defualt capacity" do
-    expect(subject.capacity).to eq(Airport::DEFAULT_CAPACITY)
+    expect(airport.capacity).to eq(Airport::DEFAULT_CAPACITY)
   end
 
   it "default capacity can be overwritten for new airports" do
@@ -16,37 +19,36 @@ describe Airport do
   end
 
   describe "#instruct_landing" do
+    
     it "raises an error if the airport is full" do
-      allow(subject).to receive(:check_weather).and_return('Sunny')
-
-      subject.capacity.times { subject.instruct_landing(Plane.new) }
-      expect { subject.instruct_landing(Plane.new) }.to raise_error "AIRPORT AT FULL CAPACITY!"
+      allow(airport).to receive(:check_weather).and_return('Sunny')
+      airport.capacity.times { airport.instruct_landing(plane) }
+      expect { airport.instruct_landing(plane) }.to raise_error "AIRPORT AT FULL CAPACITY!"
     end
 
     it "raises an error when the weather is stormy" do
       london_heathrow = Airport.new
       allow(london_heathrow).to receive(:check_weather).and_return('Stormy')
-      expect { london_heathrow.instruct_landing(Plane.new) }.to raise_error "STORMY WEATHER: CANNOT LAND"
+      expect { london_heathrow.instruct_landing(plane) }.to raise_error "STORMY WEATHER: CANNOT LAND"
     end
   end
 
   describe "#instruct_take_off" do
     it "can instruct a plane to take_off" do
-      allow(subject).to receive(:check_weather).and_return('Sunny')
+      allow(airport).to receive(:check_weather).and_return('Sunny')
 
-      boeing_737  = Plane.new
-      subject.instruct_landing(boeing_737)
-      expect(subject.instruct_take_off).to eq(boeing_737)
+      airport.instruct_landing(plane)
+      expect(airport.instruct_take_off).to eq(plane)
     end
 
     it "raises error when the weather is stormy" do
-      allow(subject).to receive(:check_weather).and_return('Stormy')
-      expect { subject.instruct_take_off }.to raise_error "STORMY WEATHER: CANNOT TAKE OFF"
+      allow(airport).to receive(:check_weather).and_return('Stormy')
+      expect { airport.instruct_take_off }.to raise_error "STORMY WEATHER: CANNOT TAKE OFF"
     end
 
     it "does not raise error when the weather is sunny" do
-      allow(subject).to receive(:check_weather).and_return('Sunny')
-      expect { subject.instruct_take_off }.to_not raise_error 
+      allow(airport).to receive(:check_weather).and_return('Sunny')
+      expect { airport.instruct_take_off }.to_not raise_error 
     end
   end
 end
