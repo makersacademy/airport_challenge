ca699a564cfd9bfb73e5b71458ea425915bde075
OrganicAC <anthonychaplin17@gmail.com>
2021-12-12 15:17:37 +0000

    Step 3 completed

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,10 +1,16 @@
 class Airport
-  
-  def land
+
+  def initialize
+    @planes = []
+  end
+
+  def land(plane)
+    fail 'At maximum capacity' if @planes.count >= 10
+    @planes << plane
   end
 
   def take_off
     true
-  end 
+  end
 
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,4 +1,5 @@
 require 'airport'
+require 'plane'
 
 describe Airport do
   describe "Land a plane at the Airport" do
@@ -7,13 +8,19 @@ describe Airport do
   end
 
   describe "Take off from the Airport and confirm has left" do
-      airport = Airport.new
-      it { is_expected.to respond_to :take_off }     
+    airport = Airport.new
+    it { is_expected.to respond_to :take_off }     
   end 
 
   it "confirms plane has taken off" do
     airport = Airport.new
     expect(airport.take_off).to eq true
   end 
-
+  
+  describe "Max Cap error" do
+    it "prevents landing when max capacity" do
+      10.times { subject.land Plane.new }
+      expect{ subject.land Plane.new }.to raise_error 'At maximum capacity'
+    end
+  end
 end
