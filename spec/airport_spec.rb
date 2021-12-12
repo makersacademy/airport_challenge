168d6f329cb092c8dce86b831e02852edf128744
Jason Humbert <jasonhumbert@outlook.com>
2021-12-12 22:12:15 +0000

    Add test to check if land method takes a Plane object as an argument

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -3,4 +3,10 @@ require 'airport'
 describe Airport do
   it { is_expected.to respond_to(:land).with(1).argument }
 
+  describe '#land' do
+    it 'instructs a plane to land' do
+      plane = Plane.new
+      expect(subject.land(plane)).to eq plane
+    end
+  end
 end
