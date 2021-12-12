9f842c1ac1cd65a3039a5779d9c66729d6c480b4
Your Name <vanessaflorio1997@gmail.com>
2021-12-12 17:14:43 +0000

    Update 3. user story

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -8,9 +8,6 @@ describe Airport do
     expect(airport).to respond_to(:land).with(1).argument
   end
 
-  it 'lets plane take off' do
-    expect(airport).to respond_to(:take_off).with(1).argument
-  end
 
   it 'prevents landing when airport is at full capacity' do
     plane = double :plane
@@ -20,4 +17,9 @@ describe Airport do
     end 
     expect { airport.land(plane) }.to raise_error 'Caution: Airport full, plane cannot land. MAX capacity: 10'
   end
+
+  it 'lets plane take off' do
+    expect(airport).to respond_to(:take_off).with(1).argument
+  end 
+  
 end
