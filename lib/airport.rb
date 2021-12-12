790630fedebb8248c3b38b636f6a3dcba4f397c0
robinucar <mehmet.ucar@imediapro.co.uk>
2021-12-12 17:11:43 +0000

    raise an error when capacity is full, defined capacity variable which is accessible reading and wrighting and defined full method as private

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,15 +1,22 @@
 #Airport class
 class Airport
-  attr_reader :planes
+  attr_accessor :planes, :capacity
   def initialize
     #create a planes array
     @planes = []
+    @capacity = 5 # default capacity is 5
   end
   def land(plane)
+    #error when is full
+    fail 'Airport is currently full. There is not any space for landing...' if full?
     #and plane will be pushed to planes array
     @planes << plane
   end
   def take_off(plane)
     @planes.delete(plane)
+  end
+  private
+  def full?
+    @planes.count >= capacity
   end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -10,6 +10,11 @@ context Airport do
       subject.land(plane)
       expect(subject.planes).to include(plane)
     end
+    it 'should not land any plane when airport is full' do
+      plane = Plane.new
+      subject.capacity.times {subject.land(plane)}
+      expect {subject.land(plane)}.to raise_error('Airport is currently full. There is not any space for landing...')
+    end
   end
   #take_off method
   describe '#take_off' do
