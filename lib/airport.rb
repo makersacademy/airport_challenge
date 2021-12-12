218f2d064e5ba6b996d95da2c28c3ec12e623451
E Cho <helloecho12345@gmail.com>
2021-12-12 23:33:55 +0000

    add capacity at airport

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,8 +1,12 @@
 class Airport
 
+  def initialize
+    @plane = []
+  end
+
   def land(plane)
-    fail 'Airport is full - plane cannot land' if @plane
-    @plane = plane
+    fail 'Airport is full - plane cannot land' if @plane.count >= 20
+    @plane << plane
   end
 
   def takeoff
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -11,7 +11,7 @@ describe Airport do
   end
 
   it "prevents a plane from landing when it is full" do
-    subject.land(Plane.new)
+    20.times { subject.land(Plane.new) }
     expect { subject.land Plane.new }.to raise_error 'Airport is full - plane cannot land'
   end
 end
