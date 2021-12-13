fb3e48d0b92bbb40f6725dce2a90edeb0c1a3302
Jason Humbert <jasonhumbert@outlook.com>
2021-12-13 00:55:47 +0000

    Wrap planes variable to make tests pass

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,9 +1,17 @@
 
 
 class Airport
-  attr_reader :plane
+  attr_reader :planes
+
+  def initialize
+    @planes = []
+  end
   
   def land(plane)
-    @plane = plane
+    planes << plane
+  end
+
+  def take_off
+    planes.pop
   end
 end
