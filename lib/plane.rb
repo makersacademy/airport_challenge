7d5601e3de55986ce419f906db101edd131c1a60
Joy Omodiale <remen.omodiale@outlook.com>
2021-12-13 08:49:55 +0000

    update

--- lib/plane.rb
+++ lib/plane.rb
@@ -1,13 +1,17 @@
 class Plane
-  # capacity is airport hangar size
-  # weather is stormy
+
+  # def initialize
+  #   @in_airport = true
+  # end
 
   def take_off
-    
+    Plane.new
+    # @in_airport = false
   end
 
   def land
-  
+    Plane.new
+    # @in_airport = true
   end
 
 end
