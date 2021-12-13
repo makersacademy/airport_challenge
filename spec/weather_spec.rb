b862219c7084f812f3af6cc49fced9b0051afcf4
Jason Humbert <jasonhumbert@outlook.com>
2021-12-13 08:27:17 +0000

    Add test to check if set method returns 0 or 1

--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -2,10 +2,17 @@ require 'weather'
 
 describe Weather do
   it { is_expected.to respond_to :stormy? }
+  it { is_expected.to respond_to :set }
 
   describe '#stormy?' do
     it 'returns true if weather is stormy' do
       expect(subject.stormy?).to be true
+    end
+  end
+
+  describe '#set' do
+    it 'randomly sets the weather condition' do
+      expect(subject.set).to eq(0).or eq(1)
     end
   end
 
