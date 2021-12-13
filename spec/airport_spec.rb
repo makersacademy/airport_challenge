30e20a50b940b9b8a06ff01585791f1505fb5c68
Jason Humbert <jasonhumbert@outlook.com>
2021-12-13 00:40:57 +0000

    Add test to check take_off method removes a plane from Airport

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,20 +2,34 @@ require 'airport'
 
 describe Airport do
   it { is_expected.to respond_to(:land).with(1).argument }
-  it { is_expected.to respond_to :plane }
+  it { is_expected.to respond_to :planes }
+  it { is_expected.to respond_to(:take_off) }
 
   let (:plane) { Plane.new }
 
   describe '#land' do
     it 'instructs a plane to land' do
-      expect(subject.land(plane)).to eq plane
+      expect(subject.land(plane)).to eq [plane]
     end
   end
 
-  describe '#plane' do
-    it 'returns a landed plane' do
+  describe '#planes' do
+    it 'returns landed planes' do
       subject.land(plane)
-      expect(subject.plane).to be plane
+      expect(subject.planes).to eq [plane]
+    end
+  end
+
+  describe '#take_off' do
+    it 'instructs a plane to take off' do
+      subject.land(plane)
+      expect(subject.take_off).to eq plane
+    end
+
+    it 'removes a plane' do
+      subject.land(plane)
+      subject.take_off
+      expect(subject.planes).not_to include(plane)
     end
   end
 end
