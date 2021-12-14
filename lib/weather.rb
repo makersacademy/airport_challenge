d7b4f325058472567eb2b9d12f40e41131fe5d87
kat-mcbrooks <katharinebrooks@hotmail.co.uk>
2021-12-14 10:14:49 +0000

    refactoring tests to use let:plane {Plane.new} throughout the tests within describe

--- lib/weather.rb
+++ lib/weather.rb
@@ -1,6 +1,5 @@
 class Weather # weather needs its own class to satisfy SRP i.e. so Airport isn't responsible for it
   def stormy? 
     rand(5) == 4 # as it's stormy in rare cases, stormy? will only return true if rand(5) generates 
-                  # 4 i.e. probability 4 in 5. 
-  end
+  end              # 4 i.e. probability 4 in 5. 
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,18 +1,18 @@
 require 'airport'
 
 describe Airport do
+  let(:plane) { Plane.new} # everytime we call plane, Plane.new will be called (can refactor the below to replace all Plane.new)
   it { is_expected.to respond_to(:land).with(1).argument } # is this test redundant now because i have tested this via the 'lands a plane test'
   it { is_expected.to respond_to(:depart).with(1).argument } # also redundant?
 
   it 'lands a plane' do
-    plane = Plane.new
     allow_any_instance_of(Weather).to receive(:stormy?).and_return(false)
     expect(subject.land(plane)).to eq [plane] # tests that land method on an airport instance will 'store' the plane instance, add it to the planes array and return the plane so that we can see its been landed at teh airport
   end
 
+  describe 'departing planes' # set context of departing 
   it 'departs a plane' do
     allow_any_instance_of(Weather).to receive(:stormy?).and_return(false)
-    plane = Plane.new
     subject.land(plane)
     expect(subject.depart(plane)).to eq [] # tests that depart method will work on aiport and will confirm that this particular plane has left the airport after departed
   end
@@ -20,20 +20,19 @@ describe Airport do
   describe '#lands a plane' do
     it 'raises an error if airport is full' do
       allow_any_instance_of(Weather).to receive(:stormy?).and_return(false)
-      subject.capacity.times { subject.land(Plane.new) }
-      expect { subject.land(Plane.new) }.to raise_error "Airport is full! No planes can land here." # tries to land 31st plane, and should get error
+      subject.capacity.times { subject.land(plane) }
+      expect { subject.land(plane) }.to raise_error "Airport is full! No planes can land here." # tries to land 31st plane, and should get error
     end
   end
 
   describe '#departs a plane' do
     it 'raises an error if this plane is not in the airport' do
       allow_any_instance_of(Weather).to receive(:stormy?).and_return(false)
-      expect { subject.depart(Plane.new) }.to raise_error "This plane is not in the airport!" # should get error if attempting to depart a plane which is not in the airport
+      expect { subject.depart(plane) }.to raise_error "This plane is not in the airport!" # should get error if attempting to depart a plane which is not in the airport
     end
   end
 
   describe 'initialisation' do
-    let(:plane) { Plane.new } # let allows variable to be shared between all it examples i.e. all tests can now use 'plane' instead of writing plane = Plane.new each time
     it 'defaults capacity' do
       allow_any_instance_of(Weather).to receive(:stormy?).and_return(false)
       described_class::DEFAULT_CAPACITY.times do
@@ -45,12 +44,12 @@ describe Airport do
 
   it 'stormy weather means planes cannot land' do
     allow_any_instance_of(Weather).to receive(:stormy?).and_return(true)
-    expect { subject.land(Plane.new) }.to raise_error "It's stormy. Planes cannot land at this time."
+    expect { subject.land(plane) }.to raise_error "It's stormy. Planes cannot land at this time."
   end
 
   it 'stormy weather means planes cannot depart' do
     allow_any_instance_of(Weather).to receive(:stormy?).and_return(true)
-    expect { subject.depart(Plane.new) }.to raise_error "It's stormy. Planes cannot depart at this time."
+    expect { subject.depart(plane) }.to raise_error "It's stormy. Planes cannot depart at this time."
   end
 
 end
