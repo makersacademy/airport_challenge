cee5f9a785e2aee8efea107655c7c077d80f6543
Aadam Ali <ali.aadam@outlook.com>
2021-12-17 12:09:18 +0000

    refactored weather check

--- lib/airport.rb
+++ lib/airport.rb
@@ -9,13 +9,23 @@ class Airport
   end
 
   def land(plane, weather)
-    fail "Can not land, weather is stormy" if weather.stormy? == true
-    fail "Airport full" if @planes.count == DEFAULT_CAPACITY
+    fail "Can not land, weather is stormy" if stormy?(weather)
+    fail "Airport full" if full?
     @planes << plane
   end
 
   def take_off(weather)
-    fail "Can not take off, weather is stormy" if weather.stormy? == true 
+    fail "Can not take off, weather is stormy" if stormy?(weather)
     @planes.pop
+  end
+
+  private
+
+  def full?
+    @planes.count == capacity
+  end
+
+  def stormy?(weather)
+    weather.stormy?
   end
 end
