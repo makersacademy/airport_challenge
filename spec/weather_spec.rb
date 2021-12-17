67dc0d7a88e54615798ac5c52513357a2611eb0a
Aadam Ali <ali.aadam@outlook.com>
2021-12-17 15:30:08 +0000

    test for random num weather generation

--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -4,4 +4,10 @@ describe Weather do
   it "responds to stormy?" do
     expect(subject).to respond_to :stormy?
   end
+
+  it "responds with true when rand num is divisible by 3" do
+    weather = Weather.new
+    weather.stub(:rand_num).and_return(3)
+    expect(weather.stormy?).to eq true
+  end
 end
