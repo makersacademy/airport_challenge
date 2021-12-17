b9980833fc8672b2535d38cbd5b2903558619c51
Marcus <marcusventin@gmail.com>
2021-12-17 10:06:39 +0000

    Added airport capacity

--- lib/airport.rb
+++ lib/airport.rb
@@ -4,10 +4,15 @@ class Airport
 
   def initialize
     @hangar = []
+    @capacity = 5
   end
 
   def land(plane)
+    fail "Airport is full" if @hangar.count >= @capacity
     @hangar << plane
   end
 
+  def take_off(plane)
+    @hangar.delete(plane)
+  end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -11,6 +11,22 @@ describe Airport do
       plane = Plane.new
       expect(subject.land(plane)[0]).to eq plane
     end
+
+    it "raises an error when capacity is reached" do
+      5.times { subject.land(Plane.new) }
+      expect{ subject.land(Plane.new) }.to raise_error "Airport is full"
+    end
   end
 
+  describe "#take_off" do
+    it "responds to take_off" do
+      expect { subject.to respond_to(:take_off).with(1).argument }
+    end
+
+    it "removes a plane from the hangar" do
+      plane = Plane.new
+      subject.land(plane)
+      expect(subject.take_off(plane)).to eq plane
+    end
+  end
 end
