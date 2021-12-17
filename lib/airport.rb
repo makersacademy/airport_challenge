e2c67223d2de6a896f15708558db1cc1b341069f
JAL Williams <just.williams04@gmail.com>
2021-12-17 13:55:45 +0000

    Completed 4/6 user stories and have accounted for edge cases

--- lib/airport.rb
+++ lib/airport.rb
@@ -2,18 +2,21 @@ require_relative = 'plane'
 
 class Airport
   attr_reader :plane, :capacity
+  DEFAULT_CAPACITY = 10
 
-  def initialize 
+  def initialize (capacity = DEFAULT_CAPACITY)
     @plane = []
-    @capacity = 10
+    @capacity = capacity
   end
 
   def land (plane)
     fail "Airport is full" if @plane.length > @capacity 
+    fail "Plane already laneded" if @plane.index(plane) != nil
     @plane.push(plane)
   end
 
-  def take_off(plane)
+  def take_off (plane)
+    fail "Plane not in airport, so can't take off" if @plane.index(plane) == nil
     puts "#{plane} has taken off"
     index = @plane.index(plane)
     @plane.delete_at(index)
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -13,6 +13,16 @@ describe Airport do
     expect(subject).to respond_to(:take_off).with(1).argument
   end
 
+  it 'raises an error if a plane that never lands in aiport tries to  take off' do
+    airport1 = Airport.new
+    airport2 = Airport.new
+    plane1 = Plane.new
+    plane2 = Plane.new
+    airport1.land(plane1)
+    airport2.land(plane2)
+    expect{airport2.take_off(plane1)}.to raise_error "Plane not in airport, so can't take off" 
+  end
+
   # Basic assumption that the airport can only hold 1 plane at a time
   it 'raises an error when planes from landing if aiport is full (ie. at capacity)' do
     # plane1 = Plane.new
@@ -27,9 +37,15 @@ describe Airport do
     expect(subject.capacity).to eq 10
   end
 
-  # it 'can over-ride the default capacity if wanted' do
-  #   tweaked_capacity = 
-  #   expect(airport.capacity).to eq 20
-  # end
+  it 'can over-ride the default capacity if wanted' do
+    airport = Airport.new(20) 
+    expect(airport.capacity).to eq 20
+  end
+
+  it 'cannot have the same plane land twice' do
+    plane1 = Plane.new
+    subject.land(plane1)
+    expect{subject.land(plane1)}.to raise_error "Plane already laneded"
+  end
 
 end
