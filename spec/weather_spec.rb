11eee3ad56194f815a6459a5fd4a2f457fcf5e29
Aadam Ali <ali.aadam@outlook.com>
2021-12-17 15:34:41 +0000

    updated test for rand_num method

--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -5,9 +5,19 @@ describe Weather do
     expect(subject).to respond_to :stormy?
   end
 
+  it "responds to rand_num" do
+    expect(subject).to respond_to :rand_num
+  end
+
   it "responds with true when rand num is divisible by 3" do
     weather = Weather.new
     weather.stub(:rand_num).and_return(3)
     expect(weather.stormy?).to eq true
+  end
+
+  it "responds with false when rand num is not divisible by 3" do
+    weather = Weather.new
+    weather.stub(:rand_num).and_return(10)
+    expect(weather.stormy?).to eq false
   end
 end
