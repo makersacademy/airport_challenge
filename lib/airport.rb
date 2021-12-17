8d03336f171a4673a7e84fd18ec9598b3eb7f818
Aadam Ali <ali.aadam@outlook.com>
2021-12-17 11:46:17 +0000

    made capacity accessible and variable

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,9 +1,15 @@
 class Airport
-  def initialize
+  attr_accessor :capacity
+
+  DEFAULT_CAPACITY = 100
+
+  def initialize(capacity=DEFAULT_CAPACITY)
     @planes = []
+    @capacity = capacity
   end
 
   def land(plane)
+    fail "Airport full" if @planes.count == DEFAULT_CAPACITY
     @planes << plane
   end
 
