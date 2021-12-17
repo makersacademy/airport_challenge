8b39bb7e5f8cf8ef25d519ee3a52904bef344cd2
Alice Gray <alicegray33@gmail.com>
2021-12-17 11:00:01 +0000

    Changing test syntax to make rubocop happy

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -1,11 +1,11 @@
 require 'plane'
 
 describe Plane do
-  let(:airport) {Airport.new}
+  let(:airport) { Airport.new }
+
   it { is_expected.to respond_to(:land).with(1).argument }
   describe '#land' do
     it 'lands at an airport' do
-      
       expect(subject.land(airport)).to eq airport  
     end
   end
@@ -13,7 +13,6 @@ describe Plane do
   it { is_expected.to respond_to(:take_off).with(1).argument }
   describe '#take_off' do
     it 'lands at an airport' do
-      
       expect(subject.take_off(airport)).to eq airport  
     end
   end
