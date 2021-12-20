f85e293c5c707176649c201c3f518ff08d047300
jyoung-gds <jonathan.young@digital.cabinet-office.gov.uk>
2021-12-20 10:06:46 +0000

    Refactored the Weather class code

--- lib/weather.rb
+++ lib/weather.rb
@@ -3,8 +3,7 @@ class Weather
   attr_reader :weather_indicator
 
   def initialize
-    storm_selector = rand(1..20)
-    if storm_selector == 20
+    if rand(1..20) == 20
       @weather_indicator = "Storm"
     else
       @weather_indicator = "Clear"
