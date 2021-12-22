3e3860cd44afe1e283f1eaeff8264e3e8686ae92
Susana <susanamncwabe@outlook.com>
2021-12-22 09:43:13 +0000

    Weather with return in the test

--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -4,5 +4,7 @@ describe Weather do
   it "tells if the weather is sunny or stormy" do
     weather = Weather.new
     expect(weather).to respond_to(:weather_today)
+    allow(weather).to receive(:weather_today).with(true).and_return("sunny")
+    allow(weather).to receive(:weather_today).with(false).and_return("stormy")
   end
 end
