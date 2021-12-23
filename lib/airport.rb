6f3a00d29c57e5bbc3ef794cdf13c9cd8deabd98
samir <s-malik@live.co.uk>
2021-12-23 16:12:28 +0000

    Hangar feature added

--- lib/airport.rb
+++ lib/airport.rb
@@ -2,14 +2,18 @@ class Airport
     def max_capacity
     end 
 
+    def initialize 
+        @planehangar = []
+    end 
+
     def land(plane)
-        fail 'Airport Full' if @plane
-        @plane = plane
+        fail 'Airport Full' if @planehangar.count >= 3
+        @planehangar << plane
     end 
 
     def takeoff(plane)
-        fail 'No planes for takeoff' unless @plane
-        @plane
+        fail 'No planes for takeoff' if @planehangar.empty?
+        @planehangar.pop
     end 
 
 end 
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -6,25 +6,37 @@ describe Airport do
     it 'responds to max capacity' do
         expect(subject).to respond_to :max_capacity
     end 
+
     it 'lands plane' do 
         plane = Plane.new
-        expect(subject.land(plane)).to eq plane
+        expect(subject.land(plane)).to eq [plane]
     end 
+
     it 'takeoff plane' do
         plane = Plane.new
         subject.land(plane)
         expect(subject.takeoff(plane)).to eq plane
     end 
+
     describe '#take_off' do
         it 'raises error when there are no planes for takeoff' do
-            expect { subject.takeoff(plane) }. to raise_error 'No planes for takeoff'
+            expect { subject.takeoff(plane) }.to raise_error 'No planes for takeoff'
         end 
     end 
+    
+    #This test is being commented out - as this test is just to test that code works for 1 plane capacity. 
 
-    describe '#fullairport' do
+    #context '#fullairport' do
+     #   it 'raises an error when full' do
+      #      subject.land(Plane.new)
+       #     expect { subject.land(plane) }.to raise_error 'Airport Full'
+        #end 
+    # end 
+
+    context 'when airport is at max capacity' do
         it 'raises an error when full' do
-            subject.land(Plane.new)
-            expect { subject.land(plane) }. to raise_error 'Airport Full'
+            3.times { subject.land Plane.new }
+            expect { subject.land Plane.new }.to raise_error 'Airport Full'
         end 
     end 
 end 
