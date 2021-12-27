97850f4bc1117805e68c1cc698b001b089d74b76
Blessing <blessing@Blessings-Air.broadband>
2021-12-27 22:25:42 +0000

    refacto previous airport  unit tests

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -4,20 +4,31 @@ require 'airport'
 describe Airport do 
     subject(:airport) { described_class.new(20) }
 
-    it 'instructs a plane to land (responds to land method)' do 
-        expect(airport).to respond_to(:land).with(1).argument
-    end 
+    # creating a double for the plane class
+    let(:plane) { double :plane }
+    
+    describe '#land' do 
 
-    it 'instructs a plane to take off' do 
-        expect(airport).to respond_to(:take_off).with(1).argument
-    end 
-
-    it 'does not allow landing when at capacity' do
-        # creating a double for the plane class
-        plane = double :plane
-        20.times do
-            airport.land(plane)
+        it 'instructs a plane to land (responds to land method)' do 
+            expect(airport).to respond_to(:land).with(1).argument
         end 
-        expect { airport.land(plane) }.to raise_error "Airport is full, cannot land the plane"
+
+        context 'when at capacity/full' do
+            it 'does not allow landing and raises an error' do
+                20.times do
+                    airport.land(plane)
+                end 
+                expect { airport.land(plane) }.to raise_error "Airport is full, cannot land the plane"
+            end 
+        end
     end 
+
+    describe '#take_off' do 
+
+        it 'instructs a plane to take off' do 
+            expect(airport).to respond_to(:take_off).with(1).argument
+        end 
+
+    end
+
 end 
