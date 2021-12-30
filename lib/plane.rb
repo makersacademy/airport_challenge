ad2b69bfba416d7cf8ded8c57f605a749578bc31
Alice Gray <alicegray33@gmail.com>
2021-12-30 08:59:39 +0000

    raises error when plane taking off from airport its not in

--- lib/plane.rb
+++ lib/plane.rb
@@ -10,9 +10,9 @@ class Plane
   end
 
   def take_off(airport)
+    raise 'Unable to take-off. Plane not in that airport' unless airport.hangar.include?(self) 
     return "Unable to take-off. Weather is stormy" if airport.check_weather == "stormy"
     airport.taxi_to_runway(self)
     return "#{self} is no longer in #{airport}" 
   end
-
 end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -1,14 +1,6 @@
 require 'plane'
 
 describe Plane do
-  # let(:company) do
-  #   company = Company.create!(price: 700)
-  #   allow(company).to receive(:foo){5}
-  #   company
-  # end
-  # let(:airport) do
-  #   allow(Airport.new).to receive(:check_weather).and_return("sunny")
-  # end
   let(:airport) do
     airport = Airport.new
     allow(airport).to receive(:check_weather).and_return("sunny")
@@ -19,8 +11,6 @@ describe Plane do
     allow(stormy_airport).to receive(:check_weather).and_return("stormy")
     stormy_airport
   end
-  # let(:airport) { Airport.new }
-  # let(:weather) { double("sunny") }
 
   it { is_expected.to respond_to(:land).with(1).argument }
   describe '#land' do
@@ -32,7 +22,6 @@ describe Plane do
       expect { subject.land(airport) }.to raise_error 'Landing prevented: Airport full'
     end
     it 'prevents landing when weather is stormy' do
-      # allow(subject).to receive(:check_weather).and_return("stormy")
       expect(subject.land(stormy_airport)).to eq "Unable to land. Weather is stormy"
     end
   end
@@ -44,8 +33,11 @@ describe Plane do
       expect(subject.take_off(airport)).to eq "#{subject} is no longer in #{airport}"
     end
     it 'prevents taking off when weather is stormy' do
-      # allow(subject).to receive(:check_weather).and_return("stormy")
+      allow(stormy_airport.hangar).to receive(:include?).and_return(true)
       expect(subject.take_off(stormy_airport)).to eq "Unable to take-off. Weather is stormy"
+    end
+    it 'prevents taking off when plane is not in that airport' do
+      expect { subject.take_off(airport) }.to raise_error 'Unable to take-off. Plane not in that airport'
     end
   end
 end
