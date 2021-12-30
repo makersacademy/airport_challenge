89084838b1aa6a1376111bd6acab53a72c9f5627
Alice Gray <alicegray33@gmail.com>
2021-12-30 10:16:14 +0000

    Removed tests that are no longer required

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -5,21 +5,18 @@ describe Airport do
 
   it { is_expected.to respond_to(:hangar) }
 
-  it { is_expected.to respond_to(:send_to_hangar).with(1).argument }
   describe '#send_to_hangar' do
     it 'stores a plane in the hangar' do
       expect(subject.send_to_hangar(plane)).to eq true
     end
   end
 
-  it { is_expected.to respond_to(:taxi_to_runway).with(1).argument }
   describe '#taxi_to_runway' do
     it 'taxis a plane to the runway' do
       expect(subject.taxi_to_runway(plane)).to eq []
     end
   end
 
-  it { is_expected.to respond_to(:full?) }
   describe '#full?' do
     it 'returns false when hanger is empty' do
       expect(subject.full?).to eq false
@@ -30,7 +27,6 @@ describe Airport do
     end
   end
 
-  it { is_expected.to respond_to(:check_weather) }
   describe '#check_weather' do
     it 'returns random weather' do
       expect(subject.check_weather)
