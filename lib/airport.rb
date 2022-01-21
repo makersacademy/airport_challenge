d9680db5ebcd7d5caec1d1235dd6a4c71a9995f0
Jaabir Yusuf <jaabir-mahmoud@hotmail.com>
2022-01-21 12:15:17 +0000

    4 user stories now implemented

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,19 +1,33 @@
 require_relative "plane"
 
 class Airport
+  MAX_CAPACITY = 10
+  # STORMY_WEATHER = false
 
   attr_reader :planes
+  attr_reader :capacity
 
-  def initialize
+  attr_accessor :capacity
+
+  def initialize(capacity=MAX_CAPACITY)
     @planes = []
+    @capacity = capacity
   end
 
   def land_plane(plane)
+    fail "Airport full" if full?
     puts "The plane is landed!"
     planes << plane
   end
 
   def takeoff_plane
+    puts "The plane has taken off!"
     planes.pop
+  end
+
+  private
+
+  def full?
+    planes.count >= capacity
   end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -12,4 +12,31 @@ describe Airport do
     subject.land_plane(plane)
     expect(subject.takeoff_plane).to eq(plane)
   end
+
+  it "has a max capacity" do
+    expect(subject.capacity).to eq(Airport::MAX_CAPACITY)
+  end
+
+  describe "#landing a plane" do
+    it "raises an error when the airport is full" do
+      subject.capacity.times { subject.land_plane(Plane.new) }
+      expect { subject.land_plane Plane.new }.to raise_error("Airport full")
+    end
+  end
+
+  # describe "#stormy weather" do
+  #   it "raises an error when landing in a stormy weather" do
+      
+  #   end
+  # end
+
+  describe "initialization" do
+    subject { Airport.new }
+    it "have initialized max capacity" do
+      Airport::MAX_CAPACITY.times do
+        subject.land_plane(Plane.new)
+      end
+      expect{ subject.land_plane(Plane.new)}.to raise_error "Airport full"
+    end
+  end
 end
