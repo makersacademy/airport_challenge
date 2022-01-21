07426eb1679f03a9faad80b4b61e1ba3fea2da66
benfielding74 <billy.fadden@live.com>
2022-01-21 16:36:48 +0000

    2nd user story

--- lib/airport.rb
+++ lib/airport.rb
@@ -4,11 +4,23 @@ class Airport
 
   attr_reader :terminal
 
-  def land_plane(plane = Plane.new)
-    @plane = plane
-    terminal = []
-    terminal << plane
-    "#{@plane} has landed"
+  def initialize
+    @terminal = []
   end
 
+  def land_plane(plane)
+    @terminal << plane
+    "#{plane} has landed"
+  end
+
+  def take_off(plane)
+    fail 'There are no planes at the terminal' if @terminal.empty?
+    @terminal.each_with_index do |gate, i|
+      if gate == plane
+        @terminal.delete_at(i)
+      else  
+        raise "This plane is not at the airport"
+      end
+    end 
+  end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,21 +2,35 @@ require 'airport'
 
 describe Airport do
   describe '#land_plane' do
-
     it { is_expected.to respond_to :terminal }
-
     it 'recognises that a plane has landed' do
       airport = Airport.new
-
       expect(airport.land_plane("Boeing 747")).to eq("Boeing 747 has landed")
-
     end
 
     it 'stores the plane at the airport terminal' do
+      airport = Airport.new
+      monarch = Plane.new
+      airport.land_plane(monarch)
 
-      
+      expect(airport.terminal).to eq([monarch])
   
     end
+  end
+
+  describe '#take_off' do
+    it { is_expected.to respond_to :take_off }
+
+    it 'checks if there is a specific plane to take off' do
+      airport = Airport.new
+      monarch = Plane.new
+      panam = Plane.new
+      airport.land_plane(monarch)
+    
+      expect { airport.take_off(panam) }.to raise_error ('This plane is not at the airport')
+    
+    end
+
   end
 
 end
