94d30644726c243fc66be781829f02974fd3c3ca
Oliver Cutting <oliver.cutting@gmail.com>
2022-01-21 12:25:06 +0000

    Airport challenge complete

---
+++ lib/airport.rb
@@ -0,0 +1,30 @@
+require_relative 'plane'
+
+class Airport
+  attr_accessor :hanger, :capacity
+  DEFAULT_CAPACITY = 20
+
+  def initialize(capacity = DEFAULT_CAPACITY)
+    @capacity = capacity
+    @hanger = []
+  end
+
+  def stormy?
+    rand(2).zero?
+  end
+
+  def land(plane)
+    fail 'Hanger is full' if full?
+    fail 'Weather is stormy' if stormy?
+    @hanger << plane
+  end
+
+  def take_off(plane)
+    fail 'Weather is stormy' if stormy?
+    @hanger.delete(plane)
+  end
+
+  def full?
+    @hanger.count >= @capacity
+  end
+end
---
+++ spec/airport_spec.rb
@@ -0,0 +1,46 @@
+require 'airport'
+
+describe Airport do
+
+  let(:plane) { double :plane }
+  it { is_expected.to respond_to :land }
+
+  context 'when it is sunny' do
+    before do
+      allow(subject).to receive(:stormy?).and_return false
+    end
+    
+    it 'holds the plane after landing' do
+      plane = double(:plane)
+      subject.land(plane)
+      expect(subject.hanger).to include plane
+    end
+    
+    it 'landing raises an error when hanger is full' do
+      subject.capacity.times { subject.land double :plane }
+      expect { subject.land double(:plane) }.to raise_error 'Hanger is full'
+    end
+
+    it 'lets planes take off' do
+      plane = double(:plane)
+      subject.land(plane)
+      expect(subject.take_off(plane)).to eq(plane)
+    end
+  end
+
+  context 'when it is stormy' do
+    before do
+      allow(subject).to receive(:stormy?).and_return true
+    end
+
+    it 'raises an error on take_off' do
+      plane = double(:plane)
+      expect { subject.take_off(plane) }.to raise_error 'Weather is stormy'
+    end
+
+    it 'raises an error on landing' do
+      plane = double(:plane)
+      expect { subject.land(plane) }.to raise_error 'Weather is stormy'
+    end
+  end
+end
