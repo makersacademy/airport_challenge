4202e1f98ecf86ff196ea12b9e486e4489fa9c46
Eden <elsw@hotmail.co.uk>
2022-01-21 13:48:31 +0000

    I have created both the plane rb and spec file. I have written teh first test for user story 1.

---
+++ lib/plane.rb
@@ -0,0 +1,7 @@
+# class Plane
+
+#   def land_plane
+#     p "what do you want the plane to do?"
+#     gets.chomp 
+#   end
+# end
---
+++ spec/plane_spec.rb
@@ -0,0 +1,26 @@
+require './lib/plane.rb'
+
+# checks tells plane lands at airport
+describe Plane do
+  it "lands at airport" do
+    plane = Plane.new
+    expect(plane.land_plane). to eq "land"
+
+  end
+
+
+
+# # instruct plane to take off
+#   it "instruct plane to take off"
+
+
+#   end
+
+# # check if plane has left airport
+#   it 'checks if plane is still in airport'
+
+# end
+
+
+
+end
