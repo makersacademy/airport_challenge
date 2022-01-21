34d3d6e5bbf49c58be992e488401fe3c643e0c3b
Cristina <cristhinagv@gmail.com>
2022-01-21 11:47:42 +0000

    User Story 2 - plane takes off from airport - unsure on how to test that it confirms plane has left airport

--- lib/airport.rb
+++ lib/airport.rb
@@ -3,4 +3,7 @@ class Airport
   def land(plane)
   end
 
+  def take_off(plane)
+  end
+
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -6,4 +6,10 @@ describe Airport do
   it 'tells plane to land' do
     expect(airport).to respond_to(:land).with(1).argument
   end
+
+  it 'tells plane to take off' do
+    expect(airport).to respond_to(:take_off).with(1).argument
+  end
+
+  
 end
