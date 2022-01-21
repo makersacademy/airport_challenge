074c71439860c5279814b792e7d02f4c0b9882e2
Cristina <cristhinagv@gmail.com>
2022-01-21 12:15:38 +0000

    User Story 3 - prevent landing if airport is full, added test expecting error if capacity of airport is that of the set value given in test

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,6 +1,14 @@
 class Airport
 
+  def initialize(capacity)
+    @capacity = capacity
+    @planes = []
+
+  end
+
   def land(plane)
+    raise "Plane cannot land. Airport is full" if @planes.count >= @capacity
+    @planes << plane
   end
 
   def take_off(plane)
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,7 +1,7 @@
 require 'airport'
 
 describe Airport do
-  airport = Airport.new
+  airport = Airport.new(20)
 
   it 'tells plane to land' do
     expect(airport).to respond_to(:land).with(1).argument
@@ -11,5 +11,10 @@ describe Airport do
     expect(airport).to respond_to(:take_off).with(1).argument
   end
 
-  
+  it 'prevents plane from landing if airport is full' do
+    20.times do 
+      airport.land(:plane)
+    end
+    expect { airport.land(:plane) }.to raise_error "Plane cannot land. Airport is full"
+  end
 end
