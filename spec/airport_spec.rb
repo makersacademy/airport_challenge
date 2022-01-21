2e2f93d3e4d81d43b1a54813ceed75db1fe2c457
Oliver Cutting <oliver.cutting@gmail.com>
2022-01-21 13:36:55 +0000

    Increased testing coverage

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -5,6 +5,12 @@ describe Airport do
   let(:plane) { double :plane }
   it { is_expected.to respond_to :land }
 
+  it 'holds custom capacities' do
+    airport = Airport.new(50)
+    allow(airport).to receive(:stormy?).and_return false
+    expect { 50.times { airport.land double :plane } }.not_to raise_error
+  end
+
   context 'when it is sunny' do
     before do
       allow(subject).to receive(:stormy?).and_return false
@@ -15,7 +21,7 @@ describe Airport do
       subject.land(plane)
       expect(subject.hanger).to include plane
     end
-    
+
     it 'landing raises an error when hanger is full' do
       subject.capacity.times { subject.land double :plane }
       expect { subject.land double(:plane) }.to raise_error 'Hanger is full'
