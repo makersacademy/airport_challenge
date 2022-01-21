63c5fd3bba1070e25ab5bf2c462c0aa5d60ab610
Mathew Grimward <mathewgrimward@gmail.com>
2022-01-21 17:34:43 +0000

    User Story 6

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -7,7 +7,7 @@ describe Airport do
   describe "#land" do
     it 'can land a plane' do
       plane = Plane.new
-      allow(subject).to receive(:stormy?) {false}
+      allow(subject).to receive(:stormy?) { false }
       expect(subject.land(plane)).to eq [plane]
     end
 
@@ -15,27 +15,27 @@ describe Airport do
       it 'can takeoff from airport' do
         plane = Plane.new
         subject.land(plane)
-        allow(subject).to receive(:stormy?) {false}
+        allow(subject).to receive(:stormy?) { false }
         expect(subject.takeoff).to eq plane
       end
 
       it "does not let planes land when stormy" do
         weather = Weather.new
-        allow(subject).to receive(:stormy?) {true}
+        allow(subject).to receive(:stormy?) { true }
         expect { subject.takeoff }.to raise_error 'It is stormy, you cannot take off'
       end
     
       describe "#land" do
         it 'does not allow planes to land when airport is full' do
           plane = Plane.new
-          allow(subject).to receive(:stormy?) {false}
+          allow(subject).to receive(:stormy?) { false }
           Airport::DEFAULT_CAPACITY.times { subject.land(plane) }
           expect { subject.land(plane) }.to raise_error 'The Airport is full, you cannot land'
         end
 
         it 'does not allow planes to land when stormy' do
           plane = Plane.new
-          allow(subject).to receive(:stormy?) {true}
+          allow(subject).to receive(:stormy?) { true }
           expect { subject.land(plane) }.to raise_error 'It is stormy, you cannot land'
         end
 
