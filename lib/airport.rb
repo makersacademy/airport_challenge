c2a9505cf0b28bd41a1a8022282f4da09284e502
Cristina <cristhinagv@gmail.com>
2022-01-21 12:58:22 +0000

    User Story 4 - added constant default_capacity to be able to be overriden

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,17 +1,26 @@
-class Airport
+require_relative 'plane'
 
-  def initialize(capacity)
+class Airport
+  DEFAULT_CAPACITY = 20
+
+  def initialize(capacity = DEFAULT_CAPACITY)
     @capacity = capacity
     @planes = []
 
   end
 
   def land(plane)
-    raise "Plane cannot land. Airport is full" if @planes.count >= @capacity
+    raise "Plane cannot land. Airport is full" if full?
     @planes << plane
   end
 
   def take_off(plane)
+  end
+
+private
+
+  def full?
+    @planes.count >= DEFAULT_CAPACITY
   end
 
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,20 +1,25 @@
 require 'airport'
 
 describe Airport do
-  airport = Airport.new(20)
+  airport = Airport.new
+  let(:plane) { double :plane }
 
-  it 'tells plane to land' do
-    expect(airport).to respond_to(:land).with(1).argument
-  end
-
-  it 'tells plane to take off' do
-    expect(airport).to respond_to(:take_off).with(1).argument
-  end
-
-  it 'prevents plane from landing if airport is full' do
-    20.times do 
-      airport.land(:plane)
+  describe '#land' do
+    it 'tells plane to land' do
+      expect(airport).to respond_to(:land).with(1).argument
     end
-    expect { airport.land(:plane) }.to raise_error "Plane cannot land. Airport is full"
+    it 'prevents plane from landing if airport is full' do
+      Airport::DEFAULT_CAPACITY.times do 
+        airport.land(:plane)
+      end
+      expect { airport.land(:plane) }.to raise_error "Plane cannot land. Airport is full"
+    end
   end
+
+  describe '#take_off' do
+    it 'tells plane to take off' do
+      expect(airport).to respond_to(:take_off).with(1).argument
+    end
+  end
+
 end
