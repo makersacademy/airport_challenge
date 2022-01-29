6ae4251982e90c86f263f6cf5b9f1827e02e7c2d
Sarah Moffat <sarah.moffat5@gmail.com>
2022-01-29 14:09:10 +0000

    Defined take off method

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,7 +1,11 @@
 require 'airport'
 
 describe Airport do
-    it 'instructs a plane to land at an airport' do
-        expect(subject).to respond_to :land_plane      
-    end  
+  it 'instructs a plane to land at an airport' do
+    expect(subject).to respond_to :land_plane      
+  end
+
+  it 'instructs a plane to take off from an airport' do
+    expect(subject).to respond_to :take_off
+  end
 end
