3a28fec6e335519d8376c2d2a8d14fb2486f0090
Jack <winterschladen.j@gmail.com>
2022-01-29 12:55:54 +0000

    Created an airport class with a required capactiy and a land & take_off method

---
+++ lib/airport.rb
@@ -0,0 +1,14 @@
+require_relative 'plane'
+class Airport
+
+  def initialize(capacity)
+  end
+
+  def land(plane)
+  end
+
+  def take_off(plane)
+    "#{plane} has successfully taken_off from #{self}."
+  end
+
+end
