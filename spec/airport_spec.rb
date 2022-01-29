8342ae2b4358377298067b853ae4c39c8f475372
Jack <winterschladen.j@gmail.com>
2022-01-29 16:31:42 +0000

    Added test to raise error if trying to take-off a plane that is not in the airport

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -12,7 +12,7 @@ describe Airport do
       expect(subject).to respond_to(:land).with(1).argument
     end
     it 'raise an error message and prevent landing when airport capacity is full' do
-        allow(subject).to receive(:weather) { false }  
+      allow(subject).to receive(:weather) { false }  
       Airport::DEFAULT_CAPACITY.times { subject.land(double("plane")) }  
       expect { subject.land(double("plane")) }.to raise_error "Airport full, landing not possible." 
     end
@@ -22,9 +22,9 @@ describe Airport do
       expect { 2.times { subject.land(plane) } }.to raise_error "This plane is already in the airport."
     end
     it 'should not allow a plane to land and raises an error if the weather is stormy' do       
-    double("plane")
-    allow(subject).to receive(:weather) { true } 
-    expect { subject.land(double) }.to raise_error "Cannot enable take-off / landing during stormy weather"
+      double("plane")
+      allow(subject).to receive(:weather) { true } 
+      expect { subject.land(double) }.to raise_error "Cannot enable take-off / landing during stormy weather"
     end
   end
 
@@ -34,21 +34,29 @@ describe Airport do
     end
     it 'should give confirmation that the plane has taken off' do
       allow(subject).to receive(:weather) { false }   
-      double("plane")
-      expect(subject.take_off(double)).to eq "#{double} has successfully taken_off from #{subject}."
+      plane = double("plane")
+      subject.land(plane)
+      expect(subject.take_off(plane)).to eq "#{plane} has successfully taken_off from #{subject}."
     end
     it 'should remove the plane that has taken off from the planes_in_airport array' do 
       allow(subject).to receive(:weather) { false }
-      double("plane")
-      subject.land(double)
-      subject.take_off(double)
-      expect(subject.planes_in_airport).not_to include(double)
+      plane = double
+      subject.land(plane)
+      subject.take_off(plane)
+      expect(subject.planes_in_airport).not_to include(plane)
+    end
+    it "should not allow a plane to take off if it isn't in the airport" do
+      allow(subject).to receive(:weather) { false }
+      plane = double
+      subject.land(plane)
+      subject.take_off(plane)
+      expect { subject.take_off(plane) }.to raise_error "This plane is not in the airport."
     end
     it 'should not allow a plane to land and raises an error if the weather is stormy' do       
       double("plane")
       allow(subject).to receive(:weather) { true } 
       expect { subject.land(double) }.to raise_error "Cannot enable take-off / landing during stormy weather"
-      end
+    end
   end
 
   # describe '#full?' do 
