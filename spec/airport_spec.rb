32e272f2de8ced9ce9a5ea831807a10b1c78fdb4
Jack <winterschladen.j@gmail.com>
2022-01-29 14:45:56 +0000

    Refactored test code to use test doubles

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -12,13 +12,14 @@ describe Airport do
       expect(subject).to respond_to(:land).with(1).argument
     end
     it 'raise an error message and prevent landing when airport capacity is full' do
-      Airport::DEFAULT_CAPACITY.times { subject.land(Plane.new) }  
-      expect { subject.land(Plane.new) }.to raise_error "Airport full, landing not possible." 
+      Airport::DEFAULT_CAPACITY.times { subject.land(double("plane")) }  
+      expect { subject.land(double("plane")) }.to raise_error "Airport full, landing not possible." 
     end
     it 'raises an error if a plane that is already in the airport is attempted to be landed' do
-      plane = Plane.new
+      plane = double
       expect { 2.times { subject.land(plane) } }.to raise_error "This plane is already in the airport."
     end
+    # it 'should not allow a plane to land if the weather is stormy' do   
   end
 
   describe '#take_off' do 
@@ -26,14 +27,14 @@ describe Airport do
       expect(subject).to respond_to(:take_off).with(1).argument
     end
     it 'should give confirmation that the plane has taken off' do
-      plane = Plane.new
-      expect(subject.take_off(plane)).to eq "#{plane} has successfully taken_off from #{subject}."
+      double("plane")
+      expect(subject.take_off(double)).to eq "#{double} has successfully taken_off from #{subject}."
     end
     it 'should remove the plane that has taken off from the planes_in_airport array' do 
-      plane = Plane.new
-      subject.land(plane)
-      subject.take_off(plane)
-      expect(subject.planes_in_airport).not_to include(plane)
+      double("plane")
+      subject.land(double)
+      subject.take_off(double)
+      expect(subject.planes_in_airport).not_to include(double)
     end
   end
 
