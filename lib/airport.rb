3cc948d87939cbb41d04ebd3b1447cb4e45d7da0
Jack <winterschladen.j@gmail.com>
2022-01-29 16:32:25 +0000

    Raises an error message if a plane tries to take off that is not in the airport

--- lib/airport.rb
+++ lib/airport.rb
@@ -2,8 +2,8 @@ require_relative 'plane'
 class Airport
 
   DEFAULT_CAPACITY = 10  
-   attr_reader :planes_in_airport
-   attr_accessor :weather_check
+  attr_reader :planes_in_airport
+  attr_reader
 
   def initialize(capacity = DEFAULT_CAPACITY)
     @capacity = capacity
@@ -13,12 +13,13 @@ class Airport
   def land(plane)
     weather_check
     raise "This plane is already in the airport." if @planes_in_airport.include?(plane)
-    raise "Airport full, landing not possible." if @capacity == @planes_in_airport.count
+    airport_full?
     @planes_in_airport << plane
   end
 
   def take_off(plane)
     weather_check
+    raise "This plane is not in the airport." unless @planes_in_airport.include?(plane)
     @planes_in_airport.delete(plane)
     "#{plane} has successfully taken_off from #{self}."
   end
@@ -32,6 +33,10 @@ class Airport
   def weather
     return false unless rand(1..50) == 50
     true
+  end
+
+  def airport_full?
+    raise "Airport full, landing not possible." if @capacity == @planes_in_airport.count
   end
 
 end
