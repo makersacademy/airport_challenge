3b51e24245fd6967f8ff4e41d6e642b82b4f8614
Francesco <francesco@monzo.com>
2022-01-29 23:31:51 +0100

    RGR added take_off method

--- lib/plane.rb
+++ lib/plane.rb
@@ -2,4 +2,8 @@ class Plane
   def land
     "Plane's landed!"
   end
+
+  def take_off
+    "Plane took off and has left airport!"
+  end
 end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -4,9 +4,17 @@ describe Plane do
   it "is a plane that can land" do
     expect(subject).to respond_to :land
   end
+  it "is a plane that can take off" do
+    expect(subject).to respond_to :take_off
+  end
   describe "#land" do
     it "should land a plane at an airport" do
       expect(subject.land).to eq "Plane's landed!"
+    end
+  end
+  describe "#take_off" do
+    it "should lift a plane from an airport" do
+      expect(subject.take_off).to eq "Plane took off and has left airport!"
     end
   end
 end
