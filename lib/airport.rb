3b60f6df482d0b42ea19affcd4a05297c3e9efc7
Jack <winterschladen.j@gmail.com>
2022-01-29 13:07:22 +0000

    Added a planes_in_airport array and a default capacity

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,7 +1,11 @@
 require_relative 'plane'
 class Airport
 
-  def initialize(capacity)
+  DEFAULT_CAPACITY = 10  
+
+  def initialize(capacity = DEFAULT_CAPACITY)
+    @capacity = capacity
+    @planes_in_airport = []
   end
 
   def land(plane)
