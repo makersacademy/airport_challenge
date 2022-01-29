058e7cf02678560d61a7acf53543ca77ee848c4c
Ed <ed321@hotmail.co.uk>
2022-01-29 15:32:54 +0000

    Added Airport take-off method & Plane flying status check

--- lib/airport.rb
+++ lib/airport.rb
@@ -3,4 +3,9 @@ class Airport
     plane
     "Landed"
   end
+  
+  def take_off
+    plane = Plane.new(flying = true)
+    return plane
+  end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -7,4 +7,14 @@ describe Airport do
       expect(subject.land(Plane.new)).to eq "Landed"
     end
   end
+  describe "#take_off" do
+    it { is_expected.to respond_to :take_off }
+    it "takes off a plane from the airport" do
+      expect(subject.take_off).to be_a(Plane)
+    end
+    it "returns a confirmation the plane is flying" do
+      plane = subject.take_off
+      expect(plane.flying).to eq true
+    end
+  end
 end
