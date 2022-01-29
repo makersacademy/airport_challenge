7aed81451f72796432e7c0293b1e974ab3f71871
Ed <ed321@hotmail.co.uk>
2022-01-29 13:41:03 +0000

    Method to accept planes landing at the airport

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,3 +1,6 @@
 class Airport
-
+  def land(plane)
+    plane
+    "Landed"
+  end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,5 +1,10 @@
 require_relative "../lib/airport"
 
 describe Airport do
-  
+  describe "#land" do
+    it { is_expected.to respond_to :land }
+    it "lands a plane at the airport" do
+      expect(subject.land(Plane.new)).to eq "Landed"
+    end
+  end
 end
