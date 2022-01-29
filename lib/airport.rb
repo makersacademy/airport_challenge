727ad58fc1a4046f3b92dcf0487de2f7b4302458
Jack <winterschladen.j@gmail.com>
2022-01-29 13:40:29 +0000

    Updated code to remove a plane from planes_in_airport array on take-off

--- lib/airport.rb
+++ lib/airport.rb
@@ -2,6 +2,7 @@ require_relative 'plane'
 class Airport
 
   DEFAULT_CAPACITY = 10  
+  attr_accessor :planes_in_airport
 
   def initialize(capacity = DEFAULT_CAPACITY)
     @capacity = capacity
@@ -17,6 +18,7 @@ class Airport
   end
 
   def take_off(plane)
+    @planes_in_airport.delete(plane)
     "#{plane} has successfully taken_off from #{self}."
   end
 
