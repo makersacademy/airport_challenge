942e833ea8a0458875ce4c5e7d42289241e936a6
AliCo-Hash <ali.cocelli@hotmail.co.uk>
2022-01-30 22:38:55 +0000

    plane cannot land while airport is ful

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,6 +1,12 @@
 class Airport
+  def initialize(capacity = 10)
+    @capacity = capacity
+    @airplanes = []
+  end
 
   def land(airplane)
+    raise 'Cannot land airplane due to airport being full' if @airplanes.length >= @capacity
+    @airplanes << airplane
   end
 
   def take_off(airplane)
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,8 +1,17 @@
 require_relative '../lib/airport.rb'
 
 describe Airport do
+  let(:airplane) {Airplane.new}
+
   describe "#land"
   it { is_expected.to respond_to(:land).with(1).argument }
+
+  it "should not allow an airplane to land while airport is full" do
+    10.times do  # 10 airplanes landed
+      subject.land(airplane)
+    end
+    expect {subject.land(:airplane).to raisue_error ("Cannot land airplane due to airport being full")}
+  end
 
   describe "#take_off"
   it { is_expected.to respond_to(:take_off).with(1).argument }
