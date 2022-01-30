ef14fa19478c276439a88d8012d6a3b20d5258df
Haroon <haroon20@hotmail.co.uk>
2022-01-30 21:01:23 +0000

    Testing langing planes

--- lib/Airport.rb
+++ lib/Airport.rb
@@ -3,11 +3,13 @@ class Airport
 
   def initialize
     @plane = []
+    @planes_landed = 0
   end
 
   def land_plane(plane)
     fail 'Airport full' if @plane.count >= 20
     @plane << plane
+    @planes_landed +=1
     plane
   end
 
