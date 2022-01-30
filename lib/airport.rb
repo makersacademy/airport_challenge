f7f55787928d394737177c427ed9714562ae7a12
Rakshak Shetty <rakshak_81@hotmail.com>
2022-01-30 20:57:12 +0000

    Step 3

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,6 +1,19 @@
+ require_relative 'aeroplane'
 class Airport
+  def initialize
+    @plane = []
+  end
   def land_plane(plane)
+    fail 'Airport full' if full?
+    @plane << plane
+    plane
   end
   def takeoff_plane(plane)
+    plane
+  end
+
+  private
+  def full?
+    @plane.count >= 20
   end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,9 +1,23 @@
 require 'airport'
 describe Airport do
-  it "instructs the plane to land" do
-    expect(subject).to respond_to(:land_plane).with(1).argument
+  describe '#land_plane' do
+    it "instructs the plane to land" do
+        plane = Aeroplane.new
+      expect(subject.land_plane(plane)).to eq plane
+    end
   end
-    it "instructs the plane to landtakeoff" do
-    expect(subject).to respond_to(:takeoff_plane).with(1).argument
+  describe '#land_plane' do
+    it 'Do not land if the Airport is full' do
+        20.times { subject.land_plane Aeroplane.new}
+        expect { subject.land_plane Aeroplane.new}. to raise_error 'Airport full'
+    end
   end
+  describe '#takeoff_plane' do
+    it "instructs the plane to takeoff" do
+       plane = Aeroplane.new
+
+       expect(subject.takeoff_plane(plane)).to eq plane
+    end
+  end
+
 end
