4f21bf612463a0fcc0470baf23879565adda4ca0
Peter O <harmless_goose@icloud.com>
2022-01-30 18:58:38 +0000

    Instructed a plane to takeoff from an airport

--- lib/plane.rb
+++ lib/plane.rb
@@ -1,7 +1,14 @@
 class Plane
 
+    @current_airport
+
     def land(airport)
         airport.hanger.push(self)
+        @current_airport = airport
+    end
+
+    def takeoff
+        @current_airport.hanger.delete(self)
     end
 
 end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -7,6 +7,13 @@ describe Plane do
         airport = Airport.new
         subject.land(airport)
         expect(airport.hanger.last).to eq subject
-      end
+    end
+
+    it "takes off from an airport" do
+        airport = Airport.new
+        subject.land(airport)
+        subject.takeoff
+        expect(airport.hanger.last).to eq nil
+    end
 
 end
