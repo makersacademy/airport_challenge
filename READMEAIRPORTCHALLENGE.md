1784f9843f112dff335060d4f6ce4e7cc0e430cc
Haroon <haroon20@hotmail.co.uk>
2022-01-30 21:27:24 +0000

    Testing landing planes

--- READMEAIRPORTCHALLENGE.md
+++ READMEAIRPORTCHALLENGE.md
@@ -1,1 +1,11 @@
-## Readme
+## Readme for the Airoprt Challenge
+
+-For the challenge we had to model the behaviour of an airport.
+
+-Planes were were allowed to take of and land
+
+-We tested each of the behaviour using both unit and feature testing.
+
+-We had a capacity limit on planes of 20
+
+-I looked through the boris bikes example to help guide my answers.  
--- spec/Airport_spec.rb
+++ spec/Airport_spec.rb
@@ -6,12 +6,14 @@ describe Airport do
       expect(subject.land_plane(plane)).to eq plane
     end
   end
+  
   describe '#land_plane' do
     it 'Do not land if the Airport is full' do
         20.times { subject.land_plane Aeroplane.new}
         expect { subject.land_plane Aeroplane.new}. to raise_error 'Airport full'
     end
   end
+
   describe '#takeoff_plane' do
     it "instructs the plane to takeoff" do
        plane = Aeroplane.new
