8e985921d54407e63242fd939561847c735a845b
Bromley <52469112+nyahehnagi@users.noreply.github.com>
2022-01-30 17:35:13 +0000

    Improved feature_spec.rb, increased liklihood of good weather

--- lib/weather.rb
+++ lib/weather.rb
@@ -2,7 +2,7 @@ class Weather
   def stormy?
     good = false
     stormy = true
-    possible_weather = [good, good, stormy]
+    possible_weather = [good, good, good, stormy]
     possible_weather.sample
   end
 end
--- spec/feature_spec.rb
+++ spec/feature_spec.rb
@@ -2,6 +2,22 @@ require './lib/plane'
 require './lib/airport'
 require './lib/weather'
 
+def land_plane_at_airport(airport, plane)
+  begin
+    airport.land(plane)
+  rescue => exception
+    "Error Landing Plane at airport:  #{exception.message}"
+  end
+end
+
+def takeoff_plane_from_airport(airport, plane)
+  begin
+    airport.take_off(plane)
+  rescue => exception
+    "Error Taking Off Plane at airport:  #{exception.message}"
+  end
+end
+
 begin
   p "plane_one object instantiated: #{plane_one = Plane.new}"
   p "plane_two object instantiated: #{plane_two = Plane.new}"
@@ -21,9 +37,13 @@ begin
   p "Plane - Takeoff plane_one #{plane_one.take_off}"
   p "Plane - Takeoff plane_two #{plane_two.take_off}"
 
-  p "Airport - land plane_one: #{airport.land(plane_one)}"
-  p "Airport - land plane_two: #{airport.land(plane_two)}"
-
+  
+  p "Airport - land plane_one: #{land_plane_at_airport(airport,plane_one)}"
+  p "Airport - take off plane_one: #{takeoff_plane_from_airport(airport,plane_one)}"
+  
+  p "Airport - land plane_two: #{land_plane_at_airport(airport,plane_two)}"
+  p "Airport - take off plane_two: #{takeoff_plane_from_airport(airport,plane_two)}"
+ 
   rescue => exception
     puts "Error Message:  #{exception.message}"
 end
