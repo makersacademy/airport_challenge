f2d598ec3e99ab2ee0e78b92624eb94b578107c9
Francesco <francesco@monzo.com>
2022-01-30 15:49:20 +0100

    passed tests, added method flying? to Plane

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -6,18 +6,18 @@ describe Airport do
     expect(subject).to respond_to(:land).with(1).argument
   end
   it "is an airport where planes take off" do
-    expect(subject).to respond_to(:take_off).with(1).argument
+    expect(subject).to respond_to(:lift).with(1).argument
   end
   describe "#land" do
     it "should land a plane" do
-      airport = Airport.new
+      plane = Plane.new
       expect(subject.land(plane)).to eq "Plane's landed!"
     end
   end
-  describe "#take_off" do
+  describe "#lift" do
     it "should lift a plane from an airport" do
-      airport = Airport.new
-      expect(subject.take_off(plane)).to eq "Plane took off and has left airport!"
+      plane = Plane.new
+      expect(subject.lift(plane)).to eq "Plane took off and has left airport!"
     end
   end
   # it "should not land if airport is full" do
