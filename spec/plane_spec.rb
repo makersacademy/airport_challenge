d354822f186dcc7415ac52ff3519e74489ec6324
Ledders26 <benlhall584@hotmail.co.uk>
2022-01-30 20:52:47 +0000

    added a test to check that when a plane is initialised, it is set to be in the air

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -3,4 +3,10 @@ require_relative '../lib/airport'
 
 describe Plane do
 
+  describe "#initialize" do
+    it "should create a parameter of in_air and set it to true when a new plane is created" do
+      plane = Plane.new
+      expect(plane.in_air).to eq true
+    end
+  end
 end
