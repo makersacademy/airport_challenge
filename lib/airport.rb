4f9f6391c02d5f9d798bf54d017ec93e0101a3c3
Rakshak Shetty <rakshak_81@hotmail.com>
2022-01-30 21:05:27 +0000

    User story 3

--- lib/airport.rb
+++ lib/airport.rb
@@ -2,10 +2,12 @@
 class Airport
   def initialize
     @plane = []
+    @planes_landed = 0
   end
   def land_plane(plane)
     fail 'Airport full' if full?
     @plane << plane
+    @planes_landed += 1
     plane
   end
   def takeoff_plane(plane)
