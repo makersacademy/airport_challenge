c92c0927e65c6302518e72e3eac7dcaace07bddf
AliCo-Hash <ali.cocelli@hotmail.co.uk>
2022-01-31 01:27:04 +0000

    stormy weather added to takeoff and landing

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,5 +1,9 @@
+require_relative 'airplane'
+
 class Airport
   DEFAULT_CAPACITY = 10
+  attr_reader :airplanes
+
   def initialize(capacity = DEFAULT_CAPACITY)
     @capacity = capacity
     @airplanes = []
@@ -7,13 +11,19 @@ class Airport
 
   def land(airplane)
     raise 'Cannot land airplane due to airport being full' if full?
+    raise 'Cannot land due to stormy weather' if stormy?
     @airplanes << airplane
   end
 
   def take_off(airplane)
+    raise 'Cannot takeoff due to stormy weather' if stormy?
   end
   
   def full?
     @airplanes.length >= @capacity
+  end
+
+  def stormy?
+    rand(1..6) > 4
   end
 end
