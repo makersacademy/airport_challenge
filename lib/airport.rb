ce65a0b84d03930e0af31f06d8e85e62f1d033c6
Trushna Rabadia <rabadiatrushna@gmail.com>
2022-01-31 00:25:50 +0000

    default capacity added

--- lib/airport.rb
+++ lib/airport.rb
@@ -2,15 +2,35 @@ require_relative 'plane'
 
 class Airport
 
-  attr_reader :plane
+  DEFAULT_CAPACITY = 10
 
-  def land_plane
-    fail 'The airport is full' unless @plane
-    @plane
+  attr_accessor :capacity
+
+  def initialize
+    @planes = []
+    @capacity = DEFAULT_CAPACITY
   end
 
-  def take_off(plane)
-    @plane = plane
+  def take_off
+    fail 'No planes available to take-off' if empty?
+    @planes.pop
+  end
+
+  def land(plane)
+    fail 'The airport is full' if full?
+    @planes << plane
+  end
+
+  private
+
+  attr_reader :planes
+
+  def full?
+    @planes.count >= capacity
+  end
+
+  def empty?
+    @planes.empty?
   end
 
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,37 +2,53 @@ require "airport"
 require "plane"
 
 describe Airport do
-  it { is_expected.to respond_to :land_plane }
+
+  it 'has a default capacity' do
+    airport = Airport.new
+    expect(airport.capacity).to eq Airport::DEFAULT_CAPACITY
+  end
+
+  # it { is_expected.to respond_to :take_off }
   
-  describe '#land_plane' do 
-    it 'lands a plane upon instruction' do
+  describe '#take_off' do 
+    it 'instructs a plane to take off' do
       airport = Airport.new
       plane = Plane.new
-      airport.take_off(plane)
-      expect(airport.land_plane).to eq plane
+      airport.land(plane)
+      expect(airport.take_off).to eq plane
     end
 
+    it 'raises an error when no planes are available' do
+      airport = Airport.new
+      expect { airport.take_off }.to raise_error 'No planes available to take-off'
+    end
+    
+    
+  end
+
+  describe '#land' do
     it 'raises an error when the airport is full' do
       airport = Airport.new
-      expect { airport.land_plane }.to raise_error 'The airport is full'
+      airport.capacity.times { airport.land Plane.new }
+      expect { airport.land(Plane) }.to raise_error 'The airport is full'
     end
+
+    # it 'lands a plane upon instruction' do
+    #   airport = Airport.new
+    #   plane = Plane.new
+    #   expect(airport.land(plane)).to eq plane
+    # end
+    
+    # it 'confirms the plane has already landed' do
+    #   airport = Airport.new
+    #   plane = Plane.new
+    #   airport.land(plane)
+    #   expect(airport.plane).to eq plane
+    # end
   end
 
-  it { is_expected.to respond_to :take_off }
+  # it { is_expected.to respond_to :land }
   
-  it 'instructs a plane to take off' do
-    airport = Airport.new
-    plane = Plane.new
-    expect(airport.take_off(plane)).to eq plane
-  end
-
-  it { is_expected.to respond_to :plane }
-
-  it 'confirms the plane has taken off' do
-    airport = Airport.new
-    plane = Plane.new
-    airport.take_off(plane)
-    expect(airport.plane).to eq plane
-  end
+  # it { is_expected.to respond_to :plane }
 
 end
