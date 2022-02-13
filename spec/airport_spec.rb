97f92a744795cf71c1f1a1469babb3b906094d77
Jaroslaw Roman Cudzich <internet.jarek@gmail.com>
2022-02-13 18:59:01 +0000

    Created a program which does not allow planes to land when the airport is full.

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,7 +1,7 @@
  require_relative './lib/airport'
 
 describe Airport do
-    subject(:airport) { described_class.new }
+    subject(:airport) { described_class.new(20) }
 
     it 'instructs a plane to land' do
       expect(airport).to respond_to(:land).with(1).argument
@@ -9,5 +9,13 @@ describe Airport do
     
     it 'instructs a plane to take off' do
         expect(airport).to respond_to(:take_off).with(1).argument 
+  end
+
+    it 'does not allow landing when at capacity' do
+        plane = double :plane
+      20.times do
+        airport.land(plane)
+      end
+      expect { airport.land(plane) }.to raise_error 'Cannot land plane: aiport full'
   end
 end
--- spec/features/user_stories_spec.rb
+++ spec/features/user_stories_spec.rb
@@ -3,7 +3,7 @@ describe 'These are all the user stories' do
 # So I can get passengers to a destination 
 # I want to instruct a plane to land at an airport
 it 'so planes land at airport, instruct a plane to land' do
-    airport = Airport.new
+    airport = Airport.new(20)
     plane = Plane.new
     expect { airport.land(plane) }.not_to raise_error
   end
@@ -12,8 +12,20 @@ it 'so planes land at airport, instruct a plane to land' do
 # So I can get passengers on the way to their destination 
 # I want to instruct a plane to take off from an airport and confirm that it is no longer in the airport
   it 'so planes take off from airports, instruct a plane to take off' do
-    airport = Airport.new
+    airport = Airport.new(20)
     plane = Plane.new
     expect { airport.take_off(plane) }.not_to raise_error
+  end
+
+# As an air traffic controller 
+# To ensure safety 
+# I want to prevent landing when the airport is full 
+  it 'does not allow planes to land when the airport is full' do
+    airport = Airport.new(20)
+    plane = Plane.new
+    20.times do
+        airport.land(plane)
+    end
+    expect { airport.land(plane) }.to raise_error 'Cannot land plane: airport full'
   end
 end
