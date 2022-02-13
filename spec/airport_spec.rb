f481f618c3405d1ccb85217ae8153e82c613d372
Jaroslaw Roman Cudzich <internet.jarek@gmail.com>
2022-02-13 19:17:18 +0000

    Refactored previous commit.

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,21 +1,27 @@
  require_relative './lib/airport'
 
 describe Airport do
-    subject(:airport) { described_class.new(20) }
+  subject(:airport) { described_class.new(20) }
+  let(:plane) { double :plane }
 
+  describe '#land' do
     it 'instructs a plane to land' do
       expect(airport).to respond_to(:land).with(1).argument
   end
+
+  context 'when full' do
+  it 'raises an error' do
+    20.times do
+      airport.land(plane)
+    end
+    expect { airport.land(plane) }.to raise_error 'Cannot land plane: aiport full'
+    end
+  end
+end
     
+describe '#take_off' do
     it 'instructs a plane to take off' do
         expect(airport).to respond_to(:take_off).with(1).argument 
-  end
-
-    it 'does not allow landing when at capacity' do
-        plane = double :plane
-      20.times do
-        airport.land(plane)
-      end
-      expect { airport.land(plane) }.to raise_error 'Cannot land plane: aiport full'
+    end
   end
 end
