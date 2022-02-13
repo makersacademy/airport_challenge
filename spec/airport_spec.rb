bb5efd0fb78e834694b68d6a369a4e5a21360abf
Jaroslaw Roman Cudzich <internet.jarek@gmail.com>
2022-02-13 17:41:49 +0000

    In order for planes to be able to land at an airport, created a program which instructs a plane to land.

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,23 +1,9 @@
-# As an air traffic controller 
-# So I can get passengers to a destination 
-# I want to instruct a plane to land at an airport
+ require_relative './lib/airport'
 
-# As an air traffic controller 
-# So I can get passengers on the way to their destination 
-# I want to instruct a plane to take off from an airport and confirm that it is no longer in the airport
+describe Airport do
+    subject(:airport) { described_class.new }
 
-# As an air traffic controller 
-# To ensure safety 
-# I want to prevent landing when the airport is full 
-
-# As the system designer
-# So that the software can be used for many different airports
-# I would like a default airport capacity that can be overridden as appropriate
-
-# As an air traffic controller 
-# To ensure safety 
-# I want to prevent takeoff when weather is stormy 
-
-# As an air traffic controller 
-# To ensure safety 
-# I want to prevent landing when weather is stormy 
+    it 'instructs a plane to land' do
+      expect(airport).to respond_to(:land).with(1).argument
+  end
+end
