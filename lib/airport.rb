8e75c8c15efbf5046ec98803f9a5f00fc974ef36
Paul <pkilgarriff@gmail.com>
2022-02-25 13:05:39 +0000

    Update Airport class  - add current_weather instance variable  - add land method with exception raising  - add take_off method with exception raising  - add weather_update method  - add full? method  - add stormy? method

--- lib/airport.rb
+++ lib/airport.rb
@@ -2,13 +2,39 @@ class Airport
   def initialize(capacity = 10)
     @capacity = capacity
     @planes = []
+    @current_weather = "Unknown"
+  end
+
+  def land(plane)
+    raise "Airport is full" if full?
+    raise "Cannot land during stormy weather" if stormy?
+
+    @planes << plane
+  end
+
+  def take_off(departing_plane)
+    raise "Cannot take off during stormy weather" if stormy?
+
+    @planes.delete(departing_plane)
+  end
+
+  def weather_update(weather)
+    @current_weather = weather
   end
 
   def report_capacity
     return @capacity
   end
 
+  def full?
+    return @planes.size >= @capacity
+  end
+
+  def stormy?
+    return @current_weather == "Stormy"
+  end
+
   def list_planes
-    @planes
+    return @planes
   end
 end
