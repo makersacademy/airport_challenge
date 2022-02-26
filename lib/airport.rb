5f3a70ffa06e86e47373f3ed4f2901db32eda61b
Samuel Raducan <samuel.thembp@gmail.com>
2022-02-26 17:20:52 +0000

    Default capacity airport now can be overridden and add logic in land method

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,16 +1,25 @@
-require_relative 'plane'
-
 class Airport
-  def initialize
+  def initialize(default_capacity = 10)
     @airport_deposit = []
+    @CAPACITY = default_capacity
   end
 
   def land(plane)
-    return plane
+    if full?
+      raise Exception.new
+    else
+      @airport_deposit << plane
+    end
   end
 
   def take_off(plane)
     return @airport_deposit.delete(plane) if @airport_deposit.include?(plane)
+  end
+
+  private
+
+  def full? 
+    @airport_deposit.length >= @CAPACITY ? true : false
   end
 
 end
