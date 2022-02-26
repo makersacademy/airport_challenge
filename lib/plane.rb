dd45c480046acafc63d427436aa28a2ff8fcde9f
Valentina <maggio.vale@gmail.com>
2022-02-26 14:26:09 +0000

    Added test in plane_spec to check if plane took off

--- lib/plane.rb
+++ lib/plane.rb
@@ -1,3 +1,5 @@
 class Plane
-
+  def taken_off?
+    true
+  end
 end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -4,4 +4,11 @@ describe Plane do
   it 'creates a new plane object' do
     plane = Plane.new
   end
+
+  describe '#taken_off?'
+  it { is_expected.to respond_to :taken_off? }
+
+  it 'confirms the plane has taken off' do
+    expect(subject.taken_off?).to eq true
+  end
 end
