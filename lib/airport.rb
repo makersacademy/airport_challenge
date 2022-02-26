298f05e048e2d234a22c9334c3317be5d3bfa755
Valentina <maggio.vale@gmail.com>
2022-02-26 13:53:42 +0000

    Started testing of take_off method

--- lib/airport.rb
+++ lib/airport.rb
@@ -11,4 +11,8 @@ class Airport
   def land(plane)
     @plane = plane
   end
+
+  def take_off(plane)
+  
+  end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -8,6 +8,8 @@ describe Airport do
 
   it { is_expected.to respond_to(:land).with(1).argument }
 
+  it { is_expected.to respond_to(:take_off).with(1).argument }
+
   describe '# land' do
     it 'accepts plane to land' do
       plane = Plane.new
