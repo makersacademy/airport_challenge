c4cc2fb8a115c7708dbb685a07e2ecfc69f1c5c5
Anusha Nesargi <anushanesargi@gmail.com>
2022-02-26 16:19:33 +0000

    does not allow take off if plane is not at the airport

--- lib/airport.rb
+++ lib/airport.rb
@@ -15,12 +15,14 @@ class Airport
   end
 
   def take_off(departing_plane)
-    
     fail "No planes in the airport" if empty? == true
     fail "Stormy weather, cannot take off" if weather == "Stormy"
+    fail "Plane not at the airport" if at_aiport?(departing_plane) == false
     puts "#{departing_plane} has taken off"
     @planes_at_airport - [departing_plane]
   end
+
+  private
 
   def full?
     @planes_at_airport.count == @DEFAULT_CAPACITY
@@ -33,6 +35,10 @@ class Airport
   def weather
     @weather_condition = ["Sunny", "Rainy", "Windy", "Stormy", "Cloudy"]
     condition = @weather_condition[rand(5) - 1]
+  end
+
+  def at_aiport?(plane)
+    @planes_at_airport.include?(plane)
   end
 
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -4,14 +4,14 @@ describe Airport do
   describe ".land" do
 
     it "expects a plane to land at the airport" do
-      destination = Airport.new
-      expect(destination.land("BA101")).to eq ["BA101"]
+      bangalore_airport = Airport.new
+      expect(bangalore_airport.land("BA101")).to eq ["BA101"]
     end
 
     it "does not allow to land if weather is stormy" do
-        destination = Airport.new
-        allow(destination).to receive(:rand).and_return(4)
-        expect { destination.land("BA101") }.to raise_error "Stormy weather, cannot land"
+        bangalore_airport = Airport.new
+        allow(bangalore_airport).to receive(:rand).and_return(4)
+        expect { bangalore_airport.land("BA101") }.to raise_error "Stormy weather, cannot land"
   
       end
 
@@ -20,49 +20,60 @@ describe Airport do
   describe ".take_off" do
 
     it "expects the plane to take off from the airport" do
-      departure = Airport.new
-      allow(departure).to receive(:rand).and_return(3)
-        departure.land("BA101")
-      allow(departure).to receive(:rand).and_return(3)
-      expect(departure.take_off("BA101")).to eq []
+      heathrow_airport = Airport.new
+      allow(heathrow_airport).to receive(:rand).and_return(3)
+        heathrow_airport.land("BA101")
+      allow(heathrow_airport).to receive(:rand).and_return(3)
+      expect(heathrow_airport.take_off("BA101")).to eq []
     end
 
     it "does not allow to take_off if weather is stormy" do
-        departure = Airport.new
-        allow(departure).to receive(:rand).and_return(3)
-        departure.land("BA101")
-        allow(departure).to receive(:rand).and_return(4)
-        expect { departure.take_off("BA101") }.to raise_error "Stormy weather, cannot take off"
+        heathrow_airport = Airport.new
+        allow(heathrow_airport).to receive(:rand).and_return(3)
+        heathrow_airport.land("BA101")
+        allow(heathrow_airport).to receive(:rand).and_return(4)
+        expect { heathrow_airport.take_off("BA101") }.to raise_error "Stormy weather, cannot take off"
   
       end
 
       it "raises an error when .take_off is called and the airport is empty" do
-        departure = Airport.new
-        expect { departure.take_off("BA101") }.to raise_error "No planes in the airport"  
+        heathrow_airport = Airport.new
+        expect { heathrow_airport.take_off("BA101") }.to raise_error "No planes in the airport"  
+      end
+
+      it "does not allow to take_off if the plane is not at the airport" do
+        heathrow_airport = Airport.new
+        allow(heathrow_airport).to receive(:rand).and_return(3)
+        heathrow_airport.land("BA101")
+        heathrow_airport.land("BA103")
+        allow(heathrow_airport).to receive(:rand).and_return(3)
+        expect(heathrow_airport.take_off("BA101")).to eq ["BA103"]
+        expect { heathrow_airport.take_off("BA102") }.to raise_error "Plane not at the airport"
+  
       end
 
   end
 
   describe ".full?" do
     it "prevents landing is airport is full" do
-      destination = Airport.new(5)
-      allow(destination).to receive(:rand).and_return(3)
-      destination.land("BA101")
-      destination.land("BA102")
-      destination.land("BA103")
-      destination.land("BA104")
-      destination.land("BA105")
-      expect { destination.land("BA101") }.to raise_error "Airport is full"
+      bangalore_airport = Airport.new(5)
+      allow(bangalore_airport).to receive(:rand).and_return(3)
+      bangalore_airport.land("BA101")
+      bangalore_airport.land("BA102")
+      bangalore_airport.land("BA103")
+      bangalore_airport.land("BA104")
+      bangalore_airport.land("BA105")
+      expect { bangalore_airport.land("BA101") }.to raise_error "Airport is full"
     end
 
   end
   
   describe "changing capacity" do
     it "changes the capacity according to users need" do
-      destination = Airport.new(2)
-      allow(destination).to receive(:rand).and_return(3)
-      destination.land("BA101")
-      expect(destination.land("BA105")).to eq  ["BA101", "BA105"]
+      bangalore_airport = Airport.new(2)
+      allow(bangalore_airport).to receive(:rand).and_return(3)
+      bangalore_airport.land("BA101")
+      expect(bangalore_airport.land("BA105")).to eq  ["BA101", "BA105"]
     end
 
   end
