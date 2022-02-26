3883afd7403d4f6f0d7821d5084492bd5ecd92a6
Samuel Raducan <samuel.thembp@gmail.com>
2022-02-26 13:31:33 +0000

    Add test for plane object to accept a paramter in the init method

--- lib/plane.rb
+++ lib/plane.rb
@@ -1,2 +1,7 @@
 class Plane
+  
+  def initialize(name)
+    @name = name
+  end
+
 end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -2,13 +2,21 @@ require 'plane'
 
 describe Plane do
 
+  before :each do
+    @plane = Plane.new('name')
+  end
+
   describe '#new' do
 
     it 'returns a new plane object' do
-      plane = Plane.new
-      expect(plane).to be_an_instance_of(Plane)
+      expect(@plane).to be_an_instance_of(Plane)
     end
-    
+
+    it 'takes one parameter and returns a plane object' do
+      plane = Plane.new('Douglas A-4 Skyhawk')
+      expect(@plane).to be_an_instance_of(Plane)
+    end
+
   end
 
 end
