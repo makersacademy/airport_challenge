806f50f789184ce01a15b5c1f4e3cf28ed4d13ef
Paul <pkilgarriff@gmail.com>
2022-02-26 20:38:45 +0000

    Update airport_spec  - add class_double for Weather  - add test for print_planes  - change references from weather_update to update_weather  - change references from has_plane? to contains_plane?

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -3,7 +3,8 @@ require 'airport'
 describe Airport do
   let(:serial_number) { 25_817 }
   let(:airport) { Airport.new("Heathrow") }
-  let(:plane) { instance_double(Plane, :serial_number => serial_number) }
+  let(:plane) { instance_double("Plane", :serial_number => serial_number) }
+  let(:weather) { class_double(Weather, :weather_report => "Sunny") }
   
   before do
     allow(plane).to receive(:change_location).with(any_args)
@@ -11,6 +12,16 @@ describe Airport do
 
   it 'has a default capacity' do
     expect(airport.capacity).to eq Airport::DEFAULT_CAPACITY
+  end
+
+  it 'prints the make and serial of the current planes' do
+    allow(plane).to receive(:registration).and_return({ make: 'Boeing', serial: 25_817 })
+    output = String.new
+    2.times do
+      airport.land(plane)
+      output << "{:make=>\"Boeing\", :serial=>25817}\n"
+    end
+    expect { airport.print_planes }.to output(output).to_stdout
   end
 
   describe '#land' do
@@ -25,16 +36,16 @@ describe Airport do
     end
 
     it 'prevents planes from landing when weather is stormy' do
-      airport.weather_update("Stormy")
+      airport.update_weather("Stormy")
       expect do
         airport.land(plane)
       end.to raise_error 'Cannot land during stormy weather'
     end
   end
 
-  describe '#weather_update' do
+  describe '#update_weather' do
     it 'can receive a weather update' do
-      expect { airport.weather_update("Sunny") }.to_not raise_error
+      expect { airport.update_weather("Sunny") }.to_not raise_error
     end
   end
 
@@ -47,7 +58,7 @@ describe Airport do
     it 'removes the plane once it takes off' do
       airport.land(plane)
       airport.take_off(serial_number)
-      expect(airport.has_plane?(serial_number)).to_not include(plane)
+      expect(airport.contains_plane?(serial_number)).to be false
     end
 
     it 'prevents a plane that is not at the airport from taking off' do
@@ -58,7 +69,7 @@ describe Airport do
 
     it 'prevents planes from taking off when weather is stormy' do
       airport.land(plane)
-      airport.weather_update("Stormy")
+      airport.update_weather("Stormy")
       expect do
         airport.take_off(serial_number)
       end.to raise_error 'Cannot take off during stormy weather'
