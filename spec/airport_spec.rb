9114c94e804aaf34b584d82871e4a7a67b4a37fb
Valentina <maggio.vale@gmail.com>
2022-02-27 17:09:35 +0000

    Replaced subject with :airport naming in airport_spec

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -4,68 +4,69 @@ require 'weather'
 
 describe Airport do
 
+  subject(:airport) { described_class.new }
   let(:plane) { Plane.new }
 
   describe '#land' do
     it 'accepts a plane to land when good weather conditions' do
-      allow(subject).to receive(:stormy_weather?).and_return(false)
-      allow(subject).to receive(:at_airport).and_return(false)
-      expect(subject.land(plane)).to eq [plane]
+      allow(airport).to receive(:stormy_weather?).and_return(false)
+      allow(airport).to receive(:at_airport).and_return(false)
+      expect(airport.land(plane)).to eq [plane]
     end
 
     it 'prevents landing when weather is stormy' do
-      allow(subject).to receive(:stormy_weather?).and_return(true)
-      allow(subject).to receive(:at_airport).and_return(false)
-      expect { subject.land(plane) }.to raise_error 'Warning: stormy weather! Landing not allowed.'
+      allow(airport).to receive(:stormy_weather?).and_return(true)
+      allow(airport).to receive(:at_airport).and_return(false)
+      expect { airport.land(plane) }.to raise_error 'Warning: stormy weather! Landing not allowed.'
     end
 
     it 'prevents landing when plane already landed at same airport' do
-      allow(subject).to receive(:stormy_weather?).and_return(false)
-      allow(subject).to receive(:at_airport).and_return(true)
-      expect { subject.land(plane) }.to raise_error 'This plane is already at this airport.' 
+      allow(airport).to receive(:stormy_weather?).and_return(false)
+      allow(airport).to receive(:at_airport).and_return(true)
+      expect { airport.land(plane) }.to raise_error 'This plane is already at this airport.' 
     end
 
     it 'prevents landing when plane already landed at other airport' do
-      allow(subject).to receive(:stormy_weather?).and_return(false)
+      allow(airport).to receive(:stormy_weather?).and_return(false)
       allow(plane).to receive(:grounded).and_return(true)
-      expect { subject.land(plane) }.to raise_error 'This plane is already at an airport.' 
+      expect { airport.land(plane) }.to raise_error 'This plane is already at an airport.' 
     end
 
     it 'prevents landing when airport is full' do
-      allow(subject).to receive(:stormy_weather?).and_return(false)
-      allow(subject).to receive(:at_airport).and_return(false)
+      allow(airport).to receive(:stormy_weather?).and_return(false)
+      allow(airport).to receive(:at_airport).and_return(false)
       allow(plane).to receive(:grounded).and_return(false)
-      Airport::DEFAULT_CAPACITY.times { subject.land(plane) }
-      expect { subject.land(plane) }.to raise_error 'Airport capacity reached. No more landing allowed.'
+      Airport::DEFAULT_CAPACITY.times { airport.land(plane) }
+      expect { airport.land(plane) }.to raise_error 'Airport capacity reached. No more landing allowed.'
     end
   end
 
   describe '#take_off' do
     it 'allows plane to take off when good weather conditions' do
-      allow(subject).to receive(:stormy_weather?).and_return(false)
-      allow(subject).to receive(:left_airport?).and_return(false)
-      subject.land(plane)
-      expect(subject.take_off(plane)).to eq plane
+      allow(airport).to receive(:stormy_weather?).and_return(false)
+      allow(airport).to receive(:left_airport?).and_return(false)
+      airport.land(plane)
+      expect(airport.take_off(plane)).to eq plane
     end
 
     it 'prevents takeoff when weather is stormy' do
-      allow(subject).to receive(:stormy_weather?).and_return(true)
-      allow(subject).to receive(:left_airport?).and_return(false)
-      allow(subject).to receive(:at_airport).and_return(true)
-      expect { subject.take_off(plane) }.to raise_error 'Warning: stormy weather! Takeoff not allowed.'
+      allow(airport).to receive(:stormy_weather?).and_return(true)
+      allow(airport).to receive(:left_airport?).and_return(false)
+      allow(airport).to receive(:at_airport).and_return(true)
+      expect { airport.take_off(plane) }.to raise_error 'Warning: stormy weather! Takeoff not allowed.'
     end
 
     it 'prevents takeoff if plane already took off' do
-      allow(subject).to receive(:stormy_weather?).and_return(false)
-      allow(subject).to receive(:left_airport?).and_return(true)
-      expect { subject.take_off(plane) }.to raise_error 'This plane has already taken off.' 
+      allow(airport).to receive(:stormy_weather?).and_return(false)
+      allow(airport).to receive(:left_airport?).and_return(true)
+      expect { airport.take_off(plane) }.to raise_error 'This plane has already taken off.' 
     end
 
     it 'prevents takeoff if plane not at airport' do
-      allow(subject).to receive(:stormy_weather?).and_return(false)
-      allow(subject).to receive(:left_airport?).and_return(false)
-      allow(subject).to receive(:at_airport).and_return(false)
-      expect { subject.take_off(plane) }.to raise_error 'This plane is not at this airport.'
+      allow(airport).to receive(:stormy_weather?).and_return(false)
+      allow(airport).to receive(:left_airport?).and_return(false)
+      allow(airport).to receive(:at_airport).and_return(false)
+      expect { airport.take_off(plane) }.to raise_error 'This plane is not at this airport.'
     end
   end
 end
