963148deccbb34de366a69b803238b441990a51c
Adam Woodcock <adam.woodcock98@gmail.com>
2022-02-27 09:31:40 +0000

    Updated unit test and implemented code

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -6,4 +6,8 @@ describe Airport do
     expect(subject).to respond_to(:land)
   end
 
+  it 'should allow the air traffic controller to specify a plane to land' do
+    expect(subject).to respond_to(:land).with(1).argument
+  end
+
 end
