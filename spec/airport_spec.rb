5f51efc6427672b7db18178e80bac102716e30ef
Joe Thornton <thornton.jp@gmail.com>
2022-02-27 12:50:23 +0000

    re-added respond_to test as I was not sure whether to remove it

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,7 +1,13 @@
 require 'airport'
 
 describe Airport do
-  it "should respond to land with 1 argument" do
+  it 'responds to land with 1 argument passed' do
     expect(subject).to respond_to(:land).with(1).argument
+  end
+  
+  it 'lands a plane' do
+    plane = Plane.new
+    # We want to return the plane we land
+    expect(subject.land(plane)).to eq plane
   end
 end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -1,7 +1,5 @@
 require 'plane'
 
 describe Plane do
-  it 'should respond to land' do
-    
-  end
+
 end
