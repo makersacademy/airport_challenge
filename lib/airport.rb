3a8429829e9adfe9d74ad85407c48eb35466e630
Chris Lovell <chrislovell@chriss-air.home>
2022-02-27 15:49:29 +0000

    created class Airport which passes the airport_spec tests

---
+++ lib/airport.rb
@@ -0,0 +1,30 @@
+require './lib/plane'
+require './lib/weather'
+
+class Airport
+  attr_reader :planes, :capacity
+
+  def initialize(airport_capacity = 10)
+    @weather = Weather.new
+    @planes = []
+    @capacity = airport_capacity
+  end
+
+  def land_plane(plane)
+    raise 'Airport at full capacity' if @planes.count >= @capacity
+    raise 'The plane is in the airport already' if plane.grounded?
+    raise 'Weather conditions are not favourable for landing' if @weather.stormy?
+    plane.grounded = true
+    @planes << plane
+  end
+
+  def take_off(plane)
+    raise 'This plane is already in the air' unless plane.grounded?
+    raise 'Weather conditions are not favourable for taking off' if @weather.stormy?
+    raise "That plane is not in this airport" unless @planes.include?(plane)
+    plane.grounded = false
+    @planes.delete(plane)
+    'Plane left the airport'
+  end
+
+end
