45da9395b8c6eb15c3536fcf2d1ab89f54ccae96
Rob Oman <rob.oman@icloud.com>
2022-02-27 15:05:37 +0000

    Add functionality for weather to be generated randomly

--- lib/weather.rb
+++ lib/weather.rb
@@ -1,5 +1,20 @@
 class Weather
 
+  WEATHER_CONDITIONS = [
+    "Sunny",
+    "Stormy"
+  ]
+
+  def conditions
+    WEATHER_CONDITIONS[random]
+  end
+
+  private
+
+  def random
+    rand(0...WEATHER_CONDITIONS.length)
+  end
+
 end
 
 
--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -1,5 +1,19 @@
 require './lib/weather'
 
 describe Weather do
+  it { is_expected.to respond_to(:conditions) }
+
+  describe "#conditions" do
+    it "generates a sunny weather condition" do
+      allow(subject).to receive(:rand).and_return(0)
+      expect(subject.conditions).to eq("Sunny")
+    end
+
+    it "generates a stormy weather condition" do
+      allow(subject).to receive(:rand).and_return(1)
+      expect(subject.conditions).to eq("Stormy")
+    end
+
+  end
 
 end
