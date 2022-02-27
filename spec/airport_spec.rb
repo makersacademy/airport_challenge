436492200630b64ec1e0c6368dac34b8ff1dd94d
Valentina <maggio.vale@gmail.com>
2022-02-27 17:03:22 +0000

    Added let to remove repetitive code for plane.new in airport_spec

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -4,37 +4,34 @@ require 'weather'
 
 describe Airport do
 
+  let(:plane) { Plane.new }
+
   describe '#land' do
     it 'accepts a plane to land when good weather conditions' do
-      plane = Plane.new
       allow(subject).to receive(:stormy_weather?).and_return(false)
       allow(subject).to receive(:at_airport).and_return(false)
       expect(subject.land(plane)).to eq [plane]
     end
 
     it 'prevents landing when weather is stormy' do
-      plane = Plane.new
       allow(subject).to receive(:stormy_weather?).and_return(true)
       allow(subject).to receive(:at_airport).and_return(false)
       expect { subject.land(plane) }.to raise_error 'Warning: stormy weather! Landing not allowed.'
     end
 
     it 'prevents landing when plane already landed at same airport' do
-      plane = Plane.new
       allow(subject).to receive(:stormy_weather?).and_return(false)
       allow(subject).to receive(:at_airport).and_return(true)
       expect { subject.land(plane) }.to raise_error 'This plane is already at this airport.' 
     end
 
     it 'prevents landing when plane already landed at other airport' do
-      plane = Plane.new
       allow(subject).to receive(:stormy_weather?).and_return(false)
       allow(plane).to receive(:grounded).and_return(true)
       expect { subject.land(plane) }.to raise_error 'This plane is already at an airport.' 
     end
 
     it 'prevents landing when airport is full' do
-      plane = Plane.new
       allow(subject).to receive(:stormy_weather?).and_return(false)
       allow(subject).to receive(:at_airport).and_return(false)
       allow(plane).to receive(:grounded).and_return(false)
@@ -45,7 +42,6 @@ describe Airport do
 
   describe '#take_off' do
     it 'allows plane to take off when good weather conditions' do
-      plane = Plane.new
       allow(subject).to receive(:stormy_weather?).and_return(false)
       allow(subject).to receive(:left_airport?).and_return(false)
       subject.land(plane)
@@ -53,7 +49,6 @@ describe Airport do
     end
 
     it 'prevents takeoff when weather is stormy' do
-      plane = Plane.new
       allow(subject).to receive(:stormy_weather?).and_return(true)
       allow(subject).to receive(:left_airport?).and_return(false)
       allow(subject).to receive(:at_airport).and_return(true)
@@ -61,14 +56,12 @@ describe Airport do
     end
 
     it 'prevents takeoff if plane already took off' do
-      plane = Plane.new
       allow(subject).to receive(:stormy_weather?).and_return(false)
       allow(subject).to receive(:left_airport?).and_return(true)
       expect { subject.take_off(plane) }.to raise_error 'This plane has already taken off.' 
     end
 
     it 'prevents takeoff if plane not at airport' do
-      plane = Plane.new
       allow(subject).to receive(:stormy_weather?).and_return(false)
       allow(subject).to receive(:left_airport?).and_return(false)
       allow(subject).to receive(:at_airport).and_return(false)
