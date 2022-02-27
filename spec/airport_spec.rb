aedcc9a90a6437672ae2e7e6bd2137871b51f5ab
Adam Woodcock <adam.woodcock98@gmail.com>
2022-02-27 09:34:08 +0000

    Created second test for take off method with one argument, implemented code to satisfy

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -10,4 +10,8 @@ describe Airport do
     expect(subject).to respond_to(:land).with(1).argument
   end
 
+  it 'should allow specific planes to take off' do
+    expect(subject).to respond_to(:take_off).with(1).argument
+  end
+
 end
