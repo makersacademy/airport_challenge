6523c45d2995de9007197dd80ecbb0b9160bd98f
Valentina <maggio.vale@gmail.com>
2022-02-27 16:28:50 +0000

    Removed redundant tests from plane_spec

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -1,22 +1,17 @@
-
 require 'plane'
 
 describe Plane do
 
   describe '#landed' do
-    
-    it { is_expected.to respond_to :landed }
 
-    it 'confirms the plane has landed' do
+    it 'checks the plane has landed' do
       expect(subject.landed).to eq true
     end
   end
 
   describe '#taken_off' do
 
-    it { is_expected.to respond_to :taken_off }
-
-    it 'confirms the plane has taken off' do
+    it 'checks the plane has taken off' do
       expect(subject.taken_off).to eq false
     end
   end
