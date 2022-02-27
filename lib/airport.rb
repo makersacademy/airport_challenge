06ee1e804d36737c81fc483aae5387a6b8e2d2f0
GDRMVRM <gdrmvrm@gmail.com>
2022-02-27 18:24:39 +0000

    plane take off and airport capacity added

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,9 +1,12 @@
 require './lib/plane'
 
 class Airport
+  attr_accessor :capacity
+  DEFAULT_CAPACITY = 25
 
-  def initialize
+  def initialize(capacity = DEFAULT_CAPACITY)
     @planes = []
+    @capacity = capacity
   end
 
   def return_planes
@@ -11,10 +14,24 @@ class Airport
   end
 
   def land(plane)
+    raise "Airport capacity is reached" if full?
     @planes << plane
   end
 
   def take_off(plane)
-    
+    raise "No plane available to take off" if empty?
+    raise "Plane not available to take off" if !@planes.include?(plane)
+    @planes.delete(plane) if @planes.include?(plane)
+  end
+
+
+  private
+
+  def empty?
+   return true if @planes.empty?
+  end
+
+  def full?
+    return true if @planes.count >= capacity
   end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -3,33 +3,52 @@ require 'plane'
 
 describe Airport do
   describe ".land" do
-    it 'will not raise an error when landing a plane' do
-      airport = Airport.new
+    it 'will not raise an error when a plane lands at the airport ' do
       plane = Plane.new
-      expect { airport.land(plane) }.to_not raise_error
+      expect { subject.land(plane) }.to_not raise_error
     end
 
     it "will land a plane at the airport" do
-      airport = Airport.new
       plane = Plane.new
-      airport.land(plane)
-      expect(airport.return_planes).to match_array([plane])
+      subject.land(plane)
+      expect(subject.return_planes).to match_array([plane])
+    end
+
+    it "will not allow to land plane when airport capcity is reached" do
+      subject.capacity.times { subject.land(Plane.new) }
+      expect {subject.land(Plane.new)}.to raise_error("Airport capacity is reached")
     end
   end
 
   describe ".take_off" do
     it 'will not raise an error when a plane takes off' do
-      airport = Airport.new
       plane = Plane.new
-      expect { airport.take_off(plane) }.to_not raise_error
+      subject.land(plane)
+      expect { subject.take_off(plane) }.to_not raise_error
     end
 
-    # it "will land a plane at the airport" do
-    #   airport = Airport.new
-    #   plane = Plane.new
-    #   airport.land(plane)
-    #   expect(airport.return_planes).to match_array([plane])
-    # end
+    it "will raise an error if no plane is available to take off" do
+      plane = Plane.new
+      subject.return_planes.count == 0
+      expect { subject.take_off(plane) }.to raise_error("No plane available to take off")
+    end
+
+    it "will raise an error when a plane taking off is not in the airport" do
+      plane1 = Plane.new
+      plane2 = Plane.new
+      subject.land(plane1)
+      expect { subject.take_off(plane2) }.to raise_error("Plane not available to take off")
+    end
+
+    it "will confirm that the plane has taken off and is no longer in the airport" do
+      plane1 = Plane.new
+      plane2 = Plane.new
+      subject.land(plane1)
+      subject.land(plane2)
+      expect(subject.return_planes).to match_array [plane1, plane2]
+      subject.take_off(plane1)
+      expect(subject.return_planes).to match_array [plane2] 
+    end
   end
 
 end
