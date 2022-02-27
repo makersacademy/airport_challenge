b973e954827a73e4c29529187df79b8e22b462cf
Felix Taljaard <felixtaljaard@Felixs-MacBook-Air.local>
2022-02-27 15:06:17 +0000

    Step user story 4: whether it is stormy?

--- lib/airport.rb
+++ lib/airport.rb
@@ -10,6 +10,7 @@ class Airport
 
   def land(plane)
     raise 'Cannot land plane as capacity is full' if full?
+    raise 'Cannot land plane: weather is stormy' if stormy?
     @planes << plane
   end
   
@@ -20,5 +21,9 @@ class Airport
 
   def full?
     @planes.length >= @capacity
+  end
+
+  def stormy?
+    rand(1..6) > 4
   end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -6,11 +6,13 @@ describe Airport do
 
     describe '#land' do
       it 'instructs a plane to land' do
+        allow(airport).to receive(:stormy).and_return false
         expect(airport).to respond_to(:land).with(1).argument
       end
       
       context 'when full' do
         it 'raises an error' do
+          allow(airport).to receive(:stormy?).and_return false
           plane = double :plane
           20.times do
             airport.land(plane)
@@ -24,8 +26,10 @@ describe Airport do
     it 'instructs a plane to take off' do
       expect(airport).to respond_to(:take_off).with(1).argument
     end
+  it 'raises an error if asked to land a plane when stormy' do
+    allow(airport).to receive(:stormy).and_return true
+    expect { airport.land(plane) }.to raise_error 'Cannot land plane: weather is stormy'
   end
 end
-
-
+end
 end
