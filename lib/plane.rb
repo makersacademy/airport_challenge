ca7ab54d2c1f3594756df14b87500236f5e1bf78
ruiined <ruiined@gmail.com>
2022-02-27 17:35:36 +0000

    added plane take_off method

--- lib/plane.rb
+++ lib/plane.rb
@@ -1,9 +1,8 @@
 class Plane
-  def initialize
-    @plane
-  end
-  
   def land(plane, airport)
     airport.receive_plane(plane)
+  end
+
+  def take_off
   end
 end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -1,9 +1,14 @@
-require 'plane'
+require 'airport'
 
 describe Plane do
   let(:plane) { Plane.new }
+  let(:airport) {Airport.new}
 
-  it "lands at an airport" do
-    expect { plane.land }.to_not raise_error
+  it "lands at the airport" do
+    expect { plane.land(plane, airport) }.to_not raise_error
+  end
+
+  it "takes off from the airport" do
+    expect { plane.take_off }.to_not raise_error
   end
 end
