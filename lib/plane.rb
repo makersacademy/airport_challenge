98e519f893375a1e15a9cff869c4d9cfc15625a9
Rob Oman <rob.oman@icloud.com>
2022-02-27 13:06:28 +0000

    Add methods for plane to land

--- lib/plane.rb
+++ lib/plane.rb
@@ -1,4 +1,16 @@
 class Plane
 
+  def initialize
+    @flying = true
+  end
+
+  def land(airport)
+    @flying = false if airport.receive(self)
+    !@flying
+  end
+
+  def flying?
+    @flying
+  end
   
 end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -1,5 +1,15 @@
 require './lib/plane'
 
 describe Plane do
+  let(:airport) { double("airport") }
 
+    it { is_expected.to respond_to(:land).with(1).argument }
+    it { is_expected.to respond_to(:flying?)}
+  
+    describe "#land" do
+      it "lands the plane" do
+        allow(airport).to receive(:receive).and_return(true)
+        expect(subject.land(airport)).to eq true
+      end
+    end
 end
