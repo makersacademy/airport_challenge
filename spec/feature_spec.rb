fc35abe851da6080d149b7461e1e9beeb3b73aba
Paul <pkilgarriff@gmail.com>
2022-02-27 21:47:05 +0000

    Add feature test  - multiple planes take off to land in different airports

--- spec/feature_spec.rb
+++ spec/feature_spec.rb
@@ -11,4 +11,26 @@ describe 'Model of multiple Airports and Planes' do
     airport2.land(plane)
     expect(airport2.contains_plane?("095")).to be true
   end
+
+  it 'can have multiple planes take off from one airport then land at different airports' do
+    airports = [
+      Airport.new("Heathrow"),
+      Airport.new("Dublin"),
+      Airport.new("Liverpool"),
+      Airport.new("New York"),
+    ]
+    planes = [
+      Plane.new("Airbus", "095"),
+      Plane.new("Boeing", "747"),
+      Plane.new("Concorde", "893"),
+    ]
+    planes.each { |plane| airports[0].land(plane) }
+    ["095", "747", "893"].each { |serial| airports[0].take_off(serial) }
+    airports[1].land(planes[0])
+    airports[2].land(planes[1])
+    airports[3].land(planes[2])
+    expect(airports[1].contains_plane?("095")).to be true
+    expect(airports[2].contains_plane?("747")).to be true
+    expect(airports[3].contains_plane?("893")).to be true
+  end
 end
