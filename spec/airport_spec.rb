8779abe7dc5167a0103a31a3abcd45bc30b7d550
Joe Thornton <thornton.jp@gmail.com>
2022-02-27 12:42:23 +0000

    added test to respond_to airport.land with 1 argument; created land method

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,5 +1,7 @@
 require 'airport'
 
 describe Airport do
-  
+  it "should respond to land with 1 argument" do
+    expect(subject).to respond_to(:land).with(1).argument
+  end
 end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -1,5 +1,7 @@
 require 'plane'
 
 describe Plane do
-  
+  it 'should respond to land' do
+    
+  end
 end
