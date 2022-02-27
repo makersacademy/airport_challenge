108ddf218c8b4f44b3f00f3ab4ce97a233c43a27
Joe Thornton <thornton.jp@gmail.com>
2022-02-27 15:52:44 +0000

    added tests to confirm that a plane is no longer at an airport after takeoff by checking which plans are at that airport

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,9 +1,25 @@
+require_relative 'plane'
+
 class Airport
+  attr_reader :planes_at_airport
+
+  def initialize
+    @planes_at_airport = []
+    @capacity = 20
+  end
+  
   def land(plane)
-    plane
+    check_for_capacity
+    @planes_at_airport << plane
   end
 
   def takeoff(plane)
+    planes_at_airport.delete(plane)
     return "#{plane} has taken off"
   end
+
+  def check_for_capacity
+    fail "Airport is at capacity" if @planes_at_airport.length == @capacity
+  end
+
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -8,13 +8,26 @@ describe Airport do
   it 'lands a plane' do
     plane = Plane.new
     # We want to return the plane we land
-    expect(subject.land(plane)).to eq plane
+    expect(subject.land(plane)).to eq [plane]
   end
 
   it 'takes off a plane and confirms it has taken off' do # I'm not sure if this test and corresponding method is doing too much
     plane = Plane.new
     subject.land(plane)
     expect(subject.takeoff(plane)).to eq("#{plane} has taken off")
+  end
+
+  it 'checks that a plane is no longer at the airport after take-off' do
+    plane = Plane.new
+    airport = Airport.new
+    airport.land(plane)
+    airport.takeoff(plane)
+    expect(airport.planes_at_airport).not_to include plane
+  end
+
+  it "won't accept more than 20 planes" do
+    20.times { subject.land(Plane.new) }
+    expect { subject.land(Plane.new) }.to raise_error "Airport is at capacity"
   end
 
 end
