26c1073fdee847df71419727585b250fccf3fd63
Kenny Sweeney-Baird <kennysweeneybaird1@gmail.com>
2022-02-28 01:30:48 +0000

    user stories done

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,6 +1,6 @@
 class Airport
   attr_reader :planes, :capacity
-  def initialize(capacity=1)
+  def initialize(capacity = 1)
     @planes = []
     @capacity = capacity
   end
@@ -8,31 +8,45 @@ class Airport
   def receive_plane(plane)
     if @planes.length >= @capacity
       raise Exception.new
+    elsif stormy?
+      return "It's too stormy, can't land!"
     else
-      if stormy?
-        return "it's too stormy, can't land!"
-      else
-        @planes << plane
-      end
+      land_valid_plane(plane)
     end
   end
 
   def take_off_plane(plane)
-    if @planes.empty?
+    if !(@planes.include?(plane))
       raise Exception.new
-    end
-
-    if stormy?
+    elsif stormy?
       return "It's too stormy, can't takeoff!"
     else
-      plane_to_be_returned = plane
-      @planes.delete(plane)
-      return plane_to_be_returned
+      take_off_valid_plane(plane)
     end
   end
 
   def stormy?
     weather_result = rand(1..10)
-    weather_result > 9 ? true : false
+    weather_result > 9
+  end
+
+  def land_valid_plane(plane)
+    if plane.flying
+      @planes << plane
+      plane.set_status_as_landed
+    else
+      return "Plane has already landed"
+    end
+  end
+
+  def take_off_valid_plane(plane)
+    unless plane.flying
+     plane_to_be_returned = plane
+      plane.set_status_as_flying
+      @planes.delete(plane)
+      return plane_to_be_returned
+    else
+      return "Plane is already flying"
+    end
   end
 end
--- spec/air_traffic_controller_spec.rb
+++ spec/air_traffic_controller_spec.rb
@@ -1,23 +1,25 @@
 require 'air_traffic_controller'
-require_relative '../lib/airplane.rb'
-require_relative '../lib/airport.rb'
+require_relative '../lib/airplane'
+require_relative '../lib/airport'
 
 describe AirTrafficController do
-  let(:plane) {AirPlane.new}
-  let(:airport) {Airport.new}
+  let(:plane) { AirPlane.new }
+  let(:airport) { Airport.new }
   context "The tests for the air traffic controller's functionality" do
     it "lands a plane at the airport" do
-      # allow(:airport).to receive(:stormy?) { true }
-    subject.land_plane(plane, airport)
-    expect(airport.planes).to eq([plane])
+      allow(airport).to receive(:rand) { 1 }
+      subject.land_plane(plane, airport)
+      expect(airport.planes).to eq([plane])
     end
 
     it "takes off a plane from the airport" do
+      allow(airport).to receive(:rand) { 1 }
       subject.land_plane(plane, airport)
       expect(subject.take_off_plane(plane, airport)).to eq(plane)
     end
 
     it "ensures the plane is no longer in the airport after taking off" do
+      allow(airport).to receive(:rand) { 1 }
       subject.land_plane(plane, airport)
       expect(subject.take_off_plane(plane, airport)).to eq(plane)
       expect(airport.planes).to eq([])
@@ -25,16 +27,22 @@ describe AirTrafficController do
 
     it "prevents a plane from landing when the airport is full" do
       stormy_airport = Airport.new
-      allow(:stormy_airport).to receive(:rand) { 10 }
-      expect(subject.land_plane(plane, stormy_airport)).to eq("it's too stormy, can't land!")
+      allow(stormy_airport).to receive(:rand) { 10 }
+      expect(subject.land_plane(plane, stormy_airport)).to eq("It's too stormy, can't land!")
     end
 
     it "Stops planes from taking off when the weather is stormy" do
       stormy_airport = Airport.new
-      allow(:stormy_airport).to receive(:rand) { 5 }
+      #makes the weather not stormy so it can receive a plane
+      allow(stormy_airport).to receive(:rand) { 1 }
       subject.land_plane(plane, stormy_airport)
-      allow(:stormy_airport).to receive(:rand) { 10 }
-      expect(subject.take_off_plane(plane, stormy_airport)).to eq("it's too stormy, can't takeoff!")
+      #makes the weather stormy so the error message will be returned
+      allow(stormy_airport).to receive(:rand) { 10 }
+      expect(subject.take_off_plane(plane, stormy_airport)).to eq("It's too stormy, can't takeoff!")
+    end
+
+    it "stops planes from taking off when they're in the air" do
+      expect{subject.take_off_plane(plane, airport)}.to raise_error
     end
   end
 end
