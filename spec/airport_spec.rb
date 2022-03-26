61927c2f4525870c138a7df7c20afd2c54c32f35
Michael Su <mailmsuu@gmail.com>
2022-03-26 12:33:10 +0000

    Re-added unit test for story 1, small changes elsewhere

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -5,13 +5,19 @@ describe Airport do
     allow(subject).to receive(:stormy?).and_return(false)
   end
 
+  it 'Should have a default capacity that can be overwritten as appropriate' do
+    airport = Airport.new(50)
+    expect(airport.capacity).to eq 50
+  end
+
   describe '#land' do 
+    it { is_expected.to respond_to :land }
+
     it 'Should not let planes land when airport is full' do
       Airport::DEFAULT_CAPACITY.times do subject.land(double) end
       expect { subject.land double }.to raise_error 'Airport full'
     end
   end
-
 
   describe '#take_off' do
     it 'Should let plane take off and confirm it is no longer in the airport' do
@@ -19,11 +25,6 @@ describe Airport do
       subject.take_off
       expect(subject.planes.count).to eq 0
     end
-  end
-
-  it 'Should have a default capacity that can be overwritten as appropriate' do
-    airport = Airport.new(50)
-    expect(airport.capacity).to eq 50
   end
 
   context "In stormy conditions" do
