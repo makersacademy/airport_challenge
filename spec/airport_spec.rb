fe6f3fd1302b6a164e7323024ba8b72908994a7f
Michael Su <mailmsuu@gmail.com>
2022-03-26 12:11:22 +0000

    Added context blocks to unit tests for better readability

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,44 +1,45 @@
 require 'airport'
 
 describe Airport do
+  before do
+    allow(subject).to receive(:stormy?).and_return(false)
+  end
 
   describe '#land' do 
-    it { is_expected.to respond_to :land }
-
     it 'Should not let planes land when airport is full' do
-      allow(subject).to receive(:stormy?).and_return(false)
       Airport::DEFAULT_CAPACITY.times do subject.land(double) end
       expect { subject.land double }.to raise_error 'Airport full'
-    end
-
-    it 'Should prevent landing when weather is stormy' do
-        allow(subject).to receive(:stormy?).and_return(true)
-        plane = double
-        expect { subject.land plane}.to raise_error 'Cannot land when stormy'
     end
   end
 
 
   describe '#take_off' do
-    it { is_expected.to respond_to :take_off }
-
     it 'Should let plane take off and confirm it is no longer in the airport' do
-      allow(subject).to receive(:stormy?).and_return(false)
       subject.land(double)
       subject.take_off
       expect(subject.planes.count).to eq 0
-    end
-
-    it 'Should prevent take off when weather is stormy' do
-      allow(subject).to receive(:stormy?).and_return(true)
-      plane = double
-      expect { subject.take_off }.to raise_error 'Cannot take off when stormy'
     end
   end
 
   it 'Should have a default capacity that can be overwritten as appropriate' do
     airport = Airport.new(50)
     expect(airport.capacity).to eq 50
+  end
+
+  context "In stormy conditions" do
+    before do
+      allow(subject).to receive(:stormy?).and_return(true)
+    end
+
+    it 'Should prevent landing when weather is stormy' do
+      plane = double
+      expect { subject.land plane}.to raise_error 'Cannot land when stormy'
+    end
+
+    it 'Should prevent take off when weather is stormy' do
+      plane = double
+      expect { subject.take_off }.to raise_error 'Cannot take off when stormy'
+    end
   end
 end
   
