eb392ae82e7a155961ae3da9ce404b9485ad3653
Baljit Rakhra <baljit.rakhra@gmail.com>
2022-03-27 12:45:15 +0100

    user story to stop landing during stomy weather added

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -3,22 +3,24 @@ require 'airport'
 describe Airport do
   let(:plane) { Plane.new }
   let(:airport) { Airport.new }
+  
   it { is_expected.to respond_to(:land).with(1).argument }    
   
-
   describe '# landing' do
-    
-    it 'the planes' do
-      expect(subject.land(plane)).to eq(subject.planes)
-    end
-    
+      
     it 'has a Default capcity' do
-      expect(subject.capacity).to eq(Airport::DEFAULT_CAPACITY)
+      expect(airport.capacity).to eq(Airport::DEFAULT_CAPACITY)
     end
 
     it '--to be prevented when airport is full' do 
-      Airport::DEFAULT_CAPACITY.times { subject.land Plane.new }
-      expect { subject.land Plane.new }.to raise_error 'Airport is full no landing allowed'
+      allow(airport).to receive(:stormy?).and_return false  
+      Airport::DEFAULT_CAPACITY.times { airport.land Plane.new }
+      expect { airport.land Plane.new }.to raise_error 'Airport is full no landing allowed'
+    end
+
+    it 'is prevented due to stormy weather' do
+      allow(airport).to receive(:stormy?).and_return true
+      expect { airport.land plane }.to raise_error "Due to stormy weather no landing allowed" 
     end
   end
   
