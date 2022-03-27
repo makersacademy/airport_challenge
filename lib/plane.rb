7b8e4b7eb6e5b5277af71d84d7de4570fc6ab5e7
almorcrette <26277447+almorcrette@users.noreply.github.com>
2022-03-27 12:54:09 +0100

    Prevent landing when weather is stormy user story complete. Test drived with RSpec - 99.10% coverage. Rubocop passed

--- lib/plane.rb
+++ lib/plane.rb
@@ -8,16 +8,16 @@ class Plane
     @location = :in_air
   end
 
-  def land(airport)
+  def land(airport, weather)
     raise "Airport full" if airport.full?
+    raise "Stormy weather" if weather.stormy? == true
     airport.hangar.push(self)
     @location = airport
     self
   end
 
-  def take_off
+  def take_off(weather)
     raise "Already in the air" if @location == :in_air
-    weather = Weather.new
     raise "Stormy weather" if weather.stormy? == true
     @location.hangar.delete(self)
     @location = :in_air
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,6 +2,10 @@ require './lib/airport'
 
 describe Airport do
 
+  before(:each) do
+    @weather = Weather.new
+  end
+
   it "is created with a default capacity" do
     airport = Airport.new
     expect(airport.capacity).to be_an(Integer)
@@ -20,7 +24,10 @@ describe Airport do
 
   it "can be full" do
     airport = Airport.new
-    airport.capacity.times { Plane.new.land(airport) }
+    airport.capacity.times do
+      allow(@weather).to receive(:stormy?).and_return(false)
+      Plane.new.land(airport, @weather)
+    end
     expect(airport.full?).to be true
   end
 
