332c98543780ba22aed37fc605c8e5fc19eb721f
Michael Su <mailmsuu@gmail.com>
2022-03-27 12:15:17 +0100

    Moved edge cases into airport_spec

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -21,6 +21,12 @@ describe Airport do
       Airport::DEFAULT_CAPACITY.times do subject.land(Plane.new) end
       expect { subject.land(Plane.new) }.to raise_error 'Airport full'
     end
+
+    it 'Planes that are landed can not land again and should be inside a airport' do
+      plane = Plane.new
+      subject.land(plane)
+      expect { subject.land(plane) }.to raise_error 'This plane is already landed'
+    end
   end
 
   describe '#take_off' do
@@ -29,6 +35,20 @@ describe Airport do
       subject.land(plane)
       subject.take_off(plane)
       expect(subject.planes.include?(plane)).to eq false
+    end
+
+    it 'Planes already in flight should not be able to take off' do
+      expect { subject.take_off(Plane.new) }.to raise_error 'Plane already in flight'
+    end
+
+    it 'Planes should only be able to take off from the airport they are in' do
+      plane = Plane.new
+      airport1 = Airport.new
+      airport2 = Airport.new
+      allow(airport1).to receive(:stormy?).and_return(false)
+      allow(airport2).to receive(:stormy?).and_return(false)
+      airport1.land(plane)
+      expect { airport2.take_off(plane) }.to raise_error 'This plane is not in this station'
     end
   end
 
