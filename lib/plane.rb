0fd147d293ed5e99a0583124e173211e4611a2c5
Coral-Ann <coral.baker@outlook.com>
2022-03-27 11:20:09 +0100

    Added/tested functionality of 'Plane.flying?' method.

--- lib/plane.rb
+++ lib/plane.rb
@@ -1,4 +1,11 @@
 class Plane
+  attr_reader :flying
+
+  def initialize
+    @flying = true
+  end
+
   def flying?
+    @flying == true ? true : false
   end
 end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -1,5 +1,12 @@
 require_relative '../lib/plane.rb'
 
 describe Plane do
-  it { is_expected.to respond_to :flying? }
+  
+  describe '#flying?' do
+    it { is_expected.to respond_to :flying? }
+
+    it 'should return true if plane is flying' do
+      expect(subject.flying?).to be_truthy
+    end
+  end
 end
