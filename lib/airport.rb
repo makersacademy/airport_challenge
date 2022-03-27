1fffebcf02c7650ccb76dd435c6ffc1d0988b23a
Coral-Ann <coral.baker@outlook.com>
2022-03-27 11:30:38 +0100

    Added/tested functionality of 'Airport.land()' method. Added double in test.

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,10 +1,17 @@
+require_relative '../lib/plane.rb'
+
 class Airport
+  attr_reader :hangar
+
+  def initialize
+    @hangar = []
+  end
 
   def land(plane)
-
+    @hangar << plane
   end
 
   def take_off(plane)
-  
+    
   end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,6 +1,17 @@
 require_relative '../lib/airport.rb'
 
 describe Airport do
-  it { is_expected.to respond_to(:land).with(1).argument }
-  it { is_expected.to respond_to(:take_off).with(1).argument }
+  let(:plane) { double :plane }
+
+  describe '#land' do
+    it { is_expected.to respond_to(:land).with(1).argument }
+
+    it 'should store a plane in hangar when landed' do
+      expect(subject.land(plane)).not_to be_empty
+    end
+  end
+
+  describe '#take_off' do
+    it { is_expected.to respond_to(:take_off).with(1).argument }
+  end
 end
