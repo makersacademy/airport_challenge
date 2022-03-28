bc303287e5d10870414b2d96128d39fb6b468159
Joe McNally <joe.t.mcnally@hotmail.com>
2022-03-28 11:52:17 +0100

    Added private methods to the Plane class in order to shorten the land and take_off methods.

--- lib/plane.rb
+++ lib/plane.rb
@@ -9,20 +9,30 @@ class Plane
   end
 
   def land(airport)
-    raise "#{self} is already landed" if @state == "Landed"
-    raise "#{self} cannot land, the weather is stormy" if airport.weather == "Stormy"
-    raise "#{airport} is full" if airport.hangar.length == airport.capacity
+    check_landing(airport)
     puts "#{self} has landed at #{airport}"
     self.state = "Landed"
     airport.hangar.push(self)
   end
 
   def take_off(airport)
-    raise "#{self} is already flying" if @state == "Flying"
-    raise "#{self} cannot take off, the weather is stormy" if airport.weather == "Stormy"
-    raise "#{self} is not currently at #{airport}" unless airport.hangar.include?(self)
+    check_take_off(airport)
     puts "#{self} has left #{airport}"
     self.state = "Flying"
     airport.hangar.delete(self)
+  end
+
+  private
+
+  def check_landing(airport)
+    raise "#{self} is already landed" if @state == "Landed"
+    raise "#{self} cannot land, the weather is stormy" if airport.weather == "Stormy"
+    raise "#{airport} is full" if airport.hangar.length == airport.capacity
+  end
+
+  def check_take_off(airport)
+    raise "#{self} is already flying" if @state == "Flying"
+    raise "#{self} cannot take off, the weather is stormy" if airport.weather == "Stormy"
+    raise "#{self} is not currently at #{airport}" unless airport.hangar.include?(self)
   end
 end
