acabf30d88779b7436a185f0a9e55db6e7154d37
Luke Storey <lukemstorey@gmail.com>
2022-04-23 17:09:28 +0100

    Track and change landed state of plane

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -1,6 +1,23 @@
 describe Plane do
-  describe '#initialize' do
-    subject { Plane }
-    it { should respond_to(:new) }
+  subject(:plane) { described_class.new}
+
+  context 'when the plane is created' do
+    it 'should default to not landed' do
+      expect(plane.landed).to be false
+    end
+  end
+
+  describe '#land' do
+    it 'should land the plane' do
+      plane.land
+      expect(plane.landed).to be_truthy
+    end
+  end
+
+  describe '#take_off' do
+    it 'should take off the plane' do
+      plane.take_off
+      expect(plane.landed).to be_falsey
+    end
   end
 end
