66408f1892e0b0abd318538fa5792da17dc3d859
Sam Button <sam_button_94@hotmail.co.uk>
2022-04-23 03:51:33 +0100

    added land test

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,8 +1,13 @@
 require './docs/airport_challenge'
+
 describe Airport do
-  it 'can create an instance of airport' do
+  it 'creates instance of airport' do
     airport = Airport.new
     expect(airport).to be_kind_of(Airport)
   end
   
+  it 'allows plane to land' do
+    airport = Airport.new
+    expect(airport).to respond_to :land
+  end
 end
