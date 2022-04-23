6c02593a32ad9a89255d0f9d0c2554a018c0d5ef
Yaroslava Yates <yaroslava.yates@gmail.com>
2022-04-23 14:18:16 +0100

    land method with 1 arg

--- spec/airport_challenge_spec.rb
+++ spec/airport_challenge_spec.rb
@@ -4,7 +4,7 @@ describe Airport do
   
   it "instructs a plane to '#land' at an airport" do
     airport = Airport.new
-    expect(airport).to respond_to(:land)
+    expect(airport).to respond_to(:land).with(1).argument
   end
 
 end
