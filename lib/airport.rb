ad8294548a54a4e9f9700bd3aa5253f6beb951dc
Luke Storey <lukemstorey@gmail.com>
2022-04-23 08:56:42 +0100

    Describe #land to take argument

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,4 +1,4 @@
 class Airport
-  def land
+  def land(plane)
   end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,5 +1,7 @@
 describe Airport do
   describe '#land' do
-    it { should respond_to :land }
+    it { should respond_to(:land) }
+
+    it { should respond_to(:land).with(1) }
   end
 end
