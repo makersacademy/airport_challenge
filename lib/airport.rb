eeaa8c391f86d0a4b48aac27f5642812336820a7
Anish <anishkakaiya@outlook.com>
2022-04-23 11:40:03 +0100

    add default capacity with changeable capacity

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,12 +1,14 @@
 class Airport
   def initialize(capacity = 5)
     @landed_planes = []
+    @capacity = capacity
   end
 
-  attr_accessor :landed_planes
+  attr_reader :capacity
+  attr_reader :landed_planes
 
   def land(plane)
-    fail "Error: Max capacity for planes in Airport has been reached." if landed_planes.count >= 5
+    fail "Error: Max capacity for planes in Airport has been reached." if landed_planes.count >= capacity
     landed_planes << plane
   end
 
