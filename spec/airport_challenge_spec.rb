d1f89761ed10392cf2355c353915110e91331d5a
Sam Button <sam_button_94@hotmail.co.uk>
2022-04-23 03:11:18 +0100

    Added airport instance test

--- spec/airport_challenge_spec.rb
+++ spec/airport_challenge_spec.rb
@@ -1,2 +1,8 @@
 require './docs/airport_challenge'
-
+describe Airport do
+  it 'can create an instance of airport' do
+    airport = Airport.new
+    expect(airport).to be_kind_of(Airport)
+  end
+  
+end
