676ce7d911977e91b0dd40e4a449ebec1c0c8cbc
Anish <anishkakaiya@outlook.com>
2022-04-23 11:16:53 +0100

    add test to raise error if airport capacity is reached

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -40,4 +40,10 @@ describe Airport do
     airport.land(plane2)
     expect(airport.take_off(plane)).to eq(plane)
   end
+
+  it "raises error if airport has reached capacity of landed planes" do
+    airport = Airport.new
+    5.times { airport.land(Plane.new) }
+    expect{airport.land(Plane.new)}.to raise_error("Error: Max capacity for planes in Airport has been reached.")
+  end
 end
