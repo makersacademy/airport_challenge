1b1403fcc0852906fcaba7d8b1531e18fce70a69
Jimmy Lyons <jimmylyons1@hotmail.co.uk>
2022-04-23 09:30:08 +0100

    Tests for new Airport Class and land_plane method

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,1 +1,7 @@
- 
+class Airport
+
+  def land_plane (plane)
+    @planes_in_airport = plane
+  end
+
+end 
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,1 +1,15 @@
- 
+ require 'airport'
+
+ describe Airport do
+   
+  it { is_expected.to respond_to :land_plane }
+
+  describe '#land_plane' do
+    it 'allows plane to land in airport' do
+      plane = "plane"
+      
+      expect(subject.land_plane(plane)).to eq(plane)
+    end
+  end
+
+ end
