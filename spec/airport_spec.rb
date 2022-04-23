c139d52fe674156f1f7509bf898d90270d54b524
Gawain Hewitt <gawain@gawainhewitt.co.uk>
2022-04-23 14:16:07 +0100

    I'm going to start again as I think my strategy is wrong, so this is to show that I got to the end of story 3 this way before I start again

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,16 +1,19 @@
 require 'airport'
 
 describe Airport do
-  let(:plane) { Plane.new }
+  
+  describe 'landing' do
+    let(:plane) { Plane.new }
 
-  it 'only allows a plane to land if there is space' do
-    plane2 = Plane.new
-    subject.land(plane)
-    expect { subject.land(plane2) }.to raise_error "can't land, airport full"
-  end
+    it 'prevents landing when the airport is full' do
+      plane2 = Plane.new
+      subject.land(plane)
+      expect { subject.land(plane2) }.to raise_error "can't land, airport full"
+    end
 
-  it 'planes that are landed cannot land again' do
-    subject.land(plane)
-    expect { subject.land(plane) }.to raise_error "plane already landed"
+    it 'planes that are landed cannot land again' do
+      subject.land(plane)
+      expect { subject.land(plane) }.to raise_error "plane already landed"
+    end
   end
 end
