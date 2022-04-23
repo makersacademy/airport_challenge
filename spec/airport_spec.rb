cc6f291bf170cfed70513cd5eae884eda9c3ff99
Anish <anishkakaiya@outlook.com>
2022-04-23 11:46:57 +0100

    refactor code to have plane_capacity constant

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -43,7 +43,7 @@ describe Airport do
 
   it "raises error if airport has reached capacity of landed planes" do
     airport = Airport.new
-    5.times { airport.land(Plane.new) }
+    Airport::PLANE_CAPACITY.times { airport.land(Plane.new) }
     expect { airport.land(Plane.new) }.to raise_error("Error: Max capacity for planes in Airport has been reached.")
   end
 
