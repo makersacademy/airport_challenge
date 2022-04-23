b8f97fc103e6a1df0a4b0e22e7ba9496ef550595
Anish <anishkakaiya@outlook.com>
2022-04-23 11:17:15 +0100

    raise error if airport capacity of 5 planes has been reached

--- lib/airport.rb
+++ lib/airport.rb
@@ -3,11 +3,16 @@ class Airport
     @landed_planes = []
   end
 
+  def landed_planes
+    @landed_planes
+  end
+
   def land(plane)
-    @landed_planes << plane
+    fail "Error: Max capacity for planes in Airport has been reached." if landed_planes.count >= 5
+    landed_planes << plane
   end
 
   def take_off(plane)
-    @landed_planes.delete(plane)
+    landed_planes.delete(plane)
   end
 end
