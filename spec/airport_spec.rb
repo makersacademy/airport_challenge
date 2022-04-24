ab7ca892f7bc2947f3f0934a135afdf85a7a9699
Luiza <luiza@Luizas-MacBook-Air.local>
2022-04-24 10:15:02 +0100

    Refactor the code using doubles

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,76 +2,81 @@ require './lib/airport'
 require './lib/weather'
 
 describe Airport do
-  # Lands a plane
-  it 'responds to method land' do
-    expect(subject).to respond_to :land
-  end
-
-  it '#land accepts one argument' do
-    expect(subject).to respond_to(:land).with(1).argument
-  end
-
-  it '#land lands a plane' do
-    plane = Plane.new
-    expect(subject.land(plane)).to eq subject.planes
-  end
-
-  it 'rasies an error when airport is full' do
-    subject.capacity.times {subject.land(Plane.new)}
-    expect {subject.land(Plane.new)}.to raise_error 'Airport full'
-  end
 
   # Capacity
-  it 'has a default capacity' do
-    expect(subject.capacity).to eq Airport::DEFAULT_CAPACITY
+  describe '#capacity' do
+
+    it 'has a default value' do
+      expect(subject.capacity).to eq Airport::DEFAULT_CAPACITY
+    end
+
+    it 'can be set to a specific value' do
+      airport = Airport.new(10)
+      expect(airport.capacity).to eq 10
+    end
+
   end
 
-  it 'allows to set capacity' do
-    airport = Airport.new(10)
-    expect(airport.capacity).to eq 10
+  # Lands a plane
+  describe '#land' do
+
+    it 'airport responds to the method' do
+      expect(subject).to respond_to :land
+    end
+
+    it 'accepts one argument' do
+      expect(subject).to respond_to(:land).with(1).argument
+    end
+
+    it 'lands a plane' do
+      plane = double (:plane)
+      expect(subject.land(plane)).to eq subject.planes
+    end
+
+    it 'rasies an error when airport is full' do
+      plane = double (:plane)
+      subject.capacity.times {subject.land(plane)}
+      expect {subject.land(plane)}.to raise_error 'Airport full'
+    end
+
   end
 
   # Takes off a plane
-  it 'responds to the take_off method' do
-    expect(subject).to respond_to :take_off
-  end
-
-  it '#take_off accepts one argument' do
-    expect(subject).to respond_to(:take_off).with(1).argument
-  end
-
-  xit '#take_off takes-off the plane' do
-    plane = Plane.new
-    subject.land(plane)
-    expect(subject.take_off(plane)).to eq plane
-  end
-
-  it 'confirms the plane has left the airport' do
-    plane = Plane.new
-    subject.land(plane)
-    weather = class_double('Weather').as_stubbed_const
-    allow(weather).to receive(:stormy?).and_return(false)
-    #expect(plane.left_airport?).to eq true
-    expect(subject.take_off(plane)).to be_left_airport
-  end
-
   describe '#take_off' do
+
+    it 'airport responds to the method' do
+      expect(subject).to respond_to :take_off
+    end
+
+    it 'accepts one argument' do
+      expect(subject).to respond_to(:take_off).with(1).argument
+    end
+
     it 'raises an error when the weather is stormy' do
-      plane = Plane.new
+      plane = double (:plane)
       subject.land(plane)
       weather = class_double('Weather').as_stubbed_const
       allow(weather).to receive(:stormy?).and_return(true)
       expect{ subject.take_off(plane) }.to raise_error 'Take-off not allowed due to adverse weather'
     end
 
-    it 'is allowed when the weather is NOT stormy' do
-      plane = Plane.new
+    it 'takes-off the plane when the weather is NOT stormy' do
+      plane = double (:plane)
       subject.land(plane)
       weather = class_double('Weather').as_stubbed_const
       allow(weather).to receive(:stormy?).and_return(false)
       expect(subject.take_off(plane)).to eq plane
     end
 
+    it 'confirms the plane has left the airport' do
+      plane = double (:plane)
+      allow(plane).to receive(:left_airport?).and_return(true)
+      subject.land(plane)
+      weather = class_double('Weather').as_stubbed_const
+      allow(weather).to receive(:stormy?).and_return(false)
+      #expect(plane.left_airport?).to eq true
+      expect(subject.take_off(plane)).to be_left_airport
+    end
 
   end 
 
