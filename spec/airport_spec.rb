daa569978320897597c20db502b0dc683045b1bf
George Dainton <gcdainton94@gmail.com>
2022-04-24 10:50:10 +0100

    Takeoff method for Airport that takes instances of Plane as argument

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -11,4 +11,14 @@ describe Airport do
     airport = Airport.new
     expect(airport).to respond_to(:land).with(1).argument
   end
+
+  it 'can respond to takeoff' do
+    plane = Plane.new 
+    airport = Airport.new
+    expect(airport). to respond_to(:takeoff).with(1).argument
+  end
+  
+
+
+
 end 
