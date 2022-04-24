7ef45a491ccd2cf541973cda4096c67b35294963
Anish <anishkakaiya@outlook.com>
2022-04-24 11:59:54 +0100

    add code to pass edge case test for landed planes

--- lib/airport.rb
+++ lib/airport.rb
@@ -13,6 +13,7 @@ class Airport
 
   def land(plane)
     fail "Error: Max capacity for planes in Airport has been reached." if full?
+    fail "Plane already landed in Airport." if landed_planes.include?(plane)
     landed_planes << plane
   end
 
