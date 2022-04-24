25b4e973d08d652177c99017575133e7f2425276
Tom O'Neill <tomoneill32@gmail.com>
2022-04-24 17:52:21 +0100

    User story 3 - no landing when the airport is full

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,18 +1,27 @@
 require_relative 'plane'
 
 class Airport
-  attr_reader :stored_planes
+  attr_reader :stored_planes, :capacity
+  CAPACITY = 5
 
-  def initialize
+  def initialize 
     @stored_planes = []
+    @capacity = CAPACITY
   end
 
   def land(plane)
+    fail 'Airport full!' if full?
     @stored_planes << plane
   end
 
   def depart(plane)
     @stored_planes.delete(plane)
+  end
+
+  private
+
+  def full?
+    @stored_planes.length >= @capacity
   end
 
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,11 +1,18 @@
 require 'airport'
 
 describe Airport do
-  it 'Can land a plane at the airport' do
-    plane = Plane.new
-    expect(subject.land(plane)).to eq([plane])
-  end
+  describe '#land' do
+    it 'Can do so' do
+      plane = Plane.new
+      expect(subject.land(plane)).to eq([plane])
+    end
 
+    it 'Will not do so if the airport is full' do
+      Airport::CAPACITY.times { subject.land(Plane.new) }
+      expect { subject.land(Plane.new) }.to raise_error 'Airport full!'
+    end
+  end
+  
   it 'Can instruct a stored plane to take off from the airport' do
     plane = Plane.new
     subject.land(plane)
