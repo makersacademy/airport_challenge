5f379bcc1a4c114d9f8a0f2ec4f7b212fdad1f57
alexandrospapagiannis <alpa31291@gmail.com>
2022-04-24 15:09:32 +0100

    Describing the land method and defining it in the airport.rb

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,4 +1,11 @@
 require 'airport'
 describe Airport do
+  
+  describe '#land' do
+    it 'expects a new instance of the class to respond to the land method' do
+      expect { subject.to respond_to(:land)}
+    end
+  end
+
 
 end
