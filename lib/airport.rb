9def9468625df7a830701f908641ba03acc8fe01
Tom O'Neill <tomoneill32@gmail.com>
2022-04-24 17:34:48 +0100

    Second user story, plane to take off from airport

--- lib/airport.rb
+++ lib/airport.rb
@@ -3,7 +3,16 @@ require_relative 'plane'
 class Airport
   attr_reader :stored_planes
 
-  def land(plane)
-    @stored_planes = plane
+  def initialize
+    @stored_planes = []
   end
+
+  def land(plane)
+    @stored_planes << plane
+  end
+
+  def depart(plane)
+    @stored_planes.delete(plane)
+  end
+
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -3,6 +3,13 @@ require 'airport'
 describe Airport do
   it 'Can land a plane at the airport' do
     plane = Plane.new
-    expect(subject.land(plane)).to eq(plane)
+    expect(subject.land(plane)).to eq([plane])
+  end
+
+  it 'Can instruct a stored plane to take off from the airport' do
+    plane = Plane.new
+    subject.land(plane)
+    subject.depart(plane)
+    expect(subject.stored_planes).to eq []
   end
 end
