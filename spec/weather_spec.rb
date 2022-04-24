d606bca7aef76f43f28962e57e04b902b78c641c
Anish <anishkakaiya@outlook.com>
2022-04-24 21:25:10 +0100

    add test to return stormy if weather is stormy

--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -4,4 +4,9 @@ describe Weather do
   it "can make a new instance of Weather class" do
     expect(Weather.new).to be_an_instance_of(Weather)
   end
+
+  it "can return a forecast" do
+    weather = Weather.new
+    allow(weather).to receive(:forecast).and_return("Stormy")
+  end
 end
