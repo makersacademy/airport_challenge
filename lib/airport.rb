41ab24c77399f31a133253e293b47129dc7e13ec
Archie <archieparchie@Archies-MacBook-Air.local>
2022-04-24 21:32:30 +0100

    Exercise almost complete, everything functioning, only I need to pslit up classes, and planes can exist in two places at once...sorry for not committing enough :(

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,17 +1,55 @@
 class Airport
 
-  def full?
-    
+  DEFAULT_CAPACITY = 10
+
+  attr_reader :capacity
+  attr_reader :plane
+  attr_reader :weather
+
+  def initialize(capacity=DEFAULT_CAPACITY)
+    @capacity = capacity
+    @hanger = []
+    weather = rand(20) + 1
+    if weather >= 15
+      @weather = 'STORMY'
+    else 
+      @weather = 'GOOD'
+    end
   end
+
+  
+  def land(plane)
+    fail 'Landing prevented; hanger is full' if @hanger.count >= @capacity
+    fail 'Landing denied; storms overhead' unless @weather == 'GOOD'
+    @hanger << plane
+  end
+  
+  def take_off(plane)
+    fail 'No planes ready to take off' if @hanger.empty?
+    fail 'Take off denied; storms incoming' if @weather == 'STORMY'
+    @hanger.delete(plane)
+  end
+
+  def check_hanger
+    @hanger
+  end
+
+  def check_weather
+    @weather
+  end
+
+  private
+
+  
+  def full?
+    @hanger.count >= @capacity ? true : false
+  end
+
+  
 
 end
 
 class Plane
 
-  def land
-  end
   
-  def take_off
-  end
-
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,27 +2,95 @@ require 'airport.rb'
 
 describe Airport do
 
-  describe '#full?' do
-    it 'checks if Airport is full' do
-      port = Airport.new
-      expect(port).to respond_to :full?
-    end
+  it 'sets user capacity when creating an airport' do
+    port = Airport.new(50)
+    expect(port.capacity).to eq 50
+  end
+
+  it 'capacity is equal to Default Capacity if no argument is given' do
+    expect(subject.capacity).to eq Airport::DEFAULT_CAPACITY
   end
 
 
-end
-
-describe Plane do
+  describe '#full?' do
+    it 'checks if Airport is full' do
+      port = Airport.new
+      expect(port.send(:full?)).to eq false
+    end
+  end
 
  describe '#land' do
       it 'lands the plane' do
         expect(subject).to respond_to :land
       end
+      it 'raises an error if hanger is full' do 
+        subject.capacity.times {subject.land(Plane.new)}
+        expect {subject.land Plane.new}.to raise_error 'Landing prevented; hanger is full'
+      end
+      it 'refuses a plane landing if weather is stormy' do
+        airport = Airport.new
+        plane = Plane.new
+        weather = airport.check_weather
+        if weather == 'STORMY' then expect {airport.land(plane)}.to raise_error 'Landing denied; storms overhead'
+        end
+        end
     end
+
+    it { is_expected.to respond_to(:land).with(1).argument }
+
 
   describe '#take_off' do
       it 'instructs plane to take off' do
         expect(subject).to respond_to :take_off
       end
+      it 'takes off correct plane passed as argument' do
+        airport = Airport.new
+        plane = Plane.new
+        airport.land(plane)
+        3.times {airport.land(Plane.new)}
+        expect(airport.take_off(plane)).to eq plane
+      end
+      it 'raises an error when hanger is empty' do
+        plane = Plane.new
+        expect {subject.take_off(plane)}.to raise_error 'No planes ready to take off'
+      end
+
+      it { is_expected.to respond_to(:take_off).with(1).argument }
+
+      it 'refuses a plane take off if weather is stormy' do
+        airport = Airport.new
+        plane = Plane.new
+        weather = airport.check_weather
+        if weather == 'STORMY' then expect {airport.take_off(plane)}.to raise_error 'Take off denied; storms incoming' 
+        end
+        end
+      end
+
+  describe '#check_hanger' do
+      it 'returns hanger' do
+        port = Airport.new
+        plane = Plane.new
+        port.land(plane)
+        expect(port.check_hanger).to be_truthy
+      end
     end
+
+    describe '#check_weather' do
+      it 'checks the weather' do
+        airport = Airport.new
+        expect(airport.check_weather).to eq('GOOD').or(eq('STORMY'))
+      end
+    end
+
+    # describe '#weather_randomizer' do
+    #   it 'returns a number from 1..20' do
+    #     expect(subject.send(:weather_randomizer)).to be_between(1,20)
+    #   end
+    # end
+
+  
+end
+
+describe Plane do
+
 end
