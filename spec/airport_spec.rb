0cd734733043b1f55d691655409a45f2f6e78ba7
Tom O'Neill <tomoneill32@gmail.com>
2022-04-25 09:58:27 +0100

    all user stories, still can't rspec stormy weather

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,6 +1,7 @@
 require 'airport'
 
 describe Airport do
+  
   describe '#land' do
     it 'Can do so' do
       plane = Plane.new
@@ -11,14 +12,26 @@ describe Airport do
       Airport::CAPACITY.times { subject.land(Plane.new) }
       expect { subject.land(Plane.new) }.to raise_error 'Airport full!'
     end
+
   end
   
-  it 'Can instruct a stored plane to take off from the airport' do
-    plane = Plane.new
-    subject.land(plane)
-    subject.depart(plane)
-    expect(subject.stored_planes).to eq []
-  end
+  describe '#depart' do
+    it 'Can instruct a stored plane to take off from the airport' do
+      plane = Plane.new
+      subject.land(plane)
+      subject.depart(plane)
+      expect(subject.stored_planes).to eq []
+    end
+
+    it 'Will not do so if there are no planes in the airport' do
+      expect { subject.depart(Plane.new) }.to raise_error 'No planes'
+    end
+
+    it 'Will not take off if the plane is not in the airport' do
+      subject.land(Plane.new)
+      expect{subject.depart(Plane.new)}.to raise_error 'Plane not in airport'
+    end
+  end 
 
   it 'can have an airport capacity that can be overwritten as appropriate' do
     subject.adjust_capacity(1)
@@ -30,5 +43,13 @@ describe Airport do
     Airport::CAPACITY.times { subject.land(Plane.new) }
     expect { subject.land(Plane.new) }.to raise_error 'Airport full!'
   end
+
+  describe 'initialize' do
+    it 'will be stormy weather and the plane will not land' do
+      allow(subject).to receive(:set_weather).and_return('Stormy')
+      expect { subject.land(Plane.new) }.to raise_error 'Cannot land - stormy weather!'
+    end
+  end
+
 
 end
