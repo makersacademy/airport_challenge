f08cc28c6943a3c0cf86dcf07652cbd6e2d5b58a
Jimmy Lyons <jimmylyons1@hotmail.co.uk>
2022-04-27 13:10:07 +0100

    Refactored RSpec tests with context

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,67 +2,69 @@ require 'airport'
 
 describe Airport do
   
+  subject(:airport) { described_class.new }
+  let(:plane) { Plane.new }
+
   it { is_expected.to be_kind_of Airport }
 
   describe '#land_plane' do
     it 'allows plane to land in airport' do   
-      subject.current_weather = 'sunny'
+      airport.current_weather = 'sunny'
       
-      expect(subject.land_plane).to be_kind_of Plane
+      expect(airport.land_plane).to be_kind_of Plane
     end
 
     it 'does not allow planes to land when airport is full' do
-      subject.current_weather = 'sunny'
-      subject.capacity.times { subject.land_plane }
+      airport.current_weather = 'sunny'
+      airport.capacity.times { airport.land_plane }
 
-      expect { subject.land_plane }.to raise_error('Airport is full')
+      expect { airport.land_plane }.to raise_error('Airport is full')
     end
 
     it 'does not allow plane to land in airport when stormy' do
-      subject.current_weather = 'stormy'
+      airport.current_weather = 'stormy'
 
-      expect { subject.land_plane }.to raise_error('Cannot fly when there is a storm')
+      expect { airport.land_plane }.to raise_error('Cannot fly when there is a storm')
     end
   end
 
   describe '#take_off' do
-    it 'allows plane to leave airport' do
-      subject.current_weather = 'sunny'
-      plane = Plane.new
-      subject.planes_in_airport << plane
+    context "when 'plane' is in 'planes_in_airport'" do
+      before(:each) { airport.planes_in_airport = [plane] } 
 
-      expect(subject.take_off(plane)).to eq plane
-    end
+      it 'does not let planes take off when stormy' do
+        airport.current_weather = 'stormy'
 
-    it 'removes plane from airport' do
-      subject.current_weather = 'sunny'
-      plane = Plane.new
-      subject.planes_in_airport = [plane]
-      subject.take_off(plane)
+        expect { airport.take_off(plane) }.to raise_error('Cannot fly when there is a storm')
+      end
+      
+      context 'when the weather is sunny' do
+        before(:each) { allow(airport).to receive(:check_for_storm).and_return(nil) }
 
-      expect(subject.planes_in_airport).to eq([])
-    end
+        it 'allows plane to leave airport' do
+          expect(airport.take_off(plane)).to eq plane
+        end
 
-    it 'does not let planes take off when stormy' do
-      plane = Plane.new
-      subject.planes_in_airport = [plane]
-      subject.current_weather = 'stormy'
+        it 'removes plane from airport' do
+          airport.take_off(plane)
 
-      expect { subject.take_off(plane) }.to raise_error('Cannot fly when there is a storm')
+          expect(airport.planes_in_airport).to eq([])
+        end
+      end
     end
   end
 
   describe '#def_capacity' do
     it 'allows you to change the capacity of the airport' do
-      subject.def_capacity(20)
+      airport.def_capacity(20)
       
-      expect(subject.capacity).to eq(20)
+      expect(airport.capacity).to eq(20)
     end
   end
 
   describe '#check_weather' do
     it 'reports what if the weather is sunny or stormy' do
-      expect(subject.check_weather).to eq('sunny').or eq('stormy')
+      expect(airport.check_weather).to eq('sunny').or eq('stormy')
     end
   end
 end
